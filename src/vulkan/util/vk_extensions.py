openharmony/third_party_mesa3d

commit 7f09749b64f90c662c93ac71c9f4960ac4b44c96

vulkan: Enable VK_KHR_performance_query on android
The tests of this extension seem to be added to Android 11 CTS,
since they can be found on android-11-x branches.

Signed-off-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7602>

src/vulkan/util/vk_extensions.py
@@ -149,6 +149,7 @@ allowed_android_version = {
     "VK_KHR_swapchain_mutable_format": 29,
     "VK_KHR_shader_atomic_int64": 29,
     "VK_KHR_vulkan_memory_model": 29,
+    "VK_KHR_performance_query": 30,
 
     "VK_GOOGLE_display_timing": 26,
     "VK_ANDROID_native_buffer": 26,

joshuaswarren/mesa-1

commit bc4fb96d064bcd84f147390c7441e648708b7b34

vulkan: disallow VK_KHR_performance_query on android
It turns out the extension was later blocked by
android.graphics.cts.VulkanFeaturesTest#testVulkanBlockedExtensions.

Fixes: 7f09749b64f ("vulkan: Enable VK_KHR_performance_query on android")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6991
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Tested-by: Roman Stratiienko <r.stratiienko@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17891>

src/vulkan/util/vk_extensions.py
@@ -133,7 +133,8 @@ def init_exts_from_xml(xml, extensions, platform_defines):
 
 # Mapping between extension name and the android version in which the extension
 # was whitelisted in Android CTS's dEQP-VK.info.device_extensions and
-# dEQP-VK.api.info.android.no_unknown_extensions.
+# dEQP-VK.api.info.android.no_unknown_extensions, excluding those blocked by
+# android.graphics.cts.VulkanFeaturesTest#testVulkanBlockedExtensions.
 ALLOWED_ANDROID_VERSION = {
     # checkInstanceExtensions on oreo-cts-release
     "VK_KHR_surface": 26,
@@ -212,7 +213,8 @@ ALLOWED_ANDROID_VERSION = {
     "VK_KHR_spirv_1_4": 30,
     "VK_KHR_pipeline_executable_properties": 30,
     "VK_KHR_shader_clock": 30,
-    "VK_KHR_performance_query": 30,
+    # blocked by testVulkanBlockedExtensions
+    # "VK_KHR_performance_query": 30,
     "VK_KHR_shader_non_semantic_info": 30,
     "VK_KHR_copy_commands2": 30,
     # on android12-tests-release