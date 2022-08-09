openharmony/third_party_mesa3d

commit 7f2022158b1e4da64eb759537abef2063601d9a5

anv: Switch to using common dynamic state tracking
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17564>

src/intel/vulkan/genX_cmd_buffer.c
@@ -1924,8 +1924,8 @@ genX(CmdExecuteCommands)(
    primary->state.current_pipeline = UINT32_MAX;
    primary->state.current_l3_config = NULL;
    primary->state.current_hash_scale = 0;
-   primary->state.gfx.dirty |= ANV_CMD_DIRTY_DYNAMIC_ALL;
    primary->state.gfx.push_constant_stages = 0;
+   vk_dynamic_graphics_state_dirty_all(&primary->vk.dynamic_graphics_state);
 
    /* Each of the secondary command buffers will use its own state base
     * address.  We need to re-emit state base address for the primary after
@@ -3413,36 +3413,31 @@ cmd_buffer_flush_mesh_inline_data(struct anv_cmd_buffer *cmd_buffer,
 static void
 cmd_buffer_emit_clip(struct anv_cmd_buffer *cmd_buffer)
 {
-   const uint32_t clip_states =
-#if GFX_VER <= 7
-      ANV_CMD_DIRTY_DYNAMIC_FRONT_FACE |
-      ANV_CMD_DIRTY_DYNAMIC_CULL_MODE |
-#endif
-      ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_TOPOLOGY |
-      ANV_CMD_DIRTY_DYNAMIC_VIEWPORT |
-      ANV_CMD_DIRTY_PIPELINE;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
 
-   if ((cmd_buffer->state.gfx.dirty & clip_states) == 0)
+   if (!(cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_PIPELINE) &&
+       !BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_IA_PRIMITIVE_TOPOLOGY) &&
+#if GFX_VER <= 7
+       !BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_RS_CULL_MODE) &&
+       !BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_RS_FRONT_FACE) &&
+#endif
+       !BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_VIEWPORT_COUNT))
       return;
 
    /* Take dynamic primitive topology in to account with
     *    3DSTATE_CLIP::ViewportXYClipTestEnable
     */
-   VkPrimitiveTopology primitive_topology =
-      cmd_buffer->state.gfx.dynamic.primitive_topology;
    VkPolygonMode dynamic_raster_mode =
       genX(raster_polygon_mode)(cmd_buffer->state.gfx.pipeline,
-                                primitive_topology);
+                                dyn->ia.primitive_topology);
    bool xy_clip_test_enable = (dynamic_raster_mode == VK_POLYGON_MODE_FILL);
 
-#if GFX_VER <= 7
-   const struct anv_dynamic_state *d = &cmd_buffer->state.gfx.dynamic;
-#endif
    struct GENX(3DSTATE_CLIP) clip = {
       GENX(3DSTATE_CLIP_header),
 #if GFX_VER <= 7
-      .FrontWinding = genX(vk_to_intel_front_face)[d->front_face],
-      .CullMode     = genX(vk_to_intel_cullmode)[d->cull_mode],
+      .FrontWinding = genX(vk_to_intel_front_face)[dyn->rs.front_face],
+      .CullMode     = genX(vk_to_intel_cullmode)[dyn->rs.cull_mode],
 #endif
       .ViewportXYClipTestEnable = xy_clip_test_enable,
    };
@@ -3454,15 +3449,14 @@ cmd_buffer_emit_clip(struct anv_cmd_buffer *cmd_buffer)
       const struct brw_vue_prog_data *last =
          anv_pipeline_get_last_vue_prog_data(pipeline);
       if (last->vue_map.slots_valid & VARYING_BIT_VIEWPORT) {
-         clip.MaximumVPIndex =
-            cmd_buffer->state.gfx.dynamic.viewport.count > 0 ?
-            cmd_buffer->state.gfx.dynamic.viewport.count - 1 : 0;
+         clip.MaximumVPIndex = dyn->vp.viewport_count > 0 ?
+                               dyn->vp.viewport_count - 1 : 0;
       }
    } else if (anv_pipeline_is_mesh(pipeline)) {
       const struct brw_mesh_prog_data *mesh_prog_data = get_mesh_prog_data(pipeline);
       if (mesh_prog_data->map.start_dw[VARYING_SLOT_VIEWPORT] >= 0) {
-         uint32_t viewport_count = cmd_buffer->state.gfx.dynamic.viewport.count;
-         clip.MaximumVPIndex = viewport_count > 0 ? viewport_count - 1 : 0;
+         clip.MaximumVPIndex = dyn->vp.viewport_count > 0 ?
+                               dyn->vp.viewport_count - 1 : 0;
       }
    }
 
@@ -3475,8 +3469,10 @@ static void
 cmd_buffer_emit_viewport(struct anv_cmd_buffer *cmd_buffer)
 {
    struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
-   uint32_t count = gfx->dynamic.viewport.count;
-   const VkViewport *viewports = gfx->dynamic.viewport.viewports;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
+   uint32_t count = dyn->vp.viewport_count;
+   const VkViewport *viewports = dyn->vp.viewports;
    struct anv_state sf_clip_state =
       anv_cmd_buffer_alloc_dynamic_state(cmd_buffer, count * 64, 64);
 
@@ -3538,8 +3534,8 @@ cmd_buffer_emit_viewport(struct anv_cmd_buffer *cmd_buffer)
        * It's theoretically possible that they could do all their clipping
        * with clip planes but that'd be a bit odd.
        */
-      if (i < gfx->dynamic.scissor.count) {
-         const VkRect2D *scissor = &gfx->dynamic.scissor.scissors[i];
+      if (i < dyn->vp.scissor_count) {
+         const VkRect2D *scissor = &dyn->vp.scissors[i];
          x_min = MAX2(x_min, scissor->offset.x);
          x_max = MIN2(x_min, scissor->offset.x + scissor->extent.width);
          y_min = MAX2(y_min, scissor->offset.y);
@@ -3576,9 +3572,10 @@ static void
 cmd_buffer_emit_depth_viewport(struct anv_cmd_buffer *cmd_buffer,
                                bool depth_clamp_enable)
 {
-   uint32_t count = cmd_buffer->state.gfx.dynamic.viewport.count;
-   const VkViewport *viewports =
-      cmd_buffer->state.gfx.dynamic.viewport.viewports;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
+   uint32_t count = dyn->vp.viewport_count;
+   const VkViewport *viewports = dyn->vp.viewports;
    struct anv_state cc_state =
       anv_cmd_buffer_alloc_dynamic_state(cmd_buffer, count * 8, 32);
 
@@ -3622,9 +3619,11 @@ static void
 cmd_buffer_emit_scissor(struct anv_cmd_buffer *cmd_buffer)
 {
    struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
-   uint32_t count = gfx->dynamic.scissor.count;
-   const VkRect2D *scissors = gfx->dynamic.scissor.scissors;
-   const VkViewport *viewports = gfx->dynamic.viewport.viewports;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
+   uint32_t count = dyn->vp.scissor_count;
+   const VkRect2D *scissors = dyn->vp.scissors;
+   const VkViewport *viewports = dyn->vp.viewports;
 
    /* Wa_1409725701:
     *    "The viewport-specific state used by the SF unit (SCISSOR_RECT) is
@@ -3697,7 +3696,8 @@ cmd_buffer_emit_scissor(struct anv_cmd_buffer *cmd_buffer)
 static void
 cmd_buffer_emit_streamout(struct anv_cmd_buffer *cmd_buffer)
 {
-   const struct anv_dynamic_state *d = &cmd_buffer->state.gfx.dynamic;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
    struct anv_graphics_pipeline *pipeline = cmd_buffer->state.gfx.pipeline;
 
 #if GFX_VER == 7
@@ -3710,7 +3710,7 @@ cmd_buffer_emit_streamout(struct anv_cmd_buffer *cmd_buffer)
 
    struct GENX(3DSTATE_STREAMOUT) so = {
       GENX(3DSTATE_STREAMOUT_header),
-      .RenderingDisable = d->raster_discard,
+      .RenderingDisable = dyn->rs.rasterizer_discard_enable,
    };
    GENX(3DSTATE_STREAMOUT_pack)(NULL, dwords, &so);
    anv_batch_emit_merge(&cmd_buffer->batch, dwords, streamout_state_dw);
@@ -3720,6 +3720,8 @@ void
 genX(cmd_buffer_flush_state)(struct anv_cmd_buffer *cmd_buffer)
 {
    struct anv_graphics_pipeline *pipeline = cmd_buffer->state.gfx.pipeline;
+   const struct vk_dynamic_graphics_state *dyn =
+      &cmd_buffer->vk.dynamic_graphics_state;
    uint32_t *p;
 
    assert((pipeline->active_stages & VK_SHADER_STAGE_COMPUTE_BIT) == 0);
@@ -3751,15 +3753,9 @@ genX(cmd_buffer_flush_state)(struct anv_cmd_buffer *cmd_buffer)
          struct anv_buffer *buffer = cmd_buffer->state.vertex_bindings[vb].buffer;
          uint32_t offset = cmd_buffer->state.vertex_bindings[vb].offset;
 
-         /* If dynamic, use stride/size from vertex binding, otherwise use
-          * stride/size that was setup in the pipeline object.
-          */
-         bool dynamic_stride = cmd_buffer->state.gfx.dynamic.dyn_vbo_stride;
-
          struct GENX(VERTEX_BUFFER_STATE) state;
          if (buffer) {
-            uint32_t stride = dynamic_stride ?
-               cmd_buffer->state.vertex_bindings[vb].stride : pipeline->vb[vb].stride;
+            uint32_t stride = dyn->vi_binding_strides[vb];
             UNUSED uint32_t size = cmd_buffer->state.vertex_bindings[vb].size;
 
 #if GFX_VER <= 7
@@ -3821,6 +3817,7 @@ genX(cmd_buffer_flush_state)(struct anv_cmd_buffer *cmd_buffer)
    uint32_t descriptors_dirty = cmd_buffer->state.descriptors_dirty &
                                 pipeline->active_stages;
    if (!cmd_buffer->state.gfx.dirty && !descriptors_dirty &&
+       !vk_dynamic_graphics_state_any_dirty(dyn) &&
        !cmd_buffer->state.push_constants_dirty)
       return;
 
@@ -3964,33 +3961,32 @@ genX(cmd_buffer_flush_state)(struct anv_cmd_buffer *cmd_buffer)
 
    cmd_buffer_emit_clip(cmd_buffer);
 
-   if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
-                                      ANV_CMD_DIRTY_DYNAMIC_RASTERIZER_DISCARD_ENABLE |
-                                      ANV_CMD_DIRTY_XFB_ENABLE))
+   if ((cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
+                                       ANV_CMD_DIRTY_XFB_ENABLE)) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_RS_RASTERIZER_DISCARD_ENABLE))
       cmd_buffer_emit_streamout(cmd_buffer);
 
-   if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_DYNAMIC_SCISSOR |
-                                      ANV_CMD_DIRTY_RENDER_TARGETS |
-                                      ANV_CMD_DIRTY_DYNAMIC_VIEWPORT |
-                                      ANV_CMD_DIRTY_PIPELINE)) {
+   if ((cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
+                                       ANV_CMD_DIRTY_RENDER_TARGETS)) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_VIEWPORTS) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_SCISSORS)) {
       cmd_buffer_emit_viewport(cmd_buffer);
       cmd_buffer_emit_depth_viewport(cmd_buffer,
                                      pipeline->depth_clamp_enable);
    }
 
-   if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_DYNAMIC_SCISSOR |
-                                      ANV_CMD_DIRTY_RENDER_TARGETS |
-                                      ANV_CMD_DIRTY_DYNAMIC_VIEWPORT))
+   if ((cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_RENDER_TARGETS) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_VIEWPORTS) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_SCISSORS))
       cmd_buffer_emit_scissor(cmd_buffer);
 
-   if (cmd_buffer->state.gfx.dirty & (ANV_CMD_DIRTY_PIPELINE |
-                                      ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_TOPOLOGY)) {
-      const struct anv_dynamic_state *d = &cmd_buffer->state.gfx.dynamic;
+   if ((cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_PIPELINE) ||
+       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_IA_PRIMITIVE_TOPOLOGY)) {
       uint32_t topology;
       if (anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL))
          topology = _3DPRIM_PATCHLIST(pipeline->patch_control_points);
       else
-         topology = genX(vk_to_intel_primitive_type)[d->primitive_topology];
+         topology = genX(vk_to_intel_primitive_type)[dyn->ia.primitive_topology];
 
       cmd_buffer->state.gfx.primitive_topology = topology;
 

src/intel/vulkan/anv_genX.h
@@ -133,14 +133,14 @@ genX(emit_urb_setup)(struct anv_device *device, struct anv_batch *batch,
                      enum intel_urb_deref_block_size *deref_block_size);
 
 void genX(emit_multisample)(struct anv_batch *batch, uint32_t samples,
-                            const struct intel_sample_position *positions);
+                            const struct vk_sample_locations_state *sl);
 
 void genX(emit_sample_pattern)(struct anv_batch *batch,
-                               const struct anv_dynamic_state *dynamic_state);
+                               const struct vk_sample_locations_state *sl);
 
 void genX(emit_shading_rate)(struct anv_batch *batch,
                              const struct anv_graphics_pipeline *pipeline,
-                             struct anv_dynamic_state *dynamic_state);
+                             const struct vk_fragment_shading_rate_state *fsr);
 
 void genX(cmd_buffer_so_memcpy)(struct anv_cmd_buffer *cmd_buffer,
                                 struct anv_address dst, struct anv_address src,

src/intel/vulkan/anv_pipeline.c
@@ -360,6 +360,7 @@ populate_gs_prog_key(const struct anv_device *device,
 
 static bool
 pipeline_has_coarse_pixel(const struct anv_graphics_pipeline *pipeline,
+                          const BITSET_WORD *dynamic,
                           const struct vk_multisample_state *ms,
                           const struct vk_fragment_shading_rate_state *fsr)
 {
@@ -396,14 +397,10 @@ pipeline_has_coarse_pixel(const struct anv_graphics_pipeline *pipeline,
    if (ms != NULL && ms->sample_shading_enable)
       return false;
 
-   /* Not dynamic & not specified for the pipeline. */
-   if ((pipeline->dynamic_states & ANV_CMD_DIRTY_DYNAMIC_SHADING_RATE) == 0 && fsr == NULL)
-      return false;
-
    /* Not dynamic & pipeline has a 1x1 fragment shading rate with no
     * possibility for element of the pipeline to change the value.
     */
-   if ((pipeline->dynamic_states & ANV_CMD_DIRTY_DYNAMIC_SHADING_RATE) == 0 &&
+   if (!BITSET_TEST(dynamic, MESA_VK_DYNAMIC_FSR) &&
        fsr->fragment_size.width <= 1 &&
        fsr->fragment_size.height <= 1 &&
        fsr->combiner_ops[0] == VK_FRAGMENT_SHADING_RATE_COMBINER_OP_KEEP_KHR &&
@@ -436,6 +433,7 @@ populate_mesh_prog_key(const struct anv_device *device,
 static void
 populate_wm_prog_key(const struct anv_graphics_pipeline *pipeline,
                      bool robust_buffer_acccess,
+                     const BITSET_WORD *dynamic,
                      const struct vk_multisample_state *ms,
                      const struct vk_fragment_shading_rate_state *fsr,
                      const struct vk_render_pass_state *rp,
@@ -489,7 +487,7 @@ populate_wm_prog_key(const struct anv_graphics_pipeline *pipeline,
    key->coarse_pixel =
       !key->persample_interp &&
       device->vk.enabled_extensions.KHR_fragment_shading_rate &&
-      pipeline_has_coarse_pixel(pipeline, ms, fsr);
+      pipeline_has_coarse_pixel(pipeline, dynamic, ms, fsr);
 }
 
 static void
@@ -1356,7 +1354,7 @@ anv_graphics_pipeline_init_keys(struct anv_graphics_pipeline *pipeline,
       case MESA_SHADER_FRAGMENT: {
          populate_wm_prog_key(pipeline,
                               pipeline->base.device->robust_buffer_access,
-                              state->ms, state->fsr, state->rp,
+                              state->dynamic, state->ms, state->fsr, state->rp,
                               &stages[s].key.wm);
          break;
       }
@@ -2032,195 +2030,6 @@ VkResult anv_CreateComputePipelines(
 }
 
 /**
- * Copy pipeline state not marked as dynamic.
- * Dynamic state is pipeline state which hasn't been provided at pipeline
- * creation time, but is dynamically provided afterwards using various
- * vkCmdSet* functions.
- *
- * The set of state considered "non_dynamic" is determined by the pieces of
- * state that have their corresponding VkDynamicState enums omitted from
- * VkPipelineDynamicStateCreateInfo::pDynamicStates.
- *
- * @param[out] pipeline    Destination non_dynamic state.
- * @param[in]  pCreateInfo Source of non_dynamic state to be copied.
- */
-static void
-copy_non_dynamic_state(struct anv_graphics_pipeline *pipeline,
-                       const struct vk_graphics_pipeline_state *state)
-{
-   anv_cmd_dirty_mask_t states = ANV_CMD_DIRTY_DYNAMIC_ALL;
-
-   anv_dynamic_state_init(&pipeline->non_dynamic_state);
-
-   states &= ~pipeline->dynamic_states;
-
-   struct anv_dynamic_state *dynamic = &pipeline->non_dynamic_state;
-
-   /* Section 9.2 of the Vulkan 1.0.15 spec says:
-    *
-    *    pViewportState is [...] NULL if the pipeline
-    *    has rasterization disabled.
-    */
-   if (state->vp) {
-      pipeline->negative_one_to_one = state->vp->negative_one_to_one;
-
-      dynamic->viewport.count = state->vp->viewport_count;
-      if (states & ANV_CMD_DIRTY_DYNAMIC_VIEWPORT) {
-         typed_memcpy(dynamic->viewport.viewports,
-                      state->vp->viewports, state->vp->viewport_count);
-      }
-
-      dynamic->scissor.count = state->vp->scissor_count;
-      if (states & ANV_CMD_DIRTY_DYNAMIC_SCISSOR) {
-         typed_memcpy(dynamic->scissor.scissors,
-                      state->vp->scissors, state->vp->scissor_count);
-      }
-   }
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_LINE_WIDTH)
-      dynamic->line_width = state->rs->line.width;
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_BIAS) {
-      dynamic->depth_bias.bias = state->rs->depth_bias.constant;
-      dynamic->depth_bias.clamp = state->rs->depth_bias.clamp;
-      dynamic->depth_bias.slope = state->rs->depth_bias.slope;
-   }
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_CULL_MODE)
-      dynamic->cull_mode = state->rs->cull_mode;
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_FRONT_FACE)
-      dynamic->front_face = state->rs->front_face;
-
-   if ((states & ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_TOPOLOGY) &&
-       (pipeline->active_stages & VK_SHADER_STAGE_VERTEX_BIT))
-      dynamic->primitive_topology = state->ia->primitive_topology;
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_RASTERIZER_DISCARD_ENABLE)
-      dynamic->raster_discard = state->rs->rasterizer_discard_enable;
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_BIAS_ENABLE)
-      dynamic->depth_bias_enable = state->rs->depth_bias.enable;
-
-   if ((states & ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_RESTART_ENABLE) &&
-       (pipeline->active_stages & VK_SHADER_STAGE_VERTEX_BIT))
-      dynamic->primitive_restart_enable = state->ia->primitive_restart_enable;
-
-   if (state->cb != NULL && (states & ANV_CMD_DIRTY_DYNAMIC_BLEND_CONSTANTS))
-      typed_memcpy(dynamic->blend_constants, state->cb->blend_constants, 4);
-
-   if (state->ds != NULL) {
-      if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_BOUNDS) {
-         dynamic->depth_bounds.min = state->ds->depth.bounds_test.min;
-         dynamic->depth_bounds.max = state->ds->depth.bounds_test.max;
-      }
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_STENCIL_COMPARE_MASK) {
-         dynamic->stencil_compare_mask.front =
-            state->ds->stencil.front.compare_mask;
-         dynamic->stencil_compare_mask.back =
-            state->ds->stencil.back.compare_mask;
-      }
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_STENCIL_WRITE_MASK) {
-         dynamic->stencil_write_mask.front =
-            state->ds->stencil.front.write_mask;
-         dynamic->stencil_write_mask.back =
-            state->ds->stencil.back.write_mask;
-      }
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_STENCIL_REFERENCE) {
-         dynamic->stencil_reference.front =
-            state->ds->stencil.front.reference;
-         dynamic->stencil_reference.back =
-            state->ds->stencil.back.reference;
-      }
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_TEST_ENABLE)
-         dynamic->depth_test_enable = state->ds->depth.test_enable;
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_WRITE_ENABLE)
-         dynamic->depth_write_enable = state->ds->depth.write_enable;
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_COMPARE_OP)
-         dynamic->depth_compare_op = state->ds->depth.compare_op;
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_DEPTH_BOUNDS_TEST_ENABLE) {
-         dynamic->depth_bounds_test_enable =
-            state->ds->depth.bounds_test.enable;
-      }
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_STENCIL_TEST_ENABLE)
-         dynamic->stencil_test_enable = state->ds->stencil.test_enable;
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_STENCIL_OP) {
-         dynamic->stencil_op.front.fail_op = state->ds->stencil.front.op.fail;
-         dynamic->stencil_op.front.pass_op = state->ds->stencil.front.op.pass;
-         dynamic->stencil_op.front.depth_fail_op = state->ds->stencil.front.op.depth_fail;
-         dynamic->stencil_op.front.compare_op = state->ds->stencil.front.op.compare;
-
-         dynamic->stencil_op.back.fail_op = state->ds->stencil.back.op.fail;
-         dynamic->stencil_op.back.pass_op = state->ds->stencil.back.op.pass;
-         dynamic->stencil_op.back.depth_fail_op = state->ds->stencil.back.op.depth_fail;
-         dynamic->stencil_op.back.compare_op = state->ds->stencil.back.op.compare;
-      }
-   }
-
-   if (state->rs != NULL) {
-      if (states & ANV_CMD_DIRTY_DYNAMIC_LINE_STIPPLE) {
-         dynamic->line_stipple.factor = state->rs->line.stipple.factor;
-         dynamic->line_stipple.pattern = state->rs->line.stipple.pattern;
-      }
-   }
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS) {
-      if (state->ms != NULL) {
-         uint32_t samples = state->ms->rasterization_samples;
-         for (uint32_t i = 0; i < samples; i++) {
-            dynamic->sample_locations.locations[i].x =
-               state->ms->sample_locations->locations[i].x;
-            dynamic->sample_locations.locations[i].y =
-               state->ms->sample_locations->locations[i].y;
-         }
-         dynamic->sample_locations.samples = samples;
-      } else {
-         dynamic->sample_locations.samples = 1;
-         dynamic->sample_locations.locations[0].x = 0.5;
-         dynamic->sample_locations.locations[0].y = 0.5;
-      }
-   }
-
-   if (state->cb != NULL) {
-      if (states & ANV_CMD_DIRTY_DYNAMIC_COLOR_BLEND_STATE)
-         dynamic->color_writes = state->cb->color_write_enables;
-
-      if (states & ANV_CMD_DIRTY_DYNAMIC_LOGIC_OP)
-         dynamic->logic_op = state->cb->logic_op;
-   }
-
-   if (states & ANV_CMD_DIRTY_DYNAMIC_SHADING_RATE) {
-      dynamic->fragment_shading_rate.rate = state->fsr->fragment_size;
-      memcpy(dynamic->fragment_shading_rate.ops, state->fsr->combiner_ops,
-             sizeof(dynamic->fragment_shading_rate.ops));
-   }
-
-   /* When binding a mesh pipeline into a command buffer, it should not affect the
-    * pre-rasterization bits of legacy graphics pipelines. So remove all the
-    * pre-rasterization flags from the non-dynamic bits from the mesh pipelines
-    * here so we don't copy any of that stuff when binding those into a command
-    * buffer.
-    */
-   if (pipeline->active_stages & VK_SHADER_STAGE_MESH_BIT_NV) {
-      states &= ~(ANV_CMD_DIRTY_DYNAMIC_VERTEX_INPUT_BINDING_STRIDE |
-                  ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_RESTART_ENABLE |
-                  ANV_CMD_DIRTY_DYNAMIC_PRIMITIVE_TOPOLOGY);
-   }
-
-
-   pipeline->non_dynamic_state_mask = states;
-}
-
-/**
  * Calculate the desired L3 partitioning based on the current state of the
  * pipeline.  For now this simply returns the conservative defaults calculated
  * by get_default_l3_weights(), but we could probably do better by gathering
@@ -2257,13 +2066,6 @@ anv_graphics_pipeline_init(struct anv_graphics_pipeline *pipeline,
    anv_batch_set_storage(&pipeline->base.batch, ANV_NULL_ADDRESS,
                          pipeline->batch_data, sizeof(pipeline->batch_data));
 
-   enum mesa_vk_dynamic_graphics_state s;
-   BITSET_FOREACH_SET(s, state->dynamic,
-                      MESA_VK_DYNAMIC_GRAPHICS_STATE_ENUM_MAX) {
-      pipeline->dynamic_states |=
-         anv_cmd_dirty_bit_for_mesa_vk_dynamic_graphics_state(s);
-   }
-
    pipeline->active_stages = 0;
    for (uint32_t i = 0; i < pCreateInfo->stageCount; i++)
       pipeline->active_stages |= pCreateInfo->pStages[i].stage;
@@ -2274,7 +2076,8 @@ anv_graphics_pipeline_init(struct anv_graphics_pipeline *pipeline,
    if (anv_pipeline_is_mesh(pipeline))
       assert(device->physical->vk.supported_extensions.NV_mesh_shader);
 
-   copy_non_dynamic_state(pipeline, state);
+   pipeline->dynamic_state.ms.sample_locations = &pipeline->sample_locations;
+   vk_dynamic_graphics_state_fill(&pipeline->dynamic_state, state);
 
    pipeline->depth_clamp_enable = state->rs->depth_clamp_enable;
    pipeline->depth_clip_enable = state->rs->depth_clip_enable;
@@ -2315,6 +2118,9 @@ anv_graphics_pipeline_init(struct anv_graphics_pipeline *pipeline,
       assert(anv_pipeline_is_mesh(pipeline));
       /* TODO(mesh): Mesh vs. Multiview with Instancing. */
    }
+
+   pipeline->negative_one_to_one =
+      state->vp != NULL && state->vp->negative_one_to_one;
 
    /* Store line mode, polygon mode and rasterization samples, these are used
     * for dynamic primitive topology.

joshuaswarren/mesa-1

commit c35c36beb82995188a3f976c20bbab53a536c1e5

anv: emit scissors when the pipeline changes
With the switch to common dynamic state tracking, something got lost
that made the scissors not always be emitted when they are not dynamic
and the pipeline is marked dirty.

Since both viewport and scissors make use of each other to calculate
their values, just stick the scissor emit in the same if block as
viewport for now.
I'd rather have them decoupled, and at least the Vulkan CTS didn't
complain when I tried it, but I don't know what other effects that
may have, especially when it comes to the guardband.

Fixes a bunch of tests under
dEQP-VK.pipeline.*.multisample.misc.*

Fixes: 7f2022158b1 ("anv: Switch to using common dynamic state tracking")

Reviewed-by: Jason Ekstrand <jason.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17964>

src/intel/vulkan/genX_cmd_buffer.c
@@ -4026,12 +4026,8 @@ genX(cmd_buffer_flush_state)(struct anv_cmd_buffer *cmd_buffer)
       cmd_buffer_emit_viewport(cmd_buffer);
       cmd_buffer_emit_depth_viewport(cmd_buffer,
                                      pipeline->depth_clamp_enable);
-   }
-
-   if ((cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_RENDER_TARGETS) ||
-       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_VIEWPORTS) ||
-       BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VP_SCISSORS))
       cmd_buffer_emit_scissor(cmd_buffer);
+   }
 
    if ((cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_PIPELINE) ||
        BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_IA_PRIMITIVE_TOPOLOGY)) {