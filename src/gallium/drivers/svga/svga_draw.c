joshuaswarren/mesa-1

commit fdcf3523a5939c960ddfc2fcad54132dd6296cfd

svga: compare shader type against compute not tess eval.
To get all graphics stages compare < COMPUTE not <= TESS_EVAL
should make it more robust against reordering.

Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17747>

src/gallium/drivers/svga/svga_state_sampler.c
@@ -245,7 +245,7 @@ update_sampler_resources(struct svga_context *svga, uint64_t dirty)
 
    assert(svga_have_vgpu10(svga));
 
-   for (shader = PIPE_SHADER_VERTEX; shader <= PIPE_SHADER_TESS_EVAL; shader++) {
+   for (shader = PIPE_SHADER_VERTEX; shader < PIPE_SHADER_COMPUTE; shader++) {
       SVGA3dShaderResourceViewId ids[PIPE_MAX_SAMPLERS];
       struct svga_winsys_surface *surfaces[PIPE_MAX_SAMPLERS];
       struct pipe_sampler_view *sampler_views[PIPE_MAX_SAMPLERS];
@@ -396,7 +396,7 @@ update_samplers(struct svga_context *svga, uint64_t dirty )
 
    assert(svga_have_vgpu10(svga));
 
-   for (shader = PIPE_SHADER_VERTEX; shader <= PIPE_SHADER_TESS_EVAL; shader++) {
+   for (shader = PIPE_SHADER_VERTEX; shader <= PIPE_SHADER_COMPUTE; shader++) {
       const unsigned count = svga->curr.num_samplers[shader];
       SVGA3dSamplerId ids[PIPE_MAX_SAMPLERS*2];
       unsigned i;

src/gallium/drivers/svga/svga_state_constants.c
@@ -1480,7 +1480,7 @@ update_rawbuf(struct svga_context *svga, uint64 dirty)
    };
 
    for (enum pipe_shader_type shader = PIPE_SHADER_VERTEX;
-        shader <= PIPE_SHADER_TESS_EVAL; shader++) {
+        shader < PIPE_SHADER_COMPUTE; shader++) {
       unsigned rawbuf_mask = svga->state.raw_constbufs[shader];
 
       update_rawbuf_mask(svga, shader);

commit 772ce7009acf623c227939cdb2b6e666b249781f

svga: compare graphics shader stages against COMPUTE
This patch fixes the graphics shader stage comparisons that are left out in
commit fdcf3523a59.

Fixes: fdcf3523a59 ("svga: compare shader type against compute not tess eval")

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18004>

src/gallium/drivers/svga/svga_draw.c
@@ -357,15 +357,15 @@ svga_validate_sampler_resources(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    }
    else {
       assert(svga_have_gl43(svga));
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader+1;
    }
 
-   for (shader = first_shader; shader <= last_shader; shader++) {
+   for (shader = first_shader; shader < last_shader; shader++) {
       unsigned count = svga->curr.num_sampler_views[shader];
       unsigned i;
       struct svga_winsys_surface *surfaces[PIPE_MAX_SAMPLERS];
@@ -440,15 +440,15 @@ svga_validate_constant_buffers(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    }
    else {
       assert(svga_have_gl43(svga));
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader + 1;
    }
 
-   for (shader = first_shader; shader <= last_shader; shader++) {
+   for (shader = first_shader; shader < last_shader; shader++) {
 
       enum pipe_error ret;
       struct svga_buffer *buffer;
@@ -542,14 +542,14 @@ svga_validate_image_views(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    }
    else {
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader + 1;
    }
 
-   for (shader = first_shader; shader <= last_shader; shader++) {
+   for (shader = first_shader; shader < last_shader; shader++) {
       ret = svga_validate_image_view_resources(svga,
                svga->state.hw_draw.num_image_views[shader],
                &svga->state.hw_draw.image_views[shader][0], rebind);
@@ -581,14 +581,14 @@ svga_validate_shader_buffers(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    }
    else {
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader + 1;
    }
 
-   for (shader = first_shader; shader <= last_shader; shader++) {
+   for (shader = first_shader; shader < last_shader; shader++) {
       ret = svga_validate_shader_buffer_resources(svga,
                svga->state.hw_draw.num_shader_buffers[shader],
                &svga->state.hw_draw.shader_buffers[shader][0], rebind);

src/gallium/drivers/svga/svga_state_sampler.c
@@ -396,7 +396,7 @@ update_samplers(struct svga_context *svga, uint64_t dirty )
 
    assert(svga_have_vgpu10(svga));
 
-   for (shader = PIPE_SHADER_VERTEX; shader <= PIPE_SHADER_COMPUTE; shader++) {
+   for (shader = PIPE_SHADER_VERTEX; shader < PIPE_SHADER_COMPUTE; shader++) {
       const unsigned count = svga->curr.num_samplers[shader];
       SVGA3dSamplerId ids[PIPE_MAX_SAMPLERS*2];
       unsigned i;

src/gallium/drivers/svga/svga_state_uav.c
@@ -363,14 +363,14 @@ svga_create_uav_list(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    } else {
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader + 1;
    }
 
    for (enum pipe_shader_type shader = first_shader;
-        shader <= last_shader; shader++) {
+        shader < last_shader; shader++) {
 
       unsigned num_image_views = svga->curr.num_image_views[shader];
       unsigned num_shader_buffers = svga->curr.num_shader_buffers[shader];
@@ -600,14 +600,14 @@ svga_save_uav_state(struct svga_context *svga,
 
    if (pipe_type == SVGA_PIPE_GRAPHICS) {
       first_shader = PIPE_SHADER_VERTEX;
-      last_shader = PIPE_SHADER_TESS_EVAL;
+      last_shader = PIPE_SHADER_COMPUTE;
    } else {
       first_shader = PIPE_SHADER_COMPUTE;
-      last_shader = PIPE_SHADER_COMPUTE;
+      last_shader = first_shader + 1;
    }
 
    for (enum pipe_shader_type shader = first_shader;
-        shader <= last_shader; shader++) {
+        shader < last_shader; shader++) {
 
       /**
        * Save the current shader images