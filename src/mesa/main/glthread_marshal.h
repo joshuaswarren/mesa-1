openharmony/third_party_mesa3d

commit 2d8faa428fc130875f9af42252ad1b7ca826ab84

glthread: don't sync for more glGetIntegerv enums for glretrace
This makes glretrace faster with glthread.

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13970>

src/mapi/glapi/gen/ARB_framebuffer_object.xml
@@ -202,7 +202,8 @@
 	<glx vendorpriv="1425"/>
     </function>
 
-    <function name="BindFramebuffer" es2="2.0">
+    <function name="BindFramebuffer" es2="2.0"
+              marshal_call_after="if (target == GL_FRAMEBUFFER || target == GL_DRAW_FRAMEBUFFER) ctx->GLThread.CurrentDrawFramebuffer = framebuffer;">
         <param name="target" type="GLenum"/>
         <param name="framebuffer" type="GLuint"/>
         <glx rop="236"/>

src/mapi/glapi/gen/gl_API.xml
@@ -5563,7 +5563,8 @@
         <glx ignore="true"/>
     </function>
 
-    <function name="UseProgram" es2="2.0" no_error="true">
+    <function name="UseProgram" es2="2.0" no_error="true"
+              marshal_call_after="ctx->GLThread.CurrentProgram = program;">
         <param name="program" type="GLuint"/>
         <glx ignore="true"/>
     </function>

src/mesa/main/glthread.h
@@ -207,6 +207,7 @@ struct glthread_state
    GLuint CurrentDrawIndirectBufferName;
    GLuint CurrentPixelPackBufferName;
    GLuint CurrentPixelUnpackBufferName;
+   GLuint CurrentQueryBufferName;
 
    /**
     * The batch index of the last occurence of glLinkProgram or
@@ -230,6 +231,9 @@ struct glthread_state
 
    /** Enable states. */
    bool CullFace;
+
+   GLuint CurrentDrawFramebuffer;
+   GLuint CurrentProgram;
 };
 
 void _mesa_glthread_init(struct gl_context *ctx);

src/mesa/main/glthread_bufferobj.c
@@ -195,6 +195,9 @@ _mesa_glthread_BindBuffer(struct gl_context *ctx, GLenum target, GLuint buffer)
    case GL_PIXEL_UNPACK_BUFFER:
       glthread->CurrentPixelUnpackBufferName = buffer;
       break;
+   case GL_QUERY_BUFFER:
+      glthread->CurrentQueryBufferName = buffer;
+      break;
    }
 }
 

src/mesa/main/glthread_get.c
@@ -43,13 +43,6 @@ _mesa_marshal_GetIntegerv(GLenum pname, GLint *p)
     * - CONTEXT_[A-Z]*(Const
     */
 
-   if (ctx->API != API_OPENGL_COMPAT) {
-      /* glthread only tracks these states for the compatibility profile. */
-      _mesa_glthread_finish_before(ctx, "GetIntegerv");
-      CALL_GetIntegerv(ctx->CurrentServerDispatch, (pname, p));
-      return;
-   }
-
    switch (pname) {
    case GL_ACTIVE_TEXTURE:
       *p = GL_TEXTURE0 + ctx->GLThread.ActiveTexture;
@@ -66,8 +59,23 @@ _mesa_marshal_GetIntegerv(GLenum pname, GLint *p)
    case GL_CLIENT_ATTRIB_STACK_DEPTH:
       *p = ctx->GLThread.ClientAttribStackTop;
       return;
+   case GL_CURRENT_PROGRAM:
+      *p = ctx->GLThread.CurrentProgram;
+      return;
    case GL_DRAW_INDIRECT_BUFFER_BINDING:
       *p = ctx->GLThread.CurrentDrawIndirectBufferName;
+      return;
+   case GL_DRAW_FRAMEBUFFER_BINDING: /* == GL_FRAMEBUFFER_BINDING */
+      *p = ctx->GLThread.CurrentDrawFramebuffer;
+      return;
+   case GL_PIXEL_PACK_BUFFER_BINDING:
+      *p = ctx->GLThread.CurrentPixelPackBufferName;
+      return;
+   case GL_PIXEL_UNPACK_BUFFER_BINDING:
+      *p = ctx->GLThread.CurrentPixelUnpackBufferName;
+      return;
+   case GL_QUERY_BUFFER_BINDING:
+      *p = ctx->GLThread.CurrentQueryBufferName;
       return;
 
    case GL_MATRIX_MODE:

joshuaswarren/mesa-1

commit 53df0167cc69f84115e8fe119a063ec1fcfd8fae

glthread: unbind framebuffers in glDeleteFramebuffers
Tests:
    dEQP-GLES2.functional.lifetime.delete_bound.framebuffer
    dEQP-GLES2.functional.state_query.integers.framebuffer_binding_getinteger

Fixes: 2d8faa428fc - glthread: don't sync for more glGetIntegerv enums for glretrace

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17976>

src/mapi/glapi/gen/ARB_framebuffer_object.xml
@@ -209,7 +209,8 @@
         <glx rop="236"/>
     </function>
 
-    <function name="DeleteFramebuffers" es2="2.0">
+    <function name="DeleteFramebuffers" es2="2.0"
+              marshal_call_after="_mesa_glthread_DeleteFramebuffers(ctx, n, framebuffers);">
         <param name="n" type="GLsizei" counter="true"/>
         <param name="framebuffers" type="const GLuint *" count="n"/>
 	<glx rop="4320"/>

src/mesa/main/glthread_marshal.h
@@ -779,6 +779,20 @@ _mesa_glthread_DeleteLists(struct gl_context *ctx, GLsizei range)
    _mesa_glthread_flush_batch(ctx);
 }
 
+static inline void
+_mesa_glthread_DeleteFramebuffers(struct gl_context *ctx, GLsizei n,
+                                  const GLuint *ids)
+{
+   if (ctx->GLThread.CurrentDrawFramebuffer) {
+      for (int i = 0; i < n; i++) {
+         if (ctx->GLThread.CurrentDrawFramebuffer == ids[i]) {
+            ctx->GLThread.CurrentDrawFramebuffer = 0;
+            break;
+         }
+      }
+   }
+}
+
 struct marshal_cmd_CallList
 {
    struct marshal_cmd_base cmd_base;