openharmony/third_party_mesa3d

commit 57b880c6d71f6292463c84479450bcefe312634d

intel/dev: Add device info for ADL-S
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7322>

include/pci_ids/iris_pci_ids.h
@@ -4,6 +4,17 @@ CHIPSET(0x4c8c, rkl_gt05, "RKL GT0.5", "Intel(R) Graphics")
 CHIPSET(0x4c90, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 CHIPSET(0x4c9a, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 
+CHIPSET(0x4680, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4681, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4682, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4683, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
+CHIPSET(0x4690, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4691, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4692, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4693, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
+CHIPSET(0x4698, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4699, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+
 CHIPSET(0x9A40, tgl_gt2, "TGL GT2", "Intel(R) Xe Graphics")
 CHIPSET(0x9A49, tgl_gt2, "TGL GT2", "Intel(R) Xe Graphics")
 CHIPSET(0x9A59, tgl_gt2, "TGL GT2", "Intel(R) Graphics")

src/intel/dev/gen_device_info.c
@@ -63,6 +63,7 @@ static const struct {
    { "tgl", 0x9a49 },
    { "rkl", 0x4c8a },
    { "dg1", 0x4905 },
+   { "adl", 0x4680 },
 };
 
 /**
@@ -956,6 +957,14 @@ static const struct gen_device_info gen_device_info_rkl_gt05 = {
 };
 
 static const struct gen_device_info gen_device_info_rkl_gt1 = {
+   GEN12_GT_FEATURES(1),
+};
+
+static const struct gen_device_info gen_device_info_adl_gt05 = {
+   GEN12_GT05_FEATURES,
+};
+
+static const struct gen_device_info gen_device_info_adl_gt1 = {
    GEN12_GT_FEATURES(1),
 };
 

joshuaswarren/mesa-1

commit 89add14c92aa3c80860b416b73083cfb685ef3f5

intel/pci_ids: Update ADL-S strings
Ref: bspec 53655
Fixes: 57b880c6d71 ("intel/dev: Add device info for ADL-S")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17569>

include/pci_ids/iris_pci_ids.h
@@ -156,13 +156,13 @@ CHIPSET(0x4c8c, rkl_gt05, "RKL GT0.5", "Intel(R) Graphics")
 CHIPSET(0x4c90, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 CHIPSET(0x4c9a, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 
-CHIPSET(0x4680, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4682, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4688, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x468a, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4690, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4692, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4693, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
+CHIPSET(0x4680, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics 770")
+CHIPSET(0x4682, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics 730")
+CHIPSET(0x4688, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics")
+CHIPSET(0x468a, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics")
+CHIPSET(0x4690, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics 770")
+CHIPSET(0x4692, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics 730")
+CHIPSET(0x4693, adl_gt1, "ADL-S GT1", "Intel(R) UHD Graphics 710")
 
 CHIPSET(0x4626, adl_gt2, "ADL GT2", "Intel(R) Graphics")
 CHIPSET(0x4628, adl_gt2, "ADL GT2", "Intel(R) Graphics")