openharmony/third_party_mesa3d

commit fbc75e8e689be87598531aa8b190b435f7b71d63

frontends/va: WA for ffmpeg 10bit encoding crash
When doing 10bit encoding in ffmpeg it uses
VaDeriveImage, and that could result in missing
mapping the chroma buffer of the input frame.

This WA to disallow ffmpeg using VaDeriveImage
function, so that VaCreateImage and VaPutImage can
be used and WA the chroma buffer mapping issue.

Reviewed-by: Leo Liu <leo.liu@amd.com>
Signed-off-by: Ruijing Dong <ruijing.dong@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17472>

src/gallium/frontends/va/image.c
@@ -223,6 +223,10 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
          "hevcencode"
    };
 
+   const char *derive_progressive_disallowlist[] = {
+         "ffmpeg"
+   };
+
    if (!ctx)
       return VA_STATUS_ERROR_INVALID_CONTEXT;
 
@@ -251,6 +255,10 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
                                    PIPE_VIDEO_ENTRYPOINT_BITSTREAM,
                                    PIPE_VIDEO_CAP_SUPPORTS_PROGRESSIVE))
          return VA_STATUS_ERROR_OPERATION_FAILED;
+   } else {
+      for (i = 0; i < ARRAY_SIZE(derive_progressive_disallowlist); i++)
+         if ((strcmp(derive_progressive_disallowlist[i], proc) == 0))
+            return VA_STATUS_ERROR_OPERATION_FAILED;
    }
 
    surfaces = surf->buffer->get_surfaces(surf->buffer);

joshuaswarren/mesa-1

commit bac8703e47c1e7c92fb38ecd04fd6d3bf8c1d4a5

gallium/va: vaDeriveImage to check PIPE_VIDEO_SUPPORTS_CONTIGUOUS_PLANES_MAP
vaDeriveImage should check if the underlying gallium driver can map contiguous planes
before skipping with disallowlist.

Fixes: fbc75e8e689be87598531aa8b190b435f7b71d63 ("radeonsi/vcn: WA 10bit encoding crash in vaapi")

Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18300>

src/gallium/frontends/va/image.c
@@ -256,9 +256,12 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
                                    PIPE_VIDEO_CAP_SUPPORTS_PROGRESSIVE))
          return VA_STATUS_ERROR_OPERATION_FAILED;
    } else {
-      for (i = 0; i < ARRAY_SIZE(derive_progressive_disallowlist); i++)
-         if ((strcmp(derive_progressive_disallowlist[i], proc) == 0))
-            return VA_STATUS_ERROR_OPERATION_FAILED;
+         if(!screen->get_video_param(screen, PIPE_VIDEO_PROFILE_UNKNOWN,
+                                   PIPE_VIDEO_ENTRYPOINT_BITSTREAM,
+                                   PIPE_VIDEO_SUPPORTS_CONTIGUOUS_PLANES_MAP))
+            for (i = 0; i < ARRAY_SIZE(derive_progressive_disallowlist); i++)
+               if ((strcmp(derive_progressive_disallowlist[i], proc) == 0))
+                  return VA_STATUS_ERROR_OPERATION_FAILED;
    }
 
    surfaces = surf->buffer->get_surfaces(surf->buffer);

src/gallium/include/pipe/p_video_enums.h
@@ -106,6 +106,10 @@ enum pipe_video_cap
    PIPE_VIDEO_CAP_VPP_MIN_OUTPUT_WIDTH = 24,
    PIPE_VIDEO_CAP_VPP_MIN_OUTPUT_HEIGHT = 25,
    PIPE_VIDEO_CAP_ENC_QUALITY_LEVEL = 26,
+   /* If true, when mapping planar textures like NV12 or P016 the mapped buffer contains
+   all the planes contiguously. This allows for use with some frontends functions that
+   require this like vaDeriveImage */
+   PIPE_VIDEO_SUPPORTS_CONTIGUOUS_PLANES_MAP = 27,
 };
 
 /* To be used with PIPE_VIDEO_CAP_VPP_ORIENTATION_MODES and for VPP state*/