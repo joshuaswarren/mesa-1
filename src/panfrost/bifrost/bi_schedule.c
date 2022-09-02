openharmony/third_party_mesa3d

commit d3dc82bd9b84bfb9eb920162ea91832fd5272e1f

pan/bi: Enable dual texture fusing pass
Everything is in place for it now -- ship it! Our Bifrost cycle model is
coarse, so take the shader-db results with a massive spoonful of salt:

total instructions in shared programs: 107504 -> 107252 (-0.23%)
instructions in affected programs: 39692 -> 39440 (-0.63%)
helped: 191
HURT: 1
helped stats (abs) min: 1.0 max: 20.0 x̄: 1.32 x̃: 1
helped stats (rel) min: 0.11% max: 9.52% x̄: 1.21% x̃: 0.98%
HURT stats (abs)   min: 1.0 max: 1.0 x̄: 1.00 x̃: 1
HURT stats (rel)   min: 2.04% max: 2.04% x̄: 2.04% x̃: 2.04%
95% mean confidence interval for instructions value: -1.60 -1.02
95% mean confidence interval for instructions %-change: -1.37% -1.01%
Instructions are helped.

total tuples in shared programs: 89864 -> 89664 (-0.22%)
tuples in affected programs: 27787 -> 27587 (-0.72%)
helped: 146
HURT: 6
helped stats (abs) min: 1.0 max: 17.0 x̄: 1.54 x̃: 1
helped stats (rel) min: 0.14% max: 15.38% x̄: 1.83% x̃: 1.25%
HURT stats (abs)   min: 1.0 max: 11.0 x̄: 4.17 x̃: 2
HURT stats (rel)   min: 0.54% max: 3.55% x̄: 1.29% x̃: 0.87%
95% mean confidence interval for tuples value: -1.64 -0.99
95% mean confidence interval for tuples %-change: -2.06% -1.36%
Tuples are helped.

total clauses in shared programs: 18253 -> 18044 (-1.15%)
clauses in affected programs: 5127 -> 4918 (-4.08%)
helped: 164
HURT: 1
helped stats (abs) min: 1.0 max: 19.0 x̄: 1.28 x̃: 1
helped stats (rel) min: 0.78% max: 28.57% x̄: 6.73% x̃: 5.88%
HURT stats (abs)   min: 1.0 max: 1.0 x̄: 1.00 x̃: 1
HURT stats (rel)   min: 2.94% max: 2.94% x̄: 2.94% x̃: 2.94%
95% mean confidence interval for clauses value: -1.50 -1.04
95% mean confidence interval for clauses %-change: -7.42% -5.91%
Clauses are helped.

total cycles in shared programs: 8118.54 -> 8103.88 (-0.18%)
cycles in affected programs: 414.96 -> 400.29 (-3.53%)
helped: 43
HURT: 27
helped stats (abs) min: 0.041665999999999315 max: 4.375 x̄: 0.41 x̃: 0
helped stats (rel) min: 0.24% max: 50.00% x̄: 11.49% x̃: 5.26%

src/panfrost/bifrost/bifrost_compile.c
@@ -3806,6 +3806,13 @@ bifrost_compile_shader_nir(nir_shader *nir,
          * skip bit is a function of only the data flow graph and is invariant
          * under valid scheduling. */
         bi_analyze_helper_requirements(ctx);
+
+        /* Fuse TEXC after analyzing helper requirements so the analysis
+         * doesn't have to know about dual textures */
+        if (likely(optimize)) {
+                bi_opt_fuse_dual_texture(ctx);
+        }
+
         bi_validate(ctx, "Late lowering");
 
         bi_register_allocate(ctx);

joshuaswarren/mesa-1

commit 490e967ade4142c30e2abfaa3ab5ab11ee7297ae

pan/bi: Fix dual texturing with uniforms
The GLSL code sequence:

   texture2D(tex0, u_coords) + texture2D(tex1, u_coords)

will be optimized to

   TEXC_DUAL tex0/tex1, u_coords, #texture_descriptor

If this optimization happens after lowering FAU, the resulting TEXC instruction
is unschedulable: both the uniform and the constant descriptor fight for the
same FAU slot.

However, if this optimization happens before lowering FAU, then the FAU lowering
will move the descriptor into a register, complicating the dual texturing fixup
in RA.

To fix this interaction, fuse dual texturing before lowering FAU and keep
texture descriptors as constants when lowering FAU of TEXC.

Fixes scheduling failure in piglit drawoverhead -test 3 with uniform reordering.

Fixes: d3dc82bd9b8 ("pan/bi: Enable dual texture fusing pass")
Fixes: 30e53d62bd7 ("pan/bi: Reorder pushed uniforms to avoid moves")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18378>

src/panfrost/bifrost/bi_schedule.c
@@ -2015,6 +2015,14 @@ bi_lower_fau(bi_context *ctx)
                 if (ins->op == BI_OPCODE_ATEST)
                         fau = ins->src[2];
 
+                /* Dual texturing requires the texture operation descriptor
+                 * encoded as an immediate so we can fix up.
+                 */
+                if (ins->op == BI_OPCODE_TEXC) {
+                        assert(ins->src[3].type == BI_INDEX_CONSTANT);
+                        constants[cwords++] = ins->src[3].value;
+                }
+
                 bi_foreach_src(ins, s) {
                         if (bi_check_fau_src(ins, s, constants, &cwords, &fau)) continue;
 

src/panfrost/bifrost/bifrost_compile.c
@@ -5107,16 +5107,6 @@ bi_compile_variant_nir(nir_shader *nir,
         if (bifrost_debug & BIFROST_DBG_SHADERS && !skip_internal)
                 bi_print_shader(ctx, stdout);
 
-        if (ctx->arch <= 8) {
-                bi_lower_fau(ctx);
-        }
-
-        /* Lowering FAU can create redundant moves. Run CSE+DCE to clean up. */
-        if (likely(optimize)) {
-                bi_opt_cse(ctx);
-                bi_opt_dead_code_eliminate(ctx);
-        }
-
         /* Analyze before register allocation to avoid false dependencies. The
          * skip bit is a function of only the data flow graph and is invariant
          * under valid scheduling. Helpers are only defined for fragment
@@ -5129,6 +5119,19 @@ bi_compile_variant_nir(nir_shader *nir,
          * doesn't have to know about dual textures */
         if (likely(optimize)) {
                 bi_opt_fuse_dual_texture(ctx);
+        }
+
+        /* Lower FAU after fusing dual texture, because fusing dual texture
+         * creates new immediates that themselves may need lowering.
+         */
+        if (ctx->arch <= 8) {
+                bi_lower_fau(ctx);
+        }
+
+        /* Lowering FAU can create redundant moves. Run CSE+DCE to clean up. */
+        if (likely(optimize)) {
+                bi_opt_cse(ctx);
+                bi_opt_dead_code_eliminate(ctx);
         }
 
         if (likely(!(bifrost_debug & BIFROST_DBG_NOPSCHED)))