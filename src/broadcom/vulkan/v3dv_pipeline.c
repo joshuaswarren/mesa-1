joshuaswarren/mesa-1

commit 9efd7217155d59f7b6d703a049c9676329a0f090

v3dv: ref/unref pipeline layout objects
Since KHR_maintenance4 it is possible to destroy pipeline layouts
immediately after creating pipelines.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18246>

src/broadcom/vulkan/v3dv_pipeline.c
@@ -150,6 +150,9 @@ v3dv_destroy_pipeline(struct v3dv_pipeline *pipeline,
    if (pipeline->executables.mem_ctx)
       ralloc_free(pipeline->executables.mem_ctx);
 
+   if (pipeline->layout)
+      v3dv_pipeline_layout_unref(device, pipeline->layout, pAllocator);
+
    vk_object_free(&device->vk, pAllocator, pipeline);
 }
 
@@ -3025,6 +3028,8 @@ pipeline_init(struct v3dv_pipeline *pipeline,
    /* This must be done after the pipeline has been compiled */
    pipeline_set_ez_state(pipeline, ds_info);
 
+   v3dv_pipeline_layout_ref(pipeline->layout);
+
    return result;
 }
 
@@ -3265,6 +3270,10 @@ compute_pipeline_init(struct v3dv_pipeline *pipeline,
    pipeline->layout = layout;
 
    VkResult result = pipeline_compile_compute(pipeline, cache, info, alloc);
+   if (result != VK_SUCCESS)
+      return result;
+
+   v3dv_pipeline_layout_ref(pipeline->layout);
 
    return result;
 }

src/broadcom/vulkan/v3dv_descriptor_set.c
@@ -339,6 +339,7 @@ v3dv_CreatePipelineLayout(VkDevice _device,
       return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
 
    layout->num_sets = pCreateInfo->setLayoutCount;
+   layout->ref_cnt = 1;
 
    uint32_t dynamic_offset_count = 0;
    for (uint32_t set = 0; set < pCreateInfo->setLayoutCount; set++) {
@@ -405,7 +406,7 @@ v3dv_DestroyPipelineLayout(VkDevice _device,
    if (!pipeline_layout)
       return;
 
-   v3dv_pipeline_layout_destroy(device, pipeline_layout, pAllocator);
+   v3dv_pipeline_layout_unref(device, pipeline_layout, pAllocator);
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL

src/broadcom/vulkan/v3dv_private.h
@@ -1842,6 +1842,11 @@ struct v3dv_pipeline_layout {
    uint32_t dynamic_offset_count;
    uint32_t push_constant_size;
 
+   /* Pipeline layouts can be destroyed after creating pipelines since
+    * maintenance4.
+    */
+   uint32_t ref_cnt;
+
    unsigned char sha1[20];
 };
 
@@ -1849,6 +1854,23 @@ void
 v3dv_pipeline_layout_destroy(struct v3dv_device *device,
                              struct v3dv_pipeline_layout *layout,
                              const VkAllocationCallbacks *alloc);
+
+static inline void
+v3dv_pipeline_layout_ref(struct v3dv_pipeline_layout *layout)
+{
+   assert(layout && layout->ref_cnt >= 1);
+   p_atomic_inc(&layout->ref_cnt);
+}
+
+static inline void
+v3dv_pipeline_layout_unref(struct v3dv_device *device,
+                           struct v3dv_pipeline_layout *layout,
+                           const VkAllocationCallbacks *alloc)
+{
+   assert(layout && layout->ref_cnt >= 1);
+   if (p_atomic_dec_zero(&layout->ref_cnt))
+      v3dv_pipeline_layout_destroy(device, layout, alloc);
+}
 
 /*
  * We are using descriptor maps for ubo/ssbo and texture/samplers, so we need

commit 02bd9911dca6fa88031f51e5c07bb61feb616661

v3dv: ref pipeline layout earlier during pipeline init
If we fail to init the pipeline the callee will then destroy it
and if we had assigned the layout to the pipeline it will try to
unref it, so make sure we ref it right after assigning it.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7206
Fixes: dEQP-VK.api.object_management.alloc_callback_fail_multiple.graphics_pipeline
Fixes: 9efd721715 ('v3dv: ref/unref pipeline layout objects')
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18426>

src/broadcom/vulkan/v3dv_pipeline.c
@@ -2928,6 +2928,7 @@ pipeline_init(struct v3dv_pipeline *pipeline,
 
    V3DV_FROM_HANDLE(v3dv_pipeline_layout, layout, pCreateInfo->layout);
    pipeline->layout = layout;
+   v3dv_pipeline_layout_ref(pipeline->layout);
 
    V3DV_FROM_HANDLE(v3dv_render_pass, render_pass, pCreateInfo->renderPass);
    assert(pCreateInfo->subpass < render_pass->subpass_count);
@@ -3025,8 +3026,6 @@ pipeline_init(struct v3dv_pipeline *pipeline,
 
    /* This must be done after the pipeline has been compiled */
    pipeline_set_ez_state(pipeline, ds_info);
-
-   v3dv_pipeline_layout_ref(pipeline->layout);
 
    return result;
 }
@@ -3267,12 +3266,11 @@ compute_pipeline_init(struct v3dv_pipeline *pipeline,
 
    pipeline->device = device;
    pipeline->layout = layout;
+   v3dv_pipeline_layout_ref(pipeline->layout);
 
    VkResult result = pipeline_compile_compute(pipeline, cache, info, alloc);
    if (result != VK_SUCCESS)
       return result;
-
-   v3dv_pipeline_layout_ref(pipeline->layout);
 
    return result;
 }