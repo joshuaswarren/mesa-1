openharmony/third_party_mesa3d

commit 9df3e8d54b54fd1c98b6eaed3366f4bf14d32958

intel/dev: Enable remaining DG2 and ATS-M device IDs
Mostly Matt Roper's kernel patch commit message:

The device IDs here are associated with DG2 add-in cards. We need to
wait for some additional functionality (e.g., small BAR recovery) to
land before we're ready to upstream these.

Ref: https://patchwork.freedesktop.org/patch/483381/?series=103098&rev=1
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Acked-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12706>

include/pci_ids/iris_pci_ids.h
@@ -221,14 +221,13 @@ CHIPSET(0x4907, sg1, "SG1", "Intel(R) Graphics")
 CHIPSET(0x4908, dg1, "DG1", "Intel(R) Graphics")
 CHIPSET(0x4909, dg1, "DG1", "Intel(R) Graphics")
 
-/* Commented devices are waiting on i915 upstream support */
-/* CHIPSET(0x4f80, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f81, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f82, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f83, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f84, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f87, dg2_g11, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x4f88, dg2_g11, "DG2", "Intel(R) Graphics") */
+CHIPSET(0x4f80, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f81, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f82, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f83, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f84, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f87, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x4f88, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5690, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5691, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5692, dg2_g10, "DG2", "Intel(R) Graphics")
@@ -237,16 +236,16 @@ CHIPSET(0x5694, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5695, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5696, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5697, dg2_g12, "DG2", "Intel(R) Graphics")
-/* CHIPSET(0x56a0, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a1, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a2, dg2_g10, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a3, dg2_g12, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a4, dg2_g12, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a5, dg2_g11, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56a6, dg2_g11, "DG2", "Intel(R) Graphics") */
+CHIPSET(0x56a0, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a1, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a2, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a3, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a4, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a5, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a6, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56b0, dg2_g11, "DG2", "Intel(R) Graphics")
-/* CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Graphics") */
+CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Graphics")
-/* CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics") */
-/* CHIPSET(0x56c0, dg2_g10, "ATS-M", "Intel(R) Graphics") */
-/* CHIPSET(0x56c1, dg2_g11, "ATS-M", "Intel(R) Graphics") */
+CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56c0, dg2_g10, "ATS-M", "Intel(R) Graphics")
+CHIPSET(0x56c1, dg2_g11, "ATS-M", "Intel(R) Graphics")

joshuaswarren/mesa-1

commit 56f891f243d385494fd21e22382a9ed57166fc54

intel/pci_ids: Drop non-upstream dg2 pci-ids
These pci-ids should be included in
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14523, since
these pci-ids will only be supported by kernels that support the
forked Linux uapi. (Note that !14523 will never be merged into
upstream Mesa.)

Ref: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/drm/i915_pciids.h?h=v6.0-rc3#n695
Fixes: 9df3e8d54b5 ("intel/dev: Enable remaining DG2 and ATS-M device IDs")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18386>

include/pci_ids/iris_pci_ids.h
@@ -222,13 +222,6 @@ CHIPSET(0x4907, sg1, "SG1", "Intel(R) Graphics")
 CHIPSET(0x4908, dg1, "DG1", "Intel(R) Graphics")
 CHIPSET(0x4909, dg1, "DG1", "Intel(R) Graphics")
 
-CHIPSET(0x4f80, dg2_g10, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f81, dg2_g10, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f82, dg2_g10, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f83, dg2_g10, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f84, dg2_g10, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f87, dg2_g11, "DG2", "Intel(R) Graphics")
-CHIPSET(0x4f88, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5690, dg2_g10, "DG2", "Intel(R) Arc(tm) A770M Graphics")
 CHIPSET(0x5691, dg2_g10, "DG2", "Intel(R) Arc(tm) A730M Graphics")
 CHIPSET(0x5692, dg2_g10, "DG2", "Intel(R) Arc(tm) A550M Graphics")