joshuaswarren/mesa-1

commit a7ffa57ce81ebc3481141e15068e697cbf71ca27

panfrost: Handle Job VA cycles when decoding a dump file
When a job loop is submitted to the GPU, as in IGT
panfrost_submit@pan-reset, this will trigger a DRM scheduler timeout and
eventually a devcoredump. However, when pandecode traverses the list of
jobs in a submit BO, it will iterate forever.

Fix it by adding already-visited CPU VA's into a mesa pointer set and
checking that the current job's CPU VA hasn't already been handled.

Signed-off-by: Adrián Larumbe <adrian.larumbe@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14034>

src/panfrost/lib/genxml/decode.c
@@ -33,6 +33,7 @@
 #include <ctype.h>
 #include "decode.h"
 
+#include "util/set.h"
 #include "midgard/disassemble.h"
 #include "bifrost/disassemble.h"
 #include "bifrost/valhall/disassemble.h"
@@ -1138,9 +1139,21 @@ GENX(pandecode_jc)(mali_ptr jc_gpu_va, unsigned gpu_id)
 {
         pandecode_dump_file_open();
 
+        struct set *va_set = _mesa_pointer_set_create(NULL);
+        struct set_entry *entry = NULL;
+
         mali_ptr next_job = 0;
 
         do {
+                struct pandecode_mapped_memory *mem =
+                        pandecode_find_mapped_gpu_mem_containing(jc_gpu_va);
+
+                entry = _mesa_set_search(va_set, mem->addr);
+                if (entry !=  NULL) {
+                        fprintf(stdout, "Job list has a cycle\n");
+                        break;
+                }
+
                 pan_unpack(PANDECODE_PTR(jc_gpu_va, struct mali_job_header_packed),
                            JOB_HEADER, h);
                 next_job = h.next;
@@ -1189,7 +1202,13 @@ GENX(pandecode_jc)(mali_ptr jc_gpu_va, unsigned gpu_id)
                 default:
                         break;
                 }
+
+                /* Add the latest visited job GPU VA to avoid cycles */
+                _mesa_set_add(va_set, mem->addr);
+
         } while ((jc_gpu_va = next_job));
+
+        _mesa_set_destroy(va_set, NULL);
 
         fflush(pandecode_dump_stream);
         pandecode_map_read_write();

commit 0a4dabe0368962e028cae616ea4cf77ea8dc2311

pan/decode: Fix job cycle detection
We need to look at the job header pointers themselves, not the memory objects
that contain them, because there can be (and usually is) multiple jobs per BO.

Fixes: a7ffa57ce81 ("panfrost: Handle Job VA cycles when decoding a dump file")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18539>

src/panfrost/lib/genxml/decode.c
@@ -1144,17 +1144,16 @@ GENX(pandecode_jc)(mali_ptr jc_gpu_va, unsigned gpu_id)
         mali_ptr next_job = 0;
 
         do {
-                struct pandecode_mapped_memory *mem =
-                        pandecode_find_mapped_gpu_mem_containing(jc_gpu_va);
+                struct mali_job_header_packed *hdr =
+                        PANDECODE_PTR(jc_gpu_va, struct mali_job_header_packed);
 
-                entry = _mesa_set_search(va_set, mem->addr);
+                entry = _mesa_set_search(va_set, hdr);
                 if (entry !=  NULL) {
                         fprintf(stdout, "Job list has a cycle\n");
                         break;
                 }
 
-                pan_unpack(PANDECODE_PTR(jc_gpu_va, struct mali_job_header_packed),
-                           JOB_HEADER, h);
+                pan_unpack(hdr, JOB_HEADER, h);
                 next_job = h.next;
 
                 DUMP_UNPACKED(JOB_HEADER, h, "Job Header (%" PRIx64 "):\n", jc_gpu_va);
@@ -1202,8 +1201,8 @@ GENX(pandecode_jc)(mali_ptr jc_gpu_va, unsigned gpu_id)
                         break;
                 }
 
-                /* Add the latest visited job GPU VA to avoid cycles */
-                _mesa_set_add(va_set, mem->addr);
+                /* Track the latest visited job CPU VA to detect cycles */
+                _mesa_set_add(va_set, hdr);
 
         } while ((jc_gpu_va = next_job));
 