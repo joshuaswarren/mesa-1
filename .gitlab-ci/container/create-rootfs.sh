joshuaswarren/mesa-1

commit 9a0c609b9e5a694dff5e54190f4e6c7c5abc8fcd

ci: performance traces: make use of no-perf label
Traces with label `no-perf` will be skipped in performance testing.

This commit adds the yq tool, which preprocesses the traces.yml file
before sending it to the piglit.

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18329>

.gitlab-ci/container/create-rootfs.sh
@@ -105,6 +105,9 @@ apt-get -y install --no-install-recommends \
 # MinIO and doesn't depend on git
 pip3 install git+http://gitlab.freedesktop.org/freedesktop/ci-templates@34f4ade99434043f88e164933f570301fd18b125
 
+# Needed for manipulation with traces yaml files.
+pip3 install yq
+
 apt-get purge -y \
         $INSTALL_CI_FAIRY_PACKAGES
 

.gitlab-ci/container/debian/x86_test-base.sh
@@ -114,6 +114,9 @@ mkdir -p /lava-files/
 # and doesn't depend on git
 pip3 install git+http://gitlab.freedesktop.org/freedesktop/ci-templates@34f4ade99434043f88e164933f570301fd18b125
 
+# Needed for manipulation with traces yaml files.
+pip3 install yq
+
 ############### Build libdrm
 
 . .gitlab-ci/container/build-libdrm.sh

.gitlab-ci/piglit/piglit-traces.sh
@@ -8,6 +8,11 @@ MINIO_ARGS="--credentials=/tmp/.minio_credentials"
 RESULTS=$(realpath -s "$PWD"/results)
 mkdir -p "$RESULTS"
 
+if [ "$PIGLIT_REPLAY_SUBCOMMAND" = "profile" ]; then
+    yq -i -Y '. | del(.traces[][] | select(.label[0,1,2,3,4,5,6,7,8,9] == "no-perf"))' \
+      "$PIGLIT_REPLAY_DESCRIPTION_FILE"  # label positions are a bit hack
+fi
+
 # WINE
 PATH="/opt/wine-stable/bin/:$PATH" # WineHQ path
 export WINEPREFIX="/dxvk-wine64" # hardcode DXVK for now
@@ -186,7 +191,7 @@ __PREFIX="trace/$PIGLIT_REPLAY_DEVICE_NAME"
 __MINIO_PATH="$PIGLIT_REPLAY_ARTIFACTS_BASE_URL"
 __MINIO_TRACES_PREFIX="traces"
 
-if [ "x$PIGLIT_REPLAY_SUBCOMMAND" != "xprofile" ]; then
+if [ "$PIGLIT_REPLAY_SUBCOMMAND" != "profile" ]; then
     quiet replay_minio_upload_images
 fi
 

src/gallium/drivers/virgl/ci/traces-virgl-iris.yml
@@ -225,7 +225,7 @@ traces:
         Assertion `attrib->_EffRelativeOffset < binding->Stride' failed.
   nheko/nheko-colors.trace:
     gl-virgl:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 9877fc41ce761c9ce15561b12ef77c27
   warzone2100/warzone2100-default.trace:
     gl-virgl:

src/gallium/drivers/virgl/ci/traces-virgl.yml
@@ -219,7 +219,7 @@ traces:
         Assertion `attrib->_EffRelativeOffset < binding->Stride' failed.
   nheko/nheko-colors.trace:
     gl-virgl:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 3a12c08087e16cfae4729f4e9d6c9387
   warzone2100/warzone2100-default.trace:
     gl-virgl:

src/intel/ci/traces-iris.yml
@@ -900,23 +900,23 @@ traces:
       checksum: 3d419f45bc5ce210a7df583e92143089
   blender/blender-demo-cube_diorama.trace:
     gl-intel-apl:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
       text: causes intermittent timeouts
     gl-intel-glx:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
     gl-intel-amly:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
     gl-intel-kbl:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
     gl-intel-whl:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
     gl-intel-cml:
-      label: [skip, no-perf]
+      label: [no-perf]
       checksum: 6a0e17d98eca33e80810b6158e998aa1
   blender/blender-demo-ellie_pose.trace:
     gl-intel-apl:

commit ad045ab79d80b00e3d4cf6099ecc0e2bae2cd6b8

ci: add jq utility
Needed as a dependency for the yq utility.

Also bump x86-build-base image.

Fixes: 9a0c609b9e5a ("ci: performance traces: make use of no-perf label")

Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18595>

.gitlab-ci/container/create-rootfs.sh
@@ -58,6 +58,7 @@ apt-get -y install --no-install-recommends \
     ca-certificates \
     firmware-realtek \
     initramfs-tools \
+    jq \
     libasan6 \
     libexpat1 \
     libpng16-16 \

.gitlab-ci/container/debian/x86_test-base.sh
@@ -62,6 +62,7 @@ apt-get install -y --no-remove \
       git-lfs \
       inetutils-syslogd \
       iptables \
+      jq \
       libasan6 \
       libexpat1 \
       libllvm13 \

.gitlab-ci/image-tags.yml
@@ -3,7 +3,7 @@ variables:
    DEBIAN_BASE_TAG: "2022-09-12-virglrenderer"
 
    DEBIAN_X86_BUILD_IMAGE_PATH: "debian/x86_build"
-   DEBIAN_BUILD_TAG: "2022-09-11-rusticl"
+   DEBIAN_BUILD_TAG: "2022-09-14-add-jq"
 
    DEBIAN_X86_BUILD_MINGW_IMAGE_PATH: "debian/x86_build-mingw"
    DEBIAN_BUILD_MINGW_TAG: "2022-08-25-shellcheck"
@@ -11,11 +11,11 @@ variables:
    DEBIAN_X86_TEST_BASE_IMAGE: "debian/x86_test-base"
 
    DEBIAN_X86_TEST_IMAGE_PATH: "debian/x86_test-gl"
-   DEBIAN_X86_TEST_GL_TAG: "2022-09-13-piglit-uprev-traces"
-   DEBIAN_X86_TEST_VK_TAG: "2022-09-13-piglit-uprev-traces"
+   DEBIAN_X86_TEST_GL_TAG: "2022-09-14-add-jq"
+   DEBIAN_X86_TEST_VK_TAG: "2022-09-14-add-jq"
 
    FEDORA_X86_BUILD_TAG: "2022-08-25-shellcheck"
-   KERNEL_ROOTFS_TAG: "2022-09-13-piglit-uprev-traces"
+   KERNEL_ROOTFS_TAG: "2022-09-14-add-jq"
 
    WINDOWS_X64_VS_PATH: "windows/x64_vs"
    WINDOWS_X64_VS_TAG: "2022-08-17-bump"