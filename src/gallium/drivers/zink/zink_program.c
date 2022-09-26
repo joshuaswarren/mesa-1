joshuaswarren/mesa-1

commit 61feb06678459f347541298b6d6aec443d8e6e9e

zink: change u_foreach_bit to regular for loop in zink_gfx_program_update
too hot to be using this

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18135>

src/gallium/drivers/zink/zink_program.c
@@ -408,8 +408,10 @@ zink_gfx_program_update(struct zink_context *ctx)
       struct hash_entry *entry = _mesa_hash_table_search_pre_hashed(ht, hash, ctx->gfx_stages);
       if (entry) {
          prog = (struct zink_gfx_program*)entry->data;
-         u_foreach_bit(stage, prog->stages_present & ~ctx->dirty_shader_stages)
-            ctx->gfx_pipeline_state.modules[stage] = prog->modules[stage]->shader;
+         for (unsigned i = 0; i < ZINK_GFX_SHADER_COUNT; i++) {
+            if (prog->stages_present & ctx->dirty_shader_stages & BITFIELD_BIT(i))
+               ctx->gfx_pipeline_state.modules[i] = prog->modules[i]->shader;
+         }
          /* ensure variants are always updated if keys have changed since last use */
          ctx->dirty_shader_stages |= prog->stages_present;
          update_gfx_program(ctx, prog);

commit aa8fe45de8dc97d4aff0eece28f8c8c4cd261618

zink: copy the non-dirty shader stages when updating gfx program
typo during refactoring

Fixes: 61feb066784 ("zink: change u_foreach_bit to regular for loop in zink_gfx_program_update")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18786>

src/gallium/drivers/zink/zink_program.c
@@ -423,7 +423,7 @@ zink_gfx_program_update(struct zink_context *ctx)
       if (entry) {
          prog = (struct zink_gfx_program*)entry->data;
          for (unsigned i = 0; i < ZINK_GFX_SHADER_COUNT; i++) {
-            if (prog->stages_present & ctx->dirty_shader_stages & BITFIELD_BIT(i))
+            if (prog->stages_present & ~ctx->dirty_shader_stages & BITFIELD_BIT(i))
                ctx->gfx_pipeline_state.modules[i] = prog->modules[i]->shader;
          }
          /* ensure variants are always updated if keys have changed since last use */