joshuaswarren/mesa-1

commit 8a8ff4233daf96bf3d682b457ce08dc0e18891b8

mesa: simplify st_egl_image binding process for texture storage
The dmabuf imported edge case is now properly handled.

Signed-off-by: Simon Zeni <simon@bl4ckb0ne.ca>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18673>

src/mesa/main/teximage.c
@@ -3543,13 +3543,32 @@ egl_image_target_texture(struct gl_context *ctx,
 
       texObj->External = GL_TRUE;
 
+      struct st_egl_image stimg;
+      bool native_supported;
+      if (!st_get_egl_image(ctx, image, PIPE_BIND_SAMPLER_VIEW, caller,
+                            &stimg, &native_supported))
+         return;
+
       if (tex_storage) {
-         st_egl_image_target_tex_storage(ctx, target, texObj, texImage,
-                                         image);
+         /* EXT_EGL_image_storage
+          * If the EGL image was created using EGL_EXT_image_dma_buf_import,
+          * then the following applies:
+          *    - <target> must be GL_TEXTURE_2D or GL_TEXTURE_EXTERNAL_OES.
+          *    Otherwise, the error INVALID_OPERATION is generated.
+          */
+         if (stimg.imported_dmabuf &&
+             (target == GL_TEXTURE_2D || target == GL_TEXTURE_EXTERNAL_OES)) {
+            _mesa_error(ctx, GL_INVALID_OPERATION,
+                        "%s(texture is imported from dmabuf)", caller);
+            return;
+         }
+         st_bind_egl_image(ctx, texObj, texImage, &stimg, true, native_supported);
       } else {
-         st_egl_image_target_texture_2d(ctx, target, texObj, texImage,
-                                        image);
+         st_bind_egl_image(ctx, texObj, texImage, &stimg,
+                           target != GL_TEXTURE_EXTERNAL_OES, native_supported);
       }
+
+      pipe_resource_reference(&stimg.texture, NULL);
 
       _mesa_dirty_texobj(ctx, texObj);
    }

src/mesa/state_tracker/st_cb_eglimage.c
@@ -162,7 +162,7 @@ is_nv12_as_r8_g8b8_supported(struct pipe_screen *screen, struct st_egl_image *ou
 /**
  * Return the gallium texture of an EGLImage.
  */
-static bool
+bool
 st_get_egl_image(struct gl_context *ctx, GLeglImageOES image_handle,
                  unsigned usage, const char *error, struct st_egl_image *out,
                  bool *native_supported)
@@ -260,7 +260,7 @@ st_egl_image_target_renderbuffer_storage(struct gl_context *ctx,
    }
 }
 
-static void
+void
 st_bind_egl_image(struct gl_context *ctx,
                   struct gl_texture_object *texObj,
                   struct gl_texture_image *texImage,
@@ -408,44 +408,6 @@ st_bind_egl_image(struct gl_context *ctx,
    _mesa_update_texture_object_swizzle(ctx, texObj);
 
    _mesa_dirty_texobj(ctx, texObj);
-}
-
-void
-st_egl_image_target_texture_2d(struct gl_context *ctx, GLenum target,
-                               struct gl_texture_object *texObj,
-                               struct gl_texture_image *texImage,
-                               GLeglImageOES image_handle)
-{
-   struct st_egl_image stimg;
-   bool native_supported;
-
-   if (!st_get_egl_image(ctx, image_handle, PIPE_BIND_SAMPLER_VIEW,
-                         "glEGLImageTargetTexture2D", &stimg,
-                         &native_supported))
-      return;
-
-   st_bind_egl_image(ctx, texObj, texImage, &stimg,
-                     target != GL_TEXTURE_EXTERNAL_OES,
-                     native_supported);
-   pipe_resource_reference(&stimg.texture, NULL);
-}
-
-void
-st_egl_image_target_tex_storage(struct gl_context *ctx, GLenum target,
-                                struct gl_texture_object *texObj,
-                                struct gl_texture_image *texImage,
-                                GLeglImageOES image_handle)
-{
-   struct st_egl_image stimg;
-   bool native_supported;
-
-   if (!st_get_egl_image(ctx, image_handle, PIPE_BIND_SAMPLER_VIEW,
-                         "glEGLImageTargetTexture2D", &stimg,
-                         &native_supported))
-      return;
-
-   st_bind_egl_image(ctx, texObj, texImage, &stimg, true, native_supported);
-   pipe_resource_reference(&stimg.texture, NULL);
 }
 
 static GLboolean

src/mesa/state_tracker/st_cb_eglimage.h
@@ -35,14 +35,16 @@ extern void
 st_init_eglimage_functions(struct dd_function_table *functions,
                            bool has_egl_image_validate);
 
-void st_egl_image_target_texture_2d(struct gl_context *ctx, GLenum target,
-                                    struct gl_texture_object *texObj,
-                                    struct gl_texture_image *texImage,
-                                    GLeglImageOES image_handle);
-void st_egl_image_target_tex_storage(struct gl_context *ctx, GLenum target,
-                                     struct gl_texture_object *texObj,
-                                     struct gl_texture_image *texImage,
-                                     GLeglImageOES image_handle);
+bool st_get_egl_image(struct gl_context *ctx, GLeglImageOES image_handle,
+                      unsigned usage, const char *error,
+                      struct st_egl_image *out, bool *native_supported);
+void st_bind_egl_image(struct gl_context *ctx,
+                       struct gl_texture_object *texObj,
+                       struct gl_texture_image *texImage,
+                       struct st_egl_image *stimg,
+                       bool tex_storage,
+                       bool native_supported);
+
 void st_egl_image_target_renderbuffer_storage(struct gl_context *ctx,
                                               struct gl_renderbuffer *rb,
                                               GLeglImageOES image_handle);

commit 95d0c95d75c3ee5a1bd20c70ed7356718f78baeb

mesa: unlock texture on error path in glEGLImageTargetTexStorageEXT
The texture mutex was not properly unlocked on error path, leading to deadlocks

Fixes: 8a8ff423 ("mesa: simplify st_egl_image binding process for texture storage")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7422

Signed-off-by: Simon Zeni <simon@bl4ckb0ne.ca>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18964>

src/mesa/main/teximage.c
@@ -3546,8 +3546,10 @@ egl_image_target_texture(struct gl_context *ctx,
       struct st_egl_image stimg;
       bool native_supported;
       if (!st_get_egl_image(ctx, image, PIPE_BIND_SAMPLER_VIEW, caller,
-                            &stimg, &native_supported))
+                            &stimg, &native_supported)) {
+         _mesa_unlock_texture(ctx, texObj);
          return;
+      }
 
       if (tex_storage) {
          /* EXT_EGL_image_storage
@@ -3560,6 +3562,7 @@ egl_image_target_texture(struct gl_context *ctx,
              !(target == GL_TEXTURE_2D || target == GL_TEXTURE_EXTERNAL_OES)) {
             _mesa_error(ctx, GL_INVALID_OPERATION,
                         "%s(texture is imported from dmabuf)", caller);
+            _mesa_unlock_texture(ctx, texObj);
             return;
          }
          st_bind_egl_image(ctx, texObj, texImage, &stimg, true, native_supported);