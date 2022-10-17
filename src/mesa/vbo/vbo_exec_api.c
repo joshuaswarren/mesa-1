openharmony/third_party_mesa3d

commit 67e9bbf97143a53d288c9f7706b532a598a8bc72

mesa/glthread: enable immediate mode
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/mesa/vbo/vbo_exec_api.c
@@ -803,11 +803,14 @@ vbo_exec_Begin(GLenum mode)
    ctx->Driver.CurrentExecPrimitive = mode;
 
    ctx->Exec = ctx->BeginEnd;
+
    /* We may have been called from a display list, in which case we should
     * leave dlist.c's dispatch table in place.
     */
-   if (ctx->CurrentClientDispatch == ctx->OutsideBeginEnd) {
-      ctx->CurrentClientDispatch = ctx->BeginEnd;
+   if (ctx->CurrentClientDispatch == ctx->MarshalExec) {
+      ctx->CurrentServerDispatch = ctx->Exec;
+   } else if (ctx->CurrentClientDispatch == ctx->OutsideBeginEnd) {
+      ctx->CurrentClientDispatch = ctx->Exec;
       _glapi_set_dispatch(ctx->CurrentClientDispatch);
    } else {
       assert(ctx->CurrentClientDispatch == ctx->Save);
@@ -858,8 +861,11 @@ vbo_exec_End(void)
    }
 
    ctx->Exec = ctx->OutsideBeginEnd;
-   if (ctx->CurrentClientDispatch == ctx->BeginEnd) {
-      ctx->CurrentClientDispatch = ctx->OutsideBeginEnd;
+
+   if (ctx->CurrentClientDispatch == ctx->MarshalExec) {
+      ctx->CurrentServerDispatch = ctx->Exec;
+   } else if (ctx->CurrentClientDispatch == ctx->BeginEnd) {
+      ctx->CurrentClientDispatch = ctx->Exec;
       _glapi_set_dispatch(ctx->CurrentClientDispatch);
    }
 

src/mapi/glapi/gen/gl_API.xml
@@ -1148,7 +1148,7 @@
         <glx rop="3"/>
     </function>
 
-    <function name="Begin" deprecated="3.1" exec="dynamic" marshal_fail="true">
+    <function name="Begin" deprecated="3.1" exec="dynamic">
         <param name="mode" type="GLenum"/>
         <glx rop="4"/>
     </function>

joshuaswarren/mesa-1

commit 8bc07d4602d1ce82856125b1899f5c7478f1a511

glthread: leave dlist dispatch in place for Begin/End
If Begin/End are called from a display list make sure to leave
the dlist.c's dispatch table in place just like the non-glthread
code does.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7335
Fixes: 67e9bbf97143 ("mesa/glthread: enable immediate mode")

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19088>

src/mesa/vbo/vbo_exec_api.c
@@ -855,7 +855,8 @@ _mesa_Begin(GLenum mode)
     * leave dlist.c's dispatch table in place.
     */
    if (ctx->GLThread.enabled) {
-      ctx->CurrentServerDispatch = ctx->Exec;
+      if (ctx->CurrentServerDispatch == ctx->OutsideBeginEnd)
+         ctx->CurrentServerDispatch = ctx->Exec;
    } else if (ctx->CurrentClientDispatch == ctx->OutsideBeginEnd) {
       ctx->CurrentClientDispatch = ctx->CurrentServerDispatch = ctx->Exec;
       _glapi_set_dispatch(ctx->CurrentClientDispatch);
@@ -914,7 +915,10 @@ _mesa_End(void)
    ctx->Exec = ctx->OutsideBeginEnd;
 
    if (ctx->GLThread.enabled) {
-      ctx->CurrentServerDispatch = ctx->Exec;
+      if (ctx->CurrentServerDispatch == ctx->BeginEnd ||
+          ctx->CurrentServerDispatch == ctx->HWSelectModeBeginEnd) {
+         ctx->CurrentServerDispatch = ctx->Exec;
+      }
    } else if (ctx->CurrentClientDispatch == ctx->BeginEnd ||
               ctx->CurrentClientDispatch == ctx->HWSelectModeBeginEnd) {
       ctx->CurrentClientDispatch = ctx->CurrentServerDispatch = ctx->Exec;