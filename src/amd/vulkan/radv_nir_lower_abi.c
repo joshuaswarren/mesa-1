joshuaswarren/mesa-1

commit 6133ec2c972a17d1483178d69ed9efa78a76782f

radv: add lowering for nir_intrinsic_load_ring_attr_{offset}_amd
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19173>

src/amd/vulkan/radv_nir_lower_abi.c
@@ -112,6 +112,21 @@ lower_abi_instr(nir_builder *b, nir_instr *instr, void *state)
    case nir_intrinsic_load_ring_es2gs_offset_amd:
       replacement = ac_nir_load_arg(b, &s->args->ac, s->args->ac.es2gs_offset);
       break;
+
+   case nir_intrinsic_load_ring_attr_amd:
+      if (s->use_llvm)
+         break;
+
+      replacement = load_ring(b, RING_PS_ATTR, s);
+      break;
+
+   case nir_intrinsic_load_ring_attr_offset_amd: {
+      nir_ssa_def *ring_attr_offset = ac_nir_load_arg(b, &s->args->ac, s->args->ac.gs_attr_offset);
+      replacement = nir_ishl(b, nir_ubfe(b, ring_attr_offset, nir_imm_int(b, 0), nir_imm_int(b, 15)),
+                             nir_imm_int(b, 9)); /* 512b increments. */
+      break;
+   }
+
    case nir_intrinsic_load_tess_rel_patch_id_amd:
       if (stage == MESA_SHADER_TESS_CTRL) {
          replacement = nir_extract_u8(b, ac_nir_load_arg(b, &s->args->ac, s->args->ac.tcs_rel_ids),

commit 7a10d2156c221fbc39d07a47117b2b1bdd35c3f2

radv: Handle attribute ring intrinsic correctly with LLVM.
Again, if we don't set progress to false we get fun stuff.

Fixes: 6133ec2c972 ("radv: add lowering for nir_intrinsic_load_ring_attr_{offset}_amd")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19404>

src/amd/vulkan/radv_nir_lower_abi.c
@@ -137,8 +137,10 @@ lower_abi_instr(nir_builder *b, nir_instr *instr, void *state)
       break;
 
    case nir_intrinsic_load_ring_attr_amd:
-      if (s->use_llvm)
+      if (s->use_llvm) {
+         progress = false;
          break;
+      }
 
       replacement = load_ring(b, RING_PS_ATTR, s);
       break;