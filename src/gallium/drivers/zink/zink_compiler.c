openharmony/third_party_mesa3d

commit 70221817fccceb03435ecc6b3a0999974c066577

zink: store the spirv_shader to the zink_shader struct for generated tcs
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14976>

src/gallium/drivers/zink/zink_compiler.c
@@ -1162,7 +1162,10 @@ zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shad
    ralloc_free(nir);
 
    /* TODO: determine if there's any reason to cache spirv output? */
-   ralloc_free(spirv);
+   if (zs->is_generated)
+      zs->spirv = spirv;
+   else
+      ralloc_free(spirv);
    return mod;
 }
 
@@ -1961,6 +1964,7 @@ zink_shader_free(struct zink_context *ctx, struct zink_shader *shader)
    }
    _mesa_set_destroy(shader->programs, NULL);
    ralloc_free(shader->nir);
+   ralloc_free(shader->spirv);
    FREE(shader);
 }
 

src/gallium/drivers/zink/zink_compiler.h
@@ -88,6 +88,7 @@ struct zink_shader {
    uint32_t ubos_used; // bitfield of which ubo indices are used
    uint32_t ssbos_used; // bitfield of which ssbo indices are used
    bool bindless;
+   struct spirv_shader *spirv;
 
    simple_mtx_t lock;
    struct set *programs;

joshuaswarren/mesa-1

commit e4bebd08c1fefa4a0b30feae4c0512e5e0f6e227

zink: do not read is_generated unless in tcs shader
It's undefined behavior in C to read a union member if another member
has been written to more recently. Let's be more careful here!

Fixes: 70221817fcc ("zink: store the spirv_shader to the zink_shader struct for generated tcs")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19457>

src/gallium/drivers/zink/zink_compiler.c
@@ -2348,7 +2348,7 @@ zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shad
    ralloc_free(nir);
 
    /* TODO: determine if there's any reason to cache spirv output? */
-   if (zs->is_generated)
+   if (zs->nir->info.stage == MESA_SHADER_TESS_CTRL && zs->is_generated)
       zs->spirv = spirv;
    else
       ralloc_free(spirv);