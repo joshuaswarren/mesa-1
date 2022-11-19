openharmony/third_party_mesa3d

commit e7f0c40d31776a6fed7f5bb13d773a365d851eb4

r600/sfn: rewrite NIR backend
This is a rewite of the NIR backend. it adds some optimization
and a scheduler.

v2: - replace some magic numbers by constants
    - make sure constructor is always used with new
    - use default initialization in more places
      (changes suggested by Filip Gawin)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17076>

src/gallium/drivers/r600/sfn/sfn_shader_fs.h
@@ -1,0 +1,88 @@
+#ifndef R600_SFN_SHADER_FS_H
+#define R600_SFN_SHADER_FS_H
+
+#include "sfn_shader.h"
+
+namespace r600 {
+
+class FragmentShader : public Shader {
+public:
+   FragmentShader(const r600_shader_key& key);
+   bool load_input(nir_intrinsic_instr *intr) override;
+   bool store_output(nir_intrinsic_instr *intr) override;
+
+   bool process_stage_intrinsic(nir_intrinsic_instr *intr) override;
+
+private:
+   class Interpolator {
+   public:
+      Interpolator();
+      bool enabled : 1;
+      unsigned ij_index : 4;
+      PRegister i;
+      PRegister j;
+   };
+
+   struct InterpolateParams {
+      PVirtualValue i,j;
+      int base;
+   };
+
+   static const int s_max_interpolators = 6;
+
+   bool do_scan_instruction(nir_instr *instr) override;
+   int do_allocate_reserved_registers() override;
+
+   void do_get_shader_info(r600_shader *sh_info) override;
+
+   bool scan_input(nir_intrinsic_instr *instr, int index_src_id);
+
+   bool load_barycentric_pixel(nir_intrinsic_instr *intr);
+   bool load_barycentric_at_sample(nir_intrinsic_instr* instr);
+   bool load_barycentric_at_offset(nir_intrinsic_instr* instr);
+   bool load_interpolated_input(nir_intrinsic_instr *intr);
+   bool load_interpolated(RegisterVec4& dest, const InterpolateParams& params,
+                          int num_dest_comp, int start_comp);
+
+   bool load_interpolated_one_comp(RegisterVec4& dest, const InterpolateParams& params, EAluOp op);
+   bool load_interpolated_two_comp(RegisterVec4& dest, const InterpolateParams& params, EAluOp op, int writemask);
+   bool load_interpolated_two_comp_for_one(RegisterVec4& dest, const InterpolateParams& params, EAluOp op,
+                                           int start, int dest_slot);
+
+   bool emit_export_pixel(nir_intrinsic_instr& intr, int num_outputs);
+   bool emit_load_sample_mask_in(nir_intrinsic_instr* instr);
+   bool emit_load_helper_invocation(nir_intrinsic_instr* instr);
+   bool emit_load_sample_pos(nir_intrinsic_instr* instr);
+   void do_finalize() override;
+
+   bool read_prop(std::istream& is) override;
+
+   void do_print_properties(std::ostream& os) const override;
+
+   bool m_dual_source_blend;
+   unsigned m_max_color_exports;
+   unsigned m_export_highest;
+   unsigned m_num_color_exports;
+   unsigned m_color_export_mask;
+   unsigned m_depth_exports;
+   ExportInstr *m_last_pixel_export;
+
+   std::bitset<s_max_interpolators> m_interpolators_used;
+   std::array<Interpolator, s_max_interpolators> m_interpolator;
+   RegisterVec4 m_pos_input;
+   Register *m_face_input{nullptr};
+   bool m_fs_write_all;
+   bool m_uses_discard{false};
+   bool m_gs_prim_id_input{false};
+   int m_ps_prim_id_input{0};
+   Register *m_sample_id_reg{nullptr};
+   Register *m_sample_mask_reg{nullptr};
+   Register *m_helper_invocation{nullptr};
+   int m_nsys_inputs{0};
+   bool m_apply_sample_mask{false};
+   int m_rat_base{0};
+};
+
+}
+
+#endif

joshuaswarren/mesa-1

commit 3b042f945a9d6b5b5adc987b1d242625648d5678

r600/sfn: sort FS color outputs before all other outputs
The color outputs must be checked against the number of available
color buffers, therefore it is best to sort the color outputs to be
on the driver locations before the other FS outputs.

Fixes: e7f0c40d31776a6fed7f5bb13d773a365d851eb4
   r600/sfn: rewrite NIR backend

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7530

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19804>

src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -169,7 +169,19 @@ insert_fsoutput_sorted(struct exec_list *var_list, nir_variable *new_var)
 
    nir_foreach_variable_in_list(var, var_list)
    {
-      if (var->data.location > new_var->data.location ||
+      if ((var->data.location >= FRAG_RESULT_DATA0 ||
+          var->data.location == FRAG_RESULT_COLOR) &&
+          (new_var->data.location < FRAG_RESULT_COLOR ||
+           new_var->data.location == FRAG_RESULT_SAMPLE_MASK)) {
+         exec_node_insert_after(&var->node, &new_var->node);
+         return;
+      } else if ((new_var->data.location >= FRAG_RESULT_DATA0 ||
+                  new_var->data.location == FRAG_RESULT_COLOR) &&
+                 (var->data.location < FRAG_RESULT_COLOR ||
+                  var->data.location == FRAG_RESULT_SAMPLE_MASK)) {
+         exec_node_insert_node_before(&var->node, &new_var->node);
+         return;
+      } else if (var->data.location > new_var->data.location ||
           (var->data.location == new_var->data.location &&
            var->data.index > new_var->data.index)) {
          exec_node_insert_node_before(&var->node, &new_var->node);
@@ -803,6 +815,9 @@ r600_shader_from_nir(struct r600_context *rctx,
    if (sh->info.stage == MESA_SHADER_FRAGMENT) {
       NIR_PASS_V(sh, nir_lower_fragcoord_wtrans);
       NIR_PASS_V(sh, r600_lower_fs_out_to_vector);
+      NIR_PASS_V(sh, nir_opt_dce);
+      NIR_PASS_V(sh, nir_remove_dead_variables, nir_var_shader_out, 0);
+      r600::sort_fsoutput(sh);
    }
    nir_variable_mode io_modes = nir_var_uniform | nir_var_shader_in | nir_var_shader_out;
 
@@ -908,9 +923,6 @@ r600_shader_from_nir(struct r600_context *rctx,
    } while (late_algebraic_progress);
 
    NIR_PASS_V(sh, nir_lower_bool_to_int32);
-
-   if (sh->info.stage == MESA_SHADER_FRAGMENT)
-      r600::sort_fsoutput(sh);
 
    NIR_PASS_V(sh, nir_lower_locals_to_regs);
 

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -46,7 +46,6 @@ FragmentShader::FragmentShader(const r600_shader_key& key):
     m_export_highest(0),
     m_num_color_exports(0),
     m_color_export_mask(0),
-    m_depth_exports(0),
     m_last_pixel_export(nullptr),
     m_pos_input(127, false),
     m_fs_write_all(false),
@@ -511,8 +510,7 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
          unsigned location =
             (m_dual_source_blend && (semantics.location == FRAG_RESULT_COLOR)
                 ? semantics.dual_source_blend_index
-                : driver_location) +
-            k - m_depth_exports;
+                : driver_location) + k;
 
          sfn_log << SfnLog::io << "Pixel output at loc:" << location << "\n";
 
@@ -547,7 +545,6 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
    } else if (semantics.location == FRAG_RESULT_DEPTH ||
               semantics.location == FRAG_RESULT_STENCIL ||
               semantics.location == FRAG_RESULT_SAMPLE_MASK) {
-      m_depth_exports++;
       emit_instruction(new ExportInstr(ExportInstr::pixel, 61, value));
       int semantic = TGSI_SEMANTIC_POSITION;
       if (semantics.location == FRAG_RESULT_STENCIL)

src/gallium/drivers/r600/sfn/sfn_shader_fs.h
@@ -75,7 +75,6 @@ private:
    unsigned m_export_highest;
    unsigned m_num_color_exports;
    unsigned m_color_export_mask;
-   unsigned m_depth_exports;
    ExportInstr *m_last_pixel_export;
 
    std::bitset<s_max_interpolators> m_interpolators_used;