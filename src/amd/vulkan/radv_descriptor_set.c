openharmony/third_party_mesa3d

commit e79e367d9286a1a285f07eea334277c7ba40ee24

radv: Use vk_descriptor_set_layout
Use the common ref counting and the common destroy entrypoint.

Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Jason Ekstrand <jason.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17818>

src/amd/vulkan/radv_descriptor_set.c
@@ -107,15 +107,6 @@ radv_mutable_descriptor_type_size_alignment(const VkMutableDescriptorTypeListVAL
    return true;
 }
 
-void
-radv_descriptor_set_layout_destroy(struct radv_device *device,
-                                   struct radv_descriptor_set_layout *set_layout)
-{
-   assert(set_layout->ref_cnt == 0);
-   vk_object_base_finish(&set_layout->base);
-   vk_free2(&device->vk.alloc, NULL, set_layout);
-}
-
 VKAPI_ATTR VkResult VKAPI_CALL
 radv_CreateDescriptorSetLayout(VkDevice _device, const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
                                const VkAllocationCallbacks *pAllocator,
@@ -166,12 +157,9 @@ radv_CreateDescriptorSetLayout(VkDevice _device, const VkDescriptorSetLayoutCrea
     * they are reference counted and may not be destroyed when vkDestroyDescriptorSetLayout is
     * called.
     */
-   set_layout =
-      vk_zalloc(&device->vk.alloc, size, 8, VK_SYSTEM_ALLOCATION_SCOPE_DEVICE);
+   set_layout = vk_descriptor_set_layout_zalloc(&device->vk, size);
    if (!set_layout)
       return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
-
-   vk_object_base_init(&device->vk, &set_layout->base, VK_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT);
 
    set_layout->flags = pCreateInfo->flags;
    set_layout->layout_size = size;
@@ -197,11 +185,10 @@ radv_CreateDescriptorSetLayout(VkDevice _device, const VkDescriptorSetLayoutCrea
    VkResult result =
       vk_create_sorted_bindings(pCreateInfo->pBindings, pCreateInfo->bindingCount, &bindings);
    if (result != VK_SUCCESS) {
-      radv_descriptor_set_layout_destroy(device, set_layout);
+      vk_descriptor_set_layout_unref(&device->vk, &set_layout->vk);
       return vk_error(device, result);
    }
 
-   set_layout->ref_cnt = 1;
    set_layout->binding_count = num_bindings;
    set_layout->shader_stages = 0;
    set_layout->dynamic_shader_stages = 0;
@@ -366,19 +353,6 @@ radv_CreateDescriptorSetLayout(VkDevice _device, const VkDescriptorSetLayoutCrea
 }
 
 VKAPI_ATTR void VKAPI_CALL
-radv_DestroyDescriptorSetLayout(VkDevice _device, VkDescriptorSetLayout _set_layout,
-                                const VkAllocationCallbacks *pAllocator)
-{
-   RADV_FROM_HANDLE(radv_device, device, _device);
-   RADV_FROM_HANDLE(radv_descriptor_set_layout, set_layout, _set_layout);
-
-   if (!set_layout)
-      return;
-
-   radv_descriptor_set_layout_unref(device, set_layout);
-}
-
-VKAPI_ATTR void VKAPI_CALL
 radv_GetDescriptorSetLayoutSupport(VkDevice device,
                                    const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
                                    VkDescriptorSetLayoutSupport *pSupport)
@@ -517,7 +491,7 @@ radv_pipeline_layout_add_set(struct radv_pipeline_layout *layout, uint32_t set_i
    layout->num_sets = MAX2(set_idx + 1, layout->num_sets);
 
    layout->set[set_idx].layout = set_layout;
-   radv_descriptor_set_layout_ref(set_layout);
+   vk_descriptor_set_layout_ref(&set_layout->vk);
 
    for (uint32_t b = 0; b < set_layout->binding_count; b++) {
       dynamic_offset_count += set_layout->binding[b].array_size * set_layout->binding[b].dynamic_offset_count;
@@ -560,7 +534,7 @@ radv_pipeline_layout_finish(struct radv_device *device, struct radv_pipeline_lay
       if (!layout->set[i].layout)
          continue;
 
-      radv_descriptor_set_layout_unref(device, layout->set[i].layout);
+      vk_descriptor_set_layout_unref(&device->vk, &layout->set[i].layout->vk);
    }
 
    vk_object_base_finish(&layout->base);
@@ -750,7 +724,7 @@ radv_descriptor_set_create(struct radv_device *device, struct radv_descriptor_po
    }
 
    pool->entry_count++;
-   radv_descriptor_set_layout_ref(layout);
+   vk_descriptor_set_layout_ref(&layout->vk);
    *out_set = set;
    return VK_SUCCESS;
 }
@@ -759,7 +733,7 @@ static void
 radv_descriptor_set_destroy(struct radv_device *device, struct radv_descriptor_pool *pool,
                             struct radv_descriptor_set *set, bool free_bo)
 {
-   radv_descriptor_set_layout_unref(device, set->header.layout);
+   vk_descriptor_set_layout_unref(&device->vk, &set->header.layout->vk);
 
    if (pool->host_memory_base)
       return;

src/amd/vulkan/radv_cmd_buffer.c
@@ -428,7 +428,7 @@ radv_destroy_cmd_buffer(struct radv_cmd_buffer *cmd_buffer)
       struct radv_descriptor_set_header *set = &cmd_buffer->descriptors[i].push_set.set;
       free(set->mapped_ptr);
       if (set->layout)
-         radv_descriptor_set_layout_unref(cmd_buffer->device, set->layout);
+         vk_descriptor_set_layout_unref(&cmd_buffer->device->vk, &set->layout->vk);
       vk_object_base_finish(&set->base);
    }
 
@@ -5294,8 +5294,8 @@ radv_init_push_descriptor_set(struct radv_cmd_buffer *cmd_buffer, struct radv_de
 
    if (set->header.layout != layout) {
       if (set->header.layout)
-         radv_descriptor_set_layout_unref(cmd_buffer->device, set->header.layout);
-      radv_descriptor_set_layout_ref(layout);
+         vk_descriptor_set_layout_unref(&cmd_buffer->device->vk, &set->header.layout->vk);
+      vk_descriptor_set_layout_ref(&layout->vk);
       set->header.layout = layout;
    }
 

src/amd/vulkan/radv_descriptor_set.h
@@ -26,6 +26,7 @@
 
 #include "radv_constants.h"
 
+#include "vulkan/runtime/vk_descriptor_set_layout.h"
 #include "vulkan/runtime/vk_object.h"
 
 #include <vulkan/vulkan.h>
@@ -51,10 +52,7 @@ struct radv_descriptor_set_binding_layout {
 };
 
 struct radv_descriptor_set_layout {
-   struct vk_object_base base;
-
-   /* Descriptor set layouts can be destroyed at almost any time */
-   uint32_t ref_cnt;
+   struct vk_descriptor_set_layout vk;
 
    /* Everything below is hashed and shouldn't contain any pointers. Be careful when modifying this
     * structure.

src/amd/vulkan/radv_device_generated_commands.c
@@ -914,9 +914,9 @@ radv_device_finish_dgc_prepare_state(struct radv_device *device)
                         &device->meta_state.alloc);
    radv_DestroyPipelineLayout(radv_device_to_handle(device),
                               device->meta_state.dgc_prepare.p_layout, &device->meta_state.alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device),
-                                   device->meta_state.dgc_prepare.ds_layout,
-                                   &device->meta_state.alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        device->meta_state.dgc_prepare.ds_layout,
+                                                        &device->meta_state.alloc);
 }
 
 VkResult

src/amd/vulkan/radv_meta_blit.c
@@ -647,8 +647,8 @@ radv_device_finish_meta_blit_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->blit.pipeline_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->blit.ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        state->blit.ds_layout, &state->alloc);
 }
 
 static VkResult

src/amd/vulkan/radv_meta_blit2d.c
@@ -601,9 +601,9 @@ radv_device_finish_meta_blit2d_state(struct radv_device *device)
       for (unsigned src = 0; src < BLIT2D_NUM_SRC_TYPES; src++) {
          radv_DestroyPipelineLayout(radv_device_to_handle(device),
                                     state->blit2d[log2_samples].p_layouts[src], &state->alloc);
-         radv_DestroyDescriptorSetLayout(radv_device_to_handle(device),
-                                         state->blit2d[log2_samples].ds_layouts[src],
-                                         &state->alloc);
+         device->vk.dispatch_table.DestroyDescriptorSetLayout(
+            radv_device_to_handle(device), state->blit2d[log2_samples].ds_layouts[src],
+            &state->alloc);
 
          for (unsigned j = 0; j < NUM_META_FS_KEYS; ++j) {
             radv_DestroyPipeline(radv_device_to_handle(device),

src/amd/vulkan/radv_meta_bufimage.c
@@ -198,8 +198,8 @@ radv_device_finish_meta_itob_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->itob.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->itob.img_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        state->itob.img_ds_layout, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->itob.pipeline, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->itob.pipeline_3d, &state->alloc);
 }
@@ -374,8 +374,8 @@ radv_device_finish_meta_btoi_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->btoi.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->btoi.img_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        state->btoi.img_ds_layout, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->btoi.pipeline, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->btoi.pipeline_3d, &state->alloc);
 }
@@ -523,8 +523,8 @@ radv_device_finish_meta_btoi_r32g32b32_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->btoi_r32g32b32.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device),
-                                   state->btoi_r32g32b32.img_ds_layout, &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(
+      radv_device_to_handle(device), state->btoi_r32g32b32.img_ds_layout, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->btoi_r32g32b32.pipeline,
                         &state->alloc);
 }
@@ -718,8 +718,8 @@ radv_device_finish_meta_itoi_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->itoi.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->itoi.img_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        state->itoi.img_ds_layout, &state->alloc);
 
    for (uint32_t i = 0; i < MAX_SAMPLES_LOG2; ++i) {
       radv_DestroyPipeline(radv_device_to_handle(device), state->itoi.pipeline[i], &state->alloc);
@@ -878,8 +878,8 @@ radv_device_finish_meta_itoi_r32g32b32_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->itoi_r32g32b32.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device),
-                                   state->itoi_r32g32b32.img_ds_layout, &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(
+      radv_device_to_handle(device), state->itoi_r32g32b32.img_ds_layout, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->itoi_r32g32b32.pipeline,
                         &state->alloc);
 }
@@ -1032,8 +1032,8 @@ radv_device_finish_meta_cleari_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->cleari.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->cleari.img_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(radv_device_to_handle(device),
+                                                        state->cleari.img_ds_layout, &state->alloc);
 
    for (uint32_t i = 0; i < MAX_SAMPLES_LOG2; ++i) {
       radv_DestroyPipeline(radv_device_to_handle(device), state->cleari.pipeline[i], &state->alloc);
@@ -1149,8 +1149,8 @@ radv_device_finish_meta_cleari_r32g32b32_state(struct radv_device *device)
 
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->cleari_r32g32b32.img_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device),
-                                   state->cleari_r32g32b32.img_ds_layout, &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(
+      radv_device_to_handle(device), state->cleari_r32g32b32.img_ds_layout, &state->alloc);
    radv_DestroyPipeline(radv_device_to_handle(device), state->cleari_r32g32b32.pipeline,
                         &state->alloc);
 }

src/amd/vulkan/radv_meta_clear.c
@@ -245,8 +245,8 @@ finish_meta_clear_htile_mask_state(struct radv_device *device)
                         &state->alloc);
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->clear_htile_mask_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->clear_htile_mask_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(
+      radv_device_to_handle(device), state->clear_htile_mask_ds_layout, &state->alloc);
 }
 
 static void
@@ -260,8 +260,8 @@ finish_meta_clear_dcc_comp_to_single_state(struct radv_device *device)
    }
    radv_DestroyPipelineLayout(radv_device_to_handle(device), state->clear_dcc_comp_to_single_p_layout,
                               &state->alloc);
-   radv_DestroyDescriptorSetLayout(radv_device_to_handle(device), state->clear_dcc_comp_to_single_ds_layout,
-                                   &state->alloc);
+   device->vk.dispatch_table.DestroyDescriptorSetLayout(
+      radv_device_to_handle(device), state->clear_dcc_comp_to_single_ds_layout, &state->alloc);
 }
 
 void

joshuaswarren/mesa-1

commit dba97f99d5e828267b27bf3e48218447f35b25ec

radv: fix hashing descriptor set layout
Shouldn't have pointers.

Fixes: e79e367d928 ("radv: Use vk_descriptor_set_layout")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20250>

src/amd/vulkan/radv_descriptor_set.c
@@ -523,11 +523,11 @@ radv_pipeline_layout_hash(struct radv_pipeline_layout *layout)
       if (!set_layout)
          continue;
 
-      /* Hash the entire set layout except for the vk_object_base and the reference counter. The
-       * rest of the set layout is carefully constructed to not have pointers so a full hash instead
-       * of a per-field hash should be ok.
+      /* Hash the entire set layout except vk_descriptor_set_layout. The rest of the set layout is
+       * carefully constructed to not have pointers so a full hash instead of a per-field hash
+       * should be ok.
        */
-      uint32_t hash_offset = sizeof(struct vk_object_base) + sizeof(uint32_t);
+      uint32_t hash_offset = sizeof(struct vk_descriptor_set_layout);
       _mesa_sha1_update(&ctx, (const char *)set_layout + hash_offset,
                         set_layout->layout_size - hash_offset);
    }