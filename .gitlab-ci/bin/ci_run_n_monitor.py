joshuaswarren/mesa-1

commit a20fd248cff38903e122d51666bb6bf363be2913

ci: allow omitting of --rev for ci_run_n_monitor.py
When --rev is omitted, try to get revision automatically.

Acked-by: Eric Engestrom <eric@igalia.com>
Reviewed-by: Martin Roukala <martin.roukala@mupuf.org>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20271>

.gitlab-ci/bin/ci_run_n_monitor.py
@@ -13,6 +13,7 @@ and show the job(s) logs.
 
 import argparse
 import re
+from subprocess import check_output
 import sys
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -235,7 +236,7 @@ def parse_args() -> None:
     )
     parser.add_argument("--target", metavar="target-job", help="Target job")
     parser.add_argument(
-        "--rev", metavar="revision", help="repository git revision", required=True
+        "--rev", metavar="revision", help="repository git revision (default: HEAD)"
     )
     parser.add_argument(
         "--token",
@@ -279,8 +280,11 @@ if __name__ == "__main__":
 
         cur_project = get_gitlab_project(gl, "mesa")
 
-        print(f"Revision: {args.rev}")
-        pipe = wait_for_pipeline(cur_project, args.rev)
+        REV: str = args.rev
+        if not REV:
+            REV = check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+        print(f"Revision: {REV}")
+        pipe = wait_for_pipeline(cur_project, REV)
         print(f"Pipeline: {pipe.web_url}")
         deps = set()
         if args.target:

commit 9a8a0d9fbe94cec749dbdf5c84fea6cc2eeda935

ci: ci_run_n_monitor.py: fix args.rev leftover
Fixes: a20fd248cff3 ("ci: allow omitting of --rev for ci_run_n_monitor.py")

Reviewed-by: Martin Roukala <martin.roukala@mupuf.org>
Reviewed-by: Guilherme Gallo <guilherme.gallo@collabora.com>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20371>

.gitlab-ci/bin/ci_run_n_monitor.py
@@ -290,7 +290,7 @@ if __name__ == "__main__":
         if args.target:
             print("🞋 job: " + Fore.BLUE + args.target + Style.RESET_ALL)
             deps = find_dependencies(
-                target_job=args.target, sha=args.rev, project_path=cur_project
+                target_job=args.target, sha=REV, project_path=cur_project
             )
         target_job_id, ret = monitor_pipeline(
             cur_project, pipe, args.target, deps, args.force_manual, args.stress