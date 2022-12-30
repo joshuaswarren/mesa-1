joshuaswarren/mesa-1

commit f1443dbb438af2f18d6848877c9f2134c93b56af

freedreno/drm: Return fence from submit flush
This moves away from embedding the submit fence inside the pipe fence,
which lets us start refcnt'ing the fence.  This will enable several
cleanups and improvements:

1. Get rid of fd_bo_fence, and just have fd_bo hold pending fd_fence
   refs instead, which will be needed for cpu_prep implementation of
   sub-allocated buffers.
2. For merged submits, we can just return a new reference to an
   existing fence.

Note that this temporarily defeats submit-merging, which will be
fixed (and improved) in a following commit.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20263>

src/freedreno/drm/freedreno_pipe.c
@@ -211,3 +211,73 @@ fd_pipe_emit_fence(struct fd_pipe *pipe, struct fd_ringbuffer *ring)
 
    return fence;
 }
+
+struct fd_fence *
+fd_fence_new(struct fd_pipe *pipe, bool use_fence_fd)
+{
+   struct fd_fence *f = calloc(1, sizeof(*f));
+
+   f->refcnt = 1;
+   f->pipe = fd_pipe_ref(pipe);
+   util_queue_fence_init(&f->ready);
+   f->use_fence_fd = use_fence_fd;
+   f->fence_fd = -1;
+
+   return f;
+}
+
+struct fd_fence *
+fd_fence_ref(struct fd_fence *f)
+{
+   simple_mtx_lock(&fence_lock);
+   fd_fence_ref_locked(f);
+   simple_mtx_unlock(&fence_lock);
+
+   return f;
+}
+
+struct fd_fence *
+fd_fence_ref_locked(struct fd_fence *f)
+{
+   simple_mtx_assert_locked(&fence_lock);
+   f->refcnt++;
+   return f;
+}
+
+void
+fd_fence_del(struct fd_fence *f)
+{
+   simple_mtx_lock(&fence_lock);
+   fd_fence_del_locked(f);
+   simple_mtx_unlock(&fence_lock);
+}
+
+void
+fd_fence_del_locked(struct fd_fence *f)
+{
+   simple_mtx_assert_locked(&fence_lock);
+
+   if (--f->refcnt)
+      return;
+
+   fd_pipe_del_locked(f->pipe);
+
+   if (f->use_fence_fd && (f->fence_fd != -1))
+      close(f->fence_fd);
+
+   free(f);
+}
+
+/**
+ * Wait until corresponding submit is flushed to kernel
+ */
+void
+fd_fence_flush(struct fd_fence *f)
+{
+   /*
+    * TODO we could simplify this to remove the flush_sync part of
+    * fd_pipe_sp_flush() and just rely on the util_queue_fence_wait()
+    */
+   fd_pipe_flush(f->pipe, f->ufence);
+   util_queue_fence_wait(&f->ready);
+}

src/freedreno/computerator/main.c
@@ -281,12 +281,10 @@ main(int argc, char **argv)
 
    backend->emit_grid(kernel, grid, submit);
 
-   struct fd_fence fence = {};
-   util_queue_fence_init(&fence.ready);
+   struct fd_fence *fence = fd_submit_flush(submit, -1, false);
 
-   fd_submit_flush(submit, -1, &fence);
-
-   util_queue_fence_wait(&fence.ready);
+   fd_fence_flush(fence);
+   fd_fence_del(fence);
 
    for (int i = 0; i < kernel->num_bufs; i++) {
       fd_bo_cpu_prep(kernel->bufs[i], pipe, FD_BO_PREP_READ);

src/freedreno/drm/freedreno_drmif.h
@@ -100,6 +100,14 @@ fd_fence_after(uint32_t a, uint32_t b)
  */
 struct fd_fence {
    /**
+    * Note refcnt is *not* atomic, but protected by fence_lock, since the
+    * fence_lock is held in fd_bo_add_fence(), which is the hotpath.
+    */
+   int32_t refcnt;
+
+   struct fd_pipe *pipe;
+
+   /**
     * The ready fence is signaled once the submit is actually flushed down
     * to the kernel, and fence/fence_fd are populated.  You must wait for
     * this fence to be signaled before reading fence/fence_fd.
@@ -115,6 +123,13 @@ struct fd_fence {
    int fence_fd;
    bool use_fence_fd;
 };
+
+struct fd_fence *fd_fence_new(struct fd_pipe *pipe, bool use_fence_fd);
+struct fd_fence *fd_fence_ref(struct fd_fence *f);
+struct fd_fence *fd_fence_ref_locked(struct fd_fence *f);
+void fd_fence_del(struct fd_fence *f);
+void fd_fence_del_locked(struct fd_fence *f);
+void fd_fence_flush(struct fd_fence *f);
 
 /*
  * bo flags:

src/freedreno/drm/freedreno_priv.h
@@ -292,8 +292,8 @@ struct fd_submit_funcs {
    struct fd_ringbuffer *(*new_ringbuffer)(struct fd_submit *submit,
                                            uint32_t size,
                                            enum fd_ringbuffer_flags flags);
-   int (*flush)(struct fd_submit *submit, int in_fence_fd,
-                struct fd_fence *out_fence);
+   struct fd_fence *(*flush)(struct fd_submit *submit, int in_fence_fd,
+                             bool use_fence_fd);
    void (*destroy)(struct fd_submit *submit);
 };
 

src/freedreno/drm/freedreno_ringbuffer.c
@@ -60,12 +60,11 @@ fd_submit_ref(struct fd_submit *submit)
    return submit;
 }
 
-int
-fd_submit_flush(struct fd_submit *submit, int in_fence_fd,
-                struct fd_fence *out_fence)
+struct fd_fence *
+fd_submit_flush(struct fd_submit *submit, int in_fence_fd, bool use_fence_fd)
 {
    submit->fence = fd_pipe_emit_fence(submit->pipe, submit->primary);
-   return submit->funcs->flush(submit, in_fence_fd, out_fence);
+   return submit->funcs->flush(submit, in_fence_fd, use_fence_fd);
 }
 
 struct fd_ringbuffer *

src/freedreno/drm/freedreno_ringbuffer.h
@@ -93,10 +93,10 @@ struct fd_ringbuffer *fd_submit_new_ringbuffer(struct fd_submit *submit,
                                                enum fd_ringbuffer_flags flags);
 
 /* in_fence_fd: -1 for no in-fence, else fence fd
- * out_fence can be NULL if no output fence is required
+ * if use_fence_fd is true the output fence will be dma_fence fd backed
  */
-int fd_submit_flush(struct fd_submit *submit, int in_fence_fd,
-                    struct fd_fence *out_fence);
+struct fd_fence *fd_submit_flush(struct fd_submit *submit, int in_fence_fd,
+                                 bool use_fence_fd);
 
 struct fd_ringbuffer;
 struct fd_reloc;

src/freedreno/drm/freedreno_ringbuffer_sp.c
@@ -186,7 +186,7 @@ fd_submit_sp_flush_prep(struct fd_submit *submit, int in_fence_fd,
    }
    simple_mtx_unlock(&fence_lock);
 
-   fd_submit->out_fence   = out_fence;
+   fd_submit->out_fence   = fd_fence_ref(out_fence);
    fd_submit->in_fence_fd = (in_fence_fd == -1) ?
          -1 : os_dupfd_cloexec(in_fence_fd);
 
@@ -218,7 +218,7 @@ fd_submit_sp_flush_cleanup(void *job, void *gdata, int thread_index)
    fd_submit_del(submit);
 }
 
-static int
+static void
 enqueue_submit_list(struct list_head *submit_list)
 {
    struct fd_submit *submit = last_submit(submit_list);
@@ -227,13 +227,7 @@ enqueue_submit_list(struct list_head *submit_list)
    list_replace(submit_list, &fd_submit->submit_list);
    list_inithead(submit_list);
 
-   struct util_queue_fence *fence;
-   if (fd_submit->out_fence) {
-      fence = &fd_submit->out_fence->ready;
-   } else {
-      util_queue_fence_init(&fd_submit->fence);
-      fence = &fd_submit->fence;
-   }
+   struct util_queue_fence *fence = &fd_submit->out_fence->ready;
 
    DEBUG_MSG("enqueue: %u", submit->fence);
 
@@ -242,8 +236,6 @@ enqueue_submit_list(struct list_head *submit_list)
                       fd_submit_sp_flush_execute,
                       fd_submit_sp_flush_cleanup,
                       0);
-
-   return 0;
 }
 
 static bool
@@ -266,9 +258,8 @@ should_defer(struct fd_submit *submit)
    return true;
 }
 
-static int
-fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd,
-                   struct fd_fence *out_fence)
+static struct fd_fence *
+fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd, bool use_fence_fd)
 {
    struct fd_device *dev = submit->pipe->dev;
    struct fd_pipe *pipe = submit->pipe;
@@ -295,6 +286,8 @@ fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd,
 
    list_addtail(&fd_submit_ref(submit)->node, &dev->deferred_submits);
 
+   struct fd_fence *out_fence = fd_fence_new(submit->pipe, use_fence_fd);
+
    bool has_shared = fd_submit_sp_flush_prep(submit, in_fence_fd, out_fence);
 
    /* The rule about skipping submit merging with shared buffers is only
@@ -318,7 +311,7 @@ fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd,
       assert(dev->deferred_cmds == fd_dev_count_deferred_cmds(dev));
       simple_mtx_unlock(&dev->submit_lock);
 
-      return 0;
+      return out_fence;
    }
 
    struct list_head submit_list;
@@ -329,7 +322,9 @@ fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd,
 
    simple_mtx_unlock(&dev->submit_lock);
 
-   return enqueue_submit_list(&submit_list);
+   enqueue_submit_list(&submit_list);
+
+   return out_fence;
 }
 
 void
@@ -399,6 +394,9 @@ fd_submit_sp_destroy(struct fd_submit *submit)
    slab_destroy_child(&fd_submit->ring_pool);
 
    fd_bo_del_array(fd_submit->bos, fd_submit->nr_bos);
+
+   if (fd_submit->out_fence)
+      fd_fence_del(fd_submit->out_fence);
 
    free(fd_submit->bos);
    free(fd_submit);

src/freedreno/drm/freedreno_ringbuffer_sp.h
@@ -73,11 +73,7 @@ struct fd_submit_sp {
     */
    struct list_head submit_list;   /* includes this submit as last element */
 
-   /* Used in case out_fence==NULL: */
-   struct util_queue_fence fence;
-
    /* Used by retire_queue, if used by backend: */
-   int out_fence_fd;
    struct util_queue_fence retire_fence;
 
    flush_submit_list_fn flush_submit_list;

commit 03284a9518b9e6e498c093a86042a52fe0ff2cc6

freedreno/drm: Flush deferred submits at context teardown
If we don't flush deferred submits, we could end up waiting on a fence
that is never signaled.

Fixes: f1443dbb438 ("freedreno/drm: Return fence from submit flush")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20465>

src/freedreno/drm/freedreno_pipe.c
@@ -126,38 +126,30 @@ fd_pipe_del_locked(struct fd_pipe *pipe)
 }
 
 /**
- * Discard any unflushed deferred submits.  This is called at context-
+ * Flush any unflushed deferred submits.  This is called at context-
  * destroy to make sure we don't leak unflushed submits.
  */
 void
 fd_pipe_purge(struct fd_pipe *pipe)
 {
    struct fd_device *dev = pipe->dev;
-   struct list_head deferred_submits;
-
-   list_inithead(&deferred_submits);
+   struct fd_fence *unflushed_fence = NULL;
 
    simple_mtx_lock(&dev->submit_lock);
 
-   foreach_submit_safe (deferred_submit, &dev->deferred_submits) {
-      if (deferred_submit->pipe != pipe)
-         continue;
-
-      list_del(&deferred_submit->node);
-      list_addtail(&deferred_submit->node, &deferred_submits);
-      dev->deferred_cmds -= fd_ringbuffer_cmd_count(deferred_submit->primary);
-   }
-
-   if (list_is_empty(&dev->deferred_submits) && dev->deferred_submits_fence) {
-      fd_fence_del(dev->deferred_submits_fence);
-      dev->deferred_submits_fence = NULL;
+   /* We only queue up deferred submits for a single pipe at a time, so
+    * if there is a deferred_submits_fence on the same pipe as us, we
+    * know we have deferred_submits queued, which need to be flushed:
+    */
+   if (dev->deferred_submits_fence && dev->deferred_submits_fence->pipe == pipe) {
+      unflushed_fence = fd_fence_ref(dev->deferred_submits_fence);
    }
 
    simple_mtx_unlock(&dev->submit_lock);
 
-   foreach_submit_safe (deferred_submit, &deferred_submits) {
-      list_del(&deferred_submit->node);
-      fd_submit_del(deferred_submit);
+   if (unflushed_fence) {
+      fd_fence_flush(unflushed_fence);
+      fd_fence_del(unflushed_fence);
    }
 }
 