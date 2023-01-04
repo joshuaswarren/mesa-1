openharmony/third_party_mesa3d

commit be2c3a8f4eeef71fccbbaf14c6d1ea6fa9c49d40

anv: implement u_trace support
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Acked-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13996>

src/intel/vulkan/anv_genX.h
@@ -143,8 +143,9 @@ void genX(blorp_exec)(struct blorp_batch *batch,
                       const struct blorp_params *params);
 
 void genX(cmd_emit_timestamp)(struct anv_batch *batch,
-                              struct anv_bo *bo,
-                              uint32_t offset);
+                              struct anv_device *device,
+                              struct anv_address addr,
+                              bool end_of_pipe);
 
 void
 genX(rasterization_mode)(VkPolygonMode raster_mode,

src/intel/vulkan/anv_measure.c
@@ -158,7 +158,11 @@ anv_measure_start_snapshot(struct anv_cmd_buffer *cmd_buffer,
 
    unsigned index = measure->base.index++;
 
-   (*device->cmd_emit_timestamp)(batch, measure->bo, index * sizeof(uint64_t));
+   (*device->cmd_emit_timestamp)(batch, cmd_buffer->device,
+                                 (struct anv_address) {
+                                    .bo = measure->bo,
+                                    .offset = index * sizeof(uint64_t) },
+                                 true /* end_of_pipe */);
 
    if (event_name == NULL)
       event_name = intel_measure_snapshot_string(type);
@@ -195,7 +199,11 @@ anv_measure_end_snapshot(struct anv_cmd_buffer *cmd_buffer,
    unsigned index = measure->base.index++;
    assert(index % 2 == 1);
 
-   (*device->cmd_emit_timestamp)(batch, measure->bo, index * sizeof(uint64_t));
+   (*device->cmd_emit_timestamp)(batch, cmd_buffer->device,
+                                 (struct anv_address) {
+                                    .bo = measure->bo,
+                                    .offset = index * sizeof(uint64_t) },
+                                 true /* end_of_pipe */);
 
    struct intel_measure_snapshot *snapshot = &(measure->base.snapshots[index]);
    memset(snapshot, 0, sizeof(*snapshot));

src/intel/vulkan/anv_private.h
@@ -57,6 +57,7 @@
 #include "util/macros.h"
 #include "util/hash_table.h"
 #include "util/list.h"
+#include "util/perf/u_trace.h"
 #include "util/sparse_array.h"
 #include "util/u_atomic.h"
 #include "util/u_vector.h"
@@ -552,6 +553,46 @@ anv_bo_is_pinned(struct anv_bo *bo)
 #endif
 }
 
+struct anv_address {
+   struct anv_bo *bo;
+   int64_t offset;
+};
+
+#define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
+
+static inline struct anv_address
+anv_address_from_u64(uint64_t addr_u64)
+{
+   assert(addr_u64 == intel_canonical_address(addr_u64));
+   return (struct anv_address) {
+      .bo = NULL,
+      .offset = addr_u64,
+   };
+}
+
+static inline bool
+anv_address_is_null(struct anv_address addr)
+{
+   return addr.bo == NULL && addr.offset == 0;
+}
+
+static inline uint64_t
+anv_address_physical(struct anv_address addr)
+{
+   if (addr.bo && anv_bo_is_pinned(addr.bo)) {
+      return intel_canonical_address(addr.bo->offset + addr.offset);
+   } else {
+      return intel_canonical_address(addr.offset);
+   }
+}
+
+static inline struct anv_address
+anv_address_add(struct anv_address addr, uint64_t offset)
+{
+   addr.offset += offset;
+   return addr;
+}
+
 /* Represents a lock-free linked list of "free" things.  This is used by
  * both the block pool and the state pools.  Unfortunately, in order to
  * solve the ABA problem, we can't use a single uint32_t head.
@@ -986,7 +1027,7 @@ struct anv_physical_device {
     int64_t                                     master_minor;
     struct drm_i915_query_engine_info *         engine_info;
 
-    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_bo *, uint32_t );
+    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, bool);
     struct intel_measure_device                 measure_device;
 };
 
@@ -1094,11 +1135,6 @@ anv_device_upload_nir(struct anv_device *device,
                       const struct nir_shader *nir,
                       unsigned char sha1_key[20]);
 
-struct anv_address {
-   struct anv_bo *bo;
-   int64_t offset;
-};
-
 struct anv_device {
     struct vk_device                            vk;
 
@@ -1179,6 +1215,8 @@ struct anv_device {
     const struct intel_l3_config                *l3_config;
 
     struct intel_debug_block_frame              *debug_frame_desc;
+
+    struct u_trace_context                      trace_context;
 };
 
 #if defined(GFX_VERx10) && GFX_VERx10 >= 90
@@ -1504,42 +1542,6 @@ anv_batch_emit_reloc(struct anv_batch *batch,
    }
 
    return address_u64;
-}
-
-
-#define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
-
-static inline struct anv_address
-anv_address_from_u64(uint64_t addr_u64)
-{
-   assert(addr_u64 == intel_canonical_address(addr_u64));
-   return (struct anv_address) {
-      .bo = NULL,
-      .offset = addr_u64,
-   };
-}
-
-static inline bool
-anv_address_is_null(struct anv_address addr)
-{
-   return addr.bo == NULL && addr.offset == 0;
-}
-
-static inline uint64_t
-anv_address_physical(struct anv_address addr)
-{
-   if (addr.bo && anv_bo_is_pinned(addr.bo)) {
-      return intel_canonical_address(addr.bo->offset + addr.offset);
-   } else {
-      return intel_canonical_address(addr.offset);
-   }
-}
-
-static inline struct anv_address
-anv_address_add(struct anv_address addr, uint64_t offset)
-{
-   addr.offset += offset;
-   return addr;
 }
 
 static inline void
@@ -3088,6 +3090,11 @@ struct anv_cmd_buffer {
     * Used to increase allocation size for long command buffers.
     */
    uint32_t                                     total_batch_size;
+
+   /**
+    *
+    */
+   struct u_trace                               trace;
 };
 
 /* Determine whether we can chain a given cmd_buffer to another one. We need
@@ -4540,6 +4547,29 @@ struct anv_memcpy_state {
    struct anv_vb_cache_range vb_bound;
    struct anv_vb_cache_range vb_dirty;
 };
+
+struct anv_utrace_flush_copy {
+   struct u_trace trace;
+
+   struct anv_reloc_list relocs;
+   struct anv_batch batch;
+   struct anv_bo *batch_bo;
+
+   struct anv_bo *trace_bo;
+
+   struct vk_sync *sync;
+
+   struct anv_memcpy_state memcpy_state;
+};
+
+void anv_device_utrace_init(struct anv_device *device);
+void anv_device_utrace_finish(struct anv_device *device);
+VkResult
+anv_device_utrace_flush_cmd_buffers(struct anv_queue *queue,
+                                    uint32_t cmd_buffer_count,
+                                    struct anv_cmd_buffer **cmd_buffers,
+                                    struct anv_utrace_flush_copy **out_flush_data);
+
 
 #define ANV_FROM_HANDLE(__anv_type, __name, __handle) \
    VK_FROM_HANDLE(__anv_type, __name, __handle)

src/intel/vulkan/anv_utrace.c
@@ -1,0 +1,279 @@
+/*
+ * Copyright © 2021 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "anv_private.h"
+
+#include "perf/intel_perf.h"
+
+static uint32_t
+command_buffers_count_utraces(struct anv_device *device,
+                              uint32_t cmd_buffer_count,
+                              struct anv_cmd_buffer **cmd_buffers,
+                              uint32_t *utrace_copies)
+{
+   if (!u_trace_context_actively_tracing(&device->trace_context))
+      return 0;
+
+   uint32_t utraces = 0;
+   for (uint32_t i = 0; i < cmd_buffer_count; i++) {
+      if (u_trace_has_points(&cmd_buffers[i]->trace)) {
+         utraces++;
+         if (!(cmd_buffers[i]->usage_flags & VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT))
+            *utrace_copies += list_length(&cmd_buffers[i]->trace.trace_chunks);
+      }
+   }
+
+   return utraces;
+}
+
+static void
+anv_utrace_delete_flush_data(struct u_trace_context *utctx,
+                             void *flush_data)
+{
+   struct anv_device *device =
+      container_of(utctx, struct anv_device, trace_context);
+   struct anv_utrace_flush_copy *flush = flush_data;
+
+   u_trace_fini(&flush->trace);
+
+   if (flush->trace_bo) {
+      assert(flush->batch_bo);
+      anv_reloc_list_finish(&flush->relocs, &device->vk.alloc);
+      anv_device_release_bo(device, flush->batch_bo);
+      anv_device_release_bo(device, flush->trace_bo);
+   }
+
+   vk_sync_destroy(&device->vk, flush->sync);
+
+   vk_free(&device->vk.alloc, flush);
+}
+
+static void
+anv_device_utrace_emit_copy_ts_buffer(struct u_trace_context *utctx,
+                                      void *cmdstream,
+                                      void *ts_from, uint32_t from_offset,
+                                      void *ts_to, uint32_t to_offset,
+                                      uint32_t count)
+{
+   struct anv_device *device =
+      container_of(utctx, struct anv_device, trace_context);
+   struct anv_utrace_flush_copy *flush = cmdstream;
+   struct anv_address from_addr = (struct anv_address) {
+      .bo = ts_from, .offset = from_offset * sizeof(uint64_t) };
+   struct anv_address to_addr = (struct anv_address) {
+      .bo = ts_to, .offset = to_offset * sizeof(uint64_t) };
+
+   anv_genX(&device->info, emit_so_memcpy)(&flush->memcpy_state,
+                                           to_addr, from_addr, count * sizeof(uint64_t));
+}
+
+VkResult
+anv_device_utrace_flush_cmd_buffers(struct anv_queue *queue,
+                                    uint32_t cmd_buffer_count,
+                                    struct anv_cmd_buffer **cmd_buffers,
+                                    struct anv_utrace_flush_copy **out_flush_data)
+{
+   struct anv_device *device = queue->device;
+   uint32_t utrace_copies = 0;
+   uint32_t utraces = command_buffers_count_utraces(device,
+                                                    cmd_buffer_count,
+                                                    cmd_buffers,
+                                                    &utrace_copies);
+   if (!utraces) {
+      *out_flush_data = NULL;
+      return VK_SUCCESS;
+   }
+
+   VkResult result;
+   struct anv_utrace_flush_copy *flush =
+      vk_zalloc(&device->vk.alloc, sizeof(struct anv_utrace_flush_copy),
+                8, VK_SYSTEM_ALLOCATION_SCOPE_DEVICE);
+   if (!flush)
+      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   u_trace_init(&flush->trace, &device->trace_context);
+
+   result = vk_sync_create(&device->vk, &device->physical->sync_syncobj_type,
+                           0, 0, &flush->sync);
+   if (result != VK_SUCCESS)
+      goto error_sync;
+
+   if (utrace_copies > 0) {
+      result =
+         anv_device_alloc_bo(device, "utrace-copy-buf", utrace_copies * 4096,
+                             ANV_BO_ALLOC_MAPPED, 0 /* explicit_address */,
+                             &flush->trace_bo);
+      if (result != VK_SUCCESS)
+         goto error_trace_buf;
+
+      result =
+         anv_device_alloc_bo(device, "utrace-copy-batch",
+                             /* 128 dwords of setup + 64 dwords per copy */
+                             align_u32(512 + 64 * utrace_copies, 4096),
+                             ANV_BO_ALLOC_MAPPED, 0 /* explicit_address */,
+                             &flush->batch_bo);
+      if (result != VK_SUCCESS)
+         goto error_batch_buf;
+
+      result = anv_reloc_list_init(&flush->relocs, &device->vk.alloc);
+      if (result != VK_SUCCESS)
+         goto error_reloc_list;
+
+      flush->batch.alloc = &device->vk.alloc;
+      flush->batch.relocs = &flush->relocs;
+      anv_batch_set_storage(&flush->batch,
+                            (struct anv_address) { .bo = flush->batch_bo, },
+                            flush->batch_bo->map, flush->batch_bo->size);
+
+      /* Emit the copies */
+      anv_genX(&device->info, emit_so_memcpy_init)(&flush->memcpy_state,
+                                                   device,
+                                                   &flush->batch);
+      for (uint32_t i = 0; i < cmd_buffer_count; i++) {
+         if (cmd_buffers[i]->usage_flags & VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT) {
+            u_trace_flush(&cmd_buffers[i]->trace, flush, false);
+         } else {
+            u_trace_clone_append(u_trace_begin_iterator(&cmd_buffers[i]->trace),
+                                 u_trace_end_iterator(&cmd_buffers[i]->trace),
+                                 &flush->trace,
+                                 flush,
+                                 anv_device_utrace_emit_copy_ts_buffer);
+         }
+      }
+      anv_genX(&device->info, emit_so_memcpy_fini)(&flush->memcpy_state);
+
+      u_trace_flush(&flush->trace, flush, true);
+
+      if (flush->batch.status != VK_SUCCESS) {
+         result = flush->batch.status;
+         goto error_batch;
+      }
+   } else {
+      for (uint32_t i = 0; i < cmd_buffer_count; i++) {
+         assert(cmd_buffers[i]->usage_flags & VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+         u_trace_flush(&cmd_buffers[i]->trace, flush, i == (cmd_buffer_count - 1));
+      }
+   }
+
+   *out_flush_data = flush;
+
+   return VK_SUCCESS;
+
+ error_batch:
+   anv_reloc_list_finish(&flush->relocs, &device->vk.alloc);
+ error_reloc_list:
+   anv_device_release_bo(device, flush->batch_bo);
+ error_batch_buf:
+   anv_device_release_bo(device, flush->trace_bo);
+ error_trace_buf:
+   vk_sync_destroy(&device->vk, flush->sync);
+ error_sync:
+   vk_free(&device->vk.alloc, flush);
+   return result;
+}
+
+static void *
+anv_utrace_create_ts_buffer(struct u_trace_context *utctx, uint32_t size_b)
+{
+   struct anv_device *device =
+      container_of(utctx, struct anv_device, trace_context);
+
+   struct anv_bo *bo = NULL;
+   UNUSED VkResult result =
+      anv_device_alloc_bo(device, "utrace-ts", align_u32(size_b, 4096),
+                          ANV_BO_ALLOC_MAPPED, 0, &bo);
+   assert(result == VK_SUCCESS);
+
+   return bo;
+}
+
+static void
+anv_utrace_destroy_ts_buffer(struct u_trace_context *utctx, void *timestamps)
+{
+   struct anv_device *device =
+      container_of(utctx, struct anv_device, trace_context);
+   struct anv_bo *bo = timestamps;
+
+   anv_device_release_bo(device, bo);
+}
+
+static void
+anv_utrace_record_ts(struct u_trace *ut, void *cs, void *timestamps, unsigned idx,
+                     bool end_of_pipe)
+{
+   struct anv_cmd_buffer *cmd_buffer = cs;
+   struct anv_device *device = cmd_buffer->device;
+   struct anv_bo *bo = timestamps;
+
+   device->physical->cmd_emit_timestamp(&cmd_buffer->batch, device,
+                                        (struct anv_address) {
+                                           .bo = bo,
+                                           .offset = idx * sizeof(uint64_t) },
+                                        end_of_pipe);
+}
+
+static uint64_t
+anv_utrace_read_ts(struct u_trace_context *utctx,
+                   void *timestamps, unsigned idx, void *flush_data)
+{
+   struct anv_device *device =
+      container_of(utctx, struct anv_device, trace_context);
+   struct anv_bo *bo = timestamps;
+   struct anv_utrace_flush_copy *flush = flush_data;
+
+   /* Only need to stall on results for the first entry: */
+   if (idx == 0) {
+      UNUSED VkResult result =
+         vk_sync_wait(&device->vk,
+                      flush->sync,
+                      0,
+                      VK_SYNC_WAIT_COMPLETE,
+                      os_time_get_absolute_timeout(OS_TIMEOUT_INFINITE));
+      assert(result == VK_SUCCESS);
+   }
+
+   uint64_t *ts = bo->map;
+
+   /* Don't translate the no-timestamp marker: */
+   if (ts[idx] == U_TRACE_NO_TIMESTAMP)
+      return U_TRACE_NO_TIMESTAMP;
+
+   return intel_device_info_timebase_scale(&device->info, ts[idx]);
+}
+
+void
+anv_device_utrace_init(struct anv_device *device)
+{
+   u_trace_context_init(&device->trace_context, device,
+                        anv_utrace_create_ts_buffer,
+                        anv_utrace_destroy_ts_buffer,
+                        anv_utrace_record_ts,
+                        anv_utrace_read_ts,
+                        anv_utrace_delete_flush_data);
+}
+
+void
+anv_device_utrace_finish(struct anv_device *device)
+{
+   u_trace_context_fini(&device->trace_context);
+}

joshuaswarren/mesa-1

commit 08321f6d7a13661fe36c68b6cdaf4a91338e3a27

anv: Emit CS stall on INTEL_MEASURE timestamp
For INTEL_MEASURE, ensure all prior instructions completed before
timestamp taken. Continue to support no CS flush case for Perfetto.
CS stall was dropped from pipecontrol when adding u_trace support.

Fixes: be2c3a8f4ee ("anv: implement u_trace support")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20502>

src/intel/vulkan/anv_genX.h
@@ -153,7 +153,7 @@ void genX(blorp_exec)(struct blorp_batch *batch,
 void genX(cmd_emit_timestamp)(struct anv_batch *batch,
                               struct anv_device *device,
                               struct anv_address addr,
-                              bool end_of_pipe);
+                              enum anv_timestamp_capture_type);
 
 void genX(batch_emit_dummy_post_sync_op)(struct anv_batch *batch,
                                          struct anv_device *device,

src/intel/vulkan/anv_measure.c
@@ -153,7 +153,7 @@ anv_measure_start_snapshot(struct anv_cmd_buffer *cmd_buffer,
                                  (struct anv_address) {
                                     .bo = measure->bo,
                                     .offset = index * sizeof(uint64_t) },
-                                 true /* end_of_pipe */);
+                                 ANV_TIMESTAMP_CAPTURE_AT_CS_STALL);
 
    if (event_name == NULL)
       event_name = intel_measure_snapshot_string(type);
@@ -196,7 +196,7 @@ anv_measure_end_snapshot(struct anv_cmd_buffer *cmd_buffer,
                                  (struct anv_address) {
                                     .bo = measure->bo,
                                     .offset = index * sizeof(uint64_t) },
-                                 true /* end_of_pipe */);
+                                 ANV_TIMESTAMP_CAPTURE_AT_CS_STALL);
 
    struct intel_measure_snapshot *snapshot = &(measure->base.snapshots[index]);
    memset(snapshot, 0, sizeof(*snapshot));

src/intel/vulkan/anv_private.h
@@ -954,6 +954,12 @@ struct anv_memregion {
    uint64_t available;
 };
 
+enum anv_timestamp_capture_type {
+    ANV_TIMESTAMP_CAPTURE_TOP_OF_PIPE,
+    ANV_TIMESTAMP_CAPTURE_END_OF_PIPE,
+    ANV_TIMESTAMP_CAPTURE_AT_CS_STALL,
+};
+
 struct anv_physical_device {
     struct vk_physical_device                   vk;
 
@@ -1061,7 +1067,7 @@ struct anv_physical_device {
     int64_t                                     master_minor;
     struct intel_query_engine_info *            engine_info;
 
-    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, bool);
+    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, enum anv_timestamp_capture_type);
     struct intel_measure_device                 measure_device;
 };
 

src/intel/vulkan/anv_utrace.c
@@ -227,11 +227,14 @@ anv_utrace_record_ts(struct u_trace *ut, void *cs,
    struct anv_device *device = cmd_buffer->device;
    struct anv_bo *bo = timestamps;
 
+   enum anv_timestamp_capture_type capture_type =
+      (end_of_pipe) ? ANV_TIMESTAMP_CAPTURE_END_OF_PIPE
+                    : ANV_TIMESTAMP_CAPTURE_TOP_OF_PIPE;
    device->physical->cmd_emit_timestamp(&cmd_buffer->batch, device,
                                         (struct anv_address) {
                                            .bo = bo,
                                            .offset = idx * sizeof(uint64_t) },
-                                        end_of_pipe);
+                                        capture_type);
 }
 
 static uint64_t

src/intel/vulkan/genX_cmd_buffer.c
@@ -7808,17 +7808,34 @@ VkResult genX(CmdSetPerformanceStreamMarkerINTEL)(
 void genX(cmd_emit_timestamp)(struct anv_batch *batch,
                               struct anv_device *device,
                               struct anv_address addr,
-                              bool end_of_pipe) {
-   if (end_of_pipe) {
+                              enum anv_timestamp_capture_type type) {
+   switch (type) {
+   case ANV_TIMESTAMP_CAPTURE_TOP_OF_PIPE: {
+      struct mi_builder b;
+      mi_builder_init(&b, device->info, batch);
+      mi_store(&b, mi_mem64(addr), mi_reg64(TIMESTAMP));
+      break;
+   }
+
+   case ANV_TIMESTAMP_CAPTURE_END_OF_PIPE:
       anv_batch_emit(batch, GENX(PIPE_CONTROL), pc) {
          pc.PostSyncOperation   = WriteTimestamp;
          pc.Address             = addr;
          anv_debug_dump_pc(pc);
       }
-   } else {
-      struct mi_builder b;
-      mi_builder_init(&b, device->info, batch);
-      mi_store(&b, mi_mem64(addr), mi_reg64(TIMESTAMP));
+      break;
+
+   case ANV_TIMESTAMP_CAPTURE_AT_CS_STALL:
+      anv_batch_emit(batch, GENX(PIPE_CONTROL), pc) {
+         pc.CommandStreamerStallEnable = true;
+         pc.PostSyncOperation    = WriteTimestamp;
+         pc.Address              = addr;
+         anv_debug_dump_pc(pc);
+      }
+      break;
+
+   default:
+      unreachable("invalid");
    }
 }
 