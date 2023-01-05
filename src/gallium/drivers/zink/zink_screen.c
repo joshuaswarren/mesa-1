joshuaswarren/mesa-1

commit 7db6e0cefeb05a72d4e23a1c7bfc9dfd129fd08d

zink: export PIPE_CAP_SHADER_ATOMIC_INT64
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18144>

src/gallium/drivers/zink/zink_screen.c
@@ -524,6 +524,8 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
 
    case PIPE_CAP_IMAGE_ATOMIC_FLOAT_ADD:
       return screen->info.have_EXT_shader_atomic_float;
+   case PIPE_CAP_SHADER_ATOMIC_INT64:
+      return screen->info.have_KHR_shader_atomic_int64;
 
    case PIPE_CAP_MULTI_DRAW_INDIRECT_PARAMS:
       return screen->info.have_KHR_draw_indirect_count;

src/gallium/drivers/zink/zink_device_info.py
@@ -89,6 +89,7 @@ EXTENSIONS = [
     Extension("VK_EXT_shader_subgroup_ballot"),
     Extension("VK_EXT_shader_subgroup_vote"),
     Extension("VK_EXT_shader_atomic_float", alias="atomic_float", features=True),
+    Extension("VK_KHR_shader_atomic_int64", alias="atomic_int", features=True),
     Extension("VK_KHR_8bit_storage",
               alias="storage_8bit",
               features=True,

commit 5e502b8bdb8c74a61eca061c16c40ed6f29550f9

zink: Only expose PIPE_CAP_SHADER_ATOMIC_INT64 if we can do shared and ssbos.
Fixes: 7db6e0cefeb0 ("zink: export PIPE_CAP_SHADER_ATOMIC_INT64")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20506>

src/gallium/drivers/zink/zink_screen.c
@@ -599,7 +599,9 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
               screen->info.atomic_float_feats.shaderSharedFloat32AtomicAdd &&
               screen->info.atomic_float_feats.shaderBufferFloat32AtomicAdd);
    case PIPE_CAP_SHADER_ATOMIC_INT64:
-      return screen->info.have_KHR_shader_atomic_int64;
+      return (screen->info.have_KHR_shader_atomic_int64 &&
+              screen->info.atomic_int_feats.shaderSharedInt64Atomics &&
+              screen->info.atomic_int_feats.shaderBufferInt64Atomics);
 
    case PIPE_CAP_MULTI_DRAW_INDIRECT_PARAMS:
       return screen->info.have_KHR_draw_indirect_count;