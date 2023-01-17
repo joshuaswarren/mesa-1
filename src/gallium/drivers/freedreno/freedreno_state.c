openharmony/third_party_mesa3d

commit fdb460180f6ab8388aec31561f404b4b9f17d55d

freedreno: simplify fd_set_shader_buffers(..)
Clear the modified bits for enabled_mask and then iterate over the
whole range and set the specific bit where there is a buffer.

Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/1963>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -128,11 +128,13 @@ fd_set_shader_buffers(struct pipe_context *pctx,
 	struct fd_shaderbuf_stateobj *so = &ctx->shaderbuf[shader];
 	const unsigned modified_bits = u_bit_consecutive(start, count);
 
-	if (buffers) {
-		for (unsigned i = 0; i < count; i++) {
-			unsigned n = i + start;
-			struct pipe_shader_buffer *buf = &so->sb[n];
+	so->enabled_mask &= ~modified_bits;
 
+	for (unsigned i = 0; i < count; i++) {
+		unsigned n = i + start;
+		struct pipe_shader_buffer *buf = &so->sb[n];
+
+		if (buffers && buffers[i].buffer) {
 			if ((buf->buffer == buffers[i].buffer) &&
 					(buf->buffer_offset == buffers[i].buffer_offset) &&
 					(buf->buffer_size == buffers[i].buffer_size))
@@ -142,20 +144,10 @@ fd_set_shader_buffers(struct pipe_context *pctx,
 			buf->buffer_size = buffers[i].buffer_size;
 			pipe_resource_reference(&buf->buffer, buffers[i].buffer);
 
-			if (buf->buffer)
-				so->enabled_mask |= BIT(n);
-			else
-				so->enabled_mask &= ~BIT(n);
-		}
-	} else {
-		for (unsigned i = 0; i < count; i++) {
-			unsigned n = i + start;
-			struct pipe_shader_buffer *buf = &so->sb[n];
-
+			so->enabled_mask |= BIT(n);
+		} else {
 			pipe_resource_reference(&buf->buffer, NULL);
 		}
-
-		so->enabled_mask &= ~modified_bits;
 	}
 
 	ctx->dirty_shader[shader] |= FD_DIRTY_SHADER_SSBO;

joshuaswarren/mesa-1

commit 8a7e71235504bf665f503e79a8693aaa9edbc9f2

freedreno: Fix tracking of enabled SSBOs
Clearing all of the modified bits an relying on OR'ing the needed bits
back in the loop below doesn't quite work out, Because of early continue
if the SSBO has not changed.

Fixes: fdb460180f6 ("freedreno: simplify fd_set_shader_buffers(..)")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20575>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -164,7 +164,6 @@ fd_set_shader_buffers(struct pipe_context *pctx, enum pipe_shader_type shader,
    struct fd_shaderbuf_stateobj *so = &ctx->shaderbuf[shader];
    const unsigned modified_bits = u_bit_consecutive(start, count);
 
-   so->enabled_mask &= ~modified_bits;
    so->writable_mask &= ~modified_bits;
    so->writable_mask |= writable_bitmask << start;
 
@@ -194,6 +193,8 @@ fd_set_shader_buffers(struct pipe_context *pctx, enum pipe_shader_type shader,
          }
       } else {
          pipe_resource_reference(&buf->buffer, NULL);
+
+         so->enabled_mask &= ~BIT(n);
       }
    }
 