joshuaswarren/mesa-1

commit 43d343dc74a5a882e5fe8a9b5c70d81f20e773ca

radv/winsys: fill real info for CHIP_GFX1100
From my AMD Radeon 7900 XT.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20476>

src/amd/vulkan/winsys/null/radv_null_winsys.c
@@ -68,7 +68,7 @@ static const struct {
    [CHIP_VANGOGH] = {0x163F, 8, false},
    [CHIP_NAVI22] = {0x73C0, 8, true},
    [CHIP_NAVI23] = {0x73E0, 8, true},
-   [CHIP_GFX1100] = {0xdead, 8, true}, /* TODO: fill with real info. */
+   [CHIP_GFX1100] = {0x73BF, 24, true},
 };
 
 static void

commit 7f0526f83b4db7d9f535291a33ce916f6cc717a7

radv/winsys: fix incorrect PCIID for GFX11 in the null winsys
Fixes: 43d343dc74a ("radv/winsys: fill real info for CHIP_GFX1100")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20850>

src/amd/vulkan/winsys/null/radv_null_winsys.c
@@ -68,7 +68,7 @@ static const struct {
    [CHIP_VANGOGH] = {0x163F, 8, false},
    [CHIP_NAVI22] = {0x73C0, 8, true},
    [CHIP_NAVI23] = {0x73E0, 8, true},
-   [CHIP_GFX1100] = {0x73BF, 24, true},
+   [CHIP_GFX1100] = {0x744C, 24, true},
 };
 
 static void