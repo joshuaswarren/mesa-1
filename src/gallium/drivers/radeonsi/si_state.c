openharmony/third_party_mesa3d

commit c7843de14a246a52043d03fb2ab64095b6ff9bc1

radeonsi: adjust RB+ blend optimization settings
based on PAL

src/gallium/drivers/radeonsi/si_state.c
@@ -250,10 +250,8 @@ static void si_emit_cb_render_state(struct si_context *sctx)
 				break;
 
 			case V_028C70_COLOR_10_11_11:
-				if (spi_format == V_028714_SPI_SHADER_FP16_ABGR) {
+				if (spi_format == V_028714_SPI_SHADER_FP16_ABGR)
 					sx_ps_downconvert |= V_028754_SX_RT_EXPORT_10_11_11 << (i * 4);
-					sx_blend_opt_epsilon |= V_028758_11BIT_FORMAT << (i * 4);
-				}
 				break;
 
 			case V_028C70_COLOR_2_10_10_10:

joshuaswarren/mesa-1

commit 3ae10921065177a4ef05919c7a3fc797f0b7d2be

radeonsi: fix RB+ blending with sRGB formats
The epsilon for 8bpc is for the linear colorspace. There is no epsilon
for sRGB.

Fixes: c7843de14a2 - radeonsi: adjust RB+ blend optimization settings

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21041>

src/gallium/drivers/radeonsi/si_state.c
@@ -193,7 +193,8 @@ static void si_emit_cb_render_state(struct si_context *sctx)
                 spi_format == V_028714_SPI_SHADER_UINT16_ABGR ||
                 spi_format == V_028714_SPI_SHADER_SINT16_ABGR) {
                sx_ps_downconvert |= V_028754_SX_RT_EXPORT_8_8_8_8 << (i * 4);
-               sx_blend_opt_epsilon |= V_028758_8BIT_FORMAT << (i * 4);
+               if (G_028C70_NUMBER_TYPE(surf->cb_color_info) != V_028C70_NUMBER_SRGB)
+                  sx_blend_opt_epsilon |= V_028758_8BIT_FORMAT << (i * 4);
             }
             break;
 