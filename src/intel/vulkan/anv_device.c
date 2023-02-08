joshuaswarren/mesa-1

commit f99b7bbcac1b6f2c93582f1fad527cb193f09f26

anv: enable video decode extensions.
Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20782>

src/intel/vulkan/anv_device.c
@@ -272,6 +272,9 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_timeline_semaphore                = true,
       .KHR_uniform_buffer_standard_layout    = true,
       .KHR_variable_pointers                 = true,
+      .KHR_video_queue                       = true,
+      .KHR_video_decode_queue                = true,
+      .KHR_video_decode_h264                 = VIDEO_CODEC_H264DEC,
       .KHR_vulkan_memory_model               = true,
       .KHR_workgroup_memory_explicit_layout  = true,
       .KHR_zero_initialize_workgroup_memory  = true,
@@ -2804,6 +2807,13 @@ void anv_GetPhysicalDeviceQueueFamilyProperties2(
                VkQueueFamilyQueryResultStatusPropertiesKHR *prop =
                   (VkQueueFamilyQueryResultStatusPropertiesKHR *)ext;
                prop->queryResultStatusSupport = VK_TRUE;
+               break;
+            }
+            case VK_STRUCTURE_TYPE_QUEUE_FAMILY_VIDEO_PROPERTIES_KHR: {
+               VkQueueFamilyVideoPropertiesKHR *prop =
+                  (VkQueueFamilyVideoPropertiesKHR *)ext;
+               if (queue_family->queueFlags & VK_QUEUE_VIDEO_DECODE_BIT_KHR)
+                  prop->videoCodecOperations = VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_KHR;
                break;
             }
             default:

commit 806a17e259cbd85dfe255dc88e7832c14296af02

anv: put the video extension behind a variable
As initialized acked.

The WSI code is currently trying to blit on the video queue.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: f99b7bbcac1b ("anv: enable video decode extensions.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21194>

src/intel/vulkan/anv_device.c
@@ -272,9 +272,9 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_timeline_semaphore                = true,
       .KHR_uniform_buffer_standard_layout    = true,
       .KHR_variable_pointers                 = true,
-      .KHR_video_queue                       = true,
-      .KHR_video_decode_queue                = true,
-      .KHR_video_decode_h264                 = VIDEO_CODEC_H264DEC,
+      .KHR_video_queue                       = device->video_decode_enabled,
+      .KHR_video_decode_queue                = device->video_decode_enabled,
+      .KHR_video_decode_h264                 = VIDEO_CODEC_H264DEC && device->video_decode_enabled,
       .KHR_vulkan_memory_model               = true,
       .KHR_workgroup_memory_explicit_layout  = true,
       .KHR_zero_initialize_workgroup_memory  = true,
@@ -765,7 +765,7 @@ anv_physical_device_init_queue_families(struct anv_physical_device *pdevice)
             .engine_class = compute_class,
          };
       }
-      if (v_count > 0) {
+      if (v_count > 0 && pdevice->video_decode_enabled) {
          pdevice->queue.families[family_count++] = (struct anv_queue_family) {
             .queueFlags = VK_QUEUE_VIDEO_DECODE_BIT_KHR,
             .queueCount = v_count,
@@ -935,6 +935,8 @@ anv_physical_device_try_create(struct vk_instance *vk_instance,
 
    device->has_implicit_ccs = device->info.has_aux_map ||
                               device->info.verx10 >= 125;
+
+   device->video_decode_enabled = debug_get_bool_option("ANV_VIDEO_DECODE", false);
 
    /* Check if we can read the GPU timestamp register from the CPU */
    uint64_t u64_ignore;

src/intel/vulkan/anv_private.h
@@ -925,6 +925,8 @@ struct anv_physical_device {
      * the total system ram to try and avoid running out of RAM.
      */
     bool                                        supports_48bit_addresses;
+    bool                                        video_decode_enabled;
+
     struct brw_compiler *                       compiler;
     struct isl_device                           isl_dev;
     struct intel_perf_config *                    perf;