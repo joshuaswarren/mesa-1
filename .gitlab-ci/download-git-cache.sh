joshuaswarren/mesa-1

commit c3d717bf2969f48531a1fc2a81a29f704a40459d

ci: migrate from wget to curl
Better error handling is more reliable.

Options:
 -L, follow location
 --retry, number of retries
 --retry-all-errors, does not fail on ALL errors, that's why there is -f
 -f, fail fast with no output at all on server errors
 --retry-delay, make curl sleep this amount of time before each retry

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20788>

.gitlab-ci/download-git-cache.sh
@@ -16,10 +16,11 @@ fi
 TMP_DIR=$(mktemp -d)
 
 echo "Downloading archived master..."
-/usr/bin/wget -O $TMP_DIR/$CI_PROJECT_NAME.tar.gz \
-              https://${MINIO_HOST}/git-cache/${FDO_UPSTREAM_REPO}/$CI_PROJECT_NAME.tar.gz
+curl --retry 4 -f --retry-all-errors --retry-delay 60 \
+	      -o "$TMP_DIR/$CI_PROJECT_NAME.tar.gz" \
+              "https://${MINIO_HOST}/git-cache/${FDO_UPSTREAM_REPO}/$CI_PROJECT_NAME.tar.gz"
 
-# check wget error code
+# check curl error code
 if [[ $? -ne 0 ]]
 then
     echo "Repository cache not available"

.gitlab-ci/bare-metal/cros-servo.sh
@@ -80,8 +80,9 @@ mkdir -p /nfs/results
 
 rm -rf /tftp/*
 if echo "$BM_KERNEL" | grep -q http; then
-  apt install -y wget
-  wget $BM_KERNEL -O /tftp/vmlinuz
+  apt-get install -y curl
+  curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+      $BM_KERNEL -o /tftp/vmlinuz
 else
   cp $BM_KERNEL /tftp/vmlinuz
 fi

.gitlab-ci/bare-metal/fastboot.sh
@@ -100,10 +100,12 @@ fi
 # moving that container to the runner.  So, if BM_KERNEL+BM_DTB are URLs,
 # fetch them instead of looking in the container.
 if echo "$BM_KERNEL $BM_DTB" | grep -q http; then
-  apt install -y wget
+  apt-get install -y curl
 
-  wget $BM_KERNEL -O kernel
-  wget $BM_DTB -O dtb
+  curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+      "$BM_KERNEL" -o kernel
+  curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+      "$BM_DTB" -o dtb
 
   cat kernel dtb > Image.gz-dtb
   rm kernel

.gitlab-ci/bare-metal/poe-powered.sh
@@ -85,8 +85,9 @@ date +'%F %T'
 
 # If BM_BOOTFS is an URL, download it
 if echo $BM_BOOTFS | grep -q http; then
-  apt install -y wget
-  wget ${FDO_HTTP_CACHE_URI:-}$BM_BOOTFS -O /tmp/bootfs.tar
+  apt-get install -y curl
+  curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+    "${FDO_HTTP_CACHE_URI:-}$BM_BOOTFS" -o /tmp/bootfs.tar
   BM_BOOTFS=/tmp/bootfs.tar
 fi
 

.gitlab-ci/common/init-stage2.sh
@@ -73,7 +73,8 @@ if [ "$HWCI_KVM" = "true" ]; then
         modprobe ${KVM_KERNEL_MODULE}
 
     mkdir -p /lava-files
-    wget -S --progress=dot:giga -O /lava-files/${KERNEL_IMAGE_NAME} \
+    curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+	-o "/lava-files/${KERNEL_IMAGE_NAME}" \
         "${KERNEL_IMAGE_BASE_URL}/${KERNEL_IMAGE_NAME}"
 fi
 

.gitlab-ci/container/alpine/x86_build.sh
@@ -10,6 +10,7 @@ EPHEMERAL="
         automake
         bzip2
         cmake
+	curl
         git
         libtool
         libepoxy-dev
@@ -17,7 +18,6 @@ EPHEMERAL="
         make
         openssl-dev
         unzip
-        wget
         xz
 	zstd-dev
         "

.gitlab-ci/container/baremetal_build.sh
@@ -7,13 +7,14 @@ set -o xtrace
 # network transfer, disk usage, and runtime on test jobs)
 
 # shellcheck disable=SC2154 # arch is assigned in previous scripts
-if wget -q --method=HEAD "${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${ARTIFACTS_SUFFIX}/${arch}/done"; then
+if curl -X HEAD -s "${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${ARTIFACTS_SUFFIX}/${arch}/done"; then
   ARTIFACTS_URL="${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${ARTIFACTS_SUFFIX}/${arch}"
 else
   ARTIFACTS_URL="${ARTIFACTS_PREFIX}/${CI_PROJECT_PATH}/${ARTIFACTS_SUFFIX}/${arch}"
 fi
 
-wget "${ARTIFACTS_URL}"/lava-rootfs.tar.zst -O rootfs.tar.zst
+curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+    "${ARTIFACTS_URL}"/lava-rootfs.tar.zst -o rootfs.tar.zst
 mkdir -p /rootfs-"$arch"
 tar -C /rootfs-"$arch" '--exclude=./dev/*' --zstd -xf rootfs.tar.zst
 rm rootfs.tar.zst
@@ -22,9 +23,12 @@ if [[ $arch == "arm64" ]]; then
     mkdir -p /baremetal-files
     pushd /baremetal-files
 
-    wget "${ARTIFACTS_URL}"/Image
-    wget "${ARTIFACTS_URL}"/Image.gz
-    wget "${ARTIFACTS_URL}"/cheza-kernel
+    curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+	-O "${ARTIFACTS_URL}"/Image
+    curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+        -O "${ARTIFACTS_URL}"/Image.gz
+    curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+        -O "${ARTIFACTS_URL}"/cheza-kernel
 
     DEVICE_TREES=""
     DEVICE_TREES="$DEVICE_TREES apq8016-sbc.dtb"
@@ -33,7 +37,8 @@ if [[ $arch == "arm64" ]]; then
     DEVICE_TREES="$DEVICE_TREES imx8mq-nitrogen.dtb"
 
     for DTB in $DEVICE_TREES; do
-        wget "${ARTIFACTS_URL}/$DTB"
+	curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+            -O "${ARTIFACTS_URL}/$DTB"
     done
 
     popd
@@ -41,14 +46,16 @@ elif [[ $arch == "armhf" ]]; then
     mkdir -p /baremetal-files
     pushd /baremetal-files
 
-    wget "${ARTIFACTS_URL}"/zImage
+    curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+        -O "${ARTIFACTS_URL}"/zImage
 
     DEVICE_TREES=""
     DEVICE_TREES="$DEVICE_TREES imx6q-cubox-i.dtb"
     DEVICE_TREES="$DEVICE_TREES tegra124-jetson-tk1.dtb"
 
     for DTB in $DEVICE_TREES; do
-        wget "${ARTIFACTS_URL}/$DTB"
+	curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+            -O "${ARTIFACTS_URL}/$DTB"
     done
 
     popd

.gitlab-ci/container/build-deqp.sh
@@ -16,12 +16,12 @@ pushd /VK-GL-CTS
 # vulkan-cts-1.3.3.0 uses zlib 1.2.12 which was removed from zlib server due to
 # a CVE. See https://zlib.net/
 # FIXME: Remove this patch when uprev to 1.3.4.0+
-wget -O- https://github.com/KhronosGroup/VK-GL-CTS/commit/6bb2e7d64261bedb503947b1b251b1eeeb49be73.patch |
-    git am -
+curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+    "https://github.com/KhronosGroup/VK-GL-CTS/commit/6bb2e7d64261bedb503947b1b251b1eeeb49be73.patch" | git am -
 
 # Apply a patch to fix a bug in 1.3.3.0 that affects some new formats
-wget -O- https://github.com/KhronosGroup/VK-GL-CTS/commit/4fa2b40411921b304f5dad8d106b212ad5b0f172.patch |
-    git am -
+curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
+    "https://github.com/KhronosGroup/VK-GL-CTS/commit/4fa2b40411921b304f5dad8d106b212ad5b0f172.patch" | git am -
 
 # --insecure is due to SSL cert failures hitting sourceforge for zlib and
 # libpng (sigh).  The archives get their checksums checked anyway, and git

commit 29dd5d5c0b83172b6416530d2e01b9ab72bca73b

ci: revert download of git cache to the wget
At this point of CI there is not curl available.

Fixes: c3d717bf2969 ("ci: migrate from wget to curl")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21414>

.gitlab-ci/download-git-cache.sh
@@ -16,11 +16,11 @@ fi
 TMP_DIR=$(mktemp -d)
 
 echo "Downloading archived master..."
-curl --retry 4 -f --retry-all-errors --retry-delay 60 \
-	      -o "$TMP_DIR/$CI_PROJECT_NAME.tar.gz" \
+/usr/bin/wget \
+	      -O "$TMP_DIR/$CI_PROJECT_NAME.tar.gz" \
               "https://${MINIO_HOST}/git-cache/${FDO_UPSTREAM_REPO}/$CI_PROJECT_NAME.tar.gz"
 
-# check curl error code
+# check wget error code
 if [[ $? -ne 0 ]]
 then
     echo "Repository cache not available"