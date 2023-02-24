joshuaswarren/mesa-1

commit 5269d044d044f2873219ec18f10a6ca756689d5d

r600/sfn: check used channels when evaluating allowed mask
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/7878

v2: Fix ws in comment (iorn10)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20283>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -468,7 +468,27 @@ AluInstr::set_sources(SrcValues src)
 
 uint8_t AluInstr::allowed_src_chan_mask() const
 {
-   return 0xf;
+   if (m_alu_slots < 2)
+       return 0xf;
+   int chan_use_count[4] = {0};
+
+   for (auto s : m_src) {
+       auto r = s->as_register();
+       if (r)
+           ++chan_use_count[r->chan()];
+   }
+   /* Each channel can only be loaded in one of three cycles,
+    * so if a channel is already used three times, we can't
+    * add another source with this channel.
+    * Since we want to move away from one channel to another, it
+    * is not important to know which is the old channel that will
+    * be freed by the channel switch.*/
+   int mask = 0;
+   for (int i = 0; i < 4; ++i) {
+       if (chan_use_count[i] < 3)
+           mask |= 1 << i;
+   }
+   return mask;
 }
 
 uint8_t

commit ab1fc18bb304b8e911caee49cb957a7353ca0cd6

r600/sfn: be more conservative with channel use in multi-slot ops
The current approach to check the bank swizzle doesn't allow to
re-evaluate bank-swizzle for instructions that where emitted
earlier, so we might end up with impossile constellations when we
allow three uses of the same channel

Fixes: 5269d044d044
  r600/sfn: check used channels when evaluating allowed mask

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8350

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21516>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -507,8 +507,20 @@ uint8_t AluInstr::allowed_src_chan_mask() const
     * is not important to know which is the old channel that will
     * be freed by the channel switch.*/
    int mask = 0;
+
+   /* Be conservative about channel use when using more than two
+    * slots. Currently a constellatioon of
+    *
+    *  ALU d.x = f(r0.x, r1.y)
+    *  ALU _.y = f(r2.y, r3.x)
+    *  ALU _.z = f(r4.x, r5.y)
+    *
+    * will fail to be split. To get constellations like this to be scheduled
+    * properly will need some work on the bank swizzle check.
+    */
+   int maxuse = m_alu_slots > 2 ? 2 : 3;
    for (int i = 0; i < 4; ++i) {
-       if (chan_use_count[i] < 3)
+       if (chan_use_count[i] < maxuse)
            mask |= 1 << i;
    }
    return mask;