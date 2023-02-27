openharmony/third_party_mesa3d

commit c13d1be071938e7de47142b95bdc64510109e8a1

llvmpipe: set nir_shader_compiler_options::use_scoped_barrier
required for vk memory model

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15377>

src/gallium/auxiliary/gallivm/lp_bld_nir.c
@@ -1907,6 +1907,7 @@ static void visit_intrinsic(struct lp_build_nir_context *bld_base,
       visit_shared_atomic(bld_base, instr, result);
       break;
    case nir_intrinsic_control_barrier:
+   case nir_intrinsic_scoped_barrier:
       visit_barrier(bld_base);
       break;
    case nir_intrinsic_group_memory_barrier:

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -628,6 +628,7 @@ static const struct nir_shader_compiler_options gallivm_nir_options = {
    .lower_device_index_to_zero = true,
    .support_16bit_alu = true,
    .lower_fisnormal = true,
+   .use_scoped_barrier = true,
 };
 
 static char *

joshuaswarren/mesa-1

commit 6b8d0b694867bf38fa4079aa4fe41a40a3dc49f5

gallivm: Fix handling of nir_intrinsic_scoped_barrier
A scoped barrier only has a control barrier behavior if there's
an execution scope.

Fixes: c13d1be07193 ("llvmpipe: set nir_shader_compiler_options::use_scoped_barrier")
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3339>

src/gallium/auxiliary/gallivm/lp_bld_nir.c
@@ -2123,8 +2123,11 @@ visit_intrinsic(struct lp_build_nir_context *bld_base,
       visit_shared_atomic(bld_base, instr, result);
       break;
    case nir_intrinsic_control_barrier:
-   case nir_intrinsic_scoped_barrier:
       visit_barrier(bld_base);
+      break;
+   case nir_intrinsic_scoped_barrier:
+      if (nir_intrinsic_execution_scope(instr) != NIR_SCOPE_NONE)
+         visit_barrier(bld_base);
       break;
    case nir_intrinsic_group_memory_barrier:
    case nir_intrinsic_memory_barrier: