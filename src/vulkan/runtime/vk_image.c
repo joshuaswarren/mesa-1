joshuaswarren/mesa-1

commit bc0fcfb4387cf4b4ad6ac7c79303ab965d62590c

vulkan/runtime: store parameters of VK_EXT_sliced_view_of_3d
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21514>

src/vulkan/runtime/vk_image.c
@@ -453,6 +453,14 @@ vk_image_view_init(struct vk_device *device,
    image_view->extent =
       vk_image_mip_level_extent(image, image_view->base_mip_level);
 
+   /* By default storage uses the same as the image properties, but it can be
+    * overriden with VkImageViewSlicedCreateInfoEXT.
+    */
+   image_view->storage.slice_offset = 0;
+   image_view->storage.slice_count = image_view->extent.depth;
+
+   const VkImageViewSlicedCreateInfoEXT *sliced_info =
+      vk_find_struct_const(pCreateInfo, IMAGE_VIEW_SLICED_CREATE_INFO_EXT);
    assert(image_view->base_mip_level + image_view->level_count
           <= image->mip_levels);
    switch (image->image_type) {
@@ -464,6 +472,18 @@ vk_image_view_init(struct vk_device *device,
              <= image->array_layers);
       break;
    case VK_IMAGE_TYPE_3D:
+      if (sliced_info) {
+         unsigned total = image_view->extent.depth;
+         image_view->storage.slice_offset = sliced_info->sliceOffset;
+         assert(image_view->storage.slice_offset < total);
+         if (sliced_info->sliceCount == VK_REMAINING_3D_SLICES_EXT) {
+            image_view->storage.slice_count = total - image_view->storage.slice_offset;
+         } else {
+            image_view->storage.slice_count = sliced_info->sliceCount;
+         }
+         assert(image_view->storage.slice_offset + image_view->storage.slice_count
+                <= image->extent.depth);
+      }
       assert(image_view->base_array_layer + image_view->layer_count
              <= image_view->extent.depth);
       break;

src/vulkan/runtime/vk_image.h
@@ -279,6 +279,11 @@ struct vk_image_view {
    uint32_t base_array_layer;
    uint32_t layer_count;
 
+   struct {
+      uint32_t slice_offset;
+      uint32_t slice_count;
+   } storage;
+
    /* VK_EXT_image_view_min_lod */
    float min_lod;
 

commit 97b3410ef4274d6e1c3d6bf923f1d9b4adaf4341

vulkan/runtime: only consider slice info with 3D image views
Because we can have 2D views of 3D images, we want to consider the
slice info only with 3D *image views*.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: bc0fcfb438 ("vulkan/runtime: store parameters of VK_EXT_sliced_view_of_3d")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21376>

src/vulkan/runtime/vk_image.c
@@ -472,7 +472,7 @@ vk_image_view_init(struct vk_device *device,
              <= image->array_layers);
       break;
    case VK_IMAGE_TYPE_3D:
-      if (sliced_info) {
+      if (sliced_info && image_view->view_type == VK_IMAGE_VIEW_TYPE_3D) {
          unsigned total = image_view->extent.depth;
          image_view->storage.z_slice_offset = sliced_info->sliceOffset;
          assert(image_view->storage.z_slice_offset < total);
@@ -481,9 +481,12 @@ vk_image_view_init(struct vk_device *device,
          } else {
             image_view->storage.z_slice_count = sliced_info->sliceCount;
          }
-         assert(image_view->storage.z_slice_offset + image_view->storage.z_slice_count
-                <= image->extent.depth);
+      } else if (image_view->view_type != VK_IMAGE_VIEW_TYPE_3D) {
+         image_view->storage.z_slice_offset = image_view->base_array_layer;
+         image_view->storage.z_slice_count = image_view->layer_count;
       }
+      assert(image_view->storage.z_slice_offset + image_view->storage.z_slice_count
+             <= image->extent.depth);
       assert(image_view->base_array_layer + image_view->layer_count
              <= image_view->extent.depth);
       break;

src/vulkan/runtime/vk_image.h
@@ -283,16 +283,17 @@ struct vk_image_view {
    struct {
       /* VkImageViewSlicedCreateInfoEXT::sliceOffset
        *
-       * This field will be 0 for 1D and 2D images or when no
-       * VkImageViewSlicedCreateInfoEXT is provided.
+       * This field will be 0 for 1D and 2D images, 2D views of 3D images, or
+       * when no VkImageViewSlicedCreateInfoEXT is provided.
        */
       uint32_t z_slice_offset;
 
       /* VkImageViewSlicedCreateInfoEXT::sliceCount
        *
-       * This field will be 1 for 1D and 2D images and the image view depth
-       * (see vk_image_view::extent) when no VkImageViewSlicedCreateInfoEXT is
-       * provided.
+       * This field will be 1 for 1D and 2D images or 2D views of 3D images.
+       * For 3D views, it will be VkImageViewSlicedCreateInfoEXT::sliceCount
+       * or image view depth (see vk_image_view::extent) when no
+       * VkImageViewSlicedCreateInfoEXT is provided.
        */
       uint32_t z_slice_count;
    } storage;