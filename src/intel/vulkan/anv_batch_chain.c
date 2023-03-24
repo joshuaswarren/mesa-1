openharmony/third_party_mesa3d

commit 457a3f65087b194ce4ad8287778d80988ac9fbc1

anv: add perfetto source
v2: Increase custom stall data (Felix)
    Fixup build (Felix)

v3: Add API enum (Rohan)
    Fixup old comment (Rohan)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Acked-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13996>

src/intel/vulkan/anv_batch_chain.c
@@ -2374,9 +2374,14 @@ anv_queue_submit(struct vk_queue *vk_queue,
       return VK_SUCCESS;
    }
 
+   uint64_t start_ts = intel_ds_begin_submit(queue->ds);
+
    pthread_mutex_lock(&device->mutex);
    result = anv_queue_submit_locked(queue, submit);
+   /* Take submission ID under lock */
    pthread_mutex_unlock(&device->mutex);
+
+   intel_ds_end_submit(queue->ds, start_ts);
 
    return result;
 }

src/intel/ds/intel_driver_ds.h
@@ -1,0 +1,193 @@
+/*
+ * Copyright © 2021 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef INTEL_DRIVER_DS_H
+#define INTEL_DRIVER_DS_H
+
+#include <stdint.h>
+
+#include "util/macros.h"
+#include "util/perf/u_trace.h"
+#include "util/u_vector.h"
+
+#include "dev/intel_device_info.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum intel_ds_api {
+   INTEL_DS_API_OPENGL,
+   INTEL_DS_API_VULKAN,
+};
+
+enum intel_ds_stall_flag {
+   INTEL_DS_DEPTH_CACHE_FLUSH_BIT         = BITFIELD_BIT(0),
+   INTEL_DS_DATA_CACHE_FLUSH_BIT          = BITFIELD_BIT(1),
+   INTEL_DS_HDC_PIPELINE_FLUSH_BIT        = BITFIELD_BIT(2),
+   INTEL_DS_RENDER_TARGET_CACHE_FLUSH_BIT = BITFIELD_BIT(3),
+   INTEL_DS_TILE_CACHE_FLUSH_BIT          = BITFIELD_BIT(4),
+   INTEL_DS_STATE_CACHE_INVALIDATE_BIT    = BITFIELD_BIT(5),
+   INTEL_DS_CONST_CACHE_INVALIDATE_BIT    = BITFIELD_BIT(6),
+   INTEL_DS_VF_CACHE_INVALIDATE_BIT       = BITFIELD_BIT(7),
+   INTEL_DS_TEXTURE_CACHE_INVALIDATE_BIT  = BITFIELD_BIT(8),
+   INTEL_DS_INST_CACHE_INVALIDATE_BIT     = BITFIELD_BIT(9),
+   INTEL_DS_STALL_AT_SCOREBOARD_BIT       = BITFIELD_BIT(10),
+   INTEL_DS_DEPTH_STALL_BIT               = BITFIELD_BIT(11),
+   INTEL_DS_CS_STALL_BIT                  = BITFIELD_BIT(12),
+};
+
+/* Convert internal driver PIPE_CONTROL stall bits to intel_ds_stall_flag. */
+typedef enum intel_ds_stall_flag (*intel_ds_stall_cb_t)(uint32_t flags);
+
+enum intel_ds_queue_stage {
+   INTEL_DS_QUEUE_STAGE_CMD_BUFFER,
+   INTEL_DS_QUEUE_STAGE_STALL,
+   INTEL_DS_QUEUE_STAGE_COMPUTE,
+   INTEL_DS_QUEUE_STAGE_RENDER_PASS,
+   INTEL_DS_QUEUE_STAGE_BLORP,
+   INTEL_DS_QUEUE_STAGE_DRAW,
+   INTEL_DS_QUEUE_STAGE_N_STAGES,
+};
+
+struct intel_ds_device {
+   struct intel_device_info info;
+
+   /* DRM fd */
+   int fd;
+
+   /* API of this device */
+   enum intel_ds_api api;
+
+   /* GPU identifier (minor number) */
+   uint32_t gpu_id;
+
+   /* Clock identifier for this device. */
+   uint32_t gpu_clock_id;
+
+   /* The timestamp at the point where we first emitted the clock_sync..
+    * this  will be a *later* timestamp that the first GPU traces (since
+    * we capture the first clock_sync from the CPU *after* the first GPU
+    * tracepoints happen).  To avoid confusing perfetto we need to drop
+    * the GPU traces with timestamps before this.
+    */
+   uint64_t sync_gpu_ts;
+
+   /* Next timestamp after which we should resend a clock correlation. */
+   uint64_t next_clock_sync_ns;
+
+   /* Unique perfetto identifier for the context */
+   uint64_t iid;
+
+   /* Event ID generator */
+   uint64_t event_id;
+
+   struct u_trace_context trace_context;
+
+   /* List of intel_ds_queue */
+   struct u_vector queues;
+};
+
+struct intel_ds_stage {
+   /* Unique hw_queue IID */
+   uint64_t queue_iid;
+
+   /* Unique stage IID */
+   uint64_t stage_iid;
+
+   /* Start timestamp of the last work element */
+   uint64_t start_ns;
+};
+
+struct intel_ds_queue {
+   /* Device this queue belongs to */
+   struct intel_ds_device *device;
+
+   /* Unique queue ID across the device */
+   uint32_t queue_id;
+
+   /* Unique name of the queue */
+   char name[80];
+
+   /* Counter incremented on each intel_ds_end_submit() call */
+   uint64_t submission_id;
+
+   struct intel_ds_stage stages[INTEL_DS_QUEUE_STAGE_N_STAGES];
+};
+
+struct intel_ds_flush_data {
+   struct intel_ds_queue *queue;
+
+   /* u_trace element in which we copy other traces in case we deal with
+    * reusable command buffers.
+    */
+   struct u_trace trace;
+
+   /* Unique submission ID associated with the trace */
+   uint64_t submission_id;
+};
+
+void intel_driver_ds_init(void);
+
+void intel_ds_device_init(struct intel_ds_device *device,
+                          struct intel_device_info *devinfo,
+                          int drm_fd,
+                          uint32_t gpu_id,
+                          enum intel_ds_api api);
+void intel_ds_device_fini(struct intel_ds_device *device);
+
+struct intel_ds_queue *intel_ds_device_add_queue(struct intel_ds_device *device,
+                                                 const char *fmt_name,
+                                                 ...);
+
+void intel_ds_flush_data_init(struct intel_ds_flush_data *data,
+                              struct intel_ds_queue *queue,
+                              uint64_t submission_id);
+
+void intel_ds_flush_data_fini(struct intel_ds_flush_data *data);
+
+#ifdef HAVE_PERFETTO
+
+uint64_t intel_ds_begin_submit(struct intel_ds_queue *queue);
+void intel_ds_end_submit(struct intel_ds_queue *queue,
+                         uint64_t start_ts);
+
+#else
+
+static inline uint64_t intel_ds_begin_submit(struct intel_ds_queue *queue)
+{
+   return 0;
+}
+
+static inline void intel_ds_end_submit(struct intel_ds_queue *queue,
+                                       uint64_t start_ts)
+{
+}
+
+#endif /* HAVE_PERFETTO */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTEL_DRIVER_DS_H */

src/intel/ds/intel_tracepoints.py
@@ -34,14 +34,18 @@ def define_tracepoints(args):
     from u_trace import TracepointArg as Arg
     from u_trace import TracepointArgStruct as ArgStruct
 
-    Header('anv_private.h', scope=HeaderScope.SOURCE)
+    Header('intel_driver_ds.h', scope=HeaderScope.SOURCE)
     Header('blorp/blorp_priv.h', scope=HeaderScope.HEADER)
+    Header('ds/intel_driver_ds.h', scope=HeaderScope.HEADER)
 
-    def begin_end_tp(name, tp_args=[], tp_struct=None, end_pipelined=True):
-        Tracepoint('begin_{0}'.format(name))
-        Tracepoint('end_{0}'.format(name),
+    def begin_end_tp(name, tp_args=[], tp_struct=None, tp_print=None, end_pipelined=True):
+        Tracepoint('intel_begin_{0}'.format(name),
+                   tp_perfetto='intel_ds_begin_{0}'.format(name))
+        Tracepoint('intel_end_{0}'.format(name),
                    args=tp_args,
                    tp_struct=tp_struct,
+                   tp_perfetto='intel_ds_end_{0}'.format(name),
+                   tp_print=tp_print,
                    end_of_pipe=end_pipelined)
 
 
@@ -73,8 +77,8 @@ def define_tracepoints(args):
                         Arg(type='uint32_t', name='height', var='height', c_format='%u'),
                             Arg(type='enum isl_aux_op', name='hiz_op', var='hiz_op', c_format='%s', to_prim_type='isl_aux_op_to_name({})'),
                             Arg(type='enum isl_aux_op', name='fast_clear_op', var='fast_clear_op', c_format='%s', to_prim_type='isl_aux_op_to_name({})'),
-                            Arg(type='enum blorp_shader_type', name='type', var='shader_type', c_format='%s', to_prim_type='blorp_shader_type_to_name({})'),
-                            Arg(type='enum blorp_shader_pipeline', name='pipe', var='shader_pipe', c_format='%s', to_prim_type='blorp_shader_pipeline_to_name({})'),])
+                            Arg(type='enum blorp_shader_type', name='blorp_type', var='shader_type', c_format='%s', to_prim_type='blorp_shader_type_to_name({})'),
+                            Arg(type='enum blorp_shader_pipeline', name='blorp_pipe', var='shader_pipe', c_format='%s', to_prim_type='blorp_shader_pipeline_to_name({})'),])
 
     begin_end_tp('draw',
                  tp_args=[ArgStruct(type='uint32_t', var='count'),],
@@ -110,42 +114,58 @@ def define_tracepoints(args):
                           ArgStruct(type='uint32_t', var='group_z'),],
                  tp_struct=[Arg(type='uint32_t', name='group_x', var='group_x', c_format='%u'),
                             Arg(type='uint32_t', name='group_y', var='group_y', c_format='%u'),
-                            Arg(type='uint32_t', name='group_z', var='group_z', c_format='%u'),])
+                            Arg(type='uint32_t', name='group_z', var='group_z', c_format='%u'),],
+                 tp_print=['group=%ux%ux%u', '__entry->group_x', '__entry->group_y', '__entry->group_z'])
+
+    def flag_bits(args):
+        bits = [Arg(type='enum intel_ds_stall_flag', name='flags', var='decode_cb(flags)', c_format='0x%x')]
+        for a in args:
+            bits.append(Arg(type='bool', name=a[1], var='__entry->flags & INTEL_DS_{0}_BIT'.format(a[0]), c_format='%u'))
+        return bits
 
     def stall_args(args):
         fmt = ''
         exprs = []
         for a in args:
             fmt += '%s'
-            exprs.append('(__entry->flags & ANV_PIPE_{0}_BIT) ? "+{1}" : ""'.format(a[0], a[1]))
+            exprs.append('(__entry->flags & INTEL_DS_{0}_BIT) ? "+{1}" : ""'.format(a[0], a[1]))
+        fmt += ' : %s'
+        exprs.append('__entry->reason ? __entry->reason : "unknown"')
         fmt = [fmt]
         fmt += exprs
         return fmt
 
-    Tracepoint('stall',
-               args=[ArgStruct(type='uint32_t', var='flags'),],
-               tp_struct=[Arg(type='uint32_t', name='flags', var='flags', c_format='0x%x'),],
-               tp_print=stall_args([['DEPTH_CACHE_FLUSH', 'depth_flush'],
-                                    ['DATA_CACHE_FLUSH', 'dc_flush'],
-                                    ['HDC_PIPELINE_FLUSH', 'hdc_flush'],
-                                    ['RENDER_TARGET_CACHE_FLUSH', 'rt_flush'],
-                                    ['TILE_CACHE_FLUSH', 'tile_flush'],
-                                    ['STATE_CACHE_INVALIDATE', 'state_inval'],
-                                    ['CONSTANT_CACHE_INVALIDATE', 'const_inval'],
-                                    ['VF_CACHE_INVALIDATE', 'vf_inval'],
-                                    ['TEXTURE_CACHE_INVALIDATE', 'tex_inval'],
-                                    ['INSTRUCTION_CACHE_INVALIDATE', 'ic_inval'],
-                                    ['STALL_AT_SCOREBOARD', 'pb_stall'],
-                                    ['DEPTH_STALL', 'depth_stall'],
-                                    ['CS_STALL', 'cs_stall'],
-                                    ]))
+    stall_flags = [['DEPTH_CACHE_FLUSH',         'depth_flush'],
+                   ['DATA_CACHE_FLUSH',          'dc_flush'],
+                   ['HDC_PIPELINE_FLUSH',        'hdc_flush'],
+                   ['RENDER_TARGET_CACHE_FLUSH', 'rt_flush'],
+                   ['TILE_CACHE_FLUSH',          'tile_flush'],
+                   ['STATE_CACHE_INVALIDATE',    'state_inval'],
+                   ['CONST_CACHE_INVALIDATE',    'const_inval'],
+                   ['VF_CACHE_INVALIDATE',       'vf_inval'],
+                   ['TEXTURE_CACHE_INVALIDATE',  'tex_inval'],
+                   ['INST_CACHE_INVALIDATE',     'ic_inval'],
+                   ['STALL_AT_SCOREBOARD',       'pb_stall'],
+                   ['DEPTH_STALL',               'depth_stall'],
+                   ['CS_STALL',                  'cs_stall']]
 
+    begin_end_tp('stall',
+                 tp_args=[ArgStruct(type='uint32_t', var='flags'),
+                          ArgStruct(type='intel_ds_stall_cb_t', var='decode_cb'),
+                          ArgStruct(type='const char *', var='reason'),],
+                 tp_struct=[Arg(type='uint32_t', name='flags', var='decode_cb(flags)', c_format='0x%x'),
+                            Arg(type='const char *', name='reason', var='reason', c_format='%s'),],
+                 tp_print=stall_args(stall_flags),
+                 end_pipelined=False)
 
 
 def generate_code(args):
     from u_trace import utrace_generate
+    from u_trace import utrace_generate_perfetto_utils
 
-    utrace_generate(cpath=args.utrace_src, hpath=args.utrace_hdr, ctx_param='struct anv_device *dev')
+    utrace_generate(cpath=args.utrace_src, hpath=args.utrace_hdr,
+                    ctx_param='struct intel_ds_device *dev')
+    utrace_generate_perfetto_utils(hpath=args.perfetto_hdr)
 
 
 def main():
@@ -153,6 +173,7 @@ def main():
     parser.add_argument('-p', '--import-path', required=True)
     parser.add_argument('--utrace-src', required=True)
     parser.add_argument('--utrace-hdr', required=True)
+    parser.add_argument('--perfetto-hdr', required=True)
     args = parser.parse_args()
     sys.path.insert(0, args.import_path)
     define_tracepoints(args)

src/intel/ds/meson.build
@@ -3,30 +3,76 @@
 #
 # SPDX-License-Identifier: MIT
 
-pps_intel_sources = [
-  'intel_pps_perf.cc',
-  'intel_pps_driver.cc',
+intel_tracepoint_files = custom_target(
+  'intel_tracepoints.[ch]',
+  input : 'intel_tracepoints.py',
+  output : ['intel_tracepoints.h',
+            'intel_tracepoints_perfetto.h',
+            'intel_tracepoints.c'],
+  command : [
+    prog_python, '@INPUT@',
+    '-p', join_paths(meson.source_root(), 'src/util/perf/'),
+    '--utrace-hdr', '@OUTPUT0@',
+    '--perfetto-hdr', '@OUTPUT1@',
+    '--utrace-src', '@OUTPUT2@',
+  ],
+  depend_files : u_trace_py,
+)
+
+libintel_driver_ds_deps = [
+  idep_mesautil,
+  idep_nir_headers,
 ]
 
+if with_perfetto
+  libintel_driver_ds_deps += dep_perfetto
+endif
 
-pps_intel_lib = static_library(
-  'pps-intel',
-  sources: pps_intel_sources,
-  include_directories: [inc_tool, inc_src, inc_include, inc_intel],
-  link_with: [libintel_perf, libintel_dev],
-  dependencies: [dep_perfetto, dep_libdrm, idep_mesautil],
-  cpp_args: '-std=c++17'
+idep_intel_driver_ds_headers = declare_dependency(
+  sources : intel_tracepoint_files[0],
+  include_directories : [inc_include, inc_mapi, inc_mesa, inc_intel],
 )
 
-compile_args_pps_intel = ['-DPPS_INTEL']
-
-pps_intel_dep = declare_dependency(
-  link_with: pps_intel_lib,
-  include_directories: [inc_tool, inc_include],
-  compile_args: compile_args_pps_intel,
+libintel_driver_ds = static_library(
+  'intel-driver-ds',
+  sources : ['intel_driver_ds.cc', intel_tracepoint_files],
+  include_directories : [inc_src, inc_include, inc_intel, inc_mapi, inc_mesa],
+  link_with : [libintel_perf, libintel_dev],
+  dependencies : libintel_driver_ds_deps,
+  cpp_args : '-std=c++17',
+  gnu_symbol_visibility : 'hidden',
 )
 
-pps_datasources += pps_intel_dep
-if not with_datasources.contains('intel')
-  with_datasources += 'intel'
+idep_intel_driver_ds = declare_dependency(
+  link_with : libintel_driver_ds,
+  include_directories : [inc_include, inc_mapi, inc_mesa, inc_intel],
+)
+
+if with_perfetto and (with_datasources.contains('intel') or with_datasources.contains('auto'))
+  pps_intel_sources = files(
+    'intel_pps_perf.cc',
+    'intel_pps_driver.cc',
+  )
+
+  pps_intel_lib = static_library(
+    'pps-intel',
+    sources: pps_intel_sources,
+    include_directories: [inc_tool, inc_src, inc_include, inc_intel],
+    link_with: [libintel_perf, libintel_dev],
+    dependencies: [dep_perfetto, dep_libdrm, idep_mesautil],
+    cpp_args: '-std=c++17'
+  )
+
+  compile_args_pps_intel = ['-DPPS_INTEL']
+
+  pps_intel_dep = declare_dependency(
+    link_with: pps_intel_lib,
+    include_directories: [inc_tool, inc_include],
+    compile_args: compile_args_pps_intel,
+  )
+
+  pps_datasources += pps_intel_dep
+  if not with_datasources.contains('intel')
+    with_datasources += 'intel'
+  endif
 endif

src/intel/meson.build
@@ -28,6 +28,7 @@ subdir('isl')
 subdir('common')
 subdir('compiler')
 subdir('perf')
+subdir('ds')
 if with_intel_tools
   subdir('tools')
 endif
@@ -36,7 +37,4 @@ if get_option('vulkan-layers').contains('intel-nullhw')
 endif
 if with_intel_vk
   subdir('vulkan')
-endif
-if with_perfetto and (with_datasources.contains('intel') or with_datasources.contains('auto'))
-  subdir('ds')
 endif

src/intel/vulkan/anv_cmd_buffer.c
@@ -302,7 +302,7 @@ static VkResult anv_create_cmd_buffer(
 
    anv_measure_init(cmd_buffer);
 
-   u_trace_init(&cmd_buffer->trace, &device->trace_context);
+   u_trace_init(&cmd_buffer->trace, &device->ds.trace_context);
 
    *pCommandBuffer = anv_cmd_buffer_to_handle(cmd_buffer);
 
@@ -407,7 +407,7 @@ anv_cmd_buffer_reset(struct anv_cmd_buffer *cmd_buffer)
    anv_measure_reset(cmd_buffer);
 
    u_trace_fini(&cmd_buffer->trace);
-   u_trace_init(&cmd_buffer->trace, &cmd_buffer->device->trace_context);
+   u_trace_init(&cmd_buffer->trace, &cmd_buffer->device->ds.trace_context);
 
    return VK_SUCCESS;
 }

src/intel/vulkan/anv_device.c
@@ -1126,6 +1126,8 @@ VkResult anv_CreateInstance(
 
    anv_init_dri_options(instance);
 
+   intel_driver_ds_init();
+
    *pInstance = anv_instance_to_handle(instance);
 
    return VK_SUCCESS;

joshuaswarren/mesa-1

commit b7b74bced0db56405aa07de16540d971546d6bc1

anv: fixup locking for utrace submission increments
This is supposed to happen under the device lock.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 457a3f6508 ("anv: add perfetto source")
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22006>

src/intel/vulkan/anv_batch_chain.c
@@ -1342,16 +1342,16 @@ anv_queue_submit(struct vk_queue *vk_queue,
       return VK_SUCCESS;
    }
 
-   uint64_t start_ts = intel_ds_begin_submit(&queue->ds);
-
    pthread_mutex_lock(&device->mutex);
+
+   uint64_t start_ts = intel_ds_begin_submit(&queue->ds);
    result = anv_queue_submit_locked(queue, submit);
    /* Take submission ID under lock */
-   pthread_mutex_unlock(&device->mutex);
-
    intel_ds_end_submit(&queue->ds, start_ts);
 
    u_trace_context_process(&device->ds.trace_context, true);
+
+   pthread_mutex_unlock(&device->mutex);
 
    return result;
 }