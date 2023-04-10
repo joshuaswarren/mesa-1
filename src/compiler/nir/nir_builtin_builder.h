joshuaswarren/mesa-1

commit c5340c5d707051867d79198f57451a675b7d1d5d

nir: add nir_fisnan helper function
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21552>

src/compiler/nir/nir_builtin_builder.h
@@ -54,9 +54,15 @@ nir_ssa_def *
 nir_get_texture_size(nir_builder *b, nir_tex_instr *tex);
 
 static inline nir_ssa_def *
+nir_fisnan(nir_builder *b, nir_ssa_def *x)
+{
+   return nir_fneu(b, x, x);
+}
+
+static inline nir_ssa_def *
 nir_nan_check2(nir_builder *b, nir_ssa_def *x, nir_ssa_def *y, nir_ssa_def *res)
 {
-   return nir_bcsel(b, nir_fneu(b, x, x), x, nir_bcsel(b, nir_fneu(b, y, y), y, res));
+   return nir_bcsel(b, nir_fisnan(b, x), x, nir_bcsel(b, nir_fisnan(b, y), y, res));
 }
 
 static inline nir_ssa_def *

commit 8280d19dd60d1a1a46b4774736d8a27030b7580e

nir: make nir_fisnan helper exact
Floating point ALU assume no NaNs unless
FLOAT_CONTROLS_SIGNED_ZERO_INF_NAN_PRESERVE_FPn or (for some opcodes)
exact=true.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Fixes: c5340c5d707 ("nir: add nir_fisnan helper function")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22206>

src/compiler/nir/nir_builtin_builder.h
@@ -56,7 +56,11 @@ nir_get_texture_size(nir_builder *b, nir_tex_instr *tex);
 static inline nir_ssa_def *
 nir_fisnan(nir_builder *b, nir_ssa_def *x)
 {
-   return nir_fneu(b, x, x);
+   bool old_exact = b->exact;
+   b->exact = true;
+   nir_ssa_def *res = nir_fneu(b, x, x);
+   b->exact = old_exact;
+   return res;
 }
 
 static inline nir_ssa_def *