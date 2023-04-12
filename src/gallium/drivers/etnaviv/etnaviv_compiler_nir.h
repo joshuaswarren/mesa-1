joshuaswarren/mesa-1

commit 4eb0e1eedaaf55c8f90f79e484ccc009ca7e0d14

nir: Combine if_uses with instruction uses
Every nir_ssa_def is part of a chain of uses, implemented with doubly linked
lists.  That means each requires 2 * 64-bit = 16 bytes per def, which is
memory intensive. Together they require 32 bytes per def. Not cool.

To cut that memory use in half, we can combine the two linked lists into a
single use list that contains both regular instruction uses and if-uses. To do
this, we augment the nir_src with a boolean "is_if", and reimplement the
abstract if-uses operations on top of that list. That boolean should fit into
the padding already in nir_src so should not actually affect memory use, and in
the future we sneak it into the bottom bit of a pointer.

However, this creates a new inefficiency: now iterating over regular uses
separate from if-uses is (nominally) more expensive. It turns out virtually
every caller of nir_foreach_if_use(_safe) also calls nir_foreach_use(_safe)
immediately before, so we rewrite most of the callers to instead call a new
single `nir_foreach_use_including_if(_safe)` which predicates the logic based on
`src->is_if`. This should mitigate the performance difference.

There's a bit of churn, but this is largely a mechanical set of changes.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22343>

src/gallium/drivers/etnaviv/etnaviv_compiler_nir.h
@@ -127,7 +127,7 @@ real_dest(nir_dest *dest, unsigned *swiz, unsigned *mask)
    if (!dest || !dest->is_ssa)
       return dest;
 
-   bool can_bypass_src = !list_length(&dest->ssa.if_uses);
+   bool can_bypass_src = nir_ssa_def_used_by_if(&dest->ssa);
    nir_instr *p_instr = dest->ssa.parent_instr;
 
    /* if used by a vecN, the "real" destination becomes the vecN destination
@@ -161,7 +161,7 @@ real_dest(nir_dest *dest, unsigned *swiz, unsigned *mask)
       case nir_op_vec2:
       case nir_op_vec3:
       case nir_op_vec4:
-         assert(list_length(&dest->ssa.if_uses) == 0);
+         assert(!nir_ssa_def_used_by_if(&dest->ssa));
          nir_foreach_use(use_src, &dest->ssa)
             assert(use_src->parent_instr == instr);
 
@@ -175,7 +175,8 @@ real_dest(nir_dest *dest, unsigned *swiz, unsigned *mask)
          default:
             continue;
          }
-         if (list_length(&dest->ssa.if_uses) || list_length(&dest->ssa.uses) > 1)
+         if (nir_ssa_def_used_by_if(&dest->ssa) ||
+             list_length(&dest->ssa.uses) > 1)
             continue;
 
          update_swiz_mask(alu, NULL, swiz, mask);

src/compiler/nir/nir.c
@@ -225,7 +225,6 @@ reg_create(void *mem_ctx, struct exec_list *list)
 
    list_inithead(&reg->uses);
    list_inithead(&reg->defs);
-   list_inithead(&reg->if_uses);
 
    reg->num_components = 0;
    reg->bit_size = 32;
@@ -888,7 +887,7 @@ nir_phi_instr_add_src(nir_phi_instr *instr, nir_block *pred, nir_src src)
    phi_src = gc_zalloc(gc_get_context(instr), nir_phi_src, 1);
    phi_src->pred = pred;
    phi_src->src = src;
-   phi_src->src.parent_instr = &instr->instr;
+   nir_src_set_parent_instr(&phi_src->src, &instr->instr);
    exec_list_push_tail(&instr->srcs, &phi_src->node);
 
    return phi_src;
@@ -1056,7 +1055,7 @@ add_use_cb(nir_src *src, void *state)
 {
    nir_instr *instr = state;
 
-   src->parent_instr = instr;
+   nir_src_set_parent_instr(src, instr);
    list_addtail(&src->use_link,
                 src->is_ssa ? &src->ssa->uses : &src->reg.reg->uses);
 
@@ -1633,19 +1632,16 @@ src_add_all_uses(nir_src *src, nir_instr *parent_instr, nir_if *parent_if)
          continue;
 
       if (parent_instr) {
-         src->parent_instr = parent_instr;
-         if (src->is_ssa)
-            list_addtail(&src->use_link, &src->ssa->uses);
-         else
-            list_addtail(&src->use_link, &src->reg.reg->uses);
+         nir_src_set_parent_instr(src, parent_instr);
       } else {
          assert(parent_if);
-         src->parent_if = parent_if;
-         if (src->is_ssa)
-            list_addtail(&src->use_link, &src->ssa->if_uses);
-         else
-            list_addtail(&src->use_link, &src->reg.reg->if_uses);
+         nir_src_set_parent_if(src, parent_if);
       }
+
+      if (src->is_ssa)
+         list_addtail(&src->use_link, &src->ssa->uses);
+      else
+         list_addtail(&src->use_link, &src->reg.reg->uses);
    }
 }
 
@@ -1677,7 +1673,7 @@ nir_if_rewrite_condition(nir_if *if_stmt, nir_src new_src)
 {
    nir_shader *shader = ralloc_parent(if_stmt);
    nir_src *src = &if_stmt->condition;
-   assert(!src_is_valid(src) || src->parent_if == if_stmt);
+   assert(!src_is_valid(src) || (src->is_if && src->parent_if == if_stmt));
 
    src_remove_all_uses(src);
    src_copy(src, &new_src, shader->gctx);
@@ -1716,7 +1712,6 @@ nir_ssa_def_init(nir_instr *instr, nir_ssa_def *def,
 {
    def->parent_instr = instr;
    list_inithead(&def->uses);
-   list_inithead(&def->if_uses);
    def->num_components = num_components;
    def->bit_size = bit_size;
    def->divergent = true; /* This is the safer default */
@@ -1747,11 +1742,12 @@ void
 nir_ssa_def_rewrite_uses(nir_ssa_def *def, nir_ssa_def *new_ssa)
 {
    assert(def != new_ssa);
-   nir_foreach_use_safe(use_src, def)
-      nir_instr_rewrite_src_ssa(use_src->parent_instr, use_src, new_ssa);
-
-   nir_foreach_if_use_safe(use_src, def)
-      nir_if_rewrite_condition_ssa(use_src->parent_if, use_src, new_ssa);
+   nir_foreach_use_including_if_safe(use_src, def) {
+      if (use_src->is_if)
+         nir_if_rewrite_condition_ssa(use_src->parent_if, use_src, new_ssa);
+      else
+         nir_instr_rewrite_src_ssa(use_src->parent_instr, use_src, new_ssa);
+   }
 }
 
 void
@@ -1760,11 +1756,12 @@ nir_ssa_def_rewrite_uses_src(nir_ssa_def *def, nir_src new_src)
    if (new_src.is_ssa) {
       nir_ssa_def_rewrite_uses(def, new_src.ssa);
    } else {
-      nir_foreach_use_safe(use_src, def)
-         nir_instr_rewrite_src(use_src->parent_instr, use_src, new_src);
-
-      nir_foreach_if_use_safe(use_src, def)
-         nir_if_rewrite_condition(use_src->parent_if, new_src);
+      nir_foreach_use_including_if_safe(use_src, def) {
+         if (use_src->is_if)
+            nir_if_rewrite_condition(use_src->parent_if, new_src);
+         else
+            nir_instr_rewrite_src(use_src->parent_instr, use_src, new_src);
+      }
    }
 }
 
@@ -1805,20 +1802,20 @@ nir_ssa_def_rewrite_uses_after(nir_ssa_def *def, nir_ssa_def *new_ssa,
    if (def == new_ssa)
       return;
 
-   nir_foreach_use_safe(use_src, def) {
-      assert(use_src->parent_instr != def->parent_instr);
-      /* Since def already dominates all of its uses, the only way a use can
-       * not be dominated by after_me is if it is between def and after_me in
-       * the instruction list.
-       */
-      if (!is_instr_between(def->parent_instr, after_me, use_src->parent_instr))
-         nir_instr_rewrite_src_ssa(use_src->parent_instr, use_src, new_ssa);
-   }
-
-   nir_foreach_if_use_safe(use_src, def) {
-      nir_if_rewrite_condition_ssa(use_src->parent_if,
-                                   &use_src->parent_if->condition,
-                                   new_ssa);
+   nir_foreach_use_including_if_safe(use_src, def) {
+      if (use_src->is_if) {
+         nir_if_rewrite_condition_ssa(use_src->parent_if,
+                                      &use_src->parent_if->condition,
+                                      new_ssa);
+      } else {
+         assert(use_src->parent_instr != def->parent_instr);
+         /* Since def already dominates all of its uses, the only way a use can
+          * not be dominated by after_me is if it is between def and after_me in
+          * the instruction list.
+          */
+         if (!is_instr_between(def->parent_instr, after_me, use_src->parent_instr))
+            nir_instr_rewrite_src_ssa(use_src->parent_instr, use_src, new_ssa);
+      }
    }
 }
 
@@ -1862,11 +1859,9 @@ nir_ssa_def_components_read(const nir_ssa_def *def)
 {
    nir_component_mask_t read_mask = 0;
 
-   if (!list_is_empty(&def->if_uses))
-      read_mask |= 1;
+   nir_foreach_use_including_if(use, def) {
+      read_mask |= use->is_if ? 1 : nir_src_components_read(use);
 
-   nir_foreach_use(use, def) {
-      read_mask |= nir_src_components_read(use);
       if (read_mask == (1 << def->num_components) - 1)
          return read_mask;
    }
@@ -2267,7 +2262,7 @@ nir_function_impl_lower_instructions(nir_function_impl *impl,
 
       assert(nir_foreach_dest(instr, dest_is_ssa, NULL));
       nir_ssa_def *old_def = nir_instr_ssa_def(instr);
-      struct list_head old_uses, old_if_uses;
+      struct list_head old_uses;
       if (old_def != NULL) {
          /* We're about to ask the callback to generate a replacement for instr.
           * Save off the uses from instr's SSA def so we know what uses to
@@ -2283,8 +2278,6 @@ nir_function_impl_lower_instructions(nir_function_impl *impl,
 
          list_replace(&old_def->uses, &old_uses);
          list_inithead(&old_def->uses);
-         list_replace(&old_def->if_uses, &old_if_uses);
-         list_inithead(&old_def->if_uses);
       }
 
       b.cursor = nir_after_instr(instr);
@@ -2296,11 +2289,12 @@ nir_function_impl_lower_instructions(nir_function_impl *impl,
             preserved = nir_metadata_none;
 
          nir_src new_src = nir_src_for_ssa(new_def);
-         list_for_each_entry_safe(nir_src, use_src, &old_uses, use_link)
-            nir_instr_rewrite_src(use_src->parent_instr, use_src, new_src);
-
-         list_for_each_entry_safe(nir_src, use_src, &old_if_uses, use_link)
-            nir_if_rewrite_condition(use_src->parent_if, new_src);
+         list_for_each_entry_safe(nir_src, use_src, &old_uses, use_link) {
+            if (use_src->is_if)
+               nir_if_rewrite_condition(use_src->parent_if, new_src);
+            else
+               nir_instr_rewrite_src(use_src->parent_instr, use_src, new_src);
+         }
 
          if (nir_ssa_def_is_unused(old_def)) {
             iter = nir_instr_free_and_dce(instr);
@@ -2310,10 +2304,9 @@ nir_function_impl_lower_instructions(nir_function_impl *impl,
          progress = true;
       } else {
          /* We didn't end up lowering after all.  Put the uses back */
-         if (old_def) {
+         if (old_def)
             list_replace(&old_uses, &old_def->uses);
-            list_replace(&old_if_uses, &old_def->if_uses);
-         }
+
          if (new_def == NIR_LOWER_INSTR_PROGRESS_REPLACE) {
             /* Only instructions without a return value can be removed like this */
             assert(!old_def);

src/compiler/nir/nir.h
@@ -876,9 +876,6 @@ typedef struct nir_register {
 
    /** set of nir_dests where this register is defined (written to) */
    struct list_head defs;
-
-   /** set of nir_ifs where this register is used as a condition */
-   struct list_head if_uses;
 } nir_register;
 
 #define nir_foreach_register(reg, reg_list) \
@@ -952,9 +949,6 @@ typedef struct nir_ssa_def {
    /** set of nir_instrs where this register is used (read from) */
    struct list_head uses;
 
-   /** set of nir_ifs where this register is used as a condition */
-   struct list_head if_uses;
-
    /** generic SSA definition index. */
    unsigned index;
 
@@ -1008,7 +1002,22 @@ typedef struct nir_src {
    };
 
    bool is_ssa;
+   bool is_if;
 } nir_src;
+
+static inline void
+nir_src_set_parent_instr(nir_src *src, nir_instr *parent_instr)
+{
+   src->is_if = false;
+   src->parent_instr = parent_instr;
+}
+
+static inline void
+nir_src_set_parent_if(nir_src *src, struct nir_if *parent_if)
+{
+   src->is_if = true;
+   src->parent_if = parent_if;
+}
 
 static inline nir_src
 nir_src_init(void)
@@ -1019,17 +1028,36 @@ nir_src_init(void)
 
 #define NIR_SRC_INIT nir_src_init()
 
-#define nir_foreach_use(src, reg_or_ssa_def) \
+#define nir_foreach_use_including_if(src, reg_or_ssa_def) \
    list_for_each_entry(nir_src, src, &(reg_or_ssa_def)->uses, use_link)
 
-#define nir_foreach_use_safe(src, reg_or_ssa_def) \
+#define nir_foreach_use_including_if_safe(src, reg_or_ssa_def) \
    list_for_each_entry_safe(nir_src, src, &(reg_or_ssa_def)->uses, use_link)
 
+#define nir_foreach_use(src, reg_or_ssa_def) \
+   nir_foreach_use_including_if(src, reg_or_ssa_def) \
+      if (!src->is_if)
+
+#define nir_foreach_use_safe(src, reg_or_ssa_def) \
+   nir_foreach_use_including_if_safe(src, reg_or_ssa_def) \
+      if (!src->is_if)
+
 #define nir_foreach_if_use(src, reg_or_ssa_def) \
-   list_for_each_entry(nir_src, src, &(reg_or_ssa_def)->if_uses, use_link)
+   nir_foreach_use_including_if(src, reg_or_ssa_def) \
+      if (src->is_if)
 
 #define nir_foreach_if_use_safe(src, reg_or_ssa_def) \
-   list_for_each_entry_safe(nir_src, src, &(reg_or_ssa_def)->if_uses, use_link)
+   nir_foreach_use_including_if_safe(src, reg_or_ssa_def) \
+      if (src->is_if)
+
+static inline bool
+nir_ssa_def_used_by_if(const nir_ssa_def *def)
+{
+   nir_foreach_if_use(_, def)
+      return true;
+
+   return false;
+}
 
 typedef struct {
    union {
@@ -4317,6 +4345,7 @@ static inline void
 nir_instr_rewrite_src_ssa(ASSERTED nir_instr *instr,
                           nir_src *src, nir_ssa_def *new_ssa)
 {
+   assert(!src->is_if);
    assert(src->parent_instr == instr);
    assert(src->is_ssa && src->ssa);
    list_del(&src->use_link);
@@ -4331,11 +4360,12 @@ static inline void
 nir_if_rewrite_condition_ssa(ASSERTED nir_if *if_stmt,
                              nir_src *src, nir_ssa_def *new_ssa)
 {
+   assert(src->is_if);
    assert(src->parent_if == if_stmt);
    assert(src->is_ssa && src->ssa);
    list_del(&src->use_link);
    src->ssa = new_ssa;
-   list_addtail(&src->use_link, &new_ssa->if_uses);
+   list_addtail(&src->use_link, &new_ssa->uses);
 }
 
 void nir_if_rewrite_condition(nir_if *if_stmt, nir_src new_src);
@@ -4367,7 +4397,7 @@ nir_component_mask_t nir_ssa_def_components_read(const nir_ssa_def *def);
 static inline bool
 nir_ssa_def_is_unused(nir_ssa_def *ssa)
 {
-   return list_is_empty(&ssa->uses) && list_is_empty(&ssa->if_uses);
+   return list_is_empty(&ssa->uses);
 }
 
 

src/compiler/nir/nir_clone.c
@@ -214,10 +214,9 @@ clone_register(clone_state *state, const nir_register *reg)
    nreg->num_array_elems = reg->num_array_elems;
    nreg->index = reg->index;
 
-   /* reconstructing uses/defs/if_uses handled by nir_instr_insert() */
+   /* reconstructing uses/defs handled by nir_instr_insert() */
    list_inithead(&nreg->uses);
    list_inithead(&nreg->defs);
-   list_inithead(&nreg->if_uses);
 
    return nreg;
 }

src/compiler/nir/nir_control_flow.c
@@ -577,14 +577,14 @@ update_if_uses(nir_cf_node *node)
       return;
 
    nir_if *if_stmt = nir_cf_node_as_if(node);
+   nir_src_set_parent_if(&if_stmt->condition, if_stmt);
 
-   if_stmt->condition.parent_if = if_stmt;
    if (if_stmt->condition.is_ssa) {
       list_addtail(&if_stmt->condition.use_link,
-                   &if_stmt->condition.ssa->if_uses);
+                   &if_stmt->condition.ssa->uses);
    } else {
       list_addtail(&if_stmt->condition.use_link,
-                   &if_stmt->condition.reg.reg->if_uses);
+                   &if_stmt->condition.reg.reg->uses);
    }
 }
 

src/compiler/nir/nir_deref.c
@@ -157,7 +157,10 @@ bool
 nir_deref_instr_has_complex_use(nir_deref_instr *deref,
                                 nir_deref_instr_has_complex_use_options opts)
 {
-   nir_foreach_use(use_src, &deref->dest.ssa) {
+   nir_foreach_use_including_if(use_src, &deref->dest.ssa) {
+      if (use_src->is_if)
+         return true;
+
       nir_instr *use_instr = use_src->parent_instr;
 
       switch (use_instr->type) {
@@ -234,9 +237,6 @@ nir_deref_instr_has_complex_use(nir_deref_instr *deref,
          return true;
       }
    }
-
-   nir_foreach_if_use(use, &deref->dest.ssa)
-      return true;
 
    return false;
 }
@@ -1187,7 +1187,9 @@ opt_deref_cast(nir_builder *b, nir_deref_instr *cast)
    assert(cast->dest.is_ssa);
    assert(cast->parent.is_ssa);
 
-   nir_foreach_use_safe(use_src, &cast->dest.ssa) {
+   nir_foreach_use_including_if_safe(use_src, &cast->dest.ssa) {
+      assert(!use_src->is_if && "there cannot be if-uses");
+
       /* If this isn't a trivial array cast, we can't propagate into
        * ptr_as_array derefs.
        */
@@ -1198,9 +1200,6 @@ opt_deref_cast(nir_builder *b, nir_deref_instr *cast)
       nir_instr_rewrite_src(use_src->parent_instr, use_src, cast->parent);
       progress = true;
    }
-
-   /* If uses would be a bit crazy */
-   assert(list_is_empty(&cast->dest.ssa.if_uses));
 
    if (nir_deref_instr_remove_if_unused(cast))
       progress = true;

src/compiler/nir/nir_from_ssa.c
@@ -1080,15 +1080,13 @@ static bool
 ssa_def_is_local_to_block(nir_ssa_def *def, UNUSED void *state)
 {
    nir_block *block = def->parent_instr->block;
-   nir_foreach_use(use_src, def) {
-      if (use_src->parent_instr->block != block ||
+   nir_foreach_use_including_if(use_src, def) {
+      if (use_src->is_if ||
+          use_src->parent_instr->block != block ||
           use_src->parent_instr->type == nir_instr_type_phi) {
          return false;
       }
    }
-
-   if (!list_is_empty(&def->if_uses))
-      return false;
 
    return true;
 }

src/compiler/nir/nir_loop_analyze.c
@@ -178,8 +178,8 @@ instr_cost(loop_info_state *state, nir_instr *instr,
             /* Also if the selects condition is only used by the select then
              * remove that alu instructons cost from the cost total also.
              */
-            if (!list_is_empty(&sel_alu->dest.dest.ssa.if_uses) ||
-                !list_is_singular(&sel_alu->dest.dest.ssa.uses))
+            if (!list_is_singular(&sel_alu->dest.dest.ssa.uses) ||
+                nir_ssa_def_used_by_if(&sel_alu->dest.dest.ssa))
                return 0;
             else
                return -1;

commit a71209916a01c986877eb418dd4beeb02a0a7950

etnaviv: Fix regression from if_uses change.
Flipped the condition in the conversion.

Fixes: 4eb0e1eedaaf ("nir: Combine if_uses with instruction uses")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22430>

src/gallium/drivers/etnaviv/etnaviv_compiler_nir.h
@@ -127,7 +127,7 @@ real_dest(nir_dest *dest, unsigned *swiz, unsigned *mask)
    if (!dest || !dest->is_ssa)
       return dest;
 
-   bool can_bypass_src = nir_ssa_def_used_by_if(&dest->ssa);
+   bool can_bypass_src = !nir_ssa_def_used_by_if(&dest->ssa);
    nir_instr *p_instr = dest->ssa.parent_instr;
 
    /* if used by a vecN, the "real" destination becomes the vecN destination