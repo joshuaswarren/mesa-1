joshuaswarren/mesa-1

commit 3a7a6e7a1ba0c99b6c4adb23127cfa2c2267beac

d3d12: Query device for D3D12_FEATURE_D3D12_OPTIONS14
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22454>

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -1474,6 +1474,12 @@ d3d12_init_screen(struct d3d12_screen *screen, IUnknown *adapter)
       debug_printf("D3D12: failed to get device options\n");
       return false;
    }
+   if (FAILED(screen->dev->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS14,
+                                               &screen->opts14,
+                                               sizeof(screen->opts14)))) {
+      debug_printf("D3D12: failed to get device options\n");
+      return false;
+   }
 #if D3D12_SDK_VERSION >= 609
    screen->dev->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS19, &screen->opts19, sizeof(screen->opts19));
 #endif

src/gallium/drivers/d3d12/d3d12_screen.h
@@ -115,6 +115,7 @@ struct d3d12_screen {
    D3D12_FEATURE_DATA_D3D12_OPTIONS2 opts2;
    D3D12_FEATURE_DATA_D3D12_OPTIONS3 opts3;
    D3D12_FEATURE_DATA_D3D12_OPTIONS4 opts4;
+   D3D12_FEATURE_DATA_D3D12_OPTIONS14 opts14;
 #if D3D12_SDK_VERSION >= 610
    D3D12_FEATURE_DATA_D3D12_OPTIONS19 opts19;
 #endif

commit b7eda00a700e104713d7b9e43b801c0e90aef535

d3d12: Do not fail d3d12_screen creation if D3D12_FEATURE_D3D12_OPTIONS14 not available
Fixes: 3a7a6e7a1ba0c99b6c4adb23127cfa2c2267beac ("d3d12: Query device for D3D12_FEATURE_D3D12_OPTIONS14")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22534>

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -1474,12 +1474,7 @@ d3d12_init_screen(struct d3d12_screen *screen, IUnknown *adapter)
       debug_printf("D3D12: failed to get device options\n");
       return false;
    }
-   if (FAILED(screen->dev->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS14,
-                                               &screen->opts14,
-                                               sizeof(screen->opts14)))) {
-      debug_printf("D3D12: failed to get device options\n");
-      return false;
-   }
+   screen->dev->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS14, &screen->opts14, sizeof(screen->opts14));
    screen->dev->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS19, &screen->opts19, sizeof(screen->opts19));
 
    screen->architecture.NodeIndex = 0;