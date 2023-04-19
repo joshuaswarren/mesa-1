joshuaswarren/mesa-1

commit 7e63e9bc652b2005d51f76ba93b922661de9ba0e

nir: Add helper to create passthrough TCS shader
Based on si_create_passthrough_tcs() as that seemed the most generic of
the various different backend driver implementations.  Uses the
load_tess_level_outer_default and load_tess_level_inner_default
intrinsics to load the gl_TessLevelOuter and gl_TessLevelInner values,
so driver will somehow need to implement those to load the values set
by pipe_context::set_tess_state() or similar.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19259>

src/compiler/nir/nir_passthrough_tcs.c
@@ -1,0 +1,110 @@
+/*
+ * Copyright © 2022 Google, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "nir.h"
+#include "nir_builder.h"
+
+/*
+ * A helper to create a passthrough TCS shader for drivers that cannot handle
+ * having a TES without TCS.
+ *
+ * Uses the load_tess_level_outer_default and load_tess_level_inner_default
+ * intrinsics to load the gl_TessLevelOuter and gl_TessLevelInner values,
+ * so driver will somehow need to implement those to load the values set
+ * by pipe_context::set_tess_state() or similar.
+ */
+
+nir_shader *
+nir_create_passthrough_tcs(const nir_shader_compiler_options *options,
+                           const nir_shader *vs, uint8_t patch_vertices)
+{
+   nir_builder b = nir_builder_init_simple_shader(MESA_SHADER_TESS_CTRL, options,
+                                                  "tcs passthrough");
+
+   unsigned num_inputs = 0;
+   unsigned num_outputs = 0;
+
+   nir_variable *in_inner =
+      nir_variable_create(b.shader, nir_var_system_value, glsl_vec_type(2),
+                          "tess inner default");
+   in_inner->data.location = SYSTEM_VALUE_TESS_LEVEL_INNER_DEFAULT;
+
+   nir_variable *out_inner =
+      nir_variable_create(b.shader, nir_var_shader_out, glsl_vec_type(2),
+                          "tess inner");
+   out_inner->data.location = VARYING_SLOT_TESS_LEVEL_INNER;
+   out_inner->data.driver_location = num_outputs++;
+
+   nir_ssa_def *inner = nir_load_var(&b, in_inner);
+   nir_store_var(&b, out_inner, inner, 0x3);
+
+   nir_variable *in_outer =
+      nir_variable_create(b.shader, nir_var_system_value, glsl_vec4_type(),
+                          "tess outer default");
+   in_outer->data.location = SYSTEM_VALUE_TESS_LEVEL_OUTER_DEFAULT;
+
+   nir_variable *out_outer =
+      nir_variable_create(b.shader, nir_var_shader_out, glsl_vec4_type(),
+                          "tess outer");
+   out_outer->data.location = VARYING_SLOT_TESS_LEVEL_OUTER;
+   out_outer->data.driver_location = num_outputs++;
+
+   nir_ssa_def *outer = nir_load_var(&b, in_outer);
+   nir_store_var(&b, out_outer, outer, 0xf);
+
+   nir_ssa_def *id = nir_load_invocation_id(&b);
+   nir_foreach_shader_out_variable(var, vs) {
+      const struct glsl_type *type;
+      unsigned semantic = var->data.location;
+      if (semantic < VARYING_SLOT_VAR31 && semantic != VARYING_SLOT_EDGE)
+         type = glsl_array_type(glsl_vec4_type(), 0, 0);
+      else if (semantic >= VARYING_SLOT_VAR0_16BIT)
+         type = glsl_array_type(glsl_vector_type(GLSL_TYPE_FLOAT16, 4), 0, 0);
+      else
+         continue;
+
+      char name[1024];
+      snprintf(name, sizeof(name), "in_%s", var->name);
+      nir_variable *in = nir_variable_create(b.shader, nir_var_shader_in, type, name);
+      in->data.location = semantic;
+      in->data.driver_location = num_inputs++;
+
+      snprintf(name, sizeof(name), "out_%s", var->name);
+      nir_variable *out = nir_variable_create(b.shader, nir_var_shader_out, type, name);
+      out->data.location = semantic;
+      out->data.driver_location = num_outputs++;
+
+      /* no need to use copy_var to save a lower pass */
+      nir_ssa_def *value = nir_load_array_var(&b, in, id);
+      nir_store_array_var(&b, out, id, value, 0xf);
+   }
+
+   b.shader->num_inputs = num_inputs;
+   b.shader->num_outputs = num_outputs;
+
+   b.shader->info.tess.tcs_vertices_out = patch_vertices;
+
+   nir_validate_shader(b.shader, "in nir_create_passthrough_tcs");
+
+   return b.shader;
+}

src/compiler/nir/meson.build
@@ -262,6 +262,7 @@ files_libnir = files(
   'nir_opt_undef.c',
   'nir_opt_uniform_atomics.c',
   'nir_opt_vectorize.c',
+  'nir_passthrough_tcs.c',
   'nir_phi_builder.c',
   'nir_phi_builder.h',
   'nir_print.c',

src/compiler/nir/nir.h
@@ -4905,6 +4905,8 @@ void nir_lower_io_to_scalar(nir_shader *shader, nir_variable_mode mask);
 bool nir_lower_io_to_scalar_early(nir_shader *shader, nir_variable_mode mask);
 bool nir_lower_io_to_vector(nir_shader *shader, nir_variable_mode mask);
 bool nir_vectorize_tess_levels(nir_shader *shader);
+nir_shader * nir_create_passthrough_tcs(const nir_shader_compiler_options *options,
+                                        const nir_shader *vs, uint8_t patch_vertices);
 
 bool nir_lower_fragcolor(nir_shader *shader, unsigned max_cbufs);
 bool nir_lower_fragcoord_wtrans(nir_shader *shader);

commit 721a13a898da2adf5b535e15dd16a0104e0c1cc7

nir: fix 2 bugs in nir_create_passthrough_tcs
- VAR31 was ignored.
- Only a half of the 16-bit slot was passed through, though I'm not sure
  if nir_lower_io handles vec8. The slots are only for GLES and I don't
  think a passthrough TCS is possible with GLES.

Fixes: 7e63e9bc652b2 - nir: Add helper to create passthrough TCS shader

Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21861>

src/compiler/nir/nir_passthrough_tcs.c
@@ -77,10 +77,9 @@ nir_create_passthrough_tcs_impl(const nir_shader_compiler_options *options,
    for (unsigned i = 0; i < num_locations; i++) {
       const struct glsl_type *type;
       unsigned semantic = locations[i];
-      if (semantic < VARYING_SLOT_VAR31 && semantic != VARYING_SLOT_EDGE)
+      if ((semantic <= VARYING_SLOT_VAR31 && semantic != VARYING_SLOT_EDGE) ||
+          semantic >= VARYING_SLOT_VAR0_16BIT)
          type = glsl_array_type(glsl_vec4_type(), 0, 0);
-      else if (semantic >= VARYING_SLOT_VAR0_16BIT)
-         type = glsl_array_type(glsl_vector_type(GLSL_TYPE_FLOAT16, 4), 0, 0);
       else
          continue;
 