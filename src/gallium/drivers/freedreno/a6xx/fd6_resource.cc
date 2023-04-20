joshuaswarren/mesa-1

commit 404baa688a21ab68a29c5704bd0d97e037e17f36

freedreno/a6xx: Allow z24s8 format casts
Allow UBWC format casts between z24s8/x24s8/z24x8.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8816
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22427>

src/gallium/drivers/freedreno/a6xx/fd6_resource.cc
@@ -119,10 +119,31 @@ is_norm(enum pipe_format format)
 }
 
 static bool
+is_z24s8(enum pipe_format format)
+{
+   switch (format) {
+   case PIPE_FORMAT_Z24_UNORM_S8_UINT:
+   case PIPE_FORMAT_Z24X8_UNORM:
+   case PIPE_FORMAT_X24S8_UINT:
+   case PIPE_FORMAT_Z24_UNORM_S8_UINT_AS_R8G8B8A8:
+      return true;
+   default:
+      return false;
+   }
+}
+
+static bool
 valid_format_cast(struct fd_resource *rsc, enum pipe_format format)
 {
    /* Special case "casting" format in hw: */
    if (format == PIPE_FORMAT_Z24_UNORM_S8_UINT_AS_R8G8B8A8)
+      return true;
+
+   /* If we support z24s8 ubwc then allow casts between the various
+    * permutations of z24s8:
+    */
+   if (fd_screen(rsc->b.b.screen)->info->a6xx.has_z24uint_s8uint &&
+         (is_z24s8(format) == is_z24s8(rsc->b.b.format)))
       return true;
 
    /* For some color values (just "solid white") compression metadata maps to

commit 2979158e0bd2f161a79e0e4efde4b0bb449f1a9b

freedreno/a6xx: Fix valid_format_cast logic for newer a6xx
Fixes a pile of
dEQP-GLES31.functional.copy_image.non_compressed.viewclass_32_bits.*
on a6xx gen2 and later.

Fixes: 404baa688a2 ("freedreno/a6xx: Allow z24s8 format casts")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22610>

src/gallium/drivers/freedreno/a6xx/fd6_resource.cc
@@ -143,7 +143,7 @@ valid_format_cast(struct fd_resource *rsc, enum pipe_format format)
     * permutations of z24s8:
     */
    if (fd_screen(rsc->b.b.screen)->info->a6xx.has_z24uint_s8uint &&
-         (is_z24s8(format) == is_z24s8(rsc->b.b.format)))
+         is_z24s8(format) && is_z24s8(rsc->b.b.format))
       return true;
 
    /* For some color values (just "solid white") compression metadata maps to