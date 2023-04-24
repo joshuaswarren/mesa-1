openharmony/third_party_mesa3d

commit aef87617c8bc200a80454feec90a99bf544dba95

draw: add view_mask rendering support
This loops the draws per-view above the instance rendering

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9399>

src/gallium/auxiliary/draw/draw_private.h
@@ -198,6 +198,7 @@ struct draw_context
          unsigned min_index;
          unsigned max_index;
          unsigned drawid;
+         unsigned viewid;
          
          /** vertex arrays */
          struct draw_vertex_buffer vbuffer[PIPE_MAX_ATTRIBS];

src/gallium/auxiliary/draw/draw_pt.c
@@ -540,7 +540,7 @@ draw_vbo(struct draw_context *draw,
    draw->pt.user.max_index = info->index_bounds_valid ? info->max_index : ~0;
    draw->pt.user.eltSize = info->index_size ? draw->pt.user.eltSizeIB : 0;
    draw->pt.user.drawid = info->drawid;
-
+   draw->pt.user.viewid = 0;
    draw->pt.vertices_per_patch = info->vertices_per_patch;
 
    if (0)
@@ -604,7 +604,13 @@ draw_vbo(struct draw_context *draw,
     * the min_index/max_index hints given by gallium frontends.
     */
 
-   draw_instances(draw, info, draws, count);
+   if (info->view_mask) {
+      u_foreach_bit(i, info->view_mask) {
+         draw->pt.user.viewid = i;
+         draw_instances(draw, info, draws, count);
+      }
+   } else
+      draw_instances(draw, info, draws, count);
 
    /* If requested emit the pipeline statistics for this run */
    if (draw->collect_statistics) {

joshuaswarren/mesa-1

commit 8d4b0b09f5838e6c61b38cd99035e7d7cb4ead1b

draw: fix viewmask iterating
the frontend has to be flushed and setup again if only
the viewmask has changed since the last draw

Fixes: aef87617c8b ("draw: add view_mask rendering support")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22628>

src/gallium/auxiliary/draw/draw_private.h
@@ -184,6 +184,7 @@ struct draw_context
       enum pipe_prim_type prim;
       unsigned opt;     /**< bitmask of PT_x flags */
       unsigned eltSize; /* saved eltSize for flushing */
+      unsigned viewid; /* saved viewid for flushing */
       ubyte vertices_per_patch;
       boolean rebind_parameters;
 

src/gallium/auxiliary/draw/draw_pt.c
@@ -103,9 +103,9 @@ draw_pt_arrays(struct draw_context *draw,
           */
          draw_do_flush(draw, DRAW_FLUSH_STATE_CHANGE);
          frontend = NULL;
-      } else if (draw->pt.eltSize != draw->pt.user.eltSize) {
-         /* Flush draw state if eltSize changed.
-          * This could be improved so only the frontend is flushed since it
+      } else if (draw->pt.eltSize != draw->pt.user.eltSize || draw->pt.viewid != draw->pt.user.viewid) {
+         /* Flush draw state if eltSize or viewid changed.
+          * eltSize changes could be improved so only the frontend is flushed since it
           * converts all indices to ushorts and the fetch part of the middle
           * always prepares both linear and indexed.
           */
@@ -121,6 +121,7 @@ draw_pt_arrays(struct draw_context *draw,
 
       draw->pt.frontend = frontend;
       draw->pt.eltSize = draw->pt.user.eltSize;
+      draw->pt.viewid = draw->pt.user.viewid;
       draw->pt.prim = prim;
       draw->pt.opt = opt;
    }