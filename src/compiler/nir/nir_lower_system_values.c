joshuaswarren/mesa-1

commit d397a468741b9ad395abedf06c3c8e5a4cb11f73

spirv/nir: wire ray interection triangle position fetch
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <f{merge_request.web_url}>

src/compiler/nir/nir_lower_system_values.c
@@ -148,6 +148,7 @@ lower_system_value_instr(nir_builder *b, nir_instr *instr, void *_state)
          case SYSTEM_VALUE_RAY_OBJECT_TO_WORLD:
          case SYSTEM_VALUE_RAY_WORLD_TO_OBJECT:
          case SYSTEM_VALUE_MESH_VIEW_INDICES:
+         case SYSTEM_VALUE_RAY_TRIANGLE_VERTEX_POSITIONS:
             /* These are all single-element arrays in our implementation, and
              * the sysval load below just drops the 0 array index.
              */
@@ -250,6 +251,19 @@ lower_system_value_instr(nir_builder *b, nir_instr *instr, void *_state)
             assert(cols[i]->num_components == num_rows);
          }
          return nir_select_from_ssa_def_array(b, cols, num_cols, column);
+      } else if (glsl_type_is_array(var->type)) {
+         unsigned num_elems = glsl_get_length(var->type);
+         const struct glsl_type *elem_type = glsl_get_array_element(var->type);
+         assert(glsl_get_components(elem_type) == intrin->dest.ssa.num_components);
+
+         nir_ssa_def *elems[4];
+         assert(ARRAY_SIZE(elems) >= num_elems);
+         for (unsigned i = 0; i < num_elems; i++) {
+            elems[i] = nir_load_system_value(b, sysval_op, i,
+                                             intrin->dest.ssa.num_components,
+                                             intrin->dest.ssa.bit_size);
+         }
+         return nir_select_from_ssa_def_array(b, elems, num_elems, column);
       } else {
          return nir_load_system_value(b, sysval_op, 0,
                                       intrin->dest.ssa.num_components,

src/compiler/nir/nir.c
@@ -2486,6 +2486,8 @@ nir_intrinsic_from_system_value(gl_system_value val)
       return nir_intrinsic_load_ray_instance_custom_index;
    case SYSTEM_VALUE_CULL_MASK:
       return nir_intrinsic_load_cull_mask;
+   case SYSTEM_VALUE_RAY_TRIANGLE_VERTEX_POSITIONS:
+      return nir_intrinsic_load_ray_triangle_vertex_positions;
    case SYSTEM_VALUE_MESH_VIEW_COUNT:
       return nir_intrinsic_load_mesh_view_count;
    case SYSTEM_VALUE_FRAG_SHADING_RATE:
@@ -2639,6 +2641,8 @@ nir_system_value_from_intrinsic(nir_intrinsic_op intrin)
       return SYSTEM_VALUE_RAY_INSTANCE_CUSTOM_INDEX;
    case nir_intrinsic_load_cull_mask:
       return SYSTEM_VALUE_CULL_MASK;
+   case nir_intrinsic_load_ray_triangle_vertex_positions:
+      return SYSTEM_VALUE_RAY_TRIANGLE_VERTEX_POSITIONS;
    case nir_intrinsic_load_frag_shading_rate:
       return SYSTEM_VALUE_FRAG_SHADING_RATE;
    case nir_intrinsic_load_mesh_view_count:

src/compiler/nir/nir.h
@@ -252,6 +252,7 @@ typedef enum {
    nir_ray_query_value_flags,
    nir_ray_query_value_world_ray_direction,
    nir_ray_query_value_world_ray_origin,
+   nir_ray_query_value_intersection_triangle_vertex_positions
 } nir_ray_query_value;
 
 typedef union {

src/compiler/nir/nir_divergence_analysis.c
@@ -697,6 +697,7 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_report_ray_intersection:
    case nir_intrinsic_rq_proceed:
    case nir_intrinsic_rq_load:
+   case nir_intrinsic_load_ray_triangle_vertex_positions:
       is_divergent = true;
       break;
 

src/compiler/nir/nir_gather_info.c
@@ -758,6 +758,7 @@ gather_intrinsic_info(nir_intrinsic_instr *instr, nir_shader *shader,
    case nir_intrinsic_load_mesh_view_count:
    case nir_intrinsic_load_gs_header_ir3:
    case nir_intrinsic_load_tcs_header_ir3:
+   case nir_intrinsic_load_ray_triangle_vertex_positions:
       BITSET_SET(shader->info.system_values_read,
                  nir_system_value_from_intrinsic(instr->intrinsic));
       break;

src/compiler/nir/nir_intrinsics.py
@@ -893,6 +893,7 @@ system_value("ray_geometry_index", 1)
 system_value("ray_instance_custom_index", 1)
 system_value("shader_record_ptr", 1, bit_sizes=[64])
 system_value("cull_mask", 1)
+system_value("ray_triangle_vertex_positions", 3, indices=[COLUMN])
 
 # Driver-specific viewport scale/offset parameters.
 #

src/compiler/shader_enums.c
@@ -380,6 +380,7 @@ gl_system_value_name(gl_system_value sysval)
      ENUM(SYSTEM_VALUE_RAY_FLAGS),
      ENUM(SYSTEM_VALUE_RAY_GEOMETRY_INDEX),
      ENUM(SYSTEM_VALUE_CULL_MASK),
+     ENUM(SYSTEM_VALUE_RAY_TRIANGLE_VERTEX_POSITIONS),
      ENUM(SYSTEM_VALUE_MESH_VIEW_COUNT),
      ENUM(SYSTEM_VALUE_MESH_VIEW_INDICES),
      ENUM(SYSTEM_VALUE_GS_HEADER_IR3),

src/compiler/shader_enums.h
@@ -841,6 +841,7 @@ typedef enum
    SYSTEM_VALUE_RAY_GEOMETRY_INDEX,
    SYSTEM_VALUE_RAY_INSTANCE_CUSTOM_INDEX,
    SYSTEM_VALUE_CULL_MASK,
+   SYSTEM_VALUE_RAY_TRIANGLE_VERTEX_POSITIONS,
    /*@}*/
 
    /**

commit 2ff4d5bf9d404b8641777f160644c09a4d34710a

nir_lower_system_values: Add ASSERTED to assert-only variable
Fixes: d397a468 ("spirv/nir: wire ray interection triangle position fetch")
Reviewed-by: Matt Turner <mattst88@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22994>

src/compiler/nir/nir_lower_system_values.c
@@ -253,7 +253,7 @@ lower_system_value_instr(nir_builder *b, nir_instr *instr, void *_state)
          return nir_select_from_ssa_def_array(b, cols, num_cols, column);
       } else if (glsl_type_is_array(var->type)) {
          unsigned num_elems = glsl_get_length(var->type);
-         const struct glsl_type *elem_type = glsl_get_array_element(var->type);
+         ASSERTED const struct glsl_type *elem_type = glsl_get_array_element(var->type);
          assert(glsl_get_components(elem_type) == intrin->dest.ssa.num_components);
 
          nir_ssa_def *elems[4];