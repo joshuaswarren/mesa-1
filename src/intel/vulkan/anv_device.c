joshuaswarren/mesa-1

commit c8575c14c09d90c2d3cfad0c5e5726ade0e3b935

anv: enable graphics pipeline libraries by default
Since we are disabling mesh, which has issues with gpl, enable gpl by
default now, leaving the renamed environment variable as a way to
disable it for debug purposes.

Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22910>

src/intel/vulkan/anv_device.c
@@ -315,7 +315,7 @@ get_device_extensions(const struct anv_physical_device *device,
                                                VK_QUEUE_GLOBAL_PRIORITY_MEDIUM_KHR,
       .EXT_global_priority_query             = device->max_context_priority >=
                                                VK_QUEUE_GLOBAL_PRIORITY_MEDIUM_KHR,
-      .EXT_graphics_pipeline_library         = device->gpl_enabled,
+      .EXT_graphics_pipeline_library         = !debug_get_bool_option("ANV_NO_GPL", false),
       .EXT_host_query_reset                  = true,
       .EXT_image_2d_view_of_3d               = true,
       .EXT_image_robustness                  = true,
@@ -589,7 +589,8 @@ get_features(const struct anv_physical_device *pdevice,
       .globalPriorityQuery = true,
 
       /* VK_EXT_graphics_pipeline_library */
-      .graphicsPipelineLibrary = pdevice->gpl_enabled,
+      .graphicsPipelineLibrary =
+         pdevice->vk.supported_features.graphicsPipelineLibrary,
 
       /* VK_KHR_fragment_shading_rate */
       .pipelineFragmentShadingRate = true,
@@ -1342,18 +1343,6 @@ anv_physical_device_try_create(struct vk_instance *vk_instance,
    device->generated_indirect_draws =
       debug_get_bool_option("ANV_ENABLE_GENERATED_INDIRECT_DRAWS",
                             true);
-
-   /* The GPL implementation is new, and may have issues in conjunction with
-    * mesh shading. Enable it by default for zink for performance reasons (where
-    * mesh shading is unused anyway), and have an env var for testing in CI or
-    * by end users.
-    * */
-   if (debug_get_bool_option("ANV_GPL",
-                             instance->vk.app_info.engine_name != NULL &&
-                             (strcmp(instance->vk.app_info.engine_name, "mesa zink") == 0 ||
-                              strcmp(instance->vk.app_info.engine_name, "DXVK") == 0))) {
-      device->gpl_enabled = true;
-   }
 
    unsigned st_idx = 0;
 

src/intel/ci/deqp-anv-tgl-vk.toml
@@ -4,12 +4,3 @@ deqp = "/deqp/external/vulkancts/modules/vulkan/deqp-vk"
 caselists = ["/deqp/mustpass/vk-master.txt"]
 fraction = 2
 renderer_check = "TGL GT2"
-
-[[deqp]]
-deqp = "/deqp/external/vulkancts/modules/vulkan/deqp-vk"
-caselists = ["/deqp/mustpass/vk-master.txt"]
-fraction = 2
-include = ["dEQP-VK.pipeline.pipeline_library"]
-prefix = "gpl-"
-[deqp.env]
-ANV_GPL = "true"

src/intel/vulkan/anv_private.h
@@ -865,7 +865,6 @@ struct anv_physical_device {
     struct intel_device_info                      info;
 
     bool                                        video_decode_enabled;
-    bool                                        gpl_enabled;
 
     struct brw_compiler *                       compiler;
     struct isl_device                           isl_dev;

commit 9ce999a932a377c72ea3ffce30c64e11874f2962

anv: enable the GPL feature based on whether the extension is supported
Instead of checking if the very same bit we want to enable is already
enabled, which obviously doesn't work.

Fixes: c8575c14c09 ("anv: enable graphics pipeline libraries by default")

Acked-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23039>

src/intel/vulkan/anv_device.c
@@ -590,7 +590,7 @@ get_features(const struct anv_physical_device *pdevice,
 
       /* VK_EXT_graphics_pipeline_library */
       .graphicsPipelineLibrary =
-         pdevice->vk.supported_features.graphicsPipelineLibrary,
+         pdevice->vk.supported_extensions.EXT_graphics_pipeline_library,
 
       /* VK_KHR_fragment_shading_rate */
       .pipelineFragmentShadingRate = true,