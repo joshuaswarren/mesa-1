openharmony/third_party_mesa3d

commit ff3b6c1dbdd6a179631b1fb1cbfc79b5e329ca90

anv: Support fetching descriptor addresses from push constants
Bindless shaders don't have binding tables so they have to get at the
descriptor sets via a different mechanism.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8637>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -26,6 +26,8 @@
 #include "compiler/brw_nir.h"
 #include "util/mesa-sha1.h"
 
+#define sizeof_field(type, field) sizeof(((type *)0)->field)
+
 void
 anv_nir_compute_push_layout(const struct anv_physical_device *pdevice,
                             bool robust_buffer_access,
@@ -64,6 +66,13 @@ anv_nir_compute_push_layout(const struct anv_physical_device *pdevice,
                push_end = MAX2(push_end, base + range);
                break;
             }
+
+            case nir_intrinsic_load_desc_set_address_intel:
+               push_start = MIN2(push_start,
+                  offsetof(struct anv_push_constants, desc_sets));
+               push_end = MAX2(push_end, push_start +
+                  sizeof_field(struct anv_push_constants, desc_sets));
+               break;
 
             default:
                break;
@@ -130,6 +139,9 @@ anv_nir_compute_push_layout(const struct anv_physical_device *pdevice,
          if (!function->impl)
             continue;
 
+         nir_builder build, *b = &build;
+         nir_builder_init(b, function->impl);
+
          nir_foreach_block(block, function->impl) {
             nir_foreach_instr_safe(instr, block) {
                if (instr->type != nir_instr_type_intrinsic)
@@ -143,6 +155,17 @@ anv_nir_compute_push_layout(const struct anv_physical_device *pdevice,
                                          nir_intrinsic_base(intrin) -
                                          push_start);
                   break;
+
+               case nir_intrinsic_load_desc_set_address_intel: {
+                  b->cursor = nir_before_instr(&intrin->instr);
+                  nir_ssa_def *pc_load = nir_load_uniform(b, 1, 64,
+                     nir_imul_imm(b, intrin->src[0].ssa, sizeof(uint64_t)),
+                     .base = offsetof(struct anv_push_constants, desc_sets),
+                     .range = sizeof_field(struct anv_push_constants, desc_sets),
+                     .dest_type = nir_type_uint64);
+                  nir_ssa_def_rewrite_uses(&intrin->dest.ssa, pc_load);
+                  break;
+               }
 
                default:
                   break;

src/intel/vulkan/anv_cmd_buffer.c
@@ -928,6 +928,25 @@ anv_cmd_buffer_bind_descriptor_set(struct anv_cmd_buffer *cmd_buffer,
    VkShaderStageFlags dirty_stages = 0;
    if (pipe_state->descriptors[set_index] != set) {
       pipe_state->descriptors[set_index] = set;
+
+      /* Ray-tracing shaders are entirely bindless and so they don't have
+       * access to HW binding tables.  This means that we have to upload the
+       * descriptor set as an 64-bit address in the push constants.
+       */
+      if (bind_point == VK_PIPELINE_BIND_POINT_RAY_TRACING_KHR) {
+         struct anv_push_constants *push = &pipe_state->push_constants;
+
+         struct anv_address set_addr = {
+            .bo = set->pool->bo,
+            .offset = set->desc_mem.offset,
+         };
+         push->desc_sets[set_index] = anv_address_physical(set_addr);
+
+         anv_reloc_list_add_bo(cmd_buffer->batch.relocs,
+                               cmd_buffer->batch.alloc,
+                               set->pool->bo);
+      }
+
       dirty_stages |= stages;
    }
 

src/intel/vulkan/anv_private.h
@@ -2679,6 +2679,9 @@ struct anv_push_constants {
    /** Pad out to a multiple of 32 bytes */
    uint32_t pad[2];
 
+   /* Base addresses for descriptor sets */
+   uint64_t desc_sets[MAX_SETS];
+
    struct {
       /** Base workgroup ID
        *

joshuaswarren/mesa-1

commit 58d143349b331bf4f9a8563d41fbf3ee8a91359f

anv: fix push range for descriptor offsets
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ff3b6c1dbd ("anv: Support fetching descriptor addresses from push constants")
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21645>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -69,12 +69,13 @@ anv_nir_compute_push_layout(nir_shader *nir,
             }
 
             case nir_intrinsic_load_desc_set_address_intel:
-            case nir_intrinsic_load_desc_set_dynamic_index_intel:
-               push_start = MIN2(push_start,
-                  offsetof(struct anv_push_constants, desc_sets));
-               push_end = MAX2(push_end, push_start +
+            case nir_intrinsic_load_desc_set_dynamic_index_intel: {
+               unsigned base = offsetof(struct anv_push_constants, desc_sets);
+               push_start = MIN2(push_start, base);
+               push_end = MAX2(push_end, base +
                   sizeof_field(struct anv_push_constants, desc_sets));
                break;
+            }
 
             default:
                break;