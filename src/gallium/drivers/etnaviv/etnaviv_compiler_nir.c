openharmony/third_party_mesa3d

commit 9c812fb6f7dca2a73a931bfad0546c6e4882d035

etnaviv: use nir_lower_idiv(..) before opt loop
nir_lower_idiv(..) creates during its lowering isub instructions.
Move nir_lower_idiv(..) before the opt loop to have a chance to
optimize/lower isub away. Also drop the drop the halti dependency to
make it easier to follow.

This fixes the following assert on GC3000:
  Unhandled ALU op: isub

Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9447>

src/gallium/drivers/etnaviv/etnaviv_compiler_nir.c
@@ -1104,6 +1104,7 @@ etna_compile_shader_nir(struct etna_shader_variant *v)
    NIR_PASS_V(s, nir_lower_indirect_derefs, nir_var_all, UINT32_MAX);
    NIR_PASS_V(s, nir_lower_tex, &(struct nir_lower_tex_options) { .lower_txp = ~0u });
    NIR_PASS_V(s, nir_lower_alu_to_scalar, etna_alu_to_scalar_filter_cb, specs);
+   NIR_PASS_V(s, nir_lower_idiv, nir_lower_idiv_fast);
 
    etna_optimize_loop(s);
 
@@ -1125,7 +1126,6 @@ etna_compile_shader_nir(struct etna_shader_variant *v)
       NIR_PASS_V(s, nir_opt_algebraic);
       NIR_PASS_V(s, nir_lower_bool_to_float);
    } else {
-      NIR_PASS_V(s, nir_lower_idiv, nir_lower_idiv_fast);
       NIR_PASS_V(s, nir_lower_bool_to_int32);
    }
 

joshuaswarren/mesa-1

commit 4def1e5e869c4c44d62be39df15668d2fe9cca56

etnaviv: nir: do not call nir_lower_idiv(..) unconditionally
GPUs without native integers lower idiv in lower_int_to_float and
there is no need to call nir_lower_idiv(..) for such GPUs.

Fixes nir crashes I am seeing with gc2000_gles2 CI job.

Fixes: 9c812fb6f7d ("etnaviv: use nir_lower_idiv(..) before opt loop")
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23450>

src/gallium/drivers/etnaviv/etnaviv_compiler_nir.c
@@ -1139,10 +1139,12 @@ etna_compile_shader(struct etna_shader_variant *v)
                                           v->key.tex_swizzle);
 
    NIR_PASS_V(s, nir_lower_alu_to_scalar, etna_alu_to_scalar_filter_cb, specs);
-   nir_lower_idiv_options idiv_options = {
-      .allow_fp16 = true,
-   };
-   NIR_PASS_V(s, nir_lower_idiv, &idiv_options);
+   if (c->specs->halti >= 2) {
+      nir_lower_idiv_options idiv_options = {
+         .allow_fp16 = true,
+      };
+      NIR_PASS_V(s, nir_lower_idiv, &idiv_options);
+   }
    NIR_PASS_V(s, nir_lower_alu);
 
    etna_optimize_loop(s);