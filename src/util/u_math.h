joshuaswarren/mesa-1

commit 321eccf72a4323cf72272b025bbbaab319e28c50

util: migrate alignment functions and macros to use ALIGN_POT
Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20153>

src/util/u_math.h
@@ -49,6 +49,7 @@
 #include "u_endian.h" /* for UTIL_ARCH_BIG_ENDIAN */
 #include "util/detect_cc.h"
 #include "util/detect_arch.h"
+#include "util/macros.h"
 
 #ifdef __HAIKU__
 #include <sys/param.h>
@@ -656,7 +657,7 @@ static inline uintptr_t
 ALIGN(uintptr_t value, int32_t alignment)
 {
    assert(util_is_power_of_two_nonzero(alignment));
-   return (((value) + (alignment) - 1) & ~((alignment) - 1));
+   return ALIGN_POT(value, alignment);
 }
 
 /**
@@ -693,13 +694,15 @@ ROUND_DOWN_TO(uint64_t value, uint32_t alignment)
 static inline int
 align(int value, int alignment)
 {
-   return (value + alignment - 1) & ~(alignment - 1);
+   assert(IS_POT(alignment));
+   return ALIGN_POT(value, alignment);
 }
 
 static inline uint64_t
 align64(uint64_t value, uint64_t alignment)
 {
-   return (value + alignment - 1) & ~(alignment - 1);
+   assert(IS_POT(alignment));
+   return ALIGN_POT(value, (uint64_t)alignment);
 }
 
 /**

commit 6c51b8d4f6bae9ed003dcaf57a0df977f4283ee3

util: Remove redundant type cast in function align64
Fixes: 321eccf72a4 ("util: migrate alignment functions and macros to use ALIGN_POT")

Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23421>

src/util/u_math.h
@@ -702,7 +702,7 @@ static inline uint64_t
 align64(uint64_t value, uint64_t alignment)
 {
    assert(IS_POT(alignment));
-   return ALIGN_POT(value, (uint64_t)alignment);
+   return ALIGN_POT(value, alignment);
 }
 
 /**