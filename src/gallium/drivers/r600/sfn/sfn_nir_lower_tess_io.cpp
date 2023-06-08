joshuaswarren/mesa-1

commit f5a83b30a4b80a41966f18484a01617fb2da24e4

r600/sfn: Make sure all components are usable when lowering TF inputs
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18983>

src/gallium/drivers/r600/sfn/sfn_nir_lower_tess_io.cpp
@@ -412,8 +412,16 @@ r600_lower_tess_io_impl(nir_builder *b, nir_instr *instr, enum pipe_prim_type pr
       nir_ssa_dest_init(&tf->instr, &tf->dest,
                         tf->num_components, 32, NULL);
       nir_builder_instr_insert(b, &tf->instr);
-
-      nir_ssa_def_rewrite_uses(&op->dest.ssa, &tf->dest.ssa);
+      if (ncomps < 4) {
+         auto undef = nir_ssa_undef(b, 1, 32);
+         nir_ssa_def *srcs[4] = {undef, undef, undef, undef};
+         for (unsigned i = 0; i < ncomps; ++i)
+            srcs[i] = nir_channel(b, &tf->dest.ssa, i);
+         auto help = nir_vec(b, srcs, 4);
+         nir_ssa_def_rewrite_uses(&op->dest.ssa, help);
+      } else {
+         nir_ssa_def_rewrite_uses(&op->dest.ssa, &tf->dest.ssa);
+      }
       nir_instr_remove(instr);
       return true;
    }

commit 0ec40d18fbf1ac391ac7387e7f18c42754dd24cb

r600/sfn: Don't rewrite TESS_EVAL inner tess level outputs
Fixes: f5a83b30a (r600/sfn: Make sure all components are usable when lowering TF inputs)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23518>

src/gallium/drivers/r600/sfn/sfn_nir_lower_tess_io.cpp
@@ -456,7 +456,7 @@ r600_lower_tess_io_impl(nir_builder *b, nir_instr *instr, enum mesa_prim prim_ty
       tf->src[0] = nir_src_for_ssa(addr_outer);
       nir_ssa_dest_init(&tf->instr, &tf->dest, tf->num_components, 32);
       nir_builder_instr_insert(b, &tf->instr);
-      if (ncomps < 4) {
+      if (ncomps < 4 && b->shader->info.stage != MESA_SHADER_TESS_EVAL) {
          auto undef = nir_ssa_undef(b, 1, 32);
          nir_ssa_def *srcs[4] = {undef, undef, undef, undef};
          for (unsigned i = 0; i < ncomps; ++i)