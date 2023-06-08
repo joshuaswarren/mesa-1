joshuaswarren/mesa-1

commit 6d07e138308481e9164828379b7a167c46103594

rusticl/event: wrong but non crashing impl of clWaitForEvents
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15439>

src/gallium/frontends/rusticl/api/event.rs
@@ -7,6 +7,7 @@ use crate::core::queue::*;
 
 use self::rusticl_opencl_gen::*;
 
+use std::collections::HashSet;
 use std::ptr;
 use std::sync::Arc;
 
@@ -38,6 +39,39 @@ impl CLInfo<cl_event_info> for cl_event {
 pub fn create_user_event(context: cl_context) -> CLResult<cl_event> {
     let c = context.get_arc()?;
     Ok(cl_event::from_arc(Event::new_user(c)))
+}
+
+pub fn wait_for_events(num_events: cl_uint, event_list: *const cl_event) -> CLResult<()> {
+    let evs = cl_event::get_arc_vec_from_arr(event_list, num_events)?;
+
+    // CL_INVALID_VALUE if num_events is zero or event_list is NULL.
+    if evs.is_empty() {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    // CL_INVALID_CONTEXT if events specified in event_list do not belong to the same context.
+    let contexts: HashSet<_> = evs.iter().map(|e| &e.context).collect();
+    if contexts.len() != 1 {
+        return Err(CL_INVALID_CONTEXT);
+    }
+
+    // TODO better impl
+    let mut err = false;
+    for e in evs {
+        if let Some(q) = &e.queue {
+            q.flush(true)?;
+        }
+
+        err |= e.status() < 0;
+    }
+
+    // CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST if the execution status of any of the events
+    // in event_list is a negative integer value.
+    if err {
+        return Err(CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST);
+    }
+
+    Ok(())
 }
 
 pub fn create_and_queue(

src/gallium/frontends/rusticl/api/icd.rs
@@ -824,9 +824,8 @@ extern "C" fn cl_get_kernel_work_group_info(
     ))
 }
 
-extern "C" fn cl_wait_for_events(_num_events: cl_uint, _event_list: *const cl_event) -> cl_int {
-    println!("cl_wait_for_events not implemented");
-    CL_OUT_OF_HOST_MEMORY
+extern "C" fn cl_wait_for_events(num_events: cl_uint, event_list: *const cl_event) -> cl_int {
+    match_err!(wait_for_events(num_events, event_list))
 }
 
 extern "C" fn cl_get_event_info(

commit fe4bb1bc9dcb5f479a4e718a299a5f036b508584

rusticl/event: flush queues from dependencies
We have to flush all event dependencies inside clWaitForEvents and
clFinish implicitly otherwise applications might wait forever.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9052
Fixes: 6d07e138308 ("rusticl/event: wrong but non crashing impl of clWaitForEvents")
Cc: mesa-stable
Signed-off-by: Karol Herbst <git@karolherbst.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23110>

src/gallium/frontends/rusticl/api/event.rs
@@ -74,13 +74,14 @@ pub fn wait_for_events(num_events: cl_uint, event_list: *const cl_event) -> CLRe
         return Err(CL_INVALID_CONTEXT);
     }
 
-    // TODO better impl
+    // find all queues we have to flush
+    for q in Event::deep_unflushed_queues(&evs) {
+        q.flush(false)?;
+    }
+
+    // now wait on all events and check if we got any errors
     let mut err = false;
     for e in evs {
-        if let Some(q) = &e.queue {
-            q.flush(false)?;
-        }
-
         err |= e.wait() < 0;
     }
 

src/gallium/frontends/rusticl/api/queue.rs
@@ -195,7 +195,13 @@ pub fn flush_queue(command_queue: cl_command_queue) -> CLResult<()> {
 
 pub fn finish_queue(command_queue: cl_command_queue) -> CLResult<()> {
     // CL_INVALID_COMMAND_QUEUE if command_queue is not a valid host command-queue.
-    command_queue.get_ref()?.flush(true)
+    let q = command_queue.get_ref()?;
+
+    for q in q.dependencies_for_pending_events() {
+        q.flush(false)?;
+    }
+
+    q.flush(true)
 }
 
 pub fn release_command_queue(command_queue: cl_command_queue) -> CLResult<()> {

src/gallium/frontends/rusticl/core/event.rs
@@ -9,6 +9,7 @@ use mesa_rust::pipe::fence::*;
 use mesa_rust_util::static_assert;
 use rusticl_opencl_gen::*;
 
+use std::collections::HashSet;
 use std::os::raw::c_void;
 use std::slice;
 use std::sync::Arc;
@@ -184,6 +185,39 @@ impl Event {
         } else {
             status
         }
+    }
+
+    fn deep_unflushed_deps_impl<'a>(&'a self, result: &mut HashSet<&'a Event>) {
+        if self.status() <= CL_SUBMITTED as i32 {
+            return;
+        }
+
+        // only scan dependencies if it's a new one
+        if result.insert(self) {
+            for e in &self.deps {
+                e.deep_unflushed_deps_impl(result);
+            }
+        }
+    }
+
+    /// does a deep search and returns a list of all dependencies including `events` which haven't
+    /// been flushed out yet
+    pub fn deep_unflushed_deps(events: &[Arc<Event>]) -> HashSet<&Event> {
+        let mut result = HashSet::new();
+
+        for e in events {
+            e.deep_unflushed_deps_impl(&mut result);
+        }
+
+        result
+    }
+
+    /// does a deep search and returns a list of all queues which haven't been flushed yet
+    pub fn deep_unflushed_queues(events: &[Arc<Event>]) -> HashSet<Arc<Queue>> {
+        Event::deep_unflushed_deps(events)
+            .iter()
+            .filter_map(|e| e.queue.clone())
+            .collect()
     }
 }
 

src/gallium/frontends/rusticl/core/queue.rs
@@ -7,6 +7,7 @@ use crate::impl_cl_type_trait;
 use mesa_rust_util::properties::*;
 use rusticl_opencl_gen::*;
 
+use std::collections::HashSet;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -92,6 +93,14 @@ impl Queue {
             }
         }
         Ok(())
+    }
+
+    pub fn dependencies_for_pending_events(&self) -> HashSet<Arc<Queue>> {
+        let p = self.pending.lock().unwrap();
+
+        let mut queues = Event::deep_unflushed_queues(&p);
+        queues.remove(self);
+        queues
     }
 }
 