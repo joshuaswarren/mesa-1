joshuaswarren/mesa-1

commit 4cdf9ea1940bf65ed726dd98e000500ce214cd8c

ac/llvm: clean up visit_load_local_invocation_index and visit_load_subgroup_id
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23216>

src/amd/llvm/ac_nir_to_llvm.c
@@ -2700,46 +2700,30 @@ static void emit_demote(struct ac_nir_context *ctx, const nir_intrinsic_instr *i
    }
 }
 
-static LLVMValueRef visit_load_local_invocation_index(struct ac_nir_context *ctx)
-{
-   if (ctx->args->tcs_wave_id.used) {
-      return ac_build_imad(&ctx->ac,
-                           ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->tcs_wave_id), 0, 3),
-                           LLVMConstInt(ctx->ac.i32, ctx->ac.wave_size, 0),
-                           ac_get_thread_id(&ctx->ac));
-   } else if (ctx->abi->vs_rel_patch_id) {
-      return ctx->abi->vs_rel_patch_id;
-   } else if (ctx->args->merged_wave_info.used) {
-      /* Thread ID in threadgroup in merged ESGS. */
-      LLVMValueRef wave_id = ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->merged_wave_info), 24, 4);
-      LLVMValueRef wave_size = LLVMConstInt(ctx->ac.i32, ctx->ac.wave_size, false);
-      LLVMValueRef threads_before = LLVMBuildMul(ctx->ac.builder, wave_id, wave_size, "");
-      return LLVMBuildAdd(ctx->ac.builder, threads_before, ac_get_thread_id(&ctx->ac), "");
-   }
-
-   LLVMValueRef result;
-   LLVMValueRef thread_id = ac_get_thread_id(&ctx->ac);
-   result = LLVMBuildAnd(ctx->ac.builder, ac_get_arg(&ctx->ac, ctx->args->tg_size),
-                         LLVMConstInt(ctx->ac.i32, 0xfc0, false), "");
-
-   if (ctx->ac.wave_size == 32)
-      result = LLVMBuildLShr(ctx->ac.builder, result, LLVMConstInt(ctx->ac.i32, 1, false), "");
-
-   return LLVMBuildAdd(ctx->ac.builder, result, thread_id, "");
-}
-
 static LLVMValueRef visit_load_subgroup_id(struct ac_nir_context *ctx)
 {
    if (ctx->stage == MESA_SHADER_COMPUTE) {
-      LLVMValueRef result;
-      result = LLVMBuildAnd(ctx->ac.builder, ac_get_arg(&ctx->ac, ctx->args->tg_size),
-                            LLVMConstInt(ctx->ac.i32, 0xfc0, false), "");
-      return LLVMBuildLShr(ctx->ac.builder, result, LLVMConstInt(ctx->ac.i32, 6, false), "");
+      if (ctx->ac.gfx_level >= GFX10_3)
+         return ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->tg_size), 20, 5);
+      else
+         return ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->tg_size), 6, 6);
+   } else if (ctx->args->tcs_wave_id.used) {
+      return ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->tcs_wave_id), 0, 3);
    } else if (ctx->args->merged_wave_info.used) {
       return ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->merged_wave_info), 24, 4);
    } else {
-      return LLVMConstInt(ctx->ac.i32, 0, false);
+      return ctx->ac.i32_0;
    }
+}
+
+static LLVMValueRef visit_load_local_invocation_index(struct ac_nir_context *ctx)
+{
+   if (ctx->abi->vs_rel_patch_id)
+      return ctx->abi->vs_rel_patch_id;
+
+   return ac_build_imad(&ctx->ac, visit_load_subgroup_id(ctx),
+                        LLVMConstInt(ctx->ac.i32, ctx->ac.wave_size, 0),
+                        ac_get_thread_id(&ctx->ac));
 }
 
 static LLVMValueRef visit_load_num_subgroups(struct ac_nir_context *ctx)

commit 6df660bac97070544b9c78c6b7239090350e0425

ac/llvm: replace MESA_SHADER_COMPUTE checks with gl_shader_stage_is_compute
This will be required for OpenCL subgroup support on radeonsi, but also
fixes some regressions today as radeonsi started to use the subgroup id
for invocation_index calculation.

Fixes: 4cdf9ea1940 ("ac/llvm: clean up visit_load_local_invocation_index and visit_load_subgroup_id")
Signed-off-by: Karol Herbst <git@karolherbst.de>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23551>

src/amd/llvm/ac_nir_to_llvm.c
@@ -1515,7 +1515,7 @@ static LLVMValueRef build_tex_intrinsic(struct ac_nir_context *ctx, const nir_te
       break;
    case nir_texop_tex:
       if (ctx->stage != MESA_SHADER_FRAGMENT &&
-          (ctx->stage != MESA_SHADER_COMPUTE ||
+          (!gl_shader_stage_is_compute(ctx->stage) ||
            ctx->info->cs.derivative_group == DERIVATIVE_GROUP_NONE)) {
          assert(!args->lod);
          args->level_zero = true;
@@ -1551,7 +1551,7 @@ static LLVMValueRef build_tex_intrinsic(struct ac_nir_context *ctx, const nir_te
 
    args->attributes = AC_ATTR_INVARIANT_LOAD;
    bool cs_derivs =
-      ctx->stage == MESA_SHADER_COMPUTE && ctx->info->cs.derivative_group != DERIVATIVE_GROUP_NONE;
+      gl_shader_stage_is_compute(ctx->stage) && ctx->info->cs.derivative_group != DERIVATIVE_GROUP_NONE;
    if (ctx->stage == MESA_SHADER_FRAGMENT || cs_derivs) {
       /* Prevent texture instructions with implicit derivatives from being
        * sinked into branches. */
@@ -2613,7 +2613,7 @@ static void emit_demote(struct ac_nir_context *ctx, const nir_intrinsic_instr *i
 
 static LLVMValueRef visit_load_subgroup_id(struct ac_nir_context *ctx)
 {
-   if (ctx->stage == MESA_SHADER_COMPUTE) {
+   if (gl_shader_stage_is_compute(ctx->stage)) {
       if (ctx->ac.gfx_level >= GFX10_3)
          return ac_unpack_param(&ctx->ac, ac_get_arg(&ctx->ac, ctx->args->tg_size), 20, 5);
       else
@@ -2639,7 +2639,7 @@ static LLVMValueRef visit_load_local_invocation_index(struct ac_nir_context *ctx
 
 static LLVMValueRef visit_load_num_subgroups(struct ac_nir_context *ctx)
 {
-   if (ctx->stage == MESA_SHADER_COMPUTE) {
+   if (gl_shader_stage_is_compute(ctx->stage)) {
       return LLVMBuildAnd(ctx->ac.builder, ac_get_arg(&ctx->ac, ctx->args->tg_size),
                           LLVMConstInt(ctx->ac.i32, 0x3f, false), "");
    } else if (ctx->args->merged_wave_info.used) {