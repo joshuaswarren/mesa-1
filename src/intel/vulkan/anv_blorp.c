openharmony/third_party_mesa3d

commit 326c9c35c5f2711a3a3fdffcfe503aa5d3fd36d6

anv: narrow flushing of the render target to buffer writes
In commit b5f5dfd529f8ef ("anv/query: flush render target before
copying results") we tracked all the render target writes to apply a
flushes in the vkCopyQueryResults(). But we can narrow this down to
only when we write a buffer (which is the only input of
vkCopyQueryResults).

v2: Drop newer render target write flags introduce by 53c5381b640dde
    ("anv: Implement VK_EXT_conditional_rendering for gen 7.5+")

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net> (v1)

src/intel/vulkan/anv_blorp.c
@@ -475,6 +475,8 @@ void anv_CmdCopyImageToBuffer(
 
    copy_buffer_to_image(cmd_buffer, dst_buffer, src_image, srcImageLayout,
                         regionCount, pRegions, false);
+
+   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_BUFFER_WRITES;
 }
 
 static bool
@@ -682,6 +684,8 @@ void anv_CmdCopyBuffer(
    }
 
    blorp_batch_finish(&batch);
+
+   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_BUFFER_WRITES;
 }
 
 void anv_CmdUpdateBuffer(
@@ -737,6 +741,8 @@ void anv_CmdUpdateBuffer(
    }
 
    blorp_batch_finish(&batch);
+
+   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_BUFFER_WRITES;
 }
 
 void anv_CmdFillBuffer(
@@ -824,6 +830,8 @@ void anv_CmdFillBuffer(
    }
 
    blorp_batch_finish(&batch);
+
+   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_BUFFER_WRITES;
 }
 
 void anv_CmdClearColorImage(

src/intel/vulkan/anv_private.h
@@ -1790,11 +1790,12 @@ enum anv_pipe_bits {
    ANV_PIPE_NEEDS_CS_STALL_BIT               = (1 << 21),
 
    /* This bit does not exist directly in PIPE_CONTROL. It means that render
-    * target operations are ongoing. Some operations like copies on the
-    * command streamer might need to be aware of this to trigger the
-    * appropriate stall before they can proceed with the copy.
+    * target operations related to transfer commands with VkBuffer as
+    * destination are ongoing. Some operations like copies on the command
+    * streamer might need to be aware of this to trigger the appropriate stall
+    * before they can proceed with the copy.
     */
-   ANV_PIPE_RENDER_TARGET_WRITES              = (1 << 22),
+   ANV_PIPE_RENDER_TARGET_BUFFER_WRITES      = (1 << 22),
 };
 
 #define ANV_PIPE_FLUSH_BITS ( \

src/intel/vulkan/genX_cmd_buffer.c
@@ -1811,7 +1811,7 @@ genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
        * saying that render target writes are ongoing.
        */
       if (bits & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
-         bits &= ~(ANV_PIPE_RENDER_TARGET_WRITES);
+         bits &= ~(ANV_PIPE_RENDER_TARGET_BUFFER_WRITES);
 
       bits &= ~(ANV_PIPE_FLUSH_BITS | ANV_PIPE_CS_STALL_BIT);
    }
@@ -2828,8 +2828,6 @@ void genX(CmdDraw)(
       prim.StartInstanceLocation    = firstInstance;
       prim.BaseVertexLocation       = 0;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 void genX(CmdDrawIndexed)(
@@ -2873,8 +2871,6 @@ void genX(CmdDrawIndexed)(
       prim.StartInstanceLocation    = firstInstance;
       prim.BaseVertexLocation       = vertexOffset;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 /* Auto-Draw / Indirect Registers */
@@ -3012,8 +3008,6 @@ void genX(CmdDrawIndirect)(
 
       offset += stride;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 void genX(CmdDrawIndexedIndirect)(
@@ -3057,8 +3051,6 @@ void genX(CmdDrawIndexedIndirect)(
 
       offset += stride;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 #define TMP_DRAW_COUNT_REG MI_ALU_REG14
@@ -3218,8 +3210,6 @@ void genX(CmdDrawIndirectCountKHR)(
 
       offset += stride;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 void genX(CmdDrawIndexedIndirectCountKHR)(
@@ -3280,8 +3270,6 @@ void genX(CmdDrawIndexedIndirectCountKHR)(
 
       offset += stride;
    }
-
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }
 
 static VkResult

src/intel/vulkan/genX_gpu_memcpy.c
@@ -299,5 +299,4 @@ genX(cmd_buffer_so_memcpy)(struct anv_cmd_buffer *cmd_buffer,
    }
 
    cmd_buffer->state.gfx.dirty |= ANV_CMD_DIRTY_PIPELINE;
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }

src/intel/vulkan/genX_query.c
@@ -733,7 +733,7 @@ void genX(CmdCopyQueryPoolResults)(
     * to ensure proper ordering of the commands from the 3d pipe and the
     * command streamer.
     */
-   if (cmd_buffer->state.pending_pipe_bits & ANV_PIPE_RENDER_TARGET_WRITES) {
+   if (cmd_buffer->state.pending_pipe_bits & ANV_PIPE_RENDER_TARGET_BUFFER_WRITES) {
       cmd_buffer->state.pending_pipe_bits |=
          ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT;
    }

src/intel/vulkan/genX_blorp_exec.c
@@ -268,5 +268,4 @@ genX(blorp_exec)(struct blorp_batch *batch,
    cmd_buffer->state.gfx.vb_dirty = ~0;
    cmd_buffer->state.gfx.dirty = ~0;
    cmd_buffer->state.push_constants_dirty = ~0;
-   cmd_buffer->state.pending_pipe_bits |= ANV_PIPE_RENDER_TARGET_WRITES;
 }

joshuaswarren/mesa-1

commit f22ffe7491291c4d311a0d9e51507a83115c64ef

anv: split ANV_PIPE_RENDER_TARGET_BUFFER_WRITES for finer grained flushing
split ANV_PIPE_RENDER_TARGET_BUFFER_WRITES into separate CS_STALL,
RT_FLUSH & TILE_FLUSH flags in order to have finer control over cache
coherency.

Tigerlake CS has it's own cache fetching directly from the memory controller,
so we need to do a tile flush to ensure the query data is visible.

This fixes test_resolve_non_issued_query_data in vkd3d on TGL.

Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Fixes: 326c9c35c5f2 ("anv: narrow flushing of the render target to buffer writes")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23500>

src/intel/vulkan/anv_blorp.c
@@ -535,6 +535,16 @@ void anv_CmdCopyBufferToImage2(
    anv_blorp_batch_finish(&batch);
 }
 
+static void
+anv_add_buffer_write_pending_bits(struct anv_cmd_buffer *cmd_buffer,
+                                  const char *reason)
+{
+   const struct intel_device_info *devinfo = cmd_buffer->device->info;
+
+   cmd_buffer->state.pending_query_bits |=
+      ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(devinfo);
+}
+
 void anv_CmdCopyImageToBuffer2(
     VkCommandBuffer                             commandBuffer,
     const VkCopyImageToBufferInfo2*             pCopyImageToBufferInfo)
@@ -554,9 +564,7 @@ void anv_CmdCopyImageToBuffer2(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after copy image to buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after copy image to buffer");
 }
 
 static bool
@@ -781,9 +789,7 @@ void anv_CmdCopyBuffer2(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after copy buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after copy buffer");
 }
 
 
@@ -845,9 +851,7 @@ void anv_CmdUpdateBuffer(
 
    anv_blorp_batch_finish(&batch);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "update buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "update buffer");
 }
 
 void
@@ -955,9 +959,8 @@ void anv_CmdFillBuffer(
                             anv_address_add(dst_buffer->address, dstOffset),
                             fillSize, data);
 
-   anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_RENDER_TARGET_BUFFER_WRITES,
-                             "after fill buffer");
+   anv_add_buffer_write_pending_bits(cmd_buffer, "after fill buffer");
+
 }
 
 void anv_CmdClearColorImage(

src/intel/vulkan/anv_genX.h
@@ -103,7 +103,8 @@ enum anv_pipe_bits
 genX(emit_apply_pipe_flushes)(struct anv_batch *batch,
                               struct anv_device *device,
                               uint32_t current_pipeline,
-                              enum anv_pipe_bits bits);
+                              enum anv_pipe_bits bits,
+                              enum anv_query_bits *query_bits);
 
 void genX(emit_so_memcpy_init)(struct anv_memcpy_state *state,
                                struct anv_device *device,

src/intel/vulkan/anv_private.h
@@ -2188,31 +2188,53 @@ enum anv_pipe_bits {
     */
    ANV_PIPE_NEEDS_END_OF_PIPE_SYNC_BIT       = (1 << 22),
 
-   /* This bit does not exist directly in PIPE_CONTROL. It means that render
-    * target operations related to transfer commands with VkBuffer as
-    * destination are ongoing. Some operations like copies on the command
-    * streamer might need to be aware of this to trigger the appropriate stall
-    * before they can proceed with the copy.
-    */
-   ANV_PIPE_RENDER_TARGET_BUFFER_WRITES      = (1 << 23),
-
    /* This bit does not exist directly in PIPE_CONTROL. It means that Gfx12
     * AUX-TT data has changed and we need to invalidate AUX-TT data.  This is
     * done by writing the AUX-TT register.
     */
-   ANV_PIPE_AUX_TABLE_INVALIDATE_BIT         = (1 << 24),
+   ANV_PIPE_AUX_TABLE_INVALIDATE_BIT         = (1 << 23),
 
    /* This bit does not exist directly in PIPE_CONTROL. It means that a
     * PIPE_CONTROL with a post-sync operation will follow. This is used to
     * implement a workaround for Gfx9.
     */
-   ANV_PIPE_POST_SYNC_BIT                    = (1 << 25),
-
-   /* This bit does not exist directly in PIPE_CONTROL. It means that render
-    * target operations related to clearing of queries are ongoing.
-    */
-   ANV_PIPE_QUERY_CLEARS_BIT                 = (1 << 26),
+   ANV_PIPE_POST_SYNC_BIT                    = (1 << 24),
 };
+
+/* These bits track the state of buffer writes for queries. They get cleared
+ * based on PIPE_CONTROL emissions.
+ */
+enum anv_query_bits {
+   ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH      = (1 << 0),
+
+   ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH    = (1 << 1),
+
+   ANV_QUERY_RENDER_TARGET_WRITES_CS_STALL      = (1 << 2),
+};
+
+/* Things we need to flush before accessing query data using the command
+ * streamer.
+ *
+ * Prior to DG2 experiments show that the command streamer is not coherent
+ * with the tile cache so we need to flush it to make any data visible to CS.
+ *
+ * Otherwise we want to flush the RT cache which is where blorp writes, either
+ * for clearing the query buffer or for clearing the destination buffer in
+ * vkCopyQueryPoolResults().
+ */
+#define ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(devinfo) \
+   (((devinfo->verx10 >= 120 && \
+      devinfo->verx10 < 125) ? ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH : 0) | \
+   ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH | \
+   ANV_QUERY_RENDER_TARGET_WRITES_CS_STALL)
+
+#define ANV_PIPE_QUERY_BITS(pending_query_bits) ( \
+   ((pending_query_bits & ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH) ?   \
+    ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT : 0) | \
+   ((pending_query_bits & ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH) ?   \
+    ANV_PIPE_TILE_CACHE_FLUSH_BIT : 0) | \
+   ((pending_query_bits & ANV_QUERY_RENDER_TARGET_WRITES_CS_STALL) ?   \
+    ANV_PIPE_CS_STALL_BIT : 0))
 
 #define ANV_PIPE_FLUSH_BITS ( \
    ANV_PIPE_DEPTH_CACHE_FLUSH_BIT | \
@@ -2253,20 +2275,6 @@ enum anv_pipe_bits {
  */
 #define ANV_PIPE_GPGPU_BITS ( \
    (GFX_VERx10 >= 125 ? ANV_PIPE_UNTYPED_DATAPORT_CACHE_FLUSH_BIT : 0))
-
-/* Things we need to flush before accessing query data using the command
- * streamer.
- *
- * Prior to DG2 experiments show that the command streamer is not coherent
- * with the tile cache so we need to flush it to make any data visible to CS.
- *
- * Otherwise we want to flush the RT cache which is where blorp writes, either
- * for clearing the query buffer or for clearing the destination buffer in
- * vkCopyQueryPoolResults().
- */
-#define ANV_PIPE_QUERY_FLUSH_BITS ( \
-   (GFX_VERx10 < 125 ? ANV_PIPE_TILE_CACHE_FLUSH_BIT : 0) | \
-   ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
 
 enum intel_ds_stall_flag
 anv_pipe_flush_bit_to_ds_stall_flag(enum anv_pipe_bits bits);
@@ -2792,6 +2800,14 @@ struct anv_cmd_state {
    struct anv_cmd_ray_tracing_state             rt;
 
    enum anv_pipe_bits                           pending_pipe_bits;
+
+   /**
+    * Tracks operations susceptible to interfere with queries, either blorp
+    * clears of the query buffer or the destination buffer of
+    * vkCmdCopyQueryResults, we need those operations to have completed before
+    * we do the work of vkCmdCopyQueryResults.
+    */
+   enum anv_query_bits                          pending_query_bits;
    VkShaderStageFlags                           descriptors_dirty;
    VkShaderStageFlags                           push_descriptors_dirty;
    VkShaderStageFlags                           push_constants_dirty;

src/intel/vulkan/genX_cmd_buffer.c
@@ -1401,7 +1401,8 @@ ALWAYS_INLINE enum anv_pipe_bits
 genX(emit_apply_pipe_flushes)(struct anv_batch *batch,
                               struct anv_device *device,
                               uint32_t current_pipeline,
-                              enum anv_pipe_bits bits)
+                              enum anv_pipe_bits bits,
+                              enum anv_query_bits *query_bits)
 {
 #if GFX_VER >= 12
    /* From the TGL PRM, Volume 2a, "PIPE_CONTROL":
@@ -1625,19 +1626,21 @@ genX(emit_apply_pipe_flushes)(struct anv_batch *batch,
          anv_debug_dump_pc(pipe);
       }
 
-      /* If a render target flush was emitted, then we can toggle off the bit
-       * saying that render target writes are ongoing.
+      /* Based on emitted flushes, clear the associated buffer write tracking
+       * bits of buffer writes.
        */
-      if (bits & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
-         bits &= ~ANV_PIPE_RENDER_TARGET_BUFFER_WRITES;
+      if (query_bits != NULL) {
+         if (bits & ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT)
+            *query_bits &= ~ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH;
 
-      /* If the conditions for flushing the query clears are met, we can
-       * toggle the bit off.
-       */
-      if ((bits & ANV_PIPE_QUERY_FLUSH_BITS) == ANV_PIPE_QUERY_FLUSH_BITS &&
-          (bits & (ANV_PIPE_END_OF_PIPE_SYNC_BIT |
-                   ANV_PIPE_CS_STALL_BIT))) {
-         bits &= ~ANV_PIPE_QUERY_CLEARS_BIT;
+         if (bits & ANV_PIPE_TILE_CACHE_FLUSH_BIT)
+            *query_bits &= ~ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH;
+
+         /* Once RT/TILE have been flushed, we can consider the CS_STALL flush */
+         if ((*query_bits & (ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH |
+                             ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH)) == 0 &&
+             (bits & (ANV_PIPE_END_OF_PIPE_SYNC_BIT | ANV_PIPE_CS_STALL_BIT)))
+            *query_bits &= ~ANV_QUERY_RENDER_TARGET_WRITES_CS_STALL;
       }
 
       bits &= ~(ANV_PIPE_FLUSH_BITS | ANV_PIPE_STALL_BITS |
@@ -1784,7 +1787,8 @@ genX(cmd_buffer_apply_pipe_flushes)(struct anv_cmd_buffer *cmd_buffer)
       genX(emit_apply_pipe_flushes)(&cmd_buffer->batch,
                                     cmd_buffer->device,
                                     cmd_buffer->state.current_pipeline,
-                                    bits);
+                                    bits,
+                                    &cmd_buffer->state.pending_query_bits);
 
 #if INTEL_NEEDS_WA_1508744258
    if (rhwo_opt_change) {
@@ -3800,10 +3804,9 @@ genX(EndCommandBuffer)(
    /* Flush query clears using blorp so that secondary query writes do not
     * race with the clear.
     */
-   if (cmd_buffer->state.pending_pipe_bits & ANV_PIPE_QUERY_CLEARS_BIT) {
+   if (cmd_buffer->state.pending_query_bits) {
       anv_add_pending_pipe_bits(cmd_buffer,
-                                ANV_PIPE_QUERY_FLUSH_BITS |
-                                ANV_PIPE_NEEDS_END_OF_PIPE_SYNC_BIT,
+                                ANV_PIPE_QUERY_BITS(cmd_buffer->state.pending_query_bits),
                                 "query clear flush prior command buffer end");
    }
 
@@ -3885,10 +3888,9 @@ genX(CmdExecuteCommands)(
    /* Flush query clears using blorp so that secondary query writes do not
     * race with the clear.
     */
-   if (primary->state.pending_pipe_bits & ANV_PIPE_QUERY_CLEARS_BIT) {
+   if (primary->state.pending_query_bits) {
       anv_add_pending_pipe_bits(primary,
-                                ANV_PIPE_QUERY_FLUSH_BITS |
-                                ANV_PIPE_NEEDS_END_OF_PIPE_SYNC_BIT,
+                                ANV_PIPE_QUERY_BITS(primary->state.pending_query_bits),
                                 "query clear flush prior to secondary buffer");
    }
 
@@ -6628,10 +6630,9 @@ genX(flush_pipeline_select)(struct anv_cmd_buffer *cmd_buffer,
     * copy/write. So we need to flush it before going to GPGPU mode.
     */
    if (cmd_buffer->state.current_pipeline == _3D &&
-       (cmd_buffer->state.pending_pipe_bits & ANV_PIPE_QUERY_CLEARS_BIT)) {
+       cmd_buffer->state.pending_query_bits) {
       anv_add_pending_pipe_bits(cmd_buffer,
-                                ANV_PIPE_QUERY_FLUSH_BITS |
-                                ANV_PIPE_END_OF_PIPE_SYNC_BIT,
+                                ANV_PIPE_QUERY_BITS(cmd_buffer->state.pending_query_bits),
                                 "query clear flush prior to GPGPU");
    }
 #endif

src/intel/vulkan/genX_cmd_draw_generated_indirect.h
@@ -719,7 +719,8 @@ genX(cmd_buffer_flush_generated_draws)(struct anv_cmd_buffer *cmd_buffer)
                                  ANV_PIPE_VF_CACHE_INVALIDATE_BIT |
 #endif
                                  ANV_PIPE_DATA_CACHE_FLUSH_BIT |
-                                 ANV_PIPE_CS_STALL_BIT);
+                                 ANV_PIPE_CS_STALL_BIT,
+                                 NULL /* query_bits */);
 
 #if GFX_VER >= 12
    anv_batch_emit(batch, GENX(MI_ARB_CHECK), arb) {

src/intel/vulkan/genX_gpu_memcpy.c
@@ -269,7 +269,8 @@ void
 genX(emit_so_memcpy_fini)(struct anv_memcpy_state *state)
 {
    genX(emit_apply_pipe_flushes)(state->batch, state->device, _3D,
-                                 ANV_PIPE_END_OF_PIPE_SYNC_BIT);
+                                 ANV_PIPE_END_OF_PIPE_SYNC_BIT,
+                                 NULL);
 }
 
 void
@@ -295,7 +296,8 @@ genX(emit_so_memcpy)(struct anv_memcpy_state *state,
                                                   src, size)) {
       genX(emit_apply_pipe_flushes)(state->batch, state->device, _3D,
                                     ANV_PIPE_CS_STALL_BIT |
-                                    ANV_PIPE_VF_CACHE_INVALIDATE_BIT);
+                                    ANV_PIPE_VF_CACHE_INVALIDATE_BIT,
+                                    NULL);
       memset(&state->vb_dirty, 0, sizeof(state->vb_dirty));
    }
 

src/intel/vulkan/genX_query.c
@@ -795,9 +795,8 @@ void genX(CmdResetQueryPool)(
                                queryCount * pool->stride,
                                0);
 
-      anv_add_pending_pipe_bits(cmd_buffer,
-                                ANV_PIPE_QUERY_CLEARS_BIT,
-                                "vkCmdResetQueryPool of timestamps");
+      cmd_buffer->state.pending_query_bits =
+         ANV_QUERY_RENDER_TARGET_WRITES_PENDING_BITS(cmd_buffer->device->info);
 
       trace_intel_end_query_clear_blorp(&cmd_buffer->trace);
       return;
@@ -991,12 +990,12 @@ emit_query_clear_flush(struct anv_cmd_buffer *cmd_buffer,
                        struct anv_query_pool *pool,
                        const char *reason)
 {
-   if ((cmd_buffer->state.pending_pipe_bits & ANV_PIPE_QUERY_CLEARS_BIT) == 0)
+   if (cmd_buffer->state.pending_query_bits == 0)
       return;
 
    anv_add_pending_pipe_bits(cmd_buffer,
-                             ANV_PIPE_QUERY_FLUSH_BITS |
-                             ANV_PIPE_END_OF_PIPE_SYNC_BIT,
+                             ANV_PIPE_QUERY_BITS(
+                                cmd_buffer->state.pending_query_bits),
                              reason);
    genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
 }
@@ -1516,45 +1515,45 @@ void genX(CmdCopyQueryPoolResults)(
    mi_builder_init(&b, cmd_buffer->device->info, &cmd_buffer->batch);
    struct mi_value result;
 
+   enum anv_pipe_bits needed_flushes = 0;
+
    /* If render target writes are ongoing, request a render target cache flush
     * to ensure proper ordering of the commands from the 3d pipe and the
     * command streamer.
     */
-   const bool need_flushes =
-      (cmd_buffer->state.pending_pipe_bits &
-       (ANV_PIPE_RENDER_TARGET_BUFFER_WRITES |
-        ANV_PIPE_QUERY_CLEARS_BIT));
+   if (cmd_buffer->state.pending_query_bits &
+       ANV_QUERY_RENDER_TARGET_WRITES_RT_FLUSH)
+      needed_flushes |= ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT;
 
-   if (need_flushes) {
+   if (cmd_buffer->state.pending_query_bits &
+       ANV_QUERY_RENDER_TARGET_WRITES_TILE_FLUSH)
+      needed_flushes |= ANV_PIPE_TILE_CACHE_FLUSH_BIT;
+
+   if (cmd_buffer->state.pending_query_bits &
+       ANV_QUERY_RENDER_TARGET_WRITES_CS_STALL)
+      needed_flushes |= ANV_PIPE_CS_STALL_BIT;
+
+   /* Occlusion & timestamp queries are written using a PIPE_CONTROL and
+    * because we're about to copy values from MI commands, we need to stall
+    * the command streamer to make sure the PIPE_CONTROL values have
+    * landed, otherwise we could see inconsistent values & availability.
+    *
+    *  From the vulkan spec:
+    *
+    *     "vkCmdCopyQueryPoolResults is guaranteed to see the effect of
+    *     previous uses of vkCmdResetQueryPool in the same queue, without any
+    *     additional synchronization."
+    */
+   if (pool->type == VK_QUERY_TYPE_OCCLUSION ||
+       pool->type == VK_QUERY_TYPE_TIMESTAMP)
+      needed_flushes |= ANV_PIPE_CS_STALL_BIT;
+
+   if (needed_flushes) {
       anv_add_pending_pipe_bits(cmd_buffer,
-                                ANV_PIPE_QUERY_FLUSH_BITS | ANV_PIPE_CS_STALL_BIT,
+                                needed_flushes,
                                 "CopyQueryPoolResults");
-   }
-
-   bool need_cs_stall =
-      (cmd_buffer->state.pending_pipe_bits & ANV_PIPE_FLUSH_BITS) ||
-      /* Occlusion & timestamp queries are written using a PIPE_CONTROL and
-       * because we're about to copy values from MI commands, we need to stall
-       * the command streamer to make sure the PIPE_CONTROL values have
-       * landed, otherwise we could see inconsistent values & availability.
-       *
-       *  From the vulkan spec:
-       *
-       *     "vkCmdCopyQueryPoolResults is guaranteed to see the effect of
-       *     previous uses of vkCmdResetQueryPool in the same queue, without
-       *     any additional synchronization."
-       */
-      pool->type == VK_QUERY_TYPE_OCCLUSION ||
-      pool->type == VK_QUERY_TYPE_TIMESTAMP;
-
-   if (need_cs_stall) {
-      anv_add_pending_pipe_bits(cmd_buffer,
-                                ANV_PIPE_CS_STALL_BIT,
-                                "CopyQueryPoolResults stall");
-   }
-
-   if (need_cs_stall || need_flushes)
       genX(cmd_buffer_apply_pipe_flushes)(cmd_buffer);
+   }
 
    struct anv_address dest_addr = anv_address_add(buffer->address, destOffset);
    for (uint32_t i = 0; i < queryCount; i++) {