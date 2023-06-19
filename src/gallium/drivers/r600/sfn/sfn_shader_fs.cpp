joshuaswarren/mesa-1

commit 94c3c8668b39609322c0832998f88c84876cbfb9

r600/sfn: use correct FS output location if not all outputs are used
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8973

Fixes e7f0c40d31776a6fed7f5bb13d773a365d851eb4
    r600/sfn: rewrite NIR backend

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23058>

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -508,19 +508,22 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
 
       for (unsigned k = 0; k < color_outputs; ++k) {
 
-         unsigned location =
-            (m_dual_source_blend && (semantics.location == FRAG_RESULT_COLOR)
-                ? semantics.dual_source_blend_index
-                : driver_location) + k;
+         unsigned location = semantics.location - FRAG_RESULT_DATA0;
 
-         sfn_log << SfnLog::io << "Pixel output at loc:" << location << "\n";
+         if (semantics.location == FRAG_RESULT_COLOR)
+            location = driver_location + k;
+
+         if (semantics.dual_source_blend_index)
+            location = semantics.dual_source_blend_index;
+
+         sfn_log << SfnLog::io << "Pixel output at loc:" << location
+                 << "("<< semantics.location << ") of "<< m_max_color_exports<<"\n";
 
          if (location >= m_max_color_exports) {
             sfn_log << SfnLog::io << "Pixel output loc:" << location
                     << " dl:" << driver_location << " skipped  because  we have only "
                     << m_max_color_exports << " CBs\n";
             return true;
-            ;
          }
 
          m_last_pixel_export = new ExportInstr(ExportInstr::pixel, location, value);
@@ -532,13 +535,19 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
 
          /* Hack: force dual source output handling if one color output has a
           * dual_source_blend_index > 0 */
-         if (semantics.location == FRAG_RESULT_COLOR &&
-             semantics.dual_source_blend_index > 0)
+         if (semantics.dual_source_blend_index > 0)
             m_dual_source_blend = true;
 
          if (m_num_color_exports > 1)
             m_fs_write_all = false;
          unsigned mask = (0xfu << (location * 4));
+
+         /* If the i-th target format is set, all previous target formats must
+          * be non-zero to avoid hangs. - from radeonsi, seems to apply to eg as well.
+          /*/
+         for (unsigned i = 0; i < location; ++i)
+            mask |= (0x1u << (i * 4));
+
          m_color_export_mask |= mask;
 
          emit_instruction(m_last_pixel_export);

commit aa3ceb011a90db124ac3b00f710578a7ae59a936

r600/sfn: on R600/R700 write a dummy pixel output if there is a gap
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9223

Fixes: 94c3c8668b39609322c0832998f88c84876cbfb9
  r600/sfn: Fix FS out handling

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23717>

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -540,6 +540,8 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
             m_fs_write_all = false;
          unsigned mask = (0xfu << (location * 4));
 
+         m_color_export_written_mask |= (1 << location);
+
          /* If the i-th target format is set, all previous target formats must
           * be non-zero to avoid hangs. - from radeonsi, seems to apply to eg as well.
           /*/
@@ -589,6 +591,27 @@ FragmentShader::emit_load_sample_pos(nir_intrinsic_instr *instr)
 void
 FragmentShader::do_finalize()
 {
+   /* On pre-evergreen not emtting something to all color exports that
+    * are enabled might lead to a hang.
+    * see: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9223
+    */
+   if (chip_class() < ISA_CC_EVERGREEN) {
+      unsigned i = 0;
+      unsigned mask = m_color_export_mask;
+
+      while (mask & (1u << (4 * i))) {
+         if (!(m_color_export_written_mask & (1u << i))) {
+            RegisterVec4 value(0, false, {7, 7, 7, 7});
+            m_last_pixel_export = new ExportInstr(ExportInstr::pixel, i, value);
+            emit_instruction(m_last_pixel_export);
+            m_num_color_exports++;
+            if (m_export_highest < i)
+               m_export_highest = i;
+         }
+         ++i;
+      }
+   }
+
    if (!m_last_pixel_export) {
       RegisterVec4 value(0, false, {7, 7, 7, 7});
       m_last_pixel_export = new ExportInstr(ExportInstr::pixel, 0, value);

src/gallium/drivers/r600/sfn/sfn_shader_fs.h
@@ -76,6 +76,7 @@ private:
    unsigned m_num_color_exports;
    unsigned m_color_export_mask;
    ExportInstr *m_last_pixel_export;
+   unsigned m_color_export_written_mask{0};
 
    std::bitset<s_max_interpolators> m_interpolators_used;
    RegisterVec4 m_pos_input;