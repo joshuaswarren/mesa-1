joshuaswarren/mesa-1

commit aa3ceb011a90db124ac3b00f710578a7ae59a936

r600/sfn: on R600/R700 write a dummy pixel output if there is a gap
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9223

Fixes: 94c3c8668b39609322c0832998f88c84876cbfb9
  r600/sfn: Fix FS out handling

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23717>

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -540,6 +540,8 @@ FragmentShader::emit_export_pixel(nir_intrinsic_instr& intr)
             m_fs_write_all = false;
          unsigned mask = (0xfu << (location * 4));
 
+         m_color_export_written_mask |= (1 << location);
+
          /* If the i-th target format is set, all previous target formats must
           * be non-zero to avoid hangs. - from radeonsi, seems to apply to eg as well.
           /*/
@@ -589,6 +591,27 @@ FragmentShader::emit_load_sample_pos(nir_intrinsic_instr *instr)
 void
 FragmentShader::do_finalize()
 {
+   /* On pre-evergreen not emtting something to all color exports that
+    * are enabled might lead to a hang.
+    * see: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9223
+    */
+   if (chip_class() < ISA_CC_EVERGREEN) {
+      unsigned i = 0;
+      unsigned mask = m_color_export_mask;
+
+      while (mask & (1u << (4 * i))) {
+         if (!(m_color_export_written_mask & (1u << i))) {
+            RegisterVec4 value(0, false, {7, 7, 7, 7});
+            m_last_pixel_export = new ExportInstr(ExportInstr::pixel, i, value);
+            emit_instruction(m_last_pixel_export);
+            m_num_color_exports++;
+            if (m_export_highest < i)
+               m_export_highest = i;
+         }
+         ++i;
+      }
+   }
+
    if (!m_last_pixel_export) {
       RegisterVec4 value(0, false, {7, 7, 7, 7});
       m_last_pixel_export = new ExportInstr(ExportInstr::pixel, 0, value);

src/gallium/drivers/r600/sfn/sfn_shader_fs.h
@@ -76,6 +76,7 @@ private:
    unsigned m_num_color_exports;
    unsigned m_color_export_mask;
    ExportInstr *m_last_pixel_export;
+   unsigned m_color_export_written_mask{0};
 
    std::bitset<s_max_interpolators> m_interpolators_used;
    RegisterVec4 m_pos_input;

commit 6f79ef32bf2aab39887972c6411388711b25e530

r600/sfn: Fix filling FS output gaps
in `a << b` with gcc 13 the shift count c is masked by the
bit count, and a value larger than 32 will result in shifts
by (c & 0x1f), which will add empty instructions if all
color outputs are written and this will eventually
result in an OOM error.

Fixes: aa3ceb011a90db124ac3b00f710578a7ae59a936
   r600/sfn: on R600/R700 write a dummy pixel output if there is a gap

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23945>

src/gallium/drivers/r600/sfn/sfn_shader_fs.cpp
@@ -595,7 +595,7 @@ FragmentShader::do_finalize()
       unsigned i = 0;
       unsigned mask = m_color_export_mask;
 
-      while (mask & (1u << (4 * i))) {
+      while (i < m_max_color_exports && (mask & (1u << (4 * i)))) {
          if (!(m_color_export_written_mask & (1u << i))) {
             RegisterVec4 value(0, false, {7, 7, 7, 7});
             m_last_pixel_export = new ExportInstr(ExportInstr::pixel, i, value);