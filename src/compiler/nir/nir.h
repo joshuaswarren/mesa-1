joshuaswarren/mesa-1

commit cae298b46d7481e7dd7b411554fe0ab8a0fe76af

nir: Add nir_foreach_phi(_safe) macro
Serious preprocessor voodoo here. There are two tricks here.

1. Iterating only phis. We know that phis come only at the beginning of a block,
   so all over the tree, we open-code iteration like:

      nir_foreach_instr(instr, block) {
         if (instr->type != phi)
            break;

         /* do stuff */
      }

   We can express this equivalently as

      nir_foreach_instr(instr, block)
         if (instr->type != phi)
            break;
         else {
            /* do stuff */
         }

   So, we can define a macro

      #define nir_foreach_phi(instr, block)
         if (instr->type != phi)
            break;
         else

   and then

      nir_foreach_phi(..)
         statement;

   and

      nir_foreach_phi(..) {
         ...
      }

   will expand to the right thing.

2. Automatically getting the phi as a phi. We want the instruction to go to some
   hidden variable, and then automatically insert nir_phi_instr *phi =
   nir_instr_as_phi(instr_internal); We can't do that directly, since we need to
   express the assignment implicitly in the control flow for the above trick to
   work. But we can do it indirectly with a loop initializer.

      for (nir_phi_instr *phi = nir_instr_as_phi(instr_internal); ...)

   That loop needs to break after exactly one iteration. We know that phi
   will always be non-null on its first iteration, since the original
   instruction is non-null, so we can use phi==NULL as a sentinel and express a
   one-iteration loop as for (phi = nonnull; phi != NULL; phi = NULL).

Putting these together gives the macros implemented used.

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22967>

src/compiler/nir/nir.h
@@ -2896,6 +2896,22 @@ nir_block_ends_in_break(nir_block *block)
 #define nir_foreach_instr_reverse_safe(instr, block) \
    foreach_list_typed_reverse_safe(nir_instr, instr, node, &(block)->instr_list)
 
+/* Phis come first in the block */
+#define nir_foreach_phi_internal(instr, phi) \
+   if (instr->type != nir_instr_type_phi) \
+      break; \
+   else \
+      for (nir_phi_instr *phi = nir_instr_as_phi(instr); phi != NULL; \
+           phi = NULL)
+
+#define nir_foreach_phi(instr, block) \
+   nir_foreach_instr(nir_foreach_phi_##instr, block) \
+      nir_foreach_phi_internal(nir_foreach_phi_##instr, instr)
+
+#define nir_foreach_phi_safe(instr, block) \
+   nir_foreach_instr_safe(nir_foreach_phi_safe_##instr, block) \
+      nir_foreach_phi_internal(nir_foreach_phi_safe_##instr, instr)
+
 static inline nir_phi_instr *
 nir_block_last_phi_instr(nir_block *block)
 {

commit e78f762688b84d9ebd24a0830c10f088ae9cf33d

nir: Fix breaking in nir_foreach_phi(_safe)
When I reading through some of my older commits I noticed that `break` in
`nir_foreach_phi` is broken because I used the two-loop trick wrong. Rewrite the
macros to fix this, and also to generally be a lot cleaner.

Fixes: cae298b46d7 ("nir: Add nir_foreach_phi(_safe) macro")
Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23957>

src/compiler/nir/nir.h
@@ -2962,20 +2962,39 @@ nir_block_ends_in_break(nir_block *block)
    foreach_list_typed_reverse_safe(nir_instr, instr, node, &(block)->instr_list)
 
 /* Phis come first in the block */
-#define nir_foreach_phi_internal(instr, phi) \
-   if (instr->type != nir_instr_type_phi) \
-      break; \
-   else \
-      for (nir_phi_instr *phi = nir_instr_as_phi(instr); phi != NULL; \
-           phi = NULL)
+static inline nir_phi_instr *
+nir_first_phi_in_block(nir_block *block)
+{
+   nir_foreach_instr(instr, block) {
+      if (instr->type == nir_instr_type_phi)
+         return nir_instr_as_phi(instr);
+      else
+         return NULL;
+   }
 
-#define nir_foreach_phi(instr, block) \
-   nir_foreach_instr(nir_foreach_phi_##instr, block) \
-      nir_foreach_phi_internal(nir_foreach_phi_##instr, instr)
+   return NULL;
+}
 
-#define nir_foreach_phi_safe(instr, block) \
-   nir_foreach_instr_safe(nir_foreach_phi_safe_##instr, block) \
-      nir_foreach_phi_internal(nir_foreach_phi_safe_##instr, instr)
+static inline nir_phi_instr *
+nir_next_phi(nir_phi_instr *phi)
+{
+   nir_instr *next = nir_instr_next(&phi->instr);
+
+   if (next && next->type == nir_instr_type_phi)
+      return nir_instr_as_phi(next);
+   else
+      return NULL;
+}
+
+#define nir_foreach_phi(instr, block)                                          \
+   for (nir_phi_instr *instr = nir_first_phi_in_block(block); instr != NULL;   \
+        instr = nir_next_phi(instr))
+
+#define nir_foreach_phi_safe(instr, block)                                     \
+   for (nir_phi_instr *instr = nir_first_phi_in_block(block),                  \
+                      *__next = instr ? nir_next_phi(instr) : NULL;            \
+        instr != NULL;                                                         \
+        instr = __next, __next = instr ? nir_next_phi(instr) : NULL)
 
 static inline nir_phi_instr *
 nir_block_last_phi_instr(nir_block *block)