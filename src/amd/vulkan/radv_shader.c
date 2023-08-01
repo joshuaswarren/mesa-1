joshuaswarren/mesa-1

commit 1f513fe0c4f0c139d1b29ac25332a251dbbc6322

radv: fix gathering XFB info if there is dead outputs
The driver should still gather XFB info even if all XFB outputs are
dead, otherwise the pipeline can't find the streamout shader.

RADV should use vk_spirv_to_nir() at some point to reduce code
duplication during SPIRV->NIR compilation.

This fixes new dEQP-VK.transform_feedback.simple.*.

Cc: mesa-stable
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17939>

src/amd/vulkan/radv_shader.c
@@ -654,6 +654,16 @@ lower_sincos(struct nir_builder *b, nir_instr *instr, void *_)
    return sincos->op == nir_op_fsin ? nir_fsin_amd(b, src) : nir_fcos_amd(b, src);
 }
 
+static bool
+is_not_xfb_output(nir_variable *var, void *data)
+{
+   if (var->data.mode != nir_var_shader_out)
+      return true;
+
+   return !var->data.explicit_xfb_buffer &&
+          !var->data.explicit_xfb_stride;
+}
+
 nir_shader *
 radv_shader_spirv_to_nir(struct radv_device *device, const struct radv_pipeline_stage *stage,
                          const struct radv_pipeline_key *key)
@@ -837,9 +847,12 @@ radv_shader_spirv_to_nir(struct radv_device *device, const struct radv_pipeline_
                      .use_layer_id_sysval = false,
                   });
 
+      nir_remove_dead_variables_options dead_vars_opts = {
+         .can_remove_var = is_not_xfb_output,
+      };
       NIR_PASS(_, nir, nir_remove_dead_variables,
                nir_var_shader_in | nir_var_shader_out | nir_var_system_value | nir_var_mem_shared,
-               NULL);
+               &dead_vars_opts);
 
       /* Variables can make nir_propagate_invariant more conservative
        * than it needs to be.
@@ -850,6 +863,11 @@ radv_shader_spirv_to_nir(struct radv_device *device, const struct radv_pipeline_
       NIR_PASS(_, nir, nir_propagate_invariant, key->invariant_geom);
 
       NIR_PASS(_, nir, nir_lower_clip_cull_distance_arrays);
+
+      if (nir->info.stage == MESA_SHADER_VERTEX ||
+          nir->info.stage == MESA_SHADER_TESS_EVAL ||
+          nir->info.stage == MESA_SHADER_GEOMETRY)
+         NIR_PASS_V(nir, nir_shader_gather_xfb_info);
 
       NIR_PASS(_, nir, nir_lower_discard_or_demote, key->ps.lower_discard_to_demote);
 
@@ -1038,10 +1056,6 @@ radv_shader_spirv_to_nir(struct radv_device *device, const struct radv_pipeline_
       radv_optimize_nir(nir, false, false);
    }
 
-   if (nir->info.stage == MESA_SHADER_VERTEX ||
-       nir->info.stage == MESA_SHADER_TESS_EVAL ||
-       nir->info.stage == MESA_SHADER_GEOMETRY)
-      NIR_PASS_V(nir, nir_shader_gather_xfb_info);
 
    return nir;
 }

commit 3e1242612472a36ec3abb3e812bb8caee6fca423

radv: fix unused non-xfb shader outputs not being removed
It was not taken into account that without Offset decoration
the output is not written into XFB.

Aside from eliminating more outputs this change prevents gl_PerVertex
builtins generated by glslang from being kept alive in case when XFB
is enabled. Keeping such outputs alive may upset a driver.

VUID-StandaloneSpirv-Offset-04716:
    "Only variables or block members in the output interface decorated
    with Offset can be captured for transform feedback, and those
    variables or block members must also be decorated with XfbBuffer
    and XfbStride, or inherit XfbBuffer and XfbStride decorations from
    a block containing them"

Additional info about glslang behavior could be found at:
 https://github.com/KhronosGroup/glslang/issues/1526

Fixes: 1f513fe0c4f0c139d1b29ac25332a251dbbc6322
("radv: fix gathering XFB info if there is dead outputs")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24318>

src/amd/vulkan/radv_shader.c
@@ -322,7 +322,45 @@ is_not_xfb_output(nir_variable *var, void *data)
    if (var->data.mode != nir_var_shader_out)
       return true;
 
-   return !var->data.explicit_xfb_buffer && !var->data.explicit_xfb_stride;
+   /* From the Vulkan 1.3.259 spec:
+    *
+    *    VUID-StandaloneSpirv-Offset-04716
+    *
+    *    "Only variables or block members in the output interface decorated
+    *    with Offset can be captured for transform feedback, and those
+    *    variables or block members must also be decorated with XfbBuffer
+    *    and XfbStride, or inherit XfbBuffer and XfbStride decorations from
+    *    a block containing them"
+    *
+    * glslang generates gl_PerVertex builtins when they are not declared,
+    * enabled XFB should not prevent them from being DCE'd.
+    *
+    * The logic should match nir_gather_xfb_info_with_varyings
+    */
+
+   if (!var->data.explicit_xfb_buffer)
+      return true;
+
+   bool is_array_block = var->interface_type != NULL &&
+      glsl_type_is_array(var->type) &&
+      glsl_without_array(var->type) == var->interface_type;
+
+   if (!is_array_block) {
+      return !var->data.explicit_offset;
+   } else {
+      /* For array of blocks we have to check each element */
+      unsigned aoa_size = glsl_get_aoa_size(var->type);
+      const struct glsl_type *itype = var->interface_type;
+      unsigned nfields = glsl_get_length(itype);
+      for (unsigned b = 0; b < aoa_size; b++) {
+         for (unsigned f = 0; f < nfields; f++) {
+            if (glsl_get_struct_field_offset(itype, f) >= 0)
+               return false;
+         }
+      }
+
+      return true;
+   }
 }
 
 /* If the shader doesn't have an index=1 output, then assume that it meant for a location=1 to be used. This works on