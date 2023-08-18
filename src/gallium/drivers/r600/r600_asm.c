joshuaswarren/mesa-1

commit f1aa26dabf7f50e3e9c003350c2a6f1f49134ec9

gallium: move vertex stride to CSO
this simplifies code in most place and enables some optimizations in
frontends

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24117>

src/gallium/drivers/r600/r600_asm.c
@@ -2815,6 +2815,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 	uint32_t *bytecode;
 	int i, j, r, fs_size;
 	struct r600_fetch_shader *shader;
+	unsigned strides[PIPE_MAX_ATTRIBS];
 
 	assert(count < 32);
 
@@ -2862,6 +2863,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 				}
 			}
 		}
+		strides[elements[i].vertex_buffer_index] = elements[i].src_stride;
 	}
 
 	for (i = 0; i < count; i++) {
@@ -2926,6 +2928,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 		r600_bytecode_clear(&bc);
 		return NULL;
 	}
+	memcpy(shader->strides, strides, sizeof(strides));
 
 	u_suballocator_alloc(&rctx->allocator_fetch_shader, fs_size, 256,
 			     &shader->offset,

src/gallium/drivers/r600/r600_pipe.h
@@ -434,6 +434,7 @@ struct r600_cso_state
 struct r600_fetch_shader {
 	struct r600_resource		*buffer;
 	unsigned			offset;
+	unsigned                        strides[PIPE_MAX_ATTRIBS];
 };
 
 struct r600_shader_state {

commit e71f9b428d5bf888e60612bb12ea7c25ad588c3a

r600: store the mask of buffers used by a vertex state
Fixes: f1aa26da (gallium: move vertex stride to CSO)

Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24754>

src/gallium/drivers/r600/r600_asm.c
@@ -2814,6 +2814,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 	unsigned format, num_format, format_comp, endian;
 	uint32_t *bytecode;
 	int i, j, r, fs_size;
+	uint32_t buffer_mask = 0;
 	struct r600_fetch_shader *shader;
 	unsigned strides[PIPE_MAX_ATTRIBS];
 
@@ -2864,6 +2865,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 			}
 		}
 		strides[elements[i].vertex_buffer_index] = elements[i].src_stride;
+		buffer_mask |= BITFIELD_BIT(elements[i].vertex_buffer_index);
 	}
 
 	for (i = 0; i < count; i++) {
@@ -2929,6 +2931,7 @@ void *r600_create_vertex_fetch_shader(struct pipe_context *ctx,
 		return NULL;
 	}
 	memcpy(shader->strides, strides, sizeof(strides));
+	shader->buffer_mask = buffer_mask;
 
 	u_suballocator_alloc(&rctx->allocator_fetch_shader, fs_size, 256,
 			     &shader->offset,

src/gallium/drivers/r600/r600_pipe.h
@@ -434,6 +434,7 @@ struct r600_cso_state
 struct r600_fetch_shader {
 	struct r600_resource		*buffer;
 	unsigned			offset;
+	uint32_t                        buffer_mask;
 	unsigned                        strides[PIPE_MAX_ATTRIBS];
 };
 