openharmony/third_party_mesa3d

commit e6c06679a8155eabe6aad6490523b3567bf5b764

radv/winsys: clear the PRT VA range when destroying a virtual BO
Instead of unmapping every range.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14116>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_bo.c
@@ -358,8 +358,18 @@ radv_amdgpu_winsys_bo_destroy(struct radeon_winsys *_ws, struct radeon_winsys_bo
    radv_amdgpu_log_bo(ws, bo, true);
 
    if (bo->is_virtual) {
+      int r;
+
+      /* Clear mappings of this PRT VA region. */
+      r = radv_amdgpu_bo_va_op(ws, bo->bo, 0, bo->size, bo->base.va, 0, 0, AMDGPU_VA_OP_CLEAR);
+      if (r) {
+         fprintf(stderr, "amdgpu: Failed to clear a PRT VA region (%d).\n", r);
+      }
+
       for (uint32_t i = 0; i < bo->range_count; ++i) {
-         radv_amdgpu_winsys_virtual_unmap(ws, bo, bo->ranges + i);
+         const struct radv_amdgpu_map_range *range = bo->ranges + i;
+         if (range->bo)
+            ws->base.buffer_destroy(&ws->base, (struct radeon_winsys_bo *)range->bo);
       }
       free(bo->bos);
       free(bo->ranges);

joshuaswarren/mesa-1

commit f05d67ef57650758e618da858632800326c03109

radv/amdgpu: Do not pass in a BO handle when clearing PRT VA region.
This field is invalid to access for virtual BOs.

Fixes: e6c06679a81 ("radv/winsys: clear the PRT VA range when destroying a virtual BO")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24805>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_bo.c
@@ -330,7 +330,7 @@ radv_amdgpu_winsys_bo_destroy(struct radeon_winsys *_ws, struct radeon_winsys_bo
       int r;
 
       /* Clear mappings of this PRT VA region. */
-      r = radv_amdgpu_bo_va_op(ws, bo->bo, 0, bo->size, bo->base.va, 0, 0, AMDGPU_VA_OP_CLEAR);
+      r = radv_amdgpu_bo_va_op(ws, NULL, 0, bo->size, bo->base.va, 0, 0, AMDGPU_VA_OP_CLEAR);
       if (r) {
          fprintf(stderr, "radv/amdgpu: Failed to clear a PRT VA region (%d).\n", r);
       }