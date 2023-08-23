joshuaswarren/mesa-1

commit f1aa26dabf7f50e3e9c003350c2a6f1f49134ec9

gallium: move vertex stride to CSO
this simplifies code in most place and enables some optimizations in
frontends

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24117>

src/gallium/drivers/svga/svga_state_vdecl.c
@@ -79,8 +79,8 @@ emit_hw_vs_vdecl(struct svga_context *svga, uint64_t dirty)
       buffer = svga_buffer(vb->buffer.resource);
       if (buffer->uploaded.start > offset) {
          tmp_neg_bias = buffer->uploaded.start - offset;
-         if (vb->stride)
-            tmp_neg_bias = (tmp_neg_bias + vb->stride - 1) / vb->stride;
+         if (ve->src_stride)
+            tmp_neg_bias = (tmp_neg_bias + ve->src_stride - 1) / ve->src_stride;
          neg_bias = MAX2(neg_bias, tmp_neg_bias);
       }
    }
@@ -103,14 +103,14 @@ emit_hw_vs_vdecl(struct svga_context *svga, uint64_t dirty)
       decls[i].identity.method = SVGA3D_DECLMETHOD_DEFAULT;
       decls[i].identity.usage = usage;
       decls[i].identity.usageIndex = index;
-      decls[i].array.stride = vb->stride;
+      decls[i].array.stride = ve->src_stride;
 
       /* Compensate for partially uploaded vbo, and
        * for the negative index bias.
        */
       decls[i].array.offset = (vb->buffer_offset
                            + ve[i].src_offset
-			   + neg_bias * vb->stride
+			   + neg_bias * ve->src_stride
 			   - buffer->uploaded.start);
 
       assert(decls[i].array.offset >= 0);

commit bbe17d30dcfad9257b0ad647f76ca07f9c8c9241

svga: fix stride used in vertex declaration
The stride for each vertex buffer should come from the corresponding
vertex element structure.

Fixes piglit/glretrace regressions running on svga vgpu9 device.

Fixes: f1aa26dabf7 ("gallium: move vertex stride to CSO")

Reviewed-by: Neha Bhende <bhenden@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24841>

src/gallium/drivers/svga/svga_state_vdecl.c
@@ -79,8 +79,8 @@ emit_hw_vs_vdecl(struct svga_context *svga, uint64_t dirty)
       buffer = svga_buffer(vb->buffer.resource);
       if (buffer->uploaded.start > offset) {
          tmp_neg_bias = buffer->uploaded.start - offset;
-         if (ve->src_stride)
-            tmp_neg_bias = (tmp_neg_bias + ve->src_stride - 1) / ve->src_stride;
+         if (ve[i].src_stride)
+            tmp_neg_bias = (tmp_neg_bias + ve[i].src_stride - 1) / ve[i].src_stride;
          neg_bias = MAX2(neg_bias, tmp_neg_bias);
       }
    }
@@ -103,14 +103,14 @@ emit_hw_vs_vdecl(struct svga_context *svga, uint64_t dirty)
       decls[i].identity.method = SVGA3D_DECLMETHOD_DEFAULT;
       decls[i].identity.usage = usage;
       decls[i].identity.usageIndex = index;
-      decls[i].array.stride = ve->src_stride;
+      decls[i].array.stride = ve[i].src_stride;
 
       /* Compensate for partially uploaded vbo, and
        * for the negative index bias.
        */
       decls[i].array.offset = (vb->buffer_offset
                            + ve[i].src_offset
-			   + neg_bias * ve->src_stride
+			   + neg_bias * ve[i].src_stride
 			   - buffer->uploaded.start);
 
       assert(decls[i].array.offset >= 0);