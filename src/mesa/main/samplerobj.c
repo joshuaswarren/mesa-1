openharmony/third_party_mesa3d

commit dd003ec5b385067d06736d2756685b1bcab97398

mesa: Add EXT_texture_mirror_clamp_to_edge to extension table
This is the OpenGL ES version of ARB_texture_mirror_clamp_to_edge.

v2: fix TexParameter validation (Erik)
v3: Use modernized extension test (Erik)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10325>

src/mesa/main/texparam.c
@@ -95,10 +95,11 @@ validate_texture_wrap_mode(struct gl_context * ctx, GLenum target, GLenum wrap)
       break;
 
    case GL_MIRROR_CLAMP_TO_EDGE_EXT:
-      supported = is_desktop_gl
-         && (e->ATI_texture_mirror_once || e->EXT_texture_mirror_clamp || e->ARB_texture_mirror_clamp_to_edge)
-         && (target != GL_TEXTURE_RECTANGLE_NV)
-         && (target != GL_TEXTURE_EXTERNAL_OES);
+      supported = (target != GL_TEXTURE_RECTANGLE_NV)
+         && (target != GL_TEXTURE_EXTERNAL_OES)
+         && (_mesa_has_ARB_texture_mirror_clamp_to_edge(ctx) ||
+            _mesa_has_ATI_texture_mirror_once(ctx) ||
+            _mesa_has_EXT_texture_mirror_clamp(ctx));
       break;
 
    case GL_MIRROR_CLAMP_TO_BORDER_EXT:

src/mesa/main/extensions_table.h
@@ -319,6 +319,7 @@ EXT(EXT_texture_format_BGRA8888             , dummy_true                        
 EXT(EXT_texture_integer                     , EXT_texture_integer                    , GLL, GLC,  x ,  x , 2006)
 EXT(EXT_texture_lod_bias                    , dummy_true                             , GLL,  x , ES1,  x , 1999)
 EXT(EXT_texture_mirror_clamp                , EXT_texture_mirror_clamp               , GLL, GLC,  x ,  x , 2004)
+EXT(EXT_texture_mirror_clamp_to_edge        , ARB_texture_mirror_clamp_to_edge       ,  x ,  x ,  x , ES2, 2017)
 EXT(EXT_texture_norm16                      , EXT_texture_norm16                     ,  x ,  x ,  x ,  31, 2014)
 EXT(EXT_texture_object                      , dummy_true                             , GLL,  x ,  x ,  x , 1995)
 EXT(EXT_texture_query_lod                   , ARB_texture_query_lod                  ,  x ,  x ,  x ,  30, 2019)

joshuaswarren/mesa-1

commit 378600c6394221ff04aa34eebe345fba5f6daee8

mesa: fix some TexParameter and SamplerParameter cases
EXT extension was added without tests so these functions did
not work properly.

Fixes: dd003ec5b38 ("mesa: Add EXT_texture_mirror_clamp_to_edge to extension table")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24845>

src/mesa/main/samplerobj.c
@@ -492,6 +492,15 @@ validate_texture_wrap_mode(struct gl_context *ctx, GLenum wrap)
 {
    const struct gl_extensions * const e = &ctx->Extensions;
 
+   bool mirror_clamp =
+      _mesa_has_ATI_texture_mirror_once(ctx) ||
+      _mesa_has_EXT_texture_mirror_clamp(ctx);
+
+   bool mirror_clamp_to_edge =
+      _mesa_has_ARB_texture_mirror_clamp_to_edge(ctx) ||
+      _mesa_has_EXT_texture_mirror_clamp_to_edge(ctx) ||
+      mirror_clamp;
+
    switch (wrap) {
    case GL_CLAMP:
       /* From GL 3.0 specification section E.1 "Profiles and Deprecated
@@ -508,9 +517,9 @@ validate_texture_wrap_mode(struct gl_context *ctx, GLenum wrap)
    case GL_CLAMP_TO_BORDER:
       return GL_TRUE;
    case GL_MIRROR_CLAMP_EXT:
-      return e->ATI_texture_mirror_once || e->EXT_texture_mirror_clamp;
+      return mirror_clamp;
    case GL_MIRROR_CLAMP_TO_EDGE_EXT:
-      return e->ATI_texture_mirror_once || e->EXT_texture_mirror_clamp || e->ARB_texture_mirror_clamp_to_edge;
+      return mirror_clamp_to_edge;
    case GL_MIRROR_CLAMP_TO_BORDER_EXT:
       return e->EXT_texture_mirror_clamp;
    default:

src/mesa/main/texparam.c
@@ -102,8 +102,9 @@ validate_texture_wrap_mode(struct gl_context * ctx, GLenum target, GLenum wrap)
       supported = (target != GL_TEXTURE_RECTANGLE_NV)
          && (target != GL_TEXTURE_EXTERNAL_OES)
          && (_mesa_has_ARB_texture_mirror_clamp_to_edge(ctx) ||
-            _mesa_has_ATI_texture_mirror_once(ctx) ||
-            _mesa_has_EXT_texture_mirror_clamp(ctx));
+             _mesa_has_EXT_texture_mirror_clamp_to_edge(ctx) ||
+             _mesa_has_ATI_texture_mirror_once(ctx) ||
+             _mesa_has_EXT_texture_mirror_clamp(ctx));
       break;
 
    case GL_MIRROR_CLAMP_TO_BORDER_EXT: