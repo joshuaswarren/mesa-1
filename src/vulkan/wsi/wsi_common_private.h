joshuaswarren/mesa-1

commit f7bdf9cdc595204dae7d3b5310f666d41acf1be3

vulkan/wsi/x11: Capture traces using a hotkey
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20573>

src/vulkan/wsi/wsi_common_x11.c
@@ -23,7 +23,13 @@
 
 #include <X11/Xlib-xcb.h>
 #include <X11/xshmfence.h>
+#define XK_MISCELLANY
+#define XK_LATIN1
+#include <X11/keysymdef.h>
 #include <xcb/xcb.h>
+#ifdef XCB_KEYSYMS_AVAILABLE
+#include <xcb/xcb_keysyms.h>
+#endif
 #include <xcb/dri3.h>
 #include <xcb/present.h>
 #include <xcb/shm.h>
@@ -50,6 +56,7 @@
 #include "vk_format.h"
 #include "vk_instance.h"
 #include "vk_physical_device.h"
+#include "vk_device.h"
 #include "vk_util.h"
 #include "vk_enum_to_str.h"
 #include "wsi_common_entrypoints.h"
@@ -1022,6 +1029,7 @@ struct x11_swapchain {
    bool                                         has_mit_shm;
 
    xcb_connection_t *                           conn;
+   xcb_connection_t *                           capture_conn;
    xcb_window_t                                 window;
    xcb_gc_t                                     gc;
    uint32_t                                     depth;
@@ -1680,6 +1688,29 @@ x11_present_to_x11_sw(struct x11_swapchain *chain, uint32_t image_index,
    return x11_swapchain_result(chain, VK_SUCCESS);
 }
 
+static void
+x11_capture_trace(struct x11_swapchain *chain)
+{
+   if (!chain->capture_conn)
+      return;
+
+   xcb_generic_event_t *event;
+   while ((event = xcb_poll_for_event(chain->capture_conn))) {
+      if ((event->response_type & ~0x80) != XCB_KEY_PRESS) {
+         free(event);
+         continue;
+      }
+
+      VK_FROM_HANDLE(vk_device, device, chain->base.device);
+
+      simple_mtx_lock(&device->trace_mtx);
+      device->trace_hotkey_trigger = true;
+      simple_mtx_unlock(&device->trace_mtx);
+
+      free(event);
+   }
+}
+
 /**
  * Send image to the X server for presentation at target_msc.
  */
@@ -1687,6 +1718,8 @@ static VkResult
 x11_present_to_x11(struct x11_swapchain *chain, uint32_t image_index,
                    uint64_t target_msc)
 {
+   x11_capture_trace(chain);
+
    VkResult result;
    if (chain->base.wsi->sw && !chain->has_mit_shm)
       result = x11_present_to_x11_sw(chain, image_index, target_msc);
@@ -2318,6 +2351,8 @@ x11_swapchain_destroy(struct wsi_swapchain *anv_chain,
                                              XCB_PRESENT_EVENT_MASK_NO_EVENT);
    xcb_discard_reply(chain->conn, cookie.sequence);
 
+   xcb_disconnect(chain->capture_conn);
+
    pthread_mutex_destroy(&chain->present_poll_mutex);
    pthread_mutex_destroy(&chain->present_progress_mutex);
    pthread_cond_destroy(&chain->present_progress_cond);
@@ -2600,6 +2635,24 @@ x11_surface_create_swapchain(VkIcdSurfaceBase *icd_surface,
                       VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
    if (chain == NULL)
       return VK_ERROR_OUT_OF_HOST_MEMORY;
+
+#ifdef XCB_KEYSYMS_AVAILABLE
+   VK_FROM_HANDLE(vk_device, vk_device, device);
+   if (vk_device->capture_trace) {
+      chain->capture_conn = xcb_connect(NULL, NULL);
+      assert(!xcb_connection_has_error(chain->capture_conn));
+
+      xcb_key_symbols_t *key_symbols = xcb_key_symbols_alloc(conn);
+      xcb_keycode_t *keycodes = xcb_key_symbols_get_keycode(key_symbols, XK_F12);
+      if (keycodes) {
+         xcb_grab_key(chain->capture_conn, 1, window, XCB_MOD_MASK_ANY, keycodes[0],
+                      XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+      }
+      xcb_key_symbols_free(key_symbols);
+
+      xcb_flush(chain->capture_conn);
+   }
+#endif
 
    int ret = pthread_mutex_init(&chain->present_progress_mutex, NULL);
    if (ret != 0) {

src/vulkan/runtime/vk_device.h
@@ -140,6 +140,7 @@ struct vk_device {
    VkResult (*capture_trace)(VkQueue queue);
 
    uint32_t current_frame;
+   bool trace_hotkey_trigger;
    simple_mtx_t trace_mtx;
 
    /* For VK_EXT_private_data */

src/vulkan/wsi/wsi_common.c
@@ -1248,8 +1248,10 @@ handle_trace(VkQueue queue, struct vk_device *device)
 #endif
 
    VkResult result = VK_SUCCESS;
-   if (frame_trigger || file_trigger)
+   if (frame_trigger || file_trigger || device->trace_hotkey_trigger)
       result = device->capture_trace(queue);
+
+   device->trace_hotkey_trigger = false;
 
    simple_mtx_unlock(&device->trace_mtx);
 

commit e0bfca7d1443461106b135b4615507f008755b1d

vulkan/wsi/x11: Implement capture hotkey using the keymap
This way, we can avoid opening another connection. The capture key is
changes to F1 because F12 has issues on Wayland. (After pressing F12,
all keys become unresponsive, refocussing the window fixes it)

Fixes: f7bdf9c  ("vulkan/wsi/x11: Capture traces using a hotkey")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9578
Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24710>

src/vulkan/wsi/wsi_common_private.h
@@ -171,6 +171,8 @@ struct wsi_swapchain {
       VkQueue queue;
    } blit;
 
+   bool capture_key_pressed;
+
    /* Command pools, one per queue family */
    VkCommandPool *cmd_pools;
 

src/vulkan/wsi/wsi_common_x11.c
@@ -1045,7 +1045,6 @@ struct x11_swapchain {
    bool                                         has_mit_shm;
 
    xcb_connection_t *                           conn;
-   xcb_connection_t *                           capture_conn;
    xcb_window_t                                 window;
    xcb_gc_t                                     gc;
    uint32_t                                     depth;
@@ -1687,24 +1686,36 @@ x11_present_to_x11_sw(struct x11_swapchain *chain, uint32_t image_index,
 static void
 x11_capture_trace(struct x11_swapchain *chain)
 {
-   if (!chain->capture_conn)
+#ifdef XCB_KEYSYMS_AVAILABLE
+   VK_FROM_HANDLE(vk_device, device, chain->base.device);
+   if (!device->physical->instance->trace_mode)
       return;
 
-   xcb_generic_event_t *event;
-   while ((event = xcb_poll_for_event(chain->capture_conn))) {
-      if ((event->response_type & ~0x80) != XCB_KEY_PRESS) {
-         free(event);
-         continue;
-      }
+   xcb_query_keymap_cookie_t keys_cookie = xcb_query_keymap(chain->conn);
 
-      VK_FROM_HANDLE(vk_device, device, chain->base.device);
+   xcb_generic_error_t *error = NULL;
+   xcb_query_keymap_reply_t *keys = xcb_query_keymap_reply(chain->conn, keys_cookie, &error);
+   if (error) {
+      free(error);
+      return;
+   }
+
+   xcb_key_symbols_t *key_symbols = xcb_key_symbols_alloc(chain->conn);
+   xcb_keycode_t *keycodes = xcb_key_symbols_get_keycode(key_symbols, XK_F1);
+   if (keycodes) {
+      xcb_keycode_t keycode = keycodes[0];
+      free(keycodes);
 
       simple_mtx_lock(&device->trace_mtx);
-      device->trace_hotkey_trigger = true;
+      bool capture_key_pressed = keys->keys[keycode / 8] & (1u << (keycode % 8));
+      device->trace_hotkey_trigger = capture_key_pressed && (capture_key_pressed != chain->base.capture_key_pressed);
+      chain->base.capture_key_pressed = capture_key_pressed;
       simple_mtx_unlock(&device->trace_mtx);
-
-      free(event);
    }
+
+   xcb_key_symbols_free(key_symbols);
+   free(keys);
+#endif
 }
 
 /**
@@ -2353,8 +2364,6 @@ x11_swapchain_destroy(struct wsi_swapchain *anv_chain,
                                              XCB_PRESENT_EVENT_MASK_NO_EVENT);
    xcb_discard_reply(chain->conn, cookie.sequence);
 
-   xcb_disconnect(chain->capture_conn);
-
    pthread_mutex_destroy(&chain->present_poll_mutex);
    pthread_mutex_destroy(&chain->present_progress_mutex);
    pthread_cond_destroy(&chain->present_progress_cond);
@@ -2637,24 +2646,6 @@ x11_surface_create_swapchain(VkIcdSurfaceBase *icd_surface,
                       VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
    if (chain == NULL)
       return VK_ERROR_OUT_OF_HOST_MEMORY;
-
-#ifdef XCB_KEYSYMS_AVAILABLE
-   VK_FROM_HANDLE(vk_device, vk_device, device);
-   if (vk_device->capture_trace) {
-      chain->capture_conn = xcb_connect(NULL, NULL);
-      assert(!xcb_connection_has_error(chain->capture_conn));
-
-      xcb_key_symbols_t *key_symbols = xcb_key_symbols_alloc(conn);
-      xcb_keycode_t *keycodes = xcb_key_symbols_get_keycode(key_symbols, XK_F12);
-      if (keycodes) {
-         xcb_grab_key(chain->capture_conn, 1, window, XCB_MOD_MASK_ANY, keycodes[0],
-                      XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
-      }
-      xcb_key_symbols_free(key_symbols);
-
-      xcb_flush(chain->capture_conn);
-   }
-#endif
 
    int ret = pthread_mutex_init(&chain->present_progress_mutex, NULL);
    if (ret != 0) {