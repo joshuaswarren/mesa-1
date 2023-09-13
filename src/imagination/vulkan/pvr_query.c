joshuaswarren/mesa-1

commit 9a036745cdc090d13b420975244f16ce2719950c

pvr: Implement vkGetQueryPoolResults API.
Signed-off-by: Rajnesh Kanwal <rajnesh.kanwal@imgtec.com>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19371>

src/imagination/vulkan/pvr_query.c
@@ -19,6 +19,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * 'pvr_write_query_to_buffer()' based on anv:
+ * Copyright © 2015 Intel Corporation
  */
 
 #include <assert.h>
@@ -115,6 +118,20 @@ void pvr_DestroyQueryPool(VkDevice _device,
    vk_object_free(&device->vk, pAllocator, pool);
 }
 
+static inline void pvr_write_query_to_buffer(uint8_t *buffer,
+                                             VkQueryResultFlags flags,
+                                             uint32_t idx,
+                                             uint64_t value)
+{
+   if (flags & VK_QUERY_RESULT_64_BIT) {
+      uint64_t *query_data = (uint64_t *)buffer;
+      query_data[idx] = value;
+   } else {
+      uint32_t *query_data = (uint32_t *)buffer;
+      query_data[idx] = value;
+   }
+}
+
 VkResult pvr_GetQueryPoolResults(VkDevice _device,
                                  VkQueryPool queryPool,
                                  uint32_t firstQuery,
@@ -124,8 +141,68 @@ VkResult pvr_GetQueryPoolResults(VkDevice _device,
                                  VkDeviceSize stride,
                                  VkQueryResultFlags flags)
 {
-   assert(!"Unimplemented");
-   return VK_SUCCESS;
+   PVR_FROM_HANDLE(pvr_query_pool, pool, queryPool);
+   PVR_FROM_HANDLE(pvr_device, device, _device);
+   const uint32_t core_count = device->pdevice->dev_runtime_info.core_count;
+   volatile uint32_t *available = pool->availability_buffer->bo->map;
+   volatile uint32_t *query_results = pool->result_buffer->bo->map;
+   uint8_t *data = (uint8_t *)pData;
+   VkResult result = VK_SUCCESS;
+
+   VG(VALGRIND_MAKE_MEM_DEFINED(&available[firstQuery],
+                                queryCount * sizeof(uint32_t)));
+
+   for (uint32_t i = 0; i < core_count; i++) {
+      VG(VALGRIND_MAKE_MEM_DEFINED(
+         &query_results[firstQuery + i * pool->result_stride],
+         queryCount * sizeof(uint32_t)));
+   }
+
+   for (uint32_t i = 0; i < queryCount; i++) {
+      const bool is_available = !!available[firstQuery + i];
+      uint64_t count = 0;
+      uint32_t idx = 0;
+
+      /* From the Vulkan 1.0 spec:
+       *
+       *    Commands that wait indefinitely for device execution (namely
+       *    vkDeviceWaitIdle, vkQueueWaitIdle, vkWaitForFences or
+       *    vkAcquireNextImageKHR with a maximum timeout, and
+       *    vkGetQueryPoolResults with the VK_QUERY_RESULT_WAIT_BIT bit set in
+       *    flags) must return in finite time even in the case of a lost device,
+       *    and return either VK_SUCCESS or VK_ERROR_DEVICE_LOST.
+       */
+      if (flags & VK_QUERY_RESULT_WAIT_BIT) {
+         /* Add support to wait for query results to be available. Also handle
+          * device loss scenario.
+          */
+         pvr_finishme("Unimplemented path.");
+      }
+
+      for (uint32_t j = 0; j < core_count; j++)
+         count += query_results[pool->result_stride * j + firstQuery + i];
+
+      if (is_available || (flags & VK_QUERY_RESULT_PARTIAL_BIT))
+         pvr_write_query_to_buffer(data, flags, idx++, count);
+      else
+         result = VK_NOT_READY;
+
+      if (flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
+         pvr_write_query_to_buffer(data, flags, idx++, count);
+
+      data += stride;
+   }
+
+   VG(VALGRIND_MAKE_MEM_UNDEFINED(&available[firstQuery],
+                                  queryCount * sizeof(uint32_t)));
+
+   for (uint32_t i = 0; i < core_count; i++) {
+      VG(VALGRIND_MAKE_MEM_UNDEFINED(
+         &query_results[firstQuery + i * pool->result_stride],
+         queryCount * sizeof(uint32_t)));
+   }
+
+   return result;
 }
 
 void pvr_CmdResetQueryPool(VkCommandBuffer commandBuffer,

commit d9d0ea5a6534f16eb09309f4f772e7231f399c17

pvr: Use correct index when writing query availability data
The availability data would be written to a different location in
the user provided buffer depending on whether the query for a given
index was available. Fix this by using fixed indicies when writing
the query and availability data.

Fixes conformance failures seen in the
dEQP-VK.query_pool.occlusion_query.get_reset_* test group when
implementing VK_EXT_host_query_reset.

Fixes: 9a036745cdc ("pvr: Implement vkGetQueryPoolResults API.")
Signed-off-by: Vlad Schiller <vlad-radu.schiller@imgtec.com>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25116>

src/imagination/vulkan/pvr_query.c
@@ -224,7 +224,6 @@ VkResult pvr_GetQueryPoolResults(VkDevice _device,
    for (uint32_t i = 0; i < queryCount; i++) {
       bool is_available = pvr_query_is_available(pool, firstQuery + i);
       uint64_t count = 0;
-      uint32_t idx = 0;
 
       if (flags & VK_QUERY_RESULT_WAIT_BIT && !is_available) {
          result = pvr_wait_for_available(device, pool, firstQuery + i);
@@ -238,12 +237,12 @@ VkResult pvr_GetQueryPoolResults(VkDevice _device,
          count += query_results[pool->result_stride * j + firstQuery + i];
 
       if (is_available || (flags & VK_QUERY_RESULT_PARTIAL_BIT))
-         pvr_write_query_to_buffer(data, flags, idx++, count);
+         pvr_write_query_to_buffer(data, flags, 0, count);
       else
          result = VK_NOT_READY;
 
       if (flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
-         pvr_write_query_to_buffer(data, flags, idx++, is_available);
+         pvr_write_query_to_buffer(data, flags, 1, is_available);
 
       data += stride;
    }