joshuaswarren/mesa-1

commit 8679500ce8793fad18644977b782fae981254a16

zink: use warn_missing_feature for missing modifier support
To avoid spamming VKCTS output.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25636>

src/gallium/drivers/zink/zink_resource.c
@@ -1764,7 +1764,8 @@ zink_resource_get_handle(struct pipe_screen *pscreen,
             assert(!res->all_binds); //TODO handle if problematic
             assert(!zink_resource_usage_is_unflushed(res));
             if (!screen->info.have_EXT_image_drm_format_modifier) {
-               mesa_loge("zink: EXT_image_drm_format_modifier support required");
+               static bool warned = false;
+               warn_missing_feature(warned, "zink: EXT_image_drm_format_modifier support required");
                return false;
             }
             unsigned bind = ZINK_BIND_DMABUF;

commit b35a6bd9eb68c377a3b36990d9597c30a49e02a1

zink: fix wording of warning
The string-argument for this function is the name of the feature, not
the entire message.

Fixes: 8679500ce87 ("zink: use warn_missing_feature for missing modifier support")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25644>

src/gallium/drivers/zink/zink_resource.c
@@ -1765,7 +1765,7 @@ zink_resource_get_handle(struct pipe_screen *pscreen,
             assert(!zink_resource_usage_is_unflushed(res));
             if (!screen->info.have_EXT_image_drm_format_modifier) {
                static bool warned = false;
-               warn_missing_feature(warned, "zink: EXT_image_drm_format_modifier support required");
+               warn_missing_feature(warned, "EXT_image_drm_format_modifier");
                return false;
             }
             unsigned bind = ZINK_BIND_DMABUF;