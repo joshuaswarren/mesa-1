joshuaswarren/mesa-1

commit 63cb0273142cc4079befb6cdfcade4f33ffc7e20

dri: Remove __driDriverExtensions leftovers
Android-14/clang-17 throws an error with it:

ld.lld: error: version script assignment of 'global' to symbol
   '__driDriverExtensions' failed: symbol not defined

Fixes: aebb352d126b ("dri: Remove the megadriver compat stub")
Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25587>

include/GL/internal/dri_interface.h
@@ -679,14 +679,6 @@ struct __DRIuseInvalidateExtensionRec {
 };
 
 /**
- * The remaining extensions describe driver extensions, immediately
- * available interfaces provided by the driver.  To start using the
- * driver, dlsym() for the __DRI_DRIVER_EXTENSIONS symbol and look for
- * the extension you need in the array.
- */
-#define __DRI_DRIVER_EXTENSIONS "__driDriverExtensions"
-
-/**
  * This symbol replaces the __DRI_DRIVER_EXTENSIONS symbol, and will be
  * suffixed by "_drivername", allowing multiple drivers to be built into one
  * library, and also giving the driver the chance to return a variable driver

src/gallium/targets/dri/dri.sym
@@ -1,6 +1,5 @@
 {
 	global:
-		__driDriverExtensions;
 		__driDriverGetExtensions*;
 		nouveau_drm_screen_create;
 		radeon_drm_winsys_create;

src/loader/loader.c
@@ -836,8 +836,6 @@ loader_open_driver(const char *driver_name,
       free(get_extensions_name);
    }
 
-   if (!extensions)
-      extensions = dlsym(driver, __DRI_DRIVER_EXTENSIONS);
    if (extensions == NULL) {
       log_(_LOADER_WARNING,
            "MESA-LOADER: driver exports no extensions (%s)\n", dlerror());

commit fa0778d092d35dff77bd8c0c352e878694d52604

include/dri_interface.h: restore define mistakenly removed in !25587
This file is a public API used by Xserver; removing something from it
means Xserver can't build anymore, and even if we fix Xserver, old
versions are still around and we want to keep them working to allow for
bisecting issues.

Fixes: 63cb0273142cc4079bef ("dri: Remove __driDriverExtensions leftovers")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9976
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25692>

include/GL/internal/dri_interface.h
@@ -679,6 +679,12 @@ struct __DRIuseInvalidateExtensionRec {
 };
 
 /**
+ * Dead, do not use; kept only to allow old Xserver to compile since
+ * this file is a public API.
+ */
+#define __DRI_DRIVER_EXTENSIONS "__driDriverExtensions"
+
+/**
  * This symbol replaces the __DRI_DRIVER_EXTENSIONS symbol, and will be
  * suffixed by "_drivername", allowing multiple drivers to be built into one
  * library, and also giving the driver the chance to return a variable driver