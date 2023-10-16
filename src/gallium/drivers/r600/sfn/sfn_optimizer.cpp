joshuaswarren/mesa-1

commit 69e99f3bb3656f49ad2b91ca5a0136e04ed7e62f

r600/sfn: copy-propagate single source texture values
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18619>

src/gallium/drivers/r600/sfn/sfn_optimizer.cpp
@@ -522,7 +522,20 @@ void SimplifySourceVecVisitor::visit(TexInstr *instr)
 {
 
    if (instr->opcode() != TexInstr::get_resinfo) {
-      replace_src(instr, instr->src());
+      auto& src = instr->src();
+      replace_src(instr, src);
+      int nvals = 0;
+      for (int i = 0; i < 4; ++i)
+         if (src[i]->chan() < 4)
+            ++nvals;
+      if (nvals == 1) {
+         for (int i = 0; i < 4; ++i)
+            if (src[i]->chan() < 4)
+               src[i]->set_pin(pin_free);
+      }
+   }
+   for (auto& prep : instr->prepare_instr()) {
+      prep->accept(*this);
    }
 }
 

src/gallium/drivers/r600/sfn/sfn_instr_tex.cpp
@@ -357,6 +357,28 @@ bool TexInstr::from_nir(nir_tex_instr *tex, Shader& shader)
    return true;
 }
 
+bool TexInstr::replace_source(PRegister old_src, PVirtualValue new_src)
+{
+   if (old_src->pin() != pin_free)
+      return false;
+
+   if (!new_src->as_register())
+      return false;
+
+   bool success = false;
+   for (int i = 0; i < 4; ++i) {
+      if (m_src[i]->equal_to(*old_src)) {
+         m_src.set_value(i, new_src->as_register());
+         success = true;
+      }
+   }
+   if (success) {
+      old_src->del_use(this);
+      new_src->as_register()->add_use(this);
+   }
+   return success;
+}
+
 struct SamplerId {
    int id;
    bool indirect;

src/gallium/drivers/r600/sfn/sfn_instr_tex.h
@@ -154,6 +154,8 @@ public:
 
    auto prepare_instr() const { return m_prepare_instr;}
 
+   bool replace_source(PRegister old_src, PVirtualValue new_src) override;
+
 private:
 
    bool do_ready() const override;

src/gallium/drivers/r600/sfn/sfn_virtualvalues.h
@@ -272,21 +272,27 @@ public:
            return m_values[i]->value();
 	}
 
-        PRegister operator [] (int i) {
-           return m_values[i]->value();
-        }
+   PRegister operator [] (int i) {
+      return m_values[i]->value();
+   }
 
-        void set_value(int i, PRegister reg) {
-           assert(reg->sel() == m_sel);
-           m_swz[i] = reg->chan();
-           m_values[i]->set_value(reg);
-        }
+   void set_value(int i, PRegister reg) {
+      if (reg->chan() < 4) {
+         for (int k = 0; k < 4; ++k) {
+            assert(i == k || m_values[k]->value()->chan() > 3 ||
+                   m_values[k]->value()->sel() == reg->sel());
+         }
+         m_sel = reg->sel();
+      }
+      m_swz[i] = reg->chan();
+      m_values[i]->set_value(reg);
+   }
 
-        bool ready(int block_id, int index) const;
+   bool ready(int block_id, int index) const;
 private:
-        int m_sel;
-        Swizzle m_swz;
-        std::array<R600_POINTER_TYPE(Element), 4> m_values;
+   int m_sel;
+   Swizzle m_swz;
+   std::array<R600_POINTER_TYPE(Element), 4> m_values;
 };
 
 bool operator == (const RegisterVec4& lhs, const RegisterVec4& rhs);

commit ab6ac5cf18a9c5b99fa77b4f71728206113c2df3

r600/sfn: When simplifying src vec4 pinnings, also check all uses
If a value would be used e.g. as Rn.x___ and also as Rn.xy__, in two
different instructions , then the first use was removing the group
property if Rn.x, which then broke the use in the second case, because
RA didn't see anymore, that Rn.x and Rn.y must be allocated with the
same register ID "n".

Fixes: 69e99f3bb3 (r600/sfn: copy-propagate single source texture values)
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9998

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25741>

src/gallium/drivers/r600/sfn/sfn_optimizer.cpp
@@ -820,7 +820,52 @@ public:
    bool has_group_dest;
 };
 
+class HasVecSrcVisitor : public ConstInstrVisitor {
+public:
+   HasVecSrcVisitor():
+       has_group_src(false)
+   {
+   }
 
+   void visit(UNUSED const AluInstr& instr) override { }
+   void visit(UNUSED const AluGroup& instr) override { }
+   void visit(UNUSED const FetchInstr& instr) override  { };
+   void visit(UNUSED const Block& instr) override { };
+   void visit(UNUSED const ControlFlowInstr& instr) override{ }
+   void visit(UNUSED const IfInstr& instr) override{ }
+   void visit(UNUSED const LDSAtomicInstr& instr) override { };
+   void visit(UNUSED const LDSReadInstr& instr) override { };
+
+   void visit(const TexInstr& instr) override { check(instr.src()); }
+   void visit(const ExportInstr& instr) override { check(instr.value()); }
+   void visit(const GDSInstr& instr) override { check(instr.src()); }
+
+   // No swizzling supported, so we want to keep the register group
+   void visit(UNUSED const ScratchIOInstr& instr) override  { has_group_src = true; };
+   void visit(UNUSED const StreamOutInstr& instr) override { has_group_src = true; }
+   void visit(UNUSED const MemRingOutInstr& instr) override { has_group_src = true; }
+   void visit(UNUSED const RatInstr& instr) override { has_group_src = true; };
+
+   void visit(UNUSED const EmitVertexInstr& instr) override { }
+
+   // We always emit at least two values
+   void visit(UNUSED const WriteTFInstr& instr) override { has_group_src = true; };
+
+
+   void check(const RegisterVec4& value);
+
+   bool has_group_src;
+};
+
+void HasVecSrcVisitor::check(const RegisterVec4& value)
+{
+   int nval = 0;
+   for (int i = 0; i < 4 && nval < 2; ++i) {
+      if (value[i]->chan() < 4)
+         ++nval;
+   }
+   has_group_src = nval > 1;
+}
 
 bool
 simplify_source_vectors(Shader& sh)
@@ -854,7 +899,14 @@ SimplifySourceVecVisitor::visit(TexInstr *instr)
                      break;
                }
 
-               if (check_dests.has_group_dest)
+               HasVecSrcVisitor check_src;
+               for (auto p : src[i]->uses()) {
+                  p->accept(check_src);
+                  if (check_src.has_group_src)
+                     break;
+               }
+
+               if (check_dests.has_group_dest || check_src.has_group_src)
                   break;
 
                if (src[i]->pin() == pin_group)