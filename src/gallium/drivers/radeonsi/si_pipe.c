joshuaswarren/mesa-1

commit f51f83e39eaa35576acfda09b06b030448630a31

radeonsi/gfx11: enable register shadowing by default
required by SET_SH_REG_PAIRS_PACKED*

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23517>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -1148,7 +1148,8 @@ static struct pipe_screen *radeonsi_screen_create_impl(struct radeon_winsys *ws,
       sscreen->info.use_display_dcc_with_retile_blit = false;
    }
 
-   if (sscreen->debug_flags & DBG(SHADOW_REGS))
+   if (sscreen->debug_flags & DBG(SHADOW_REGS) ||
+       sscreen->info.gfx_level >= GFX11)
       sscreen->info.register_shadowing_required = true;
 
    if (sscreen->debug_flags & DBG(NO_GFX))

commit 97e7082a73810e810436fdba40a08dc3b2cf72e9

radeonsi: disable register shadowing without SR-IOV to fix bad performance
Fixes: f51f83e39ea - radeonsi/gfx11: enable register shadowing by default

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25484>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -1175,8 +1175,7 @@ static struct pipe_screen *radeonsi_screen_create_impl(struct radeon_winsys *ws,
       sscreen->info.use_display_dcc_with_retile_blit = false;
    }
 
-   if (sscreen->debug_flags & DBG(SHADOW_REGS) ||
-       sscreen->info.gfx_level >= GFX11) {
+   if (sscreen->debug_flags & DBG(SHADOW_REGS)) {
       sscreen->info.register_shadowing_required = true;
       /* Recompute has_set_pairs_packets. */
       sscreen->info.has_set_pairs_packets = sscreen->info.gfx_level >= GFX11 &&