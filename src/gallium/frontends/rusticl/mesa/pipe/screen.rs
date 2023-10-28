joshuaswarren/mesa-1

commit 92bc294ea4fce429b6aa10dbab7f09ae5bf7186e

rusticl/mem: support read/write/copy ops for images
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15439>

src/gallium/frontends/rusticl/mesa/pipe/screen.rs
@@ -83,6 +83,20 @@ impl PipeScreen {
         )
     }
 
+    fn resource_create(&self, tmpl: &pipe_resource) -> Option<PipeResource> {
+        PipeResource::new(unsafe { (*self.screen).resource_create.unwrap()(self.screen, tmpl) })
+    }
+
+    fn resource_create_from_user(
+        &self,
+        tmpl: &pipe_resource,
+        mem: *mut c_void,
+    ) -> Option<PipeResource> {
+        PipeResource::new(unsafe {
+            (*self.screen).resource_from_user_memory.unwrap()(self.screen, tmpl, mem)
+        })
+    }
+
     pub fn resource_create_buffer(&self, size: u32) -> Option<PipeResource> {
         let mut tmpl = pipe_resource::default();
 
@@ -93,7 +107,7 @@ impl PipeScreen {
         tmpl.array_size = 1;
         tmpl.bind = PIPE_BIND_GLOBAL;
 
-        PipeResource::new(unsafe { (*self.screen).resource_create.unwrap()(self.screen, &tmpl) })
+        self.resource_create(&tmpl)
     }
 
     pub fn resource_create_buffer_from_user(
@@ -110,9 +124,52 @@ impl PipeScreen {
         tmpl.array_size = 1;
         tmpl.bind = PIPE_BIND_GLOBAL;
 
-        PipeResource::new(unsafe {
-            (*self.screen).resource_from_user_memory.unwrap()(self.screen, &tmpl, mem)
-        })
+        self.resource_create_from_user(&tmpl, mem)
+    }
+
+    pub fn resource_create_texture(
+        &self,
+        width: u32,
+        height: u16,
+        depth: u16,
+        array_size: u16,
+        target: pipe_texture_target,
+        format: pipe_format,
+    ) -> Option<PipeResource> {
+        let mut tmpl = pipe_resource::default();
+
+        tmpl.set_target(target);
+        tmpl.set_format(format);
+        tmpl.width0 = width;
+        tmpl.height0 = height;
+        tmpl.depth0 = depth;
+        tmpl.array_size = array_size;
+        tmpl.bind = PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_SHADER_IMAGE;
+
+        self.resource_create(&tmpl)
+    }
+
+    pub fn resource_create_texture_from_user(
+        &self,
+        width: u32,
+        height: u16,
+        depth: u16,
+        array_size: u16,
+        target: pipe_texture_target,
+        format: pipe_format,
+        mem: *mut c_void,
+    ) -> Option<PipeResource> {
+        let mut tmpl = pipe_resource::default();
+
+        tmpl.set_target(target);
+        tmpl.set_format(format);
+        tmpl.width0 = width;
+        tmpl.height0 = height;
+        tmpl.depth0 = depth;
+        tmpl.array_size = array_size;
+        tmpl.bind = PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_SHADER_IMAGE;
+
+        self.resource_create_from_user(&tmpl, mem)
     }
 
     pub fn param(&self, cap: pipe_cap) -> i32 {

src/gallium/frontends/rusticl/api/icd.rs
@@ -973,52 +973,83 @@ extern "C" fn cl_enqueue_copy_buffer(
 }
 
 extern "C" fn cl_enqueue_read_image(
-    _command_queue: cl_command_queue,
-    _image: cl_mem,
-    _blocking_read: cl_bool,
-    _origin: *const usize,
-    _region: *const usize,
-    _row_pitch: usize,
-    _slice_pitch: usize,
-    _ptr: *mut ::std::os::raw::c_void,
-    _num_events_in_wait_list: cl_uint,
-    _event_wait_list: *const cl_event,
-    _event: *mut cl_event,
+    command_queue: cl_command_queue,
+    image: cl_mem,
+    blocking_read: cl_bool,
+    origin: *const usize,
+    region: *const usize,
+    row_pitch: usize,
+    slice_pitch: usize,
+    ptr: *mut ::std::os::raw::c_void,
+    num_events_in_wait_list: cl_uint,
+    event_wait_list: *const cl_event,
+    event: *mut cl_event,
 ) -> cl_int {
-    println!("cl_enqueue_read_image not implemented");
-    CL_OUT_OF_HOST_MEMORY
+    match_err!(enqueue_read_image(
+        command_queue,
+        image,
+        blocking_read,
+        origin,
+        region,
+        row_pitch,
+        slice_pitch,
+        ptr,
+        num_events_in_wait_list,
+        event_wait_list,
+        event,
+    ))
 }
 
 extern "C" fn cl_enqueue_write_image(
-    _command_queue: cl_command_queue,
-    _image: cl_mem,
-    _blocking_write: cl_bool,
-    _origin: *const usize,
-    _region: *const usize,
-    _input_row_pitch: usize,
-    _input_slice_pitch: usize,
-    _ptr: *const ::std::os::raw::c_void,
-    _num_events_in_wait_list: cl_uint,
-    _event_wait_list: *const cl_event,
-    _event: *mut cl_event,
+    command_queue: cl_command_queue,
+    image: cl_mem,
+    blocking_write: cl_bool,
+    origin: *const usize,
+    region: *const usize,
+    input_row_pitch: usize,
+    input_slice_pitch: usize,
+    ptr: *const ::std::os::raw::c_void,
+    num_events_in_wait_list: cl_uint,
+    event_wait_list: *const cl_event,
+    event: *mut cl_event,
 ) -> cl_int {
-    println!("cl_enqueue_write_image not implemented");
-    CL_OUT_OF_HOST_MEMORY
+    match_err!(enqueue_write_image(
+        command_queue,
+        image,
+        blocking_write,
+        origin,
+        region,
+        input_row_pitch,
+        input_slice_pitch,
+        ptr,
+        num_events_in_wait_list,
+        event_wait_list,
+        event,
+    ))
 }
 
 extern "C" fn cl_enqueue_copy_image(
-    _command_queue: cl_command_queue,
-    _src_image: cl_mem,
-    _dst_image: cl_mem,
-    _src_origin: *const usize,
-    _dst_origin: *const usize,
-    _region: *const usize,
-    _num_events_in_wait_list: cl_uint,
-    _event_wait_list: *const cl_event,
-    _event: *mut cl_event,
+    command_queue: cl_command_queue,
+    src_image: cl_mem,
+    dst_image: cl_mem,
+    src_origin: *const usize,
+    dst_origin: *const usize,
+    region: *const usize,
+    num_events_in_wait_list: cl_uint,
+    event_wait_list: *const cl_event,
+    event: *mut cl_event,
 ) -> cl_int {
-    println!("cl_enqueue_copy_image not implemented");
-    CL_OUT_OF_HOST_MEMORY
+    match_err!(enqueue_copy_image(
+        command_queue,
+        src_image,
+        dst_image,
+        src_origin,
+        dst_origin,
+        region,
+        num_events_in_wait_list,
+        event_wait_list,
+        event,
+    ))
 }
 
 extern "C" fn cl_enqueue_copy_image_to_buffer(

src/gallium/frontends/rusticl/api/kernel.rs
@@ -410,7 +410,7 @@ pub fn enqueue_ndrange_kernel(
             local_work_size,
             global_work_size,
             global_work_offset,
-        )
+        )?
     };
 
     create_and_queue(q, CL_COMMAND_NDRANGE_KERNEL, evs, event, false, cb)

src/gallium/frontends/rusticl/api/memory.rs
@@ -8,6 +8,7 @@ use crate::api::icd::*;
 use crate::api::types::*;
 use crate::api::util::*;
 use crate::core::device::*;
+use crate::core::format::*;
 use crate::core::memory::*;
 use crate::*;
 
@@ -329,41 +330,9 @@ fn validate_image_format<'a>(
 ) -> CLResult<(&'a cl_image_format, u8)> {
     // CL_INVALID_IMAGE_FORMAT_DESCRIPTOR ... if image_format is NULL.
     let format = unsafe { image_format.as_ref() }.ok_or(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR)?;
-
-    let channels = match format.image_channel_order {
-        CL_R | CL_A | CL_DEPTH | CL_LUMINANCE | CL_INTENSITY => 1,
-
-        CL_RG | CL_RA | CL_Rx => 2,
-
-        CL_RGB | CL_RGx | CL_sRGB => 3,
-
-        CL_RGBA | CL_ARGB | CL_BGRA | CL_ABGR | CL_RGBx | CL_sRGBA | CL_sBGRA | CL_sRGBx => 4,
-
-        _ => return Err(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR),
-    };
-
-    let channel_size = match format.image_channel_data_type {
-        CL_SNORM_INT8 | CL_UNORM_INT8 | CL_SIGNED_INT8 | CL_UNSIGNED_INT8 => 1,
-
-        CL_SNORM_INT16 | CL_UNORM_INT16 | CL_SIGNED_INT16 | CL_UNSIGNED_INT16 | CL_HALF_FLOAT
-        | CL_UNORM_SHORT_565 | CL_UNORM_SHORT_555 => 2,
-
-        CL_SIGNED_INT32
-        | CL_UNSIGNED_INT32
-        | CL_FLOAT
-        | CL_UNORM_INT_101010
-        | CL_UNORM_INT_101010_2 => 4,
-
-        _ => return Err(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR),
-    };
-
-    let packed = [
-        CL_UNORM_SHORT_565,
-        CL_UNORM_SHORT_555,
-        CL_UNORM_INT_101010,
-        CL_UNORM_INT_101010,
-    ]
-    .contains(&format.image_channel_data_type);
+    let pixel_size = format
+        .pixel_size()
+        .ok_or(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR)?;
 
     // special validation
     let valid_combination = match format.image_channel_data_type {
@@ -377,14 +346,7 @@ fn validate_image_format<'a>(
         return Err(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR);
     }
 
-    Ok((
-        format,
-        if packed {
-            channel_size
-        } else {
-            channels * channel_size
-        },
-    ))
+    Ok((format, pixel_size))
 }
 
 fn validate_image_desc(
@@ -402,14 +364,11 @@ fn validate_image_desc(
     // image_type describes the image type and must be either CL_MEM_OBJECT_IMAGE1D,
     // CL_MEM_OBJECT_IMAGE1D_BUFFER, CL_MEM_OBJECT_IMAGE1D_ARRAY, CL_MEM_OBJECT_IMAGE2D,
     // CL_MEM_OBJECT_IMAGE2D_ARRAY, or CL_MEM_OBJECT_IMAGE3D.
-    let (dims, array) = match desc.image_type {
-        CL_MEM_OBJECT_IMAGE1D | CL_MEM_OBJECT_IMAGE1D_BUFFER => (1, false),
-        CL_MEM_OBJECT_IMAGE1D_ARRAY => (1, true),
-        CL_MEM_OBJECT_IMAGE2D => (2, false),
-        CL_MEM_OBJECT_IMAGE2D_ARRAY => (2, true),
-        CL_MEM_OBJECT_IMAGE3D => (3, false),
-        _ => return Err(err),
-    };
+    if !CL_IMAGE_TYPES.contains(&desc.image_type) {
+        return Err(err);
+    }
+
+    let (dims, array) = desc.type_info();
 
     // image_width is the width of the image in pixels. For a 2D image and image array, the image
     // width must be a value ≥ 1 and ≤ CL_DEVICE_IMAGE2D_MAX_WIDTH. For a 3D image, the image width
@@ -495,6 +454,8 @@ fn validate_image_desc(
         if desc.image_row_pitch != 0 || desc.image_slice_pitch != 0 {
             return Err(err);
         }
+        desc.image_row_pitch = desc.image_width * elem_size;
+        desc.image_slice_pitch = desc.image_row_pitch * desc.image_height;
     } else {
         if desc.image_row_pitch == 0 {
             desc.image_row_pitch = desc.image_width * elem_size;
@@ -729,7 +690,7 @@ pub fn create_image_with_properties(
         elem_size,
         host_ptr,
         props,
-    )))
+    )?))
 }
 
 pub fn create_image(
@@ -1515,11 +1476,198 @@ pub fn enqueue_map_buffer(
         Box::new(|_, _| Ok(())),
     )?;
 
-    Ok(b.map(&q, offset, size, block))
+    b.map(&q, offset, size, block)
     // TODO
     // CL_MISALIGNED_SUB_BUFFER_OFFSET if buffer is a sub-buffer object and offset specified when the sub-buffer object is created is not aligned to CL_DEVICE_MEM_BASE_ADDR_ALIGN value for the device associated with queue. This error code is missing before version 1.1.
     // CL_MAP_FAILURE if there is a failure to map the requested region into the host address space. This error cannot occur for buffer objects created with CL_MEM_USE_HOST_PTR or CL_MEM_ALLOC_HOST_PTR.
     // CL_INVALID_OPERATION if mapping would lead to overlapping regions being mapped for writing.
+}
+
+pub fn enqueue_read_image(
+    command_queue: cl_command_queue,
+    image: cl_mem,
+    blocking_read: cl_bool,
+    origin: *const usize,
+    region: *const usize,
+    mut row_pitch: usize,
+    mut slice_pitch: usize,
+    ptr: *mut ::std::os::raw::c_void,
+    num_events_in_wait_list: cl_uint,
+    event_wait_list: *const cl_event,
+    event: *mut cl_event,
+) -> CLResult<()> {
+    let q = command_queue.get_arc()?;
+    let i = image.get_arc()?;
+    let block = check_cl_bool(blocking_read).ok_or(CL_INVALID_VALUE)?;
+    let evs = event_list_from_cl(&q, num_events_in_wait_list, event_wait_list)?;
+    let pixel_size = i.image_format.pixel_size().unwrap() as usize;
+
+    // CL_INVALID_CONTEXT if the context associated with command_queue and image are not the same
+    if i.context != q.context {
+        return Err(CL_INVALID_CONTEXT);
+    }
+
+    // CL_INVALID_OPERATION if clEnqueueReadImage is called on image which has been created with
+    // CL_MEM_HOST_WRITE_ONLY or CL_MEM_HOST_NO_ACCESS.
+    if bit_check(i.flags, CL_MEM_HOST_WRITE_ONLY | CL_MEM_HOST_NO_ACCESS) {
+        return Err(CL_INVALID_OPERATION);
+    }
+
+    // CL_INVALID_VALUE if origin or region is NULL.
+    // CL_INVALID_VALUE if ptr is NULL.
+    if origin.is_null() || region.is_null() || ptr.is_null() {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    // CL_INVALID_VALUE if image is a 1D or 2D image and slice_pitch or input_slice_pitch is not 0.
+    if !i.image_desc.has_slice() && slice_pitch != 0 {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    let r = unsafe { CLVec::from_raw(region) };
+    let o = unsafe { CLVec::from_raw(origin) };
+
+    // If row_pitch (or input_row_pitch) is set to 0, the appropriate row pitch is calculated based
+    // on the size of each element in bytes multiplied by width.
+    if row_pitch == 0 {
+        row_pitch = r[0] * pixel_size;
+    }
+
+    // If slice_pitch (or input_slice_pitch) is set to 0, the appropriate slice pitch is calculated
+    // based on the row_pitch × height.
+    if slice_pitch == 0 {
+        slice_pitch = row_pitch * r[1];
+    }
+
+    create_and_queue(
+        q,
+        CL_COMMAND_READ_IMAGE,
+        evs,
+        event,
+        block,
+        Box::new(move |q, ctx| {
+            i.read_to_user_rect(
+                ptr,
+                q,
+                ctx,
+                &r,
+                &o,
+                i.image_desc.image_row_pitch,
+                i.image_desc.image_slice_pitch,
+                &CLVec::default(),
+                row_pitch,
+                slice_pitch,
+            )
+        }),
+    )
+
+    //• CL_INVALID_VALUE if the region being read or written specified by origin and region is out of bounds.
+    //• CL_INVALID_VALUE if values in origin and region do not follow rules described in the argument description for origin and region.
+    //• CL_INVALID_IMAGE_SIZE if image dimensions (image width, height, specified or compute row and/or slice pitch) for image are not supported by device associated with queue.
+    //• CL_IMAGE_FORMAT_NOT_SUPPORTED if image format (image channel order and data type) for image are not supported by device associated with queue.
+    //• CL_INVALID_OPERATION if the device associated with command_queue does not support images (i.e. CL_DEVICE_IMAGE_SUPPORT specified in the Device Queries table is CL_FALSE).
+    //• CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST if the read and write operations are blocking and the execution status of any of the events in event_wait_list is a negative integer value.
+}
+
+pub fn enqueue_write_image(
+    command_queue: cl_command_queue,
+    image: cl_mem,
+    blocking_write: cl_bool,
+    origin: *const usize,
+    region: *const usize,
+    mut row_pitch: usize,
+    mut slice_pitch: usize,
+    ptr: *const ::std::os::raw::c_void,
+    num_events_in_wait_list: cl_uint,
+    event_wait_list: *const cl_event,
+    event: *mut cl_event,
+) -> CLResult<()> {
+    let q = command_queue.get_arc()?;
+    let i = image.get_arc()?;
+    let block = check_cl_bool(blocking_write).ok_or(CL_INVALID_VALUE)?;
+    let evs = event_list_from_cl(&q, num_events_in_wait_list, event_wait_list)?;
+    let pixel_size = i.image_format.pixel_size().unwrap() as usize;
+
+    // CL_INVALID_CONTEXT if the context associated with command_queue and image are not the same
+    if i.context != q.context {
+        return Err(CL_INVALID_CONTEXT);
+    }
+
+    // CL_INVALID_OPERATION if clEnqueueWriteImage is called on image which has been created with
+    // CL_MEM_HOST_READ_ONLY or CL_MEM_HOST_NO_ACCESS.
+    if bit_check(i.flags, CL_MEM_HOST_READ_ONLY | CL_MEM_HOST_NO_ACCESS) {
+        return Err(CL_INVALID_OPERATION);
+    }
+
+    // CL_INVALID_VALUE if origin or region is NULL.
+    // CL_INVALID_VALUE if ptr is NULL.
+    if origin.is_null() || region.is_null() || ptr.is_null() {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    // CL_INVALID_VALUE if image is a 1D or 2D image and slice_pitch or input_slice_pitch is not 0.
+    if !i.image_desc.has_slice() && slice_pitch != 0 {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    let r = unsafe { CLVec::from_raw(region) };
+    let o = unsafe { CLVec::from_raw(origin) };
+
+    // If row_pitch (or input_row_pitch) is set to 0, the appropriate row pitch is calculated based
+    // on the size of each element in bytes multiplied by width.
+    if row_pitch == 0 {
+        row_pitch = r[0] * pixel_size;
+    }
+
+    // If slice_pitch (or input_slice_pitch) is set to 0, the appropriate slice pitch is calculated
+    // based on the row_pitch × height.
+    if slice_pitch == 0 {
+        slice_pitch = row_pitch * r[1];
+    }
+
+    create_and_queue(
+        q,
+        CL_COMMAND_WRITE_BUFFER_RECT,
+        evs,
+        event,
+        block,
+        Box::new(move |q, ctx| {
+            i.write_from_user_rect(
+                ptr,
+                q,
+                ctx,
+                &r,
+                &CLVec::default(),
+                row_pitch,
+                slice_pitch,
+                &o,
+                i.image_desc.image_row_pitch,
+                i.image_desc.image_slice_pitch,
+            )
+        }),
+    )
+
+    //• CL_INVALID_VALUE if the region being read or written specified by origin and region is out of bounds.
+    //• CL_INVALID_VALUE if values in origin and region do not follow rules described in the argument description for origin and region.
+    //• CL_INVALID_IMAGE_SIZE if image dimensions (image width, height, specified or compute row and/or slice pitch) for image are not supported by device associated with queue.
+    //• CL_IMAGE_FORMAT_NOT_SUPPORTED if image format (image channel order and data type) for image are not supported by device associated with queue.
+    //• CL_INVALID_OPERATION if the device associated with command_queue does not support images (i.e. CL_DEVICE_IMAGE_SUPPORT specified in the Device Queries table is CL_FALSE).
+    //• CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST if the read and write operations are blocking and the execution status of any of the events in event_wait_list is a negative integer value.
+}
+
+pub fn enqueue_copy_image(
+    _command_queue: cl_command_queue,
+    _src_image: cl_mem,
+    _dst_image: cl_mem,
+    _src_origin: *const usize,
+    _dst_origin: *const usize,
+    _region: *const usize,
+    _num_events_in_wait_list: cl_uint,
+    _event_wait_list: *const cl_event,
+    _event: *mut cl_event,
+) -> CLResult<()> {
+    println!("enqueue_copy_image not implemented");
+    Err(CL_OUT_OF_HOST_MEMORY)
 }
 
 pub fn enqueue_unmap_mem_object(

src/gallium/frontends/rusticl/core/context.rs
@@ -3,6 +3,8 @@ extern crate rusticl_opencl_gen;
 
 use crate::api::icd::*;
 use crate::core::device::*;
+use crate::core::format::*;
+use crate::core::util::*;
 use crate::impl_cl_type_trait;
 
 use self::mesa_rust::pipe::resource::*;
@@ -57,6 +59,79 @@ impl Context {
             let resource = dev
                 .screen()
                 .resource_create_buffer_from_user(adj_size, user_ptr)
+                .ok_or(CL_OUT_OF_RESOURCES);
+            res.insert(Arc::clone(dev), Arc::new(resource?));
+        }
+        Ok(res)
+    }
+
+    pub fn create_texture(
+        &self,
+        desc: &cl_image_desc,
+        format: &cl_image_format,
+    ) -> CLResult<HashMap<Arc<Device>, Arc<PipeResource>>> {
+        let width = desc
+            .image_width
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let height = desc
+            .image_height
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let depth = desc
+            .image_depth
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let array_size = desc
+            .image_array_size
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let target = cl_mem_type_to_texture_target(desc.image_type);
+        let format = format.to_pipe_format().unwrap();
+
+        let mut res = HashMap::new();
+        for dev in &self.devs {
+            let resource = dev
+                .screen()
+                .resource_create_texture(width, height, depth, array_size, target, format)
+                .ok_or(CL_OUT_OF_RESOURCES);
+            res.insert(Arc::clone(dev), Arc::new(resource?));
+        }
+        Ok(res)
+    }
+
+    pub fn create_texture_from_user(
+        &self,
+        desc: &cl_image_desc,
+        format: &cl_image_format,
+        user_ptr: *mut c_void,
+    ) -> CLResult<HashMap<Arc<Device>, Arc<PipeResource>>> {
+        let width = desc
+            .image_width
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let height = desc
+            .image_height
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let depth = desc
+            .image_depth
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let array_size = desc
+            .image_array_size
+            .try_into()
+            .map_err(|_| CL_OUT_OF_HOST_MEMORY)?;
+        let target = cl_mem_type_to_texture_target(desc.image_type);
+        let format = format.to_pipe_format().unwrap();
+
+        let mut res = HashMap::new();
+        for dev in &self.devs {
+            let resource = dev
+                .screen()
+                .resource_create_texture_from_user(
+                    width, height, depth, array_size, target, format, user_ptr,
+                )
                 .ok_or(CL_OUT_OF_RESOURCES);
             res.insert(Arc::clone(dev), Arc::new(resource?));
         }

src/gallium/frontends/rusticl/core/format.rs
@@ -179,3 +179,77 @@ pub const FORMATS: &[RusticlImageFormat] = &[
         pipe_format::PIPE_FORMAT_B8G8R8A8_UNORM,
     ),
 ];
+
+pub trait CLFormatInfo {
+    fn channels(&self) -> Option<u8>;
+    fn format_info(&self) -> Option<(u8, bool)>;
+    fn to_pipe_format(&self) -> Option<pipe_format>;
+
+    fn channel_size(&self) -> Option<u8> {
+        if let Some(packed) = self.is_packed() {
+            assert!(!packed);
+            self.format_info().map(|i| i.0)
+        } else {
+            None
+        }
+    }
+
+    fn packed_size(&self) -> Option<u8> {
+        if let Some(packed) = self.is_packed() {
+            assert!(packed);
+            self.format_info().map(|i| i.0)
+        } else {
+            None
+        }
+    }
+
+    fn is_packed(&self) -> Option<bool> {
+        self.format_info().map(|i| i.1)
+    }
+
+    fn pixel_size(&self) -> Option<u8> {
+        if let Some(packed) = self.is_packed() {
+            if packed {
+                self.packed_size()
+            } else {
+                self.channels().zip(self.channel_size()).map(|(c, s)| c * s)
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl CLFormatInfo for cl_image_format {
+    #[allow(non_upper_case_globals)]
+    fn channels(&self) -> Option<u8> {
+        match self.image_channel_order {
+            CL_R | CL_A | CL_DEPTH | CL_INTENSITY | CL_LUMINANCE => Some(1),
+            CL_RG | CL_RA | CL_Rx => Some(2),
+            CL_RGB | CL_RGx | CL_sRGB => Some(3),
+            CL_RGBA | CL_ARGB | CL_BGRA | CL_ABGR | CL_RGBx | CL_sRGBA | CL_sBGRA | CL_sRGBx => {
+                Some(4)
+            }
+            _ => None,
+        }
+    }
+
+    fn format_info(&self) -> Option<(u8, bool)> {
+        match self.image_channel_data_type {
+            CL_SIGNED_INT8 | CL_UNSIGNED_INT8 | CL_SNORM_INT8 | CL_UNORM_INT8 => Some((1, false)),
+            CL_SIGNED_INT16 | CL_UNSIGNED_INT16 | CL_SNORM_INT16 | CL_UNORM_INT16
+            | CL_HALF_FLOAT => Some((2, false)),
+            CL_SIGNED_INT32 | CL_UNSIGNED_INT32 | CL_FLOAT => Some((4, false)),
+            CL_UNORM_SHORT_555 | CL_UNORM_SHORT_565 => Some((2, true)),
+            CL_UNORM_INT_101010 | CL_UNORM_INT_101010_2 => Some((4, true)),
+            _ => None,
+        }
+    }
+
+    fn to_pipe_format(&self) -> Option<pipe_format> {
+        FORMATS
+            .iter()
+            .find(|f| f.cl_image_format == *self)
+            .map(|f| f.pipe)
+    }
+}

src/gallium/frontends/rusticl/core/kernel.rs
@@ -409,7 +409,7 @@ impl Kernel {
         block: &[usize],
         grid: &[usize],
         offsets: &[usize],
-    ) -> EventSig {
+    ) -> CLResult<EventSig> {
         let nir = self.nirs.get(&q.device).unwrap();
         let mut block = create_kernel_arr::<u32>(block, 1);
         let mut grid = create_kernel_arr::<u32>(grid, 1);
@@ -436,7 +436,7 @@ impl Kernel {
                 KernelArgValue::Constant(c) => input.extend_from_slice(c),
                 KernelArgValue::MemObject(mem) => {
                     input.extend_from_slice(&mem.offset.to_ne_bytes());
-                    resource_info.push((Some(mem.get_res_of_dev(&q.device).clone()), arg.offset));
+                    resource_info.push((Some(mem.get_res_of_dev(&q.device)?.clone()), arg.offset));
                 }
                 KernelArgValue::LocalMem(size) => {
                     // TODO 32 bit
@@ -491,7 +491,7 @@ impl Kernel {
         }
 
         let k = self.clone();
-        Box::new(move |q, ctx| {
+        Ok(Box::new(move |q, ctx| {
             let nir = k.nirs.get(&q.device).unwrap();
             let mut input = input.clone();
             let mut resources = Vec::with_capacity(resource_info.len());
@@ -545,7 +545,7 @@ impl Kernel {
             }
 
             Ok(())
-        })
+        }))
     }
 
     pub fn access_qualifier(&self, idx: cl_uint) -> cl_kernel_arg_access_qualifier {

commit 6a98bd1b47bdb8e9ee59f932cd06398bc3b25173

rusticl/mesa: pass PIPE_BIND_LINEAR in resource_create_texture_from_user
Host pointer allocations are all linear laid out, so just tell the drivers
in case they don't assume this implicitly.

Fixes: 92bc294ea4f ("rusticl/mem: support read/write/copy ops for images")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25937>

src/gallium/frontends/rusticl/mesa/pipe/screen.rs
@@ -216,7 +216,7 @@ impl PipeScreen {
         tmpl.height0 = height;
         tmpl.depth0 = depth;
         tmpl.array_size = array_size;
-        tmpl.bind = PIPE_BIND_SAMPLER_VIEW;
+        tmpl.bind = PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_LINEAR;
 
         if support_image {
             tmpl.bind |= PIPE_BIND_SHADER_IMAGE;