joshuaswarren/mesa-1

commit 948e862d6f8f4008c6dc8b4f3a35ab0f0b2593d3

ci: hide Mesa install phase
So far we show installation part together with meson tests.

Reviewed-by: Martin Roukala <martin.roukala@mupuf.org>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25843>

.gitlab-ci/meson/build.sh
@@ -104,6 +104,7 @@ fi
 
 uncollapsed_section_switch meson-test "meson: test"
 LC_ALL=C.UTF-8 meson test --num-processes "${FDO_CI_CONCURRENT:-4}" --print-errorlogs ${MESON_TEST_ARGS}
+section_switch meson-configure "meson: install"
 if command -V mold &> /dev/null ; then
     mold --run ninja install
 else

commit 39845290b5a42af3a4f09c1bf72b09a7de73cb98

ci: fixup section names
Copy-paste typo + missed section_end update.

Fixes: 948e862d6f8f ("ci: hide Mesa install phase")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25982>

.gitlab-ci/meson/build.sh
@@ -104,11 +104,11 @@ fi
 
 uncollapsed_section_switch meson-test "meson: test"
 LC_ALL=C.UTF-8 meson test --num-processes "${FDO_CI_CONCURRENT:-4}" --print-errorlogs ${MESON_TEST_ARGS}
-section_switch meson-configure "meson: install"
+section_switch meson-install "meson: install"
 if command -V mold &> /dev/null ; then
     mold --run ninja install
 else
     ninja install
 fi
 cd ..
-section_end meson-test
+section_end meson-install