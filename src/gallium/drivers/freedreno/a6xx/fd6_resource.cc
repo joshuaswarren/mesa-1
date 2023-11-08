joshuaswarren/mesa-1

commit 586f0110c70b3f48698cec7941b3766bf9cccf59

freedreno: Handle DRM_FORMAT_MOD_QCOM_TILED3 import
This is something that we could allocate and export.  So we must import
them as well.

Fixes import failures in spec@ext_image_dma_buf_import@ext_image_dma_buf_import-modifiers

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25995>

src/gallium/drivers/freedreno/a6xx/fd6_resource.cc
@@ -322,12 +322,14 @@ fd6_layout_resource_for_modifier(struct fd_resource *rsc, uint64_t modifier)
                     PRSC_ARGS(&rsc->b.b));
       }
       return 0;
+   case DRM_FORMAT_MOD_QCOM_TILED3:
    case DRM_FORMAT_MOD_INVALID:
       if (can_do_ubwc(&rsc->b.b)) {
          perf_debug("%" PRSC_FMT
                     ": not UBWC: imported with DRM_FORMAT_MOD_INVALID!",
                     PRSC_ARGS(&rsc->b.b));
       }
+      rsc->layout.tile_mode = fd6_tile_mode(&rsc->b.b);
       return 0;
    default:
       return -1;

commit 0ff8b3f4b14290ecf7fdd5e4dbe58f90ecee5f62

freedreno/a6xx: Assume MOD_INVALID imports are linear
Without !25945 we must assume imports with invalid modifier are linear.
When both sides support metadata, we can promote the modifier.

Fixes: 586f0110c70b ("freedreno: Handle DRM_FORMAT_MOD_QCOM_TILED3 import")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26115>

src/gallium/drivers/freedreno/a6xx/fd6_resource.cc
@@ -323,13 +323,17 @@ fd6_layout_resource_for_modifier(struct fd_resource *rsc, uint64_t modifier)
       }
       return 0;
    case DRM_FORMAT_MOD_QCOM_TILED3:
+      rsc->layout.tile_mode = fd6_tile_mode(&rsc->b.b);
+      FALLTHROUGH;
    case DRM_FORMAT_MOD_INVALID:
+      /* For now, without buffer metadata, we must assume that buffers
+       * imported with INVALID modifier are linear
+       */
       if (can_do_ubwc(&rsc->b.b)) {
          perf_debug("%" PRSC_FMT
                     ": not UBWC: imported with DRM_FORMAT_MOD_INVALID!",
                     PRSC_ARGS(&rsc->b.b));
       }
-      rsc->layout.tile_mode = fd6_tile_mode(&rsc->b.b);
       return 0;
    default:
       return -1;