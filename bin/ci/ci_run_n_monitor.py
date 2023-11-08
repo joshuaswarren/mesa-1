joshuaswarren/mesa-1

commit 5e50a7a43556cbc6dbf9e080088211d86d86a44e

ci_run_n_monitor: compile target_jobs_regex only once
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26070>

bin/ci/ci_run_n_monitor.py
@@ -87,7 +87,7 @@ def pretty_wait(sec: int) -> None:
 def monitor_pipeline(
     project,
     pipeline,
-    target_job: str,
+    target_jobs_regex: re.Pattern,
     dependencies,
     force_manual: bool,
     stress: int,
@@ -97,8 +97,6 @@ def monitor_pipeline(
     target_statuses: dict[str, str] = defaultdict(str)
     stress_status_counter = defaultdict(lambda: defaultdict(int))
     target_id = None
-
-    target_jobs_regex = re.compile(target_job.strip())
 
     while True:
         to_cancel = []
@@ -295,13 +293,13 @@ def parse_args() -> None:
     return args
 
 
-def find_dependencies(target_job: str, project_path: str, iid: int) -> set[str]:
+def find_dependencies(target_jobs_regex: re.Pattern, project_path: str, iid: int) -> set[str]:
     gql_instance = GitlabGQL()
     dag, _ = create_job_needs_dag(
         gql_instance, {"projectPath": project_path.path_with_namespace, "iid": iid}
     )
 
-    target_dep_dag = filter_dag(dag, target_job)
+    target_dep_dag = filter_dag(dag, target_jobs_regex)
     if not target_dep_dag:
         print(Fore.RED + "The job(s) were not found in the pipeline." + Fore.RESET)
         sys.exit(1)
@@ -348,14 +346,16 @@ if __name__ == "__main__":
         print(f"Revision: {REV}")
         print(f"Pipeline: {pipe.web_url}")
 
+        target_jobs_regex = re.compile(args.target.strip())
+
         deps = set()
         if args.target:
             print("🞋 job: " + Fore.BLUE + args.target + Style.RESET_ALL)
             deps = find_dependencies(
-                target_job=args.target, iid=pipe.iid, project_path=cur_project
+                target_job=target_jobs_regex, iid=pipe.iid, project_path=cur_project
             )
         target_job_id, ret = monitor_pipeline(
-            cur_project, pipe, args.target, deps, args.force_manual, args.stress
+            cur_project, pipe, target_jobs_regex, deps, args.force_manual, args.stress
         )
 
         if target_job_id:

commit 3bb900fb092cab5c7fbd78a03a55fa029763fbd2

ci/bin: Fix find_dependency function calls
`find_dependency` signature changed, so we need to update their callings
correctly.

Fixes: 5e50a7a43556cbc6dbf9e080088211d86d86a44e

Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25940>

bin/ci/ci_run_n_monitor.py
@@ -352,7 +352,7 @@ if __name__ == "__main__":
         if args.target:
             print("🞋 job: " + Fore.BLUE + args.target + Style.RESET_ALL)
             deps = find_dependencies(
-                target_job=target_jobs_regex, iid=pipe.iid, project_path=cur_project
+                target_jobs_regex=target_jobs_regex, iid=pipe.iid, project_path=cur_project
             )
         target_job_id, ret = monitor_pipeline(
             cur_project, pipe, target_jobs_regex, deps, args.force_manual, args.stress