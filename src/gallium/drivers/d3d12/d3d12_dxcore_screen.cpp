joshuaswarren/mesa-1

commit b4dbf06e00941f8b34b5043de76d27acacf1f565

d3d12: Allow creating d3d12_dxcore_screen from existing ID3D12Device
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26223>

src/gallium/drivers/d3d12/d3d12_dxcore_screen.cpp
@@ -247,3 +247,33 @@ d3d12_create_dxcore_screen(struct sw_winsys *winsys, LUID *adapter_luid)
 
    return &screen->base.base;
 }
+
+struct pipe_screen *
+d3d12_create_dxcore_screen_from_d3d12_device(struct sw_winsys *winsys, IUnknown* pDevUnk, LUID **out_adapter_luid)
+{
+   struct d3d12_dxcore_screen *screen = CALLOC_STRUCT(d3d12_dxcore_screen);
+   if (!screen)
+      return nullptr;
+
+   if (FAILED(pDevUnk->QueryInterface(IID_PPV_ARGS(&screen->base.dev)))) {
+      d3d12_destroy_screen(&screen->base);
+      return nullptr;
+   }
+
+   LUID adapter_luid = screen->base.dev->GetAdapterLuid();
+   if (!d3d12_init_screen_base(&screen->base, winsys, &adapter_luid)) {
+      d3d12_destroy_screen(&screen->base);
+      return nullptr;
+   }
+   screen->base.base.destroy = d3d12_destroy_dxcore_screen;
+   screen->base.init = d3d12_init_dxcore_screen;
+   screen->base.deinit = d3d12_deinit_dxcore_screen;
+
+   if (!d3d12_init_dxcore_screen(&screen->base)) {
+      d3d12_destroy_dxcore_screen(&screen->base.base);
+      return nullptr;
+   }
+
+   *out_adapter_luid = &screen->base.adapter_luid;
+   return &screen->base.base;
+}

src/gallium/drivers/d3d12/d3d12_public.h
@@ -28,6 +28,10 @@ struct pipe_screen;
 struct sw_winsys;
 typedef struct _LUID LUID;
 
+#ifdef _WIN32
+#include <unknwn.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -35,6 +39,10 @@ extern "C" {
 #ifdef _WIN32
 struct pipe_screen *
 d3d12_create_dxgi_screen(struct sw_winsys *winsys, LUID *adapter_luid);
+
+struct pipe_screen *
+d3d12_create_dxcore_screen_from_d3d12_device(struct sw_winsys *winsys, IUnknown* pDevUnknown, LUID **out_adapter_luid);
+
 #endif
 
 struct pipe_screen *

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -1422,30 +1422,32 @@ d3d12_init_screen(struct d3d12_screen *screen, IUnknown *adapter)
 {
    assert(screen->base.destroy != nullptr);
 
+   // Device can be imported with d3d12_create_dxcore_screen_from_d3d12_device
+   if (!screen->dev) {
 #ifndef _GAMING_XBOX
-   ID3D12DeviceFactory *factory = try_create_device_factory(screen->d3d12_mod);
+      ID3D12DeviceFactory *factory = try_create_device_factory(screen->d3d12_mod);
 
 #ifndef DEBUG
-   if (d3d12_debug & D3D12_DEBUG_DEBUG_LAYER)
+      if (d3d12_debug & D3D12_DEBUG_DEBUG_LAYER)
 #endif
-      enable_d3d12_debug_layer(screen->d3d12_mod, factory);
+         enable_d3d12_debug_layer(screen->d3d12_mod, factory);
 
-   if (d3d12_debug & D3D12_DEBUG_GPU_VALIDATOR)
-      enable_gpu_validation(screen->d3d12_mod, factory);
+      if (d3d12_debug & D3D12_DEBUG_GPU_VALIDATOR)
+         enable_gpu_validation(screen->d3d12_mod, factory);
 
-   screen->dev = create_device(screen->d3d12_mod, adapter, factory);
+      screen->dev = create_device(screen->d3d12_mod, adapter, factory);
 
-   if (factory)
-      factory->Release();
+      if (factory)
+         factory->Release();
 #else
-   screen->dev = create_device(screen->d3d12_mod, adapter);
+      screen->dev = create_device(screen->d3d12_mod, adapter);
 #endif
 
-   if (!screen->dev) {
-      debug_printf("D3D12: failed to create device\n");
-      return false;
+      if (!screen->dev) {
+         debug_printf("D3D12: failed to create device\n");
+         return false;
+      }
    }
-
    screen->adapter_luid = GetAdapterLuid(screen->dev);
 
 #ifndef _GAMING_XBOX

commit 989a381d28dc9394f857cb8a52d909900c404d7f

d3d12: fix usage of GetAdapterLuid() in mingw/GCC using ABI helper
Fixes: b4dbf06e009 ("d3d12: Allow creating d3d12_dxcore_screen from existing ID3D12Device")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26338>

src/gallium/drivers/d3d12/d3d12_dxcore_screen.cpp
@@ -260,7 +260,7 @@ d3d12_create_dxcore_screen_from_d3d12_device(struct sw_winsys *winsys, IUnknown*
       return nullptr;
    }
 
-   LUID adapter_luid = screen->base.dev->GetAdapterLuid();
+   LUID adapter_luid = GetAdapterLuid(screen->base.dev);
    if (!d3d12_init_screen_base(&screen->base, winsys, &adapter_luid)) {
       d3d12_destroy_screen(&screen->base);
       return nullptr;