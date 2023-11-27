joshuaswarren/mesa-1

commit b82a21aeed3353dc9319fbd778d66fe95a85fda5

nvk: Enable subgroups features
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24998>

src/nouveau/vulkan/nvk_shader.c
@@ -79,8 +79,8 @@ get_prog_optimize(void)
    return debug_get_num_option("NV50_PROG_OPTIMIZE", 3);
 }
 
-static uint32_t
-get_nak_stages(void)
+VkShaderStageFlags
+nvk_nak_stages(void)
 {
    const struct debug_control flags[] = {
       { "vs", BITFIELD64_BIT(MESA_SHADER_VERTEX) },
@@ -99,7 +99,7 @@ get_nak_stages(void)
 static bool
 use_nak(gl_shader_stage stage)
 {
-   return get_nak_stages() & BITFIELD64_BIT(stage);
+   return nvk_nak_stages() & BITFIELD64_BIT(stage);
 }
 
 uint64_t
@@ -107,7 +107,7 @@ nvk_physical_device_compiler_flags(const struct nvk_physical_device *pdev)
 {
    uint64_t prog_debug = get_prog_debug();
    uint64_t prog_optimize = get_prog_optimize();
-   uint64_t nak_stages = get_nak_stages();
+   uint64_t nak_stages = nvk_nak_stages();
    uint64_t nak_flags = nak_debug_flags(pdev->nak);
 
    assert(prog_debug <= UINT8_MAX);
@@ -153,6 +153,9 @@ nvk_physical_device_spirv_options(const struct nvk_physical_device *pdev,
          .runtime_descriptor_array = true,
          .shader_clock = true,
          .shader_viewport_index_layer = true,
+         .subgroup_ballot = true,
+         .subgroup_basic = true,
+         .subgroup_vote = true,
          .tessellation = true,
          .transform_feedback = true,
          .variable_pointers = true,

src/nouveau/vulkan/nvk_physical_device.c
@@ -533,6 +533,12 @@ nvk_get_device_properties(const struct nvk_instance *instance,
       .sparseResidencyNonResidentStrict = true,
 
       /* Vulkan 1.1 properties */
+      .subgroupSize = 32,
+      .subgroupSupportedStages = nvk_nak_stages(),
+      .subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BALLOT_BIT |
+                                     VK_SUBGROUP_FEATURE_BASIC_BIT |
+                                     VK_SUBGROUP_FEATURE_VOTE_BIT,
+      .subgroupQuadOperationsInAllStages = false,
       .pointClippingBehavior = VK_POINT_CLIPPING_BEHAVIOR_USER_CLIP_PLANES_ONLY,
       .maxMultiviewViewCount = NVK_MAX_MULTIVIEW_VIEW_COUNT,
       .maxMultiviewInstanceIndex = UINT32_MAX,

src/nouveau/vulkan/nvk_shader.h
@@ -96,6 +96,8 @@ nvk_shader_address(const struct nvk_shader *shader)
    return shader->upload_addr + shader->upload_padding;
 }
 
+VkShaderStageFlags nvk_nak_stages(void);
+
 uint64_t
 nvk_physical_device_compiler_flags(const struct nvk_physical_device *pdev);
 

commit f43ef4cacab3af1c6144e7d4a7d3e6da63f65b65

nvk: Limit shader stages to supported stages
Fixes: b82a21aeed33 ("nvk: Enable subgroups features")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26382>

src/nouveau/vulkan/nvk_shader.c
@@ -67,6 +67,14 @@ pipe_shader_type_from_mesa(gl_shader_stage stage)
 VkShaderStageFlags
 nvk_nak_stages(const struct nv_device_info *info)
 {
+   const VkShaderStageFlags all =
+      VK_SHADER_STAGE_VERTEX_BIT |
+      VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT |
+      VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT |
+      VK_SHADER_STAGE_GEOMETRY_BIT |
+      VK_SHADER_STAGE_FRAGMENT_BIT |
+      VK_SHADER_STAGE_COMPUTE_BIT;
+
    const struct debug_control flags[] = {
       { "vs", BITFIELD64_BIT(MESA_SHADER_VERTEX) },
       { "tcs", BITFIELD64_BIT(MESA_SHADER_TESS_CTRL) },
@@ -74,13 +82,13 @@ nvk_nak_stages(const struct nv_device_info *info)
       { "gs", BITFIELD64_BIT(MESA_SHADER_GEOMETRY) },
       { "fs", BITFIELD64_BIT(MESA_SHADER_FRAGMENT) },
       { "cs", BITFIELD64_BIT(MESA_SHADER_COMPUTE) },
-      { "all", ~0 },
+      { "all", all },
       { NULL, 0 },
    };
 
    const char *env_str = getenv("NVK_USE_NAK");
    if (env_str == NULL)
-      return info->cls_eng3d >= TURING_A ? ~0 : 0;
+      return info->cls_eng3d >= TURING_A ? all : 0;
    else
       return parse_debug_string(env_str, flags);
 }
@@ -88,7 +96,7 @@ nvk_nak_stages(const struct nv_device_info *info)
 static bool
 use_nak(const struct nvk_physical_device *pdev, gl_shader_stage stage)
 {
-   return nvk_nak_stages(&pdev->info) & BITFIELD64_BIT(stage);
+   return nvk_nak_stages(&pdev->info) & mesa_to_vk_shader_stage(stage);
 }
 
 uint64_t