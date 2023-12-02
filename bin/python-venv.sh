joshuaswarren/mesa-1

commit 2db78704dd065b7caa74e66f111518a615fbf902

bin/python-venv: detect python version change
The venv only works for a specific python version; when updating python,
the venv needs to be regenerated.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26354>

bin/python-venv.sh
@@ -7,11 +7,28 @@ shift
 venv_dir="$(dirname "$requirements_file")"/.venv
 readonly venv_dir
 readonly venv_req=$venv_dir/requirements.txt
+readonly venv_python_version=$venv_dir/python-version.txt
+
+if [ -d "$venv_dir" ] && [ ! -r "$venv_python_version" ]
+then
+  echo "Python environment predates Python version checks."
+  echo "It might be invalid and needs to be regenerated."
+  rm -rf "$venv_dir"
+elif ! cmp --quiet <(python --version) "$venv_python_version"
+then
+  old=$(cat "$venv_python_version")
+  new=$(python --version)
+  echo "Python version has changed ($old -> $new)."
+  echo "Python environment needs to be regenerated."
+  unset old new
+  rm -rf "$venv_dir"
+fi
 
 if ! [ -r "$venv_dir/bin/activate" ]
 then
   echo "Creating Python environment..."
   python -m venv "$venv_dir"
+  python --version > "$venv_python_version"
 fi
 
 # shellcheck disable=1091

commit fd07364e01ab881af93c24e3ab5c9aa27c570f0d

bin/python-venv: fix venv folder check
My last refactor before merging !26354 was incorrect and broke the
script when use for the first time (or after deleting the venv folder).

Fixes: 2db78704dd065b7caa74 ("bin/python-venv: detect python version change")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26453>

bin/python-venv.sh
@@ -9,19 +9,22 @@ readonly venv_dir
 readonly venv_req=$venv_dir/requirements.txt
 readonly venv_python_version=$venv_dir/python-version.txt
 
-if [ -d "$venv_dir" ] && [ ! -r "$venv_python_version" ]
+if [ -d "$venv_dir" ]
 then
-  echo "Python environment predates Python version checks."
-  echo "It might be invalid and needs to be regenerated."
-  rm -rf "$venv_dir"
-elif ! cmp --quiet <(python --version) "$venv_python_version"
-then
-  old=$(cat "$venv_python_version")
-  new=$(python --version)
-  echo "Python version has changed ($old -> $new)."
-  echo "Python environment needs to be regenerated."
-  unset old new
-  rm -rf "$venv_dir"
+  if [ ! -r "$venv_python_version" ]
+  then
+    echo "Python environment predates Python version checks."
+    echo "It might be invalid and needs to be regenerated."
+    rm -rf "$venv_dir"
+  elif ! cmp --quiet <(python --version) "$venv_python_version"
+  then
+    old=$(cat "$venv_python_version")
+    new=$(python --version)
+    echo "Python version has changed ($old -> $new)."
+    echo "Python environment needs to be regenerated."
+    unset old new
+    rm -rf "$venv_dir"
+  fi
 fi
 
 if ! [ -r "$venv_dir/bin/activate" ]