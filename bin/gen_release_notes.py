joshuaswarren/mesa-1

commit 61c778ef1f3db543db962f0a1cacd7438e5428ea

docs: use version-number as toctree-title for relnotes
Using the full titles makes most of these take up two lines in the
toctree. The version number is really the only thing we should care
about, though.

Reviewed-by: Marcin Ślusarz <marcin.slusarz@intel.com>
Reviewed-by: Eric Engestrom <eric@igalia.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21956>

bin/gen_release_notes.py
@@ -323,9 +323,10 @@ def update_release_notes_index(version: str) -> None:
         if first_list and line.startswith('-'):
             first_list = False
             new_relnotes.append(f'-  :doc:`{version} release notes <relnotes/{version}>`\n')
-        if not first_list and second_list and line.startswith('   relnotes/'):
+        if (not first_list and second_list and
+            re.match('   \d+.\d+(.\d+)? <relnotes/\d+.\d+(.\d+)?>', line)):
             second_list = False
-            new_relnotes.append(f'   relnotes/{version}\n')
+            new_relnotes.append(f'   {version} <relnotes/{version}>\n')
         new_relnotes.append(line)
 
     with relnotes_index_path.open('w') as f:

commit e028b91771cf23e83e57a80c03c9e052d8ac4f92

bin/gen_release_notes: fix regex raw string
Fixes: 61c778ef1f3db543db96 ("docs: use version-number as toctree-title for relnotes")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26468>

bin/gen_release_notes.py
@@ -325,7 +325,7 @@ def update_release_notes_index(version: str) -> None:
             first_list = False
             new_relnotes.append(f'-  :doc:`{version} release notes <relnotes/{version}>`\n')
         if (not first_list and second_list and
-            re.match('   \d+.\d+(.\d+)? <relnotes/\d+.\d+(.\d+)?>', line)):
+            re.match(r'   \d+.\d+(.\d+)? <relnotes/\d+.\d+(.\d+)?>', line)):
             second_list = False
             new_relnotes.append(f'   {version} <relnotes/{version}>\n')
         new_relnotes.append(line)