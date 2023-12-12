joshuaswarren/mesa-1

commit b6ca052dbe3662e7280e8e2242f6db577c2d4a36

nak: Implement fmulz and ffmaz
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10261
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26569>

src/nouveau/compiler/nak/from_nir.rs
@@ -703,7 +703,7 @@ impl<'a> ShaderFromNir<'a> {
                 dst
             }
             nir_op_fexp2 => b.mufu(MuFuOp::Exp2, srcs[0]),
-            nir_op_ffma => {
+            nir_op_ffma | nir_op_ffmaz => {
                 let ftype = FloatType::from_bits(alu.def.bit_size().into());
                 assert!(alu.def.bit_size() == 32);
                 let dst = b.alloc_ssa(RegFile::GPR, 1);
@@ -713,7 +713,7 @@ impl<'a> ShaderFromNir<'a> {
                     saturate: self.try_saturate_alu_dst(&alu.def),
                     rnd_mode: self.float_ctl[ftype].rnd_mode,
                     ftz: self.float_ctl[ftype].ftz,
-                    dnz: false,
+                    dnz: alu.op == nir_op_ffmaz,
                 });
                 dst
             }
@@ -732,7 +732,7 @@ impl<'a> ShaderFromNir<'a> {
                 });
                 dst
             }
-            nir_op_fmul => {
+            nir_op_fmul | nir_op_fmulz => {
                 let ftype = FloatType::from_bits(alu.def.bit_size().into());
                 assert!(alu.def.bit_size() == 32);
                 let dst = b.alloc_ssa(RegFile::GPR, 1);
@@ -742,7 +742,7 @@ impl<'a> ShaderFromNir<'a> {
                     saturate: self.try_saturate_alu_dst(&alu.def),
                     rnd_mode: self.float_ctl[ftype].rnd_mode,
                     ftz: self.float_ctl[ftype].ftz,
-                    dnz: false,
+                    dnz: alu.op == nir_op_fmulz,
                 });
                 dst
             }

src/nouveau/compiler/nak/api.rs
@@ -123,6 +123,10 @@ fn nir_options(dev: &nv_device_info) -> nir_shader_compiler_options {
     op.has_sudot_4x8 = dev.sm >= 70;
     op.max_unroll_iterations = 32;
 
+    // We set .ftz on f32 by default so we can support fmulz whenever the client
+    // doesn't explicitly request denorms.
+    op.has_fmulz_no_denorms = true;
+
     op
 }
 

commit a635ad46c5b796bf79f73462f18782bdf53ed368

nak: Don't set both FTZ and DNZ at the same time
Fixes: b6ca052dbe36 ("nak: Implement fmulz and ffmaz")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26654>

src/nouveau/compiler/nak/from_nir.rs
@@ -712,7 +712,9 @@ impl<'a> ShaderFromNir<'a> {
                     srcs: [srcs[0], srcs[1], srcs[2]],
                     saturate: self.try_saturate_alu_dst(&alu.def),
                     rnd_mode: self.float_ctl[ftype].rnd_mode,
-                    ftz: self.float_ctl[ftype].ftz,
+                    // The hardware doesn't like FTZ+DNZ and DNZ implies FTZ
+                    // anyway so only set one of the two bits.
+                    ftz: alu.op != nir_op_ffmaz && self.float_ctl[ftype].ftz,
                     dnz: alu.op == nir_op_ffmaz,
                 });
                 dst
@@ -741,7 +743,9 @@ impl<'a> ShaderFromNir<'a> {
                     srcs: [srcs[0], srcs[1]],
                     saturate: self.try_saturate_alu_dst(&alu.def),
                     rnd_mode: self.float_ctl[ftype].rnd_mode,
-                    ftz: self.float_ctl[ftype].ftz,
+                    // The hardware doesn't like FTZ+DNZ and DNZ implies FTZ
+                    // anyway so only set one of the two bits.
+                    ftz: alu.op != nir_op_fmulz && self.float_ctl[ftype].ftz,
                     dnz: alu.op == nir_op_fmulz,
                 });
                 dst