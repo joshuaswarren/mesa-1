joshuaswarren/mesa-1

commit c8d259d9a9b231e86f1065c6058da0a3910e18e0

d3d12: Fix screen->winsys leak in d3d12_screen
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26456>

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -765,6 +765,10 @@ d3d12_deinit_screen(struct d3d12_screen *screen)
       screen->dev->Release();
       screen->dev = nullptr;
    }
+   if (screen->winsys) {
+      screen->winsys->destroy(screen->winsys);
+      screen->winsys = nullptr;
+   }
 }
 
 void

commit 7feba673e1d2788864bf30667b0a67a656c5abd0

d3d12: Only destroy the winsys during screen destruction, not reset
Fixes: c8d259d9 ("d3d12: Fix screen->winsys leak in d3d12_screen")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26744>

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -766,15 +766,15 @@ d3d12_deinit_screen(struct d3d12_screen *screen)
       screen->dev->Release();
       screen->dev = nullptr;
    }
-   if (screen->winsys) {
-      screen->winsys->destroy(screen->winsys);
-      screen->winsys = nullptr;
-   }
 }
 
 void
 d3d12_destroy_screen(struct d3d12_screen *screen)
 {
+   if (screen->winsys) {
+      screen->winsys->destroy(screen->winsys);
+      screen->winsys = nullptr;
+   }
    slab_destroy_parent(&screen->transfer_pool);
    mtx_destroy(&screen->submit_mutex);
    mtx_destroy(&screen->descriptor_pool_mutex);