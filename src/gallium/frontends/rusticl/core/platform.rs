joshuaswarren/mesa-1

commit eb1a8f2d558fa837a09684ae0b6ef789a68e47bd

rusticl/platform: add RUSTICL_FEATURES boilerplate
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22649>

src/gallium/frontends/rusticl/core/platform.rs
@@ -21,6 +21,8 @@ pub struct PlatformDebug {
     pub program: bool,
 }
 
+pub struct PlatformFeatures {}
+
 static PLATFORM_ENV_ONCE: Once = Once::new();
 static PLATFORM_ONCE: Once = Once::new();
 
@@ -33,6 +35,7 @@ static mut PLATFORM: Platform = Platform {
     devs: Vec::new(),
 };
 static mut PLATFORM_DBG: PlatformDebug = PlatformDebug { program: false };
+static mut PLATFORM_FEATURES: PlatformFeatures = PlatformFeatures {};
 
 fn load_env() {
     let debug = unsafe { &mut PLATFORM_DBG };
@@ -41,6 +44,15 @@ fn load_env() {
             match flag {
                 "program" => debug.program = true,
                 _ => eprintln!("Unknown RUSTICL_DEBUG flag found: {}", flag),
+            }
+        }
+    }
+
+    let features = unsafe { &mut PLATFORM_FEATURES };
+    if let Ok(feature_flags) = env::var("RUSTICL_FEATURES") {
+        for flag in feature_flags.split(',') {
+            match flag {
+                _ => eprintln!("Unknown RUSTICL_FEATURES flag found: {}", flag),
             }
         }
     }
@@ -60,6 +72,11 @@ impl Platform {
     pub fn dbg() -> &'static PlatformDebug {
         debug_assert!(PLATFORM_ENV_ONCE.is_completed());
         unsafe { &PLATFORM_DBG }
+    }
+
+    pub fn features() -> &'static PlatformFeatures {
+        debug_assert!(PLATFORM_ENV_ONCE.is_completed());
+        unsafe { &PLATFORM_FEATURES }
     }
 
     fn init(&mut self) {

commit dc2f1d3dbdd65735bb816c493a713271722a912b

rusticl: do not warn on empty RUSTICL_DEBUG or RUSTICL_FEATURES
Fixes: eb1a8f2d558 ("rusticl/platform: add RUSTICL_FEATURES boilerplate")
Fixes: d6ce7527f56 ("rusticl/program: allow dumping compilation logs through RUSTICL_DEBUG")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26764>

src/gallium/frontends/rusticl/core/platform.rs
@@ -79,6 +79,7 @@ fn load_env() {
                 "clc" => debug.clc = true,
                 "program" => debug.program = true,
                 "sync" => debug.sync_every_event = true,
+                "" => (),
                 _ => eprintln!("Unknown RUSTICL_DEBUG flag found: {}", flag),
             }
         }
@@ -90,6 +91,7 @@ fn load_env() {
             match flag {
                 "fp16" => features.fp16 = true,
                 "fp64" => features.fp64 = true,
+                "" => (),
                 _ => eprintln!("Unknown RUSTICL_FEATURES flag found: {}", flag),
             }
         }