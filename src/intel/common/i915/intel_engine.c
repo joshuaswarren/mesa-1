joshuaswarren/mesa-1

commit dba6ec9e28ea562ae1e2df0f27e3af8ddb057990

intel/common: Move i915 files to i915 folder
Following the organization done in intel/dev and intel/vulkan.

Probably due to some rebase issue we had a duplicated copyright header
in intel_gem_i915.h that is being removed in here too.

Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Marcin Ślusarz <marcin.slusarz@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21256>

src/intel/common/i915/intel_engine.c
@@ -21,7 +21,7 @@
  * IN THE SOFTWARE.
  */
 
-#include "intel_engine_i915.h"
+#include "i915/intel_engine.h"
 
 #include <stdlib.h>
 

src/intel/common/intel_engine.c
@@ -26,7 +26,7 @@
 #include "util/macros.h"
 
 #include "intel_engine.h"
-#include "intel_engine_i915.h"
+#include "i915/intel_engine.h"
 
 struct intel_query_engine_info *
 intel_engine_get_info(int fd, enum intel_kmd_type type)

src/intel/common/i915/intel_engine.h
@@ -23,7 +23,7 @@
 
 #pragma once
 
-#include "intel_engine.h"
+#include "common/intel_engine.h"
 
 #include "drm-uapi/i915_drm.h"
 

src/intel/common/i915/intel_gem.c
@@ -21,10 +21,10 @@
  * IN THE SOFTWARE.
  */
 
-#include "intel_gem_i915.h"
+#include "i915/intel_gem.h"
 
-#include "intel_gem.h"
-#include "intel_engine_i915.h"
+#include "common/intel_gem.h"
+#include "i915/intel_engine.h"
 
 #include "drm-uapi/i915_drm.h"
 

src/intel/common/i915/intel_gem.h
@@ -26,31 +26,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/*
- * Copyright © 2022 Intel Corporation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
-#include "intel_gem_i915.h"
-#include "intel_gem.h"
+#include "common/intel_gem.h"
 
 #include "drm-uapi/i915_drm.h"
 

src/intel/common/intel_gem.c
@@ -23,8 +23,8 @@
 #include "intel_gem.h"
 #include "drm-uapi/i915_drm.h"
 
-#include "intel_engine_i915.h"
-#include "intel_gem_i915.h"
+#include "i915/intel_engine.h"
+#include "i915/intel_gem.h"
 
 bool
 intel_gem_supports_syncobj_wait(int fd)

src/intel/common/meson.build
@@ -19,6 +19,10 @@
 # SOFTWARE.
 
 files_libintel_common = files(
+  'i915/intel_engine.c',
+  'i915/intel_engine.h',
+  'i915/intel_gem.c',
+  'i915/intel_gem.h',
   'intel_aux_map.c',
   'intel_aux_map.h',
   'intel_buffer_alloc.h',
@@ -26,12 +30,8 @@ files_libintel_common = files(
   'intel_decoder.h',
   'intel_disasm.c',
   'intel_disasm.h',
-  'intel_engine_i915.c',
-  'intel_engine_i915.h',
   'intel_engine.c',
   'intel_engine.h',
-  'intel_gem_i915.c',
-  'intel_gem_i915.h',
   'intel_gem.c',
   'intel_gem.h',
   'intel_genX_state.h',

commit f0d1015565bfbd3cfd4b27e19fbf2c1987fbe6c4

intel/common: add directory prefix to intel_gem.h include
Otherwise common/intel_gem.h may get included instead of
common/i915/intel_gem.h when building in a different directory.

Fixes: dba6ec9e28e ("intel/common: Move i915 files to i915 folder")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26784>

src/intel/common/i915/intel_engine.c
@@ -25,7 +25,7 @@
 
 #include <stdlib.h>
 
-#include "intel_gem.h"
+#include "i915/intel_gem.h"
 
 static enum intel_engine_class
 i915_engine_class_to_intel(enum drm_i915_gem_engine_class i915)