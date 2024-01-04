joshuaswarren/mesa-1

commit 0c7323e77c0edc76a700fae7786ac742ffee253e

intel/compiler: Move earlier scheduler code that is not mode-specific
This will be useful later on when we reuse the same scheduler for
multiple modes.

Reviewed-by: Matt Turner <mattst88@gmail.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25841>

src/intel/compiler/brw_schedule_instructions.cpp
@@ -815,6 +815,8 @@ fs_instruction_scheduler::fs_instruction_scheduler(const fs_visitor *v,
          this->hw_liveout[i] = linear_zalloc_array(lin_ctx, BITSET_WORD,
                                              BITSET_WORDS(hw_reg_count));
 
+      setup_liveness(v->cfg);
+
       this->written = linear_alloc_array(lin_ctx, bool, grf_count);
 
       this->reads_remaining = linear_alloc_array(lin_ctx, int, grf_count);
@@ -828,6 +830,17 @@ fs_instruction_scheduler::fs_instruction_scheduler(const fs_visitor *v,
       this->written = NULL;
       this->reads_remaining = NULL;
       this->hw_reads_remaining = NULL;
+   }
+
+   foreach_block(block, v->cfg) {
+      set_current_block(block);
+
+      for (schedule_node *n = current.start; n < current.end; n++)
+         n->issue_time = calculate_issue_time(n->inst);
+
+      calculate_deps();
+      compute_delays();
+      compute_exits();
    }
 }
 
@@ -1937,8 +1950,6 @@ fs_instruction_scheduler::run()
    }
 
    if (!post_reg_alloc) {
-      setup_liveness(v->cfg);
-
       memset(reads_remaining, 0, grf_count * sizeof(*reads_remaining));
       memset(hw_reads_remaining, 0, hw_reg_count * sizeof(*hw_reads_remaining));
       memset(written, 0, grf_count * sizeof(*written));
@@ -1947,16 +1958,10 @@ fs_instruction_scheduler::run()
    foreach_block(block, v->cfg) {
       set_current_block(block);
 
-      for (schedule_node *n = current.start; n < current.end; n++) {
-         if (!post_reg_alloc)
+      if (!post_reg_alloc) {
+         for (schedule_node *n = current.start; n < current.end; n++)
             count_reads_remaining(n->inst);
-         n->issue_time = calculate_issue_time(n->inst);
       }
-
-      calculate_deps();
-
-      compute_delays();
-      compute_exits();
 
       schedule_instructions();
    }
@@ -1978,11 +1983,6 @@ vec4_instruction_scheduler::run()
          /* We always execute as two vec4s in parallel. */
          n->issue_time = 2;
       }
-
-      calculate_deps();
-
-      compute_delays();
-      compute_exits();
 
       /* Add DAG heads to the list of available instructions. */
       assert(current.available.is_empty());

commit c4f1a9dbc01daff0be8cd292ee7f197a73c731af

intel/compiler: revert part of "Move earlier scheduler code that is not mode-specific"
This removed a bunch of calls from the vec4 code that aren't called anywhere else.

Bring back the bits that were removed.

Fixes glxgears on gen5

Fixes: 0c7323e77c0ed ("intel/compiler: Move earlier scheduler code that is not mode-specific")
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26862>

src/intel/compiler/brw_schedule_instructions.cpp
@@ -2019,6 +2019,11 @@ vec4_instruction_scheduler::run()
          n->issue_time = 2;
       }
 
+      calculate_deps();
+
+      compute_delays();
+      compute_exits();
+
       assert(current.available.is_empty());
       for (schedule_node *n = current.start; n < current.end; n++) {
          reset_node_tmp(n);