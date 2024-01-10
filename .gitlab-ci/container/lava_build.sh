joshuaswarren/mesa-1

commit 304a53cd4e85d9ed1fa0babfa0e77bedf60ab065

ci: enable ci-deb-repo for libdrm 2.4.119 (and others in the future)
Reviewed-by: Eric Engestrom <eric@igalia.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26814>

.gitlab-ci/container/lava_build.sh
@@ -147,6 +147,8 @@ CONTAINER_EPHEMERAL=(
     zstd
 )
 
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
+
 apt-get update
 apt-get install -y --no-remove \
 		   -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' \

.gitlab-ci/image-tags.yml
@@ -7,7 +7,7 @@
 
 variables:
    DEBIAN_X86_64_BUILD_BASE_IMAGE: "debian/x86_64_build-base"
-   DEBIAN_BASE_TAG: "2024-01-05-meson-bump"
+   DEBIAN_BASE_TAG: "2024-01-06-libdrm"
 
    DEBIAN_X86_64_BUILD_IMAGE_PATH: "debian/x86_64_build"
    DEBIAN_BUILD_TAG: "2023-12-27-libdrm"
@@ -25,9 +25,10 @@ variables:
    ALPINE_X86_64_BUILD_TAG: "2023-10-30-ci-improv"
    ALPINE_X86_64_LAVA_SSH_TAG: "2023-06-26-first-version"
    FEDORA_X86_64_BUILD_TAG: "2024-01-05-meson-bump"
-   KERNEL_ROOTFS_TAG: "2023-12-22-vkcts"
+   KERNEL_ROOTFS_TAG: "2024-01-06-libdrm"
    KERNEL_TAG: "v6.6.4-for-mesa-ci-e4f4c500f7fb"
    KERNEL_REPO: "gfx-ci/linux"
+   PKG_REPO_REV: "67f2c46b"
 
    WINDOWS_X64_MSVC_PATH: "windows/x86_64_msvc"
    WINDOWS_X64_MSVC_TAG: "2023-12-22-msvc"

.gitlab-ci/container/cross_build.sh
@@ -40,6 +40,9 @@ DEPS=(
 )
 
 dpkg --add-architecture $arch
+
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
+
 apt-get update
 
 apt-get install -y --no-remove "${DEPS[@]}" "${EPHEMERAL[@]}" \
@@ -72,8 +75,6 @@ fi
 # dependencies where we want a specific version
 MULTIARCH_PATH=$(dpkg-architecture -A $arch -qDEB_TARGET_MULTIARCH)
 export EXTRA_MESON_ARGS="--cross-file=/cross_file-${arch}.txt -D libdir=lib/${MULTIARCH_PATH}"
-. .gitlab-ci/container/build-libdrm.sh
-
 . .gitlab-ci/container/build-wayland.sh
 
 . .gitlab-ci/container/build-directx-headers.sh

.gitlab-ci/container/debian/arm64_build.sh
@@ -8,6 +8,7 @@ export LLVM_VERSION="${LLVM_VERSION:=15}"
 
 apt-get -y install ca-certificates
 sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list.d/*
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
 apt-get update
 
 # Ephemeral packages (installed for this script and removed again at the end)

.gitlab-ci/container/debian/arm_test.sh
@@ -28,6 +28,7 @@ DEPS=(
 
 apt-get install -y ca-certificates
 sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list.d/*
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
 apt-get update
 
 apt-get install -y --no-remove "${DEPS[@]}"

.gitlab-ci/container/debian/x86_64_build-base.sh
@@ -13,6 +13,7 @@ export LLVM_VERSION="${LLVM_VERSION:=15}"
 
 apt-get install -y ca-certificates
 sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list.d/*
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
 
 # Ephemeral packages (installed for this script and removed again at
 # the end)

.gitlab-ci/container/debian/x86_64_test-base.sh
@@ -14,6 +14,8 @@ apt-get install -y ca-certificates gnupg2 software-properties-common
 
 sed -i -e 's/http:\/\/deb/https:\/\/deb/g' /etc/apt/sources.list.d/*
 
+echo "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main" | tee /etc/apt/sources.list.d/gfx-ci_.list
+
 export LLVM_VERSION="${LLVM_VERSION:=15}"
 
 # Ephemeral packages (installed for this script and removed again at the end)

.gitlab-ci/container/gitlab-ci.yml
@@ -63,7 +63,7 @@ debian/x86_64_build-base:
     - .fdo.container-build@debian
     - .container
   variables:
-    MESA_IMAGE_TAG: &debian-x86_64_build-base ${DEBIAN_BASE_TAG}
+    MESA_IMAGE_TAG: &debian-x86_64_build-base "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
 
 .use-debian/x86_64_build-base:
   extends:
@@ -171,7 +171,7 @@ debian/android_build:
 debian/x86_64_test-base:
   extends: debian/x86_64_build-base
   variables:
-    MESA_IMAGE_TAG: &debian-x86_64_test-base "${DEBIAN_BASE_TAG}--${KERNEL_TAG}"
+    MESA_IMAGE_TAG: &debian-x86_64_test-base "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}--${KERNEL_TAG}"
 
 .use-debian/x86_64_test-base:
   extends:
@@ -240,7 +240,7 @@ debian/arm64_build:
   tags:
     - aarch64
   variables:
-    MESA_IMAGE_TAG: &debian-arm64_build ${DEBIAN_BASE_TAG}
+    MESA_IMAGE_TAG: &debian-arm64_build "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
 
 .use-debian/arm64_build:
   extends:
@@ -369,7 +369,7 @@ debian/arm32_test:
   needs:
     - kernel+rootfs_arm32
   variables:
-    MESA_IMAGE_TAG: &debian-arm32_test ${DEBIAN_BASE_TAG}
+    MESA_IMAGE_TAG: &debian-arm32_test "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
 
 debian/arm64_test:
   extends:
@@ -377,7 +377,7 @@ debian/arm64_test:
   needs:
     - kernel+rootfs_arm64
   variables:
-    MESA_IMAGE_TAG: &debian-arm64_test ${DEBIAN_BASE_TAG}
+    MESA_IMAGE_TAG: &debian-arm64_test "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
 
 .use-debian/arm_test:
   variables:

commit e1761657c7a091562a297daf32f7cdf8baf0efbf

ci/rootfs: add libdrm also inside the rootfs
Fixes: 304a53cd4e85 ("ci: enable ci-deb-repo for libdrm 2.4.119 (and others in the future)")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26971>

.gitlab-ci/container/lava_build.sh
@@ -219,7 +219,8 @@ mmdebstrap \
     --include "${PKG_BASE[*]} ${PKG_CI[*]} ${PKG_DEP[*]} ${PKG_MESA_DEP[*]} ${PKG_ARCH[*]}" \
     bookworm \
     "$ROOTFS/" \
-    "http://deb.debian.org/debian"
+    "http://deb.debian.org/debian" \
+    "deb [trusted=yes] https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo/-/raw/${PKG_REPO_REV}/ ${FDO_DISTRIBUTION_VERSION%-*} main"
 
 ############### Install mold
 . .gitlab-ci/container/build-mold.sh

.gitlab-ci/image-tags.yml
@@ -25,7 +25,7 @@ variables:
    ALPINE_X86_64_BUILD_TAG: "2023-01-07-libdrm2_4_119"
    ALPINE_X86_64_LAVA_SSH_TAG: "2023-06-26-first-version"
    FEDORA_X86_64_BUILD_TAG: "2024-01-06-libdrm"
-   KERNEL_ROOTFS_TAG: "2024-01-10-deb-fix"
+   KERNEL_ROOTFS_TAG: "2024-01-10-libdrm"
    KERNEL_TAG: "v6.6.4-for-mesa-ci-e4f4c500f7fb"
    KERNEL_REPO: "gfx-ci/linux"
    PKG_REPO_REV: "67f2c46b"