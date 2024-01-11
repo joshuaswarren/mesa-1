joshuaswarren/mesa-1

commit 5a3192fbf80bf7d37cf50ad623bed79815af2148

zink: handle patch variable locations for separate shaders better
these don't overlap with other locations so they can keep whatever their
current assignments are

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24757>

src/gallium/drivers/zink/zink_compiler.c
@@ -4833,6 +4833,8 @@ fixup_io_locations(nir_shader *nir)
                continue;
             if (var->data.location == VARYING_SLOT_VAR0)
                var->data.driver_location = 0;
+            else if (var->data.patch)
+               var->data.driver_location = var->data.location - VARYING_SLOT_VAR0;
             else
                var->data.driver_location = var->data.location;
          }
@@ -4858,7 +4860,10 @@ fixup_io_locations(nir_shader *nir)
                else
                   size += glsl_count_vec4_slots(var->type, false, false);
             }
-            var->data.driver_location = slot;
+            if (var->data.patch)
+               var->data.driver_location = var->data.location - VARYING_SLOT_VAR0;
+            else
+               var->data.driver_location = slot;
             found = true;
          }
          slot += size;

commit 567da5991078ae483aabd8ce5b0853b4672f0f5f

zink: fix separate shader patch variable location adjustment
in spirv, these start at location 0, not location 32

fixes #10414

Fixes: 5a3192fbf80 ("zink: handle patch variable locations for separate shaders better")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26981>

src/gallium/drivers/zink/zink_compiler.c
@@ -4909,7 +4909,7 @@ fixup_io_locations(nir_shader *nir)
             if (var->data.location == VARYING_SLOT_VAR0)
                var->data.driver_location = 0;
             else if (var->data.patch)
-               var->data.driver_location = var->data.location - VARYING_SLOT_VAR0;
+               var->data.driver_location = var->data.location - VARYING_SLOT_PATCH0;
             else
                var->data.driver_location = var->data.location;
          }
@@ -4936,7 +4936,7 @@ fixup_io_locations(nir_shader *nir)
                   size += glsl_count_vec4_slots(var->type, false, false);
             }
             if (var->data.patch)
-               var->data.driver_location = var->data.location - VARYING_SLOT_VAR0;
+               var->data.driver_location = var->data.location - VARYING_SLOT_PATCH0;
             else
                var->data.driver_location = slot;
             found = true;