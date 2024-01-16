joshuaswarren/mesa-1

commit 7e69316cffea0a2f45a56fd8c9597be696119146

ci: uprev deqp-runner from 0.16.1 to 0.18.0
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26270>

.gitlab-ci/container/build-deqp-runner.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2086 # we want word splitting
 
+# When changing this file, you need to bump the following
+# .gitlab-ci/image-tags.yml tags:
+# DEBIAN_BASE_TAG
+# DEBIAN_X86_64_TEST_ANDROID_TAG
+# KERNEL_ROOTFS_TAG
+
 set -ex
 
 if [ -n "${DEQP_RUNNER_GIT_TAG}${DEQP_RUNNER_GIT_REV}" ]; then
@@ -27,7 +33,7 @@ if [ -z "$ANDROID_NDK_HOME" ]; then
 else
     mkdir -p /deqp-runner
     pushd /deqp-runner
-    git clone --branch v0.16.1 --depth 1 https://gitlab.freedesktop.org/anholt/deqp-runner.git deqp-runner-git
+    git clone --branch v0.18.0 --depth 1 https://gitlab.freedesktop.org/anholt/deqp-runner.git deqp-runner-git
     pushd deqp-runner-git
 
     cargo install --locked  \

.gitlab-ci/image-tags.yml
@@ -7,7 +7,7 @@
 
 variables:
    DEBIAN_X86_64_BUILD_BASE_IMAGE: "debian/x86_64_build-base"
-   DEBIAN_BASE_TAG: "2023-11-13-nak-v2"
+   DEBIAN_BASE_TAG: "2023-11-18-deqp-runner"
 
    DEBIAN_X86_64_BUILD_IMAGE_PATH: "debian/x86_64_build"
    DEBIAN_BUILD_TAG: "2023-10-30-ci-improv"
@@ -18,14 +18,14 @@ variables:
    DEBIAN_X86_64_TEST_IMAGE_VK_PATH: "debian/x86_64_test-vk"
    DEBIAN_X86_64_TEST_ANDROID_IMAGE_PATH: "debian/x86_64_test-android"
 
-   DEBIAN_X86_64_TEST_ANDROID_TAG: "2023-11-06-deqp-fixes"
+   DEBIAN_X86_64_TEST_ANDROID_TAG: "2023-11-18-deqp-runner"
    DEBIAN_X86_64_TEST_GL_TAG: "2023-11-06-deqp-fixes"
    DEBIAN_X86_64_TEST_VK_TAG: "2023-11-06-deqp-fixes"
 
    ALPINE_X86_64_BUILD_TAG: "2023-10-30-ci-improv"
    ALPINE_X86_64_LAVA_SSH_TAG: "2023-06-26-first-version"
    FEDORA_X86_64_BUILD_TAG: "2023-10-30-ci-improv"
-   KERNEL_ROOTFS_TAG: "2023-11-06-deqp-fixes"
+   KERNEL_ROOTFS_TAG: "2023-11-18-deqp-runner"
    KERNEL_TAG: "v6.4.12-for-mesa-ci-f6b4ad45f48d"
    KERNEL_REPO: "gfx-ci/linux"
 

commit 8c8fe0c86a3d160ba11c57ad0da055ee7ee6b187

ci/deqp: uprev deqp-runner for Linux too to 0.18.0
Previous commit upreved deqp only for the Android

Fixes: 7e69316cffea ("ci: uprev deqp-runner from 0.16.1 to 0.18.0")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>

[Eric]
- rename the deqp-runner version to DEQP_RUNNER_VERSION instead of DEQP_VERSION
- update image tags
- fix expectations lists

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27062>

.gitlab-ci/container/build-deqp-runner.sh
@@ -9,6 +9,8 @@
 
 set -ex
 
+DEQP_RUNNER_VERSION=0.18.0
+
 if [ -n "${DEQP_RUNNER_GIT_TAG}${DEQP_RUNNER_GIT_REV}" ]; then
     # Build and install from source
     DEQP_RUNNER_CARGO_ARGS="--git ${DEQP_RUNNER_GIT_URL:-https://gitlab.freedesktop.org/anholt/deqp-runner.git}"
@@ -22,7 +24,7 @@ if [ -n "${DEQP_RUNNER_GIT_TAG}${DEQP_RUNNER_GIT_REV}" ]; then
     DEQP_RUNNER_CARGO_ARGS="${DEQP_RUNNER_CARGO_ARGS} ${EXTRA_CARGO_ARGS}"
 else
     # Install from package registry
-    DEQP_RUNNER_CARGO_ARGS="--version 0.16.0 ${EXTRA_CARGO_ARGS} -- deqp-runner"
+    DEQP_RUNNER_CARGO_ARGS="--version ${DEQP_RUNNER_VERSION} ${EXTRA_CARGO_ARGS} -- deqp-runner"
 fi
 
 if [ -z "$ANDROID_NDK_HOME" ]; then
@@ -33,7 +35,7 @@ if [ -z "$ANDROID_NDK_HOME" ]; then
 else
     mkdir -p /deqp-runner
     pushd /deqp-runner
-    git clone --branch v0.18.0 --depth 1 https://gitlab.freedesktop.org/anholt/deqp-runner.git deqp-runner-git
+    git clone --branch v${DEQP_RUNNER_VERSION} --depth 1 https://gitlab.freedesktop.org/anholt/deqp-runner.git deqp-runner-git
     pushd deqp-runner-git
 
     cargo install --locked  \

.gitlab-ci/image-tags.yml
@@ -7,7 +7,7 @@
 
 variables:
    DEBIAN_X86_64_BUILD_BASE_IMAGE: "debian/x86_64_build-base"
-   DEBIAN_BASE_TAG: "2024-01-10-deb-fix"
+   DEBIAN_BASE_TAG: "2024-01-14-runner"
 
    DEBIAN_X86_64_BUILD_IMAGE_PATH: "debian/x86_64_build"
    DEBIAN_BUILD_TAG: "2024-01-04-find"
@@ -18,14 +18,14 @@ variables:
    DEBIAN_X86_64_TEST_IMAGE_VK_PATH: "debian/x86_64_test-vk"
    DEBIAN_X86_64_TEST_ANDROID_IMAGE_PATH: "debian/x86_64_test-android"
 
-   DEBIAN_X86_64_TEST_ANDROID_TAG: "2024-01-14-check-deqp"
+   DEBIAN_X86_64_TEST_ANDROID_TAG: "2024-01-14-runner"
    DEBIAN_X86_64_TEST_GL_TAG: "2024-01-14-check-deqp"
    DEBIAN_X86_64_TEST_VK_TAG: "2024-01-14-check-deqp"
 
    ALPINE_X86_64_BUILD_TAG: "2023-01-07-libdrm2_4_119"
    ALPINE_X86_64_LAVA_SSH_TAG: "2023-06-26-first-version"
    FEDORA_X86_64_BUILD_TAG: "2024-01-06-libdrm"
-   KERNEL_ROOTFS_TAG: "2024-01-14-check-deqp"
+   KERNEL_ROOTFS_TAG: "2024-01-14-runner"
    KERNEL_TAG: "v6.6.4-for-mesa-ci-e4f4c500f7fb"
    KERNEL_REPO: "gfx-ci/linux"
    PKG_REPO_REV: "67f2c46b"