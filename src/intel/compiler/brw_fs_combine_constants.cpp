joshuaswarren/mesa-1

commit 00e4e5f1ba21e67af25e8a336661ba0626d3bde7

intel/fs: Don't add MOV instructions to DO blocks in combine constants
There was a subtle bug related to CFG tracking. Namely, some branch
instructions may point *only* to the block after the DO instruction
for the loop. If the MOV instructions are in the DO block, the may not
have liveness properly tracked.

Like in !25132, having the MOV instructions in blocks that might
contain other instructions helps scheduling.

shader-db:

All Broadwell and newer Intel GPUs had similar results (Ice Lake shown)
total cycles in shared programs: 848577248 -> 848557268 (<.01%)
cycles in affected programs: 78256396 -> 78236416 (-0.03%)
helped: 361 / HURT: 18

fossil-db:

All Skylake and newer Intel GPUs had similar results (Ice Lake shown)
Totals:
Cycles: 15021501924 -> 15021372904 (-0.00%); split: -0.00%, +0.00%

Totals from 735 (0.11% of 656080) affected shaders:
Cycles: 676429502 -> 676300482 (-0.02%); split: -0.02%, +0.00%

Reviewed-by: Francisco Jerez <currojerez@riseup.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26439>

src/intel/compiler/brw_fs_combine_constants.cpp
@@ -1591,14 +1591,61 @@ fs_visitor::opt_combine_constants()
       }
    }
 
+   bool rebuild_cfg = false;
+
    /* Insert MOVs to load the constant values into GRFs. */
    for (int i = 0; i < table.len; i++) {
       struct imm *imm = &table.imm[i];
+
       /* Insert it either before the instruction that generated the immediate
        * or after the last non-control flow instruction of the common ancestor.
        */
-      exec_node *n = (imm->inst ? imm->inst :
-                      imm->block->last_non_control_flow_inst()->next);
+      exec_node *n;
+      bblock_t *insert_block;
+      if (imm->inst != nullptr) {
+         n = imm->inst;
+         insert_block = imm->block;
+      } else {
+         if (imm->block->start()->opcode == BRW_OPCODE_DO) {
+            /* DO blocks are weird. They can contain only the single DO
+             * instruction. As a result, MOV instructions cannot be added to
+             * the DO block.
+             */
+            bblock_t *next_block = imm->block->next();
+            if (next_block->starts_with_control_flow()) {
+               /* This is the difficult case. This occurs for code like
+                *
+                *    do {
+                *       do {
+                *          ...
+                *       } while (...);
+                *    } while (...);
+                *
+                * when the MOV instructions need to be inserted between the
+                * two DO instructions.
+                *
+                * To properly handle this scenario, a new block would need to
+                * be inserted. Doing so would require modifying arbitrary many
+                * CONTINUE, BREAK, and WHILE instructions to point to the new
+                * block.
+                *
+                * It is unlikely that this would ever be correct. Instead,
+                * insert the MOV instructions in the known wrong place and
+                * rebuild the CFG at the end of the pass.
+                */
+               insert_block = imm->block;
+               n = insert_block->last_non_control_flow_inst()->next;
+
+               rebuild_cfg = true;
+            } else {
+               insert_block = next_block;
+               n = insert_block->start();
+            }
+         } else {
+            insert_block = imm->block;
+            n = insert_block->last_non_control_flow_inst()->next;
+         }
+      }
 
       /* From the BDW and CHV PRM, 3D Media GPGPU, Special Restrictions:
        *
@@ -1613,7 +1660,7 @@ fs_visitor::opt_combine_constants()
        * both HF slots within a DWord with the constant.
        */
       const uint32_t width = devinfo->ver == 8 && imm->is_half_float ? 2 : 1;
-      const fs_builder ibld = bld.at(imm->block, n).exec_all().group(width, 0);
+      const fs_builder ibld = bld.at(insert_block, n).exec_all().group(width, 0);
 
       fs_reg reg(VGRF, imm->nr);
       reg.offset = imm->subreg_offset;
@@ -1785,8 +1832,16 @@ fs_visitor::opt_combine_constants()
       }
    }
 
+   if (rebuild_cfg) {
+      delete cfg;
+      cfg = NULL;
+      calculate_cfg();
+   }
+
    ralloc_free(const_ctx);
-   invalidate_analysis(DEPENDENCY_INSTRUCTIONS | DEPENDENCY_VARIABLES);
+
+   invalidate_analysis(DEPENDENCY_INSTRUCTIONS | DEPENDENCY_VARIABLES |
+                       (rebuild_cfg ? DEPENDENCY_BLOCKS : DEPENDENCY_NOTHING));
 
    return true;
 }

commit bdac401340e1d71d4686ac230e264e3417e48aa5

intel/compiler: Fix rebuilding the CFG in fs_combine_constants
When building the CFG the instructions are taken of the list in
fs_visitor and added to the lists inside each block.  The single
"exec_node" in the instruction is used for those memberships.

In the case the pass rebuilt the CFG, it had no instructions, so
calculate_cfg() had nothing to work with.  For now fix the bug by
pulling all the instructions back to the original list.

We can do better here, but punting until upcoming work on
CFG itself.

Issue found in an unpublished CTS test.  Small reproduction in our
unit tests now enabled.

Fixes: 00e4e5f1ba2 ("intel/fs: Don't add MOV instructions to DO blocks in combine constants")
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27131>

src/intel/compiler/brw_fs_combine_constants.cpp
@@ -1834,6 +1834,16 @@ fs_visitor::opt_combine_constants()
    }
 
    if (rebuild_cfg) {
+      /* When the CFG is initially built, the instructions are removed from
+       * the list of instructions stored in fs_visitor -- the same exec_node
+       * is used for membership in that list and in a block list.  So we need
+       * to pull them back before rebuilding the CFG.
+       */
+      assert(exec_list_length(&instructions) == 0);
+      foreach_block(block, cfg) {
+         exec_list_append(&instructions, &block->instructions);
+      }
+
       delete cfg;
       cfg = NULL;
       calculate_cfg();

src/intel/compiler/test_fs_combine_constants.cpp
@@ -98,7 +98,7 @@ TEST_F(FSCombineConstantsTest, Simple)
    ASSERT_EQ(bblock_end(block)->opcode, BRW_OPCODE_SEL);
 }
 
-TEST_F(FSCombineConstantsTest, DISABLED_DoContainingDo)
+TEST_F(FSCombineConstantsTest, DoContainingDo)
 {
    fs_builder bld = make_builder(shader);
 