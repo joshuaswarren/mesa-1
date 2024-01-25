joshuaswarren/mesa-1

commit c7a456134abcf89e4d0c2ac1f0dddb86eb18d79e

radv/amdgpu: Handle -ENODATA and -ETIME from cs_submit
This can be sent in the event of a soft/hard recovery.

Signed-off-by: Joshua Ashton <joshua@froggi.es>

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Friedrich Vock <friedrich.vock@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27091>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
@@ -1767,7 +1767,16 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx, struct radv_amdgpu_cs_request
          fprintf(stderr, "radv/amdgpu: Not enough memory for command submission.\n");
          result = VK_ERROR_OUT_OF_HOST_MEMORY;
       } else if (r == -ECANCELED) {
-         fprintf(stderr, "radv/amdgpu: The CS has been cancelled because the context is lost.\n");
+         fprintf(stderr,
+                 "radv/amdgpu: The CS has been cancelled because the context is lost. This context is innocent.\n");
+         result = VK_ERROR_DEVICE_LOST;
+      } else if (r == -ENODATA) {
+         fprintf(stderr, "radv/amdgpu: The CS has been cancelled because the context is lost. This context is guilty "
+                         "of a soft recovery.\n");
+         result = VK_ERROR_DEVICE_LOST;
+      } else if (r == -ETIME) {
+         fprintf(stderr, "radv/amdgpu: The CS has been cancelled because the context is lost. This context is guilty "
+                         "of a hard recovery.\n");
          result = VK_ERROR_DEVICE_LOST;
       } else {
          fprintf(stderr,

commit ca3c627e4b430ec960b1a579e28e3a2226924c56

radv/amdgpu: Fix build on BSD
Some BSDs don't define ENODATA.

Fixes: c7a45613 ("radv/amdgpu: Handle -ENODATA and -ETIME from cs_submit")
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27181>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 #include "drm-uapi/amdgpu_drm.h"
 
+#include "util/detect_os.h"
 #include "util/os_time.h"
 #include "util/u_memory.h"
 #include "ac_debug.h"
@@ -42,6 +43,15 @@
 #include "vk_drm_syncobj.h"
 #include "vk_sync.h"
 #include "vk_sync_dummy.h"
+
+/* Some BSDs don't define ENODATA (and ENODATA is replaced with different error
+ * codes in the kernel).
+ */
+#if DETECT_OS_OPENBSD
+#define ENODATA ENOTSUP
+#elif DETECT_OS_FREEBSD || DETECT_OS_DRAGONFLY
+#define ENODATA ECONNREFUSED
+#endif
 
 /* Maximum allowed total number of submitted IBs. */
 #define RADV_MAX_IBS_PER_SUBMIT 192