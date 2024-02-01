joshuaswarren/mesa-1

commit ac97674dd4b06bd4aa2a9066b8305bd89f8244b0

radv/rt: Add workaround to make leaves always active
DOOM Eternal builds acceleration structures with inactive primitives and
tries to make them active in later AS updates. This is disallowed by the
spec and triggers a GPU hang. Fix the hang by working around the bug.

Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27034>

src/amd/vulkan/bvh/build_helpers.h
@@ -156,6 +156,7 @@
 
 #define VK_GEOMETRY_TYPE_TRIANGLES_KHR 0
 #define VK_GEOMETRY_TYPE_AABBS_KHR     1
+#define VK_GEOMETRY_TYPE_INSTANCES_KHR 2
 
 #define VK_GEOMETRY_INSTANCE_TRIANGLE_FACING_CULL_DISABLE_BIT_KHR 1
 #define VK_GEOMETRY_INSTANCE_TRIANGLE_FLIP_FACING_BIT_KHR         2

src/amd/vulkan/bvh/leaf.comp
@@ -87,6 +87,14 @@ main(void)
       is_active = build_instance(bounds, src_ptr, dst_ptr, global_id);
    }
 
+#if ALWAYS_ACTIVE
+   if (!is_active && args.geom_data.geometry_type != VK_GEOMETRY_TYPE_INSTANCES_KHR) {
+      bounds.min = vec3(0.0);
+      bounds.max = vec3(0.0);
+      is_active = true;
+   }
+#endif
+
    if (is_active) {
       REF(radv_ir_node) ir_node = INDEX(radv_ir_node, args.ir, primitive_id);
       DEREF(ir_node).aabb = bounds;

src/amd/vulkan/bvh/meson.build
@@ -53,7 +53,12 @@ bvh_shaders = [
   [
     'leaf.comp',
     'leaf',
-    [],
+    ['ALWAYS_ACTIVE=0'],
+  ],
+  [
+    'leaf.comp',
+    'leaf_always_active',
+    ['ALWAYS_ACTIVE=1'],
   ],
   [
     'morton.comp',

src/amd/vulkan/radv_acceleration_structure.c
@@ -41,6 +41,10 @@ static const uint32_t leaf_spv[] = {
 #include "bvh/leaf.spv.h"
 };
 
+static const uint32_t leaf_always_active_spv[] = {
+#include "bvh/leaf_always_active.spv.h"
+};
+
 static const uint32_t morton_spv[] = {
 #include "bvh/morton.spv.h"
 };
@@ -538,9 +542,14 @@ radv_device_init_accel_struct_build_state(struct radv_device *device)
    if (device->meta_state.accel_struct_build.radix_sort)
       goto exit;
 
-   result = create_build_pipeline_spv(device, leaf_spv, sizeof(leaf_spv), sizeof(struct leaf_args),
-                                      &device->meta_state.accel_struct_build.leaf_pipeline,
-                                      &device->meta_state.accel_struct_build.leaf_p_layout);
+   if (device->instance->drirc.force_active_accel_struct_leaves)
+      result = create_build_pipeline_spv(device, leaf_always_active_spv, sizeof(leaf_always_active_spv),
+                                         sizeof(struct leaf_args), &device->meta_state.accel_struct_build.leaf_pipeline,
+                                         &device->meta_state.accel_struct_build.leaf_p_layout);
+   else
+      result = create_build_pipeline_spv(device, leaf_spv, sizeof(leaf_spv), sizeof(struct leaf_args),
+                                         &device->meta_state.accel_struct_build.leaf_pipeline,
+                                         &device->meta_state.accel_struct_build.leaf_p_layout);
    if (result != VK_SUCCESS)
       goto exit;
 

src/amd/vulkan/radv_instance.c
@@ -160,6 +160,7 @@ static const driOptionDescription radv_dri_options[] = {
       DRI_CONF_RADV_OVERRIDE_COMPUTE_SHADER_VERSION(0)
       DRI_CONF_RADV_OVERRIDE_RAY_TRACING_SHADER_VERSION(0)
       DRI_CONF_RADV_SSBO_NON_UNIFORM(false)
+      DRI_CONF_RADV_FORCE_ACTIVE_ACCEL_STRUCT_LEAVES(false)
       DRI_CONF_RADV_APP_LAYER()
    DRI_CONF_SECTION_END
 };
@@ -251,6 +252,9 @@ radv_init_dri_options(struct radv_instance *instance)
 
    instance->drirc.vk_require_etc2 = driQueryOptionb(&instance->drirc.options, "vk_require_etc2");
    instance->drirc.vk_require_astc = driQueryOptionb(&instance->drirc.options, "vk_require_astc");
+
+   instance->drirc.force_active_accel_struct_leaves =
+      driQueryOptionb(&instance->drirc.options, "radv_force_active_accel_struct_leaves");
 }
 
 static const struct vk_instance_extension_table radv_instance_extensions_supported = {

src/amd/vulkan/radv_private.h
@@ -386,6 +386,7 @@ struct radv_instance {
       bool report_llvm9_version_string;
       bool vk_require_etc2;
       bool vk_require_astc;
+      bool force_active_accel_struct_leaves;
       char *app_layer;
       uint8_t override_graphics_shader_version;
       uint8_t override_compute_shader_version;

src/util/00-radv-defaults.conf
@@ -106,6 +106,7 @@ Application bugs worked around in this file:
         <application name="DOOM Eternal" application_name_match="DOOMEternal">
             <option name="radv_zero_vram" value="true" />
             <option name="radv_legacy_sparse_binding" value="true" />
+            <option name="radv_force_active_accel_struct_leaves" value="true" />
         </application>
 
         <application name="No Man's Sky" application_name_match="No Man's Sky">

src/util/driconf.h
@@ -716,6 +716,10 @@
 #define DRI_CONF_RADV_CLEAR_LDS(def) \
    DRI_CONF_OPT_B(radv_clear_lds, def, "Clear LDS at the end of shaders. Might decrease performance.")
 
+#define DRI_CONF_RADV_FORCE_ACTIVE_ACCEL_STRUCT_LEAVES(def) \
+   DRI_CONF_OPT_B(radv_force_active_accel_struct_leaves, def, \
+                  "Force leaf nodes of acceleration structures to be marked active.")
+
 /**
  * \brief ANV specific configuration options
  */

commit 08a41d4892ac47c53aabef39cb194ab4c5a51dbe

radv/rt: Write inactive node data in ALWAYS_ACTIVE workaround
Fixes: ac97674d ("radv/rt: Add workaround to make leaves always active")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27340>

src/amd/vulkan/bvh/build_helpers.h
@@ -460,6 +460,7 @@ TYPE(AccelerationStructureInstance, 8);
 bool
 build_triangle(inout radv_aabb bounds, VOID_REF dst_ptr, radv_bvh_geometry_data geom_data, uint32_t global_id)
 {
+   bool is_valid = true;
    triangle_indices indices = load_indices(geom_data.indices, geom_data.index_format, global_id);
 
    triangle_vertices vertices = load_vertices(geom_data.data, indices, geom_data.vertex_format, geom_data.stride);
@@ -469,7 +470,11 @@ build_triangle(inout radv_aabb bounds, VOID_REF dst_ptr, radv_bvh_geometry_data 
     * format does not have a NaN representation, then all triangles are considered active.
     */
    if (isnan(vertices.vertex[0].x) || isnan(vertices.vertex[1].x) || isnan(vertices.vertex[2].x))
+#if ALWAYS_ACTIVE
+      is_valid = false;
+#else
       return false;
+#endif
 
    if (geom_data.transform != NULL) {
       mat4 transform = mat4(1.0);
@@ -498,12 +503,13 @@ build_triangle(inout radv_aabb bounds, VOID_REF dst_ptr, radv_bvh_geometry_data 
    DEREF(node).geometry_id_and_flags = geom_data.geometry_id;
    DEREF(node).id = 9;
 
-   return true;
+   return is_valid;
 }
 
 bool
 build_aabb(inout radv_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_t geometry_id, uint32_t global_id)
 {
+   bool is_valid = true;
    REF(radv_bvh_aabb_node) node = REF(radv_bvh_aabb_node)(dst_ptr);
 
    for (uint32_t vec = 0; vec < 2; vec++)
@@ -520,12 +526,16 @@ build_aabb(inout radv_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_t 
     * NaN, and the first is not, the behavior is undefined.
     */
    if (isnan(bounds.min.x))
+#if ALWAYS_ACTIVE
+      is_valid = false;
+#else
       return false;
+#endif
 
    DEREF(node).primitive_id = global_id;
    DEREF(node).geometry_id_and_flags = geometry_id;
 
-   return true;
+   return is_valid;
 }
 
 radv_aabb