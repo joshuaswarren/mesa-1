joshuaswarren/mesa-1

commit b984d17e3a22c8461ad1ff8c6fbf1e5bb427ef85

anv: make Wa_16013994831 to use intel_needs_workaround
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22560>

src/intel/vulkan/genX_gpu_memcpy.c
@@ -82,7 +82,7 @@ emit_common_so_memcpy(struct anv_batch *batch, struct anv_device *device,
    }
 
    /* Wa_16013994831 - Disable preemption during streamout. */
-   if (intel_device_info_is_dg2(device->info))
+   if (intel_needs_workaround(device->info, 16013994831))
       genX(batch_set_preemption)(batch, false);
 #endif
 

src/intel/vulkan/genX_cmd_buffer.c
@@ -3206,7 +3206,7 @@ genX(streamout_prologue)(struct anv_cmd_buffer *cmd_buffer)
     * level preemption for another reason in genX_state.c so we can skip this
     * for Gfx12.
     */
-   if (!intel_device_info_is_dg2(cmd_buffer->device->info))
+   if (!intel_needs_workaround(cmd_buffer->device->info, 16013994831))
       return;
 
    if (cmd_buffer->state.gfx.pipeline->uses_xfb) {

commit 227c95df21841d9407ed7debc3ce04b1959174a6

anv: fix Wa_16013994831 macros
The commit that switched to the WA framework forgot to update one of
the ifdef section.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: b984d17e3a ("anv: make Wa_16013994831 to use intel_needs_workaround")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27676>

src/intel/vulkan/genX_gfx_state.c
@@ -68,7 +68,7 @@ static const uint32_t genX(vk_to_intel_blend_op)[] = {
 static void
 genX(streamout_prologue)(struct anv_cmd_buffer *cmd_buffer)
 {
-#if GFX_VERx10 >= 120
+#if INTEL_WA_16013994831_GFX_VER
    /* Wa_16013994831 - Disable preemption during streamout, enable back
     * again if XFB not used by the current pipeline.
     *

src/intel/vulkan/genX_gpu_memcpy.c
@@ -82,7 +82,9 @@ emit_common_so_memcpy(struct anv_batch *batch, struct anv_device *device,
       anv_batch_emit(batch, GENX(3DSTATE_MESH_CONTROL), mesh);
       anv_batch_emit(batch, GENX(3DSTATE_TASK_CONTROL), task);
    }
+#endif
 
+#if INTEL_WA_16013994831_GFX_VER
    /* Wa_16013994831 - Disable preemption during streamout. */
    if (intel_needs_workaround(device->info, 16013994831))
       genX(batch_set_preemption)(batch, device->info, _3D, false);