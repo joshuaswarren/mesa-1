joshuaswarren/mesa-1

commit d3a7be86090b6a2bfdd02b8e3d04c0bdb0e28ee4

anv: add analysis for push descriptor uses and store it in shader cache
We'll use this information to avoid :
   - binding table emission
   - allocation of surface states

v2: Fix anv_nir_push_desc_ubo_fully_promoted()

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19050>

src/intel/vulkan/anv_nir_push_descriptor_analysis.c
@@ -1,0 +1,242 @@
+/*
+ * Copyright © 2022 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "anv_nir.h"
+
+const struct anv_descriptor_set_layout *
+anv_pipeline_layout_get_push_set(const struct anv_pipeline_layout *layout,
+                                 uint8_t *set_idx)
+{
+   for (unsigned s = 0; s < ARRAY_SIZE(layout->set); s++) {
+      struct anv_descriptor_set_layout *set_layout = layout->set[s].layout;
+
+      if (!set_layout ||
+          !(set_layout->flags &
+            VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR))
+         continue;
+
+      if (set_idx)
+         *set_idx = s;
+
+      return set_layout;
+   }
+
+   return NULL;
+}
+
+/* This function returns a bitfield of used descriptors in the push descriptor
+ * set. You can only call this function before calling
+ * anv_nir_apply_pipeline_layout() as information required is lost after
+ * applying the pipeline layout.
+ */
+uint32_t
+anv_nir_compute_used_push_descriptors(nir_shader *shader,
+                                      const struct anv_pipeline_layout *layout)
+{
+   uint8_t push_set;
+   const struct anv_descriptor_set_layout *push_set_layout =
+      anv_pipeline_layout_get_push_set(layout, &push_set);
+   if (push_set_layout == NULL)
+      return 0;
+
+   uint32_t used_push_bindings = 0;
+   nir_foreach_variable_with_modes(var, shader,
+                                   nir_var_uniform |
+                                   nir_var_image |
+                                   nir_var_mem_ubo |
+                                   nir_var_mem_ssbo) {
+      if (var->data.descriptor_set == push_set) {
+         uint32_t desc_idx =
+            push_set_layout->binding[var->data.binding].descriptor_index;
+         assert(desc_idx < MAX_PUSH_DESCRIPTORS);
+         used_push_bindings |= BITFIELD_BIT(desc_idx);
+      }
+   }
+
+   nir_foreach_function(function, shader) {
+      if (!function->impl)
+         continue;
+
+      nir_foreach_block(block, function->impl) {
+         nir_foreach_instr(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+            if (intrin->intrinsic != nir_intrinsic_vulkan_resource_index)
+               continue;
+
+            uint8_t set = nir_intrinsic_desc_set(intrin);
+            if (set != push_set)
+               continue;
+
+            uint32_t binding = nir_intrinsic_binding(intrin);
+            uint32_t desc_idx =
+               push_set_layout->binding[binding].descriptor_index;
+            assert(desc_idx < MAX_PUSH_DESCRIPTORS);
+
+            used_push_bindings |= BITFIELD_BIT(desc_idx);
+         }
+      }
+   }
+
+   return used_push_bindings;
+}
+
+/* This function checks whether the shader accesses the push descriptor
+ * buffer. This function must be called after anv_nir_compute_push_layout().
+ */
+bool
+anv_nir_loads_push_desc_buffer(nir_shader *nir,
+                               const struct anv_pipeline_layout *layout,
+                               const struct anv_pipeline_bind_map *bind_map)
+{
+   uint8_t push_set;
+   const struct anv_descriptor_set_layout *push_set_layout =
+      anv_pipeline_layout_get_push_set(layout, &push_set);
+   if (push_set_layout == NULL)
+      return false;
+
+   nir_foreach_function(function, nir) {
+      if (!function->impl)
+         continue;
+
+      nir_foreach_block(block, function->impl) {
+         nir_foreach_instr(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+            if (intrin->intrinsic != nir_intrinsic_load_ubo)
+               continue;
+
+            const nir_const_value *const_bt_idx =
+               nir_src_as_const_value(intrin->src[0]);
+            if (const_bt_idx == NULL)
+               continue;
+
+            const unsigned bt_idx = const_bt_idx[0].u32;
+
+            const struct anv_pipeline_binding *binding =
+               &bind_map->surface_to_descriptor[bt_idx];
+            if (binding->set == ANV_DESCRIPTOR_SET_DESCRIPTORS &&
+                binding->index == push_set)
+               return true;
+         }
+      }
+   }
+
+   return false;
+}
+
+/* This function computes a bitfield of all the UBOs bindings in the push
+ * descriptor set that are fully promoted to push constants. If a binding's
+ * bit in the field is set, the corresponding binding table entry will not be
+ * accessed by the shader. This function must be called after
+ * anv_nir_compute_push_layout().
+ */
+uint32_t
+anv_nir_push_desc_ubo_fully_promoted(nir_shader *nir,
+                                     const struct anv_pipeline_layout *layout,
+                                     const struct anv_pipeline_bind_map *bind_map)
+{
+   uint8_t push_set;
+   const struct anv_descriptor_set_layout *push_set_layout =
+      anv_pipeline_layout_get_push_set(layout, &push_set);
+   if (push_set_layout == NULL)
+      return false;
+
+   uint32_t ubos_fully_promoted = 0;
+   for (uint32_t b = 0; b < push_set_layout->binding_count; b++) {
+      const struct anv_descriptor_set_binding_layout *bind_layout =
+         &push_set_layout->binding[b];
+      if (bind_layout->type == -1)
+         continue;
+
+      assert(bind_layout->descriptor_index < MAX_PUSH_DESCRIPTORS);
+      if (bind_layout->type == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER)
+         ubos_fully_promoted |= BITFIELD_BIT(bind_layout->descriptor_index);
+   }
+
+   nir_foreach_function(function, nir) {
+      if (!function->impl)
+         continue;
+
+      nir_foreach_block(block, function->impl) {
+         nir_foreach_instr(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+            if (intrin->intrinsic != nir_intrinsic_load_ubo)
+               continue;
+
+            const nir_const_value *const_bt_idx =
+               nir_src_as_const_value(intrin->src[0]);
+            if (const_bt_idx == NULL)
+               continue;
+
+            const unsigned bt_idx = const_bt_idx[0].u32;
+
+            /* Skip if this isn't a load from push descriptor buffer. */
+            const struct anv_pipeline_binding *binding =
+               &bind_map->surface_to_descriptor[bt_idx];
+            if (binding->set != push_set)
+               continue;
+
+            const uint32_t desc_idx =
+               push_set_layout->binding[binding->binding].descriptor_index;
+            assert(desc_idx < MAX_PUSH_DESCRIPTORS);
+
+            bool promoted = false;
+
+            /* If the offset in the entry is dynamic, we can't tell if
+             * promoted or not.
+             */
+            const nir_const_value *const_load_offset =
+               nir_src_as_const_value(intrin->src[1]);
+            if (const_load_offset != NULL) {
+               /* Check if the load was promoted to a push constant. */
+               const unsigned load_offset = const_load_offset[0].u32;
+               const int load_bytes = nir_intrinsic_dest_components(intrin) *
+                  (nir_dest_bit_size(intrin->dest) / 8);
+
+               for (unsigned i = 0; i < ARRAY_SIZE(bind_map->push_ranges); i++) {
+                  if (bind_map->push_ranges[i].start * 32 <= load_offset &&
+                      (bind_map->push_ranges[i].start +
+                       bind_map->push_ranges[i].length) * 32 >=
+                      (load_offset + load_bytes)) {
+                     promoted = true;
+                     break;
+                  }
+               }
+            }
+
+            if (!promoted)
+               ubos_fully_promoted &= ~BITFIELD_BIT(desc_idx);
+         }
+      }
+   }
+
+   return ubos_fully_promoted;
+}

src/intel/vulkan/anv_pipeline.c
@@ -662,9 +662,13 @@ struct anv_pipeline_stage {
 
    nir_shader *nir;
 
+   struct anv_push_descriptor_info push_desc_info;
+
    struct anv_pipeline_binding surface_to_descriptor[256];
    struct anv_pipeline_binding sampler_to_descriptor[256];
    struct anv_pipeline_bind_map bind_map;
+
+   bool uses_bt_for_push_descs;
 
    union brw_any_prog_data prog_data;
 
@@ -787,6 +791,38 @@ anv_pipeline_hash_ray_tracing_combined_shader(struct anv_ray_tracing_pipeline *p
    _mesa_sha1_final(&ctx, sha1_out);
 }
 
+static void
+anv_stage_nullify_unused_push_desc_surfaces(struct anv_pipeline_stage *stage,
+                                            struct anv_pipeline_layout *layout)
+{
+   uint8_t push_set;
+   const struct anv_descriptor_set_layout *push_set_layout =
+      anv_pipeline_layout_get_push_set(layout, &push_set);
+   if (push_set_layout == NULL)
+      return;
+
+   const uint32_t to_keep_descriptors =
+      stage->push_desc_info.used_descriptors &
+      ~stage->push_desc_info.fully_promoted_ubo_descriptors;
+
+   for (unsigned s = 0; s < stage->bind_map.surface_count; s++) {
+      if (stage->bind_map.surface_to_descriptor[s].set == ANV_DESCRIPTOR_SET_DESCRIPTORS &&
+          stage->bind_map.surface_to_descriptor[s].index == push_set &&
+          !stage->push_desc_info.used_set_buffer)
+         stage->bind_map.surface_to_descriptor[s].set = ANV_DESCRIPTOR_SET_NULL;
+
+
+      if (stage->bind_map.surface_to_descriptor[s].set == push_set) {
+         const uint32_t binding =
+            stage->bind_map.surface_to_descriptor[s].binding;
+         const uint32_t desc_index =
+            push_set_layout->binding[binding].descriptor_index;
+         if (!(BITFIELD_BIT(desc_index) & to_keep_descriptors))
+            stage->bind_map.surface_to_descriptor[s].set = ANV_DESCRIPTOR_SET_NULL;
+      }
+   }
+}
+
 static nir_shader *
 anv_pipeline_stage_get_nir(struct anv_pipeline *pipeline,
                            struct vk_pipeline_cache *cache,
@@ -887,6 +923,9 @@ anv_pipeline_lower_nir(struct anv_pipeline *pipeline,
 
    NIR_PASS(_, nir, brw_nir_lower_ray_queries, &pdevice->info);
 
+   stage->push_desc_info.used_descriptors =
+      anv_nir_compute_used_push_descriptors(nir, layout);
+
    /* Apply the actual pipeline layout to UBOs, SSBOs, and textures */
    NIR_PASS_V(nir, anv_nir_apply_pipeline_layout,
               pdevice, pipeline->device->robust_buffer_access,
@@ -961,6 +1000,12 @@ anv_pipeline_lower_nir(struct anv_pipeline *pipeline,
    if (gl_shader_stage_is_compute(nir->info.stage) ||
        gl_shader_stage_is_mesh(nir->info.stage))
       NIR_PASS(_, nir, brw_nir_lower_cs_intrinsics);
+
+   stage->push_desc_info.used_set_buffer =
+      anv_nir_loads_push_desc_buffer(nir, layout, &stage->bind_map);
+   stage->push_desc_info.fully_promoted_ubo_descriptors =
+      anv_nir_push_desc_ubo_fully_promoted(nir, layout, &stage->bind_map);
+   anv_stage_nullify_unused_push_desc_surfaces(stage, layout);
 
    stage->nir = nir;
 }
@@ -1266,12 +1311,15 @@ anv_pipeline_link_fs(const struct brw_compiler *compiler,
             rt_bindings[rt] = (struct anv_pipeline_binding) {
                .set = ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS,
                .index = rt,
+               .binding = UINT32_MAX,
+
             };
          } else {
             /* Setup a null render target */
             rt_bindings[rt] = (struct anv_pipeline_binding) {
                .set = ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS,
                .index = UINT32_MAX,
+               .binding = UINT32_MAX,
             };
          }
       }
@@ -1453,6 +1501,14 @@ anv_pipeline_add_executables(struct anv_pipeline *pipeline,
    }
 
    pipeline->ray_queries = MAX2(pipeline->ray_queries, bin->prog_data->ray_queries);
+
+   if (bin->push_desc_info.used_set_buffer) {
+      pipeline->use_push_descriptor_buffer |=
+         BITFIELD_BIT(mesa_to_vk_shader_stage(stage->stage));
+   }
+   if (bin->push_desc_info.used_descriptors &
+       ~bin->push_desc_info.fully_promoted_ubo_descriptors)
+      pipeline->use_push_descriptor |= mesa_to_vk_shader_stage(stage->stage);
 }
 
 static void
@@ -1921,7 +1977,8 @@ anv_graphics_pipeline_compile(struct anv_graphics_pipeline *pipeline,
                                   brw_prog_data_size(s),
                                   stage->stats, stage->num_stats,
                                   stage->nir->xfb_info,
-                                  &stage->bind_map);
+                                  &stage->bind_map,
+                                  &stage->push_desc_info);
       if (!bin) {
          ralloc_free(stage_ctx);
          result = vk_error(pipeline, VK_ERROR_OUT_OF_HOST_MEMORY);
@@ -2034,6 +2091,7 @@ anv_pipeline_compile_cs(struct anv_compute_pipeline *pipeline,
       stage.bind_map.surface_count = 1;
       stage.bind_map.surface_to_descriptor[0] = (struct anv_pipeline_binding) {
          .set = ANV_DESCRIPTOR_SET_NUM_WORK_GROUPS,
+         .binding = UINT32_MAX,
       };
 
       stage.nir = anv_pipeline_stage_get_nir(&pipeline->base, cache, mem_ctx, &stage);
@@ -2081,7 +2139,8 @@ anv_pipeline_compile_cs(struct anv_compute_pipeline *pipeline,
                                      &stage.prog_data.base,
                                      sizeof(stage.prog_data.cs),
                                      stage.stats, stage.num_stats,
-                                     NULL, &stage.bind_map);
+                                     NULL, &stage.bind_map,
+                                     &stage.push_desc_info);
       if (!bin) {
          ralloc_free(mem_ctx);
          return vk_error(pipeline, VK_ERROR_OUT_OF_HOST_MEMORY);
@@ -2444,7 +2503,8 @@ compile_upload_rt_shader(struct anv_ray_tracing_pipeline *pipeline,
                                &stage->prog_data.base,
                                sizeof(stage->prog_data.bs),
                                stage->stats, 1,
-                               NULL, &empty_bind_map);
+                               NULL, &empty_bind_map,
+                               false /* push_descriptor_uses_bt */);
    if (bin == NULL)
       return vk_error(pipeline, VK_ERROR_OUT_OF_HOST_MEMORY);
 
@@ -2872,6 +2932,7 @@ anv_device_init_rt_shaders(struct anv_device *device)
 
       trampoline_nir->info.subgroup_size = SUBGROUP_SIZE_REQUIRE_16;
 
+      struct anv_push_descriptor_info push_desc_info = {};
       struct anv_pipeline_bind_map bind_map = {
          .surface_count = 0,
          .sampler_count = 0,
@@ -2900,7 +2961,8 @@ anv_device_init_rt_shaders(struct anv_device *device)
                                   trampoline_prog_data.base.program_size,
                                   &trampoline_prog_data.base,
                                   sizeof(trampoline_prog_data),
-                                  NULL, 0, NULL, &bind_map);
+                                  NULL, 0, NULL, &bind_map,
+                                  &push_desc_info);
 
       ralloc_free(tmp_ctx);
 
@@ -2932,6 +2994,7 @@ anv_device_init_rt_shaders(struct anv_device *device)
 
       NIR_PASS_V(trivial_return_nir, brw_nir_lower_rt_intrinsics, device->info);
 
+      struct anv_push_descriptor_info push_desc_info = {};
       struct anv_pipeline_bind_map bind_map = {
          .surface_count = 0,
          .sampler_count = 0,
@@ -2953,7 +3016,8 @@ anv_device_init_rt_shaders(struct anv_device *device)
                                   &return_key, sizeof(return_key),
                                   return_data, return_prog_data.base.program_size,
                                   &return_prog_data.base, sizeof(return_prog_data),
-                                  NULL, 0, NULL, &bind_map);
+                                  NULL, 0, NULL, &bind_map,
+                                  &push_desc_info);
 
       ralloc_free(tmp_ctx);
 

src/intel/vulkan/anv_blorp.c
@@ -63,12 +63,14 @@ upload_blorp_shader(struct blorp_batch *batch, uint32_t stage,
       .surface_count = 0,
       .sampler_count = 0,
    };
+   struct anv_push_descriptor_info push_desc_info = {};
 
    struct anv_shader_bin *bin =
       anv_device_upload_kernel(device, device->internal_cache, stage,
                                key, key_size, kernel, kernel_size,
                                prog_data, prog_data_size,
-                               NULL, 0, NULL, &bind_map);
+                               NULL, 0, NULL, &bind_map,
+                               &push_desc_info);
 
    if (!bin)
       return false;

src/intel/vulkan/anv_nir.h
@@ -82,6 +82,17 @@ void anv_nir_validate_push_layout(struct brw_stage_prog_data *prog_data,
 
 bool anv_nir_add_base_work_group_id(nir_shader *shader);
 
+uint32_t anv_nir_compute_used_push_descriptors(nir_shader *shader,
+                                               const struct anv_pipeline_layout *layout);
+
+bool anv_nir_loads_push_desc_buffer(nir_shader *nir,
+                                    const struct anv_pipeline_layout *layout,
+                                    const struct anv_pipeline_bind_map *bind_map);
+
+uint32_t anv_nir_push_desc_ubo_fully_promoted(nir_shader *nir,
+                                              const struct anv_pipeline_layout *layout,
+                                              const struct anv_pipeline_bind_map *bind_map);
+
 #ifdef __cplusplus
 }
 #endif

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -1351,6 +1351,7 @@ anv_nir_apply_pipeline_layout(nir_shader *shader,
          map->surface_to_descriptor[map->surface_count] =
             (struct anv_pipeline_binding) {
                .set = ANV_DESCRIPTOR_SET_DESCRIPTORS,
+               .binding = UINT32_MAX,
                .index = s,
             };
          state.set[s].desc_offset = map->surface_count;
@@ -1440,6 +1441,7 @@ anv_nir_apply_pipeline_layout(nir_shader *shader,
                      map->surface_to_descriptor[map->surface_count++] =
                         (struct anv_pipeline_binding) {
                            .set = set,
+                           .binding = b,
                            .index = binding->descriptor_index + i,
                            .plane = p,
                         };
@@ -1450,6 +1452,7 @@ anv_nir_apply_pipeline_layout(nir_shader *shader,
                   map->surface_to_descriptor[map->surface_count++] =
                      (struct anv_pipeline_binding) {
                         .set = set,
+                        .binding = b,
                         .index = binding->descriptor_index + i,
                         .dynamic_offset_index =
                            layout->set[set].dynamic_offset_start +
@@ -1483,6 +1486,7 @@ anv_nir_apply_pipeline_layout(nir_shader *shader,
                   map->sampler_to_descriptor[map->sampler_count++] =
                      (struct anv_pipeline_binding) {
                         .set = set,
+                        .binding = b,
                         .index = binding->descriptor_index + i,
                         .plane = p,
                      };

commit c3df5dc3a0c34fa5d99037074c068abd62d741a9

anv: fixup push descriptor shader analysis
There are a couple mistakes here :

   - using a bitfield as an index to generate a bitfield...

   - in anv_nir_push_desc_ubo_fully_promoted(), confusing binding
     table access of the descriptor buffer with actual descriptors

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: d3a7be8609 ("anv: add analysis for push descriptor uses and store it in shader cache")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27504>

src/intel/compiler/brw_nir.h
@@ -125,8 +125,7 @@ brw_nir_ubo_surface_index_is_pushable(nir_src src)
 
    if (intrin && intrin->intrinsic == nir_intrinsic_resource_intel) {
       return (nir_intrinsic_resource_access_intel(intrin) &
-              nir_resource_intel_pushable) &&
-             nir_src_is_const(intrin->src[1]);
+              nir_resource_intel_pushable);
    }
 
    return nir_src_is_const(src);
@@ -149,6 +148,14 @@ brw_nir_ubo_surface_index_get_push_block(nir_src src)
    return nir_intrinsic_resource_block_intel(intrin);
 }
 
+/* This helper return the binding table index of a surface access (any
+ * buffer/image/etc...). It works off the source of one of the intrinsics
+ * (load_ubo, load_ssbo, store_ssbo, load_image, store_image, etc...).
+ *
+ * If the source is constant, then this is the binding table index. If we're
+ * going through a resource_intel intel intrinsic, then we need to check
+ * src[1] of that intrinsic.
+ */
 static inline unsigned
 brw_nir_ubo_surface_index_get_bti(nir_src src)
 {
@@ -158,8 +165,19 @@ brw_nir_ubo_surface_index_get_bti(nir_src src)
    assert(src.ssa->parent_instr->type == nir_instr_type_intrinsic);
 
    nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(src.ssa->parent_instr);
-   assert(intrin->intrinsic == nir_intrinsic_resource_intel);
-   assert(nir_src_is_const(intrin->src[1]));
+   if (!intrin || intrin->intrinsic != nir_intrinsic_resource_intel)
+      return UINT32_MAX;
+
+   /* In practice we could even drop this intrinsic because the bindless
+    * access always operate from a base offset coming from a push constant, so
+    * they can never be constant.
+    */
+   if (nir_intrinsic_resource_access_intel(intrin) &
+       nir_resource_intel_bindless)
+      return UINT32_MAX;
+
+   if (!nir_src_is_const(intrin->src[1]))
+      return UINT32_MAX;
 
    return nir_src_as_uint(intrin->src[1]);
 }

src/intel/vulkan/anv_nir_push_descriptor_analysis.c
@@ -126,18 +126,17 @@ anv_nir_loads_push_desc_buffer(nir_shader *nir,
             if (intrin->intrinsic != nir_intrinsic_load_ubo)
                continue;
 
-            const nir_const_value *const_bt_idx =
-               nir_src_as_const_value(intrin->src[0]);
-            if (const_bt_idx == NULL)
+            const unsigned bt_idx =
+               brw_nir_ubo_surface_index_get_bti(intrin->src[0]);
+            if (bt_idx == UINT32_MAX)
                continue;
-
-            const unsigned bt_idx = const_bt_idx[0].u32;
 
             const struct anv_pipeline_binding *binding =
                &bind_map->surface_to_descriptor[bt_idx];
             if (binding->set == ANV_DESCRIPTOR_SET_DESCRIPTORS &&
-                binding->index == push_set)
+                binding->index == push_set) {
                return true;
+            }
          }
       }
    }
@@ -162,6 +161,7 @@ anv_nir_push_desc_ubo_fully_promoted(nir_shader *nir,
    if (push_set_layout == NULL)
       return 0;
 
+   /* Assume every UBO can be promoted first. */
    uint32_t ubos_fully_promoted = 0;
    for (uint32_t b = 0; b < push_set_layout->binding_count; b++) {
       const struct anv_descriptor_set_binding_layout *bind_layout =
@@ -174,6 +174,10 @@ anv_nir_push_desc_ubo_fully_promoted(nir_shader *nir,
          ubos_fully_promoted |= BITFIELD_BIT(bind_layout->descriptor_index);
    }
 
+   /* For each load_ubo intrinsic, if the descriptor index or the offset is
+    * not a constant, we could not promote to push constant. Then check the
+    * offset + size against the push ranges.
+    */
    nir_foreach_function_impl(impl, nir) {
       nir_foreach_block(block, impl) {
          nir_foreach_instr(instr, block) {
@@ -184,45 +188,65 @@ anv_nir_push_desc_ubo_fully_promoted(nir_shader *nir,
             if (intrin->intrinsic != nir_intrinsic_load_ubo)
                continue;
 
-            if (!brw_nir_ubo_surface_index_is_pushable(intrin->src[0]))
+            /* Don't check the load_ubo from descriptor buffers */
+            nir_intrinsic_instr *resource =
+               intrin->src[0].ssa->parent_instr->type == nir_instr_type_intrinsic ?
+               nir_instr_as_intrinsic(intrin->src[0].ssa->parent_instr) : NULL;
+            if (resource == NULL || resource->intrinsic != nir_intrinsic_resource_intel)
                continue;
 
-            const unsigned bt_idx =
-               brw_nir_ubo_surface_index_get_bti(intrin->src[0]);
-
-            /* Skip if this isn't a load from push descriptor buffer. */
-            const struct anv_pipeline_binding *binding =
-               &bind_map->surface_to_descriptor[bt_idx];
-            if (binding->set != push_set)
+            /* Skip load_ubo not loading from the push descriptor */
+            if (nir_intrinsic_desc_set(resource) != push_set)
                continue;
+
+            uint32_t binding = nir_intrinsic_binding(resource);
+
+            /* If we have indirect indexing in the binding, no push promotion
+             * in possible for the entire binding.
+             */
+            if (!nir_src_is_const(resource->src[1])) {
+               for (uint32_t i = 0; i < push_set_layout->binding[binding].array_size; i++) {
+                  ubos_fully_promoted &=
+                     ~BITFIELD_BIT(push_set_layout->binding[binding].descriptor_index + i);
+               }
+               continue;
+            }
+
+            const nir_const_value *const_bt_id =
+               nir_src_as_const_value(resource->src[1]);
+            uint32_t bt_id = const_bt_id[0].u32;
+
+            const struct anv_pipeline_binding *pipe_bind =
+               &bind_map->surface_to_descriptor[bt_id];
 
             const uint32_t desc_idx =
-               push_set_layout->binding[binding->binding].descriptor_index;
-            assert(desc_idx < MAX_PUSH_DESCRIPTORS);
-
-            bool promoted = false;
+               push_set_layout->binding[binding].descriptor_index;
 
             /* If the offset in the entry is dynamic, we can't tell if
              * promoted or not.
              */
             const nir_const_value *const_load_offset =
                nir_src_as_const_value(intrin->src[1]);
-            if (const_load_offset != NULL) {
-               /* Check if the load was promoted to a push constant. */
-               const unsigned load_offset = const_load_offset[0].u32;
-               const int load_bytes = nir_intrinsic_dest_components(intrin) *
-                  (intrin->def.bit_size / 8);
+            if (const_load_offset == NULL) {
+               ubos_fully_promoted &= ~BITFIELD_BIT(desc_idx);
+               continue;
+            }
 
-               for (unsigned i = 0; i < ARRAY_SIZE(bind_map->push_ranges); i++) {
-                  if (bind_map->push_ranges[i].set == binding->set &&
-                      bind_map->push_ranges[i].index == desc_idx &&
-                      bind_map->push_ranges[i].start * 32 <= load_offset &&
-                      (bind_map->push_ranges[i].start +
-                       bind_map->push_ranges[i].length) * 32 >=
-                      (load_offset + load_bytes)) {
-                     promoted = true;
-                     break;
-                  }
+            /* Check if the load was promoted to a push constant. */
+            const unsigned load_offset = const_load_offset[0].u32;
+            const int load_bytes = nir_intrinsic_dest_components(intrin) *
+               (intrin->def.bit_size / 8);
+
+            bool promoted = false;
+            for (unsigned i = 0; i < ARRAY_SIZE(bind_map->push_ranges); i++) {
+               if (bind_map->push_ranges[i].set == pipe_bind->set &&
+                   bind_map->push_ranges[i].index == desc_idx &&
+                   bind_map->push_ranges[i].start * 32 <= load_offset &&
+                   (bind_map->push_ranges[i].start +
+                    bind_map->push_ranges[i].length) * 32 >=
+                   (load_offset + load_bytes)) {
+                  promoted = true;
+                  break;
                }
             }
 

src/intel/vulkan/anv_pipeline.c
@@ -1734,7 +1734,7 @@ anv_pipeline_account_shader(struct anv_pipeline *pipeline,
 
    if (shader->push_desc_info.used_set_buffer) {
       pipeline->use_push_descriptor_buffer |=
-         BITFIELD_BIT(mesa_to_vk_shader_stage(shader->stage));
+         mesa_to_vk_shader_stage(shader->stage);
    }
    if (shader->push_desc_info.used_descriptors &
        ~shader->push_desc_info.fully_promoted_ubo_descriptors)