joshuaswarren/mesa-1

commit a7edf3f92ecdc217cd578e42685ac01014bf6ea1

nvk: Advertise VK_KHR_graphics_pipeline_library
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9635
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27024>

src/nouveau/vulkan/nvk_physical_device.c
@@ -177,6 +177,7 @@ nvk_get_device_extensions(const struct nvk_instance *instance,
       .EXT_extended_dynamic_state2 = true,
       .EXT_extended_dynamic_state3 = true,
       .EXT_external_memory_dma_buf = true,
+      .EXT_graphics_pipeline_library = true,
       .EXT_host_query_reset = true,
       .EXT_image_2d_view_of_3d = true,
       .EXT_image_robustness = true,
@@ -463,6 +464,9 @@ nvk_get_device_features(const struct nv_device_info *info,
       .extendedDynamicState3CoverageReductionMode = false,
       .extendedDynamicState3RepresentativeFragmentTestEnable = false,
       .extendedDynamicState3ShadingRateImageEnable = false,
+
+      /* VK_EXT_graphics_pipeline_library */
+      .graphicsPipelineLibrary = true,
 
       /* VK_EXT_image_2d_view_of_3d */
       .image2DViewOf3D = true,
@@ -804,6 +808,10 @@ nvk_get_device_properties(const struct nvk_instance *instance,
 
       /* VK_EXT_extended_dynamic_state3 */
       .dynamicPrimitiveTopologyUnrestricted = true,
+
+      /* VK_EXT_graphics_pipeline_library */
+      .graphicsPipelineLibraryFastLinking = true,
+      .graphicsPipelineLibraryIndependentInterpolationDecoration = true,
 
       /* VK_KHR_line_rasterization */
       .lineSubPixelPrecisionBits = 8,

commit 90df6e401d9e55bb9e4934c75dc55f3dad349501

nvk: Advertise VK_KHR_pipeline_library
This is required by VK_EXT_graphics_pipeline_library

Fixes: a7edf3f92ecd ("nvk: Advertise VK_KHR_graphics_pipeline_library")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27860>

src/nouveau/vulkan/nvk_physical_device.c
@@ -117,6 +117,7 @@ nvk_get_device_extensions(const struct nvk_instance *instance,
       .KHR_map_memory2 = true,
       .KHR_multiview = true,
       .KHR_pipeline_executable_properties = true,
+      .KHR_pipeline_library = true,
 
 #ifdef NVK_USE_WSI_PLATFORM
       /* Hide these behind dri configs for now since we cannot implement it