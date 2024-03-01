joshuaswarren/mesa-1

commit 6de574a36467baef070db088f2d922a18f84e3e3

anv: Use DRM_XE_VM_BIND_OP_UNMAP_ALL to unbind whole bos
For non-sparse usage there is no difference between
DRM_XE_VM_BIND_OP_UNMAP_ALL and DRM_XE_VM_BIND_OP_UNMAP but for sparse
the same bo can be bound to more than one virtual address.

Then in a case like:
img = vkCreateImage()
mem = vkAllocateMemory()
vkQueueBindSparse(img, mem)
vkFreeMemory(mem)

Note that the sparse VMA bind still points to the closed bo(done in
vkFreeMemory()), but with DRM_XE_VM_BIND_OP_UNMAP_ALL all VMAs
over the bos are removed.

Access to a unbound VMA has a defined behavior(page fault) while
access to a bound VMA without backing gem/bo don't have defined
behavior.

Suggested-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27601>

src/intel/vulkan/xe/anv_kmd_backend.c
@@ -171,6 +171,13 @@ xe_vm_bind_op(struct anv_device *device,
             xe_bind->op = DRM_XE_VM_BIND_OP_MAP;
             xe_bind->obj = bo->gem_handle;
          }
+      } else if (bind->op == ANV_VM_UNBIND_ALL) {
+         xe_bind->op = DRM_XE_VM_BIND_OP_UNMAP_ALL;
+         xe_bind->obj = bo->gem_handle;
+         assert(bind->address == 0);
+         assert(bind->size == 0);
+      } else {
+         assert(bind->op == ANV_VM_UNBIND);
       }
 
       /* userptr and bo_offset are an union! */
@@ -223,10 +230,10 @@ static int xe_vm_unbind_bo(struct anv_device *device, struct anv_bo *bo)
 {
    struct anv_vm_bind bind = {
       .bo = bo,
-      .address = bo->offset,
+      .address = 0,
       .bo_offset = 0,
-      .size = bo->actual_size,
-      .op = ANV_VM_UNBIND,
+      .size = 0,
+      .op = ANV_VM_UNBIND_ALL,
    };
    struct anv_sparse_submission submit = {
       .queue = NULL,

src/intel/vulkan/anv_kmd_backend.h
@@ -42,8 +42,12 @@ struct anv_sparse_submission;
 struct anv_trtt_batch_bo;
 
 enum anv_vm_bind_op {
+   /* bind vma specified in anv_vm_bind */
    ANV_VM_BIND,
+   /* unbind vma specified in anv_vm_bind */
    ANV_VM_UNBIND,
+   /* unbind all vmas of anv_vm_bind::bo, address and size fields must be set to 0 */
+   ANV_VM_UNBIND_ALL,
 };
 
 struct anv_vm_bind {

commit c3bf6e71926a638db8654a97f7b59f474eaf7fe3

anv/xe: Consider pat_index while unbinding the bo
Xe KMD also checks if cpu_caching caching set during bo creationg
matches with caching of the PAT index set in the VM unbind.

This was being unnoticed until now by luck and lack of testing in MTL.

So here always setting PAT index for all VM operations that has a bo
associated.

v2: (Jose)
- Move pat_index little bit up
- Copy commit message from iris patch

Fixes: 6de574a3646 ("anv: Use DRM_XE_VM_BIND_OP_UNMAP_ALL to unbind whole bos")
Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27893>

src/intel/vulkan/xe/anv_kmd_backend.c
@@ -155,6 +155,10 @@ xe_vm_bind_op(struct anv_device *device,
    for (int i = 0; i < submit->binds_len; i++) {
       struct anv_vm_bind *bind = &submit->binds[i];
       struct anv_bo *bo = bind->bo;
+      uint16_t pat_index = 0;
+
+      if (bo)
+         pat_index = anv_device_get_pat_entry(device, bo->alloc_flags)->index;
 
       struct drm_xe_vm_bind_op *xe_bind = &xe_binds[i];
       *xe_bind = (struct drm_xe_vm_bind_op) {
@@ -165,12 +169,10 @@ xe_vm_bind_op(struct anv_device *device,
          .op = DRM_XE_VM_BIND_OP_UNMAP,
          .flags = capture_vm_in_error_dump(device, bo),
          .prefetch_mem_region_instance = 0,
+         .pat_index = pat_index,
       };
 
       if (bind->op == ANV_VM_BIND) {
-         const enum anv_bo_alloc_flags alloc_flags = bo ? bo->alloc_flags : 0;
-
-         xe_bind->pat_index = anv_device_get_pat_entry(device, alloc_flags)->index;
          if (!bo) {
             xe_bind->op = DRM_XE_VM_BIND_OP_MAP;
             xe_bind->flags |= DRM_XE_VM_BIND_FLAG_NULL;