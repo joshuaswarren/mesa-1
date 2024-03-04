joshuaswarren/mesa-1

commit 78b6e2f2c7a66cef5868a5171966120d22943ea1

compiler/types: Add a few more helpers to get builtin types
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26658>

src/compiler/glsl_types.h
@@ -1015,9 +1015,16 @@ static inline const struct glsl_type *glsl_void_type(void) { return &glsl_type_b
 static inline const struct glsl_type *glsl_float_type(void) { return &glsl_type_builtin_float; }
 static inline const struct glsl_type *glsl_float16_t_type(void) { return &glsl_type_builtin_float16_t; }
 static inline const struct glsl_type *glsl_double_type(void) { return &glsl_type_builtin_double; }
+static inline const struct glsl_type *glsl_vec2_type(void) { return &glsl_type_builtin_vec2; }
+static inline const struct glsl_type *glsl_dvec2_type(void) { return &glsl_type_builtin_ivec2; }
+static inline const struct glsl_type *glsl_uvec2_type(void) { return &glsl_type_builtin_uvec2; }
+static inline const struct glsl_type *glsl_ivec2_type(void) { return &glsl_type_builtin_ivec2; }
+static inline const struct glsl_type *glsl_bvec2_type(void) { return &glsl_type_builtin_bvec2; }
 static inline const struct glsl_type *glsl_vec4_type(void) { return &glsl_type_builtin_vec4; }
+static inline const struct glsl_type *glsl_dvec4_type(void) { return &glsl_type_builtin_ivec4; }
 static inline const struct glsl_type *glsl_uvec4_type(void) { return &glsl_type_builtin_uvec4; }
 static inline const struct glsl_type *glsl_ivec4_type(void) { return &glsl_type_builtin_ivec4; }
+static inline const struct glsl_type *glsl_bvec4_type(void) { return &glsl_type_builtin_bvec4; }
 static inline const struct glsl_type *glsl_int_type(void) { return &glsl_type_builtin_int; }
 static inline const struct glsl_type *glsl_uint_type(void) { return &glsl_type_builtin_uint; }
 static inline const struct glsl_type *glsl_int64_t_type(void) { return &glsl_type_builtin_int64_t; }

commit a4ce37d8abe05708c305de92217463ac49a1979f

compiler/types: Fix glsl_dvec*_type() helpers
Commit 78b6e2f2c7a contained a typo in the glsl_dvec4_type() helper,
instead returning a glsl_ivec4_type. As an ivec4 is 2x smaller than
a dvec4, this also broke piglit sanity on crocus/hsw.

This also fixes the dvec2 helper, though it has not been specifically
tested anywhere.

Fixes: 78b6e2f2c7a ("compiler/types: Add a few more helpers to get builtin types")

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27917>

src/compiler/glsl_types.h
@@ -931,12 +931,12 @@ static inline const glsl_type *glsl_float_type(void) { return &glsl_type_builtin
 static inline const glsl_type *glsl_float16_t_type(void) { return &glsl_type_builtin_float16_t; }
 static inline const glsl_type *glsl_double_type(void) { return &glsl_type_builtin_double; }
 static inline const glsl_type *glsl_vec2_type(void) { return &glsl_type_builtin_vec2; }
-static inline const glsl_type *glsl_dvec2_type(void) { return &glsl_type_builtin_ivec2; }
+static inline const glsl_type *glsl_dvec2_type(void) { return &glsl_type_builtin_dvec2; }
 static inline const glsl_type *glsl_uvec2_type(void) { return &glsl_type_builtin_uvec2; }
 static inline const glsl_type *glsl_ivec2_type(void) { return &glsl_type_builtin_ivec2; }
 static inline const glsl_type *glsl_bvec2_type(void) { return &glsl_type_builtin_bvec2; }
 static inline const glsl_type *glsl_vec4_type(void) { return &glsl_type_builtin_vec4; }
-static inline const glsl_type *glsl_dvec4_type(void) { return &glsl_type_builtin_ivec4; }
+static inline const glsl_type *glsl_dvec4_type(void) { return &glsl_type_builtin_dvec4; }
 static inline const glsl_type *glsl_uvec4_type(void) { return &glsl_type_builtin_uvec4; }
 static inline const glsl_type *glsl_ivec4_type(void) { return &glsl_type_builtin_ivec4; }
 static inline const glsl_type *glsl_bvec4_type(void) { return &glsl_type_builtin_bvec4; }