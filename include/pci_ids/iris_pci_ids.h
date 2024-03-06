openharmony/third_party_mesa3d

commit 56960b6efb01a4f1b40a2df7bd9b2ab9315c3736

intel/dev: Add DG2 G12 PCI IDs
Ref: https://patchwork.freedesktop.org/patch/483381/?series=103098&rev=1
Ref: https://patchwork.freedesktop.org/patch/msgid/20220425211251.77154-3-matthew.d.roper@intel.com
Cc: 22.1 <mesa-stable>
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16449>

include/pci_ids/iris_pci_ids.h
@@ -236,13 +236,19 @@ CHIPSET(0x5692, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5693, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5694, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x5695, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x5696, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x5697, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56a0, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56a1, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56a2, dg2_g10, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a3, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56a4, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56a5, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56a6, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56b0, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56c0, dg2_g10, "ATS-M", "Intel(R) Graphics")
 CHIPSET(0x56c1, dg2_g11, "ATS-M", "Intel(R) Graphics")
 #endif

joshuaswarren/mesa-1

commit 522b7fef051776acad3dda85d7363e98516c58e0

intel/dev: update DG2 device names
Ref: https://ark.intel.com/content/www/us/en/ark/products/codename/226095/products-formerly-alchemist.html
Ref: DG2 PRM, Volume 4: Configurations, Steppings and Device IDs
Fixes: 56960b6efb0 ("intel/dev: Add DG2 G12 PCI IDs")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27944>

include/pci_ids/iris_pci_ids.h
@@ -234,8 +234,8 @@ CHIPSET(0x5692, dg2_g10, "DG2", "Intel(R) Arc(tm) A550M Graphics")
 CHIPSET(0x5693, dg2_g11, "DG2", "Intel(R) Arc(tm) A370M Graphics")
 CHIPSET(0x5694, dg2_g11, "DG2", "Intel(R) Arc(tm) A350M Graphics")
 CHIPSET(0x5695, dg2_g11, "DG2", "Intel(R) Graphics")
-CHIPSET(0x5696, dg2_g12, "DG2", "Intel(R) Graphics")
-CHIPSET(0x5697, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x5696, dg2_g12, "DG2", "Intel(R) Arc(tm) A570M Graphics")
+CHIPSET(0x5697, dg2_g12, "DG2", "Intel(R) Arc(tm) A530M Graphics")
 CHIPSET(0x56a0, dg2_g10, "DG2", "Intel(R) Arc(tm) A770 Graphics")
 CHIPSET(0x56a1, dg2_g10, "DG2", "Intel(R) Arc(tm) A750 Graphics")
 CHIPSET(0x56a2, dg2_g10, "DG2", "Intel(R) Arc(tm) A580 Graphics")
@@ -245,8 +245,8 @@ CHIPSET(0x56a5, dg2_g11, "DG2", "Intel(R) Arc(tm) A380 Graphics")
 CHIPSET(0x56a6, dg2_g11, "DG2", "Intel(R) Arc(tm) A310 Graphics")
 CHIPSET(0x56b0, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A30M Graphics")
 CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A40/A50 Graphics")
-CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Graphics")
-CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Arc(tm) Pro A60M Graphics")
+CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Arc(tm) Pro A60 Graphics")
 CHIPSET(0x56ba, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56bb, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56bc, dg2_g11, "DG2", "Intel(R) Graphics")