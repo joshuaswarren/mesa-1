joshuaswarren/mesa-1

commit bfbc3154d23a0586b4f4c80cc79513e0049d8b16

etnaviv: switch to U_FIXED(..) macro
Lets have a look at trace from blob (GC3000):
    0x000b000b, /*   [10080] NTE.SAMPLER[0].SIZE := WIDTH=11,HEIGHT=11 */
    0x2001b86e, /*   [10100] NTE.SAMPLER[0].LOG_SIZE := WIDTH=3.437500,HEIGHT=3.437500,ASTC=0,INT_FILTER=1,SRGB=0 */

If we ignore the WIDTH and HEIGHT outputs and only look at the raw value (0x2001b86e) we can see that 0x6e is used for WIDTH and HEIGHT.

Lets have a look at an other texutre size 180x180:
    0x00b400b4, /*   [10080] NTE.SAMPLER[0].SIZE := WIDTH=180,HEIGHT=180 */
    0x2003bcef, /*   [10100] NTE.SAMPLER[0].LOG_SIZE := WIDTH=7.468750,HEIGHT=7.468750,ASTC=0,INT_FILTER=1,SRGB=0 */

Lets use the same test on a different GPU that uses texture descriptors (GC7000):
    [44] SIZE: 0x000b000b WIDTH=11,HEIGHT=11
    ...
    [74] LOG_SIZE_EXT: 0x03750375 WIDTH=3.457031,HEIGHT=3.457031

If we ignore the WIDTH and HEIGHT outputs and only look at the raw value (0x03750375) we can see that 0x375 is used for WIDTH and HEIGHT.

Lets have a look at an other texutre size 180x180:
    [44] SIZE: 0x00b400b4 WIDTH=180,HEIGHT=180
    ..
    [74] LOG_SIZE_EXT: 0x077d077d WIDTH=7.488281,HEIGHT=7.488281

etnaviv creates the following values for the two texture sizes:
  11 -> 0x6f
  180 -> 0xf0

Lets switch to U_FIXED(..) which results in equal values.

Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24364>

src/gallium/drivers/etnaviv/etnaviv_util.h
@@ -26,6 +26,7 @@
 #define H_ETNA_UTIL
 
 #include <math.h>
+#include "mesa/main/macros.h"
 
 /* for conditionally setting boolean flag(s): */
 #define COND(bool, val) ((bool) ? (val) : 0)
@@ -47,26 +48,14 @@ etna_cfloat_to_uint8(float f)
 static inline uint32_t
 etna_float_to_fixp55(float f)
 {
-   if (f >= 15.953125f)
-      return 511;
-
-   if (f < -16.0f)
-      return 512;
-
-   return (int32_t)(f * 32.0f + 0.5f);
+   return U_FIXED(f, 5);
 }
 
 /* float to fixp 8.8 */
 static inline uint32_t
 etna_float_to_fixp88(float f)
 {
-   if (f >= (32767.0 - 1.0f) / 256.0f)
-      return 32767;
-
-   if (f < -16.0f)
-      return 32768;
-
-   return (int32_t)(f * 256.0f + 0.5f);
+   return U_FIXED(f, 8);
 }
 
 /* texture size to log2 in fixp 5.5 format */

commit 9839c2976e6207590bec6b959ae0f28f087badcd

etnaviv: fix fixpoint conversion of negative values
The hand rolled etnaviv conversion functions were able to handle
negative input values when converting to fixpoint. By replacing
them with U_FIXED all negative values are clamped to zero, which
breaks usages where negative inputs are valid, like lodbias.

Fixes: bfbc3154d23a ("etnaviv: switch to U_FIXED(..) macro")
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28224>

src/gallium/drivers/etnaviv/etnaviv_util.h
@@ -35,14 +35,14 @@
 static inline uint32_t
 etna_float_to_fixp55(float f)
 {
-   return U_FIXED(f, 5);
+   return S_FIXED(f, 5);
 }
 
 /* float to fixp 8.8 */
 static inline uint32_t
 etna_float_to_fixp88(float f)
 {
-   return U_FIXED(f, 8);
+   return S_FIXED(f, 8);
 }
 
 /* texture size to log2 in fixp 5.5 format */