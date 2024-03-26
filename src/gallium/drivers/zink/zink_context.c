joshuaswarren/mesa-1

commit 322d7ce27b316e3978d3ce232288120c79c805b1

zink: use KHR_dynamic_rendering_local_read
no more renderpasses!

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27299>

src/gallium/drivers/zink/zink_context.c
@@ -2972,7 +2972,8 @@ zink_batch_rp(struct zink_context *ctx)
     * - dynamic rendering doesn't have input attachments
     */
    if (!zink_screen(ctx->base.screen)->info.have_KHR_dynamic_rendering ||
-       (ctx->transient_attachments && !zink_screen(ctx->base.screen)->info.have_EXT_multisampled_render_to_single_sampled) || ctx->fbfetch_outputs)
+       (ctx->transient_attachments && !zink_screen(ctx->base.screen)->info.have_EXT_multisampled_render_to_single_sampled) ||
+       (ctx->fbfetch_outputs && !zink_screen(ctx->base.screen)->info.have_KHR_dynamic_rendering_local_read))
       clear_buffers = zink_begin_render_pass(ctx);
    else
       clear_buffers = begin_rendering(ctx);

commit 3e195ad54efec3fcd3fc699b6b94339027c8d23e

zink: set dynamic rendering color attachment layouts
this is otherwise broken for fbfetch

Fixes: 322d7ce27b3 ("zink: use KHR_dynamic_rendering_local_read")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28340>

src/gallium/drivers/zink/zink_context.c
@@ -2911,6 +2911,7 @@ begin_rendering(struct zink_context *ctx)
          if (!iv)
             /* dead swapchain */
             return 0;
+         ctx->dynamic_fb.attachments[i].imageLayout = zink_resource(surf->base.texture)->layout;
       }
       ctx->dynamic_fb.attachments[i].imageView = iv;
    }