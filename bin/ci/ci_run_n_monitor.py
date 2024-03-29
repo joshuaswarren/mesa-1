joshuaswarren/mesa-1

commit 89b9710f8ac16694e201116b3921b426dcab7c3b

ci_run_n_monitor: allow passing multiple targets
When generating the list of targets from a script, being able to just
pass `--target "${list[@]}"` is very convenient.

The list of targets is simply converted to an "or" regex, matching any
of the `--target`s given.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27252>

bin/ci/ci_run_n_monitor.py
@@ -269,6 +269,7 @@ def parse_args() -> None:
         metavar="target-job",
         help="Target job regex. For multiple targets, separate with pipe | character",
         required=True,
+        nargs=argparse.ONE_OR_MORE,
     )
     parser.add_argument(
         "--token",
@@ -381,10 +382,11 @@ if __name__ == "__main__":
         print(f"Revision: {REV}")
         print(f"Pipeline: {pipe.web_url}")
 
-        target_jobs_regex = re.compile(args.target.strip())
+        target = '|'.join(args.target)
+        target_jobs_regex = re.compile(target.strip())
 
         deps = set()
-        print("🞋 job: " + Fore.BLUE + args.target + Style.RESET_ALL)
+        print("🞋 job: " + Fore.BLUE + target + Style.RESET_ALL)
         deps = find_dependencies(
             target_jobs_regex=target_jobs_regex, iid=pipe.iid, project_path=cur_project
         )

commit 937328fff95d8bc30f7296762ff79d63af2f208f

ci_run_n_monitor: explain how to pass multiple targets without having to use regexes
Fixes: 89b9710f8ac16694e201 ("ci_run_n_monitor: allow passing multiple targets")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28461>

bin/ci/ci_run_n_monitor.py
@@ -289,7 +289,8 @@ def parse_args() -> None:
     parser.add_argument(
         "--target",
         metavar="target-job",
-        help="Target job regex. For multiple targets, separate with pipe | character",
+        help="Target job regex. For multiple targets, pass multiple values, "
+             "eg. `--target foo bar`.",
         required=True,
         nargs=argparse.ONE_OR_MORE,
     )