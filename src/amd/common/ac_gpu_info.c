joshuaswarren/mesa-1

commit 7161084f4ce6c2b68dc0752a1c3d41cd744c1b90

ac/gpu_info: query the number of ip instance
query the number of ip instances for VCN and JPEG

v2: use num_queues and remove new variable (Marek)
v3: remove whitespace (Leo)
v4: remove redundant print jpeg_decode (Marek)

Signed-off-by: Sathishkumar S <sathishkumar.sundararaju@amd.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27884>

src/amd/common/ac_gpu_info.c
@@ -292,6 +292,11 @@ static int amdgpu_query_hw_ip_info(amdgpu_device_handle dev, unsigned type,
 {
    return -EINVAL;
 }
+static int amdgpu_query_hw_ip_count(amdgpu_device_handle dev, unsigned type,
+   uint32_t *count)
+{
+   return -EINVAL;
+}
 static int amdgpu_query_heap_info(amdgpu_device_handle dev, uint32_t heap,
    uint32_t flags, struct amdgpu_heap_info *info)
 {
@@ -678,7 +683,15 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
                   device_info.family == FAMILY_MDN)
             info->ip[AMD_IP_GFX].ver_minor = info->ip[AMD_IP_COMPUTE].ver_minor = 3;
       }
-      info->ip[ip_type].num_queues = util_bitcount(ip_info.available_rings);
+      if (ip_type >= AMD_IP_VCN_DEC && ip_type <= AMD_IP_VCN_JPEG) {
+         uint32_t num_inst;
+         r = amdgpu_query_hw_ip_count(dev, ip_type, &num_inst);
+         if (r)
+            fprintf(stderr, "amdgpu: failed to query ip count for vcn or jpeg\n");
+         else
+            info->ip[ip_type].num_queues = num_inst;
+      } else
+         info->ip[ip_type].num_queues = util_bitcount(ip_info.available_rings);
 
       /* According to the kernel, only SDMA and VPE require 256B alignment, but use it
        * for all queues because the kernel reports wrong limits for some of the queues.

commit cf39141c40f5ddfcd2ff2a80f6734bf8276e4248

ac/gpu_info: fix regression in vulkan hw decode
commit 7161084f4ce breaks vulkan hardware decoding. num_queues must
not be more than 1 for vcn ip since vcn has to retain context among
dependent jobs. use num_instances variable to track ip count.

Fixes: 7161084f4ce ("ac/gpu_info: query the number of ip instance")

v2: rebase on 7f7c18ed513 (ac/gpu_info: Add vcn dec and enc version query)

Signed-off-by: Sathishkumar S <sathishkumar.sundararaju@amd.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Acked-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28252>

src/amd/common/ac_gpu_info.c
@@ -599,6 +599,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    struct amdgpu_buffer_size_alignments alignment_info = {0};
    uint32_t vce_version = 0, vce_feature = 0, uvd_version = 0, uvd_feature = 0,
             vcn_version = 0, vcn_feature = 0;
+   uint32_t num_instances = 0;
    int r, i, j;
    amdgpu_device_handle dev = dev_p;
 
@@ -685,15 +686,12 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
                   device_info.family == FAMILY_MDN)
             info->ip[AMD_IP_GFX].ver_minor = info->ip[AMD_IP_COMPUTE].ver_minor = 3;
       }
-      if (ip_type >= AMD_IP_VCN_DEC && ip_type <= AMD_IP_VCN_JPEG) {
-         uint32_t num_inst;
-         r = amdgpu_query_hw_ip_count(dev, ip_type, &num_inst);
-         if (r)
-            fprintf(stderr, "amdgpu: failed to query ip count for vcn or jpeg\n");
-         else
-            info->ip[ip_type].num_queues = num_inst;
-      } else
-         info->ip[ip_type].num_queues = util_bitcount(ip_info.available_rings);
+      info->ip[ip_type].num_queues = util_bitcount(ip_info.available_rings);
+
+      /* query ip count */
+      r = amdgpu_query_hw_ip_count(dev, ip_type, &num_instances);
+      if (!r)
+         info->ip[ip_type].num_instances = num_instances;
 
       /* According to the kernel, only SDMA and VPE require 256B alignment, but use it
        * for all queues because the kernel reports wrong limits for some of the queues.

src/amd/common/ac_gpu_info.h
@@ -26,6 +26,7 @@ struct amd_ip_info {
    uint8_t ver_minor;
    uint8_t ver_rev;
    uint8_t num_queues;
+   uint8_t num_instances;
    uint32_t ib_alignment;
    uint32_t ib_pad_dw_mask;
 };