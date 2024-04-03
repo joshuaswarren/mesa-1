joshuaswarren/mesa-1

commit fbc4994e596c3d8baac016722d69499ab7896e4d

tu: Basic a7xx support
Works:
- sysmem rendering

Doesn't work:
- gmem rendering
- 3d blits
- TESS and GS

Wild Life Extreme benchmarks runs without issues, most Sascha Willems
Vulkan demos are working.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23217>

src/freedreno/vulkan/tu_cmd_buffer.h
@@ -599,11 +599,14 @@ void tu_render_pass_state_merge(struct tu_render_pass_state *dst,
 VkResult tu_cmd_buffer_begin(struct tu_cmd_buffer *cmd_buffer,
                              const VkCommandBufferBeginInfo *pBeginInfo);
 
+template <chip CHIP>
 void
 tu_emit_cache_flush(struct tu_cmd_buffer *cmd_buffer);
 
+template <chip CHIP>
 void tu_emit_cache_flush_renderpass(struct tu_cmd_buffer *cmd_buffer);
 
+template <chip CHIP>
 void tu_emit_cache_flush_ccu(struct tu_cmd_buffer *cmd_buffer,
                              struct tu_cs *cs,
                              enum tu_cmd_ccu_state ccu_state);
@@ -624,12 +627,16 @@ void
 tu_restore_suspended_pass(struct tu_cmd_buffer *cmd,
                           struct tu_cmd_buffer *suspended);
 
+template <chip CHIP>
 void tu_cmd_render(struct tu_cmd_buffer *cmd);
 
+enum fd_gpu_event : uint32_t;
+
+template <chip CHIP>
 void
-tu6_emit_event_write(struct tu_cmd_buffer *cmd,
-                     struct tu_cs *cs,
-                     enum vgt_event_type event);
+tu_emit_event_write(struct tu_cmd_buffer *cmd,
+                    struct tu_cs *cs,
+                    enum fd_gpu_event event);
 
 static inline struct tu_descriptor_state *
 tu_get_descriptors_state(struct tu_cmd_buffer *cmd_buffer,

src/freedreno/common/freedreno_gpu_event.h
@@ -1,0 +1,104 @@
+/*
+ * Copyright © 2023 Igalia S.L.
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef __FREEDRENO_GPU_EVENT_H__
+#define __FREEDRENO_GPU_EVENT_H__
+
+#include "adreno_pm4.xml.h"
+
+enum fd_gpu_event : uint32_t {
+    FD_WRITE_PRIMITIVE_COUNTS = 0,
+    FD_START_PRIMITIVE_CTRS,
+    FD_STOP_PRIMITIVE_CTRS,
+    FD_START_FRAGMENT_CTRS,
+    FD_STOP_FRAGMENT_CTRS,
+    FD_START_COMPUTE_CTRS,
+    FD_STOP_COMPUTE_CTRS,
+    FD_ZPASS_DONE,
+    FD_RB_DONE,
+    FD_FLUSH_SO_0,
+    FD_FLUSH_SO_1,
+    FD_FLUSH_SO_2,
+    FD_FLUSH_SO_3,
+    FD_CACHE_FLUSH,
+    FD_CACHE_INVALIDATE,
+    FD_CCU_INVALIDATE_DEPTH,
+    FD_CCU_INVALIDATE_COLOR,
+    FD_CCU_FLUSH_BLIT_CACHE,
+    FD_CCU_FLUSH_DEPTH,
+    FD_CCU_FLUSH_COLOR,
+    FD_LRZ_CLEAR,
+    FD_LRZ_FLUSH,
+    FD_BLIT,
+    FD_LABEL,
+
+    FD_GPU_EVENT_MAX,
+};
+
+struct fd_gpu_event_info {
+    enum vgt_event_type raw_event;
+    bool needs_seqno;
+};
+
+template <chip CHIP>
+constexpr struct fd_gpu_event_info fd_gpu_events[FD_GPU_EVENT_MAX] = {};
+
+template <>
+constexpr inline struct fd_gpu_event_info fd_gpu_events<A6XX>[FD_GPU_EVENT_MAX] = {
+    {WRITE_PRIMITIVE_COUNTS, false},  /* FD_WRITE_PRIMITIVE_COUNTS */
+    {START_PRIMITIVE_CTRS, false},    /* FD_START_PRIMITIVE_CTRS */
+    {STOP_PRIMITIVE_CTRS, false},     /* FD_STOP_PRIMITIVE_CTRS */
+    {START_FRAGMENT_CTRS, false},     /* FD_START_FRAGMENT_CTRS */
+    {STOP_FRAGMENT_CTRS, false},      /* FD_STOP_FRAGMENT_CTRS */
+    {START_COMPUTE_CTRS, false},      /* FD_START_COMPUTE_CTRS */
+    {STOP_COMPUTE_CTRS, false},       /* FD_STOP_COMPUTE_CTRS */
+    {ZPASS_DONE, false},              /* FD_ZPASS_DONE */
+    {RB_DONE_TS, true},               /* FD_RB_DONE */
+    {FLUSH_SO_0, false},              /* FD_FLUSH_SO_0 */
+    {FLUSH_SO_1, false},              /* FD_FLUSH_SO_1 */
+    {FLUSH_SO_2, false},              /* FD_FLUSH_SO_2 */
+    {FLUSH_SO_3, false},              /* FD_FLUSH_SO_3 */
+    {CACHE_FLUSH_TS, true},           /* FD_CACHE_FLUSH */
+    {CACHE_INVALIDATE, false},        /* FD_CACHE_INVALIDATE */
+    {PC_CCU_INVALIDATE_DEPTH, false}, /* FD_CCU_INVALIDATE_DEPTH */
+    {PC_CCU_INVALIDATE_COLOR, false}, /* FD_CCU_INVALIDATE_COLOR */
+    {PC_CCU_RESOLVE_TS, true},        /* FD_CCU_FLUSH_BLIT_CACHE */
+    {PC_CCU_FLUSH_DEPTH_TS, true},    /* FD_CCU_FLUSH_DEPTH */
+    {PC_CCU_FLUSH_COLOR_TS, true},    /* FD_CCU_FLUSH_COLOR */
+    {LRZ_CLEAR, false},               /* FD_LRZ_CLEAR */
+    {LRZ_FLUSH, false},               /* FD_LRZ_FLUSH */
+    {BLIT, false},                    /* FD_BLIT */
+    {LABEL, false},                   /* FD_LABEL */
+};
+
+template <>
+constexpr inline struct fd_gpu_event_info fd_gpu_events<A7XX>[FD_GPU_EVENT_MAX] = {
+    {WRITE_PRIMITIVE_COUNTS, false},  /* FD_WRITE_PRIMITIVE_COUNTS */
+    {START_PRIMITIVE_CTRS, false},    /* FD_START_PRIMITIVE_CTRS */
+    {STOP_PRIMITIVE_CTRS, false},     /* FD_STOP_PRIMITIVE_CTRS */
+    {START_FRAGMENT_CTRS, false},     /* FD_START_FRAGMENT_CTRS */
+    {STOP_FRAGMENT_CTRS, false},      /* FD_STOP_FRAGMENT_CTRS */
+    {START_COMPUTE_CTRS, false},      /* FD_START_COMPUTE_CTRS */
+    {STOP_COMPUTE_CTRS, false},       /* FD_STOP_COMPUTE_CTRS */
+    {ZPASS_DONE, false},              /* FD_ZPASS_DONE */
+    {RB_DONE_TS, true},               /* FD_RB_DONE */
+    {FLUSH_SO_0, false},              /* FD_FLUSH_SO_0 */
+    {FLUSH_SO_1, false},              /* FD_FLUSH_SO_1 */
+    {FLUSH_SO_2, false},              /* FD_FLUSH_SO_2 */
+    {FLUSH_SO_3, false},              /* FD_FLUSH_SO_3 */
+    {CACHE_FLUSH7, false},            /* FD_CACHE_FLUSH */
+    {CACHE_INVALIDATE7, false},       /* FD_CACHE_INVALIDATE */
+    {CCU_INVALIDATE_DEPTH, false},    /* FD_CCU_INVALIDATE_DEPTH */
+    {CCU_INVALIDATE_COLOR, false},    /* FD_CCU_INVALIDATE_COLOR */
+    {CCU_RESOLVE_CLEAN, false},       /* FD_CCU_FLUSH_BLIT_CACHE */
+    {CCU_FLUSH_DEPTH, false},         /* FD_CCU_FLUSH_DEPTH */
+    {CCU_FLUSH_COLOR, false},         /* FD_CCU_FLUSH_COLOR */
+    {LRZ_CLEAR, false},               /* FD_LRZ_CLEAR */
+    {LRZ_FLUSH, false},               /* FD_LRZ_FLUSH */
+    {BLIT, false},                    /* FD_BLIT */
+    {LABEL, false},                   /* FD_LABEL */
+};
+
+#endif

src/freedreno/registers/adreno/a6xx.xml
@@ -2107,8 +2107,8 @@ to upconvert to 32b float internally?
 	<reg32 offset="0x8113" name="GRAS_UNKNOWN_8113" variants="A7XX-" usage="rp_blit"/>
 
 	<!-- Always written together and always equal 09510840 00000a62 -->
-	<reg32 offset="0x8120" name="GRAS_UNKNOWN_8120" variants="A7XX-"/>
-	<reg32 offset="0x8121" name="GRAS_UNKNOWN_8121" variants="A7XX-"/>
+	<reg32 offset="0x8120" name="GRAS_UNKNOWN_8120" variants="A7XX-" usage="cmd"/>
+	<reg32 offset="0x8121" name="GRAS_UNKNOWN_8121" variants="A7XX-" usage="cmd"/>
 
 	<!-- 0x8112-0x83ff invalid -->
 
@@ -4001,7 +4001,7 @@ to upconvert to 32b float internally?
 	<!-- Used in VK_KHR_fragment_shading_rate -->
 	<reg32 offset="0xa9ad" name="HLSQ_UNKNOWN_A9AD" variants="A7XX-"/>
 
-	<reg32 offset="0xa9ae" name="HLSQ_UNKNOWN_A9AE" variants="A7XX-">
+	<reg32 offset="0xa9ae" name="HLSQ_UNKNOWN_A9AE" variants="A7XX-" usage="cmd">
 		<bitfield name="UNK0" low="0" high="7" type="uint"/>
 		<bitfield name="UNK8" pos="8" type="boolean"/>
 	</reg32>

src/freedreno/vulkan/tu_clear_blit.h
@@ -16,12 +16,15 @@ void tu_init_clear_blit_shaders(struct tu_device *dev);
 
 void tu_destroy_clear_blit_shaders(struct tu_device *dev);
 
+template <chip CHIP>
 void
 tu6_clear_lrz(struct tu_cmd_buffer *cmd, struct tu_cs *cs, struct tu_image* image, const VkClearValue *value);
 
+template <chip CHIP>
 void
 tu6_dirty_lrz_fc(struct tu_cmd_buffer *cmd, struct tu_cs *cs, struct tu_image* image);
 
+template <chip CHIP>
 void
 tu_resolve_sysmem(struct tu_cmd_buffer *cmd,
                   struct tu_cs *cs,
@@ -31,16 +34,19 @@ tu_resolve_sysmem(struct tu_cmd_buffer *cmd,
                   uint32_t layers,
                   const VkRect2D *rect);
 
+template <chip CHIP>
 void
 tu_clear_sysmem_attachment(struct tu_cmd_buffer *cmd,
                            struct tu_cs *cs,
                            uint32_t a);
 
+template <chip CHIP>
 void
 tu_clear_gmem_attachment(struct tu_cmd_buffer *cmd,
                          struct tu_cs *cs,
                          uint32_t a);
 
+template <chip CHIP>
 void
 tu_load_gmem_attachment(struct tu_cmd_buffer *cmd,
                         struct tu_cs *cs,
@@ -49,6 +55,7 @@ tu_load_gmem_attachment(struct tu_cmd_buffer *cmd,
                         bool force_load);
 
 /* note: gmem store can also resolve */
+template <chip CHIP>
 void
 tu_store_gmem_attachment(struct tu_cmd_buffer *cmd,
                          struct tu_cs *cs,

src/freedreno/vulkan/tu_cs.h
@@ -498,6 +498,7 @@ struct tu_reg_value {
 #define __bo_type struct tu_bo *
 
 #include "a6xx-pack.xml.h"
+#include "adreno-pm4-pack.xml.h"
 
 #define __assert_eq(a, b)                                               \
    do {                                                                 \

src/freedreno/vulkan/tu_device.cc
@@ -612,7 +612,8 @@ tu_physical_device_init(struct tu_physical_device *device,
       goto fail_free_name;
    }
    switch (fd_dev_gen(&device->dev_id)) {
-   case 6: {
+   case 6:
+   case 7: {
       device->info = info;
       uint32_t depth_cache_size =
          device->info->num_ccu * device->info->a6xx.sysmem_per_ccu_cache_size;

commit 20d3ce1b8630de78028fa6488d9b451d83a130d1

tu: Implement CCHE invalidation
We need invalidate CCHE when we optimize out an invalidation of UCHE,
for example a storage image write to texture read. We missed this
earlier because of the blob's tendency to always over-flush, but the
blob does use this when building acceleration structures.

Fixes: fbc4994e596 ("tu: Basic a7xx support")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28445>

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -203,6 +203,10 @@ tu6_emit_flushes(struct tu_cmd_buffer *cmd_buffer,
        * via UCHE. This isn't necessary on A6XX, all writes should be visible implictly.
        */
       tu_emit_event_write<CHIP>(cmd_buffer, cs, FD_CCU_FLUSH_BLIT_CACHE);
+   if (CHIP >= A7XX && (flushes & TU_CMD_FLAG_CCHE_INVALIDATE) &&
+       /* Invalidating UCHE seems to also invalidate CCHE */
+       !(flushes & TU_CMD_FLAG_CACHE_INVALIDATE))
+      tu_cs_emit_pkt7(cs, CP_CCHE_INVALIDATE, 0);
    if (flushes & TU_CMD_FLAG_WAIT_MEM_WRITES)
       tu_cs_emit_pkt7(cs, CP_WAIT_MEM_WRITES, 0);
    if (flushes & TU_CMD_FLAG_WAIT_FOR_IDLE)
@@ -3365,6 +3369,13 @@ tu_flush_for_access(struct tu_cache_state *cache,
       flush_bits |= TU_CMD_FLAG_BINDLESS_DESCRIPTOR_INVALIDATE;
    }
 
+   /* There are multiple incoherent copies of CCHE, so any read through it may
+    * require invalidating it and we cannot optimize away invalidates.
+    */
+   if (dst_mask & TU_ACCESS_CCHE_READ) {
+      flush_bits |= TU_CMD_FLAG_CCHE_INVALIDATE;
+   }
+
    /* The blit cache is a special case dependency between CP_EVENT_WRITE::BLIT
     * (from GMEM loads/clears) to any GMEM attachment reads done via the UCHE
     * (Eg: Input attachments/CP_BLIT) which needs an explicit BLIT_CACHE_FLUSH
@@ -3484,7 +3495,7 @@ vk2tu_access(VkAccessFlags2 flags, VkPipelineStageFlags2 stages, bool image_only
                        VK_PIPELINE_STAGE_2_VERTEX_INPUT_BIT |
                        VK_PIPELINE_STAGE_2_VERTEX_ATTRIBUTE_INPUT_BIT |
                        SHADER_STAGES))
-       mask |= TU_ACCESS_UCHE_READ;
+       mask |= TU_ACCESS_UCHE_READ | TU_ACCESS_CCHE_READ;
 
    if (gfx_read_access(flags, stages,
                        VK_ACCESS_2_INPUT_ATTACHMENT_READ_BIT,
@@ -3494,7 +3505,8 @@ vk2tu_access(VkAccessFlags2 flags, VkPipelineStageFlags2 stages, bool image_only
    if (gfx_read_access(flags, stages,
                        VK_ACCESS_2_DESCRIPTOR_BUFFER_READ_BIT_EXT,
                        SHADER_STAGES)) {
-      mask |= TU_ACCESS_UCHE_READ | TU_ACCESS_BINDLESS_DESCRIPTOR_READ;
+      mask |= TU_ACCESS_UCHE_READ | TU_ACCESS_BINDLESS_DESCRIPTOR_READ |
+              TU_ACCESS_CCHE_READ;
    }
 
    if (gfx_write_access(flags, stages,

src/freedreno/vulkan/tu_cmd_buffer.h
@@ -138,6 +138,13 @@ enum tu_cmd_access_mask {
    /* Similar to UCHE_READ, but specifically for GMEM attachment reads. */
    TU_ACCESS_UCHE_READ_GMEM = 1 << 15,
 
+   /* The CCHE is a write-through cache which sits behind UCHE, with multiple
+    * incoherent copies. Because it's write-through we only have to worry
+    * about invalidating it for reads. It's invalidated by "ccinv" in the
+    * shader and CP_CCHE_INVALIDATE in the command stream.
+    */
+   TU_ACCESS_CCHE_READ = 1 << 16,
+
    TU_ACCESS_READ =
       TU_ACCESS_UCHE_READ |
       TU_ACCESS_CCU_COLOR_READ |
@@ -145,7 +152,8 @@ enum tu_cmd_access_mask {
       TU_ACCESS_CCU_COLOR_INCOHERENT_READ |
       TU_ACCESS_CCU_DEPTH_INCOHERENT_READ |
       TU_ACCESS_SYSMEM_READ |
-      TU_ACCESS_BINDLESS_DESCRIPTOR_READ,
+      TU_ACCESS_BINDLESS_DESCRIPTOR_READ |
+      TU_ACCESS_CCHE_READ,
 
    TU_ACCESS_WRITE =
       TU_ACCESS_UCHE_WRITE |
@@ -192,14 +200,15 @@ enum tu_cmd_flush_bits {
    TU_CMD_FLAG_CCU_INVALIDATE_COLOR = 1 << 3,
    TU_CMD_FLAG_CACHE_FLUSH = 1 << 4,
    TU_CMD_FLAG_CACHE_INVALIDATE = 1 << 5,
-   TU_CMD_FLAG_WAIT_MEM_WRITES = 1 << 6,
-   TU_CMD_FLAG_WAIT_FOR_IDLE = 1 << 7,
-   TU_CMD_FLAG_WAIT_FOR_ME = 1 << 8,
-   TU_CMD_FLAG_BINDLESS_DESCRIPTOR_INVALIDATE = 1 << 9,
+   TU_CMD_FLAG_CCHE_INVALIDATE = 1 << 6,
+   TU_CMD_FLAG_WAIT_MEM_WRITES = 1 << 7,
+   TU_CMD_FLAG_WAIT_FOR_IDLE = 1 << 8,
+   TU_CMD_FLAG_WAIT_FOR_ME = 1 << 9,
+   TU_CMD_FLAG_BINDLESS_DESCRIPTOR_INVALIDATE = 1 << 10,
    /* This is an unusual flush that isn't automatically executed if pending,
     * as it isn't necessary. Therefore, it's not included in ALL_FLUSH.
     */
-   TU_CMD_FLAG_BLIT_CACHE_FLUSH = 1 << 10,
+   TU_CMD_FLAG_BLIT_CACHE_FLUSH = 1 << 11,
 
    TU_CMD_FLAG_ALL_FLUSH =
       TU_CMD_FLAG_CCU_FLUSH_DEPTH |
@@ -215,6 +224,7 @@ enum tu_cmd_flush_bits {
       TU_CMD_FLAG_CCU_INVALIDATE_COLOR |
       TU_CMD_FLAG_CACHE_INVALIDATE |
       TU_CMD_FLAG_BINDLESS_DESCRIPTOR_INVALIDATE |
+      TU_CMD_FLAG_CCHE_INVALIDATE |
       /* Treat CP_WAIT_FOR_ME as a "cache" that needs to be invalidated when a
        * a command that needs CP_WAIT_FOR_ME is executed. This means we may
        * insert an extra WAIT_FOR_ME before an indirect command requiring it