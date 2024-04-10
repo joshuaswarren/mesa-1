joshuaswarren/mesa-1

commit d53c8b073b5814cc8d91875c244dabd3c784c897

intel/dev: Add 0x56ba-0x56bd DG2 PCI IDs
Cc: mesa-stable
Ref: https://lists.freedesktop.org/archives/intel-gfx/2023-October/337287.html
Ref: BSpec 44477
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25673>

include/pci_ids/iris_pci_ids.h
@@ -245,6 +245,10 @@ CHIPSET(0x56b0, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A30M Graphics")
 CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A40/A50 Graphics")
 CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56ba, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56bb, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56bc, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56bd, dg2_g11, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56c0, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex Series 170 Graphics")
 CHIPSET(0x56c1, atsm_g11, "ATS-M", "Intel(R) Data Center GPU Flex Series 140 Graphics")
 

commit c9361f569954f6a621f4624e769d28b04578e107

intel/dev: update DG2 device names
Ref: https://ark.intel.com/content/www/us/en/ark/products/237549/intel-arc-a380e-graphics.html
Ref: https://ark.intel.com/content/www/us/en/ark/products/237552/intel-arc-a310e-graphics.html
Ref: https://ark.intel.com/content/www/us/en/ark/products/237550/intel-arc-a370e-graphics.html
Ref: https://ark.intel.com/content/www/us/en/ark/products/237551/intel-arc-a350e-graphics.html
Ref: https://github.com/intel/compute-runtime/blob/864f42116cf4b0f5a91699cfe099d0c9186ca45b/shared/source/dll/devices/devices_base.inl#L49
Fixes: d53c8b073b5 ("intel/dev: Add 0x56ba-0x56bd DG2 PCI IDs")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28643>

include/pci_ids/iris_pci_ids.h
@@ -247,10 +247,10 @@ CHIPSET(0x56b0, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A30M Graphics")
 CHIPSET(0x56b1, dg2_g11, "DG2", "Intel(R) Arc(tm) Pro A40/A50 Graphics")
 CHIPSET(0x56b2, dg2_g12, "DG2", "Intel(R) Arc(tm) Pro A60M Graphics")
 CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Arc(tm) Pro A60 Graphics")
-CHIPSET(0x56ba, dg2_g11, "DG2", "Intel(R) Graphics")
-CHIPSET(0x56bb, dg2_g11, "DG2", "Intel(R) Graphics")
-CHIPSET(0x56bc, dg2_g11, "DG2", "Intel(R) Graphics")
-CHIPSET(0x56bd, dg2_g11, "DG2", "Intel(R) Graphics")
+CHIPSET(0x56ba, dg2_g11, "DG2", "Intel(R) Arc(tm) A380E Graphics")
+CHIPSET(0x56bb, dg2_g11, "DG2", "Intel(R) Arc(tm) A310E Graphics")
+CHIPSET(0x56bc, dg2_g11, "DG2", "Intel(R) Arc(tm) A370E Graphics")
+CHIPSET(0x56bd, dg2_g11, "DG2", "Intel(R) Arc(tm) A350E Graphics")
 CHIPSET(0x56be, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56bf, dg2_g10, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56c0, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170")