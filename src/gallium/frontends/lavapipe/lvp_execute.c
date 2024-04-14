joshuaswarren/mesa-1

commit 032394e5f108c75fca5387246d5937a8a2f9e03e

lavapipe: Implement VK_KHR_acceleration_structure
Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25616>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -24,6 +24,7 @@
 /* use a gallium context to execute a command buffer */
 
 #include "lvp_private.h"
+#include "lvp_acceleration_structure.h"
 
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
@@ -3829,7 +3830,7 @@ static void handle_draw_mesh_tasks_indirect_count(struct vk_cmd_queue_entry *cmd
 }
 
 static VkBuffer
-get_buffer(struct rendering_state *state, uint8_t *ptr, size_t *offset)
+get_buffer(struct rendering_state *state, const uint8_t *ptr, size_t *offset)
 {
    simple_mtx_lock(&state->device->bda_lock);
    hash_table_foreach(&state->device->bda, he) {
@@ -4309,6 +4310,109 @@ handle_dispatch_graph(struct vk_cmd_queue_entry *cmd, struct rendering_state *st
 }
 #endif
 
+static struct pipe_resource *
+get_buffer_pipe(struct rendering_state *state, const void *ptr)
+{
+   size_t offset;
+   VK_FROM_HANDLE(lvp_buffer, buffer, get_buffer(state, ptr, &offset));
+   return buffer->bo;
+}
+
+static void
+handle_copy_acceleration_structure(struct vk_cmd_queue_entry *cmd, struct rendering_state *state)
+{
+   struct vk_cmd_copy_acceleration_structure_khr *copy = &cmd->u.copy_acceleration_structure_khr;
+
+   VK_FROM_HANDLE(vk_acceleration_structure, src, copy->info->src);
+   VK_FROM_HANDLE(vk_acceleration_structure, dst, copy->info->dst);
+
+   struct pipe_box box = { 0 };
+   u_box_1d(src->offset, MIN2(src->size, dst->size), &box);
+   state->pctx->resource_copy_region(state->pctx, lvp_buffer_from_handle(dst->buffer)->bo, 0,
+                                     dst->offset, 0, 0,
+                                     lvp_buffer_from_handle(src->buffer)->bo, 0, &box);
+}
+
+static void
+handle_copy_memory_to_acceleration_structure(struct vk_cmd_queue_entry *cmd, struct rendering_state *state)
+{
+   struct vk_cmd_copy_memory_to_acceleration_structure_khr *copy = &cmd->u.copy_memory_to_acceleration_structure_khr;
+
+   VK_FROM_HANDLE(vk_acceleration_structure, accel_struct, copy->info->dst);
+
+   struct lvp_bvh_header *dst = (void *)(uintptr_t)vk_acceleration_structure_get_va(accel_struct);
+   const struct lvp_accel_struct_serialization_header *src = copy->info->src.hostAddress;
+
+   memcpy(dst, &src->instances[src->instance_count], src->compacted_size);
+
+   for (uint32_t i = 0; i < src->instance_count; i++) {
+      uint8_t *leaf_nodes = (uint8_t *)dst;
+      leaf_nodes += dst->leaf_nodes_offset;
+      struct lvp_bvh_instance_node *node = (struct lvp_bvh_instance_node *)leaf_nodes;
+      node[i].bvh_ptr = src->instances[i];
+   }
+}
+
+static void
+handle_copy_acceleration_structure_to_memory(struct vk_cmd_queue_entry *cmd, struct rendering_state *state)
+{
+   struct vk_cmd_copy_acceleration_structure_to_memory_khr *copy = &cmd->u.copy_acceleration_structure_to_memory_khr;
+
+   VK_FROM_HANDLE(vk_acceleration_structure, accel_struct, copy->info->src);
+
+   struct lvp_bvh_header *src = (void *)(uintptr_t)vk_acceleration_structure_get_va(accel_struct);
+   struct lvp_accel_struct_serialization_header *dst = copy->info->dst.hostAddress;
+
+   lvp_device_get_cache_uuid(dst->driver_uuid);
+   lvp_device_get_cache_uuid(dst->accel_struct_compat);
+   dst->serialization_size = src->serialization_size;
+   dst->compacted_size = accel_struct->size;
+   dst->instance_count = src->instance_count;
+
+   for (uint32_t i = 0; i < src->instance_count; i++) {
+      uint8_t *leaf_nodes = (uint8_t *)src;
+      leaf_nodes += src->leaf_nodes_offset;
+      struct lvp_bvh_instance_node *node = (struct lvp_bvh_instance_node *)leaf_nodes;
+      dst->instances[i] = node[i].bvh_ptr;
+   }
+
+   memcpy(&dst->instances[dst->instance_count], src, accel_struct->size);
+}
+
+static void
+handle_build_acceleration_structures(struct vk_cmd_queue_entry *cmd, struct rendering_state *state)
+{
+   struct vk_cmd_build_acceleration_structures_khr *build = &cmd->u.build_acceleration_structures_khr;
+
+   for (uint32_t i = 0; i < build->info_count; i++)
+      lvp_build_acceleration_structure(&build->infos[i], build->pp_build_range_infos[i]);
+}
+
+static void
+handle_write_acceleration_structures_properties(struct vk_cmd_queue_entry *cmd, struct rendering_state *state)
+{
+   struct vk_cmd_write_acceleration_structures_properties_khr *write = &cmd->u.write_acceleration_structures_properties_khr;
+
+   VK_FROM_HANDLE(lvp_query_pool, pool, write->query_pool);
+
+   uint64_t *dst = pool->data;
+   dst += write->first_query;
+
+   for (uint32_t i = 0; i < write->acceleration_structure_count; i++) {
+      VK_FROM_HANDLE(vk_acceleration_structure, accel_struct, write->acceleration_structures[i]);
+
+      if (write->query_type == VK_QUERY_TYPE_ACCELERATION_STRUCTURE_COMPACTED_SIZE_KHR) {
+         dst[i] = accel_struct->size;
+         continue;
+      }
+
+      assert (write->query_type == VK_QUERY_TYPE_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE_KHR);
+
+      struct lvp_bvh_header *header = (void *)(uintptr_t)vk_acceleration_structure_get_va(accel_struct);
+      dst[i] = header->serialization_size;
+   }
+}
+
 void lvp_add_enqueue_cmd_entrypoints(struct vk_device_dispatch_table *disp)
 {
    struct vk_device_dispatch_table cmd_enqueue_dispatch;
@@ -4448,6 +4552,13 @@ void lvp_add_enqueue_cmd_entrypoints(struct vk_device_dispatch_table *disp)
 
    ENQUEUE_CMD(CmdSetRenderingAttachmentLocationsKHR)
    ENQUEUE_CMD(CmdSetRenderingInputAttachmentIndicesKHR)
+
+   ENQUEUE_CMD(CmdCopyAccelerationStructureKHR)
+   ENQUEUE_CMD(CmdCopyMemoryToAccelerationStructureKHR)
+   ENQUEUE_CMD(CmdCopyAccelerationStructureToMemoryKHR)
+   ENQUEUE_CMD(CmdBuildAccelerationStructuresKHR)
+   ENQUEUE_CMD(CmdBuildAccelerationStructuresIndirectKHR)
+   ENQUEUE_CMD(CmdWriteAccelerationStructuresPropertiesKHR)
 
 #undef ENQUEUE_CMD
 }
@@ -4814,6 +4925,23 @@ static void lvp_execute_cmd_buffer(struct list_head *cmds,
          break;
       case VK_CMD_SET_RENDERING_INPUT_ATTACHMENT_INDICES_KHR:
          handle_rendering_input_attachment_indices(cmd, state);
+         break;
+      case VK_CMD_COPY_ACCELERATION_STRUCTURE_KHR:
+         handle_copy_acceleration_structure(cmd, state);
+         break;
+      case VK_CMD_COPY_MEMORY_TO_ACCELERATION_STRUCTURE_KHR:
+         handle_copy_memory_to_acceleration_structure(cmd, state);
+         break;
+      case VK_CMD_COPY_ACCELERATION_STRUCTURE_TO_MEMORY_KHR:
+         handle_copy_acceleration_structure_to_memory(cmd, state);
+         break;
+      case VK_CMD_BUILD_ACCELERATION_STRUCTURES_KHR:
+         handle_build_acceleration_structures(cmd, state);
+         break;
+      case VK_CMD_BUILD_ACCELERATION_STRUCTURES_INDIRECT_KHR:
+         break;
+      case VK_CMD_WRITE_ACCELERATION_STRUCTURES_PROPERTIES_KHR:
+         handle_write_acceleration_structures_properties(cmd, state);
          break;
       default:
          fprintf(stderr, "Unsupported command %s\n", vk_cmd_queue_type_names[cmd->type]);

src/gallium/auxiliary/gallivm/lp_bld_jit_types.h
@@ -237,6 +237,7 @@ struct lp_descriptor {
          struct lp_jit_image image;
       };
       struct lp_jit_buffer buffer;
+      uint64_t accel_struct;
    };
 
    /* Store sample/image functions in the same location since some d3d12 games

src/gallium/frontends/lavapipe/lvp_acceleration_structure.h
@@ -1,0 +1,108 @@
+
+/*
+ * Copyright © 2021 Google
+ * Copyright © 2023 Valve Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef LVP_ACCELERATION_STRUCTURE_H
+#define LVP_ACCELERATION_STRUCTURE_H
+
+#include "lvp_private.h"
+
+#define LVP_GEOMETRY_OPAQUE (1u << 31)
+
+#define LVP_INSTANCE_FORCE_OPAQUE                 (1u << 31)
+#define LVP_INSTANCE_NO_FORCE_NOT_OPAQUE          (1u << 30)
+#define LVP_INSTANCE_TRIANGLE_FACING_CULL_DISABLE (1u << 29)
+#define LVP_INSTANCE_TRIANGLE_FLIP_FACING         (1u << 28)
+
+#define lvp_bvh_node_triangle 0
+#define lvp_bvh_node_internal 1
+#define lvp_bvh_node_instance 2
+#define lvp_bvh_node_aabb     3
+
+typedef struct {
+   float values[3][4];
+} lvp_mat3x4;
+
+typedef struct {
+   float x;
+   float y;
+   float z;
+} lvp_vec3;
+
+typedef struct lvp_aabb {
+   lvp_vec3 min;
+   lvp_vec3 max;
+} lvp_aabb;
+
+struct lvp_bvh_triangle_node {
+   float coords[3][3];
+
+   uint32_t padding;
+
+   uint32_t primitive_id;
+   /* flags in upper 4 bits */
+   uint32_t geometry_id_and_flags;
+};
+
+struct lvp_bvh_aabb_node {
+   lvp_aabb bounds;
+
+   uint32_t primitive_id;
+   /* flags in upper 4 bits */
+   uint32_t geometry_id_and_flags;
+};
+
+struct lvp_bvh_instance_node {
+   uint64_t bvh_ptr;
+
+   /* lower 24 bits are the custom instance index, upper 8 bits are the visibility mask */
+   uint32_t custom_instance_and_mask;
+   /* lower 24 bits are the sbt offset, upper 8 bits are VkGeometryInstanceFlagsKHR */
+   uint32_t sbt_offset_and_flags;
+
+   lvp_mat3x4 wto_matrix;
+   uint32_t padding;
+
+   uint32_t instance_id;
+
+   /* Object to world matrix transposed from the initial transform. */
+   lvp_mat3x4 otw_matrix;
+};
+
+struct lvp_bvh_box_node {
+   lvp_aabb bounds[2];
+   uint32_t children[2];
+};
+
+struct lvp_bvh_header {
+   lvp_aabb bounds;
+
+   uint32_t serialization_size;
+   uint32_t instance_count;
+   uint32_t leaf_nodes_offset;
+
+   uint32_t padding;
+};
+
+struct lvp_accel_struct_serialization_header {
+   uint8_t driver_uuid[VK_UUID_SIZE];
+   uint8_t accel_struct_compat[VK_UUID_SIZE];
+   uint64_t serialization_size;
+   uint64_t compacted_size;
+   uint64_t instance_count;
+   uint64_t instances[];
+};
+
+/* The root node is the first node after the header. */
+#define LVP_BVH_ROOT_NODE_OFFSET (sizeof(struct lvp_bvh_header))
+#define LVP_BVH_ROOT_NODE        (LVP_BVH_ROOT_NODE_OFFSET | lvp_bvh_node_internal)
+#define LVP_BVH_INVALID_NODE     0xFFFFFFFF
+
+void
+lvp_build_acceleration_structure(VkAccelerationStructureBuildGeometryInfoKHR *info,
+                                 const VkAccelerationStructureBuildRangeInfoKHR *ranges);
+
+#endif

src/gallium/frontends/lavapipe/lvp_cmd_buffer.c
@@ -138,6 +138,9 @@ VKAPI_ATTR void VKAPI_CALL lvp_CmdPushDescriptorSetWithTemplate2KHR(
       case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
          info_size += sizeof(VkBufferView) * entry->descriptorCount;
          break;
+      case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR:
+         info_size += sizeof(VkAccelerationStructureKHR) * entry->descriptorCount;
+         break;
       case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
       case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER:
       case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
@@ -300,6 +303,23 @@ VKAPI_ATTR void VKAPI_CALL lvp_CmdPushDescriptorSet2KHR(
                   typed_memcpy(arr, write->pBufferInfo, write->descriptorCount);
                }
                break;
+
+            case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR: {
+               const VkWriteDescriptorSetAccelerationStructureKHR *accel_structs =
+                  vk_find_struct_const(write->pNext, WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR);
+
+               uint32_t accel_structs_size = sizeof(VkAccelerationStructureKHR) * accel_structs->accelerationStructureCount;
+               VkWriteDescriptorSetAccelerationStructureKHR *write_accel_structs =
+                  rzalloc_size(ctx, sizeof(VkWriteDescriptorSetAccelerationStructureKHR) + accel_structs_size);
+            
+               write_accel_structs->sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR;
+               write_accel_structs->accelerationStructureCount = accel_structs->accelerationStructureCount;
+               write_accel_structs->pAccelerationStructures = (void *)&write_accel_structs[1];
+               memcpy((void *)write_accel_structs->pAccelerationStructures, accel_structs->pAccelerationStructures, accel_structs_size);
+
+               dstwrite->pNext = write_accel_structs;
+               break;
+            }
 
             default:
                break;

src/gallium/frontends/lavapipe/lvp_descriptor_set.c
@@ -22,6 +22,7 @@
  */
 
 #include "lvp_private.h"
+#include "vk_acceleration_structure.h"
 #include "vk_descriptors.h"
 #include "vk_util.h"
 #include "util/u_math.h"
@@ -617,7 +618,18 @@ VKAPI_ATTR void VKAPI_CALL lvp_UpdateDescriptorSets(
          }
          break;
 
+      case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR:
+         for (uint32_t j = 0; j < write->descriptorCount; j++) {
+            const VkWriteDescriptorSetAccelerationStructureKHR *accel_structs =
+               vk_find_struct_const(write->pNext, WRITE_DESCRIPTOR_SET_ACCELERATION_STRUCTURE_KHR);
+            VK_FROM_HANDLE(vk_acceleration_structure, accel_struct, accel_structs->pAccelerationStructures[j]);
+
+            desc[j].accel_struct = accel_struct ? vk_acceleration_structure_get_va(accel_struct) : 0;
+         }
+         break;
+
       default:
+         unreachable("Unsupported descriptor type");
          break;
       }
    }
@@ -802,6 +814,8 @@ lvp_descriptor_update_template_entry_size(VkDescriptorType type)
    case VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
    case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
       return sizeof(VkBufferView);
+   case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR:
+      return sizeof(VkAccelerationStructureKHR);
    case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
    case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER:
    case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
@@ -984,7 +998,15 @@ lvp_descriptor_set_update_with_template(VkDevice _device, VkDescriptorSet descri
             }
             break;
          }
+
+         case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR: {
+            VK_FROM_HANDLE(vk_acceleration_structure, accel_struct, *(VkAccelerationStructureKHR *)pSrc);
+            desc[idx].accel_struct = accel_struct ? vk_acceleration_structure_get_va(accel_struct) : 0;
+            break;
+         }
+
          default:
+            unreachable("Unsupported descriptor type");
             break;
          }
 
@@ -1159,7 +1181,12 @@ VKAPI_ATTR void VKAPI_CALL lvp_GetDescriptorEXT(
       }
       break;
    }
+   case VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR: {
+      desc->accel_struct = pCreateInfo->data.accelerationStructure;
+      break;
+   }
    default:
+      unreachable("Unsupported descriptor type");
       break;
    }
 }

src/gallium/frontends/lavapipe/lvp_private.h
@@ -56,6 +56,7 @@ typedef uint32_t xcb_window_t;
 #include <vulkan/vk_icd.h>
 
 #include "lvp_entrypoints.h"
+#include "vk_acceleration_structure.h"
 #include "vk_buffer.h"
 #include "vk_buffer_view.h"
 #include "vk_device.h"
@@ -581,12 +582,16 @@ struct lvp_buffer_view {
    struct lp_texture_handle *image_handle;
 };
 
+#define LVP_QUERY_ACCELERATION_STRUCTURE_COMPACTED_SIZE (PIPE_QUERY_TYPES)
+#define LVP_QUERY_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE (PIPE_QUERY_TYPES + 1)
+
 struct lvp_query_pool {
    struct vk_object_base base;
    VkQueryType type;
    uint32_t count;
    VkQueryPipelineStatisticFlags pipeline_stats;
    enum pipe_query_type base_type;
+   void *data; /* Used by queries that are not implemented by pipe_query */
    struct pipe_query *queries[0];
 };
 

src/gallium/frontends/lavapipe/lvp_query.c
@@ -32,6 +32,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateQueryPool(
 {
    LVP_FROM_HANDLE(lvp_device, device, _device);
 
+   uint32_t query_size = sizeof(struct pipe_query *);
    enum pipe_query_type pipeq;
    switch (pCreateInfo->queryType) {
    case VK_QUERY_TYPE_OCCLUSION:
@@ -50,13 +51,21 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateQueryPool(
    case VK_QUERY_TYPE_MESH_PRIMITIVES_GENERATED_EXT:
       pipeq = PIPE_QUERY_PRIMITIVES_GENERATED;
       break;
+   case VK_QUERY_TYPE_ACCELERATION_STRUCTURE_COMPACTED_SIZE_KHR:
+      query_size = sizeof(uint64_t);
+      pipeq = LVP_QUERY_ACCELERATION_STRUCTURE_COMPACTED_SIZE;
+      break;
+   case VK_QUERY_TYPE_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE_KHR:
+      query_size = sizeof(uint64_t);
+      pipeq = LVP_QUERY_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE;
+      break;
    default:
       return VK_ERROR_FEATURE_NOT_PRESENT;
    }
 
    struct lvp_query_pool *pool;
    size_t pool_size = sizeof(*pool)
-      + pCreateInfo->queryCount * sizeof(struct pipe_query *);
+      + pCreateInfo->queryCount * query_size;
 
    pool = vk_zalloc2(&device->vk.alloc, pAllocator,
                     pool_size, 8,
@@ -70,6 +79,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateQueryPool(
    pool->count = pCreateInfo->queryCount;
    pool->base_type = pipeq;
    pool->pipeline_stats = pCreateInfo->pipelineStatistics;
+   pool->data = &pool->queries;
 
    *pQueryPool = lvp_query_pool_to_handle(pool);
    return VK_SUCCESS;
@@ -86,9 +96,12 @@ VKAPI_ATTR void VKAPI_CALL lvp_DestroyQueryPool(
    if (!pool)
       return;
 
-   for (unsigned i = 0; i < pool->count; i++)
-      if (pool->queries[i])
-         device->queue.ctx->destroy_query(device->queue.ctx, pool->queries[i]);
+   if (pool->base_type != LVP_QUERY_ACCELERATION_STRUCTURE_COMPACTED_SIZE &&
+       pool->base_type != LVP_QUERY_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE) {
+      for (unsigned i = 0; i < pool->count; i++)
+         if (pool->queries[i])
+            device->queue.ctx->destroy_query(device->queue.ctx, pool->queries[i]);
+   }
    vk_object_base_finish(&pool->base);
    vk_free2(&device->vk.alloc, pAllocator, pool);
 }
@@ -113,6 +126,20 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_GetQueryPoolResults(
       uint8_t *dest = (uint8_t *)((char *)pData + (stride * (i - firstQuery)));
       union pipe_query_result result;
       bool ready = false;
+
+      if (pool->base_type == LVP_QUERY_ACCELERATION_STRUCTURE_COMPACTED_SIZE ||
+          pool->base_type == LVP_QUERY_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE) {
+         if (flags & VK_QUERY_RESULT_64_BIT) {
+            uint64_t *dst = (uint64_t *)dest;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = src[i];
+         } else {
+            uint32_t *dst = (uint32_t *)dest;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = src[i];
+         }
+         continue;
+      }
 
       if (pool->queries[i]) {
          ready = device->queue.ctx->get_query_result(device->queue.ctx,
@@ -193,6 +220,10 @@ VKAPI_ATTR void VKAPI_CALL lvp_ResetQueryPool(
 {
    LVP_FROM_HANDLE(lvp_device, device, _device);
    LVP_FROM_HANDLE(lvp_query_pool, pool, queryPool);
+
+   if (pool->base_type == LVP_QUERY_ACCELERATION_STRUCTURE_COMPACTED_SIZE ||
+       pool->base_type == LVP_QUERY_ACCELERATION_STRUCTURE_SERIALIZATION_SIZE)
+      return;
 
    for (uint32_t i = 0; i < queryCount; i++) {
       uint32_t idx = i + firstQuery;

commit 5c90a87d866e0ed81d50c8498070e2c66fa2c523

lavapipe: Handle accel struct queries in handle_copy_query_pool_results
Fixes: 032394e ("lavapipe: Implement VK_KHR_acceleration_structure")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28716>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -2980,6 +2980,27 @@ static void handle_copy_query_pool_results(struct vk_cmd_queue_entry *cmd,
    unsigned result_size = copycmd->flags & VK_QUERY_RESULT_64_BIT ? 8 : 4;
    for (unsigned i = copycmd->first_query; i < copycmd->first_query + copycmd->query_count; i++) {
       unsigned offset = copycmd->dst_offset + (copycmd->stride * (i - copycmd->first_query));
+
+      if (pool->base_type >= PIPE_QUERY_TYPES) {
+         struct pipe_transfer *transfer;
+         uint8_t *map = pipe_buffer_map(state->pctx, lvp_buffer_from_handle(copycmd->dst_buffer)->bo, PIPE_MAP_WRITE, &transfer);
+         map += offset;
+
+         if (flags & VK_QUERY_RESULT_64_BIT) {
+            uint64_t *dst = (uint64_t *)map;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = src[i];
+         } else {
+            uint32_t *dst = (uint32_t *)map;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = (uint32_t) (src[i] & UINT32_MAX);
+         }
+
+         state->pctx->buffer_unmap(state->pctx, transfer);
+
+         continue;
+      }
+
       if (pool->queries[i]) {
          unsigned num_results = 0;
          if (copycmd->flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT) {