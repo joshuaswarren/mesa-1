joshuaswarren/mesa-1

commit 7b5191aca5b0258cd308fa8e288708f218091d92

nir/gather_info: Record per-primitive outputs without variables.
Previously, this information would have been lost when the
shader has no I/O variables.

Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28489>

src/compiler/nir/nir_gather_info.c
@@ -594,6 +594,8 @@ gather_intrinsic_info(nir_intrinsic_instr *instr, nir_shader *shader,
       } else {
          shader->info.outputs_written |= slot_mask;
          shader->info.outputs_written_16bit |= slot_mask_16bit;
+         if (instr->intrinsic == nir_intrinsic_store_per_primitive_output)
+            shader->info.per_primitive_outputs |= slot_mask;
          if (!nir_src_is_const(*nir_get_io_offset_src(instr))) {
             shader->info.outputs_accessed_indirectly |= slot_mask;
             shader->info.outputs_accessed_indirectly_16bit |= slot_mask_16bit;

commit b86a2657cebabd717641a77044049622a74af640

nir/gather_info: Clear per-primitive I/O masks at the beginning.
Fixes: 7b5191aca5b0258cd308fa8e288708f218091d92
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28685>

src/compiler/nir/nir_gather_info.c
@@ -991,6 +991,8 @@ nir_shader_gather_info(nir_shader *shader, nir_function_impl *entrypoint)
    shader->info.outputs_accessed_indirectly = 0;
    shader->info.patch_inputs_read_indirectly = 0;
    shader->info.patch_outputs_accessed_indirectly = 0;
+   shader->info.per_primitive_inputs = 0;
+   shader->info.per_primitive_outputs = 0;
 
    shader->info.uses_resource_info_query = false;
 
@@ -1026,7 +1028,6 @@ nir_shader_gather_info(nir_shader *shader, nir_function_impl *entrypoint)
    gather_func_info(entrypoint, shader, visited_funcs, dead_ctx);
    ralloc_free(dead_ctx);
 
-   shader->info.per_primitive_outputs = 0;
    shader->info.per_view_outputs = 0;
    nir_foreach_shader_out_variable(var, shader) {
       if (var->data.per_primitive) {
@@ -1043,7 +1044,6 @@ nir_shader_gather_info(nir_shader *shader, nir_function_impl *entrypoint)
       }
    }
 
-   shader->info.per_primitive_inputs = 0;
    if (shader->info.stage == MESA_SHADER_FRAGMENT) {
       nir_foreach_shader_in_variable(var, shader) {
          if (var->data.per_primitive) {