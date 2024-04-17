joshuaswarren/mesa-1

commit 55a605083339085e064772587c3ebbfa4ce8e148

vulkan/video: add a nal_unit lookup for hevc
This is needed to by drivers to get a nal unit type
for the picture type.

Reviewed-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25874>

src/vulkan/runtime/vk_video.c
@@ -1298,4 +1298,55 @@ enum H264NALUType
    /* 24...31 UNSPECIFIED */
 };
 
+enum HEVCNALUnitType {
+   HEVC_NAL_TRAIL_N        = 0,
+   HEVC_NAL_TRAIL_R        = 1,
+   HEVC_NAL_TSA_N          = 2,
+   HEVC_NAL_TSA_R          = 3,
+   HEVC_NAL_STSA_N         = 4,
+   HEVC_NAL_STSA_R         = 5,
+   HEVC_NAL_RADL_N         = 6,
+   HEVC_NAL_RADL_R         = 7,
+   HEVC_NAL_RASL_N         = 8,
+   HEVC_NAL_RASL_R         = 9,
+   HEVC_NAL_VCL_N10        = 10,
+   HEVC_NAL_VCL_R11        = 11,
+   HEVC_NAL_VCL_N12        = 12,
+   HEVC_NAL_VCL_R13        = 13,
+   HEVC_NAL_VCL_N14        = 14,
+   HEVC_NAL_VCL_R15        = 15,
+   HEVC_NAL_BLA_W_LP       = 16,
+   HEVC_NAL_BLA_W_RADL     = 17,
+   HEVC_NAL_BLA_N_LP       = 18,
+   HEVC_NAL_IDR_W_RADL     = 19,
+   HEVC_NAL_IDR_N_LP       = 20,
+   HEVC_NAL_CRA_NUT        = 21,
+   HEVC_NAL_VPS_NUT        = 32,
+   HEVC_NAL_SPS_NUT        = 33,
+   HEVC_NAL_PPS_NUT        = 34,
+};
+
+unsigned
+vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag)
+{
+   switch (pic_type) {
+   case STD_VIDEO_H265_PICTURE_TYPE_IDR:
+      return HEVC_NAL_IDR_W_RADL;
+   case STD_VIDEO_H265_PICTURE_TYPE_I:
+      return HEVC_NAL_CRA_NUT;
+   case STD_VIDEO_H265_PICTURE_TYPE_P:
+      return HEVC_NAL_TRAIL_R;
+   case STD_VIDEO_H265_PICTURE_TYPE_B:
+      if (irap_pic_flag)
+         return HEVC_NAL_RASL_R;
+      else
+         return HEVC_NAL_TRAIL_R;
+      break;
+   default:
+      assert(0);
+      break;
+   }
+   return 0;
+}
+
 #endif

src/vulkan/runtime/vk_video.h
@@ -248,6 +248,10 @@ vk_video_find_h265_enc_std_sps(const struct vk_video_session_parameters *params,
 const StdVideoH265PictureParameterSet *
 vk_video_find_h265_enc_std_pps(const struct vk_video_session_parameters *params,
                                uint32_t id);
+
+unsigned
+vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag);
+
 #endif
 
 #ifdef __cplusplus

commit 05787a5fe667e3867bc08a20a75ca1b0c47c34a2

vulkan/video: hevc: b-frames can be reference or not
b-frames can be considered as reference, so the NAL type
should refer to reference type and either RASL or TRAIL
depending on the irap_pic_flag.

Fixes: 55a605083 ("vulkan/video: add a nal_unit lookup for hevc")

Reviewed-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28657>

src/amd/vulkan/radv_video_enc.c
@@ -923,7 +923,7 @@ radv_enc_slice_header_hevc(struct radv_cmd_buffer *cmd_buffer, const VkVideoEnco
    struct radv_device *device = radv_cmd_buffer_device(cmd_buffer);
    const struct radv_physical_device *pdev = radv_device_physical(device);
    struct radeon_cmdbuf *cs = cmd_buffer->cs;
-   unsigned nal_unit_type = vk_video_get_h265_nal_unit(pic->pic_type, pic->flags.IrapPicFlag);
+   unsigned nal_unit_type = vk_video_get_h265_nal_unit(pic);
 
    ENC_BEGIN;
    radeon_emit(cs, pdev->vcn_enc_cmds.slice_header);

src/vulkan/runtime/vk_video.c
@@ -1476,9 +1476,9 @@ enum HEVCNALUnitType {
 };
 
 unsigned
-vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag)
+vk_video_get_h265_nal_unit(const StdVideoEncodeH265PictureInfo *pic_info)
 {
-   switch (pic_type) {
+   switch (pic_info->pic_type) {
    case STD_VIDEO_H265_PICTURE_TYPE_IDR:
       return HEVC_NAL_IDR_W_RADL;
    case STD_VIDEO_H265_PICTURE_TYPE_I:
@@ -1486,10 +1486,16 @@ vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag)
    case STD_VIDEO_H265_PICTURE_TYPE_P:
       return HEVC_NAL_TRAIL_R;
    case STD_VIDEO_H265_PICTURE_TYPE_B:
-      if (irap_pic_flag)
-         return HEVC_NAL_RASL_R;
+      if (pic_info->flags.IrapPicFlag)
+         if (pic_info->flags.is_reference)
+            return HEVC_NAL_RASL_R;
+         else
+            return HEVC_NAL_RASL_N;
       else
-         return HEVC_NAL_TRAIL_R;
+          if (pic_info->flags.is_reference)
+            return HEVC_NAL_TRAIL_R;
+         else
+            return HEVC_NAL_TRAIL_N;
       break;
    default:
       assert(0);

src/vulkan/runtime/vk_video.h
@@ -322,7 +322,7 @@ vk_video_encode_h264_pps(const StdVideoH264PictureParameterSet *pps,
                          void *data_ptr);
 
 unsigned
-vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag);
+vk_video_get_h265_nal_unit(const StdVideoEncodeH265PictureInfo *pic_info);
 
 void
 vk_video_encode_h265_vps(const StdVideoH265VideoParameterSet *vps,