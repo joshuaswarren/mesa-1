openharmony/third_party_mesa3d

commit d2c9e4423cbf1e5e8e129d189e34f4104793cb81

panfrost: Skip blit shader labelling if the buffer has no space
Fixes stack corruption in dEQP-GLES31.functional.draw_buffers_indexed.
random.max_implementation_draw_buffers.10

Fixes: b1d8112b1f5 ("panfrost: Create a blitter library to replace the existing preload helpers")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10792>

src/panfrost/lib/pan_blitter.c
@@ -468,6 +468,14 @@ pan_blitter_get_blit_shader(struct panfrost_device *dev,
                 default: unreachable("Invalid dim\n");
                 }
 
+                coord_comps = MAX2(coord_comps,
+                                   (key->surfaces[i].dim ? : 3) +
+                                   (key->surfaces[i].array ? 1 : 0));
+                first = false;
+
+                if (sig_offset >= sizeof(sig))
+                        continue;
+
                 sig_offset += snprintf(sig + sig_offset, sizeof(sig) - sig_offset,
                                        "%s[%s;%s;%s%s;src_samples=%d,dst_samples=%d]",
                                        first ? "" : ",",
@@ -476,10 +484,6 @@ pan_blitter_get_blit_shader(struct panfrost_device *dev,
                                        key->surfaces[i].array ? "[]" : "",
                                        key->surfaces[i].src_samples,
                                        key->surfaces[i].dst_samples);
-                first = false;
-                coord_comps = MAX2(coord_comps,
-                                   (key->surfaces[i].dim ? : 3) +
-                                   (key->surfaces[i].array ? 1 : 0));
         }
 
         nir_builder b =

joshuaswarren/mesa-1

commit 65d10d4fbf46c3e0aa6bb80cc0882d57132b21c6

panfrost: correct first-tracking for signature
If we unconditionally assign false to first *before* we use it, it's
never true when used. Instead, let's assign it *both* at the end *and*
when continuing.

Fixes: d2c9e4423cb ("panfrost: Skip blit shader labelling if the buffer has no space")
CID: 1476270
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28856>

src/panfrost/lib/pan_blitter.c
@@ -491,10 +491,11 @@ pan_blitter_get_blit_shader(struct pan_blitter_cache *cache,
 
       coord_comps = MAX2(coord_comps, (key->surfaces[i].dim ?: 3) +
                                          (key->surfaces[i].array ? 1 : 0));
-      first = false;
 
-      if (sig_offset >= sizeof(sig))
+      if (sig_offset >= sizeof(sig)) {
+         first = false;
          continue;
+      }
 
       sig_offset +=
          snprintf(sig + sig_offset, sizeof(sig) - sig_offset,
@@ -502,6 +503,8 @@ pan_blitter_get_blit_shader(struct pan_blitter_cache *cache,
                   first ? "" : ",", gl_frag_result_name(key->surfaces[i].loc),
                   type_str, dim_str, key->surfaces[i].array ? "[]" : "",
                   key->surfaces[i].src_samples, key->surfaces[i].dst_samples);
+
+      first = false;
    }
 
    nir_builder b = nir_builder_init_simple_shader(