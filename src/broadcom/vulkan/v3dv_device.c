joshuaswarren/mesa-1

commit f94d86c0f24ba5ee30efea6162e01d90fe0a7d29

v3dv: mark some promoted extensions as supported
There are few EXT_ extensions that were promoted to KHR_, but we didn't
enabled them as supported.

This makes some CTS tests to be run as unsupported when they should be
supported instead.

For example, we were passing 16/108 line rasterization tests instead of
40/108 because we did not enabled KHR_line rasterization.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia>
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28090>

src/broadcom/vulkan/v3dv_device.c
@@ -155,6 +155,9 @@ get_device_extensions(const struct v3dv_physical_device *device,
       .KHR_get_memory_requirements2         = true,
       .KHR_image_format_list                = true,
       .KHR_imageless_framebuffer            = true,
+      .KHR_index_type_uint8                 = true,
+      .KHR_line_rasterization               = true,
+      .KHR_load_store_op_none               = true,
       .KHR_performance_query                = device->caps.perfmon,
       .KHR_relaxed_block_layout             = true,
       .KHR_maintenance1                     = true,
@@ -183,6 +186,7 @@ get_device_extensions(const struct v3dv_physical_device *device,
       .KHR_incremental_present              = true,
 #endif
       .KHR_variable_pointers                = true,
+      .KHR_vertex_attribute_divisor         = true,
       .KHR_vulkan_memory_model              = true,
       .KHR_zero_initialize_workgroup_memory = true,
       .EXT_4444_formats                     = true,

commit 20f7acbf6313db572cf01cfa211a599831712408

v3dv: fix VK_KHR_vertex_attribute_divisor
When this was promoted to EXT it expanded its properties struct to add a new
supportsNonZeroFirstInstance field.

Fixes: f94d86c0f2 ("v3dv: mark some promoted extensions as supported")
Fixes: dEQP-VK.api.info.vulkan1p2_limits_validation.khr_vertex_attribute_divisor

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28964>

src/broadcom/vulkan/v3dv_device.c
@@ -1462,6 +1462,13 @@ v3dv_GetPhysicalDeviceProperties2(VkPhysicalDevice physicalDevice,
          props->maxVertexAttribDivisor = 0xffff;
          break;
       }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VERTEX_ATTRIBUTE_DIVISOR_PROPERTIES_KHR: {
+         VkPhysicalDeviceVertexAttributeDivisorPropertiesKHR *props =
+            (VkPhysicalDeviceVertexAttributeDivisorPropertiesKHR *)ext;
+         props->maxVertexAttribDivisor = 0xffff;
+         props->supportsNonZeroFirstInstance = true;
+         break;
+      }
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PERFORMANCE_QUERY_PROPERTIES_KHR : {
          VkPhysicalDevicePerformanceQueryPropertiesKHR *props =
             (VkPhysicalDevicePerformanceQueryPropertiesKHR *)ext;