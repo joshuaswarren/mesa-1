joshuaswarren/mesa-1

commit f30a87d8975adf70d31e847b2fc033d75f5c5493

glx/sw: check for modifier support in the kopper path
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24075>

src/glx/drisw_glx.c
@@ -32,6 +32,7 @@
 #include <dlfcn.h>
 #include "dri_common.h"
 #include "drisw_priv.h"
+#include "dri3_priv.h"
 #include <X11/extensions/shmproto.h>
 #include <assert.h>
 #include <vulkan/vulkan_core.h>
@@ -997,6 +998,11 @@ driswCreateScreenDriver(int screen, struct glx_display *priv,
    psc->base.configs = configs;
    glx_config_destroy_list(psc->base.visuals);
    psc->base.visuals = visuals;
+
+   if (pdpyp->zink) {
+      bool err;
+      psc->has_multibuffer = dri3_check_multibuffer(priv->dpy, &err);
+   }
 
    psc->driver_configs = driver_configs;
 

src/glx/drisw_priv.h
@@ -56,6 +56,8 @@ struct drisw_screen
 
    void *driver;
    const char *name;
+
+   bool has_multibuffer;
 };
 
 struct drisw_drawable

commit 380c4166b1d16d7db49e29d961e6ec5492a7de78

drisw: fix build without dri3
commit f30a87d8975 ("glx/sw: check for modifier support in the kopper path")
added dri3_priv.h header and dri3_check_multibuffer() function in drisw that
can be build without dri3.

Commit d196af6246b added a guard around dri3_check_multibuffer()
function but not around dri3_priv.h header.

Add HAVE_DRI3 guard around dri3_priv.h header.

Fixes: f30a87d8975 ("glx/sw: check for modifier support in the kopper path")

v2: Remove the guard around dri3_check_multibuffer() function.

Signed-off-by: Romain Naour <romain.naour@smile.fr>
Signed-off-by: Antoine Coutant <antoine.coutant@smile.fr>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27478>

src/glx/drisw_glx.c
@@ -32,7 +32,9 @@
 #include <dlfcn.h>
 #include "dri_common.h"
 #include "drisw_priv.h"
+#ifdef HAVE_DRI3
 #include "dri3_priv.h"
+#endif
 #include <X11/extensions/shmproto.h>
 #include <assert.h>
 #include <vulkan/vulkan_core.h>