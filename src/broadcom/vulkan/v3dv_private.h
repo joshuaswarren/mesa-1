joshuaswarren/mesa-1

commit 1bec575390ffb183f11eebf6bf08071887cf3608

v3dv: provide implementation for vkCmdBindVertexBuffers2
Mostly equal to vkCmdBindVertexBuffers, but adding strides, that with
VK_EXT_extended_dynamic_state become dynamic, and setting pSizes.

It is worth to note that at this moment we don't use
CmdBindVertexBuffers2 pSizes.

Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27609>

src/broadcom/vulkan/v3dvx_pipeline.c
@@ -656,7 +656,6 @@ pack_shader_state_attribute_record(struct v3dv_pipeline *pipeline,
 
       attr.instance_divisor = MIN2(pipeline->vb[binding].instance_divisor,
                                    0xffff);
-      attr.stride = pipeline->vb[binding].stride;
       attr.type = get_attr_type(desc);
    }
 }

src/broadcom/vulkan/v3dv_cmd_buffer.c
@@ -3421,21 +3421,28 @@ v3dv_CmdPipelineBarrier2(VkCommandBuffer commandBuffer,
 }
 
 VKAPI_ATTR void VKAPI_CALL
-v3dv_CmdBindVertexBuffers(VkCommandBuffer commandBuffer,
-                          uint32_t firstBinding,
-                          uint32_t bindingCount,
-                          const VkBuffer *pBuffers,
-                          const VkDeviceSize *pOffsets)
+v3dv_CmdBindVertexBuffers2(VkCommandBuffer commandBuffer,
+                           uint32_t firstBinding,
+                           uint32_t bindingCount,
+                           const VkBuffer *pBuffers,
+                           const VkDeviceSize *pOffsets,
+                           const VkDeviceSize *pSizes,
+                           const VkDeviceSize *pStrides)
 {
    V3DV_FROM_HANDLE(v3dv_cmd_buffer, cmd_buffer, commandBuffer);
    struct v3dv_vertex_binding *vb = cmd_buffer->state.vertex_bindings;
 
-   /* We have to defer setting up vertex buffer since we need the buffer
-    * stride from the pipeline.
-    */
-
    assert(firstBinding + bindingCount <= MAX_VBS);
    bool vb_state_changed = false;
+   if (pStrides) {
+      vk_cmd_set_vertex_binding_strides(&cmd_buffer->vk,
+                                        firstBinding, bindingCount,
+                                        pStrides);
+      struct vk_dynamic_graphics_state *dyn = &cmd_buffer->vk.dynamic_graphics_state;
+      if (BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_VI_BINDING_STRIDES))
+         vb_state_changed = true;
+   }
+   /* FIXME: at this moment we don't do any thing with pSizes. */
    for (uint32_t i = 0; i < bindingCount; i++) {
       if (vb[firstBinding + i].buffer != v3dv_buffer_from_handle(pBuffers[i])) {
          vb[firstBinding + i].buffer = v3dv_buffer_from_handle(pBuffers[i]);

src/broadcom/vulkan/v3dvx_cmd_buffer.c
@@ -2486,6 +2486,8 @@ v3dX(cmd_buffer_emit_gl_shader_state)(struct v3dv_cmd_buffer *cmd_buffer)
             cs_loaded_any = true;
          }
 
+         attr.stride =
+            cmd_buffer->vk.dynamic_graphics_state.vi_binding_strides[binding];
          attr.maximum_index = 0xffffff;
       }
 

commit 22335f3bcd7efe98ea569f0629c6174c803eb07d

v3dv: drop unused stride field from v3dv_pipeline_vertex_binding
This is unused since 1bec575390 where we added support for dynamic
state vertex strides.

Fixes: 1bec575390 ('v3dv: provide implementation for vkCmdBindVertexBuffers2')
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29425>

src/broadcom/vulkan/v3dv_private.h
@@ -2273,11 +2273,8 @@ struct v3dv_pipeline {
 
    bool negative_one_to_one;
 
-   /* Accessed by binding. So vb[binding]->stride is the stride of the vertex
-    * array with such binding
-    */
+   /* Indexed by vertex binding. */
    struct v3dv_pipeline_vertex_binding {
-      uint32_t stride;
       uint32_t instance_divisor;
    } vb[MAX_VBS];
    uint32_t vb_count;

src/broadcom/vulkan/v3dvx_pipeline.c
@@ -685,7 +685,6 @@ v3dX(pipeline_pack_compile_state)(struct v3dv_pipeline *pipeline,
       const VkVertexInputBindingDescription *desc =
          &vi_info->pVertexBindingDescriptions[i];
 
-      pipeline->vb[desc->binding].stride = desc->stride;
       pipeline->vb[desc->binding].instance_divisor = desc->inputRate;
    }
 