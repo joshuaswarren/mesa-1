joshuaswarren/mesa-1

commit ca23c7d9da5e756df8af009e9028c9ff6f5bf49d

intel: Add driver support for hardware generated local invocation IDs
This adds a few new fields in the brw_cs_prog_data struct and then
uses them to fill in the relevant COMPUTE_WALKER fields.

Although the Tile Layout field theoretically has different settings for
32/64/128bpe, it appears that the recommended programming is to always
pick either TileY 32bpe or Linear.  It's not very practical to look at
the surface formats involved, anyway.

Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27167>

src/gallium/drivers/iris/iris_state.c
@@ -8677,6 +8677,14 @@ iris_upload_compute_walker(struct iris_context *ice,
          cw.PostSync.MOCS                  = iris_mocs(NULL, &screen->isl_dev, 0);
          cw.InterfaceDescriptor            = idd;
 
+#if GFX_VERx10 >= 125
+         cw.GenerateLocalID = cs_prog_data->generate_local_id != 0;
+         cw.EmitLocal       = cs_prog_data->generate_local_id;
+         cw.WalkOrder       = cs_prog_data->walk_order;
+         cw.TileLayout = cs_prog_data->walk_order == BRW_WALK_ORDER_YXZ ?
+                         TileY32bpe : Linear;
+#endif
+
          assert(brw_cs_push_const_total_size(cs_prog_data, dispatch.threads) == 0);
       }
    }

src/intel/vulkan/genX_cmd_compute.c
@@ -359,6 +359,11 @@ emit_compute_walker(struct anv_cmd_buffer *cmd_buffer,
 #if GFX_VERx10 == 125
          .SystolicModeEnable             = prog_data->uses_systolic,
 #endif
+         .GenerateLocalID                = prog_data->generate_local_id != 0,
+         .EmitLocal                      = prog_data->generate_local_id,
+         .WalkOrder                      = prog_data->walk_order,
+         .TileLayout = prog_data->walk_order == BRW_WALK_ORDER_YXZ ?
+                       TileY32bpe : Linear,
          .LocalXMaximum                  = prog_data->local_size[0] - 1,
          .LocalYMaximum                  = prog_data->local_size[1] - 1,
          .LocalZMaximum                  = prog_data->local_size[2] - 1,

src/intel/blorp/blorp_genX_exec.h
@@ -2191,6 +2191,14 @@ blorp_exec_compute(struct blorp_batch *batch, const struct blorp_params *params)
       cw.IndirectDataStartAddress       = push_const_offset;
       cw.IndirectDataLength             = push_const_size;
 
+#if GFX_VERx10 >= 125
+      cw.GenerateLocalID                = cs_prog_data->generate_local_id != 0;
+      cw.EmitLocal                      = cs_prog_data->generate_local_id;
+      cw.WalkOrder                      = cs_prog_data->walk_order;
+      cw.TileLayout = cs_prog_data->walk_order == BRW_WALK_ORDER_YXZ ?
+                      TileY32bpe : Linear;
+#endif
+
       cw.InterfaceDescriptor = (struct GENX(INTERFACE_DESCRIPTOR_DATA)) {
          .KernelStartPointer = params->cs_prog_kernel,
          .SamplerStatePointer = samplers_offset,

src/intel/compiler/brw_compiler.h
@@ -1322,6 +1322,15 @@ struct brw_push_const_block {
    unsigned size;       /* Bytes, register aligned */
 };
 
+enum PACKED brw_compute_walk_order {
+   BRW_WALK_ORDER_XYZ = 0,
+   BRW_WALK_ORDER_XZY = 1,
+   BRW_WALK_ORDER_YXZ = 2,
+   BRW_WALK_ORDER_YZX = 3,
+   BRW_WALK_ORDER_ZXY = 4,
+   BRW_WALK_ORDER_ZYX = 5,
+};
+
 struct brw_cs_prog_data {
    struct brw_stage_prog_data base;
 
@@ -1344,6 +1353,8 @@ struct brw_cs_prog_data {
    bool uses_inline_data;
    bool uses_btd_stack_ids;
    bool uses_systolic;
+   uint8_t generate_local_id;
+   enum brw_compute_walk_order walk_order;
 
    struct {
       struct brw_push_const_block cross_thread;

src/intel/vulkan/genX_simple_shader.c
@@ -557,6 +557,14 @@ genX(emit_simple_shader_dispatch)(struct anv_simple_shader *state,
          cw.ExecutionMask                  = dispatch.right_mask;
          cw.PostSync.MOCS                  = anv_mocs(device, NULL, 0);
 
+#if GFX_VERx10 >= 125
+         cw.GenerateLocalID                = prog_data->generate_local_id != 0;
+         cw.EmitLocal                      = prog_data->generate_local_id;
+         cw.WalkOrder                      = prog_data->walk_order;
+         cw.TileLayout = prog_data->walk_order == BRW_WALK_ORDER_YXZ ?
+                         TileY32bpe : Linear;
+#endif
+
          cw.InterfaceDescriptor = (struct GENX(INTERFACE_DESCRIPTOR_DATA)) {
             .KernelStartPointer                = state->kernel->kernel.offset +
                                                  brw_cs_prog_data_prog_offset(prog_data,

commit 53c6c0e43088b9e489a21c4b6480baec2eb420ae

intel: fix HW generated local-id with indirect compute walker
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ca23c7d9da ("intel: Add driver support for hardware generated local invocation IDs")
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29473>

src/gallium/drivers/iris/iris_state.c
@@ -8802,6 +8802,7 @@ static void iris_emit_execute_indirect_dispatch(struct iris_context *ice,
    const struct iris_screen *screen = batch->screen;
    struct iris_compiled_shader *shader =
       ice->shaders.prog[MESA_SHADER_COMPUTE];
+   const struct iris_cs_data *cs_data = iris_cs_data(shader);
    const struct intel_cs_dispatch_info dispatch =
       iris_get_cs_dispatch_info(screen->devinfo, shader, grid->block);
    struct iris_bo *indirect = iris_resource_bo(grid->indirect);
@@ -8810,6 +8811,11 @@ static void iris_emit_execute_indirect_dispatch(struct iris_context *ice,
    struct GENX(COMPUTE_WALKER_BODY) body = {};
    body.SIMDSize            = dispatch_size;
    body.MessageSIMD         = dispatch_size;
+   body.GenerateLocalID     = cs_data->generate_local_id != 0;
+   body.EmitLocal           = cs_data->generate_local_id;
+   body.WalkOrder           = cs_data->walk_order;
+   body.TileLayout          = cs_data->walk_order == INTEL_WALK_ORDER_YXZ ?
+                              TileY32bpe : Linear;
    body.LocalXMaximum       = grid->block[0] - 1;
    body.LocalYMaximum       = grid->block[1] - 1;
    body.LocalZMaximum       = grid->block[2] - 1;

src/intel/vulkan/genX_cmd_compute.c
@@ -315,6 +315,11 @@ emit_indirect_compute_walker(struct anv_cmd_buffer *cmd_buffer,
       .MessageSIMD              = dispatch_size,
       .IndirectDataStartAddress = comp_state->push_data.offset,
       .IndirectDataLength       = comp_state->push_data.alloc_size,
+      .GenerateLocalID          = prog_data->generate_local_id != 0,
+      .EmitLocal                = prog_data->generate_local_id,
+      .WalkOrder                = prog_data->walk_order,
+      .TileLayout               = prog_data->walk_order == INTEL_WALK_ORDER_YXZ ?
+                                  TileY32bpe : Linear,
       .LocalXMaximum            = prog_data->local_size[0] - 1,
       .LocalYMaximum            = prog_data->local_size[1] - 1,
       .LocalZMaximum            = prog_data->local_size[2] - 1,