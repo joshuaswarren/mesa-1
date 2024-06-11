joshuaswarren/mesa-1

commit 042ad3e09f910f4d3ebeeceb264e6bdf80e91593

ci/meson: reuse meson installation
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29394>

.gitlab-ci/container/install-meson.sh
@@ -1,0 +1,4 @@
+#!/usr/bin/env bash
+
+# We need at least 1.4.0 for rusticl
+pip3 install --break-system-packages 'meson==1.4.0'

.gitlab-ci/container/debian/arm64_build.sh
@@ -95,8 +95,7 @@ arch=armhf
 
 . .gitlab-ci/container/build-libclc.sh
 
-# We need at least 1.4.0 for rusticl
-pip3 install --break-system-packages 'meson==1.4.0'
+. .gitlab-ci/container/install-meson.sh
 
 . .gitlab-ci/container/build-rust.sh
 

.gitlab-ci/container/debian/x86_64_build-base.sh
@@ -95,8 +95,7 @@ apt-get install -y --no-remove "${DEPS[@]}" "${EPHEMERAL[@]}" \
 # Needed for ci-fairy, this revision is able to upload files to S3
 pip3 install --break-system-packages git+http://gitlab.freedesktop.org/freedesktop/ci-templates@ffe4d1b10aab7534489f0c4bbc4c5899df17d3f2
 
-# We need at least 1.4.0 for rusticl
-pip3 install --break-system-packages 'meson==1.4.0'
+. .gitlab-ci/container/install-meson.sh
 
 . .gitlab-ci/container/build-rust.sh
 

.gitlab-ci/container/fedora/x86_64_build.sh
@@ -101,8 +101,7 @@ tar -xvf $XORGMACROS_VERSION.tar.bz2 && rm $XORGMACROS_VERSION.tar.bz2
 cd $XORGMACROS_VERSION; ./configure; make install; cd ..
 rm -rf $XORGMACROS_VERSION
 
-# We need at least 1.4.0 for rusticl
-pip install meson==1.4.0
+. .gitlab-ci/container/install-meson.sh
 
 . .gitlab-ci/container/build-mold.sh
 

commit 51ed89906d68311b46d4991fa9140bbd2ce213df

ci: fix meson install script
042ad3e09f910f4d3ebe ("ci/meson: reuse meson installation") forgot to
bump the image tags, and as a result this was merged untested and turned
out to be broken, as the fedora image.

The issue is that python in the Fedora image is not flagged as
EXTERNALLY-MANAGED, unlike what Debian does, so the
`--break-system-packages` is invalid.

Instead, remove this flag from the debian image as it makes very little
sense in a docker image.

Fixes: 042ad3e09f910f4d3ebe ("ci/meson: reuse meson installation")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29673>

.gitlab-ci/container/install-meson.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# When changing this file, you need to bump the following
+# .gitlab-ci/image-tags.yml tags:
+# DEBIAN_BUILD_TAG
+# FEDORA_X86_64_BUILD_TAG
+
+rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED
+
 # We need at least 1.4.0 for rusticl
-pip3 install --break-system-packages 'meson==1.4.0'
+pip3 install 'meson==1.4.0'

.gitlab-ci/image-tags.yml
@@ -16,7 +16,7 @@ variables:
    DEBIAN_BASE_TAG: "20240529-rust"
 
    DEBIAN_X86_64_BUILD_IMAGE_PATH: "debian/x86_64_build"
-   DEBIAN_BUILD_TAG: "20240529-pkg-config"
+   DEBIAN_BUILD_TAG: "20240611-meson"
 
    DEBIAN_X86_64_TEST_BASE_IMAGE: "debian/x86_64_test-base"
    DEBIAN_ARM64_TEST_BASE_IMAGE: "debian/arm64_test-base"
@@ -34,7 +34,7 @@ variables:
 
    ALPINE_X86_64_BUILD_TAG: "20240517-m0ld"
    ALPINE_X86_64_LAVA_SSH_TAG: "20240401-wlproto"
-   FEDORA_X86_64_BUILD_TAG: "20240509-meson"
+   FEDORA_X86_64_BUILD_TAG: "20240611-meson"
    KERNEL_TAG: "v6.6.21-mesa-f8ea"
    KERNEL_REPO: "gfx-ci/linux"
    PKG_REPO_REV: "3cc12a2a"