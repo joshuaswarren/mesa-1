joshuaswarren/mesa-1

commit 8628c04852148e1acc869e7f040dc212a59c152f

rusticl/kernel: add missing preprocessor definitions
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15439>

src/gallium/frontends/rusticl/core/program.rs
@@ -58,9 +58,15 @@ struct ProgramDevBuild {
 
 fn prepare_options(options: &str, dev: &Device) -> Vec<CString> {
     let mut options = options.to_owned();
+    if !options.contains("-cl-std=CL") {
+        options.push_str(" -cl-std=CL");
+        options.push_str(dev.clc_version.api_str());
+    }
     if !dev.image_supported() {
         options.push_str(" -U__IMAGE_SUPPORT__");
     }
+    options.push_str(" -D__OPENCL_VERSION__=");
+    options.push_str(dev.cl_version.clc_str());
     options
         .split_whitespace()
         .map(|a| match a {

src/gallium/frontends/rusticl/core/version.rs
@@ -60,6 +60,18 @@ impl CLVersion {
             CLVersion::Cl3_0 => "3.0",
         }
     }
+
+    pub fn clc_str(&self) -> &'static str {
+        match self {
+            CLVersion::Cl1_0 => "100",
+            CLVersion::Cl1_1 => "110",
+            CLVersion::Cl1_2 => "120",
+            CLVersion::Cl2_0 => "200",
+            CLVersion::Cl2_1 => "210",
+            CLVersion::Cl2_2 => "220",
+            CLVersion::Cl3_0 => "300",
+        }
+    }
 }
 
 impl TryFrom<u32> for CLVersion {

commit 1115cc13b58712e1bb547b5ac228fbb60ad18e6c

rusticl: add -cl-std only when it's not defined
This fixes piglit "Invalid CL Version Declaration" test.

Fixes: 8628c0485214 ("rusticl/kernel: add missing preprocessor definitions")

Reviewed-by: Karol Herbst <kherbst@redhat.com>
Signed-off-by: David Heidelberg <david@ixit.cz>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29638>

src/gallium/frontends/rusticl/core/program.rs
@@ -298,7 +298,7 @@ pub struct ProgramDevBuild {
 
 fn prepare_options(options: &str, dev: &Device) -> Vec<CString> {
     let mut options = options.to_owned();
-    if !options.contains("-cl-std=CL") {
+    if !options.contains("-cl-std=") {
         options.push_str(" -cl-std=CL");
         options.push_str(dev.clc_version.api_str());
     }