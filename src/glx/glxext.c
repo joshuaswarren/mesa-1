joshuaswarren/mesa-1

commit 0612aaf566910dcc79f58a185e027fd6b593cb30

glx: pass implicit load param through allocation
Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28139>

src/glx/glxext.c
@@ -763,7 +763,7 @@ glx_screen_cleanup(struct glx_screen *psc)
 ** If that works then fetch the per screen configs data.
 */
 static Bool
-AllocAndFetchScreenConfigs(Display * dpy, struct glx_display * priv, Bool zink)
+AllocAndFetchScreenConfigs(Display * dpy, struct glx_display * priv, Bool zink, Bool implicit)
 {
    struct glx_screen *psc;
    GLint i, screens;
@@ -783,19 +783,19 @@ AllocAndFetchScreenConfigs(Display * dpy, struct glx_display * priv, Bool zink)
 #if defined(GLX_USE_DRM)
 #if defined(HAVE_DRI3)
       if (priv->dri3Display)
-         psc = priv->dri3Display->createScreen(i, priv, false);
+         psc = priv->dri3Display->createScreen(i, priv, implicit);
 #endif /* HAVE_DRI3 */
       if (psc == NULL && priv->dri2Display)
-	      psc = priv->dri2Display->createScreen(i, priv, false);
+	      psc = priv->dri2Display->createScreen(i, priv, implicit);
 #endif /* GLX_USE_DRM */
 
 #ifdef GLX_USE_WINDOWSGL
       if (psc == NULL && priv->windowsdriDisplay)
-	      psc = priv->windowsdriDisplay->createScreen(i, priv, false);
+	      psc = priv->windowsdriDisplay->createScreen(i, priv, implicit);
 #endif
 
       if ((psc == GLX_LOADER_USE_ZINK || psc == NULL) && priv->driswDisplay)
-	      psc = priv->driswDisplay->createScreen(i, priv, false);
+	      psc = priv->driswDisplay->createScreen(i, priv, psc == GLX_LOADER_USE_ZINK ? false : implicit);
 #endif /* GLX_DIRECT_RENDERING && !GLX_USE_APPLEGL */
 
 #if defined(GLX_USE_APPLE)
@@ -941,14 +941,14 @@ __glXInitialize(Display * dpy)
    }
 #endif
 
-   if (!AllocAndFetchScreenConfigs(dpy, dpyPriv, zink | try_zink)) {
+   if (!AllocAndFetchScreenConfigs(dpy, dpyPriv, zink | try_zink, zink || try_zink ? try_zink : !env)) {
       Bool fail = True;
 #if defined(GLX_DIRECT_RENDERING) && (!defined(GLX_USE_APPLEGL) || defined(GLX_USE_APPLE))
       if (try_zink) {
          free(dpyPriv->screens);
          dpyPriv->driswDisplay->destroyDisplay(dpyPriv->driswDisplay);
          dpyPriv->driswDisplay = driswCreateDisplay(dpy, TRY_ZINK_NO);
-         fail = !AllocAndFetchScreenConfigs(dpy, dpyPriv, False);
+         fail = !AllocAndFetchScreenConfigs(dpy, dpyPriv, False, true);
       }
 #endif
       if (fail) {

commit 257fce84d17aa1041db72015ba507ae24faac75b

glx: fix build -D glx-direct=false
Fixes: 0612aaf566910dcc79f5 ("glx: pass implicit load param through allocation")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29732>

src/glx/glxext.c
@@ -885,11 +885,11 @@ __glXInitialize(Display * dpy)
 
    Bool zink = False;
    Bool try_zink = False;
+   const char *env = getenv("MESA_LOADER_DRIVER_OVERRIDE");
 
 #if defined(GLX_DIRECT_RENDERING) && (!defined(GLX_USE_APPLEGL) || defined(GLX_USE_APPLE))
    Bool glx_direct = !debug_get_bool_option("LIBGL_ALWAYS_INDIRECT", false);
    Bool glx_accel = !debug_get_bool_option("LIBGL_ALWAYS_SOFTWARE", false);
-   const char *env = getenv("MESA_LOADER_DRIVER_OVERRIDE");
 
    zink = env && !strcmp(env, "zink");
    try_zink = False;