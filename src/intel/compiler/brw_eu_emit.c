joshuaswarren/mesa-1

commit ccb3ede647756914aac7daa5e8d2358dc4377089

intel/compiler: Initial bits for DPAS instruction
v2: Add brw_ir_performance.cpp and brw_fs_generator.cpp changes. Fix
overlapping register allocation (via has_source_and_destination_hazard). Fix
incorrect destination register file encoding.

v3: Prevent lower_regioning from trying to "fix" DPAS sources.

v4: Add instruction latency information for scheduling and perf
estimates.

v5: Remove all mention of DPASW. Suggested by Curro and Caio. Update
the comment in fs_inst::has_source_and_destination_hazard. Suggested
by Caio.

v6: Add some comments near the src2 calculation in
fs_inst::size_read. Suggested by Caio.

Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25994>

src/intel/compiler/brw_eu_emit.c
@@ -1016,6 +1016,60 @@ brw_alu3(struct brw_codegen *p, unsigned opcode, struct brw_reg dest,
    return inst;
 }
 
+static brw_inst *
+brw_dpas_three_src(struct brw_codegen *p, enum gfx12_systolic_depth opcode,
+                   unsigned sdepth, unsigned rcount, struct brw_reg dest,
+                   struct brw_reg src0, struct brw_reg src1, struct brw_reg src2)
+{
+   const struct intel_device_info *devinfo = p->devinfo;
+   brw_inst *inst = next_insn(p, opcode);
+
+   assert(dest.file == BRW_GENERAL_REGISTER_FILE);
+   brw_inst_set_dpas_3src_dst_reg_file(devinfo, inst,
+                                       BRW_GENERAL_REGISTER_FILE);
+   brw_inst_set_dpas_3src_dst_reg_nr(devinfo, inst, dest.nr);
+   brw_inst_set_dpas_3src_dst_subreg_nr(devinfo, inst, dest.subnr);
+
+   if (brw_reg_type_is_floating_point(dest.type)) {
+      brw_inst_set_dpas_3src_exec_type(devinfo, inst,
+                                       BRW_ALIGN1_3SRC_EXEC_TYPE_FLOAT);
+   } else {
+      brw_inst_set_dpas_3src_exec_type(devinfo, inst,
+                                       BRW_ALIGN1_3SRC_EXEC_TYPE_INT);
+   }
+
+   brw_inst_set_dpas_3src_sdepth(devinfo, inst, sdepth);
+   brw_inst_set_dpas_3src_rcount(devinfo, inst, rcount - 1);
+
+   brw_inst_set_dpas_3src_dst_type(devinfo, inst, dest.type);
+   brw_inst_set_dpas_3src_src0_type(devinfo, inst, src0.type);
+   brw_inst_set_dpas_3src_src1_type(devinfo, inst, src1.type);
+   brw_inst_set_dpas_3src_src2_type(devinfo, inst, src2.type);
+
+   assert(src0.file == BRW_GENERAL_REGISTER_FILE ||
+          (src0.file == BRW_ARCHITECTURE_REGISTER_FILE &&
+           src0.nr == BRW_ARF_NULL));
+
+   brw_inst_set_dpas_3src_src0_reg_file(devinfo, inst, src0.file);
+   brw_inst_set_dpas_3src_src0_reg_nr(devinfo, inst, src0.nr);
+   brw_inst_set_dpas_3src_src0_subreg_nr(devinfo, inst, src0.subnr);
+
+   assert(src1.file == BRW_GENERAL_REGISTER_FILE);
+
+   brw_inst_set_dpas_3src_src1_reg_file(devinfo, inst, src1.file);
+   brw_inst_set_dpas_3src_src1_reg_nr(devinfo, inst, src1.nr);
+   brw_inst_set_dpas_3src_src1_subreg_nr(devinfo, inst, src1.subnr);
+   brw_inst_set_dpas_3src_src1_subbyte(devinfo, inst, BRW_SUB_BYTE_PRECISION_NONE);
+
+   assert(src2.file == BRW_GENERAL_REGISTER_FILE);
+
+   brw_inst_set_dpas_3src_src2_reg_file(devinfo, inst, src2.file);
+   brw_inst_set_dpas_3src_src2_reg_nr(devinfo, inst, src2.nr);
+   brw_inst_set_dpas_3src_src2_subreg_nr(devinfo, inst, src2.subnr);
+   brw_inst_set_dpas_3src_src2_subbyte(devinfo, inst, BRW_SUB_BYTE_PRECISION_NONE);
+
+   return inst;
+}
 
 /***********************************************************************
  * Convenience routines.
@@ -1246,6 +1300,15 @@ brw_PLN(struct brw_codegen *p, struct brw_reg dest,
    src1.width = BRW_WIDTH_8;
    src1.hstride = BRW_HORIZONTAL_STRIDE_1;
    return brw_alu2(p, BRW_OPCODE_PLN, dest, src0, src1);
+}
+
+brw_inst *
+brw_DPAS(struct brw_codegen *p, enum gfx12_systolic_depth sdepth,
+         unsigned rcount, struct brw_reg dest, struct brw_reg src0,
+         struct brw_reg src1, struct brw_reg src2)
+{
+   return brw_dpas_three_src(p, BRW_OPCODE_DPAS, sdepth, rcount, dest, src0,
+                             src1, src2);
 }
 
 brw_inst *

src/intel/compiler/brw_eu.c
@@ -696,6 +696,7 @@ static const struct opcode_desc opcode_descs[] = {
    { BRW_OPCODE_DP2,      87,  "dp2",     2,    1,    GFX_LT(GFX11) },
    { BRW_OPCODE_DP4A,     88,  "dp4a",    3,    1,    GFX_GE(GFX12) },
    { BRW_OPCODE_LINE,     89,  "line",    2,    1,    GFX_LE(GFX10) },
+   { BRW_OPCODE_DPAS,     89,  "dpas",    3,    1,    GFX_GE(GFX125) },
    { BRW_OPCODE_PLN,      90,  "pln",     2,    1,    GFX_GE(GFX45) & GFX_LE(GFX10) },
    { BRW_OPCODE_MAD,      91,  "mad",     3,    1,    GFX_GE(GFX6) },
    { BRW_OPCODE_LRP,      92,  "lrp",     3,    1,    GFX_GE(GFX6) & GFX_LE(GFX10) },

src/intel/compiler/brw_eu.h
@@ -1908,6 +1908,10 @@ void brw_CMPN(struct brw_codegen *p,
               struct brw_reg src0,
               struct brw_reg src1);
 
+brw_inst *brw_DPAS(struct brw_codegen *p, enum gfx12_systolic_depth sdepth,
+                   unsigned rcount, struct brw_reg dest, struct brw_reg src0,
+                   struct brw_reg src1, struct brw_reg src2);
+
 void
 brw_untyped_atomic(struct brw_codegen *p,
                    struct brw_reg dst,

src/intel/compiler/brw_eu_defines.h
@@ -254,6 +254,7 @@ enum opcode {
    BRW_OPCODE_DP2,
    BRW_OPCODE_DP4A, /**< Gfx12+ */
    BRW_OPCODE_LINE,
+   BRW_OPCODE_DPAS,  /**< Gfx12.5+ */
    BRW_OPCODE_PLN, /**< G45+ */
    BRW_OPCODE_MAD, /**< Gfx6+ */
    BRW_OPCODE_LRP, /**< Gfx6+ */
@@ -1135,6 +1136,24 @@ enum tgl_sbid_mode {
    TGL_SBID_SRC = 1,
    TGL_SBID_DST = 2,
    TGL_SBID_SET = 4
+};
+
+
+enum gfx12_sub_byte_precision {
+   BRW_SUB_BYTE_PRECISION_NONE = 0,
+
+   /** 4 bits. Signedness determined by base type */
+   BRW_SUB_BYTE_PRECISION_4BIT = 1,
+
+   /** 2 bits. Signedness determined by base type */
+   BRW_SUB_BYTE_PRECISION_2BIT = 2,
+};
+
+enum gfx12_systolic_depth {
+   BRW_SYSTOLIC_DEPTH_16 = 0,
+   BRW_SYSTOLIC_DEPTH_2 = 1,
+   BRW_SYSTOLIC_DEPTH_4 = 2,
+   BRW_SYSTOLIC_DEPTH_8 = 3,
 };
 
 #ifdef __cplusplus

src/intel/compiler/brw_eu_validate.c
@@ -687,7 +687,10 @@ general_restrictions_based_on_operand_types(const struct brw_isa_info *isa,
       return error_msg;
 
    if (devinfo->ver >= 11) {
-      if (num_sources == 3) {
+      /* A register type of B or UB for DPAS actually means 4 bytes packed into
+       * a D or UD, so it is allowed.
+       */
+      if (num_sources == 3 && brw_inst_opcode(isa, inst) != BRW_OPCODE_DPAS) {
          ERROR_IF(brw_reg_type_to_size(brw_inst_3src_a1_src1_type(devinfo, inst)) == 1 ||
                   brw_reg_type_to_size(brw_inst_3src_a1_src2_type(devinfo, inst)) == 1,
                   "Byte data type is not supported for src1/2 register regioning. This includes "

src/intel/compiler/brw_fs.cpp
@@ -395,6 +395,21 @@ fs_inst::has_source_and_destination_hazard() const
       default:
          return !is_uniform(src[0]);
       }
+   case BRW_OPCODE_DPAS:
+      /* This is overly conservative. The actual hazard is more complicated to
+       * describe. When the repeat count is N, the single instruction behaves
+       * like N instructions with a repeat count of one, but the destination
+       * and source registers are incremented (in somewhat complex ways) for
+       * each instruction.
+       *
+       * This means the source and destination register is actually a range of
+       * registers. The hazard exists of an earlier iteration would write a
+       * register that should be read by a later iteration.
+       *
+       * There may be some advantage to properly modeling this, but for now,
+       * be overly conservative.
+       */
+      return rcount > 1;
    default:
       /* The SIMD16 compressed instruction
        *
@@ -844,6 +859,9 @@ fs_inst::components_read(unsigned i) const
       else
          return 1;
 
+   case BRW_OPCODE_DPAS:
+      unreachable("Do not use components_read() for DPAS.");
+
    default:
       return 1;
    }
@@ -901,6 +919,26 @@ fs_inst::size_read(int arg) const
       if (arg == 0) {
          assert(src[2].file == IMM);
          return src[2].ud;
+      }
+      break;
+
+   case BRW_OPCODE_DPAS:
+      switch (arg) {
+      case 0:
+         if (src[0].type == BRW_REGISTER_TYPE_HF) {
+            return rcount * REG_SIZE / 2;
+         } else {
+            return rcount * REG_SIZE;
+         }
+      case 1:
+         return sdepth * REG_SIZE;
+      case 2:
+         /* This is simpler than the formula described in the Bspec, but it
+          * covers all of the cases that we support on DG2.
+          */
+         return rcount * REG_SIZE;
+      default:
+         unreachable("Invalid source number.");
       }
       break;
 

src/intel/compiler/brw_fs_builder.h
@@ -834,6 +834,27 @@ namespace brw {
          return inst;
       }
 
+      instruction *
+      DPAS(const dst_reg &dst, const src_reg &src0, const src_reg &src1, const src_reg &src2,
+           unsigned sdepth, unsigned rcount) const
+      {
+         assert(_dispatch_width == 8);
+         assert(sdepth == 8);
+         assert(rcount == 1 || rcount == 2 || rcount == 4 || rcount == 8);
+
+         instruction *inst = emit(BRW_OPCODE_DPAS, dst, src0, src1, src2);
+         inst->sdepth = sdepth;
+         inst->rcount = rcount;
+
+         if (dst.type == BRW_REGISTER_TYPE_HF) {
+            inst->size_written = rcount * REG_SIZE / 2;
+         } else {
+            inst->size_written = rcount * REG_SIZE;
+         }
+
+         return inst;
+      }
+
       fs_visitor *shader;
 
       fs_inst *BREAK()    { return emit(BRW_OPCODE_BREAK); }

src/intel/compiler/brw_fs_generator.cpp
@@ -1606,6 +1606,19 @@ fs_generator::enable_debug(const char *shader_name)
    this->shader_name = shader_name;
 }
 
+static gfx12_systolic_depth
+translate_systolic_depth(unsigned d)
+{
+   /* Could also return (ffs(d) - 1) & 3. */
+   switch (d) {
+   case 2:  return BRW_SYSTOLIC_DEPTH_2;
+   case 4:  return BRW_SYSTOLIC_DEPTH_4;
+   case 8:  return BRW_SYSTOLIC_DEPTH_8;
+   case 16: return BRW_SYSTOLIC_DEPTH_16;
+   default: unreachable("Invalid systolic depth.");
+   }
+}
+
 int
 fs_generator::generate_code(const cfg_t *cfg, int dispatch_width,
                             struct shader_stats shader_stats,
@@ -1789,6 +1802,12 @@ fs_generator::generate_code(const cfg_t *cfg, int dispatch_width,
 
       case BRW_OPCODE_LINE:
          brw_LINE(p, dst, src[0], src[1]);
+         break;
+
+      case BRW_OPCODE_DPAS:
+         assert(devinfo->verx10 >= 125);
+         brw_DPAS(p, translate_systolic_depth(inst->sdepth), inst->rcount,
+                  dst, src[0], src[1], src[2]);
          break;
 
       case BRW_OPCODE_MAD:

commit a796fbba8148bcb7450bf10f76370e34b0c2a71d

intel/brw: Fix typo in DPAS emission code
The enums were mixed up.  Code was working because they were being
used only for their numerical values.

Fixes: ccb3ede6477 ("intel/compiler: Initial bits for DPAS instruction")
Acked-by: Iván Briano <ivan.briano@intel.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29762>

src/intel/compiler/brw_eu_emit.c
@@ -744,8 +744,8 @@ brw_alu3(struct brw_codegen *p, unsigned opcode, struct brw_reg dest,
 }
 
 static brw_inst *
-brw_dpas_three_src(struct brw_codegen *p, enum gfx12_systolic_depth opcode,
-                   unsigned sdepth, unsigned rcount, struct brw_reg dest,
+brw_dpas_three_src(struct brw_codegen *p, enum opcode opcode,
+                   enum gfx12_systolic_depth sdepth, unsigned rcount, struct brw_reg dest,
                    struct brw_reg src0, struct brw_reg src1, struct brw_reg src2)
 {
    const struct intel_device_info *devinfo = p->devinfo;