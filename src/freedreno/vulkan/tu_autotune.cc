joshuaswarren/mesa-1

commit a9b7a7e86cb7fbe1487e81a0afd9ec5c651e6493

tu/autotune: Use `CP_EVENT_WRITE7::ZPASS_DONE` on A7XX
The `RB_SAMPLE_COUNT_ADDR` register no longer exists on A7XX and
the address is provided as a part of `CP_EVENT_WRITE7`.

Signed-off-by: Mark Collins <mark@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26461>

src/freedreno/vulkan/tu_autotune.cc
@@ -621,6 +621,7 @@ tu_autotune_use_bypass(struct tu_autotune *at,
    return fallback_use_bypass(pass, framebuffer, cmd_buffer);
 }
 
+template <chip CHIP>
 void
 tu_autotune_begin_renderpass(struct tu_cmd_buffer *cmd,
                              struct tu_cs *cs,
@@ -649,12 +650,21 @@ tu_autotune_begin_renderpass(struct tu_cmd_buffer *cmd,
 
    tu_cs_emit_regs(cs, A6XX_RB_SAMPLE_COUNT_CONTROL(.copy = true));
 
-   tu_cs_emit_regs(cs, A6XX_RB_SAMPLE_COUNT_ADDR(.qword = result_iova));
-   /* A7XX TODO: Fixup ZPASS_DONE */
-   tu_cs_emit_pkt7(cs, CP_EVENT_WRITE, 1);
-   tu_cs_emit(cs, ZPASS_DONE);
+   if (CHIP >= A7XX) {
+      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+      tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                       .write_sample_count = true).value);
+      tu_cs_emit_qw(cs, result_iova);
+   } else {
+      tu_cs_emit_regs(cs,
+                        A6XX_RB_SAMPLE_COUNT_ADDR(.qword = result_iova));
+      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE, 1);
+      tu_cs_emit(cs, ZPASS_DONE);
+   }
 }
+TU_GENX(tu_autotune_begin_renderpass);
 
+template <chip CHIP>
 void tu_autotune_end_renderpass(struct tu_cmd_buffer *cmd,
                                 struct tu_cs *cs,
                                 struct tu_renderpass_result *autotune_result)
@@ -670,9 +680,16 @@ void tu_autotune_end_renderpass(struct tu_cmd_buffer *cmd,
 
    tu_cs_emit_regs(cs, A6XX_RB_SAMPLE_COUNT_CONTROL(.copy = true));
 
-   tu_cs_emit_regs(cs, A6XX_RB_SAMPLE_COUNT_ADDR(.qword = result_iova));
-
-   /* A7XX TODO: Fixup ZPASS_DONE */
-   tu_cs_emit_pkt7(cs, CP_EVENT_WRITE, 1);
-   tu_cs_emit(cs, ZPASS_DONE);
+   if (CHIP >= A7XX) {
+      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+      tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                       .write_sample_count = true).value);
+      tu_cs_emit_qw(cs, result_iova);
+   } else {
+      tu_cs_emit_regs(cs,
+                        A6XX_RB_SAMPLE_COUNT_ADDR(.qword = result_iova));
+      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE, 1);
+      tu_cs_emit(cs, ZPASS_DONE);
+   }
 }
+TU_GENX(tu_autotune_end_renderpass);

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -1750,7 +1750,7 @@ tu6_sysmem_render_begin(struct tu_cmd_buffer *cmd, struct tu_cs *cs,
    tu_cs_emit_pkt7(cs, CP_SET_MODE, 1);
    tu_cs_emit(cs, 0x0);
 
-   tu_autotune_begin_renderpass(cmd, cs, autotune_result);
+   tu_autotune_begin_renderpass<CHIP>(cmd, cs, autotune_result);
 
    tu_cs_sanity_check(cs);
 }
@@ -1760,7 +1760,7 @@ static void
 tu6_sysmem_render_end(struct tu_cmd_buffer *cmd, struct tu_cs *cs,
                       struct tu_renderpass_result *autotune_result)
 {
-   tu_autotune_end_renderpass(cmd, cs, autotune_result);
+   tu_autotune_end_renderpass<CHIP>(cmd, cs, autotune_result);
 
    /* Do any resolves of the last subpass. These are handled in the
     * tile_store_cs in the gmem path.
@@ -1863,7 +1863,7 @@ tu6_tile_render_begin(struct tu_cmd_buffer *cmd, struct tu_cs *cs,
       }
    }
 
-   tu_autotune_begin_renderpass(cmd, cs, autotune_result);
+   tu_autotune_begin_renderpass<CHIP>(cmd, cs, autotune_result);
 
    tu_cs_sanity_check(cs);
 }
@@ -1916,7 +1916,7 @@ static void
 tu6_tile_render_end(struct tu_cmd_buffer *cmd, struct tu_cs *cs,
                     struct tu_renderpass_result *autotune_result)
 {
-   tu_autotune_end_renderpass(cmd, cs, autotune_result);
+   tu_autotune_end_renderpass<CHIP>(cmd, cs, autotune_result);
 
    tu_cs_emit_call(cs, &cmd->draw_epilogue_cs);
 

src/freedreno/vulkan/tu_query.cc
@@ -1171,7 +1171,7 @@ emit_end_occlusion_query(struct tu_cmd_buffer *cmdbuf,
       }
    } else {
       /* A7XX TODO: Calculate (end - begin) via ZPASS_DONE. */
-      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE, 3);
+      tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
       tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
                                        .write_sample_count = true).value);
       tu_cs_emit_qw(cs, end_iova);

src/freedreno/registers/adreno/a6xx.xml
@@ -2839,7 +2839,7 @@ to upconvert to 32b float internally?
 		</reg32>
 	</array>
 	<!-- 0x891b-0x8926 invalid -->
-	<reg64 offset="0x8927" name="RB_SAMPLE_COUNT_ADDR" type="waddress" align="16" usage="cmd"/>
+	<reg64 offset="0x8927" name="RB_SAMPLE_COUNT_ADDR" type="waddress" align="16" usage="cmd" variants="A6XX"/>
 	<!-- 0x8929-0x89ff invalid -->
 
 	<!-- TODO: there are some registers in the 0x8a00-0x8bff range -->

src/freedreno/vulkan/tu_autotune.h
@@ -141,10 +141,12 @@ struct tu_cs *tu_autotune_on_submit(struct tu_device *dev,
 
 struct tu_autotune_results_buffer;
 
+template <chip CHIP>
 void tu_autotune_begin_renderpass(struct tu_cmd_buffer *cmd,
                                   struct tu_cs *cs,
                                   struct tu_renderpass_result *autotune_result);
 
+template <chip CHIP>
 void tu_autotune_end_renderpass(struct tu_cmd_buffer *cmd,
                                 struct tu_cs *cs,
                                 struct tu_renderpass_result *autotune_result);

commit 904ecbe021bb95c16d9862e39fa9c0982df1ae04

tu: fix ZPASS_DONE interference between occlusion queries and autotuner
On newer devices where ZPASS_DONE events have sample count writing
abilities the firmware expects these events to come in begin-end pairs,
essentially corresponding to a typical occlusion query usage. Since this
event is also used in the autotuner we have to avoid event pairs to be
emitted in an interleaved fashion.

Additional renderpass state now tracks whether a given renderpass contains
an occlusion query. If so, autotuner will emit miscellaneous ZPASS_DONE
events in order to form its own begin-end pairs before and after the
renderpass commands.

Occlusion query behavior inside a renderpass doesn't change. But when used
outside of a renderpass, possible autotuner usage requires to again emit
ZPASS_DONE events that end up forming begin-end pairs of these events both
at the start and the end of the query.

Signed-off-by: Zan Dobersek <zdobersek@igalia.com>
Fixes: a9b7a7e86cb ("tu/autotune: Use `CP_EVENT_WRITE7::ZPASS_DONE` on A7XX")
Tested-by: Mike Lothian <mike@fireburn.co.uk>
Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29403>

src/freedreno/vulkan/tu_autotune.cc
@@ -672,6 +672,21 @@ tu_autotune_begin_renderpass(struct tu_cmd_buffer *cmd,
       tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
                                        .write_sample_count = true).value);
       tu_cs_emit_qw(cs, result_iova);
+
+      /* If the renderpass contains an occlusion query with its own ZPASS_DONE,
+       * we have to provide a fake ZPASS_DONE event here to logically close the
+       * previous one, preventing firmware from misbehaving due to nested events.
+       * This writes into the samples_end field, which will be overwritten in
+       * tu_autotune_end_renderpass.
+       */
+      if (cmd->state.rp.has_zpass_done_sample_count_write_in_rp) {
+         tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+         tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                          .write_sample_count = true,
+                                          .sample_count_end_offset = true,
+                                          .write_accum_sample_count_diff = true).value);
+         tu_cs_emit_qw(cs, result_iova);
+      }
    } else {
       tu_cs_emit_regs(cs,
                         A6XX_RB_SAMPLE_COUNT_ADDR(.qword = result_iova));
@@ -697,10 +712,24 @@ void tu_autotune_end_renderpass(struct tu_cmd_buffer *cmd,
    tu_cs_emit_regs(cs, A6XX_RB_SAMPLE_COUNT_CONTROL(.copy = true));
 
    if (cmd->device->physical_device->info->a7xx.has_event_write_sample_count) {
+      /* If the renderpass contains ZPASS_DONE events we emit a fake ZPASS_DONE
+       * event here, composing a pair of these events that firmware handles without
+       * issue. This first event writes into the samples_end field and the second
+       * event overwrites it. The second event also enables the accumulation flag
+       * even when we don't use that result because the blob always sets it.
+       */
+      if (cmd->state.rp.has_zpass_done_sample_count_write_in_rp) {
+         tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+         tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                          .write_sample_count = true).value);
+         tu_cs_emit_qw(cs, result_iova + offsetof(struct tu_renderpass_samples, samples_end));
+      }
+
       tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
       tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
                                        .write_sample_count = true,
-                                       .sample_count_end_offset = true).value);
+                                       .sample_count_end_offset = true,
+                                       .write_accum_sample_count_diff = true).value);
       tu_cs_emit_qw(cs, result_iova);
    } else {
       result_iova += offsetof(struct tu_renderpass_samples, samples_end);

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -3888,6 +3888,7 @@ tu_render_pass_state_merge(struct tu_render_pass_state *dst,
    dst->has_tess |= src->has_tess;
    dst->has_gs |= src->has_gs;
    dst->has_prim_generated_query_in_rp |= src->has_prim_generated_query_in_rp;
+   dst->has_zpass_done_sample_count_write_in_rp |= src->has_zpass_done_sample_count_write_in_rp;
    dst->disable_gmem |= src->disable_gmem;
    dst->sysmem_single_prim_mode |= src->sysmem_single_prim_mode;
    dst->draw_cs_writes_to_cond_pred |= src->draw_cs_writes_to_cond_pred;

src/freedreno/vulkan/tu_cmd_buffer.h
@@ -278,6 +278,7 @@ struct tu_render_pass_state
    bool has_tess;
    bool has_gs;
    bool has_prim_generated_query_in_rp;
+   bool has_zpass_done_sample_count_write_in_rp;
    bool disable_gmem;
    bool sysmem_single_prim_mode;
    bool shared_viewport;

src/freedreno/vulkan/tu_query.cc
@@ -880,6 +880,27 @@ emit_begin_occlusion_query(struct tu_cmd_buffer *cmdbuf,
       tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
                                        .write_sample_count = true).value);
       tu_cs_emit_qw(cs, begin_iova);
+
+      /* ZPASS_DONE events should come in begin-end pairs. When emitting and
+       * occlusion query outside of a renderpass, we emit a fake end event that
+       * closes the previous one since the autotuner's ZPASS_DONE use could end
+       * up causing problems. This events writes into the end field of the query
+       * slot, but it will be overwritten by events in emit_end_occlusion_query
+       * with the proper value.
+       * When inside a renderpass, the corresponding ZPASS_DONE event will be
+       * emitted in emit_end_occlusion_query. We note the use of ZPASS_DONE on
+       * the state object, enabling autotuner to optimize its own events.
+       */
+      if (!cmdbuf->state.pass) {
+         tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+         tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                          .write_sample_count = true,
+                                          .sample_count_end_offset = true,
+                                          .write_accum_sample_count_diff = true).value);
+         tu_cs_emit_qw(cs, begin_iova);
+      } else {
+         cmdbuf->state.rp.has_zpass_done_sample_count_write_in_rp = true;
+      }
    }
 }
 
@@ -1188,6 +1209,20 @@ emit_end_occlusion_query(struct tu_cmd_buffer *cmdbuf,
          tu_cs_emit(cs, CCU_CLEAN_DEPTH);
       }
    } else {
+      /* When outside of renderpass, potential autotuner activity can cause
+       * interference between ZPASS_DONE event pairs. In that case, like at the
+       * beginning of the occlusion query, a fake ZPASS_DONE event is emitted to
+       * compose a begin-end event pair. The first event will write into the end
+       * field, but that will be overwritten by the second ZPASS_DONE which will
+       * also handle the diff accumulation.
+       */
+      if (!cmdbuf->state.pass) {
+         tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
+         tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
+                                          .write_sample_count = true).value);
+         tu_cs_emit_qw(cs, end_iova);
+      }
+
       tu_cs_emit_pkt7(cs, CP_EVENT_WRITE7, 3);
       tu_cs_emit(cs, CP_EVENT_WRITE7_0(.event = ZPASS_DONE,
                                        .write_sample_count = true,