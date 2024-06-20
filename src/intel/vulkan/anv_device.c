joshuaswarren/mesa-1

commit 9c85d6174c19fe6ec1869338c9ba5ee64d4b6c03

anv: move trtt submissions over to the anv_async_submit
We can remove a bunch of TRTT specific code from the backends as well
as manual submission tracking.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28975>

src/intel/vulkan/anv_device.c
@@ -3246,14 +3246,25 @@ anv_device_destroy_context_or_vm(struct anv_device *device)
    }
 }
 
-static void
+static VkResult
 anv_device_init_trtt(struct anv_device *device)
 {
    struct anv_trtt *trtt = &device->trtt;
 
+   VkResult result =
+      vk_sync_create(&device->vk,
+                     &device->physical->sync_syncobj_type,
+                     VK_SYNC_IS_TIMELINE,
+                     0 /* initial_value */,
+                     &trtt->timeline);
+   if (result != VK_SUCCESS)
+      return result;
+
    simple_mtx_init(&trtt->mutex, mtx_plain);
 
    list_inithead(&trtt->in_flight_batches);
+
+   return VK_SUCCESS;
 }
 
 static void
@@ -3261,31 +3272,9 @@ anv_device_finish_trtt(struct anv_device *device)
 {
    struct anv_trtt *trtt = &device->trtt;
 
-   if (trtt->timeline_val > 0) {
-      struct drm_syncobj_timeline_wait wait = {
-         .handles = (uintptr_t)&trtt->timeline_handle,
-         .points = (uintptr_t)&trtt->timeline_val,
-         .timeout_nsec = INT64_MAX,
-         .count_handles = 1,
-         .flags = DRM_SYNCOBJ_WAIT_FLAGS_WAIT_ALL,
-         .first_signaled = false,
-      };
-      if (intel_ioctl(device->fd, DRM_IOCTL_SYNCOBJ_TIMELINE_WAIT, &wait))
-         fprintf(stderr, "TR-TT syncobj wait failed!\n");
+   anv_sparse_trtt_garbage_collect_batches(device, true);
 
-      list_for_each_entry_safe(struct anv_trtt_batch_bo, trtt_bbo,
-                               &trtt->in_flight_batches, link)
-         anv_trtt_batch_bo_free(device, trtt_bbo);
-
-   }
-
-   if (trtt->timeline_handle > 0) {
-      struct drm_syncobj_destroy destroy = {
-         .handle = trtt->timeline_handle,
-      };
-      if (intel_ioctl(device->fd, DRM_IOCTL_SYNCOBJ_DESTROY, &destroy))
-         fprintf(stderr, "TR-TT syncobj destroy failed!\n");
-   }
+   vk_sync_destroy(&device->vk, trtt->timeline);
 
    simple_mtx_destroy(&trtt->mutex);
 
@@ -3915,6 +3904,10 @@ VkResult anv_CreateDevice(
       }
    }
 
+   result = anv_device_init_trtt(device);
+   if (result != VK_SUCCESS)
+      goto fail_companion_cmd_pool;
+
    anv_device_init_blorp(device);
 
    anv_device_init_border_colors(device);
@@ -3928,8 +3921,6 @@ VkResult anv_CreateDevice(
    anv_device_utrace_init(device);
 
    anv_device_init_embedded_samplers(device);
-
-   anv_device_init_trtt(device);
 
    BITSET_ONES(device->gfx_dirty_state);
    BITSET_CLEAR(device->gfx_dirty_state, ANV_GFX_STATE_INDEX_BUFFER);
@@ -3963,13 +3954,13 @@ VkResult anv_CreateDevice(
 
    result = anv_genX(device->info, init_device_state)(device);
    if (result != VK_SUCCESS)
-      goto fail_companion_cmd_pool;
+      goto fail_inits;
 
    *pDevice = anv_device_to_handle(device);
 
    return VK_SUCCESS;
 
- fail_companion_cmd_pool:
+ fail_inits:
    anv_device_finish_trtt(device);
    anv_device_finish_embedded_samplers(device);
    anv_device_utrace_finish(device);
@@ -3977,7 +3968,7 @@ VkResult anv_CreateDevice(
    anv_device_finish_rt_shaders(device);
    anv_device_finish_astc_emu(device);
    anv_device_finish_internal_kernels(device);
-
+ fail_companion_cmd_pool:
    if (device->info->verx10 >= 125) {
       vk_common_DestroyCommandPool(anv_device_to_handle(device),
                                    device->companion_rcs_cmd_pool, NULL);
@@ -4089,6 +4080,7 @@ void anv_DestroyDevice(
 
    struct anv_physical_device *pdevice = device->physical;
 
+   /* Do TRTT batch garbage collection before destroying queues. */
    anv_device_finish_trtt(device);
 
    for (uint32_t i = 0; i < device->queue_count; i++)

src/intel/vulkan/anv_batch_chain.c
@@ -1668,37 +1668,6 @@ anv_queue_submit_simple_batch(struct anv_queue *queue,
    return result;
 }
 
-VkResult
-anv_queue_submit_trtt_batch(struct anv_sparse_submission *submit,
-                            struct anv_batch *batch)
-{
-   struct anv_queue *queue = submit->queue;
-   struct anv_device *device = queue->device;
-   VkResult result = VK_SUCCESS;
-
-   uint32_t batch_size = align(batch->next - batch->start, 8);
-   struct anv_trtt_batch_bo *trtt_bbo;
-   result = anv_trtt_batch_bo_new(device, batch_size, &trtt_bbo);
-   if (result != VK_SUCCESS)
-      return result;
-
-   memcpy(trtt_bbo->bo->map, batch->start, trtt_bbo->size);
-#ifdef SUPPORT_INTEL_INTEGRATED_GPUS
-   if (device->physical->memory.need_flush &&
-       anv_bo_needs_host_cache_flush(trtt_bbo->bo->alloc_flags))
-      intel_flush_range(trtt_bbo->bo->map, trtt_bbo->size);
-#endif
-
-   if (INTEL_DEBUG(DEBUG_BATCH)) {
-      intel_print_batch(queue->decoder, trtt_bbo->bo->map, trtt_bbo->bo->size,
-                        trtt_bbo->bo->offset, false);
-   }
-
-   result = device->kmd_backend->execute_trtt_batch(submit, trtt_bbo);
-
-   return result;
-}
-
 void
 anv_cmd_buffer_clflush(struct anv_cmd_buffer **cmd_buffers,
                        uint32_t num_cmd_buffers)

src/intel/vulkan/anv_gem_stubs.c
@@ -66,13 +66,6 @@ stub_execute_simple_batch(struct anv_queue *queue, struct anv_bo *batch_bo,
 }
 
 static VkResult
-stub_execute_trtt_batch(struct anv_sparse_submission *submit,
-                        struct anv_trtt_batch_bo *trtt_bbo)
-{
-   return VK_ERROR_UNKNOWN;
-}
-
-static VkResult
 stub_queue_exec_locked(struct anv_queue *queue,
                        uint32_t wait_count,
                        const struct vk_sync_wait *waits,
@@ -180,7 +173,6 @@ const struct anv_kmd_backend *anv_stub_kmd_backend_get(void)
       .vm_bind_bo = stub_vm_bind_bo,
       .vm_unbind_bo = stub_vm_bind_bo,
       .execute_simple_batch = stub_execute_simple_batch,
-      .execute_trtt_batch = stub_execute_trtt_batch,
       .queue_exec_locked = stub_queue_exec_locked,
       .queue_exec_async = stub_queue_exec_async,
       .bo_alloc_flags_to_bo_flags = stub_bo_alloc_flags_to_bo_flags,

src/intel/vulkan/anv_genX.h
@@ -38,8 +38,10 @@
 
 struct intel_sample_positions;
 struct intel_urb_config;
+struct anv_async_submit;
 struct anv_embedded_sampler;
 struct anv_pipeline_embedded_sampler_binding;
+struct anv_trtt_bind;
 
 typedef struct nir_builder nir_builder;
 typedef struct nir_shader nir_shader;
@@ -351,9 +353,16 @@ genX(simple_shader_push_state_address)(struct anv_simple_shader *state,
 void
 genX(emit_simple_shader_end)(struct anv_simple_shader *state);
 
-VkResult genX(init_trtt_context_state)(struct anv_queue *queue);
+VkResult genX(init_trtt_context_state)(struct anv_device *device,
+                                       struct anv_async_submit *submit);
 
-VkResult genX(write_trtt_entries)(struct anv_trtt_submission *submit);
+void genX(write_trtt_entries)(struct anv_async_submit *submit,
+                              struct anv_trtt_bind *l3l2_binds,
+                              uint32_t n_l3l2_binds,
+                              struct anv_trtt_bind *l1_binds,
+                              uint32_t n_l1_binds);
+
+void genX(async_submit_end)(struct anv_async_submit *submit);
 
 void
 genX(cmd_buffer_emit_push_descriptor_buffer_surface)(struct anv_cmd_buffer *cmd_buffer,

src/intel/vulkan/anv_kmd_backend.h
@@ -40,7 +40,6 @@ struct anv_query_pool;
 struct anv_async_submit;
 struct anv_utrace_submit;
 struct anv_sparse_submission;
-struct anv_trtt_batch_bo;
 
 enum anv_vm_bind_op {
    /* bind vma specified in anv_vm_bind */
@@ -113,8 +112,6 @@ struct anv_kmd_backend {
                                     bool is_companion_rcs_batch);
    /* The caller is expected to hold device->mutex when calling this vfunc.
     */
-   VkResult (*execute_trtt_batch)(struct anv_sparse_submission *submit,
-                                  struct anv_trtt_batch_bo *trtt_bbo);
    VkResult (*queue_exec_locked)(struct anv_queue *queue,
                                  uint32_t wait_count,
                                  const struct vk_sync_wait *waits,

src/intel/vulkan/anv_private.h
@@ -768,35 +768,6 @@ struct anv_state_stream {
    struct util_dynarray all_blocks;
 };
 
-struct anv_sparse_submission {
-   struct anv_queue *queue;
-
-   struct anv_vm_bind *binds;
-   int binds_len;
-   int binds_capacity;
-
-   uint32_t wait_count;
-   uint32_t signal_count;
-
-   struct vk_sync_wait *waits;
-   struct vk_sync_signal *signals;
-};
-
-struct anv_trtt_bind {
-   uint64_t pte_addr;
-   uint64_t entry_addr;
-};
-
-struct anv_trtt_submission {
-   struct anv_sparse_submission *sparse;
-
-   struct anv_trtt_bind *l3l2_binds;
-   struct anv_trtt_bind *l1_binds;
-
-   int l3l2_binds_len;
-   int l1_binds_len;
-};
-
 /* The block_pool functions exported for testing only.  The block pool should
  * only be used via a state pool (see below).
  */
@@ -1788,19 +1759,6 @@ struct anv_device_astc_emu {
     VkPipeline pipeline;
 };
 
-struct anv_trtt_batch_bo {
-   struct anv_bo *bo;
-   uint32_t size;
-
-   /* Once device->trtt.timeline_handle signals timeline_val as complete we
-    * can free this struct and its members.
-    */
-   uint64_t timeline_val;
-
-   /* Part of device->trtt.in_flight_batches. */
-   struct list_head link;
-};
-
 struct anv_device {
     struct vk_device                            vk;
 
@@ -2028,12 +1986,11 @@ struct anv_device {
        struct anv_bo *cur_page_table_bo;
        uint64_t next_page_table_bo_offset;
 
-       /* Timeline syncobj used to track completion of the TR-TT batch BOs. */
-       uint32_t timeline_handle;
+       struct vk_sync *timeline;
        uint64_t timeline_val;
 
-       /* List of struct anv_trtt_batch_bo batches that are in flight and can
-        * be freed once their timeline gets signaled.
+       /* List of struct anv_trtt_submission that are in flight and can be
+        * freed once their vk_sync gets signaled.
         */
        struct list_head in_flight_batches;
     } trtt;
@@ -2203,17 +2160,6 @@ VkResult anv_queue_submit(struct vk_queue *queue,
 VkResult anv_queue_submit_simple_batch(struct anv_queue *queue,
                                        struct anv_batch *batch,
                                        bool is_companion_rcs_batch);
-VkResult anv_queue_submit_trtt_batch(struct anv_sparse_submission *submit,
-                                     struct anv_batch *batch);
-
-static inline void
-anv_trtt_batch_bo_free(struct anv_device *device,
-                       struct anv_trtt_batch_bo *trtt_bbo)
-{
-   anv_bo_pool_free(&device->batch_bo_pool, trtt_bbo->bo);
-   list_del(&trtt_bbo->link);
-   vk_free(&device->vk.alloc, trtt_bbo);
-}
 
 void anv_queue_trace(struct anv_queue *queue, const char *label,
                      bool frame, bool begin);
@@ -2521,6 +2467,32 @@ anv_async_submit_done(struct anv_async_submit *submit);
 bool
 anv_async_submit_wait(struct anv_async_submit *submit);
 
+struct anv_sparse_submission {
+   struct anv_queue *queue;
+
+   struct anv_vm_bind *binds;
+   int binds_len;
+   int binds_capacity;
+
+   uint32_t wait_count;
+   uint32_t signal_count;
+
+   struct vk_sync_wait *waits;
+   struct vk_sync_signal *signals;
+};
+
+struct anv_trtt_bind {
+   uint64_t pte_addr;
+   uint64_t entry_addr;
+};
+
+struct anv_trtt_submission {
+   struct anv_async_submit base;
+
+   struct anv_sparse_submission *sparse;
+
+   struct list_head link;
+};
 
 struct anv_device_memory {
    struct vk_device_memory                      vk;
@@ -3217,6 +3189,9 @@ VkResult anv_sparse_bind_image_memory(struct anv_queue *queue,
 VkResult anv_sparse_bind(struct anv_device *device,
                          struct anv_sparse_submission *sparse_submit);
 
+VkResult anv_sparse_trtt_garbage_collect_batches(struct anv_device *device,
+                                                 bool wait_completion);
+
 VkSparseImageFormatProperties
 anv_sparse_calc_image_format_properties(struct anv_physical_device *pdevice,
                                         VkImageAspectFlags aspect,
@@ -3236,8 +3211,6 @@ VkResult anv_sparse_image_check_support(struct anv_physical_device *pdevice,
                                         VkSampleCountFlagBits samples,
                                         VkImageType type,
                                         VkFormat format);
-VkResult anv_trtt_batch_bo_new(struct anv_device *device, uint32_t batch_size,
-                               struct anv_trtt_batch_bo **out_trtt_bbo);
 
 struct anv_buffer {
    struct vk_buffer vk;

src/intel/vulkan/anv_sparse.c
@@ -396,19 +396,10 @@ trtt_get_page_table_bo(struct anv_device *device, struct anv_bo **bo,
 }
 
 static VkResult
-anv_trtt_init_context_state(struct anv_queue *queue)
+anv_trtt_init_context_state(struct anv_device *device,
+                            struct anv_async_submit *submit)
 {
-   struct anv_device *device = queue->device;
    struct anv_trtt *trtt = &device->trtt;
-
-   struct drm_syncobj_create create = {
-      .handle = 0,
-      .flags = 0,
-   };
-   if (intel_ioctl(device->fd, DRM_IOCTL_SYNCOBJ_CREATE, &create))
-      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
-   assert(create.handle != 0);
-   trtt->timeline_handle = create.handle;
 
    struct anv_bo *l3_bo;
    VkResult result = trtt_get_page_table_bo(device, &l3_bo, &trtt->l3_addr);
@@ -430,24 +421,13 @@ anv_trtt_init_context_state(struct anv_queue *queue)
       goto fail_free_l3;
    }
 
-   result = anv_genX(device->info, init_trtt_context_state)(queue);
+   result = anv_genX(device->info, init_trtt_context_state)(device, submit);
 
    return result;
 
 fail_free_l3:
    vk_free(&device->vk.alloc, trtt->l3_mirror);
    return result;
-}
-
-static void
-anv_trtt_bind_list_add_entry(struct anv_trtt_bind *binds, int *binds_len,
-                             uint64_t pte_addr, uint64_t entry_addr)
-{
-   binds[*binds_len] = (struct anv_trtt_bind) {
-      .pte_addr = pte_addr,
-      .entry_addr = entry_addr,
-   };
-   (*binds_len)++;
 }
 
 /* For L3 and L2 pages, null and invalid entries are indicated by bits 1 and 0
@@ -457,13 +437,27 @@ anv_trtt_bind_list_add_entry(struct anv_trtt_bind *binds, int *binds_len,
 #define ANV_TRTT_L3L2_NULL_ENTRY (1 << 1)
 #define ANV_TRTT_L3L2_INVALID_ENTRY (1 << 0)
 
+static void
+anv_trtt_bind_list_add_entry(struct anv_trtt_bind *binds, uint32_t *binds_len,
+                             uint64_t pte_addr, uint64_t entry_addr)
+{
+   binds[*binds_len] = (struct anv_trtt_bind) {
+      .pte_addr = pte_addr,
+      .entry_addr = entry_addr,
+   };
+   (*binds_len)++;
+}
+
 /* Adds elements to the anv_trtt_bind structs passed. This doesn't write the
  * entries to the HW yet.
  */
 static VkResult
 anv_trtt_bind_add(struct anv_device *device,
                   uint64_t trtt_addr, uint64_t dest_addr,
-                  struct anv_trtt_submission *s)
+                  struct anv_trtt_bind *l3l2_binds,
+                  uint32_t *n_l3l2_binds,
+                  struct anv_trtt_bind *l1_binds,
+                  uint32_t *n_l1_binds)
 {
    VkResult result = VK_SUCCESS;
    struct anv_trtt *trtt = &device->trtt;
@@ -480,9 +474,10 @@ anv_trtt_bind_add(struct anv_device *device,
       if (is_null_bind) {
          trtt->l3_mirror[l3_index] = ANV_TRTT_L3L2_NULL_ENTRY;
 
-         anv_trtt_bind_list_add_entry(s->l3l2_binds, &s->l3l2_binds_len,
-               trtt->l3_addr + l3_index * sizeof(uint64_t),
-               ANV_TRTT_L3L2_NULL_ENTRY);
+         anv_trtt_bind_list_add_entry(l3l2_binds, n_l3l2_binds,
+                                      trtt->l3_addr + l3_index *
+                                      sizeof(uint64_t),
+                                      ANV_TRTT_L3L2_NULL_ENTRY);
 
          return VK_SUCCESS;
       }
@@ -494,8 +489,9 @@ anv_trtt_bind_add(struct anv_device *device,
 
       trtt->l3_mirror[l3_index] = l2_addr;
 
-      anv_trtt_bind_list_add_entry(s->l3l2_binds, &s->l3l2_binds_len,
-            trtt->l3_addr + l3_index * sizeof(uint64_t), l2_addr);
+      anv_trtt_bind_list_add_entry(l3l2_binds, n_l3l2_binds,
+                                   trtt->l3_addr + l3_index *
+                                   sizeof(uint64_t), l2_addr);
    }
    assert(l2_addr != 0 && l2_addr != ANV_TRTT_L3L2_NULL_ENTRY);
 
@@ -508,9 +504,9 @@ anv_trtt_bind_add(struct anv_device *device,
          trtt->l2_mirror[l3_index * 512 + l2_index] =
             ANV_TRTT_L3L2_NULL_ENTRY;
 
-         anv_trtt_bind_list_add_entry(s->l3l2_binds, &s->l3l2_binds_len,
-               l2_addr + l2_index * sizeof(uint64_t),
-               ANV_TRTT_L3L2_NULL_ENTRY);
+         anv_trtt_bind_list_add_entry(l3l2_binds, n_l3l2_binds,
+                                      l2_addr + l2_index * sizeof(uint64_t),
+                                      ANV_TRTT_L3L2_NULL_ENTRY);
 
          return VK_SUCCESS;
       }
@@ -522,13 +518,65 @@ anv_trtt_bind_add(struct anv_device *device,
 
       trtt->l2_mirror[l3_index * 512 + l2_index] = l1_addr;
 
-      anv_trtt_bind_list_add_entry(s->l3l2_binds, &s->l3l2_binds_len,
-            l2_addr + l2_index * sizeof(uint64_t), l1_addr);
+      anv_trtt_bind_list_add_entry(l3l2_binds, n_l3l2_binds,
+                                   l2_addr + l2_index * sizeof(uint64_t),
+                                   l1_addr);
    }
    assert(l1_addr != 0 && l1_addr != ANV_TRTT_L3L2_NULL_ENTRY);
 
-   anv_trtt_bind_list_add_entry(s->l1_binds, &s->l1_binds_len,
-            l1_addr + l1_index * sizeof(uint32_t), dest_addr);
+   anv_trtt_bind_list_add_entry(l1_binds, n_l1_binds,
+                                l1_addr + l1_index * sizeof(uint32_t),
+                                dest_addr);
+
+   return VK_SUCCESS;
+}
+
+VkResult
+anv_sparse_trtt_garbage_collect_batches(struct anv_device *device,
+                                        bool wait_completion)
+{
+   struct anv_trtt *trtt = &device->trtt;
+
+   uint64_t last_value;
+   if (!wait_completion) {
+      VkResult result =
+         vk_sync_get_value(&device->vk, trtt->timeline, &last_value);
+      if (result != VK_SUCCESS)
+         return result;
+   } else {
+      last_value = trtt->timeline_val;
+   }
+
+   list_for_each_entry_safe(struct anv_trtt_submission, submit,
+                            &trtt->in_flight_batches, link) {
+      if (submit->base.signal.signal_value <= last_value) {
+         list_del(&submit->link);
+         anv_async_submit_fini(&submit->base);
+         vk_free(&device->vk.alloc, submit);
+         continue;
+      }
+
+      if (!wait_completion)
+         break;
+
+      VkResult result = vk_sync_wait(
+         &device->vk,
+         submit->base.signal.sync,
+         submit->base.signal.signal_value,
+         VK_SYNC_WAIT_COMPLETE,
+         os_time_get_absolute_timeout(OS_TIMEOUT_INFINITE));
+      if (result == VK_SUCCESS) {
+         list_del(&submit->link);
+         anv_async_submit_fini(&submit->base);
+         vk_free(&device->vk.alloc, submit);
+         continue;
+      }
+
+      /* If the wait failed but the caller wanted completion, return the
+       * error.
+       */
+      return result;
+   }
 
    return VK_SUCCESS;
 }
@@ -544,6 +592,35 @@ anv_sparse_bind_trtt(struct anv_device *device,
     * give one, such as resource creation. */
    if (!sparse_submit->queue)
       sparse_submit->queue = trtt->queue;
+
+   struct anv_trtt_submission *submit =
+      vk_zalloc(&device->vk.alloc, sizeof(*submit), 8,
+                VK_SYSTEM_ALLOCATION_SCOPE_DEVICE);
+   if (submit == NULL)
+      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   result = anv_async_submit_init(&submit->base, sparse_submit->queue,
+                                  &device->batch_bo_pool,
+                                  false, false);
+   if (result != VK_SUCCESS)
+      goto error_async;
+
+   simple_mtx_lock(&trtt->mutex);
+
+   anv_sparse_trtt_garbage_collect_batches(device, false);
+
+   submit->base.signal = (struct vk_sync_signal) {
+      .sync = trtt->timeline,
+      .signal_value = ++trtt->timeline_val,
+   };
+
+   /* If the TRTT L3 table was never set, initialize it as part of this
+    * submission.
+    */
+   if (!trtt->l3_addr)
+      anv_trtt_init_context_state(device, &submit->base);
+
+   assert(trtt->l3_addr);
 
    /* These capacities are conservative estimations. For L1 binds the
     * number will match exactly unless we skip NULL binds due to L2 already
@@ -561,26 +638,15 @@ anv_sparse_bind_trtt(struct anv_device *device,
       l3l2_binds_capacity += (pages / 1024 + 1) * 2;
    }
 
+   /* Turn a series of virtual address maps, into a list of L3/L2/L1 TRTT page
+    * table updates.
+    */
    STACK_ARRAY(struct anv_trtt_bind, l3l2_binds, l3l2_binds_capacity);
    STACK_ARRAY(struct anv_trtt_bind, l1_binds, l1_binds_capacity);
-   struct anv_trtt_submission trtt_submit = {
-      .sparse = sparse_submit,
-      .l3l2_binds = l3l2_binds,
-      .l1_binds = l1_binds,
-      .l3l2_binds_len = 0,
-      .l1_binds_len = 0,
-   };
-
-   simple_mtx_lock(&trtt->mutex);
-
-   if (!trtt->l3_addr)
-      anv_trtt_init_context_state(sparse_submit->queue);
-
-   assert(trtt->l3_addr);
-
-   for (int b = 0; b < sparse_submit->binds_len; b++) {
+   uint32_t n_l3l2_binds = 0, n_l1_binds = 0;
+   for (int b = 0; b < sparse_submit->binds_len && result == VK_SUCCESS; b++) {
       struct anv_vm_bind *vm_bind = &sparse_submit->binds[b];
-      for (size_t i = 0; i < vm_bind->size; i += 64 * 1024) {
+      for (size_t i = 0; i < vm_bind->size && result == VK_SUCCESS; i += 64 * 1024) {
          uint64_t trtt_addr = vm_bind->address + i;
          uint64_t dest_addr =
             (vm_bind->op == ANV_VM_BIND && vm_bind->bo) ?
@@ -588,29 +654,74 @@ anv_sparse_bind_trtt(struct anv_device *device,
                ANV_TRTT_L1_NULL_TILE_VAL;
 
          result = anv_trtt_bind_add(device, trtt_addr, dest_addr,
-                                    &trtt_submit);
-         if (result != VK_SUCCESS)
-            goto out;
+                                    l3l2_binds, &n_l3l2_binds,
+                                    l1_binds, &n_l1_binds);
       }
    }
 
-   assert(trtt_submit.l3l2_binds_len <= l3l2_binds_capacity);
-   assert(trtt_submit.l1_binds_len <= l1_binds_capacity);
+   assert(n_l3l2_binds <= l3l2_binds_capacity);
+   assert(n_l1_binds <= l1_binds_capacity);
 
-   sparse_debug("trtt_binds: num_vm_binds:%02d l3l2:%04d l1:%04d\n",
-                sparse_submit->binds_len, trtt_submit.l3l2_binds_len,
-                trtt_submit.l1_binds_len);
+   /* Convert the L3/L2/L1 TRTT page table updates in anv_trtt_bind elements
+    * into MI commands.
+    */
+   if (result == VK_SUCCESS) {
+      sparse_debug("trtt_binds: num_vm_binds:%02d l3l2:%04d l1:%04d\n",
+                   sparse_submit->binds_len, n_l3l2_binds, n_l1_binds);
 
-   if (trtt_submit.l3l2_binds_len || trtt_submit.l1_binds_len)
-      result = anv_genX(device->info, write_trtt_entries)(&trtt_submit);
+      if (n_l3l2_binds || n_l1_binds) {
+         anv_genX(device->info, write_trtt_entries)(
+            &submit->base, l3l2_binds, n_l3l2_binds, l1_binds, n_l1_binds);
+      }
+   }
 
-   if (result == VK_SUCCESS)
-      ANV_RMV(vm_binds, device, sparse_submit->binds, sparse_submit->binds_len);
-
-out:
-   simple_mtx_unlock(&trtt->mutex);
    STACK_ARRAY_FINISH(l1_binds);
    STACK_ARRAY_FINISH(l3l2_binds);
+
+   anv_genX(device->info, async_submit_end)(&submit->base);
+
+   if (submit->base.batch.status != VK_SUCCESS) {
+      result = submit->base.batch.status;
+      goto error_add_bind;
+   }
+
+   /* Add all the BOs backing TRTT page tables to the reloc list.
+    *
+    * TODO: we could narrow down the list by using anv_address structures in
+    *       anv_trtt_bind for the pte_addr.
+    */
+   if (device->physical->uses_relocs) {
+      for (int i = 0; i < trtt->num_page_table_bos; i++) {
+         result = anv_reloc_list_add_bo(&submit->base.relocs,
+                                        trtt->page_table_bos[i]);
+         if (result != VK_SUCCESS)
+            goto error_add_bind;
+      }
+   }
+
+   result =
+      device->kmd_backend->queue_exec_async(&submit->base,
+                                            sparse_submit->wait_count,
+                                            sparse_submit->waits,
+                                            sparse_submit->signal_count,
+                                            sparse_submit->signals);
+   if (result != VK_SUCCESS)
+      goto error_add_bind;
+
+
+   list_addtail(&submit->link, &trtt->in_flight_batches);
+
+   simple_mtx_unlock(&trtt->mutex);
+
+   ANV_RMV(vm_binds, device, sparse_submit->binds, sparse_submit->binds_len);
+
+   return VK_SUCCESS;
+
+ error_add_bind:
+   simple_mtx_unlock(&trtt->mutex);
+   anv_async_submit_fini(&submit->base);
+ error_async:
+   vk_free(&device->vk.alloc, submit);
    return result;
 }
 
@@ -1298,66 +1409,4 @@ anv_sparse_image_check_support(struct anv_physical_device *pdevice,
       return VK_ERROR_FORMAT_NOT_SUPPORTED;
 
    return VK_SUCCESS;
-}
-
-static VkResult
-anv_trtt_garbage_collect_batches(struct anv_device *device)
-{
-   struct anv_trtt *trtt = &device->trtt;
-
-   if (trtt->timeline_val % 8 != 7)
-      return VK_SUCCESS;
-
-   uint64_t cur_timeline_val = 0;
-   struct drm_syncobj_timeline_array array = {
-      .handles = (uintptr_t)&trtt->timeline_handle,
-      .points = (uintptr_t)&cur_timeline_val,
-      .count_handles = 1,
-      .flags = 0,
-   };
-   if (intel_ioctl(device->fd, DRM_IOCTL_SYNCOBJ_QUERY, &array))
-      return vk_error(device, VK_ERROR_UNKNOWN);
-
-   list_for_each_entry_safe(struct anv_trtt_batch_bo, trtt_bbo,
-                            &trtt->in_flight_batches, link) {
-      if (trtt_bbo->timeline_val > cur_timeline_val)
-         return VK_SUCCESS;
-
-      anv_trtt_batch_bo_free(device, trtt_bbo);
-   }
-
-   return VK_SUCCESS;
-}
-
-VkResult
-anv_trtt_batch_bo_new(struct anv_device *device, uint32_t batch_size,
-                      struct anv_trtt_batch_bo **out_trtt_bbo)
-{
-   struct anv_trtt *trtt = &device->trtt;
-   VkResult result;
-
-   anv_trtt_garbage_collect_batches(device);
-
-   struct anv_trtt_batch_bo *trtt_bbo =
-      vk_alloc(&device->vk.alloc, sizeof(*trtt_bbo), 8,
-               VK_SYSTEM_ALLOCATION_SCOPE_DEVICE);
-   if (!trtt_bbo)
-      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
-
-   result = anv_bo_pool_alloc(&device->batch_bo_pool, batch_size,
-                              &trtt_bbo->bo);
-   if (result != VK_SUCCESS)
-      goto out;
-
-   trtt_bbo->size = batch_size;
-   trtt_bbo->timeline_val = ++trtt->timeline_val;
-
-   list_addtail(&trtt_bbo->link, &trtt->in_flight_batches);
-
-   *out_trtt_bbo = trtt_bbo;
-
-   return VK_SUCCESS;
-out:
-   vk_free(&device->vk.alloc, trtt_bbo);
-   return result;
 }

commit f1123d4f8878f9d5c5fa9bb50cd00cf01e1991fc

anv: avoid initalizing TRTT stuff without sparseBinding
9c85d6174c ("anv: move trtt submissions over to the anv_async_submit")
added a hard dependency on timeline semaphore which is still optional.
And since it gates the sparseBinding feature, we should not use it if
sparseBinding is not enabled.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 9c85d6174c ("anv: move trtt submissions over to the anv_async_submit")
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29779>

src/intel/vulkan/anv_device.c
@@ -3249,6 +3249,10 @@ anv_device_destroy_context_or_vm(struct anv_device *device)
 static VkResult
 anv_device_init_trtt(struct anv_device *device)
 {
+   if (device->physical->sparse_type != ANV_SPARSE_TYPE_TRTT ||
+       !device->vk.enabled_features.sparseBinding)
+      return VK_SUCCESS;
+
    struct anv_trtt *trtt = &device->trtt;
 
    VkResult result =
@@ -3270,6 +3274,10 @@ anv_device_init_trtt(struct anv_device *device)
 static void
 anv_device_finish_trtt(struct anv_device *device)
 {
+   if (device->physical->sparse_type != ANV_SPARSE_TYPE_TRTT ||
+       !device->vk.enabled_features.sparseBinding)
+      return;
+
    struct anv_trtt *trtt = &device->trtt;
 
    anv_sparse_trtt_garbage_collect_batches(device, true);