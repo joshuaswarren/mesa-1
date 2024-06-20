joshuaswarren/mesa-1

commit 1a0bb42b0dc848030c475fce65f2870e0445c444

u_gralloc/fallback: Extract modifier from QCOM native_handle
After Turnip moved to u_gralloc, some users started complaining
about issues. It turns out that modern platforms do not have
grallocs{0,1} but have gralloc4 only. There's no
known way to build mesa3d with gralloc4 support using NDK.

So we add this workaround to fallback gralloc to mimic the original
behavior of the Turnip driver before switching to u_gralloc.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11171
Fixes: 350fc1917384 ("turnip: ANB/AHB support")
Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29260>

src/util/u_gralloc/u_gralloc_fallback.c
@@ -107,6 +107,9 @@ fallback_gralloc_get_buffer_info(struct u_gralloc *gralloc,
    int stride = 0;
    int fds[3];
 
+   if (hnd->handle->numFds == 0)
+      return -EINVAL;
+
    if (is_hal_format_yuv(hnd->hal_format)) {
       int ret = fallback_gralloc_get_yuv_info(gralloc, hnd, out);
       /*
@@ -125,11 +128,7 @@ fallback_gralloc_get_buffer_info(struct u_gralloc *gralloc,
     * color compression state buffer, but the rest of the code isn't ready
     * yet to deal with modifiers:
     */
-   num_planes = get_native_buffer_fds(hnd->handle, fds);
-   if (num_planes == 0)
-      return -EINVAL;
-
-   assert(num_planes == 1);
+   num_planes = 1;
 
    drm_fourcc = get_fourcc_from_hal_format(hnd->hal_format);
    if (drm_fourcc == -1) {
@@ -148,6 +147,15 @@ fallback_gralloc_get_buffer_info(struct u_gralloc *gralloc,
    out->num_planes = num_planes;
    out->fds[0] = fds[0];
    out->strides[0] = stride;
+
+#ifdef HAS_FREEDRENO
+   uint32_t gmsm = ('g' << 24) | ('m' << 16) | ('s' << 8) | 'm';
+   if (hnd->handle->numInts >= 2 && hnd->handle->data[hnd->handle->numFds] == gmsm) {
+      /* This UBWC flag was introduced in a5xx. */
+      bool ubwc = hnd->handle->data[hnd->handle->numFds + 1] & 0x08000000;
+      out->modifier = ubwc ? DRM_FORMAT_MOD_QCOM_COMPRESSED : DRM_FORMAT_MOD_LINEAR;
+   }
+#endif
 
    return 0;
 }

src/util/u_gralloc/meson.build
@@ -23,6 +23,10 @@ if dep_android_mapper4.found()
   options_for_u_gralloc += 'cpp_std=c++17'
 endif
 
+if with_freedreno_vk or with_gallium_freedreno
+  c_args_for_u_gralloc += '-DHAS_FREEDRENO'
+endif
+
 _libmesa_u_gralloc = static_library(
   '_mesa_u_gralloc',
   [files_u_gralloc],

commit 3ac59f27e0a6b139bd7f6f6796b9e6046b2bf896

u_gralloc/fallback: Set fd from handle directly
Fix returned fd by populating directly from the handle, instead of
from the fds array which is never populated.

Fixes: 1a0bb42b0dc8 ("u_gralloc/fallback: Extract modifier from QCOM native_handle")
Reviewed-by: Roman Stratiienko <r.stratiienko@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29785>

src/util/u_gralloc/u_gralloc_fallback.c
@@ -105,7 +105,6 @@ fallback_gralloc_get_buffer_info(struct u_gralloc *gralloc,
    int num_planes = 0;
    int drm_fourcc = 0;
    int stride = 0;
-   int fds[3];
 
    if (hnd->handle->numFds == 0)
       return -EINVAL;
@@ -145,7 +144,7 @@ fallback_gralloc_get_buffer_info(struct u_gralloc *gralloc,
    out->drm_fourcc = drm_fourcc;
    out->modifier = DRM_FORMAT_MOD_INVALID;
    out->num_planes = num_planes;
-   out->fds[0] = fds[0];
+   out->fds[0] = hnd->handle->data[0];
    out->strides[0] = stride;
 
 #ifdef HAS_FREEDRENO