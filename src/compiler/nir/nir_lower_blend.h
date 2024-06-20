openharmony/third_party_mesa3d

commit 4663d9795280780f2df99223fa2c438af8e75dee

Convert a few files to UTF-8
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11788>

src/compiler/nir/nir_lower_blend.h
@@ -1,5 +1,5 @@
 /*
- * Copyright Â 2019 Alyssa Rosenzweig
+ * Copyright Ã‚ 2019 Alyssa Rosenzweig
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),

src/gallium/drivers/svga/include/svga3d_surfacedefs.h
@@ -1,6 +1,6 @@
 /**************************************************************************
  *
- * Copyright © 1998-2015 VMware, Inc., Palo Alto, CA., USA
+ * Copyright Â© 1998-2015 VMware, Inc., Palo Alto, CA., USA
  * All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a

src/mesa/x86/mmx_blendtmp.h
@@ -1,5 +1,5 @@
 /*
- * Written by José Fonseca <j_r_fonseca@yahoo.co.uk>
+ * Written by JosÃ© Fonseca <j_r_fonseca@yahoo.co.uk>
  */
 
 

joshuaswarren/mesa-1

commit 0b705a561e120f2475826a4ae107edf18ef79177

nir: fix utf-8 encoding-issue
This UTF-8 encoding issue seems to cause issues with the doxygen
docs-integration.

Fixes: 4663d979528 ("Convert a few files to UTF-8")
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29801>

src/compiler/nir/nir_lower_blend.h
@@ -1,5 +1,5 @@
 /*
- * Copyright Â 2019 Alyssa Rosenzweig
+ * Copyright © 2019 Alyssa Rosenzweig
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),