joshuaswarren/mesa-1

commit cb944e2b422f10a8efc461770fb6dcc3fc790530

egl/dri: Use pipe_format instead of DRI_IMAGE_FORMAT
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27709>

src/egl/drivers/dri2/egl_dri2.c
@@ -2430,7 +2430,7 @@ dri2_create_image_mesa_drm_buffer(_EGLDisplay *disp, _EGLContext *ctx,
 
    switch (attrs.DRMBufferFormatMESA) {
    case EGL_DRM_BUFFER_FORMAT_ARGB32_MESA:
-      format = __DRI_IMAGE_FORMAT_ARGB8888;
+      format = PIPE_FORMAT_B8G8R8A8_UNORM;
       pitch = attrs.DRMBufferStrideMESA;
       break;
    default:
@@ -2883,7 +2883,7 @@ dri2_create_drm_image_mesa(_EGLDisplay *disp, const EGLint *attr_list)
 
    switch (attrs.DRMBufferFormatMESA) {
    case EGL_DRM_BUFFER_FORMAT_ARGB32_MESA:
-      format = __DRI_IMAGE_FORMAT_ARGB8888;
+      format = PIPE_FORMAT_B8G8R8A8_UNORM;
       break;
    default:
       _eglError(EGL_BAD_PARAMETER, __func__);

commit 54ba347a5e940ce29333c11a50459900d811a302

egl/dri: Use packed pipe_format
This is consistent with __DRI_IMAGE_FORMAT_ARGB8888 and the rest of
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27709 .

This makes no difference with little endian, it does with big endian
though.

Fixes: cb944e2b422f ("egl/dri: Use pipe_format instead of DRI_IMAGE_FORMAT")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29781>

src/egl/drivers/dri2/egl_dri2.c
@@ -2447,7 +2447,7 @@ dri2_create_image_mesa_drm_buffer(_EGLDisplay *disp, _EGLContext *ctx,
 
    switch (attrs.DRMBufferFormatMESA) {
    case EGL_DRM_BUFFER_FORMAT_ARGB32_MESA:
-      format = PIPE_FORMAT_B8G8R8A8_UNORM;
+      format = PIPE_FORMAT_BGRA8888_UNORM;
       pitch = attrs.DRMBufferStrideMESA;
       break;
    default:
@@ -2900,7 +2900,7 @@ dri2_create_drm_image_mesa(_EGLDisplay *disp, const EGLint *attr_list)
 
    switch (attrs.DRMBufferFormatMESA) {
    case EGL_DRM_BUFFER_FORMAT_ARGB32_MESA:
-      format = PIPE_FORMAT_B8G8R8A8_UNORM;
+      format = PIPE_FORMAT_BGRA8888_UNORM;
       break;
    default:
       _eglError(EGL_BAD_PARAMETER, __func__);