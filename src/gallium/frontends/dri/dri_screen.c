joshuaswarren/mesa-1

commit d2641b90ed47c9754af7eaa97ae425629e3f1d06

st/dri: Check format properties from format helpers
When populating DRI configs, driconf allows us to exclude
10bpc/RGBA/float configs. Rather than having a hardcoded set of formats
which satisfy those properties, just check the actual format
definitions.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27709>

src/gallium/frontends/dri/dri_screen.c
@@ -386,26 +386,27 @@ dri_fill_in_modes(struct dri_screen *screen)
 
       /* Expose only BGRA ordering if the loader doesn't support RGBA ordering. */
       if (!allow_rgba_ordering &&
-          (pipe_formats[f] == PIPE_FORMAT_RGBA8888_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_RGBX8888_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_RGBA8888_SRGB  ||
-           pipe_formats[f] == PIPE_FORMAT_RGBX8888_SRGB  ||
-           pipe_formats[f] == PIPE_FORMAT_R5G5B5A1_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R5G5B5X1_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R4G4B4A4_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R4G4B4X4_UNORM))
+          util_format_get_component_shift(pipe_formats[f],
+                                          UTIL_FORMAT_COLORSPACE_RGB, 0)
+#if UTIL_ARCH_BIG_ENDIAN
+         >
+#else
+         <
+#endif
+          util_format_get_component_shift(pipe_formats[f],
+                                          UTIL_FORMAT_COLORSPACE_RGB, 2))
          continue;
 
       if (!allow_rgb10 &&
-          (pipe_formats[f] == PIPE_FORMAT_B10G10R10A2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_B10G10R10X2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R10G10B10A2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R10G10B10X2_UNORM))
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 0) == 10 &&
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 1) == 10 &&
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 2) == 10)
          continue;
 
-      if (!allow_fp16 &&
-          (pipe_formats[f] == PIPE_FORMAT_R16G16B16A16_FLOAT ||
-           pipe_formats[f] == PIPE_FORMAT_R16G16B16X16_FLOAT))
+      if (!allow_fp16 && util_format_is_float(pipe_formats[f]))
          continue;
 
       if (!p_screen->is_format_supported(p_screen, pipe_formats[f],

commit 05abd369611d0ea6bf8c7467dd400ebbe1469c2e

dri: Fix BGR format exclusion
The check we had for BGR vs. RGB formats was testing completely the
wrong thing. Fix it so we can restore the previous set of configs we
expose to the frontend, which also fixes surfaceless platform on s390x.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Fixes: d2641b90ed47 ("st/dri: Check format properties from format helpers")
Closes: mesa/mesa#11360
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29837>

src/gallium/frontends/dri/dri_screen.c
@@ -386,17 +386,21 @@ dri_fill_in_modes(struct dri_screen *screen)
       uint8_t msaa_modes[MSAA_VISUAL_MAX_SAMPLES];
 
       /* Expose only BGRA ordering if the loader doesn't support RGBA ordering. */
-      if (!allow_rgba_ordering &&
-          util_format_get_component_shift(pipe_formats[f],
-                                          UTIL_FORMAT_COLORSPACE_RGB, 0)
+      if (!allow_rgba_ordering) {
+          unsigned sh_ax = util_format_get_component_shift(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 3);
+          unsigned sh_b = util_format_get_component_shift(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 2);
 #if UTIL_ARCH_BIG_ENDIAN
-         >
+          unsigned sz_b = util_format_get_component_bits(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 2);
+
+          if (sz_b + sh_b == sh_ax)
+             continue;
 #else
-         <
+          unsigned sz_ax = util_format_get_component_bits(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 3);
+
+          if (sz_ax + sh_ax == sh_b)
+             continue;
 #endif
-          util_format_get_component_shift(pipe_formats[f],
-                                          UTIL_FORMAT_COLORSPACE_RGB, 2))
-         continue;
+       }
 
       if (!allow_rgb10 &&
           util_format_get_component_bits(pipe_formats[f],