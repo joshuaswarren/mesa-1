joshuaswarren/mesa-1

commit 1236ff0c396f0914bfe81c50beef3693f9e1dbd9

anv,driconf: Add fake non device local memory WA for Total War: Warhammer 3
Cc: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8721
Signed-off-by: Sviatoslav Peleshko <sviatoslav.peleshko@globallogic.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29127>

src/intel/vulkan/anv_device.c
@@ -93,6 +93,7 @@ static const driOptionDescription anv_dri_options[] = {
       DRI_CONF_SHADER_SPILLING_RATE(0)
       DRI_CONF_OPT_B(intel_tbimr, true, "Enable TBIMR tiled rendering")
       DRI_CONF_ANV_COMPRESSION_CONTROL_ENABLED(false)
+      DRI_CONF_ANV_FAKE_NONLOCAL_MEMORY(false)
    DRI_CONF_SECTION_END
 
    DRI_CONF_SECTION_DEBUG
@@ -2697,6 +2698,8 @@ anv_init_dri_options(struct anv_instance *instance)
             driQueryOptionb(&instance->dri_options, "anv_external_memory_implicit_sync");
     instance->compression_control_enabled =
        driQueryOptionb(&instance->dri_options, "compression_control_enabled");
+    instance->anv_fake_nonlocal_memory =
+            driQueryOptionb(&instance->dri_options, "anv_fake_nonlocal_memory");
 }
 
 VkResult anv_CreateInstance(
@@ -2878,6 +2881,26 @@ void anv_GetPhysicalDeviceMemoryProperties(
          .size    = physical_device->memory.heaps[i].size,
          .flags   = physical_device->memory.heaps[i].flags,
       };
+   }
+
+   /* Some games (e.g. Total War: WARHAMMER III) sometimes completely refuse
+    * to use memory types with VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT set.
+    * On iGPUs we have only device-local memory, so we must hide it
+    * from the flags.
+    *
+    * Additionally, TW also seems to crash if a non-local but also
+    * non host-visible memory is present, so we should be careful which
+    * memory types we hide this flag from.
+    */
+   if (physical_device->instance->anv_fake_nonlocal_memory &&
+       !anv_physical_device_has_vram(physical_device)) {
+      for (uint32_t i = 0; i < physical_device->memory.type_count; i++) {
+         if (pMemoryProperties->memoryTypes[i].propertyFlags &
+             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+            pMemoryProperties->memoryTypes[i].propertyFlags &=
+                  ~VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+         }
+      }
    }
 }
 

src/intel/vulkan/anv_private.h
@@ -1288,6 +1288,7 @@ struct anv_instance {
     bool                                        has_fake_sparse;
     bool                                        disable_fcv;
     bool                                        compression_control_enabled;
+    bool                                        anv_fake_nonlocal_memory;
 
     /* HW workarounds */
     bool                                        no_16bit;

src/util/00-mesa-defaults.conf
@@ -1248,6 +1248,9 @@ TODO: document the other workarounds.
         <application name="DIRT 5" executable="DIRT5.exe">
             <option name="fp64_workaround_enabled" value="true" />
         </application>
+        <application name="Total War: WARHAMMER III" executable="TotalWarhammer3">
+            <option name="anv_fake_nonlocal_memory" value="true" />
+        </application>
         <!--
         Disable 16-bit feature on zink and angle so that GLES mediump doesn't
         lower to our inefficent 16-bit shader support.  No need to do so for

src/util/driconf.h
@@ -790,6 +790,10 @@
 #define DRI_CONF_ANV_COMPRESSION_CONTROL_ENABLED(def) \
    DRI_CONF_OPT_B(compression_control_enabled, def, "Enable VK_EXT_image_compression_control support")
 
+#define DRI_CONF_ANV_FAKE_NONLOCAL_MEMORY(def) \
+   DRI_CONF_OPT_B(anv_fake_nonlocal_memory, def, \
+                  "Present host-visible device-local memory types as non device-local")
+
 /**
  * \brief HASVK specific configuration options
  */

commit 7f2d99631413d67823476a92ae2c22410cb702e5

anv: reimplement the anv_fake_nonlocal_memory workaround
Commit 1236ff0c396f ("anv,driconf: Add fake non device local memory WA
for Total War: Warhammer 3") implemented a workaround to make
Warhammer 3 work on ADL, but the game still doesn't work on LNL, which
uses xe.ko, and MTL, which uses i915.ko: it still fails at launch
claiming it couldn't allocate memory.

So in this implementation, instead of clearing DEVICE_LOCAL_BIT we
just duplicate our memory types, one having the bit and one not
having.

v2:
 - Check for VK_MAX_MEMORY_TYPES (José)
 - Invert the order of the memory types (José)
 - Fix white space issue (José)
v3:
 - Comment our non-spec-compliance (José)
 - Remove useless lines (José)

Link: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8721
Fixes: 1236ff0c396f ("anv,driconf: Add fake non device local memory WA for Total War: Warhammer 3")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30162>

src/intel/vulkan/anv_device.c
@@ -2030,6 +2030,31 @@ anv_physical_device_init_heaps(struct anv_physical_device *device, int fd)
    if (result != VK_SUCCESS)
       return result;
 
+   /* Some games (e.g., Total War: WARHAMMER III) sometimes seem to expect to
+    * find memory types both with and without
+    * VK_MEMORY_TYPE_PROPERTY_DEVICE_LOCAL_BIT. So here we duplicate all our
+    * memory types just to make these games happy.
+    * This behavior is not spec-compliant as we still only have one heap that
+    * is now inconsistent with some of the memory types, but the game doesn't
+    * seem to care about it.
+    */
+   if (device->instance->anv_fake_nonlocal_memory &&
+       !anv_physical_device_has_vram(device)) {
+      const uint32_t base_types_count = device->memory.type_count;
+      for (int i = 0; i < base_types_count; i++) {
+         if (!(device->memory.types[i].propertyFlags &
+               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT))
+            continue;
+
+         struct anv_memory_type *new_type =
+            &device->memory.types[device->memory.type_count++];
+         *new_type = device->memory.types[i];
+
+         device->memory.types[i].propertyFlags &=
+            ~VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+      }
+   }
+
    /* Replicate all non protected memory types for descriptor buffers because
     * we want to identify memory allocations to place them in the right memory
     * heap.
@@ -2068,6 +2093,8 @@ anv_physical_device_init_heaps(struct anv_physical_device *device, int fd)
       *new_type = device->memory.types[i];
       new_type->descriptor_buffer = true;
    }
+
+   assert(device->memory.type_count <= VK_MAX_MEMORY_TYPES);
 
    for (unsigned i = 0; i < device->memory.type_count; i++) {
       VkMemoryPropertyFlags props = device->memory.types[i].propertyFlags;
@@ -2933,26 +2960,6 @@ void anv_GetPhysicalDeviceMemoryProperties(
          .size    = physical_device->memory.heaps[i].size,
          .flags   = physical_device->memory.heaps[i].flags,
       };
-   }
-
-   /* Some games (e.g. Total War: WARHAMMER III) sometimes completely refuse
-    * to use memory types with VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT set.
-    * On iGPUs we have only device-local memory, so we must hide it
-    * from the flags.
-    *
-    * Additionally, TW also seems to crash if a non-local but also
-    * non host-visible memory is present, so we should be careful which
-    * memory types we hide this flag from.
-    */
-   if (physical_device->instance->anv_fake_nonlocal_memory &&
-       !anv_physical_device_has_vram(physical_device)) {
-      for (uint32_t i = 0; i < physical_device->memory.type_count; i++) {
-         if (pMemoryProperties->memoryTypes[i].propertyFlags &
-             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
-            pMemoryProperties->memoryTypes[i].propertyFlags &=
-                  ~VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
-         }
-      }
    }
 }
 