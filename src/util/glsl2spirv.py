joshuaswarren/mesa-1

commit 988878a995cfcbca85d0af8b27d1e155c596d087

meson: use glslang --depfile argument when possible
This reduces the amount of manual dependency tracking developers need to
do. This is turned on if glslang >= 11.3.0 is used, or 11.9.0 on
Windows, but otherwise the status quo is maintained. This means I have
not removed any use of `depend_files`. We could make make these hard
requirements and remove the use of `depend_files` too.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28329>

src/util/glsl2spirv.py
@@ -38,6 +38,9 @@ if T.TYPE_CHECKING:
         extra: T.Optional[str]
         vn: str
         stage: str
+        includes: T.List[str]
+        defines: T.List[str]
+        depfile: T.Optional[str]
 
 
 def get_args() -> 'Arguments':
@@ -84,6 +87,12 @@ def get_args() -> 'Arguments':
                         default=[],
                         action='append',
                         help="Defines")
+    
+    parser.add_argument('--depfile',
+                        dest="depfile",
+                        default=None,
+                        action='store',
+                        help='Where glslangValidator should write its depfile, if unset no depfile will be written.')
 
     args = parser.parse_args()
     return args
@@ -169,6 +178,9 @@ def process_file(args: 'Arguments') -> None:
 
     if args.create_entry is not None:
         cmd_list.extend(["--entry-point", args.create_entry])
+
+    if args.depfile is not None:
+        cmd_list.extend(['--depfile', args.depfile])
 
     for f in args.includes:
         cmd_list.append('-I' + f)

meson.build
@@ -623,8 +623,22 @@ if vdpau_drivers_path == ''
   vdpau_drivers_path = join_paths(get_option('libdir'), 'vdpau')
 endif
 
-prog_glslang = find_program('glslangValidator', native : true, required : with_vulkan_overlay_layer or with_aco_tests or with_amd_vk or with_intel_vk)
+# GLSL has interesting version output and Meson doesn't parse it correctly as of
+# Meson 1.4.0
+prog_glslang = find_program('glslangValidator', native : true,
+                            required : with_vulkan_overlay_layer or with_aco_tests or with_amd_vk or with_intel_vk)
 if prog_glslang.found()
+  # Check if glslang has depfile support. Support was added in 11.3.0, but
+  # Windows path support was broken until 11.9.0.
+  # 
+  # It is intentional to check the build machine, since we need to ensure that
+  # glslang will output valid paths on the build platform
+  _glslang_check = build_machine.system() == 'windows' ? '>= 11.9.0' : '>= 11.3.0'
+  if run_command(prog_glslang, ['--version'], check : false).stdout().split(':')[2].version_compare(_glslang_check)
+    glslang_depfile = ['--depfile', '@DEPFILE@']
+  else
+    glslang_depfile = []
+  endif
   if run_command(prog_glslang, [ '--quiet', '--version' ], check : false).returncode() == 0
     glslang_quiet = ['--quiet']
   else

src/amd/compiler/tests/glsl_scraper.py
@@ -91,6 +91,8 @@ class Shader:
         if self.target_env:
             args += ['--target-env', self.target_env]
         args += ['-o', out_file.name, in_file.name]
+        if depfile is not None:
+            args.extend(['--depfile', depfile])
         with subprocess.Popen(args,
                               stdout = subprocess.PIPE,
                               stderr = subprocess.PIPE,
@@ -300,6 +302,8 @@ def parse_args():
                         default='glslangValidator',
                         dest='glslang',
                         help='Full path to the glslangValidator shader compiler.')
+    p.add_argument('--depfile', metavar='DEPFILE', default=None, action="store",
+                   help="Where to have glslang write a depfile")
     p.add_argument('infile', metavar='INFILE')
 
     return p.parse_args()
@@ -309,6 +313,7 @@ args = parse_args()
 infname = args.infile
 outfname = args.outfile
 glslang = args.glslang
+depfile = args.depfile
 
 with open_file(infname, 'r') as infile:
     parser = Parser(infile)

src/amd/compiler/tests/meson.build
@@ -30,10 +30,16 @@ spirv_files = files(
   'test_d3d11_derivs.cpp',
 )
 
-gen_spirv = generator(prog_python,
-                      output    : '@BASENAME@-spirv.h',
-                      arguments : [join_paths(meson.current_source_dir(), 'glsl_scraper.py'),
-                                   '@INPUT@', '--with-glslang', prog_glslang.full_path(), '-o', '@OUTPUT@'])
+gen_spirv = generator(
+  prog_python,
+  output : '@BASENAME@-spirv.h',
+  arguments : [
+    join_paths(meson.current_source_dir(), 'glsl_scraper.py'),
+    '@INPUT@', '--with-glslang', prog_glslang.full_path(), '-o', '@OUTPUT@',
+    glslang_depfile,
+  ],
+  depfile: '@BASENAME@-spriv.h.d',
+)
 gen_spirv_files = gen_spirv.process(spirv_files)
 
 test(

src/amd/vulkan/bvh/meson.build
@@ -72,19 +72,21 @@ bvh_includes = files(
 bvh_spv = []
 foreach s : bvh_shaders
   command = [
-    prog_glslang, '-V', '-I' + bvh_include_dir, '--target-env', 'spirv1.5', '-x', '-o', '@OUTPUT@', '@INPUT@'
+    prog_glslang, '-V', '-I' + bvh_include_dir, '--target-env', 'spirv1.5',
+    '-x', '-o', '@OUTPUT@', '@INPUT@', glslang_depfile, glslang_quiet,
   ]
-  command += glslang_quiet
 
   foreach define : s[2]
     command += '-D' + define
   endforeach
 
+  _bvh_name = '@0@.spv.h'.format(s[1])
   bvh_spv += custom_target(
-    s[1] + '.spv.h',
+    _bvh_name,
     input : s[0],
-    output : s[1] + '.spv.h',
+    output : _bvh_name,
     command : command,
+    depfile : f'@_bvh_name@.d',
     depend_files: bvh_includes
   )
 endforeach

src/amd/vulkan/radix_sort/shaders/meson.build
@@ -26,10 +26,16 @@ shader_include_files = files(
 
 radix_sort_spv = []
 foreach s : radix_sort_shaders
+  _name = f'@s@.spv.h'
   radix_sort_spv += custom_target(
-    s + '.spv.h',
+    _name,
     input : s,
-    output : s + '.spv.h',
-    command : [prog_glslang, '-V', '-I' + shader_include_dir, '--target-env', 'spirv1.3', '-x', '-o', '@OUTPUT@', '@INPUT@'] + glslang_quiet,
-    depend_files: shader_include_files)
+    output : _name,
+    command : [
+      prog_glslang, '-V', '-I' + shader_include_dir, '--target-env', 'spirv1.3',
+      '-x', '-o', '@OUTPUT@', '@INPUT@', glslang_quiet, glslang_depfile,
+    ],
+    depfile : f'@_name@.d',
+    depend_files : shader_include_files,
+  )
 endforeach

src/intel/shaders/meson.build
@@ -13,7 +13,9 @@ if with_intel_vk
       '--vn', 'float64_spv_source',
       '--glsl-version', '450',
       '-Olib',
-    ]
+      glslang_depfile,
+    ],
+    depfile : 'float64_spv.h.d',
   )
 endif
 

src/vulkan/overlay-layer/meson.build
@@ -24,9 +24,17 @@ overlay_shaders = [
 ]
 overlay_spv = []
 foreach s : ['overlay.frag', 'overlay.vert']
+  _name = f'@s@.spv.h'
   overlay_spv += custom_target(
-    s + '.spv.h', input : s, output : s + '.spv.h',
-    command : [prog_glslang, '-V', '-x', '-o', '@OUTPUT@', '@INPUT@'] + glslang_quiet)
+    _name,
+    input : s,
+    output : _name,
+    command : [
+      prog_glslang, '-V', '-x', '-o', '@OUTPUT@', '@INPUT@', glslang_quiet,
+      glslang_depfile,
+    ],
+    depfile : f'@_name@.d',
+  )
 endforeach
 
 vklayer_files = files(

commit 65a292c243e9d433dd361c14ca878b3e9aabb414

util/glsl2spirv: fixup the generated depfile when copying sources
So that the depfile contains a reference to the original source rather
than the copied one. This is necessary to avoid ninja not finding the
copy and causing spurious rebuilds when the copy has been removed, as
well as correctly tracking changes to the input files.

fixes: 988878a995cfcbca85d0af8b27d1e155c596d087

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30132>

src/util/glsl2spirv.py
@@ -145,7 +145,7 @@ def postprocess_file(args: 'Arguments') -> None:
         w.writelines(lines)
 
 
-def preprocess_file(args: 'Arguments', origin_file: T.TextIO, directory: os.PathLike) -> str:
+def preprocess_file(args: 'Arguments', origin_file: T.TextIO, directory: os.PathLike, filemap: T.Dict[str, str]) -> str:
     with open(os.path.join(directory, os.path.basename(origin_file.name)), "w") as copy_file:
         lines = origin_file.readlines()
 
@@ -157,13 +157,36 @@ def preprocess_file(args: 'Arguments', origin_file: T.TextIO, directory: os.Path
 
         copy_file.writelines(lines)
 
+    filemap[copy_file.name] = origin_file.name
     return copy_file.name
 
 
+def fixup_depfile(depfile: str, filemap: T.Mapping[str, str]) -> None:
+    """Replace generated file references in the depfile with their source.
+    """
+    depends: T.List[str] = []
+    out: T.Optional[str] = None
+    with open(depfile, 'r') as f:
+        for line in f.readlines():
+            if ':' in line:
+                out, line = line.split(':', 1)
+            depends.extend(l.strip() for l in line.split())
+
+    with open(depfile, 'w') as f:
+        f.write(out)
+        f.write(': ')
+        for dep in depends:
+            f.write(filemap.get(dep, dep))
+            f.write(' ')
+        f.write('\n')
+
+
 def process_file(args: 'Arguments') -> None:
+    filemap: T.Dict[str, str] = {}
+
     with open(args.input, "r") as infile:
-        copy_file = preprocess_file(args, infile,
-                                    os.path.dirname(args.output))
+        copy_file = preprocess_file(
+            args, infile, os.path.dirname(args.output), filemap)
 
     cmd_list = [args.glslang]
 
@@ -184,7 +207,6 @@ def process_file(args: 'Arguments') -> None:
 
     for f in args.includes:
         cmd_list.append('-I' + f)
-
     for d in args.defines:
         cmd_list.append('-D' + d)
 
@@ -206,6 +228,9 @@ def process_file(args: 'Arguments') -> None:
 
     if args.create_entry is not None:
         os.remove(copy_file)
+
+    if args.depfile is not None:
+        fixup_depfile(args.depfile, filemap)
 
 
 def main() -> None: