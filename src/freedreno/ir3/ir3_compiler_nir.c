joshuaswarren/mesa-1

commit c1ac1eb3ce4ca6d5d9f0fc416fdf977ca92e9154

ir3: lower vectorized NIR instructions
Use the new repeat group builders to lower vectorized NIR instructions.
Add NIR pass to vectorize NIR before lowering.

Support for repeated instruction is added over a number of different
commits. Here's how they all tie together:

ir3 is a scalar architecture and as such most instructions cannot be
vectorized. However, many instructions support the (rptN) modifier that
allows us to mimic vector instructions. Whenever an instruction has the
(rptN) modifier set it will execute N more times, incrementing its
destination register for each repetition. Additionally, source registers
with the (r) flag set will also be incremented.

For example:

(rpt1)add.f r0.x, (r)r1.x, r2.x

is the same as:

add.f r0.x, r1.x, r2.x
add.f r0.y, r1.y, r2.x

The main benefit of using repeated instructions is a reduction in code
size. Since every iteration is still executed as a scalar instruction,
there's no direct benefit in terms of runtime. The only exception seems
to be for 3-source instructions pre-a7xx: if one of the sources is
constant (i.e., without the (r) flag), a repeated instruction executes
faster than the equivalent expanded sequence. Presumably, this is
because the ALU only has 2 register read ports. I have not been able to
measure this difference on a7xx though.

Support for repeated instructions consists of two parts. First, we need
to make sure NIR is (mostly) vectorized when translating to ir3. I have
not been able to find a way to keep NIR vectorized all the way and still
generate decent code. Therefore, I have taken the approach of
vectorizing the (scalarized) NIR right before translating it to ir3.

Secondly, ir3 needs to be adapted to ingest vectorized NIR and translate
it to repeated instructions. To this end, I have introduced the concept
of "repeat groups" to ir3. A repeat group is a group of instructions
that were produced from a vectorized NIR operation and linked together.

src/freedreno/ir3/ir3_rpt.c
@@ -5,6 +5,52 @@
 
 #include "ir3_nir.h"
 
+bool
+ir3_supports_vectorized_nir_op(nir_op op)
+{
+   switch (op) {
+      /* TODO: emitted as absneg which can often be folded away (e.g., into
+       * (neg)). This seems to often fail when repeated.
+       */
+   case nir_op_b2b1:
+
+      /* dsx/dsy don't seem to support repeat. */
+   case nir_op_fddx:
+   case nir_op_fddx_coarse:
+   case nir_op_fddx_fine:
+   case nir_op_fddy:
+   case nir_op_fddy_coarse:
+   case nir_op_fddy_fine:
+
+      /* dp2acc/dp4acc don't seem to support repeat. */
+   case nir_op_udot_4x8_uadd:
+   case nir_op_udot_4x8_uadd_sat:
+   case nir_op_sudot_4x8_iadd:
+   case nir_op_sudot_4x8_iadd_sat:
+
+      /* Among SFU instructions, only rcp doesn't seem to support repeat. */
+   case nir_op_frcp:
+      return false;
+
+   default:
+      return true;
+   }
+}
+
+uint8_t
+ir3_nir_vectorize_filter(const nir_instr *instr, const void *data)
+{
+   if (instr->type != nir_instr_type_alu)
+      return 0;
+
+   struct nir_alu_instr *alu = nir_instr_as_alu(instr);
+
+   if (!ir3_supports_vectorized_nir_op(alu->op))
+      return 0;
+
+   return 4;
+}
+
 static void
 rpt_list_split(struct list_head *list, struct list_head *at)
 {

src/freedreno/ir3/ir3_context.c
@@ -121,9 +121,17 @@ ir3_context_init(struct ir3_compiler *compiler, struct ir3_shader *shader,
    if ((so->type == MESA_SHADER_FRAGMENT) && compiler->has_fs_tex_prefetch)
       NIR_PASS_V(ctx->s, ir3_nir_lower_tex_prefetch);
 
-   NIR_PASS(progress, ctx->s, nir_convert_to_lcssa, true, true);
+   bool vectorized = false;
+   NIR_PASS(vectorized, ctx->s, nir_opt_vectorize, ir3_nir_vectorize_filter,
+            NULL);
 
-   NIR_PASS(progress, ctx->s, nir_lower_phis_to_scalar, true);
+   if (vectorized) {
+      NIR_PASS_V(ctx->s, nir_opt_undef);
+      NIR_PASS_V(ctx->s, nir_copy_prop);
+      NIR_PASS_V(ctx->s, nir_opt_dce);
+   }
+
+   NIR_PASS(progress, ctx->s, nir_convert_to_lcssa, true, true);
 
    /* This has to go at the absolute end to make sure that all SSA defs are
     * correctly marked.

src/freedreno/ir3/ir3_nir.h
@@ -61,6 +61,9 @@ void ir3_nir_lower_tess_eval(nir_shader *shader, struct ir3_shader_variant *v,
                              unsigned topology);
 void ir3_nir_lower_gs(nir_shader *shader);
 
+bool ir3_supports_vectorized_nir_op(nir_op op);
+uint8_t ir3_nir_vectorize_filter(const nir_instr *instr, const void *data);
+
 /*
  * 64b related lowering:
  */

commit bdbbb6e16d91199e8ca059c8b5ceac9ca555b3ba

ir3: Fix nir_op_vec8/vec16
For simplicity we create a large repeat group and then split it up
before RA.

Fixes: c1ac1eb3ce4 ("ir3: lower vectorized NIR instructions")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30691>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -590,7 +590,6 @@ emit_alu(struct ir3_context *ctx, nir_alu_instr *alu)
 
    dst_sz = alu->def.num_components;
    assert(dst_sz == 1 || ir3_supports_vectorized_nir_op(alu->op));
-   assert(dst_sz <= ARRAY_SIZE(src[0].rpts));
 
    bool use_shared = !alu->def.divergent &&
       ctx->compiler->has_scalar_alu &&
@@ -613,21 +612,20 @@ emit_alu(struct ir3_context *ctx, nir_alu_instr *alu)
    if ((alu->op == nir_op_vec2) || (alu->op == nir_op_vec3) ||
        (alu->op == nir_op_vec4) || (alu->op == nir_op_vec8) ||
        (alu->op == nir_op_vec16)) {
-      struct ir3_instruction_rpt src;
-      assert(info->num_inputs <= ARRAY_SIZE(src.rpts));
-
       for (int i = 0; i < info->num_inputs; i++) {
          nir_alu_src *asrc = &alu->src[i];
-         src.rpts[i] =
+         struct ir3_instruction *src =
             ir3_get_src_shared(ctx, &asrc->src, use_shared)[asrc->swizzle[0]];
-         compile_assert(ctx, src.rpts[i]);
+         compile_assert(ctx, src);
+         def[i] = ir3_MOV(b, src, dst_type);
       }
 
-      dst = ir3_MOV_rpt(b, dst_sz, src, dst_type);
-      cp_instrs(def, dst.rpts, dst_sz);
+      ir3_instr_create_rpt(def, info->num_inputs);
       ir3_put_def(ctx, &alu->def);
       return;
    }
+
+   assert(dst_sz <= ARRAY_SIZE(src[0].rpts));
 
    for (int i = 0; i < info->num_inputs; i++) {
       nir_alu_src *asrc = &alu->src[i];

src/freedreno/ir3/ir3_rpt.c
@@ -122,6 +122,8 @@ static bool
 can_rpt(struct ir3_instruction *instr, struct ir3_instruction *rpt,
         unsigned rpt_n)
 {
+   if (rpt_n >= 4)
+      return false;
    if (rpt->ip != instr->ip + rpt_n)
       return false;
    if (rpt->opc != instr->opc)