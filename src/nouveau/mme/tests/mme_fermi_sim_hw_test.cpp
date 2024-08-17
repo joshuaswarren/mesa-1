joshuaswarren/mesa-1

commit 157dd8ce893d66eb1edeeaec572c6b94ee4d411d

nouveau/mme: Add Fermi builder
Co-Authored-By: Jason Ekstrand <jason.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

src/nouveau/mme/mme_fermi_builder.h
@@ -1,0 +1,115 @@
+#ifndef MME_BUILDER_H
+#error "This file must only be included by mme_builder.h"
+#endif
+
+#include "mme_fermi.h"
+#include "mme_value.h"
+
+#include "util/bitscan.h"
+#include "util/enum_operators.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MME_FERMI_BUILDER_MAX_INSTS 128
+
+enum mme_fermi_instr_parts {
+   MME_FERMI_INSTR_PART_OP     = BITFIELD_BIT(0),
+   MME_FERMI_INSTR_PART_ASSIGN = BITFIELD_BIT(1)
+};
+
+struct mme_fermi_builder {
+   bool first_loaded;
+   uint32_t inst_count;
+   enum mme_fermi_instr_parts inst_parts;
+   struct mme_fermi_inst insts[MME_FERMI_BUILDER_MAX_INSTS];
+   uint32_t cf_depth;
+   struct mme_value loop_counter;
+   struct mme_cf cf_stack[8];
+};
+
+void mme_fermi_builder_init(struct mme_builder *b);
+
+uint32_t * mme_fermi_builder_finish(struct mme_fermi_builder *b, size_t *size_out);
+
+void mme_fermi_add_inst(struct mme_builder *b,
+                        const struct mme_fermi_inst *inst);
+
+static inline bool
+mme_fermi_is_empty(struct mme_fermi_builder *b)
+{
+   return b->inst_count == 0;
+}
+
+#define mme_fermi_asm(b, __inst)                                     \
+   for (struct mme_fermi_inst __inst = { MME_FERMI_INST_DEFAULTS };  \
+        !__inst.end_next;                                            \
+        mme_fermi_add_inst((b), &__inst), __inst.end_next = true)
+
+void mme_fermi_mthd_arr(struct mme_builder *b,
+                        uint16_t mthd,
+                        struct mme_value index);
+
+void mme_fermi_emit(struct mme_builder *b,
+                    struct mme_value data);
+
+void mme_fermi_start_loop(struct mme_builder *b,
+                          struct mme_value count);
+void mme_fermi_end_loop(struct mme_builder *b);
+
+void mme_fermi_start_if(struct mme_builder *b,
+                        enum mme_cmp_op op,
+                        bool if_true,
+                        struct mme_value x,
+                        struct mme_value y);
+void mme_fermi_end_if(struct mme_builder *b);
+
+void mme_fermi_start_while(struct mme_builder *b);
+void mme_fermi_end_while(struct mme_builder *b,
+                         enum mme_cmp_op op,
+                         bool if_true,
+                         struct mme_value x,
+                         struct mme_value y);
+
+void mme_fermi_load_to(struct mme_builder *b,
+                       struct mme_value dst);
+
+struct mme_value mme_fermi_load(struct mme_builder *b);
+
+void
+mme_fermi_alu_to(struct mme_builder *b,
+                 struct mme_value dst,
+                 enum mme_alu_op op,
+                 struct mme_value x,
+                 struct mme_value y);
+
+static inline void
+mme_fermi_alu64_to(struct mme_builder *b,
+                   struct mme_value64 dst,
+                   enum mme_alu_op op_lo,
+                   enum mme_alu_op op_hi,
+                   struct mme_value64 x,
+                   struct mme_value64 y)
+{
+   assert(dst.lo.type == MME_VALUE_TYPE_REG);
+   assert(dst.hi.type == MME_VALUE_TYPE_REG);
+
+   mme_fermi_alu_to(b, dst.lo, op_lo, x.lo, y.lo);
+   mme_fermi_alu_to(b, dst.hi, op_hi, x.hi, y.hi);
+}
+
+void
+mme_fermi_merge_to(struct mme_builder *b, struct mme_value dst,
+                   struct mme_value x, struct mme_value y,
+                   uint16_t dst_pos, uint16_t bits, uint16_t src_pos);
+
+void mme_fermi_state_arr_to(struct mme_builder *b,
+                            struct mme_value dst,
+                            uint16_t state,
+                            struct mme_value index);
+
+#ifdef __cplusplus
+}
+#endif
+

src/nouveau/mme/meson.build
@@ -74,6 +74,7 @@ libnouveau_mme_files = files(
   'mme_builder.h',
   'mme_fermi.c',
   'mme_fermi.h',
+  'mme_fermi_builder.c',
   'mme_fermi_dump.c',
   'mme_tu104.c',
   'mme_tu104.h',

src/nouveau/mme/mme_builder.h
@@ -59,13 +59,18 @@ struct mme_cf {
 struct mme_builder;
 
 #include "mme_tu104_builder.h"
+#include "mme_fermi_builder.h"
 
+#define MME_CLS_FERMI 0x9000
 #define MME_CLS_TURING 0xc500
 
 struct mme_builder {
    uint16_t cls;
    struct mme_reg_alloc reg_alloc;
-   struct mme_tu104_builder tu104;
+   union {
+      struct mme_tu104_builder tu104;
+      struct mme_fermi_builder fermi;
+   };
 };
 
 static inline void
@@ -76,6 +81,8 @@ mme_builder_init(struct mme_builder *b, struct nv_device_info *dev)
 
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_builder_init(b);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_builder_init(b);
    else
       unreachable("Unsupported GPU class");
 }
@@ -85,6 +92,8 @@ mme_builder_finish(struct mme_builder *b, size_t *size_out)
 {
    if (b->cls >= MME_CLS_TURING)
       return mme_tu104_builder_finish(&b->tu104, size_out);
+   else if (b->cls >= MME_CLS_FERMI)
+      return mme_fermi_builder_finish(&b->fermi, size_out);
    else
       unreachable("Unsupported GPU class");
 }
@@ -110,6 +119,8 @@ mme_alu_to(struct mme_builder *b,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_alu_to(b, dst, op, x, y);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_alu_to(b, dst, op, x, y);
    else
       unreachable("Unsupported GPU class");
 }
@@ -144,6 +155,8 @@ mme_alu64_to(struct mme_builder *b,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_alu64_to(b, dst, op_lo, op_hi, x, y);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_alu64_to(b, dst, op_lo, op_hi, x, y);
    else
       unreachable("Unsupported GPU class");
 }
@@ -318,6 +331,8 @@ mme_merge_to(struct mme_builder *b, struct mme_value dst,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_merge_to(b, dst, x, y, dst_pos, bits, src_pos);
+  else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_merge_to(b, dst, x, y, dst_pos, bits, src_pos);
    else
       unreachable("Unsupported GPU class");
 }
@@ -344,6 +359,8 @@ mme_state_arr_to(struct mme_builder *b, struct mme_value dst,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_state_arr_to(b, dst, state, index);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_state_arr_to(b, dst, state, index);
    else
       unreachable("Unsupported GPU class");
 }
@@ -385,16 +402,29 @@ mme_load_to(struct mme_builder *b, struct mme_value dst)
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_load_to(b, dst);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_load_to(b, dst);
    else
       unreachable("Unsupported GPU class");
 }
 
 static inline struct mme_value
-mme_load(struct mme_builder *b)
+mme_tu104_load(struct mme_builder *b)
 {
    struct mme_value dst = mme_alloc_reg(b);
-   mme_load_to(b, dst);
+   mme_tu104_load_to(b, dst);
    return dst;
+}
+
+static inline struct mme_value
+mme_load(struct mme_builder *b)
+{
+   if (b->cls >= MME_CLS_TURING)
+      return mme_tu104_load(b);
+   else if (b->cls >= MME_CLS_FERMI)
+      return mme_fermi_load(b);
+   else
+      unreachable("Unsupported GPU class");
 }
 
 static inline struct mme_value64
@@ -411,6 +441,8 @@ mme_mthd_arr(struct mme_builder *b, uint16_t mthd,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_mthd(b, mthd, index);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_mthd_arr(b, mthd, index);
    else
       unreachable("Unsupported GPU class");
 }
@@ -427,6 +459,8 @@ mme_emit(struct mme_builder *b,
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_emit(b, data);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_emit(b, data);
    else
       unreachable("Unsupported GPU class");
 }
@@ -457,6 +491,8 @@ mme_start_loop(struct mme_builder *b, struct mme_value count)
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_start_loop(b, count);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_start_loop(b, count);
    else
       unreachable("Unsupported GPU class");
 }
@@ -466,6 +502,8 @@ mme_end_loop(struct mme_builder *b)
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_end_loop(b);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_end_loop(b);
    else
       unreachable("Unsupported GPU class");
 }
@@ -481,6 +519,8 @@ mme_start_if_##op(struct mme_builder *b,                          \
 {                                                                 \
    if (b->cls >= MME_CLS_TURING)                                  \
       mme_tu104_start_if(b, MME_CMP_OP_##OP, if_true, x, y);      \
+   else if (b->cls >= MME_CLS_FERMI)                              \
+      mme_fermi_start_if(b, MME_CMP_OP_##OP, if_true, x, y);      \
    else                                                           \
       unreachable("Unsupported GPU class");                       \
 }
@@ -503,6 +543,8 @@ mme_end_if(struct mme_builder *b)
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_end_if(b);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_end_if(b);
    else
       unreachable("Unsupported GPU class");
 }
@@ -516,6 +558,8 @@ mme_start_while(struct mme_builder *b)
 {
    if (b->cls >= MME_CLS_TURING)
       mme_tu104_start_while(b);
+   else if (b->cls >= MME_CLS_FERMI)
+      mme_fermi_start_while(b);
    else
       unreachable("Unsupported GPU class");
 }
@@ -527,6 +571,8 @@ mme_end_while_##op(struct mme_builder *b,                         \
 {                                                                 \
    if (b->cls >= MME_CLS_TURING)                                  \
       mme_tu104_end_while(b, MME_CMP_OP_##OP, if_true, x, y);     \
+   else if (b->cls >= MME_CLS_FERMI)                              \
+      mme_fermi_end_while(b, MME_CMP_OP_##OP, if_true, x, y);     \
    else                                                           \
       unreachable("Unsupported GPU class");                       \
 }

commit 207ef4b0bf82f2913b18bfe3015e3c595eeb9a06

nouveau/mme: Fix add64 of immediates on Fermi
Fixes: 157dd8ce893d ("nouveau/mme: Add Fermi builder")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30703>

src/nouveau/mme/mme_fermi_builder.c
@@ -698,28 +698,35 @@ mme_to_fermi_alu_op(enum mme_alu_op op)
    }
 }
 
-void
-mme_fermi_alu_to(struct mme_builder *b,
-                 struct mme_value dst,
-                 enum mme_alu_op op,
-                 struct mme_value x,
-                 struct mme_value y)
+static bool
+is_imm18_nonzero(struct mme_value x)
+{
+   return x.type == MME_VALUE_TYPE_IMM && x.imm != 0 && is_int18(x.imm);
+}
+
+static void
+mme_fermi_build_alu(struct mme_builder *b,
+                    struct mme_value dst,
+                    enum mme_alu_op op,
+                    struct mme_value x,
+                    struct mme_value y,
+                    bool need_carry)
 {
    struct mme_fermi_builder *fb = &b->fermi;
 
    switch (op) {
    case MME_ALU_OP_ADD:
-      if (x.type == MME_VALUE_TYPE_IMM && x.imm != 0 && is_int18(x.imm)) {
+      if (is_imm18_nonzero(x) && !need_carry) {
          mme_fermi_add_imm18(fb, dst, y, x.imm);
          return;
       }
-      if (y.type == MME_VALUE_TYPE_IMM && y.imm != 0 && is_int18(y.imm)) {
+      if (is_imm18_nonzero(y) && !need_carry) {
          mme_fermi_add_imm18(fb, dst, x, y.imm);
          return;
       }
       break;
    case MME_ALU_OP_SUB:
-      if (y.type == MME_VALUE_TYPE_IMM && is_int18(-y.imm)) {
+      if (y.type == MME_VALUE_TYPE_IMM && is_int18(-y.imm) && !need_carry) {
          mme_fermi_add_imm18(fb, dst, x, -y.imm);
          return;
       }
@@ -762,6 +769,30 @@ mme_fermi_alu_to(struct mme_builder *b,
    mme_free_reg_if_tmp(b, y, y_reg);
 }
 
+void
+mme_fermi_alu_to(struct mme_builder *b,
+                 struct mme_value dst,
+                 enum mme_alu_op op,
+                 struct mme_value x,
+                 struct mme_value y)
+{
+   mme_fermi_build_alu(b, dst, op, x, y, false);
+}
+
+void
+mme_fermi_alu64_to(struct mme_builder *b,
+                   struct mme_value64 dst,
+                   enum mme_alu_op op_lo,
+                   enum mme_alu_op op_hi,
+                   struct mme_value64 x,
+                   struct mme_value64 y)
+{
+   assert(dst.lo.type == MME_VALUE_TYPE_REG);
+   assert(dst.hi.type == MME_VALUE_TYPE_REG);
+
+   mme_fermi_build_alu(b, dst.lo, op_lo, x.lo, y.lo, true);
+   mme_fermi_build_alu(b, dst.hi, op_hi, x.hi, y.hi, true);
+}
 
 void mme_fermi_state_arr_to(struct mme_builder *b,
                             struct mme_value dst,

src/nouveau/mme/mme_fermi_builder.h
@@ -90,20 +90,13 @@ mme_fermi_alu_to(struct mme_builder *b,
                  struct mme_value x,
                  struct mme_value y);
 
-static inline void
+void
 mme_fermi_alu64_to(struct mme_builder *b,
                    struct mme_value64 dst,
                    enum mme_alu_op op_lo,
                    enum mme_alu_op op_hi,
                    struct mme_value64 x,
-                   struct mme_value64 y)
-{
-   assert(dst.lo.type == MME_VALUE_TYPE_REG);
-   assert(dst.hi.type == MME_VALUE_TYPE_REG);
-
-   mme_fermi_alu_to(b, dst.lo, op_lo, x.lo, y.lo);
-   mme_fermi_alu_to(b, dst.hi, op_hi, x.hi, y.hi);
-}
+                   struct mme_value64 y);
 
 void
 mme_fermi_bfe_to(struct mme_builder *b, struct mme_value dst,

src/nouveau/mme/tests/mme_fermi_sim_hw_test.cpp
@@ -291,6 +291,94 @@ TEST_F(mme_fermi_sim_test, addc)
    test_macro(&b, macro, params);
 }
 
+TEST_F(mme_fermi_sim_test, add_imm_carry)
+{
+   mme_builder b;
+   mme_builder_init(&b, devinfo);
+
+   mme_value max = mme_load(&b);
+
+   mme_value add_res = mme_alloc_reg(&b);
+   mme_value add_imm_res = mme_alloc_reg(&b);
+   mme_value carry = mme_alloc_reg(&b);
+
+   /* Dummy add clears the carry register */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ALU_REG;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(add_res);
+      i.src[0] = MME_FERMI_REG_ZERO;
+      i.src[1] = MME_FERMI_REG_ZERO;
+      i.alu_op = MME_FERMI_ALU_OP_ADD;
+   }
+
+   /* ADD_IMM should not touch carry */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ADD_IMM;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(add_imm_res);
+      i.src[0] = mme_fermi_value_as_reg(max);
+      i.imm = 1;
+   }
+
+   /* Grab the carry */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ALU_REG;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(carry);
+      i.src[0] = MME_FERMI_REG_ZERO;
+      i.src[1] = MME_FERMI_REG_ZERO;
+      i.alu_op = MME_FERMI_ALU_OP_ADDC;
+   }
+
+   /* Store everything after all that ALU so none of the stores mess up the
+    * carry behind our back.
+    */
+   mme_store_imm_addr(&b, data_addr + 0, add_res, false);
+   mme_store_imm_addr(&b, data_addr + 4, add_imm_res, false);
+   mme_store_imm_addr(&b, data_addr + 8, carry, false);
+
+   /* Set carry to 1 */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ALU_REG;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(add_res);
+      i.src[0] = mme_fermi_value_as_reg(max);
+      i.src[1] = mme_fermi_value_as_reg(max);
+      i.alu_op = MME_FERMI_ALU_OP_ADD;
+   }
+
+   /* ADD_IMM should not touch carry */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ADD_IMM;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(add_imm_res);
+      i.src[0] = MME_FERMI_REG_ZERO;
+      i.imm = 1;
+   }
+
+   /* Grab the carry */
+   mme_fermi_asm(&b, i) {
+      i.op = MME_FERMI_OP_ALU_REG;
+      i.assign_op = MME_FERMI_ASSIGN_OP_MOVE;
+      i.dst = mme_fermi_value_as_reg(carry);
+      i.src[0] = MME_FERMI_REG_ZERO;
+      i.src[1] = MME_FERMI_REG_ZERO;
+      i.alu_op = MME_FERMI_ALU_OP_ADDC;
+   }
+
+   mme_store_imm_addr(&b, data_addr + 12, add_res, true);
+   mme_store_imm_addr(&b, data_addr + 16, add_imm_res, true);
+   mme_store_imm_addr(&b, data_addr + 20, carry, true);
+
+   auto macro = mme_builder_finish_vec(&b);
+
+   std::vector<uint32_t> params;
+   params.push_back(UINT32_MAX);
+
+   test_macro(&b, macro, params);
+}
+
 TEST_F(mme_fermi_sim_test, sub)
 {
    mme_builder b;