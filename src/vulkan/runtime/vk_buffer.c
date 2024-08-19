joshuaswarren/mesa-1

commit 030a51fd42def2a2440792a32ca06ec0528e145b

vulkan/runtime: implement vkGetBufferMemoryRequirements2()
Signed-off-by: Mohamed Ahmed <mohamedahmedegypt2001@gmail.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21898>

src/vulkan/runtime/vk_buffer.c
@@ -92,6 +92,33 @@ vk_common_GetBufferMemoryRequirements(VkDevice _device,
    *pMemoryRequirements = reqs.memoryRequirements;
 }
 
+VKAPI_ATTR void VKAPI_CALL
+vk_common_GetBufferMemoryRequirements2(VkDevice _device,
+                                       const VkBufferMemoryRequirementsInfo2 *pInfo,
+                                       VkMemoryRequirements2 *pMemoryRequirements)
+{
+   VK_FROM_HANDLE(vk_device, device, _device);
+   VK_FROM_HANDLE(vk_buffer, buffer, pInfo->buffer);
+
+   VkBufferCreateInfo pCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+      .pNext = NULL,
+      .usage = buffer->usage,
+      .size = buffer->size,
+      .flags = buffer->create_flags,
+      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+      .queueFamilyIndexCount = 0,
+      .pQueueFamilyIndices = NULL,
+   };
+   VkDeviceBufferMemoryRequirements info = {
+      .sType = VK_STRUCTURE_TYPE_DEVICE_BUFFER_MEMORY_REQUIREMENTS,
+      .pNext = NULL,
+      .pCreateInfo = &pCreateInfo,
+   };
+   
+   device->dispatch_table.GetDeviceBufferMemoryRequirements(_device, &info, pMemoryRequirements);
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL
 vk_common_BindBufferMemory(VkDevice _device,
                            VkBuffer buffer,

commit da13ea3fb9caa8d2c80289b606a7c5cbfc49f75b

vulkan/runtime: fix GetBufferMemoryRequirements2 for maintenance4
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 030a51fd42 ("vulkan/runtime: implement vkGetBufferMemoryRequirements2()")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30714>

src/vulkan/runtime/vk_buffer.c
@@ -107,9 +107,14 @@ vk_common_GetBufferMemoryRequirements2(VkDevice _device,
    VK_FROM_HANDLE(vk_device, device, _device);
    VK_FROM_HANDLE(vk_buffer, buffer, pInfo->buffer);
 
+   VkBufferUsageFlags2CreateInfoKHR usage2_info = {
+      .sType = VK_STRUCTURE_TYPE_BUFFER_USAGE_FLAGS_2_CREATE_INFO_KHR,
+      .pNext = NULL,
+      .usage = buffer->usage,
+   };
    VkBufferCreateInfo pCreateInfo = {
       .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
-      .pNext = NULL,
+      .pNext = &usage2_info,
       .usage = buffer->usage,
       .size = buffer->size,
       .flags = buffer->create_flags,