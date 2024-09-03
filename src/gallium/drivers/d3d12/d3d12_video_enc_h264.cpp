joshuaswarren/mesa-1

commit 6f21a41812e0b8cee044f44a1fdba169582e9350

d3d12: Video Encode H264 to use direct DPB from frontend
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30908>

src/gallium/drivers/d3d12/d3d12_video_enc_h264.cpp
@@ -358,25 +358,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_h264(struct d3d12_video
    }
 
    picParams.pH264PicData->pic_parameter_set_id = pH264BitstreamBuilder->get_active_pps().pic_parameter_set_id;
-   picParams.pH264PicData->idr_pic_id = h264Pic->idr_pic_id;
-   picParams.pH264PicData->FrameType = d3d12_video_encoder_convert_frame_type_h264(h264Pic->picture_type);
-   picParams.pH264PicData->PictureOrderCountNumber = h264Pic->pic_order_cnt;
-   picParams.pH264PicData->FrameDecodingOrderNumber = h264Pic->frame_num;
-
-   picParams.pH264PicData->List0ReferenceFramesCount = 0;
-   picParams.pH264PicData->pList0ReferenceFrames = nullptr;
-   picParams.pH264PicData->List1ReferenceFramesCount = 0;
-   picParams.pH264PicData->pList1ReferenceFrames = nullptr;
-
-   if (picParams.pH264PicData->FrameType == D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_P_FRAME) {
-      picParams.pH264PicData->List0ReferenceFramesCount = h264Pic->num_ref_idx_l0_active_minus1 + 1;
-      picParams.pH264PicData->pList0ReferenceFrames = h264Pic->ref_idx_l0_list;
-   } else if (picParams.pH264PicData->FrameType == D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_B_FRAME) {
-      picParams.pH264PicData->List0ReferenceFramesCount = h264Pic->num_ref_idx_l0_active_minus1 + 1;
-      picParams.pH264PicData->pList0ReferenceFrames = h264Pic->ref_idx_l0_list;
-      picParams.pH264PicData->List1ReferenceFramesCount = h264Pic->num_ref_idx_l1_active_minus1 + 1;
-      picParams.pH264PicData->pList1ReferenceFrames = h264Pic->ref_idx_l1_list;
-   }
 
    if ((pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_Flags & D3D12_VIDEO_ENCODER_RATE_CONTROL_FLAG_ENABLE_DELTA_QP) != 0)
    {
@@ -391,33 +372,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_h264(struct d3d12_video
          pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit);
       picParams.pH264PicData->pRateControlQPMap = pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit.data();
       picParams.pH264PicData->QPMapValuesCount = pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit.size();
-   }
-}
-
-D3D12_VIDEO_ENCODER_FRAME_TYPE_H264
-d3d12_video_encoder_convert_frame_type_h264(enum pipe_h2645_enc_picture_type picType)
-{
-   switch (picType) {
-      case PIPE_H2645_ENC_PICTURE_TYPE_P:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_P_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_B:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_B_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_I:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_I_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_IDR:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_H264_IDR_FRAME;
-      } break;
-      default:
-      {
-         unreachable("Unsupported pipe_h2645_enc_picture_type");
-      } break;
    }
 }
 

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -340,6 +340,35 @@ d3d12_video_encoder_update_picparams_tracking(struct d3d12_video_encoder *pD3D12
 }
 
 bool
+d3d12_video_encoder_uses_direct_dpb(enum pipe_video_format codec)
+{
+   switch (codec) {
+#if VIDEO_CODEC_H264ENC
+      case PIPE_VIDEO_FORMAT_MPEG4_AVC:
+      {
+         return true;
+      } break;
+#endif
+#if VIDEO_CODEC_H265ENC
+      case PIPE_VIDEO_FORMAT_HEVC:
+      {
+         return false;
+      } break;
+#endif
+#if VIDEO_CODEC_AV1ENC
+      case PIPE_VIDEO_FORMAT_AV1:
+      {
+        return false;
+      } break;
+#endif
+      default:
+      {
+         unreachable("Unsupported pipe_video_format");
+      } break;
+   }
+}
+
+bool
 d3d12_video_encoder_reconfigure_encoder_objects(struct d3d12_video_encoder *pD3D12Enc,
                                                 struct pipe_video_buffer *  srcTexture,
                                                 struct pipe_picture_desc *  picture)
@@ -388,35 +417,40 @@ d3d12_video_encoder_reconfigure_encoder_objects(struct d3d12_video_encoder *pD3D
          debug_printf("[d3d12_video_encoder] Reconfiguration triggered -> Re-creating Reference Pictures Manager\n");
       }
 
-      D3D12_RESOURCE_FLAGS resourceAllocFlags =
-         D3D12_RESOURCE_FLAG_VIDEO_ENCODE_REFERENCE_ONLY | D3D12_RESOURCE_FLAG_DENY_SHADER_RESOURCE;
-      bool     fArrayOfTextures = ((pD3D12Enc->m_currentEncodeCapabilities.m_SupportFlags &
-                                D3D12_VIDEO_ENCODER_SUPPORT_FLAG_RECONSTRUCTED_FRAMES_REQUIRE_TEXTURE_ARRAYS) == 0);
-      uint32_t texturePoolSize  = d3d12_video_encoder_get_current_max_dpb_capacity(pD3D12Enc) +
-                                 1u;   // adding an extra slot as we also need to count the current frame output recon
-                                       // allocation along max reference frame allocations
-      assert(texturePoolSize < UINT16_MAX);
-      pD3D12Enc->m_upDPBStorageManager.reset();
-      if (fArrayOfTextures) {
-         pD3D12Enc->m_upDPBStorageManager = std::make_unique<d3d12_array_of_textures_dpb_manager>(
-            static_cast<uint16_t>(texturePoolSize),
-            pD3D12Enc->m_pD3D12Screen->dev,
-            pD3D12Enc->m_currentEncodeConfig.m_encodeFormatInfo.Format,
-            pD3D12Enc->m_currentEncodeConfig.m_currentResolution,
-            (D3D12_RESOURCE_FLAG_VIDEO_ENCODE_REFERENCE_ONLY | D3D12_RESOURCE_FLAG_DENY_SHADER_RESOURCE),
-            true,   // setNullSubresourcesOnAllZero - D3D12 Video Encode expects nullptr pSubresources if AoT,
-            pD3D12Enc->m_NodeMask,
-            /*use underlying pool, we can't reuse upper level allocations, need D3D12_RESOURCE_FLAG_VIDEO_ENCODE_REFERENCE_ONLY*/
-            true);
-      } else {
-         pD3D12Enc->m_upDPBStorageManager = std::make_unique<d3d12_texture_array_dpb_manager>(
-            static_cast<uint16_t>(texturePoolSize),
-            pD3D12Enc->m_pD3D12Screen->dev,
-            pD3D12Enc->m_currentEncodeConfig.m_encodeFormatInfo.Format,
-            pD3D12Enc->m_currentEncodeConfig.m_currentResolution,
-            resourceAllocFlags,
-            pD3D12Enc->m_NodeMask);
+      enum pipe_video_format codec = u_reduce_video_profile(pD3D12Enc->base.profile);
+      if (!d3d12_video_encoder_uses_direct_dpb(codec))
+      {
+         D3D12_RESOURCE_FLAGS resourceAllocFlags =
+            D3D12_RESOURCE_FLAG_VIDEO_ENCODE_REFERENCE_ONLY | D3D12_RESOURCE_FLAG_DENY_SHADER_RESOURCE;
+         bool     fArrayOfTextures = ((pD3D12Enc->m_currentEncodeCapabilities.m_SupportFlags &
+                                 D3D12_VIDEO_ENCODER_SUPPORT_FLAG_RECONSTRUCTED_FRAMES_REQUIRE_TEXTURE_ARRAYS) == 0);
+         uint32_t texturePoolSize  = d3d12_video_encoder_get_current_max_dpb_capacity(pD3D12Enc) +
+                                    1u;   // adding an extra slot as we also need to count the current frame output recon
+                                          // allocation along max reference frame allocations
+         assert(texturePoolSize < UINT16_MAX);
+         pD3D12Enc->m_upDPBStorageManager.reset();
+         if (fArrayOfTextures) {
+            pD3D12Enc->m_upDPBStorageManager = std::make_unique<d3d12_array_of_textures_dpb_manager>(
+               static_cast<uint16_t>(texturePoolSize),
+               pD3D12Enc->m_pD3D12Screen->dev,
+               pD3D12Enc->m_currentEncodeConfig.m_encodeFormatInfo.Format,
+               pD3D12Enc->m_currentEncodeConfig.m_currentResolution,
+               resourceAllocFlags,
+               true,   // setNullSubresourcesOnAllZero - D3D12 Video Encode expects nullptr pSubresources if AoT,
+               pD3D12Enc->m_NodeMask,
+               /*use underlying pool, we can't reuse upper level allocations, need D3D12_RESOURCE_FLAG_VIDEO_ENCODE_REFERENCE_ONLY*/
+               true);
+         } else {
+            pD3D12Enc->m_upDPBStorageManager = std::make_unique<d3d12_texture_array_dpb_manager>(
+               static_cast<uint16_t>(texturePoolSize),
+               pD3D12Enc->m_pD3D12Screen->dev,
+               pD3D12Enc->m_currentEncodeConfig.m_encodeFormatInfo.Format,
+               pD3D12Enc->m_currentEncodeConfig.m_currentResolution,
+               resourceAllocFlags,
+               pD3D12Enc->m_NodeMask);
+         }
       }
+
       d3d12_video_encoder_create_reference_picture_manager(pD3D12Enc, picture);
    }
 
@@ -564,19 +598,7 @@ d3d12_video_encoder_create_reference_picture_manager(struct d3d12_video_encoder 
 #if VIDEO_CODEC_H264ENC
       case PIPE_VIDEO_FORMAT_MPEG4_AVC:
       {
-         bool gopHasPFrames =
-            (pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_H264GroupOfPictures.PPicturePeriod > 0) &&
-            ((pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_H264GroupOfPictures.GOPLength == 0) ||
-             (pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_H264GroupOfPictures.PPicturePeriod <
-              pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_H264GroupOfPictures.GOPLength));
-
-         pD3D12Enc->m_upDPBManager = std::make_unique<d3d12_video_encoder_references_manager_h264>(
-            gopHasPFrames,
-            *pD3D12Enc->m_upDPBStorageManager,
-            // Max number of frames to be used as a reference, without counting the current recon picture
-            d3d12_video_encoder_get_current_max_dpb_capacity(pD3D12Enc)
-         );
-
+         pD3D12Enc->m_upDPBManager = std::make_unique<d3d12_video_encoder_references_manager_h264>();
          struct pipe_h264_enc_picture_desc *pH264Pic = (struct pipe_h264_enc_picture_desc *) picture;
          pD3D12Enc->m_upBitstreamBuilder = std::make_unique<d3d12_video_bitstream_builder_h264>(pH264Pic->insert_aud_nalu);
       } break;

src/gallium/drivers/d3d12/d3d12_video_enc.h
@@ -539,6 +539,9 @@ d3d12_video_encoder_update_picparams_region_of_interest_qpmap(struct d3d12_video
                                                               int32_t min_delta_qp,
                                                               int32_t max_delta_qp,
                                                               std::vector<T>& pQPMap);
+bool
+d3d12_video_encoder_uses_direct_dpb(enum pipe_video_format codec);
+
 ///
 /// d3d12_video_encoder functions ends
 ///

src/gallium/drivers/d3d12/d3d12_video_enc_h264.h
@@ -54,8 +54,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_h264(struct d3d12_video
                                                               struct pipe_picture_desc *  picture,
                                                               D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA &picParams,
                                                               bool &bUsedAsReference);
-D3D12_VIDEO_ENCODER_FRAME_TYPE_H264
-d3d12_video_encoder_convert_frame_type_h264(enum pipe_h2645_enc_picture_type picType);
 uint32_t
 d3d12_video_encoder_build_codec_headers_h264(struct d3d12_video_encoder *pD3D12Enc,
                                              std::vector<uint64_t> &pWrittenCodecUnitsSizes);

src/gallium/drivers/d3d12/d3d12_video_encoder_references_manager_h264.h
@@ -21,53 +21,59 @@
  * IN THE SOFTWARE.
  */
 
-#ifndef D3D12_VIDEO_ENCODE_FIFO_REFERENCES_MANAGER_H264_H
-#define D3D12_VIDEO_ENCODE_FIFO_REFERENCES_MANAGER_H264_H
+#ifndef D3D12_VIDEO_ENCODE_REFERENCES_MANAGER_H264_H
+#define D3D12_VIDEO_ENCODE_REFERENCES_MANAGER_H264_H
 
 #include "d3d12_video_types.h"
 #include "d3d12_video_encoder_references_manager.h"
-#include "d3d12_video_dpb_storage_manager.h"
 
 class d3d12_video_encoder_references_manager_h264 : public d3d12_video_encoder_references_manager_interface
 {
  public:
-   void                                      end_frame();
-   void                                      begin_frame(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA curFrameData, bool bUsedAsReference, struct pipe_picture_desc* picture);
-   D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE get_current_frame_recon_pic_output_allocation();
+   void begin_frame(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA curFrameData,
+                    bool bUsedAsReference,
+                    struct pipe_picture_desc *picture);
    bool get_current_frame_picture_control_data(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA &codecAllocation);
-   bool is_current_frame_used_as_reference();
    D3D12_VIDEO_ENCODE_REFERENCE_FRAMES get_current_reference_frames();
 
-   d3d12_video_encoder_references_manager_h264(bool                                       gopHasInterCodedFrames,
-                                               d3d12_video_dpb_storage_manager_interface &rDpbStorageManager,
-                                               uint32_t                                   MaxDPBCapacity);
-
+   bool is_current_frame_used_as_reference()
+   {
+      return m_isCurrentFrameUsedAsReference;
+   }
+   D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE get_current_frame_recon_pic_output_allocation()
+   {
+      return m_CurrentFrameReferencesData.ReconstructedPicTexture;
+   }
+   void end_frame()
+   { }
+   d3d12_video_encoder_references_manager_h264()
+   { }
    ~d3d12_video_encoder_references_manager_h264()
    { }
 
  private:
    // Class helpers
-   void prepare_current_frame_recon_pic_allocation();
-   void reset_gop_tracking_and_dpb();
    void update_fifo_dpb_push_front_cur_recon_pic();
    void print_dpb();
    void print_l0_l1_lists();
+   void print_mmco_lists();
 
    // Class members
-
-   uint32_t m_MaxDPBCapacity      = 0;
+   struct d3d12_video_dpb
+   {
+      std::vector<ID3D12Resource *> pResources;
+      std::vector<uint32_t> pSubresources;
+   };
 
    struct current_frame_references_data
    {
       std::vector<D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_H264> pReferenceFramesReconPictureDescriptors;
-      D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE                          ReconstructedPicTexture;
+      D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE ReconstructedPicTexture;
+      d3d12_video_dpb ReferenceTextures;
+      std::vector<UINT> pList0ReferenceFrames;
+      std::vector<UINT> pList1ReferenceFrames;
    };
-
-   d3d12_video_dpb_storage_manager_interface &m_rDPBStorageManager;
-
    current_frame_references_data m_CurrentFrameReferencesData;
-
-   bool m_gopHasInterFrames = false;
 
    bool m_isCurrentFrameUsedAsReference = false;
    D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_H264 m_curFrameState = {};

commit 06c3a3490d67a5e9f0303b3826afa56d9354e188

d3d12: Video Encode H264 - Store L0/L1 active nums at time to encode them in the headers
Fixes: 6f21a41812e ("d3d12: Video Encode H264 to use direct DPB from frontend")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30997>

src/gallium/drivers/d3d12/d3d12_video_enc_h264.cpp
@@ -364,7 +364,18 @@ d3d12_video_encoder_update_current_frame_pic_params_info_h264(struct d3d12_video
       picParams.pH264PicData->Flags |= D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_H264_FLAG_REQUEST_NUM_REF_IDX_ACTIVE_OVERRIDE_FLAG_SLICE;
    }
 
+   //
+   // These need to be set here so they're available for SPS/PPS header building (reference manager updates after that, for slice header params)
+   //
    picParams.pH264PicData->pic_parameter_set_id = pH264BitstreamBuilder->get_active_pps().pic_parameter_set_id;
+   picParams.pH264PicData->List0ReferenceFramesCount = 0;
+   picParams.pH264PicData->List1ReferenceFramesCount = 0;
+   if ((h264Pic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_P) ||
+       (h264Pic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B))
+      picParams.pH264PicData->List0ReferenceFramesCount = h264Pic->num_ref_idx_l0_active_minus1 + 1;
+
+   if (h264Pic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B)
+      picParams.pH264PicData->List1ReferenceFramesCount = h264Pic->num_ref_idx_l1_active_minus1 + 1;
 
    if ((pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc[h264Pic->pic_ctrl.temporal_id].m_Flags & D3D12_VIDEO_ENCODER_RATE_CONTROL_FLAG_ENABLE_DELTA_QP) != 0)
    {