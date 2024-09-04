joshuaswarren/mesa-1

commit 23ea23f779ca116f567ea83054a9c62b4df50312

dril: use the super fallback path for software fallback
just in case gbm init fails somehow, swrast should still be able to
return some kind of fbconfigs

Fixes: c1277a0bd25 ("dril: always take the egl init path")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30979>

src/gallium/targets/dril/dril_target.c
@@ -435,8 +435,13 @@ drilCreateNewScreen(int scrn, int fd,
                     const __DRIconfig ***driver_configs, void *data)
 {
    const __DRIconfig **configs = init_dri2_configs(fd);
-   if (!configs)
-      return NULL;
+   if (!configs && fd == -1) {
+      // otherwise set configs to point to our config list
+      configs = calloc(ARRAY_SIZE(drilConfigs) + 1, sizeof(void *));
+      for (int i = 0; i < ARRAY_SIZE(drilConfigs); i++) {
+         configs[i] = mem_dup(&drilConfigs[i], sizeof(drilConfigs[i]));
+      }
+   }
 
    // outpointer it
    *driver_configs = configs;

commit b0cb116cd2fede744cc562a1014139f3449ecda0

dril: also create double-buffered configs in swrast fallback
Fixes: 23ea23f779c ("dril: use the super fallback path for software fallback")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31013>

src/gallium/targets/dril/dril_target.c
@@ -437,9 +437,16 @@ drilCreateNewScreen(int scrn, int fd,
    const __DRIconfig **configs = init_dri2_configs(fd);
    if (!configs && fd == -1) {
       // otherwise set configs to point to our config list
-      configs = calloc(ARRAY_SIZE(drilConfigs) + 1, sizeof(void *));
+      configs = calloc(ARRAY_SIZE(drilConfigs) * 2 + 1, sizeof(void *));
+      int c = 0;
       for (int i = 0; i < ARRAY_SIZE(drilConfigs); i++) {
-         configs[i] = mem_dup(&drilConfigs[i], sizeof(drilConfigs[i]));
+         /* create normal config */
+         configs[c++] = mem_dup(&drilConfigs[i], sizeof(drilConfigs[i]));
+
+         /* create double-buffered config */
+         configs[c] = mem_dup(&drilConfigs[i], sizeof(drilConfigs[i]));
+         struct gl_config *cfg = (void*)configs[c++];
+         cfg->doubleBufferMode = 1;
       }
    }
 