joshuaswarren/mesa-1

commit 8b78e1993ec394b1ffbd2f0622a7f6e90999a0c2

ci/alpine: use llvm variables
Fixes: 42feb4e4567c ("ci: build a host version of mesa for cross builds")

Reviewed-by: Eric Engestrom <eric@igalia.com>
Signed-off-by: David Heidelberg <david@ixit.cz>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30482>

.gitlab-ci/container/alpine/x86_64_build.sh
@@ -18,7 +18,7 @@ DEPS=(
     bash
     bison
     ccache
-    clang16-dev
+    clang${LLVM_VERSION}-dev
     cmake
     clang-dev
     coreutils
@@ -31,8 +31,8 @@ DEPS=(
     glslang
     graphviz
     linux-headers
-    llvm16-static
-    llvm16-dev
+    llvm${LLVM_VERSION}-static
+    llvm${LLVM_VERSION}-dev
     meson
     mold
     musl-dev

commit d953ba38c65cd1e07e2ae8b05d8ed7af5e021426

ci/alpine: Fix shellcheck errors
Signed-off-by: Daniel Stone <daniels@collabora.com>
Fixes: 8b78e1993ec3 ("ci/alpine: use llvm variables")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30978>

.gitlab-ci/container/alpine/x86_64_build.sh
@@ -16,7 +16,7 @@ DEPS=(
     bash
     bison
     ccache
-    clang${LLVM_VERSION}-dev
+    "clang${LLVM_VERSION}-dev"
     cmake
     clang-dev
     coreutils
@@ -29,8 +29,8 @@ DEPS=(
     glslang
     graphviz
     linux-headers
-    llvm${LLVM_VERSION}-static
-    llvm${LLVM_VERSION}-dev
+    "llvm${LLVM_VERSION}-static"
+    "llvm${LLVM_VERSION}-dev"
     meson
     mold
     musl-dev