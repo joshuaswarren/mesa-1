joshuaswarren/mesa-1

commit d555f3fe3db2a5f17c135241439ab5102090aaa5

intel/dev: Update hwconfig => max_threads_per_psd for Xe2
Backport-to: 24.2
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30887>

src/intel/dev/intel_hwconfig.c
@@ -217,9 +217,13 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
       break;
    case INTEL_HWCONFIG_TOTAL_VS_THREADS_POCS:
       break; /* ignore */
-   case INTEL_HWCONFIG_TOTAL_PS_THREADS:
-      DEVINFO_HWCONFIG(max_threads_per_psd, item->val[0] / 2);
+   case INTEL_HWCONFIG_TOTAL_PS_THREADS: {
+      unsigned threads = item->val[0];
+      if (devinfo->verx10 == 125)
+         threads /= 2;
+      DEVINFO_HWCONFIG(max_threads_per_psd, threads);
       break;
+   }
    case INTEL_HWCONFIG_URB_SIZE_PER_SLICE_IN_KB:
       DEVINFO_HWCONFIG(urb.size, item->val[0]);
       break;

commit 75a99cf38236e844ce42b3b0b7796b1800935067

intel/dev: Fix warning for max_threads_per_psd when devinfo->verx10 == 120
Although we don't want to rely on hwconfig for devinfo->verx10 == 120,
due to the dependence on closed source software, we do check to see if
hwconfig reports different values in the DEVINFO_HWCONFIG macro.

Matt was seeing this warning on 8086:a7a0:

> MESA: warning: INTEL_HWCONFIG_TOTAL_PS_THREADS (128) != devinfo->max_threads_per_psd (64)

Reported-by: Matt Turner <mattst88@gmail.com>
Fixes: d555f3fe3db ("intel/dev: Update hwconfig => max_threads_per_psd for Xe2")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31077>

src/intel/dev/intel_hwconfig.c
@@ -219,7 +219,7 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
       break; /* ignore */
    case INTEL_HWCONFIG_TOTAL_PS_THREADS: {
       unsigned threads = item->val[0];
-      if (devinfo->verx10 == 125)
+      if (devinfo->ver == 12)
          threads /= 2;
       DEVINFO_HWCONFIG(max_threads_per_psd, threads);
       break;