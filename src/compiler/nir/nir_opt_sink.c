joshuaswarren/mesa-1

commit c34be3d84297b04cfe524c71313f25be03f74fe0

nir: sink/move inverse_ballot like moves
It's just a copy for the backends that don't lower it.

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29502>

src/compiler/nir/nir_opt_sink.c
@@ -113,6 +113,8 @@ nir_can_move_instr(nir_instr *instr, nir_move_options options)
          return options & nir_move_load_input;
       case nir_intrinsic_load_uniform:
          return options & nir_move_load_uniform;
+      case nir_intrinsic_inverse_ballot:
+         return options & nir_move_copies;
       case nir_intrinsic_load_constant_agx:
       case nir_intrinsic_load_local_pixel_agx:
          return true;

commit 8c6bd024fe6089a2f7905af582048db81e933a1c

nir/opt_sink: do not sink inverse_ballot out of loops
Inverse_ballot result is undefined if the input is not dynamically uniform.
And sinking out of loops might make the input divergent.

Fixes: c34be3d8429 ("nir: sink/move inverse_ballot like moves")

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30906>

src/compiler/nir/nir_opt_sink.c
@@ -227,6 +227,7 @@ can_sink_out_of_loop(nir_intrinsic_instr *intrin)
     */
    return intrin->intrinsic != nir_intrinsic_load_ubo &&
           intrin->intrinsic != nir_intrinsic_load_ubo_vec4 &&
+          intrin->intrinsic != nir_intrinsic_inverse_ballot &&
           intrin->intrinsic != nir_intrinsic_load_ssbo;
 }
 