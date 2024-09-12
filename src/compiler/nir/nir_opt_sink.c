openharmony/third_party_mesa3d

commit e3d585b05d4f4fc514f6c06cddf57d67d34457d5

nir/nir_opt_move,sink: Include load_ubo_vec4 as a load_ubo instr.
We weren't doing much motion in nir-to-tgsi because we considered all our
lowered load-ubos as unmovable.

softpipe shader-db:

total temps in shared programs: 563942 -> 563136 (-0.14%)
temps in affected programs: 9833 -> 9027 (-8.20%)

r300 shader-db:

instructions in affected programs: 22858 -> 23575 (3.14%)
temps in affected programs: 2039 -> 1813 (-11.08%)

(NIR had given r300 -19% instrs for +40% temps, so this feels like a
worthwhile trade back).

Reivewed-by: Jesse Natalie <jenatali@microsoft.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14138>

src/compiler/nir/nir_opt_sink.c
@@ -56,6 +56,7 @@ nir_can_move_instr(nir_instr *instr, nir_move_options options)
       nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
       switch (intrin->intrinsic) {
       case nir_intrinsic_load_ubo:
+      case nir_intrinsic_load_ubo_vec4:
          return options & nir_move_load_ubo;
       case nir_intrinsic_load_ssbo:
          return (options & nir_move_load_ssbo) && nir_intrinsic_can_reorder(intrin);

joshuaswarren/mesa-1

commit b07213293d3ae2b5f8b303970ed07707b4cc2e05

nir/opt_sink: do not sink load_ubo_vec4 out of loops
Same reason as for load_ubo.

Fixes: e3d585b05d4 ("nir/nir_opt_move,sink: Include load_ubo_vec4 as a load_ubo instr.")

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30906>

src/compiler/nir/nir_opt_sink.c
@@ -226,6 +226,7 @@ can_sink_out_of_loop(nir_intrinsic_instr *intrin)
     * by nir_lower_non_uniform_access.
     */
    return intrin->intrinsic != nir_intrinsic_load_ubo &&
+          intrin->intrinsic != nir_intrinsic_load_ubo_vec4 &&
           intrin->intrinsic != nir_intrinsic_load_ssbo;
 }
 