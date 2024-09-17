joshuaswarren/mesa-1

commit daa23f3c77749cef05c7f561ffb09844460a859f

radeonsi/gfx12: remove CP DMA workarounds because CP DMA is never used on gfx12
except for cache prefetches.

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31168>

src/gallium/drivers/radeonsi/si_cp_dma.c
@@ -187,6 +187,7 @@ void si_cp_dma_clear_buffer(struct si_context *sctx, struct radeon_cmdbuf *cs,
    uint64_t va = (sdst ? sdst->gpu_address : 0) + offset;
    bool is_first = true;
 
+   assert(!sctx->screen->info.cp_sdma_ge_use_system_memory_scope);
    assert(size && size % 4 == 0);
 
    if (user_flags & SI_OP_SYNC_GE_BEFORE)
@@ -197,10 +198,6 @@ void si_cp_dma_clear_buffer(struct si_context *sctx, struct radeon_cmdbuf *cs,
 
    if (user_flags & SI_OP_SYNC_PS_BEFORE)
       sctx->flags |= SI_CONTEXT_PS_PARTIAL_FLUSH;
-
-   /* TODO: Range-invalidate GL2 or always use compute shaders */
-   if (sctx->screen->info.cp_sdma_ge_use_system_memory_scope)
-      sctx->flags |= SI_CONTEXT_INV_L2;
 
    /* Mark the buffer range of destination as valid (initialized),
     * so that transfer_map knows it should wait for the GPU when mapping
@@ -303,6 +300,7 @@ void si_cp_dma_copy_buffer(struct si_context *sctx, struct pipe_resource *dst,
    unsigned gds_flags = (dst ? 0 : CP_DMA_DST_IS_GDS) | (src ? 0 : CP_DMA_SRC_IS_GDS);
    bool is_first = true;
 
+   assert(!sctx->screen->info.cp_sdma_ge_use_system_memory_scope);
    assert(size);
 
    if (dst) {
@@ -363,10 +361,6 @@ void si_cp_dma_copy_buffer(struct si_context *sctx, struct pipe_resource *dst,
 
    if ((dst || src) && !(user_flags & SI_OP_SKIP_CACHE_INV_BEFORE))
          sctx->flags |= si_get_flush_flags(sctx, coher, cache_policy);
-
-   /* TODO: Range-flush GL2 for src and range-invalidate GL2 for dst, or always use compute shaders */
-   if (sctx->screen->info.cp_sdma_ge_use_system_memory_scope)
-      sctx->flags |= SI_CONTEXT_INV_L2;
 
    if (sctx->flags)
       si_mark_atom_dirty(sctx, &sctx->atoms.s.cache_flush);

src/gallium/drivers/radeonsi/si_test_dma_perf.c
@@ -215,6 +215,12 @@ void si_test_dma_perf(struct si_screen *sscreen)
                      }
                   } else if (method == METHOD_CP_DMA) {
                      /* CP DMA */
+                     if (sscreen->info.cp_sdma_ge_use_system_memory_scope) {
+                        /* The CP DMA code doesn't implement this case. */
+                        success = false;
+                        continue;
+                     }
+
                      if (is_copy) {
                         si_cp_dma_copy_buffer(sctx, dst, src, dst_offset, src_offset, size,
                                               SI_OP_SYNC_BEFORE_AFTER, SI_COHERENCY_SHADER, L2_LRU);

commit aec7743e2f430dec60641e903bee03caef5c3a11

radeonsi/gfx12: fix shader uploads via CP DMA
It was incorrectly assumed that CP DMA is never used.

Fixes: daa23f3c777 - radeonsi/gfx12: remove CP DMA workarounds because CP DMA is never used on gfx12

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31082>

src/gallium/drivers/radeonsi/si_cp_dma.c
@@ -20,7 +20,7 @@
 
 static bool cp_dma_use_L2(struct si_context *sctx)
 {
-   return sctx->gfx_level >= GFX7;
+   return sctx->gfx_level >= GFX7 && !sctx->screen->info.cp_sdma_ge_use_system_memory_scope;
 }
 
 /* The max number of bytes that can be copied per packet. */
@@ -242,7 +242,6 @@ void si_cp_dma_copy_buffer(struct si_context *sctx, struct pipe_resource *dst,
                            struct pipe_resource *src, uint64_t dst_offset, uint64_t src_offset,
                            unsigned size, unsigned user_flags)
 {
-   assert(!sctx->screen->info.cp_sdma_ge_use_system_memory_scope);
    assert(size);
    assert(dst && src);
 