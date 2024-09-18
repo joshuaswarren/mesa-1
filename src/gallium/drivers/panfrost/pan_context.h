joshuaswarren/mesa-1

commit 1288eb887a3c09542f82dd37153ddef1a927c5a5

panfrost: properly lower DrawID sysval on v9 GPUs
We only use special DrawID register on v10 GPUs so we still need to
lower to sysval on any earlier generation.

Fixes commit 2861a62f7a5f0775a6878ccfb443ab9a7570b4ab

Signed-off-by: Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30933>

src/gallium/drivers/panfrost/pan_nir_lower_sysvals.c
@@ -90,7 +90,7 @@ sysval_for_intrinsic(nir_intrinsic_instr *intr, unsigned *offset)
       *offset = 8;
       return PAN_SYSVAL_VERTEX_INSTANCE_OFFSETS;
 
-#if PAN_ARCH <= 7
+#if PAN_ARCH <= 9
    case nir_intrinsic_load_draw_id:
       return PAN_SYSVAL_DRAWID;
 #endif

commit 13748d462167c4618ebdacd1be7d6d1971fa5054

panfrost: Pass the architecture to panfrost_nir_lower_sysvals()
pan_nir_lower_sysvals.c is not a per-gen file. Pass the architecture
to panfrost_nir_lower_sysvals() to replace the existing #if PAN_ARCH <= 9
section.

Fixes: 1288eb887a3c ("panfrost: properly lower DrawID sysval on v9")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31164>

src/gallium/drivers/panfrost/pan_context.h
@@ -455,7 +455,7 @@ void panfrost_disk_cache_init(struct panfrost_screen *screen);
 
 bool panfrost_nir_remove_fragcolor_stores(nir_shader *s, unsigned nr_cbufs);
 
-bool panfrost_nir_lower_sysvals(nir_shader *s,
+bool panfrost_nir_lower_sysvals(nir_shader *s, unsigned arch,
                                 struct panfrost_sysvals *sysvals);
 
 bool panfrost_nir_lower_res_indices(nir_shader *shader,

src/gallium/drivers/panfrost/pan_nir_lower_sysvals.c
@@ -25,6 +25,7 @@
 #include "pan_context.h"
 
 struct ctx {
+   unsigned arch;
    struct panfrost_sysvals *sysvals;
    struct hash_table_u64 *sysval_to_id;
    unsigned sysval_ubo;
@@ -54,7 +55,7 @@ lookup_sysval(struct hash_table_u64 *sysval_to_id,
 }
 
 static unsigned
-sysval_for_intrinsic(nir_intrinsic_instr *intr, unsigned *offset)
+sysval_for_intrinsic(unsigned arch, nir_intrinsic_instr *intr, unsigned *offset)
 {
    switch (intr->intrinsic) {
    case nir_intrinsic_load_ssbo_address:
@@ -91,10 +92,11 @@ sysval_for_intrinsic(nir_intrinsic_instr *intr, unsigned *offset)
       *offset = 8;
       return PAN_SYSVAL_VERTEX_INSTANCE_OFFSETS;
 
-#if PAN_ARCH <= 9
    case nir_intrinsic_load_draw_id:
+      if (arch >= 10)
+         return ~0;
+
       return PAN_SYSVAL_DRAWID;
-#endif
 
    case nir_intrinsic_load_multisampled_pan:
       return PAN_SYSVAL_MULTISAMPLED;
@@ -142,7 +144,7 @@ lower(nir_builder *b, nir_instr *instr, void *data)
    if (instr->type == nir_instr_type_intrinsic) {
       nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
       old = &intr->def;
-      sysval = sysval_for_intrinsic(intr, &offset);
+      sysval = sysval_for_intrinsic(ctx->arch, intr, &offset);
 
       if (sysval == ~0)
          return false;
@@ -180,7 +182,8 @@ lower(nir_builder *b, nir_instr *instr, void *data)
 }
 
 bool
-panfrost_nir_lower_sysvals(nir_shader *shader, struct panfrost_sysvals *sysvals)
+panfrost_nir_lower_sysvals(nir_shader *shader, unsigned arch,
+                           struct panfrost_sysvals *sysvals)
 {
    bool progress = false;
 
@@ -194,6 +197,7 @@ panfrost_nir_lower_sysvals(nir_shader *shader, struct panfrost_sysvals *sysvals)
    } while (progress);
 
    struct ctx ctx = {
+      .arch = arch,
       .sysvals = sysvals,
       .sysval_to_id = _mesa_hash_table_u64_create(NULL),
    };

src/gallium/drivers/panfrost/pan_shader.c
@@ -164,7 +164,7 @@ panfrost_shader_compile(struct panfrost_screen *screen, const nir_shader *ir,
                  panfrost_device_gpu_id(dev) < 0x700);
    }
 
-   NIR_PASS_V(s, panfrost_nir_lower_sysvals, &out->sysvals);
+   NIR_PASS_V(s, panfrost_nir_lower_sysvals, dev->arch, &out->sysvals);
 
    /* Lower resource indices */
    NIR_PASS_V(s, panfrost_nir_lower_res_indices, &inputs);