joshuaswarren/mesa-1

commit d4e5e7bbb9e0a3be30ab1f27886ceaaaa5359025

egl/wayland/sw: pass damage region through from put_image2 to wl_surface_damage
this propagates damage from drisw to wayland; y inversion matches protocol

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27703>

src/egl/drivers/dri2/platform_wayland.c
@@ -2470,7 +2470,12 @@ dri2_wl_swrast_put_image2(__DRIdrawable *draw, int op, int x, int y, int w,
    char *src, *dst;
 
    assert(copy_width <= stride);
-   wl_surface_damage(dri2_surf->wl_surface_wrapper, 0, 0, INT32_MAX, INT32_MAX);
+   if (wl_proxy_get_version((struct wl_proxy *)dri2_surf->wl_surface_wrapper) <
+       WL_SURFACE_DAMAGE_BUFFER_SINCE_VERSION)
+      wl_surface_damage(dri2_surf->wl_surface_wrapper, 0, 0, INT32_MAX, INT32_MAX);
+   else
+      wl_surface_damage_buffer(dri2_surf->wl_surface_wrapper,
+                               x, dri2_surf->base.Height - y - h, w, h);
 
    dst = dri2_wl_swrast_get_backbuffer_data(dri2_surf);
 

commit 8ca4b486f89c43295643978070025df141bb013c

egl/dri/wl: Move swrast damage region from put to swap
Pass application provided damage region to the compositor instead
of damaging the entire display. This also gives us the possibility
in the future to have put image only copy the parts of the framebuffer
that were modified.

Fixes: d4e5e7bbb9e0a3be30ab1f27886ceaaaa5359025
Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31210>

src/egl/drivers/dri2/platform_wayland.c
@@ -2627,12 +2627,6 @@ dri2_wl_swrast_put_image2(__DRIdrawable *draw, int op, int x, int y, int w,
    char *src, *dst;
 
    assert(copy_width <= stride);
-   if (wl_proxy_get_version((struct wl_proxy *)dri2_surf->wl_surface_wrapper) <
-       WL_SURFACE_DAMAGE_BUFFER_SINCE_VERSION)
-      wl_surface_damage(dri2_surf->wl_surface_wrapper, 0, 0, INT32_MAX, INT32_MAX);
-   else
-      wl_surface_damage_buffer(dri2_surf->wl_surface_wrapper,
-                               x, y, w, h);
 
    dst = dri2_wl_swrast_get_backbuffer_data(dri2_surf);
 
@@ -2719,6 +2713,13 @@ dri2_wl_swrast_swap_buffers_with_damage(_EGLDisplay *disp, _EGLSurface *draw,
          /* 'back' here will be promoted to 'current' */
          dri2_surf->back->wl_buffer, dri2_surf->dx,
          dri2_surf->dy);
+
+   /* If the compositor doesn't support damage_buffer, we deliberately
+    * ignore the damage region and post maximum damage, due to
+    * https://bugs.freedesktop.org/78190 */
+   if (!n_rects || !try_damage_buffer(dri2_surf, rects, n_rects))
+      wl_surface_damage(dri2_surf->wl_surface_wrapper, 0, 0, INT32_MAX,
+                        INT32_MAX);
 
    /* guarantee full copy for partial update */
    int w = n_rects == 1 ? (rects[2] - rects[0]) : 0;