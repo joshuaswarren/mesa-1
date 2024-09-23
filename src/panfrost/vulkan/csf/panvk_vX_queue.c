joshuaswarren/mesa-1

commit 0db09c662942663f00790170fb009564bb52e2cc

panvk: Transition to explicit VA assignment on v10+
Panthor allows us to control the GPU virtual address space,
which, among other things, will allow us to support sparse
bindings.

Let's tweak the device initialization and BO mapping to
takes this explicit VA management mode into account.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: John Anthony <john.anthony@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30969>

src/panfrost/vulkan/panvk_device.h
@@ -23,10 +23,16 @@
 #include "pan_blend.h"
 #include "pan_blitter.h"
 
+#include "util/vma.h"
+
 #define PANVK_MAX_QUEUE_FAMILIES 1
 
 struct panvk_device {
    struct vk_device vk;
+
+   struct {
+      struct util_vma_heap heap;
+   } as;
 
    struct {
       struct pan_kmod_vm *vm;

src/panfrost/vulkan/panvk_device_memory.c
@@ -89,6 +89,16 @@ panvk_AllocateMemory(VkDevice _device,
       },
    };
 
+   if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      op.va.start =
+         util_vma_heap_alloc(&device->as.heap, op.va.size,
+                             op.va.size > 0x200000 ? 0x200000 : 0x1000);
+      if (!op.va.start) {
+         result = vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
+         goto err_put_bo;
+      }
+   }
+
    int ret =
       pan_kmod_vm_bind(device->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    if (ret) {
@@ -164,6 +174,9 @@ panvk_FreeMemory(VkDevice _device, VkDeviceMemory _mem,
    ASSERTED int ret =
       pan_kmod_vm_bind(device->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    assert(!ret);
+
+   if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA))
+      util_vma_heap_free(&device->as.heap, op.va.start, op.va.size);
 
    pan_kmod_bo_put(mem->bo);
    vk_device_memory_destroy(&device->vk, pAllocator, &mem->vk);

src/panfrost/vulkan/panvk_priv_bo.c
@@ -52,6 +52,13 @@ panvk_priv_bo_create(struct panvk_device *dev, size_t size, uint32_t flags,
       },
    };
 
+   if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      op.va.start = util_vma_heap_alloc(
+         &dev->as.heap, op.va.size, op.va.size > 0x200000 ? 0x200000 : 0x1000);
+      if (!op.va.start)
+         goto err_munmap_bo;
+   }
+
    ret = pan_kmod_vm_bind(dev->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    if (ret)
       goto err_munmap_bo;
@@ -102,6 +109,9 @@ panvk_priv_bo_destroy(struct panvk_priv_bo *priv_bo)
    ASSERTED int ret =
       pan_kmod_vm_bind(dev->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    assert(!ret);
+
+   if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA))
+      util_vma_heap_free(&dev->as.heap, op.va.start, op.va.size);
 
    if (priv_bo->addr.host) {
       ret = os_munmap(priv_bo->addr.host, pan_kmod_bo_size(priv_bo->bo));

src/panfrost/vulkan/panvk_vX_device.c
@@ -269,9 +269,13 @@ panvk_per_arch(create_device)(struct panvk_physical_device *physical_device,
       device->kmod.dev, PANVK_VA_RESERVE_BOTTOM);
    uint64_t user_va_end =
       panfrost_clamp_to_usable_va_range(device->kmod.dev, 1ull << 32);
+   uint32_t vm_flags = PAN_ARCH <= 7 ? PAN_KMOD_VM_FLAG_AUTO_VA : 0;
+
+   util_vma_heap_init(&device->as.heap, user_va_start,
+                      user_va_end - user_va_start);
 
    device->kmod.vm =
-      pan_kmod_vm_create(device->kmod.dev, PAN_KMOD_VM_FLAG_AUTO_VA,
+      pan_kmod_vm_create(device->kmod.dev, vm_flags,
                          user_va_start, user_va_end - user_va_start);
 
    if (!device->kmod.vm) {
@@ -365,6 +369,7 @@ err_free_priv_bos:
    panvk_priv_bo_unref(device->tiler_heap);
    panvk_device_cleanup_mempools(device);
    pan_kmod_vm_destroy(device->kmod.vm);
+   util_vma_heap_finish(&device->as.heap);
 
 err_destroy_kdev:
    pan_kmod_dev_destroy(device->kmod.dev);
@@ -398,6 +403,7 @@ panvk_per_arch(destroy_device)(struct panvk_device *device,
    panvk_priv_bo_unref(device->sample_positions);
    panvk_device_cleanup_mempools(device);
    pan_kmod_vm_destroy(device->kmod.vm);
+   util_vma_heap_finish(&device->as.heap);
 
    if (device->debug.decode_ctx)
       pandecode_destroy_context(device->debug.decode_ctx);

commit e7d187fc2635599b16097a80222a3c3dc8d5aaaf

panvk: Protect access to the virtual address heap
Device memory can be allocated from different threads and thus requires
locking when allocating/freeing virtual addresses.

Fixes: 0db09c662942 ("panvk: Transition to explicit VA assignment on v10+")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31282>

src/panfrost/vulkan/csf/panvk_vX_queue.c
@@ -44,6 +44,11 @@ finish_render_desc_ringbuf(struct panvk_queue *queue)
       ASSERTED int ret =
          pan_kmod_vm_bind(dev->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
       assert(!ret);
+
+      simple_mtx_lock(&dev->as.lock);
+      util_vma_heap_free(&dev->as.heap, ringbuf->addr.dev,
+                         RENDER_DESC_RINGBUF_SIZE * 2);
+      simple_mtx_unlock(&dev->as.lock);
    }
 
    if (ringbuf->addr.host) {
@@ -64,6 +69,7 @@ init_render_desc_ringbuf(struct panvk_queue *queue)
    uint32_t flags = panvk_device_adjust_bo_flags(dev, PAN_KMOD_BO_FLAG_NO_MMAP);
    struct panvk_desc_ringbuf *ringbuf = &queue->render_desc_ringbuf;
    const size_t size = RENDER_DESC_RINGBUF_SIZE;
+   uint64_t dev_addr = 0;
    VkResult result;
    int ret;
 
@@ -76,7 +82,7 @@ init_render_desc_ringbuf(struct panvk_queue *queue)
       ringbuf->addr.host = pan_kmod_bo_mmap(
          ringbuf->bo, 0, size, PROT_READ | PROT_WRITE, MAP_SHARED, NULL);
       if (ringbuf->addr.host == MAP_FAILED) {
-         result = vk_errorf(phys_dev, VK_ERROR_OUT_OF_DEVICE_MEMORY,
+         result = vk_errorf(phys_dev, VK_ERROR_OUT_OF_HOST_MEMORY,
                             "Failed to CPU map ringbuf BO");
          goto err_finish_ringbuf;
       }
@@ -85,7 +91,15 @@ init_render_desc_ringbuf(struct panvk_queue *queue)
    /* We choose the alignment to guarantee that we won't ever cross a 4G
     * boundary when accessing the mapping. This way we can encode the wraparound
     * using 32-bit operations. */
-   uint64_t dev_addr = util_vma_heap_alloc(&dev->as.heap, size * 2, size * 2);
+   simple_mtx_lock(&dev->as.lock);
+   dev_addr = util_vma_heap_alloc(&dev->as.heap, size * 2, size * 2);
+   simple_mtx_unlock(&dev->as.lock);
+
+   if (!dev_addr) {
+      result = vk_errorf(phys_dev, VK_ERROR_OUT_OF_DEVICE_MEMORY,
+                         "Failed to allocate virtual address for ringbuf BO");
+      goto err_finish_ringbuf;
+   }
 
    struct pan_kmod_vm_op vm_ops[] = {
       {
@@ -152,6 +166,12 @@ init_render_desc_ringbuf(struct panvk_queue *queue)
    return VK_SUCCESS;
 
 err_finish_ringbuf:
+   if (dev_addr && !ringbuf->addr.dev) {
+      simple_mtx_lock(&dev->as.lock);
+      util_vma_heap_free(&dev->as.heap, dev_addr, size * 2);
+      simple_mtx_unlock(&dev->as.lock);
+   }
+
    finish_render_desc_ringbuf(queue);
    return result;
 }

src/panfrost/vulkan/panvk_device.h
@@ -31,6 +31,7 @@ struct panvk_device {
    struct vk_device vk;
 
    struct {
+      simple_mtx_t lock;
       struct util_vma_heap heap;
    } as;
 

src/panfrost/vulkan/panvk_device_memory.c
@@ -90,9 +90,11 @@ panvk_AllocateMemory(VkDevice _device,
    };
 
    if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&device->as.lock);
       op.va.start =
          util_vma_heap_alloc(&device->as.heap, op.va.size,
                              op.va.size > 0x200000 ? 0x200000 : 0x1000);
+      simple_mtx_unlock(&device->as.lock);
       if (!op.va.start) {
          result = vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
          goto err_put_bo;
@@ -103,7 +105,7 @@ panvk_AllocateMemory(VkDevice _device,
       pan_kmod_vm_bind(device->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    if (ret) {
       result = vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
-      goto err_put_bo;
+      goto err_return_va;
    }
 
    mem->addr.dev = op.va.start;
@@ -136,6 +138,13 @@ panvk_AllocateMemory(VkDevice _device,
    *pMem = panvk_device_memory_to_handle(mem);
 
    return VK_SUCCESS;
+
+err_return_va:
+   if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&device->as.lock);
+      util_vma_heap_free(&device->as.heap, op.va.start, op.va.size);
+      simple_mtx_unlock(&device->as.lock);
+   }
 
 err_put_bo:
    pan_kmod_bo_put(mem->bo);
@@ -175,8 +184,11 @@ panvk_FreeMemory(VkDevice _device, VkDeviceMemory _mem,
       pan_kmod_vm_bind(device->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    assert(!ret);
 
-   if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA))
+   if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&device->as.lock);
       util_vma_heap_free(&device->as.heap, op.va.start, op.va.size);
+      simple_mtx_unlock(&device->as.lock);
+   }
 
    pan_kmod_bo_put(mem->bo);
    vk_device_memory_destroy(&device->vk, pAllocator, &mem->vk);

src/panfrost/vulkan/panvk_priv_bo.c
@@ -53,15 +53,17 @@ panvk_priv_bo_create(struct panvk_device *dev, size_t size, uint32_t flags,
    };
 
    if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&dev->as.lock);
       op.va.start = util_vma_heap_alloc(
          &dev->as.heap, op.va.size, op.va.size > 0x200000 ? 0x200000 : 0x1000);
+      simple_mtx_unlock(&dev->as.lock);
       if (!op.va.start)
          goto err_munmap_bo;
    }
 
    ret = pan_kmod_vm_bind(dev->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    if (ret)
-      goto err_munmap_bo;
+      goto err_return_va;
 
    priv_bo->addr.dev = op.va.start;
 
@@ -74,6 +76,13 @@ panvk_priv_bo_create(struct panvk_device *dev, size_t size, uint32_t flags,
    p_atomic_set(&priv_bo->refcnt, 1);
 
    return priv_bo;
+
+err_return_va:
+   if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&dev->as.lock);
+      util_vma_heap_free(&dev->as.heap, op.va.start, op.va.size);
+      simple_mtx_unlock(&dev->as.lock);
+   }
 
 err_munmap_bo:
    if (priv_bo->addr.host) {
@@ -110,8 +119,11 @@ panvk_priv_bo_destroy(struct panvk_priv_bo *priv_bo)
       pan_kmod_vm_bind(dev->kmod.vm, PAN_KMOD_VM_OP_MODE_IMMEDIATE, &op, 1);
    assert(!ret);
 
-   if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA))
+   if (!(dev->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      simple_mtx_lock(&dev->as.lock);
       util_vma_heap_free(&dev->as.heap, op.va.start, op.va.size);
+      simple_mtx_unlock(&dev->as.lock);
+   }
 
    if (priv_bo->addr.host) {
       ret = os_munmap(priv_bo->addr.host, pan_kmod_bo_size(priv_bo->bo));

src/panfrost/vulkan/panvk_vX_device.c
@@ -282,6 +282,7 @@ panvk_per_arch(create_device)(struct panvk_physical_device *physical_device,
       panfrost_clamp_to_usable_va_range(device->kmod.dev, 1ull << 32);
    uint32_t vm_flags = PAN_ARCH <= 7 ? PAN_KMOD_VM_FLAG_AUTO_VA : 0;
 
+   simple_mtx_init(&device->as.lock, mtx_plain);
    util_vma_heap_init(&device->as.heap, user_va_start,
                       user_va_end - user_va_start);
 
@@ -381,6 +382,7 @@ err_free_priv_bos:
    panvk_device_cleanup_mempools(device);
    pan_kmod_vm_destroy(device->kmod.vm);
    util_vma_heap_finish(&device->as.heap);
+   simple_mtx_destroy(&device->as.lock);
 
 err_destroy_kdev:
    pan_kmod_dev_destroy(device->kmod.dev);
@@ -415,6 +417,7 @@ panvk_per_arch(destroy_device)(struct panvk_device *device,
    panvk_device_cleanup_mempools(device);
    pan_kmod_vm_destroy(device->kmod.vm);
    util_vma_heap_finish(&device->as.heap);
+   simple_mtx_destroy(&device->as.lock);
 
    if (device->debug.decode_ctx)
       pandecode_destroy_context(device->debug.decode_ctx);