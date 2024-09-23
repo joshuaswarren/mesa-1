joshuaswarren/mesa-1

commit c7c9a5ff3ebdefe61895308f4c184ed0e1f8cf91

pan/bi: Respect swizzles for more vector ops
We need to respect the ALU swizzle, this takes a vector. Fixes incorrect
pack_64_2x32 translation hit when wiring up lower_mem_access_bit_sizes for
OpenCL.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22228>

src/panfrost/compiler/bifrost_compile.c
@@ -2239,16 +2239,19 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
 
    case nir_op_pack_64_2x32:
       bi_collect_v2i32_to(b, dst,
-                          bi_extract(b, bi_src_index(&instr->src[0].src), 0),
-                          bi_extract(b, bi_src_index(&instr->src[0].src), 1));
+                          bi_extract(b, bi_src_index(&instr->src[0].src),
+                                     instr->src[0].swizzle[0]),
+                          bi_extract(b, bi_src_index(&instr->src[0].src),
+                                     instr->src[0].swizzle[1]));
       return;
 
    case nir_op_pack_uvec2_to_uint: {
       bi_index src = bi_src_index(&instr->src[0].src);
 
       assert(sz == 32 && src_sz == 32);
-      bi_mkvec_v2i16_to(b, dst, bi_half(bi_extract(b, src, 0), false),
-                        bi_half(bi_extract(b, src, 1), false));
+      bi_mkvec_v2i16_to(
+         b, dst, bi_half(bi_extract(b, src, instr->src[0].swizzle[0]), false),
+         bi_half(bi_extract(b, src, instr->src[0].swizzle[1]), false));
       return;
    }
 
@@ -2256,10 +2259,11 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
       bi_index src = bi_src_index(&instr->src[0].src);
 
       assert(sz == 32 && src_sz == 32);
-      bi_mkvec_v4i8_to(b, dst, bi_byte(bi_extract(b, src, 0), 0),
-                       bi_byte(bi_extract(b, src, 1), 0),
-                       bi_byte(bi_extract(b, src, 2), 0),
-                       bi_byte(bi_extract(b, src, 3), 0));
+      bi_mkvec_v4i8_to(
+         b, dst, bi_byte(bi_extract(b, src, instr->src[0].swizzle[0]), 0),
+         bi_byte(bi_extract(b, src, instr->src[0].swizzle[1]), 0),
+         bi_byte(bi_extract(b, src, instr->src[0].swizzle[2]), 0),
+         bi_byte(bi_extract(b, src, instr->src[0].swizzle[3]), 0));
       return;
    }
 

commit 68b6abc7e13c10bef82974f4320a0b003d73c2ed

pan/va: Fix nir_op_pack_uvec4_to_uint
We don't have a generic v4i8 on Valhall, we have to lower it to two
v2i8. Fortunately, bi_make_vec_to() hides the Bifrost/Valhall
differences, so use that for nir_op_pack_uvec4_to_uint.

Fixes: c7c9a5ff3ebd ("pan/bi: Respect swizzles for more vector ops")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31280>

src/panfrost/compiler/bifrost_compile.c
@@ -2462,11 +2462,15 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
       bi_index src = bi_src_index(&instr->src[0].src);
 
       assert(sz == 32 && src_sz == 32);
-      bi_mkvec_v4i8_to(
-         b, dst, bi_byte(bi_extract(b, src, instr->src[0].swizzle[0]), 0),
-         bi_byte(bi_extract(b, src, instr->src[0].swizzle[1]), 0),
-         bi_byte(bi_extract(b, src, instr->src[0].swizzle[2]), 0),
-         bi_byte(bi_extract(b, src, instr->src[0].swizzle[3]), 0));
+
+      bi_index srcs[4] = {
+         bi_extract(b, src, instr->src[0].swizzle[0]),
+         bi_extract(b, src, instr->src[0].swizzle[1]),
+         bi_extract(b, src, instr->src[0].swizzle[2]),
+         bi_extract(b, src, instr->src[0].swizzle[3]),
+      };
+      unsigned channels[4] = {0};
+      bi_make_vec_to(b, dst, srcs, channels, 4, 8);
       return;
    }
 