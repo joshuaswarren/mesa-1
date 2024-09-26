joshuaswarren/mesa-1

commit 6bd1300f0e545f39ade6faaf380c5e9044e95cca

radv/video/enc: report pps overrides in feedback for h265
radv does change h265 usually so report in feedback info.

Fixes: ae9f38177de2 ("radv/video: add h265 encode support")
Reviewed-by: Lynne <dev@lynne.ee>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31135>

src/amd/vulkan/radv_video_enc.c
@@ -1990,6 +1990,13 @@ radv_GetEncodedVideoSessionParametersKHR(VkDevice device,
          assert(pps);
          char *data_ptr = pData ? (char *)pData + vps_size + sps_size : NULL;
          vk_video_encode_h265_pps(pps, size_limit, &pps_size, data_ptr);
+
+         if (pFeedbackInfo) {
+            struct VkVideoEncodeH265SessionParametersFeedbackInfoKHR *h265_feedback_info =
+               vk_find_struct(pFeedbackInfo->pNext, VIDEO_ENCODE_H265_SESSION_PARAMETERS_FEEDBACK_INFO_KHR);
+            pFeedbackInfo->hasOverrides = VK_TRUE;
+            h265_feedback_info->hasStdPPSOverrides = VK_TRUE;
+         }
       }
       total_size = sps_size + pps_size + vps_size;
       break;

commit d8ba25b862c1f4166f21772ac2fb874603a66ea9

radv/video: handle missing h265 feedback struct.
I'm not sure this should be missing, but handle if if it is.

Reviewed-by: Lynne <dev@lynne.ee>
Fixes: 6bd1300f0e54 ("radv/video/enc: report pps overrides in feedback for h265")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31158>

src/amd/vulkan/radv_video_enc.c
@@ -1998,7 +1998,8 @@ radv_GetEncodedVideoSessionParametersKHR(VkDevice device,
             struct VkVideoEncodeH265SessionParametersFeedbackInfoKHR *h265_feedback_info =
                vk_find_struct(pFeedbackInfo->pNext, VIDEO_ENCODE_H265_SESSION_PARAMETERS_FEEDBACK_INFO_KHR);
             pFeedbackInfo->hasOverrides = VK_TRUE;
-            h265_feedback_info->hasStdPPSOverrides = VK_TRUE;
+            if (h265_feedback_info)
+               h265_feedback_info->hasStdPPSOverrides = VK_TRUE;
          }
       }
       total_size = sps_size + pps_size + vps_size;