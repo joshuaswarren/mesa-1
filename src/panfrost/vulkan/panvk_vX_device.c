joshuaswarren/mesa-1

commit a172deaf62a50c7930ee973dae533902a704772d

panvk: Add a read-write non-cached memory pool for CSF events
Events cross the device boundary and can be read/written by the host.
We could add the necessary cache flush and made those GPU-cached, but
it's way simpler to allocate non-cached memory.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: John Anthony <john.anthony@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30969>

src/panfrost/vulkan/panvk_vX_device.c
@@ -61,13 +61,24 @@ panvk_device_init_mempools(struct panvk_device *dev)
    struct panvk_pool_properties rw_pool_props = {
       .create_flags = 0,
       .slab_size = 16 * 1024,
-      .label = "Device RW memory pool",
+      .label = "Device RW cached memory pool",
       .owns_bos = false,
       .needs_locking = true,
       .prealloc = false,
    };
 
    panvk_pool_init(&dev->mempools.rw, dev, NULL, &rw_pool_props);
+
+   struct panvk_pool_properties rw_nc_pool_props = {
+      .create_flags = PAN_KMOD_BO_FLAG_GPU_UNCACHED,
+      .slab_size = 16 * 1024,
+      .label = "Device RW uncached memory pool",
+      .owns_bos = false,
+      .needs_locking = true,
+      .prealloc = false,
+   };
+
+   panvk_pool_init(&dev->mempools.rw_nc, dev, NULL, &rw_nc_pool_props);
 
    struct panvk_pool_properties exec_pool_props = {
       .create_flags = PAN_KMOD_BO_FLAG_EXECUTABLE,

src/panfrost/vulkan/panvk_device.h
@@ -69,6 +69,7 @@ struct panvk_device {
 
    struct {
       struct panvk_pool rw;
+      struct panvk_pool rw_nc;
       struct panvk_pool exec;
    } mempools;
 

commit 13692c68aa7e51a3db8d368da45fda1efd8190f4

panvk: Cleanup rw_nc pool in panvk_device_cleanup_mempools
panvk_pool_cleanup call was missing.

Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Fixes: a172deaf62a ("panvk: Add a read-write non-cached memory pool for CSF events"
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31382>

src/panfrost/vulkan/panvk_vX_device.c
@@ -97,6 +97,7 @@ static void
 panvk_device_cleanup_mempools(struct panvk_device *dev)
 {
    panvk_pool_cleanup(&dev->mempools.rw);
+   panvk_pool_cleanup(&dev->mempools.rw_nc);
    panvk_pool_cleanup(&dev->mempools.exec);
 }
 