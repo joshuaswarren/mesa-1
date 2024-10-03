joshuaswarren/mesa-1

commit 7171c370363fdb12ad97f75f851a894a3796edbf

u_trace: add support for indirect data
Allows a driver to declare indirect arguments for its tracepoints and
pass an address. u_trace will request a copy of the data which should
be implemented on the command processor.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Co-Authored-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Reviewed-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29944>

src/freedreno/vulkan/tu_perfetto.cc
@@ -474,7 +474,8 @@ tu_perfetto_submit(struct tu_device *dev,
 #define CREATE_EVENT_CALLBACK(event_name, stage_id)                                 \
    void tu_perfetto_start_##event_name(                                             \
       struct tu_device *dev, uint64_t ts_ns, uint16_t tp_idx,                       \
-      const void *flush_data, const struct trace_start_##event_name *payload)       \
+      const void *flush_data, const struct trace_start_##event_name *payload,       \
+      const void *indirect_data)                                                    \
    {                                                                                \
       stage_start(                                                                  \
          dev, ts_ns, stage_id, NULL, payload, sizeof(*payload),                     \
@@ -483,7 +484,8 @@ tu_perfetto_submit(struct tu_device *dev,
                                                                                     \
    void tu_perfetto_end_##event_name(                                               \
       struct tu_device *dev, uint64_t ts_ns, uint16_t tp_idx,                       \
-      const void *flush_data, const struct trace_end_##event_name *payload)         \
+      const void *flush_data, const struct trace_end_##event_name *payload,         \
+      const void *indirect_data)                                                    \
    {                                                                                \
       stage_end(                                                                    \
          dev, ts_ns, stage_id, flush_data, payload,                                 \
@@ -510,7 +512,8 @@ tu_perfetto_start_cmd_buffer_annotation(
    uint64_t ts_ns,
    uint16_t tp_idx,
    const void *flush_data,
-   const struct trace_start_cmd_buffer_annotation *payload)
+   const struct trace_start_cmd_buffer_annotation *payload,
+   const void *indirect_data)
 {
    /* No extra func necessary, the only arg is in the end payload.*/
    stage_start(dev, ts_ns, CMD_BUFFER_ANNOTATION_STAGE_ID, payload->str, payload,
@@ -523,7 +526,8 @@ tu_perfetto_end_cmd_buffer_annotation(
    uint64_t ts_ns,
    uint16_t tp_idx,
    const void *flush_data,
-   const struct trace_end_cmd_buffer_annotation *payload)
+   const struct trace_end_cmd_buffer_annotation *payload,
+   const void *indirect_data)
 {
    /* Pass the payload string as the app_event, which will appear right on the
     * event block, rather than as metadata inside.
@@ -538,7 +542,8 @@ tu_perfetto_start_cmd_buffer_annotation_rp(
    uint64_t ts_ns,
    uint16_t tp_idx,
    const void *flush_data,
-   const struct trace_start_cmd_buffer_annotation_rp *payload)
+   const struct trace_start_cmd_buffer_annotation_rp *payload,
+   const void *indirect_data)
 {
    /* No extra func necessary, the only arg is in the end payload.*/
    stage_start(dev, ts_ns, CMD_BUFFER_ANNOTATION_RENDER_PASS_STAGE_ID,
@@ -551,7 +556,8 @@ tu_perfetto_end_cmd_buffer_annotation_rp(
    uint64_t ts_ns,
    uint16_t tp_idx,
    const void *flush_data,
-   const struct trace_end_cmd_buffer_annotation_rp *payload)
+   const struct trace_end_cmd_buffer_annotation_rp *payload,
+   const void *indirect_data)
 {
    /* Pass the payload string as the app_event, which will appear right on the
     * event block, rather than as metadata inside.

src/gallium/drivers/radeonsi/si_perfetto.cpp
@@ -260,7 +260,8 @@ extern "C" {
 #define CREATE_DUAL_EVENT_CALLBACK(event_name, stage)                                             \
 void si_ds_begin_##event_name(struct si_ds_device *device, uint64_t ts_ns, uint16_t tp_idx,       \
                               const void *flush_data,                                             \
-                              const struct trace_si_begin_##event_name *payload)                  \
+                              const struct trace_si_begin_##event_name *payload,                  \
+                              const void *indirect_data)                                          \
 {                                                                                                 \
    const struct si_ds_flush_data *flush = (const struct si_ds_flush_data *) flush_data;           \
    begin_event(flush->queue, ts_ns, stage);                                                       \
@@ -268,7 +269,8 @@ void si_ds_begin_##event_name(struct si_ds_device *device, uint64_t ts_ns, uint1
                                                                                                   \
 void si_ds_end_##event_name(struct si_ds_device *device, uint64_t ts_ns, uint16_t tp_idx,         \
                             const void *flush_data,                                               \
-                            const struct trace_si_end_##event_name *payload)                      \
+                            const struct trace_si_end_##event_name *payload,                      \
+                            const void *indirect_data)                                            \
 {                                                                                                 \
    const struct si_ds_flush_data *flush =  (const struct si_ds_flush_data *) flush_data;          \
    end_event(flush->queue, ts_ns, stage, flush->submission_id, NULL, payload,                     \

src/intel/ds/intel_driver_ds.cc
@@ -229,7 +229,7 @@ send_descriptors(IntelRenderpassDataSource::TraceContext &ctx,
    sync_timestamp(ctx, device);
 }
 
-typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, const void*);
+typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, const void*, const void *);
 
 static void
 begin_event(struct intel_ds_queue *queue, uint64_t ts_ns,
@@ -258,7 +258,8 @@ end_event(struct intel_ds_queue *queue, uint64_t ts_ns,
           uint32_t submission_id,
           uint16_t tracepoint_idx,
           const char *app_event,
-          const void* payload = nullptr,
+          const void *payload = nullptr,
+          const void *indirect_data = nullptr,
           trace_payload_as_extra_func payload_as_extra = nullptr)
 {
    struct intel_ds_device *device = queue->device;
@@ -279,7 +280,6 @@ end_event(struct intel_ds_queue *queue, uint64_t ts_ns,
 
    if (!start_ns)
       return;
-
 
    IntelRenderpassDataSource::Trace([=](IntelRenderpassDataSource::TraceContext tctx) {
       if (auto state = tctx.GetIncrementalState(); state->was_cleared) {
@@ -317,7 +317,7 @@ end_event(struct intel_ds_queue *queue, uint64_t ts_ns,
       event->set_submission_id(submission_id);
 
       if (payload && payload_as_extra) {
-         payload_as_extra(event, payload);
+         payload_as_extra(event, payload, indirect_data);
       }
    });
 
@@ -384,7 +384,8 @@ extern "C" {
                                uint64_t ts_ns,                          \
                                uint16_t tp_idx,                         \
                                const void *flush_data,                  \
-                               const struct trace_intel_begin_##event_name *payload) \
+                               const struct trace_intel_begin_##event_name *payload, \
+                               const void *indirect_data)               \
    {                                                                    \
       const struct intel_ds_flush_data *flush =                         \
          (const struct intel_ds_flush_data *) flush_data;               \
@@ -396,12 +397,13 @@ extern "C" {
                              uint64_t ts_ns,                            \
                              uint16_t tp_idx,                           \
                              const void *flush_data,                    \
-                             const struct trace_intel_end_##event_name *payload) \
+                             const struct trace_intel_end_##event_name *payload, \
+                             const void *indirect_data)                 \
    {                                                                    \
       const struct intel_ds_flush_data *flush =                         \
          (const struct intel_ds_flush_data *) flush_data;               \
       end_event(flush->queue, ts_ns, stage, flush->submission_id,       \
-                tp_idx, NULL, payload,                                  \
+                tp_idx, NULL, payload, indirect_data,                   \
                 (trace_payload_as_extra_func)                           \
                 &trace_payload_as_extra_intel_end_##event_name);        \
    }                                                                    \
@@ -442,7 +444,8 @@ intel_ds_begin_cmd_buffer_annotation(struct intel_ds_device *device,
                                      uint64_t ts_ns,
                                      uint16_t tp_idx,
                                      const void *flush_data,
-                                     const struct trace_intel_begin_cmd_buffer_annotation *payload)
+                                     const struct trace_intel_begin_cmd_buffer_annotation *payload,
+                                     const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
@@ -454,12 +457,13 @@ intel_ds_end_cmd_buffer_annotation(struct intel_ds_device *device,
                                    uint64_t ts_ns,
                                    uint16_t tp_idx,
                                    const void *flush_data,
-                                   const struct trace_intel_end_cmd_buffer_annotation *payload)
+                                   const struct trace_intel_end_cmd_buffer_annotation *payload,
+                                   const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
    end_event(flush->queue, ts_ns, INTEL_DS_QUEUE_STAGE_CMD_BUFFER,
-             flush->submission_id, tp_idx, payload->str, NULL, NULL);
+             flush->submission_id, tp_idx, payload->str, NULL, NULL, NULL);
 }
 
 void
@@ -467,7 +471,8 @@ intel_ds_begin_queue_annotation(struct intel_ds_device *device,
                                 uint64_t ts_ns,
                                 uint16_t tp_idx,
                                 const void *flush_data,
-                                const struct trace_intel_begin_queue_annotation *payload)
+                                const struct trace_intel_begin_queue_annotation *payload,
+                                const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
@@ -479,12 +484,13 @@ intel_ds_end_queue_annotation(struct intel_ds_device *device,
                               uint64_t ts_ns,
                               uint16_t tp_idx,
                               const void *flush_data,
-                              const struct trace_intel_end_queue_annotation *payload)
+                              const struct trace_intel_end_queue_annotation *payload,
+                              const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
    end_event(flush->queue, ts_ns, INTEL_DS_QUEUE_STAGE_QUEUE,
-             flush->submission_id, tp_idx, payload->str, NULL, NULL);
+             flush->submission_id, tp_idx, payload->str, NULL, NULL, NULL);
 }
 
 void
@@ -492,7 +498,8 @@ intel_ds_begin_stall(struct intel_ds_device *device,
                      uint64_t ts_ns,
                      uint16_t tp_idx,
                      const void *flush_data,
-                     const struct trace_intel_begin_stall *payload)
+                     const struct trace_intel_begin_stall *payload,
+                     const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
@@ -504,12 +511,13 @@ intel_ds_end_stall(struct intel_ds_device *device,
                    uint64_t ts_ns,
                    uint16_t tp_idx,
                    const void *flush_data,
-                   const struct trace_intel_end_stall *payload)
+                   const struct trace_intel_end_stall *payload,
+                   const void *indirect_data)
 {
    const struct intel_ds_flush_data *flush =
       (const struct intel_ds_flush_data *) flush_data;
    end_event(flush->queue, ts_ns, INTEL_DS_QUEUE_STAGE_STALL,
-             flush->submission_id, tp_idx, NULL, payload,
+             flush->submission_id, tp_idx, NULL, payload, indirect_data,
              (trace_payload_as_extra_func)custom_trace_payload_as_extra_end_stall);
 }
 

src/util/perf/u_trace.py
@@ -59,6 +59,8 @@ class Tracepoint(object):
         def needs_storage(a):
             if a.c_format is None:
                 return False
+            if a.is_indirect:
+                return False
             return True
 
         self.name = name
@@ -81,6 +83,14 @@ class Tracepoint(object):
                 self.has_variable_arg = True
                 break
         self.tp_print_custom = tp_print
+
+        # Compute the offset of each indirect argument
+        self.indirect_args = [x for x in args if x.is_indirect]
+        indirect_sizes = []
+        for indirect in self.indirect_args:
+            indirect.indirect_offset = ' + '.join(indirect_sizes) if len(indirect_sizes) > 0 else 0
+            indirect_sizes.append(f"sizeof({indirect.type}")
+
         self.tp_perfetto = tp_perfetto
         self.tp_markers = tp_markers
         self.tp_flags = tp_flags
@@ -105,7 +115,7 @@ class Tracepoint(object):
 class TracepointArgStruct():
     """Represents struct that is being passed as an argument
     """
-    def __init__(self, type, var, c_format=None, fields=[]):
+    def __init__(self, type, var, c_format=None, fields=[], is_indirect=False):
         """Parameters:
 
         - type: argument's C type.
@@ -117,17 +127,25 @@ class TracepointArgStruct():
         self.type = type
         self.var = var
         self.name = var
+        self.is_indirect = is_indirect
+        self.indirect_offset = 0
         self.is_struct = True
         self.c_format = c_format
         self.fields = fields
         self.to_prim_type = None
 
-        self.func_param = f"{self.type} {self.var}"
+        if self.is_indirect:
+            self.func_param = f"struct u_trace_address {self.var}"
+        else:
+            self.func_param = f"{self.type} {self.var}"
 
     def value_expr(self, entry_name):
         ret = None
         if self.is_struct:
-            ret = ", ".join([f"{entry_name}->{self.name}.{f}" for f in self.fields])
+            if self.is_indirect:
+                ret = ", ".join([f"__{self.name}->{f}" for f in self.fields])
+            else:
+                ret = ", ".join([f"{entry_name}->{self.name}.{f}" for f in self.fields])
         else:
             ret = f"{entry_name}->{self.name}"
         return ret
@@ -136,7 +154,7 @@ class TracepointArg(object):
     """Class that represents either an argument being passed or a field in a struct
     """
     def __init__(self, type, var, c_format=None, name=None, to_prim_type=None,
-                 length_arg=None, copy_func=None):
+                 length_arg=None, copy_func=None, is_indirect=False):
         """Parameters:
 
         - type: argument's C type.
@@ -162,8 +180,12 @@ class TracepointArg(object):
         self.copy_func = copy_func
 
         self.is_struct = False
+        self.is_indirect = is_indirect
+        self.indirect_offset = 0
 
-        if self.type == "str":
+        if self.is_indirect:
+            pass
+        elif self.type == "str":
             if self.length_arg and self.length_arg.isdigit():
                 self.struct_member = f"char {self.name}[{length_arg} + 1]"
             else:
@@ -171,13 +193,18 @@ class TracepointArg(object):
         else:
             self.struct_member = f"{self.type} {self.name}"
 
-        if self.type == "str":
+        if self.is_indirect:
+            self.func_param = f"struct u_trace_address {self.var}"
+        elif self.type == "str":
             self.func_param = f"const char *{self.var}"
         else:
             self.func_param = f"{self.type} {self.var}"
 
     def value_expr(self, entry_name):
-        ret = f"{entry_name}->{self.name}"
+        if self.is_indirect:
+            ret = f"*__{self.name}"
+        else:
+            ret = f"{entry_name}->{self.name}"
         if not self.is_struct and self.to_prim_type:
             ret = self.to_prim_type.format(ret)
         return ret
@@ -298,7 +325,8 @@ void ${trace.tp_perfetto}(
    uint64_t ts_ns,
    uint16_t tp_idx,
    const void *flush_data,
-   const struct trace_${trace_name} *payload);
+   const struct trace_${trace_name} *payload,
+   const void *indirect_data);
 #endif
 %    endif
 void __trace_${trace_name}(
@@ -416,11 +444,14 @@ ${trace_toggle_name}_config_variable(void)
  * ${trace_name}
  */
  % if trace.can_generate_print():
-static void __print_${trace_name}(FILE *out, const void *arg) {
+static void __print_${trace_name}(FILE *out, const void *arg, const void *indirect) {
   % if len(trace.tp_struct) > 0:
    const struct trace_${trace_name} *__entry =
       (const struct trace_${trace_name} *)arg;
   % endif
+  % for arg in trace.indirect_args:
+   const ${arg.type} *__${arg.name} = (const ${arg.type} *) ((char *)indirect + ${arg.indirect_offset});
+  % endfor
   % if trace.tp_print_custom is not None:
    fprintf(out, "${trace.tp_print_custom[0]}\\n"
    % for arg in trace.tp_print_custom[1:]:
@@ -439,11 +470,14 @@ static void __print_${trace_name}(FILE *out, const void *arg) {
    );
 }
 
-static void __print_json_${trace_name}(FILE *out, const void *arg) {
+static void __print_json_${trace_name}(FILE *out, const void *arg, const void *indirect) {
   % if len(trace.tp_struct) > 0:
    const struct trace_${trace_name} *__entry =
       (const struct trace_${trace_name} *)arg;
   % endif
+  % for arg in trace.indirect_args:
+   const ${arg.type} *__${arg.var} = (const ${arg.type} *) ((char *)indirect + ${arg.indirect_offset});
+  % endfor
   % if trace.tp_print_custom is not None:
    fprintf(out, "\\"unstructured\\": \\"${trace.tp_print_custom[0]}\\""
    % for arg in trace.tp_print_custom[1:]:
@@ -475,26 +509,35 @@ __attribute__((format(printf, 3, 4))) void ${trace.tp_markers}(struct u_trace_co
 static void __emit_label_${trace_name}(struct u_trace_context *utctx, void *cs, struct trace_${trace_name} *entry) {
    ${trace.tp_markers}(utctx, cs, "${trace_name}("
    % for idx,arg in enumerate(trace.tp_print):
+   % if not arg.is_indirect:
       "${"," if idx != 0 else ""}${arg.name}=${arg.c_format}"
+   % endif
    % endfor
       ")"
    % for arg in trace.tp_print:
+   % if not arg.is_indirect:
       ,${arg.value_expr('entry')}
+   % endif
    % endfor
    );
 }
 
  % endif
 static const struct u_tracepoint __tp_${trace_name} = {
-    ALIGN_POT(sizeof(struct trace_${trace_name}), 8),   /* keep size 64b aligned */
     "${trace_name}",
+    ALIGN_POT(sizeof(struct trace_${trace_name}), 8),   /* keep size 64b aligned */
+    0
+ % for arg in trace.indirect_args:
+    + sizeof(${arg.type})
+ % endfor
+    ,
     ${0 if len(trace.tp_flags) == 0 else " | ".join(trace.tp_flags)},
     ${index},
     __print_${trace_name},
     __print_json_${trace_name},
  % if trace.tp_perfetto is not None:
 #ifdef HAVE_PERFETTO
-    (void (*)(void *pctx, uint64_t, uint16_t, const void *, const void *))${trace.tp_perfetto},
+    (void (*)(void *pctx, uint64_t, uint16_t, const void *, const void *, const void *))${trace.tp_perfetto},
 #endif
  % endif
 };
@@ -509,9 +552,20 @@ void __trace_${trace_name}(
  % endfor
 ) {
    struct trace_${trace_name} entry;
+ % if len(trace.indirect_args) > 0:
+   struct u_trace_address indirects[] = {
+  % for arg in trace.indirect_args:
+      ${arg.var},
+  % endfor
+   };
+   uint8_t indirect_sizes[] = {
+  % for arg in trace.indirect_args:
+      sizeof(${arg.type}),
+  % endfor
+   };
+ % endif
    UNUSED struct trace_${trace_name} *__entry =
       enabled_traces & U_TRACE_TYPE_REQUIRE_QUEUING ?
- % if trace.has_variable_arg:
       (struct trace_${trace_name} *)u_trace_appendv(ut, ${"cs," if trace.need_cs_param else "NULL,"} &__tp_${trace_name},
                                                     0
   % for arg in trace.tp_struct:
@@ -519,10 +573,13 @@ void __trace_${trace_name}(
                                                     + ${arg.length_arg}
    % endif
   % endfor
+                                                    ,
+  % if len(trace.indirect_args) > 0:
+                                                    ARRAY_SIZE(indirects), indirects, indirect_sizes
+  % else:
+                                                    0, NULL, NULL
+  % endif
                                                     ) :
- % else:
-      (struct trace_${trace_name} *)u_trace_append(ut, ${"cs," if trace.need_cs_param else "NULL,"} &__tp_${trace_name}) :
- % endif
       &entry;
  % for arg in trace.tp_struct:
   % if arg.copy_func is None:
@@ -625,7 +682,8 @@ UNUSED static const char *${basename}_names[] = {
 % for trace_name, trace in TRACEPOINTS.items():
 static void UNUSED
 trace_payload_as_extra_${trace_name}(perfetto::protos::pbzero::GpuRenderStageEvent *event,
-                                     const struct trace_${trace_name} *payload)
+                                     const struct trace_${trace_name} *payload,
+                                     const void *indirect_data)
 {
  % if trace.tp_perfetto is not None and len(trace.tp_print) > 0:
    char buf[128];
@@ -635,6 +693,9 @@ trace_payload_as_extra_${trace_name}(perfetto::protos::pbzero::GpuRenderStageEve
       auto data = event->add_extra_data();
       data->set_name("${arg.name}");
 
+   % if arg.is_indirect:
+      const ${arg.type}* __${arg.var} = (const ${arg.type}*)((uint8_t *)indirect_data + ${arg.indirect_offset});
+   % endif
       sprintf(buf, "${arg.c_format}", ${arg.value_expr("payload")});
 
       data->set_value(buf);

src/freedreno/vulkan/tu_device.cc
@@ -2521,10 +2521,13 @@ tu_CreateDevice(VkPhysicalDevice physicalDevice,
    device->submit_count = 0;
    u_trace_context_init(&device->trace_context, device,
                      sizeof(uint64_t),
+                     0,
                      tu_trace_create_buffer,
                      tu_trace_destroy_buffer,
                      TU_CALLX(device, tu_trace_record_ts),
                      tu_trace_read_ts,
+                     NULL,
+                     NULL,
                      tu_trace_delete_flush_data);
 
    tu_breadcrumbs_init(device);

src/gallium/auxiliary/util/u_trace_gallium.c
@@ -62,16 +62,22 @@ void
 u_trace_pipe_context_init(struct u_trace_context *utctx,
                           struct pipe_context *pctx,
                           uint32_t timestamp_size_B,
+                          uint32_t max_indirect_size_B,
                           u_trace_record_ts record_timestamp,
                           u_trace_read_ts read_timestamp,
+                          u_trace_capture_data capture_data,
+                          u_trace_get_data get_data,
                           u_trace_delete_flush_data delete_flush_data)
 {
    u_trace_context_init(utctx, pctx,
                         timestamp_size_B,
+                        max_indirect_size_B,
                         u_trace_pipe_create_buffer,
                         u_trace_pipe_delete_buffer,
                         record_timestamp,
                         read_timestamp,
+                        capture_data,
+                        get_data,
                         delete_flush_data);
 }
 

commit 8d460729ca668ed19e6bc2d46fba29492034445d

u_trace: Fix trace_payload_as_extra_func desync between drivers
Buffer with indirect args wasn't passed to the function which
adds extra event args. Since function definition depends on the
common code, the definition is moved to a single place.

Fixes: 7171c370363fdb12ad97f75f851a894a3796edbf
("u_trace: add support for indirect data")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31090>

src/freedreno/vulkan/tu_perfetto.cc
@@ -182,8 +182,6 @@ stage_push(struct tu_device *dev)
    return &p->stages[p->stage_depth++];
 }
 
-typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, const void*);
-
 static struct tu_perfetto_stage *
 stage_pop(struct tu_device *dev)
 {
@@ -207,6 +205,7 @@ stage_start(struct tu_device *dev,
             const char *app_event,
             const void *payload = nullptr,
             size_t payload_size = 0,
+            const void *indirect = nullptr,
             trace_payload_as_extra_func payload_as_extra = nullptr)
 {
    struct tu_perfetto_stage *stage = stage_push(dev);
@@ -245,6 +244,7 @@ static void
 stage_end(struct tu_device *dev, uint64_t ts_ns, enum tu_stage_id stage_id,
           const void *flush_data,
           const void* payload = nullptr,
+          const void *indirect = nullptr,
           trace_payload_as_extra_func payload_as_extra = nullptr)
 {
    struct tu_perfetto_stage *stage = stage_pop(dev);
@@ -296,12 +296,12 @@ stage_end(struct tu_device *dev, uint64_t ts_ns, enum tu_stage_id stage_id,
       if (stage->payload) {
          if (stage->start_payload_function)
             ((trace_payload_as_extra_func) stage->start_payload_function)(
-               event, stage->payload);
+               event, stage->payload, nullptr);
          free((void *)stage->payload);
       }
 
       if (payload && payload_as_extra)
-         payload_as_extra(event, payload);
+         payload_as_extra(event, payload, indirect);
    });
 }
 
@@ -480,7 +480,7 @@ tu_perfetto_submit(struct tu_device *dev,
       const void *indirect_data)                                                    \
    {                                                                                \
       stage_start(                                                                  \
-         dev, ts_ns, stage_id, NULL, payload, sizeof(*payload),                     \
+         dev, ts_ns, stage_id, NULL, payload, sizeof(*payload), indirect_data,      \
          (trace_payload_as_extra_func) &trace_payload_as_extra_start_##event_name); \
    }                                                                                \
                                                                                     \
@@ -490,7 +490,7 @@ tu_perfetto_submit(struct tu_device *dev,
       const void *indirect_data)                                                    \
    {                                                                                \
       stage_end(                                                                    \
-         dev, ts_ns, stage_id, flush_data, payload,                                 \
+         dev, ts_ns, stage_id, flush_data, payload, indirect_data,                  \
          (trace_payload_as_extra_func) &trace_payload_as_extra_end_##event_name);   \
    }
 

src/gallium/drivers/radeonsi/si_perfetto.cpp
@@ -154,9 +154,6 @@ static void send_descriptors(SIRenderpassDataSource::TraceContext &ctx,
    sync_timestamp(ctx, device);
 }
 
-typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, 
-                                            const void*);
-
 static void begin_event(struct si_ds_queue *queue, uint64_t ts_ns, enum si_ds_queue_stage stage_id)
 {
    PERFETTO_LOG("begin event called - ts_ns=%" PRIu64, ts_ns);
@@ -237,7 +234,7 @@ static void end_event(struct si_ds_queue *queue, uint64_t ts_ns, enum si_ds_queu
       event->set_submission_id(submission_id);
 
       if (payload && payload_as_extra) {
-         payload_as_extra(event, payload);
+         payload_as_extra(event, payload, nullptr);
       }
    });
 

src/intel/ds/intel_driver_ds.cc
@@ -229,8 +229,6 @@ send_descriptors(IntelRenderpassDataSource::TraceContext &ctx,
    sync_timestamp(ctx, device);
 }
 
-typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, const void*, const void *);
-
 static void
 begin_event(struct intel_ds_queue *queue, uint64_t ts_ns,
             enum intel_ds_queue_stage stage_id)

src/util/perf/u_trace.py
@@ -679,6 +679,8 @@ UNUSED static const char *${basename}_names[] = {
 % endfor
 };
 
+typedef void (*trace_payload_as_extra_func)(perfetto::protos::pbzero::GpuRenderStageEvent *, const void*, const void*);
+
 % for trace_name, trace in TRACEPOINTS.items():
 static void UNUSED
 trace_payload_as_extra_${trace_name}(perfetto::protos::pbzero::GpuRenderStageEvent *event,