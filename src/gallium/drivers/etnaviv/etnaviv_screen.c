joshuaswarren/mesa-1

commit 166349af1efd3e5719c5493684ac35d6811326a8

etnaviv: disable 64bpp render/sampler formats
Vivante hardware handles 64bpp render targets and samplers in a odd way
by splitting the buffer and using a pair of texture samplers or a pair
of MRT outputs to access those resources. This isn't implemented in the
driver right now, so we should not advertise support for those formats.

CC: mesa-stable
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26982>

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -458,6 +458,11 @@ gpu_supports_texture_format(struct etna_screen *screen, uint32_t fmt,
 {
    bool supported = true;
 
+   /* Requires split sampler support, which the driver doesn't support, yet. */
+   if (!util_format_is_compressed(format) &&
+       util_format_get_blocksizebits(format) > 32)
+      return false;
+
    if (fmt == TEXTURE_FORMAT_ETC1)
       supported = VIV_FEATURE(screen, chipFeatures, ETC1_TEXTURE_COMPRESSION);
 
@@ -498,6 +503,10 @@ gpu_supports_render_format(struct etna_screen *screen, enum pipe_format format,
    const uint32_t fmt = translate_pe_format(format);
 
    if (fmt == ETNA_NO_MATCH)
+      return false;
+
+   /* Requires split target support, which the driver doesn't support, yet. */
+   if (util_format_get_blocksizebits(format) > 32)
       return false;
 
    if (sample_count > 1) {

commit 71a3a2cee8bd7d24983fc45185ee714a0173c9e8

etnaviv: Improve split sampler check
Check if the block size is bigger then 64. We want to block
e.g. one of the following formats:
 - R32G32B32A32_FLOAT
 - R32G32B32A32_SINT
 - R32G32B32A32_ZINT

Unbreaks e.g. dEQP-GLES3.functional.fbo.completeness.renderable.texture.color0.rgb16i

Fixes: 166349af1ef ("etnaviv: disable 64bpp render/sampler formats")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27111>

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -484,7 +484,7 @@ gpu_supports_texture_format(struct etna_screen *screen, uint32_t fmt,
 
    /* Requires split sampler support, which the driver doesn't support, yet. */
    if (!util_format_is_compressed(format) &&
-       util_format_get_blocksizebits(format) > 32)
+       util_format_get_blocksizebits(format) > 64)
       return false;
 
    if (fmt == TEXTURE_FORMAT_ETC1)
@@ -530,7 +530,7 @@ gpu_supports_render_format(struct etna_screen *screen, enum pipe_format format,
       return false;
 
    /* Requires split target support, which the driver doesn't support, yet. */
-   if (util_format_get_blocksizebits(format) > 32)
+   if (util_format_get_blocksizebits(format) > 64)
       return false;
 
    if (sample_count > 1) {