joshuaswarren/mesa-1

commit c3eaee3b2b5e8234ff127f907c41360b5e1e0510

ci/lava: handle non-zero exit codes
The LAVA job submitter always exits with code 1, regardless
of the HWCI_TEST_SCRIPT's exit code. This commit fixes the
LAVA job submitter to exit with the actual code returned by
the test.

Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31189>

.gitlab-ci/lava/utils/lava_job.py
@@ -35,6 +35,7 @@ class LAVAJob:
         self._is_finished = False
         self.log: dict[str, Any] = log
         self.status = "not_submitted"
+        self._exit_code = None
         self.__exception: Optional[Exception] = None
 
     def heartbeat(self) -> None:
@@ -49,6 +50,15 @@ class LAVAJob:
     def status(self, new_status: str) -> None:
         self._status = new_status
         self.log["status"] = self._status
+
+    @property
+    def exit_code(self) -> int:
+        return self._exit_code
+
+    @exit_code.setter
+    def exit_code(self, code: int) -> None:
+        self._exit_code = code
+        self.log["exit_code"] = self._exit_code
 
     @property
     def job_id(self) -> int:
@@ -158,9 +168,10 @@ class LAVAJob:
         last_line = None  # Print all lines. lines[:None] == lines[:]
 
         for idx, line in enumerate(lava_lines):
-            if result := re.search(r"hwci: mesa: (pass|fail)", line):
+            if result := re.search(r"hwci: mesa: (pass|fail), exit_code: (\d+)", line):
                 self._is_finished = True
-                self.status = result[1]
+                self.status = result.group(1)
+                self.exit_code = int(result.group(2))
 
                 last_line = idx + 1
                 # We reached the log end here. hwci script has finished.

.gitlab-ci/lava/lava_job_submitter.py
@@ -505,9 +505,10 @@ class LAVAJobSubmitter(PathResolver):
             raise SystemExit(1)
 
         STRUCTURAL_LOG["job_combined_status"] = last_attempt_job.status
+        STRUCTURAL_LOG["job_exit_code"] = last_attempt_job.exit_code
 
         if last_attempt_job.status != "pass":
-            raise SystemExit(1)
+            raise SystemExit(last_attempt_job.exit_code)
 
 
 class StructuredLoggerWrapper:
@@ -520,6 +521,7 @@ class StructuredLoggerWrapper:
         STRUCTURAL_LOG["farm"] = self.__submitter.farm
         STRUCTURAL_LOG["job_combined_fail_reason"] = None
         STRUCTURAL_LOG["job_combined_status"] = "not_submitted"
+        STRUCTURAL_LOG["job_exit_code"] = None
         STRUCTURAL_LOG["dut_attempt_counter"] = 0
 
         # Initialize dut_jobs list to enable appends

commit 28017a5efa646a39270122a590cb736383d714aa

ci/lava: set exit code in exception case
Set exit_code to 1 in case of an exception; otherwise,
the job exits with 0, and GitLab shows the job as successful.

Fixes: c3eaee3b2b5 ("ci/lava: handle non-zero exit codes")
Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31556>

.gitlab-ci/lava/utils/lava_job.py
@@ -183,6 +183,9 @@ class LAVAJob:
         self.cancel()
         self.exception = exception
 
+        # Set the exit code to nonzero value
+        self.exit_code = 1
+
         # Give more accurate status depending on exception
         if isinstance(exception, MesaCIKnownIssueException):
             self.status = "canceled"