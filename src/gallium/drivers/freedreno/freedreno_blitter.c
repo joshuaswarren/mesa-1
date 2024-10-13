joshuaswarren/mesa-1

commit 479d615a1dc31ffbfb93925c2cb5d6240342d259

freedreno: Implement stencil blit fallback
Now that there is a util_blitter_clear_depth_stencil() helper, we can
use that to implement stencil blits in the fallback path.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30064>

src/gallium/drivers/freedreno/freedreno_blitter.c
@@ -104,6 +104,8 @@ fd_blitter_pipe_begin(struct fd_context *ctx, bool render_cond) assert_dt
    util_blitter_save_fragment_sampler_views(
       ctx->blitter, ctx->tex[PIPE_SHADER_FRAGMENT].num_textures,
       ctx->tex[PIPE_SHADER_FRAGMENT].textures);
+   util_blitter_save_fragment_constant_buffer_slot(ctx->blitter,
+                                                   ctx->constbuf[PIPE_SHADER_FRAGMENT].cb);
    if (!render_cond)
       util_blitter_save_render_condition(ctx->blitter, ctx->cond_query,
                                          ctx->cond_cond, ctx->cond_mode);
@@ -117,22 +119,20 @@ fd_blitter_pipe_end(struct fd_context *ctx) assert_dt
 {
 }
 
-bool
-fd_blitter_blit(struct fd_context *ctx, const struct pipe_blit_info *info)
+static void
+fd_blitter_prep(struct fd_context *ctx, const struct pipe_blit_info *info)
+   assert_dt
 {
-   struct pipe_context *pctx = &ctx->base;
    struct pipe_resource *dst = info->dst.resource;
    struct pipe_resource *src = info->src.resource;
    struct pipe_context *pipe = &ctx->base;
-   struct pipe_surface *dst_view, dst_templ;
-   struct pipe_sampler_view src_templ, *src_view;
 
    /* If the blit is updating the whole contents of the resource,
     * invalidate it so we don't trigger any unnecessary tile loads in the 3D
     * path.
     */
    if (util_blit_covers_whole_resource(info))
-      pctx->invalidate_resource(pctx, info->dst.resource);
+      pipe->invalidate_resource(pipe, info->dst.resource);
 
    /* The blit format may not match the resource format in this path, so
     * we need to validate that we can use the src/dst resource with the
@@ -153,6 +153,18 @@ fd_blitter_blit(struct fd_context *ctx, const struct pipe_blit_info *info)
    DBG_BLIT(info, NULL);
 
    fd_blitter_pipe_begin(ctx, info->render_condition_enable);
+}
+
+bool
+fd_blitter_blit(struct fd_context *ctx, const struct pipe_blit_info *info)
+{
+   struct pipe_resource *dst = info->dst.resource;
+   struct pipe_resource *src = info->src.resource;
+   struct pipe_context *pipe = &ctx->base;
+   struct pipe_surface *dst_view, dst_templ;
+   struct pipe_sampler_view src_templ, *src_view;
+
+   fd_blitter_prep(ctx, info);
 
    /* Initialize the surface. */
    default_dst_texture(&dst_templ, dst, info->dst.level, info->dst.box.z);
@@ -311,6 +323,34 @@ fd_blitter_clear_depth_stencil(struct pipe_context *pctx, struct pipe_surface *p
    fd_blitter_pipe_end(ctx);
 }
 
+static void
+fd_blit_stencil_fallback(struct fd_context *ctx, const struct pipe_blit_info *info)
+   assert_dt
+{
+   struct pipe_context *pctx = &ctx->base;
+   struct pipe_surface *dst_view, dst_templ;
+
+   util_blitter_default_dst_texture(&dst_templ, info->dst.resource,
+                                    info->dst.level, info->dst.box.z);
+
+   dst_view = pctx->create_surface(pctx, info->dst.resource, &dst_templ);
+
+   fd_blitter_prep(ctx, info);
+
+   util_blitter_clear_depth_stencil(ctx->blitter, dst_view, PIPE_CLEAR_STENCIL,
+                                    0, 0, info->dst.box.x, info->dst.box.y,
+                                    info->dst.box.width, info->dst.box.height);
+
+   fd_blitter_prep(ctx, info);
+
+   util_blitter_stencil_fallback(
+      ctx->blitter, info->dst.resource, info->dst.level, &info->dst.box,
+      info->src.resource, info->src.level, &info->src.box,
+      info->scissor_enable ? &info->scissor : NULL);
+
+   pipe_surface_release(pctx, &dst_view);
+}
+
 /**
  * Optimal hardware path for blitting pixels.
  * Scaling, format conversion, up- and downsampling (resolve) are allowed.
@@ -328,8 +368,10 @@ fd_blit(struct pipe_context *pctx, const struct pipe_blit_info *blit_info)
       return true;
 
    if (info.mask & PIPE_MASK_S) {
-      DBG("cannot blit stencil, skipping");
+      fd_blit_stencil_fallback(ctx, &info);
       info.mask &= ~PIPE_MASK_S;
+      if (!info.mask)
+         return true;
    }
 
    if (!util_blitter_is_blit_supported(ctx->blitter, &info)) {

commit fbdf4d7fd2382bae98c9810d88d47c99e70b096c

freedreno: Balance out u_blitter cb0 save/restore
Fixes: 479d615a1dc3 ("freedreno: Implement stencil blit fallback")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31569>

src/gallium/drivers/freedreno/freedreno_blitter.c
@@ -99,6 +99,7 @@ fd_blitter_pipe_begin(struct fd_context *ctx, bool render_cond) assert_dt
 static void
 fd_blitter_pipe_end(struct fd_context *ctx) assert_dt
 {
+   util_blitter_restore_constant_buffer_state(ctx->blitter);
 }
 
 static void
@@ -195,9 +196,6 @@ fd_blitter_clear(struct pipe_context *pctx, unsigned buffers,
     */
    fd_blitter_pipe_begin(ctx, false);
 
-   util_blitter_save_fragment_constant_buffer_slot(
-      ctx->blitter, ctx->constbuf[PIPE_SHADER_FRAGMENT].cb);
-
    util_blitter_common_clear_setup(blitter, pfb->width, pfb->height, buffers,
                                    NULL, NULL);
 
@@ -265,7 +263,6 @@ fd_blitter_clear(struct pipe_context *pctx, unsigned buffers,
    /* We expect that this should not have triggered a change in pfb: */
    assert(util_framebuffer_state_equal(pfb, &ctx->framebuffer));
 
-   util_blitter_restore_constant_buffer_state(blitter);
    util_blitter_restore_vertex_states(blitter);
    util_blitter_restore_fragment_states(blitter);
    util_blitter_restore_textures(blitter);