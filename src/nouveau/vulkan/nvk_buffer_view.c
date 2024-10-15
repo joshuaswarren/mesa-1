joshuaswarren/mesa-1

commit 1dbb38637fbf93800be94bad795c0666f235203c

nvk: Implement VK_EXT_shader_image_atomic_int64
The compiler work is done but we still need to advertise formats and
deal with clears.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9647
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26246>

src/nouveau/vulkan/nvk_image.c
@@ -69,7 +69,8 @@ nvk_get_image_plane_format_features(struct nvk_physical_device *pdev,
          features |= VK_FORMAT_FEATURE_2_STORAGE_READ_WITHOUT_FORMAT_BIT;
    }
 
-   if (p_format == PIPE_FORMAT_R32_UINT || p_format == PIPE_FORMAT_R32_SINT)
+   if (p_format == PIPE_FORMAT_R32_UINT || p_format == PIPE_FORMAT_R32_SINT ||
+       p_format == PIPE_FORMAT_R64_UINT || p_format == PIPE_FORMAT_R64_SINT)
       features |= VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
 
    if (features != 0) {

src/nouveau/vulkan/nvk_cmd_clear.c
@@ -298,6 +298,9 @@ nvk_CmdClearColorImage(VkCommandBuffer commandBuffer,
    };
 
    VkFormat vk_format = image->vk.format;
+   if (vk_format == VK_FORMAT_R64_UINT || vk_format == VK_FORMAT_R64_SINT)
+      vk_format = VK_FORMAT_R32G32_UINT;
+
    enum pipe_format p_format = vk_format_to_pipe_format(vk_format);
    assert(p_format != PIPE_FORMAT_NONE);
 

src/nouveau/vulkan/nvk_physical_device.c
@@ -168,6 +168,8 @@ nvk_get_device_extensions(const struct nv_device_info *info,
       .EXT_sample_locations = info->cls_eng3d >= MAXWELL_B,
       .EXT_sampler_filter_minmax = info->cls_eng3d >= MAXWELL_B,
       .EXT_separate_stencil_usage = true,
+      .EXT_shader_image_atomic_int64 = info->cls_eng3d >= MAXWELL_A &&
+                                       nvk_use_nak(info),
       .EXT_shader_demote_to_helper_invocation = true,
       .EXT_shader_subgroup_ballot = true,
       .EXT_shader_subgroup_vote = true,
@@ -444,6 +446,12 @@ nvk_get_device_features(const struct nv_device_info *info,
       .robustBufferAccess2 = true,
       .robustImageAccess2 = true,
       .nullDescriptor = true,
+
+      /* VK_EXT_shader_image_atomic_int64 */
+      .shaderImageInt64Atomics = info->cls_eng3d >= MAXWELL_A &&
+                                 nvk_use_nak(info),
+      .sparseImageInt64Atomics = info->cls_eng3d >= MAXWELL_A &&
+                                 nvk_use_nak(info),
 
       /* VK_EXT_transform_feedback */
       .transformFeedback = true,

src/nouveau/vulkan/nvk_shader.c
@@ -142,6 +142,7 @@ nvk_physical_device_spirv_options(const struct nvk_physical_device *pdev,
          .draw_parameters = true,
          .fragment_barycentric = true,
          .geometry_streams = true,
+         .image_atomic_int64 = true,
          .image_read_without_format = true,
          .image_write_without_format = true,
          .int8 = true,

commit 5ecc266a458d86ca9057f3ef61877265946f4018

nvk: Advertise 64-bit atomics on buffer views
We also add an nvk_format_supports_atomics() helper.  This helper lives
in NVK for now because it's not just about the format and hardware but
also about whether or not we have compiler support in NAK.

Fixes: 1dbb38637fbf ("nvk: Implement VK_EXT_shader_image_atomic_int64")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31633>

src/nouveau/vulkan/nvk_buffer_view.c
@@ -35,7 +35,7 @@ nvk_get_buffer_format_features(struct nvk_physical_device *pdev,
             features |= VK_FORMAT_FEATURE_2_STORAGE_READ_WITHOUT_FORMAT_BIT;
       }
 
-      if (p_format == PIPE_FORMAT_R32_UINT || p_format == PIPE_FORMAT_R32_SINT)
+      if (nvk_format_supports_atomics(&pdev->info, p_format))
          features |= VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_ATOMIC_BIT;
    }
 

src/nouveau/vulkan/nvk_format.c
@@ -17,6 +17,21 @@
 #include "cl9097.h"
 #include "cl90c0.h"
 
+bool
+nvk_format_supports_atomics(const struct nv_device_info *dev,
+                            enum pipe_format p_format)
+{
+   switch (p_format) {
+   case PIPE_FORMAT_R32_UINT:
+   case PIPE_FORMAT_R32_SINT:
+   case PIPE_FORMAT_R64_UINT:
+   case PIPE_FORMAT_R64_SINT:
+      return true;
+   default:
+      return false;
+   }
+}
+
 #define VA_FMT(vk_fmt, widths, swap_rb, type) \
    [VK_FORMAT_##vk_fmt] = \
    { NV9097_SET_VERTEX_ATTRIBUTE_A_COMPONENT_BIT_WIDTHS_##widths, \

src/nouveau/vulkan/nvk_format.h
@@ -7,6 +7,9 @@
 
 #include "nvk_private.h"
 
+#include "util/format/u_formats.h"
+#include "nv_device_info.h"
+
 struct nvk_physical_device;
 
 struct nvk_va_format {
@@ -14,6 +17,10 @@ struct nvk_va_format {
    uint8_t swap_rb:1;
    uint8_t type:7;
 };
+
+bool
+nvk_format_supports_atomics(const struct nv_device_info *dev,
+                            enum pipe_format p_format);
 
 const struct nvk_va_format *
 nvk_get_va_format(const struct nvk_physical_device *pdev, VkFormat format);

src/nouveau/vulkan/nvk_image.c
@@ -81,8 +81,7 @@ nvk_get_image_plane_format_features(struct nvk_physical_device *pdev,
          features |= VK_FORMAT_FEATURE_2_STORAGE_READ_WITHOUT_FORMAT_BIT;
    }
 
-   if (p_format == PIPE_FORMAT_R32_UINT || p_format == PIPE_FORMAT_R32_SINT ||
-       p_format == PIPE_FORMAT_R64_UINT || p_format == PIPE_FORMAT_R64_SINT)
+   if (nvk_format_supports_atomics(&pdev->info, p_format))
       features |= VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
 
    if (p_format == PIPE_FORMAT_R8_UINT && tiling == VK_IMAGE_TILING_OPTIMAL)