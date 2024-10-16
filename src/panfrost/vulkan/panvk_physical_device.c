joshuaswarren/mesa-1

commit 2611e429f0e546217f58d4a5b8b2fb051c8c7c09

panvk: expose KHR_external_memory and KHR_external_memory_capabilities
Seems to be implemented already, let's expose them!

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31487>

src/panfrost/vulkan/panvk_physical_device.c
@@ -69,6 +69,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .KHR_device_group = true,
       .KHR_descriptor_update_template = true,
       .KHR_driver_properties = true,
+      .KHR_external_memory = true,
       .KHR_external_semaphore = true,
       .KHR_get_memory_requirements2 = true,
       .KHR_maintenance1 = true,

src/panfrost/vulkan/panvk_instance.c
@@ -50,6 +50,7 @@ panvk_EnumerateInstanceVersion(uint32_t *pApiVersion)
 
 static const struct vk_instance_extension_table panvk_instance_extensions = {
    .KHR_device_group_creation = true,
+   .KHR_external_memory_capabilities = true,
    .KHR_external_semaphore_capabilities = true,
    .KHR_get_physical_device_properties2 = true,
 #ifdef PANVK_USE_WSI_PLATFORM

commit b33553e6bac6b861a530c2aaa4fe6ea869052bcc

panvk: fill in VkExternalBufferProperties
Opaque fds are internally dma-bufs.  We also support both export and
import.

Fixes: 2611e429f0e ("panvk: expose KHR_external_memory and KHR_external_memory_capabilities")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31649>

src/panfrost/vulkan/panvk_physical_device.c
@@ -1477,5 +1477,27 @@ panvk_GetPhysicalDeviceExternalBufferProperties(
    const VkPhysicalDeviceExternalBufferInfo *pExternalBufferInfo,
    VkExternalBufferProperties *pExternalBufferProperties)
 {
-   panvk_stub();
+   const VkExternalMemoryHandleTypeFlags supported_handle_types =
+      VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT |
+      VK_EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT;
+
+   /* From the Vulkan 1.3.298 spec:
+    *
+    *    compatibleHandleTypes must include at least handleType.
+    */
+   VkExternalMemoryHandleTypeFlags handle_types =
+      pExternalBufferInfo->handleType;
+   VkExternalMemoryFeatureFlags features = 0;
+   if (pExternalBufferInfo->handleType & supported_handle_types) {
+      handle_types |= supported_handle_types;
+      features |= VK_EXTERNAL_MEMORY_FEATURE_EXPORTABLE_BIT |
+                  VK_EXTERNAL_MEMORY_FEATURE_IMPORTABLE_BIT;
+   }
+
+   pExternalBufferProperties->externalMemoryProperties =
+      (VkExternalMemoryProperties){
+         .externalMemoryFeatures = features,
+         .exportFromImportedHandleTypes = handle_types,
+         .compatibleHandleTypes = handle_types,
+      };
 }