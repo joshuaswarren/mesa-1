openharmony/third_party_mesa3d

commit 44d4f6b6ced6080548600be2d282d58f6ef043ca

glx/dri3: Implement GLX_EXT_swap_control_tear
Not wired up for DRI2 because it would require server-side support,
which I'm not especially interested in writing.

Fixes: mesa/mesa#96
Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6671>

src/glx/glx_pbuffer.c
@@ -352,6 +352,10 @@ __glXGetDrawableAttribute(Display * dpy, GLXDrawable drawable,
       } else if (attribute == GLX_MAX_SWAP_INTERVAL_EXT) {
          *value = INT_MAX;
          return 0;
+      } else if (attribute == GLX_LATE_SWAPS_TEAR_EXT) {
+         *value = __glXExtensionBitIsEnabled(pdraw->psc,
+                                             EXT_swap_control_tear_bit);
+         return 0;
       }
    }
 #endif

src/glx/dri3_glx.c
@@ -742,6 +742,7 @@ dri3_bind_extensions(struct dri3_screen *psc, struct glx_display * priv,
    extensions = psc->core->getExtensions(psc->driScreen);
 
    __glXEnableDirectExtension(&psc->base, "GLX_EXT_swap_control");
+   __glXEnableDirectExtension(&psc->base, "GLX_EXT_swap_control_tear");
    __glXEnableDirectExtension(&psc->base, "GLX_SGI_swap_control");
    __glXEnableDirectExtension(&psc->base, "GLX_MESA_swap_control");
    __glXEnableDirectExtension(&psc->base, "GLX_SGI_make_current_read");

src/glx/glxcmds.c
@@ -1863,7 +1863,8 @@ glXSwapIntervalEXT(Display *dpy, GLXDrawable drawable, int interval)
       return;
    }
 
-   if (interval < 0) {
+   if (interval < 0 &&
+       !__glXExtensionBitIsEnabled(pdraw->psc, EXT_swap_control_tear_bit)) {
       __glXSendError(dpy, BadValue, interval, 0, True);
       return;
    }

src/glx/glxextensions.c
@@ -148,6 +148,7 @@ static const struct extension_info known_glx_extensions[] = {
    { GLX(EXT_framebuffer_sRGB),        VER(0,0), Y, Y, N, N },
    { GLX(EXT_import_context),          VER(0,0), Y, Y, N, N },
    { GLX(EXT_swap_control),            VER(0,0), Y, N, N, Y },
+   { GLX(EXT_swap_control_tear),       VER(0,0), Y, N, N, Y },
    { GLX(EXT_texture_from_pixmap),     VER(0,0), Y, N, N, N },
    { GLX(EXT_visual_info),             VER(0,0), Y, Y, N, N },
    { GLX(EXT_visual_rating),           VER(0,0), Y, Y, N, N },

src/glx/glxextensions.h
@@ -52,6 +52,7 @@ enum
    EXT_framebuffer_sRGB_bit,
    EXT_import_context_bit,
    EXT_swap_control_bit,
+   EXT_swap_control_tear_bit,
    EXT_texture_from_pixmap_bit,
    EXT_visual_info_bit,
    EXT_visual_rating_bit,

src/loader/loader_dri3_helper.c
@@ -969,7 +969,7 @@ loader_dri3_swap_buffers_msc(struct loader_dri3_drawable *draw,
        */
       ++draw->send_sbc;
       if (target_msc == 0 && divisor == 0 && remainder == 0)
-         target_msc = draw->msc + draw->swap_interval *
+         target_msc = draw->msc + abs(draw->swap_interval) *
                       (draw->send_sbc - draw->recv_sbc);
       else if (divisor == 0 && remainder > 0) {
          /* From the GLX_OML_sync_control spec:
@@ -989,11 +989,19 @@ loader_dri3_swap_buffers_msc(struct loader_dri3_drawable *draw,
        *     "If <interval> is set to a value of 0, buffer swaps are not
        *      synchronized to a video frame."
        *
+       * From GLX_EXT_swap_control_tear:
+       *
+       *     "If <interval> is negative, the minimum number of video frames
+       *      between buffer swaps is the absolute value of <interval>. In this
+       *      case, if abs(<interval>) video frames have already passed from
+       *      the previous swap when the swap is ready to be performed, the
+       *      swap will occur without synchronization to a video frame."
+       *
        * Implementation note: It is possible to enable triple buffering
        * behaviour by not using XCB_PRESENT_OPTION_ASYNC, but this should not be
        * the default.
        */
-      if (draw->swap_interval == 0)
+      if (draw->swap_interval <= 0)
           options |= XCB_PRESENT_OPTION_ASYNC;
 
       /* If we need to populate the new back, but need to reuse the back

joshuaswarren/mesa-1

commit 2eefe1659baef51406a121f2831908064b78f7e3

glx: Fix the GLX_EXT_swap_control_tear drawable attributes
GLX_SWAP_INTERVAL_EXT is always positive, GLX_LATE_SWAPS_TEAR_EXT is how
you tell whether the drawable is set to do it. This aligns us with the
spec and NVIDIA's GLX.

Closes: mesa/mesa#10193
Fixes: 44d4f6b6ced glx/dri3: Implement GLX_EXT_swap_control_tear
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31655>

src/glx/glx_pbuffer.c
@@ -296,14 +296,13 @@ __glXGetDrawableAttribute(Display * dpy, GLXDrawable drawable,
 
    if (pdraw) {
       if (attribute == GLX_SWAP_INTERVAL_EXT) {
-         *value = pdraw->psc->driScreen.getSwapInterval(pdraw);
+         *value = abs(pdraw->psc->driScreen.getSwapInterval(pdraw));
          return 1;
       } else if (attribute == GLX_MAX_SWAP_INTERVAL_EXT) {
          *value = pdraw->psc->driScreen.maxSwapInterval;
          return 1;
       } else if (attribute == GLX_LATE_SWAPS_TEAR_EXT) {
-         *value = __glXExtensionBitIsEnabled(pdraw->psc,
-                                             EXT_swap_control_tear_bit);
+         *value = pdraw->psc->driScreen.getSwapInterval(pdraw) < 0;
          return 1;
       }
    }