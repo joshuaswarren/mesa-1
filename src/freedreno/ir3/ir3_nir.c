joshuaswarren/mesa-1

commit 5cd646c5da8123a3dd893b1f3205726869fe6965

ir3: fix calling ir3_const_state_mut in the binning VS
lower_driver_param_to_ubo would call ir3_const_state_mut
unconditionally. However, since 33652c5e420 ("ir3, tu: Use a UBO for VS
primitive params on a750+"), it can be called for the binning VS,
causing an assert. This commit makes sure to only call
ir3_const_state_mut when it's really necessary to have mutable access to
the const state.

Fixes: e50c93a6147 ("ir3: make ir3_const_state less error-prone to use")
Signed-off-by: Job Noorman <jnoorman@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30718>

src/freedreno/ir3/ir3_nir_lower_driver_params_to_ubo.c
@@ -9,11 +9,37 @@
 #include "ir3_compiler.h"
 #include "ir3_nir.h"
 
+static nir_def *
+load_primitive_map_ubo(nir_builder *b, struct ir3_shader_variant *v,
+                       unsigned components, unsigned offset)
+{
+   struct ir3_const_state *const_state = ir3_const_state_mut(v);
+   return ir3_load_driver_ubo(b, components, &const_state->primitive_map_ubo,
+                              offset);
+}
+
+static nir_def *
+load_primitive_param_ubo(nir_builder *b, struct ir3_shader_variant *v,
+                         unsigned components, unsigned offset)
+{
+   struct ir3_const_state *const_state = ir3_const_state_mut(v);
+   return ir3_load_driver_ubo(b, components, &const_state->primitive_param_ubo,
+                              offset);
+}
+
+static nir_def *
+load_driver_params_ubo(nir_builder *b, struct ir3_shader_variant *v,
+                       unsigned components, unsigned offset)
+{
+   struct ir3_const_state *const_state = ir3_const_state_mut(v);
+   return ir3_load_driver_ubo(b, components, &const_state->driver_params_ubo,
+                              offset);
+}
+
 static bool
 lower_driver_param_to_ubo(nir_builder *b, nir_intrinsic_instr *intr, void *in)
 {
    struct ir3_shader_variant *v = in;
-   struct ir3_const_state *const_state = ir3_const_state_mut(v);
 
    unsigned components = nir_intrinsic_dest_components(intr);
 
@@ -22,33 +48,26 @@ lower_driver_param_to_ubo(nir_builder *b, nir_intrinsic_instr *intr, void *in)
    nir_def *result;
    switch (intr->intrinsic) {
    case nir_intrinsic_load_primitive_location_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_map_ubo,
-                                   nir_intrinsic_driver_location(intr));
+      result = load_primitive_map_ubo(b, v, components,
+                                      nir_intrinsic_driver_location(intr));
       break;
    case nir_intrinsic_load_vs_primitive_stride_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 0);
+      result = load_primitive_param_ubo(b, v, components, 0);
       break;
    case nir_intrinsic_load_vs_vertex_stride_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 1);
+      result = load_primitive_param_ubo(b, v, components, 1);
       break;
    case nir_intrinsic_load_hs_patch_stride_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 2);
+      result = load_primitive_param_ubo(b, v, components, 2);
       break;
    case nir_intrinsic_load_patch_vertices_in:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 3);
+      result = load_primitive_param_ubo(b, v, components, 3);
       break;
    case nir_intrinsic_load_tess_param_base_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 4);
+      result = load_primitive_param_ubo(b, v, components, 4);
       break;
    case nir_intrinsic_load_tess_factor_base_ir3:
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->primitive_param_ubo, 6);
+      result = load_primitive_param_ubo(b, v, components, 6);
       break;
    /* These are still loaded using CP_LOAD_STATE for compatibility with indirect
     * draws where the CP does a CP_LOAD_STATE for us internally:
@@ -63,9 +82,7 @@ lower_driver_param_to_ubo(nir_builder *b, nir_intrinsic_instr *intr, void *in)
       if (!ir3_get_driver_param_info(b->shader, intr, &param_info))
          return false;
 
-      result = ir3_load_driver_ubo(b, components,
-                                   &const_state->driver_params_ubo,
-                                   param_info.offset);
+      result = load_driver_params_ubo(b, v, components, param_info.offset);
    }
    }
 

commit 2c155b470869af47d2e06babe6eb484be3460e26

ir3: Fix binning pass driver UBOs
Fixes: 5cd646c5da81 ("ir3: fix calling ir3_const_state_mut in the binning VS")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31857>

src/freedreno/ir3/ir3_nir.c
@@ -13,6 +13,9 @@
 #include "ir3_nir.h"
 #include "ir3_shader.h"
 
+/* For use by binning_pass shaders, where const_state is const, but expected
+ * to be already set up when we compiled the corresponding non-binning variant
+ */
 nir_def *
 ir3_get_shared_driver_ubo(nir_builder *b, const struct ir3_driver_ubo *ubo)
 {
@@ -78,6 +81,30 @@ ir3_update_driver_ubo(nir_shader *nir, const struct ir3_driver_ubo *ubo, const c
    var->data.driver_location = ubo->idx;
 }
 
+static nir_def *
+load_driver_ubo(nir_builder *b, unsigned components, nir_def *ubo, unsigned offset)
+{
+   return nir_load_ubo(b, components, 32, ubo,
+                       nir_imm_int(b, offset * sizeof(uint32_t)),
+                       .align_mul = 16,
+                       .align_offset = (offset % 4) * sizeof(uint32_t),
+                       .range_base = offset * sizeof(uint32_t),
+                       .range = components * sizeof(uint32_t));
+}
+
+/* For use by binning_pass shaders, where const_state is const, but expected
+ * to be already set up when we compiled the corresponding non-binning variant
+ */
+nir_def *
+ir3_load_shared_driver_ubo(nir_builder *b, unsigned components,
+                           const struct ir3_driver_ubo *ubo,
+                           unsigned offset)
+{
+   assert(ubo->size >= MAX2(ubo->size, offset + components));
+
+   return load_driver_ubo(b, components, ir3_get_shared_driver_ubo(b, ubo), offset);
+}
+
 nir_def *
 ir3_load_driver_ubo(nir_builder *b, unsigned components,
                     struct ir3_driver_ubo *ubo,
@@ -85,12 +112,7 @@ ir3_load_driver_ubo(nir_builder *b, unsigned components,
 {
    ubo->size = MAX2(ubo->size, offset + components);
 
-   return nir_load_ubo(b, components, 32, ir3_get_driver_ubo(b, ubo),
-                       nir_imm_int(b, offset * sizeof(uint32_t)),
-                       .align_mul = 16,
-                       .align_offset = (offset % 4) * sizeof(uint32_t),
-                       .range_base = offset * sizeof(uint32_t),
-                       .range = components * sizeof(uint32_t));
+   return load_driver_ubo(b, components, ir3_get_driver_ubo(b, ubo), offset);
 }
 
 nir_def *

src/freedreno/ir3/ir3_nir.h
@@ -97,6 +97,9 @@ nir_def *ir3_get_driver_ubo(nir_builder *b, struct ir3_driver_ubo *ubo);
 nir_def *ir3_get_driver_consts_ubo(nir_builder *b,
                                    struct ir3_shader_variant *v);
 void ir3_update_driver_ubo(nir_shader *nir, const struct ir3_driver_ubo *ubo, const char *name);
+nir_def *ir3_load_shared_driver_ubo(nir_builder *b, unsigned components,
+                                    const struct ir3_driver_ubo *ubo,
+                                    unsigned offset);
 nir_def *ir3_load_driver_ubo(nir_builder *b, unsigned components,
                              struct ir3_driver_ubo *ubo,
                              unsigned offset);

src/freedreno/ir3/ir3_nir_lower_driver_params_to_ubo.c
@@ -13,6 +13,10 @@ static nir_def *
 load_primitive_map_ubo(nir_builder *b, struct ir3_shader_variant *v,
                        unsigned components, unsigned offset)
 {
+   if (v->binning_pass) {
+      return ir3_load_shared_driver_ubo(
+         b, components, &ir3_const_state(v)->primitive_map_ubo, offset);
+   }
    struct ir3_const_state *const_state = ir3_const_state_mut(v);
    return ir3_load_driver_ubo(b, components, &const_state->primitive_map_ubo,
                               offset);
@@ -22,6 +26,10 @@ static nir_def *
 load_primitive_param_ubo(nir_builder *b, struct ir3_shader_variant *v,
                          unsigned components, unsigned offset)
 {
+   if (v->binning_pass) {
+      return ir3_load_shared_driver_ubo(
+         b, components, &ir3_const_state(v)->primitive_param_ubo, offset);
+   }
    struct ir3_const_state *const_state = ir3_const_state_mut(v);
    return ir3_load_driver_ubo(b, components, &const_state->primitive_param_ubo,
                               offset);
@@ -31,6 +39,10 @@ static nir_def *
 load_driver_params_ubo(nir_builder *b, struct ir3_shader_variant *v,
                        unsigned components, unsigned offset)
 {
+   if (v->binning_pass) {
+      return ir3_load_shared_driver_ubo(
+         b, components, &ir3_const_state(v)->driver_params_ubo, offset);
+   }
    struct ir3_const_state *const_state = ir3_const_state_mut(v);
    return ir3_load_driver_ubo(b, components, &const_state->driver_params_ubo,
                               offset);