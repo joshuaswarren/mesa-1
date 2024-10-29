openharmony/third_party_mesa3d

commit f36c4333240656ab069436d03061977f02a72f39

freedreno: add env var to override tiles-per-pipe
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6461>

src/gallium/drivers/freedreno/freedreno_gmem.c
@@ -24,6 +24,7 @@
  *    Rob Clark <robclark@freedesktop.org>
  */
 
+#include "util/debug.h"
 #include "pipe/p_state.h"
 #include "util/hash_table.h"
 #include "util/u_dump.h"
@@ -325,6 +326,11 @@ gmem_stateobj_init(struct fd_screen *screen, struct gmem_key *key)
 				div_round_up(gmem->nbins_x, tpp_x)) > npipes)
 			tpp_x += 1;
 	}
+
+#ifdef DEBUG
+	tpp_x = env_var_as_unsigned("TPP_X", tpp_x);
+	tpp_y = env_var_as_unsigned("TPP_Y", tpp_x);
+#endif
 
 	gmem->maxpw = tpp_x;
 	gmem->maxph = tpp_y;

joshuaswarren/mesa-1

commit a1062d3b94fb83d837089ee506f4ea0490926a2d

freedreno: Fix tile-per-pipe debug overrides
Fixes: f36c43332406 ("freedreno: add env var to override tiles-per-pipe")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31857>

src/gallium/drivers/freedreno/freedreno_gmem.c
@@ -317,7 +317,7 @@ gmem_stateobj_init(struct fd_screen *screen, struct gmem_key *key)
 
 #if MESA_DEBUG
    tpp_x = debug_get_num_option("TPP_X", tpp_x);
-   tpp_y = debug_get_num_option("TPP_Y", tpp_x);
+   tpp_y = debug_get_num_option("TPP_Y", tpp_y);
 #endif
 
    gmem->maxpw = tpp_x;