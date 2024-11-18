joshuaswarren/mesa-1

commit 14c317c5570825086aa619e127a7723ca62a6d53

anv: add VK_EXT_host_image_copy support
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24276>

src/intel/vulkan/anv_image.c
@@ -247,6 +247,12 @@ anv_image_choose_isl_surf_usage(struct anv_physical_device *device,
        vk_usage & VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR)
       isl_usage |= ISL_SURF_USAGE_VIDEO_DECODE_BIT;
 
+   /* We disable aux surfaces for host read/write images so that we can update
+    * the main surface without caring about the auxiliary surface.
+    */
+   if (vk_usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT)
+      isl_usage |= ISL_SURF_USAGE_DISABLE_AUX_BIT;
+
    if (vk_create_flags & VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT)
       isl_usage |= ISL_SURF_USAGE_CUBE_BIT;
 
@@ -1866,7 +1872,7 @@ anv_image_finish(struct anv_image *image)
          anv_device_unmap_bo(device,
                              image->bindings[b].address.bo,
                              image->bindings[b].host_map,
-                             image->bindings[b].memory_range.size,
+                             image->bindings[b].map_size,
                              false /* replace */);
       }
    }
@@ -2083,20 +2089,30 @@ anv_image_is_pat_compressible(struct anv_device *device, struct anv_image *image
     *    For images created with a color format, the memoryTypeBits member is
     *    identical for all VkImage objects created with the same combination
     *    of values for the tiling member, the
-    *    VK_IMAGE_CREATE_SPARSE_BINDING_BIT bit of the flags member, the
+    *    VK_IMAGE_CREATE_SPARSE_BINDING_BIT bit and
+    *    VK_IMAGE_CREATE_PROTECTED_BIT bit of the flags member, the
     *    VK_IMAGE_CREATE_SPLIT_INSTANCE_BIND_REGIONS_BIT bit of the flags
-    *    member, handleTypes member of VkExternalMemoryImageCreateInfo, and
-    *    the VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT of the usage member in
-    *    the VkImageCreateInfo structure passed to vkCreateImage.
+    *    member, the VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT bit of the usage
+    *    member if the
+    *    VkPhysicalDeviceHostImageCopyPropertiesEXT::identicalMemoryTypeRequirements
+    *    property is VK_FALSE, handleTypes member of
+    *    VkExternalMemoryImageCreateInfo, and the
+    *    VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT of the usage member in the
+    *    VkImageCreateInfo structure passed to vkCreateImage.
     *
     *    For images created with a depth/stencil format, the memoryTypeBits
     *    member is identical for all VkImage objects created with the same
     *    combination of values for the format member, the tiling member, the
-    *    VK_IMAGE_CREATE_SPARSE_BINDING_BIT bit of the flags member, the
+    *    VK_IMAGE_CREATE_SPARSE_BINDING_BIT bit and
+    *    VK_IMAGE_CREATE_PROTECTED_BIT bit of the flags member, the
     *    VK_IMAGE_CREATE_SPLIT_INSTANCE_BIND_REGIONS_BIT bit of the flags
-    *    member, handleTypes member of VkExternalMemoryImageCreateInfo, and
-    *    the VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT of the usage member in
-    *    the VkImageCreateInfo structure passed to vkCreateImage.
+    *    member, the VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT bit of the usage
+    *    member if the
+    *    VkPhysicalDeviceHostImageCopyPropertiesEXT::identicalMemoryTypeRequirements
+    *    property is VK_FALSE, handleTypes member of
+    *    VkExternalMemoryImageCreateInfo, and the
+    *    VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT of the usage member in the
+    *    VkImageCreateInfo structure passed to vkCreateImage.
     */
 
    /* There are no compression-enabled modifiers on Xe2, and all legacy
@@ -2503,18 +2519,28 @@ anv_image_bind_address(struct anv_device *device,
    image->bindings[binding].address = address;
 
    /* Map bindings for images with host transfer usage, so that we don't have
-    * to map/unmap things at every host operation.
+    * to map/unmap things at every host operation. We map cached, that means
+    * that the copy operations need to cflush on platforms that have no
+    * host_cache+host_coherent memory types.
     */
    if (image->vk.usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT) {
+      uint64_t offset = image->bindings[binding].address.offset +
+                        image->bindings[binding].memory_range.offset;
+      uint64_t map_offset, map_size;
+      anv_sanitize_map_params(device, offset,
+                              image->bindings[binding].memory_range.size,
+                              &map_offset, &map_size);
+
       VkResult result = anv_device_map_bo(device,
                                           image->bindings[binding].address.bo,
-                                          image->bindings[binding].address.offset +
-                                          image->bindings[binding].memory_range.offset,
-                                          image->bindings[binding].memory_range.size,
+                                          map_offset, map_size,
                                           NULL /* placed_addr */,
                                           &image->bindings[binding].host_map);
       if (result != VK_SUCCESS)
          return result;
+
+      image->bindings[binding].map_delta = (offset - map_offset);
+      image->bindings[binding].map_size = map_size;
    }
 
    ANV_RMV(image_bind, device, image, binding);
@@ -2708,7 +2734,8 @@ VkResult anv_BindImageMemory2(
 }
 
 static void
-anv_get_image_subresource_layout(const struct anv_image *image,
+anv_get_image_subresource_layout(struct anv_device *device,
+                                 const struct anv_image *image,
                                  const VkImageSubresource2KHR *subresource,
                                  VkSubresourceLayout2KHR *layout)
 {
@@ -2804,11 +2831,11 @@ anv_get_image_subresource_layout(const struct anv_image *image,
       row_pitch_B = isl_surf->row_pitch_B;
    }
 
+   const uint32_t level = subresource->imageSubresource.mipLevel;
    if (isl_surf) {
       /* ISL tries to give us a single layer but the Vulkan API expect the
        * entire 3D size.
        */
-      const uint32_t level = subresource->imageSubresource.mipLevel;
       const uint32_t layer = subresource->imageSubresource.arrayLayer;
       const uint32_t z = u_minify(isl_surf->logical_level0_px.d, level) - 1;
       uint64_t z0_start_tile_B, z0_end_tile_B;
@@ -2832,6 +2859,37 @@ anv_get_image_subresource_layout(const struct anv_image *image,
       /* Not a surface so those fields don't make sense */
       layout->subresourceLayout.depthPitch = 0;
       layout->subresourceLayout.arrayPitch = 0;
+   }
+
+   VkSubresourceHostMemcpySizeEXT *host_memcpy_size =
+      vk_find_struct(layout->pNext, SUBRESOURCE_HOST_MEMCPY_SIZE_EXT);
+   if (host_memcpy_size) {
+      if (!isl_surf) {
+         host_memcpy_size->size = 0;
+      } else if (anv_image_can_host_memcpy(image)) {
+         host_memcpy_size->size = layout->subresourceLayout.size;
+      } else {
+         /* If we cannot do straight memcpy of the image, compute a linear
+          * size. This will be the format in which we store the data.
+          */
+         struct isl_surf lin_surf;
+         bool ok =
+            isl_surf_init(&device->physical->isl_dev, &lin_surf,
+                          .dim          = isl_surf->dim,
+                          .format       = isl_surf->format,
+                          .width        = u_minify(
+                             isl_surf->logical_level0_px.w, level),
+                          .height       = u_minify(
+                             isl_surf->logical_level0_px.h, level),
+                          .depth        = u_minify(
+                             isl_surf->logical_level0_px.d, level),
+                          .array_len    = 1,
+                          .levels       = 1,
+                          .samples      = isl_surf->samples,
+                          .tiling_flags = ISL_TILING_LINEAR_BIT);
+         assert(ok);
+         host_memcpy_size->size = lin_surf.size_B;
+      }
    }
 
    VkImageCompressionPropertiesEXT *comp_props =
@@ -2864,18 +2922,19 @@ void anv_GetDeviceImageSubresourceLayoutKHR(
       return;
    }
 
-   anv_get_image_subresource_layout(&image, pInfo->pSubresource, pLayout);
+   anv_get_image_subresource_layout(device, &image, pInfo->pSubresource, pLayout);
 }
 
 void anv_GetImageSubresourceLayout2KHR(
-    VkDevice                                    device,
+    VkDevice                                    _device,
     VkImage                                     _image,
     const VkImageSubresource2KHR*               pSubresource,
     VkSubresourceLayout2KHR*                    pLayout)
 {
+   ANV_FROM_HANDLE(anv_device, device, _device);
    ANV_FROM_HANDLE(anv_image, image, _image);
 
-   anv_get_image_subresource_layout(image, pSubresource, pLayout);
+   anv_get_image_subresource_layout(device, image, pSubresource, pLayout);
 }
 
 static VkImageUsageFlags

src/intel/vulkan/anv_formats.c
@@ -580,7 +580,8 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
                VK_FORMAT_FEATURE_2_BLIT_SRC_BIT |
                VK_FORMAT_FEATURE_2_BLIT_DST_BIT |
                VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT |
-               VK_FORMAT_FEATURE_2_TRANSFER_DST_BIT;
+               VK_FORMAT_FEATURE_2_TRANSFER_DST_BIT |
+               VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
 
       if (aspects & VK_IMAGE_ASPECT_DEPTH_BIT) {
          flags |= VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_FILTER_LINEAR_BIT |
@@ -633,7 +634,8 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
                 VK_FORMAT_FEATURE_2_TRANSFER_DST_BIT;
 
       flags |= VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT |
-               VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_FILTER_MINMAX_BIT;
+               VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_FILTER_MINMAX_BIT |
+               VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
 
       if (isl_format_supports_filtering(devinfo, plane_format.isl_format))
          flags |= VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_FILTER_LINEAR_BIT;
@@ -840,6 +842,11 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
           */
          flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_BIT;
          flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
+
+         /* Host transfer don't touch the AUX data, so if that is required by
+          * the modifier, just drop support on the format.
+          */
+         flags &= ~VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
       }
 
       if (isl_mod_info->supports_clear_color && plane_format.isl_format !=
@@ -1321,6 +1328,7 @@ anv_get_image_format_properties(
    VkAndroidHardwareBufferUsageANDROID *android_usage = NULL;
    VkTextureLODGatherFormatPropertiesAMD *texture_lod_gather_props = NULL;
    VkImageCompressionPropertiesEXT *comp_props = NULL;
+   VkHostImageCopyDevicePerformanceQueryEXT *host_props = NULL;
    bool from_wsi = false;
 
    /* Extract input structs */
@@ -1371,6 +1379,9 @@ anv_get_image_format_properties(
       case VK_STRUCTURE_TYPE_IMAGE_COMPRESSION_PROPERTIES_EXT:
          comp_props = (void *) s;
          break;
+      case VK_STRUCTURE_TYPE_HOST_IMAGE_COPY_DEVICE_PERFORMANCE_QUERY_EXT:
+         host_props = (void *) s;
+         break;
       default:
          vk_debug_ignored_stype(s->sType);
          break;
@@ -1378,6 +1389,11 @@ anv_get_image_format_properties(
    }
 
    if (format == NULL)
+      goto unsupported;
+
+   /* Would require some annoying tracking or kernel support. */
+   if ((info->flags & VK_IMAGE_CREATE_SPARSE_BINDING_BIT) &&
+       (info->usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT))
       goto unsupported;
 
    if (info->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
@@ -1795,16 +1811,46 @@ anv_get_image_format_properties(
       }
    }
 
+   const bool aux_supported =
+      vk_format_has_depth(info->format) ||
+      isl_format_supports_ccs_d(devinfo, format->planes[0].isl_format) ||
+      anv_formats_ccs_e_compatible(devinfo, info->flags, info->format,
+                                   info->tiling, info->usage,
+                                   format_list_info);
+
    if (comp_props) {
-      bool ccs_supported =
-         anv_formats_ccs_e_compatible(devinfo, info->flags, info->format,
-                                      info->tiling, info->usage,
-                                      format_list_info);
       comp_props->imageCompressionFixedRateFlags =
          VK_IMAGE_COMPRESSION_FIXED_RATE_NONE_EXT;
-      comp_props->imageCompressionFlags = ccs_supported ?
+      comp_props->imageCompressionFlags = aux_supported ?
          VK_IMAGE_COMPRESSION_DEFAULT_EXT :
          VK_IMAGE_COMPRESSION_DISABLED_EXT;
+   }
+
+   if (host_props) {
+      const bool compressed_format =
+         isl_format_is_compressed(format->planes[0].isl_format);
+      /* We're required to return optimalDeviceAccess for compressed formats:
+       *
+       *    "If VkPhysicalDeviceImageFormatInfo2::format is a block-compressed
+       *     format and vkGetPhysicalDeviceImageFormatProperties2 returns
+       *     VK_SUCCESS, the implementation must return VK_TRUE in
+       *     optimalDeviceAccess."
+       *
+       * When compression is not supported, the size of the image will not be
+       * changing to support host image transfers.
+       *
+       * TODO: We might be able to still allocate the compression data so that
+       *       we can report identicalMemoryLayout=true, but we might still
+       *       have to report optimalDeviceAccess=false to signal potential
+       *       perf loss.
+       */
+      if (compressed_format || !aux_supported) {
+         host_props->optimalDeviceAccess = true;
+         host_props->identicalMemoryLayout = true;
+      } else {
+         host_props->optimalDeviceAccess = false;
+         host_props->identicalMemoryLayout = false;
+      }
    }
 
    return VK_SUCCESS;

src/intel/vulkan/anv_physical_device.c
@@ -215,6 +215,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .EXT_global_priority_query             = device->max_context_priority >=
                                                VK_QUEUE_GLOBAL_PRIORITY_MEDIUM_KHR,
       .EXT_graphics_pipeline_library         = !debug_get_bool_option("ANV_NO_GPL", false),
+      .EXT_host_image_copy                   = !device->emu_astc_ldr,
       .EXT_host_query_reset                  = true,
       .EXT_image_2d_view_of_3d               = true,
       /* Because of Xe2 PAT selected compression and the Vulkan spec
@@ -832,6 +833,9 @@ get_features(const struct anv_physical_device *pdevice,
 
       /* VK_EXT_pipeline_protected_access */
       .pipelineProtectedAccess = true,
+
+      /* VK_EXT_host_image_copy */
+      .hostImageCopy = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without
@@ -1529,6 +1533,77 @@ get_properties(const struct anv_physical_device *pdevice,
    {
       props->graphicsPipelineLibraryFastLinking = true;
       props->graphicsPipelineLibraryIndependentInterpolationDecoration = true;
+   }
+
+   /* VK_EXT_host_image_copy */
+   {
+      static const VkImageLayout supported_layouts[] = {
+         VK_IMAGE_LAYOUT_GENERAL,
+         VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+         VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_READ_ONLY_STENCIL_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_STENCIL_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_DEPTH_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_STENCIL_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_STENCIL_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL,
+         VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL,
+         VK_IMAGE_LAYOUT_FRAGMENT_SHADING_RATE_ATTACHMENT_OPTIMAL_KHR,
+         VK_IMAGE_LAYOUT_ATTACHMENT_FEEDBACK_LOOP_OPTIMAL_EXT,
+         VK_IMAGE_LAYOUT_RENDERING_LOCAL_READ_KHR,
+      };
+
+      props->pCopySrcLayouts = (VkImageLayout *) supported_layouts;
+      props->copySrcLayoutCount = ARRAY_SIZE(supported_layouts);
+      props->pCopyDstLayouts = (VkImageLayout *) supported_layouts;
+      props->copyDstLayoutCount = ARRAY_SIZE(supported_layouts);
+
+      /* This UUID essentially tells you if you can share an optimially tiling
+       * image with another driver. Much of the tiling decisions are based on :
+       *
+       *    - device generation (different tilings based on generations)
+       *    - device workarounds
+       *    - driver build (as we implement workarounds or performance tunings,
+       *      the tiling decision changes)
+       *
+       * So we're using a hash of the verx10 field + driver_build_sha1.
+       *
+       * Unfortunately there is a HW issue on SKL GT4 that makes it use some
+       * different tilings sometimes (see isl_gfx7.c).
+       */
+      {
+         struct mesa_sha1 sha1_ctx;
+         uint8_t sha1[20];
+
+         _mesa_sha1_init(&sha1_ctx);
+         _mesa_sha1_update(&sha1_ctx, pdevice->driver_build_sha1,
+                           sizeof(pdevice->driver_build_sha1));
+         _mesa_sha1_update(&sha1_ctx, &pdevice->info.platform,
+                           sizeof(pdevice->info.platform));
+         if (pdevice->info.platform == INTEL_PLATFORM_SKL &&
+             pdevice->info.gt == 4) {
+            _mesa_sha1_update(&sha1_ctx, &pdevice->info.gt,
+                              sizeof(pdevice->info.gt));
+         }
+         _mesa_sha1_final(&sha1_ctx, sha1);
+
+         assert(ARRAY_SIZE(sha1) >= VK_UUID_SIZE);
+         memcpy(props->optimalTilingLayoutUUID, sha1, VK_UUID_SIZE);
+      }
+
+      /* System without ReBAR cannot map all memory types on the host and that
+       * affects the memory types an image can use for host memory copies.
+       *
+       * System with compressed memory types also cannot expose all image
+       * memory types for host image copies.
+       */
+      props->identicalMemoryTypeRequirements = pdevice->has_small_bar ||
+         pdevice->memory.compressed_mem_types != 0;
    }
 
    /* VK_EXT_legacy_vertex_attributes */

src/intel/vulkan/anv_private.h
@@ -5358,6 +5358,8 @@ struct anv_image {
       struct anv_address address;
       struct anv_sparse_binding_data sparse_data;
       void *host_map;
+      uint64_t map_delta;
+      uint64_t map_size;
    } bindings[ANV_IMAGE_MEMORY_BINDING_END];
 
    /**
@@ -5460,6 +5462,22 @@ anv_image_format_is_d16_or_s8(const struct anv_image *image)
       image->vk.format == VK_FORMAT_D24_UNORM_S8_UINT ||
       image->vk.format == VK_FORMAT_D32_SFLOAT_S8_UINT ||
       image->vk.format == VK_FORMAT_S8_UINT;
+}
+
+static inline bool
+anv_image_can_host_memcpy(const struct anv_image *image)
+{
+   const struct isl_surf *surf = &image->planes[0].primary_surface.isl;
+   struct isl_tile_info tile_info;
+   isl_surf_get_tile_info(surf, &tile_info);
+
+   const bool array_pitch_aligned_to_tile =
+      surf->array_pitch_el_rows % tile_info.logical_extent_el.height == 0;
+
+   return image->vk.tiling != VK_IMAGE_TILING_LINEAR &&
+          image->n_planes == 1 &&
+          array_pitch_aligned_to_tile &&
+          image->vk.mip_levels == 1;
 }
 
 /* The ordering of this enum is important */

src/intel/vulkan/meson.build
@@ -158,6 +158,7 @@ libanv_files = files(
   'anv_formats.c',
   'anv_genX.h',
   'anv_image.c',
+  'anv_image_host_copy.c',
   'anv_image_view.c',
   'anv_internal_kernels.c',
   'anv_internal_kernels.h',

commit 9e8c4e5f29e10e074cf6df1548635546ffbf46a3

anv: fix incorrect aspect flag for depth/stencil formats
We're asking if compression is supported and
anv_formats_ccs_e_compatible() is assuming color aspect.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 14c317c557 ("anv: add VK_EXT_host_image_copy support")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12155
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32153>

src/intel/vulkan/anv_image.c
@@ -478,6 +478,7 @@ storage_image_format_supports_atomic(const struct intel_device_info *devinfo,
 static bool
 formats_ccs_e_compatible(const struct intel_device_info *devinfo,
                          VkImageCreateFlags create_flags,
+                         VkImageAspectFlagBits aspect,
                          enum isl_format format, VkImageTiling vk_tiling,
                          const VkImageFormatListCreateInfo *fmt_list)
 {
@@ -500,7 +501,7 @@ formats_ccs_e_compatible(const struct intel_device_info *devinfo,
 
       enum isl_format view_format =
          anv_get_isl_format(devinfo, fmt_list->pViewFormats[i],
-                            VK_IMAGE_ASPECT_COLOR_BIT, vk_tiling);
+                            aspect, vk_tiling);
 
       if (!isl_formats_are_ccs_e_compatible(devinfo, format, view_format))
          return false;
@@ -534,8 +535,8 @@ anv_formats_ccs_e_compatible(const struct intel_device_info *devinfo,
       enum isl_format format =
          anv_get_isl_format(devinfo, vk_format, aspect, vk_tiling);
 
-      if (!formats_ccs_e_compatible(devinfo, create_flags, format, vk_tiling,
-                                    fmt_list))
+      if (!formats_ccs_e_compatible(devinfo, create_flags, aspect,
+                                    format, vk_tiling, fmt_list))
          return false;
 
       if (vk_usage & VK_IMAGE_USAGE_STORAGE_BIT) {