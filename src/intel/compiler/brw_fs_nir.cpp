joshuaswarren/mesa-1

commit 553d109b009903b880b33fe87f9a00d9b02329e3

brw: factor out source extraction for rematerialization
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29663>

src/intel/compiler/brw_fs_nir.cpp
@@ -4630,48 +4630,42 @@ try_rebuild_resource(nir_to_brw_state &ntb, const brw::fs_builder &bld, nir_def 
             break;
          }
 
+         fs_reg srcs[3];
+         for (unsigned s = 0; s < nir_op_infos[alu->op].num_inputs; s++) {
+            srcs[s] = offset(
+               ntb.resource_insts[alu->src[s].src.ssa->index]->dst,
+               ubld8, alu->src[s].swizzle[0]);
+            assert(srcs[s].file != BAD_FILE);
+            assert(srcs[s].type == BRW_TYPE_UD);
+         }
+
          switch (alu->op) {
          case nir_op_iadd: {
-            fs_reg src0 = ntb.resource_insts[alu->src[0].src.ssa->index]->dst;
-            fs_reg src1 = ntb.resource_insts[alu->src[1].src.ssa->index]->dst;
-            assert(src0.file != BAD_FILE && src1.file != BAD_FILE);
-            assert(src0.type == BRW_TYPE_UD);
-            assert(src1.type == BRW_TYPE_UD);
-            ubld8.ADD(src0.file != IMM ? src0 : src1,
-                      src0.file != IMM ? src1 : src0,
+            ubld8.ADD(srcs[0].file != IMM ? srcs[0] : srcs[1],
+                      srcs[0].file != IMM ? srcs[1] : srcs[0],
                       &ntb.resource_insts[def->index]);
             break;
          }
          case nir_op_iadd3: {
-            fs_reg dst = ubld8.vgrf(BRW_TYPE_UD);
-            fs_reg src0 = ntb.resource_insts[alu->src[0].src.ssa->index]->dst;
-            fs_reg src1 = ntb.resource_insts[alu->src[1].src.ssa->index]->dst;
-            fs_reg src2 = ntb.resource_insts[alu->src[2].src.ssa->index]->dst;
-            assert(src0.file != BAD_FILE && src1.file != BAD_FILE && src2.file != BAD_FILE);
-            assert(src0.type == BRW_TYPE_UD);
+            fs_reg dst = ubld8.vgrf(srcs[0].type);
             ntb.resource_insts[def->index] =
                ubld8.ADD3(dst,
-                          src1.file == IMM ? src1 : src0,
-                          src1.file == IMM ? src0 : src1,
-                          src2);
+                          srcs[1].file == IMM ? srcs[1] : srcs[0],
+                          srcs[1].file == IMM ? srcs[0] : srcs[1],
+                          srcs[2]);
             break;
          }
          case nir_op_ushr: {
-            fs_reg src0 = ntb.resource_insts[alu->src[0].src.ssa->index]->dst;
-            fs_reg src1 = ntb.resource_insts[alu->src[1].src.ssa->index]->dst;
-            assert(src0.file != BAD_FILE && src1.file != BAD_FILE);
-            assert(src0.type == BRW_TYPE_UD);
-            assert(src1.type == BRW_TYPE_UD);
-            ubld8.SHR(src0, src1, &ntb.resource_insts[def->index]);
+            enum brw_reg_type utype =
+               brw_type_with_size(srcs[0].type,
+                                  brw_type_size_bits(srcs[0].type));
+            ubld8.SHR(retype(srcs[0], utype),
+                      retype(srcs[1], utype),
+                      &ntb.resource_insts[def->index]);
             break;
          }
          case nir_op_ishl: {
-            fs_reg src0 = ntb.resource_insts[alu->src[0].src.ssa->index]->dst;
-            fs_reg src1 = ntb.resource_insts[alu->src[1].src.ssa->index]->dst;
-            assert(src0.file != BAD_FILE && src1.file != BAD_FILE);
-            assert(src0.type == BRW_TYPE_UD);
-            assert(src1.type == BRW_TYPE_UD);
-            ubld8.SHL(src0, src1, &ntb.resource_insts[def->index]);
+            ubld8.SHL(srcs[0], srcs[1], &ntb.resource_insts[def->index]);
             break;
          }
          case nir_op_mov: {

commit 4974884bf81483fa6b006d89424d205f18d38360

brw: Fix try_rebuild_source's ult32/ushr handling to use unsigned types
We were accidentally doing a signed integer comparison here for ult32,
or a sign-extending shift for ushr.

One notable bit of fallout was that load_global_uniform_block_intel
address calculations broke on platforms that don't have native 64-bit
integer support, as the iadd64 lowering for "do I need to carry?" was
using ult32...and performing the wrong comparison.  We spotted this in
Borderlands 3 on Alchemist once we turned on other optimizations.

Thanks to Lionel Landwerlin for helping spot the problem!

Fixes: 553d109b00 ("brw: factor out source extraction for rematerialization")
Fixes: b1d452aab4 ("brw: enable A64 loads source rematerialization")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31995>

src/intel/compiler/brw_fs_nir.cpp
@@ -4816,7 +4816,7 @@ try_rebuild_source(nir_to_brw_state &ntb, const brw::fs_builder &bld,
          }
          case nir_op_ushr: {
             enum brw_reg_type utype =
-               brw_type_with_size(srcs[0].type,
+               brw_type_with_size(BRW_TYPE_UD,
                                   brw_type_size_bits(srcs[0].type));
             ubld.SHR(retype(srcs[0], utype),
                      retype(srcs[1], utype),
@@ -4834,10 +4834,10 @@ try_rebuild_source(nir_to_brw_state &ntb, const brw::fs_builder &bld,
          case nir_op_ult32: {
             if (brw_type_size_bits(srcs[0].type) != 32)
                break;
-            brw_reg dst = ubld.vgrf(srcs[0].type);
             enum brw_reg_type utype =
-               brw_type_with_size(srcs[0].type,
+               brw_type_with_size(BRW_TYPE_UD,
                                   brw_type_size_bits(srcs[0].type));
+            brw_reg dst = ubld.vgrf(utype);
             ntb.resource_insts[def->index] =
                ubld.CMP(dst,
                         retype(srcs[0], utype),