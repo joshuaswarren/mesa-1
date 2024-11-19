joshuaswarren/mesa-1

commit 883cf626c4797c7229ebdd60d25b3f53a3f0cba9

nir/lcssa: Fix rematerializing derefs
This would pull derefs out of loops by emitting the pattern
`deref(phi(deref))` which is not allowed by nir_validate.

Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23712>

src/compiler/nir/nir_to_lcssa.c
@@ -223,6 +223,11 @@ convert_loop_exit_for_ssa(nir_def *def, void *void_state)
    if (all_uses_inside_loop)
       return true;
 
+   if (def->parent_instr->type == nir_instr_type_deref) {
+      nir_rematerialize_deref_in_use_blocks(nir_instr_as_deref(def->parent_instr));
+      return true;
+   }
+
    /* Initialize a phi-instruction */
    nir_phi_instr *phi = nir_phi_instr_create(state->shader);
    nir_def_init(&phi->instr, &phi->def, def->num_components,
@@ -238,23 +243,6 @@ convert_loop_exit_for_ssa(nir_def *def, void *void_state)
 
    nir_instr_insert_before_block(state->block_after_loop, &phi->instr);
    nir_def *dest = &phi->def;
-
-   /* deref instructions need a cast after the phi */
-   if (def->parent_instr->type == nir_instr_type_deref) {
-      nir_deref_instr *cast =
-         nir_deref_instr_create(state->shader, nir_deref_type_cast);
-
-      nir_deref_instr *instr = nir_instr_as_deref(def->parent_instr);
-      cast->modes = instr->modes;
-      cast->type = instr->type;
-      cast->parent = nir_src_for_ssa(&phi->def);
-      cast->cast.ptr_stride = nir_deref_instr_array_stride(instr);
-
-      nir_def_init(&cast->instr, &cast->def,
-                   phi->def.num_components, phi->def.bit_size);
-      nir_instr_insert(nir_after_phis(state->block_after_loop), &cast->instr);
-      dest = &cast->def;
-   }
 
    /* Run through all uses and rewrite those outside the loop to point to
     * the phi instead of pointing to the ssa-def.
@@ -340,8 +328,8 @@ convert_to_lcssa(nir_cf_node *cf_node, lcssa_state *state)
          }
       }
 
-      nir_foreach_block_in_cf_node(block, cf_node) {
-         nir_foreach_instr(instr, block) {
+      nir_foreach_block_in_cf_node_reverse(block, cf_node) {
+         nir_foreach_instr_reverse_safe(instr, block) {
             nir_foreach_def(instr, convert_loop_exit_for_ssa, state);
 
             /* for outer loops, invariant instructions can be variant */
@@ -381,8 +369,8 @@ nir_convert_loop_to_lcssa(nir_loop *loop)
    state->skip_invariants = false;
    state->skip_bool_invariants = false;
 
-   nir_foreach_block_in_cf_node(block, &loop->cf_node) {
-      nir_foreach_instr(instr, block)
+   nir_foreach_block_in_cf_node_reverse(block, &loop->cf_node) {
+      nir_foreach_instr_reverse_safe(instr, block)
          nir_foreach_def(instr, convert_loop_exit_for_ssa, state);
    }
 

commit 96ab16eb7fc4b790d4e47a10e131c5de27ebed28

nir/lcssa: fix premature exit of loop after rematerializing derefs
If we have NIR such as:

32x4  %48 = @load_vulkan_descriptor (%47) (desc_type=SSBO)
32x4  %76 = deref_cast (tint_symbol_11 *)%48 (ssbo tint_symbol_11)  (ptr_stride=0, align_mul=4, align_offset=0)
32x4  %77 = deref_struct &%76->tint_symbol_10 (ssbo int)  // &((tint_symbol_11 *)%48)->tint_symbol_10

A single nir_rematerialize_deref_in_use_blocks() will rematerialize the
deref_struct and then it's deref_cast. However,
nir_foreach_instr_reverse_safe is not safe if the next iteration's
instruction is removed. This can result in the instruction loop exiting
and the load_vulkan_descriptor never having an LCSSA phi.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Fixes: 883cf626c479 ("nir/lcssa: Fix rematerializing derefs")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11770
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32225>

src/compiler/nir/nir_to_lcssa.c
@@ -281,6 +281,28 @@ setup_loop_state(lcssa_state *state, nir_loop *loop)
 }
 
 static void
+convert_block_to_lcssa(nir_block *block, lcssa_state *state)
+{
+   nir_instr *instr = nir_block_last_instr(block);
+   while (instr) {
+      /* We assume "next" will not be removed. It is used to obtain the next iteration's
+       * pointer if "instr" is removed.
+       */
+      nir_instr *next = nir_instr_next(instr);
+
+      nir_foreach_def(instr, convert_loop_exit_for_ssa, state);
+
+      /* for outer loops, invariant instructions can be variant */
+      if (state->skip_invariants && instr->pass_flags == invariant)
+         instr->pass_flags = undefined;
+
+      nir_instr *if_removed = next ? nir_instr_prev(next) : nir_block_last_instr(block);
+      bool has_instr_been_removed = if_removed != instr;
+      instr = has_instr_been_removed ? if_removed : nir_instr_prev(instr);
+   }
+}
+
+static void
 convert_to_lcssa(nir_cf_node *cf_node, lcssa_state *state)
 {
    switch (cf_node->type) {
@@ -328,15 +350,8 @@ convert_to_lcssa(nir_cf_node *cf_node, lcssa_state *state)
          }
       }
 
-      nir_foreach_block_in_cf_node_reverse(block, cf_node) {
-         nir_foreach_instr_reverse_safe(instr, block) {
-            nir_foreach_def(instr, convert_loop_exit_for_ssa, state);
-
-            /* for outer loops, invariant instructions can be variant */
-            if (state->skip_invariants && instr->pass_flags == invariant)
-               instr->pass_flags = undefined;
-         }
-      }
+      nir_foreach_block_in_cf_node_reverse(block, cf_node)
+         convert_block_to_lcssa(block, state);
 
    end:
       /* For outer loops, the LCSSA-phi should be considered not invariant */
@@ -369,10 +384,8 @@ nir_convert_loop_to_lcssa(nir_loop *loop)
    state->skip_invariants = false;
    state->skip_bool_invariants = false;
 
-   nir_foreach_block_in_cf_node_reverse(block, &loop->cf_node) {
-      nir_foreach_instr_reverse_safe(instr, block)
-         nir_foreach_def(instr, convert_loop_exit_for_ssa, state);
-   }
+   nir_foreach_block_in_cf_node_reverse(block, &loop->cf_node)
+      convert_block_to_lcssa(block, state);
 
    ralloc_free(state);
 }

src/compiler/nir/tests/control_flow_tests.cpp
@@ -130,3 +130,38 @@ TEST_F(nir_cf_test, delete_break_in_loop)
 
    nir_metadata_require(b->impl, nir_metadata_dominance);
 }
+
+/* This tests a bug in nir_convert_loop_to_lcssa, two consecutive deref
+ * instructions can cause the function's loop to exit prematurely if
+ * rematerializing the second causes the first to also be rematerialized. */
+TEST_F(nir_cf_test, lcssa_iter_safety_during_deref_remat)
+{
+   nir_variable *ubo_var_array = nir_variable_create(
+      b->shader, nir_var_mem_ubo, glsl_array_type(glsl_int_type(), 4, 0), "ubo_array");
+   nir_variable *out_var = nir_variable_create(
+      b->shader, nir_var_shader_out, glsl_int_type(), "out");
+
+   nir_loop *loop = nir_push_loop(b);
+
+   nir_def *index = nir_imm_int(b, 2);
+   nir_deref_instr *deref = nir_build_deref_var(b, ubo_var_array);
+   deref = nir_build_deref_array(b, deref, index);
+   nir_jump(b, nir_jump_break);
+
+   nir_pop_loop(b, loop);
+
+   nir_def *val = nir_load_deref(b, deref);
+   nir_store_deref(b, nir_build_deref_var(b, out_var), val, 0x1);
+
+   nir_convert_loop_to_lcssa(loop);
+
+   nir_block *block_after_loop = nir_cf_node_as_block(nir_cf_node_next(&loop->cf_node));
+
+   EXPECT_FALSE(nir_def_is_unused(index));
+   nir_foreach_use_including_if(src, index)
+      EXPECT_TRUE(!nir_src_is_if(src) && nir_src_parent_instr(src)->type == nir_instr_type_phi &&
+                  nir_src_parent_instr(src)->block == block_after_loop);
+
+   nir_validate_shader(b->shader, NULL);
+   nir_validate_ssa_dominance(b->shader, NULL);
+}