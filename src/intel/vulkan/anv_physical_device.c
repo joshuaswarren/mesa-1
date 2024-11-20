joshuaswarren/mesa-1

commit fe9c409b7914ff321b2e0fd673a677ccf9d96aaa

anv: expose VK_EXT_pipeline_protected_access
Intel's protection mechanism is descriptor based. There is nothing
going on in the shaders.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31339>

src/intel/vulkan/anv_physical_device.c
@@ -244,6 +244,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .EXT_pipeline_creation_cache_control   = true,
       .EXT_pipeline_creation_feedback        = true,
       .EXT_pipeline_library_group_handles    = rt_enabled,
+      .EXT_pipeline_protected_access         = device->has_protected_contexts,
       .EXT_pipeline_robustness               = true,
       .EXT_post_depth_coverage               = true,
       .EXT_primitives_generated_query        = true,
@@ -813,6 +814,9 @@ get_features(const struct anv_physical_device *pdevice,
 
       /* VK_KHR_dynamic_rendering_local_read */
       .dynamicRenderingLocalRead = true,
+
+      /* VK_EXT_pipeline_protected_access */
+      .pipelineProtectedAccess = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without

commit de3560235861754433179e4857e45a25079eb13d

anv: Fix feature pipelineProtectedAccess
We enable VK_EXT_pipeline_protected_access only if
anv_physical_device::has_protected_contexts. Therefore we should do the
same for vk_features::pipelineProtectedAccess.

Fixes: fe9c409 ("anv: expose VK_EXT_pipeline_protected_access")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32206>

src/intel/vulkan/anv_physical_device.c
@@ -898,7 +898,7 @@ get_features(const struct anv_physical_device *pdevice,
       .dynamicRenderingLocalRead = true,
 
       /* VK_EXT_pipeline_protected_access */
-      .pipelineProtectedAccess = true,
+      .pipelineProtectedAccess = pdevice->has_protected_contexts,
 
       /* VK_EXT_host_image_copy */
       .hostImageCopy = true,