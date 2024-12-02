joshuaswarren/mesa-1

commit bc5f0efc58f5b2ec22cc5c235e7f06617249dc2d

nvk: Advertise Vulkan 1.4
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32433>

src/nouveau/vulkan/nvk_physical_device.c
@@ -85,7 +85,7 @@ nvk_get_vk_version(const struct nv_device_info *info)
    return VK_MAKE_VERSION(1, 1, VK_HEADER_VERSION);
 #endif
 
-   return VK_MAKE_VERSION(1, 3, VK_HEADER_VERSION);
+   return VK_MAKE_VERSION(1, 4, VK_HEADER_VERSION);
 }
 
 static void
@@ -887,7 +887,7 @@ nvk_get_device_properties(const struct nvk_instance *instance,
       .independentResolve = true,
       .driverID = VK_DRIVER_ID_MESA_NVK,
       .conformanceVersion =
-         conformant ? (VkConformanceVersion) { 1, 3, 7, 3 }
+         conformant ? (VkConformanceVersion) { 1, 4, 0, 0 }
                     : (VkConformanceVersion) { 0, 0, 0, 0 },
       .denormBehaviorIndependence = VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_ALL,
       .roundingModeIndependence = VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_ALL,

src/nouveau/vulkan/meson.build
@@ -151,7 +151,7 @@ nouveau_icd = custom_target(
   output : 'nouveau_icd.@0@.json'.format(host_machine.cpu()),
   command : [
     prog_python, '@INPUT0@',
-    '--api-version', '1.3', '--xml', '@INPUT1@',
+    '--api-version', '1.4', '--xml', '@INPUT1@',
     '--lib-path', join_paths(icd_lib_path, icd_file_name),
     '--out', '@OUTPUT@',
   ],
@@ -168,7 +168,7 @@ custom_target(
   output : _dev_icdname,
   command : [
     prog_python, '@INPUT0@',
-    '--api-version', '1.3', '--xml', '@INPUT1@',
+    '--api-version', '1.4', '--xml', '@INPUT1@',
     '--lib-path', meson.current_build_dir() / icd_file_name,
     '--out', '@OUTPUT@',
   ],

src/nouveau/vulkan/nvk_instance.c
@@ -25,7 +25,7 @@ nvk_EnumerateInstanceVersion(uint32_t *pApiVersion)
 {
    uint32_t version_override = vk_get_version_override();
    *pApiVersion = version_override ? version_override :
-                  VK_MAKE_VERSION(1, 3, VK_HEADER_VERSION);
+                  VK_MAKE_VERSION(1, 4, VK_HEADER_VERSION);
 
    return VK_SUCCESS;
 }

commit fc970b2f47a23540892c0403bc5d51edcffb8b24

nvk: Only support Vulkan 1.4 on Turing+
It needs hostImageCopy which we don't have working pre-Turing yet.

Fixes: bc5f0efc58f5 ("nvk: Advertise Vulkan 1.4")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32443>

src/nouveau/vulkan/nvk_physical_device.c
@@ -85,6 +85,12 @@ nvk_get_vk_version(const struct nv_device_info *info)
    return VK_MAKE_VERSION(1, 1, VK_HEADER_VERSION);
 #endif
 
+   /* Vulkan 1.4 requires hostImageCopy which is currently only supported on
+    * Turing+.
+    */
+   if (info->cls_eng3d < TURING_A)
+      return VK_MAKE_VERSION(1, 3, VK_HEADER_VERSION);
+
    return VK_MAKE_VERSION(1, 4, VK_HEADER_VERSION);
 }
 