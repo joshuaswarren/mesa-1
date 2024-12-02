joshuaswarren/mesa-1

commit 04a1b57afb6cf1c0143d4161e71126eabf7a2ed8

dri: Unify createImage and createImageWithModifiers
There's no real reason for the two to exist separately. Nuke the old
createImage in favour of just having createImageWithModifiers.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30245>

src/gallium/frontends/dri/dri2.c
@@ -1148,12 +1148,13 @@ dri2_get_modifier_num_planes(__DRIscreen *_screen,
 }
 
 static __DRIimage *
-dri2_create_image_common(__DRIscreen *_screen,
-                         int width, int height,
-                         int format, unsigned int use,
-                         const uint64_t *modifiers,
-                         const unsigned count,
-                         void *loaderPrivate)
+dri2_create_image(__DRIscreen *_screen,
+                  int width, int height,
+                  int format,
+                  const uint64_t *modifiers,
+                  const unsigned _count,
+                  unsigned int use,
+                  void *loaderPrivate)
 {
    const struct dri2_format_mapping *map = dri2_get_mapping_by_format(format);
    struct dri_screen *screen = dri_screen(_screen);
@@ -1161,9 +1162,24 @@ dri2_create_image_common(__DRIscreen *_screen,
    __DRIimage *img;
    struct pipe_resource templ;
    unsigned tex_usage = 0;
+   unsigned count = _count;
 
    if (!map)
       return NULL;
+
+   if (count == 1 && modifiers[0] == DRM_FORMAT_MOD_INVALID) {
+      count = 0;
+      modifiers = NULL;
+   } else if (count == 1 && modifiers[0] == DRM_FORMAT_MOD_LINEAR &&
+              !pscreen->resource_create_with_modifiers) {
+      count = 0;
+      modifiers = NULL;
+      use |= __DRI_IMAGE_USE_LINEAR;
+   }
+   else if ((count > 1 || modifiers) &&
+            !pscreen->resource_create_with_modifiers) {
+      return NULL;
+   }
 
    if (pscreen->is_format_supported(pscreen, map->pipe_format, screen->target,
                                     0, 0, PIPE_BIND_RENDER_TARGET))
@@ -1233,27 +1249,6 @@ dri2_create_image_common(__DRIscreen *_screen,
    img->loader_private = loaderPrivate;
    img->screen = screen;
    return img;
-}
-
-static __DRIimage *
-dri2_create_image(__DRIscreen *_screen,
-                   int width, int height, int format,
-                   unsigned int use, void *loaderPrivate)
-{
-   return dri2_create_image_common(_screen, width, height, format, use,
-                                   NULL /* modifiers */, 0 /* count */,
-                                   loaderPrivate);
-}
-
-static __DRIimage *
-dri2_create_image_with_modifiers(__DRIscreen *dri_screen,
-                                int width, int height, int format,
-                                const uint64_t *modifiers,
-                                const unsigned count, unsigned int use,
-                                void *loaderPrivate)
-{
-   return dri2_create_image_common(dri_screen, width, height, format, use,
-                                   modifiers, count, loaderPrivate);
 }
 
 static bool
@@ -1932,7 +1927,6 @@ static const __DRIimageExtension dri2ImageExtensionTempl = {
     .getCapabilities              = dri2_get_capabilities,
     .mapImage                     = dri2_map_image,
     .unmapImage                   = dri2_unmap_image,
-    .createImageWithModifiers     = NULL,
     .queryDmaBufFormats           = NULL,
     .queryDmaBufModifiers         = NULL,
     .queryDmaBufFormatModifierAttribs = NULL,
@@ -1957,7 +1951,6 @@ const __DRIimageExtension driVkImageExtension = {
     .getCapabilities              = dri2_get_capabilities,
     .mapImage                     = dri2_map_image,
     .unmapImage                   = dri2_unmap_image,
-    .createImageWithModifiers     = dri2_create_image_with_modifiers,
     .queryDmaBufFormats           = dri2_query_dma_buf_formats,
     .queryDmaBufModifiers         = dri2_query_dma_buf_modifiers,
     .queryDmaBufFormatModifierAttribs = dri2_query_dma_buf_format_modifier_attribs,
@@ -2209,10 +2202,6 @@ dri2_init_screen_extensions(struct dri_screen *screen,
    nExt = &screen->screen_extensions[ARRAY_SIZE(dri_screen_extensions_base)];
 
    screen->image_extension = dri2ImageExtensionTempl;
-   if (pscreen->resource_create_with_modifiers) {
-      screen->image_extension.createImageWithModifiers =
-         dri2_create_image_with_modifiers;
-   }
 
    if (pscreen->get_param(pscreen, PIPE_CAP_NATIVE_FENCE_FD)) {
       screen->image_extension.setInFenceFd = dri2_set_in_fence_fd;

src/egl/drivers/dri2/egl_dri2.c
@@ -2885,7 +2885,7 @@ dri2_create_drm_image_mesa(_EGLDisplay *disp, const EGLint *attr_list)
 
    dri2_img->dri_image =
       dri2_dpy->image->createImage(dri2_dpy->dri_screen_render_gpu, attrs.Width,
-                                   attrs.Height, format, dri_use, dri2_img);
+                                   attrs.Height, format, NULL, 0, dri_use, dri2_img);
    if (dri2_img->dri_image == NULL) {
       free(dri2_img);
       _eglError(EGL_BAD_ALLOC, "dri2_create_drm_image_mesa");

src/egl/drivers/dri2/platform_android.c
@@ -492,7 +492,7 @@ get_front_bo(struct dri2_egl_surface *dri2_surf, unsigned int format)
    } else if (dri2_surf->base.Type == EGL_PBUFFER_BIT) {
       dri2_surf->dri_image_front = dri2_dpy->image->createImage(
          dri2_dpy->dri_screen_render_gpu, dri2_surf->base.Width,
-         dri2_surf->base.Height, format, 0, NULL);
+         dri2_surf->base.Height, format, NULL, 0, 0, NULL);
       if (!dri2_surf->dri_image_front) {
          _eglLog(_EGL_WARNING, "dri2_image_front allocation failed");
          return -1;

src/egl/drivers/dri2/platform_device.c
@@ -50,7 +50,7 @@ device_alloc_image(struct dri2_egl_display *dri2_dpy,
 {
    return dri2_dpy->image->createImage(
       dri2_dpy->dri_screen_render_gpu, dri2_surf->base.Width,
-      dri2_surf->base.Height, dri2_surf->visual, 0, NULL);
+      dri2_surf->base.Height, dri2_surf->visual, NULL, 0, 0, NULL);
 }
 
 static void

src/egl/drivers/dri2/platform_surfaceless.c
@@ -45,7 +45,7 @@ surfaceless_alloc_image(struct dri2_egl_display *dri2_dpy,
 {
    return dri2_dpy->image->createImage(
       dri2_dpy->dri_screen_render_gpu, dri2_surf->base.Width,
-      dri2_surf->base.Height, dri2_surf->visual, 0, NULL);
+      dri2_surf->base.Height, dri2_surf->visual, NULL, 0, 0, NULL);
 }
 
 static void

src/gallium/include/mesa_interface.h
@@ -1429,11 +1429,6 @@ struct __DRIimageExtensionRec {
 
     void (*destroyImage)(__DRIimage *image);
 
-    __DRIimage *(*createImage)(__DRIscreen *screen,
-			       int width, int height, int format,
-			       unsigned int use,
-			       void *loaderPrivate);
-
    unsigned char (*queryImage)(__DRIimage *image, int attrib, int *value);
 
    /**
@@ -1635,6 +1630,10 @@ struct __DRIimageExtensionRec {
     * Creates an image with implementation's favorite modifiers and the
     * provided usage flags.
     *
+    * Passing either zero modifiers, or a modifier list consisting only
+    * of DRM_FORMAT_MOD_INVALID, allows the implementation to select a
+    * layout with implicit modifiers.
+    *
     * The created image should be destroyed with destroyImage().
     *
     * Returns the new DRIimage. The chosen modifier can be obtained later on
@@ -1644,12 +1643,12 @@ struct __DRIimageExtensionRec {
     *
     * \since 19
     */
-   __DRIimage *(*createImageWithModifiers)(__DRIscreen *screen,
-                                           int width, int height, int format,
-                                           const uint64_t *modifiers,
-                                           const unsigned int modifier_count,
-                                           unsigned int use,
-                                           void *loaderPrivate);
+   __DRIimage *(*createImage)(__DRIscreen *screen,
+                              int width, int height, int format,
+                              const uint64_t *modifiers,
+                              const unsigned int modifier_count,
+                              unsigned int use,
+                              void *loaderPrivate);
 
    /**
     * Set an in-fence-fd on the image.  If a fence-fd is already set

src/gbm/backends/dri/gbm_dri.c
@@ -972,11 +972,6 @@ gbm_dri_bo_create(struct gbm_device *gbm,
    /* Gallium drivers requires shared in order to get the handle/stride */
    dri_use |= __DRI_IMAGE_USE_SHARE;
 
-   if (modifiers && !dri->image->createImageWithModifiers) {
-      errno = ENOSYS;
-      goto failed;
-   }
-
    /* If the driver supports fixed-rate compression, filter the acceptable
     * modifiers by the compression rate. */
    if (modifiers && dri->image->queryCompressionModifiers) {
@@ -1154,13 +1149,7 @@ gbm_dri_surface_create(struct gbm_device *gbm,
 		       uint32_t format, uint32_t flags,
                        const uint64_t *modifiers, const unsigned count)
 {
-   struct gbm_dri_device *dri = gbm_dri_device(gbm);
    struct gbm_dri_surface *surf;
-
-   if (modifiers && !dri->image->createImageWithModifiers) {
-      errno = ENOSYS;
-      return NULL;
-   }
 
    if (count)
       assert(modifiers);

src/loader/loader_dri3_helper.c
@@ -1462,8 +1462,7 @@ dri3_alloc_render_buffer(struct loader_dri3_drawable *draw, unsigned int format,
    if (draw->dri_screen_render_gpu == draw->dri_screen_display_gpu) {
 #ifdef HAVE_DRI3_MODIFIERS
       if (draw->multiplanes_available &&
-          draw->ext->image->queryDmaBufModifiers &&
-          draw->ext->image->createImageWithModifiers) {
+          draw->ext->image->queryDmaBufModifiers) {
          xcb_dri3_get_supported_modifiers_cookie_t mod_cookie;
          xcb_dri3_get_supported_modifiers_reply_t *mod_reply;
          xcb_generic_error_t *error = NULL;
@@ -1528,11 +1527,10 @@ dri3_alloc_render_buffer(struct loader_dri3_drawable *draw, unsigned int format,
       if (!buffer->image)
          goto no_image;
    } else {
-      buffer->image = draw->ext->image->createImage(draw->dri_screen_render_gpu,
-                                                    width, height,
-                                                    format,
-                                                    0,
-                                                    buffer);
+      buffer->image =
+         draw->ext->image->createImage(draw->dri_screen_render_gpu,
+                                       width, height, format,
+                                       NULL, 0, 0, buffer);
 
       if (!buffer->image)
          goto no_image;
@@ -1546,6 +1544,7 @@ dri3_alloc_render_buffer(struct loader_dri3_drawable *draw, unsigned int format,
            draw->ext->image->createImage(draw->dri_screen_display_gpu,
                                          width, height,
                                          dri3_linear_format_for_format(draw, format),
+                                         NULL, 0,
                                          __DRI_IMAGE_USE_SHARE |
                                          __DRI_IMAGE_USE_LINEAR |
                                          __DRI_IMAGE_USE_BACKBUFFER |
@@ -1557,14 +1556,15 @@ dri3_alloc_render_buffer(struct loader_dri3_drawable *draw, unsigned int format,
       if (!pixmap_buffer) {
          buffer->linear_buffer =
            draw->ext->image->createImage(draw->dri_screen_render_gpu,
-                                         width, height,
-                                         dri3_linear_format_for_format(draw, format),
-                                         __DRI_IMAGE_USE_SHARE |
-                                         __DRI_IMAGE_USE_LINEAR |
-                                         __DRI_IMAGE_USE_BACKBUFFER |
-                                         __DRI_IMAGE_USE_SCANOUT |
-                                         __DRI_IMAGE_USE_PRIME_BUFFER,
-                                         buffer);
+                                        width, height,
+                                        dri3_linear_format_for_format(draw, format),
+                                        NULL, 0,
+                                        __DRI_IMAGE_USE_SHARE |
+                                        __DRI_IMAGE_USE_LINEAR |
+                                        __DRI_IMAGE_USE_BACKBUFFER |
+                                        __DRI_IMAGE_USE_SCANOUT |
+                                        __DRI_IMAGE_USE_PRIME_BUFFER,
+                                        buffer);
 
          pixmap_buffer = buffer->linear_buffer;
          if (!buffer->linear_buffer) {

commit ad134b8faaf9e50f469f0af590353b738fc3ee15

dri: revert INVALID modifier special-casing
Commit 04a1b57afb6c ("dri: Unify createImage and createImageWithModifiers")
has introduced new behavior for drivers which don't support explicit
format modifiers. Before this commit, INVALID was not special-cased
and any call to dri_create_image() with one or more modifiers returned
NULL. After this commit, INVALID gained a special meaning: it indicates
that the implicit modifier is accepted by the caller. This is surprising
and is an API break.

This causes further API breaks: for instance, before this commit a BO
created via gbm_bo_create_with_modifiers() was guaranteed to always
return a non-INVALID modifier in gbm_bo_get_modifier().

This is inconsistent with gbm_dri_surface_create(): that function
treats INVALID as a bad entry in the modifier list, and fails if
it's the only acceptable modifier.

Additionally, drivers don't special-case INVALID and just ignore it
if they see it in a modifier list. This causes more inconsistencies.
For instance, let's say that a library user passes the modifier list
{ INVALID, FOO } to GBM. If a driver supports explicit modifiers and
doesn't support FOO for scanout, it'll return NULL. If a driver
doesn't support explicit modifiers, the current logic would return
a non-NULL BO with an INVALID modifier. This discrepency makes it
harder to reason about the system: half of the API ignores INVALID,
while the other half assumes INVALID indicates an implicit modifier.

To fix these issues, revert to the behavior before the commit, and
require use of the dedicated API without supplying any modifier for
implicit modifiers.

Signed-off-by: Simon Ser <contact@emersion.fr>
Fixes: 04a1b57afb6c ("dri: Unify createImage and createImageWithModifiers")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32396>

src/gallium/frontends/dri/dri2.c
@@ -1070,26 +1070,8 @@ dri_create_image(struct dri_screen *screen,
    if (!map)
       return NULL;
 
-   if (count == 1 && modifiers[0] == DRM_FORMAT_MOD_INVALID) {
-      count = 0;
-      modifiers = NULL;
-   }
-
-   if (!pscreen->resource_create_with_modifiers && count > 0) {
-      bool invalid_ok = false;
-
-      for (unsigned i = 0; i < _count; i++) {
-         if (modifiers[i] == DRM_FORMAT_MOD_INVALID)
-            invalid_ok = true;
-      }
-
-      if (invalid_ok) {
-         count = 0;
-         modifiers = NULL;
-      } else {
-         return NULL;
-      }
-   }
+   if (!pscreen->resource_create_with_modifiers && count > 0)
+      return NULL;
 
    if (pscreen->is_format_supported(pscreen, map->pipe_format, screen->target,
                                     0, 0, PIPE_BIND_RENDER_TARGET))