joshuaswarren/mesa-1

commit 4d3c92b1480e302ad41124bb5f029685d15de12b

zink: filled quad emulation gs generation function
This change adds a function to generate a geometry shader that will take a quad
as an input (passed as a line strip with adjacency because quads are not
available as an input primitives for GS) and generate a triangle strip.

This can be used to emulate the quad primitive.

Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21238>

src/gallium/drivers/zink/zink_compiler.c
@@ -901,6 +901,87 @@ lower_64bit_pack(nir_shader *shader)
                                        nir_metadata_block_index | nir_metadata_dominance, NULL);
 }
 
+nir_shader *
+zink_create_quads_emulation_gs(const nir_shader_compiler_options *options,
+                               const nir_shader *prev_stage)
+{
+   nir_builder b = nir_builder_init_simple_shader(MESA_SHADER_GEOMETRY,
+                                                  options,
+                                                  "filled quad gs");
+
+   nir_shader *nir = b.shader;
+   nir->info.gs.input_primitive = SHADER_PRIM_LINES_ADJACENCY;
+   nir->info.gs.output_primitive = SHADER_PRIM_TRIANGLE_STRIP;
+   nir->info.gs.vertices_in = 4;
+   nir->info.gs.vertices_out = 4;
+   nir->info.gs.invocations = 1;
+   nir->info.gs.active_stream_mask = 1;
+
+   nir_variable *in_vars[VARYING_SLOT_MAX];
+   nir_variable *out_vars[VARYING_SLOT_MAX];
+   unsigned num_vars = 0;
+
+   /* Create input/output variables. */
+   nir_foreach_shader_out_variable(var, prev_stage) {
+      assert(!var->data.patch);
+
+      char name[100];
+      if (var->name)
+         snprintf(name, sizeof(name), "in_%s", var->name);
+      else
+         snprintf(name, sizeof(name), "in_%d", var->data.driver_location);
+
+      nir_variable *in = nir_variable_create(nir, nir_var_shader_in,
+                                             glsl_array_type(var->type,
+                                                             4,
+                                                             false),
+                                             name);
+      in->data.location = var->data.location;
+      in->data.location_frac = var->data.location_frac;
+      in->data.driver_location = var->data.driver_location;
+      in->data.interpolation = var->data.interpolation;
+      in->data.compact = var->data.compact;
+
+      if (var->name)
+         snprintf(name, sizeof(name), "out_%s", var->name);
+      else
+         snprintf(name, sizeof(name), "out_%d", var->data.driver_location);
+
+      nir_variable *out = nir_variable_create(nir, nir_var_shader_out,
+                                              var->type, name);
+      out->data.location = var->data.location;
+      out->data.location_frac = var->data.location_frac;
+      out->data.driver_location = var->data.driver_location;
+      out->data.interpolation = var->data.interpolation;
+      out->data.compact = var->data.compact;
+
+      in_vars[num_vars] = in;
+      out_vars[num_vars++] = out;
+   }
+
+   for (unsigned i = 0; i < 4; ++i) {
+      /* swap indices 2 and 3 */
+      int idx = i == 2 ? 3 : (i == 3 ? 2 : i);
+      /* Copy inputs to outputs. */
+      for (unsigned j = 0; j < num_vars; ++j) {
+         if (in_vars[j]->data.location == VARYING_SLOT_EDGE) {
+            continue;
+         }
+         /* no need to use copy_var to save a lower pass */
+         nir_ssa_def *value = nir_load_array_var_imm(&b, in_vars[j], idx);
+         nir_store_var(&b, out_vars[j], value,
+                       (1u << value->num_components) - 1);
+      }
+      nir_emit_vertex(&b, 0);
+   }
+
+   nir_end_primitive(&b, 0);
+   nir_shader_gather_info(nir, nir_shader_get_entrypoint(nir));
+   nir_validate_shader(nir, "in zink_create_quads_emulation_gs");
+
+   return nir;
+}
+
 void
 zink_screen_init_compiler(struct zink_screen *screen)
 {

src/gallium/drivers/zink/zink_compiler.h
@@ -54,6 +54,9 @@ zink_get_compiler_options(struct pipe_screen *screen,
 struct nir_shader *
 zink_tgsi_to_nir(struct pipe_screen *screen, const struct tgsi_token *tokens);
 
+nir_shader*
+zink_create_quads_emulation_gs(const nir_shader_compiler_options *options,
+                               const nir_shader *prev_stage);
 
 void
 zink_screen_init_compiler(struct zink_screen *screen);

commit b17b32217cab6ddd4f7d12eda1222118aa36ba05

zink: fix gl_PrimitiveID reads with quads
Zink emulates quads with a GS, which imposes requirements for gl_PrimitiveID.
Handle them here. Previously Zink went out of spec.

Fixes spec@glsl-1.50@execution@primitive-id-no-gs-quads and
spec@glsl-1.50@execution@primitive-id-no-gs-quad-strip.

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Antonino Maniscalco <antomani103@gmail.com>
Fixes: 4d3c92b1480 ("zink: filled quad emulation gs generation function")
Closes: #12214
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32397>

src/gallium/drivers/zink/zink_compiler.c
@@ -22,6 +22,7 @@
  */
 
 #include "nir_opcodes.h"
+#include "shader_enums.h"
 #include "zink_context.h"
 #include "zink_compiler.h"
 #include "zink_descriptors.h"
@@ -1096,6 +1097,8 @@ zink_create_quads_emulation_gs(const nir_shader_compiler_options *options,
    /* Create input/output variables. */
    nir_foreach_shader_out_variable(var, prev_stage) {
       assert(!var->data.patch);
+      assert(var->data.location != VARYING_SLOT_PRIMITIVE_ID &&
+            "not a VS output");
 
       /* input vars can't be created for those */
       if (var->data.location == VARYING_SLOT_LAYER ||
@@ -1132,6 +1135,30 @@ zink_create_quads_emulation_gs(const nir_shader_compiler_options *options,
       out_vars[num_vars++] = out;
    }
 
+   /* When a geometry shader is not used, a fragment shader may read primitive
+    * ID and get an implicit value without the vertex shader writing an ID. This
+    * case needs to work even when we inject a GS internally.
+    *
+    * However, if a geometry shader precedes a fragment shader that reads
+    * primitive ID, Vulkan requires that the geometry shader write primitive ID.
+    * To handle this case correctly, we must write primitive ID, copying the
+    * fixed-function gl_PrimitiveIDIn input which matches what the fragment
+    * shader will expect.
+    *
+    * If the fragment shader doesn't read primitive ID, this copy will likely be
+    * optimized out at link-time by the Vulkan driver. Unless this is
+    * non-monolithic -- in which case we don't know whether the fragment shader
+    * will read primitive ID either. In both cases, the right thing for Zink
+    * to do is copy primitive ID unconditionally.
+    */
+   in_vars[num_vars] = nir_create_variable_with_location(
+         nir, nir_var_shader_in, VARYING_SLOT_PRIMITIVE_ID, glsl_int_type());
+
+   out_vars[num_vars] = nir_create_variable_with_location(
+         nir, nir_var_shader_out, VARYING_SLOT_PRIMITIVE_ID, glsl_int_type());
+
+   num_vars++;
+
    int mapping_first[] = {0, 1, 2, 0, 2, 3};
    int mapping_last[] = {0, 1, 3, 1, 2, 3};
    nir_def *last_pv_vert_def = nir_load_provoking_last(&b);
@@ -1146,7 +1173,12 @@ zink_create_quads_emulation_gs(const nir_shader_compiler_options *options,
          if (in_vars[j]->data.location == VARYING_SLOT_EDGE) {
             continue;
          }
-         nir_deref_instr *in_value = nir_build_deref_array(&b, nir_build_deref_var(&b, in_vars[j]), idx);
+
+         /* gl_PrimitiveIDIn is not arrayed, all other inputs are */
+         nir_deref_instr *in_value = nir_build_deref_var(&b, in_vars[j]);
+         if (in_vars[j]->data.location != VARYING_SLOT_PRIMITIVE_ID)
+            in_value = nir_build_deref_array(&b, in_value, idx);
+
          copy_vars(&b, nir_build_deref_var(&b, out_vars[j]), in_value);
       }
       nir_emit_vertex(&b, 0);