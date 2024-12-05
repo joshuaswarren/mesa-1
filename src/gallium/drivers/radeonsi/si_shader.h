joshuaswarren/mesa-1

commit 7195fb5fd4a5412e802d86bbf8be81dd635ef485

radeonsi: enable NGG culling for non-monolithic TES and GS
It doesn't enable back face culling and small line culling. Those can only
be enabled for monolithic shaders. It only enables view culling and small
triangle culling.

Doing this has these minor advantages:

1. We can enable at least some culling immediately instead of when the first
   monolithic shader finishes compilation.

2. If back face culling and clip planes are disabled, we no longer compile
   monolithic TES and GS shader variants to get only view culling and small
   triangle culling.

3. shader-db will show culling code changes for TES and GS.

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32257>

src/gallium/drivers/radeonsi/si_shader.h
@@ -1152,7 +1152,15 @@ static inline bool si_shader_uses_discard(struct si_shader *shader)
 
 static inline bool si_shader_culling_enabled(struct si_shader *shader)
 {
-   return !!shader->key.ge.opt.ngg_culling;
+   if (shader->key.ge.opt.ngg_culling)
+      return true;
+
+   unsigned output_prim = si_get_output_prim_simplified(shader->selector, &shader->key);
+
+   /* This enables NGG culling for non-monolithic TES and GS. */
+   return shader->key.ge.as_ngg && !shader->key.ge.as_es &&
+          shader->selector->ngg_cull_vert_threshold == 0 &&
+          (output_prim == MESA_PRIM_TRIANGLES || output_prim == MESA_PRIM_LINES);
 }
 
 #ifdef __cplusplus

src/gallium/drivers/radeonsi/si_state_draw.cpp
@@ -2255,12 +2255,12 @@ static void si_draw(struct pipe_context *ctx,
 
          if (util_prim_is_lines(sctx->current_rast_prim)) {
             /* Overwrite it to mask out face cull flags. */
-            ngg_culling = rs->ngg_cull_flags_lines;
-            ngg_culling |= SI_NGG_CULL_VS_LINES;
+            ngg_culling = rs->ngg_cull_flags_lines |
+                          (!HAS_TESS && !HAS_GS ? SI_NGG_CULL_VS_LINES : 0);
          } else {
-            ngg_culling = sctx->viewport0_y_inverted ? rs->ngg_cull_flags_tris_y_inverted :
-                                                       rs->ngg_cull_flags_tris;
-            ngg_culling |= SI_NGG_CULL_VS_TRIANGLES;
+            ngg_culling = (sctx->viewport0_y_inverted ? rs->ngg_cull_flags_tris_y_inverted :
+                                                        rs->ngg_cull_flags_tris) |
+                          (!HAS_TESS && !HAS_GS ? SI_NGG_CULL_VS_TRIANGLES : 0);
          }
 
          if (ngg_culling != old_ngg_culling) {

commit 76c3d47247a6e4336f3f6a357731a10c340a52a0

radeonsi: try to fix Navi14 regression in debug builds
Assertion failure:
    ../src/gallium/drivers/radeonsi/si_state_shaders.cpp:1369: unsigned int si_get_input_prim(const
    si_shader_selector*, const si_shader_key*, bool): Assertion `gs->stage == MESA_SHADER_VERTEX' failed.

Fixes: 7195fb5fd4a541 ("radeonsi: enable NGG culling for non-monolithic TES and GS")

Tested-by: Michel Dänzer <mdaenzer@redhat.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32288>

src/gallium/drivers/radeonsi/si_shader.h
@@ -1154,14 +1154,19 @@ static inline bool si_shader_uses_discard(struct si_shader *shader)
 
 static inline bool si_shader_culling_enabled(struct si_shader *shader)
 {
+   /* Legacy VS/TES/GS and ES don't cull in the shader. */
+   if (!shader->key.ge.as_ngg || shader->key.ge.as_es) {
+      assert(!shader->key.ge.opt.ngg_culling);
+      return false;
+   }
+
    if (shader->key.ge.opt.ngg_culling)
       return true;
 
    unsigned output_prim = si_get_output_prim_simplified(shader->selector, &shader->key);
 
    /* This enables NGG culling for non-monolithic TES and GS. */
-   return shader->key.ge.as_ngg && !shader->key.ge.as_es &&
-          shader->selector->ngg_cull_vert_threshold == 0 &&
+   return shader->selector->ngg_cull_vert_threshold == 0 &&
           (output_prim == MESA_PRIM_TRIANGLES || output_prim == MESA_PRIM_LINES);
 }
 