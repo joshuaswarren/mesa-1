joshuaswarren/mesa-1

commit 2c2ebe7257ec65d96849186a133e49364139c404

panvk: update feature support
Turn on `imageCubeArray` and `fragmentStoresAndAtomics`, which we
already support (the latter only on v10 and later).

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32464>

src/panfrost/vulkan/panvk_physical_device.c
@@ -272,6 +272,7 @@ get_features(const struct panvk_physical_device *device,
       .depthBiasClamp = true,
       .robustBufferAccess = true,
       .fullDrawIndexUint32 = true,
+      .imageCubeArray = true,
       .independentBlend = true,
       .sampleRateShading = true,
       .logicOp = true,
@@ -281,6 +282,7 @@ get_features(const struct panvk_physical_device *device,
       .samplerAnisotropy = true,
       .textureCompressionETC2 = true,
       .textureCompressionASTC_LDR = true,
+      .fragmentStoresAndAtomics = arch >= 10,
       .shaderUniformBufferArrayDynamicIndexing = true,
       .shaderSampledImageArrayDynamicIndexing = true,
       .shaderStorageBufferArrayDynamicIndexing = true,

commit 75ba0222e08c12a0f42b89155b4c4fad6f7f1719

panvk: disable imageCubeArray on bifrost
We haven't wired this up correctly on Bifrost, so let's make this V10
only for now.

Fixes: 2c2ebe7257e ("panvk: update feature support")
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32610>

src/panfrost/vulkan/panvk_physical_device.c
@@ -272,7 +272,7 @@ get_features(const struct panvk_physical_device *device,
       .depthBiasClamp = true,
       .robustBufferAccess = true,
       .fullDrawIndexUint32 = true,
-      .imageCubeArray = true,
+      .imageCubeArray = arch >= 10,
       .independentBlend = true,
       .sampleRateShading = true,
       .logicOp = true,