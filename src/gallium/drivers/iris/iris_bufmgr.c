joshuaswarren/mesa-1

commit da935cc69dc0e388436f81457c235b87f8a2a868

iris: Align fresh BO allocations to 2MB in size
This should allow us to use 64K pages in more cases, and since the
suballocator is typically used for BOs smaller than 2MB, it probably
isn't going to waste a horrendous amount of memory.

Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25447>

src/gallium/drivers/iris/iris_bufmgr.c
@@ -1067,6 +1067,15 @@ alloc_fresh_bo(struct iris_bufmgr *bufmgr, uint64_t bo_size, unsigned flags)
    if (!bo)
       return NULL;
 
+   /* Try to allocate memory in multiples of 2MB, as this allows us to use
+    * 64K pages rather than the less-efficient 4K pages.  Most BOs smaller
+    * than 64MB should hit the BO cache or slab allocations anyway, so this
+    * shouldn't waste too much memory.  We do exclude small (< 1MB) sizes to
+    * be defensive in case any of those bypass the caches and end up here.
+    */
+   if (bo_size >= 1024 * 1024)
+      bo_size = ALIGN(bo_size, 2 * 1024 * 1024);
+
    bo->real.heap = flags_to_heap(bufmgr, flags);
 
    const struct intel_memory_class_instance *regions[2];

commit 0443e9080ea36bc51a36eab2831947bd4a21f361

iris: Tune the BO cache's bucket sizes
With the introduction of the slab allocator, most of our small
allocations now hit that rather than directly hitting the bucket
cache.  Those now show up as 2MB slab allocations from the cache's
perspective.  So, we don't need quite as many buckets.  (Note that
only allocations in IRIS_MEMZONE_OTHER are suballocated today.)

Previously, we had 55 buckets, going from 4KB to 112MB, with sizes
N, N+1/4, N+1/2, N+3/4 for a series of power-of-two N's.

This patch prunes it down to 25 buckets:

   - 4K-4MB => power-of-two sizes only
   - 6MB    => a one-off bucket to reduce waste between 4MB and 8MB
   - 8MB+   => the usual N, N+1/4, N+1/2, N+3/4 system
   - 64MB   => the largest bucket size

In particular, this eliminates the 1.75MB, 2.5MB, 3MB, 3.5MB, and 7MB
buckets in favor of multiples of 2MB.  Allocating multiples of 2MB is
preferable because it allows the kernel to allocate 64KB pages rather
than being stuck using inefficient 4K pages.  And, the amount of waste
from bumping to the next multiple of 2MB isn't huge in that range of
sizes.  We also eliminate buckets larger than 64MB because they're
rarely used, and also the amount of waste from rounding up to the
80/96/112MB buckets can get pretty large.

Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Fixes: da935cc69dc0 ("iris: Align fresh BO allocations to 2MB in size")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10219
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32635>

src/gallium/drivers/iris/iris_bufmgr.c
@@ -186,7 +186,7 @@ struct iris_slab {
    struct iris_bo *entries;
 };
 
-#define BUCKET_ARRAY_SIZE (14 * 4)
+#define BUCKET_ARRAY_SIZE 25
 
 struct iris_bucket_cache {
    struct bo_cache_bucket bucket[BUCKET_ARRAY_SIZE];
@@ -296,33 +296,29 @@ bucket_for_size(struct iris_bufmgr *bufmgr, uint64_t size,
        (flags & (BO_ALLOC_SHARED | BO_ALLOC_SCANOUT)))
       return NULL;
 
-   /* Calculating the pages and rounding up to the page size. */
-   const unsigned pages = (size + PAGE_SIZE - 1) / PAGE_SIZE;
+   const unsigned _4MB = 4 * 1024 * 1024;
+   const unsigned _6MB = 6 * 1024 * 1024;
+   const unsigned _8MB = 8 * 1024 * 1024;
+   const unsigned _64MB = 64 * 1024 * 1024;
+   unsigned index;
 
-   /* Row  Bucket sizes    clz((x-1) | 3)   Row    Column
-    *        in pages                      stride   size
-    *   0:   1  2  3  4 -> 30 30 30 30        4       1
-    *   1:   5  6  7  8 -> 29 29 29 29        4       1
-    *   2:  10 12 14 16 -> 28 28 28 28        8       2
-    *   3:  20 24 28 32 -> 27 27 27 27       16       4
-    */
-   const unsigned row = 30 - __builtin_clz((pages - 1) | 3);
-   const unsigned row_max_pages = 4 << row;
-
-   /* The '& ~2' is the special case for row 1. In row 1, max pages /
-    * 2 is 2, but the previous row maximum is zero (because there is
-    * no previous row). All row maximum sizes are power of 2, so that
-    * is the only case where that bit will be set.
-    */
-   const unsigned prev_row_max_pages = (row_max_pages / 2) & ~2;
-   int col_size_log2 = row - 1;
-   col_size_log2 += (col_size_log2 < 0);
-
-   const unsigned col = (pages - prev_row_max_pages +
-                        ((1 << col_size_log2) - 1)) >> col_size_log2;
-
-   /* Calculating the index based on the row and column. */
-   const unsigned index = (row * 4) + (col - 1);
+   if (size <= 4096) {
+      index = 0;
+   } else if (size <= _4MB) {
+      index = util_logbase2_ceil(size) - 12;
+   } else if (size <= _6MB) {
+      index = 11;
+   } else if (size <= _8MB) {
+      index = 12;
+   } else if (size <= _64MB) {
+      const unsigned power = util_logbase2(size);
+      const unsigned base_size = 1u << power;
+      const unsigned quarter_size = base_size / 4;
+      const unsigned quarter = DIV_ROUND_UP(size - base_size, quarter_size);
+      index = 12 + (power - 23) * 4 + quarter;
+   } else {
+      return NULL;
+   }
 
    return (index < cache->num_buckets) ? &cache->bucket[index] : NULL;
 }
@@ -2172,6 +2168,8 @@ add_bucket(struct iris_bufmgr *bufmgr, int size, enum iris_heap heap)
    struct iris_bucket_cache *cache = &bufmgr->bucket_cache[heap];
    unsigned int i = cache->num_buckets++;
 
+   assert(i < BUCKET_ARRAY_SIZE);
+
    list_inithead(&cache->bucket[i].head);
    cache->bucket[i].size = size;
 
@@ -2183,28 +2181,27 @@ add_bucket(struct iris_bufmgr *bufmgr, int size, enum iris_heap heap)
 static void
 init_cache_buckets(struct iris_bufmgr *bufmgr, enum iris_heap heap)
 {
-   uint64_t size, cache_max_size = 64 * 1024 * 1024;
+   const unsigned _6MB = 6 * 1024 * 1024;
+   const unsigned _8MB = 8 * 1024 * 1024;
+   const unsigned _64MB = 64 * 1024 * 1024;
 
-   /* OK, so power of two buckets was too wasteful of memory.
-    * Give 3 other sizes between each power of two, to hopefully
-    * cover things accurately enough.  (The alternative is
-    * probably to just go for exact matching of sizes, and assume
-    * that for things like composited window resize the tiled
-    * width/height alignment and rounding of sizes to pages will
-    * get us useful cache hit rates anyway)
-    */
-   add_bucket(bufmgr, PAGE_SIZE,     heap);
-   add_bucket(bufmgr, PAGE_SIZE * 2, heap);
-   add_bucket(bufmgr, PAGE_SIZE * 3, heap);
-
-   /* Initialize the linked lists for BO reuse cache. */
-   for (size = 4 * PAGE_SIZE; size <= cache_max_size; size *= 2) {
+   /* power-of-two buckets from 4K to 4MB */
+   for (uint64_t size = 4096; size < _8MB; size *= 2)
       add_bucket(bufmgr, size, heap);
 
+   /* 6MB */
+   add_bucket(bufmgr, _6MB, heap);
+
+   /* 8MB+: three sizes between each power of two to reduce waste */
+   for (uint64_t size = _8MB; size < _64MB; size *= 2) {
+      add_bucket(bufmgr, size, heap);
       add_bucket(bufmgr, size + size * 1 / 4, heap);
       add_bucket(bufmgr, size + size * 2 / 4, heap);
       add_bucket(bufmgr, size + size * 3 / 4, heap);
    }
+
+   /* 64MB */
+   add_bucket(bufmgr, _64MB, heap);
 }
 
 static struct intel_buffer *