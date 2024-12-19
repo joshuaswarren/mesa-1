joshuaswarren/mesa-1

commit 2c2ebe7257ec65d96849186a133e49364139c404

panvk: update feature support
Turn on `imageCubeArray` and `fragmentStoresAndAtomics`, which we
already support (the latter only on v10 and later).

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32464>

src/panfrost/vulkan/panvk_physical_device.c
@@ -272,6 +272,7 @@ get_features(const struct panvk_physical_device *device,
       .depthBiasClamp = true,
       .robustBufferAccess = true,
       .fullDrawIndexUint32 = true,
+      .imageCubeArray = true,
       .independentBlend = true,
       .sampleRateShading = true,
       .logicOp = true,
@@ -281,6 +282,7 @@ get_features(const struct panvk_physical_device *device,
       .samplerAnisotropy = true,
       .textureCompressionETC2 = true,
       .textureCompressionASTC_LDR = true,
+      .fragmentStoresAndAtomics = arch >= 10,
       .shaderUniformBufferArrayDynamicIndexing = true,
       .shaderSampledImageArrayDynamicIndexing = true,
       .shaderStorageBufferArrayDynamicIndexing = true,

commit d24f84b5f7efdd6e28da4c68b1d66979465a3bcd

panvk: drop fragmentStoresAndAtomics support for now
This is failing a bunch of tests on CI. In particular, it seems some
details are wrong WRT robust image access and atomics. There's some
lowering in place for that, but a quick test didn't fix it for me, so
let's walk this back and try again later.

Fixes: 2c2ebe7257e ("panvk: update feature support")
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32625>

src/panfrost/vulkan/panvk_physical_device.c
@@ -275,7 +275,6 @@ get_features(const struct panvk_physical_device *device,
       .samplerAnisotropy = true,
       .textureCompressionETC2 = true,
       .textureCompressionASTC_LDR = true,
-      .fragmentStoresAndAtomics = arch >= 10,
       .shaderUniformBufferArrayDynamicIndexing = true,
       .shaderSampledImageArrayDynamicIndexing = true,
       .shaderStorageBufferArrayDynamicIndexing = true,