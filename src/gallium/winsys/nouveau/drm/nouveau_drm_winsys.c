joshuaswarren/mesa-1

commit 2a2cfaec34f2c3f8c55b8f98116afee0519df00d

nouveau: import libdrm_nouveau
Most of the code is copied as is, but there are a couple of changes:
- drop pre nvif code
- nouveau_list -> list_head
- pthread_mutex_t -> simple_mtx_t
- nvif_unwrap removed
- no assignments in if statements
- nouveau_object_mthd -> nouveau_device_info
- reworked object allocation UAPI calls

Most of the reworked code was copied from NVK or a previous version of it

Signed-off-by: Karol Herbst <kherbst@redhat.com>
Acked-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27853>

src/gallium/winsys/nouveau/drm/nouveau_drm_winsys.c
@@ -17,8 +17,8 @@
 #include "nouveau/nouveau_winsys.h"
 #include "nouveau/nouveau_screen.h"
 
-#include <nvif/class.h>
-#include <nvif/cl0080.h>
+#include "nvif/class.h"
+#include "nvif/cl0080.h"
 
 static struct hash_table *fd_tab = NULL;
 
@@ -79,10 +79,7 @@ nouveau_drm_screen_create(int fd)
 	if (ret)
 		goto err;
 
-	ret = nouveau_device_new(&drm->client, NV_DEVICE,
-				 &(struct nv_device_v0) {
-					.device = ~0ULL,
-				 }, sizeof(struct nv_device_v0), &dev);
+	ret = nouveau_device_new(&drm->client, &dev);
 	if (ret)
 		goto err;
 

android/Android.mk
@@ -79,11 +79,6 @@ LOCAL_SHARED_LIBRARIES += libdrm_radeon
 MESON_GEN_PKGCONFIGS += libdrm_radeon:$(LIBDRM_VERSION)
 endif
 
-ifneq ($(filter nouveau,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libdrm_nouveau
-MESON_GEN_PKGCONFIGS += libdrm_nouveau:$(LIBDRM_VERSION)
-endif
-
 ifneq ($(filter d3d12,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
 LOCAL_HEADER_LIBRARIES += DirectX-Headers
 LOCAL_STATIC_LIBRARIES += DirectX-Guids

meson.build
@@ -1633,12 +1633,10 @@ endif
 # bar are both on use 2.4.3 for both of them
 dep_libdrm_amdgpu = null_dep
 dep_libdrm_radeon = null_dep
-dep_libdrm_nouveau = null_dep
 dep_libdrm_intel = null_dep
 
 _drm_amdgpu_ver = '2.4.119'
 _drm_radeon_ver = '2.4.71'
-_drm_nouveau_ver = '2.4.102'
 _drm_intel_ver = '2.4.75'
 _drm_ver = '2.4.109'
 
@@ -1646,7 +1644,6 @@ _libdrm_checks = [
   ['intel', with_gallium_i915],
   ['amdgpu', (with_amd_vk and not with_platform_windows) or with_gallium_radeonsi],
   ['radeon', (with_gallium_radeonsi or with_gallium_r300 or with_gallium_r600)],
-  ['nouveau', with_any_nouveau],
 ]
 
 # Loop over the enables versions and get the highest libdrm requirement for all

src/gallium/drivers/nouveau/ci/gitlab-ci-inc.yml
@@ -7,6 +7,7 @@
     - changes: &nouveau_file_list
       - src/nouveau/*
       - src/nouveau/codegen/**/*
+      - src/nouveau/drm/**/*
       - src/nouveau/drm-shim/**/*
       - src/nouveau/headers/**/*
       - src/nouveau/winsys/**/*

src/gallium/drivers/nouveau/meson.build
@@ -185,11 +185,12 @@ libnouveau = static_library(
     inc_src, inc_include, inc_gallium, inc_gallium_aux, inc_mapi, inc_mesa,
   ],
   gnu_symbol_visibility : 'hidden',
-  dependencies : [dep_libdrm, idep_nouveau_codegen, dep_libdrm_nouveau, idep_mesautil, idep_nir_headers],
+  link_with : [libnouveauwinsys],
+  dependencies : [dep_libdrm, idep_libnouveauwinsys, idep_nouveau_codegen, idep_mesautil, idep_nir_headers],
 )
 
 driver_nouveau = declare_dependency(
   compile_args : '-DGALLIUM_NOUVEAU',
   dependencies : [idep_nir, idep_nouveau_codegen],
-  link_with : [libnouveauwinsys, libnouveau_codegen, libnouveau],
+  link_with : [libnouveau_codegen, libnouveau],
 )

src/gallium/drivers/nouveau/nouveau_context.h
@@ -3,7 +3,7 @@
 
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
-#include <nouveau.h>
+#include "nouveau_winsys.h"
 
 #define NOUVEAU_MAX_SCRATCH_BUFS 4
 

src/gallium/drivers/nouveau/nouveau_screen.c
@@ -16,10 +16,10 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include <nouveau_drm.h>
+#include "drm-uapi/nouveau_drm.h"
 #include <xf86drm.h>
-#include <nvif/class.h>
-#include <nvif/cl0080.h>
+#include "nvif/class.h"
+#include "nvif/cl0080.h"
 
 #include "nouveau_winsys.h"
 #include "nouveau_screen.h"
@@ -261,13 +261,13 @@ nouveau_pushbuf_destroy(struct nouveau_pushbuf **push)
 }
 
 static bool
-nouveau_check_for_uma(int chipset, struct nouveau_object *obj)
+nouveau_check_for_uma(int chipset, struct nouveau_device *dev)
 {
    struct nv_device_info_v0 info = {
       .version = 0,
    };
 
-   nouveau_object_mthd(obj, NV_DEVICE_V0_INFO, &info, sizeof(info));
+   nouveau_device_info(dev, &info);
 
    return (info.platform == NV_DEVICE_INFO_V0_IGP) || (info.platform == NV_DEVICE_INFO_V0_SOC);
 }
@@ -286,6 +286,7 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
    struct pipe_screen *pscreen = &screen->base;
    struct nv04_fifo nv04_data = { .vram = 0xbeef0201, .gart = 0xbeef0202 };
    struct nvc0_fifo nvc0_data = { };
+   struct nve0_fifo nve0_data = { .engine = NOUVEAU_FIFO_ENGINE_GR };
    uint64_t time;
    int size, ret;
    void *data;
@@ -313,9 +314,12 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
    if (dev->chipset < 0xc0) {
       data = &nv04_data;
       size = sizeof(nv04_data);
-   } else {
+   } else if (dev->chipset < 0xe0) {
       data = &nvc0_data;
       size = sizeof(nvc0_data);
+   } else {
+      data = &nve0_data;
+      size = sizeof(nve0_data);
    }
 
    bool enable_svm = debug_get_bool_option("NOUVEAU_SVM", false);
@@ -432,7 +436,7 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
       PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_STREAM_OUTPUT |
       PIPE_BIND_COMMAND_ARGS_BUFFER;
 
-   screen->is_uma = nouveau_check_for_uma(dev->chipset, &dev->object);
+   screen->is_uma = nouveau_check_for_uma(dev->chipset, dev);
 
    memset(&mm_config, 0, sizeof(mm_config));
    nouveau_fence_list_init(&screen->fence);

src/gallium/drivers/nouveau/nouveau_vp3_video.c
@@ -394,7 +394,7 @@ firmware_present(struct pipe_screen *pscreen, enum pipe_video_profile profile)
       struct nouveau_object *channel = NULL, *bsp = NULL;
       struct nv04_fifo nv04_data = {.vram = 0xbeef0201, .gart = 0xbeef0202};
       struct nvc0_fifo nvc0_args = {};
-      struct nve0_fifo nve0_args = {.engine = NVE0_FIFO_ENGINE_BSP};
+      struct nve0_fifo nve0_args = {.engine = NOUVEAU_FIFO_ENGINE_BSP};
       void *data = NULL;
       int size;
 

commit d64f470ae281456c8151fe1796e15ba79c23bb42

gallium/winsys/nouveau: Don't mark the api PUBLIC
We don't want to export this symbol from our shared object - any use of
this function from outside of mesa (like the ddx) should get the version
from libdrm, not the private copy in mesa.

Fixes: 2a2cfaec34 ("nouveau: import libdrm_nouveau")
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32740>

src/gallium/winsys/nouveau/drm/nouveau_drm_winsys.c
@@ -82,7 +82,7 @@ err_dev_new:
    return NULL;
 }
 
-PUBLIC struct pipe_screen *
+struct pipe_screen *
 nouveau_drm_screen_create(int fd)
 {
    return u_pipe_screen_lookup_or_create(os_dupfd_cloexec(fd), NULL, NULL,