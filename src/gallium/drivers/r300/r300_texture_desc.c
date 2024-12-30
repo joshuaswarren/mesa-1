joshuaswarren/mesa-1

commit 5f24ed6a5e41b578012097b34e456ea6d7a37714

r300: enable tiling for scanout to fix DRI3 performance
Also don't use square tiling for scanout because the DDX doesn't use it
either.

Reviewed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28209>

src/gallium/drivers/r300/r300_texture_desc.c
@@ -527,7 +527,9 @@ static void r300_setup_tiling(struct r300_screen *screen,
             break;
 
         case 2:
-            tex->tex.microtile = RADEON_LAYOUT_SQUARETILED;
+            tex->tex.microtile =
+                tex->b.bind & PIPE_BIND_SCANOUT ?
+                     RADEON_LAYOUT_TILED : RADEON_LAYOUT_SQUARETILED;
             break;
     }
 

src/gallium/drivers/r300/r300_texture.c
@@ -1120,8 +1120,8 @@ struct pipe_resource *r300_texture_create(struct pipe_screen *screen,
     struct r300_screen *rscreen = r300_screen(screen);
     enum radeon_bo_layout microtile, macrotile;
 
-    if ((base->flags & R300_RESOURCE_FLAG_TRANSFER) ||
-        (base->bind & (PIPE_BIND_SCANOUT | PIPE_BIND_LINEAR))) {
+    if (base->flags & R300_RESOURCE_FLAG_TRANSFER ||
+        base->bind & PIPE_BIND_LINEAR) {
         microtile = RADEON_LAYOUT_LINEAR;
         macrotile = RADEON_LAYOUT_LINEAR;
     } else {

commit 49fa4b0fea2be702007f22ac688ba2d7c17e13e2

r300: disable microtiling for scanout buffers
Does not work with R300/R400 due to kernel driver limitations.
Suggested by Marek Olšák.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12010
Fixes: 5f24ed6a5e41b578012097b34e456ea6d7a37714
Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32638>

src/gallium/drivers/r300/r300_texture_desc.c
@@ -510,10 +510,11 @@ static void r300_setup_tiling(struct r300_screen *screen,
             break;
 
         case 2:
-            tex->tex.microtile =
-                tex->b.bind & PIPE_BIND_SCANOUT ?
-                     RADEON_LAYOUT_TILED : RADEON_LAYOUT_SQUARETILED;
+            tex->tex.microtile = RADEON_LAYOUT_SQUARETILED;
             break;
+    }
+    if (tex->b.bind & PIPE_BIND_SCANOUT) {
+        tex->tex.microtile = RADEON_LAYOUT_LINEAR;
     }
 
     if (dbg_no_tiling) {