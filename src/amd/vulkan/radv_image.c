joshuaswarren/mesa-1

commit 16907a34bfe279bad4643a08135e813fe487ac2a

radv: align video images internal width/height inside the driver.
Due to how the decoders work, they will write garbage data into
the padding, and later using the image for sampling with linear
images will use the garbage to create broken results. Let the
user specify the image size and align it up in the driver, so
sampling of the image later has the correct w/h.

cc: mesa-stable

Reviewed-by: Lynne <dev@lynne.ee>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23227>

src/amd/vulkan/radv_image.c
@@ -1747,6 +1747,7 @@ radv_get_ac_surf_info(struct radv_device *device, const struct radv_image *image
 VkResult
 radv_image_create_layout(struct radv_device *device, struct radv_image_create_info create_info,
                          const struct VkImageDrmFormatModifierExplicitCreateInfoEXT *mod_info,
+                         const struct VkVideoProfileListInfoKHR *profile_list,
                          struct radv_image *image)
 {
    /* Clear the pCreateInfo pointer so we catch issues in the delayed case when we test in the
@@ -1761,6 +1762,19 @@ radv_image_create_layout(struct radv_device *device, struct radv_image_create_in
    assert(!mod_info || mod_info->drmFormatModifierPlaneCount >= image->plane_count);
 
    radv_image_reset_layout(device->physical_device, image);
+
+   /*
+    * Due to how the decoder works, the user can't supply an oversized image, because if it attempts
+    * to sample it later with a linear filter, it will get garbage after the height it wants,
+    * so we let the user specify the width/height unaligned, and align them preallocation.
+    */
+   if (image->vk.usage & (VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR)) {
+      assert(profile_list);
+      uint32_t width_align, height_align;
+      vk_video_get_profile_alignments(profile_list, &width_align, &height_align);
+      image_info.width = align(image_info.width, width_align);
+      image_info.height = align(image_info.height, height_align);
+   }
 
    unsigned plane_count = radv_get_internal_plane_count(device->physical_device, image->vk.format);
    for (unsigned plane = 0; plane < plane_count; ++plane) {
@@ -1943,6 +1957,8 @@ radv_image_create(VkDevice _device, const struct radv_image_create_info *create_
    const struct VkImageDrmFormatModifierExplicitCreateInfoEXT *explicit_mod =
       vk_find_struct_const(pCreateInfo->pNext, IMAGE_DRM_FORMAT_MODIFIER_EXPLICIT_CREATE_INFO_EXT);
    assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO);
+   const struct VkVideoProfileListInfoKHR *profile_list =
+      vk_find_struct_const(pCreateInfo->pNext, VIDEO_PROFILE_LIST_INFO_KHR);
 
    unsigned plane_count = radv_get_internal_plane_count(device->physical_device, format);
 
@@ -2003,7 +2019,7 @@ radv_image_create(VkDevice _device, const struct radv_image_create_info *create_
       return VK_SUCCESS;
    }
 
-   VkResult result = radv_image_create_layout(device, *create_info, explicit_mod, image);
+   VkResult result = radv_image_create_layout(device, *create_info, explicit_mod, profile_list, image);
    if (result != VK_SUCCESS) {
       radv_destroy_image(device, alloc, image);
       return result;

src/amd/vulkan/radv_android.c
@@ -675,7 +675,7 @@ radv_import_ahb_memory(struct radv_device *device, struct radv_device_memory *me
       struct radv_image_create_info create_info = {.no_metadata_planes = true,
                                                    .bo_metadata = &metadata};
 
-      result = radv_image_create_layout(device, create_info, NULL, mem->image);
+      result = radv_image_create_layout(device, create_info, NULL, NULL, mem->image);
       if (result != VK_SUCCESS) {
          device->ws->buffer_destroy(device->ws, mem->bo);
          mem->bo = NULL;

src/amd/vulkan/radv_device_memory.c
@@ -187,7 +187,7 @@ radv_alloc_memory(struct radv_device *device, const VkMemoryAllocateInfo *pAlloc
          /* This gives a basic ability to import radeonsi images
           * that don't have DCC. This is not guaranteed by any
           * spec and can be removed after we support modifiers. */
-         result = radv_image_create_layout(device, create_info, NULL, mem->image);
+         result = radv_image_create_layout(device, create_info, NULL, NULL, mem->image);
          if (result != VK_SUCCESS) {
             device->ws->buffer_destroy(device->ws, mem->bo);
             goto fail;

src/amd/vulkan/radv_private.h
@@ -2852,6 +2852,7 @@ struct radv_image_create_info {
 VkResult
 radv_image_create_layout(struct radv_device *device, struct radv_image_create_info create_info,
                          const struct VkImageDrmFormatModifierExplicitCreateInfoEXT *mod_info,
+                         const struct VkVideoProfileListInfoKHR *profile_list,
                          struct radv_image *image);
 
 VkResult radv_image_create(VkDevice _device, const struct radv_image_create_info *info,

commit 109312381f2d1b40a78518fe701c6283a6db6852

radv: Fix sampling from image layers of video decode target
Video decode target needs custom height alignment, but tex descriptor
still needs to be set to the original size the image was created with.
This makes the descriptor wrong for layer > 0, so we need to calculate
the layer offset and add it to bo address for this case.

Fixes: 16907a34bfe ("radv: align video images internal width/height inside the driver.")
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32069>

src/amd/vulkan/radv_image.c
@@ -767,7 +767,7 @@ radv_query_opaque_metadata(struct radv_device *device, struct radv_image *image,
                                 plane_height, image->vk.extent.depth, 0.0f, desc, NULL, NULL, NULL);
 
    radv_set_mutable_tex_desc_fields(device, image, base_level_info, plane_id, 0, 0, surface->blk_w, false, false, false,
-                                    false, desc, NULL);
+                                    false, desc, NULL, 0);
 
    ac_surface_compute_umd_metadata(&pdev->info, surface, image->vk.mip_levels, desc, &md->size_metadata, md->metadata,
                                    instance->debug_flags & RADV_DEBUG_EXTRA_MD);

src/amd/vulkan/radv_image_view.c
@@ -53,12 +53,12 @@ radv_set_mutable_tex_desc_fields(struct radv_device *device, struct radv_image *
                                  const struct legacy_surf_level *base_level_info, unsigned plane_id,
                                  unsigned base_level, unsigned first_level, unsigned block_width, bool is_stencil,
                                  bool is_storage_image, bool disable_compression, bool enable_write_compression,
-                                 uint32_t *state, const struct ac_surf_nbc_view *nbc_view)
+                                 uint32_t *state, const struct ac_surf_nbc_view *nbc_view, uint64_t offset)
 {
    struct radv_image_plane *plane = &image->planes[plane_id];
    const uint32_t bind_idx = image->disjoint ? plane_id : 0;
    struct radv_image_binding *binding = &image->bindings[bind_idx];
-   uint64_t gpu_address = binding->bo ? radv_image_get_va(image, bind_idx) : 0;
+   uint64_t gpu_address = binding->bo ? radv_image_get_va(image, bind_idx) + offset : 0;
    const struct radv_physical_device *pdev = radv_device_physical(device);
 
    const struct ac_mutable_tex_state ac_state = {
@@ -371,6 +371,7 @@ radv_image_view_make_descriptor(struct radv_image_view *iview, struct radv_devic
    union radv_descriptor *descriptor;
    uint32_t hw_level = iview->vk.base_mip_level;
    bool force_zero_base_mip = false;
+   uint64_t offset = 0;
 
    if (is_storage_image) {
       descriptor = &iview->storage_descriptor;
@@ -394,6 +395,13 @@ radv_image_view_make_descriptor(struct radv_image_view *iview, struct radv_devic
 
          /* Clear the base array layer because addrlib adds it as part of the base addr offset. */
          first_layer = 0;
+      } else {
+         /* Video decode target uses custom height alignment. */
+         if (image->vk.usage & VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR) {
+            assert(image->planes[plane_id].surface.u.gfx9.swizzle_mode == 0);
+            offset += first_layer * image->planes[plane_id].surface.u.gfx9.surf_slice_size;
+            first_layer = 0;
+         }
       }
    } else {
       /* On GFX6-8, there are some cases where the view must use mip0 and minified image sizes:
@@ -421,6 +429,12 @@ radv_image_view_make_descriptor(struct radv_image_view *iview, struct radv_devic
          extent.height = image->vk.extent.height;
          extent.depth = image->vk.extent.depth;
       }
+
+      /* Video decode target uses custom height alignment. */
+      if (image->vk.usage & VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR) {
+         offset += first_layer * image->planes[plane_id].surface.u.legacy.level[0].slice_size_dw * 4;
+         first_layer = 0;
+      }
    }
 
    radv_make_texture_descriptor(
@@ -447,7 +461,7 @@ radv_image_view_make_descriptor(struct radv_image_view *iview, struct radv_devic
    radv_set_mutable_tex_desc_fields(device, image, base_level_info, plane_id,
                                     force_zero_base_mip ? iview->vk.base_mip_level : 0, iview->vk.base_mip_level, blk_w,
                                     is_stencil, is_storage_image, disable_compression, enable_write_compression,
-                                    descriptor->plane_descriptors[descriptor_plane_id], &iview->nbc_view);
+                                    descriptor->plane_descriptors[descriptor_plane_id], &iview->nbc_view, offset);
 }
 
 /**

src/amd/vulkan/radv_image_view.h
@@ -66,7 +66,7 @@ void radv_set_mutable_tex_desc_fields(struct radv_device *device, struct radv_im
                                       const struct legacy_surf_level *base_level_info, unsigned plane_id,
                                       unsigned base_level, unsigned first_level, unsigned block_width, bool is_stencil,
                                       bool is_storage_image, bool disable_compression, bool enable_write_compression,
-                                      uint32_t *state, const struct ac_surf_nbc_view *nbc_view);
+                                      uint32_t *state, const struct ac_surf_nbc_view *nbc_view, uint64_t offset);
 
 void radv_make_texture_descriptor(struct radv_device *device, struct radv_image *image, bool is_storage_image,
                                   VkImageViewType view_type, VkFormat vk_format, const VkComponentMapping *mapping,