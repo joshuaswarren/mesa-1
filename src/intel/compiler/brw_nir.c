joshuaswarren/mesa-1

commit 7e7b2246f18685e9e318087a7cad801533f56676

nir/opt_load_store_vectorize: make hole_size signed to indicate overlapping loads
A negative hole size means the loads overlap. This will be used by drivers
to handle overlapping loads in the callback easily.

Reviewed-by: Mel Henning <drawoc@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32699>

src/intel/compiler/brw_nir.c
@@ -1465,7 +1465,7 @@ bool
 brw_nir_should_vectorize_mem(unsigned align_mul, unsigned align_offset,
                              unsigned bit_size,
                              unsigned num_components,
-                             unsigned hole_size,
+                             int64_t hole_size,
                              nir_intrinsic_instr *low,
                              nir_intrinsic_instr *high,
                              void *data)

src/amd/common/ac_shader_util.c
@@ -115,10 +115,10 @@ void ac_set_nir_options(struct radeon_info *info, bool use_llvm,
 
 bool
 ac_nir_mem_vectorize_callback(unsigned align_mul, unsigned align_offset, unsigned bit_size,
-                              unsigned num_components, unsigned hole_size, nir_intrinsic_instr *low,
+                              unsigned num_components, int64_t hole_size, nir_intrinsic_instr *low,
                               nir_intrinsic_instr *high, void *data)
 {
-   if (num_components > 4 || hole_size)
+   if (num_components > 4 || hole_size > 0)
       return false;
 
    bool is_scratch = false;

src/amd/common/ac_shader_util.h
@@ -245,7 +245,7 @@ void ac_set_nir_options(struct radeon_info *info, bool use_llvm,
                         nir_shader_compiler_options *options);
 
 bool ac_nir_mem_vectorize_callback(unsigned align_mul, unsigned align_offset, unsigned bit_size,
-                                   unsigned num_components, unsigned hole_size,
+                                   unsigned num_components, int64_t hole_size,
                                    nir_intrinsic_instr *low, nir_intrinsic_instr *high, void *data);
 
 unsigned ac_get_spi_shader_z_format(bool writes_z, bool writes_stencil, bool writes_samplemask,

src/asahi/compiler/agx_compile.c
@@ -2839,10 +2839,10 @@ agx_optimize_loop_nir(nir_shader *nir)
 bool
 agx_mem_vectorize_cb(unsigned align_mul, unsigned align_offset,
                      unsigned bit_size, unsigned num_components,
-                     unsigned hole_size, nir_intrinsic_instr *low,
+                     int64_t hole_size, nir_intrinsic_instr *low,
                      nir_intrinsic_instr *high, void *data)
 {
-   if (hole_size)
+   if (hole_size > 0)
       return false;
 
    /* Must be aligned to the size of the load */

src/asahi/compiler/agx_compile.h
@@ -305,7 +305,7 @@ bool agx_nir_lower_cull_distance_fs(struct nir_shader *s,
                                     unsigned nr_distances);
 bool agx_mem_vectorize_cb(unsigned align_mul, unsigned align_offset,
                           unsigned bit_size, unsigned num_components,
-                          unsigned hole_size, nir_intrinsic_instr *low,
+                          int64_t hole_size, nir_intrinsic_instr *low,
                           nir_intrinsic_instr *high, void *data);
 
 void agx_compile_shader_nir(nir_shader *nir, struct agx_shader_key *key,

src/broadcom/compiler/nir_to_vir.c
@@ -2089,12 +2089,12 @@ static bool
 mem_vectorize_callback(unsigned align_mul, unsigned align_offset,
                        unsigned bit_size,
                        unsigned num_components,
-                       unsigned hole_size,
+                       int64_t hole_size,
                        nir_intrinsic_instr *low,
                        nir_intrinsic_instr *high,
                        void *data)
 {
-        if (hole_size || !nir_num_components_valid(num_components))
+        if (hole_size > 0 || !nir_num_components_valid(num_components))
                 return false;
 
         /* TMU general access only supports 32-bit vectors */

src/compiler/nir/nir.h
@@ -6007,7 +6007,7 @@ typedef bool (*nir_should_vectorize_mem_func)(unsigned align_mul,
                                               unsigned num_components,
                                               /* The hole between low and
                                                * high if they are not adjacent. */
-                                              unsigned hole_size,
+                                              int64_t hole_size,
                                               nir_intrinsic_instr *low,
                                               nir_intrinsic_instr *high,
                                               void *data);

src/compiler/nir/nir_lower_shader_calls.c
@@ -1920,7 +1920,7 @@ should_vectorize(unsigned align_mul,
                  unsigned align_offset,
                  unsigned bit_size,
                  unsigned num_components,
-                 unsigned hole_size,
+                 int64_t hole_size,
                  nir_intrinsic_instr *low, nir_intrinsic_instr *high,
                  void *data)
 {

commit f51e287068ec78deecb65bdeb89d525d3c171669

brw: Fix vectorizer hole_size condition after signedness change
Marek recently changed hole_size to be signed, rather than unsigned.
A negative hole_size means that the two loads overlap - and thus are
prime candidates to be combined.

My original hole_size handling was:

   if hole_size > 4 * (8 - low->num_components) then don't vectorize

For non-overlapping loads, this worked: NIR's largest vector is vec16,
and if low was already a vec16, combining it with anything would exceed
that, so it'd never be considered.  That meant low would always be a
vec8 or less, so (8 - low->num_components) was a positive number.

Now that we see overlapping loads, we can see a vec16 low, vec4 high,
and also a negative hole size, giving us fun comparisons like:

   -16 > 4 * (8 - 16)   =>   -16 > -32   => true, don't vectorize

Which is absolutely the wrong thing to do, because the high load's data
is entirely included within the former load's data.

The idea here was to make sure the second load would be able to pack at
least one component into the first's V8 result.  But even this isn't the
best, because...even if it's simply adjacent, doing one V16 load is more
efficient than requesting two back to back V8 loads.

So, we just simplify down to a static check: if there's an entire V8 of
hole, don't vectorize.  This already won't happen because the core pass
has max_hole set to 28 bytes (7 32-bit components), but that could
change based on the needs of other drivers, so let's be defensive.

fossil-db results on Alchemist:

   Instrs: 161533978 -> 161295137 (-0.15%); split: -0.20%, +0.05%
   Subgroup size: 8092544 -> 8092568 (+0.00%)
   Send messages: 7915233 -> 7844503 (-0.89%); split: -0.94%, +0.05%
   Cycle count: 16577700697 -> 16702609256 (+0.75%); split: -0.59%, +1.35%
   Spill count: 72338 -> 67226 (-7.07%); split: -7.36%, +0.29%
   Fill count: 134058 -> 125980 (-6.03%); split: -6.83%, +0.80%
   Scratch Memory Size: 4092928 -> 3786752 (-7.48%); split: -7.53%, +0.05%
   Max live registers: 33031460 -> 32945994 (-0.26%); split: -0.27%, +0.01%
   Max dispatch width: 5778384 -> 5778536 (+0.00%); split: +0.26%, -0.26%
   Non SSA regs after NIR: 179809505 -> 152735471 (-15.06%); split: -15.08%, +0.03%

Fixes: 7e7b2246f18 ("nir/opt_load_store_vectorize: make hole_size signed to indicate overlapping loads")

src/intel/compiler/brw_nir.c
@@ -1488,7 +1488,7 @@ brw_nir_should_vectorize_mem(unsigned align_mul, unsigned align_offset,
          if (num_components > 32)
             return false;
 
-         if (hole_size > 4 * (8 - low->num_components))
+         if (hole_size >= 8 * 4)
             return false;
       }
    } else {