joshuaswarren/mesa-1

commit 208580d367ffa2fb96ecbbf19ab9a27f14524846

pan/genxml: Enforce explicit packed types on pan_[un]pack
Provide a pan_cast_and_[un]pack() to help with the transition.
Those helpers should only be used when the caller is sure the
destination is big enough to emit the descriptor.

Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32962>

src/panfrost/lib/genxml/decode.h
@@ -189,7 +189,7 @@ pandecode_log_cont(struct pandecode_context *ctx, const char *format, ...)
 
 #define DUMP_CL(ctx, T, cl, ...)                                               \
    {                                                                           \
-      pan_unpack(cl, T, temp);                                                 \
+      pan_unpack((MALI_##T##_PACKED_T *)cl, T, temp);                          \
       DUMP_UNPACKED(ctx, T, temp, __VA_ARGS__);                                \
    }
 
@@ -201,7 +201,8 @@ pandecode_log_cont(struct pandecode_context *ctx, const char *format, ...)
    }
 
 #define MAP_ADDR(ctx, T, addr, cl)                                             \
-   const uint8_t *cl = pandecode_fetch_gpu_mem(ctx, addr, pan_size(T));
+   const MALI_##T##_PACKED_T *cl =                                             \
+      pandecode_fetch_gpu_mem(ctx, addr, pan_size(T));
 
 #define DUMP_ADDR(ctx, T, addr, ...)                                           \
    {                                                                           \
@@ -238,13 +239,15 @@ void GENX(pandecode_dcd)(struct pandecode_context *ctx,
 void GENX(pandecode_texture)(struct pandecode_context *ctx, uint64_t u,
                              unsigned tex);
 #else
-void GENX(pandecode_texture)(struct pandecode_context *ctx, const void *cl,
+void GENX(pandecode_texture)(struct pandecode_context *ctx,
+                             const struct mali_texture_packed *cl,
                              unsigned tex);
 #endif
 
 #if PAN_ARCH >= 5
-uint64_t GENX(pandecode_blend)(struct pandecode_context *ctx, void *descs,
-                               int rt_no, uint64_t frag_shader);
+uint64_t GENX(pandecode_blend)(struct pandecode_context *ctx,
+                               struct mali_blend_packed *descs, int rt_no,
+                               uint64_t frag_shader);
 #endif
 
 #if PAN_ARCH >= 6

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -316,7 +316,7 @@ panfrost_emit_blend(struct panfrost_batch *batch, void *rts,
 
       /* Disable blending for unbacked render targets */
       if (rt_count == 0 || !batch->key.cbufs[i] || !so->info[i].enabled) {
-         pan_pack(rts + i * pan_size(BLEND), BLEND, cfg) {
+         pan_pack(packed, BLEND, cfg) {
             cfg.enable = false;
 #if PAN_ARCH >= 6
             cfg.internal.mode = MALI_BLEND_MODE_OFF;
@@ -358,6 +358,8 @@ panfrost_emit_blend(struct panfrost_batch *batch, void *rts,
 
 #if PAN_ARCH >= 6
       struct panfrost_compiled_shader *fs = ctx->prog[PIPE_SHADER_FRAGMENT];
+      struct mali_internal_blend_packed *internal_blend_packed =
+         (struct mali_internal_blend_packed *)&packed->opaque[2];
 
       /* Words 2 and 3: Internal blend */
       if (blend_shaders[i]) {
@@ -368,7 +370,7 @@ panfrost_emit_blend(struct panfrost_batch *batch, void *rts,
          assert(!fs->bin.bo || (blend_shaders[i] & (0xffffffffull << 32)) ==
                                   (fs->bin.gpu & (0xffffffffull << 32)));
 
-         pan_pack(&packed->opaque[2], INTERNAL_BLEND, cfg) {
+         pan_pack(internal_blend_packed, INTERNAL_BLEND, cfg) {
             cfg.mode = MALI_BLEND_MODE_SHADER;
             cfg.shader.pc = (uint32_t)blend_shaders[i];
 
@@ -380,7 +382,7 @@ panfrost_emit_blend(struct panfrost_batch *batch, void *rts,
 #endif
          }
       } else {
-         pan_pack(&packed->opaque[2], INTERNAL_BLEND, cfg) {
+         pan_pack(internal_blend_packed, INTERNAL_BLEND, cfg) {
             cfg.mode = info.opaque ? MALI_BLEND_MODE_OPAQUE
                                    : MALI_BLEND_MODE_FIXED_FUNCTION;
 
@@ -757,7 +759,7 @@ panfrost_emit_viewport(struct panfrost_batch *batch)
    if (!T.cpu)
       return 0;
 
-   pan_pack(T.cpu, VIEWPORT, cfg) {
+   pan_cast_and_pack(T.cpu, VIEWPORT, cfg) {
       cfg.scissor_minimum_x = minx;
       cfg.scissor_minimum_y = miny;
       cfg.scissor_maximum_x = maxx;
@@ -769,7 +771,7 @@ panfrost_emit_viewport(struct panfrost_batch *batch)
 
    return T.gpu;
 #else
-   pan_pack(&batch->scissor, SCISSOR, cfg) {
+   pan_cast_and_pack(&batch->scissor, SCISSOR, cfg) {
       cfg.scissor_minimum_x = minx;
       cfg.scissor_minimum_y = miny;
       cfg.scissor_maximum_x = maxx;
@@ -1197,7 +1199,7 @@ panfrost_upload_rt_conversion_sysval(struct panfrost_batch *batch,
       uniform->u[0] =
          GENX(pan_blend_get_internal_desc)(format, rt, size, false) >> 32;
    } else {
-      pan_pack(&uniform->u[0], INTERNAL_CONVERSION, cfg)
+      pan_cast_and_pack(&uniform->u[0], INTERNAL_CONVERSION, cfg)
          cfg.memory_format =
             GENX(panfrost_format_from_pipe_format)(PIPE_FORMAT_NONE)->hw;
    }
@@ -1827,7 +1829,7 @@ static uint64_t
 panfrost_upload_wa_sampler(struct panfrost_batch *batch)
 {
    struct panfrost_ptr T = pan_pool_alloc_desc(&batch->pool.base, SAMPLER);
-   pan_pack(T.cpu, SAMPLER, cfg)
+   pan_cast_and_pack(T.cpu, SAMPLER, cfg)
       ;
    return T.gpu;
 }
@@ -1939,7 +1941,8 @@ emit_image_bufs(struct panfrost_batch *batch, enum pipe_shader_type shader,
       }
 
       if (is_buffer) {
-         pan_pack(bufs + (i * 2) + 1, ATTRIBUTE_BUFFER_CONTINUATION_3D, cfg) {
+         pan_cast_and_pack(&bufs[(i * 2) + 1], ATTRIBUTE_BUFFER_CONTINUATION_3D,
+                           cfg) {
             cfg.s_dimension =
                rsrc->base.width0 / util_format_get_blocksize(image->format);
             cfg.t_dimension = cfg.r_dimension = 1;
@@ -1948,7 +1951,8 @@ emit_image_bufs(struct panfrost_batch *batch, enum pipe_shader_type shader,
          continue;
       }
 
-      pan_pack(bufs + (i * 2) + 1, ATTRIBUTE_BUFFER_CONTINUATION_3D, cfg) {
+      pan_cast_and_pack(&bufs[(i * 2) + 1], ATTRIBUTE_BUFFER_CONTINUATION_3D,
+                        cfg) {
          unsigned level = image->u.tex.level;
          unsigned samples = rsrc->image.layout.nr_samples;
 
@@ -2009,8 +2013,9 @@ panfrost_emit_image_attribs(struct panfrost_batch *batch, uint64_t *buffers,
 
    /* We need an empty attrib buf to stop the prefetching on Bifrost */
 #if PAN_ARCH >= 6
-   pan_pack(bufs.cpu + ((buf_count - 1) * pan_size(ATTRIBUTE_BUFFER)),
-            ATTRIBUTE_BUFFER, cfg)
+   struct  mali_attribute_buffer_packed *attrib_bufs = bufs.cpu;
+
+   pan_pack(&attrib_bufs[buf_count - 1], ATTRIBUTE_BUFFER, cfg)
       ;
 #endif
 
@@ -2148,7 +2153,8 @@ panfrost_emit_vertex_data(struct panfrost_batch *batch, uint64_t *buffers)
             cfg.divisor_e = extra_flags;
          }
 
-         pan_pack(bufs + k + 1, ATTRIBUTE_BUFFER_CONTINUATION_NPOT, cfg) {
+         pan_cast_and_pack(&bufs[k + 1], ATTRIBUTE_BUFFER_CONTINUATION_NPOT,
+                           cfg) {
             cfg.divisor_numerator = magic_divisor;
             cfg.divisor = divisor;
          }
@@ -2162,14 +2168,17 @@ panfrost_emit_vertex_data(struct panfrost_batch *batch, uint64_t *buffers)
 #if PAN_ARCH <= 5
    /* Add special gl_VertexID/gl_InstanceID buffers */
    if (special_vbufs) {
-      panfrost_vertex_id(ctx->padded_count, &bufs[k], ctx->instance_count > 1);
+      panfrost_vertex_id(ctx->padded_count,
+                         (struct mali_attribute_vertex_id_packed *)&bufs[k],
+                         ctx->instance_count > 1);
 
       pan_pack(out + PAN_VERTEX_ID, ATTRIBUTE, cfg) {
          cfg.buffer_index = k++;
          cfg.format = so->formats[PAN_VERTEX_ID];
       }
 
-      panfrost_instance_id(ctx->padded_count, &bufs[k],
+      panfrost_instance_id(ctx->padded_count,
+                           (struct mali_attribute_instance_id_packed *)&bufs[k],
                            ctx->instance_count > 1);
 
       pan_pack(out + PAN_INSTANCE_ID, ATTRIBUTE, cfg) {
@@ -3640,7 +3649,7 @@ panfrost_create_depth_stencil_state(
 #else
    /* Pack with nodefaults so only explicitly set fields affect pan_merge() when
     * emitting depth stencil descriptor */
-   pan_pack_nodefaults(&so->desc, DEPTH_STENCIL, cfg) {
+   pan_cast_and_pack_nodefaults(&so->desc, DEPTH_STENCIL, cfg) {
       cfg.front_compare_function = (enum mali_func)front.func;
       cfg.front_stencil_fail = pan_pipe_to_stencil_op(front.fail_op);
       cfg.front_depth_fail = pan_pipe_to_stencil_op(front.zfail_op);
@@ -3824,7 +3833,8 @@ prepare_shader(struct panfrost_compiled_shader *state,
                struct panfrost_pool *pool, bool upload)
 {
 #if PAN_ARCH <= 7
-   void *out = &state->partial_rsd;
+   struct mali_renderer_state_packed *out =
+      (struct mali_renderer_state_packed *)&state->partial_rsd;
 
    if (upload) {
       struct panfrost_ptr ptr =
@@ -3857,8 +3867,10 @@ prepare_shader(struct panfrost_compiled_shader *state,
 
    state->state = panfrost_pool_take_ref(pool, ptr.gpu);
 
+   struct mali_shader_program_packed *programs = ptr.cpu;
+
    /* Generic, or IDVS/points */
-   pan_pack(ptr.cpu, SHADER_PROGRAM, cfg) {
+   pan_cast_and_pack(&programs[0], SHADER_PROGRAM, cfg) {
       cfg.stage = pan_shader_stage(&state->info);
 
       if (cfg.stage == MALI_SHADER_STAGE_FRAGMENT)
@@ -3880,7 +3892,7 @@ prepare_shader(struct panfrost_compiled_shader *state,
       return;
 
    /* IDVS/triangles */
-   pan_pack(ptr.cpu + pan_size(SHADER_PROGRAM), SHADER_PROGRAM, cfg) {
+   pan_pack(&programs[1], SHADER_PROGRAM, cfg) {
       cfg.stage = pan_shader_stage(&state->info);
       cfg.vertex_warp_limit = MALI_WARP_LIMIT_HALF;
       cfg.register_allocation =
@@ -3893,7 +3905,7 @@ prepare_shader(struct panfrost_compiled_shader *state,
    if (!secondary_enable)
       return;
 
-   pan_pack(ptr.cpu + (pan_size(SHADER_PROGRAM) * 2), SHADER_PROGRAM, cfg) {
+   pan_pack(&programs[2], SHADER_PROGRAM, cfg) {
       unsigned work_count = state->info.vs.secondary_work_reg_count;
 
       cfg.stage = pan_shader_stage(&state->info);

src/gallium/drivers/panfrost/pan_cmdstream.h
@@ -153,7 +153,8 @@ panfrost_overdraw_alpha(const struct panfrost_context *ctx, bool zero)
 
 static inline void
 panfrost_emit_primitive_size(struct panfrost_context *ctx, bool points,
-                             uint64_t size_array, void *prim_size)
+                             uint64_t size_array,
+                             struct mali_primitive_size_packed *prim_size)
 {
    struct panfrost_rasterizer *rast = ctx->rasterizer;
 

src/gallium/drivers/panfrost/pan_csf.c
@@ -28,6 +28,7 @@
 #include "genxml/cs_builder.h"
 #include "panfrost/lib/genxml/cs_builder.h"
 
+#include "gen_macros.h"
 #include "pan_cmdstream.h"
 #include "pan_context.h"
 #include "pan_csf.h"
@@ -702,7 +703,7 @@ csf_emit_tiler_desc(struct panfrost_batch *batch, const struct pan_fb_info *fb)
       tiler.geometry_buffer_size = ctx->csf.tmp_geom_bo->kmod_bo->size;
    }
 
-   batch->csf.pending_tiler_desc = 0;
+   batch->csf.pending_tiler_desc = NULL;
 }
 
 void
@@ -895,8 +896,8 @@ GENX(csf_launch_grid)(struct panfrost_batch *batch,
    cs_move32_to(b, cs_reg32(b, 32), 0);
 
    /* Compute workgroup size */
-   uint32_t wg_size[4];
-   pan_pack(wg_size, COMPUTE_SIZE_WORKGROUP, cfg) {
+   struct mali_compute_size_workgroup_packed wg_size;
+   pan_pack(&wg_size, COMPUTE_SIZE_WORKGROUP, cfg) {
       cfg.workgroup_size_x = info->block[0];
       cfg.workgroup_size_y = info->block[1];
       cfg.workgroup_size_z = info->block[2];
@@ -911,7 +912,7 @@ GENX(csf_launch_grid)(struct panfrost_batch *batch,
                                      (info->variable_shared_mem == 0);
    }
 
-   cs_move32_to(b, cs_reg32(b, 33), wg_size[0]);
+   cs_move32_to(b, cs_reg32(b, 33), wg_size.opaque[0]);
 
    /* Offset */
    for (unsigned i = 0; i < 3; ++i)
@@ -998,8 +999,8 @@ GENX(csf_launch_xfb)(struct panfrost_batch *batch,
    cs_move32_to(b, cs_reg32(b, 32), batch->ctx->offset_start);
 
    /* Compute workgroup size */
-   uint32_t wg_size[4];
-   pan_pack(wg_size, COMPUTE_SIZE_WORKGROUP, cfg) {
+   struct mali_compute_size_workgroup_packed wg_size;
+   pan_pack(&wg_size, COMPUTE_SIZE_WORKGROUP, cfg) {
       cfg.workgroup_size_x = 1;
       cfg.workgroup_size_y = 1;
       cfg.workgroup_size_z = 1;
@@ -1009,7 +1010,7 @@ GENX(csf_launch_xfb)(struct panfrost_batch *batch,
        */
       cfg.allow_merging_workgroups = true;
    }
-   cs_move32_to(b, cs_reg32(b, 33), wg_size[0]);
+   cs_move32_to(b, cs_reg32(b, 33), wg_size.opaque[0]);
 
    /* Offset */
    for (unsigned i = 0; i < 3; ++i)
@@ -1119,7 +1120,7 @@ csf_emit_draw_state(struct panfrost_batch *batch,
 
    struct pipe_rasterizer_state *rast = &ctx->rasterizer->base;
 
-   uint32_t primitive_flags = 0;
+   struct mali_primitive_flags_packed primitive_flags;
    pan_pack(&primitive_flags, PRIMITIVE_FLAGS, cfg) {
       if (panfrost_writes_point_size(ctx))
          cfg.point_size_array_format = MALI_POINT_SIZE_ARRAY_FORMAT_FP16;
@@ -1138,9 +1139,11 @@ csf_emit_draw_state(struct panfrost_batch *batch,
                                     : MALI_FIFO_FORMAT_BASIC;
    }
 
-   cs_move32_to(b, cs_reg32(b, 56), primitive_flags);
+   cs_move32_to(b, cs_reg32(b, 56), primitive_flags.opaque[0]);
 
-   uint32_t dcd_flags0 = 0, dcd_flags1 = 0;
+   struct mali_dcd_flags_0_packed dcd_flags0;
+   struct mali_dcd_flags_1_packed dcd_flags1;
+
    pan_pack(&dcd_flags0, DCD_FLAGS_0, cfg) {
       enum mesa_prim reduced_mode = u_reduced_prim(info->mode);
       bool polygon = reduced_mode == MESA_PRIM_TRIANGLES;
@@ -1245,15 +1248,16 @@ csf_emit_draw_state(struct panfrost_batch *batch,
       }
    }
 
-   cs_move32_to(b, cs_reg32(b, 57), dcd_flags0);
-   cs_move32_to(b, cs_reg32(b, 58), dcd_flags1);
+   cs_move32_to(b, cs_reg32(b, 57), dcd_flags0.opaque[0]);
+   cs_move32_to(b, cs_reg32(b, 58), dcd_flags1.opaque[0]);
 
-   uint64_t primsize = 0;
+   struct mali_primitive_size_packed primsize;
    panfrost_emit_primitive_size(ctx, info->mode == MESA_PRIM_POINTS, 0,
                                 &primsize);
-   cs_move64_to(b, cs_reg64(b, 60), primsize);
+   struct mali_primitive_size_packed *primsize_ptr = &primsize;
+   cs_move64_to(b, cs_reg64(b, 60), *((uint64_t*)primsize_ptr));
 
-   uint32_t flags_override;
+   struct mali_primitive_flags_packed flags_override;
    /* Pack with nodefaults so only explicitly set override fields affect the
     * previously set register values */
    pan_pack_nodefaults(&flags_override, PRIMITIVE_FLAGS, cfg) {
@@ -1262,7 +1266,7 @@ csf_emit_draw_state(struct panfrost_batch *batch,
       cfg.secondary_shader = secondary_shader;
    };
 
-   return flags_override;
+   return flags_override.opaque[0];
 }
 
 static struct cs_index
@@ -1430,7 +1434,7 @@ GENX(csf_init_context)(struct panfrost_context *ctx)
    if (ctx->csf.heap.desc_bo == NULL)
       goto err_tiler_heap_desc_bo;
 
-   pan_pack(ctx->csf.heap.desc_bo->ptr.cpu, TILER_HEAP, heap) {
+   pan_cast_and_pack(ctx->csf.heap.desc_bo->ptr.cpu, TILER_HEAP, heap) {
       heap.size = pan_screen(ctx->base.screen)->csf_tiler_heap.chunk_size;
       heap.base = thc.first_heap_chunk_gpu_va;
       heap.bottom = heap.base + 64;

src/gallium/drivers/panfrost/pan_csf.h
@@ -78,7 +78,7 @@ struct panfrost_csf_batch {
 
    struct panfrost_ptr tiler_oom_ctx;
 
-   void *pending_tiler_desc;
+   struct mali_tiler_context_packed *pending_tiler_desc;
 };
 
 struct panfrost_csf_context {

src/gallium/drivers/panfrost/pan_fb_preload.c
@@ -132,7 +132,7 @@ static void
 pan_preload_emit_blend(unsigned rt,
                        const struct pan_image_view *iview,
                        const struct pan_preload_shader_data *preload_shader,
-                       uint64_t blend_shader, void *out)
+                       uint64_t blend_shader, struct mali_blend_packed *out)
 {
    assert(blend_shader == 0 || PAN_ARCH <= 5);
 
@@ -212,14 +212,13 @@ pan_preload_is_ms(struct pan_preload_views *views)
 static void
 pan_preload_emit_blends(const struct pan_preload_shader_data *preload_shader,
                         struct pan_preload_views *views,
-                        uint64_t *blend_shaders, void *out)
+                        uint64_t *blend_shaders, struct mali_blend_packed *out)
 {
    for (unsigned i = 0; i < MAX2(views->rt_count, 1); ++i) {
-      void *dest = out + pan_size(BLEND) * i;
       const struct pan_image_view *rt_view = views->rts[i];
       uint64_t blend_shader = blend_shaders ? blend_shaders[i] : 0;
 
-      pan_preload_emit_blend(i, rt_view, preload_shader, blend_shader, dest);
+      pan_preload_emit_blend(i, rt_view, preload_shader, blend_shader, &out[i]);
    }
 }
 #endif
@@ -228,7 +227,7 @@ pan_preload_emit_blends(const struct pan_preload_shader_data *preload_shader,
 static void
 pan_preload_emit_rsd(const struct pan_preload_shader_data *preload_shader,
                      struct pan_preload_views *views, uint64_t *blend_shaders,
-                     void *out)
+                     struct mali_renderer_state_packed *out)
 {
    UNUSED bool zs = (views->z || views->s);
    bool ms = pan_preload_is_ms(views);
@@ -312,7 +311,7 @@ pan_preload_emit_rsd(const struct pan_preload_shader_data *preload_shader,
 
 #if PAN_ARCH >= 5
    pan_preload_emit_blends(preload_shader, views, blend_shaders,
-                           out + pan_size(RENDERER_STATE));
+                           (void*)((uint8_t*)out + pan_size(RENDERER_STATE)));
 #endif
 }
 #endif
@@ -827,7 +826,7 @@ pan_preload_emit_varying(struct pan_pool *pool)
    if (!varying.cpu)
       return 0;
 
-   pan_pack(varying.cpu, ATTRIBUTE, cfg) {
+   pan_cast_and_pack(varying.cpu, ATTRIBUTE, cfg) {
       cfg.buffer_index = 0;
       cfg.offset_enable = PAN_ARCH <= 5;
       cfg.format =
@@ -853,7 +852,7 @@ pan_preload_emit_varying_buffer(struct pan_pool *pool, uint64_t coordinates)
    if (!varying_buffer.cpu)
       return 0;
 
-   pan_pack(varying_buffer.cpu, BUFFER, cfg) {
+   pan_cast_and_pack(varying_buffer.cpu, BUFFER, cfg) {
       cfg.address = coordinates;
       cfg.size = 4 * sizeof(float) * 4;
    }
@@ -867,15 +866,15 @@ pan_preload_emit_varying_buffer(struct pan_pool *pool, uint64_t coordinates)
    if (!varying_buffer.cpu)
       return 0;
 
-   pan_pack(varying_buffer.cpu, ATTRIBUTE_BUFFER, cfg) {
+   pan_cast_and_pack(varying_buffer.cpu, ATTRIBUTE_BUFFER, cfg) {
       cfg.pointer = coordinates;
       cfg.stride = 4 * sizeof(float);
       cfg.size = cfg.stride * 4;
    }
 
    if (padding_buffer) {
-      pan_pack(varying_buffer.cpu + pan_size(ATTRIBUTE_BUFFER),
-               ATTRIBUTE_BUFFER, cfg)
+      pan_cast_and_pack(varying_buffer.cpu + pan_size(ATTRIBUTE_BUFFER),
+                        ATTRIBUTE_BUFFER, cfg)
          ;
    }
 #endif
@@ -891,7 +890,7 @@ pan_preload_emit_sampler(struct pan_pool *pool, bool nearest_filter)
    if (!sampler.cpu)
       return 0;
 
-   pan_pack(sampler.cpu, SAMPLER, cfg) {
+   pan_cast_and_pack(sampler.cpu, SAMPLER, cfg) {
       cfg.seamless_cube_map = false;
       cfg.normalized_coordinates = false;
       cfg.minify_nearest = nearest_filter;
@@ -1027,7 +1026,7 @@ pan_preload_emit_zs(struct pan_pool *pool, bool z, bool s)
    if (!zsd.cpu)
       return 0;
 
-   pan_pack(zsd.cpu, DEPTH_STENCIL, cfg) {
+   pan_cast_and_pack(zsd.cpu, DEPTH_STENCIL, cfg) {
       cfg.depth_function = MALI_FUNC_ALWAYS;
       cfg.depth_write_enable = z;
 
@@ -1066,7 +1065,7 @@ pan_preload_emit_viewport(struct pan_pool *pool, uint16_t minx, uint16_t miny,
    if (!vp.cpu)
       return 0;
 
-   pan_pack(vp.cpu, VIEWPORT, cfg) {
+   pan_cast_and_pack(vp.cpu, VIEWPORT, cfg) {
       cfg.scissor_minimum_x = minx;
       cfg.scissor_minimum_y = miny;
       cfg.scissor_maximum_x = maxx;
@@ -1078,9 +1077,9 @@ pan_preload_emit_viewport(struct pan_pool *pool, uint16_t minx, uint16_t miny,
 #endif
 
 static void
-pan_preload_emit_dcd(struct pan_fb_preload_cache *cache,
-                     struct pan_pool *pool, struct pan_fb_info *fb, bool zs,
-                     uint64_t coordinates, uint64_t tsd, void *out,
+pan_preload_emit_dcd(struct pan_fb_preload_cache *cache, struct pan_pool *pool,
+                     struct pan_fb_info *fb, bool zs, uint64_t coordinates,
+                     uint64_t tsd, struct mali_draw_packed *out,
                      bool always_write)
 {
    unsigned tex_count = 0;
@@ -1163,7 +1162,7 @@ pan_preload_emit_dcd(struct pan_fb_preload_cache *cache,
       return;
    }
 
-   pan_pack(spd.cpu, SHADER_PROGRAM, cfg) {
+   pan_cast_and_pack(spd.cpu, SHADER_PROGRAM, cfg) {
       cfg.stage = MALI_SHADER_STAGE_FRAGMENT;
       cfg.fragment_coverage_bitmask_type = MALI_COVERAGE_BITMASK_TYPE_GL;
       cfg.register_allocation = MALI_SHADER_REGISTER_ALLOCATION_32_PER_THREAD;

src/gallium/drivers/panfrost/pan_jm.c
@@ -65,11 +65,12 @@ GENX(jm_init_batch)(struct panfrost_batch *batch)
 #if PAN_ARCH == 5
    struct mali_framebuffer_pointer_packed ptr;
 
-   pan_pack(ptr.opaque, FRAMEBUFFER_POINTER, cfg) {
+   pan_pack(&ptr, FRAMEBUFFER_POINTER, cfg) {
       cfg.pointer = batch->framebuffer.gpu;
       cfg.render_target_count = 1; /* a necessary lie */
    }
 
+   /* XXX: THIS IS A BUG, FIXME */
    batch->tls.gpu = ptr.opaque[0];
 #endif
 #endif
@@ -411,7 +412,7 @@ jm_emit_tiler_desc(struct panfrost_batch *batch)
 
    struct panfrost_ptr t = pan_pool_alloc_desc(&batch->pool.base, TILER_HEAP);
 
-   pan_pack(t.cpu, TILER_HEAP, heap) {
+   pan_cast_and_pack(t.cpu, TILER_HEAP, heap) {
       heap.size = panfrost_bo_size(dev->tiler_heap);
       heap.base = dev->tiler_heap->ptr.gpu;
       heap.bottom = dev->tiler_heap->ptr.gpu;
@@ -423,7 +424,7 @@ jm_emit_tiler_desc(struct panfrost_batch *batch)
    assert(max_levels >= 2);
 
    t = pan_pool_alloc_desc(&batch->pool.base, TILER_CONTEXT);
-   pan_pack(t.cpu, TILER_CONTEXT, tiler) {
+   pan_cast_and_pack(t.cpu, TILER_CONTEXT, tiler) {
       /* TODO: Select hierarchy mask more effectively */
       tiler.hierarchy_mask = (max_levels >= 8) ? 0xFF : 0x28;
 
@@ -471,7 +472,8 @@ jm_emit_draw_descs(struct panfrost_batch *batch, struct MALI_DRAW *d,
 }
 
 static void
-jm_emit_vertex_draw(struct panfrost_batch *batch, void *section)
+jm_emit_vertex_draw(struct panfrost_batch *batch,
+                    struct mali_draw_packed *section)
 {
    pan_pack(section, DRAW, cfg) {
       cfg.state = batch->rsd[PIPE_SHADER_VERTEX];
@@ -507,8 +509,8 @@ jm_emit_vertex_job(struct panfrost_batch *batch,
 #endif /* PAN_ARCH <= 7 */
 
 static void
-jm_emit_tiler_draw(void *out, struct panfrost_batch *batch, bool fs_required,
-                   enum mesa_prim prim)
+jm_emit_tiler_draw(struct mali_draw_packed *out, struct panfrost_batch *batch,
+                   bool fs_required, enum mesa_prim prim)
 {
    struct panfrost_context *ctx = batch->ctx;
    struct pipe_rasterizer_state *rast = &ctx->rasterizer->base;
@@ -672,7 +674,7 @@ static void
 jm_emit_primitive(struct panfrost_batch *batch,
                   const struct pipe_draw_info *info,
                   const struct pipe_draw_start_count_bias *draw,
-                  bool secondary_shader, void *out)
+                  bool secondary_shader, struct mali_primitive_packed *out)
 {
    struct panfrost_context *ctx = batch->ctx;
    UNUSED struct pipe_rasterizer_state *rast = &ctx->rasterizer->base;

src/panfrost/.clang-format
@@ -73,6 +73,8 @@ ForEachMacros: [
   'nodearray_dense_foreach',
   'nodearray_dense_foreach_64',
   'nodearray_sparse_foreach',
+  'pan_cast_and_pack',
+  'pan_cast_and_pack_nodefaults',
   'pan_foreach_instr_in_block_rev',
   'pan_foreach_predecessor',
   'pan_foreach_successor',

commit 9ae5d6e0b4cfd8e0e25dd5f0a18771778a88597e

panvk: Fix descriptor decode
The expansion of DUMP_CL is missing parenthesis, making the dumping of
descriptors incorrect.

Fixes: 208580d367f ("pan/genxml: Enforce explicit packed types on pan_[un]pack")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33040>

src/panfrost/lib/genxml/decode.h
@@ -189,7 +189,7 @@ pandecode_log_cont(struct pandecode_context *ctx, const char *format, ...)
 
 #define DUMP_CL(ctx, T, cl, ...)                                               \
    {                                                                           \
-      pan_unpack((MALI_##T##_PACKED_T *)cl, T, temp);                          \
+      pan_unpack((MALI_##T##_PACKED_T *)(cl), T, temp);                        \
       DUMP_UNPACKED(ctx, T, temp, __VA_ARGS__);                                \
    }
 