openharmony/third_party_mesa3d

commit 48bfa00b237e01058565be87bfffcb6ffeefc510

zink: move shader keys to be persistent on pipeline state
save a cycle or two zeroing and populating this on every recalc

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12842>

src/gallium/drivers/zink/zink_state.c
@@ -25,6 +25,7 @@
 
 #include "zink_context.h"
 #include "zink_format.h"
+#include "zink_program.h"
 #include "zink_screen.h"
 
 #include "compiler/shader_enums.h"
@@ -178,11 +179,45 @@ zink_bind_vertex_elements_state(struct pipe_context *pctx,
          ctx->vertex_state_changed = !zink_screen(pctx->screen)->info.have_EXT_vertex_input_dynamic_state;
          ctx->vertex_buffers_dirty = ctx->element_state->hw_state.num_bindings > 0;
       }
-      if (ctx->element_state->decomposed_attrs != state->decomposed_attrs ||
-          ctx->element_state->decomposed_attrs_without_w != state->decomposed_attrs_without_w)
-         ctx->dirty_shader_stages |= BITFIELD_BIT(PIPE_SHADER_VERTEX);
-      state->decomposed_attrs = ctx->element_state->decomposed_attrs;
-      state->decomposed_attrs_without_w = ctx->element_state->decomposed_attrs_without_w;
+      const struct zink_vs_key *vs = zink_get_vs_key(ctx);
+      uint32_t decomposed_attrs = 0, decomposed_attrs_without_w = 0;
+      switch (vs->size) {
+      case 1:
+         decomposed_attrs = vs->u8.decomposed_attrs;
+         decomposed_attrs_without_w = vs->u8.decomposed_attrs_without_w;
+         break;
+      case 2:
+         decomposed_attrs = vs->u16.decomposed_attrs;
+         decomposed_attrs_without_w = vs->u16.decomposed_attrs_without_w;
+         break;
+      case 4:
+         decomposed_attrs = vs->u16.decomposed_attrs;
+         decomposed_attrs_without_w = vs->u16.decomposed_attrs_without_w;
+         break;
+      }
+      if (ctx->element_state->decomposed_attrs != decomposed_attrs ||
+          ctx->element_state->decomposed_attrs_without_w != decomposed_attrs_without_w) {
+         unsigned size = MAX2(ctx->element_state->decomposed_attrs_size, ctx->element_state->decomposed_attrs_without_w_size);
+         struct zink_shader_key *key = (struct zink_shader_key *)zink_set_vs_key(ctx);
+         key->size -= 2 * key->key.vs.size;
+         switch (size) {
+         case 1:
+            key->key.vs.u8.decomposed_attrs = ctx->element_state->decomposed_attrs;
+            key->key.vs.u8.decomposed_attrs_without_w = ctx->element_state->decomposed_attrs_without_w;
+            break;
+         case 2:
+            key->key.vs.u16.decomposed_attrs = ctx->element_state->decomposed_attrs;
+            key->key.vs.u16.decomposed_attrs_without_w = ctx->element_state->decomposed_attrs_without_w;
+            break;
+         case 4:
+            key->key.vs.u32.decomposed_attrs = ctx->element_state->decomposed_attrs;
+            key->key.vs.u32.decomposed_attrs_without_w = ctx->element_state->decomposed_attrs_without_w;
+            break;
+         default: break;
+         }
+         key->key.vs.size = size;
+         key->size += 2 * size;
+      }
       state->element_state = &ctx->element_state->hw_state;
    } else {
      state->element_state = NULL;
@@ -380,6 +415,10 @@ zink_bind_blend_state(struct pipe_context *pctx, void *cso)
       state->blend_state = cso;
       state->blend_id = blend ? blend->hash : 0;
       state->dirty = true;
+      bool force_dual_color_blend = zink_screen(pctx->screen)->driconf.dual_color_blend_by_location &&
+                                    blend && blend->dual_src_blend && state->blend_state->attachments[1].blendEnable;
+      if (force_dual_color_blend != zink_get_fs_key(ctx)->force_dual_color_blend)
+         zink_set_fs_key(ctx)->force_dual_color_blend = force_dual_color_blend;
       ctx->blend_state_changed = true;
    }
 }
@@ -635,7 +674,6 @@ zink_bind_rasterizer_state(struct pipe_context *pctx, void *cso)
 {
    struct zink_context *ctx = zink_context(pctx);
    struct zink_screen *screen = zink_screen(pctx->screen);
-   bool clip_halfz = ctx->rast_state ? ctx->rast_state->base.clip_halfz : false;
    bool point_quad_rasterization = ctx->rast_state ? ctx->rast_state->base.point_quad_rasterization : false;
    bool scissor = ctx->rast_state ? ctx->rast_state->base.scissor : false;
    bool pv_last = ctx->rast_state ? ctx->rast_state->hw_state.pv_last : false;
@@ -654,8 +692,8 @@ zink_bind_rasterizer_state(struct pipe_context *pctx, void *cso)
       ctx->gfx_pipeline_state.dirty = true;
       ctx->rast_state_changed = true;
 
-      if (clip_halfz != ctx->rast_state->base.clip_halfz) {
-         ctx->last_vertex_stage_dirty = true;
+      if (zink_get_last_vertex_key(ctx)->clip_halfz != ctx->rast_state->base.clip_halfz) {
+         zink_set_last_vertex_key(ctx)->clip_halfz = ctx->rast_state->base.clip_halfz;
          ctx->vp_state_changed = true;
       }
 
@@ -663,11 +701,8 @@ zink_bind_rasterizer_state(struct pipe_context *pctx, void *cso)
          ctx->gfx_pipeline_state.dyn_state1.front_face = ctx->rast_state->front_face;
          ctx->gfx_pipeline_state.dirty |= !zink_screen(pctx->screen)->info.have_EXT_extended_dynamic_state;
       }
-      if (ctx->rast_state->base.point_quad_rasterization != point_quad_rasterization) {
-         ctx->dirty_shader_stages |= BITFIELD_BIT(PIPE_SHADER_FRAGMENT);
-         ctx->gfx_pipeline_state.coord_replace_bits = ctx->rast_state->base.sprite_coord_enable;
-         ctx->gfx_pipeline_state.coord_replace_yinvert = !!ctx->rast_state->base.sprite_coord_mode;
-      }
+      if (ctx->rast_state->base.point_quad_rasterization != point_quad_rasterization)
+         zink_set_fs_point_coord_key(ctx);
       if (ctx->rast_state->base.scissor != scissor)
          ctx->scissor_changed = true;
    }

src/gallium/drivers/zink/zink_compiler.c
@@ -782,7 +782,7 @@ zink_compiler_assign_io(nir_shader *producer, nir_shader *consumer)
 }
 
 VkShaderModule
-zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shader *base_nir, struct zink_shader_key *key)
+zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shader *base_nir, const struct zink_shader_key *key)
 {
    VkShaderModule mod = VK_NULL_HANDLE;
    void *streamout = NULL;

src/gallium/drivers/zink/zink_compiler.h
@@ -99,7 +99,7 @@ zink_screen_init_compiler(struct zink_screen *screen);
 void
 zink_compiler_assign_io(nir_shader *producer, nir_shader *consumer);
 VkShaderModule
-zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shader *nir, struct zink_shader_key *key);
+zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, nir_shader *nir, const struct zink_shader_key *key);
 
 struct zink_shader *
 zink_shader_create(struct zink_screen *screen, struct nir_shader *nir,

src/gallium/drivers/zink/zink_context.c
@@ -1000,12 +1000,22 @@ zink_set_inlinable_constants(struct pipe_context *pctx,
 {
    struct zink_context *ctx = (struct zink_context *)pctx;
    const uint32_t bit = BITFIELD_BIT(shader);
+   uint32_t *inlinable_uniforms;
+   struct zink_shader_key *key = NULL;
 
+   if (shader == PIPE_SHADER_COMPUTE) {
+      inlinable_uniforms = ctx->compute_inlinable_uniforms;
+   } else {
+      key = &ctx->gfx_pipeline_state.shader_keys.key[shader];
+      inlinable_uniforms = key->base.inlined_uniform_values;
+   }
    if (!(ctx->inlinable_uniforms_valid_mask & bit) ||
-       memcmp(ctx->inlinable_uniforms[shader], values, num_values * 4)) {
-      memcpy(ctx->inlinable_uniforms[shader], values, num_values * 4);
+       memcmp(inlinable_uniforms, values, num_values * 4)) {
+      memcpy(inlinable_uniforms, values, num_values * 4);
       ctx->dirty_shader_stages |= bit;
       ctx->inlinable_uniforms_valid_mask |= bit;
+      if (key)
+         key->inline_uniforms = true;
    }
 }
 
@@ -1017,6 +1027,21 @@ unbind_ubo(struct zink_context *ctx, struct zink_resource *res, enum pipe_shader
    res->ubo_bind_mask[pstage] &= ~BITFIELD_BIT(slot);
    res->ubo_bind_count[pstage == PIPE_SHADER_COMPUTE]--;
    update_res_bind_count(ctx, res, pstage == PIPE_SHADER_COMPUTE, true);
+}
+
+static void
+invalidate_inlined_uniforms(struct zink_context *ctx, enum pipe_shader_type pstage)
+{
+   unsigned bit = BITFIELD_BIT(pstage);
+   if (!(ctx->inlinable_uniforms_valid_mask & bit))
+      return;
+   ctx->inlinable_uniforms_valid_mask &= ~bit;
+   ctx->dirty_shader_stages |= bit;
+   if (pstage == PIPE_SHADER_COMPUTE)
+      return;
+
+   struct zink_shader_key *key = &ctx->gfx_pipeline_state.shader_keys.key[pstage];
+   key->inline_uniforms = false;
 }
 
 static void
@@ -1086,7 +1111,7 @@ zink_set_constant_buffer(struct pipe_context *pctx,
    }
    if (index == 0) {
       /* Invalidate current inlinable uniforms. */
-      ctx->inlinable_uniforms_valid_mask &= ~(1 << shader);
+      invalidate_inlined_uniforms(ctx, shader);
    }
 
    if (update)
@@ -2066,7 +2091,6 @@ zink_set_framebuffer_state(struct pipe_context *pctx,
       ctx->scissor_changed = true;
    rebind_fb_state(ctx, NULL, true);
    ctx->fb_state.samples = util_framebuffer_get_num_samples(state);
-   uint8_t rast_samples = ctx->fb_state.samples - 1;
    /* get_framebuffer adds a ref if the fb is reused or created;
     * always do get_framebuffer first to avoid deleting the same fb
     * we're about to use
@@ -2095,9 +2119,16 @@ zink_set_framebuffer_state(struct pipe_context *pctx,
    ctx->fb_changed |= ctx->framebuffer != fb;
    ctx->framebuffer = fb;
 
-   /* in vulkan, gl_SampleMask needs to be explicitly ignored for sampleCount == 1 */
-   if ((ctx->gfx_pipeline_state.rast_samples > 0) != (rast_samples > 0))
-      ctx->dirty_shader_stages |= 1 << PIPE_SHADER_FRAGMENT;
+   uint8_t rast_samples = ctx->fb_state.samples - 1;
+   /* update the shader key if applicable:
+    * if gl_SampleMask[] is written to, we have to ensure that we get a shader with the same sample count:
+    * in GL, rast_samples==1 means ignore gl_SampleMask[]
+    * in VK, gl_SampleMask[] is never ignored
+    */
+   if (rast_samples != ctx->gfx_pipeline_state.rast_samples &&
+       (!ctx->gfx_stages[PIPE_SHADER_FRAGMENT] ||
+        ctx->gfx_stages[PIPE_SHADER_FRAGMENT]->nir->info.outputs_written & (1 << FRAG_RESULT_SAMPLE_MASK)))
+      zink_set_fs_key(ctx)->samples = ctx->fb_state.samples > 0;
    if (ctx->gfx_pipeline_state.rast_samples != rast_samples) {
       ctx->sample_locations_changed |= ctx->gfx_pipeline_state.sample_locations_enabled;
       ctx->gfx_pipeline_state.dirty = true;
@@ -3646,6 +3677,12 @@ zink_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
    if (!ctx->blitter)
       goto fail;
 
+   ctx->gfx_pipeline_state.shader_keys.last_vertex.key.vs_base.last_vertex_stage = true;
+   ctx->last_vertex_stage_dirty = true;
+   ctx->gfx_pipeline_state.shader_keys.key[PIPE_SHADER_VERTEX].size = sizeof(struct zink_vs_key_base);
+   ctx->gfx_pipeline_state.shader_keys.key[PIPE_SHADER_TESS_EVAL].size = sizeof(struct zink_vs_key_base);
+   ctx->gfx_pipeline_state.shader_keys.key[PIPE_SHADER_GEOMETRY].size = sizeof(struct zink_vs_key_base);
+   ctx->gfx_pipeline_state.shader_keys.key[PIPE_SHADER_FRAGMENT].size = sizeof(struct zink_fs_key);
    _mesa_hash_table_init(&ctx->compute_program_cache, ctx, _mesa_hash_pointer, _mesa_key_pointer_equal);
    _mesa_hash_table_init(&ctx->framebuffer_cache, ctx, hash_framebuffer_imageless, equals_framebuffer_imageless);
    _mesa_set_init(&ctx->render_pass_state_cache, ctx, hash_rp_state, equals_rp_state);

src/gallium/drivers/zink/zink_context.h
@@ -193,7 +193,7 @@ struct zink_context {
 
    unsigned shader_has_inlinable_uniforms_mask;
    unsigned inlinable_uniforms_valid_mask;
-   uint32_t inlinable_uniforms[PIPE_SHADER_TYPES][MAX_INLINABLE_UNIFORMS];
+   uint32_t compute_inlinable_uniforms[MAX_INLINABLE_UNIFORMS];
 
    struct pipe_constant_buffer ubos[PIPE_SHADER_TYPES][PIPE_MAX_CONSTANT_BUFFERS];
    struct pipe_shader_buffer ssbos[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_BUFFERS];

src/gallium/drivers/zink/zink_draw.cpp
@@ -179,6 +179,9 @@ update_gfx_program(struct zink_context *ctx)
    if (ctx->last_vertex_stage_dirty) {
       enum pipe_shader_type pstage = pipe_shader_type_from_mesa(ctx->last_vertex_stage->nir->info.stage);
       ctx->dirty_shader_stages |= BITFIELD_BIT(pstage);
+      memcpy(&ctx->gfx_pipeline_state.shader_keys.key[pstage].key.vs_base,
+             &ctx->gfx_pipeline_state.shader_keys.last_vertex.key.vs_base,
+             sizeof(struct zink_vs_key_base));
       ctx->last_vertex_stage_dirty = false;
    }
    unsigned bits = BITFIELD_MASK(PIPE_SHADER_COMPUTE);
@@ -489,14 +492,13 @@ zink_draw_vbo(struct pipe_context *pctx,
    uint8_t vertices_per_patch = ctx->gfx_pipeline_state.patch_vertices ? ctx->gfx_pipeline_state.patch_vertices - 1 : 0;
    if (ctx->gfx_pipeline_state.vertices_per_patch != vertices_per_patch)
       ctx->gfx_pipeline_state.dirty = true;
-   bool drawid_broken = ctx->gfx_pipeline_state.drawid_broken;
-   ctx->gfx_pipeline_state.drawid_broken = false;
+   bool drawid_broken = false;
    if (reads_drawid && (!dindirect || !dindirect->buffer))
-      ctx->gfx_pipeline_state.drawid_broken = (drawid_offset != 0 ||
-                           (!HAS_MULTIDRAW && num_draws > 1) ||
-                           (HAS_MULTIDRAW && num_draws > 1 && !dinfo->increment_draw_id));
-   if (drawid_broken != ctx->gfx_pipeline_state.drawid_broken)
-      ctx->dirty_shader_stages |= BITFIELD_BIT(PIPE_SHADER_VERTEX);
+      drawid_broken = (drawid_offset != 0 ||
+                      (!HAS_MULTIDRAW && num_draws > 1) ||
+                      (HAS_MULTIDRAW && num_draws > 1 && !dinfo->increment_draw_id));
+   if (drawid_broken != zink_get_last_vertex_key(ctx)->push_drawid)
+      zink_set_last_vertex_key(ctx)->push_drawid = drawid_broken;
    ctx->gfx_pipeline_state.vertices_per_patch = vertices_per_patch;
    if (mode_changed) {
       bool points_changed = false;
@@ -508,7 +510,7 @@ zink_draw_vbo(struct pipe_context *pctx,
          points_changed = true;
       }
       if (points_changed && ctx->rast_state->base.point_quad_rasterization)
-         ctx->dirty_shader_stages |= BITFIELD_BIT(PIPE_SHADER_FRAGMENT);
+         zink_set_fs_point_coord_key(ctx);
    }
    ctx->gfx_pipeline_state.gfx_prim_mode = mode;
 
@@ -762,7 +764,7 @@ zink_draw_vbo(struct pipe_context *pctx,
       VKCTX(CmdBeginTransformFeedbackEXT)(batch->state->cmdbuf, 0, ctx->num_so_targets, counter_buffers, counter_buffer_offsets);
    }
 
-   bool needs_drawid = reads_drawid && ctx->gfx_pipeline_state.drawid_broken;
+   bool needs_drawid = reads_drawid && zink_get_last_vertex_key(ctx)->push_drawid;
    work_count += num_draws;
    if (index_size > 0) {
       if (dindirect && dindirect->buffer) {

src/gallium/drivers/zink/zink_framebuffer.c
@@ -160,7 +160,7 @@ zink_get_framebuffer_imageless(struct zink_context *ctx)
    state.width = MAX2(ctx->fb_state.width, 1);
    state.height = MAX2(ctx->fb_state.height, 1);
    state.layers = MAX2(util_framebuffer_get_num_layers(&ctx->fb_state), 1) - 1;
-   state.samples = ctx->gfx_pipeline_state.rast_samples;
+   state.samples = ctx->fb_state.samples - 1;
 
    struct zink_framebuffer *fb;
    struct hash_entry *entry = _mesa_hash_table_search(&ctx->framebuffer_cache, &state);
@@ -289,7 +289,7 @@ zink_get_framebuffer(struct zink_context *ctx)
    state.width = MAX2(ctx->fb_state.width, 1);
    state.height = MAX2(ctx->fb_state.height, 1);
    state.layers = MAX2(util_framebuffer_get_num_layers(&ctx->fb_state), 1) - 1;
-   state.samples = ctx->gfx_pipeline_state.rast_samples;
+   state.samples = ctx->fb_state.samples - 1;
 
    struct zink_framebuffer *fb;
    simple_mtx_lock(&screen->framebuffer_mtx);

src/gallium/drivers/zink/zink_pipeline.h
@@ -27,6 +27,7 @@
 #include <vulkan/vulkan.h>
 
 #include "pipe/p_state.h"
+#include "zink_shader_keys.h"
 #include "zink_state.h"
 
 struct zink_blend_state;
@@ -75,11 +76,10 @@ struct zink_gfx_pipeline_state {
    bool have_EXT_extended_dynamic_state;
    bool have_EXT_extended_dynamic_state2;
    uint8_t has_points; //either gs outputs points or prim type is points
-   uint8_t coord_replace_bits;
-   bool coord_replace_yinvert;
-   bool drawid_broken;
-   uint32_t decomposed_attrs;
-   uint32_t decomposed_attrs_without_w;
+   struct {
+      struct zink_shader_key key[5];
+      struct zink_shader_key last_vertex;
+   } shader_keys;
    struct zink_blend_state *blend_state;
    struct zink_render_pass *render_pass;
    VkPipeline pipeline;

joshuaswarren/mesa-1

commit f781538f58c10bc221179235a0277484f8d0d45d

zink: fix decomposed_attrs val error when zink_vs_key->size is 4
decomposed_attrs and decomposed_attrs_without_w should get from vs->u32
when zink_vs_key->size is 4.

Fixes: 48bfa00b237e ("zink: move shader keys to be persistent on pipeline state")
Signed-off-by: Lu Yao <yaolu@kylinos.cn>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33079>

src/gallium/drivers/zink/zink_state.c
@@ -204,8 +204,8 @@ zink_bind_vertex_elements_state(struct pipe_context *pctx,
          decomposed_attrs_without_w = vs->u16.decomposed_attrs_without_w;
          break;
       case 4:
-         decomposed_attrs = vs->u16.decomposed_attrs;
-         decomposed_attrs_without_w = vs->u16.decomposed_attrs_without_w;
+         decomposed_attrs = vs->u32.decomposed_attrs;
+         decomposed_attrs_without_w = vs->u32.decomposed_attrs_without_w;
          break;
       }
       if (ctx->element_state->decomposed_attrs != decomposed_attrs ||