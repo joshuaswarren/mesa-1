joshuaswarren/mesa-1

commit c2d94e9c38de3217791fcaffe46fc1217c0cbca3

v3d: avoid load/store of tile buffer on invalidated framebuffer
With the information of the invalidated framebuffer we avoid the stores
of the results of tile buffer rendering that are not going to be used
again or the loads of the tile buffer when the buffer data can be ignored
as it would be never read.

Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30341>

src/gallium/drivers/v3d/v3dx_draw.c
@@ -1357,14 +1357,29 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info,
                                 u_stream_outputs_for_vertices(info->mode, draws[0].count);
         }
 
-        uint32_t clear_mask = job->clear_tlb | job->clear_draw;
+        if (v3d->zsa && job->zsbuf) {
+                struct v3d_resource *rsc = v3d_resource(job->zsbuf->texture);
+                if (rsc->invalidated) {
+                        /* Currently gallium only applies invalidates if it
+                         * affects both depth and stencil together.
+                         */
+                        job->invalidated_load |=
+                                PIPE_CLEAR_DEPTH | PIPE_CLEAR_STENCIL;
+                        rsc->invalidated = false;
+                        if (rsc->separate_stencil)
+                                rsc->separate_stencil->invalidated = false;
+                }
+        }
+
+        uint32_t no_load_mask =
+                job->clear_tlb | job->clear_draw | job->invalidated_load;
         if (v3d->zsa && job->zsbuf && v3d->zsa->base.depth_enabled) {
                 struct v3d_resource *rsc = v3d_resource(job->zsbuf->texture);
                 v3d_job_add_bo(job, rsc->bo);
-                job->load |= PIPE_CLEAR_DEPTH & ~clear_mask;
+                job->load |= PIPE_CLEAR_DEPTH & ~no_load_mask;
                 if (v3d->zsa->base.depth_writemask)
                         job->store |= PIPE_CLEAR_DEPTH;
-                rsc->initialized_buffers = PIPE_CLEAR_DEPTH;
+                rsc->initialized_buffers |= PIPE_CLEAR_DEPTH;
         }
 
         if (v3d->zsa && job->zsbuf && v3d->zsa->base.stencil[0].enabled) {
@@ -1374,13 +1389,14 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info,
 
                 v3d_job_add_bo(job, rsc->bo);
 
-                job->load |= PIPE_CLEAR_STENCIL & ~clear_mask;
+                job->load |= PIPE_CLEAR_STENCIL & ~no_load_mask;
                 if (v3d->zsa->base.stencil[0].writemask ||
                     v3d->zsa->base.stencil[1].writemask) {
                         job->store |= PIPE_CLEAR_STENCIL;
                 }
                 rsc->initialized_buffers |= PIPE_CLEAR_STENCIL;
         }
+
 
         for (int i = 0; i < job->nr_cbufs; i++) {
                 uint32_t bit = PIPE_CLEAR_COLOR0 << i;
@@ -1390,7 +1406,12 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info,
                         continue;
                 struct v3d_resource *rsc = v3d_resource(job->cbufs[i]->texture);
 
-                job->load |= bit & ~clear_mask;
+                if (rsc->invalidated) {
+                        job->invalidated_load |= bit;
+                        rsc->invalidated = false;
+                } else {
+                        job->load |= bit & ~no_load_mask;
+                }
                 if (v3d->blend->base.rt[blend_rt].colormask)
                         job->store |= bit;
                 v3d_job_add_bo(job, rsc->bo);

src/gallium/drivers/v3d/v3d_context.c
@@ -118,6 +118,7 @@ v3d_invalidate_resource(struct pipe_context *pctx, struct pipe_resource *prsc)
         struct v3d_resource *rsc = v3d_resource(prsc);
 
         rsc->initialized_buffers = 0;
+        rsc->invalidated = true;
 
         struct hash_entry *entry = _mesa_hash_table_search(v3d->write_jobs,
                                                            prsc);
@@ -125,8 +126,17 @@ v3d_invalidate_resource(struct pipe_context *pctx, struct pipe_resource *prsc)
                 return;
 
         struct v3d_job *job = entry->data;
-        if (job->key.zsbuf && job->key.zsbuf->texture == prsc)
+        if (job->key.zsbuf && job->key.zsbuf->texture == prsc) {
                 job->store &= ~(PIPE_CLEAR_DEPTH | PIPE_CLEAR_STENCIL);
+                return;
+        }
+
+        for (int i = 0; i < job->nr_cbufs; i++) {
+                if (job->cbufs[i] && job->cbufs[i]->texture == prsc) {
+                        job->store &= ~(PIPE_CLEAR_COLOR0 << i);
+                        return;
+                }
+        }
 }
 
 /**

src/gallium/drivers/v3d/v3d_context.h
@@ -448,6 +448,10 @@ struct v3d_job {
          * clear.
          */
         uint32_t clear_draw;
+        /* Bitmask of PIPE_CLEAR_* of attached buffers that were invalidated
+         * by glInvalidateFramebuffer so we can avoid loading them.
+         */
+        uint32_t invalidated_load;
         /* Bitmask of PIPE_CLEAR_* of buffers that have been read by a draw
          * call without having been cleared first.
          */

src/gallium/drivers/v3d/v3d_resource.h
@@ -125,6 +125,12 @@ struct v3d_resource {
         uint32_t initialized_buffers;
 
         /**
+         * The resource has been invalidated by glInvalidateFramebuffer so
+         * it doesn't need to be loaded until another job writes to it.
+         */
+        bool invalidated;
+
+        /**
          * A serial ID that is incremented every time a new BO is bound to a
          * resource. We use this to track scenarios where we might need to
          * update other resources to point to the new BO (like sampler states

commit ae7b416130e2bb04163372345c85a0eb15ebf3a8

v3d: Apply FBO resources invalidations on job creation
We can handle invalidation of the FBO attachments at job
creation. It solves that we were skipping invalidations in jobs
that had been created by a clear call, as before this change
invalidations were only taken into account the first draw calls
of the job. In these cases where there is a clear after FB
invalidation the resource attachment was tracked as invalidated
for more time than expected. So the stores of the job with the
clear were not being loaded by the next job attaching it because
of the not correct application of the invalidation.

Fixes: c2d94e9c38d ("v3d: avoid load/store of tile buffer on invalidated framebuffer")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12456
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33148>

src/gallium/drivers/v3d/v3d_job.c
@@ -426,6 +426,10 @@ v3d_get_job_for_fbo(struct v3d_context *v3d)
                         struct v3d_resource *rsc = v3d_resource(cbufs[i]->texture);
                         if (!rsc->writes)
                                 job->clear_tlb |= PIPE_CLEAR_COLOR0 << i;
+                        if (rsc->invalidated) {
+                                job->invalidated_load |= PIPE_CLEAR_COLOR0 << i;
+                                rsc->invalidated = false;
+                        }
                 }
         }
 
@@ -439,6 +443,16 @@ v3d_get_job_for_fbo(struct v3d_context *v3d)
 
                 if (!rsc->writes)
                         job->clear_tlb |= PIPE_CLEAR_STENCIL;
+                if (rsc->invalidated) {
+                        /* Currently gallium only applies invalidates if it
+                         * affects both depth and stencil together.
+                         */
+                         job->invalidated_load |=
+                                 PIPE_CLEAR_DEPTH | PIPE_CLEAR_STENCIL;
+                        rsc->invalidated = false;
+                        if (rsc->separate_stencil)
+                                rsc->separate_stencil->invalidated = false;
+                }
         }
 
         job->tile_desc.draw_x = DIV_ROUND_UP(v3d->framebuffer.width,

src/gallium/drivers/v3d/v3dx_draw.c
@@ -1094,13 +1094,8 @@ v3d_update_job_tlb_load_store(struct v3d_job *job) {
                 if (job->store & bit || !job->cbufs[i])
                         continue;
                 struct v3d_resource *rsc = v3d_resource(job->cbufs[i]->texture);
+                job->load |= bit & ~no_load_mask;
 
-                if (rsc->invalidated) {
-                        job->invalidated_load |= bit;
-                        rsc->invalidated = false;
-                } else {
-                        job->load |= bit & ~no_load_mask;
-                }
                 if (v3d->blend->base.rt[blend_rt].colormask)
                         job->store |= bit;
                 v3d_job_add_bo(job, rsc->bo);
@@ -1399,20 +1394,6 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info,
                 for (int i = 0; i < v3d->streamout.num_targets; i++)
                         v3d_stream_output_target(v3d->streamout.targets[i])->offset +=
                                 u_stream_outputs_for_vertices(info->mode, draws[0].count);
-        }
-
-        if (v3d->zsa && job->zsbuf) {
-                struct v3d_resource *rsc = v3d_resource(job->zsbuf->texture);
-                if (rsc->invalidated) {
-                        /* Currently gallium only applies invalidates if it
-                         * affects both depth and stencil together.
-                         */
-                        job->invalidated_load |=
-                                PIPE_CLEAR_DEPTH | PIPE_CLEAR_STENCIL;
-                        rsc->invalidated = false;
-                        if (rsc->separate_stencil)
-                                rsc->separate_stencil->invalidated = false;
-                }
         }
 
         v3d_update_job_tlb_load_store(job);