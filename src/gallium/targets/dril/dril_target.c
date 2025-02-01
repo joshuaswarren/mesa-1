joshuaswarren/mesa-1

commit 23ea23f779ca116f567ea83054a9c62b4df50312

dril: use the super fallback path for software fallback
just in case gbm init fails somehow, swrast should still be able to
return some kind of fbconfigs

Fixes: c1277a0bd25 ("dril: always take the egl init path")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30979>

src/gallium/targets/dril/dril_target.c
@@ -435,8 +435,13 @@ drilCreateNewScreen(int scrn, int fd,
                     const __DRIconfig ***driver_configs, void *data)
 {
    const __DRIconfig **configs = init_dri2_configs(fd);
-   if (!configs)
-      return NULL;
+   if (!configs && fd == -1) {
+      // otherwise set configs to point to our config list
+      configs = calloc(ARRAY_SIZE(drilConfigs) + 1, sizeof(void *));
+      for (int i = 0; i < ARRAY_SIZE(drilConfigs); i++) {
+         configs[i] = mem_dup(&drilConfigs[i], sizeof(drilConfigs[i]));
+      }
+   }
 
    // outpointer it
    *driver_configs = configs;

commit 0261ab369a3739e2083aea5603c1e044578d8edf

dril: Check for null config in dril_target.c
fixes: 23ea23f7

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33336>

src/gallium/targets/dril/dril_target.c
@@ -453,7 +453,9 @@ drilCreateNewScreen(int scrn, int fd,
                     const __DRIconfig ***driver_configs, void *data)
 {
    const __DRIconfig **configs = init_dri2_configs(fd);
-   if (!configs && fd == -1) {
+   if (!configs) {
+      if (fd != -1)
+         return NULL;
       // otherwise set configs to point to our config list
       configs = calloc(ARRAY_SIZE(drilConfigs) * 2 + 1, sizeof(void *));
       int c = 0;