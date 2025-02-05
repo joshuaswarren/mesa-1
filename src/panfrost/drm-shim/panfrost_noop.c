joshuaswarren/mesa-1

commit 8d835dcb89395a199334daa45d872a86c08306fb

panfrost: Report default value for GROUP_PRIORITIES_INFO in drm-shim
Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31961>

src/panfrost/drm-shim/panfrost_noop.c
@@ -214,8 +214,9 @@ panthor_ioctl_dev_query(int fd, unsigned long request, void *arg)
       struct drm_panthor_group_priorities_info *priorities_info =
          (struct drm_panthor_group_priorities_info *)dev_query->pointer;
 
-      /* Noop values */
-      priorities_info->allowed_mask = 0;
+      /* Default values */
+      priorities_info->allowed_mask =
+         PANTHOR_GROUP_PRIORITY_LOW | PANTHOR_GROUP_PRIORITY_MEDIUM;
 
       return 0;
    }

commit 09634a1c641988d0745c6c303823b44488699d41

panfrost: Fix group priorities in drm-shim
Those were supposed to use BITFIELD_BIT.

Fixes: 8d835dcb8939 ("panfrost: Report default value for GROUP_PRIORITIES_INFO in drm-shim")
Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33371>

src/panfrost/drm-shim/panfrost_noop.c
@@ -216,7 +216,7 @@ panthor_ioctl_dev_query(int fd, unsigned long request, void *arg)
 
       /* Default values */
       priorities_info->allowed_mask =
-         PANTHOR_GROUP_PRIORITY_LOW | PANTHOR_GROUP_PRIORITY_MEDIUM;
+         BITFIELD_BIT(PANTHOR_GROUP_PRIORITY_LOW) | BITFIELD_BIT(PANTHOR_GROUP_PRIORITY_MEDIUM);
 
       return 0;
    }