joshuaswarren/mesa-1

commit 9634dd1fb3ec68c8548053abb1a45688292b31a9

radeonsi: add has_non_uniform_tex_access shader info
Can be used to skip nir_lower_non_uniform_access pass.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22573>

src/gallium/drivers/radeonsi/si_shader_info.c
@@ -426,6 +426,9 @@ static void scan_instruction(const struct nir_shader *nir, struct si_shader_info
          if (nir_deref_instr_has_indirect(nir_src_as_deref(*deref)))
             info->uses_indirect_descriptor = true;
       }
+
+      info->has_non_uniform_tex_access =
+         tex->texture_non_uniform || tex->sampler_non_uniform;
    } else if (instr->type == nir_instr_type_intrinsic) {
       nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
       const char *intr_name = nir_intrinsic_infos[intr->intrinsic].name;

src/gallium/drivers/radeonsi/si_shader.h
@@ -479,6 +479,7 @@ struct si_shader_info {
    bool uses_indirect_descriptor;
    bool has_divergent_loop;
    bool uses_sampleid;
+   bool has_non_uniform_tex_access;
 
    bool uses_vmem_sampler_or_bvh;
    bool uses_vmem_load_other; /* all other VMEM loads and atomics with return */

commit 02bc6f62b64d976eb19f409f36c442f1d23645ab

radeonsi: fix has_non_uniform_tex_access info
Fixes: 9634dd1fb3e ("radeonsi: add has_non_uniform_tex_access shader info")
Tested-by: Mike Lothian <mike@fireburn.co.uk>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33440>

src/gallium/drivers/radeonsi/si_shader_info.c
@@ -308,7 +308,7 @@ static void scan_instruction(const struct nir_shader *nir, struct si_shader_info
             info->uses_indirect_descriptor = true;
       }
 
-      info->has_non_uniform_tex_access =
+      info->has_non_uniform_tex_access |=
          tex->texture_non_uniform || tex->sampler_non_uniform;
 
       info->has_shadow_comparison |= tex->is_shadow;