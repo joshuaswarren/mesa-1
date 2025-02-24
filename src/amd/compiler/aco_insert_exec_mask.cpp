joshuaswarren/mesa-1

commit 52453202c6884e650a2983c8a62d51744ec0293c

aco/insert_exec_mask: don't restore exec in continue_or_break blocks
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33527>

src/amd/compiler/aco_insert_exec_mask.cpp
@@ -642,15 +642,12 @@ add_branch_code(exec_ctx& ctx, Block* block)
       assert(block->instructions.back()->opcode == aco_opcode::p_branch);
       block->instructions.pop_back();
 
-      bool need_parallelcopy = false;
-      while (!(ctx.info[idx].exec.back().type & mask_type_loop)) {
+      while (!(ctx.info[idx].exec.back().type & mask_type_loop))
          ctx.info[idx].exec.pop_back();
-         need_parallelcopy = true;
-      }
 
-      if (need_parallelcopy)
-         bld.copy(Definition(exec, bld.lm), ctx.info[idx].exec.back().op);
-      bld.branch(aco_opcode::p_cbranch_nz, Operand(exec, bld.lm), block->linear_succs[1],
+      Temp cond = bld.sopc(Builder::s_cmp_lg, bld.def(s1, scc), ctx.info[idx].exec.back().op,
+                           Operand::zero(bld.lm.bytes()));
+      bld.branch(aco_opcode::p_cbranch_nz, Operand(cond, scc), block->linear_succs[1],
                  block->linear_succs[0]);
    } else if (block->kind & block_kind_uniform) {
       Pseudo_branch_instruction& branch = block->instructions.back()->branch();

commit af3badac5de030fb634124226a62b6df9fb5ab74

aco/insert_exec: fix continue_or_break on gfx6-7
s_cmp_lg_u64 is gfx8+

Fixes: 52453202c68 ("aco/insert_exec_mask: don't restore exec in continue_or_break blocks")

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33715>

src/amd/compiler/aco_insert_exec_mask.cpp
@@ -645,8 +645,10 @@ add_branch_code(exec_ctx& ctx, Block* block)
       while (!(ctx.info[idx].exec.back().type & mask_type_loop))
          ctx.info[idx].exec.pop_back();
 
-      Temp cond = bld.sopc(Builder::s_cmp_lg, bld.def(s1, scc), ctx.info[idx].exec.back().op,
-                           Operand::zero(bld.lm.bytes()));
+      Temp cond = bld.sop2(Builder::s_or, bld.def(bld.lm), bld.def(s1, scc),
+                           ctx.info[idx].exec.back().op, Operand::zero(bld.lm.bytes()))
+                     .def(1)
+                     .getTemp();
       bld.branch(aco_opcode::p_cbranch_nz, Operand(cond, scc), block->linear_succs[1],
                  block->linear_succs[0]);
    } else if (block->kind & block_kind_uniform) {