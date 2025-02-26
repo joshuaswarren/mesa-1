joshuaswarren/mesa-1

commit de14be21c4798b6e1e54e3907e731cb77a70c864

winsys/amdgpu: don't add fence dependency of other queues for userq
In case of userq, there will be only 1 userq per process. So all the jobs for
that process goes into single queue. Hence there is no need to add fence
of other queues even if info num_queues is > 1.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29010>

src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp
@@ -1620,8 +1620,17 @@ static void amdgpu_cs_submit_ib(void *job, void *gdata, int thread_index)
       struct amdgpu_fence *prev_fence =
          (struct amdgpu_fence*)queue->fences[prev_seq_no % AMDGPU_FENCE_RING_SIZE];
 
-      if (prev_fence && (aws->info.ip[acs->ip_type].num_queues > 1 || queue->last_ctx != acs->ctx))
-         add_seq_no_to_list(aws, &seq_no_dependencies, queue_index, prev_seq_no);
+      /* Add a dependency on a previous fence, unless we can determine that
+       * it's useless because the execution order is guaranteed.
+       */
+      if (prev_fence) {
+         bool same_ctx = queue->last_ctx == acs->ctx;
+         /* userqueue submission mode uses a single queue per process. */
+         bool same_queue = aws->info.ip[acs->ip_type].num_queues > 1 &&
+                           queue_type != USERQ;
+         if (!same_ctx || !same_queue)
+            add_seq_no_to_list(aws, &seq_no_dependencies, queue_index, prev_seq_no);
+      }
    }
 
    /* Since the kernel driver doesn't synchronize execution between different

commit 1a157c51ad6d05ccd8b5edb44fb9cee806f6c68c

winsys/amdgpu: same_queue variable should be set if there is only one queue
Fixes: de14be21c47 ("winsys/amdgpu: don't add fence dependency of other queues for userq")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33661>

src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp
@@ -1653,8 +1653,8 @@ static void amdgpu_cs_submit_ib(void *job, void *gdata, int thread_index)
       if (prev_fence) {
          bool same_ctx = queue->last_ctx == acs->ctx;
          /* userqueue submission mode uses a single queue per process. */
-         bool same_queue = aws->info.ip[acs->ip_type].num_queues > 1 &&
-                           queue_type != USERQ;
+         bool same_queue = queue_type == USERQ || aws->info.ip[acs->ip_type].num_queues == 1;
+
          if (!same_ctx || !same_queue)
             add_seq_no_to_list(aws, &seq_no_dependencies, queue_index, prev_seq_no);
       }