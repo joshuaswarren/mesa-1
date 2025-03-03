joshuaswarren/mesa-1

commit 34e32a4b1749ea45765202ff1d9ed0ede4329dd5

anv: fix video profile lists
these didn't include dmabuf layout or mutable formats despite both
being supported

Reviewed-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31317>

src/intel/vulkan/anv_video.c
@@ -324,16 +324,33 @@ anv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
 
    vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
       p->format = VK_FORMAT_G8_B8R8_2PLANE_420_UNORM;
+      p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
       p->imageType = VK_IMAGE_TYPE_2D;
       p->imageTiling = VK_IMAGE_TILING_OPTIMAL;
+      p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+   }
+
+   vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
+      p->format = VK_FORMAT_G8_B8R8_2PLANE_420_UNORM;
+      p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
+      p->imageType = VK_IMAGE_TYPE_2D;
+      p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
       p->imageUsageFlags = pVideoFormatInfo->imageUsage;
    }
 
    if (need_10bit) {
       vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
          p->format = VK_FORMAT_G10X6_B10X6R10X6_2PLANE_420_UNORM_3PACK16;
+         p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
          p->imageType = VK_IMAGE_TYPE_2D;
          p->imageTiling = VK_IMAGE_TILING_OPTIMAL;
+         p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+      }
+      vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
+         p->format = VK_FORMAT_G10X6_B10X6R10X6_2PLANE_420_UNORM_3PACK16;
+         p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
+         p->imageType = VK_IMAGE_TYPE_2D;
+         p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
          p->imageUsageFlags = pVideoFormatInfo->imageUsage;
       }
    }

commit 99b2c597719a846756d2ce1e8ae37980a5ce3103

anv: Do not support the tiling of DRM modifier if DECODE_DST
Fixes: 34e32a4b ("anv: fix video profile lists");

Signed-off-by: Hyunjun Ko <zzoon@igalia.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33784>

src/intel/vulkan/anv_video.c
@@ -345,6 +345,14 @@ anv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
    const struct VkVideoProfileListInfoKHR *prof_list = (struct VkVideoProfileListInfoKHR *)
       vk_find_struct_const(pVideoFormatInfo->pNext, VIDEO_PROFILE_LIST_INFO_KHR);
 
+   /* We only support VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT with
+    * Y-tiling/Tile4, as supported by the hardware for video decoding.
+    * However, we are unable to determine the tiling without modifiers here.
+    * So just disable them all.
+    */
+   const bool decode_dst = !!(pVideoFormatInfo->imageUsage &
+                              VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR);
+
    if (prof_list) {
       for (unsigned i = 0; i < prof_list->profileCount; i++) {
          const VkVideoProfileInfoKHR *profile = &prof_list->pProfiles[i];
@@ -359,12 +367,14 @@ anv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
                p->imageUsageFlags = pVideoFormatInfo->imageUsage;
             }
 
-            vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
-               p->format = VK_FORMAT_G8_B8R8_2PLANE_420_UNORM;
-               p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
-               p->imageType = VK_IMAGE_TYPE_2D;
-               p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
-               p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+            if (!decode_dst) {
+               vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
+                  p->format = VK_FORMAT_G8_B8R8_2PLANE_420_UNORM;
+                  p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
+                  p->imageType = VK_IMAGE_TYPE_2D;
+                  p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
+                  p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+               }
             }
          }
 
@@ -377,12 +387,14 @@ anv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
                p->imageTiling = VK_IMAGE_TILING_OPTIMAL;
                p->imageUsageFlags = pVideoFormatInfo->imageUsage;
             }
-            vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
-               p->format = VK_FORMAT_G10X6_B10X6R10X6_2PLANE_420_UNORM_3PACK16;
-               p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
-               p->imageType = VK_IMAGE_TYPE_2D;
-               p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
-               p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+            if (!decode_dst) {
+               vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p) {
+                  p->format = VK_FORMAT_G10X6_B10X6R10X6_2PLANE_420_UNORM_3PACK16;
+                  p->imageCreateFlags = VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
+                  p->imageType = VK_IMAGE_TYPE_2D;
+                  p->imageTiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
+                  p->imageUsageFlags = pVideoFormatInfo->imageUsage;
+               }
             }
          }
       }