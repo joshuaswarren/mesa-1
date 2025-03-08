joshuaswarren/mesa-1

commit 5c90a87d866e0ed81d50c8498070e2c66fa2c523

lavapipe: Handle accel struct queries in handle_copy_query_pool_results
Fixes: 032394e ("lavapipe: Implement VK_KHR_acceleration_structure")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28716>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -2980,6 +2980,27 @@ static void handle_copy_query_pool_results(struct vk_cmd_queue_entry *cmd,
    unsigned result_size = copycmd->flags & VK_QUERY_RESULT_64_BIT ? 8 : 4;
    for (unsigned i = copycmd->first_query; i < copycmd->first_query + copycmd->query_count; i++) {
       unsigned offset = copycmd->dst_offset + (copycmd->stride * (i - copycmd->first_query));
+
+      if (pool->base_type >= PIPE_QUERY_TYPES) {
+         struct pipe_transfer *transfer;
+         uint8_t *map = pipe_buffer_map(state->pctx, lvp_buffer_from_handle(copycmd->dst_buffer)->bo, PIPE_MAP_WRITE, &transfer);
+         map += offset;
+
+         if (flags & VK_QUERY_RESULT_64_BIT) {
+            uint64_t *dst = (uint64_t *)map;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = src[i];
+         } else {
+            uint32_t *dst = (uint32_t *)map;
+            uint64_t *src = (uint64_t *)pool->data;
+            *dst = (uint32_t) (src[i] & UINT32_MAX);
+         }
+
+         state->pctx->buffer_unmap(state->pctx, transfer);
+
+         continue;
+      }
+
       if (pool->queries[i]) {
          unsigned num_results = 0;
          if (copycmd->flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT) {

commit 3d559ad5e2e78e70cebd523fd7ffd85703a26fb0

lavapipe: fix accel struct device query copy
This change:
1. use vulkan flags instead of pipe query flags
2. set the avail bit when requested

Fixes: 5c90a87d866 ("lavapipe: Handle accel struct queries in handle_copy_query_pool_results")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33951>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -2965,14 +2965,18 @@ static void handle_copy_query_pool_results(struct vk_cmd_queue_entry *cmd,
          uint8_t *map = pipe_buffer_map(state->pctx, lvp_buffer_from_handle(copycmd->dst_buffer)->bo, PIPE_MAP_WRITE, &transfer);
          map += offset;
 
-         if (flags & VK_QUERY_RESULT_64_BIT) {
+         if (copycmd->flags & VK_QUERY_RESULT_64_BIT) {
             uint64_t *dst = (uint64_t *)map;
             uint64_t *src = (uint64_t *)pool->data;
             *dst = src[i];
+            if (copycmd->flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
+               *(dst + 1) = 1;
          } else {
             uint32_t *dst = (uint32_t *)map;
             uint64_t *src = (uint64_t *)pool->data;
             *dst = (uint32_t) (src[i] & UINT32_MAX);
+            if (copycmd->flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
+               *(dst + 1) = 1;
          }
 
          state->pctx->buffer_unmap(state->pctx, transfer);