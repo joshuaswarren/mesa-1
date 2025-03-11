joshuaswarren/mesa-1

commit 0b72dbb56ddba4f673638d8c846c4a38220bdf25

panvk: enable subgroupExtendedTypes
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33365>

src/panfrost/vulkan/panvk_physical_device.c
@@ -219,6 +219,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .KHR_shader_non_semantic_info = true,
       .KHR_shader_quad_control = arch >= 10, /* requires vk1.1 */
       .KHR_shader_relaxed_extended_instruction = true,
+      .KHR_shader_subgroup_extended_types = true,
       .KHR_shader_subgroup_rotate = true,
       .KHR_shader_subgroup_uniform_control_flow = arch >= 10, /* requires vk1.1 */
       .KHR_storage_buffer_storage_class = true,
@@ -348,7 +349,7 @@ get_features(const struct panvk_physical_device *device,
       .scalarBlockLayout = true,
       .imagelessFramebuffer = true,
       .uniformBufferStandardLayout = true,
-      .shaderSubgroupExtendedTypes = false,
+      .shaderSubgroupExtendedTypes = true,
       .separateDepthStencilLayouts = true,
       .hostQueryReset = true,
       .timelineSemaphore = true,

commit 2f32580c7826ebac488fbf62e0263dfbb9bde4f4

panvk: fix extension requirement
This extension requires Vulkan 1.1, which we don't yet expose on Bifrost
GPUs.

Fixes: 0b72dbb56dd ("panvk: enable subgroupExtendedTypes")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33971>

src/panfrost/vulkan/panvk_physical_device.c
@@ -219,7 +219,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .KHR_shader_non_semantic_info = true,
       .KHR_shader_quad_control = arch >= 10, /* requires vk1.1 */
       .KHR_shader_relaxed_extended_instruction = true,
-      .KHR_shader_subgroup_extended_types = true,
+      .KHR_shader_subgroup_extended_types = arch >= 10, /* requires v1.1 */
       .KHR_shader_subgroup_rotate = true,
       .KHR_shader_subgroup_uniform_control_flow = arch >= 10, /* requires vk1.1 */
       .KHR_storage_buffer_storage_class = true,