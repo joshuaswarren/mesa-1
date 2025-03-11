joshuaswarren/mesa-1

commit 351caca1d89867360ea850504009b1f21ac822b6

intel/dev: reduce warning noise from urb settings
This sets up the min value as if stage was active, later on
we set this to zero if such is not the case.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12141
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33353>

src/intel/dev/intel_device_info.c
@@ -447,6 +447,14 @@ static const struct intel_device_info intel_device_info_hsw_gt3 = {
    .timestamp_frequency = 12500000,                 \
    .max_constant_urb_size_kb = 32
 
+#define GFX8_URB_MIN_ENTRIES                          \
+   .min_entries = {                                   \
+      [MESA_SHADER_VERTEX]    = 64,                   \
+      [MESA_SHADER_TESS_CTRL] = 1,                    \
+      [MESA_SHADER_TESS_EVAL] = 34,                   \
+      [MESA_SHADER_GEOMETRY]  = 2,                    \
+   }
+
 static const struct intel_device_info intel_device_info_bdw_gt1 = {
    GFX8_FEATURES, .gt = 1,
    .platform = INTEL_PLATFORM_BDW,
@@ -456,10 +464,7 @@ static const struct intel_device_info intel_device_info_bdw_gt1 = {
    .l3_banks = 2,
    .max_cs_threads = 42,
    .urb = {
-      .min_entries = {
-         [MESA_SHADER_VERTEX]    = 64,
-         [MESA_SHADER_TESS_EVAL] = 34,
-      },
+      GFX8_URB_MIN_ENTRIES,
       .max_entries = {
          [MESA_SHADER_VERTEX]    = 2560,
          [MESA_SHADER_TESS_CTRL] = 504,
@@ -480,10 +485,7 @@ static const struct intel_device_info intel_device_info_bdw_gt2 = {
    .l3_banks = 4,
    .max_cs_threads = 56,
    .urb = {
-      .min_entries = {
-         [MESA_SHADER_VERTEX]    = 64,
-         [MESA_SHADER_TESS_EVAL] = 34,
-      },
+      GFX8_URB_MIN_ENTRIES,
       .max_entries = {
          [MESA_SHADER_VERTEX]    = 2560,
          [MESA_SHADER_TESS_CTRL] = 504,
@@ -503,10 +505,7 @@ static const struct intel_device_info intel_device_info_bdw_gt3 = {
    .l3_banks = 8,
    .max_cs_threads = 56,
    .urb = {
-      .min_entries = {
-         [MESA_SHADER_VERTEX]    = 64,
-         [MESA_SHADER_TESS_EVAL] = 34,
-      },
+      GFX8_URB_MIN_ENTRIES,
       .max_entries = {
          [MESA_SHADER_VERTEX]    = 2560,
          [MESA_SHADER_TESS_CTRL] = 504,
@@ -556,10 +555,7 @@ static const struct intel_device_info intel_device_info_chv = {
    .max_cs_threads = 56,                            \
    .timestamp_frequency = 12000000,                 \
    .urb = {                                         \
-      .min_entries = {                              \
-         [MESA_SHADER_VERTEX]    = 64,              \
-         [MESA_SHADER_TESS_EVAL] = 34,              \
-      },                                            \
+      GFX8_URB_MIN_ENTRIES,                         \
       .max_entries = {                              \
          [MESA_SHADER_VERTEX]    = 1856,            \
          [MESA_SHADER_TESS_CTRL] = 672,             \
@@ -867,10 +863,7 @@ static const struct intel_device_info intel_device_info_cfl_gt3 = {
    }
 
 #define GFX11_URB_MIN_MAX_ENTRIES                     \
-   .min_entries = {                                   \
-      [MESA_SHADER_VERTEX]    = 64,                   \
-      [MESA_SHADER_TESS_EVAL] = 34,                   \
-   },                                                 \
+   GFX8_URB_MIN_ENTRIES,                              \
    .max_entries = {                                   \
       [MESA_SHADER_VERTEX]    = 2384,                 \
       [MESA_SHADER_TESS_CTRL] = 1032,                 \
@@ -965,10 +958,7 @@ static const struct intel_device_info intel_device_info_ehl_2x4 = {
    .max_cs_threads = 112, /* threads per DSS */     \
    .urb = {                                         \
       .size = 512, /* For intel_stub_gpu */         \
-      .min_entries = {                              \
-         [MESA_SHADER_VERTEX]    = 64,              \
-         [MESA_SHADER_TESS_EVAL] = 34,              \
-      },                                            \
+      GFX8_URB_MIN_ENTRIES,                         \
       .max_entries = {                              \
          [MESA_SHADER_VERTEX]    = 3576,            \
          [MESA_SHADER_TESS_CTRL] = 1548,            \
@@ -1082,10 +1072,7 @@ static const struct intel_device_info intel_device_info_sg1 = {
 };
 
 #define XEHP_URB_MIN_MAX_ENTRIES                        \
-   .min_entries = {                                     \
-      [MESA_SHADER_VERTEX]    = 64,                     \
-      [MESA_SHADER_TESS_EVAL] = 34,                     \
-   },                                                   \
+   GFX8_URB_MIN_ENTRIES,                                \
    .max_entries = {                                     \
       [MESA_SHADER_VERTEX]    = 3832, /* BSpec 47138 */ \
       [MESA_SHADER_TESS_CTRL] = 1548, /* BSpec 47137 */ \

commit ba5472335bbd14ad86d62b576ca119dfa42b07c2

intel/dev: Set minimum HS URB entries to 0.
The documentation for 3DSTATE_URB_HS has 0 as the minimum number of HS
URB entries for all platforms.  See BSpecs 32162, 47137, 56271 for
Gfx6-11, Xe, and Xe2-3, respectively.

This should silence warnings about our device info field not matching
the hwconfig tables.

Notably, nothing in our drivers currently uses this value so it cannot
have a functional impact.

Fixes: 351caca1d898 ("intel/dev: reduce warning noise from urb settings")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33764>

src/intel/dev/intel_device_info.c
@@ -439,7 +439,6 @@ static const struct intel_device_info intel_device_info_hsw_gt3 = {
 #define GFX8_URB_MIN_ENTRIES                          \
    .min_entries = {                                   \
       [MESA_SHADER_VERTEX]    = 64,                   \
-      [MESA_SHADER_TESS_CTRL] = 1,                    \
       [MESA_SHADER_TESS_EVAL] = 34,                   \
       [MESA_SHADER_GEOMETRY]  = 2,                    \
    }