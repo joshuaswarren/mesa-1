joshuaswarren/mesa-1

commit 22295543ed8cb8a6638d44a32132af3f879fa61e

nir: Serialize all parameter attributes
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33246>

src/compiler/nir/nir_serialize.c
@@ -2001,6 +2001,8 @@ write_function(write_ctx *ctx, const nir_function *fxn)
       if (has_name)
          val |= 0x10000;
 
+      if (fxn->params[i].is_return)
+         val |= (1u << 17);
       blob_write_uint32(ctx->blob, val);
       if (has_name)
          blob_write_string(ctx->blob, fxn->params[i].name);
@@ -2050,6 +2052,7 @@ read_function(read_ctx *ctx)
 
       fxn->params[i].num_components = val & 0xff;
       fxn->params[i].bit_size = (val >> 8) & 0xff;
+      fxn->params[i].is_return = val & (1u << 16);
       fxn->params[i].type = decode_type_from_blob(ctx->blob);
       fxn->params[i].mode = decode_deref_modes(blob_read_uint32(ctx->blob));
    }

commit 104799b4f2601b7611c24b4a22ef4e1f5f33d55d

nir/serialize: fix decoding of is_return and is_uniform
Fixes: 22295543ed8 ("nir: Serialize all parameter attributes")
Fixes: fd94fef98dc ("nir: Add parameter divergence info")

Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34052>

src/compiler/nir/nir_serialize.c
@@ -2062,8 +2062,8 @@ read_function(read_ctx *ctx)
 
       fxn->params[i].num_components = val & 0xff;
       fxn->params[i].bit_size = (val >> 8) & 0xff;
-      fxn->params[i].is_return = val & (1u << 16);
-      fxn->params[i].is_uniform = val & (1u << 17);
+      fxn->params[i].is_return = val & (1u << 17);
+      fxn->params[i].is_uniform = val & (1u << 18);
       fxn->params[i].type = decode_type_from_blob(ctx->blob);
       fxn->params[i].mode = decode_deref_modes(blob_read_uint32(ctx->blob));
       fxn->params[i].driver_attributes = blob_read_uint32(ctx->blob);