joshuaswarren/mesa-1

commit 851845da9516497a01bb8f3f0ebb1b98566dc2f4

ir3: Set need_full_quad depending on info.fs.require_full_quads
The info from NIR is more granular, so that we don't have to
enable full quad for coarse derivatives.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33801>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -5775,6 +5775,10 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
    if (so->type == MESA_SHADER_FRAGMENT &&
        ctx->s->info.fs.needs_quad_helper_invocations) {
       so->need_pixlod = true;
+   }
+
+   if (so->type == MESA_SHADER_FRAGMENT &&
+       ctx->s->info.fs.require_full_quads) {
       so->need_full_quad = true;
    }
 

commit e2bd208e5536f76618d2b60a24312f958f5ec7d1

ir3: Use needs_full_helper_invocations
require_full_quads is incorrect because it isn't actually enabled by
quad operations. Use the newly-introduced needs_quad_helper_invocations
instead.

Reviewed-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Fixes: 851845da951 ("ir3: Set need_full_quad depending on info.fs.require_full_quads")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33862>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -5750,7 +5750,7 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
    }
 
    if (so->type == MESA_SHADER_FRAGMENT &&
-       ctx->s->info.fs.require_full_quads) {
+       ctx->s->info.fs.needs_full_quad_helper_invocations) {
       so->need_full_quad = true;
    }
 