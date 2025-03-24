joshuaswarren/mesa-1

commit 3789be651e8c5064abd1ffab5f65ee5e26435974

panvk/csf: Pass a non-zero flush-id to benefit from cache flush reduction
The cache flush reduction mechanism relies on the flush ID to
conditionally eliminate cache flush requests if another cache flush
happened between the time the flush ID was retrieved and the time
the flush operation happens.

Store the current flush ID at EndCommandBuffer() time, and pass it
back to the submit ioctl().

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31230>

src/panfrost/vulkan/csf/panvk_vX_cmd_buffer.c
@@ -148,6 +148,7 @@ VKAPI_ATTR VkResult VKAPI_CALL
 panvk_per_arch(EndCommandBuffer)(VkCommandBuffer commandBuffer)
 {
    VK_FROM_HANDLE(panvk_cmd_buffer, cmdbuf, commandBuffer);
+   struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
 
    emit_tls(cmdbuf);
 
@@ -161,6 +162,8 @@ panvk_per_arch(EndCommandBuffer)(VkCommandBuffer commandBuffer)
          finish_cs(cmdbuf, i);
       }
    }
+
+   cmdbuf->flush_id = panthor_kmod_get_flush_id(dev->kmod.dev);
 
    return vk_command_buffer_end(&cmdbuf->vk);
 }

src/panfrost/vulkan/csf/panvk_vX_queue.c
@@ -251,7 +251,7 @@ init_subqueue(struct panvk_queue *queue, enum panvk_subqueue_id subqueue)
       .queue_index = subqueue,
       .stream_size = cs_root_chunk_size(&b),
       .stream_addr = cs_root_chunk_gpu_addr(&b),
-      .latest_flush = 0,
+      .latest_flush = panthor_kmod_get_flush_id(dev->kmod.dev),
       .syncs = DRM_PANTHOR_OBJ_ARRAY(1, &syncop),
    };
    struct drm_panthor_group_submit gsubmit = {
@@ -568,7 +568,7 @@ panvk_queue_submit(struct vk_queue *vk_queue, struct vk_queue_submit *submit)
             .queue_index = j,
             .stream_size = cs_root_chunk_size(&cmdbuf->state.cs[j].builder),
             .stream_addr = cs_root_chunk_gpu_addr(&cmdbuf->state.cs[j].builder),
-            .latest_flush = 0,
+            .latest_flush = cmdbuf->flush_id,
          };
       }
    }

commit fde56ada8ed62a1ddf918bc820188cc2268b19e2

panvk/csf: rework cache flush reduction
Per Vulkan spec 7.9. Host Write Ordering Guarantees, queue submission
commands automatically perform a domain operation from host to device
for all writes performed before the command executes. That is to say,
host updates to the mappings can occur after the end of the command
recording and must be flushed implicitly at submission boundary.

Before this change, necessary cache flushes could be missed once the
app starts reusing pre-recorded command buffers. e.g. a simple buffer
copy cmd while the app only updates the source buffer mapping in
different submissions. This changes backs out most of the current
version of cache flush reduction while still assigning LATEST_FLUSH_ID
to at least the final batch itself. This aligns with panfrost_batch
submit behavior on the gallium side.

Test: dEQP-VK.synchronization*.timeline_semaphore.* pass w/o flakiness
      via venus-on-panvk

Fixes: 3789be651e8 ("panvk/csf: Pass a non-zero flush-id to benefit from cache flush reduction")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34093>

src/panfrost/vulkan/csf/panvk_cmd_buffer.h
@@ -362,8 +362,6 @@ struct panvk_cmd_buffer {
    struct panvk_pool tls_pool;
    struct list_head push_sets;
 
-   uint32_t flush_id;
-
    struct {
       struct u_trace uts[PANVK_SUBQUEUE_COUNT];
    } utrace;

src/panfrost/vulkan/csf/panvk_vX_cmd_buffer.c
@@ -192,7 +192,6 @@ VKAPI_ATTR VkResult VKAPI_CALL
 panvk_per_arch(EndCommandBuffer)(VkCommandBuffer commandBuffer)
 {
    VK_FROM_HANDLE(panvk_cmd_buffer, cmdbuf, commandBuffer);
-   struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
 
    emit_tls(cmdbuf);
    flush_sync_points(cmdbuf);
@@ -207,8 +206,6 @@ panvk_per_arch(EndCommandBuffer)(VkCommandBuffer commandBuffer)
          finish_cs(cmdbuf, i);
       }
    }
-
-   cmdbuf->flush_id = panthor_kmod_get_flush_id(dev->kmod.dev);
 
    return vk_command_buffer_end(&cmdbuf->vk);
 }

src/panfrost/vulkan/csf/panvk_vX_queue.c
@@ -939,7 +939,7 @@ panvk_queue_submit_init_cmdbufs(struct panvk_queue_submit *submit,
                .queue_index = j,
                .stream_size = cs_root_chunk_size(b),
                .stream_addr = cs_root_chunk_gpu_addr(b),
-               .latest_flush = cmdbuf->flush_id,
+               .latest_flush = panthor_kmod_get_flush_id(dev->kmod.dev),
             };
       }
 