joshuaswarren/mesa-1

commit e211fd9b1c9a2d90afe118d4275e5c6638b5eb67

nak: Assert instr_sched matches calc_instr_deps
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32311>

src/nouveau/compiler/nak/calc_instr_deps.rs
@@ -341,7 +341,8 @@ fn assign_barriers(f: &mut Function, sm: &dyn ShaderModel) {
     }
 }
 
-fn calc_delays(f: &mut Function, sm: &dyn ShaderModel) {
+fn calc_delays(f: &mut Function, sm: &dyn ShaderModel) -> u32 {
+    let mut min_num_static_cycles = 0;
     for b in f.blocks.iter_mut().rev() {
         let mut cycle = 0_u32;
 
@@ -446,6 +447,7 @@ fn calc_delays(f: &mut Function, sm: &dyn ShaderModel) {
 
             cycle = min_start;
         }
+        min_num_static_cycles += cycle;
     }
 
     // It's unclear exactly why but the blob inserts a Nop with a delay of 2
@@ -464,6 +466,8 @@ fn calc_delays(f: &mut Function, sm: &dyn ShaderModel) {
             MappedInstrs::One(instr)
         }
     });
+
+    min_num_static_cycles
 }
 
 impl Shader<'_> {
@@ -502,10 +506,17 @@ impl Shader<'_> {
         if DEBUG.serial() {
             self.assign_deps_serial();
         } else {
+            let mut min_num_static_cycles = 0;
             for f in &mut self.functions {
                 assign_barriers(f, self.sm);
-                calc_delays(f, self.sm);
+                min_num_static_cycles += calc_delays(f, self.sm);
             }
+
+            // Check that this file's model matches up with the model in
+            // opt_instr_sched_postpass. postpass includes an estimate of
+            // variable latency delays in its count, so we expect it to be >=
+            // the estimate here
+            assert!(self.info.num_static_cycles >= min_num_static_cycles);
         }
     }
 }

commit 99137395ded3035e70187f048f33f989897dd533

nak: Put the cycle count assert behind a debug flag
Fixes: e211fd9b1c9a ("nak: Assert instr_sched matches calc_instr_deps")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33573>

src/nouveau/compiler/nak/api.rs
@@ -25,6 +25,7 @@ enum DebugFlags {
     Spill,
     Annotate,
     NoUgpr,
+    Cycles,
 }
 
 pub struct Debug {
@@ -49,6 +50,7 @@ impl Debug {
                 "spill" => flags |= 1 << DebugFlags::Spill as u8,
                 "annotate" => flags |= 1 << DebugFlags::Annotate as u8,
                 "nougpr" => flags |= 1 << DebugFlags::NoUgpr as u8,
+                "cycles" => flags |= 1 << DebugFlags::Cycles as u8,
                 unk => eprintln!("Unknown NAK_DEBUG flag \"{}\"", unk),
             }
         }
@@ -77,6 +79,10 @@ pub trait GetDebugFlags {
 
     fn no_ugpr(&self) -> bool {
         self.debug_flags() & (1 << DebugFlags::NoUgpr as u8) != 0
+    }
+
+    fn cycles(&self) -> bool {
+        self.debug_flags() & (1 << DebugFlags::Cycles as u8) != 0
     }
 }
 

src/nouveau/compiler/nak/calc_instr_deps.rs
@@ -512,11 +512,16 @@ impl Shader<'_> {
                 min_num_static_cycles += calc_delays(f, self.sm);
             }
 
-            // Check that this file's model matches up with the model in
-            // opt_instr_sched_postpass. postpass includes an estimate of
-            // variable latency delays in its count, so we expect it to be >=
-            // the estimate here
-            assert!(self.info.num_static_cycles >= min_num_static_cycles);
+            if DEBUG.cycles() {
+                // This is useful for debugging differences in the scheduler
+                // cycle count model and the calc_delays() model.  However, it
+                // isn't totally valid since assign_barriers() can add extra
+                // dependencies for barrier re-use and those may add cycles.
+                // The chances of it doing this are low, thanks to our LRU
+                // allocation strategy, but it's still not an assert we want
+                // running in production.
+                assert!(self.info.num_static_cycles >= min_num_static_cycles);
+            }
         }
     }
 }