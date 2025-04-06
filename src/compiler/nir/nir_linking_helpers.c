joshuaswarren/mesa-1

commit dacf57983475366c525d9e45ee9919cdc5daea77

nir: fix uniform cloning helper
glsl allows for ubos to have the same name but different bindings.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Fixes: c304201fd0ed ("nir: expose reusable linking helpers for cloning uniform loads")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12852
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34138>

src/compiler/nir/nir_linking_helpers.c
@@ -1171,7 +1171,9 @@ nir_clone_uniform_variable(nir_shader *nir, nir_variable *uniform, bool spirv)
    nir_foreach_variable_with_modes(v, nir, uniform->data.mode) {
       if ((spirv && uniform->data.mode & nir_var_mem_ubo &&
            v->data.binding == uniform->data.binding) ||
-          (!spirv && !strcmp(uniform->name, v->name))) {
+          (!spirv &&
+           ((!strcmp(uniform->name, v->name) && !uniform->data.explicit_binding && !v->data.explicit_binding) ||
+            (uniform->data.explicit_binding && v->data.explicit_binding && v->data.binding == uniform->data.binding)))) {
          new_var = v;
          break;
       }

commit 5569cdc6130da4786074a1a50eaa3f6a0fa61969

glsl: fix regression in ubo cloning
Fixes KHR-GL46.layout_binding.block_layout_binding_block_VertexShader
with radeonsi.

Fixes: dacf57983475 ("nir: fix uniform cloning helper")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34337>

src/compiler/nir/nir_linking_helpers.c
@@ -1172,8 +1172,9 @@ nir_clone_uniform_variable(nir_shader *nir, nir_variable *uniform, bool spirv)
       if ((spirv && uniform->data.mode & nir_var_mem_ubo &&
            v->data.binding == uniform->data.binding) ||
           (!spirv &&
-           ((!strcmp(uniform->name, v->name) && !uniform->data.explicit_binding && !v->data.explicit_binding) ||
-            (uniform->data.explicit_binding && v->data.explicit_binding && v->data.binding == uniform->data.binding)))) {
+           (!strcmp(uniform->name, v->name) &&
+            uniform->data.explicit_binding == v->data.explicit_binding &&
+            uniform->data.binding == v->data.binding))) {
          new_var = v;
          break;
       }