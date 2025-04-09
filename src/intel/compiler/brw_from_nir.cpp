joshuaswarren/mesa-1

commit c5df0d832ad38aac23fae4ed466d844bb5ebdb1f

brw/nir: Fix source handling of nir_intrinsic_load_barycentric_at_offset
The source of nir_intrinsic_load_barycentric_at_offset is a vector, so
-1 should be passed to get_nir_src. This is also done for texture
sampling intrinsics.

I skimmed the other user of get_nir_src, and I believe they are
correct. This one was just missed as LNL support landed an many, many
rebases of the original MR occurred.

v2: Fix another get_nir_src call. Suggested by Lionel.

Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com> [v1]
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Fixes: db1737d9fa2 ("brw/nir: Fix up handling of sources that might be convergent vectors")
Closes: #12464
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33007>

src/intel/compiler/brw_from_nir.cpp
@@ -4493,7 +4493,7 @@ brw_from_nir_emit_fs_intrinsic(nir_to_brw_state &ntb,
       if (devinfo->ver >= 20) {
          emit_pixel_interpolater_alu_at_offset(
             bld, dest,
-            retype(get_nir_src(ntb, instr->src[0]), BRW_TYPE_F),
+            retype(get_nir_src(ntb, instr->src[0], -1), BRW_TYPE_F),
             interpolation);
 
       } else if (nir_const_value *const_offset = nir_src_as_const_value(instr->src[0])) {
@@ -4539,7 +4539,7 @@ brw_from_nir_emit_fs_intrinsic(nir_to_brw_state &ntb,
       if (bary_intrin == nir_intrinsic_load_barycentric_at_offset ||
           bary_intrin == nir_intrinsic_load_barycentric_at_sample) {
          /* Use the result of the PI message. */
-         dst_xy = retype(get_nir_src(ntb, instr->src[0]), BRW_TYPE_F);
+         dst_xy = retype(get_nir_src(ntb, instr->src[0], -1), BRW_TYPE_F);
       } else {
          /* Use the delta_xy values computed from the payload */
          enum intel_barycentric_mode bary = brw_barycentric_mode(

commit 832d7028c7a94d9b70afd19890bec850b9add2f2

brw/nir: Use offset() for all uses of offs in emit_pixel_interpolater_alu_at_offset
This is necessary to appropriately uniformize the first component
access of a convergent vector. Without this, this is produced:

    load_payload(16) %18:D, 0d, 0d NoMask group0
    add(32) %21:F, %18+0.0:F, 0.5f
    add(32) %22:F, %18+2.0<0>:F, 0.5f

This is the correct code:

    load_payload(16) %18:D, 0d, 0d NoMask group0
    add(32) %21:F, %18+0.0<0>:F, 0.5f
    add(32) %22:F, %18+2.0<0>:F, 0.5f

Without c5df0d832ad, the code generated was more incorrect, but happened
to work for this test case:

    load_payload(16) %18:D, 0d, 0d NoMask group0
    add(32) %21:F, %18+0.0<0>:F, 0.5f
    add(32) %22:F, %18+0.4<0>:F, 0.5f

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: c5df0d832ad ("brw/nir: Fix source handling of nir_intrinsic_load_barycentric_at_offset")
Closes: #12969
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34427>

src/intel/compiler/brw_from_nir.cpp
@@ -2159,7 +2159,7 @@ emit_pixel_interpolater_alu_at_offset(const brw_builder &bld,
 
    /* Account for half-pixel X/Y coordinate offset. */
    const brw_reg off_x = bld.vgrf(BRW_TYPE_F);
-   bld.ADD(off_x, offs, brw_imm_f(0.5));
+   bld.ADD(off_x, offset(offs, bld, 0), brw_imm_f(0.5));
 
    const brw_reg off_y = bld.vgrf(BRW_TYPE_F);
    bld.ADD(off_y, offset(offs, bld, 1), brw_imm_f(0.5));