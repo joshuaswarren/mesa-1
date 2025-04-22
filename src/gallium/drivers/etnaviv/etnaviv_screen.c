joshuaswarren/mesa-1

commit 110f20b84cf2a3793dacc0b2883718d0fe8a1979

etnaviv: split dummy RT backing store from reloc
The dummy BO used as backing store for the dummy render target
can be reused for other usages where we don't care about the
actual content of the buffer. Split it out from the dummy RT
reloc to make this more clear.

Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32947>

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -91,8 +91,8 @@ etna_screen_destroy(struct pipe_screen *pscreen)
 {
    struct etna_screen *screen = etna_screen(pscreen);
 
-   if (screen->dummy_desc_reloc.bo)
-      etna_bo_del(screen->dummy_desc_reloc.bo);
+   if (screen->dummy_bo)
+      etna_bo_del(screen->dummy_bo);
 
    if (screen->dummy_rt_reloc.bo)
       etna_bo_del(screen->dummy_rt_reloc.bo);
@@ -1166,11 +1166,11 @@ etna_screen_create(struct etna_device *dev, struct etna_gpu *gpu,
 
 
    /* create dummy RT buffer, used when rendering with no color buffer */
-   screen->dummy_rt_reloc.bo = etna_bo_new(screen->dev, 64 * 64 * 4,
-                                           DRM_ETNA_GEM_CACHE_WC);
-   if (!screen->dummy_rt_reloc.bo)
+   screen->dummy_bo = etna_bo_new(screen->dev, 64 * 64 * 4, DRM_ETNA_GEM_CACHE_WC);
+   if (!screen->dummy_bo)
       goto fail;
 
+   screen->dummy_rt_reloc.bo = screen->dummy_bo;
    screen->dummy_rt_reloc.offset = 0;
    screen->dummy_rt_reloc.flags = ETNA_RELOC_READ | ETNA_RELOC_WRITE;
 

src/gallium/drivers/etnaviv/etnaviv_screen.h
@@ -67,6 +67,9 @@ struct etna_screen {
    struct etna_compiler *compiler;
    struct util_queue shader_compiler_queue;
 
+   /* dummy BO available to user that don't care about the content */
+   struct etna_bo *dummy_bo;
+
    /* dummy render target for GPUs that can't fully disable the color pipe */
    struct etna_reloc dummy_rt_reloc;
 

commit dfba8a88b0d25dd55cd19caf9a56cb5bb6ca6c90

etnaviv: Release screen->dummy_desc_reloc.bo
We are currently trying to release twice the same dummy BO, while
leaking the other one.

Fixes: 110f20b84cf2 ("etnaviv: split dummy RT backing store from reloc")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34627>

src/gallium/drivers/etnaviv/etnaviv_screen.c
@@ -95,8 +95,8 @@ etna_screen_destroy(struct pipe_screen *pscreen)
    if (screen->dummy_bo)
       etna_bo_del(screen->dummy_bo);
 
-   if (screen->dummy_rt_reloc.bo)
-      etna_bo_del(screen->dummy_rt_reloc.bo);
+   if (screen->dummy_desc_reloc.bo)
+      etna_bo_del(screen->dummy_desc_reloc.bo);
 
    if (screen->perfmon)
       etna_perfmon_del(screen->perfmon);