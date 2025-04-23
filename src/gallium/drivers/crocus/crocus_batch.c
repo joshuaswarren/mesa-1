joshuaswarren/mesa-1

commit 139ee32e00df286f91a68bb578c15f4d84f4227e

intel: Switch uint64_t intel_debug to a bitset
We are reaching our limit of adding flags to intel_debug
(apporaching 64 flags). Switch intel_debug to a bitset,
which gives us almost "unlimited" bits to use in the future.

v2(Michael Cheng): Fixed a few ci errors

Signed-off-by: Michael Cheng <michael.cheng@intel.com>
Reviewed-by: Casey Bowman <casey.g.bowman@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34596>

src/intel/dev/intel_debug.c
@@ -39,7 +39,7 @@
 #include "util/u_math.h"
 #include "c11/threads.h"
 
-uint64_t intel_debug = 0;
+BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_BIT_COUNT)] = {0};
 
 enum intel_no_x {
    DEBUG_NO16 = 16,
@@ -47,85 +47,80 @@ enum intel_no_x {
    DEBUG_NO32 = 39,
 };
 
-#define DEBUG_BIT(x) (1ull << (x))
 
 static const struct debug_control debug_control[] = {
-   { "tex",         DEBUG_BIT(DEBUG_TEXTURE) },
-   { "blit",        DEBUG_BIT(DEBUG_BLIT) },
-   { "fall",        DEBUG_BIT(DEBUG_PERF) },
-   { "perf",        DEBUG_BIT(DEBUG_PERF) },
-   { "perfmon",     DEBUG_BIT(DEBUG_PERFMON) },
-   { "bat",         DEBUG_BIT(DEBUG_BATCH) },
-   { "buf",         DEBUG_BIT(DEBUG_BUFMGR) },
-   { "fs",          DEBUG_BIT(DEBUG_WM) },
-   { "gs",          DEBUG_BIT(DEBUG_GS) },
-   { "sync",        DEBUG_BIT(DEBUG_SYNC) },
-   { "sf",          DEBUG_BIT(DEBUG_SF) },
-   { "submit",      DEBUG_BIT(DEBUG_SUBMIT) },
-   { "wm",          DEBUG_BIT(DEBUG_WM) },
-   { "urb",         DEBUG_BIT(DEBUG_URB) },
-   { "vs",          DEBUG_BIT(DEBUG_VS) },
-   { "clip",        DEBUG_BIT(DEBUG_CLIP) },
-   { "no16",        DEBUG_BIT(DEBUG_NO16) },
-   { "blorp",       DEBUG_BIT(DEBUG_BLORP) },
-   { "nodualobj",   DEBUG_BIT(DEBUG_NO_DUAL_OBJECT_GS) },
-   { "optimizer",   DEBUG_BIT(DEBUG_OPTIMIZER) },
-   { "ann",         DEBUG_BIT(DEBUG_ANNOTATION) },
-   { "no8",         DEBUG_BIT(DEBUG_NO8) },
-   { "no-oaconfig", DEBUG_BIT(DEBUG_NO_OACONFIG) },
-   { "spill_fs",    DEBUG_BIT(DEBUG_SPILL_FS) },
-   { "spill_vec4",  DEBUG_BIT(DEBUG_SPILL_VEC4) },
-   { "cs",          DEBUG_BIT(DEBUG_CS) },
-   { "hex",         DEBUG_BIT(DEBUG_HEX) },
-   { "nocompact",   DEBUG_BIT(DEBUG_NO_COMPACTION) },
-   { "hs",          DEBUG_BIT(DEBUG_TCS) },
-   { "tcs",         DEBUG_BIT(DEBUG_TCS) },
-   { "ds",          DEBUG_BIT(DEBUG_TES) },
-   { "tes",         DEBUG_BIT(DEBUG_TES) },
-   { "l3",          DEBUG_BIT(DEBUG_L3) },
-   { "do32",        DEBUG_BIT(DEBUG_DO32) },
-   { "norbc",       DEBUG_BIT(DEBUG_NO_CCS) },
-   { "noccs",       DEBUG_BIT(DEBUG_NO_CCS) },
-   { "nohiz",       DEBUG_BIT(DEBUG_NO_HIZ) },
-   { "color",       DEBUG_BIT(DEBUG_COLOR) },
-   { "reemit",      DEBUG_BIT(DEBUG_REEMIT) },
-   { "soft64",      DEBUG_BIT(DEBUG_SOFT64) },
-   { "bt",          DEBUG_BIT(DEBUG_BT) },
-   { "pc",          DEBUG_BIT(DEBUG_PIPE_CONTROL) },
-   { "nofc",        DEBUG_BIT(DEBUG_NO_FAST_CLEAR) },
-   { "no32",        DEBUG_BIT(DEBUG_NO32) },
-   { "shaders",     DEBUG_BIT(DEBUG_WM) | DEBUG_BIT(DEBUG_VS) | DEBUG_BIT(DEBUG_TCS) |
-                    DEBUG_BIT(DEBUG_TES) | DEBUG_BIT(DEBUG_GS) | DEBUG_BIT(DEBUG_CS) |
-                    DEBUG_BIT(DEBUG_RT) | DEBUG_BIT(DEBUG_TASK) | DEBUG_BIT(DEBUG_MESH) },
-   { "rt",          DEBUG_BIT(DEBUG_RT) },
-   { "rt_notrace",  DEBUG_BIT(DEBUG_RT_NO_TRACE) },
-   { "bvh_blas",        DEBUG_BIT(DEBUG_BVH_BLAS) },
-   { "bvh_tlas",        DEBUG_BIT(DEBUG_BVH_TLAS) },
-   { "bvh_blas_ir_hdr", DEBUG_BIT(DEBUG_BVH_BLAS_IR_HDR) },
-   { "bvh_tlas_ir_hdr", DEBUG_BIT(DEBUG_BVH_TLAS_IR_HDR) },
-   { "bvh_blas_ir_as",  DEBUG_BIT(DEBUG_BVH_BLAS_IR_AS) },
-   { "bvh_tlas_ir_as",  DEBUG_BIT(DEBUG_BVH_TLAS_IR_AS) },
-   { "bvh_no_build",    DEBUG_BIT(DEBUG_BVH_NO_BUILD) },
-   { "task",        DEBUG_BIT(DEBUG_TASK) },
-   { "mesh",        DEBUG_BIT(DEBUG_MESH) },
-   { "stall",       DEBUG_BIT(DEBUG_STALL) },
-   { "capture-all", DEBUG_BIT(DEBUG_CAPTURE_ALL) },
-   { "perf-symbol-names", DEBUG_BIT(DEBUG_PERF_SYMBOL_NAMES) },
-   { "swsb-stall",  DEBUG_BIT(DEBUG_SWSB_STALL) },
-   { "heaps",       DEBUG_BIT(DEBUG_HEAPS) },
-   { "isl",         DEBUG_BIT(DEBUG_ISL) },
-   { "sparse",      DEBUG_BIT(DEBUG_SPARSE) },
-   { "draw_bkp",    DEBUG_BIT(DEBUG_DRAW_BKP) },
-   { "bat-stats",   DEBUG_BIT(DEBUG_BATCH_STATS) },
-   { "reg-pressure", DEBUG_BIT(DEBUG_REG_PRESSURE) },
-   { "shader-print", DEBUG_BIT(DEBUG_SHADER_PRINT) },
-   { "cl-quiet",     DEBUG_BIT(DEBUG_CL_QUIET) },
-   { "no-send-gather", DEBUG_BIT(DEBUG_NO_SEND_GATHER) },
-   { "shaders-lineno", DEBUG_BIT(DEBUG_SHADERS_LINENO) },
-
+   { "tex",         DEBUG_TEXTURE },
+   { "blit",        DEBUG_BLIT },
+   { "fall",        DEBUG_PERF },
+   { "perf",        DEBUG_PERF },
+   { "perfmon",     DEBUG_PERFMON },
+   { "bat",         DEBUG_BATCH },
+   { "buf",         DEBUG_BUFMGR },
+   { "fs",          DEBUG_WM },
+   { "gs",          DEBUG_GS },
+   { "sync",        DEBUG_SYNC },
+   { "sf",          DEBUG_SF },
+   { "submit",      DEBUG_SUBMIT },
+   { "wm",          DEBUG_WM },
+   { "urb",         DEBUG_URB },
+   { "vs",          DEBUG_VS },
+   { "clip",        DEBUG_CLIP },
+   { "no16",        DEBUG_NO16 },
+   { "blorp",       DEBUG_BLORP },
+   { "nodualobj",   DEBUG_NO_DUAL_OBJECT_GS },
+   { "optimizer",   DEBUG_OPTIMIZER },
+   { "ann",         DEBUG_ANNOTATION },
+   { "no8",         DEBUG_NO8 },
+   { "no-oaconfig", DEBUG_NO_OACONFIG },
+   { "spill_fs",    DEBUG_SPILL_FS },
+   { "spill_vec4",  DEBUG_SPILL_VEC4 },
+   { "cs",          DEBUG_CS },
+   { "hex",         DEBUG_HEX },
+   { "nocompact",   DEBUG_NO_COMPACTION },
+   { "hs",          DEBUG_TCS },
+   { "tcs",         DEBUG_TCS },
+   { "ds",          DEBUG_TES },
+   { "tes",         DEBUG_TES },
+   { "l3",          DEBUG_L3 },
+   { "do32",        DEBUG_DO32 },
+   { "norbc",       DEBUG_NO_CCS },
+   { "noccs",       DEBUG_NO_CCS },
+   { "nohiz",       DEBUG_NO_HIZ },
+   { "color",       DEBUG_COLOR },
+   { "reemit",      DEBUG_REEMIT },
+   { "soft64",      DEBUG_SOFT64 },
+   { "bt",          DEBUG_BT },
+   { "pc",          DEBUG_PIPE_CONTROL },
+   { "nofc",        DEBUG_NO_FAST_CLEAR },
+   { "no32",        DEBUG_NO32 },
+   { "shaders",     DEBUG_WM },
+   { "rt",          DEBUG_RT },
+   { "rt_notrace",  DEBUG_RT_NO_TRACE },
+   { "bvh_blas",    DEBUG_BVH_BLAS },
+   { "bvh_tlas",    DEBUG_BVH_TLAS },
+   { "bvh_blas_ir_hdr", DEBUG_BVH_BLAS_IR_HDR },
+   { "bvh_tlas_ir_hdr", DEBUG_BVH_TLAS_IR_HDR },
+   { "bvh_blas_ir_as",  DEBUG_BVH_BLAS_IR_AS },
+   { "bvh_tlas_ir_as",  DEBUG_BVH_TLAS_IR_AS },
+   { "bvh_no_build",    DEBUG_BVH_NO_BUILD },
+   { "task",        DEBUG_TASK },
+   { "mesh",        DEBUG_MESH },
+   { "stall",       DEBUG_STALL },
+   { "capture-all", DEBUG_CAPTURE_ALL },
+   { "perf-symbol-names", DEBUG_PERF_SYMBOL_NAMES },
+   { "swsb-stall",  DEBUG_SWSB_STALL },
+   { "heaps",       DEBUG_HEAPS },
+   { "isl",         DEBUG_ISL },
+   { "sparse",      DEBUG_SPARSE },
+   { "draw_bkp",    DEBUG_DRAW_BKP },
+   { "bat-stats",   DEBUG_BATCH_STATS },
+   { "reg-pressure", DEBUG_REG_PRESSURE },
+   { "shader-print", DEBUG_SHADER_PRINT },
+   { "cl-quiet",     DEBUG_CL_QUIET },
+   { "no-send-gather", DEBUG_NO_SEND_GATHER },
+   { "shaders-lineno", DEBUG_SHADERS_LINENO },
    { NULL, 0 }
 };
-
 uint64_t intel_simd = 0;
 
 static const struct debug_control simd_control[] = {
@@ -210,9 +205,46 @@ uint32_t intel_debug_bkp_before_draw_count = 0;
 uint32_t intel_debug_bkp_after_draw_count = 0;
 
 static void
+parse_debug_bitset(const char *env, const struct debug_control *tbl)
+{
+   /* Check if env is NULL or empty */
+   if (!env || !*env)
+      return;
+
+   char *copy = strdup(env);
+   if (!copy)
+      return;
+
+   /* Tokenize the string by space or comma */
+   for (char *tok = strtok(copy, ", "); tok; tok = strtok(NULL, ", ")) {
+      /* Check for negation prefix, useful if user would like to disable certian flags */
+      bool negate = (*tok == '~' || *tok == '-');
+      if (negate)
+         tok++;
+
+      for (unsigned i = 0; tbl[i].string; i++) {
+         if (strcasecmp(tok, tbl[i].string) != 0)
+            continue;
+
+         unsigned bit = tbl[i].flag;
+
+         if (negate)
+            BITSET_CLEAR(intel_debug, bit);
+         else
+            BITSET_SET(intel_debug, bit);
+
+         break;
+      }
+   }
+   free(copy);
+}
+
+static void
 process_intel_debug_variable_once(void)
 {
-   intel_debug = parse_debug_string(getenv("INTEL_DEBUG"), debug_control);
+   BITSET_ZERO(intel_debug);
+   parse_debug_bitset(getenv("INTEL_DEBUG"), debug_control);
+
    intel_simd = parse_debug_string(getenv("INTEL_SIMD_DEBUG"), simd_control);
    intel_debug_batch_frame_start =
       debug_get_num_option("INTEL_DEBUG_BATCH_FRAME_START", 0);
@@ -235,13 +267,18 @@ process_intel_debug_variable_once(void)
    if (!(intel_simd & DEBUG_RT_SIMD))
       intel_simd |=   DEBUG_RT_SIMD;
 
-   if (intel_debug & DEBUG_NO8)
+   if (BITSET_TEST(intel_debug, DEBUG_NO8))
       intel_simd &= ~DEBUG_SIMD8_ALL;
-   if (intel_debug & DEBUG_NO16)
+
+   if (BITSET_TEST(intel_debug, DEBUG_NO16))
       intel_simd &= ~DEBUG_SIMD16_ALL;
-   if (intel_debug & DEBUG_NO32)
+
+   if (BITSET_TEST(intel_debug, DEBUG_NO32))
       intel_simd &= ~DEBUG_SIMD32_ALL;
-   intel_debug &= ~(DEBUG_NO8 | DEBUG_NO16 | DEBUG_NO32);
+
+   BITSET_CLEAR(intel_debug, DEBUG_NO8);
+   BITSET_CLEAR(intel_debug, DEBUG_NO16);
+   BITSET_CLEAR(intel_debug, DEBUG_NO32);
 }
 
 void

src/intel/dev/intel_debug.h
@@ -28,6 +28,7 @@
 
 #include <stdint.h>
 #include "compiler/shader_enums.h"
+#include "util/bitset.h"
 #include "util/macros.h"
 
 #ifdef __cplusplus
@@ -40,10 +41,12 @@ extern "C" {
  * list of debugging flags, as well as some macros for handling them.
  */
 
-extern uint64_t intel_debug;
+#define INTEL_DEBUG_BIT_COUNT 128
+extern BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_BIT_COUNT)];
+
 
 /* Check if a debug flag is enabled by testing its bit position */
-#define INTEL_DEBUG(flag) unlikely(intel_debug & (1ull << (flag)))
+#define INTEL_DEBUG(flag) unlikely(BITSET_TEST(intel_debug, (flag)))
 
 enum intel_debug_flag {
    DEBUG_TEXTURE = 0,

src/intel/compiler/elk/elk_test_simd_selection.cpp
@@ -272,7 +272,7 @@ TEST_F(SIMDSelectionCS, SpillAtSIMD16)
 
 TEST_F(SIMDSelectionCS, EnvironmentVariable32)
 {
-   intel_debug |= DEBUG_DO32;
+   BITSET_SET(intel_debug, DEBUG_DO32);
 
    ASSERT_TRUE(elk_simd_should_compile(simd_state, SIMD8));
    elk_simd_mark_compiled(simd_state, SIMD8, not_spilled);
@@ -286,7 +286,7 @@ TEST_F(SIMDSelectionCS, EnvironmentVariable32)
 
 TEST_F(SIMDSelectionCS, EnvironmentVariable32ButSpills)
 {
-   intel_debug |= DEBUG_DO32;
+   BITSET_SET(intel_debug, DEBUG_DO32);
 
    ASSERT_TRUE(elk_simd_should_compile(simd_state, SIMD8));
    elk_simd_mark_compiled(simd_state, SIMD8, not_spilled);

commit 8d64e75eb8bbda5ac22bfa0676f63fc3d65a0e95

intel: fixup a few debugging option checks
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 139ee32e00 ("intel: Switch uint64_t intel_debug to a bitset")
Reviewed-by: Michael Cheng <michael.cheng@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34667>

src/gallium/drivers/crocus/crocus_batch.c
@@ -947,7 +947,9 @@ _crocus_batch_flush(struct crocus_batch *batch, const char *file, int line)
    finish_growing_bos(&batch->state);
    int ret = submit_batch(batch);
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_SUBMIT | DEBUG_PIPE_CONTROL)) {
+   if (INTEL_DEBUG(DEBUG_BATCH) ||
+       INTEL_DEBUG(DEBUG_SUBMIT) ||
+       INTEL_DEBUG(DEBUG_PIPE_CONTROL)) {
       int bytes_for_commands = crocus_batch_bytes_used(batch);
       int second_bytes = 0;
       if (batch->command.bo != batch->exec_bos[0]) {
@@ -965,7 +967,7 @@ _crocus_batch_flush(struct crocus_batch *batch, const char *file, int line)
               batch->command.relocs.reloc_count,
               batch->state.relocs.reloc_count);
 
-      if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_SUBMIT)) {
+      if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_SUBMIT)) {
          dump_fence_list(batch);
          dump_validation_list(batch);
       }

src/gallium/drivers/crocus/crocus_state.c
@@ -771,7 +771,7 @@ crocus_calculate_urb_fence(struct crocus_batch *batch, unsigned csize,
             exit(1);
          }
 
-         if (INTEL_DEBUG(DEBUG_URB|DEBUG_PERF))
+         if (INTEL_DEBUG(DEBUG_URB) || INTEL_DEBUG(DEBUG_PERF))
             fprintf(stderr, "URB CONSTRAINED\n");
       }
 

src/gallium/drivers/iris/iris_batch.c
@@ -231,7 +231,7 @@ iris_init_batch(struct iris_context *ice,
          batch->other_batches[batch->num_other_batches++] = other_batch;
    }
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS)) {
+   if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_BATCH_STATS)) {
       const unsigned decode_flags = INTEL_BATCH_DECODE_DEFAULT_FLAGS |
          (INTEL_DEBUG(DEBUG_COLOR) ? INTEL_BATCH_DECODE_IN_COLOR : 0);
 
@@ -550,7 +550,7 @@ iris_batch_free(const struct iris_context *ice, struct iris_batch *batch)
 
    _mesa_hash_table_destroy(batch->bo_aux_modes, NULL);
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS))
+   if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_BATCH_STATS))
       intel_batch_decode_ctx_finish(&batch->decoder);
 }
 
@@ -925,7 +925,9 @@ _iris_batch_flush(struct iris_batch *batch, const char *file, int line)
 
    iris_finish_batch(batch);
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_SUBMIT | DEBUG_PIPE_CONTROL)) {
+   if (INTEL_DEBUG(DEBUG_BATCH) ||
+       INTEL_DEBUG(DEBUG_SUBMIT) ||
+       INTEL_DEBUG(DEBUG_PIPE_CONTROL)) {
       const char *basefile = strstr(file, "iris/");
       if (basefile)
          file = basefile + 5;

src/intel/compiler/brw_compiler.c
@@ -221,13 +221,21 @@ brw_get_compiler_config_value(const struct brw_compiler *compiler)
    insert_u64_bit(&config, compiler->mesh.mue_compaction);
    bits++;
 
-   uint64_t mask = DEBUG_DISK_CACHE_MASK;
-   bits += util_bitcount64(mask);
+   enum intel_debug_flag debug_bits[] = {
+      DEBUG_NO_DUAL_OBJECT_GS,
+      DEBUG_SPILL_FS,
+      DEBUG_SPILL_VEC4,
+      DEBUG_NO_COMPACTION,
+      DEBUG_DO32,
+      DEBUG_SOFT64,
+      DEBUG_NO_SEND_GATHER,
+   };
+   for (uint32_t i = 0; i < ARRAY_SIZE(debug_bits); i++) {
+      insert_u64_bit(&config, INTEL_DEBUG(debug_bits[i]));
+      bits++;
+   }
 
-   u_foreach_bit64(bit, mask)
-      insert_u64_bit(&config, INTEL_DEBUG(1ULL << bit));
-
-   mask = SIMD_DISK_CACHE_MASK;
+   uint64_t mask = SIMD_DISK_CACHE_MASK;
    bits += util_bitcount64(mask);
 
    u_foreach_bit64(bit, mask)

src/intel/compiler/elk/elk_compiler.c
@@ -155,13 +155,21 @@ elk_get_compiler_config_value(const struct elk_compiler *compiler)
    insert_u64_bit(&config, compiler->precise_trig);
    bits++;
 
-   uint64_t mask = DEBUG_DISK_CACHE_MASK;
-   bits += util_bitcount64(mask);
+   enum intel_debug_flag debug_bits[] = {
+      DEBUG_NO_DUAL_OBJECT_GS,
+      DEBUG_SPILL_FS,
+      DEBUG_SPILL_VEC4,
+      DEBUG_NO_COMPACTION,
+      DEBUG_DO32,
+      DEBUG_SOFT64,
+      DEBUG_NO_SEND_GATHER,
+   };
+   for (uint32_t i = 0; i < ARRAY_SIZE(debug_bits); i++) {
+      insert_u64_bit(&config, INTEL_DEBUG(debug_bits[i]));
+      bits++;
+   }
 
-   u_foreach_bit64(bit, mask)
-      insert_u64_bit(&config, INTEL_DEBUG(1ULL << bit));
-
-   mask = SIMD_DISK_CACHE_MASK;
+   uint64_t mask = SIMD_DISK_CACHE_MASK;
    bits += util_bitcount64(mask);
 
    u_foreach_bit64(bit, mask)

src/intel/compiler/elk/elk_eu_compact.c
@@ -1964,8 +1964,12 @@ elk_compact_instructions(struct elk_codegen *p, int start_offset,
       if (try_compact_instruction(&c, dst, &inst)) {
          compacted_count++;
 
-         if (INTEL_DEBUG(DEBUG_VS | DEBUG_GS | DEBUG_TCS |
-                         DEBUG_WM | DEBUG_CS | DEBUG_TES)) {
+         if (INTEL_DEBUG(DEBUG_VS) ||
+             INTEL_DEBUG(DEBUG_GS) ||
+             INTEL_DEBUG(DEBUG_TCS) ||
+             INTEL_DEBUG(DEBUG_WM) ||
+             INTEL_DEBUG(DEBUG_CS) ||
+             INTEL_DEBUG(DEBUG_TES)) {
             elk_inst uncompacted;
             uncompact_instruction(&c, &uncompacted, dst);
             if (memcmp(&saved, &uncompacted, sizeof(uncompacted))) {

src/intel/dev/intel_debug.c
@@ -39,88 +39,92 @@
 #include "util/u_math.h"
 #include "c11/threads.h"
 
-BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_BIT_COUNT)] = {0};
+BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_MAX)] = {0};
 
-enum intel_no_x {
-   DEBUG_NO16 = 16,
-   DEBUG_NO8 = 20,
-   DEBUG_NO32 = 39,
+struct debug_control_bitset {
+   const char *string;
+   uint32_t range[2];
 };
 
-
-static const struct debug_control debug_control[] = {
-   { "tex",         DEBUG_TEXTURE },
-   { "blit",        DEBUG_BLIT },
-   { "fall",        DEBUG_PERF },
-   { "perf",        DEBUG_PERF },
-   { "perfmon",     DEBUG_PERFMON },
-   { "bat",         DEBUG_BATCH },
-   { "buf",         DEBUG_BUFMGR },
-   { "fs",          DEBUG_WM },
-   { "gs",          DEBUG_GS },
-   { "sync",        DEBUG_SYNC },
-   { "sf",          DEBUG_SF },
-   { "submit",      DEBUG_SUBMIT },
-   { "wm",          DEBUG_WM },
-   { "urb",         DEBUG_URB },
-   { "vs",          DEBUG_VS },
-   { "clip",        DEBUG_CLIP },
-   { "no16",        DEBUG_NO16 },
-   { "blorp",       DEBUG_BLORP },
-   { "nodualobj",   DEBUG_NO_DUAL_OBJECT_GS },
-   { "optimizer",   DEBUG_OPTIMIZER },
-   { "ann",         DEBUG_ANNOTATION },
-   { "no8",         DEBUG_NO8 },
-   { "no-oaconfig", DEBUG_NO_OACONFIG },
-   { "spill_fs",    DEBUG_SPILL_FS },
-   { "spill_vec4",  DEBUG_SPILL_VEC4 },
-   { "cs",          DEBUG_CS },
-   { "hex",         DEBUG_HEX },
-   { "nocompact",   DEBUG_NO_COMPACTION },
-   { "hs",          DEBUG_TCS },
-   { "tcs",         DEBUG_TCS },
-   { "ds",          DEBUG_TES },
-   { "tes",         DEBUG_TES },
-   { "l3",          DEBUG_L3 },
-   { "do32",        DEBUG_DO32 },
-   { "norbc",       DEBUG_NO_CCS },
-   { "noccs",       DEBUG_NO_CCS },
-   { "nohiz",       DEBUG_NO_HIZ },
-   { "color",       DEBUG_COLOR },
-   { "reemit",      DEBUG_REEMIT },
-   { "soft64",      DEBUG_SOFT64 },
-   { "bt",          DEBUG_BT },
-   { "pc",          DEBUG_PIPE_CONTROL },
-   { "nofc",        DEBUG_NO_FAST_CLEAR },
-   { "no32",        DEBUG_NO32 },
-   { "shaders",     DEBUG_WM },
-   { "rt",          DEBUG_RT },
-   { "rt_notrace",  DEBUG_RT_NO_TRACE },
-   { "bvh_blas",    DEBUG_BVH_BLAS },
-   { "bvh_tlas",    DEBUG_BVH_TLAS },
-   { "bvh_blas_ir_hdr", DEBUG_BVH_BLAS_IR_HDR },
-   { "bvh_tlas_ir_hdr", DEBUG_BVH_TLAS_IR_HDR },
-   { "bvh_blas_ir_as",  DEBUG_BVH_BLAS_IR_AS },
-   { "bvh_tlas_ir_as",  DEBUG_BVH_TLAS_IR_AS },
-   { "bvh_no_build",    DEBUG_BVH_NO_BUILD },
-   { "task",        DEBUG_TASK },
-   { "mesh",        DEBUG_MESH },
-   { "stall",       DEBUG_STALL },
-   { "capture-all", DEBUG_CAPTURE_ALL },
-   { "perf-symbol-names", DEBUG_PERF_SYMBOL_NAMES },
-   { "swsb-stall",  DEBUG_SWSB_STALL },
-   { "heaps",       DEBUG_HEAPS },
-   { "isl",         DEBUG_ISL },
-   { "sparse",      DEBUG_SPARSE },
-   { "draw_bkp",    DEBUG_DRAW_BKP },
-   { "bat-stats",   DEBUG_BATCH_STATS },
-   { "reg-pressure", DEBUG_REG_PRESSURE },
-   { "shader-print", DEBUG_SHADER_PRINT },
-   { "cl-quiet",     DEBUG_CL_QUIET },
-   { "no-send-gather", DEBUG_NO_SEND_GATHER },
-   { "shaders-lineno", DEBUG_SHADERS_LINENO },
-   { "show_shader_stage", DEBUG_SHOW_SHADER_STAGE },
-   { NULL, 0 }
+static const struct debug_control_bitset debug_control[] = {
+#define OPT1(name, bit) \
+   { .string = name, .range = { bit, bit }, }
+#define OPT2(name, start, end) \
+   { .string = name, .range = { start, end }, }
+   OPT1("tex",               DEBUG_TEXTURE),
+   OPT1("blit",              DEBUG_BLIT),
+   OPT1("fall",              DEBUG_PERF),
+   OPT1("perf",              DEBUG_PERF),
+   OPT1("perfmon",           DEBUG_PERFMON),
+   OPT1("bat",               DEBUG_BATCH),
+   OPT1("buf",               DEBUG_BUFMGR),
+   OPT1("fs",                DEBUG_WM),
+   OPT1("gs",                DEBUG_GS),
+   OPT1("sync",              DEBUG_SYNC),
+   OPT1("sf",                DEBUG_SF),
+   OPT1("submit",            DEBUG_SUBMIT),
+   OPT1("wm",                DEBUG_WM),
+   OPT1("urb",               DEBUG_URB),
+   OPT1("vs",                DEBUG_VS),
+   OPT1("clip",              DEBUG_CLIP),
+   OPT1("no16",              DEBUG_NO16),
+   OPT1("blorp",             DEBUG_BLORP),
+   OPT1("nodualobj",         DEBUG_NO_DUAL_OBJECT_GS),
+   OPT1("optimizer",         DEBUG_OPTIMIZER),
+   OPT1("ann",               DEBUG_ANNOTATION),
+   OPT1("no8",               DEBUG_NO8),
+   OPT1("no-oaconfig",       DEBUG_NO_OACONFIG),
+   OPT1("spill_fs",          DEBUG_SPILL_FS),
+   OPT1("spill_vec4",        DEBUG_SPILL_VEC4),
+   OPT1("cs",                DEBUG_CS),
+   OPT1("hex",               DEBUG_HEX),
+   OPT1("nocompact",         DEBUG_NO_COMPACTION),
+   OPT1("hs",                DEBUG_TCS),
+   OPT1("tcs",               DEBUG_TCS),
+   OPT1("ds",                DEBUG_TES),
+   OPT1("tes",               DEBUG_TES),
+   OPT1("l3",                DEBUG_L3),
+   OPT1("do32",              DEBUG_DO32),
+   OPT1("norbc",             DEBUG_NO_CCS),
+   OPT1("noccs",             DEBUG_NO_CCS),
+   OPT1("nohiz",             DEBUG_NO_HIZ),
+   OPT1("color",             DEBUG_COLOR),
+   OPT1("reemit",            DEBUG_REEMIT),
+   OPT1("soft64",            DEBUG_SOFT64),
+   OPT1("bt",                DEBUG_BT),
+   OPT1("pc",                DEBUG_PIPE_CONTROL),
+   OPT1("nofc",              DEBUG_NO_FAST_CLEAR),
+   OPT1("no32",              DEBUG_NO32),
+   OPT2("shaders",           DEBUG_VS, DEBUG_RT),
+   OPT1("rt",                DEBUG_RT),
+   OPT1("rt_notrace",        DEBUG_RT_NO_TRACE),
+   OPT1("bvh_blas",          DEBUG_BVH_BLAS),
+   OPT1("bvh_tlas",          DEBUG_BVH_TLAS),
+   OPT1("bvh_blas_ir_hdr",   DEBUG_BVH_BLAS_IR_HDR),
+   OPT1("bvh_tlas_ir_hdr",   DEBUG_BVH_TLAS_IR_HDR),
+   OPT1("bvh_blas_ir_as",    DEBUG_BVH_BLAS_IR_AS),
+   OPT1("bvh_tlas_ir_as",    DEBUG_BVH_TLAS_IR_AS),
+   OPT1("bvh_no_build",      DEBUG_BVH_NO_BUILD),
+   OPT1("task",              DEBUG_TASK),
+   OPT1("mesh",              DEBUG_MESH),
+   OPT1("stall",             DEBUG_STALL),
+   OPT1("capture-all",       DEBUG_CAPTURE_ALL),
+   OPT1("perf-symbol-names", DEBUG_PERF_SYMBOL_NAMES),
+   OPT1("swsb-stall",        DEBUG_SWSB_STALL),
+   OPT1("heaps",             DEBUG_HEAPS),
+   OPT1("isl",               DEBUG_ISL),
+   OPT1("sparse",            DEBUG_SPARSE),
+   OPT1("draw_bkp",          DEBUG_DRAW_BKP),
+   OPT1("bat-stats",         DEBUG_BATCH_STATS),
+   OPT1("reg-pressure",      DEBUG_REG_PRESSURE),
+   OPT1("shader-print",      DEBUG_SHADER_PRINT),
+   OPT1("cl-quiet",          DEBUG_CL_QUIET),
+   OPT1("no-send-gather",    DEBUG_NO_SEND_GATHER),
+   OPT1("shaders-lineno",    DEBUG_SHADERS_LINENO),
+   OPT1("show_shader_stage", DEBUG_SHOW_SHADER_STAGE),
+   { NULL, }
+#undef OPT1
+#undef OPT2
 };
 uint64_t intel_simd = 0;
 
@@ -206,7 +210,7 @@ uint32_t intel_debug_bkp_before_draw_count = 0;
 uint32_t intel_debug_bkp_after_draw_count = 0;
 
 static void
-parse_debug_bitset(const char *env, const struct debug_control *tbl)
+parse_debug_bitset(const char *env, const struct debug_control_bitset *tbl)
 {
    /* Check if env is NULL or empty */
    if (!env || !*env)
@@ -227,13 +231,12 @@ parse_debug_bitset(const char *env, const struct debug_control *tbl)
          if (strcasecmp(tok, tbl[i].string) != 0)
             continue;
 
-         unsigned bit = tbl[i].flag;
-
-         if (negate)
-            BITSET_CLEAR(intel_debug, bit);
-         else
-            BITSET_SET(intel_debug, bit);
-
+         for (unsigned bit = tbl[i].range[0]; bit <= tbl[i].range[1]; bit++) {
+            if (negate)
+               BITSET_CLEAR(intel_debug, bit);
+            else
+               BITSET_SET(intel_debug, bit);
+         }
          break;
       }
    }

src/intel/dev/intel_debug.h
@@ -41,13 +41,6 @@ extern "C" {
  * list of debugging flags, as well as some macros for handling them.
  */
 
-#define INTEL_DEBUG_BIT_COUNT 128
-extern BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_BIT_COUNT)];
-
-
-/* Check if a debug flag is enabled by testing its bit position */
-#define INTEL_DEBUG(flag) unlikely(BITSET_TEST(intel_debug, (flag)))
-
 enum intel_debug_flag {
    DEBUG_TEXTURE = 0,
    DEBUG_BLIT,
@@ -55,31 +48,22 @@ enum intel_debug_flag {
    DEBUG_PERFMON,
    DEBUG_BATCH,
    DEBUG_BUFMGR,
-   DEBUG_GS,
    DEBUG_SYNC,
    DEBUG_SF,
    DEBUG_SUBMIT,
-   DEBUG_WM,
    DEBUG_URB,
-   DEBUG_VS,
    DEBUG_CLIP,
    DEBUG_STALL,
    DEBUG_BLORP,
-   /* 16 is reserved */
-   DEBUG_NO_DUAL_OBJECT_GS = 17,
+   DEBUG_NO_DUAL_OBJECT_GS,
    DEBUG_OPTIMIZER,
    DEBUG_ANNOTATION,
-   /* 20 is reserved */
-   DEBUG_NO_OACONFIG = 21,
+   DEBUG_NO_OACONFIG,
    DEBUG_SPILL_FS,
    DEBUG_SPILL_VEC4,
-   DEBUG_CS,
    DEBUG_HEX,
    DEBUG_NO_COMPACTION,
-   DEBUG_TCS,
-   DEBUG_TES,
    DEBUG_L3,
-   DEBUG_DO32,
    DEBUG_NO_CCS,
    DEBUG_NO_HIZ,
    DEBUG_COLOR,
@@ -88,10 +72,6 @@ enum intel_debug_flag {
    DEBUG_BT,
    DEBUG_PIPE_CONTROL,
    DEBUG_NO_FAST_CLEAR,
-   /* 39 is reserved */
-   DEBUG_RT = 40,
-   DEBUG_TASK,
-   DEBUG_MESH,
    DEBUG_CAPTURE_ALL,
    DEBUG_PERF_SYMBOL_NAMES,
    DEBUG_SWSB_STALL,
@@ -114,25 +94,42 @@ enum intel_debug_flag {
    DEBUG_RT_NO_TRACE,
    DEBUG_SHADERS_LINENO,
    DEBUG_SHOW_SHADER_STAGE,
+   /* Keep the stages grouped */
+   DEBUG_VS,
+   DEBUG_TCS,
+   DEBUG_TES,
+   DEBUG_GS,
+   DEBUG_WM,
+   DEBUG_TASK,
+   DEBUG_MESH,
+   DEBUG_CS,
+   DEBUG_RT,
+   DEBUG_NO8,
+
+   DEBUG_NO16,
+   DEBUG_NO32,
+   DEBUG_DO32,
+
+   /* Must be the last entry */
+   INTEL_DEBUG_MAX,
 };
 
+extern BITSET_WORD intel_debug[BITSET_WORDS(INTEL_DEBUG_MAX)];
 
-#define DEBUG_ANY                 (~0ull)
+
+/* Check if a debug flag is enabled by testing its bit position */
+#define INTEL_DEBUG(flag) unlikely(BITSET_TEST(intel_debug, (flag)))
 
 /* These flags are not compatible with the disk shader cache */
 #define DEBUG_DISK_CACHE_DISABLE_MASK 0
 
-/* These flags may affect program generation */
-#define DEBUG_DISK_CACHE_MASK \
-   (DEBUG_NO_DUAL_OBJECT_GS | DEBUG_SPILL_FS | \
-   DEBUG_SPILL_VEC4 | DEBUG_NO_COMPACTION | DEBUG_DO32 | DEBUG_SOFT64 | \
-   DEBUG_NO_SEND_GATHER)
-
 /* Flags to determine what bvh to dump out */
-#define DEBUG_BVH_ANV (DEBUG_BVH_BLAS | DEBUG_BVH_TLAS)
-#define DEBUG_BVH_IR_HDR (DEBUG_BVH_BLAS_IR_HDR | DEBUG_BVH_TLAS_IR_HDR)
-#define DEBUG_BVH_IR_AS (DEBUG_BVH_BLAS_IR_AS | DEBUG_BVH_TLAS_IR_AS)
-#define DEBUG_BVH_ANY (DEBUG_BVH_ANV | DEBUG_BVH_IR_HDR | DEBUG_BVH_IR_AS)
+#define INTEL_DEBUG_BVH_ANY (unlikely(INTEL_DEBUG(DEBUG_BVH_BLAS) ||    \
+                                      INTEL_DEBUG(DEBUG_BVH_TLAS) ||    \
+                                      INTEL_DEBUG(DEBUG_BVH_BLAS_IR_HDR) || \
+                                      INTEL_DEBUG(DEBUG_BVH_TLAS_IR_HDR) || \
+                                      INTEL_DEBUG(DEBUG_BVH_BLAS_IR_AS) || \
+                                      INTEL_DEBUG(DEBUG_BVH_TLAS_IR_AS)))
 
 extern uint64_t intel_simd;
 extern uint32_t intel_debug_bkp_before_draw_count;

src/intel/vulkan/anv_batch_chain.c
@@ -1264,7 +1264,7 @@ anv_cmd_buffer_exec_batch_debug(struct anv_queue *queue,
                                 struct anv_query_pool *perf_query_pool,
                                 uint32_t perf_query_pass)
 {
-   if (!INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS))
+   if (!INTEL_DEBUG(DEBUG_BATCH) && !INTEL_DEBUG(DEBUG_BATCH_STATS))
       return;
 
    struct anv_device *device = queue->device;

src/intel/vulkan/anv_device.c
@@ -384,7 +384,7 @@ VkResult anv_CreateDevice(
    if (result != VK_SUCCESS)
       goto fail_alloc;
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS)) {
+   if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_BATCH_STATS)) {
       for (unsigned i = 0; i < physical_device->queue.family_count; i++) {
          struct intel_batch_decode_ctx *decoder = &device->decoder[i];
 
@@ -1296,7 +1296,7 @@ void anv_DestroyDevice(
 
    anv_device_destroy_context_or_vm(device);
 
-   if (INTEL_DEBUG(DEBUG_BATCH | DEBUG_BATCH_STATS)) {
+   if (INTEL_DEBUG(DEBUG_BATCH) || INTEL_DEBUG(DEBUG_BATCH_STATS)) {
       for (unsigned i = 0; i < pdevice->queue.family_count; i++) {
          if (INTEL_DEBUG(DEBUG_BATCH_STATS))
             intel_batch_print_stats(&device->decoder[i]);

src/intel/vulkan/anv_private.h
@@ -2363,7 +2363,7 @@ anv_queue_post_submit(struct anv_queue *queue, VkResult submit_result)
 
 #if ANV_SUPPORT_RT && !ANV_SUPPORT_RT_GRL
    /* The recorded bvh is dumped to files upon command buffer completion */
-   if (INTEL_DEBUG(DEBUG_BVH_ANY))
+   if (INTEL_DEBUG_BVH_ANY)
       anv_dump_bvh_to_files(queue->device);
 #endif
 

src/intel/vulkan/anv_queue.c
@@ -94,7 +94,9 @@ anv_queue_init(struct anv_device *device, struct anv_queue *queue,
    /* Add a debug fence to wait on submissions if we're using the synchronized
     * submission feature, shader-print feature, or BVH dump.
     */
-   if (INTEL_DEBUG(DEBUG_SYNC | DEBUG_SHADER_PRINT | DEBUG_BVH_ANY)) {
+   if (INTEL_DEBUG(DEBUG_SYNC) ||
+       INTEL_DEBUG(DEBUG_SHADER_PRINT) ||
+       INTEL_DEBUG_BVH_ANY) {
       result = vk_sync_create(&device->vk,
                               &device->physical->sync_syncobj_type,
                               0, 0, &queue->sync);

src/intel/vulkan/genX_acceleration_structure.c
@@ -573,7 +573,7 @@ anv_init_header(VkCommandBuffer commandBuffer,
       anv_cmd_buffer_update_addr(cmd_buffer, addr, header_size, header_ptr);
    }
 
-   if (INTEL_DEBUG(DEBUG_BVH_ANY)) {
+   if (INTEL_DEBUG_BVH_ANY) {
       genx_batch_emit_pipe_control(&cmd_buffer->batch, cmd_buffer->device->info,
                                    cmd_buffer->state.current_pipeline,
                                    ANV_PIPE_END_OF_PIPE_SYNC_BIT |