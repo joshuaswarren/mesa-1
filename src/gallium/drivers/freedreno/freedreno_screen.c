joshuaswarren/mesa-1

commit 093fc64f51a6feaf4cc22ce1f4b6c7e05b51dda5

freedreno,tu: Read and pass to compiler uche_trap_base
KGSL always exposed uche_trap_base, and MSM only recently
got support for it.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29860>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -923,6 +923,9 @@ fd_screen_create(int fd,
    if (fd_device_version(dev) >= FD_VERSION_ROBUSTNESS)
       screen->has_robustness = true;
 
+   if (fd_pipe_get_param(screen->pipe, FD_UCHE_TRAP_BASE, &val))
+      screen->uche_trap_base = screen->gen >= 6 ? 0x1fffffffff000ull : 0ull;
+
    screen->has_syncobj = fd_has_syncobj(screen->dev);
 
    /* parse driconf configuration now for device specific overrides: */

src/freedreno/drm/freedreno_drmif.h
@@ -47,6 +47,7 @@ enum fd_param_id {
    FD_SUSPEND_COUNT, /* # of times the GPU has suspended, and potentially lost state */
    FD_SYSPROF,       /* Settable (for CAP_SYS_ADMIN) param for system profiling */
    FD_VA_SIZE,       /* GPU virtual address size */
+   FD_UCHE_TRAP_BASE,
 };
 
 /**

src/freedreno/drm/msm/msm_pipe.c
@@ -84,6 +84,8 @@ msm_pipe_get_param(struct fd_pipe *pipe, enum fd_param_id param,
       return query_param(pipe, MSM_PARAM_SUSPENDS, value);
    case FD_VA_SIZE:
       return query_param(pipe, MSM_PARAM_VA_SIZE, value);
+   case FD_UCHE_TRAP_BASE:
+      return query_param(pipe, MSM_PARAM_UCHE_TRAP_BASE, value);
    default:
       ERROR_MSG("invalid param id: %d", param);
       return -1;

src/freedreno/drm/virtio/virtio_pipe.c
@@ -88,6 +88,8 @@ virtio_pipe_get_param(struct fd_pipe *pipe, enum fd_param_id param,
    case FD_VA_SIZE:
       *value = virtio_dev->vdrm->caps.u.msm.va_size;
       return 0;
+   case FD_UCHE_TRAP_BASE:
+      return query_param(pipe, MSM_PARAM_UCHE_TRAP_BASE, value);
    default:
       ERROR_MSG("invalid param id: %d", param);
       return -1;

src/freedreno/ir3/ir3_compiler.h
@@ -54,6 +54,8 @@ struct ir3_compiler_options {
 
    /* "dual_color_blend_by_location" workaround is enabled: */
    bool dual_color_blend_by_location;
+
+   uint64_t uche_trap_base;
 };
 
 struct ir3_compiler {

src/freedreno/ir3/ir3_disk_cache.c
@@ -36,13 +36,22 @@ ir3_disk_cache_init(struct ir3_compiler *compiler)
    const char *renderer = fd_dev_name(compiler->dev_id);
    const struct build_id_note *note =
       build_id_find_nhdr_for_addr(ir3_disk_cache_init);
-   assert(note && build_id_length(note) == 20); /* sha1 */
+   unsigned build_id_len = build_id_length(note);
+   assert(note && build_id_len == 20); /* sha1 */
 
    const uint8_t *id_sha1 = build_id_data(note);
    assert(id_sha1);
 
+   struct mesa_sha1 ctx;
+   uint8_t sha1[SHA1_DIGEST_LENGTH];
+   _mesa_sha1_init(&ctx);
+   _mesa_sha1_update(&ctx, id_sha1, build_id_len);
+   _mesa_sha1_update(&ctx, &compiler->options.uche_trap_base,
+                     sizeof(compiler->options.uche_trap_base));
+   _mesa_sha1_final(&ctx, sha1);
+
    char timestamp[41];
-   _mesa_sha1_format(timestamp, id_sha1);
+   _mesa_sha1_format(timestamp, sha1);
 
    uint64_t driver_flags = ir3_shader_debug_hash_key();
    compiler->disk_cache = disk_cache_create(renderer, timestamp, driver_flags);

src/freedreno/vulkan/tu_device.cc
@@ -72,6 +72,7 @@ tu_device_get_cache_uuid(struct tu_physical_device *device, void *uuid)
 
    _mesa_sha1_update(&ctx, &family, sizeof(family));
    _mesa_sha1_update(&ctx, &driver_flags, sizeof(driver_flags));
+   _mesa_sha1_update(&ctx, &device->uche_trap_base, sizeof(device->uche_trap_base));
    _mesa_sha1_final(&ctx, sha1);
 
    memcpy(uuid, sha1, VK_UUID_SIZE);
@@ -2588,6 +2589,7 @@ tu_CreateDevice(VkPhysicalDevice physicalDevice,
          .storage_16bit = physical_device->info->a6xx.storage_16bit,
          .storage_8bit = physical_device->info->a7xx.storage_8bit,
          .shared_push_consts = !TU_DEBUG(PUSH_CONSTS_PER_STAGE),
+         .uche_trap_base = physical_device->uche_trap_base,
       };
       device->compiler = ir3_compiler_create(
          NULL, &physical_device->dev_id, physical_device->info, &ir3_options);

src/freedreno/vulkan/tu_device.h
@@ -114,6 +114,8 @@ struct tu_physical_device
    uint32_t vpc_attr_buf_offset_bypass;
    uint32_t vpc_attr_buf_size_bypass;
 
+   uint64_t uche_trap_base;
+
    /* Amount of usable descriptor sets, this excludes any reserved set */
    uint32_t usable_sets;
    /* Index of the reserved descriptor set, may be -1 if unset */

commit 75ca72a9c4342bc3c50ad2d7ac505298a532bf57

freedreno: Fix shader-clock when kernel exposes UCHE_TRAP_BASE
Fixes: 093fc64f51a6 ("freedreno,tu: Read and pass to compiler uche_trap_base)
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35010>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -918,8 +918,11 @@ fd_screen_create(int fd,
    if (fd_device_version(dev) >= FD_VERSION_ROBUSTNESS)
       screen->has_robustness = true;
 
-   if (fd_pipe_get_param(screen->pipe, FD_UCHE_TRAP_BASE, &val))
+   if (fd_pipe_get_param(screen->pipe, FD_UCHE_TRAP_BASE, &val)) {
       screen->uche_trap_base = screen->gen >= 6 ? 0x1fffffffff000ull : 0ull;
+   } else {
+      screen->uche_trap_base = val;
+   }
 
    screen->has_syncobj = fd_has_syncobj(screen->dev);
 