joshuaswarren/mesa-1

commit cfcc9f2a36ae5ed905d032a3d15fa692724dbe67

nouveau/headers: Run rustfmt on generated files
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28255>

src/nouveau/headers/lib_rs_gen.py
@@ -88,6 +88,10 @@ def main():
     try:
         with open(args.out_rs, 'w', encoding='utf-8') as f:
             f.write(TEMPLATE_RS.render(root=root))
+            try:
+                subprocess.run(['rustfmt', args.out_rs], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     except Exception:
         # In the event there's an error, this imports some helpers from mako

commit 9eddbabca2136713883f648887e13569150acc4c

nouveau/headers: Run rustfmt after file is closed
If we run a subprocess while the file is still open, we may not have
flushed the file contents to disk.

Fixes: cfcc9f2a ("nouveau/headers: Run rustfmt on generated files")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35008>

src/nouveau/headers/lib_rs_gen.py
@@ -109,11 +109,6 @@ def main():
     try:
         with open(args.out_rs, 'w', encoding='utf-8') as f:
             f.write(TEMPLATE_RS.render(root=root))
-            try:
-                subprocess.run(['rustfmt', args.out_rs], check=True)
-            except (subprocess.CalledProcessError, FileNotFoundError):
-                pass
-
     except Exception:
         # In the event there's an error, this imports some helpers from mako
         # to print a useful stack trace and prints it, then exits with
@@ -123,6 +118,11 @@ def main():
         from mako import exceptions
         print(exceptions.text_error_template().render(), file=sys.stderr)
         sys.exit(1)
+
+    try:
+        subprocess.run(['rustfmt', args.out_rs], check=True)
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        pass
 
 if __name__ == '__main__':
     main()