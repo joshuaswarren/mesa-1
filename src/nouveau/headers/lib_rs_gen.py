joshuaswarren/mesa-1

commit cfcc9f2a36ae5ed905d032a3d15fa692724dbe67

nouveau/headers: Run rustfmt on generated files
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28255>

src/nouveau/headers/lib_rs_gen.py
@@ -88,6 +88,10 @@ def main():
     try:
         with open(args.out_rs, 'w', encoding='utf-8') as f:
             f.write(TEMPLATE_RS.render(root=root))
+            try:
+                subprocess.run(['rustfmt', args.out_rs], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     except Exception:
         # In the event there's an error, this imports some helpers from mako

commit ee1d9af310f810711ef650b564262944d6307412

nouveau/headers: Ignore PermissionError in rustfmt
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13172
Fixes: cfcc9f2a ("nouveau/headers: Run rustfmt on generated files")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35008>

src/nouveau/headers/lib_rs_gen.py
@@ -121,7 +121,7 @@ def main():
 
     try:
         subprocess.run(['rustfmt', args.out_rs], check=True)
-    except (subprocess.CalledProcessError, FileNotFoundError):
+    except (subprocess.CalledProcessError, FileNotFoundError, PermissionError):
         pass
 
 if __name__ == '__main__':