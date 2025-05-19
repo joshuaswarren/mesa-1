openharmony/third_party_mesa3d

commit 49e5a85573d6023bfca573dc40bfdd699a58b686

st/mesa: Optionally override RGB/RGBX dst alpha blend factors
Intel's blending hardware does not properly return 1.0 for destination
alpha for RGBX formats; it requires the factors to be overridden to
either zero or one.  Broadcom vc4 and v3d also could use this override.
While overriding these factors is safe in general, Nouveau and Radeon
would prefer not to.  Their blending hardware already returns correct
values for RGB/RGBX formats, and would like to avoid the resulting
per-buffer blending and independent blend factors (rgb != a) since it
can cause additional overhead.

I considered simply handling this in the driver, but it's not as nice.
pipe_blend_state doesn't have any format information, so we'd need the
hardware blend state to depend on both pipe_blend_state and
pipe_framebuffer_state.  Furthermore, Intel GPUs don't have a native
RGBX_SNORM format, so I avoid exposing one, which makes Gallium fall
back to RGBA_SNORM.  The pipe_surfaces we get in the driver have an RGBA
format, making it impossible to tell that there shouldn't be an alpha
channel.  One could argue that st not handling it in that case is a bug.
To work around this, we'd have to expose RGBX pipe formats, mapped to
RGBA hardware formats, and add format swizzling special cases.  All
doable, but it ends up being more code than I'd like.

st_atom_blend already has access to the right information and it's
trivial to accomplish there, so we just add a cap bit and do that.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/mesa/main/mtypes.h
@@ -3506,6 +3506,7 @@ struct gl_framebuffer
    bool _HasAttachments;
 
    GLbitfield _IntegerBuffers;  /**< Which color buffers are integer valued */
+   GLbitfield _RGBBuffers;  /**< Which color buffers have baseformat == RGB */
 
    /* ARB_color_buffer_float */
    GLboolean _AllColorBuffersFixedPoint; /* no integer, no float */

src/mesa/state_tracker/st_atom_blend.c
@@ -41,6 +41,7 @@
 
 #include "framebuffer.h"
 #include "main/blend.h"
+#include "main/glformats.h"
 #include "main/macros.h"
 
 /**
@@ -126,8 +127,9 @@ colormask_per_rt(const struct gl_context *ctx, unsigned num_cb)
  * Figure out if blend enables/state are different per rt.
  */
 static GLboolean
-blend_per_rt(const struct gl_context *ctx, unsigned num_cb)
+blend_per_rt(const struct st_context *st, unsigned num_cb)
 {
+   const struct gl_context *ctx = st->ctx;
    GLbitfield cb_mask = u_bit_consecutive(0, num_cb);
    GLbitfield blend_enabled = ctx->Color.BlendEnabled & cb_mask;
 
@@ -145,7 +147,47 @@ blend_per_rt(const struct gl_context *ctx, unsigned num_cb)
        * must be handled on a per buffer basis. */
       return GL_TRUE;
    }
+
+   if (st->needs_rgb_dst_alpha_override && ctx->DrawBuffer->_RGBBuffers) {
+      /* Overriding requires independent blend functions (not just enables),
+       * require drivers exposing PIPE_CAP_RGB_OVERRIDE_DST_ALPHA_BLEND to
+       * also expose PIPE_CAP_INDEP_BLEND_FUNC.
+       */
+      assert(st->has_indep_blend_func);
+
+      /* If some of the buffers are RGB, we may need to override blend
+       * factors that reference destination-alpha to constants.  We may
+       * need different blend factor overrides per buffer (say one uses
+       * a DST_ALPHA factor and another uses INV_DST_ALPHA), so we flip
+       * on independent blending.  This may not be required in all cases,
+       * but burning the CPU to figure it out is probably not worthwhile.
+       */
+      return GL_TRUE;
+   }
+
    return GL_FALSE;
+}
+
+/**
+ * Modify blend function to force destination alpha to 1.0
+ *
+ * If \c function specifies a blend function that uses destination alpha,
+ * replace it with a function that hard-wires destination alpha to 1.0.
+ * This is useful when emulating a GL RGB format with an RGBA pipe_format.
+ */
+static enum pipe_blendfactor
+fix_xrgb_alpha(enum pipe_blendfactor factor)
+{
+   switch (factor) {
+   case PIPE_BLENDFACTOR_DST_ALPHA:
+      return PIPE_BLENDFACTOR_ONE;
+
+   case PIPE_BLENDFACTOR_INV_DST_ALPHA:
+   case PIPE_BLENDFACTOR_SRC_ALPHA_SATURATE:
+      return PIPE_BLENDFACTOR_ZERO;
+   default:
+      return factor;
+   }
 }
 
 void
@@ -160,7 +202,7 @@ st_update_blend( struct st_context *st )
    memset(blend, 0, sizeof(*blend));
 
    if (num_cb > 1 &&
-       (blend_per_rt(ctx, num_cb) || colormask_per_rt(ctx, num_cb))) {
+       (blend_per_rt(st, num_cb) || colormask_per_rt(ctx, num_cb))) {
       num_state = num_cb;
       blend->independent_blend_enable = 1;
    }
@@ -215,6 +257,18 @@ st_update_blend( struct st_context *st )
                translate_blend(ctx->Color.Blend[j].SrcA);
             blend->rt[i].alpha_dst_factor =
                translate_blend(ctx->Color.Blend[j].DstA);
+         }
+
+         const struct gl_renderbuffer *rb =
+            ctx->DrawBuffer->_ColorDrawBuffers[i];
+
+         if (st->needs_rgb_dst_alpha_override && rb &&
+             (ctx->DrawBuffer->_RGBBuffers & (1 << i))) {
+            struct pipe_rt_blend_state *rt = &blend->rt[i];
+            rt->rgb_src_factor = fix_xrgb_alpha(rt->rgb_src_factor);
+            rt->rgb_dst_factor = fix_xrgb_alpha(rt->rgb_dst_factor);
+            rt->alpha_src_factor = fix_xrgb_alpha(rt->alpha_src_factor);
+            rt->alpha_dst_factor = fix_xrgb_alpha(rt->alpha_dst_factor);
          }
       }
    }

src/gallium/auxiliary/util/u_screen.c
@@ -77,6 +77,7 @@ u_pipe_screen_get_param_defaults(struct pipe_screen *pscreen,
    case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:
    case PIPE_CAP_SEAMLESS_CUBE_MAP:
    case PIPE_CAP_SEAMLESS_CUBE_MAP_PER_TEXTURE:
+   case PIPE_CAP_RGB_OVERRIDE_DST_ALPHA_BLEND:
       return 0;
 
    case PIPE_CAP_MIN_TEXEL_OFFSET:

src/gallium/include/pipe/p_defines.h
@@ -853,6 +853,7 @@ enum pipe_cap
    PIPE_CAP_SURFACE_SAMPLE_COUNT,
    PIPE_CAP_TGSI_ATOMFADD,
    PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE,
+   PIPE_CAP_RGB_OVERRIDE_DST_ALPHA_BLEND,
 };
 
 /**

src/mesa/main/fbobject.c
@@ -1003,6 +1003,7 @@ _mesa_test_framebuffer_completeness(struct gl_context *ctx,
    fb->_HasSNormOrFloatColorBuffer = GL_FALSE;
    fb->_HasAttachments = true;
    fb->_IntegerBuffers = 0;
+   fb->_RGBBuffers = 0;
 
    /* Start at -2 to more easily loop over all attachment points.
     *  -2: depth buffer
@@ -1148,6 +1149,9 @@ _mesa_test_framebuffer_completeness(struct gl_context *ctx,
          /* check if integer color */
          if (_mesa_is_format_integer_color(attFormat))
             fb->_IntegerBuffers |= (1 << i);
+
+         if (f == GL_RGB)
+            fb->_RGBBuffers |= (1 << i);
 
          fb->_AllColorBuffersFixedPoint =
             fb->_AllColorBuffersFixedPoint &&

src/mesa/state_tracker/st_context.c
@@ -464,6 +464,10 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
       screen->get_param(screen, PIPE_CAP_MULTI_DRAW_INDIRECT);
    st->has_single_pipe_stat =
       screen->get_param(screen, PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE);
+   st->has_indep_blend_func =
+      screen->get_param(screen, PIPE_CAP_INDEP_BLEND_FUNC);
+   st->needs_rgb_dst_alpha_override =
+      screen->get_param(screen, PIPE_CAP_RGB_OVERRIDE_DST_ALPHA_BLEND);
 
    st->has_hw_atomics =
       screen->get_shader_param(screen, PIPE_SHADER_FRAGMENT,

src/mesa/state_tracker/st_context.h
@@ -128,6 +128,8 @@ struct st_context
    boolean has_half_float_packing;
    boolean has_multi_draw_indirect;
    boolean has_single_pipe_stat;
+   boolean has_indep_blend_func;
+   boolean needs_rgb_dst_alpha_override;
    boolean can_bind_const_buffer_as_vertex;
 
    /**

joshuaswarren/mesa-1

commit 6c1e6f60b4ba4ac3768998dc43197dfee037de06

mesa/st: fix _IsRGBDraw bitfield use
Previously we were assuming that all color attachments were active.

Fixes: 49e5a85573d6 ("st/mesa: Optionally override RGB/RGBX dst alpha blend factors")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35014>

src/mesa/main/buffers.c
@@ -762,7 +762,8 @@ _mesa_update_drawbuffer_masks(struct gl_context *ctx,
                           &ctx->DrawBuffer->_IntegerDrawBuffers);
    update_drawbuffer_mask(ctx, fb, &ctx->DrawBuffer->_BlendForceAlphaToOne,
                           &ctx->DrawBuffer->_BlendForceAlphaToOneDraw);
-
+   update_drawbuffer_mask(ctx, fb, &ctx->DrawBuffer->_IsRGB,
+                          &ctx->DrawBuffer->_IsRGBDraw);
 }
 
 /**

src/mesa/main/mtypes.h
@@ -2695,7 +2695,8 @@ struct gl_framebuffer
    GLbitfield _IntegerDrawBuffers;  /**< Which color draw buffers are integer valued */
    GLbitfield _BlendForceAlphaToOne;  /**< Which color attachments need blend factor adjustment */
    GLbitfield _BlendForceAlphaToOneDraw;  /**< Which color buffers need blend factor adjustment */
-   GLbitfield _IsRGB;  /**< Which color buffers have an RGB base format? */
+   GLbitfield _IsRGB;  /**< Which color attachments have an RGB base format? */
+   GLbitfield _IsRGBDraw;  /**< Which color buffers have an RGB base format? */
    GLbitfield _FP32Buffers; /**< Which color buffers are FP32 */
 
    /* ARB_color_buffer_float */

src/mesa/state_tracker/st_atom_blend.c
@@ -143,7 +143,7 @@ allow_rgb_colormask_promotion(const struct st_context *st,
     * without needing independent blending.  (If none should be promoted,
     * we can just skip this optimization as it doesn't do anything.)
     */
-   bool same = ctx->DrawBuffer->_IsRGB == u_bit_consecutive(0, num_cb) &&
+   bool same = ctx->DrawBuffer->_IsRGBDraw == u_bit_consecutive(0, num_cb) &&
                (ctx->Color.ColorMask & full_mask) == rgb_mask;
 
    /* We can support different per-RT promotion decisions if we driver
@@ -256,7 +256,7 @@ st_update_blend( struct st_context *st )
        * partial writes may require preserving/combining new and old data.
        */
       if (promote_rgb_colormasks &&
-          colormask == 0x7 && (ctx->DrawBuffer->_IsRGB & (1 << i)))
+          colormask == 0x7 && (ctx->DrawBuffer->_IsRGBDraw & (1 << i)))
          colormask = 0xf;
 
       blend->rt[i].colormask = colormask;