openharmony/third_party_mesa3d

commit 07a9401436b7ca59a900d9cb5c621cbc85696341

gallivm: use llvm intrinsics for 16-bit round/trunc/roundeven
Otherwise the inf translations don't seem to work, and the VK CTS
fails

Fixes VK CTS dEQP-VK.spirv_assembly.instruction.graphics.float16.arithmetic*

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11816>

src/gallium/auxiliary/gallivm/lp_bld_arit.c
@@ -2030,6 +2030,12 @@ lp_build_trunc(struct lp_build_context *bld,
    assert(type.floating);
    assert(lp_check_value(type, a));
 
+   if (type.width == 16) {
+      char intrinsic[64];
+      lp_format_intrinsic(intrinsic, 64, "llvm.trunc", bld->vec_type);
+      return lp_build_intrinsic_unary(builder, intrinsic, bld->vec_type, a);
+   }
+
    if (arch_rounding_available(type)) {
       return lp_build_round_arch(bld, a, LP_BUILD_ROUND_TRUNCATE);
    }
@@ -2082,6 +2088,12 @@ lp_build_round(struct lp_build_context *bld,
 
    assert(type.floating);
    assert(lp_check_value(type, a));
+
+   if (type.width == 16) {
+      char intrinsic[64];
+      lp_format_intrinsic(intrinsic, 64, "llvm.round", bld->vec_type);
+      return lp_build_intrinsic_unary(builder, intrinsic, bld->vec_type, a);
+   }
 
    if (arch_rounding_available(type)) {
       return lp_build_round_arch(bld, a, LP_BUILD_ROUND_NEAREST);

src/gallium/auxiliary/gallivm/lp_bld_nir.c
@@ -32,6 +32,7 @@
 #include "lp_bld_logic.h"
 #include "lp_bld_quad.h"
 #include "lp_bld_flow.h"
+#include "lp_bld_intr.h"
 #include "lp_bld_struct.h"
 #include "lp_bld_debug.h"
 #include "lp_bld_printf.h"
@@ -798,7 +799,13 @@ static LLVMValueRef do_alu_action(struct lp_build_nir_context *bld_base,
       result = lp_build_rcp(get_flt_bld(bld_base, src_bit_size[0]), src[0]);
       break;
    case nir_op_fround_even:
-      result = lp_build_round(get_flt_bld(bld_base, src_bit_size[0]), src[0]);
+      if (src_bit_size[0] == 16) {
+	 struct lp_build_context *bld = get_flt_bld(bld_base, 16);
+	 char intrinsic[64];
+	 lp_format_intrinsic(intrinsic, 64, "llvm.roundeven", bld->vec_type);
+	 result = lp_build_intrinsic_unary(builder, intrinsic, bld->vec_type, src[0]);
+      } else
+	 result = lp_build_round(get_flt_bld(bld_base, src_bit_size[0]), src[0]);
       break;
    case nir_op_frsq:
       result = lp_build_rsqrt(get_flt_bld(bld_base, src_bit_size[0]), src[0]);

joshuaswarren/mesa-1

commit 44c010b04736b547880ceb4c4e6657a4c71aa99d

gallivm: Use `llvm.roundeven` in lp_build_round()
`lp_build_round` intends to implement round with ties-to-even behavior,
as can be seen by its test's use of `nearbyint` to generate reference
values and by it use in implementing `nir_op_fround_even`.

Fixes: 07a9401436b ("gallivm: use llvm intrinsics for 16-bit round/trunc/roundeven")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34937>

src/gallium/auxiliary/gallivm/lp_bld_arit.c
@@ -2120,7 +2120,7 @@ lp_build_round(struct lp_build_context *bld,
 
    if (type.width == 16) {
       char intrinsic[64];
-      lp_format_intrinsic(intrinsic, 64, "llvm.round", bld->vec_type);
+      lp_format_intrinsic(intrinsic, 64, "llvm.roundeven", bld->vec_type);
       return lp_build_intrinsic_unary(builder, intrinsic, bld->vec_type, a);
    }
 

src/gallium/auxiliary/gallivm/lp_bld_nir_soa.c
@@ -3356,13 +3356,7 @@ do_alu_action(struct lp_build_nir_soa_context *bld,
       result = lp_build_rcp(float_bld, src[0]);
       break;
    case nir_op_fround_even:
-      if (src_bit_size[0] == 16) {
-         char intrinsic[64];
-         lp_format_intrinsic(intrinsic, 64, "llvm.roundeven", float_bld->vec_type);
-         result = lp_build_intrinsic_unary(builder, intrinsic, float_bld->vec_type, src[0]);
-      } else {
-         result = lp_build_round(float_bld, src[0]);
-      }
+      result = lp_build_round(float_bld, src[0]);
       break;
    case nir_op_frsq:
       result = lp_build_rsqrt(float_bld, src[0]);