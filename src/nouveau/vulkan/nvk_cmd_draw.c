joshuaswarren/mesa-1

commit 6742c5373fd2e58e95a0f0c4cfe85fce735f5949

nvk: allocate VAB memory area
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

src/nouveau/vulkan/nvk_cmd_draw.c
@@ -308,6 +308,16 @@ nvk_queue_init_context_draw_state(struct nvk_queue *queue)
    P_NV9097_SET_VERTEX_STREAM_SUBSTITUTE_A(p, zero_addr >> 32);
    P_NV9097_SET_VERTEX_STREAM_SUBSTITUTE_B(p, zero_addr);
 
+   if (dev->ctx->eng3d.cls >= FERMI_A &&
+       dev->ctx->eng3d.cls < MAXWELL_A) {
+      assert(dev->vab_memory);
+      uint64_t vab_addr = dev->vab_memory->offset;
+      P_MTHD(p, NV9097, SET_VAB_MEMORY_AREA_A);
+      P_NV9097_SET_VAB_MEMORY_AREA_A(p, vab_addr >> 32);
+      P_NV9097_SET_VAB_MEMORY_AREA_B(p, vab_addr);
+      P_NV9097_SET_VAB_MEMORY_AREA_C(p, SIZE_BYTES_256K);
+   }
+
    if (dev->ctx->eng3d.cls == MAXWELL_A)
       P_IMMD(p, NVB097, SET_SELECT_MAXWELL_TEXTURE_HEADERS, V_TRUE);
 

src/nouveau/vulkan/nvk_device.c
@@ -8,6 +8,8 @@
 
 #include "vulkan/wsi/wsi_common.h"
 
+#include "cl9097.h"
+#include "clb097.h"
 #include "clc397.h"
 
 static void
@@ -224,10 +226,19 @@ nvk_CreateDevice(VkPhysicalDevice physicalDevice,
    memset(zero_map, 0, 0x1000);
    nouveau_ws_bo_unmap(device->zero_page, zero_map);
 
+   if (device->ctx->eng3d.cls >= FERMI_A &&
+       device->ctx->eng3d.cls < MAXWELL_A) {
+      /* max size is 256k */
+      device->vab_memory =
+         nouveau_ws_bo_new(device->pdev->dev, 1 << 17, 1 << 20, NOUVEAU_WS_BO_LOCAL);
+      if (device->vab_memory == NULL)
+         goto fail_zero_page;
+   }
+
    result = nvk_queue_init(device, &device->queue,
                            &pCreateInfo->pQueueCreateInfos[0], 0);
    if (result != VK_SUCCESS)
-      goto fail_zero_page;
+      goto fail_vab_memory;
 
    result = nvk_device_init_meta(device);
    if (result != VK_SUCCESS)
@@ -239,6 +250,9 @@ nvk_CreateDevice(VkPhysicalDevice physicalDevice,
 
 fail_queue:
    nvk_queue_finish(device, &device->queue);
+fail_vab_memory:
+   if (device->vab_memory)
+      nouveau_ws_bo_destroy(device->vab_memory);
 fail_zero_page:
    nouveau_ws_bo_destroy(device->zero_page);
 fail_queue_submit:
@@ -277,6 +291,8 @@ nvk_DestroyDevice(VkDevice _device, const VkAllocationCallbacks *pAllocator)
    pthread_cond_destroy(&device->queue_submit);
    pthread_mutex_destroy(&device->mutex);
    nvk_queue_finish(device, &device->queue);
+   if (device->vab_memory)
+      nouveau_ws_bo_destroy(device->vab_memory);
    nouveau_ws_bo_destroy(device->zero_page);
    vk_device_finish(&device->vk);
    nvk_slm_area_finish(&device->slm);

src/nouveau/vulkan/nvk_device.h
@@ -40,6 +40,7 @@ struct nvk_device {
    struct nvk_heap event_heap;
    struct nvk_slm_area slm;
    struct nouveau_ws_bo *zero_page;
+   struct nouveau_ws_bo *vab_memory;
 
    struct nvk_queue queue;
 

commit 47663ca212289bbbfc24c2e178f7e1bd98bc5d76

nvk: Allocate the correct VAB size on Kepler
We were allocating 128 KiB but claimed 256 KiB.  Allocate the right size
and assert that the size matches.

Fixes: 6742c5373fd2 ("nvk: allocate VAB memory area")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35172>

src/nouveau/vulkan/nvk_cmd_draw.c
@@ -576,6 +576,7 @@ nvk_push_draw_state_init(struct nvk_queue *queue, struct nv_push *p)
       P_MTHD(p, NV9097, SET_VAB_MEMORY_AREA_A);
       P_NV9097_SET_VAB_MEMORY_AREA_A(p, vab_addr >> 32);
       P_NV9097_SET_VAB_MEMORY_AREA_B(p, vab_addr);
+      assert(dev->vab_memory->va->size_B == 256 * 1024);
       P_NV9097_SET_VAB_MEMORY_AREA_C(p, SIZE_BYTES_256K);
    }
 

src/nouveau/vulkan/nvk_device.c
@@ -243,7 +243,7 @@ nvk_CreateDevice(VkPhysicalDevice physicalDevice,
        pdev->info.cls_eng3d < MAXWELL_A) {
       /* max size is 256k */
       result = nvkmd_dev_alloc_mem(dev->nvkmd, &pdev->vk.base,
-                                   1 << 17, 1 << 20, NVKMD_MEM_LOCAL,
+                                   256 * 1024, 0, NVKMD_MEM_LOCAL,
                                    &dev->vab_memory);
       if (result != VK_SUCCESS)
          goto fail_slm;