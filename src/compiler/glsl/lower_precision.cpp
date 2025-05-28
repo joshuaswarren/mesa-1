openharmony/third_party_mesa3d

commit ae7f5fc09387566f1eb0ece8de344135a9584d89

glsl: lower samplers with highp coordinates correctly
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5002>

src/compiler/glsl/lower_precision.cpp
@@ -449,6 +449,24 @@ is_lowerable_builtin(ir_call *ir,
          return desc->channel[i].size <= 10; /* unorm/snorm */
    }
 
+   /* Handle special calls. */
+   if (ir->callee->is_builtin() && ir->actual_parameters.length()) {
+      ir_rvalue *param = (ir_rvalue*)ir->actual_parameters.get_head();
+      ir_variable *var = param->variable_referenced();
+
+      /* Handle builtin wrappers around ir_texture opcodes. These wrappers will
+       * be inlined by lower_precision() if we return true here, so that we can
+       * get to ir_texture later and do proper lowering.
+       *
+       * We should lower the type of the return value if the sampler type
+       * uses lower precision. The function parameters don't matter.
+       */
+      if (var && var->type->without_array()->is_sampler()) {
+         return var->data.precision == GLSL_PRECISION_MEDIUM ||
+                var->data.precision == GLSL_PRECISION_LOW;
+      }
+   }
+
    if (!ir->callee->is_builtin())
       return false;
 

src/compiler/glsl/tests/lower_precision_test.py
@@ -921,12 +921,33 @@ TESTS = [
          }
          """,
          r'\(expression +uint +/'),
+    Test("f32 sampler array",
+         """
+         #version 320 es
+         precision mediump float;
+         precision mediump int;
+
+         uniform sampler2D tex[2];
+         // highp shouldn't affect the return value of texture2D
+         uniform highp vec2 coord;
+         uniform float divisor;
+         uniform int index;
+
+         out highp vec4 color;
+
+         void main()
+         {
+                 color = texture2D(tex[index], coord) / divisor;
+         }
+         """,
+         r'\(expression +f16vec4 +/.*\(tex +f16vec4 +'),
     Test("f32 texture sample",
          """
          precision mediump float;
 
          uniform sampler2D tex;
-         uniform vec2 coord;
+         // highp shouldn't affect the return value of texture2D
+         uniform highp vec2 coord;
          uniform float divisor;
 
          void main()
@@ -942,7 +963,8 @@ TESTS = [
          precision mediump int;
 
          uniform mediump isampler2D tex;
-         uniform vec2 coord;
+         // highp shouldn't affect the return value of texture
+         uniform highp vec2 coord;
          uniform int divisor;
 
          out highp ivec4 color;
@@ -960,7 +982,8 @@ TESTS = [
          precision mediump int;
 
          uniform mediump usampler2D tex;
-         uniform vec2 coord;
+         // highp shouldn't affect the return value of texture
+         uniform highp vec2 coord;
          uniform uint divisor;
 
          out highp uvec4 color;
@@ -996,7 +1019,8 @@ TESTS = [
          precision mediump int;
 
          layout(rgba16f) readonly uniform mediump image2D img;
-         uniform ivec2 coord;
+         // highp shouldn't affect the return value of imageLoad
+         uniform highp ivec2 coord;
          uniform float divisor;
 
          out highp vec4 color;
@@ -1014,7 +1038,8 @@ TESTS = [
          precision mediump int;
 
          layout(rgba16i) readonly uniform mediump iimage2D img;
-         uniform ivec2 coord;
+         // highp shouldn't affect the return value of imageLoad
+         uniform highp ivec2 coord;
          uniform int divisor;
 
          out highp ivec4 color;
@@ -1032,7 +1057,8 @@ TESTS = [
          precision mediump int;
 
          layout(rgba16ui) readonly uniform mediump uimage2D img;
-         uniform ivec2 coord;
+         // highp shouldn't affect the return value of imageLoad
+         uniform highp ivec2 coord;
          uniform uint divisor;
 
          out highp uvec4 color;

joshuaswarren/mesa-1

commit 75ec370009ff73ade58a0a1bbc1ee56a5de69aee

glsl: fix sampler and image type checking in lower_precision
Use the param type, not the referenced variable. The referenced variable
can be a structure, which wouldn't be recognized as a sampler or image.

Fixes: ae7f5fc09387 - glsl: lower samplers with highp coordinates correctly

Acked-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Tested-by: Dieter Nützel Dieter@nuetzel-hh.de on gfx8 (Polaris 20)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34959>

src/compiler/glsl/lower_precision.cpp
@@ -423,7 +423,7 @@ handle_call(ir_call *ir, const struct set *lowerable_rvalues)
       ir_variable *resource = param->variable_referenced();
 
       assert(ir->callee->return_precision == GLSL_PRECISION_HIGH);
-      assert(glsl_type_is_image(glsl_without_array(resource->type)));
+      assert(glsl_type_is_image(param->type));
 
       /* GLSL ES 3.20 requires that images have a precision modifier, but if
        * you set one, it doesn't do anything, because all intrinsics are
@@ -466,7 +466,7 @@ handle_call(ir_call *ir, const struct set *lowerable_rvalues)
        * We should lower the type of the return value if the sampler type
        * uses lower precision. The function parameters don't matter.
        */
-      if (var && glsl_type_is_sampler(glsl_without_array(var->type))) {
+      if (var && glsl_type_is_sampler(param->type)) {
          /* textureGatherOffsets always takes a highp array of constants. As
           * per the discussion https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16547#note_1393704
           * trying to lower the precision results in segfault later on