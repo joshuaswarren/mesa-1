joshuaswarren/mesa-1

commit 0a78b61695799b533eef79b07de0635a9b42e798

zink: Check queue families when binding image resources
We check for iamge layouts and feedback loops when we bind image
resources but not queue families.  If the resource isn't on the graphics
queue, we need to add it to need_barriers so we can transition it back
to our queue.

Fixes: 12b7192c69c4 ("zink: handle implicit sync for dmabufs")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33952>

src/gallium/drivers/zink/zink_context.c
@@ -1853,6 +1853,9 @@ check_for_layout_update(struct zink_context *ctx, struct zink_resource *res, boo
    if (!is_compute && res->fb_binds && !(ctx->feedback_loops & res->fb_binds)) {
       /* always double check feedback loops */
       ret = !!_mesa_set_add(ctx->need_barriers[0], res);
+   } else if (res->queue != zink_screen(ctx->base.screen)->gfx_queue && res->queue != VK_QUEUE_FAMILY_IGNORED) {
+      /* Check if we need a queue family transfer */
+      ret = !!_mesa_set_add(ctx->need_barriers[0], res);
    } else {
       if (res->bind_count[is_compute] && layout && res->layout != layout)
          ret = !!_mesa_set_add(ctx->need_barriers[is_compute], res);

commit 605635176035a4941b270b2b961ab447a0013a8b

zink: fix queue transition check in check_for_layout_update()
this only applies if the resource has active binds, otherwise it triggers crashes

Fixes: 0a78b616957 ("zink: Check queue families when binding image resources")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35234>

src/gallium/drivers/zink/zink_context.c
@@ -1844,11 +1844,10 @@ check_for_layout_update(struct zink_context *ctx, struct zink_resource *res, boo
    if (!is_compute && res->fb_binds && !(ctx->feedback_loops & res->fb_binds)) {
       /* always double check feedback loops */
       ret = !!_mesa_set_add(ctx->need_barriers[0], res);
-   } else if (res->queue != zink_screen(ctx->base.screen)->gfx_queue && res->queue != VK_QUEUE_FAMILY_IGNORED) {
-      /* Check if we need a queue family transfer */
-      ret = !!_mesa_set_add(ctx->need_barriers[0], res);
    } else {
-      if (res->bind_count[is_compute] && layout && res->layout != layout)
+      if (res->bind_count[is_compute] && ((layout && res->layout != layout) ||
+                                          /* Check if we need a queue family transfer */
+                                          (res->queue != zink_screen(ctx->base.screen)->gfx_queue && res->queue != VK_QUEUE_FAMILY_IGNORED)))
          ret = !!_mesa_set_add(ctx->need_barriers[is_compute], res);
       if (res->bind_count[!is_compute] && other_layout && (layout != other_layout || res->layout != other_layout))
          ret = !!_mesa_set_add(ctx->need_barriers[!is_compute], res);