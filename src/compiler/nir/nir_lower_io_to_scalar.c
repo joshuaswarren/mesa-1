joshuaswarren/mesa-1

commit 1f50b2c344de8942919db0b2b0d6e9fbf499e899

nir/lower_io_to_scalar: don't create output stores that have no effect
This fixes NIR validation errors that happen with certain shaders.

Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29406>

src/compiler/nir/nir_lower_io_to_scalar.c
@@ -144,6 +144,30 @@ lower_store_output_to_scalar(nir_builder *b, nir_intrinsic_instr *intr)
       unsigned newi = is_64bit ? i * 2 : i;
       unsigned newc = nir_intrinsic_component(intr);
       unsigned new_component = (newc + newi) % 4;
+      nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
+      bool has_xfb = false;
+
+      if (nir_intrinsic_has_io_xfb(intr)) {
+         /* Find out which components are written via xfb. */
+         for (unsigned c = 0; c <= new_component; c++) {
+            nir_io_xfb xfb = c < 2 ? nir_intrinsic_io_xfb(intr) : nir_intrinsic_io_xfb2(intr);
+
+            if (new_component < c + xfb.out[c % 2].num_components) {
+               has_xfb = true;
+               break;
+            }
+         }
+      }
+
+      /* After scalarization, some channels might not write anywhere - i.e.
+       * they are not a sysval output, they don't feed the next shader, and
+       * they don't write xfb. Don't create such stores.
+       */
+      if ((sem.no_sysval_output ||
+           !nir_slot_is_sysval_output(sem.location, MESA_SHADER_NONE)) &&
+          (sem.no_varying || !nir_slot_is_varying(sem.location)) &&
+          !has_xfb)
+         continue;
 
       nir_intrinsic_instr *chan_intr =
          nir_intrinsic_instr_create(b->shader, intr->intrinsic);

commit 9d641189a49df6a7ff1ee6f5a40dacc43ebaa34a

nir/lower_to_scalar: fix opt_varying with output reads
no_varying cannot be used to eliminate stores on locations which may
be subsequently read

Fixes: 1f50b2c344d ("nir/lower_io_to_scalar: don't create output stores that have no effect")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35325>

src/compiler/nir/nir_lower_io_to_scalar.c
@@ -159,9 +159,16 @@ lower_store_output_to_scalar(nir_builder *b, nir_intrinsic_instr *intr)
        * they are not a sysval output, they don't feed the next shader, and
        * they don't write xfb. Don't create such stores.
        */
-      if ((sem.no_sysval_output ||
-           !nir_slot_is_sysval_output(sem.location, MESA_SHADER_NONE)) &&
-          (sem.no_varying ||
+      bool tcs_read = b->shader->info.stage == MESA_SHADER_TESS_CTRL &&
+                (sem.location >= VARYING_SLOT_VAR0_16BIT ?
+                    b->shader->info.outputs_read_16bit &
+                    BITFIELD_BIT(sem.location - VARYING_SLOT_VAR0_16BIT) :
+                 sem.location >= VARYING_SLOT_PATCH0 ?
+                    b->shader->info.patch_outputs_read &
+                    BITFIELD_BIT(sem.location - VARYING_SLOT_PATCH0) :
+                 b->shader->info.outputs_read & BITFIELD64_BIT(sem.location));
+      if ((sem.no_sysval_output || !nir_slot_is_sysval_output(sem.location, MESA_SHADER_NONE)) &&
+          ((sem.no_varying && !tcs_read) ||
            !nir_slot_is_varying(sem.location, MESA_SHADER_NONE)) &&
           !has_xfb)
          continue;