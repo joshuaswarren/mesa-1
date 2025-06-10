openharmony/third_party_mesa3d

commit 7332115e5efbb58d85745be532709be4076ec0ab

r600: don't set an index_bias for indirect draw calls
The bug #4657 mixed up which commit was actually responsible for
the regression, so re-apply this patch.

The indirect draw call already encodes the index bias so that no
additional encoding in the hardware is needed in this case.

This fixes a regression with a number of tests from
   dEQP-GLES31.functional.draw_indirect.random.*

Fixes: 8dad19adfd03eaa43e8845f41998fdc8eb415f0c
  "gallium/u_vbuf: use updated pipe_draw_start_count while using draw_vbo"

Closes #4671

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10388>

src/gallium/drivers/r600/r600_state_common.c
@@ -2225,7 +2225,7 @@ static void r600_draw_vbo(struct pipe_context *ctx, const struct pipe_draw_info 
 		}
 		index_bias = info->index_bias;
 	} else {
-		index_bias = draws[0].start;
+		index_bias = indirect ? 0 : draws[0].start;
 	}
 
 	/* Set the index offset and primitive restart. */

joshuaswarren/mesa-1

commit a1f2035c5e5f39f9381129b27cc3a5a68fbefca7

r600: index_bias should be forced to zero for all indirect draw calls
The previous fix 7332115e5efb is the right way to proceed, but it
should also apply when index_size is non-zero.

This change was tested on palm and cayman. Here is the test fixed:
spec/arb_multi_draw_indirect/arb_draw_elements_base_vertex-multidrawelements -indirect: fail pass

Fixes: 7332115e5efb ("r600: don't set an index_bias for indirect draw calls")
Signed-off-by: Patrick Lerda <patrick9876@free.fr>
Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34726>

src/gallium/drivers/r600/r600_state_common.c
@@ -2277,7 +2277,7 @@ static void r600_draw_vbo(struct pipe_context *ctx, const struct pipe_draw_info 
 			index_offset -= start_offset;
 			has_user_indices = false;
 		}
-		index_bias = draws->index_bias;
+		index_bias = unlikely(indirect) ? 0 : draws->index_bias;
 	} else {
 		index_bias = indirect ? 0 : draws[0].start;
 	}