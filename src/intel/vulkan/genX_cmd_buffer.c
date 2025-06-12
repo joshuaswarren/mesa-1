joshuaswarren/mesa-1

commit 26a148e167d25d83263841ca8e2fada83ad1e9b6

anv: implement VK_KHR_maintenance9
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35381>

src/intel/vulkan/genX_cmd_buffer.c
@@ -1150,6 +1150,10 @@ transition_color_buffer(struct anv_cmd_buffer *cmd_buffer,
       anv_layout_to_fast_clear_type(devinfo, image, aspect, final_layout,
                                     dst_queue_flags);
 
+   /* We make this guarantee with optimalImageTransferToQueueFamilies */
+   if (devinfo->ver >= 20 && (src_queue_external || dst_queue_external))
+      assert(initial_aux_usage == final_aux_usage);
+
    /* We must override the anv_layout_to_* functions because they are unaware
     * of acquire/release direction.
     */

src/intel/vulkan/anv_physical_device.c
@@ -198,6 +198,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_maintenance6                      = true,
       .KHR_maintenance7                      = true,
       .KHR_maintenance8                      = true,
+      .KHR_maintenance9                      = true,
       .KHR_map_memory2                       = true,
       .KHR_multiview                         = true,
       .KHR_performance_query =
@@ -964,6 +965,9 @@ get_features(const struct anv_physical_device *pdevice,
       /* VK_KHR_fragment_shader_barycentric */
       .fragmentShaderBarycentric =
          pdevice->vk.supported_extensions.KHR_fragment_shader_barycentric,
+
+      /* VK_KHR_maintenance9 */
+      .maintenance9 = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without
@@ -1515,6 +1519,14 @@ get_properties(const struct anv_physical_device *pdevice,
       props->maxDescriptorSetUpdateAfterBindTotalUniformBuffersDynamic = MAX_DYNAMIC_BUFFERS;
       props->maxDescriptorSetUpdateAfterBindTotalStorageBuffersDynamic = MAX_DYNAMIC_BUFFERS;
       props->maxDescriptorSetUpdateAfterBindTotalBuffersDynamic = MAX_DYNAMIC_BUFFERS;
+   }
+
+   /* VK_KHR_maintenance9 */
+   {
+      /* Swizzling of Tile64 images is different in 2D/3D */
+      props->image2DViewOf3DSparse = false;
+      props->defaultVertexAttributeValue =
+         VK_DEFAULT_VERTEX_ATTRIBUTE_VALUE_ZERO_ZERO_ZERO_ZERO_KHR;
    }
 
    /* VK_KHR_performance_query */
@@ -2925,6 +2937,17 @@ void anv_GetPhysicalDeviceQueueFamilyProperties2(
                }
                break;
             }
+
+            case VK_STRUCTURE_TYPE_QUEUE_FAMILY_OWNERSHIP_TRANSFER_PROPERTIES_KHR: {
+               VkQueueFamilyOwnershipTransferPropertiesKHR *prop =
+                  (VkQueueFamilyOwnershipTransferPropertiesKHR *)ext;
+               if (pdevice->info.ver >= 20)
+                  prop->optimalImageTransferToQueueFamilies = BITSET_MASK(pdevice->queue.family_count);
+               else
+                  prop->optimalImageTransferToQueueFamilies = 0;
+               break;
+            }
+
             default:
                vk_debug_ignored_stype(ext->sType);
             }

src/intel/vulkan/genX_query.c
@@ -51,6 +51,12 @@
 
 #include "vk_util.h"
 
+static void *
+query_slot(struct anv_query_pool *pool, uint32_t query)
+{
+   return pool->bo->map + query * pool->stride;
+}
+
 static struct anv_address
 anv_query_address(struct anv_query_pool *pool, uint32_t query)
 {
@@ -321,6 +327,13 @@ VkResult genX(CreateQueryPool)(
       }
    }
 
+   if (pCreateInfo->flags & VK_QUERY_POOL_CREATE_RESET_BIT_KHR) {
+      for (uint32_t q = 0; q < pool->vk.query_count; q++) {
+         uint64_t *slot = query_slot(pool, q);
+         *slot = 0;
+      }
+   }
+
    ANV_RMV(query_pool_create, device, pool, false);
 
    *pQueryPool = anv_query_pool_to_handle(pool);
@@ -475,12 +488,6 @@ cpu_write_query_result(void *dst_slot, VkQueryResultFlags flags,
       uint32_t *dst32 = dst_slot;
       dst32[value_index] = result;
    }
-}
-
-static void *
-query_slot(struct anv_query_pool *pool, uint32_t query)
-{
-   return pool->bo->map + query * pool->stride;
 }
 
 static bool

commit 901d6368260e18183261fbf4da6b5a0fe69bd478

anv: missing bit from maintenance9
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 26a148e167 ("anv: implement VK_KHR_maintenance9")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35467>

src/intel/vulkan/genX_cmd_buffer.c
@@ -4315,9 +4315,9 @@ cmd_buffer_accumulate_barrier_bits(struct anv_cmd_buffer *cmd_buffer,
    VkPipelineStageFlags2 src_stages = 0;
    VkPipelineStageFlags2 dst_stages = 0;
 
+   struct anv_device *device = cmd_buffer->device;
 #if GFX_VER < 20
    bool apply_sparse_flushes = false;
-   struct anv_device *device = cmd_buffer->device;
 #endif
    bool flush_query_copies = false;
 
@@ -4434,8 +4434,22 @@ cmd_buffer_accumulate_barrier_bits(struct anv_cmd_buffer *cmd_buffer,
 
          uint32_t base_layer, layer_count;
          if (image->vk.image_type == VK_IMAGE_TYPE_3D) {
-            base_layer = 0;
-            layer_count = u_minify(image->vk.extent.depth, range->baseMipLevel);
+            /* VK_KHR_maintenance9:
+             *
+             *    "The effects of image memory barriers and image layout
+             *    transitions on 3D images created with
+             *    VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT are scoped to the
+             *    slices specified by the user-provided
+             *    VkImageSubresourceRange."
+             */
+            if ((image->vk.create_flags & VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT) &&
+                device->vk.enabled_features.maintenance9) {
+               base_layer = range->baseArrayLayer;
+               layer_count = range->layerCount;
+            } else {
+               base_layer = 0;
+               layer_count = u_minify(image->vk.extent.depth, range->baseMipLevel);
+            }
          } else {
             base_layer = range->baseArrayLayer;
             layer_count = vk_image_subresource_layer_count(&image->vk, range);