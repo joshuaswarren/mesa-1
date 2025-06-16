joshuaswarren/mesa-1

commit c47de2b11e749bd85914183c4a49d4abccaaa402

intel/dev: Add BMG PCI IDs 0xe220-0xe223
Ref: bspec 68090
Backport-to: 25.0, 25.1
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35139>

include/pci_ids/iris_pci_ids.h
@@ -282,6 +282,10 @@ CHIPSET(0xe211, bmg, "BMG G21", "Intel(R) Graphics")
 CHIPSET(0xe212, bmg, "BMG G21", "Intel(R) Graphics")
 CHIPSET(0xe215, bmg, "BMG", "Intel(R) Graphics")
 CHIPSET(0xe216, bmg, "BMG", "Intel(R) Graphics")
+CHIPSET(0xe220, bmg, "BMG", "Intel(R) Graphics")
+CHIPSET(0xe221, bmg, "BMG", "Intel(R) Graphics")
+CHIPSET(0xe222, bmg, "BMG", "Intel(R) Graphics")
+CHIPSET(0xe223, bmg, "BMG", "Intel(R) Graphics")
 
 CHIPSET(0xb080, ptl, "PTL", "Intel(R) Graphics", FORCE_PROBE)
 CHIPSET(0xb081, ptl, "PTL", "Intel(R) Graphics", FORCE_PROBE)

commit 8d971682a306c4b9042260f51c5cae5803bd5842

intel/dev: Update names for BMG G31 PCI IDs
Ref: bspec 68090
Fixes: c47de2b11e7 ("intel/dev: Add BMG PCI IDs 0xe220-0xe223")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35509>

include/pci_ids/iris_pci_ids.h
@@ -282,10 +282,10 @@ CHIPSET(0xe211, bmg, "BMG G21", "Intel(R) Graphics")
 CHIPSET(0xe212, bmg, "BMG G21", "Intel(R) Graphics")
 CHIPSET(0xe215, bmg, "BMG", "Intel(R) Graphics")
 CHIPSET(0xe216, bmg, "BMG", "Intel(R) Graphics")
-CHIPSET(0xe220, bmg, "BMG", "Intel(R) Graphics")
-CHIPSET(0xe221, bmg, "BMG", "Intel(R) Graphics")
-CHIPSET(0xe222, bmg, "BMG", "Intel(R) Graphics")
-CHIPSET(0xe223, bmg, "BMG", "Intel(R) Graphics")
+CHIPSET(0xe220, bmg, "BMG G31", "Intel(R) Graphics")
+CHIPSET(0xe221, bmg, "BMG G31", "Intel(R) Graphics")
+CHIPSET(0xe222, bmg, "BMG G31", "Intel(R) Graphics")
+CHIPSET(0xe223, bmg, "BMG G31", "Intel(R) Graphics")
 
 CHIPSET(0xb080, ptl, "PTL", "Intel(R) Graphics", FORCE_PROBE)
 CHIPSET(0xb081, ptl, "PTL", "Intel(R) Graphics", FORCE_PROBE)