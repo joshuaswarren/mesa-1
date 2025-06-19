joshuaswarren/mesa-1

commit 969cd0e3bc2b3bb0e0fbb56e4a2a03fe3b4acc55

anv: avoid memory type changes with INTEL_DEBUG=noccs
Otherwise replay of renderdoc captures don't work.

Instead avoid passing the flag down the allocator.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33583>

src/intel/vulkan/xe/anv_device.c
@@ -99,7 +99,7 @@ VkResult
 anv_xe_physical_device_init_memory_types(struct anv_physical_device *device)
 {
    if (anv_physical_device_has_vram(device)) {
-      if (device->info.ver >= 20 && !INTEL_DEBUG(DEBUG_NO_CCS)) {
+      if (device->info.ver >= 20) {
          device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
             .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
             .heapIndex = 0,
@@ -149,13 +149,11 @@ anv_xe_physical_device_init_memory_types(struct anv_physical_device *device)
          .heapIndex = 0,
       };
    } else {
-      if (device->info.ver >= 20 && !INTEL_DEBUG(DEBUG_NO_CCS)) {
-         device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
-            .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-            .heapIndex = 0,
-            .compressed = true,
-         };
-      }
+      device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
+         .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+         .heapIndex = 0,
+         .compressed = true,
+      };
       device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
          .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
                           VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |

src/intel/vulkan/anv_device.c
@@ -1552,7 +1552,9 @@ VkResult anv_AllocateMemory(
    /* TODO: Disabling compression on external bos will cause problems once we
     * have a modifier that supports compression (Xe2+).
     */
-   if (!(alloc_flags & ANV_BO_ALLOC_EXTERNAL) && mem_type->compressed)
+   if (!(alloc_flags & ANV_BO_ALLOC_EXTERNAL) &&
+       mem_type->compressed &&
+       !INTEL_DEBUG(DEBUG_NO_CCS))
       alloc_flags |= ANV_BO_ALLOC_COMPRESSED;
 
    if (mem_type->dynamic_visible)

src/intel/vulkan/anv_image.c
@@ -2144,9 +2144,6 @@ resolve_anb_image(struct anv_device *device,
 static bool
 anv_image_is_pat_compressible(struct anv_device *device, struct anv_image *image)
 {
-   if (INTEL_DEBUG(DEBUG_NO_CCS))
-      return false;
-
    if (device->info->ver < 20)
       return false;
 

commit 41750a5bf53c5796ef9bed934ccece1673475231

anv: only use compressed memory types on Xe2+
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 969cd0e3bc ("anv: avoid memory type changes with INTEL_DEBUG=noccs")
Tested-by: Ian Romanick <ian.d.romanick@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35615>

src/intel/vulkan/xe/anv_device.c
@@ -154,7 +154,7 @@ anv_xe_physical_device_init_memory_types(struct anv_physical_device *device)
       device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
          .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
          .heapIndex = 0,
-         .compressed = true,
+         .compressed = device->info.ver >= 20,
       };
       device->memory.types[device->memory.type_count++] = (struct anv_memory_type) {
          .propertyFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |