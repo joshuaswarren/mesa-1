joshuaswarren/mesa-1

commit a3eaf7332534eb37088a86497d9fd71af8278c20

panvk: advertise descriptor indexing on valhall
Our existing descriptor implementation already supports
update-after-bind on both bifrost and valhall, but on bifrost the some
of the descriptor limits are lower than the minimum required by the
extension.

Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35408>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -125,6 +125,7 @@ panvk_per_arch(get_physical_device_extensions)(
 #ifdef VK_USE_PLATFORM_DISPLAY_KHR
       .EXT_display_control = true,
 #endif
+      .EXT_descriptor_indexing = PAN_ARCH >= 9,
       .EXT_extended_dynamic_state = true,
       .EXT_extended_dynamic_state2 = true,
       .EXT_external_memory_dma_buf = true,
@@ -281,7 +282,10 @@ panvk_per_arch(get_physical_device_features)(
       .shaderFloat16 = PAN_ARCH >= 10,
       .shaderInt8 = true,
 
-      .descriptorIndexing = false,
+      /* In theory, update-after-bind is supported on bifrost, but the
+       * descriptor limits would be too low for the descriptorIndexing feature.
+       */
+      .descriptorIndexing = PAN_ARCH >= 9,
       .shaderInputAttachmentArrayDynamicIndexing = true,
       .shaderUniformTexelBufferArrayDynamicIndexing = true,
       .shaderStorageTexelBufferArrayDynamicIndexing = true,
@@ -292,14 +296,14 @@ panvk_per_arch(get_physical_device_features)(
       .shaderInputAttachmentArrayNonUniformIndexing = true,
       .shaderUniformTexelBufferArrayNonUniformIndexing = true,
       .shaderStorageTexelBufferArrayNonUniformIndexing = true,
-      .descriptorBindingUniformBufferUpdateAfterBind = false,
-      .descriptorBindingSampledImageUpdateAfterBind = false,
-      .descriptorBindingStorageImageUpdateAfterBind = false,
-      .descriptorBindingStorageBufferUpdateAfterBind = false,
-      .descriptorBindingUniformTexelBufferUpdateAfterBind = false,
-      .descriptorBindingStorageTexelBufferUpdateAfterBind = false,
-      .descriptorBindingUpdateUnusedWhilePending = false,
-      .descriptorBindingPartiallyBound = false,
+      .descriptorBindingUniformBufferUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingSampledImageUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingStorageImageUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingStorageBufferUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingUniformTexelBufferUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingStorageTexelBufferUpdateAfterBind = PAN_ARCH >= 9,
+      .descriptorBindingUpdateUnusedWhilePending = PAN_ARCH >= 9,
+      .descriptorBindingPartiallyBound = PAN_ARCH >= 9,
       .descriptorBindingVariableDescriptorCount = true,
       .runtimeDescriptorArray = true,
 
@@ -769,30 +773,46 @@ panvk_per_arch(get_physical_device_properties)(
       .shaderRoundingModeRTZFloat16 = true,
       .shaderRoundingModeRTZFloat32 = true,
       .shaderRoundingModeRTZFloat64 = false,
-      /* XXX: VK_EXT_descriptor_indexing */
-      .maxUpdateAfterBindDescriptorsInAllPools = 0,
+      /* VK_EXT_descriptor_indexing */
+      .maxUpdateAfterBindDescriptorsInAllPools =
+         PAN_ARCH >= 9 ? UINT32_MAX : 0,
       .shaderUniformBufferArrayNonUniformIndexingNative = false,
       .shaderSampledImageArrayNonUniformIndexingNative = false,
       .shaderStorageBufferArrayNonUniformIndexingNative = false,
       .shaderStorageImageArrayNonUniformIndexingNative = false,
       .shaderInputAttachmentArrayNonUniformIndexingNative = false,
-      .robustBufferAccessUpdateAfterBind = false,
+      .robustBufferAccessUpdateAfterBind = PAN_ARCH >= 9,
       .quadDivergentImplicitLod = false,
-      .maxPerStageDescriptorUpdateAfterBindSamplers = 0,
-      .maxPerStageDescriptorUpdateAfterBindUniformBuffers = 0,
-      .maxPerStageDescriptorUpdateAfterBindStorageBuffers = 0,
-      .maxPerStageDescriptorUpdateAfterBindSampledImages = 0,
-      .maxPerStageDescriptorUpdateAfterBindStorageImages = 0,
-      .maxPerStageDescriptorUpdateAfterBindInputAttachments = 0,
-      .maxPerStageUpdateAfterBindResources = 0,
-      .maxDescriptorSetUpdateAfterBindSamplers = 0,
-      .maxDescriptorSetUpdateAfterBindUniformBuffers = 0,
-      .maxDescriptorSetUpdateAfterBindUniformBuffersDynamic = 0,
-      .maxDescriptorSetUpdateAfterBindStorageBuffers = 0,
-      .maxDescriptorSetUpdateAfterBindStorageBuffersDynamic = 0,
-      .maxDescriptorSetUpdateAfterBindSampledImages = 0,
-      .maxDescriptorSetUpdateAfterBindStorageImages = 0,
-      .maxDescriptorSetUpdateAfterBindInputAttachments = 0,
+      .maxPerStageDescriptorUpdateAfterBindSamplers =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_SAMPLERS : 0,
+      .maxPerStageDescriptorUpdateAfterBindUniformBuffers =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_UNIFORM_BUFFERS : 0,
+      .maxPerStageDescriptorUpdateAfterBindStorageBuffers =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_STORAGE_BUFFERS : 0,
+      .maxPerStageDescriptorUpdateAfterBindSampledImages =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_SAMPLED_IMAGES : 0,
+      .maxPerStageDescriptorUpdateAfterBindStorageImages =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_STORAGE_IMAGES : 0,
+      .maxPerStageDescriptorUpdateAfterBindInputAttachments =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_INPUT_ATTACHMENTS : 0,
+      .maxPerStageUpdateAfterBindResources =
+         PAN_ARCH >= 9 ? MAX_PER_STAGE_RESOURCES : 0,
+      .maxDescriptorSetUpdateAfterBindSamplers =
+         PAN_ARCH >= 9 ? MAX_PER_SET_SAMPLERS : 0,
+      .maxDescriptorSetUpdateAfterBindUniformBuffers =
+         PAN_ARCH >= 9 ? MAX_PER_SET_UNIFORM_BUFFERS : 0,
+      .maxDescriptorSetUpdateAfterBindUniformBuffersDynamic =
+         PAN_ARCH >= 9 ? MAX_DYNAMIC_UNIFORM_BUFFERS : 0,
+      .maxDescriptorSetUpdateAfterBindStorageBuffers =
+         PAN_ARCH >= 9 ? MAX_PER_SET_STORAGE_BUFFERS : 0,
+      .maxDescriptorSetUpdateAfterBindStorageBuffersDynamic =
+         PAN_ARCH >= 9 ? MAX_DYNAMIC_STORAGE_BUFFERS : 0,
+      .maxDescriptorSetUpdateAfterBindSampledImages =
+         PAN_ARCH >= 9 ? MAX_PER_SET_SAMPLED_IMAGES : 0,
+      .maxDescriptorSetUpdateAfterBindStorageImages =
+         PAN_ARCH >= 9 ? MAX_PER_SET_STORAGE_IMAGES : 0,
+      .maxDescriptorSetUpdateAfterBindInputAttachments =
+         PAN_ARCH >= 9 ? MAX_PER_SET_INPUT_ATTACHMENTS : 0,
       .filterMinmaxSingleComponentFormats = PAN_ARCH >= 10,
       .filterMinmaxImageComponentMapping = PAN_ARCH >= 10,
       .maxTimelineSemaphoreValueDifference = INT64_MAX,

commit b3adf03f1b34af9f5b6454130ef15f58d054fbef

panvk: report the maximum supported size for a variable count iub
If VkDescriptorSetVariableDescriptorCountLayoutSupport is passed to
vkGetDescriptorSetLayoutSupport, we should report the maximum size in
bytes for inline uniform blocks.

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Fixes: a3eaf733 ("panvk: advertise descriptor indexing on valhall")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35545>

src/panfrost/vulkan/panvk_descriptor_set_layout.h
@@ -19,10 +19,11 @@
 
 #include "genxml/gen_macros.h"
 
-#define PANVK_DESCRIPTOR_SIZE       32
-#define MAX_DYNAMIC_UNIFORM_BUFFERS 16
-#define MAX_DYNAMIC_STORAGE_BUFFERS 8
-#define MAX_PUSH_DESCS              32
+#define PANVK_DESCRIPTOR_SIZE         32
+#define MAX_DYNAMIC_UNIFORM_BUFFERS   16
+#define MAX_DYNAMIC_STORAGE_BUFFERS   8
+#define MAX_PUSH_DESCS                32
+#define MAX_INLINE_UNIFORM_BLOCK_SIZE (1 << 16)
 #define MAX_DYNAMIC_BUFFERS                                                    \
    (MAX_DYNAMIC_UNIFORM_BUFFERS + MAX_DYNAMIC_STORAGE_BUFFERS)
 

src/panfrost/vulkan/panvk_vX_descriptor_set_layout.c
@@ -253,6 +253,7 @@ panvk_per_arch(GetDescriptorSetLayoutSupport)(
 
    unsigned desc_count = 0, dyn_buf_count = 0, non_variable_count = 0,
             variable_stride = 0;
+   VkDescriptorType variable_type = {0};
    for (unsigned i = 0; i < pCreateInfo->bindingCount; i++) {
       const VkDescriptorSetLayoutBinding *binding = &pCreateInfo->pBindings[i];
       VkDescriptorType type = binding->descriptorType;
@@ -321,6 +322,7 @@ panvk_per_arch(GetDescriptorSetLayoutSupport)(
                type == VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK ? 4 : 1;
          }
 
+         variable_type = type;
          variable_stride = stride;
          assert(variable_stride);
       }
@@ -340,7 +342,24 @@ panvk_per_arch(GetDescriptorSetLayoutSupport)(
       return;
 
    pSupport->supported = true;
-   if (var_desc_count)
-      var_desc_count->maxVariableDescriptorCount = variable_stride != 0 ?
-         (PANVK_MAX_DESCS_PER_SET - non_variable_count) / variable_stride : 0;
+
+   if (!var_desc_count)
+      return;
+
+   var_desc_count->maxVariableDescriptorCount = 0;
+
+   if (!variable_stride)
+      return;
+
+   if (variable_type == VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK) {
+      /* Maximum byte size for inline uniform block */
+      unsigned available_size =
+         panvk_get_iub_size(PANVK_MAX_DESCS_PER_SET - non_variable_count);
+      var_desc_count->maxVariableDescriptorCount =
+         MIN2(available_size, MAX_INLINE_UNIFORM_BLOCK_SIZE);
+   } else {
+      /* Maximum descriptor count for any other descriptor type */
+      var_desc_count->maxVariableDescriptorCount =
+         (PANVK_MAX_DESCS_PER_SET - non_variable_count) / variable_stride;
+   }
 }

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -32,7 +32,6 @@
  * descriptor metadata  */
 #define RESERVED_UBO_COUNT                   6
 #define MAX_INLINE_UNIFORM_BLOCK_DESCRIPTORS (32 - RESERVED_UBO_COUNT)
-#define MAX_INLINE_UNIFORM_BLOCK_SIZE        (1 << 16)
 
 void
 panvk_per_arch(get_physical_device_extensions)(