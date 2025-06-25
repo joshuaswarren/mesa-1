joshuaswarren/mesa-1

commit 0a4244759173cd2c4df83e02bb9cb8627b3fdc33

pan/layout: Check the wsi_layout consistency in wsi_row_pitch_to_row_stride()
We do a bunch of WSI -> GPU translation in there, so it makes sense to
also check that the offset and row stride are properly aligned, and
that the pixel width that results from this transformation is greater or
equal to the image width.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Tested-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35015>

src/panfrost/lib/pan_layout.c
@@ -76,15 +76,15 @@ pan_image_renderblock_size_el(uint64_t modifier, enum pipe_format format)
    return pan_afbc_renderblock_size(modifier);
 }
 
-static inline unsigned
-format_minimum_alignment(unsigned arch, enum pipe_format format, uint64_t mod)
+static unsigned
+linear_or_tiled_row_align_req(unsigned arch, enum pipe_format format,
+                              uint64_t modifier)
 {
-   if (drm_is_afbc(mod))
-      return 16;
+   assert(modifier == DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED ||
+          modifier == DRM_FORMAT_MOD_LINEAR);
 
-   if (drm_is_afrc(mod))
-      return pan_afrc_buffer_alignment_from_modifier(mod);
-
+   /* Prior to v7 we assume a cacheline alignment, though this could be relaxed
+    * on some formats if we have to, like we do on v7+. */
    if (arch < 7)
       return 64;
 
@@ -185,25 +185,84 @@ pan_image_layout_get_wsi_layout(const struct pan_image_props *props,
    }
 }
 
-static unsigned
-wsi_row_pitch_to_row_stride(unsigned wsi_row_pitch_B, enum pipe_format format,
-                            uint64_t modifier)
+static bool
+wsi_row_pitch_to_row_stride(unsigned arch, const struct pan_image_props *props,
+                            const struct pan_image_wsi_layout *wsi_layout,
+                            unsigned *row_stride_B)
 {
-   if (drm_is_afbc(modifier)) {
-      unsigned width_px = wsi_row_pitch_B / util_format_get_blocksize(format);
+   unsigned row_align_mask, offset_align_mask, width_px;
+   unsigned wsi_row_pitch_B = wsi_layout->row_pitch_B;
+   enum pipe_format format = props->format;
+   uint64_t modifier = props->modifier;
 
-      return pan_afbc_row_stride(modifier, width_px);
+   if (drm_is_afbc(modifier)) {
+      /* We assume single pixel blocks in AFBC. */
+      assert(util_format_get_blockwidth(format) == 1 &&
+             util_format_get_blockheight(format) == 1);
+
+      struct pan_image_block_size afbc_tile_extent_px =
+         pan_afbc_renderblock_size(modifier);
+      unsigned afbc_tile_payload_size_B = afbc_tile_extent_px.width *
+                                          afbc_tile_extent_px.height *
+                                          util_format_get_blocksize(format);
+      unsigned afbc_tile_payload_row_stride_B =
+         wsi_row_pitch_B * afbc_tile_extent_px.height;
+
+      offset_align_mask = pan_afbc_header_align(arch, modifier) - 1;
+      row_align_mask =
+         pan_afbc_header_row_stride_align(arch, format, modifier) - 1;
+      width_px =
+         (afbc_tile_payload_row_stride_B / afbc_tile_payload_size_B) *
+         afbc_tile_extent_px.width;
+      *row_stride_B = pan_afbc_row_stride(modifier, width_px);
    } else if (drm_is_afrc(modifier)) {
       struct pan_image_block_size tile_size_px =
          pan_afrc_tile_size(format, modifier);
+      unsigned afrc_blk_size_B =
+         pan_afrc_block_size_from_modifier(props->modifier) *
+         AFRC_CLUMPS_PER_TILE;
 
-      return wsi_row_pitch_B * tile_size_px.height;
+      row_align_mask = pan_afrc_buffer_alignment_from_modifier(modifier) - 1;
+      offset_align_mask = row_align_mask;
+      *row_stride_B = wsi_row_pitch_B * tile_size_px.height;
+      width_px = (*row_stride_B / afrc_blk_size_B) * tile_size_px.width;
    } else {
       struct pan_image_block_size block_size_el =
          pan_image_renderblock_size_el(modifier, format);
+      unsigned tile_size_B = block_size_el.width * block_size_el.height *
+                             util_format_get_blocksize(format);
 
-      return wsi_row_pitch_B * block_size_el.height;
+      /* The row_stride_B -> width_px conversion is assuming a 1x1 pixel
+       * block size for non-compressed formats. Revisit when adding support
+       * for block-based YUV. */
+      assert(util_format_is_compressed(format) ||
+             (util_format_get_blockwidth(format) == 1 &&
+              util_format_get_blockheight(format) == 1));
+
+      row_align_mask =
+         linear_or_tiled_row_align_req(arch, format, modifier) - 1;
+      offset_align_mask = row_align_mask;
+      *row_stride_B = wsi_row_pitch_B * block_size_el.height;
+      width_px = (*row_stride_B / tile_size_B) *
+                 (block_size_el.width * util_format_get_blockwidth(format));
    }
+
+   if (*row_stride_B & row_align_mask) {
+      mesa_loge("WSI pitch not properly aligned");
+      return false;
+   }
+
+   if (wsi_layout->offset_B & offset_align_mask) {
+      mesa_loge("WSI offset not properly aligned");
+      return false;
+   }
+
+   if (width_px < props->extent_px.width) {
+      mesa_loge("WSI pitch too small");
+      return false;
+   }
+
+   return true;
 }
 
 /* Computes the offset of an image surface at a particular level/face. Add to
@@ -235,32 +294,21 @@ pan_image_layout_init(unsigned arch, const struct pan_image_props *props,
    bool afbc = drm_is_afbc(props->modifier);
    bool afrc = drm_is_afrc(props->modifier);
    int align_req_B =
-      format_minimum_alignment(arch, props->format, props->modifier);
-   uint64_t offset_B = 0, wsi_row_stride_B = 0;
+      afbc ? pan_afbc_header_row_stride_align(arch, props->format,
+                                              props->modifier)
+      : afrc
+         ? pan_afrc_buffer_alignment_from_modifier(props->modifier)
+         : linear_or_tiled_row_align_req(arch, props->format, props->modifier);
+   unsigned wsi_row_stride_B = 0;
+   uint64_t offset_B = 0;
 
    /* Mandate alignment */
    if (wsi_layout) {
-      bool rejected = false;
-
-      int align_mask = align_req_B - 1;
+      if (!wsi_row_pitch_to_row_stride(arch, props, wsi_layout,
+                                       &wsi_row_stride_B))
+         return false;
 
       offset_B = wsi_layout->offset_B;
-      wsi_row_stride_B = wsi_row_pitch_to_row_stride(
-         wsi_layout->row_pitch_B, props->format, props->modifier);
-
-      if (arch >= 7) {
-         rejected =
-            ((offset_B & align_mask) || (wsi_row_stride_B & align_mask));
-      } else {
-         rejected = (offset_B & align_mask);
-      }
-
-      if (rejected) {
-         mesa_loge(
-            "panfrost: rejecting image due to unsupported offset or stride "
-            "alignment.\n");
-         return false;
-      }
    }
 
    unsigned fmt_blocksize_B = util_format_get_blocksize(props->format);
@@ -321,12 +369,9 @@ pan_image_layout_init(unsigned arch, const struct pan_image_props *props,
       }
 
       if (wsi_layout && !afbc && !afrc) {
-         /* Make sure the explicit stride is valid */
-         if (wsi_row_stride_B < row_stride_B) {
-            mesa_loge("panfrost: rejecting image due to invalid row stride.\n");
-            return false;
-         }
-
+         /* Explicit stride should be rejected by wsi_row_pitch_to_row_stride()
+          * if it's too small. */
+         assert(wsi_row_stride_B >= row_stride_B);
          row_stride_B = wsi_row_stride_B;
       } else if (linear) {
          /* Keep lines alignment on 64 byte for performance */
@@ -340,17 +385,16 @@ pan_image_layout_init(unsigned arch, const struct pan_image_props *props,
       if (afbc) {
          slice->row_stride_B =
             pan_afbc_row_stride(props->modifier, effective_width_el);
+
+         /* Explicit stride should be rejected by wsi_row_pitch_to_row_stride()
+          * if it's too small. */
+         assert(!wsi_layout || wsi_row_stride_B >= slice->row_stride_B);
          slice->afbc.stride_sb = effective_width_el / block_size_el.width;
          slice->afbc.nr_sblocks = slice->afbc.stride_sb *
                                   (effective_height_el / block_size_el.height);
          slice->afbc.header_size_B =
             ALIGN_POT(slice->afbc.nr_sblocks * AFBC_HEADER_BYTES_PER_TILE,
                       pan_afbc_body_align(arch, props->modifier));
-
-         if (wsi_layout && wsi_row_stride_B < slice->row_stride_B) {
-            mesa_loge("panfrost: rejecting image due to invalid row stride.\n");
-            return false;
-         }
 
          /* AFBC body size */
          slice->afbc.body_size_B = slice_one_size_B;

src/panfrost/lib/tests/test-layout.cpp
@@ -254,9 +254,9 @@ TEST(WSI, FromWSILinear)
       row_stride_from_wsi_pitch(1920 * 4, 1920, PIPE_FORMAT_R8G8B8A8_UINT,
                                 DRM_FORMAT_MOD_LINEAR),
       1920 * 4);
-   EXPECT_EQ(row_stride_from_wsi_pitch(64, 53, PIPE_FORMAT_R8_SNORM,
+   EXPECT_EQ(row_stride_from_wsi_pitch(53, 53, PIPE_FORMAT_R8_SNORM,
                                        DRM_FORMAT_MOD_LINEAR),
-             64);
+             53);
    EXPECT_EQ(row_stride_from_wsi_pitch(64, 32, PIPE_FORMAT_ETC2_RGB8,
                                        DRM_FORMAT_MOD_LINEAR),
              64);

commit 7fa4d00cd98707a1de7c477f7e354d69790a058d

pan/layout: Relax alignment constraints on pre-v7 for imports of linear/u-tiled
Commit 0a4244759173 ("pan/layout: Check the wsi_layout consistency in
wsi_row_pitch_to_row_stride()") introduced stricter alignments on pre-v7
for linear/tiled imports. It turns out alignment constraints on pre-v7 are
weaker than they are on v7+, and this got lost in the refactoring.

Let's fix linear_or_tiled_row_align_req() helper so we can import images
that don't have their base address/row_pitch aligned on a cacheline, and
only cacheline alignment for images we create ourselves.

Also add unittests to make sure we don't regress that in the future.
With this being tested, we can also use
pan_linear_or_tiled_row_align_req() in offset_align_for_mod()
(test-layout.cpp) instead of open-coding it.

Fixes: 0a4244759173 ("pan/layout: Check the wsi_layout consistency in wsi_row_pitch_to_row_stride()")
Fixes: 595e1d52b5a7 ("pan/layout: Test WSI import behavior on all supported format/mods")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Tested-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35555>

src/panfrost/lib/pan_layout.c
@@ -106,36 +106,6 @@ pan_image_renderblock_size_el(uint64_t modifier, enum pipe_format format,
    };
 }
 
-static unsigned
-linear_or_tiled_row_align_req(unsigned arch, enum pipe_format format,
-                              uint64_t modifier)
-{
-   assert(modifier == DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED ||
-          modifier == DRM_FORMAT_MOD_LINEAR);
-
-   /* Prior to v7 we assume a cacheline alignment, though this could be relaxed
-    * on some formats if we have to, like we do on v7+. */
-   if (arch < 7)
-      return 64;
-
-   switch (format) {
-   /* For v7+, NV12/NV21/I420 have a looser alignment requirement of 16 bytes */
-   case PIPE_FORMAT_R8_G8B8_420_UNORM:
-   case PIPE_FORMAT_G8_B8R8_420_UNORM:
-   case PIPE_FORMAT_R8_G8_B8_420_UNORM:
-   case PIPE_FORMAT_R8_B8_G8_420_UNORM:
-   case PIPE_FORMAT_R8_G8B8_422_UNORM:
-   case PIPE_FORMAT_R8_B8G8_422_UNORM:
-      return 16;
-   /* the 10 bit formats have even looser alignment */
-   case PIPE_FORMAT_R10_G10B10_420_UNORM:
-   case PIPE_FORMAT_R10_G10B10_422_UNORM:
-      return 1;
-   default:
-      return 64;
-   }
-}
-
 /*
  * Computes sizes for checksumming, which is 8 bytes per 16x16 tile.
  * Checksumming is believed to be a CRC variant (CRC64 based on the size?).
@@ -318,7 +288,7 @@ wsi_row_pitch_to_row_stride(
                              get_plane_blocksize(format, plane_idx);
 
       row_align_mask =
-         linear_or_tiled_row_align_req(arch, format, modifier) - 1;
+         pan_linear_or_tiled_row_align_req(arch, format, plane_idx) - 1;
       offset_align_mask = row_align_mask;
       *row_stride_B = wsi_row_pitch_B * block_size_el.height;
       width_px = (*row_stride_B / tile_size_B) *
@@ -381,12 +351,21 @@ pan_image_layout_init(
 
    const bool afbc = drm_is_afbc(props->modifier);
    const bool afrc = drm_is_afrc(props->modifier);
-   const int align_req_B =
-      afbc ? pan_afbc_header_row_stride_align(arch, props->format,
-                                              props->modifier)
-           : (afrc ? pan_afrc_buffer_alignment_from_modifier(props->modifier)
-                   : linear_or_tiled_row_align_req(arch, props->format,
-                                                   props->modifier));
+   int align_req_B;
+
+   if (afbc) {
+      align_req_B =
+         pan_afbc_header_row_stride_align(arch, props->format, props->modifier);
+   } else if (afrc) {
+      align_req_B = pan_afrc_buffer_alignment_from_modifier(props->modifier);
+   } else {
+      /* This is the alignment for non-explicit layout, and we want things
+       * aligned on at least a cacheline for performance reasons in that case.
+       */
+      align_req_B =
+         pan_linear_or_tiled_row_align_req(arch, props->format, plane_idx);
+      align_req_B = MAX2(align_req_B, 64);
+   }
 
    /* Mandate alignment */
    unsigned wsi_row_stride_B = 0;

src/panfrost/lib/pan_layout.h
@@ -187,6 +187,68 @@ unsigned pan_image_get_wsi_row_pitch(const struct pan_image_props *props,
                                      const struct pan_image_layout *layout,
                                      unsigned level);
 
+static inline unsigned
+pan_linear_or_tiled_row_align_req(unsigned arch, enum pipe_format format,
+                                  unsigned plane_idx)
+{
+   if (arch < 7) {
+      unsigned nplanes = util_format_get_num_planes(format);
+
+      /* If this is a planar format, align on the plane blocksize. */
+      if (nplanes > 1) {
+         enum pipe_format plane_format =
+            util_format_get_plane_format(format, plane_idx);
+
+         return util_next_power_of_two(util_format_get_blocksize(plane_format));
+      }
+
+      /* Align on blocksize if the format is compressed. */
+      if (util_format_is_compressed(format))
+         return util_next_power_of_two(util_format_get_blocksize(format));
+
+      const struct util_format_description *fdesc =
+         util_format_description(format);
+      unsigned comp_sz_bits = 0;
+      for (unsigned i = 0; i < ARRAY_SIZE(fdesc->channel); i++) {
+         if (!fdesc->channel[0].size)
+            continue;
+
+         /* Align on a pixel if any component is not 8-bit aligned or not a
+          * power of two. */
+         if (fdesc->channel[0].size % 8 != 0 ||
+             !util_is_power_of_two_nonzero(fdesc->channel[0].size))
+            return util_next_power_of_two(util_format_get_blocksize(format));
+
+         /* Align on a pixel if not all components have the same size. */
+         if (comp_sz_bits != 0 && comp_sz_bits != fdesc->channel[0].size)
+            return util_next_power_of_two(util_format_get_blocksize(format));
+
+         comp_sz_bits = fdesc->channel[0].size;
+      }
+
+      /* If all components are the same size, 8-bit aligned and a power of two,
+       * align on a component. */
+      return comp_sz_bits / 8;
+   }
+
+   switch (format) {
+   /* For v7+, NV12/NV21/I420 have a looser alignment requirement of 16 bytes */
+   case PIPE_FORMAT_R8_G8B8_420_UNORM:
+   case PIPE_FORMAT_G8_B8R8_420_UNORM:
+   case PIPE_FORMAT_R8_G8_B8_420_UNORM:
+   case PIPE_FORMAT_R8_B8_G8_420_UNORM:
+   case PIPE_FORMAT_R8_G8B8_422_UNORM:
+   case PIPE_FORMAT_R8_B8G8_422_UNORM:
+      return 16;
+   /* the 10 bit formats have even looser alignment */
+   case PIPE_FORMAT_R10_G10B10_420_UNORM:
+   case PIPE_FORMAT_R10_G10B10_422_UNORM:
+      return 1;
+   default:
+      return 64;
+   }
+}
+
 #ifdef __cplusplus
 } /* extern C */
 #endif

src/panfrost/lib/tests/test-layout.cpp
@@ -29,6 +29,37 @@
 
 #include <gtest/gtest.h>
 
+TEST(Align, UTiledLinear)
+{
+   struct {
+      unsigned arch;
+      enum pipe_format format;
+      unsigned plane_idx;
+      unsigned alignment;
+   } cases[] = {
+      { 6, PIPE_FORMAT_ETC2_RGB8, 0, 8 },
+      { 6, PIPE_FORMAT_R32G32B32_FLOAT, 0, 4 },
+      { 6, PIPE_FORMAT_R8G8B8A8_UNORM, 0, 1 },
+      { 6, PIPE_FORMAT_R5G6B5_UNORM, 0, 2 },
+      { 6, PIPE_FORMAT_R8_G8B8_420_UNORM, 0, 1 },
+      { 6, PIPE_FORMAT_R8_G8B8_420_UNORM, 1, 2 },
+      { 7, PIPE_FORMAT_ETC2_RGB8, 0, 64 },
+      { 7, PIPE_FORMAT_R32G32B32_FLOAT, 0, 64 },
+      { 7, PIPE_FORMAT_R8G8B8A8_UNORM, 0, 64 },
+      { 7, PIPE_FORMAT_R5G6B5_UNORM, 0, 64 },
+      { 7, PIPE_FORMAT_R8_G8B8_420_UNORM, 0, 16 },
+      { 7, PIPE_FORMAT_R8_G8B8_420_UNORM, 1, 16 },
+      { 7, PIPE_FORMAT_R10_G10B10_420_UNORM, 0, 1 },
+      { 7, PIPE_FORMAT_R10_G10B10_420_UNORM, 1, 1 },
+   };
+   for (unsigned i = 0; i < ARRAY_SIZE(cases); ++i) {
+      unsigned align = pan_linear_or_tiled_row_align_req(
+         cases[i].arch, cases[i].format, cases[i].plane_idx);
+
+      EXPECT_EQ(align, cases[i].alignment);
+   }
+}
+
 TEST(BlockSize, Linear)
 {
    enum pipe_format format[] = {PIPE_FORMAT_R32G32B32_FLOAT,
@@ -588,26 +619,7 @@ offset_align_for_mod(unsigned arch, const struct pan_image_props *iprops,
       assert(modifier == DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED ||
              modifier == DRM_FORMAT_MOD_LINEAR);
 
-      if (arch < 7)
-         return 64;
-
-      switch (format) {
-      /* For v7+, NV12/NV21/I420 have a looser alignment requirement of 16 bytes
-       */
-      case PIPE_FORMAT_R8_G8B8_420_UNORM:
-      case PIPE_FORMAT_G8_B8R8_420_UNORM:
-      case PIPE_FORMAT_R8_G8_B8_420_UNORM:
-      case PIPE_FORMAT_R8_B8_G8_420_UNORM:
-      case PIPE_FORMAT_R8_G8B8_422_UNORM:
-      case PIPE_FORMAT_R8_B8G8_422_UNORM:
-         return 16;
-      /* the 10 bit formats have even looser alignment */
-      case PIPE_FORMAT_R10_G10B10_420_UNORM:
-      case PIPE_FORMAT_R10_G10B10_422_UNORM:
-         return 1;
-      default:
-         return 64;
-      }
+      return pan_linear_or_tiled_row_align_req(arch, format, plane_idx);
    }
 }
 