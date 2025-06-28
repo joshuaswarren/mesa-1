joshuaswarren/mesa-1

commit 0c2d6425390a0dd01fc2eac2c4d2a0f88f807961

intel: introduce new VUE layout for separate compiled shader with mesh
Mesh shaders have per vertex block in URB pretty much identical to the
VUE format. Let's just reuse that concept to do all of our layout in
the payload attribute registers. This will ensure that we have
consistent VUE layout between Mesh & non-Mesh pipelines.

We need a new way of laying out the VUE though as we have to
accomodate a HW constraint of maximum (per-primitive + per-vertex) of
32 varying. This means we cannot have 2 locations in the payload for
things like PrimitiveID which can come from either the per-primitive
or the per-vertex block. The new layout places the PrimitiveID at the
end of the per-vertex attributes and shrinks the delivery dynamically
if the mesh stage is active. The shader is compiled with a
MOV_INDIRECT to read the PrimitiveID from the right location in the
attributes.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34109>

src/intel/compiler/brw_nir.c
@@ -658,11 +658,75 @@ lower_barycentric_at_offset(nir_builder *b, nir_intrinsic_instr *intrin,
    return true;
 }
 
+static bool
+lower_indirect_primitive_id(nir_builder *b,
+                            nir_intrinsic_instr *intrin,
+                            void *data)
+{
+   if (intrin->intrinsic != nir_intrinsic_load_per_primitive_input)
+      return false;
+
+   if (nir_intrinsic_io_semantics(intrin).location != VARYING_SLOT_PRIMITIVE_ID)
+      return false;
+
+   nir_def *indirect_primitive_id = data;
+   nir_def_replace(&intrin->def, indirect_primitive_id);
+
+   return true;
+}
+
 void
 brw_nir_lower_fs_inputs(nir_shader *nir,
                         const struct intel_device_info *devinfo,
                         const struct brw_wm_prog_key *key)
 {
+   nir_def *indirect_primitive_id = NULL;
+   if (key->base.vue_layout == INTEL_VUE_LAYOUT_SEPARATE_MESH &&
+       (nir->info.inputs_read & VARYING_BIT_PRIMITIVE_ID)) {
+      nir_builder _b = nir_builder_at(nir_before_impl(nir_shader_get_entrypoint(nir))), *b = &_b;
+      nir_def *index = nir_ushr_imm(b,
+                                    nir_load_fs_msaa_intel(b),
+                                    INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET);
+      nir_def *max_poly = nir_load_max_polygon_intel(b);
+      /* Build the per-vertex offset into the attribute section of the thread
+       * payload. There is always one GRF of padding in front.
+       *
+       * The computation is fairly complicated due to the layout of the
+       * payload. You can find a description of the layout in
+       * brw_compile_fs.cpp brw_assign_urb_setup().
+       *
+       * Gfx < 20 packs 2 slots per GRF (hence the %/ 2 in the formula)
+       * Gfx >= 20 pack 5 slots per GRF (hence the %/ 5 in the formula)
+       *
+       * Then an additional offset needs to added to handle how multiple
+       * polygon data is interleaved.
+       */
+      nir_def *per_vertex_offset = nir_iadd_imm(
+         b,
+         devinfo->ver >= 20 ?
+         nir_iadd(b,
+                  nir_imul(b, nir_udiv_imm(b, index, 5), nir_imul_imm(b, max_poly, 64)),
+                  nir_imul_imm(b, nir_umod_imm(b, index, 5), 12)) :
+         nir_iadd_imm(
+            b,
+            nir_iadd(
+               b,
+               nir_imul(b, nir_udiv_imm(b, index, 2), nir_imul_imm(b, max_poly, 32)),
+               nir_imul_imm(b, nir_umod_imm(b, index, 2), 16)),
+            12),
+         devinfo->grf_size);
+      /* When the attribute index is INTEL_MSAA_FLAG_PRIMITIVE_ID_MESH_INDEX,
+       * it means the value is coming from the per-primitive block. We always
+       * lay out PrimitiveID at offset 0 in the per-primitive block.
+       */
+      nir_def *attribute_offset = nir_bcsel(
+         b,
+         nir_ieq_imm(b, index, INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_MESH),
+         nir_imm_int(b, 0), per_vertex_offset);
+      indirect_primitive_id =
+         nir_read_attribute_payload_intel(b, attribute_offset);
+   }
+
    nir_foreach_shader_in_variable(var, nir) {
       var->data.driver_location = var->data.location;
 
@@ -678,6 +742,14 @@ brw_nir_lower_fs_inputs(nir_shader *nir,
 
          var->data.interpolation = flat ? INTERP_MODE_FLAT
                                         : INTERP_MODE_SMOOTH;
+      }
+
+      /* Always pull the PrimitiveID from the per-primitive block if mesh can be involved.
+       */
+      if (var->data.location == VARYING_SLOT_PRIMITIVE_ID &&
+         key->mesh_input != INTEL_NEVER) {
+         var->data.per_primitive = true;
+         nir->info.per_primitive_inputs |= VARYING_BIT_PRIMITIVE_ID;
       }
    }
 
@@ -702,6 +774,13 @@ brw_nir_lower_fs_inputs(nir_shader *nir,
                lower_barycentric_at_offset,
                nir_metadata_control_flow,
                NULL);
+   }
+
+   if (indirect_primitive_id != NULL) {
+      NIR_PASS(_, nir, nir_shader_intrinsics_pass,
+               lower_indirect_primitive_id,
+               nir_metadata_control_flow,
+               indirect_primitive_id);
    }
 
    /* This pass needs actual constants */

src/intel/compiler/intel_shader_enums.h
@@ -30,6 +30,9 @@ intel_sometimes_invert(enum intel_sometimes x)
    return (enum intel_sometimes)((int)INTEL_ALWAYS - (int)x);
 }
 
+#define INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET (20)
+#define INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_MESH   (32)
+
 enum intel_msaa_flags {
    /** Must be set whenever any dynamic MSAA is used
     *
@@ -63,6 +66,15 @@ enum intel_msaa_flags {
     * in the render target messages.
     */
    INTEL_MSAA_FLAG_COARSE_RT_WRITES = (1 << 18),
+
+   /** Index of the PrimitiveID attribute relative to the first read
+    * attribute.
+    *
+    * This is not a flag but a value that cover bits 20:31. Value 32 means the
+    * PrimitiveID is coming from the PerPrimitive block, written by the Mesh
+    * shader.
+    */
+   INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX = (1 << INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET),
 };
 MESA_DEFINE_CPP_ENUM_BITFIELD_OPERATORS(intel_msaa_flags)
 
@@ -133,6 +145,10 @@ enum intel_vue_layout {
     * Mesh support.
     */
    INTEL_VUE_LAYOUT_SEPARATE,
+   /**
+    * Layout is separate and works with Mesh shaders.
+    */
+   INTEL_VUE_LAYOUT_SEPARATE_MESH,
 };
 
 /**
@@ -157,11 +173,70 @@ struct intel_vue_map {
    /**
     * The layout of the VUE
     *
-    * Separable programs (GL_ARB_separate_shader_objects) can be mixed and matched
-    * without the linker having a chance to dead code eliminate unused varyings.
+    * Separable programs (GL_ARB_separate_shader_objects) can be mixed and
+    * matched without the linker having a chance to dead code eliminate unused
+    * varyings.
     *
     * This means that we have to use a fixed slot layout, based on the output's
     * location field, rather than assigning slots in a compact contiguous block.
+    *
+    * When using Mesh, another constraint arises which is the HW limits for
+    * loading per-primitive & per-vertex data, limited to 32 varying in total.
+    * This requires us to be quite inventive with the way we lay things out.
+    * Take a fragment shader loading the following data :
+    *
+    *    float gl_ClipDistance[];
+    *    uint gl_PrimitiveID;
+    *    vec4 someAppValue[29];
+    *
+    * According to the Vulkan spec, someAppValue will occupy 29 slots,
+    * gl_PrimitiveID 1 slot, gl_ClipDistance[] up to 2 slots. If the input is
+    * coming from a VS/DS/GS shader, we can load all of this through a single
+    * block using 3DSTATE_SBE::VertexURBEntryReadLength = 16 (maximum
+    * programmable value) and the layout with
+    * BRW_VUE_MAP_LAYOUT_FIXED/BRW_VUE_MAP_LAYOUT_SEPARATE will be this :
+    *
+    *   -----------------------
+    *   | gl_ClipDistance 0-3 |
+    *   |---------------------|
+    *   | gl_ClipDistance 4-7 |
+    *   |---------------------|
+    *   |   gl_PrimitiveID    |
+    *   |---------------------|
+    *   |   someAppValue[]    |
+    *   |---------------------|
+    *
+    * This works nicely as everything is coming from the same location in the
+    * URB.
+    *
+    * When mesh shaders are involved, gl_PrimitiveID is located in a different
+    * place in the URB (the per-primitive block) and requires programming
+    * 3DSTATE_SBE_MESH::PerPrimitiveURBEntryOutputReadLength to load some
+    * additional data. The HW has a limit such that
+    * 3DSTATE_SBE_MESH::PerPrimitiveURBEntryOutputReadLength +
+    * 3DSTATE_SBE_MESH::PerVertexURBEntryOutputReadLength <= 16. With the
+    * layout above, we would not be able to accomodate that HW limit.
+    *
+    * The solution to this is to lay the built-in varyings out
+    * (gl_ClipDistance omitted since it's part of the VUE header and cannot
+    * live any other place) at the end of the VUE like this :
+    *
+    *   -----------------------
+    *   | gl_ClipDistance 0-3 |
+    *   |---------------------|
+    *   | gl_ClipDistance 4-7 |
+    *   |---------------------|
+    *   |   someAppValue[]    |
+    *   |---------------------|
+    *   |   gl_PrimitiveID    |
+    *   |---------------------|
+    *
+    * This layout adds another challenge because with separate shader
+    * compilations, we cannot tell in the consumer shader how many outputs the
+    * producer has, so we don't know where the gl_PrimitiveID lives. The
+    * solution to this other problem is to read the built-in with a
+    * MOV_INDIRECT and have the offset of the MOV_INDIRECT loaded through a
+    * push constant.
     */
    enum intel_vue_layout layout;
 
@@ -361,6 +436,7 @@ struct intel_fs_params {
    uint32_t rasterization_samples;
    bool coarse_pixel;
    bool alpha_to_coverage;
+   uint32_t primitive_id_index;
 };
 
 static inline enum intel_msaa_flags
@@ -391,6 +467,9 @@ intel_fs_msaa_flags(struct intel_fs_params params)
 
    if (params.alpha_to_coverage)
       fs_msaa_flags |= INTEL_MSAA_FLAG_ALPHA_TO_COVERAGE;
+
+   fs_msaa_flags |= (enum intel_msaa_flags)(
+      params.primitive_id_index << INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET);
 
    return fs_msaa_flags;
 }

src/gallium/drivers/iris/iris_state.c
@@ -4678,30 +4678,39 @@ iris_create_so_decl_list(const struct pipe_stream_output_info *info,
 }
 
 static inline int
-iris_compute_first_urb_slot_required(uint64_t inputs_read,
+iris_compute_first_urb_slot_required(struct iris_compiled_shader *fs_shader,
                                      const struct intel_vue_map *prev_stage_vue_map)
 {
 #if GFX_VER >= 9
-   return brw_compute_first_fs_urb_slot_required(inputs_read, prev_stage_vue_map);
+   uint32_t read_offset, read_length, num_varyings, primid_offset;
+   brw_compute_sbe_per_vertex_urb_read(prev_stage_vue_map,
+                                       false /* mesh*/,
+                                       brw_wm_prog_data(fs_shader->brw_prog_data),
+                                       &read_offset, &read_length, &num_varyings,
+                                       &primid_offset);
+   return 2 * read_offset;
 #else
-   return elk_compute_first_urb_slot_required(inputs_read, prev_stage_vue_map);
+   const struct iris_fs_data *fs_data = iris_fs_data(fs_shader);
+   return elk_compute_first_urb_slot_required(fs_data->inputs, prev_stage_vue_map);
 #endif
 }
 
 static void
-iris_compute_sbe_urb_read_interval(uint64_t fs_input_slots,
+iris_compute_sbe_urb_read_interval(struct iris_compiled_shader *fs_shader,
                                    const struct intel_vue_map *last_vue_map,
                                    bool two_sided_color,
                                    unsigned *out_offset,
                                    unsigned *out_length)
 {
+   const struct iris_fs_data *fs_data = iris_fs_data(fs_shader);
+
    /* The compiler computes the first URB slot without considering COL/BFC
     * swizzling (because it doesn't know whether it's enabled), so we need
     * to do that here too.  This may result in a smaller offset, which
     * should be safe.
     */
    const unsigned first_slot =
-      iris_compute_first_urb_slot_required(fs_input_slots, last_vue_map);
+      iris_compute_first_urb_slot_required(fs_shader, last_vue_map);
 
    /* This becomes the URB read offset (counted in pairs of slots). */
    assert(first_slot % 2 == 0);
@@ -4710,6 +4719,7 @@ iris_compute_sbe_urb_read_interval(uint64_t fs_input_slots,
    /* We need to adjust the inputs read to account for front/back color
     * swizzling, as it can make the URB length longer.
     */
+   uint64_t fs_input_slots = fs_data->inputs;
    for (int c = 0; c <= 1; c++) {
       if (fs_input_slots & (VARYING_BIT_COL0 << c)) {
          /* If two sided color is enabled, the fragment shader's gl_Color
@@ -4898,7 +4908,7 @@ iris_emit_sbe(struct iris_batch *batch, const struct iris_context *ice)
       &iris_vue_data(ice->shaders.last_vue_shader)->vue_map;
 
    unsigned urb_read_offset, urb_read_length;
-   iris_compute_sbe_urb_read_interval(fs_data->inputs,
+   iris_compute_sbe_urb_read_interval(ice->shaders.prog[MESA_SHADER_FRAGMENT],
                                       last_vue_map,
                                       cso_rast->light_twoside,
                                       &urb_read_offset, &urb_read_length);

src/intel/compiler/brw_compiler.c
@@ -194,11 +194,6 @@ brw_compiler_create(void *mem_ctx, const struct intel_device_info *devinfo)
       compiler->nir_options[i] = nir_options;
    }
 
-   compiler->mesh.mue_header_packing =
-         (unsigned)debug_get_num_option("INTEL_MESH_HEADER_PACKING", 3);
-   compiler->mesh.mue_compaction =
-         debug_get_bool_option("INTEL_MESH_COMPACTION", true);
-
    return compiler;
 }
 
@@ -217,8 +212,6 @@ brw_get_compiler_config_value(const struct brw_compiler *compiler)
    insert_u64_bit(&config, compiler->precise_trig);
    bits++;
    insert_u64_bit(&config, compiler->lower_dpas);
-   bits++;
-   insert_u64_bit(&config, compiler->mesh.mue_compaction);
    bits++;
 
    enum intel_debug_flag debug_bits[] = {
@@ -243,9 +236,6 @@ brw_get_compiler_config_value(const struct brw_compiler *compiler)
 
    mask = 3;
    bits += util_bitcount64(mask);
-
-   u_foreach_bit64(bit, mask)
-      insert_u64_bit(&config, (compiler->mesh.mue_header_packing & (1ULL << bit)) != 0);
 
    assert(bits <= util_bitcount64(UINT64_MAX));
 

src/intel/compiler/brw_compiler.h
@@ -122,11 +122,6 @@ struct brw_compiler {
    int spilling_rate;
 
    struct nir_shader *clc_shader;
-
-   struct {
-      unsigned mue_header_packing;
-      bool mue_compaction;
-   } mesh;
 };
 
 #define brw_shader_debug_log(compiler, data, fmt, ... ) do {    \
@@ -371,7 +366,7 @@ struct brw_wm_prog_key {
    /* Whether or not we are running on a multisampled framebuffer */
    enum intel_sometimes multisample_fbo:2;
 
-   /* Whether the preceding shader stage is mesh */
+   /* Whether the shader is dispatch with a preceeding mesh shader */
    enum intel_sometimes mesh_input:2;
 
    bool coherent_fb_fetch:1;
@@ -388,7 +383,7 @@ brw_wm_prog_key_is_dynamic(const struct brw_wm_prog_key *key)
    return key->alpha_to_coverage == INTEL_SOMETIMES ||
       key->persample_interp == INTEL_SOMETIMES ||
       key->multisample_fbo == INTEL_SOMETIMES ||
-      key->mesh_input == INTEL_SOMETIMES;
+      key->base.vue_layout == INTEL_VUE_LAYOUT_SEPARATE_MESH;
 }
 
 struct brw_cs_prog_key {
@@ -785,6 +780,13 @@ struct brw_wm_prog_data {
    uint64_t inputs;
 
    /**
+    * The FS per-primitive inputs (some bits can be in both inputs &
+    * per_primitive_inputs if the shader is compiled without being linked to
+    * the previous stage)
+    */
+   uint64_t per_primitive_inputs;
+
+   /**
     * Map from gl_varying_slot to the position within the FS setup data
     * payload where the varying's attribute vertex deltas should be delivered.
     * For varying slots that are not used by the FS, the value is -1.
@@ -1049,8 +1051,6 @@ typedef enum
 void brw_print_vue_map(FILE *fp, const struct intel_vue_map *vue_map,
                        gl_shader_stage stage);
 
-void brw_print_fs_urb_setup(FILE *fp, const struct brw_wm_prog_data *prog_data);
-
 /**
  * Convert a VUE slot number into a byte offset within the VUE.
  */
@@ -1068,6 +1068,16 @@ brw_varying_to_offset(const struct intel_vue_map *vue_map, unsigned varying)
 {
    return brw_vue_slot_to_offset(vue_map->varying_to_slot[varying]);
 }
+
+void
+brw_compute_per_primitive_map(int *out_per_primitive_map,
+                              uint32_t *out_per_primitive_stride,
+                              uint32_t *out_first_offset,
+                              uint32_t base_offset,
+                              nir_shader *nir,
+                              uint32_t variables_mode,
+                              uint64_t slots_valid,
+                              bool separate_shader);
 
 void brw_compute_vue_map(const struct intel_device_info *devinfo,
                          struct intel_vue_map *vue_map,
@@ -1190,25 +1200,40 @@ struct brw_tue_map {
 };
 
 struct brw_mue_map {
-   int32_t start_dw[VARYING_SLOT_MAX];
-   uint32_t len_dw[VARYING_SLOT_MAX];
-   uint32_t per_primitive_indices_dw;
-
-   uint32_t size_dw;
+   /* Total size in bytes of the MUE (32B aligned) */
+   uint32_t size;
 
    uint32_t max_primitives;
-   uint32_t per_primitive_start_dw;
-   uint32_t per_primitive_header_size_dw;
-   uint32_t per_primitive_data_size_dw;
-   uint32_t per_primitive_pitch_dw;
-   bool user_data_in_primitive_header;
-
    uint32_t max_vertices;
-   uint32_t per_vertex_start_dw;
-   uint32_t per_vertex_header_size_dw;
-   uint32_t per_vertex_data_size_dw;
-   uint32_t per_vertex_pitch_dw;
-   bool user_data_in_vertex_header;
+
+   /* Stride in bytes between sets of primitive indices */
+   uint32_t per_primitive_indices_stride;
+
+   /* Per primitive offset from the start of the MUE (32B aligned) */
+   uint32_t per_primitive_offset;
+
+   /* Per primitive stride in bytes (32B aligned) */
+   uint32_t per_primitive_stride;
+
+   /* Whether the per primitive block includes a header */
+   bool has_per_primitive_header;
+
+   /* Per vertex offset in bytes from the start of the MUE (32B aligned) */
+   uint32_t per_vertex_offset;
+
+   /* Size of the per vertex header (32B aligned) */
+   uint32_t per_vertex_header_size;
+
+   /* Per vertex stride in bytes (32B aligned) */
+   uint32_t per_vertex_stride;
+
+   /* VUE map for the per vertex attributes */
+   struct intel_vue_map vue_map;
+
+   /* Offset in bytes of each per primitive relative to
+    * per_primitive_offset (-1 if unused)
+    */
+   int per_primitive_offsets[VARYING_SLOT_MAX];
 };
 
 struct brw_task_prog_data {
@@ -1628,7 +1653,28 @@ brw_stage_has_packed_dispatch(ASSERTED const struct intel_device_info *devinfo,
  */
 int
 brw_compute_first_fs_urb_slot_required(uint64_t inputs_read,
-                                       const struct intel_vue_map *prev_stage_vue_map);
+                                       const struct intel_vue_map *prev_stage_vue_map,
+                                       bool mesh);
+
+void
+brw_compute_sbe_per_vertex_urb_read(const struct intel_vue_map *prev_stage_vue_map,
+                                    bool mesh,
+                                    const struct brw_wm_prog_data *wm_prog_data,
+                                    uint32_t *out_first_slot,
+                                    uint32_t *num_slots,
+                                    uint32_t *out_num_varyings,
+                                    uint32_t *out_primitive_id_offset);
+
+/**
+ * Computes the URB offset at which SBE should read the per primitive date
+ * written by the mesh shader.
+ */
+void
+brw_compute_sbe_per_primitive_urb_read(uint64_t inputs_read,
+                                       uint32_t num_varyings,
+                                       const struct brw_mue_map *mue_map,
+                                       uint32_t *out_read_offset,
+                                       uint32_t *out_read_length);
 
 /* From InlineData in 3DSTATE_TASK_SHADER_DATA and 3DSTATE_MESH_SHADER_DATA. */
 #define BRW_TASK_MESH_INLINE_DATA_SIZE_DW 8

src/intel/compiler/brw_from_nir.cpp
@@ -4153,17 +4153,14 @@ brw_interp_reg(const brw_builder &bld, unsigned location,
 {
    brw_shader &s = *bld.shader;
    assert(s.stage == MESA_SHADER_FRAGMENT);
-   assert(BITFIELD64_BIT(location) & ~s.nir->info.per_primitive_inputs);
+   assert((BITFIELD64_BIT(location) & ~s.nir->info.per_primitive_inputs) ||
+          location == VARYING_SLOT_PRIMITIVE_ID);
 
    const struct brw_wm_prog_data *prog_data = brw_wm_prog_data(s.prog_data);
 
    assert(prog_data->urb_setup[location] >= 0);
    unsigned nr = prog_data->urb_setup[location];
    channel += prog_data->urb_setup_channel[location];
-
-   /* Adjust so we start counting from the first per_vertex input. */
-   assert(nr >= prog_data->num_per_primitive_inputs);
-   nr -= prog_data->num_per_primitive_inputs;
 
    const unsigned per_vertex_start = prog_data->num_per_primitive_inputs;
    const unsigned regnr = per_vertex_start + (nr * 4) + channel;
@@ -4196,13 +4193,14 @@ brw_per_primitive_reg(const brw_builder &bld, int location, unsigned comp)
 
    const struct brw_wm_prog_data *prog_data = brw_wm_prog_data(s.prog_data);
 
-   comp += prog_data->urb_setup_channel[location];
+   comp += (s.fs.per_primitive_offsets[location] % 16) / 4;
 
-   assert(prog_data->urb_setup[location] >= 0);
+   const unsigned regnr = s.fs.per_primitive_offsets[location] / 16 + comp / 4;
 
-   const unsigned regnr = prog_data->urb_setup[location] + comp / 4;
-
+   assert(s.fs.per_primitive_offsets[location] >= 0);
    assert(regnr < prog_data->num_per_primitive_inputs);
+
+   brw_reg loc_reg = brw_attr_reg(regnr, BRW_TYPE_UD);
 
    if (s.max_polygons > 1) {
       /* In multipolygon dispatch each primitive constant is a
@@ -4211,11 +4209,10 @@ brw_per_primitive_reg(const brw_builder &bld, int location, unsigned comp)
        * component() to select the specified parameter.
        */
       const brw_reg tmp = bld.vgrf(BRW_TYPE_UD);
-      bld.MOV(tmp, offset(brw_attr_reg(regnr, BRW_TYPE_UD),
-                          s.dispatch_width, comp % 4));
+      bld.MOV(tmp, offset(loc_reg, s.dispatch_width, comp % 4));
       return retype(tmp, BRW_TYPE_F);
    } else {
-      return component(brw_attr_reg(regnr, BRW_TYPE_F), comp % 4);
+      return component(loc_reg, comp % 4);
    }
 }
 
@@ -4419,7 +4416,7 @@ brw_from_nir_emit_fs_intrinsic(nir_to_brw_state &ntb,
          break;
       }
 
-      if (BITFIELD64_BIT(base) & s.nir->info.per_primitive_inputs) {
+      if (instr->intrinsic == nir_intrinsic_load_per_primitive_input) {
          assert(base != VARYING_SLOT_PRIMITIVE_INDICES);
          for (unsigned int i = 0; i < num_components; i++) {
             bld.MOV(offset(dest, bld, i),

commit d0133499714baaa07e3a61a0b4fde3f73772e3ba

brw: fix vertex attribute offset computation
The formula uses scalar indices (4bytes), not slots (16bytes).

We also incorrectly passed a scalar (vertex case) & slot (mesh case)
offset in the push constants. Use slots instead so that the value is
smaller and we can pack more stuff into fs_msaa_flags.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 0c2d642539 ("intel: introduce new VUE layout for separate compiled shader with mesh")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35103>

src/intel/compiler/brw_compile_fs.cpp
@@ -1944,10 +1944,16 @@ brw_compute_sbe_per_vertex_urb_read(const struct intel_vue_map *prev_stage_vue_m
              * use that.
              */
             if (wm_prog_data->urb_setup[VARYING_SLOT_PRIMITIVE_ID] >= 0) {
-               if (first_slot == INT32_MAX)
-                  first_slot = 0;
-               primitive_id_slot =
-                  first_slot + wm_prog_data->urb_setup[VARYING_SLOT_PRIMITIVE_ID];
+               if (first_slot == INT32_MAX) {
+                  first_slot =
+                     wm_prog_data->urb_setup[VARYING_SLOT_PRIMITIVE_ID];
+               }
+               /* urb_setup[VARYING_SLOT_PRIMITIVE_ID] is relative to the
+                * first read slot, so bring primitive_id_slot back into the
+                * absolute indexing of the VUE.
+                */
+               primitive_id_slot = first_slot +
+                  wm_prog_data->urb_setup[VARYING_SLOT_PRIMITIVE_ID];
             } else {
                primitive_id_slot = ++last_slot;
             }
@@ -1955,9 +1961,10 @@ brw_compute_sbe_per_vertex_urb_read(const struct intel_vue_map *prev_stage_vue_m
             primitive_id_slot =
                prev_stage_vue_map->varying_to_slot[VARYING_SLOT_PRIMITIVE_ID];
          }
+         first_slot = MIN2(primitive_id_slot, first_slot);
          last_slot = MAX2(primitive_id_slot, last_slot);
 
-         *out_primitive_id_offset = 4 * (primitive_id_slot - first_slot);
+         *out_primitive_id_offset = primitive_id_slot - first_slot;
       }
    }
 

src/intel/compiler/brw_nir.c
@@ -706,9 +706,11 @@ brw_nir_lower_fs_inputs(nir_shader *nir,
    if (key->base.vue_layout == INTEL_VUE_LAYOUT_SEPARATE_MESH &&
        (nir->info.inputs_read & VARYING_BIT_PRIMITIVE_ID)) {
       nir_builder _b = nir_builder_at(nir_before_impl(nir_shader_get_entrypoint(nir))), *b = &_b;
-      nir_def *index = nir_ushr_imm(b,
-                                    nir_load_fs_msaa_intel(b),
-                                    INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET);
+      nir_def *index = nir_ubitfield_extract_imm(
+         b,
+         nir_load_fs_msaa_intel(b),
+         INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET,
+         INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_SIZE);
       nir_def *max_poly = nir_load_max_polygon_intel(b);
       /* Build the per-vertex offset into the attribute section of the thread
        * payload. There is always one GRF of padding in front.
@@ -723,18 +725,21 @@ brw_nir_lower_fs_inputs(nir_shader *nir,
        * Then an additional offset needs to added to handle how multiple
        * polygon data is interleaved.
        */
+      nir_def *scalar_index = nir_imul_imm(b, index, 4);
       nir_def *per_vertex_offset = nir_iadd_imm(
          b,
          devinfo->ver >= 20 ?
          nir_iadd(b,
-                  nir_imul(b, nir_udiv_imm(b, index, 5), nir_imul_imm(b, max_poly, 64)),
-                  nir_imul_imm(b, nir_umod_imm(b, index, 5), 12)) :
+                  nir_imul(b, nir_udiv_imm(b, scalar_index, 5),
+                              nir_imul_imm(b, max_poly, 64)),
+                  nir_imul_imm(b, nir_umod_imm(b, scalar_index, 5), 12)) :
          nir_iadd_imm(
             b,
             nir_iadd(
                b,
-               nir_imul(b, nir_udiv_imm(b, index, 2), nir_imul_imm(b, max_poly, 32)),
-               nir_imul_imm(b, nir_umod_imm(b, index, 2), 16)),
+               nir_imul(b, nir_udiv_imm(b, scalar_index, 2),
+                           nir_imul_imm(b, max_poly, 32)),
+               nir_imul_imm(b, nir_umod_imm(b, scalar_index, 2), 16)),
             12),
          devinfo->grf_size);
       /* When the attribute index is INTEL_MSAA_FLAG_PRIMITIVE_ID_MESH_INDEX,

src/intel/compiler/intel_shader_enums.h
@@ -31,6 +31,7 @@ intel_sometimes_invert(enum intel_sometimes x)
 }
 
 #define INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET (20)
+#define INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_SIZE   (6)
 #define INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_MESH   (32)
 
 enum intel_msaa_flags {
@@ -472,6 +473,7 @@ intel_fs_msaa_flags(struct intel_fs_params params)
    if (params.alpha_to_coverage)
       fs_msaa_flags |= INTEL_MSAA_FLAG_ALPHA_TO_COVERAGE;
 
+   assert(params.primitive_id_index < (1u << INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_SIZE));
    fs_msaa_flags |= (enum intel_msaa_flags)(
       params.primitive_id_index << INTEL_MSAA_FLAG_PRIMITIVE_ID_INDEX_OFFSET);
 