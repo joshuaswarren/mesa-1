openharmony/third_party_mesa3d

commit f28d998fb2f1e6569222300f0b47a4605c062c2b

v3d: add support for no buffer object bound
From the GL_OES_texture_buffer spec:

      "If no buffer object is bound to the buffer texture, the results
       of the texel access are undefined."

this can be interpreted as allowing any result to come back, but not
terminate the program.

The current solution is not entirely complete, as it could still try to
get a wrong address for the shader state address.

This can be checked with piglit test
arb_texture_buffer_object-render-no-bo; the test is skip because it
requires OpenGL 3.1, but if overriding the version then it will crash.

Signed-off-by: Alejandro Piñeiro <apinheiro@igalia.com>
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13409>

src/gallium/drivers/v3d/v3d_uniforms.c
@@ -172,6 +172,20 @@ write_tmu_p0(struct v3d_job *job,
         int unit = v3d_unit_data_get_unit(data);
         struct pipe_sampler_view *psview = texstate->textures[unit];
         struct v3d_sampler_view *sview = v3d_sampler_view(psview);
+        /* GL_OES_texture_buffer spec:
+         *     "If no buffer object is bound to the buffer texture, the
+         *      results of the texel access are undefined."
+         *
+         * This can be interpreted as allowing any result to come back, but
+         * not terminate the program (and some tests interpret that).
+         *
+         * FIXME: just return is not a full valid solution, as it could still
+         * try to get a wrong address for the shader state address. Perhaps we
+         * would need to set up a BO with a "default texture state"
+         */
+        if (sview == NULL)
+                return;
+
         struct v3d_resource *rsc = v3d_resource(sview->texture);
 
         cl_aligned_reloc(&job->indirect, uniforms, sview->bo,

joshuaswarren/mesa-1

commit 5015f1c86e01ac4f5a38d383411419d28bccab79

v3d: fix support for no buffer object bound
Piglit arb_texture_buffer_object-render-no-bo was generating
gpu resets because the uniform stream was missing the last
Fragment Shader uniform. So it was reading instead of the last
fragment shader uniform the first uniform of the vertex shader.

And using that unrelated VS uniform as the sampler address where
the texture should be read.

So now if a buffer object is not bound for a texture buffer object
we write the texture state base address to 0 (NULL) so the default
texture state is used.

So only is needed to set the 4 lower bits of the tmu_p0 with
the bit-mask of word enables.

Fixes: f28d998fb2f ("v3d: add support for no buffer object bound")
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35847>

src/gallium/drivers/v3d/v3d_uniforms.c
@@ -141,12 +141,13 @@ write_tmu_p0(struct v3d_job *job,
          * This can be interpreted as allowing any result to come back, but
          * not terminate the program (and some tests interpret that).
          *
-         * FIXME: just return is not a full valid solution, as it could still
-         * try to get a wrong address for the shader state address. Perhaps we
-         * would need to set up a BO with a "default texture state"
+         * We write the texture state base address to 0 (NULL) so the default
+         * texture state is used.
          */
-        if (sview == NULL)
+        if (sview == NULL) {
+                cl_aligned_u32(uniforms, v3d_unit_data_get_offset(data) & 0xf);
                 return;
+        }
 
         struct v3d_resource *rsc = v3d_resource(sview->texture);
 