joshuaswarren/mesa-1

commit 7b845cabd3919d488b497112545da01da22984c2

gallium: de-pointerize pipe_surface
this allows eliminating surface refcounting and objects
which, relatively speaking, don't serve much purpose

see MR for details

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34054>

src/mesa/state_tracker/st_cb_clear.c
@@ -406,10 +406,10 @@ st_Clear(struct gl_context *ctx, GLbitfield mask)
          if (b != BUFFER_NONE && mask & (1 << b)) {
             struct gl_renderbuffer *rb
                = ctx->DrawBuffer->Attachment[b].Renderbuffer;
-            struct pipe_surface *surface = _mesa_renderbuffer_get_surface(ctx, rb);
+            enum pipe_format format = _mesa_renderbuffer_get_format(ctx, rb);
             int colormask_index = ctx->Extensions.EXT_draw_buffers2 ? i : 0;
 
-            if (!rb || !surface)
+            if (!rb || !rb->texture)
                continue;
 
             unsigned colormask =
@@ -419,7 +419,7 @@ st_Clear(struct gl_context *ctx, GLbitfield mask)
                continue;
 
             unsigned surf_colormask =
-               util_format_colormask(util_format_description(surface->format));
+               util_format_colormask(util_format_description(format));
 
             bool scissor = is_scissor_enabled(ctx, rb);
             if ((scissor && !st->can_scissor_clear) ||
@@ -434,8 +434,7 @@ st_Clear(struct gl_context *ctx, GLbitfield mask)
    }
 
    if (mask & BUFFER_BIT_DEPTH) {
-      struct pipe_surface *surface = _mesa_renderbuffer_get_surface(ctx, depthRb);
-      if (surface && ctx->Depth.Mask) {
+      if (depthRb->texture && ctx->Depth.Mask) {
          bool scissor = is_scissor_enabled(ctx, depthRb);
          if ((scissor && !st->can_scissor_clear) ||
              is_window_rectangle_enabled(ctx))
@@ -446,8 +445,7 @@ st_Clear(struct gl_context *ctx, GLbitfield mask)
       }
    }
    if (mask & BUFFER_BIT_STENCIL) {
-      struct pipe_surface *surface = _mesa_renderbuffer_get_surface(ctx, stencilRb);
-      if (surface && !is_stencil_disabled(ctx, stencilRb)) {
+      if (stencilRb->texture && !is_stencil_disabled(ctx, stencilRb)) {
          bool scissor = is_scissor_enabled(ctx, stencilRb);
          if ((scissor && !st->can_scissor_clear) ||
              is_window_rectangle_enabled(ctx) ||

commit 79b9072413a4806c7329468932b901022627a123

st/clear: handle null color renderbuffer
Fixes: 7b845cabd39 ("gallium: de-pointerize pipe_surface")

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35973>

src/mesa/state_tracker/st_cb_clear.c
@@ -406,12 +406,12 @@ st_Clear(struct gl_context *ctx, GLbitfield mask)
          if (b != BUFFER_NONE && mask & (1 << b)) {
             struct gl_renderbuffer *rb
                = ctx->DrawBuffer->Attachment[b].Renderbuffer;
-            enum pipe_format format = _mesa_renderbuffer_get_format(ctx, rb);
             int colormask_index = ctx->Extensions.EXT_draw_buffers2 ? i : 0;
 
             if (!rb || !rb->texture)
                continue;
 
+            enum pipe_format format = _mesa_renderbuffer_get_format(ctx, rb);
             unsigned colormask =
                GET_COLORMASK(ctx->Color.ColorMask, colormask_index);
 