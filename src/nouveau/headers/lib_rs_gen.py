joshuaswarren/mesa-1

commit cfcc9f2a36ae5ed905d032a3d15fa692724dbe67

nouveau/headers: Run rustfmt on generated files
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28255>

src/nouveau/headers/lib_rs_gen.py
@@ -88,6 +88,10 @@ def main():
     try:
         with open(args.out_rs, 'w', encoding='utf-8') as f:
             f.write(TEMPLATE_RS.render(root=root))
+            try:
+                subprocess.run(['rustfmt', args.out_rs], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     except Exception:
         # In the event there's an error, this imports some helpers from mako

commit d1f36e08f9db2db120ebae2fdac5208e7335bc22

nouveau/headers: Stop running rustfmt
rustfmt would change not only lib.rs, which is output by this script,
but also all of the other files in the library. The problem is, those
other files are inputs to lib_rs_gen so the script was changing its own
inputs which meant ninja needed to be run twice before we reached a
fixed point. Remove rustfmt for now to prevent this issue.

Fixes: cfcc9f2a ("nouveau/headers: Run rustfmt on generated files")
Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36121>

src/nouveau/headers/lib_rs_gen.py
@@ -119,10 +119,6 @@ def main():
         print(exceptions.text_error_template().render(), file=sys.stderr)
         sys.exit(1)
 
-    try:
-        subprocess.run(['rustfmt', args.out_rs], check=True)
-    except (subprocess.CalledProcessError, FileNotFoundError, PermissionError):
-        pass
 
 if __name__ == '__main__':
     main()