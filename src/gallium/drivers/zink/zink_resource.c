joshuaswarren/mesa-1

commit 47b5fe526363a49fd0ab28d39752d49b9f93de84

zink: only copy resource during add_bind if it is valid
this otherwise is a big no-op

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36010>

src/gallium/drivers/zink/zink_resource.c
@@ -1772,13 +1772,15 @@ add_resource_bind(struct zink_context *ctx, struct zink_resource *res, unsigned 
    res->layout = VK_IMAGE_LAYOUT_UNDEFINED;
    res->obj = new_obj;
    res->queue = VK_QUEUE_FAMILY_IGNORED;
-   for (unsigned i = 0; i <= res->base.b.last_level; i++) {
-      struct pipe_box box;
-      u_box_3d(0, 0, 0,
-               u_minify(res->base.b.width0, i),
-               u_minify(res->base.b.height0, i), res->base.b.array_size, &box);
-      box.depth = util_num_layers(&res->base.b, i);
-      ctx->base.resource_copy_region(&ctx->base, &res->base.b, i, 0, 0, 0, &staging.base.b, i, &box);
+   if (res->valid) {
+      for (unsigned i = 0; i <= res->base.b.last_level; i++) {
+         struct pipe_box box;
+         u_box_3d(0, 0, 0,
+                  u_minify(res->base.b.width0, i),
+                  u_minify(res->base.b.height0, i), res->base.b.array_size, &box);
+         box.depth = util_num_layers(&res->base.b, i);
+         ctx->base.resource_copy_region(&ctx->base, &res->base.b, i, 0, 0, 0, &staging.base.b, i, &box);
+      }
    }
    res->rebind_count++;
    if (old_obj->exportable) {

commit fd8961fe3a071c497c4b7a0500fcebbfa30a8abd

zink: fix valid contents check for adding new bind
the previous one didn't account for buffers

Fixes: 47b5fe52636 ("zink: only copy resource during add_bind if it is valid")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36201>

src/gallium/drivers/zink/zink_resource.c
@@ -1777,7 +1777,9 @@ add_resource_bind(struct zink_context *ctx, struct zink_resource *res, unsigned 
    res->layout = VK_IMAGE_LAYOUT_UNDEFINED;
    res->obj = new_obj;
    res->queue = VK_QUEUE_FAMILY_IGNORED;
-   if (res->valid) {
+   bool valid_contents = (res->obj->is_buffer && (res->valid_buffer_range.end || res->base.valid_buffer_range.end)) ||
+                         (!res->obj->is_buffer && res->valid);
+   if (valid_contents) {
       for (unsigned i = 0; i <= res->base.b.last_level; i++) {
          struct pipe_box box;
          u_box_3d(0, 0, 0,