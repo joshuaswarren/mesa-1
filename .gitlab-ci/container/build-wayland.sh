joshuaswarren/mesa-1

commit f309bf19eacbda8d91c262039aaf63dc95b0438f

ci: enable wayland platform in more debian builds
CI has recently gained coverage of wayland platform in EGL tests, but
some CI mesa builds such as arm builds currently don't enable the
wayland platform.
Enable it so test applications can properly initialize EGL with a
wayland platform.

Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
Reviewed-by: David Heidelberg <david.heidelberg@collabora.com>
Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20480>

.gitlab-ci/container/build-wayland.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086 # we want word splitting
 
 set -ex
 
@@ -8,7 +9,7 @@ export WAYLAND_PROTOCOLS_VERSION="1.24"
 git clone https://gitlab.freedesktop.org/wayland/wayland
 cd wayland
 git checkout "$LIBWAYLAND_VERSION"
-meson -Ddocumentation=false -Ddtd_validation=false -Dlibraries=true _build
+meson -Ddocumentation=false -Ddtd_validation=false -Dlibraries=true _build $EXTRA_MESON_ARGS
 ninja -C _build install
 cd ..
 rm -rf wayland
@@ -16,7 +17,7 @@ rm -rf wayland
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols
 cd wayland-protocols
 git checkout "$WAYLAND_PROTOCOLS_VERSION"
-meson _build
+meson _build $EXTRA_MESON_ARGS
 ninja -C _build install
 cd ..
 rm -rf wayland-protocols

.gitlab-ci/image-tags.yml
@@ -1,6 +1,6 @@
 variables:
    DEBIAN_X86_BUILD_BASE_IMAGE: "debian/x86_build-base"
-   DEBIAN_BASE_TAG: "2023-01-04-crosvm-virglrenderer"
+   DEBIAN_BASE_TAG: "2023-01-05-libwayland"
 
    DEBIAN_X86_BUILD_IMAGE_PATH: "debian/x86_build"
    DEBIAN_BUILD_TAG: "2022-11-03-ci-fairy-s3"

.gitlab-ci/build/gitlab-ci.yml
@@ -362,7 +362,7 @@ debian-android:
       -D glx=dri
       -D gbm=enabled
       -D egl=enabled
-      -D platforms=x11
+      -D platforms=x11,wayland
       -D osmesa=false
     GALLIUM_ST: >
       -D dri3=enabled

.gitlab-ci/container/cross_build.sh
@@ -18,6 +18,7 @@ apt-get install -y --no-remove \
         crossbuild-essential-$arch \
         libelf-dev:$arch \
         libexpat1-dev:$arch \
+        libffi-dev:$arch \
         libpciaccess-dev:$arch \
         libstdc++6:$arch \
         libvulkan-dev:$arch \
@@ -35,6 +36,7 @@ apt-get install -y --no-remove \
         libxrandr-dev:$arch \
         libxshmfence-dev:$arch \
         libxxf86vm-dev:$arch \
+        libwayland-dev:$arch \
         wget
 
 if [[ $arch != "armhf" ]]; then
@@ -50,7 +52,6 @@ if [[ $arch != "armhf" ]]; then
     # around this.
     apt-get install -y --no-remove --no-install-recommends \
             libclang-cpp${LLVM}:$arch \
-            libffi-dev:$arch \
             libgcc-s1:$arch \
             libtinfo-dev:$arch \
             libz3-dev:$arch \
@@ -67,6 +68,8 @@ fi
 # dependencies where we want a specific version
 EXTRA_MESON_ARGS="--cross-file=/cross_file-${arch}.txt -D libdir=lib/$(dpkg-architecture -A $arch -qDEB_TARGET_MULTIARCH)"
 . .gitlab-ci/container/build-libdrm.sh
+
+. .gitlab-ci/container/build-wayland.sh
 
 apt-get purge -y \
         $STABLE_EPHEMERAL

.gitlab-ci/container/debian/arm_build.sh
@@ -50,6 +50,7 @@ apt-get -y install \
 	libxrandr-dev \
 	libxshmfence-dev \
 	libxxf86vm-dev \
+	libwayland-dev \
 	llvm-11-dev \
 	ninja-build \
 	pkg-config \
@@ -83,6 +84,8 @@ arch=armhf
 # dependencies where we want a specific version
 EXTRA_MESON_ARGS=
 . .gitlab-ci/container/build-libdrm.sh
+
+. .gitlab-ci/container/build-wayland.sh
 
 apt-get purge -y $STABLE_EPHEMERAL
 

src/broadcom/ci/deqp-broadcom-rpi3-egl.toml
@@ -8,12 +8,14 @@ deqp_args = [
     "--deqp-gl-config-name=rgba8888d24s8ms0",
 ]
 
-[[deqp]]
-deqp = "/deqp/modules/egl/deqp-egl-wayland"
-caselists = ["/deqp/mustpass/egl-master.txt"]
-deqp_args = [
-    "--deqp-surface-width=256", "--deqp-surface-height=256",
-    "--deqp-surface-type=pbuffer", "--deqp-visibility=hidden",
-    "--deqp-gl-config-name=rgba8888d24s8ms0",
-]
-prefix = "wayland-"
+# Disabled due to timeouts since wayland platform was enabled
+# See https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20480#note_1702748
+#[[deqp]]
+#deqp = "/deqp/modules/egl/deqp-egl-wayland"
+#caselists = ["/deqp/mustpass/egl-master.txt"]
+#deqp_args = [
+#    "--deqp-surface-width=256", "--deqp-surface-height=256",
+#    "--deqp-surface-type=pbuffer", "--deqp-visibility=hidden",
+#    "--deqp-gl-config-name=rgba8888d24s8ms0",
+#]
+#prefix = "wayland-"

src/broadcom/ci/deqp-broadcom-rpi4-egl.toml
@@ -8,12 +8,14 @@ deqp_args = [
     "--deqp-gl-config-name=rgba8888d24s8ms0",
 ]
 
-[[deqp]]
-deqp = "/deqp/modules/egl/deqp-egl-wayland"
-caselists = ["/deqp/mustpass/egl-master.txt"]
-deqp_args = [
-    "--deqp-surface-width=256", "--deqp-surface-height=256",
-    "--deqp-surface-type=pbuffer", "--deqp-visibility=hidden",
-    "--deqp-gl-config-name=rgba8888d24s8ms0",
-]
-prefix = "wayland-"
+# Disabled due to timeouts since wayland platform was enabled
+# See https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20480#note_1702748
+#[[deqp]]
+#deqp = "/deqp/modules/egl/deqp-egl-wayland"
+#caselists = ["/deqp/mustpass/egl-master.txt"]
+#deqp_args = [
+#    "--deqp-surface-width=256", "--deqp-surface-height=256",
+#    "--deqp-surface-type=pbuffer", "--deqp-visibility=hidden",
+#    "--deqp-gl-config-name=rgba8888d24s8ms0",
+#]
+#prefix = "wayland-"

commit 015065399d909340909d1e8780a45e8bee4c00b1

ci: build and install native libwayland
If the same image is used for both cross and native builds, the native
build needs to be installed as well.

Signed-off-by: Julian Orth <ju.orth@gmail.com>
Fixes: f309bf19eac ("ci: enable wayland platform in more debian builds")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36191>

.gitlab-ci/container/build-wayland.sh
@@ -22,14 +22,14 @@ git clone https://gitlab.freedesktop.org/wayland/wayland
 cd wayland
 git checkout "$LIBWAYLAND_VERSION"
 
-# Build the scanner first in case we're a cross build
-# Note the lack of EXTRA_MESON_ARGS here.  This is always a native build
-meson setup -Dtests=false -Ddocumentation=false -Ddtd_validation=false -Dlibraries=false -Dscanner=true _scanner
-meson install -C _scanner
-
-# Now build libwayland using the given scanner
-meson setup -Ddocumentation=false -Ddtd_validation=false -Dlibraries=true -Dscanner=false _build ${EXTRA_MESON_ARGS:-}
+# Build the native library and scanner first in case we're a cross build.
+# Note the lack of EXTRA_MESON_ARGS here.
+meson setup -Dtests=false -Ddocumentation=false -Ddtd_validation=false -Dlibraries=true -Dscanner=true _build
 meson install -C _build
+
+# Now build cross libwayland using the native scanner
+meson setup -Dtests=false -Ddocumentation=false -Ddtd_validation=false -Dlibraries=true -Dscanner=false _cross ${EXTRA_MESON_ARGS:-}
+meson install -C _cross
 cd ..
 rm -rf wayland
 

.gitlab-ci/image-tags.yml
@@ -19,20 +19,20 @@ include:
   - .gitlab-ci/conditional-build-image-tags.yml
 
 variables:
-  DEBIAN_BASE_TAG: "20250715-apitrace"
+  DEBIAN_BASE_TAG: "20250722-libwayland"
 
-  DEBIAN_BUILD_TAG: "20250710-libwayland"
+  DEBIAN_BUILD_TAG: "20250722-libwayland"
 
-  DEBIAN_TEST_ANDROID_TAG: "20250718-angle-6a"
-  DEBIAN_TEST_GL_TAG: "20250718-piglit-09"
+  DEBIAN_TEST_ANDROID_TAG: "20250722-libwayland"
+  DEBIAN_TEST_GL_TAG: "20250722-libwayland"
   DEBIAN_TEST_VIDEO_TAG: "20250609-helper"
-  DEBIAN_TEST_VK_TAG: "20250718-piglit-09"
+  DEBIAN_TEST_VK_TAG: "20250722-libwayland"
 
-  ALPINE_X86_64_BUILD_TAG: "20250709-libwayland"
+  ALPINE_X86_64_BUILD_TAG: "20250722-libwayland"
   ALPINE_X86_64_LAVA_SSH_TAG: "20250423-rootfs"
   ALPINE_X86_64_LAVA_TRIGGER_TAG: "20250717-lava"
 
-  FEDORA_X86_64_BUILD_TAG: "20250709-libwayland"
+  FEDORA_X86_64_BUILD_TAG: "20250722-libwayland"
 
   KERNEL_TAG: "v6.14-mesa-4277"
   KERNEL_REPO: "gfx-ci/linux"