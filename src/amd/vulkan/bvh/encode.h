joshuaswarren/mesa-1

commit a83a5ee462aa083ba9ee80d98217f37940b2fb98

radv: Use the BVH8 format on GFX12
Reviewed-by: Natalie Vock <natalie.vock@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34273>

src/amd/vulkan/bvh/encode.h
@@ -52,4 +52,273 @@ radv_encode_instance_gfx10_3(VOID_REF dst_addr, vk_ir_instance_node src)
    DEREF(dst).instance_id = src.instance_id;
 }
 
+struct bit_writer {
+   uint64_t addr;
+   uint32_t offset;
+   uint32_t temp;
+   uint32_t count;
+   uint32_t total_count;
+};
+
+void
+bit_writer_init(out bit_writer writer, uint64_t addr)
+{
+   writer.addr = addr;
+   writer.offset = 0;
+   writer.temp = 0;
+   writer.count = 0;
+   writer.total_count = 0;
+}
+
+void
+bit_writer_write(inout bit_writer writer, uint32_t data, uint32_t bit_size)
+{
+   writer.total_count += bit_size;
+
+   if (writer.count + bit_size >= 32) {
+      writer.temp = writer.temp | (data << writer.count);
+
+      REF(uint32_t) dst = REF(uint32_t)(writer.addr + writer.offset);
+      DEREF(dst) = writer.temp;
+      writer.offset += 4;
+
+      bit_size = bit_size - (32 - writer.count);
+      if (writer.count == 0)
+         data = 0;
+      else
+         data = data >> (32 - writer.count);
+
+      writer.temp = 0;
+      writer.count = 0;
+   }
+
+   writer.temp = writer.temp | (data << writer.count);
+   writer.count += bit_size;
+}
+
+void
+bit_writer_skip_to(inout bit_writer writer, uint32_t target)
+{
+   /* Flush the remaining data. */
+   if (writer.count > 0) {
+      REF(uint32_t) dst = REF(uint32_t)(writer.addr + writer.offset);
+      DEREF(dst) = writer.temp;
+   }
+
+   writer.count = target % 32;
+   writer.total_count = target;
+   writer.offset = (target / 32) * 4;
+}
+
+void
+bit_writer_finish(inout bit_writer writer)
+{
+   /* Flush the remaining data. */
+   if (writer.count > 0) {
+      REF(uint32_t) dst = REF(uint32_t)(writer.addr + writer.offset);
+      DEREF(dst) = writer.temp;
+   }
+
+   writer.temp = 0;
+   writer.count = 0;
+   writer.total_count = 0;
+}
+
+void
+radv_encode_triangle_gfx12(VOID_REF dst, vk_ir_triangle_node src)
+{
+   bit_writer child_writer;
+   bit_writer_init(child_writer, dst);
+
+   bit_writer_write(child_writer, 31, 5); /* x_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 31, 5); /* y_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 31, 5); /* z_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 0, 5);  /* trailing_zero_bits */
+   bit_writer_write(child_writer, 14, 4); /* geometry_index_base_bits_div_2 */
+   bit_writer_write(child_writer, 14, 4); /* geometry_index_bits_div_2 */
+   bit_writer_write(child_writer, 0, 3);  /* triangle_pair_count_minus_one */
+   bit_writer_write(child_writer, 0, 1);  /* vertex_type */
+   bit_writer_write(child_writer, 28, 5); /* primitive_index_base_bits */
+   bit_writer_write(child_writer, 28, 5); /* primitive_index_bits */
+   /* header + 9 floats + geometry_id */
+   bit_writer_write(child_writer, RADV_GFX12_PRIMITIVE_NODE_HEADER_SIZE + 9 * 32 + 28, 10);
+
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[0][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[0][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[0][2]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[1][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[1][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[1][2]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[2][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[2][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.coords[2][2]), 32);
+
+   bit_writer_write(child_writer, src.geometry_id_and_flags & 0xfffffff, 28);
+   bit_writer_write(child_writer, src.triangle_id, 28);
+
+   bit_writer_skip_to(child_writer, 32 * 32 - RADV_GFX12_PRIMITIVE_NODE_PAIR_DESC_SIZE);
+
+   uint32_t opaque = (src.geometry_id_and_flags & VK_GEOMETRY_OPAQUE) != 0 ? 1 : 0;
+
+   bit_writer_write(child_writer, 1, 1);      /* prim_range_stop */
+   bit_writer_write(child_writer, 0, 1);      /* tri1_double_sided */
+   bit_writer_write(child_writer, 0, 1);      /* tri1_opaque */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v0_index */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v1_index */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v2_index */
+   bit_writer_write(child_writer, 0, 1);      /* tri0_double_sided */
+   bit_writer_write(child_writer, opaque, 1); /* tri0_opaque */
+   bit_writer_write(child_writer, 0, 4);      /* tri0_v0_index */
+   bit_writer_write(child_writer, 1, 4);      /* tri0_v1_index */
+   bit_writer_write(child_writer, 2, 4);      /* tri0_v2_index */
+
+   bit_writer_finish(child_writer);
+}
+
+void
+radv_encode_aabb_gfx12(VOID_REF dst, vk_ir_aabb_node src)
+{
+   bit_writer child_writer;
+   bit_writer_init(child_writer, dst);
+
+   bit_writer_write(child_writer, 0, 5);  /* x_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 0, 5);  /* y_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 0, 5);  /* z_vertex_bits_minus_one */
+   bit_writer_write(child_writer, 0, 5);  /* trailing_zero_bits */
+   bit_writer_write(child_writer, 14, 4); /* geometry_index_base_bits_div_2 */
+   bit_writer_write(child_writer, 14, 4); /* geometry_index_bits_div_2 */
+   bit_writer_write(child_writer, 0, 3);  /* triangle_pair_count_minus_one */
+   bit_writer_write(child_writer, 0, 1);  /* vertex_type */
+   bit_writer_write(child_writer, 28, 5); /* primitive_index_base_bits */
+   bit_writer_write(child_writer, 28, 5); /* primitive_index_bits */
+   /* header + 6 floats + geometry_id */
+   bit_writer_write(child_writer, RADV_GFX12_PRIMITIVE_NODE_HEADER_SIZE + 6 * 32 + 28, 10);
+
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.min.x), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.min.y), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.min.z), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.max.x), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.max.y), 32);
+   bit_writer_write(child_writer, floatBitsToUint(src.base.aabb.max.z), 32);
+
+   bit_writer_write(child_writer, src.geometry_id_and_flags & 0xfffffff, 28);
+   bit_writer_write(child_writer, src.primitive_id, 28);
+
+   bit_writer_skip_to(child_writer, 32 * 32 - RADV_GFX12_PRIMITIVE_NODE_PAIR_DESC_SIZE);
+
+   uint32_t opaque = (src.geometry_id_and_flags & VK_GEOMETRY_OPAQUE) != 0 ? 1 : 0;
+
+   bit_writer_write(child_writer, 1, 1);      /* prim_range_stop */
+   bit_writer_write(child_writer, 0, 1);      /* tri1_double_sided */
+   bit_writer_write(child_writer, 0, 1);      /* tri1_opaque */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v0_index */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v1_index */
+   bit_writer_write(child_writer, 0, 4);      /* tri1_v2_index */
+   bit_writer_write(child_writer, 0, 1);      /* tri0_double_sided */
+   bit_writer_write(child_writer, opaque, 1); /* tri0_opaque */
+   bit_writer_write(child_writer, 0xf, 4);    /* tri0_v0_index */
+   bit_writer_write(child_writer, 0xf, 4);    /* tri0_v1_index */
+   bit_writer_write(child_writer, 0, 4);      /* tri0_v2_index */
+
+   bit_writer_finish(child_writer);
+}
+
+/* Writes both the HW node and user data. */
+void
+radv_encode_instance_gfx12(VOID_REF dst, vk_ir_instance_node src)
+{
+   bit_writer child_writer;
+   bit_writer_init(child_writer, dst);
+
+   radv_accel_struct_header blas_header = DEREF(REF(radv_accel_struct_header)(src.base_ptr));
+
+   mat4 transform = mat4(src.otw_matrix);
+   mat4 wto_matrix = transpose(inverse(transpose(transform)));
+
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[0][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[0][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[0][2]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[0][3]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[1][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[1][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[1][2]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[1][3]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[2][0]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[2][1]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[2][2]), 32);
+   bit_writer_write(child_writer, floatBitsToUint(wto_matrix[2][3]), 32);
+
+   uint32_t flags = src.sbt_offset_and_flags >> 24;
+   uint32_t instance_pointer_flags = 0;
+   if ((flags & VK_GEOMETRY_INSTANCE_FORCE_OPAQUE_BIT_KHR) != 0)
+      instance_pointer_flags |= 1;
+   if ((flags & VK_GEOMETRY_INSTANCE_FORCE_NO_OPAQUE_BIT_KHR) != 0)
+      instance_pointer_flags |= 2;
+   if ((flags & VK_GEOMETRY_INSTANCE_TRIANGLE_FACING_CULL_DISABLE_BIT_KHR) != 0 ||
+       blas_header.geometry_type == VK_GEOMETRY_TYPE_AABBS_KHR)
+      instance_pointer_flags |= 4;
+   if ((flags & VK_GEOMETRY_INSTANCE_TRIANGLE_FLIP_FACING_BIT_KHR) != 0)
+      instance_pointer_flags |= 8;
+
+   if (blas_header.geometry_type == VK_GEOMETRY_TYPE_TRIANGLES_KHR)
+      instance_pointer_flags |= 512;
+   else
+      instance_pointer_flags |= 256;
+
+   uint64_t bvh_addr = addr_to_node(src.base_ptr + blas_header.bvh_offset);
+   bit_writer_write(child_writer, uint32_t(bvh_addr & 0xffffffff), 32);
+   bit_writer_write(child_writer, uint32_t(bvh_addr >> 32) | (instance_pointer_flags << (54 - 32)), 32);
+   bit_writer_write(child_writer, src.custom_instance_and_mask & 0xffffff, 32);
+   bit_writer_write(child_writer, src.sbt_offset_and_flags & 0xffffff, 24);
+   bit_writer_write(child_writer, src.custom_instance_and_mask >> 24, 8);
+
+   bit_writer_write(child_writer, floatBitsToUint(blas_header.aabb.min.x), 32);
+   bit_writer_write(child_writer, floatBitsToUint(blas_header.aabb.min.y), 32);
+   bit_writer_write(child_writer, floatBitsToUint(blas_header.aabb.min.z), 32);
+
+   vec3 child_extent = blas_header.aabb.max - blas_header.aabb.min;
+   uvec3 child_extent_exponents = uvec3(ceil(clamp(log2(child_extent) + 127.0, vec3(0.0), vec3(255))));
+
+   bit_writer_write(child_writer, child_extent_exponents.x, 8);
+   bit_writer_write(child_writer, child_extent_exponents.y, 8);
+   bit_writer_write(child_writer, child_extent_exponents.z, 8);
+   bit_writer_write(child_writer, 0, 4);
+   bit_writer_write(child_writer, 0, 4);
+
+   bit_writer_write(child_writer, 0, 12);
+   bit_writer_write(child_writer, 0, 12);
+   bit_writer_write(child_writer, 4, 8);
+   bit_writer_write(child_writer, 0, 12);
+   bit_writer_write(child_writer, 0xfff, 12);
+   bit_writer_write(child_writer, 0xff, 8);
+   bit_writer_write(child_writer, 0xfff, 12);
+   bit_writer_write(child_writer, 0xfff, 12);
+   bit_writer_write(child_writer, radv_bvh_node_box32, 4);
+   bit_writer_write(child_writer, 1, 4);
+
+   for (uint32_t remaining_child_index = 0; remaining_child_index < 3; remaining_child_index++) {
+      bit_writer_write(child_writer, 0xfff, 12);
+      bit_writer_write(child_writer, 0xfff, 12);
+      bit_writer_write(child_writer, 0xff, 8);
+      bit_writer_write(child_writer, 0xfff, 12);
+      bit_writer_write(child_writer, 0, 12);
+      bit_writer_write(child_writer, 0, 8);
+      bit_writer_write(child_writer, 0, 12);
+      bit_writer_write(child_writer, 0, 12);
+      bit_writer_write(child_writer, 0, 8);
+   }
+
+   bit_writer_finish(child_writer);
+
+   REF(radv_gfx12_instance_node_user_data) user_data =
+      REF(radv_gfx12_instance_node_user_data)(dst + RADV_GFX12_BVH_NODE_SIZE);
+   DEREF(user_data).otw_matrix = src.otw_matrix;
+   DEREF(user_data).custom_instance = src.custom_instance_and_mask & 0xffffff;
+   DEREF(user_data).instance_index = src.instance_id;
+   DEREF(user_data).bvh_offset = blas_header.bvh_offset;
+   DEREF(user_data).blas_addr = src.base_ptr;
+   DEREF(user_data).primitive_base_indices_offset = blas_header.primitive_base_indices_offset;
+   DEREF(user_data).leaf_node_offsets_offset = blas_header.leaf_node_offsets_offset;
+}
+
 #endif

src/amd/vulkan/bvh/build_helpers.h
@@ -17,6 +17,10 @@ TYPE(radv_bvh_aabb_node, 4);
 TYPE(radv_bvh_instance_node, 8);
 TYPE(radv_bvh_box16_node, 4);
 TYPE(radv_bvh_box32_node, 4);
+TYPE(radv_gfx12_box_node, 4);
+TYPE(radv_gfx12_instance_node, 8);
+TYPE(radv_gfx12_instance_node_user_data, 4);
+TYPE(radv_gfx12_primitive_node, 4);
 
 uint32_t
 id_to_offset(uint32_t id)

src/amd/vulkan/bvh/build_interface.h
@@ -35,6 +35,16 @@ struct encode_args {
    uint32_t geometry_type;
 };
 
+struct encode_gfx12_args {
+   VOID_REF intermediate_bvh;
+   VOID_REF output_base;
+   REF(vk_ir_header) header;
+   uint32_t output_bvh_offset;
+   uint32_t leaf_node_offsets_offset;
+   uint32_t leaf_node_count;
+   uint32_t geometry_type;
+};
+
 struct header_args {
    REF(vk_ir_header) src;
    REF(radv_accel_struct_header) dst;

src/amd/vulkan/bvh/bvh.h
@@ -58,10 +58,12 @@ struct radv_accel_struct_header {
    uint64_t size;
 
    /* Everything after this gets updated/copied from the CPU. */
+   uint32_t geometry_type;
    uint32_t geometry_count;
    uint32_t primitive_base_indices_offset;
    uint64_t instance_offset;
    uint64_t instance_count;
+   uint32_t leaf_node_offsets_offset;
    uint32_t build_flags;
 };
 
@@ -113,5 +115,61 @@ struct radv_bvh_box32_node {
 
 #define RADV_BVH_ROOT_NODE    radv_bvh_node_box32
 #define RADV_BVH_INVALID_NODE 0xffffffffu
+
+/* GFX12 */
+
+#define RADV_GFX12_BVH_NODE_SIZE 128
+
+struct radv_gfx12_box_child {
+   uint32_t dword0;
+   uint32_t dword1;
+   uint32_t dword2;
+};
+
+#ifndef VULKAN
+typedef struct radv_gfx12_box_child radv_gfx12_box_child;
+#endif
+
+struct radv_gfx12_box_node {
+   uint32_t internal_base_id;
+   uint32_t primitive_base_id;
+   uint32_t unused;
+   vec3 origin;
+   uint32_t child_count_exponents;
+   uint32_t obb_matrix_index;
+   radv_gfx12_box_child children[8];
+};
+
+struct radv_gfx12_instance_node {
+   mat3x4 wto_matrix;
+   uint64_t pointer_flags_bvh_addr;
+   uint32_t unused;
+   uint32_t cull_mask_user_data;
+   vec3 origin;
+   uint32_t child_count_exponents;
+   radv_gfx12_box_child children[4];
+};
+
+struct radv_gfx12_instance_node_user_data {
+   mat3x4 otw_matrix;
+   uint32_t custom_instance;
+   uint32_t instance_index;
+   uint32_t bvh_offset;
+   uint32_t padding;
+   uint64_t blas_addr;
+   uint32_t primitive_base_indices_offset;
+   uint32_t leaf_node_offsets_offset;
+   uint32_t unused[12];
+};
+
+/* Size of the primitive header section in bits. */
+#define RADV_GFX12_PRIMITIVE_NODE_HEADER_SIZE 52
+
+/* Size of a primitive pair description in bits. */
+#define RADV_GFX12_PRIMITIVE_NODE_PAIR_DESC_SIZE 29
+
+struct radv_gfx12_primitive_node {
+   uint32_t dwords[32];
+};
 
 #endif /* BVH_H */

src/amd/vulkan/bvh/copy.comp
@@ -71,7 +71,10 @@ main(void)
          DEREF(REF(uvec4)(copy_src_addr + offset));
 
       /* Do the adjustment inline in the same invocation that copies the data so that we don't have
-       * to synchronize. */
+       * to synchronize. This is only possible on pre-GFX12 HW because leaf nodes have a different
+       * order on GFX12.
+       */
+#if !GFX12
       if (offset < node_end && offset >= node_offset &&
           (offset - node_offset) % SIZEOF(radv_bvh_instance_node) == 0) {
          uint64_t idx = (offset - node_offset) / SIZEOF(radv_bvh_instance_node);
@@ -85,5 +88,6 @@ main(void)
             DEREF(REF(radv_bvh_instance_node)(copy_dst_addr + offset)).bvh_ptr = addr_to_node(blas_addr + bvh_offset);
          }
       }
+#endif
    }
 }

src/amd/vulkan/bvh/copy_blas_addrs_gfx12.comp
@@ -1,0 +1,65 @@
+/*
+ * Copyright © 2022 Valve Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#version 460
+
+#extension GL_GOOGLE_include_directive : require
+
+#extension GL_EXT_shader_explicit_arithmetic_types_int8 : require
+#extension GL_EXT_shader_explicit_arithmetic_types_int16 : require
+#extension GL_EXT_shader_explicit_arithmetic_types_int32 : require
+#extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
+#extension GL_EXT_shader_explicit_arithmetic_types_float16 : require
+#extension GL_EXT_scalar_block_layout : require
+#extension GL_EXT_buffer_reference : require
+#extension GL_EXT_buffer_reference2 : require
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+#include "build_interface.h"
+
+layout(push_constant) uniform CONSTS
+{
+   copy_args args;
+};
+
+void
+main(void)
+{
+   uint32_t global_id = gl_GlobalInvocationID.x;
+   uint32_t total_invocations = gl_NumWorkGroups.x * 64;
+
+   uint64_t accel_struct_addr = args.mode == RADV_COPY_MODE_SERIALIZE ? args.src_addr : args.dst_addr;
+   uint64_t serialized_addr = args.mode == RADV_COPY_MODE_SERIALIZE ? args.dst_addr : args.src_addr;
+
+   uint64_t blas_addrs = serialized_addr + SIZEOF(radv_accel_struct_serialization_header);
+
+   radv_accel_struct_serialization_header serialization_header =
+      DEREF(REF(radv_accel_struct_serialization_header)(serialized_addr));
+
+   radv_accel_struct_header header = DEREF(REF(radv_accel_struct_header)(accel_struct_addr));
+
+   for (uint32_t i = global_id; i < serialization_header.instance_count; i += total_invocations) {
+      uint64_t instance_offset_addr = accel_struct_addr + (header.leaf_node_offsets_offset + i * 4);
+      uint64_t instance_addr = accel_struct_addr + (header.bvh_offset + DEREF(REF(uint32_t)(instance_offset_addr)));
+      REF(radv_gfx12_instance_node) instance_node = REF(radv_gfx12_instance_node)(instance_addr);
+      REF(radv_gfx12_instance_node_user_data) instance_data =
+         REF(radv_gfx12_instance_node_user_data)(instance_addr + SIZEOF(radv_gfx12_instance_node));
+
+      if (args.mode == RADV_COPY_MODE_SERIALIZE) {
+         DEREF(INDEX(uint64_t, blas_addrs, i)) = DEREF(instance_data).blas_addr;
+      } else {
+         uint32_t bvh_offset = DEREF(instance_data).bvh_offset;
+
+         /* Replace the address while keeping the pointer flags. */
+         uint64_t pointer_flags_bvh_addr = DEREF(instance_node).pointer_flags_bvh_addr;
+         uint64_t blas_addr = DEREF(INDEX(uint64_t, blas_addrs, i));
+         DEREF(instance_node).pointer_flags_bvh_addr =
+            (pointer_flags_bvh_addr & 0xFFC0000000000000ul) | addr_to_node(blas_addr + bvh_offset);
+         DEREF(instance_data).blas_addr = blas_addr;
+      }
+   }
+}

commit 17d91fc96d9a2827ebadf104c94aaccef9521e63

radv/bvh: Fix flush in bit_writer_skip_to
If temp is not cleared, the next flushed dword will contain data from
the previous one.

Fixes: a83a5ee ("radv: Use the BVH8 format on GFX12")
Reviewed-by: Natalie Vock <natalie.vock@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35445>

src/amd/vulkan/bvh/encode.h
@@ -109,6 +109,7 @@ bit_writer_skip_to(inout bit_writer writer, uint32_t target)
    if (writer.count > 0) {
       REF(uint32_t) dst = REF(uint32_t)(writer.addr + writer.offset);
       DEREF(dst) = writer.temp;
+      writer.temp = 0;
    }
 
    writer.count = target % 32;