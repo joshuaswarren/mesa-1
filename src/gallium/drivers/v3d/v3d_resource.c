joshuaswarren/mesa-1

commit d0b3096fe623cdd01917f9c4ef3ff6e022928c66

v3d: Add V3D_TFU_READAHEAD padding for allocated resources
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36159>

src/gallium/drivers/v3d/v3d_resource.c
@@ -102,17 +102,18 @@ v3d_resource_bo_alloc(struct v3d_resource *rsc)
         struct pipe_screen *pscreen = prsc->screen;
         struct v3d_bo *bo;
 
-        /* Buffers may be read using ldunifa, which prefetches the next
-         * 4 bytes after a read. If the buffer's size is exactly a multiple
-         * of a page size and the shader reads the last 4 bytes with ldunifa
-         * the prefetching would read out of bounds and cause an MMU error,
-         * so we allocate extra space to avoid kernel error spamming.
+        /* Buffers may be read using ldunifa, which prefetches the next 4
+         * bytes after a read. If the buffer's size is exactly a multiple of a
+         * page size and the shader reads the last 4 bytes with ldunifa the
+         * prefetching would read out of bounds and cause an MMU error, so we
+         * allocate extra space to avoid kernel error spamming. We also need
+         * to add a V3D_TFU_READAHEAD padding to avoid invalid reads done by
+         * the TFU unit after the end of the last page allocated.
          */
-        uint32_t size = rsc->size;
-        if (rsc->base.target == PIPE_BUFFER && (size % 4096 == 0))
-                size += 4;
-
-        bo = v3d_bo_alloc(v3d_screen(pscreen), size, "resource");
+        uint32_t padding =
+                rsc->base.target == PIPE_BUFFER ? 4 : V3D_TFU_READAHEAD_SIZE;
+        bo = v3d_bo_alloc(v3d_screen(pscreen), rsc->size + padding,
+                          "resource");
         if (bo) {
                 v3d_bo_unreference(&rsc->bo);
                 rsc->bo = bo;

commit 07f93fe29158e801416010745a939a933bf2971a

v3d: Add V3D_TFU_READAHEAD padding for renderonly resources
Fixes: d0b3096fe62 ("v3d: Add V3D_TFU_READAHEAD padding for allocated resources")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13508
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Reviewed-by: Juan A. Suarez <jasuarez@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36407>

src/gallium/drivers/v3d/v3d_resource.c
@@ -847,11 +847,13 @@ v3d_resource_create_with_modifiers(struct pipe_screen *pscreen,
         if (screen->ro && (tmpl->bind & PIPE_BIND_SCANOUT)) {
                 assert(!rsc->tiled);
                 struct winsys_handle handle;
+                uint32_t scanout_height =
+                        align(rsc->size + V3D_TFU_READAHEAD_SIZE, 4096) / 4096;
                 struct pipe_resource scanout_tmpl = {
                         .target = prsc->target,
                         .format = PIPE_FORMAT_RGBA8888_UNORM,
                         .width0 = 1024, /* one page */
-                        .height0 = align(rsc->size, 4096) / 4096,
+                        .height0 = scanout_height,
                         .depth0 = 1,
                         .array_size = 1,
                 };