joshuaswarren/mesa-1

commit ffd3525abb4e6141c65175ba90b6925d7a202108

radv: add support for formats with host-transfer
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35974>

src/amd/vulkan/radv_formats.c
@@ -501,6 +501,15 @@ radv_physical_device_get_format_properties(struct radv_physical_device *pdev, Vk
       buffer = 0;
    }
 
+   /* No depth/stencil support yet due to VKCTS issues. */
+   if (!vk_format_is_depth_or_stencil(format)) {
+      if (linear & VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT)
+         linear |= VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
+
+      if (tiled & VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT)
+         tiled |= VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
+   }
+
    out_properties->linearTilingFeatures = linear;
    out_properties->optimalTilingFeatures = tiled;
    out_properties->bufferFeatures = buffer;
@@ -628,6 +637,9 @@ radv_get_modifier_flags(struct radv_physical_device *pdev, VkFormat format, uint
 
    /* Unconditionally disable DISJOINT support for modifiers for now */
    features &= ~VK_FORMAT_FEATURE_2_DISJOINT_BIT;
+
+   /* Unconditionally disable HOST_TRANSFER support for modifiers for now */
+   features &= ~VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
 
    if (ac_modifier_has_dcc(modifier)) {
       /* We don't enable DCC for multi-planar formats */
@@ -1030,9 +1042,18 @@ radv_get_image_format_properties(struct radv_physical_device *pdev, const VkPhys
          goto unsupported;
    }
 
-   /* Sparse resources with multi-planar formats are unsupported. */
+   if (image_usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT) {
+      if (!(format_feature_flags & VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT))
+         goto unsupported;
+   }
+
    if (info->flags & VK_IMAGE_CREATE_SPARSE_BINDING_BIT) {
+      /* Sparse resources with multi-planar formats are unsupported. */
       if (vk_format_get_plane_count(format) > 1)
+         goto unsupported;
+
+      /* Sparse resources with host-transfer are unsupported. */
+      if (image_usage & VK_IMAGE_USAGE_HOST_TRANSFER_BIT_EXT)
          goto unsupported;
    }
 

commit ce0fb5e1bff2a24165cf87a1da58bf8cdb1b39be

radv: don't set HOST_IMAGE_TRANSFER_BIT if host_image_copy not enabled
This can't work if the extension isn't enabled, so only set if the
extension is enabled.

Fixes: ffd3525abb4e ("radv: add support for formats with host-transfer")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36400>

src/amd/vulkan/radv_formats.c
@@ -502,7 +502,7 @@ radv_physical_device_get_format_properties(struct radv_physical_device *pdev, Vk
    }
 
    /* No depth/stencil support yet due to VKCTS issues. */
-   if (!vk_format_is_depth_or_stencil(format)) {
+   if (radv_host_image_copy_enabled(pdev) && !vk_format_is_depth_or_stencil(format)) {
       if (linear & VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT)
          linear |= VK_FORMAT_FEATURE_2_HOST_IMAGE_TRANSFER_BIT_EXT;
 

src/amd/vulkan/radv_physical_device.c
@@ -151,7 +151,7 @@ radv_cooperative_matrix2_nv_enabled(const struct radv_physical_device *pdev)
    return instance->drirc.cooperative_matrix2_nv;
 }
 
-static bool
+bool
 radv_host_image_copy_enabled(const struct radv_physical_device *pdev)
 {
    const struct radv_instance *instance = radv_physical_device_instance(pdev);

src/amd/vulkan/radv_physical_device.h
@@ -261,6 +261,8 @@ radv_use_llvm_for_stage(const struct radv_physical_device *pdev, UNUSED gl_shade
    return pdev->use_llvm;
 }
 
+bool radv_host_image_copy_enabled(const struct radv_physical_device *pdev);
+
 bool radv_enable_rt(const struct radv_physical_device *pdev);
 
 bool radv_emulate_rt(const struct radv_physical_device *pdev);