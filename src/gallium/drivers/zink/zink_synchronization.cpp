joshuaswarren/mesa-1

commit 999f757a199bc5da396b884cfd094fb3a4c53933

zink: use unsynchronized cmdbuf for unsynchronized image GENERAL barriers
this fixes all the random sync issues

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35979>

src/gallium/drivers/zink/zink_synchronization.cpp
@@ -410,7 +410,7 @@ zink_resource_image_barrier(struct zink_context *ctx, struct zink_resource *res,
    bool completed = zink_resource_usage_check_completion_fast(zink_screen(ctx->base.screen), res, rw);
    bool usage_matches = !completed && zink_resource_usage_matches(res, ctx->bs);
    VkCommandBuffer cmdbuf = GENERAL && new_layout == VK_IMAGE_LAYOUT_GENERAL ?
-                            (is_write ? zink_get_cmdbuf(ctx, NULL, res) : zink_get_cmdbuf(ctx, res, NULL)) :
+                            (UNSYNCHRONIZED ? ctx->bs->unsynchronized_cmdbuf : is_write ? zink_get_cmdbuf(ctx, NULL, res) : zink_get_cmdbuf(ctx, res, NULL)) :
                             update_unordered_access_and_get_cmdbuf<UNSYNCHRONIZED>::apply(ctx, res, usage_matches, is_write);
 
    assert(new_layout);

commit d37ca840770887a89fdc6548876c28e60f056cfc

zink: fix assert for unsynchronized non-GENERAL image barriers
if this is already unsync then everything is fine

Fixes: 999f757a199 ("zink: use unsynchronized cmdbuf for unsynchronized image GENERAL barriers")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36576>

src/gallium/drivers/zink/zink_synchronization.cpp
@@ -354,7 +354,7 @@ template <>
 struct update_unordered_access_and_get_cmdbuf<true> {
    static VkCommandBuffer apply(struct zink_context *ctx, struct zink_resource *res, bool usage_matches, bool is_write)
    {
-      assert(!usage_matches);
+      assert(!usage_matches || (res->obj->unordered_write && res->obj->unordered_read));
       res->obj->unordered_write = true;
       res->obj->unordered_read = true;
       ctx->bs->has_unsync = true;