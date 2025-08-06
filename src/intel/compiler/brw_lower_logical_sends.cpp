joshuaswarren/mesa-1

commit 1455adcafea594161b49a179817033a1576d4179

intel/compiler: implement Wa_14014595444 for DG2
According to the workaround, we should setup MLOD as parameter
4 and 5 for the sample_b message.

v2: only SAMPLE_B, not SAMPLE_B_C (Lionel)

Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18408>

src/intel/compiler/brw_lower_logical_sends.cpp
@@ -1068,6 +1068,11 @@ lower_sampler_logical_send_gfx7(const fs_builder &bld, fs_inst *inst, opcode op,
       }
 
       bld.MOV(sources[length++], min_lod);
+
+      /* Wa_14014595444: Populate MLOD as parameter 5 (twice). */
+       if (devinfo->verx10 == 125 && op == FS_OPCODE_TXB &&
+          !inst->shadow_compare)
+         bld.MOV(sources[length++], min_lod);
    }
 
    const fs_reg src_payload =

commit 796cbd374777cba764639be8920fcb952d471ca7

intel/compiler: use the WA framework when emitting WA 14014595444
Fixes: 1455adc "intel/compiler: implement Wa_14014595444 for DG2"
Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36262>

src/intel/compiler/brw_lower_logical_sends.cpp
@@ -1113,8 +1113,8 @@ lower_sampler_logical_send(const brw_builder &bld, brw_inst *inst,
       bld.MOV(sources[length++], min_lod);
 
       /* Wa_14014595444: Populate MLOD as parameter 5 (twice). */
-       if (devinfo->verx10 == 125 && op == FS_OPCODE_TXB_LOGICAL &&
-           shadow_c.file == BAD_FILE)
+       if (intel_needs_workaround(devinfo, 14014595444) &&
+           op == FS_OPCODE_TXB_LOGICAL && shadow_c.file == BAD_FILE)
          bld.MOV(sources[length++], min_lod);
    }
 