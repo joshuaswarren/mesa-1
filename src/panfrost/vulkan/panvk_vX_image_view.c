joshuaswarren/mesa-1

commit 931a40d9482f232274426a4f43c83e827a0989a9

panvk: add EXT_texture_compression_astc_hdr support
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34828>

src/panfrost/vulkan/panvk_vX_image_view.c
@@ -315,6 +315,7 @@ panvk_per_arch(CreateImageView)(VkDevice _device,
 
    view->pview = (struct pan_image_view){
       .format = vk_format_to_pipe_format(view->vk.view_format),
+      .astc.hdr = util_format_is_astc_hdr(view->vk.view_format),
       .dim = panvk_view_type_to_mali_tex_dim(view->vk.view_type),
       .nr_samples = image->vk.samples,
       .first_level = view->vk.base_mip_level,

src/panfrost/vulkan/panvk_physical_device.c
@@ -322,6 +322,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .EXT_shader_subgroup_vote = true,
       .EXT_subgroup_size_control = has_vk1_1,
       .EXT_texel_buffer_alignment = true,
+      .EXT_texture_compression_astc_hdr = true,
       .EXT_tooling_info = true,
       .EXT_vertex_attribute_divisor = true,
       .EXT_vertex_input_dynamic_state = true,
@@ -358,6 +359,12 @@ static bool
 has_texture_compression_astc_ldr(const struct panvk_physical_device *physical_device)
 {
    return has_compressed_formats(physical_device, BITFIELD_BIT(MALI_ASTC_2D_LDR));
+}
+
+static bool
+has_texture_compression_astc_hdr(const struct panvk_physical_device *physical_device)
+{
+   return has_compressed_formats(physical_device, BITFIELD_BIT(MALI_ASTC_2D_HDR));
 }
 
 static bool
@@ -495,7 +502,7 @@ get_features(const struct panvk_instance *instance,
       .subgroupSizeControl = true,
       .computeFullSubgroups = true,
       .synchronization2 = true,
-      .textureCompressionASTC_HDR = false,
+      .textureCompressionASTC_HDR = has_texture_compression_astc_hdr(device),
       .shaderZeroInitializeWorkgroupMemory = true,
       .dynamicRendering = true,
       .dynamicRenderingLocalRead = true,

src/panfrost/vulkan/panvk_vX_buffer_view.c
@@ -57,6 +57,7 @@ panvk_per_arch(CreateBufferView)(VkDevice _device,
    if (buffer->vk.usage & tex_usage_mask) {
       struct pan_buffer_view bview = {
          .format = pfmt,
+         .astc.hdr = util_format_is_astc_hdr(pfmt),
          .width_el = view->vk.elements,
          .base = address,
       };

commit aa2e44a49fa9e5e118b881a48a09f63b9ce0838a

panvk: fix EXT_texture_compression_astc_support
We can't mix pipe_format and VkFormat values, whoops!

Fixes: 931a40d9482 ("panvk: add EXT_texture_compression_astc_hdr support")
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36601>

src/panfrost/vulkan/panvk_vX_image_view.c
@@ -314,9 +314,10 @@ panvk_per_arch(CreateImageView)(VkDevice _device,
    if (view == NULL)
       return panvk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
 
+   enum pipe_format pfmt = vk_format_to_pipe_format(view->vk.view_format);
    view->pview = (struct pan_image_view){
-      .format = vk_format_to_pipe_format(view->vk.view_format),
-      .astc.hdr = util_format_is_astc_hdr(view->vk.view_format),
+      .format = pfmt,
+      .astc.hdr = util_format_is_astc_hdr(pfmt),
       .dim = panvk_view_type_to_mali_tex_dim(view->vk.view_type),
       .nr_samples = image->vk.samples,
       .first_level = view->vk.base_mip_level,