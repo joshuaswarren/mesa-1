joshuaswarren/mesa-1

commit 28bdd5a1a6b6263e2fc51be9e1498d6493d29fa4

rusticl/mesa: use pipe_sampler_view_reference
pipe_sampler_views are reference counted, so we shouldn't delete them
directly.

Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36243>

src/gallium/frontends/rusticl/mesa/pipe/resource.rs
@@ -350,8 +350,7 @@ impl<'c, 'r> PipeSamplerView<'c, 'r> {
 impl Drop for PipeSamplerView<'_, '_> {
     fn drop(&mut self) {
         unsafe {
-            let ctx = self.view.as_ref().context;
-            (*ctx).sampler_view_release.unwrap()(ctx, self.view.as_ptr())
+            pipe_sampler_view_reference(&mut self.view.as_ptr(), ptr::null_mut());
         }
     }
 }

commit 1e976f686ac859a68432afe59618726c9af6c261

rusticl: use pipe_sampler_view_release
Fixes: 28bdd5a1a6b ("rusticl/mesa: use pipe_sampler_view_reference")
Suggested-by: Seán de Búrca <leftmostcat@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36613>

src/gallium/frontends/rusticl/mesa/pipe/context.rs
@@ -711,7 +711,7 @@ fn has_required_cbs(context: &pipe_context) -> bool {
         & has_required_feature!(context, launch_grid)
         & has_required_feature!(context, memory_barrier)
         & has_required_feature!(context, resource_copy_region)
-        // implicitly used through pipe_sampler_view_reference
+        // implicitly used through pipe_sampler_view_release
         & has_required_feature!(context, sampler_view_destroy)
         & has_required_feature!(context, set_constant_buffer)
         & has_required_feature!(context, set_global_binding)

src/gallium/frontends/rusticl/mesa/pipe/resource.rs
@@ -350,7 +350,7 @@ impl<'c, 'r> PipeSamplerView<'c, 'r> {
 impl Drop for PipeSamplerView<'_, '_> {
     fn drop(&mut self) {
         unsafe {
-            pipe_sampler_view_reference(&mut self.view.as_ptr(), ptr::null_mut());
+            pipe_sampler_view_release(self.view.as_ptr());
         }
     }
 }