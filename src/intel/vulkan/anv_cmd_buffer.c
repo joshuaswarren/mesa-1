joshuaswarren/mesa-1

commit 8b62cf285dafdc5ade91ef74ab2dafc2be6b5a77

anv: store gfx/compute bound shaders on command buffer state
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36512>

src/intel/vulkan/anv_cmd_buffer.c
@@ -503,9 +503,9 @@ anv_cmd_buffer_set_ray_query_buffer(struct anv_cmd_buffer *cmd_buffer,
  * state appropriately.
  */
 static void
-anv_cmd_buffer_flush_pipeline_state(struct anv_cmd_buffer *cmd_buffer,
-                                    struct anv_graphics_pipeline *old_pipeline,
-                                    struct anv_graphics_pipeline *new_pipeline)
+anv_cmd_buffer_flush_pipeline_hw_state(struct anv_cmd_buffer *cmd_buffer,
+                                       struct anv_graphics_pipeline *old_pipeline,
+                                       struct anv_graphics_pipeline *new_pipeline)
 {
    struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
    struct anv_gfx_dynamic_state *hw_state = &gfx->dyn_state;
@@ -681,11 +681,12 @@ void anv_CmdBindPipeline(
       if (cmd_buffer->state.compute.base.pipeline == pipeline)
          return;
 
-      cmd_buffer->state.compute.base.pipeline = pipeline;
-      cmd_buffer->state.compute.pipeline_dirty = true;
-
       struct anv_compute_pipeline *compute_pipeline =
          anv_pipeline_to_compute(pipeline);
+
+      cmd_buffer->state.compute.shader = compute_pipeline->cs;
+      cmd_buffer->state.compute.pipeline_dirty = true;
+
       set_dirty_for_bind_map(cmd_buffer, MESA_SHADER_COMPUTE,
                              &compute_pipeline->cs->bind_map);
 
@@ -711,10 +712,16 @@ void anv_CmdBindPipeline(
          cmd_buffer->state.gfx.base.pipeline == NULL ? NULL :
          anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
 
-      cmd_buffer->state.gfx.base.pipeline = pipeline;
       cmd_buffer->state.gfx.dirty |=
          get_pipeline_dirty_stages(cmd_buffer->device,
                                    old_pipeline, new_pipeline);
+
+      STATIC_ASSERT(sizeof(cmd_buffer->state.gfx.shaders) ==
+                    sizeof(new_pipeline->base.shaders));
+      memcpy(cmd_buffer->state.gfx.shaders,
+             new_pipeline->base.shaders,
+             sizeof(cmd_buffer->state.gfx.shaders));
+      cmd_buffer->state.gfx.active_stages = pipeline->active_stages;
 
       anv_foreach_stage(stage, new_pipeline->base.base.active_stages) {
          set_dirty_for_bind_map(cmd_buffer, stage,
@@ -757,7 +764,13 @@ void anv_CmdBindPipeline(
          }
       }
 
-      anv_cmd_buffer_flush_pipeline_state(cmd_buffer, old_pipeline, new_pipeline);
+      cmd_buffer->state.gfx.min_sample_shading = new_pipeline->min_sample_shading;
+      cmd_buffer->state.gfx.sample_shading_enable = new_pipeline->sample_shading_enable;
+      cmd_buffer->state.gfx.instance_multiplier = new_pipeline->instance_multiplier;
+      cmd_buffer->state.gfx.primitive_id_index = new_pipeline->primitive_id_index;
+      cmd_buffer->state.gfx.first_vue_slot = new_pipeline->first_vue_slot;
+
+      anv_cmd_buffer_flush_pipeline_hw_state(cmd_buffer, old_pipeline, new_pipeline);
       break;
    }
 
@@ -765,7 +778,6 @@ void anv_CmdBindPipeline(
       if (cmd_buffer->state.rt.base.pipeline == pipeline)
          return;
 
-      cmd_buffer->state.rt.base.pipeline = pipeline;
       cmd_buffer->state.rt.pipeline_dirty = true;
 
       struct anv_ray_tracing_pipeline *rt_pipeline =
@@ -787,6 +799,8 @@ void anv_CmdBindPipeline(
       UNREACHABLE("invalid bind point");
       break;
    }
+
+   state->pipeline = pipeline;
 
    if (pipeline->ray_queries > 0)
       anv_cmd_buffer_set_ray_query_buffer(cmd_buffer, state, pipeline, stages);
@@ -1332,12 +1346,11 @@ struct anv_state
 anv_cmd_buffer_cs_push_constants(struct anv_cmd_buffer *cmd_buffer)
 {
    const struct intel_device_info *devinfo = cmd_buffer->device->info;
-   struct anv_cmd_pipeline_state *pipe_state = &cmd_buffer->state.compute.base;
+   struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   struct anv_cmd_pipeline_state *pipe_state = &comp_state->base;
    struct anv_push_constants *data = &pipe_state->push_constants;
-   struct anv_compute_pipeline *pipeline =
-      anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-   const struct brw_cs_prog_data *cs_prog_data = get_cs_prog_data(pipeline);
-   const struct anv_push_range *range = &pipeline->cs->bind_map.push_ranges[0];
+   const struct brw_cs_prog_data *cs_prog_data = get_cs_prog_data(comp_state);
+   const struct anv_push_range *range = &comp_state->shader->bind_map.push_ranges[0];
 
    const struct intel_cs_dispatch_info dispatch =
       brw_cs_get_dispatch_info(devinfo, cs_prog_data, NULL);

src/intel/vulkan/anv_pipeline.c
@@ -2877,8 +2877,10 @@ anv_graphics_pipeline_emit(struct anv_graphics_pipeline *pipeline,
    pipeline->view_mask = state->rp->view_mask;
 
    if (anv_pipeline_is_primitive(pipeline)) {
-      const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
-      const struct brw_wm_prog_data *wm_prog_data = get_wm_prog_data(pipeline);
+      const struct brw_vs_prog_data *vs_prog_data =
+         get_pipeline_vs_prog_data(pipeline);
+      const struct brw_wm_prog_data *wm_prog_data =
+         get_pipeline_wm_prog_data(pipeline);
 
       /* The total number of vertex elements we need to program. We might need
        * a couple more to implement some of the draw parameters.

src/intel/vulkan/anv_private.h
@@ -4107,6 +4107,26 @@ enum anv_depth_reg_mode {
 struct anv_cmd_graphics_state {
    struct anv_cmd_pipeline_state base;
 
+   /* Shaders bound */
+   struct anv_shader_bin *shaders[ANV_GRAPHICS_SHADER_STAGE_COUNT];
+
+   /* Bitfield of valid entries in the shaders array */
+   VkShaderStageFlags active_stages;
+
+   uint32_t vs_source_hash;
+   uint32_t fs_source_hash;
+
+   /* Pipeline information */
+   uint32_t instance_multiplier;
+
+   bool kill_pixel;
+   bool uses_xfb;
+   bool sample_shading_enable;
+   float min_sample_shading;
+   uint32_t primitive_id_index;
+   uint32_t first_vue_slot;
+
+   /* Render pass information */
    VkRenderingFlags rendering_flags;
    VkRect2D render_area;
    uint32_t layer_count;
@@ -4199,6 +4219,8 @@ struct anv_cmd_graphics_state {
  */
 struct anv_cmd_compute_state {
    struct anv_cmd_pipeline_state base;
+
+   struct anv_shader_bin *shader;
 
    bool pipeline_dirty;
 
@@ -4596,6 +4618,13 @@ anv_cmd_buffer_descriptor_buffer_address(struct anv_cmd_buffer *cmd_buffer,
       return cmd_buffer->device->physical->va.push_descriptor_buffer_pool.addr;
 
    return cmd_buffer->state.descriptor_buffers.address[buffer_index];
+}
+
+static inline bool
+anv_cmd_buffer_has_gfx_stage(struct anv_cmd_buffer *cmd_buffer,
+                             gl_shader_stage stage)
+{
+   return cmd_buffer->state.gfx.shaders[stage] != NULL;
 }
 
 VkResult anv_cmd_buffer_init_batch_bo_chain(struct anv_cmd_buffer *cmd_buffer);
@@ -5278,6 +5307,13 @@ anv_pipeline_is_mesh(const struct anv_graphics_pipeline *pipeline)
 }
 
 static inline bool
+anv_gfx_has_stage(const struct anv_cmd_graphics_state *gfx,
+                  gl_shader_stage stage)
+{
+   return (gfx->active_stages & mesa_to_vk_shader_stage(stage)) != 0;
+}
+
+static inline bool
 anv_gfx_all_color_write_masked(const struct anv_cmd_graphics_state *gfx,
                                const struct vk_dynamic_graphics_state *dyn)
 {
@@ -5310,11 +5346,24 @@ anv_cmd_graphic_state_update_has_uint_rt(struct anv_cmd_graphics_state *state)
 
 #define ANV_DECL_GET_GRAPHICS_PROG_DATA_FUNC(prefix, stage)             \
 static inline const struct brw_##prefix##_prog_data *                   \
-get_##prefix##_prog_data(const struct anv_graphics_pipeline *pipeline)  \
+get_pipeline_##prefix##_prog_data(                                      \
+   const struct anv_graphics_pipeline *pipeline)                        \
 {                                                                       \
    if (anv_pipeline_has_stage(pipeline, stage)) {                       \
       return (const struct brw_##prefix##_prog_data *)                  \
          pipeline->base.shaders[stage]->prog_data;                      \
+   } else {                                                             \
+      return NULL;                                                      \
+   }                                                                    \
+}                                                                       \
+                                                                        \
+static inline const struct brw_##prefix##_prog_data *                   \
+get_gfx_##prefix##_prog_data(                                           \
+   const struct anv_cmd_graphics_state *gfx)                            \
+{                                                                       \
+   if (anv_gfx_has_stage(gfx, stage)) {                                 \
+      return (const struct brw_##prefix##_prog_data *)                  \
+         (gfx)->shaders[stage]->prog_data;                              \
    } else {                                                             \
       return NULL;                                                      \
    }                                                                    \
@@ -5329,21 +5378,21 @@ ANV_DECL_GET_GRAPHICS_PROG_DATA_FUNC(mesh, MESA_SHADER_MESH)
 ANV_DECL_GET_GRAPHICS_PROG_DATA_FUNC(task, MESA_SHADER_TASK)
 
 static inline const struct brw_cs_prog_data *
-get_cs_prog_data(const struct anv_compute_pipeline *pipeline)
+get_cs_prog_data(const struct anv_cmd_compute_state *comp_state)
 {
-   assert(pipeline->cs);
-   return (const struct brw_cs_prog_data *) pipeline->cs->prog_data;
+   assert(comp_state->shader);
+   return (const struct brw_cs_prog_data *) comp_state->shader->prog_data;
 }
 
 static inline const struct brw_vue_prog_data *
 anv_pipeline_get_last_vue_prog_data(const struct anv_graphics_pipeline *pipeline)
 {
    if (anv_pipeline_has_stage(pipeline, MESA_SHADER_GEOMETRY))
-      return &get_gs_prog_data(pipeline)->base;
+      return &get_pipeline_gs_prog_data(pipeline)->base;
    else if (anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL))
-      return &get_tes_prog_data(pipeline)->base;
+      return &get_pipeline_tes_prog_data(pipeline)->base;
    else
-      return &get_vs_prog_data(pipeline)->base;
+      return &get_pipeline_vs_prog_data(pipeline)->base;
 }
 
 VkResult

src/intel/vulkan/genX_cmd_compute.c
@@ -104,11 +104,11 @@ cmd_buffer_flush_compute_state(struct anv_cmd_buffer *cmd_buffer)
 {
    struct anv_device *device = cmd_buffer->device;
    struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   const UNUSED struct intel_device_info *devinfo = cmd_buffer->device->info;
    struct anv_compute_pipeline *pipeline =
       anv_pipeline_to_compute(comp_state->base.pipeline);
-   const UNUSED struct intel_device_info *devinfo = cmd_buffer->device->info;
 
-   assert(pipeline->cs);
+   assert(comp_state->shader);
 
    genX(cmd_buffer_config_l3)(cmd_buffer,
                               pipeline->cs->prog_data->total_shared > 0 ?
@@ -145,7 +145,7 @@ cmd_buffer_flush_compute_state(struct anv_cmd_buffer *cmd_buffer)
       anv_batch_emit_batch(&cmd_buffer->batch, &pipeline->base.batch);
 
 #if GFX_VERx10 >= 125
-      const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+      const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
       genX(cmd_buffer_ensure_cfe_state)(cmd_buffer, prog_data->base.total_scratch);
 #endif
 
@@ -178,7 +178,7 @@ cmd_buffer_flush_compute_state(struct anv_cmd_buffer *cmd_buffer)
          cmd_buffer,
          &cmd_buffer->state.compute.base,
          VK_SHADER_STAGE_COMPUTE_BIT,
-         (const struct anv_shader_bin **)&pipeline->cs, 1);
+         (const struct anv_shader_bin **)&comp_state->shader, 1);
       cmd_buffer->state.descriptors_dirty &= ~VK_SHADER_STAGE_COMPUTE_BIT;
 
 #if GFX_VERx10 < 125
@@ -317,9 +317,8 @@ compute_load_indirect_params(struct anv_cmd_buffer *cmd_buffer,
       const uint32_t mocs = isl_mocs(&cmd_buffer->device->isl_dev, 0, false);
       mi_builder_set_mocs(&b, mocs);
 
-      struct anv_compute_pipeline *pipeline =
-         anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-      const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+      struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+      const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
 
       assert(util_is_power_of_two_or_zero(prog_data->local_size[0]));
       size_x = mi_udiv32_imm(&b, size_x, prog_data->local_size[0]);
@@ -436,7 +435,6 @@ compute_update_async_threads_limit(struct anv_cmd_buffer *cmd_buffer,
 
 static inline void
 emit_indirect_compute_walker(struct anv_cmd_buffer *cmd_buffer,
-                             const struct anv_shader_bin *shader,
                              const struct brw_cs_prog_data *prog_data,
                              struct anv_address indirect_addr)
 {
@@ -483,8 +481,8 @@ emit_indirect_compute_walker(struct anv_cmd_buffer *cmd_buffer,
       .ExecutionMask            = dispatch.right_mask,
       .PostSync.MOCS            = anv_mocs(cmd_buffer->device, NULL, 0),
       .InterfaceDescriptor =
-         get_interface_descriptor_data(cmd_buffer, shader, prog_data,
-                                       &dispatch),
+         get_interface_descriptor_data(cmd_buffer, comp_state->shader,
+                                       prog_data, &dispatch),
       .EmitInlineParameter      = prog_data->uses_inline_data,
       .InlineData               = {
          [ANV_INLINE_PARAM_PUSH_ADDRESS_OFFSET / 4 + 0]   = push_addr64 & 0xffffffff,
@@ -511,7 +509,6 @@ emit_indirect_compute_walker(struct anv_cmd_buffer *cmd_buffer,
 
 static inline void
 emit_compute_walker(struct anv_cmd_buffer *cmd_buffer,
-                    const struct anv_compute_pipeline *pipeline,
                     struct anv_address indirect_addr,
                     const struct brw_cs_prog_data *prog_data,
                     struct intel_cs_dispatch_info dispatch,
@@ -555,10 +552,10 @@ emit_compute_walker(struct anv_cmd_buffer *cmd_buffer,
       .ThreadGroupIDZDimension        = groupCountZ,
       .ExecutionMask                  = dispatch.right_mask,
       .PostSync                       = {
-         .MOCS                        = anv_mocs(pipeline->base.device, NULL, 0),
+         .MOCS                        = anv_mocs(cmd_buffer->device, NULL, 0),
       },
       .InterfaceDescriptor =
-         get_interface_descriptor_data(cmd_buffer, pipeline->cs,
+         get_interface_descriptor_data(cmd_buffer, comp_state->shader,
                                        prog_data, &dispatch),
       .EmitInlineParameter            = prog_data->uses_inline_data,
       .InlineData                     = {
@@ -593,14 +590,14 @@ emit_compute_walker(struct anv_cmd_buffer *cmd_buffer,
 
 static inline void
 emit_gpgpu_walker(struct anv_cmd_buffer *cmd_buffer,
-                  const struct anv_compute_pipeline *pipeline, bool indirect,
+                  bool indirect,
                   const struct brw_cs_prog_data *prog_data,
                   uint32_t groupCountX, uint32_t groupCountY,
                   uint32_t groupCountZ)
 {
    const bool predicate = cmd_buffer->state.conditional_render_enabled;
 
-   const struct intel_device_info *devinfo = pipeline->base.device->info;
+   const struct intel_device_info *devinfo = cmd_buffer->device->info;
    const struct intel_cs_dispatch_info dispatch =
       brw_cs_get_dispatch_info(devinfo, prog_data, NULL);
 
@@ -625,7 +622,6 @@ emit_gpgpu_walker(struct anv_cmd_buffer *cmd_buffer,
 
 static inline void
 emit_cs_walker(struct anv_cmd_buffer *cmd_buffer,
-               const struct anv_compute_pipeline *pipeline,
                const struct brw_cs_prog_data *prog_data,
                struct intel_cs_dispatch_info dispatch,
                struct anv_address indirect_addr,
@@ -650,7 +646,7 @@ emit_cs_walker(struct anv_cmd_buffer *cmd_buffer,
     */
    if (is_indirect && !is_unaligned_size_x &&
        cmd_buffer->device->info->has_indirect_unroll) {
-      emit_indirect_compute_walker(cmd_buffer, pipeline->cs, prog_data,
+      emit_indirect_compute_walker(cmd_buffer, prog_data,
                                    indirect_addr);
       return;
    }
@@ -661,10 +657,10 @@ emit_cs_walker(struct anv_cmd_buffer *cmd_buffer,
             is_unaligned_size_x);
 
 #if GFX_VERx10 >= 125
-   emit_compute_walker(cmd_buffer, pipeline, indirect_addr, prog_data,
+   emit_compute_walker(cmd_buffer, indirect_addr, prog_data,
                        dispatch, groupCountX, groupCountY, groupCountZ);
 #else
-   emit_gpgpu_walker(cmd_buffer, pipeline, is_indirect, prog_data,
+   emit_gpgpu_walker(cmd_buffer, is_indirect, prog_data,
                      groupCountX, groupCountY, groupCountZ);
 #endif
 }
@@ -679,9 +675,8 @@ void genX(CmdDispatchBase)(
     uint32_t                                    groupCountZ)
 {
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
-   struct anv_compute_pipeline *pipeline =
-      anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+   struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
    struct intel_cs_dispatch_info dispatch =
       brw_cs_get_dispatch_info(cmd_buffer->device->info, prog_data, NULL);
 
@@ -709,7 +704,7 @@ void genX(CmdDispatchBase)(
 
    genX(emit_breakpoint)(&cmd_buffer->batch, cmd_buffer->device, true);
 
-   emit_cs_walker(cmd_buffer, pipeline, prog_data, dispatch,
+   emit_cs_walker(cmd_buffer, prog_data, dispatch,
                   ANV_NULL_ADDRESS /* no indirect data */,
                   groupCountX, groupCountY, groupCountZ,
                   false);
@@ -733,9 +728,8 @@ emit_unaligned_cs_walker(
     struct intel_cs_dispatch_info               dispatch)
 {
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
-   struct anv_compute_pipeline *pipeline =
-      anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+   struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
 
    if (anv_batch_has_error(&cmd_buffer->batch))
       return;
@@ -770,7 +764,7 @@ emit_unaligned_cs_walker(
       genX(cmd_emit_conditional_render_predicate)(cmd_buffer);
 
 #if GFX_VERx10 >= 125
-   emit_compute_walker(cmd_buffer, pipeline, ANV_NULL_ADDRESS, prog_data,
+   emit_compute_walker(cmd_buffer, ANV_NULL_ADDRESS, prog_data,
                        dispatch, groupCountX, groupCountY, groupCountZ);
 #endif
 
@@ -798,9 +792,8 @@ genX(cmd_dispatch_unaligned)(
     uint32_t                                    invocations_z)
 {
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
-   struct anv_compute_pipeline *pipeline =
-      anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+   struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
 
    /* Group X can be unaligned for RT dispatches. */
    uint32_t groupCountX = invocations_x / prog_data->local_size[0];
@@ -849,9 +842,8 @@ genX(cmd_buffer_dispatch_indirect)(struct anv_cmd_buffer *cmd_buffer,
                                    struct anv_address indirect_addr,
                                    bool is_unaligned_size_x)
 {
-   struct anv_compute_pipeline *pipeline =
-      anv_pipeline_to_compute(cmd_buffer->state.compute.base.pipeline);
-   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(pipeline);
+   struct anv_cmd_compute_state *comp_state = &cmd_buffer->state.compute;
+   const struct brw_cs_prog_data *prog_data = get_cs_prog_data(comp_state);
    UNUSED struct anv_batch *batch = &cmd_buffer->batch;
    struct intel_cs_dispatch_info dispatch =
       brw_cs_get_dispatch_info(cmd_buffer->device->info, prog_data, NULL);
@@ -876,8 +868,8 @@ genX(cmd_buffer_dispatch_indirect)(struct anv_cmd_buffer *cmd_buffer,
 
    genX(emit_breakpoint)(&cmd_buffer->batch, cmd_buffer->device, true);
 
-   emit_cs_walker(cmd_buffer, pipeline, prog_data, dispatch, indirect_addr, 0,
-                  0, 0, is_unaligned_size_x);
+   emit_cs_walker(cmd_buffer, prog_data, dispatch, indirect_addr,
+                  0, 0, 0, is_unaligned_size_x);
 
    genX(emit_breakpoint)(&cmd_buffer->batch, cmd_buffer->device, false);
 

src/intel/vulkan/genX_cmd_draw_generated_indirect.h
@@ -64,9 +64,8 @@ genX(cmd_buffer_emit_generate_draws)(struct anv_cmd_buffer *cmd_buffer,
    if (push_data_state.map == NULL)
       return ANV_STATE_NULL;
 
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
    const bool use_tbimr = cmd_buffer->state.gfx.dyn_state.use_tbimr;
 
    struct anv_address draw_count_addr;
@@ -80,10 +79,10 @@ genX(cmd_buffer_emit_generate_draws)(struct anv_cmd_buffer *cmd_buffer,
 
    const bool wa_16011107343 =
       intel_needs_workaround(device->info, 16011107343) &&
-      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_CTRL);
+      anv_cmd_buffer_has_gfx_stage(cmd_buffer, MESA_SHADER_TESS_CTRL);
    const bool wa_22018402687 =
       intel_needs_workaround(device->info, 22018402687) &&
-      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL);
+      anv_cmd_buffer_has_gfx_stage(cmd_buffer, MESA_SHADER_TESS_EVAL);
 
    const uint32_t wa_insts_size =
       ((wa_16011107343 ? GENX(3DSTATE_HS_length) : 0) +
@@ -96,6 +95,11 @@ genX(cmd_buffer_emit_generate_draws)(struct anv_cmd_buffer *cmd_buffer,
       anv_cmd_buffer_alloc_temporary_state(cmd_buffer, wa_insts_size, 4) :
       ANV_STATE_NULL;
    UNUSED uint32_t wa_insts_offset = 0;
+
+#if INTEL_WA_16011107343_GFX_VER || INTEL_WA_22018402687_GFX_VER
+   struct anv_graphics_pipeline *pipeline =
+      anv_pipeline_to_graphics(gfx->base.pipeline);
+#endif
 
 #if INTEL_WA_16011107343_GFX_VER
    if (wa_16011107343) {
@@ -145,7 +149,7 @@ genX(cmd_buffer_emit_generate_draws)(struct anv_cmd_buffer *cmd_buffer,
       .draw_base              = item_base,
       .max_draw_count         = max_count,
       .ring_count             = ring_count,
-      .instance_multiplier    = pipeline->instance_multiplier,
+      .instance_multiplier    = gfx->instance_multiplier,
       .draw_count             = anv_address_is_null(count_addr) ? max_count : 0,
       .generated_cmds_addr    = anv_address_physical(generated_cmds_addr),
       .draw_count_addr        = anv_address_physical(draw_count_addr),
@@ -212,9 +216,8 @@ genX(cmd_buffer_get_draw_id_addr)(struct anv_cmd_buffer *cmd_buffer,
 #if GFX_VER >= 11
    return ANV_NULL_ADDRESS;
 #else
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
    if (!vs_prog_data->uses_drawid)
       return ANV_NULL_ADDRESS;
 
@@ -234,9 +237,8 @@ genX(cmd_buffer_get_generated_draw_stride)(struct anv_cmd_buffer *cmd_buffer)
 #if GFX_VER >= 11
    return 4 * GENX(3DPRIMITIVE_EXTENDED_length);
 #else
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
 
    uint32_t len = 0;
 
@@ -303,9 +305,8 @@ genX(cmd_buffer_emit_indirect_generated_draws_inplace)(struct anv_cmd_buffer *cm
          device->physical->va.dynamic_state_pool.size);
    }
 
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
 
    if (vs_prog_data->uses_baseinstance ||
        vs_prog_data->uses_firstvertex) {
@@ -485,9 +486,8 @@ genX(cmd_buffer_emit_indirect_generated_draws_inring)(struct anv_cmd_buffer *cmd
       },
       cmd_buffer->generation.ring_bo->size);
 
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
 
    if (vs_prog_data->uses_baseinstance ||
        vs_prog_data->uses_firstvertex) {

src/intel/vulkan/genX_cmd_draw_helpers.h
@@ -106,9 +106,8 @@ update_dirty_vbs_for_gfx8_vb_flush(struct anv_cmd_buffer *cmd_buffer,
 #if GFX_VER == 9
    const struct vk_dynamic_graphics_state *dyn =
       &cmd_buffer->vk.dynamic_graphics_state;
-   struct anv_graphics_pipeline *pipeline =
-      anv_pipeline_to_graphics(cmd_buffer->state.gfx.base.pipeline);
-   const struct brw_vs_prog_data *vs_prog_data = get_vs_prog_data(pipeline);
+   const struct anv_cmd_graphics_state *gfx = &cmd_buffer->state.gfx;
+   const struct brw_vs_prog_data *vs_prog_data = get_gfx_vs_prog_data(gfx);
 
    uint64_t vb_used = dyn->vi->bindings_valid;
    if (vs_prog_data->uses_firstvertex ||

commit ed048f608fcd4b28ed00c93abf5f7c6fa1cfddcf

anv: fix source hash utrace prints
Right now we're just printing 0 values

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 8b62cf285d ("anv: store gfx/compute bound shaders on command buffer state")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36663>

src/intel/vulkan/anv_cmd_buffer.c
@@ -764,6 +764,9 @@ void anv_CmdBindPipeline(
          }
       }
 
+      cmd_buffer->state.gfx.vs_source_hash = new_pipeline->vs_source_hash;
+      cmd_buffer->state.gfx.fs_source_hash = new_pipeline->fs_source_hash;
+
       cmd_buffer->state.gfx.min_sample_shading = new_pipeline->min_sample_shading;
       cmd_buffer->state.gfx.sample_shading_enable = new_pipeline->sample_shading_enable;
       cmd_buffer->state.gfx.instance_multiplier = new_pipeline->instance_multiplier;