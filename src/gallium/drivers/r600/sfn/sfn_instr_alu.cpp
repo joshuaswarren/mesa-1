joshuaswarren/mesa-1

commit b40aea9ebb39031de882e9668fc8943bddf0e780

r600/sfn: Implement fsat for 64 bit ops
Because a plain mov with the fsat modifier doesn't do a proper 64 bit fsat
we either have to propagate the op as modifier to the instruction that
creates the value, or we add a fake op that applies the fsat op, i.e. we
implement the mov as an add_64 with zero as the second value.

Fixes:  f689894a3d49f62ccf36667e93196a89786de8f5
    r600/sfn: drop use of nir source mods

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23754>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -1373,6 +1373,8 @@ static bool
 emit_alu_f2f32(const nir_alu_instr& alu, Shader& shader);
 static bool
 emit_alu_abs64(const nir_alu_instr& alu, Shader& shader);
+static bool
+emit_alu_fsat64(const nir_alu_instr& alu, Shader& shader);
 
 static bool
 emit_alu_op2(const nir_alu_instr& alu,
@@ -1488,6 +1490,8 @@ AluInstr::from_nir(nir_alu_instr *alu, Shader& shader)
          return emit_alu_mov_64bit(*alu, shader);
       case nir_op_fneg:
          return emit_alu_neg(*alu, shader);
+      case nir_op_fsat:
+         return emit_alu_fsat64(*alu, shader);
       case nir_op_ffract:
          return emit_alu_op1_64bit(*alu, op1_fract_64, shader, true);
       case nir_op_feq32:
@@ -1996,6 +2000,81 @@ emit_alu_abs64(const nir_alu_instr& alu, Shader& shader)
    shader.emit_instruction(ir);
    return true;
 }
+
+static bool
+try_propagat_fsat64(const nir_alu_instr& alu, Shader& shader)
+{
+   auto& value_factory = shader.value_factory();
+   auto src0 = value_factory.src64(alu.src[0], 0, 0);
+   auto reg0 = src0->as_register();
+   if (!reg0)
+      return false;
+
+   if (!reg0->has_flag(Register::ssa))
+      return false;
+
+   if (reg0->parents().size() != 1)
+      return false;
+
+   if (!reg0->uses().empty())
+      return false;
+
+   auto parent = (*reg0->parents().begin())->as_alu();
+   if (!parent)
+      return false;
+
+   auto opinfo = alu_ops.at(parent->opcode());
+   if (!opinfo.can_clamp)
+      return false;
+
+   parent->set_alu_flag(alu_dst_clamp);
+   return true;
+}
+
+
+static bool
+emit_alu_fsat64(const nir_alu_instr& alu, Shader& shader)
+{
+   auto& value_factory = shader.value_factory();
+
+   assert(nir_dest_num_components(alu.dest.dest) == 1);
+
+   if (try_propagat_fsat64(alu, shader)) {
+      auto ir = new AluInstr(op1_mov,
+                             value_factory.dest(alu.dest, 0, pin_chan),
+                             value_factory.src64(alu.src[0], 0, 0),
+                             AluInstr::write);
+      shader.emit_instruction(ir);
+
+      shader.emit_instruction(new AluInstr(op1_mov,
+                                           value_factory.dest(alu.dest, 1, pin_chan),
+                                           value_factory.src64(alu.src[0], 0, 1),
+                              AluInstr::last_write));
+   } else {
+
+      /* dest clamp doesn't work on plain 64 bit move, so add a zero
+       * to apply the modifier */
+
+      auto group = new AluGroup();
+      auto ir = new AluInstr(op2_add_64,
+                             value_factory.dest(alu.dest, 0, pin_chan),
+                             value_factory.src64(alu.src[0], 0, 1),
+                             value_factory.literal(0),
+                             AluInstr::write);
+      ir->set_alu_flag(alu_dst_clamp);
+      group->add_instruction(ir);
+
+      group->add_instruction(new AluInstr(op2_add_64,
+                                          value_factory.dest(alu.dest, 1, pin_chan),
+                                          value_factory.src64(alu.src[0], 0, 0),
+                                          value_factory.literal(0),
+                                          AluInstr::last_write));
+      shader.emit_instruction(group);
+
+   }
+   return true;
+}
+
 
 static bool
 emit_alu_op2_64bit(const nir_alu_instr& alu,

src/gallium/drivers/r600/sfn/sfn_alu_defines.cpp
@@ -129,7 +129,7 @@ const std::map<EAluOp, AluOp> alu_ops = {
    {op2_ashr_int,                  AluOp(2, 0, 0, 0, T, A, A, "ASHR_INT")                 },
    {op2_lshr_int,                  AluOp(2, 0, 0, 0, T, A, A, "LSHR_INT")                 },
    {op2_lshl_int,                  AluOp(2, 0, 0, 0, T, A, A, "LSHL_INT")                 },
-   {op2_mul_64,                    AluOp(2, 1, 0, 1, A, A, A, "MUL_64")                   },
+   {op2_mul_64,                    AluOp(2, 1, 1, 1, A, A, A, "MUL_64")                   },
    {op2_pred_setgt_uint,           AluOp(2, 0, 0, 0, A, A, A, "PRED_SETGT_UINT")          },
    {op2_pred_setge_uint,           AluOp(2, 0, 0, 0, A, A, A, "PRED_SETGE_UINT")          },
    {op2_pred_sete,                 AluOp(2, 1, 0, 0, A, A, A, "PRED_SETE")                },
@@ -200,7 +200,7 @@ const std::map<EAluOp, AluOp> alu_ops = {
    {op2_pred_sete_64,              AluOp(2, 1, 0, 1, V, V, V, "PRED_SETE_64")             },
    {op2_pred_setge_64,             AluOp(2, 1, 0, 1, V, V, V, "PRED_SETGE_64")            },
    {OP2V_MUL_64,                   AluOp(2, 1, 0, 1, V, V, V, "MUL_64")                   },
-   {op2_add_64,                    AluOp(2, 1, 0, 1, V, V, V, "ADD_64")                   },
+   {op2_add_64,                    AluOp(2, 1, 1, 1, V, V, V, "ADD_64")                   },
    {op2_sad_accum_prev_uint,       AluOp(2, 0, 0, 0, V, V, V, "SAD_ACCUM_PREV_UINT")      },
    {op2_dot,                       AluOp(2, 1, 0, 0, V, V, V, "DOT")                      },
    {op1_mul_prev,                  AluOp(2, 1, 0, 0, V, V, V, "MUL_PREV")                 },
@@ -218,7 +218,7 @@ const std::map<EAluOp, AluOp> alu_ops = {
    {op3_bfi_int,                   AluOp(3, 0, 0, 0, V, V, V, "BFI_INT")                  },
    {op3_fma,                       AluOp(3, 1, 1, 0, V, V, V, "FMA")                      },
    {op3_cndne_64,                  AluOp(3, 1, 0, 1, V, V, V, "CNDNE_64")                 },
-   {op3_fma_64,                    AluOp(3, 1, 0, 1, V, V, V, "FMA_64")                   },
+   {op3_fma_64,                    AluOp(3, 1, 1, 1, V, V, V, "FMA_64")                   },
    {op3_lerp_uint,                 AluOp(3, 0, 0, 0, V, V, V, "LERP_UINT")                },
    {op3_bit_align_int,             AluOp(3, 0, 0, 0, V, V, V, "BIT_ALIGN_INT")            },
    {op3_byte_align_int,            AluOp(3, 0, 0, 0, V, V, V, "BYTE_ALIGN_INT")           },

commit d63686dc7b1bc44b8f46a245f62d457e91901eeb

r600/sfn: check number of fsat64 source uses properly
Fixes: b40aea9ebb39031de882e9668fc8943bddf0e780
    r600/sfn: Implement fsat for 64 bit ops

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36585>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -1925,6 +1925,9 @@ emit_alu_abs64(const nir_alu_instr& alu, Shader& shader)
 static bool
 try_propagat_fsat64(const nir_alu_instr& alu, Shader& shader)
 {
+   if (list_length(&alu.src[0].src.ssa->uses) > 1)
+      return false;
+
    auto& value_factory = shader.value_factory();
    auto src0 = value_factory.src64(alu.src[0], 0, 0);
    auto reg0 = src0->as_register();
@@ -1932,12 +1935,6 @@ try_propagat_fsat64(const nir_alu_instr& alu, Shader& shader)
       return false;
 
    if (!reg0->has_flag(Register::ssa))
-      return false;
-
-   if (reg0->parents().size() != 1)
-      return false;
-
-   if (!reg0->uses().empty())
       return false;
 
    auto parent = (*reg0->parents().begin())->as_alu();