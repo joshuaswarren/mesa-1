joshuaswarren/mesa-1

commit 3507fe093f9a69b1d02f02a6fbab379f0417fefa

anv: Use spirv_capabilities for the float64 shader
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Reviewed-by: Iván Briano <ivan.briano@intel.com>
Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28905>

src/intel/vulkan/anv_pipeline_cache.c
@@ -637,14 +637,16 @@ anv_load_fp64_shader(struct anv_device *device)
    if (device->fp64_nir)
       return;
 
+   const struct spirv_capabilities spirv_caps = {
+      .Addresses = true,
+      .Float64 = true,
+      .Int8 = true,
+      .Int16 = true,
+      .Int64 = true,
+   };
+
    struct spirv_to_nir_options spirv_options = {
-      .caps = {
-         .address = true,
-         .float64 = true,
-         .int8 = true,
-         .int16 = true,
-         .int64 = true,
-      },
+      .capabilities = &spirv_caps,
       .environment = NIR_SPIRV_VULKAN,
       .create_library = true
    };

commit d499d928efa8fa9629a202432a3b03833824d147

anv: Set the Shader capability when compiling the FP64 shader
Otherwise the SPIR-V parser prints a warning the first time the driver
is loaded after a fresh compile.

Fixes: 3507fe093f9a ("anv: Use spirv_capabilities for the float64 shader")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36783>

src/intel/vulkan/anv_pipeline_cache.c
@@ -548,6 +548,7 @@ anv_load_fp64_shader(struct anv_device *device)
       .Int8 = true,
       .Int16 = true,
       .Int64 = true,
+      .Shader = true,
    };
 
    struct spirv_to_nir_options spirv_options = {