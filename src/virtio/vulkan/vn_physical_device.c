joshuaswarren/mesa-1

commit 87b4c1a3054e41759633e327c0361f4f2435bb03

venus: support wsi maintenance1 extensions
Venus has long supported creating swapchain image alias via binding. So
below are exposed without extra work needed:
- VK_EXT_surface_maintenance1
- VK_EXT_swapchain_maintenance1

Test: dEQP-VK.wsi.*.maintenance1.*

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33782>

src/virtio/vulkan/vn_physical_device.c
@@ -317,6 +317,7 @@ vn_physical_device_init_features(struct vn_physical_device *physical_dev)
     * See vn_physical_device_get_native_extensions.
     */
    feats->deviceMemoryReport = true;
+   feats->swapchainMaintenance1 = true;
 
    /* Disable unsupported ExtendedDynamicState3Features */
    if (exts->EXT_extended_dynamic_state3) {
@@ -1027,6 +1028,7 @@ vn_physical_device_get_native_extensions(
       exts->KHR_incremental_present = true;
       exts->KHR_swapchain = true;
       exts->KHR_swapchain_mutable_format = true;
+      exts->EXT_swapchain_maintenance1 = true;
    }
 
    /* VK_EXT_pci_bus_info is required by common wsi to decide whether native

src/virtio/vulkan/vn_device.c
@@ -306,6 +306,8 @@ vn_device_fix_create_info(const struct vn_device *dev,
             VK_KHR_SWAPCHAIN_MUTABLE_FORMAT_EXTENSION_NAME;
          block_exts[block_count++] =
             VK_KHR_INCREMENTAL_PRESENT_EXTENSION_NAME;
+         block_exts[block_count++] =
+            VK_EXT_SWAPCHAIN_MAINTENANCE_1_EXTENSION_NAME;
       }
 
       if (app_exts->ANDROID_native_buffer) {

src/virtio/vulkan/vn_instance.c
@@ -38,6 +38,7 @@ static const struct vk_instance_extension_table
       .KHR_get_surface_capabilities2 = true,
       .KHR_surface = true,
       .KHR_surface_protected_capabilities = true,
+      .EXT_surface_maintenance1 = true,
       .EXT_swapchain_colorspace = true,
 #endif
 #ifdef VK_USE_PLATFORM_WAYLAND_KHR

commit 86e04c32f5643b6d5c19eb024853f648d8b3848d

venus: hide swapchainMaintenance1 behind wsi guard
..otherwise would give false alarm on Android.

Fixes: 87b4c1a3054 ("venus: support wsi maintenance1 extensions")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36834>

src/virtio/vulkan/vn_physical_device.c
@@ -410,7 +410,10 @@ vn_physical_device_init_features(struct vn_physical_device *physical_dev)
     * See vn_physical_device_get_native_extensions.
     */
    feats->deviceMemoryReport = true;
+
+#ifdef VN_USE_WSI_PLATFORM
    feats->swapchainMaintenance1 = true;
+#endif
 
    /* Host commands require custom protocol level support. Disable it
     * since it'd be non-trivial to make it performant.