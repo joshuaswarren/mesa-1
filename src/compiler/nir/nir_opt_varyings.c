joshuaswarren/mesa-1

commit 35d4e137a315e1ea7a85c88d2cef0a7132961d8c

nir/opt_varyings: completely exclude mediump from type changes
It broke mediump XFB, which needs the correct type for the up-conversion.

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35315>

src/compiler/nir/nir_opt_varyings.c
@@ -4186,7 +4186,6 @@ relocate_slot(struct linkage_info *linkage, struct scalar_slot *slot,
          }
 
          nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
-         unsigned bit_size = nir_intrinsic_infos[intr->intrinsic].has_dest ? intr->def.bit_size : intr->src[0].ssa->bit_size;
 
          /* Set all types to float to facilitate full IO vectorization.
           * This is skipped only if mediump is not lowered to 16 bits.
@@ -4198,7 +4197,7 @@ relocate_slot(struct linkage_info *linkage, struct scalar_slot *slot,
           * Set nir_shader_compiler_options::lower_mediump_io if you want to
           * lower mediump to 16 bits in the GLSL linker before this pass.
           */
-         if (bit_size != 32 || !sem.medium_precision) {
+         if (!sem.medium_precision) {
             nir_alu_type type = nir_intrinsic_has_src_type(intr) ? nir_intrinsic_src_type(intr) : nir_intrinsic_dest_type(intr);
             type = nir_alu_type_get_type_size(type) | nir_type_float;
 

commit 5a9311983cc1e869797a0fe3bfd8b6413051386b

nir/opt_varyings: fix build with PRINT_RELOCATE_SLOT
Fixes: 35d4e137a31 ("nir/opt_varyings: completely exclude mediump from type changes")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36411>

src/compiler/nir/nir_opt_varyings.c
@@ -4246,6 +4246,8 @@ relocate_slot(struct linkage_info *linkage, struct scalar_slot *slot,
          }
 
 #if PRINT_RELOCATE_SLOT
+         unsigned bit_size = nir_intrinsic_infos[intr->intrinsic].has_dest ? intr->def.bit_size : intr->src[0].ssa->bit_size;
+
          assert(bit_size == 16 || bit_size == 32);
 
          fprintf(stderr, "--- relocating: %s.%c%s%s -> %s.%c%s%s FS_VEC4_TYPE_%s\n",