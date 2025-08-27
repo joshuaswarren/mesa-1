joshuaswarren/mesa-1

commit deec6a36530ac681e407a3961008ff1e1d4baede

brw: move final send lowering up into the IR
Because we do emit the final send message form in code generation, a
lot of emissions look like this :

  add(8)  vgrf0,    u0, 0x100
  mov(1)   a0.1, vgrf0          # emitted by the generator
  send(8)   ...,  a0.1

By moving address register manipulation in the IR, we can get this
down to :

  add(1)  a0.1,   u0, 0x100
  send(8)  ..., a0.1

This reduce register pressure around some send messages by 1 vgrf.

All lost shaders in the below results are fragment SIMD32, due to the
throughput estimator. If turned off, we loose no SIMD32 shaders with
this change.

DG2 results:

  Assassin's Creed Valhalla:
  Totals from 2044 (96.87% of 2110) affected shaders:
  Instrs: 852879 -> 832044 (-2.44%); split: -2.45%, +0.00%
  Subgroup size: 23832 -> 23824 (-0.03%)
  Cycle count: 53345742 -> 52144277 (-2.25%); split: -5.08%, +2.82%
  Spill count: 729 -> 554 (-24.01%); split: -28.40%, +4.39%
  Fill count: 2005 -> 1256 (-37.36%)
  Scratch Memory Size: 25600 -> 19456 (-24.00%); split: -32.00%, +8.00%
  Max live registers: 116765 -> 115058 (-1.46%)
  Max dispatch width: 19152 -> 18872 (-1.46%); split: +0.21%, -1.67%

  Cyberpunk 2077:
  Totals from 1181 (93.43% of 1264) affected shaders:
  Instrs: 667192 -> 663615 (-0.54%); split: -0.55%, +0.01%
  Subgroup size: 13016 -> 13032 (+0.12%)
  Cycle count: 17383539 -> 17986073 (+3.47%); split: -0.93%, +4.39%
  Spill count: 12 -> 8 (-33.33%)
  Fill count: 9 -> 6 (-33.33%)

  Dota2:
  Totals from 173 (11.59% of 1493) affected shaders:
  Cycle count: 274403 -> 280817 (+2.34%); split: -0.01%, +2.34%
  Max live registers: 5787 -> 5779 (-0.14%)
  Max dispatch width: 1344 -> 1152 (-14.29%)

  Hitman3:
  Totals from 5072 (95.39% of 5317) affected shaders:
  Instrs: 2879952 -> 2841804 (-1.32%); split: -1.32%, +0.00%
  Cycle count: 153208505 -> 165860401 (+8.26%); split: -2.22%, +10.48%
  Spill count: 3942 -> 3200 (-18.82%)
  Fill count: 10158 -> 8846 (-12.92%)

src/intel/compiler/brw_opt_address_reg_load.cpp
@@ -1,0 +1,75 @@
+/*
+ * Copyright © 2024 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "brw_fs.h"
+#include "brw_fs_builder.h"
+#include "brw_cfg.h"
+#include "brw_eu.h"
+
+/** @file brw_fs_opt_address_reg_load.cpp
+ *
+ * Turn this sequence :
+ *
+ *    add(8) vgrf64:UD, vgrf63:UD,        192u
+ *    mov(1)   a0.4:UD, vgrf64+0.0<0>:UD
+ *
+ * into :
+ *
+ *    add(1)   a0.4:UD, vgrf63+0.0<0>:UD, 192u
+ */
+
+using namespace brw;
+
+static bool
+opt_address_reg_load_local(fs_visitor &s, bblock_t *block, const brw::def_analysis &defs)
+{
+   bool progress = false;
+
+   foreach_inst_in_block_safe(fs_inst, inst, block) {
+      if (!inst->dst.is_address() || inst->opcode != BRW_OPCODE_MOV)
+         continue;
+
+      fs_inst *src_inst = defs.get(inst->src[0]);
+      if (src_inst == NULL)
+         continue;
+
+      if (src_inst->uses_address_register_implicitly() ||
+          src_inst->sources > 2)
+         continue;
+
+      fs_builder ubld = fs_builder(&s).at(block, inst).exec_all().group(1, 0);
+      brw_reg sources[3];
+      for (unsigned i = 0; i < src_inst->sources; i++) {
+         sources[i] = inst->src[i].file == VGRF ? component(src_inst->src[i], 0) : src_inst->src[i];
+      }
+      ubld.emit(src_inst->opcode, inst->dst, sources, src_inst->sources);
+
+      inst->remove(block);
+
+      progress = true;
+   }
+
+   return progress;
+}
+
+bool
+brw_opt_address_reg_load(fs_visitor &s)
+{
+   bool progress = false;
+   const brw::def_analysis &defs = s.def_analysis.require();
+
+   foreach_block(block, s.cfg) {
+      foreach_inst_in_block_safe(fs_inst, inst, block) {
+         progress = opt_address_reg_load_local(s, block, defs) || progress;
+      }
+   }
+
+   if (progress) {
+      s.cfg->adjust_block_ips();
+      s.invalidate_analysis(DEPENDENCY_INSTRUCTIONS);
+   }
+
+   return progress;
+}

src/intel/compiler/brw_compile_fs.cpp
@@ -627,15 +627,23 @@ brw_emit_repclear_shader(fs_visitor &s)
 
       write = bld.emit(SHADER_OPCODE_SEND);
       write->resize_sources(3);
+
+      /* We can use a headerless message for the first render target */
+      write->header_size = i == 0 ? 0 : 2;
+      write->mlen = 1 + write->header_size;
+
       write->sfid = GFX6_SFID_DATAPORT_RENDER_CACHE;
-      write->src[0] = brw_imm_ud(0);
+      write->src[0] = brw_imm_ud(
+         brw_fb_write_desc(
+            s.devinfo, i,
+            BRW_DATAPORT_RENDER_TARGET_WRITE_SIMD16_SINGLE_SOURCE_REPLICATED,
+            i == key->nr_color_regions - 1, false) |
+         brw_message_desc(s.devinfo, write->mlen,
+                          0 /* rlen */, write->header_size));
       write->src[1] = brw_imm_ud(0);
       write->src[2] = i == 0 ? color_output : header;
       write->check_tdr = true;
       write->send_has_side_effects = true;
-      write->desc = brw_fb_write_desc(s.devinfo, i,
-         BRW_DATAPORT_RENDER_TARGET_WRITE_SIMD16_SINGLE_SOURCE_REPLICATED,
-         i == key->nr_color_regions - 1, false);
 
       /* We can use a headerless message for the first render target */
       write->header_size = i == 0 ? 0 : 2;

src/intel/compiler/brw_eu.h
@@ -1430,7 +1430,6 @@ brw_send_indirect_message(struct brw_codegen *p,
                           struct brw_reg dst,
                           struct brw_reg payload,
                           struct brw_reg desc,
-                          unsigned desc_imm,
                           bool eot);
 
 void
@@ -1440,10 +1439,8 @@ brw_send_indirect_split_message(struct brw_codegen *p,
                                 struct brw_reg payload0,
                                 struct brw_reg payload1,
                                 struct brw_reg desc,
-                                unsigned desc_imm,
                                 struct brw_reg ex_desc,
-                                unsigned ex_desc_imm,
-                                bool ex_desc_scratch,
+                                unsigned ex_mlen,
                                 bool ex_bso,
                                 bool eot);
 

src/intel/compiler/brw_eu_defines.h
@@ -816,6 +816,13 @@ enum ENUM_PACKED gfx10_align1_3src_exec_type {
 #define BRW_THREAD_ATOMIC     1
 #define BRW_THREAD_SWITCH     2
 
+/* Subregister of the address register used for particular purposes */
+enum brw_address_subreg {
+   BRW_ADDRESS_SUBREG_INDIRECT_DESC = 0,
+   BRW_ADDRESS_SUBREG_INDIRECT_EX_DESC = 2,
+   BRW_ADDRESS_SUBREG_INDIRECT_SPILL_DESC = 4,
+};
+
 enum ENUM_PACKED brw_vertical_stride {
    BRW_VERTICAL_STRIDE_0               = 0,
    BRW_VERTICAL_STRIDE_1               = 1,

src/intel/compiler/brw_eu_emit.c
@@ -1438,7 +1438,6 @@ brw_send_indirect_message(struct brw_codegen *p,
                           struct brw_reg dst,
                           struct brw_reg payload,
                           struct brw_reg desc,
-                          unsigned desc_imm,
                           bool eot)
 {
    const struct intel_device_info *devinfo = p->devinfo;
@@ -1451,35 +1450,16 @@ brw_send_indirect_message(struct brw_codegen *p,
    if (desc.file == IMM) {
       send = next_insn(p, BRW_OPCODE_SEND);
       brw_set_src0(p, send, retype(payload, BRW_TYPE_UD));
-      brw_set_desc(p, send, desc.ud | desc_imm);
+      brw_set_desc(p, send, desc.ud);
    } else {
-      const struct tgl_swsb swsb = brw_get_default_swsb(p);
-      struct brw_reg addr = retype(brw_address_reg(0), BRW_TYPE_UD);
-
-      brw_push_insn_state(p);
-      brw_set_default_access_mode(p, BRW_ALIGN_1);
-      brw_set_default_mask_control(p, BRW_MASK_DISABLE);
-      brw_set_default_exec_size(p, BRW_EXECUTE_1);
-      brw_set_default_predicate_control(p, BRW_PREDICATE_NONE);
-      brw_set_default_flag_reg(p, 0, 0);
-      brw_set_default_swsb(p, tgl_swsb_src_dep(swsb));
-
-      /* Load the indirect descriptor to an address register using OR so the
-       * caller can specify additional descriptor bits with the desc_imm
-       * immediate.
-       */
-      brw_OR(p, addr, desc, brw_imm_ud(desc_imm));
-
-      brw_pop_insn_state(p);
-
-      brw_set_default_swsb(p, tgl_swsb_dst_dep(swsb, 1));
+      assert(desc.file == ADDRESS);
+      assert(desc.subnr == 0);
       send = next_insn(p, BRW_OPCODE_SEND);
       brw_set_src0(p, send, retype(payload, BRW_TYPE_UD));
-
       if (devinfo->ver >= 12)
          brw_eu_inst_set_send_sel_reg32_desc(devinfo, send, true);
       else
-         brw_set_src1(p, send, addr);
+         brw_set_src1(p, send, desc);
    }
 
    brw_set_dest(p, send, dst);
@@ -1494,10 +1474,8 @@ brw_send_indirect_split_message(struct brw_codegen *p,
                                 struct brw_reg payload0,
                                 struct brw_reg payload1,
                                 struct brw_reg desc,
-                                unsigned desc_imm,
                                 struct brw_reg ex_desc,
-                                unsigned ex_desc_imm,
-                                bool ex_desc_scratch,
+                                unsigned ex_mlen,
                                 bool ex_bso,
                                 bool eot)
 {
@@ -1507,105 +1485,6 @@ brw_send_indirect_split_message(struct brw_codegen *p,
    dst = retype(dst, BRW_TYPE_UW);
 
    assert(desc.type == BRW_TYPE_UD);
-
-   if (desc.file == IMM) {
-      desc.ud |= desc_imm;
-   } else {
-      const struct tgl_swsb swsb = brw_get_default_swsb(p);
-      struct brw_reg addr = retype(brw_address_reg(0), BRW_TYPE_UD);
-
-      brw_push_insn_state(p);
-      brw_set_default_access_mode(p, BRW_ALIGN_1);
-      brw_set_default_mask_control(p, BRW_MASK_DISABLE);
-      brw_set_default_exec_size(p, BRW_EXECUTE_1);
-      brw_set_default_predicate_control(p, BRW_PREDICATE_NONE);
-      brw_set_default_flag_reg(p, 0, 0);
-      brw_set_default_swsb(p, tgl_swsb_src_dep(swsb));
-
-      /* Load the indirect descriptor to an address register using OR so the
-       * caller can specify additional descriptor bits with the desc_imm
-       * immediate.
-       */
-      brw_OR(p, addr, desc, brw_imm_ud(desc_imm));
-
-      brw_pop_insn_state(p);
-      desc = addr;
-
-      brw_set_default_swsb(p, tgl_swsb_dst_dep(swsb, 1));
-   }
-
-   if (ex_desc.file == IMM &&
-       !ex_desc_scratch &&
-       (devinfo->ver >= 12 ||
-        ((ex_desc.ud | ex_desc_imm) & INTEL_MASK(15, 12)) == 0)) {
-      /* ATS-M PRMs, Volume 2d: Command Reference: Structures,
-       * EU_INSTRUCTION_SEND instruction
-       *
-       *    "ExBSO: Exists If: ([ExDesc.IsReg]==true)"
-       */
-      assert(!ex_bso);
-      ex_desc.ud |= ex_desc_imm;
-   } else {
-      const struct tgl_swsb swsb = brw_get_default_swsb(p);
-      struct brw_reg addr = retype(brw_address_reg(2), BRW_TYPE_UD);
-
-      /* On Xe2+ ExBSO addressing is implicitly enabled for the UGM
-       * shared function.
-       */
-      ex_bso |= (devinfo->ver >= 20 && sfid == GFX12_SFID_UGM);
-
-      brw_push_insn_state(p);
-      brw_set_default_access_mode(p, BRW_ALIGN_1);
-      brw_set_default_mask_control(p, BRW_MASK_DISABLE);
-      brw_set_default_exec_size(p, BRW_EXECUTE_1);
-      brw_set_default_predicate_control(p, BRW_PREDICATE_NONE);
-      brw_set_default_flag_reg(p, 0, 0);
-      brw_set_default_swsb(p, tgl_swsb_src_dep(swsb));
-
-      /* Load the indirect extended descriptor to an address register using OR
-       * so the caller can specify additional descriptor bits with the
-       * desc_imm immediate.
-       *
-       * Even though the instruction dispatcher always pulls the SFID and EOT
-       * fields from the instruction itself, actual external unit which
-       * processes the message gets the SFID and EOT from the extended
-       * descriptor which comes from the address register.  If we don't OR
-       * those two bits in, the external unit may get confused and hang.
-       */
-      unsigned imm_part = ex_bso ? 0 : (ex_desc_imm | sfid | eot << 5);
-
-      if (ex_desc_scratch) {
-         assert(devinfo->verx10 >= 125);
-         brw_AND(p, addr,
-                 retype(brw_vec1_grf(0, 5), BRW_TYPE_UD),
-                 brw_imm_ud(INTEL_MASK(31, 10)));
-
-         if (devinfo->ver >= 20 && sfid == GFX12_SFID_UGM) {
-            const unsigned ex_mlen = brw_message_ex_desc_ex_mlen(devinfo, ex_desc_imm);
-            assert(ex_desc_imm == brw_message_ex_desc(devinfo, ex_mlen));
-            brw_SHR(p, addr, addr, brw_imm_ud(4));
-         } else {
-            /* Or the scratch surface offset together with the immediate part
-             * of the extended descriptor.
-             */
-            brw_OR(p, addr, addr, brw_imm_ud(imm_part));
-         }
-
-      } else if (ex_desc.file == IMM) {
-         /* ex_desc bits 15:12 don't exist in the instruction encoding prior
-          * to Gfx12, so we may have fallen back to an indirect extended
-          * descriptor.
-          */
-         brw_MOV(p, addr, brw_imm_ud(ex_desc.ud | imm_part));
-      } else {
-         brw_OR(p, addr, ex_desc, brw_imm_ud(imm_part));
-      }
-
-      brw_pop_insn_state(p);
-      ex_desc = addr;
-
-      brw_set_default_swsb(p, tgl_swsb_dst_dep(swsb, 1));
-   }
 
    send = next_insn(p, devinfo->ver >= 12 ? BRW_OPCODE_SEND : BRW_OPCODE_SENDS);
    brw_set_dest(p, send, dst);
@@ -1630,10 +1509,8 @@ brw_send_indirect_split_message(struct brw_codegen *p,
       brw_eu_inst_set_send_sel_reg32_ex_desc(devinfo, send, 1);
       brw_eu_inst_set_send_ex_desc_ia_subreg_nr(devinfo, send, phys_subnr(devinfo, ex_desc) >> 2);
 
-      if (devinfo->ver >= 20 && sfid == GFX12_SFID_UGM) {
-         const unsigned ex_mlen = brw_message_ex_desc_ex_mlen(devinfo, ex_desc_imm);
+      if (devinfo->ver >= 20 && sfid == GFX12_SFID_UGM)
          brw_eu_inst_set_bits(send, 103, 99, ex_mlen / reg_unit(devinfo));
-      }
    }
 
    if (ex_bso) {
@@ -1644,7 +1521,7 @@ brw_send_indirect_split_message(struct brw_codegen *p,
        */
       if (devinfo->ver < 20 || sfid != GFX12_SFID_UGM)
          brw_eu_inst_set_send_ex_bso(devinfo, send, true);
-      brw_eu_inst_set_send_src1_len(devinfo, send, GET_BITS(ex_desc_imm, 10, 6));
+      brw_eu_inst_set_send_src1_len(devinfo, send, ex_mlen / reg_unit(devinfo));
    }
    brw_eu_inst_set_sfid(devinfo, send, sfid);
    brw_eu_inst_set_eot(devinfo, send, eot);

src/intel/compiler/brw_fs.cpp
@@ -958,18 +958,24 @@ fs_visitor::assign_curb_setup()
          fs_inst *send = ubld.emit(SHADER_OPCODE_SEND, dest, srcs, 4);
 
          send->sfid = GFX12_SFID_UGM;
-         send->desc = lsc_msg_desc(devinfo, LSC_OP_LOAD,
-                                   LSC_ADDR_SURFTYPE_FLAT,
-                                   LSC_ADDR_SIZE_A32,
-                                   LSC_DATA_SIZE_D32,
-                                   num_regs * 8 /* num_channels */,
-                                   true /* transpose */,
-                                   LSC_CACHE(devinfo, LOAD, L1STATE_L3MOCS));
+         uint32_t desc = lsc_msg_desc(devinfo, LSC_OP_LOAD,
+                                      LSC_ADDR_SURFTYPE_FLAT,
+                                      LSC_ADDR_SIZE_A32,
+                                      LSC_DATA_SIZE_D32,
+                                      num_regs * 8 /* num_channels */,
+                                      true /* transpose */,
+                                      LSC_CACHE(devinfo, LOAD, L1STATE_L3MOCS));
          send->header_size = 0;
          send->mlen = lsc_msg_addr_len(devinfo, LSC_ADDR_SIZE_A32, 1);
          send->size_written =
             lsc_msg_dest_len(devinfo, LSC_DATA_SIZE_D32, num_regs * 8) * REG_SIZE;
          send->send_is_volatile = true;
+
+         send->src[0] = brw_imm_ud(desc |
+                                   brw_message_desc(devinfo,
+                                                    send->mlen,
+                                                    send->size_written / REG_SIZE,
+                                                    send->header_size));
 
          i += num_regs;
       }

src/intel/compiler/brw_fs.h
@@ -635,6 +635,7 @@ bool brw_lower_pack(fs_visitor &s);
 bool brw_lower_regioning(fs_visitor &s);
 bool brw_lower_scalar_fp64_MAD(fs_visitor &s);
 bool brw_lower_scoreboard(fs_visitor &s);
+bool brw_lower_send_descriptors(fs_visitor &s);
 bool brw_lower_sends_overlapping_payload(fs_visitor &s);
 bool brw_lower_simd_width(fs_visitor &s);
 bool brw_lower_sub_sat(fs_visitor &s);
@@ -642,6 +643,7 @@ bool brw_lower_subgroup_ops(fs_visitor &s);
 bool brw_lower_uniform_pull_constant_loads(fs_visitor &s);
 void brw_lower_vgrfs_to_fixed_grfs(fs_visitor &s);
 
+bool brw_opt_address_reg_load(fs_visitor &s);
 bool brw_opt_algebraic(fs_visitor &s);
 bool brw_opt_bank_conflicts(fs_visitor &s);
 bool brw_opt_cmod_propagation(fs_visitor &s);

src/intel/compiler/brw_fs_generator.cpp
@@ -167,31 +167,20 @@ fs_generator::generate_send(fs_inst *inst,
                             struct brw_reg payload,
                             struct brw_reg payload2)
 {
-   const unsigned rlen = inst->dst.is_null() ? 0 : inst->size_written / REG_SIZE;
-
-   uint32_t desc_imm = inst->desc |
-      brw_message_desc(devinfo, inst->mlen, rlen, inst->header_size);
-
-   uint32_t ex_desc_imm = inst->ex_desc |
-      brw_message_ex_desc(devinfo, inst->ex_mlen);
-
-   if (ex_desc.file != IMM || ex_desc.ud || ex_desc_imm ||
-       inst->send_ex_desc_scratch) {
+   if (ex_desc.file == IMM && ex_desc.ud == 0) {
+      brw_send_indirect_message(p, inst->sfid, dst, payload, desc, inst->eot);
+      if (inst->check_tdr)
+         brw_eu_inst_set_opcode(p->isa, brw_last_inst, BRW_OPCODE_SENDC);
+   } else {
       /* If we have any sort of extended descriptor, then we need SENDS.  This
        * also covers the dual-payload case because ex_mlen goes in ex_desc.
        */
       brw_send_indirect_split_message(p, inst->sfid, dst, payload, payload2,
-                                      desc, desc_imm, ex_desc, ex_desc_imm,
-                                      inst->send_ex_desc_scratch,
+                                      desc, ex_desc, inst->ex_mlen,
                                       inst->send_ex_bso, inst->eot);
       if (inst->check_tdr)
          brw_eu_inst_set_opcode(p->isa, brw_last_inst,
                              devinfo->ver >= 12 ? BRW_OPCODE_SENDC : BRW_OPCODE_SENDSC);
-   } else {
-      brw_send_indirect_message(p, inst->sfid, dst, payload, desc, desc_imm,
-                                   inst->eot);
-      if (inst->check_tdr)
-         brw_eu_inst_set_opcode(p->isa, brw_last_inst, BRW_OPCODE_SENDC);
    }
 }
 

commit 7e65d367ff25358f2e9a19a12c1017697dc39706

brw: Fix checking sources of wrong instruction in opt_address_reg_load
Fixes: deec6a36530 ("brw: move final send lowering up into the IR")
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37019>

src/intel/compiler/brw_opt_address_reg_load.cpp
@@ -40,7 +40,8 @@ opt_address_reg_load_local(brw_shader &s, bblock_t *block, const brw_def_analysi
       brw_builder ubld = brw_builder(&s).before(inst).uniform();
       brw_reg sources[3];
       for (unsigned i = 0; i < src_inst->sources; i++) {
-         sources[i] = inst->src[i].file == VGRF ? component(src_inst->src[i], 0) : src_inst->src[i];
+         const brw_reg src = src_inst->src[i];
+         sources[i] = src.file == VGRF ? component(src, 0) : src;
       }
       ubld.emit(src_inst->opcode, inst->dst, sources, src_inst->sources);
 