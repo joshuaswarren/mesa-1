joshuaswarren/mesa-1

commit 6cdd45f8bb935b9b08ea17936f10f77b5d40d52b

vulkan/wsi: Only test for dma-buf sync file support once
Instead of each helper having a VK_ERROR_FEATURE_NOT_PRESENT fast-reject
path, drop those paths and check at the top of each caller.  This
ensures that we do the check once per wsi_device, and only on a known
test dma-buf and that any subsequent fails turn into fails rather than
silently turning off explicit/implicit sync in potentially inconsistent
ways.

Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36816>

src/vulkan/wsi/wsi_common_drm.c
@@ -47,11 +47,6 @@
 static VkResult
 wsi_dma_buf_export_sync_file(int dma_buf_fd, int *sync_file_fd)
 {
-   /* Don't keep trying an IOCTL that doesn't exist. */
-   static bool no_dma_buf_sync_file = false;
-   if (no_dma_buf_sync_file)
-      return VK_ERROR_FEATURE_NOT_PRESENT;
-
    struct dma_buf_export_sync_file export = {
       .flags = DMA_BUF_SYNC_RW,
       .fd = -1,
@@ -59,7 +54,6 @@ wsi_dma_buf_export_sync_file(int dma_buf_fd, int *sync_file_fd)
    int ret = drmIoctl(dma_buf_fd, DMA_BUF_IOCTL_EXPORT_SYNC_FILE, &export);
    if (ret) {
       if (errno == ENOTTY || errno == EBADF || errno == ENOSYS) {
-         no_dma_buf_sync_file = true;
          return VK_ERROR_FEATURE_NOT_PRESENT;
       } else {
          mesa_loge("MESA: failed to export sync file '%s'", strerror(errno));
@@ -75,11 +69,6 @@ wsi_dma_buf_export_sync_file(int dma_buf_fd, int *sync_file_fd)
 static VkResult
 wsi_dma_buf_import_sync_file(int dma_buf_fd, int sync_file_fd)
 {
-   /* Don't keep trying an IOCTL that doesn't exist. */
-   static bool no_dma_buf_sync_file = false;
-   if (no_dma_buf_sync_file)
-      return VK_ERROR_FEATURE_NOT_PRESENT;
-
    struct dma_buf_import_sync_file import = {
       .flags = DMA_BUF_SYNC_RW,
       .fd = sync_file_fd,
@@ -87,7 +76,6 @@ wsi_dma_buf_import_sync_file(int dma_buf_fd, int sync_file_fd)
    int ret = drmIoctl(dma_buf_fd, DMA_BUF_IOCTL_IMPORT_SYNC_FILE, &import);
    if (ret) {
       if (errno == ENOTTY || errno == EBADF || errno == ENOSYS) {
-         no_dma_buf_sync_file = true;
          return VK_ERROR_FEATURE_NOT_PRESENT;
       } else {
          mesa_loge("MESA: failed to import sync file '%s'", strerror(errno));
@@ -234,6 +222,12 @@ wsi_create_sync_for_dma_buf_wait(const struct wsi_swapchain *chain,
 {
    VK_FROM_HANDLE(vk_device, device, chain->device);
    VkResult result;
+
+   /* First, check if we even have the feature */
+   result = wsi_drm_check_dma_buf_sync_file_import_export(chain->wsi,
+                                                          chain->device);
+   if (result != VK_SUCCESS)
+      return result;
 
    const struct vk_sync_type *sync_type =
       get_sync_file_sync_type(device, req_features);

commit 150701c2efed69bc59b73c05bd5fb4fdaa05b2f5

vulkan/wsi: Move a couple of dma-buf sync checks
In 6cdd45f8bb93 ("vulkan/wsi: Only test for dma-buf sync file support
once"), I moved the dma-buf sync file import/export check earlier.  This
is fine for hardware implementations where we have real dma-buf
import/export but it broke lavapipe because the check itself ignored
whether or not we actually have dma-buf import/export.  Add a couple
more checks to wsi_drm_check_dma_buf_sync_file_import_export() so it's
safe even for SW drivers.  Also, in wsi_create_sync_for_dma_buf_wait(),
check if we actually have a dma-buf.

Fixes: 6cdd45f8bb93 ("vulkan/wsi: Only test for dma-buf sync file support once")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37033>

src/vulkan/wsi/wsi_common_drm.c
@@ -102,6 +102,23 @@ wsi_drm_check_dma_buf_sync_file_import_export(const struct wsi_device *wsi,
    if (likely(cached_result != 0))
       return (VkResult) (cached_result + 1);
 
+   /* If we don't have dma-buf export, we don't have dma-buf sync, either */
+   VkResult result;
+   if (wsi->GetMemoryFdKHR == NULL) {
+      result = VK_ERROR_FEATURE_NOT_PRESENT;
+      goto out;
+   }
+
+   /* If semaphores don't support sync FD or if the kernel doesn't support
+    * sync file import/export, then WSI will fall back to requesting BO-based
+    * implicit sync from the driver.
+    */
+   if (!(wsi->semaphore_export_handle_types &
+         VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT)) {
+      result = VK_ERROR_FEATURE_NOT_PRESENT;
+      goto out;
+   }
+
    struct VkExportMemoryAllocateInfo export_info = {
        .sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO,
        .handleTypes = VK_EXTERNAL_MEMORY_HANDLE_TYPE_DMA_BUF_BIT_EXT,
@@ -110,7 +127,6 @@ wsi_drm_check_dma_buf_sync_file_import_export(const struct wsi_device *wsi,
        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
        .allocationSize = 4096,
        .pNext = (void *)&export_info};
-   VkResult result;
    VkDeviceMemory mem;
    result = wsi->AllocateMemory(device, &mem_info, NULL, &mem);
    if (result != VK_SUCCESS) {
@@ -222,6 +238,9 @@ wsi_create_sync_for_dma_buf_wait(const struct wsi_swapchain *chain,
 {
    VK_FROM_HANDLE(vk_device, device, chain->device);
    VkResult result;
+
+   if (image->dma_buf_fd < 0)
+      return VK_ERROR_FEATURE_NOT_PRESENT;
 
    /* First, check if we even have the feature */
    result = wsi_drm_check_dma_buf_sync_file_import_export(chain->wsi,
@@ -354,14 +373,6 @@ wsi_drm_init_swapchain_implicit_sync(struct wsi_swapchain *chain)
     * window system protocol.
     */
    if (chain->image_info.explicit_sync)
-      return VK_SUCCESS;
-
-   /* If semaphores don't support sync FD or if the kernel doesn't support
-    * sync file import/export, then WSI will fall back to requesting BO-based
-    * implicit sync from the driver.
-    */
-   if (!(chain->wsi->semaphore_export_handle_types &
-         VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT))
       return VK_SUCCESS;
 
    VkResult result =