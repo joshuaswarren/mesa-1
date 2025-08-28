joshuaswarren/mesa-1

commit beb29733e893c0f3b8a1e265a2fed01c9865f747

nvk: Implement VK_EXT/KHR_buffer_device_address
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

src/nouveau/vulkan/nvk_device_memory.c
@@ -301,3 +301,13 @@ nvk_GetMemoryFdKHR(VkDevice _device,
       return vk_error(device, VK_ERROR_FEATURE_NOT_PRESENT);
    }
 }
+
+VKAPI_ATTR uint64_t VKAPI_CALL
+nvk_GetDeviceMemoryOpaqueCaptureAddress(
+   UNUSED VkDevice device,
+   const VkDeviceMemoryOpaqueCaptureAddressInfo* pInfo)
+{
+   VK_FROM_HANDLE(nvk_device_memory, mem, pInfo->memory);
+
+   return mem->bo->offset;
+}

src/nouveau/vulkan/nvk_buffer.c
@@ -79,3 +79,21 @@ nvk_BindBufferMemory2(VkDevice _device,
    }
    return VK_SUCCESS;
 }
+
+VKAPI_ATTR VkDeviceAddress VKAPI_CALL
+nvk_GetBufferDeviceAddress(UNUSED VkDevice device,
+                           const VkBufferDeviceAddressInfo *pInfo)
+{
+   VK_FROM_HANDLE(nvk_buffer, buffer, pInfo->buffer);
+
+   return nvk_buffer_address(buffer, 0);
+}
+
+VKAPI_ATTR uint64_t VKAPI_CALL
+nvk_GetBufferOpaqueCaptureAddress(UNUSED VkDevice device,
+                                  const VkBufferDeviceAddressInfo *pInfo)
+{
+   VK_FROM_HANDLE(nvk_buffer, buffer, pInfo->buffer);
+
+   return nvk_buffer_address(buffer, 0);
+}

src/nouveau/vulkan/nvk_physical_device.c
@@ -78,6 +78,9 @@ nvk_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
       .imagelessFramebuffer = true,
       .separateDepthStencilLayouts = true,
       .hostQueryReset = true,
+      .bufferDeviceAddress = true,
+      .bufferDeviceAddressCaptureReplay = false,
+      .bufferDeviceAddressMultiDevice = false,
    };
 
    VkPhysicalDeviceVulkan13Features core_1_3 = {
@@ -107,6 +110,13 @@ nvk_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
          VkPhysicalDeviceBorderColorSwizzleFeaturesEXT *f = (void *)ext;
          f->borderColorSwizzle = true;
          f->borderColorSwizzleFromImage = false;
+         break;
+      }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_BUFFER_DEVICE_ADDRESS_FEATURES_EXT: {
+         VkPhysicalDeviceBufferDeviceAddressFeaturesEXT *f = (void *)ext;
+         f->bufferDeviceAddress = true;
+         f->bufferDeviceAddressCaptureReplay = false;
+         f->bufferDeviceAddressMultiDevice = false;
          break;
       }
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_CUSTOM_BORDER_COLOR_FEATURES_EXT: {
@@ -349,6 +359,7 @@ nvk_get_device_extensions(const struct nvk_physical_device *device,
                           struct vk_device_extension_table *ext)
 {
    *ext = (struct vk_device_extension_table) {
+      .KHR_buffer_device_address = true,
       .KHR_copy_commands2 = true,
       .KHR_create_renderpass2 = true,
       .KHR_dedicated_allocation = true,
@@ -369,6 +380,7 @@ nvk_get_device_extensions(const struct nvk_physical_device *device,
 #endif
       .KHR_variable_pointers = true,
       .EXT_border_color_swizzle = true,
+      .EXT_buffer_device_address = true,
       .EXT_custom_border_color = true,
       .EXT_extended_dynamic_state = true,
       .EXT_extended_dynamic_state2 = true,

src/nouveau/vulkan/nvk_shader.c
@@ -61,8 +61,10 @@ nvk_physical_device_spirv_options(const struct nvk_physical_device *pdevice,
    return (struct spirv_to_nir_options) {
       .caps = {
          .image_write_without_format = true,
+         .physical_storage_buffer_address = true,
       },
       .ssbo_addr_format = nvk_buffer_addr_format(rs->storage_buffers),
+      .phys_ssbo_addr_format = nir_address_format_64bit_global,
       .ubo_addr_format = nvk_buffer_addr_format(rs->uniform_buffers),
       .shared_addr_format = nir_address_format_32bit_offset,
    };
@@ -237,6 +239,8 @@ nvk_lower_nir(struct nvk_device *device, nir_shader *nir,
             nir_address_format_32bit_offset);
 
    NIR_PASS(_, nir, nvk_nir_lower_descriptors, rs, layout);
+   NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_global,
+            nir_address_format_64bit_global);
    NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_ssbo,
             nvk_buffer_addr_format(rs->storage_buffers));
    NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_ubo,

commit 521bde27279f70a74ac0465c65004e030c5a3a8c

nvk: Return 0 for opaque memory capture replay.
If implementation does not actually replay the VA, it must return 0
to not violate:

"If the memory object was allocated with a non-zero value of
opaqueCaptureAddress, the return value must be the same address."

Fixes RenderDoc capture replay, which asserts on the this spec rule
being followed.

Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>
Fixes: beb29733 ("nvk: Implement VK_EXT/KHR_buffer_device_address")
Reviewed-by: Mohamed Ahmed <mohamedahmedegypt2001@gmail.com>
Closes #13784

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37047>

src/nouveau/vulkan/nvk_device_memory.c
@@ -433,7 +433,6 @@ nvk_GetDeviceMemoryOpaqueCaptureAddress(
    UNUSED VkDevice device,
    const VkDeviceMemoryOpaqueCaptureAddressInfo* pInfo)
 {
-   VK_FROM_HANDLE(nvk_device_memory, mem, pInfo->memory);
-
-   return mem->mem->va->addr;
+   /* Addresses are replayed at buffer and image creation, not memory. */
+   return 0;
 }