joshuaswarren/mesa-1

commit dcc4bfdb599c4f4384b568a58fe78d49eae52a5a

tu: Align BO size to page size
The kernel was rounding the size up for us, but it doesn't like a
non-aligned map size, so just sanitize the size here.

tu_cs was relying on the size not being rounded to keep the maximum size
2^20-1 or less, so fix that by using the initial unrounded size.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32533>

src/freedreno/vulkan/tu_cs.cc
@@ -176,7 +176,7 @@ tu_cs_add_bo(struct tu_cs *cs, uint32_t size)
    bos->bos[bos->bo_count++] = new_bo;
 
    cs->start = cs->cur = cs->reserved_end = (uint32_t *) new_bo->map;
-   cs->end = cs->start + new_bo->size / sizeof(uint32_t);
+   cs->end = cs->start + size;
 
    return VK_SUCCESS;
 }

src/freedreno/vulkan/tu_knl.cc
@@ -40,6 +40,8 @@ tu_bo_init_new_explicit_iova(struct tu_device *dev,
    MESA_TRACE_FUNC();
    struct tu_instance *instance = dev->physical_device->instance;
 
+   size = align64(size, os_page_size);
+
    VkResult result =
       dev->instance->knl->bo_init(dev, base, out_bo, size, client_iova,
                                   mem_property, flags, name);
@@ -65,6 +67,7 @@ tu_bo_init_dmabuf(struct tu_device *dev,
                   uint64_t size,
                   int fd)
 {
+   size = align64(size, os_page_size);
    VkResult result = dev->instance->knl->bo_init_dmabuf(dev, bo, size, fd);
    if (result != VK_SUCCESS)
       return result;

commit 19458a0953e514b16af85e1c6ecf577e135589cb

tu: Fix the lack of IB size sanitization in several cases in tu_cs
A BO can be larger than the requested size due to page-size alignment.
Sanitize the IB size derived from the BO at all call sites that create
IBs for CS, so we never exceed the HW limit.

Was found when replaying the following capture:
 Sid Meiers Civilization VI_289070 _dx11_World Map_ultra_720p.rdc

Fixes: dcc4bfdb599 ("tu: Align BO size to page size")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37082>

src/freedreno/vulkan/tu_cs.cc
@@ -9,6 +9,15 @@
 #include "tu_rmv.h"
 #include "tu_suballoc.h"
 
+/* There is a limit to IB size supported by HW,
+ * which appears to be 0x0fffff.
+ */
+inline uint32_t
+tu_sanitize_ib_size(uint32_t size)
+{
+   return MIN2(size, 0x0fffff);
+}
+
 /**
  * Initialize a command stream.
  */
@@ -300,7 +309,8 @@ tu_cs_set_writeable(struct tu_cs *cs, bool writeable)
       cs->start = cs->cur = cs->reserved_end = new_bos->start;
       if (new_bos->bo_count) {
          struct tu_bo *bo = new_bos->bos[new_bos->bo_count - 1];
-         cs->end = (uint32_t *)bo->map + bo->size / sizeof(uint32_t);
+         cs->end = (uint32_t *) bo->map +
+                   tu_sanitize_ib_size(bo->size / sizeof(uint32_t));
       } else {
          cs->end = NULL;
       }
@@ -468,10 +478,8 @@ tu_cs_reserve_space(struct tu_cs *cs, uint32_t reserved_size)
          tu_cs_emit(cs, RENDER_MODE_CP_COND_REG_EXEC_1_DWORDS(0));
       }
 
-      /* double the size for the next bo, also there is an upper
-       * bound on IB size, which appears to be 0x0fffff
-       */
-      new_size = MIN2(new_size << 1, 0x0fffff);
+      /* Double the size for the next bo. */
+      new_size = tu_sanitize_ib_size(new_size << 1);
       if (cs->next_bo_size < new_size)
          cs->next_bo_size = new_size;
    }
@@ -518,7 +526,8 @@ tu_cs_reset(struct tu_cs *cs)
       cs->read_only.bo_count = 1;
 
       cs->start = cs->cur = cs->reserved_end = (uint32_t *) cs->read_only.bos[0]->map;
-      cs->end = cs->start + cs->read_only.bos[0]->size / sizeof(uint32_t);
+      cs->end = cs->start + tu_sanitize_ib_size(cs->read_only.bos[0]->size /
+                                                sizeof(uint32_t));
    }
 
    if (cs->read_write.bo_count) {