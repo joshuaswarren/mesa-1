joshuaswarren/mesa-1

commit b9d92f58246185ed2643565847480707197e9a2d

freedreno/registers: De-open-code some offsets
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37009>

src/freedreno/common/freedreno_devices.py
@@ -1350,8 +1350,9 @@ add_gpus([
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8899,   0x00000000],
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
 
-            [0x930a, 0],
-            [0x960a, 1],
+            [A6XXRegs.REG_A7XX_VPC_UNKNOWN_930A, 0],
+            [A6XXRegs.REG_A7XX_PC_UNKNOWN_9EB6, 0],
+
             [A6XXRegs.REG_A7XX_SP_PS_OUTPUT_CONST_CNTL, 0],
             [A6XXRegs.REG_A7XX_SP_PS_OUTPUT_CONST_MASK, 0],
         ],

src/freedreno/registers/adreno/a6xx.xml
@@ -2186,6 +2186,9 @@ by a particular renderpass/blit.
 		<bitfield name="SIZE_GMEM" low="0" high="31"/>
 	</reg32>
 
+	<reg32 offset="0x930a" name="VPC_UNKNOWN_930A" variants="A7XX"/>
+	<reg32 offset="0x9eb6" name="PC_UNKNOWN_9EB6" variants="A7XX"/>
+
 	<!-- 0x9307-0x95ff invalid -->
 
 	<!-- TODO: 0x9600-0x97ff range -->

commit 07e7afdbaad745fe8eb61a005449d89afe844c73

freedreno/a750: Fix typo in recent magic regs change
Caused GPU hangs.

Fixes: b9d92f58246 ("freedreno/registers: De-open-code some offsets")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37079>

src/freedreno/common/freedreno_devices.py
@@ -1351,7 +1351,7 @@ add_gpus([
             [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
 
             [A6XXRegs.REG_A7XX_VPC_UNKNOWN_930A, 0],
-            [A6XXRegs.REG_A7XX_PC_UNKNOWN_9EB6, 0],
+            [A6XXRegs.REG_A7XX_VPC_FLATSHADE_MODE_CNTL, 1],
 
             [A6XXRegs.REG_A7XX_SP_PS_OUTPUT_CONST_CNTL, 0],
             [A6XXRegs.REG_A7XX_SP_PS_OUTPUT_CONST_MASK, 0],

src/freedreno/registers/adreno/a6xx.xml
@@ -2261,7 +2261,6 @@ by a particular renderpass/blit.
 	<reg32 offset="0x9b09" name="PC_ATTR_BUF_GMEM_SIZE" variants="A7XX" type="uint" usage="rp_blit"/>
 
 	<reg32 offset="0x930a" name="VPC_UNKNOWN_930A" variants="A7XX"/>
-	<reg32 offset="0x9eb6" name="PC_UNKNOWN_9EB6" variants="A7XX"/>
 
 	<reg32 offset="0x960a" name="VPC_FLATSHADE_MODE_CNTL" variants="A7XX"/>
 