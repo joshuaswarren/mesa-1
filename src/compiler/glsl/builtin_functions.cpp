joshuaswarren/mesa-1

commit bf94eb48d65c147a5d1ea9081404550394e81d4e

mesa: Add support for GL_EXT_shader_clock
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35630>

src/compiler/glsl/builtin_functions.cpp
@@ -720,13 +720,15 @@ supports_nv_fragment_shader_interlock(const _mesa_glsl_parse_state *state)
 static bool
 shader_clock(const _mesa_glsl_parse_state *state)
 {
-   return state->ARB_shader_clock_enable;
+   return state->ARB_shader_clock_enable ||
+          state->EXT_shader_clock_enable;
 }
 
 static bool
 shader_clock_int64(const _mesa_glsl_parse_state *state)
 {
-   return state->ARB_shader_clock_enable &&
+   return (state->ARB_shader_clock_enable ||
+           state->EXT_shader_clock_enable) &&
           (state->ARB_gpu_shader_int64_enable ||
            state->AMD_gpu_shader_int64_enable);
 }

src/compiler/glsl/glsl_parser_extras.cpp
@@ -821,6 +821,7 @@ static const _mesa_glsl_extension _mesa_glsl_supported_extensions[] = {
    EXT_AEP(EXT_gpu_shader5),
    EXT_AEP(EXT_primitive_bounding_box),
    EXT(EXT_separate_shader_objects),
+   EXT(EXT_shader_clock),
    EXT(EXT_shader_framebuffer_fetch),
    EXT(EXT_shader_framebuffer_fetch_non_coherent),
    EXT(EXT_shader_group_vote),

src/compiler/glsl/glsl_parser_extras.h
@@ -880,6 +880,8 @@ struct _mesa_glsl_parse_state {
    bool EXT_primitive_bounding_box_warn;
    bool EXT_separate_shader_objects_enable;
    bool EXT_separate_shader_objects_warn;
+   bool EXT_shader_clock_enable;
+   bool EXT_shader_clock_warn;
    bool EXT_shader_framebuffer_fetch_enable;
    bool EXT_shader_framebuffer_fetch_warn;
    bool EXT_shader_framebuffer_fetch_non_coherent_enable;

src/mesa/main/extensions_table.h
@@ -294,6 +294,7 @@ EXT(EXT_semaphore_fd                        , EXT_semaphore_fd                  
 EXT(EXT_semaphore_win32                     , EXT_semaphore_win32                    , GLL, GLC,  x , ES2, 2017)
 EXT(EXT_separate_shader_objects             , dummy_true                             ,  x ,  x ,  x , ES2, 2013)
 EXT(EXT_separate_specular_color             , dummy_true                             , GLL,  x ,  x ,  x , 1997)
+EXT(EXT_shader_clock                        , ARB_shader_clock                       , GLL, GLC,  x ,  x , 2025)
 EXT(EXT_shader_framebuffer_fetch            , EXT_shader_framebuffer_fetch           , GLL, GLC,  x , ES2, 2013)
 EXT(EXT_shader_framebuffer_fetch_non_coherent, EXT_shader_framebuffer_fetch_non_coherent, GLL, GLC,  x, ES2, 2018)
 EXT(EXT_shader_group_vote                   , ARB_shader_group_vote                  ,   x,   x,  x ,  30, 2013)

src/mesa/state_tracker/st_extensions.c
@@ -1053,7 +1053,6 @@ void st_init_extensions(struct pipe_screen *screen,
    EXT_CAP(ARB_sample_locations,             programmable_sample_locations);
    EXT_CAP(ARB_seamless_cube_map,            seamless_cube_map);
    EXT_CAP(ARB_shader_ballot,                shader_ballot);
-   EXT_CAP(ARB_shader_clock,                 shader_clock);
    EXT_CAP(ARB_shader_draw_parameters,       draw_parameters);
    EXT_CAP(ARB_shader_group_vote,            shader_group_vote);
    EXT_CAP(EXT_shader_image_load_formatted,  image_load_formatted);

commit 0f1e0eab0a7ede203e7ca2779aaa0d379c2942eb

mesa: Fix support for GL_EXT_shader_clock
Missing 32-bit entry point in GLSL

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Fixes: bf94eb48 ("mesa: Add support for GL_EXT_shader_clock")
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36041>

src/compiler/glsl/builtin_functions.cpp
@@ -5639,6 +5639,11 @@ builtin_builder::create_builtins()
                               &glsl_type_builtin_uvec2),
                 NULL);
 
+   add_function("clock2x32EXT",
+                _shader_clock(shader_clock,
+                              &glsl_type_builtin_uvec2),
+                NULL);
+
    add_function("clockARB",
                 _shader_clock(shader_clock_int64,
                               &glsl_type_builtin_uint64_t),