joshuaswarren/mesa-1

commit e85629e17c0ca5aa873bad2b641f7f27aae91f1a

anv: expose VK_KHR_maintenance8 support
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33138>

src/intel/vulkan/anv_physical_device.c
@@ -178,6 +178,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_maintenance5                      = true,
       .KHR_maintenance6                      = true,
       .KHR_maintenance7                      = true,
+      .KHR_maintenance8                      = true,
       .KHR_map_memory2                       = true,
       .KHR_multiview                         = true,
       .KHR_performance_query =
@@ -927,6 +928,9 @@ get_features(const struct anv_physical_device *pdevice,
       /* VK_EXT_shader_image_atomic_int64 */
       .shaderImageInt64Atomics = true,
       .sparseImageInt64Atomics = false,
+
+      /* VK_KHR_maintenance8 */
+      .maintenance8 = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without

commit e1f8e4c430dfca1ff0e214ee5aa85b58b3e4db34

anv: temporary disable KHR_maintenance8
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: e85629e17c ("anv: expose VK_KHR_maintenance8 support")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37050>

src/intel/vulkan/anv_physical_device.c
@@ -194,7 +194,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .KHR_maintenance5                      = true,
       .KHR_maintenance6                      = true,
       .KHR_maintenance7                      = true,
-      .KHR_maintenance8                      = true,
+      .KHR_maintenance8                      = false, /* Temporary, will reenable on Xe2+ */
       .KHR_maintenance9                      = true,
       .KHR_map_memory2                       = true,
       .KHR_multiview                         = true,