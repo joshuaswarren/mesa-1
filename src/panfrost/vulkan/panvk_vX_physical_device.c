joshuaswarren/mesa-1

commit 9042e1efea204e7e2b832c4909317acfc83f6704

panvk: advertise vulkan 1.4 on v10+
VK_EXT_host_image_copy was the last extension needed.

Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35910>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -502,7 +502,7 @@ get_api_version()
       return version_override;
 
    if (PAN_ARCH >= 10)
-      return VK_MAKE_API_VERSION(0, 1, 3, VK_HEADER_VERSION);
+      return VK_MAKE_API_VERSION(0, 1, 4, VK_HEADER_VERSION);
 
    return VK_MAKE_API_VERSION(0, 1, 0, VK_HEADER_VERSION);
 }

commit a6ca28d71f7f8072160764ee4b3108cc68879aa3

panvk: fix up vk1.4 properties
These two properties reports how the interaction between MSAA coverage
and occlusion queries works. We need to report the correct value here,
otherwise applications might misbehave.

Fixes: 9042e1efea2 ("panvk: advertise vulkan 1.4 on v10+")
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37136>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -892,6 +892,10 @@ panvk_per_arch(get_physical_device_properties)(
       /* Vulkan 1.3 properties */
       /* XXX: 1.3 support */
 
+      /* Vulkan 1.4 properties */
+      .earlyFragmentMultisampleCoverageAfterSampleCounting = true,
+      .earlyFragmentSampleMaskTestBeforeSampleCounting = true,
+
       /* VK_EXT_subgroup_size_control */
       .minSubgroupSize = pan_subgroup_size(PAN_ARCH),
       .maxSubgroupSize = pan_subgroup_size(PAN_ARCH),