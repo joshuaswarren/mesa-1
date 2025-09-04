joshuaswarren/mesa-1

commit 6f4708f55a102c7519fbe06876a4a0b3cc45aa6a

egl,glx: allow OpenGL with old libx11, but disable glthread if it's unsafe
pass bool thread_safe to the dri frontend, and enable glthread accordingly

Reviewed-by: Eric Engestrom <eric@igalia.com>
Acked-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36378>

src/egl/drivers/dri2/egl_dri2.c
@@ -59,6 +59,7 @@
 #ifdef HAVE_X11_PLATFORM
 #include "X11/Xlibint.h"
 #include "x11_dri3.h"
+#include "x11_display.h"
 #endif
 
 #include "GL/mesa_glinterop.h"
@@ -1235,9 +1236,16 @@ dri2_create_context(_EGLDisplay *disp, _EGLConfig *conf,
                                   &num_attribs))
       goto cleanup;
 
+   bool thread_safe = true;
+
+#ifdef HAVE_X11_PLATFORM
+   if (disp->Platform == _EGL_PLATFORM_X11)
+      thread_safe = x11_xlib_display_is_thread_safe(disp->PlatformDisplay);
+#endif
+
    dri2_ctx->dri_context = driCreateContextAttribs(
       dri2_dpy->dri_screen_render_gpu, api, dri_config, shared, num_attribs / 2,
-      ctx_attribs, &error, dri2_ctx);
+      ctx_attribs, &error, dri2_ctx, thread_safe);
    dri2_create_context_attribs_error(error);
 
    if (!dri2_ctx->dri_context)

meson.build
@@ -2058,7 +2058,7 @@ dep_glproto_version = '>= 1.4.14'
 dep_xcb_dri3_version = '>= 1.13'
 dep_xcb_glx_version = '>= 1.8.1'
 dep_xcb_present_version = '>= 1.13'
-dep_xlib_version = '>= 1.8'
+dep_xlib_version = '>= 1.6' # RHEL8 has 1.6, RHEL9 has 1.7, U22.04 has 1.7
 dep_xlib_xrandr_version = '>= 1.3'
 dep_xshmfence_version = '>= 1.1'
 

src/egl/drivers/dri2/platform_x11.c
@@ -52,7 +52,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include "x11_dri3.h"
-#include "x11_display.h"
 #include "kopper_interface.h"
 #include "loader.h"
 #include "platform_x11.h"
@@ -1069,8 +1068,6 @@ dri2_get_xcb_connection(_EGLDisplay *disp, struct dri2_egl_display *dri2_dpy)
       screen = dri2_find_screen_for_display(disp, screen);
    } else if (disp->Platform == _EGL_PLATFORM_X11) {
       Display *dpy = disp->PlatformDisplay;
-      if (!x11_xlib_display_is_thread_safe(dpy))
-         return EGL_FALSE;
       dri2_dpy->conn = XGetXCBConnection(dpy);
       screen = DefaultScreen(dpy);
    } else {

src/gallium/frontends/dri/dri_context.c
@@ -48,7 +48,8 @@ dri_create_context(struct dri_screen *screen,
                    const struct __DriverContextConfig *ctx_config,
                    unsigned *error,
                    struct dri_context *sharedContextPrivate,
-                   void *loaderPrivate)
+                   void *loaderPrivate,
+                   bool thread_safe)
 {
    struct dri_context *ctx = NULL;
    struct st_context *st_share = NULL;
@@ -228,6 +229,10 @@ dri_create_context(struct dri_screen *screen,
       }
       enable_glthread = user_enable_glthread;
    }
+
+   if (!thread_safe)
+      enable_glthread = false;
+
    /* Do this last. */
    if (enable_glthread)
       _mesa_glthread_init(ctx->st->ctx);

src/gallium/frontends/dri/dri_context.h
@@ -101,7 +101,8 @@ dri_create_context(struct dri_screen *screen,
                    const struct __DriverContextConfig *ctx_config,
                    unsigned *error,
                    struct dri_context *sharedContextPrivate,
-                   void *loaderPrivate);
+                   void *loaderPrivate,
+                   bool thread_safe);
 
 #endif
 

src/gallium/frontends/dri/dri_util.c
@@ -421,7 +421,8 @@ driCreateContextAttribs(struct dri_screen *screen, int api,
                         unsigned num_attribs,
                         const uint32_t *attribs,
                         unsigned *error,
-                        void *data)
+                        void *data,
+                        bool thread_safe)
 {
     const struct gl_config *modes = (config != NULL) ? &config->modes : NULL;
     gl_api mesa_api;
@@ -594,27 +595,27 @@ driCreateContextAttribs(struct dri_screen *screen, int api,
 
     struct dri_context *ctx = dri_create_context(screen, mesa_api,
                                                  modes, &ctx_config, error,
-                                                 shared, data);
+                                                 shared, data, thread_safe);
     return ctx;
 }
 
 static struct dri_context *
 driCreateNewContextForAPI(struct dri_screen *screen, int api,
                           const struct dri_config *config,
-                          struct dri_context *shared, void *data)
+                          struct dri_context *shared, void *data, bool thread_safe)
 {
     unsigned error;
 
     return driCreateContextAttribs(screen, api, config, shared, 0, NULL,
-                                   &error, data);
+                                   &error, data, thread_safe);
 }
 
 struct dri_context *
 driCreateNewContext(struct dri_screen *screen, const struct dri_config *config,
-                    struct dri_context *shared, void *data)
+                    struct dri_context *shared, void *data, bool thread_safe)
 {
     return driCreateNewContextForAPI(screen, __DRI_API_OPENGL,
-                                     config, shared, data);
+                                     config, shared, data, thread_safe);
 }
 
 /**

src/gallium/frontends/dri/dri_util.h
@@ -119,7 +119,8 @@ driCreateContextAttribs(struct dri_screen *psp, int api,
                         unsigned num_attribs,
                         const uint32_t *attribs,
                         unsigned *error,
-                        void *data);
+                        void *data,
+                        bool thread_safe);
 
 extern uint32_t
 driImageFormatToSizedInternalGLFormat(uint32_t image_format);
@@ -140,7 +141,8 @@ driSwapBuffers(struct dri_drawable *drawable);
 PUBLIC void
 driSwapBuffersWithDamage(struct dri_drawable *drawable, int nrects, const int *rects);
 PUBLIC struct dri_context *
-driCreateNewContext(struct dri_screen *screen, const struct dri_config *config, struct dri_context *shared, void *data);
+driCreateNewContext(struct dri_screen *screen, const struct dri_config *config,
+                    struct dri_context *shared, void *data, bool thread_safe);
 PUBLIC int
 driCopyContext(struct dri_context *dest, struct dri_context *src, unsigned long mask);
 PUBLIC void

src/gallium/frontends/dri/loader_dri3_helper.c
@@ -184,7 +184,7 @@ loader_dri3_blit_context_get(struct loader_dri3_drawable *draw)
 
    if (!blit_context.ctx) {
       blit_context.ctx = driCreateNewContext(draw->dri_screen_render_gpu,
-                                                           NULL, NULL, NULL);
+                                             NULL, NULL, NULL, true);
       blit_context.cur_screen = draw->dri_screen_render_gpu;
    }
 

commit 83a8125d8a1f0b3fbdb27157254bc3f3300521fa

egl,glx,X11: Handle case when PlatformDisplay is EGL_DEFAULT_DISPLAY
If the PlatformDisplay is initialized to EGL_DEFAULt_DEVICE (i.e. 0)
acquire a connection to the display to query the thread savety.

v2: rework after getting a better understanding of what is ging on.

v3: check whether XOpenDisplay was successfull (Yonggang Luo)

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13740
Fixes: 6f4708f55a1 ("egl,glx: allow OpenGL with old libx11, but disable glthread if it's unsafe")

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37006>

src/egl/drivers/dri2/egl_dri2.c
@@ -1239,8 +1239,17 @@ dri2_create_context(_EGLDisplay *disp, _EGLConfig *conf,
    bool thread_safe = true;
 
 #ifdef HAVE_X11_PLATFORM
-   if (disp->Platform == _EGL_PLATFORM_X11)
-      thread_safe = x11_xlib_display_is_thread_safe(disp->PlatformDisplay);
+   if (disp->Platform == _EGL_PLATFORM_X11) {
+      if (disp->PlatformDisplay != EGL_DEFAULT_DISPLAY) {
+         thread_safe = x11_xlib_display_is_thread_safe(disp->PlatformDisplay);
+      } else {
+         Display *display = XOpenDisplay(NULL);
+         if (display) {
+            thread_safe = x11_xlib_display_is_thread_safe(display);
+            XCloseDisplay(display);
+         }
+      }
+   }
 #endif
 
    dri2_ctx->dri_context = driCreateContextAttribs(