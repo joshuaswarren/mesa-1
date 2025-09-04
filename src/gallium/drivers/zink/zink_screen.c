joshuaswarren/mesa-1

commit 59d80887cb7232e2f515dc2938dd7564c95970f8

zink: just check multiview availability to advertise extensions
now that legacy renderpasses are dropped, this can be more general

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36417>

src/gallium/drivers/zink/zink_screen.c
@@ -686,7 +686,7 @@ zink_init_screen_caps(struct zink_screen *screen)
 
    caps->null_textures = screen->info.rb_image_feats.robustImageAccess;
    /* support OVR_multiview and OVR_multiview2 */
-   caps->multiview = screen->info.have_vulkan13 ? 2 * screen->info.feats11.multiview : 0;
+   caps->multiview = screen->info.feats11.multiview;
    caps->texrect = false;
    caps->multi_draw_indirect_partial_stride = false;
    caps->anisotropic_filter = screen->info.feats.features.samplerAnisotropy;

commit ba18c79475924d582a94941f97e42e4029b8ca14

zink: reeneable OVR_multiview2
Fixes: 59d80887cb7 ("zink: just check multiview availability to advertise extensions")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37184>

src/gallium/drivers/zink/zink_screen.c
@@ -687,7 +687,7 @@ zink_init_screen_caps(struct zink_screen *screen)
 
    caps->null_textures = screen->info.rb_image_feats.robustImageAccess;
    /* support OVR_multiview and OVR_multiview2 */
-   caps->multiview = screen->info.feats11.multiview;
+   caps->multiview = screen->info.feats11.multiview * 2;
    caps->texrect = false;
    caps->multi_draw_indirect_partial_stride = false;
    caps->anisotropic_filter = screen->info.feats.features.samplerAnisotropy;