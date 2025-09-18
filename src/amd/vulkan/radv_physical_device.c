joshuaswarren/mesa-1

commit efd6208ec9b9101365427335c7d4ebd62dad01c9

radv: Add radv_enable_float16_gfx8 drirc and enable for Indiana Jones TGC
This is a hard requirement from the game preventing it to start on GFX8.
Adding this allows playing it on GFX8.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34114>

src/amd/vulkan/radv_physical_device.c
@@ -827,7 +827,8 @@ radv_physical_device_get_features(const struct radv_physical_device *pdev, struc
       .storagePushConstant8 = true,
       .shaderBufferInt64Atomics = true,
       .shaderSharedInt64Atomics = true,
-      .shaderFloat16 = pdev->info.has_packed_math_16bit,
+      .shaderFloat16 = pdev->info.has_packed_math_16bit ||
+                       (pdev->info.gfx_level == GFX8 && instance->drirc.expose_float16_gfx8),
       .shaderInt8 = true,
 
       .descriptorIndexing = true,

src/amd/vulkan/radv_instance.c
@@ -193,6 +193,7 @@ static const driOptionDescription radv_dri_options[] = {
       DRI_CONF_RADV_LOWER_TERMINATE_TO_DISCARD(false)
       DRI_CONF_RADV_APP_LAYER()
       DRI_CONF_RADV_EMULATE_RT(false)
+      DRI_CONF_RADV_ENABLE_FLOAT16_GFX8(false)
    DRI_CONF_SECTION_END
 };
 // clang-format on
@@ -289,6 +290,8 @@ radv_init_dri_options(struct radv_instance *instance)
       driQueryOptionb(&instance->drirc.options, "radv_lower_terminate_to_discard");
 
    instance->drirc.emulate_rt = driQueryOptionb(&instance->drirc.options, "radv_emulate_rt");
+
+   instance->drirc.expose_float16_gfx8 = driQueryOptionb(&instance->drirc.options, "radv_enable_float16_gfx8");
 }
 
 static const struct vk_instance_extension_table radv_instance_extensions_supported = {

src/amd/vulkan/radv_instance.h
@@ -73,6 +73,7 @@ struct radv_instance {
       bool disable_dcc_stores;
       bool lower_terminate_to_discard;
       bool emulate_rt;
+      bool expose_float16_gfx8;
       char *app_layer;
       uint8_t override_graphics_shader_version;
       uint8_t override_compute_shader_version;

src/util/00-radv-defaults.conf
@@ -115,6 +115,7 @@ Application bugs worked around in this file:
             <option name="radv_invariant_geom" value="true" />
             <option name="radv_lower_terminate_to_discard" value="true" />
             <option name="radv_emulate_rt" value="true" />
+            <option name="radv_enable_float16_gfx8" value="true" />
         </application>
 
         <application name="DOOM (2016)" application_name_match="DOOM$">

src/util/driconf.h
@@ -802,6 +802,10 @@
    DRI_CONF_OPT_B(radv_emulate_rt, def, \
                   "Expose RT extensions on GFX10 and below through software emulation.")
 
+#define DRI_CONF_RADV_ENABLE_FLOAT16_GFX8(def) \
+   DRI_CONF_OPT_B(radv_enable_float16_gfx8, def, \
+                  "Expose float16 on GFX8, where it's supported but usually not beneficial.")
+
 /**
  * \brief ANV specific configuration options
  */

commit 8391acf27ccaf9528c5e514c11d7b505824411fc

radv: make sure fp16 is enabled consistently on gfx8
Fixes `dEQP-VK.api.info.vulkan1p4_limits_validation.general`

Fixes: efd6208ec9b910136542 ("radv: Add radv_enable_float16_gfx8 drirc and enable for Indiana Jones TGC")
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37442>

src/amd/vulkan/radv_physical_device.c
@@ -1458,6 +1458,7 @@ radv_get_compiler_string(struct radv_physical_device *pdev)
 static void
 radv_get_physical_device_properties(struct radv_physical_device *pdev)
 {
+   const struct radv_instance *instance = radv_physical_device_instance(pdev);
    VkSampleCountFlags sample_counts = 0xf;
 
    size_t max_descriptor_set_size = radv_max_descriptor_set_size();
@@ -1469,7 +1470,8 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
       device_type = VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU;
    }
 
-   bool has_fp16 = pdev->info.has_packed_math_16bit;
+   bool has_fp16 = pdev->info.has_packed_math_16bit ||
+      (pdev->info.gfx_level == GFX8 && instance->drirc.features.expose_float16_gfx8);
 
    VkShaderStageFlags taskmesh_stages =
       radv_taskmesh_enabled(pdev) ? VK_SHADER_STAGE_MESH_BIT_EXT | VK_SHADER_STAGE_TASK_BIT_EXT : 0;