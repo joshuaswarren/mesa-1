joshuaswarren/mesa-1

commit 62a64fc0de1a3f7f86b80b1360b106bfdfc0a973

radeonsi: really support eglExportDMABUFImageQueryMESA
Now it should return explicite modifier and plane number.

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31658>

src/gallium/drivers/radeonsi/si_texture.c
@@ -669,6 +669,14 @@ static bool si_resource_get_param(struct pipe_screen *screen, struct pipe_contex
    struct si_texture *tex = (struct si_texture *)resource;
    struct winsys_handle whandle;
 
+   /* Compute texture modifier when needed.
+    * This allows to return the correct values for the PIPE_RESOURCE_PARAM_NPLANES and
+    * PIPE_RESOURCE_PARAM_MODIFIER queries.
+    */
+   if ((param == PIPE_RESOURCE_PARAM_NPLANES || param == PIPE_RESOURCE_PARAM_MODIFIER) &&
+       resource->target != PIPE_BUFFER)
+         ac_compute_surface_modifier(&sscreen->info, &tex->surface, resource->nr_samples);
+
    switch (param) {
    case PIPE_RESOURCE_PARAM_NPLANES:
       if (resource->target == PIPE_BUFFER)

commit 80c409770e9b65bafa7d8801432d0d594f79212c

radeonsi: hide real modifier export behind AMD_DEBUG
Some applications are not ready to handle multi plane
modifiers.

User who want this feature can use AMD_DEBUG=export_modifier
to enable it again.

Fixes: 62a64fc0de1 ("radeonsi: really support eglExportDMABUFImageQueryMESA")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13917
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Tested-by: Mikhail Gavrilov <mikhail.v.gavrilov@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37433>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -84,6 +84,7 @@ static const struct debug_named_value radeonsi_debug_options[] = {
 
    {"tmz", DBG(TMZ), "Force allocation of scanout/depth/stencil buffer as encrypted"},
    {"sqtt", DBG(SQTT), "Enable SQTT"},
+   {"export_modifier", DBG(EXPORT_MODIFIER), "Export real modifier instead of DRM_FORMAT_MOD_INVALID"},
 
    DEBUG_NAMED_VALUE_END /* must be last */
 };

src/gallium/drivers/radeonsi/si_pipe.h
@@ -212,6 +212,7 @@ enum
 
    DBG_TMZ,
    DBG_SQTT,
+   DBG_EXPORT_MODIFIER,
 
    DBG_COUNT
 };

src/gallium/drivers/radeonsi/si_texture.c
@@ -674,7 +674,8 @@ static bool si_resource_get_param(struct pipe_screen *screen, struct pipe_contex
     * PIPE_RESOURCE_PARAM_MODIFIER queries.
     */
    if ((param == PIPE_RESOURCE_PARAM_NPLANES || param == PIPE_RESOURCE_PARAM_MODIFIER) &&
-       resource->target != PIPE_BUFFER)
+       resource->target != PIPE_BUFFER &&
+       (sscreen->debug_flags & DBG(EXPORT_MODIFIER)))
          ac_compute_surface_modifier(&sscreen->info, &tex->surface, resource->nr_samples);
 
    switch (param) {