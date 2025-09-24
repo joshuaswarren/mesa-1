joshuaswarren/mesa-1

commit 4e2f95eb68d78fa77e76c9d972f3a86701105fca

asahi: Flip kmsro around to allocate on the GPU
Our display controller can handle arbitrary GPU imports, so there is no
reason to use dumb KMS buffers. Allocate everything on the GPU instead.

This also allows us to be lazy about mapping things to the KMS side, so
only clients that really want a KMS handle actually do that, which stops
us from ending up with a bunch of junk mapped to DCP (e.g. X11 clients
always request SCANOUT even under XWayland).

Signed-off-by: Asahi Lina <lina@asahilina.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22353>

src/gallium/drivers/asahi/agx_pipe.c
@@ -223,12 +223,6 @@ agx_resource_from_handle(struct pipe_screen *pscreen,
 
    ail_make_miptree(&rsc->layout);
 
-   if (dev->ro) {
-      rsc->scanout =
-         renderonly_create_gpu_import_for_resource(prsc, dev->ro, NULL);
-      /* failure is expected in some cases.. */
-   }
-
    if (prsc->target == PIPE_BUFFER) {
       assert(rsc->layout.tiling == AIL_TILING_LINEAR);
       util_range_init(&rsc->valid_buffer_range);
@@ -245,7 +239,6 @@ agx_resource_get_handle(struct pipe_screen *pscreen, struct pipe_context *ctx,
                         unsigned usage)
 {
    struct agx_device *dev = agx_device(pscreen);
-   struct renderonly_scanout *scanout;
    struct pipe_resource *cur = pt;
 
    /* Even though asahi doesn't support multi-planar formats, we
@@ -258,13 +251,20 @@ agx_resource_get_handle(struct pipe_screen *pscreen, struct pipe_context *ctx,
          return false;
    }
 
-   struct agx_resource *rsrc = (struct agx_resource *)cur;
-   scanout = rsrc->scanout;
+   struct agx_resource *rsrc = agx_resource(cur);
 
    if (handle->type == WINSYS_HANDLE_TYPE_KMS && dev->ro) {
       rsrc_debug(rsrc, "Get handle: %p (KMS RO)\n", rsrc);
 
-      return renderonly_get_handle(scanout, handle);
+      if (!rsrc->scanout && dev->ro && (rsrc->base.bind & PIPE_BIND_SCANOUT)) {
+         rsrc->scanout =
+            renderonly_scanout_for_resource(&rsrc->base, dev->ro, NULL);
+      }
+
+      if (!rsrc->scanout)
+         return false;
+
+      return renderonly_get_handle(rsrc->scanout, handle);
    } else if (handle->type == WINSYS_HANDLE_TYPE_KMS) {
       rsrc_debug(rsrc, "Get handle: %p (KMS)\n", rsrc);
 
@@ -522,48 +522,6 @@ agx_resource_create_with_modifiers(struct pipe_screen *screen,
    if (templ->target == PIPE_BUFFER) {
       assert(nresource->layout.tiling == AIL_TILING_LINEAR);
       util_range_init(&nresource->valid_buffer_range);
-   }
-
-   if (dev->ro && (templ->bind & PIPE_BIND_SCANOUT)) {
-      struct winsys_handle handle;
-      assert(util_format_get_blockwidth(templ->format) == 1);
-      assert(util_format_get_blockheight(templ->format) == 1);
-
-      unsigned width = templ->width0;
-      unsigned stride =
-         templ->width0 * util_format_get_blocksize(templ->format);
-      unsigned size = nresource->layout.size_B;
-      unsigned effective_rows = DIV_ROUND_UP(size, stride);
-
-      struct pipe_resource scanout_tmpl = {
-         .target = nresource->base.target,
-         .format = templ->format,
-         .width0 = width,
-         .height0 = effective_rows,
-         .depth0 = 1,
-         .array_size = 1,
-      };
-
-      nresource->scanout =
-         renderonly_scanout_for_resource(&scanout_tmpl, dev->ro, &handle);
-
-      if (!nresource->scanout) {
-         agx_msg("Failed to create scanout resource\n");
-         free(nresource);
-         return NULL;
-      }
-      assert(handle.type == WINSYS_HANDLE_TYPE_FD);
-      nresource->bo = agx_bo_import(dev, handle.handle);
-      close(handle.handle);
-
-      if (!nresource->bo) {
-         free(nresource);
-         return NULL;
-      }
-
-      agx_resource_debug(nresource, "New[RO]: ");
-
-      return &nresource->base;
    }
 
    if (winsys && templ->bind & PIPE_BIND_DISPLAY_TARGET) {

src/gallium/winsys/kmsro/drm/kmsro_drm_winsys.c
@@ -76,7 +76,7 @@ struct pipe_screen *kmsro_drm_screen_create(int fd,
       {
          .name = "asahi",
          .create_screen = asahi_drm_screen_create_renderonly,
-         .create_for_resource = renderonly_create_kms_dumb_buffer_for_resource,
+         .create_for_resource = renderonly_create_gpu_import_for_resource,
       },
 #endif
 

commit e5882a207eae97da88c60aaa11df27700733f684

asahi: Set PIPE_BIND_SCANOUT in agx_resource_from_handle().
Commit 4e2f95eb68d7 optimized agx_resource_from_handle() to lazily
defer assignment of a kms-ro renderonly_scanout object to an imported
resource until its kms winsys handle is actually queried by a caller
via agx_resource_get_handle(), to avoid unnecessary import into the
DCP display controller. Only resources with bind flag PIPE_BIND_SCANOUT
will get a renderonly_scanout object assigned during such queries.

Problem: This prevents Mesa GBM's gbm_bo_import() function from properly
importing dmabufs for direct scanout use by some Wayland compositors,
e.g., GNOME mutter.

gbm_bo_import() of dmabuf fd's (GBM_BO_IMPORT_FD / GBM_BO_IMPORT_FD_MODIFIER),
even with GBM_BO_USE_SCANOUT flag, will not mark an imported bo with the
PIPE_BIND_SCANOUT bind flag before internally assigning its KMS winsys
handle via screen->resource_get_handle() -> agx_resource_get_handle(),
causing silent failure of that query. Therefore gbm_bo_import() seems
to return a successfully created gbm_bo with all proper properties,
but gbm_bo_get_handle() and gbm_bo_get_handle_for_plane() will return
invalid handles. These invalid handles cause drmAddFbXXX ioctl calls to
fail, and therefore failure of direct scanout of wl_buffers.

Setting PIPE_BIND_SCANOUT for a resource in agx_resource_from_handle()
may retain the optimization and makes gbm_bo_get_handle[_for_plane]()
work. This fixes direct scanout of fullscreen wl_surface / wl_buffers
under at least GNOME mutter 48.

Fixes: 4e2f95eb68d7 ("asahi: Flip kmsro around to allocate on the GPU")
Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37538>

src/gallium/drivers/asahi/agx_pipe.c
@@ -191,7 +191,11 @@ agx_resource_from_handle(struct pipe_screen *pscreen,
    pipe_reference_init(&prsc->reference, 1);
    prsc->screen = pscreen;
 
-   prsc->bind |= PIPE_BIND_SHARED;
+   /* Set PIPE_BIND_SCANOUT for lazy on-demand creation of renderonly
+    * scanout resource if agx_resource_get_handle is called for
+    * WINSYS_HANDLE_TYPE_KMS on a kms-ro screen.
+    */
+   prsc->bind |= PIPE_BIND_SHARED | PIPE_BIND_SCANOUT;
 
    rsc->bo = agx_bo_import(dev, whandle->handle);
    /* Sometimes an import can fail e.g. on an invalid buffer fd, out of