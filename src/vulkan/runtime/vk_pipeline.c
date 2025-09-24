joshuaswarren/mesa-1

commit 9a4a5e5b97ae916cdedc54ba1099637710756ea8

vulkan: setup max_subgroup_size for drivers without varying/max/min size support
Ensures max_subgroup_size is set to the subgroupSize physical device
property on drivers that don't support VK_EXT_shader_object,
VK_EXT_subgroup_size_control, or Vulkan 1.3.

Fixes: daf47f284a5 ("vulkan: set nir subgroup size shader info")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37477>

src/vulkan/runtime/vk_pipeline.c
@@ -122,11 +122,11 @@ vk_set_subgroup_size(struct vk_device *device,
       /* Starting with SPIR-V 1.6, varying subgroup size is the default */
    } else {
       shader->info.api_subgroup_size = properties->subgroupSize;
+      shader->info.max_subgroup_size = properties->subgroupSize;
       if (require_full) {
          assert(shader->info.stage == MESA_SHADER_COMPUTE ||
                 shader->info.stage == MESA_SHADER_MESH ||
                 shader->info.stage == MESA_SHADER_TASK);
-         shader->info.max_subgroup_size = properties->subgroupSize;
          shader->info.min_subgroup_size = properties->subgroupSize;
       }
    }

commit 78b530d7ee0279d042ebe5dc19359d41d31f29f1

vulkan/runtime: only set shader subgroup info if non-zero
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Fixes: 9a4a5e5b97 ("vulkan: setup max_subgroup_size for drivers without var...")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37533>

src/vulkan/runtime/vk_pipeline.c
@@ -120,7 +120,7 @@ vk_set_subgroup_size(struct vk_device *device,
       shader->info.min_subgroup_size = req_subgroup_size;
    } else if (allow_varying || spirv_version >= 0x10600) {
       /* Starting with SPIR-V 1.6, varying subgroup size is the default */
-   } else {
+   } else if (properties->subgroupSize) {
       shader->info.api_subgroup_size = properties->subgroupSize;
       shader->info.max_subgroup_size = properties->subgroupSize;
       if (require_full) {