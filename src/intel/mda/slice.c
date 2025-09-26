joshuaswarren/mesa-1

commit 7374e6580501ffb41d787b92c33401d4f376f542

intel/mda: Add code to produce mesa debug archives
Uses the tar format to collect multiple output files.  It can
be inspected using the regular UNIX tools, but a later commit
will add a specialized tool to perform common tasks.

The tar implementation is enough to fulfill the current needs
without adding a dependency.  There's also a small test mostly
to ensure scaffolding is there in case we need to expand the
implementation.

Acked-by: Kenneth Graunke <kenneth@whitecape.org>
Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29146>

src/intel/mda/slice.c
@@ -1,0 +1,211 @@
+/*
+ * Copyright 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "util/ralloc.h"
+#include "util/hash_table.h"
+
+#include "slice.h"
+
+slice
+slice_from_cstr(const char *str)
+{
+   return (slice){ str, str ? strlen(str) : 0 };
+}
+
+bool
+slice_is_empty(slice s)
+{
+   return s.len == 0;
+}
+
+bool
+slice_equal(slice a, slice b)
+{
+   return a.len == b.len && (a.len == 0 || memcmp(a.data, b.data, a.len) == 0);
+}
+
+bool
+slice_equal_cstr(slice s, const char *cstr)
+{
+   slice cstr_slice = slice_from_cstr(cstr);
+   return slice_equal(s, cstr_slice);
+}
+
+char *
+slice_to_cstr(void *mem_ctx, slice s)
+{
+   char *str = ralloc_size(mem_ctx, s.len + 1);
+   memcpy(str, s.data, s.len);
+   str[s.len] = '\0';
+   return str;
+}
+
+slice
+slice_find_char(slice s, char c)
+{
+   for (int i = 0; i < s.len; i++) {
+      if (s.data[i] == c) {
+         return slice_substr_from(s, i);
+      }
+   }
+   return (slice){};
+}
+
+slice
+slice_find_str(slice s, slice needle)
+{
+   if (needle.len == 0)
+      return s;
+   if (needle.len > s.len)
+      return (slice){};
+
+   for (int i = 0; i <= s.len - needle.len; i++) {
+      if (memcmp(s.data + i, needle.data, needle.len) == 0) {
+         return slice_substr_from(s, i);
+      }
+   }
+   return (slice){};
+}
+
+bool
+slice_contains_str(slice s, slice needle)
+{
+   return !slice_is_empty(slice_find_str(s, needle));
+}
+
+bool
+slice_starts_with(slice s, slice prefix)
+{
+   if (prefix.len > s.len)
+      return false;
+   return memcmp(s.data, prefix.data, prefix.len) == 0;
+}
+
+bool
+slice_ends_with(slice s, slice suffix)
+{
+   if (suffix.len > s.len)
+      return false;
+   return memcmp(s.data + s.len - suffix.len, suffix.data, suffix.len) == 0;
+}
+
+slice
+slice_strip_prefix(slice s, slice prefix)
+{
+   if (slice_starts_with(s, prefix))
+      return slice_substr_from(s, prefix.len);
+   return s;
+}
+
+slice
+slice_substr_from(slice s, int start)
+{
+   if (start < 0)
+      start = 0;
+   if (start >= s.len)
+      return (slice){};
+   return (slice){.data = s.data + start, .len = s.len - start};
+}
+
+slice
+slice_substr_to(slice s, int end)
+{
+   if (end < 0)
+      end = 0;
+   if (end > s.len)
+      end = s.len;
+   return (slice){.data = s.data, .len = end};
+}
+
+slice
+slice_substr(slice s, int start, int end)
+{
+   if (start < 0)
+      start = 0;
+   if (end > s.len)
+      end = s.len;
+
+   if (start >= end)
+      return (slice){};
+   return (slice){.data = s.data + start, .len = end - start};
+}
+
+slice_cut_result
+slice_cut(slice s, char c)
+{
+   return slice_cut_n(s, c, 1);
+}
+
+slice_cut_result
+slice_cut_n(slice s, char c, int n)
+{
+   if (n <= 0) {
+      slice_cut_result result = { .before = s, .after = {}, .found = false };
+      return result;
+   }
+
+   slice current = s;
+   int count = 0;
+
+   while (!slice_is_empty(current)) {
+      slice found = slice_find_char(current, c);
+      if (slice_is_empty(found))
+         break;
+
+      count++;
+      if (count == n) {
+         int pos = found.data - s.data;
+         slice_cut_result result = {
+            .before = slice_substr_to(s, pos),
+            .after = slice_substr_from(s, pos + 1),
+            .found = true
+         };
+         return result;
+      }
+
+      /* Move past this occurrence. */
+      current = slice_substr_from(found, 1);
+   }
+
+   /* Not enough occurrences found. */
+   slice_cut_result result = { .before = s, .after = {}, .found = false };
+   return result;
+}
+
+static uint32_t
+slice_ptr_hash(const void *key)
+{
+   const slice *s = (const slice *)key;
+   return _mesa_hash_string_with_length(s->data, s->len);
+}
+
+static bool
+slice_ptr_equal(const void *a, const void *b)
+{
+   const slice *sa = (const slice *)a;
+   const slice *sb = (const slice *)b;
+   return slice_equal(*sa, *sb);
+}
+
+struct hash_table *
+slice_hash_table_create(void *mem_ctx)
+{
+   return _mesa_hash_table_create(mem_ctx, slice_ptr_hash, slice_ptr_equal);
+}
+
+struct hash_entry *
+slice_hash_table_insert(struct hash_table *ht, slice key, void *data)
+{
+   /* Create a new slice that will be stable in memory, so its pointer can
+    * be used as key.
+    */
+   slice *key_copy = rzalloc(ht, slice);
+   *key_copy = key;
+
+   return _mesa_hash_table_insert(ht, key_copy, data);
+}

.gitignore
@@ -6,3 +6,4 @@
 *.out
 /build
 .venv/
+*.mda.tar

src/intel/mda/debug_archiver.c
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2024 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "debug_archiver.h"
+
+#include "tar.h"
+#include "util/ralloc.h"
+
+#include <string.h>
+
+struct debug_archiver
+{
+   FILE *f;
+   tar_writer *tw;
+   char prefix[128];
+};
+
+debug_archiver *
+debug_archiver_open(void *mem_ctx, const char *name, const char *info)
+{
+   debug_archiver *da = rzalloc(mem_ctx, debug_archiver);
+
+   char *filename = ralloc_asprintf(mem_ctx, "%s.mda.tar", name);
+   da->f = fopen(filename, "wb+");
+
+   da->tw = rzalloc(da, tar_writer);
+   tar_writer_init(da->tw, da->f);
+
+   debug_archiver_set_prefix(da, "");
+
+   tar_writer_start_file(da->tw, "mesa.txt");
+   fprintf(da->f, "Mesa %s\n", info);
+   tar_writer_finish_file(da->tw);
+
+   return da;
+}
+
+void
+debug_archiver_set_prefix(debug_archiver *da, const char *prefix)
+{
+   if (!prefix || !*prefix) {
+      strcpy(da->prefix, "mda");
+   } else {
+      snprintf(da->prefix, ARRAY_SIZE(da->prefix) - 1, "mda/%s", prefix);
+   }
+
+   da->tw->prefix = da->prefix;
+}
+
+void
+debug_archiver_write_file(debug_archiver *da,
+                          const char *filename,
+                          const char *data, unsigned size)
+{
+   tar_writer_start_file(da->tw, filename);
+   fwrite(data, size, 1, da->tw->file);
+   tar_writer_finish_file(da->tw);
+}
+
+FILE *
+debug_archiver_start_file(debug_archiver *da, const char *filename)
+{
+   tar_writer_start_file(da->tw, filename);
+   return da->f;
+}
+
+void
+debug_archiver_finish_file(debug_archiver *da)
+{
+   tar_writer_finish_file(da->tw);
+}
+
+void
+debug_archiver_close(debug_archiver *da)
+{
+   if (da != NULL) {
+      fclose(da->f);
+      ralloc_free(da);
+   }
+}

src/intel/mda/debug_archiver.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2024 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef MDA_DEBUG_ARCHIVE_H
+#define MDA_DEBUG_ARCHIVE_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct debug_archiver debug_archiver;
+
+debug_archiver *debug_archiver_open(void *mem_ctx, const char *name, const char *info);
+
+void debug_archiver_set_prefix(debug_archiver *da, const char *prefix);
+
+void debug_archiver_write_file(debug_archiver *da, const char *filename, const char *data, unsigned size);
+
+FILE *debug_archiver_start_file(debug_archiver *da, const char *filename);
+void debug_archiver_finish_file(debug_archiver *da);
+
+void debug_archiver_close(debug_archiver *da);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MDA_DEBUG_ARCHIVE_H */

src/intel/mda/meson.build
@@ -1,0 +1,41 @@
+# Copyright 2024 Intel Corporation
+# SPDX-License-Identifier: MIT
+
+_libmda = static_library(
+  'mda',
+  [
+    'debug_archiver.h',
+    'debug_archiver.c',
+    'slice.h',
+    'slice.c',
+    'tar.h',
+    'tar.c',
+  ],
+  include_directories: [inc_src],
+  c_args: [c_msvc_compat_args],
+  gnu_symbol_visibility: 'hidden',
+)
+
+idep_mda = declare_dependency(
+  link_with: _libmda,
+)
+
+if with_tests
+  test('mda_tests',
+    executable(
+      'mda_tests',
+      [
+        'tar_test.cpp',
+        'slice_test.cpp',
+      ],
+      dependencies: [
+        idep_gtest,
+        idep_mda,
+        idep_mesautil,
+      ],
+      c_args: [c_msvc_compat_args],
+    ),
+    suite: 'mda',
+    protocol: 'gtest',
+  )
+endif

src/intel/mda/slice.h
@@ -1,0 +1,81 @@
+/*
+ * Copyright 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef MDA_SLICE_H
+#define MDA_SLICE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "util/hash_table.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Non-owning string slice.  Makes convenient to refer to parts of an existing
+ * buffer instead of duplicating into new strings.
+ */
+typedef struct slice {
+   const char *data;
+   int len;
+} slice;
+
+/* To be used when printf formatting pattern "%.*s". */
+#define SLICE_FMT(s) (s).len, (s).data
+
+slice slice_from_cstr(const char *str);
+
+bool slice_is_empty(slice s);
+bool slice_equal(slice a, slice b);
+bool slice_equal_cstr(slice s, const char *cstr);
+bool slice_contains_str(slice s, slice needle);
+bool slice_starts_with(slice s, slice prefix);
+bool slice_ends_with(slice s, slice suffix);
+
+char *slice_to_cstr(void *mem_ctx, slice s);
+
+slice slice_find_char(slice s, char c);
+slice slice_find_str(slice s, slice needle);
+
+slice slice_strip_prefix(slice s, slice prefix);
+slice slice_substr_from(slice s, int start);
+slice slice_substr_to(slice s, int end);
+slice slice_substr(slice s, int start, int end);
+
+typedef struct slice_cut_result {
+   slice before;
+   slice after;
+   bool found;
+} slice_cut_result;
+
+slice_cut_result slice_cut(slice s, char c);
+slice_cut_result slice_cut_n(slice s, char c, int n);
+
+/* Hash table support.
+ *
+ * Mesa src/util/hash_table.h has support for keys up to pointer
+ * size, so a slice by itself can't be stored directly in the
+ * same way a number would.  So the functions below will use
+ * pointers to slices, but ensure that when _stored_ as keys,
+ * a copy of the slice itself will be made and owned by the
+ * hash table.
+ *
+ * Note that the contents of the slices themselves are not
+ * owned by the slices, so also not by the hash table.
+ */
+
+struct hash_table *slice_hash_table_create(void *mem_ctx);
+struct hash_entry *slice_hash_table_insert(struct hash_table *ht, slice key, void *data);
+
+static inline struct hash_entry *slice_hash_table_search(struct hash_table *ht, slice key) {
+   return _mesa_hash_table_search(ht, &key);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MDA_SLICE_H */

src/intel/mda/slice_test.cpp
@@ -1,0 +1,83 @@
+/*
+ * Copyright 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <gtest/gtest.h>
+
+#include "slice.h"
+#include "slice_test.h"
+
+TEST(Slice, Cut)
+{
+   slice s = slice_from_cstr("hello:world");
+
+   slice_cut_result result = slice_cut(s, ':');
+   ASSERT_TRUE(result.found);
+   ASSERT_SLICE_EQ(result.before, "hello");
+   ASSERT_SLICE_EQ(result.after, "world");
+
+   slice s2 = slice_from_cstr("no separator");
+   slice_cut_result result2 = slice_cut(s2, ':');
+   ASSERT_FALSE(result2.found);
+   ASSERT_SLICE_EQ(result2.before, s2);
+   ASSERT_SLICE_EMPTY(result2.after);
+}
+
+TEST(Slice, CutN)
+{
+   slice s = slice_from_cstr("a:b:c:d");
+
+   slice_cut_result result1 = slice_cut_n(s, ':', 2);
+   ASSERT_TRUE(result1.found);
+   ASSERT_SLICE_EQ(result1.before, "a:b");
+   ASSERT_SLICE_EQ(result1.after, "c:d");
+
+   slice_cut_result result2 = slice_cut_n(s, ':', 1);
+   ASSERT_TRUE(result2.found);
+   ASSERT_SLICE_EQ(result2.before, "a");
+   ASSERT_SLICE_EQ(result2.after, "b:c:d");
+
+   slice_cut_result result3 = slice_cut_n(s, ':', 5);
+   ASSERT_FALSE(result3.found);
+   ASSERT_SLICE_EQ(result3.before, s);
+   ASSERT_SLICE_EMPTY(result3.after);
+
+   slice_cut_result result4 = slice_cut_n(s, ':', 0);
+   ASSERT_FALSE(result4.found);
+   slice_cut_result result5 = slice_cut_n(s, ':', -1);
+   ASSERT_FALSE(result5.found);
+}
+
+TEST(Slice, HashTable)
+{
+   struct hash_table *ht = slice_hash_table_create(NULL);
+
+   const char *strings[] = {
+      "NIR-CS/v1", "NIR-CS/v2", "BRW-CS/v1", "BRW-CS/v2",
+      "ASM-CS/v1", "ASM-CS/v2", "NIR-FS/v1", "BRW-FS/v1"
+   };
+   int values[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+   for (int i = 0; i < 8; i++) {
+      slice key = slice_from_cstr(strings[i]);
+      slice_hash_table_insert(ht, key, &values[i]);
+   }
+
+   ASSERT_EQ(_mesa_hash_table_num_entries(ht), 8u);
+
+   for (int i = 0; i < 8; i++) {
+      slice key = slice_from_cstr(strings[i]);
+      struct hash_entry *found = slice_hash_table_search(ht, key);
+      ASSERT_NE(found, nullptr);
+      ASSERT_EQ(*(int*)found->data, values[i]);
+   }
+
+   const int index = 2;
+   slice same_content = slice_from_cstr(strings[index]);
+   struct hash_entry *found = slice_hash_table_search(ht, same_content);
+   ASSERT_NE(found, nullptr);
+   ASSERT_EQ(*(int*)found->data, values[index]);
+
+   _mesa_hash_table_destroy(ht, NULL);
+}

src/intel/mda/slice_test.h
@@ -1,0 +1,132 @@
+/*
+ * Copyright 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <gtest/gtest.h>
+#include <string>
+#include <type_traits>
+#include <cstdio>
+
+#include "slice.h"
+
+static inline
+std::ostream& operator<<(std::ostream& os, const slice& s)
+{
+   os << "{data=\"";
+   if (s.data) {
+      os << std::string(s.data, s.len) << "\"";
+
+      bool has_nul = false;
+      for (int i = 0; i < s.len; i++) {
+         if (s.data[i] == '\0') {
+            has_nul = true;
+            break;
+         }
+      }
+
+      if (has_nul) {
+         os << " [bytes: ";
+         for (int i = 0; i < s.len; i++) {
+            if (i > 0) os << " ";
+            char buf[3];
+            snprintf(buf, sizeof(buf), "%02x", (unsigned char)s.data[i]);
+            os << buf;
+         }
+         os << "]";
+      }
+   } else {
+      os << "(null)\"";
+   }
+   os << ", len=" << s.len << "}";
+   return os;
+}
+
+static inline testing::AssertionResult
+AssertSliceEmpty(const char* slice_expr, slice s)
+{
+   if (slice_is_empty(s))
+      return testing::AssertionSuccess();
+
+   return testing::AssertionFailure()
+      << slice_expr << " is not empty\n"
+      << "  " << slice_expr << " = " << s;
+}
+
+#define EXPECT_SLICE_EMPTY(slice) EXPECT_PRED_FORMAT1(AssertSliceEmpty, slice)
+#define ASSERT_SLICE_EMPTY(slice) ASSERT_PRED_FORMAT1(AssertSliceEmpty, slice)
+
+
+static inline testing::AssertionResult
+AssertSliceNotEmpty(const char* slice_expr, slice s)
+{
+   if (!slice_is_empty(s))
+      return testing::AssertionSuccess();
+
+   return testing::AssertionFailure()
+      << slice_expr << " is empty when it should not be\n"
+      << "  " << slice_expr << " = " << s;
+}
+
+#define EXPECT_SLICE_NOT_EMPTY(slice) EXPECT_PRED_FORMAT1(AssertSliceNotEmpty, slice)
+#define ASSERT_SLICE_NOT_EMPTY(slice) ASSERT_PRED_FORMAT1(AssertSliceNotEmpty, slice)
+
+
+/* Use generics here to be able to compare slice with not only other
+ * slices but also regular C strings (including literals).
+ */
+template<typename T>
+testing::AssertionResult AssertSliceEqual(const char* slice_expr,
+                                           const char* other_expr,
+                                           slice s,
+                                           const T& other) {
+   /* String literals have type const char[N], not const char*, so we need decay
+    * to convert array types to pointer types for uniform handling.  Note each
+    * different size N would be have been a different type to handle below.
+    */
+   using DecayedT = std::decay_t<T>;
+
+   static_assert(std::is_same_v<DecayedT, slice> ||
+                 std::is_same_v<DecayedT, const char*> ||
+                 std::is_same_v<DecayedT, char*> ||
+                 std::is_array_v<T>,
+                 "Second argument must be slice, const char*, char*, or string literal");
+
+   if constexpr (std::is_same_v<DecayedT, const char*> ||
+                 std::is_same_v<DecayedT, char*> ||
+                 std::is_array_v<T>) {
+      if (slice_equal_cstr(s, other))
+         return testing::AssertionSuccess();
+
+      std::stringstream ss;
+      ss << slice_expr << " and " << other_expr << " are not equal\n"
+         << "  " << slice_expr << " = " << s << "\n"
+         << "  " << other_expr << " = \"";
+
+      if constexpr (std::is_array_v<T>)
+         ss << other;
+      else
+         ss << (other ? other : "(null)");
+
+      ss << "\"";
+
+      return testing::AssertionFailure() << ss.str();
+
+   } else {
+      static_assert(std::is_same_v<DecayedT, slice>);
+
+      if (slice_equal(s, other))
+         return testing::AssertionSuccess();
+
+      return testing::AssertionFailure()
+             << slice_expr << " and " << other_expr << " are not equal\n"
+             << "  " << slice_expr << " = " << s << "\n"
+             << "  " << other_expr << " = " << other;
+   }
+}
+
+#define EXPECT_SLICE_EQ(slice, other) EXPECT_PRED_FORMAT2(AssertSliceEqual, slice, other)
+#define ASSERT_SLICE_EQ(slice, other) ASSERT_PRED_FORMAT2(AssertSliceEqual, slice, other)
+

commit 10bbee350f9cf06ae94e712d6bcdbd36f6e383b9

intel/mda: Fix gnu-empty-initializer warning
This also causes build errors on older Android prebuilt clang.

Fixes: 7374e658 ("intel/mda: Add code to produce mesa debug archives")
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37586>

src/intel/mda/slice.c
@@ -53,7 +53,7 @@ slice_find_char(slice s, char c)
          return slice_substr_from(s, i);
       }
    }
-   return (slice){};
+   return (slice){0};
 }
 
 slice
@@ -62,14 +62,14 @@ slice_find_str(slice s, slice needle)
    if (needle.len == 0)
       return s;
    if (needle.len > s.len)
-      return (slice){};
+      return (slice){0};
 
    for (int i = 0; i <= s.len - needle.len; i++) {
       if (memcmp(s.data + i, needle.data, needle.len) == 0) {
          return slice_substr_from(s, i);
       }
    }
-   return (slice){};
+   return (slice){0};
 }
 
 bool
@@ -108,7 +108,7 @@ slice_substr_from(slice s, int start)
    if (start < 0)
       start = 0;
    if (start >= s.len)
-      return (slice){};
+      return (slice){0};
    return (slice){.data = s.data + start, .len = s.len - start};
 }
 
@@ -131,7 +131,7 @@ slice_substr(slice s, int start, int end)
       end = s.len;
 
    if (start >= end)
-      return (slice){};
+      return (slice){0};
    return (slice){.data = s.data + start, .len = end - start};
 }
 
@@ -145,7 +145,7 @@ slice_cut_result
 slice_cut_n(slice s, char c, int n)
 {
    if (n <= 0) {
-      slice_cut_result result = { .before = s, .after = {}, .found = false };
+      slice_cut_result result = { .before = s, .after = {0}, .found = false };
       return result;
    }
 
@@ -173,7 +173,7 @@ slice_cut_n(slice s, char c, int n)
    }
 
    /* Not enough occurrences found. */
-   slice_cut_result result = { .before = s, .after = {}, .found = false };
+   slice_cut_result result = { .before = s, .after = {0}, .found = false };
    return result;
 }
 