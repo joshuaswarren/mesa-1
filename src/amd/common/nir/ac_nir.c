joshuaswarren/mesa-1

commit 043ec68071d2e9da7ab444bb64e93770bb869d46

nir/opt_offsets: allow for unsigned wraps when folding load/store_shared2_amd offsets
Totals from 131 (0.16% of 79839) affected shaders: (Navi48)

Instrs: 217026 -> 216541 (-0.22%); split: -0.24%, +0.01%
CodeSize: 1150136 -> 1146772 (-0.29%); split: -0.31%, +0.02%
Latency: 4225732 -> 4225549 (-0.00%); split: -0.01%, +0.00%
InvThroughput: 840231 -> 839823 (-0.05%); split: -0.05%, +0.00%
VClause: 3815 -> 3816 (+0.03%)
Copies: 15414 -> 15358 (-0.36%); split: -0.38%, +0.02%
PreSGPRs: 6322 -> 6323 (+0.02%)
PreVGPRs: 6064 -> 6062 (-0.03%)
VALU: 117317 -> 116873 (-0.38%); split: -0.40%, +0.02%
SALU: 25384 -> 25331 (-0.21%); split: -0.22%, +0.02%
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37453>

src/compiler/nir/nir_opt_offsets.c
@@ -180,23 +180,20 @@ try_fold_shared2(nir_builder *b,
    bool is_load = intrin->intrinsic == nir_intrinsic_load_shared2_amd;
    unsigned comp_size = (is_load ? intrin->def.bit_size : intrin->src[0].ssa->bit_size) / 8;
    unsigned stride = (nir_intrinsic_st64(intrin) ? 64 : 1) * comp_size;
-   unsigned offset0 = nir_intrinsic_offset0(intrin) * stride;
-   unsigned offset1 = nir_intrinsic_offset1(intrin) * stride;
+   uint32_t offset0 = nir_intrinsic_offset0(intrin) * stride;
+   uint32_t offset1 = nir_intrinsic_offset1(intrin) * stride;
    nir_src *off_src = &intrin->src[offset_src_idx];
 
    uint32_t const_offset = 0;
    nir_scalar replace_src = { NULL, 0 };
    bool modified_shader = false;
    if (!nir_src_is_const(*off_src)) {
-      opt_offsets_state state2 = *state;
-      state2.progress = false;
-
-      uint32_t max = UINT32_MAX - MAX2(offset0, offset1);
+      uint32_t max = INT32_MAX - MAX2(offset0, offset1); /* Avoid negative offsets. */
       replace_src = nir_get_scalar(off_src->ssa, 0);
-      if (!try_extract_const_addition(b, state, &replace_src, &const_offset, max, true))
+      if (!try_extract_const_addition(b, state, &replace_src, &const_offset, max, false))
          return false;
 
-      modified_shader = state2.progress;
+      modified_shader = true;
    } else {
       const_offset = nir_src_as_uint(*off_src);
    }

commit b010f5298750356285bdd6715c60fba82f53db8c

nir/opt_offsets: call allow_offset_wrap() for try_fold_shared2()
This prevents applying wrapping offsets on GFX6.

Fixes: 043ec68071d2e9da7ab444bb64e93770bb869d46 ('nir/opt_offsets: allow for unsigned wraps when folding load/store_shared2_amd offsets')
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37667>

src/amd/common/nir/ac_nir.c
@@ -904,6 +904,8 @@ ac_nir_allow_offset_wrap_cb(nir_intrinsic_instr *instr, const void *data)
    case nir_intrinsic_store_shared:
    case nir_intrinsic_shared_atomic:
    case nir_intrinsic_shared_atomic_swap:
+   case nir_intrinsic_load_shared2_amd:
+   case nir_intrinsic_store_shared2_amd:
       /* GFX6 uses a 16-bit adder and can't handle unsigned wrap. */
       return gfx_level >= GFX7;
    default: return false;

src/compiler/nir/nir_opt_offsets.c
@@ -175,7 +175,8 @@ static bool
 try_fold_shared2(nir_builder *b,
                  nir_intrinsic_instr *intrin,
                  opt_offsets_state *state,
-                 unsigned offset_src_idx)
+                 unsigned offset_src_idx,
+                 bool need_nuw)
 {
    bool is_load = intrin->intrinsic == nir_intrinsic_load_shared2_amd;
    unsigned comp_size = (is_load ? intrin->def.bit_size : intrin->src[0].ssa->bit_size) / 8;
@@ -190,7 +191,7 @@ try_fold_shared2(nir_builder *b,
    if (!nir_src_is_const(*off_src)) {
       uint32_t max = INT32_MAX - MAX2(offset0, offset1); /* Avoid negative offsets. */
       replace_src = nir_get_scalar(off_src->ssa, 0);
-      if (!try_extract_const_addition(b, state, &replace_src, &const_offset, max, false))
+      if (!try_extract_const_addition(b, state, &replace_src, &const_offset, max, need_nuw))
          return false;
 
       modified_shader = true;
@@ -271,9 +272,9 @@ process_instr(nir_builder *b, nir_instr *instr, void *s)
    case nir_intrinsic_store_shared_ir3:
       return try_fold_load_store(b, intrin, state, 1, get_max(state, intrin, state->options->shared_max), need_nuw);
    case nir_intrinsic_load_shared2_amd:
-      return try_fold_shared2(b, intrin, state, 0);
+      return try_fold_shared2(b, intrin, state, 0, need_nuw);
    case nir_intrinsic_store_shared2_amd:
-      return try_fold_shared2(b, intrin, state, 1);
+      return try_fold_shared2(b, intrin, state, 1, need_nuw);
    case nir_intrinsic_load_buffer_amd:
       need_nuw &= !!(nir_intrinsic_access(intrin) & ACCESS_IS_SWIZZLED_AMD);
       return try_fold_load_store(b, intrin, state, 1, get_max(state, intrin, state->options->buffer_max), need_nuw);