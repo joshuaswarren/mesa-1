joshuaswarren/mesa-1

commit a529cf8ea88d87dcc349564a096f5a9746640c33

zink: always add mutable to transient surface creation when needed
cc: mesa-stable

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37743>

src/gallium/drivers/zink/zink_surface.c
@@ -297,6 +297,8 @@ zink_create_transient_surface(struct zink_context *ctx, const struct pipe_surfac
       rtempl.nr_samples = nr_samples;
       rtempl.bind &= ~(PIPE_BIND_LINEAR | ZINK_BIND_DMABUF);
       rtempl.bind |= ZINK_BIND_TRANSIENT;
+      if (psurf->format != rtempl.format)
+         rtempl.bind |= ZINK_BIND_MUTABLE;
       res->transient = zink_resource(ctx->base.screen->resource_create(ctx->base.screen, &rtempl));
       transient = res->transient;
       if (unlikely(!transient)) {

commit e9e5b393c443ea4a6078cd705ccbf5f08411a90f

zink: only add mutable bind for transient surfaces when necessary
Fixes: a529cf8ea88 ("zink: always add mutable to transient surface creation when needed")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37767>

src/gallium/drivers/zink/zink_surface.c
@@ -297,7 +297,7 @@ zink_create_transient_surface(struct zink_context *ctx, const struct pipe_surfac
       rtempl.nr_samples = nr_samples;
       rtempl.bind &= ~(PIPE_BIND_LINEAR | ZINK_BIND_DMABUF);
       rtempl.bind |= ZINK_BIND_TRANSIENT;
-      if (psurf->format != rtempl.format)
+      if (zink_format_needs_mutable(rtempl.format, psurf->format))
          rtempl.bind |= ZINK_BIND_MUTABLE;
       res->transient = zink_resource(ctx->base.screen->resource_create(ctx->base.screen, &rtempl));
       transient = res->transient;