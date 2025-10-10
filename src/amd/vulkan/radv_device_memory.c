joshuaswarren/mesa-1

commit 150e5d74c227cdac4a1b63b20d1253f30655104b

wsi/drm: Don't request implicit sync if we're doing implicit sync ourselves.
This will avoid kernel overhead on tu (implicit syncs every BO) and radv
(implicit syncs the swapchain BOs) for doing implicit synchronization on
non-explicit-sync WSI backends (old X11 and Wayland, KHR_display without
!36591, and headless).

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19309>

src/vulkan/wsi/wsi_common.c
@@ -1803,7 +1803,7 @@ wsi_create_buffer_blit_context(const struct wsi_swapchain *chain,
       .sType = VK_STRUCTURE_TYPE_WSI_MEMORY_ALLOCATE_INFO_MESA,
       .pNext = NULL,
       .implicit_sync = info->image_type == WSI_IMAGE_TYPE_DRM &&
-                       !info->explicit_sync,
+                       !info->explicit_sync && !chain->dma_buf_semaphore,
    };
    VkMemoryDedicatedAllocateInfo buf_mem_dedicated_info = {
       .sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO,

src/vulkan/wsi/wsi_common.h
@@ -71,6 +71,12 @@ struct wsi_image_create_info {
 struct wsi_memory_allocate_info {
     VkStructureType sType;
     const void *pNext;
+    /**
+     * If set, then the driver needs to do implicit synchronization on this BO.
+     *
+     * For DRM drivers, this flag will only get set before linux 6.0, at which
+     * point DMA_BUF_IOCTL_IMPORT_SYNC_FILE was added.
+     */
     bool implicit_sync;
 };
 

src/vulkan/wsi/wsi_common_drm.c
@@ -746,7 +746,10 @@ wsi_create_native_image_mem(const struct wsi_swapchain *chain,
    const struct wsi_memory_allocate_info memory_wsi_info = {
       .sType = VK_STRUCTURE_TYPE_WSI_MEMORY_ALLOCATE_INFO_MESA,
       .pNext = NULL,
-      .implicit_sync = !info->explicit_sync,
+      /* Ask for the driver to add implicit sync to the image if we're not
+       * handling implict sync ourselves.
+       */
+      .implicit_sync = !info->explicit_sync && !chain->dma_buf_semaphore,
    };
    const VkExportMemoryAllocateInfo memory_export_info = {
       .sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO,

commit dc128ef053e6408a0cf624a1a9715438bc312a5a

radv: Allocate BOs as implicit sync even if the WSI is doing implicit sync.
As noted, the flag we allocate with controls whether *anyone* can implicit
sync on the BO through amdgpu interfaces, not just whether our fd does.
This restores radv to the behavior before the regressing commit.

Fixes: 150e5d74c227 ("wsi/drm: Don't request implicit sync if we're doing implicit sync ourselves.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37772>

src/amd/vulkan/radv_device_memory.c
@@ -121,7 +121,13 @@ radv_alloc_memory(struct radv_device *device, const VkMemoryAllocateInfo *pAlloc
    }
 
    if (wsi_info) {
-      if (wsi_info->implicit_sync)
+      /* Even if the WSI is managing implicit-sync fencing on the dma-buf with
+       * dma-buf fence import/export, RADEON_FLAG_IMPLICIT_SYNC also controls
+       * whether other users of the BO (such as an X server using an
+       * implicit-syncing GL driver for rendering) will respect the BO's
+       * implicit sync.
+       */
+      if (wsi_info->implicit_sync || wsi_info->dma_buf_sync_file)
          flags |= RADEON_FLAG_IMPLICIT_SYNC;
 
       /* Mark the linear prime buffer (aka the destination of the prime blit

src/vulkan/util/vk_internal_exts.h
@@ -51,6 +51,16 @@ struct wsi_memory_allocate_info {
      * point DMA_BUF_IOCTL_IMPORT_SYNC_FILE was added.
      */
     bool implicit_sync;
+
+    /**
+     * If set, then the WSI will attach implicit synchronization's fences to
+     * (and reference implicit sync fences from) the dma-buf for this memory, to
+     * meet the requirement for implicit synchronization with the window system.
+     * This does mean that the kernel BO must be able to participate in implicit
+     * sync still when used as part of a non-explicit-synced submit (e.g. by a
+     * GL driver in the X server).
+     */
+    bool dma_buf_sync_file;
 };
 
 /* To be chained into VkSurfaceCapabilities2KHR */

src/vulkan/wsi/wsi_common.c
@@ -1973,6 +1973,7 @@ wsi_create_buffer_blit_context(const struct wsi_swapchain *chain,
       .pNext = NULL,
       .implicit_sync = info->image_type == WSI_IMAGE_TYPE_DRM &&
                        !info->explicit_sync && !chain->dma_buf_semaphore,
+      .dma_buf_sync_file = chain->dma_buf_semaphore,
    };
    VkMemoryDedicatedAllocateInfo buf_mem_dedicated_info = {
       .sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO,