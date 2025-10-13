joshuaswarren/mesa-1

commit b9af18afd65481f7d4b506f69eb2e8f16f7fc9ff

etnaviv: use direct BLT/RS blit hook for internal copies
etna_copy_resource() and etna_copy_resource_box() are used to keep the
internal shadow copies of a resource up to date. They are supposed to
always use the RS or BLT engines to do the copy, never requiring any
fallbacks or fake format handling. They should also work regardless of
the current render condition state. So instead of going through the
pipe_context blit hook, directly call the RS or BLT blit hook on the
etna_context.

CC: mesa-stable
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35526>

src/gallium/drivers/etnaviv/etnaviv_clear_blit.c
@@ -211,6 +211,7 @@ void
 etna_copy_resource(struct pipe_context *pctx, struct pipe_resource *dst,
                    struct pipe_resource *src, int first_level, int last_level)
 {
+   struct etna_context *ctx = etna_context(pctx);
    struct etna_resource *src_priv = etna_resource(src);
    struct etna_resource *dst_priv = etna_resource(dst);
 
@@ -251,7 +252,7 @@ etna_copy_resource(struct pipe_context *pctx, struct pipe_resource *dst,
 
       for (int z = 0; z < depth; z++) {
          blit.src.box.z = blit.dst.box.z = z;
-         pctx->blit(pctx, &blit);
+         ctx->blit(pctx, &blit);
       }
 
       if (src == dst)
@@ -266,6 +267,7 @@ etna_copy_resource_box(struct pipe_context *pctx, struct pipe_resource *dst,
                        struct pipe_resource *src, int dst_level, int src_level,
                        struct pipe_box *box)
 {
+   struct etna_context *ctx = etna_context(pctx);
    struct etna_resource *src_priv = etna_resource(src);
    struct etna_resource *dst_priv = etna_resource(dst);
 
@@ -289,7 +291,7 @@ etna_copy_resource_box(struct pipe_context *pctx, struct pipe_resource *dst,
 
    for (int z = 0; z < box->depth; z++) {
       blit.src.box.z = blit.dst.box.z = box->z + z;
-      pctx->blit(pctx, &blit);
+      ctx->blit(pctx, &blit);
    }
 
    if (src == dst)

commit 2afda2496973f21ac1d2342ccb2c5b4df6a30712

etnaviv: fix YUV tiler blits
The YUV tiling blits are an internal copy into a shadow of the resource.
Thus they don't go through the external context blit hook anymore. Call
the YUV blit function from the two code paths that handle internal copies.

Fixes: b9af18afd654 ("etnaviv: use direct BLT/RS blit hook for internal copies")
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37605>

src/gallium/drivers/etnaviv/etnaviv_clear_blit.c
@@ -134,10 +134,6 @@ etna_blit(struct pipe_context *pctx, const struct pipe_blit_info *blit_info)
    if (ctx->blit(pctx, &info))
       goto success;
 
-   if (etna_format_needs_yuv_tiler(blit_info->src.format) &&
-       etna_try_yuv_blit(pctx, blit_info))
-      goto success;
-
    if (util_try_blit_via_copy_region(pctx, &info, false))
       goto success;
 
@@ -275,7 +271,10 @@ etna_copy_resource(struct pipe_context *pctx, struct pipe_resource *dst,
 
       for (int z = 0; z < depth; z++) {
          blit.src.box.z = blit.dst.box.z = z;
-         ctx->blit(pctx, &blit);
+         if (unlikely(etna_format_needs_yuv_tiler(blit.src.format)))
+            etna_try_yuv_blit(pctx, &blit);
+         else
+            ctx->blit(pctx, &blit);
       }
 
       if (src == dst)
@@ -314,7 +313,10 @@ etna_copy_resource_box(struct pipe_context *pctx, struct pipe_resource *dst,
 
    for (int z = 0; z < box->depth; z++) {
       blit.src.box.z = blit.dst.box.z = box->z + z;
-      ctx->blit(pctx, &blit);
+      if (unlikely(etna_format_needs_yuv_tiler(blit.src.format)))
+         etna_try_yuv_blit(pctx, &blit);
+      else
+         ctx->blit(pctx, &blit);
    }
 
    if (src == dst)