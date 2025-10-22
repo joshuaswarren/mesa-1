joshuaswarren/mesa-1

commit 8d6f000e4b3514cff87184a1727f0e4e711b0f8f

nir/lower_io: add get_io_index_src_number support for image intrinsics
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Alyssa Anne Rosenzweig <alyssa.rosenzweig@intel.com>
Reviewed-by: Francisco Jerez <currojerez@riseup.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37394>

src/compiler/nir/nir_lower_io.c
@@ -1061,7 +1061,22 @@ nir_get_io_index_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_store_shared_block_intel:
    case nir_intrinsic_load_ubo_uniform_block_intel:
    case nir_intrinsic_load_ssbo_uniform_block_intel:
+#define IMG_CASE(name) case nir_intrinsic_image_##name: case nir_intrinsic_bindless_image_##name
+   IMG_CASE(load):
+   IMG_CASE(store):
+   IMG_CASE(sparse_load):
+   IMG_CASE(atomic):
+   IMG_CASE(atomic_swap):
+   IMG_CASE(size):
+   IMG_CASE(samples):
+   IMG_CASE(texel_address):
+   IMG_CASE(samples_identical):
+   IMG_CASE(descriptor_amd):
+   IMG_CASE(format):
+   IMG_CASE(order):
+   IMG_CASE(fragment_mask_load_amd):
       return 0;
+#undef IMG_CASE
    case nir_intrinsic_store_ssbo:
    case nir_intrinsic_store_per_vertex_output:
    case nir_intrinsic_store_per_view_output:

commit cc8084a3d3371a4167cea1dc49c1d51c2a579e1f

nir/lower_io: add missing levels intrinsics to get_io_index_src_number
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 8d6f000e4b ("nir/lower_io: add get_io_index_src_number support for image intrinsics")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38012>

src/compiler/nir/nir_lower_io.c
@@ -1069,6 +1069,7 @@ nir_get_io_index_src_number(const nir_intrinsic_instr *instr)
    IMG_CASE(atomic):
    IMG_CASE(atomic_swap):
    IMG_CASE(size):
+   IMG_CASE(levels):
    IMG_CASE(samples):
    IMG_CASE(texel_address):
    IMG_CASE(samples_identical):