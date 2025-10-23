joshuaswarren/mesa-1

commit 2f689adc2987554e7d8bb4e1194bcef9bf5c48d0

util: mimic KCMP_FILE via epoll when KCMP is missing
On Linux platforms where KCMP isn't allowed epoll can be used
as a fallback mechanism to provide the same feature.

Acked-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34447>

src/util/os_file.c
@@ -207,6 +207,8 @@ os_read_file(const char *filename, size_t *size)
 #define KCMP_FILE 0
 #endif
 
+#elif DETECT_OS_LINUX
+#include <sys/epoll.h>
 #endif
 
 #if DETECT_OS_DRAGONFLY || DETECT_OS_FREEBSD
@@ -266,6 +268,45 @@ os_same_file_description(int fd1, int fd2)
       return -1;
 
    return (fd1_kfile < fd2_kfile) | ((fd1_kfile > fd2_kfile) << 1);
+#elif DETECT_OS_LINUX
+   int r;
+
+   int efd = epoll_create1(EPOLL_CLOEXEC);
+   if (efd < 0)
+      return -1;
+
+   struct epoll_event evt = {};
+   /* Get a new file descriptor number for fd1. */
+   int tmp = os_dupfd_cloexec(fd1);
+   /* Add it to evt. */
+   r = epoll_ctl(efd, EPOLL_CTL_ADD, tmp, &evt);
+   if (r)
+      goto error;
+
+   /* Now use dup2 to get tmp to point to fd2's file description. */
+   r = dup2(fd2, tmp);
+   if (r < 0)
+      goto error;
+
+   /* Last step: add tmp again to evt. Given that we're adding the
+    * same file description as fd2 (thanks to dup2), it will fail with
+    * EEXIST if fd1 and fd2 both point to the same file description.
+    */
+   r = epoll_ctl(efd, EPOLL_CTL_ADD, tmp, &evt);
+   if (r) {
+      if (errno == EEXIST)
+         r = 0;
+      else
+         r = -1;
+   } else {
+      r = 1;
+   }
+
+error:
+   close(tmp);
+   close(efd);
+
+   return r;
 #else
    /* Otherwise we can't tell */
    return -1;

commit be862913b72381005abfb43c2fed8ba3318e1d1e

util: Fix gnu-empty-initializer error
Fixes the following building error happening with clang:

../src/util/os_file.c:291:29: error: use of GNU empty initializer extension [-Werror,-Wgnu-empty-initializer]
   struct epoll_event evt = {};
                            ^
1 error generated.

Fixes: 2f689adc ("util: mimic KCMP_FILE via epoll when KCMP is missing")
Cc: "25.3"
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37944>

src/util/os_file.c
@@ -288,7 +288,7 @@ os_same_file_description(int fd1, int fd2)
    if (efd < 0)
       return -1;
 
-   struct epoll_event evt = {};
+   struct epoll_event evt = {0};
    /* Get a new file descriptor number for fd1. */
    int tmp = os_dupfd_cloexec(fd1);
    /* Add it to evt. */