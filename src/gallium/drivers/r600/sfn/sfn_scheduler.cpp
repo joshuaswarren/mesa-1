joshuaswarren/mesa-1

commit b093cc825e467d4fa9a074124a2904b83e5851b5

r600/sfn: make sure that kill and update pred are not in the same group
The two don't seem to mix well.

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37096>

src/gallium/drivers/r600/sfn/sfn_scheduler.cpp
@@ -616,6 +616,9 @@ BlockScheduler::schedule_alu(Shader::ShaderBlocks& out_blocks, ValueFactory& vf)
       if (!alu_vec_ready.empty())
          success |= schedule_alu_to_group_vec(group);
 
+      if (group->has_kill_op())
+         break;
+
       /* Apparently one can't schedule a t-slot if there is already
        * and LDS instruction scheduled.
        * TODO: check whether this is only relevant for actual LDS instructions
@@ -865,6 +868,8 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
    bool success = false;
    auto i = alu_vec_ready.begin();
    auto e = alu_vec_ready.end();
+   bool group_has_kill = false;
+   bool group_has_update_pred = false;
    while (i != e) {
       sfn_log << SfnLog::schedule << "Try schedule to vec " << **i;
 
@@ -873,9 +878,21 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
          continue;
       }
 
-      // precausion: don't kill while we hae LDS queue reads in the pipeline
-      if ((*i)->is_kill() && m_current_block->lds_group_active())
+      bool is_kill = (*i)->is_kill();
+      bool does_update_pred = (*i)->has_alu_flag(alu_update_pred);
+
+      // don't kill while we hae LDS queue reads in the pipeline
+      if (is_kill && (m_current_block->lds_group_active())) {
+         ++i;
          continue;
+      }
+
+      // don't put a kill and an update of the predicate into the
+      // same group
+      if ((group_has_kill && does_update_pred) || (group_has_update_pred && is_kill)) {
+         ++i;
+         continue;
+      }
 
       if (!m_current_block->try_reserve_kcache(**i)) {
          sfn_log << SfnLog::schedule << " failed (kcache)\n";
@@ -885,6 +902,7 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
 
       if (group->add_vec_instructions(*i)) {
          (*i)->pin_dest_to_chan();
+         group_has_update_pred |= (*i)->has_alu_flag(alu_update_pred);
          auto old_i = i;
          ++i;
          if ((*old_i)->has_alu_flag(alu_is_lds)) {
@@ -924,6 +942,9 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
 
          alu_vec_ready.erase(old_i);
          success = true;
+
+         group_has_kill |= is_kill;
+
          sfn_log << SfnLog::schedule << " success\n";
       } else {
          ++i;

commit 5d520e67aaf0f0882e043944e880ecd745c00bf8

r600/sfn: make sure kill and update_exec don't happen in one group
v2: - Correctly test in multi-slot split whether the group has kill if
      we want to add a multi-slot op.
    - update group_has_predicate if an according vector op was added

Fixes: b093cc825e4 ("r600/sfn: make sure that kill and update pred are not in the same group")

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38112>

src/gallium/drivers/r600/sfn/sfn_scheduler.cpp
@@ -870,7 +870,7 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
    auto i = alu_vec_ready.begin();
    auto e = alu_vec_ready.end();
    bool group_has_kill = group->has_kill_op();
-   bool group_has_update_pred = false;
+   bool group_has_update_pred = group->has_update_exec();
    while (i != e) {
       sfn_log << SfnLog::schedule << "Try schedule to vec " << **i;
 
@@ -945,6 +945,7 @@ BlockScheduler::schedule_alu_to_group_vec(AluGroup *group)
          success = true;
 
          group_has_kill |= is_kill;
+         group_has_update_pred |= does_update_pred;
 
          sfn_log << SfnLog::schedule << " success\n";
       } else {
@@ -965,7 +966,19 @@ BlockScheduler::schedule_alu_multislot_to_group_vec(AluGroup *group, ValueFactor
    auto i = alu_multi_slot_ready.begin();
    auto e = alu_multi_slot_ready.end();
 
+   bool group_has_kill = group->has_kill_op();
+
    while (i != e && util_bitcount(group->free_slot_mask()) > 1) {
+
+      /* A kill instruction and a predicate update in the same
+       * group don't mix well, so skip adding a predicate changing
+       * multi-slot op if we already have a kill. (There are no
+       * multi-slot kill ops).
+       */
+      if (group_has_kill && (*i)->has_alu_flag(alu_update_exec)) {
+         ++i;
+         continue;
+      }
 
       auto dest = (*i)->dest();
 
@@ -1038,6 +1051,10 @@ BlockScheduler::schedule_alu_to_group_trans(AluGroup *group,
    bool success = false;
    auto i = readylist.begin();
    auto e = readylist.end();
+
+   bool group_has_kill = group->has_kill_op();
+   bool group_has_update_pred = group->has_update_exec();
+
    while (i != e) {
 
       if (check_array_reads(**i)) {
@@ -1048,6 +1065,12 @@ BlockScheduler::schedule_alu_to_group_trans(AluGroup *group,
       sfn_log << SfnLog::schedule << "Try schedule to trans " << **i;
       if (!m_current_block->try_reserve_kcache(**i)) {
          sfn_log << SfnLog::schedule << " failed (kcache)\n";
+         ++i;
+         continue;
+      }
+
+      if ((group_has_kill && (*i)->has_alu_flag(alu_update_exec)) ||
+          (group_has_update_pred && (*i)->is_kill())) {
          ++i;
          continue;
       }