joshuaswarren/mesa-1

commit 9ad50d977e3a6a40784d81450ef79c310f000239

wsi/display: Clean up DRM hdr/color state on swapchain destruction
Reset our color outcome to defaults, and update the state.
We need to clean up after our mess, for any other compositors,
etc that come after us that may not be aware of properties
we have set.
We can do this by just setting ourselves back to sRGB and therefore
SDR and updating like normal.
We only need to do this if we have a color outcome serial that isn't
0, the default.

Signed-off-by: Joshua Ashton <joshua@froggi.es>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35461>

src/vulkan/wsi/wsi_common_display.c
@@ -372,6 +372,9 @@ struct wsi_display_sync {
 
 static uint64_t fence_sequence;
 
+static void
+_wsi_display_cleanup_state(struct wsi_display_swapchain *chain);
+
 ICD_DEFINE_NONDISP_HANDLE_CASTS(wsi_display_mode, VkDisplayModeKHR)
 ICD_DEFINE_NONDISP_HANDLE_CASTS(wsi_display_connector, VkDisplayKHR)
 
@@ -1528,6 +1531,8 @@ wsi_display_swapchain_destroy(struct wsi_swapchain *drv_chain,
    struct wsi_display_swapchain *chain =
       (struct wsi_display_swapchain *) drv_chain;
 
+   _wsi_display_cleanup_state(chain);
+
    for (uint32_t i = 0; i < chain->base.image_count; i++)
       wsi_display_image_finish(drv_chain, &chain->images[i]);
 
@@ -2497,6 +2502,30 @@ out:
    drmModeAtomicFree(req);
 
    return ret;
+}
+
+static void
+_wsi_display_cleanup_state(struct wsi_display_swapchain *chain)
+{
+   VkIcdSurfaceDisplay *surface = chain->surface;
+   wsi_display_mode *display_mode =
+      wsi_display_mode_from_handle(surface->displayMode);
+   wsi_display_connector *connector = display_mode->connector;
+
+   /* Reset our color outcome to defaults, and update the state.
+    * We need to clean up after our mess, for any other compositors,
+    * etc that come after us that may not be aware of properties
+    * we have set.
+    * We can do this by just setting ourselves back to sRGB and therefore
+    * SDR and updating like normal.
+    * We only need to do this if we have a color outcome serial that isn't
+    * 0, the default.
+    */
+   if (chain->color_outcome_serial) {
+      chain->color_outcome_serial = 0;
+      chain->base.image_info.color_space = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+      drm_atomic_commit(connector, &chain->images[0]);
+   }
 }
 
 /*

commit 03e5bb18326959073133c3b70462b8fbab3e6390

wsi/display: Allow atomic modeset for change of Colorspace or HDR poperties
At least some drivers need a full modeset to change the Colorspace
property or to en-/disable HDR mode. E.g., at least amdgpu-kms as
tested under Linux 6.8 on Polaris needs it. Otherwise the atomic
commit for disabling HDR in _wsi_display_cleanup_state() will fail,
and the connector stays stuck in HDR mode after vkDestroySwapchainKHR().

Fixes: 9ad50d977e3a ("wsi/display: Clean up DRM hdr/color state on swapchain destruction")
Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Reviewed-by: Autumn Ashton <misyl@froggi.es>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37880>

src/vulkan/wsi/wsi_common_display.c
@@ -2733,6 +2733,11 @@ drm_atomic_commit(wsi_display_connector *connector, struct wsi_display_image *im
                                   connector->colorspace_enum[drm_colorspace]);
       }
 
+      /* At least some drivers need a modeset for HDR or Colorspace change, e.g., amdgpu
+       * at least for Colorspace change or HDR en-/disable.
+       */
+      flags |= DRM_MODE_ATOMIC_ALLOW_MODESET;
+
       connector->color_outcome_serial = image->chain->color_outcome_serial;
    }
 