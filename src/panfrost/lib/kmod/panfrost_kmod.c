joshuaswarren/mesa-1

commit 1ba403ea907ae1c66dc5f1de465db26ebe1816a9

pan/kmod: query and cache available context priorities from KMD
Priority values will be used later one when creating job contexts.

Reviewed-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Ashley Smith <ashley.smith@collabora.com>
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Signed-off-by: Adrián Larumbe <adrian.larumbe@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37075>

src/panfrost/lib/kmod/panfrost_kmod.c
@@ -211,9 +211,20 @@ panfrost_dev_query_props(const struct pan_kmod_dev *dev,
          fd, DRM_PANFROST_PARAM_SYSTEM_TIMESTAMP_FREQUENCY, true, 0);
    }
 
-   /* Panfrost currently doesn't support priorities, assumes default priority as
-    * medium */
-   props->allowed_group_priorities_mask = PAN_KMOD_GROUP_ALLOW_PRIORITY_MEDIUM;
+   /* Support for priorities was added in panfrost 1.5, assumes default
+    * priority as medium if the param doesn't exist. */
+   uint64_t prios =
+      panfrost_query_raw(fd, DRM_PANFROST_PARAM_ALLOWED_JM_CTX_PRIORITIES,
+                         false, PANFROST_JM_CTX_PRIORITY_MEDIUM);
+
+   if (prios & PANFROST_JM_CTX_PRIORITY_LOW)
+      props->allowed_group_priorities_mask |= PAN_KMOD_GROUP_ALLOW_PRIORITY_LOW;
+   if (prios & PANFROST_JM_CTX_PRIORITY_MEDIUM)
+      props->allowed_group_priorities_mask |=
+         PAN_KMOD_GROUP_ALLOW_PRIORITY_MEDIUM;
+   if (prios & PANFROST_JM_CTX_PRIORITY_HIGH)
+      props->allowed_group_priorities_mask |=
+         PAN_KMOD_GROUP_ALLOW_PRIORITY_HIGH;
 }
 
 static uint32_t

commit 9f3a577302f863bdb1ca0f4276a31c0b6bff1cec

pan/kmod: fix priority query logic
The PANFROST_JM_CTX_PRIORITY values aren't bitmasks, but enum values.
But the kernel interface uses the BIT()-macro on them, so we need to do
the same. We don't have the macro, but it's trivial to do this with a
bitshift instead.

Fixes: 1ba403ea907 ("pan/kmod: query and cache available context priorities from KMD")
CID: 1666511
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37903>

src/panfrost/lib/kmod/panfrost_kmod.c
@@ -215,14 +215,14 @@ panfrost_dev_query_props(const struct pan_kmod_dev *dev,
     * priority as medium if the param doesn't exist. */
    uint64_t prios =
       panfrost_query_raw(fd, DRM_PANFROST_PARAM_ALLOWED_JM_CTX_PRIORITIES,
-                         false, PANFROST_JM_CTX_PRIORITY_MEDIUM);
+                         false, BITFIELD_BIT(PANFROST_JM_CTX_PRIORITY_MEDIUM));
 
-   if (prios & PANFROST_JM_CTX_PRIORITY_LOW)
+   if (prios & BITFIELD_BIT(PANFROST_JM_CTX_PRIORITY_LOW))
       props->allowed_group_priorities_mask |= PAN_KMOD_GROUP_ALLOW_PRIORITY_LOW;
-   if (prios & PANFROST_JM_CTX_PRIORITY_MEDIUM)
+   if (prios & BITFIELD_BIT(PANFROST_JM_CTX_PRIORITY_MEDIUM))
       props->allowed_group_priorities_mask |=
          PAN_KMOD_GROUP_ALLOW_PRIORITY_MEDIUM;
-   if (prios & PANFROST_JM_CTX_PRIORITY_HIGH)
+   if (prios & BITFIELD_BIT(PANFROST_JM_CTX_PRIORITY_HIGH))
       props->allowed_group_priorities_mask |=
          PAN_KMOD_GROUP_ALLOW_PRIORITY_HIGH;
 }