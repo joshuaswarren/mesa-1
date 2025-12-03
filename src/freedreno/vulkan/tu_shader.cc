joshuaswarren/mesa-1

commit b11bb6d7aedc4e2f69c9267d176be38f8c1b6be7

tu: Set use_layer_id_sysval for nir_lower_input_attachments
We can just use nir_lower_sysvals_to_varyings instead.

Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38562>

src/freedreno/vulkan/tu_shader.cc
@@ -2782,7 +2782,7 @@ tu_shader_create(struct tu_device *dev,
    if (nir->info.stage == MESA_SHADER_FRAGMENT) {
       const nir_input_attachment_options att_options = {
          .use_fragcoord_sysval = true,
-         .use_layer_id_sysval = false,
+         .use_layer_id_sysval = true,
          /* When using multiview rendering, we must use
           * gl_ViewIndex as the layer id to pass to the texture
           * sampling function. gl_Layer doesn't work when
@@ -2797,6 +2797,12 @@ tu_shader_create(struct tu_device *dev,
             key->unscaled_input_fragcoord,
       };
       NIR_PASS(_, nir, nir_lower_input_attachments, &att_options);
+
+      const nir_lower_sysvals_to_varyings_options sysval_options = {
+         .layer_id = true,
+         .view_index = true,
+      };
+      NIR_PASS(_, nir, nir_lower_sysvals_to_varyings, &sysval_options);
    }
 
    /* This has to happen before lower_input_attachments, because we have to

commit 3552eca703c0f1ecde8aa97baf58102c16449e5e

tu: Call nir_lower_sysvals_to_varyings once
Remove the early call to nir_lower_sysvals_to_varyings. This guarantees
that we only call it after lower_system_values has replaced the
variables with intrinsics. It turns out that a sequence like this
doesn't work:

1. shader declares Layer builtin as a signed integer
2. nir_lower_sysvals_to_varyings turns it into a varying (but keeps the
  signed integer type)
3. nir_lower_input_attachments (or some other pass) creates a
  nir_load_layer_id intrinsic.
4. nir_lower_sysvals_to_varyings is called again, and when creating the
  varying variable it passes an unsigned type to
  nir_get_variable_with_location(), which asserts because there is
  already a signed integer variable.

By making lower_sysvals_to_varyings happen late for layer, we can avoid
this happening by lowering away the variable before (2).

Fixes: b11bb6d7aed ("tu: Set use_layer_id_sysval for nir_lower_input_attachments")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38793>

src/freedreno/vulkan/tu_shader.cc
@@ -224,12 +224,6 @@ tu_spirv_to_nir(struct tu_device *dev,
       nir_print_shader(nir, stderr);
    }
 
-   const struct nir_lower_sysvals_to_varyings_options sysvals_to_varyings = {
-      .point_coord = true,
-      .layer_id = true,
-   };
-   NIR_PASS(_, nir, nir_lower_sysvals_to_varyings, &sysvals_to_varyings);
-
    NIR_PASS(_, nir, nir_lower_global_vars_to_local);
 
    /* Older glslang missing bf6efd0316d8 ("SPV: Fix #2293: keep relaxed
@@ -2791,6 +2785,7 @@ tu_shader_create(struct tu_device *dev,
       NIR_PASS(_, nir, nir_lower_input_attachments, &att_options);
 
       const nir_lower_sysvals_to_varyings_options sysval_options = {
+         .point_coord = true,
          .layer_id = true,
          .view_index = true,
       };