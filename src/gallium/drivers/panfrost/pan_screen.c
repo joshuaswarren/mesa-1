joshuaswarren/mesa-1

commit 8699b99d91b2f1c88e8e238b580bea6aea21c294

panvk: enable VK_KHR_shader_clock
CTS passing and manual tests show it's working well

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35488>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -87,6 +87,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_sampler_mirror_clamp_to_edge = true,
       .KHR_sampler_ycbcr_conversion = PAN_ARCH >= 10,
       .KHR_separate_depth_stencil_layouts = true,
+      .KHR_shader_clock = true,
       .KHR_shader_draw_parameters = true,
       .KHR_shader_expect_assume = true,
       .KHR_shader_float_controls = true,
@@ -424,6 +425,10 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_EXT_pipeline_robustness */
       .pipelineRobustness = true,
+
+      /* VK_KHR_shader_clock */
+      .shaderSubgroupClock = device->kmod.props.gpu_can_query_timestamp,
+      .shaderDeviceClock = device->kmod.props.gpu_can_query_timestamp,
 
       /* VK_KHR_shader_float_controls2 */
       .shaderFloatControls2 = true,

commit 77551b014c846a325302dd4a841c034ac2ea5c64

panfrost,panvk: Enable shader_realtime_clock on panthor 1.6
shader_realtime_clock requires a newer kernel version in order to enable
GLB_COUNTER_EN this change adds a check on this kernel functionality.

Remove GL_EXT_shader_realtime_clock from extensions as this now depends
on kernel version.

Fixes: 8699b99d ("panvk: enable VK_KHR_shader_clock")
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37915>

src/gallium/drivers/panfrost/pan_screen.c
@@ -729,7 +729,8 @@ panfrost_init_screen_caps(struct panfrost_screen *screen)
    caps->shader_clock = dev->arch >= 6 &&
       dev->kmod.props.gpu_can_query_timestamp;
    caps->shader_realtime_clock = dev->arch >= 6 &&
-      dev->kmod.props.gpu_can_query_timestamp;
+      dev->kmod.props.gpu_can_query_timestamp &&
+      dev->kmod.props.timestamp_device_coherent;
 
    /* pixel_local_storage is initially for valhall and bifrost only */
    caps->shader_pixel_local_storage_fast_size =

src/panfrost/lib/kmod/pan_kmod.h
@@ -205,6 +205,9 @@ struct pan_kmod_dev_props {
    /* Support cycle count and timestamp propagation as job requirement */
    bool gpu_can_query_timestamp;
 
+   /* Cycle counter and timestamp device coherent propogation is enabled */
+   bool timestamp_device_coherent;
+
    /* GPU Timestamp frequency */
    uint64_t timestamp_frequency;
 

src/panfrost/lib/kmod/panfrost_kmod.c
@@ -211,6 +211,9 @@ panfrost_dev_query_props(const struct pan_kmod_dev *dev,
          fd, DRM_PANFROST_PARAM_SYSTEM_TIMESTAMP_FREQUENCY, true, 0);
    }
 
+   /* Device coherent timestamps are always enabled on panfrost */
+   props->timestamp_device_coherent = true;
+
    /* Support for priorities was added in panfrost 1.5, assumes default
     * priority as medium if the param doesn't exist. */
    uint64_t prios =

src/panfrost/lib/kmod/panthor_kmod.c
@@ -286,6 +286,10 @@ panthor_dev_query_props(const struct pan_kmod_dev *dev,
          panthor_dev->props.group_priorities.allowed_mask),
    };
 
+   if (dev->driver.version.major > 1 || dev->driver.version.minor >= 6) {
+      props->timestamp_device_coherent = true;
+   }
+
    static_assert(sizeof(props->texture_features) ==
                     sizeof(panthor_dev->props.gpu.texture_features),
                  "Mismatch in texture_features array size");

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -493,7 +493,7 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_KHR_shader_clock */
       .shaderSubgroupClock = device->kmod.props.gpu_can_query_timestamp,
-      .shaderDeviceClock = device->kmod.props.gpu_can_query_timestamp,
+      .shaderDeviceClock = device->kmod.props.timestamp_device_coherent,
 
       /* VK_KHR_shader_quad_control */
       .shaderQuadControl = true,