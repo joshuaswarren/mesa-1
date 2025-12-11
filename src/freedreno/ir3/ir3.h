joshuaswarren/mesa-1

commit 2cd927cc2b26dd589e50054b4310e1f4bfc8959c

ir3: Add (sy) before end of preamble when necessary
We need to ensure GPR writes completes before the end of the preamble
to avoid writes landing after another preamble has already started.

Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38450>

src/freedreno/ir3/ir3.h
@@ -3311,6 +3311,13 @@ regmask_get(regmask_t *regmask, struct ir3_register *reg)
    }
    return false;
 }
+
+static inline bool
+regmask_get_any_gpr(regmask_t *regmask)
+{
+   return BITSET_TEST_RANGE(regmask->full, 0, GPR_REG_SIZE * 2) ||
+      BITSET_TEST_RANGE(regmask->half, 0, GPR_REG_SIZE);
+}
 /* ************************************************************************* */
 
 struct ir3_nop_state {

src/freedreno/ir3/ir3_legalize.c
@@ -754,6 +754,11 @@ legalize_block(struct ir3_legalize_ctx *ctx, struct ir3_block *block)
       if (writes_addr1(n) && block->in_early_preamble)
          n->srcs[0]->flags |= IR3_REG_R;
 
+      if ((n->opc == OPC_SHPE) && (ctx->compiler->gen >= 8) &&
+          regmask_get_any_gpr(&state->needs_sy)) {
+         last_n = insert_nop_flags(ctx, state, last_n, &build, IR3_INSTR_SY);
+      }
+
       /* cat5+ does not have an (ss) bit, if needed we need to
        * insert a nop to carry the sync flag.  Would be kinda
        * clever if we were aware of this during scheduling, but

commit 5f8db030cd5ed8b8107e773f5d00b7ffe39fdb3e

ir3: Fix early-preamble (sy)
This is about writes to shared regs, not GPR (as early-preamble can only
use shared regs).  It's a pretty hypothetical case, but might as well
get it correct.

Fixes: 2cd927cc2b26 ("ir3: Add (sy) before end of preamble when necessary")
Reported-by: Job Noorman <jnoorman@igalia.com>
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38868>

src/freedreno/ir3/ir3.h
@@ -3352,10 +3352,9 @@ regmask_get(regmask_t *regmask, struct ir3_register *reg)
 }
 
 static inline bool
-regmask_get_any_gpr(regmask_t *regmask)
+regmask_get_any_shared(regmask_t *regmask)
 {
-   return BITSET_TEST_RANGE(regmask->full, 0, GPR_REG_SIZE * 2) ||
-      BITSET_TEST_RANGE(regmask->half, 0, GPR_REG_SIZE);
+   return BITSET_TEST_RANGE(regmask->shared, 0, 2 * SHARED_REG_SIZE);
 }
 /* ************************************************************************* */
 

src/freedreno/ir3/ir3_legalize.c
@@ -754,8 +754,10 @@ legalize_block(struct ir3_legalize_ctx *ctx, struct ir3_block *block)
             n->flags |= IR3_INSTR_U;
       }
 
+      /* Ensure no pending uGPR writes before EP ends: */
       if ((n->opc == OPC_SHPE) && (ctx->compiler->gen >= 8) &&
-          regmask_get_any_gpr(&state->needs_sy)) {
+          block->in_early_preamble &&
+          regmask_get_any_shared(&state->needs_sy)) {
          last_n = insert_nop_flags(ctx, state, last_n, &build, IR3_INSTR_SY);
       }
 