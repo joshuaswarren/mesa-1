joshuaswarren/mesa-1

commit 45851b93192254fdebc494eed7f2947143425ff6

panfrost: allocate tile-buffer for dummy render-targets
There's two limitations we have to cater to:

1. The HW needs at least one render-target. We can disable write-back for
   it, but it needs to allocate tile-buffer space for it.
2. The HW can't have "holes" in the render-targets.

In both of those cases, we already set up dummy RGBA8 UNORM as the format,
and disable write-back. But we forgot to take this into account when
calculating the tile buffer allocation.

This makes what we program the HW to do consistent, meaning we don't end
up smashing the tile-buffer space. We might be able to do something
better by adjusting how we program these buffers, but let's leave that
for later.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33925>

src/panfrost/lib/pan_desc.c
@@ -354,15 +354,23 @@ pan_bytes_per_pixel_tib(enum pipe_format format)
 static unsigned
 pan_cbuf_bytes_per_pixel(const struct pan_fb_info *fb)
 {
+   /* dummy/non-existent render-targets use RGBA8 UNORM, e.g 4 bytes */
+   const unsigned dummy_rt_size = 4 * fb->nr_samples;
+
    unsigned sum = 0;
 
+   if (!fb->rt_count) {
+      /* The HW needs at least one render-target */
+      return dummy_rt_size;
+   }
+
    for (int cb = 0; cb < fb->rt_count; ++cb) {
+      unsigned rt_size = dummy_rt_size;
       const struct pan_image_view *rt = fb->rts[cb].view;
+      if (rt)
+         rt_size = pan_bytes_per_pixel_tib(rt->format) * rt->nr_samples;
 
-      if (!rt)
-         continue;
-
-      sum += pan_bytes_per_pixel_tib(rt->format) * rt->nr_samples;
+      sum += rt_size;
    }
 
    return sum;

commit 3a4d2411c87d0b152f7e96c9fef0289f9f31a269

panfrost: do not over-estimate memory needed for dummy-rt
This matches better what we do in pan_emit_fbd, where we don't increase
the cbuf_offset variable for unused render-targets. This way we simply
make sure we *at least* can fit a dummy-RT (as per the HW spec), but
since we don't write to it we also don't need to give it dedicated
memory beyond that.

This also seemingly fixes a subtle bug where we don't deal with PLS if
there's no active render-targets.

Fixes: 45851b93192 ("panfrost: allocate tile-buffer for dummy render-targets")
Fixes: d6b2dd7e2ad ("pan/lib: prepare for pixel local storage support")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38968>

src/panfrost/lib/pan_desc.c
@@ -471,23 +471,20 @@ pan_bytes_per_pixel_tib(enum pipe_format format)
 static unsigned
 pan_cbuf_bytes_per_pixel(const struct pan_fb_info *fb)
 {
-   /* dummy/non-existent render-targets use RGBA8 UNORM, e.g 4 bytes */
-   const unsigned dummy_rt_size = 4 * fb->nr_samples;
-
+   bool need_dummy = false;
    unsigned sum = 0;
 
-   if (!fb->rt_count) {
-      /* The HW needs at least one render-target */
-      return dummy_rt_size;
-   }
-
-   for (int cb = 0; cb < fb->rt_count; ++cb) {
-      unsigned rt_size = dummy_rt_size;
+   for (int cb = 0; cb < MAX2(fb->rt_count, 1); ++cb) {
       const struct pan_image_view *rt = fb->rts[cb].view;
       if (rt)
-         rt_size = pan_bytes_per_pixel_tib(rt->format) * rt->nr_samples;
+         sum += pan_bytes_per_pixel_tib(rt->format) * rt->nr_samples;
+      else
+         need_dummy = true;
+   }
 
-      sum += rt_size;
+   if (need_dummy) {
+      /* dummy/non-existent render-targets use RGBA8 UNORM, e.g 4 bytes */
+      sum = MAX2(sum, 4 * fb->nr_samples);
    }
 
    if (fb->pls_enabled) {