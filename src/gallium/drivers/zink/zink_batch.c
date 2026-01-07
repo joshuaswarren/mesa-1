joshuaswarren/mesa-1

commit 969f348d29497f141968ea8317620ec78ca53f50

zink: add renderdoc handling
renderdoc won't work with zink in frontends that aren't dri,
so ZINK_RENDERDOC should be used to specify start:end frames
to ensure that the vulkan command stream is captured

this is not a renderdoc issue: there are no frame boundaries in rusticl
or gallium-nine, so there is no possible way that renderdoc could
determine when/how to split frames

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20651>

meson.build
@@ -1438,7 +1438,8 @@ if not ['linux'].contains(host_machine.system())
   endif
 endif
 
-foreach h : ['xlocale.h', 'linux/futex.h', 'endian.h', 'dlfcn.h', 'sys/shm.h', 'cet.h', 'pthread_np.h']
+foreach h : ['xlocale.h', 'linux/futex.h', 'endian.h', 'dlfcn.h', 'sys/shm.h',
+             'cet.h', 'pthread_np.h', 'renderdoc_app.h']
   if cc.check_header(h)
     pre_args += '-DHAVE_@0@'.format(h.to_upper().underscorify())
   endif

src/gallium/drivers/zink/zink_batch.c
@@ -437,6 +437,26 @@ zink_start_batch(struct zink_context *ctx, struct zink_batch *batch)
       batch->last_batch_usage = &last_state->usage;
    }
 
+#ifdef HAVE_RENDERDOC_APP_H
+   if (VKCTX(CmdInsertDebugUtilsLabelEXT) && screen->renderdoc_api) {
+      VkDebugUtilsLabelEXT capture_label;
+      /* Magic fallback which lets us bridge the Wine barrier over to Linux RenderDoc. */
+      capture_label.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT;
+      capture_label.pNext = NULL;
+      capture_label.pLabelName = "vr-marker,frame_end,type,application";
+      memset(capture_label.color, 0, sizeof(capture_label.color));
+      VKCTX(CmdInsertDebugUtilsLabelEXT)(batch->state->barrier_cmdbuf, &capture_label);
+      VKCTX(CmdInsertDebugUtilsLabelEXT)(batch->state->cmdbuf, &capture_label);
+   }
+
+   unsigned renderdoc_frame = p_atomic_read(&screen->renderdoc_frame);
+   if (!(ctx->flags & ZINK_CONTEXT_COPY_ONLY) && screen->renderdoc_api && !screen->renderdoc_capturing &&
+        ((screen->renderdoc_capture_all && screen->screen_id == 1) || (renderdoc_frame >= screen->renderdoc_capture_start && renderdoc_frame <= screen->renderdoc_capture_end))) {
+      screen->renderdoc_api->StartFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
+      screen->renderdoc_capturing = true;
+   }
+#endif
+
    if (!ctx->queries_disabled)
       zink_resume_queries(ctx, batch);
 
@@ -635,6 +655,12 @@ zink_end_batch(struct zink_context *ctx, struct zink_batch *batch)
       submit_queue(bs, NULL, 0);
       post_submit(bs, NULL, 0);
    }
+#ifdef HAVE_RENDERDOC_APP_H
+   if (!(ctx->flags & ZINK_CONTEXT_COPY_ONLY) && screen->renderdoc_capturing && p_atomic_read(&screen->renderdoc_frame) > screen->renderdoc_capture_end) {
+      screen->renderdoc_api->EndFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
+      screen->renderdoc_capturing = false;
+   }
+#endif
 }
 
 static int

src/gallium/drivers/zink/zink_context.c
@@ -3730,8 +3730,11 @@ zink_flush(struct pipe_context *pctx,
       ctx->rp_changed |= fbfetch_outputs > 0;
    }
 
-   if (ctx->needs_present && (flags & PIPE_FLUSH_END_OF_FRAME)) {
-      if (ctx->needs_present->obj->image)
+   if (flags & PIPE_FLUSH_END_OF_FRAME) {
+#ifdef HAVE_RENDERDOC_APP_H
+      p_atomic_inc(&screen->renderdoc_frame);
+#endif
+      if (ctx->needs_present && ctx->needs_present->obj->image)
          zink_screen(ctx->base.screen)->image_barrier(ctx, ctx->needs_present, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR, 0, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT);
       ctx->needs_present = NULL;
    }

src/gallium/drivers/zink/zink_screen.c
@@ -49,6 +49,8 @@
 
 #include "util/u_cpu_detect.h"
 
+static int num_screens = 0;
+
 #if DETECT_OS_WINDOWS
 #include <io.h>
 #define VK_LIBNAME "vulkan-1.dll"
@@ -1384,6 +1386,11 @@ zink_destroy_screen(struct pipe_screen *pscreen)
 {
    struct zink_screen *screen = zink_screen(pscreen);
 
+#ifdef HAVE_RENDERDOC_APP_H
+   if (screen->renderdoc_capture_all && p_atomic_dec_zero(&num_screens))
+      screen->renderdoc_api->EndFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
+#endif
+
    hash_table_foreach(&screen->dts, entry)
       zink_kopper_deinit_displaytarget(screen, entry->data);
    simple_mtx_destroy(&screen->dt_lock);
@@ -1977,6 +1984,44 @@ populate_format_props(struct zink_screen *screen)
       screen->need_2D_sparse = !screen->base.get_sparse_texture_virtual_page_size(&screen->base, PIPE_TEXTURE_1D, false, PIPE_FORMAT_R32_FLOAT, 0, 16, NULL, NULL, NULL);
 }
 
+static void
+setup_renderdoc(struct zink_screen *screen)
+{
+#ifdef HAVE_RENDERDOC_APP_H
+   void *renderdoc = dlopen("librenderdoc.so", RTLD_NOW | RTLD_NOLOAD);
+   /* not loaded */
+   if (!renderdoc)
+      return;
+
+   pRENDERDOC_GetAPI get_api = dlsym(renderdoc, "RENDERDOC_GetAPI");
+   if (!get_api)
+      return;
+
+   /* need synchronous dispatch for renderdoc coherency */
+   screen->threaded = false;
+   get_api(eRENDERDOC_API_Version_1_0_0, (void*)&screen->renderdoc_api);
+   screen->renderdoc_api->SetActiveWindow(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
+
+   const char *capture_id = debug_get_option("ZINK_RENDERDOC", NULL);
+   if (!capture_id)
+      return;
+   int count = sscanf(capture_id, "%u:%u", &screen->renderdoc_capture_start, &screen->renderdoc_capture_end);
+   if (count != 2) {
+      count = sscanf(capture_id, "%u", &screen->renderdoc_capture_start);
+      if (!count) {
+         if (!strcmp(capture_id, "all")) {
+            screen->renderdoc_capture_all = true;
+         } else {
+            printf("`ZINK_RENDERDOC` usage: ZINK_RENDERDOC=all|frame_no[:end_frame_no]\n");
+            abort();
+         }
+      }
+      screen->renderdoc_capture_end = screen->renderdoc_capture_start;
+   }
+   p_atomic_set(&screen->renderdoc_frame, 1);
+#endif
+}
+
 bool
 zink_screen_init_semaphore(struct zink_screen *screen)
 {
@@ -2536,6 +2581,7 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
       goto fail;
    }
 
+   setup_renderdoc(screen);
    if (screen->threaded && !util_queue_init(&screen->flush_queue, "zfq", 8, 1, UTIL_QUEUE_INIT_RESIZE_IF_FULL, screen)) {
       mesa_loge("zink: Failed to create flush queue.\n");
       goto fail;
@@ -2805,6 +2851,8 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
                           !screen->driver_workarounds.no_hw_gl_point;
    if (!screen->optimal_keys)
       screen->info.have_EXT_graphics_pipeline_library = false;
+
+   screen->screen_id = p_atomic_inc_return(&num_screens);
 
    return screen;
 

src/gallium/drivers/zink/zink_types.h
@@ -61,6 +61,9 @@
 #include "zink_shader_keys.h"
 #include "vk_dispatch_table.h"
 
+#ifdef HAVE_RENDERDOC_APP_H
+#include "renderdoc_app.h"
+#endif
 
 /* the descriptor binding id for fbfetch/input attachment */
 #define ZINK_FBFETCH_BINDING 5
@@ -1294,6 +1297,17 @@ struct zink_screen {
    VkDebugUtilsMessengerEXT debugUtilsCallbackHandle;
 
    uint32_t cur_custom_border_color_samplers;
+
+   unsigned screen_id;
+
+#ifdef HAVE_RENDERDOC_APP_H
+   RENDERDOC_API_1_0_0 *renderdoc_api;
+   unsigned renderdoc_capture_start;
+   unsigned renderdoc_capture_end;
+   unsigned renderdoc_frame;
+   bool renderdoc_capturing;
+   bool renderdoc_capture_all;
+#endif
 
    struct vk_dispatch_table vk;
 

commit 0ce268bbdeb4dd3b5601c880ce462a38e3686ccd

vk/runtime,zink: only integrate renderdoc on supported platforms
It is not actually available to all the platforms mesa can be compiled
to, so let's keep an opt-in list of supported platforms instead, and
compile it out on all other platforms.

Fixes: 969f348d29497f141968 ("zink: add renderdoc handling")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39176>

meson.build
@@ -2228,6 +2228,13 @@ if get_option('android-libperfetto').enabled()
   pre_args += '-DANDROID_LIBPERFETTO'
 endif
 
+# Platforms where RenderDoc integration actually works
+renderdoc_integration_supported = [
+  'linux',
+  'android',
+].contains(host_machine.system())
+pre_args += '-DHAVE_RENDERDOC_INTEGRATION=@0@'.format(renderdoc_integration_supported)
+
 with_teflon = get_option('teflon')
 if with_teflon and with_tests
   dep_flatbuffers = dependency('flatbuffers')

src/gallium/drivers/zink/zink_batch.c
@@ -570,6 +570,7 @@ zink_start_batch(struct zink_context *ctx)
 
    bs->fence.completed = false;
 
+#if HAVE_RENDERDOC_INTEGRATION
    if (VKCTX(CmdInsertDebugUtilsLabelEXT) && screen->renderdoc_api) {
       VkDebugUtilsLabelEXT capture_label;
       /* Magic fallback which lets us bridge the Wine barrier over to Linux RenderDoc. */
@@ -588,6 +589,7 @@ zink_start_batch(struct zink_context *ctx)
       screen->renderdoc_api->StartFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
       screen->renderdoc_capturing = true;
    }
+#endif
 
    /* descriptor buffers must always be bound at the start of a batch */
    if (zink_descriptor_mode == ZINK_DESCRIPTOR_MODE_DB && !(ctx->flags & ZINK_CONTEXT_COPY_ONLY))
@@ -940,11 +942,13 @@ zink_end_batch(struct zink_context *ctx)
       submit_queue(bs, NULL, 0);
    }
 
+#if HAVE_RENDERDOC_INTEGRATION
    if (!(ctx->flags & ZINK_CONTEXT_COPY_ONLY) && screen->renderdoc_capturing && !screen->renderdoc_capture_all &&
        p_atomic_read(&screen->renderdoc_frame) > screen->renderdoc_capture_end) {
       screen->renderdoc_api->EndFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
       screen->renderdoc_capturing = false;
    }
+#endif
 }
 
 static int

src/gallium/drivers/zink/zink_context.c
@@ -116,11 +116,13 @@ zink_context_destroy(struct pipe_context *pctx)
    struct pipe_framebuffer_state fb = {0};
    pctx->set_framebuffer_state(pctx, &fb);
 
+#if HAVE_RENDERDOC_INTEGRATION
    if (screen->base.num_contexts == 1 && screen->renderdoc_capturing) {
       screen->renderdoc_capture_all = false;
       ctx->bs->has_work = true;
       pctx->flush(pctx, NULL, 0);
    }
+#endif
 
    if (util_queue_is_initialized(&screen->flush_queue))
       util_queue_finish(&screen->flush_queue);
@@ -4223,7 +4225,9 @@ zink_flush(struct pipe_context *pctx,
    }
 
    if (flags & PIPE_FLUSH_END_OF_FRAME) {
+#if HAVE_RENDERDOC_INTEGRATION
       p_atomic_inc(&screen->renderdoc_frame);
+#endif
       if (ctx->needs_present && ctx->needs_present->obj->dt_idx != UINT32_MAX &&
           zink_is_swapchain(ctx->needs_present)) {
          zink_kopper_readback_update(ctx, ctx->needs_present);

src/gallium/drivers/zink/zink_screen.c
@@ -1545,8 +1545,10 @@ zink_destroy_screen(struct pipe_screen *pscreen)
       }
    }
 
+#if HAVE_RENDERDOC_INTEGRATION
    if (screen->renderdoc_capture_all && p_atomic_dec_zero(&num_screens))
       screen->renderdoc_api->EndFrameCapture(RENDERDOC_DEVICEPOINTER_FROM_VKINSTANCE(screen->instance), NULL);
+#endif
 
    hash_table_foreach(&screen->dts, entry)
       zink_kopper_deinit_displaytarget(screen, entry->data);
@@ -2326,7 +2328,7 @@ populate_format_props(struct zink_screen *screen)
 static void
 setup_renderdoc(struct zink_screen *screen)
 {
-#ifndef _WIN32
+#if HAVE_RENDERDOC_INTEGRATION
    const char *capture_id = debug_get_option("ZINK_RENDERDOC", NULL);
    if (!capture_id)
       return;

src/gallium/drivers/zink/zink_types.h
@@ -66,7 +66,9 @@
 #include "vk_dispatch_table.h"
 #include "util/perf/cpu_trace.h"
 
+#if HAVE_RENDERDOC_INTEGRATION
 #include "renderdoc_app.h"
+#endif
 
 /* the descriptor binding id for fbfetch/input attachment */
 #define ZINK_FBFETCH_BINDING 5
@@ -1487,12 +1489,14 @@ struct zink_screen {
 
    unsigned screen_id;
 
+#if HAVE_RENDERDOC_INTEGRATION
    RENDERDOC_API_1_0_0 *renderdoc_api;
    unsigned renderdoc_capture_start;
    unsigned renderdoc_capture_end;
    unsigned renderdoc_frame;
    bool renderdoc_capturing;
    bool renderdoc_capture_all;
+#endif
 
    struct vk_uncompacted_dispatch_table vk;
 

src/vulkan/runtime/vk_instance.c
@@ -210,7 +210,9 @@ vk_instance_init(struct vk_instance *instance,
       instance->trace_trigger_file = os_get_option_secure("MESA_VK_TRACE_TRIGGER");
    }
 
+#if HAVE_RENDERDOC_INTEGRATION
    simple_mtx_init(&instance->renderdoc_mtx, mtx_plain);
+#endif
 
 #if !VK_LITE_RUNTIME_INSTANCE
    glsl_type_singleton_init_or_ref();
@@ -238,7 +240,9 @@ vk_instance_finish(struct vk_instance *instance)
    glsl_type_singleton_decref();
 #endif
 
+#if HAVE_RENDERDOC_INTEGRATION
    simple_mtx_destroy(&instance->renderdoc_mtx);
+#endif
 
    if (unlikely(!list_is_empty(&instance->debug_utils.callbacks))) {
       list_for_each_entry_safe(struct vk_debug_utils_messenger, messenger,
@@ -647,7 +651,7 @@ vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t *pSupportedVersion)
 void
 vk_instance_start_renderdoc_capture(struct vk_instance *instance)
 {
-#ifndef _WIN32
+#if HAVE_RENDERDOC_INTEGRATION
    simple_mtx_lock(&instance->renderdoc_mtx);
 
    if (!instance->renderdoc_api) {
@@ -669,6 +673,7 @@ vk_instance_start_renderdoc_capture(struct vk_instance *instance)
 void
 vk_instance_end_renderdoc_capture(struct vk_instance *instance)
 {
+#if HAVE_RENDERDOC_INTEGRATION
    if (!instance->renderdoc_api)
       return;
 
@@ -678,4 +683,5 @@ vk_instance_end_renderdoc_capture(struct vk_instance *instance)
       instance->renderdoc_api->EndFrameCapture(NULL, NULL);
 
    simple_mtx_unlock(&instance->renderdoc_mtx);
+#endif
 }

src/vulkan/runtime/vk_instance.h
@@ -32,7 +32,9 @@
 #include "util/simple_mtx.h"
 #include "util/u_debug.h"
 
+#if HAVE_RENDERDOC_INTEGRATION
 #include "renderdoc_app.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -180,9 +182,11 @@ struct vk_instance {
    /** Whether the capture mode is per-submit. */
    bool trace_per_submit;
 
+#if HAVE_RENDERDOC_INTEGRATION
    /** For triggering renderdoc captures from inside the driver. */
    simple_mtx_t renderdoc_mtx;
    RENDERDOC_API_1_0_0 *renderdoc_api;
+#endif
 };
 
 VK_DEFINE_HANDLE_CASTS(vk_instance, base, VkInstance,