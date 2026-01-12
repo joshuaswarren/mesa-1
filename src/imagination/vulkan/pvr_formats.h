joshuaswarren/mesa-1

commit 8ee1452832276f019446453be378f096ff7c9e41

pvr: Enable PBE_FILTERABLE_F16
Signed-off-by: Ashish Chauhan <Ashish.Chauhan@imgtec.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36412>

src/imagination/vulkan/pvr_formats.h
@@ -245,7 +245,8 @@ uint32_t pvr_get_tex_format_aspect(VkFormat vk_format,
 uint32_t pvr_get_pbe_packmode(VkFormat vk_format);
 uint32_t pvr_get_pbe_accum_format(VkFormat vk_format);
 uint32_t pvr_get_pbe_accum_format_size_in_bytes(VkFormat vk_format);
-bool pvr_format_is_pbe_downscalable(VkFormat vk_format);
+bool pvr_format_is_pbe_downscalable(const struct pvr_device_info *dev_info,
+                                    VkFormat vk_format);
 
 void pvr_get_hw_clear_color(VkFormat vk_format,
                             VkClearColorValue value,

src/imagination/vulkan/pvr_formats.c
@@ -1151,7 +1151,8 @@ unsupported:
       };
 }
 
-bool pvr_format_is_pbe_downscalable(VkFormat vk_format)
+bool pvr_format_is_pbe_downscalable(const struct pvr_device_info *dev_info,
+                                    VkFormat vk_format)
 {
    if (vk_format_is_int(vk_format)) {
       /* PBE downscale behavior for integer formats does not match Vulkan
@@ -1164,7 +1165,8 @@ bool pvr_format_is_pbe_downscalable(VkFormat vk_format)
    switch (pvr_get_pbe_packmode(vk_format)) {
    default:
       return true;
-
+   case ROGUE_PBESTATE_PACKMODE_F16:
+      return PVR_HAS_FEATURE(dev_info, pbe_filterable_f16);
    case ROGUE_PBESTATE_PACKMODE_U16U16U16U16:
    case ROGUE_PBESTATE_PACKMODE_S16S16S16S16:
    case ROGUE_PBESTATE_PACKMODE_U32U32U32U32:

src/imagination/vulkan/pvr_pass.c
@@ -493,7 +493,8 @@ VkResult pvr_CreateRenderPass2(VkDevice _device,
       attachment->sample_count = desc->samples;
       attachment->initial_layout = desc->initialLayout;
       attachment->is_pbe_downscalable =
-         pvr_format_is_pbe_downscalable(attachment->vk_format);
+         pvr_format_is_pbe_downscalable(&device->pdevice->dev_info,
+                                        attachment->vk_format);
       attachment->index = i;
 
       if (attachment->sample_count > pass->max_sample_count)

commit 9915612e81a352d6f896648b9032b50e74fe15cd

pvr: add missing forward-declaration
Not sure how we missed this, but we need the declaration here. This
probably only worked because of transitative includes, which we shound't
rely on.

Fixes: 8ee14528322 ("pvr: Enable PBE_FILTERABLE_F16")
Reviewed-by: Ashish Chauhan <ashish.chauhan@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39035>

src/imagination/vulkan/pvr_formats.h
@@ -34,6 +34,8 @@
 #include "util/format/u_formats.h"
 #include "vk_format.h"
 
+struct pvr_device_info;
+
 /* This is based on VkClearColorValue which is an array of RGBA, and on the
  * output register usage for the biggest 32 bit 4 component formats which use up
  * all 4 output registers.