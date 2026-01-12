joshuaswarren/mesa-1

commit 30a1714176d48bbd0eaaec957f028aa756bb1b5f

pvr: build pvr_arch_*.c as a multi-arch sources
This will allow us to build this multiple times for different
architectures. For now, it only defines a single architecture, because
that's what we currently support. But this makes room for future
architectures, that will follow relatively soon.

Co-authored-by: Ashish Chauhan <ashish.chauhan@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38922>

src/imagination/vulkan/pvr_arch_framebuffer.c
@@ -23,7 +23,6 @@
 #include "pvr_hw_pass.h"
 #include "pvr_image.h"
 #include "pvr_pass.h"
-#include "pvr_physical_device.h"
 #include "pvr_rt_dataset.h"
 #include "pvr_spm.h"
 
@@ -92,12 +91,12 @@ err_mutex_destroy:
    return false;
 }
 
-VkResult
-pvr_render_state_setup(struct pvr_device *device,
-                       const VkAllocationCallbacks *pAllocator,
-                       struct pvr_render_state *rstate,
-                       uint32_t render_count,
-                       const struct pvr_renderpass_hwsetup_render *renders)
+VkResult PVR_PER_ARCH(render_state_setup)(
+   struct pvr_device *device,
+   const VkAllocationCallbacks *pAllocator,
+   struct pvr_render_state *rstate,
+   uint32_t render_count,
+   const struct pvr_renderpass_hwsetup_render *renders)
 {
    struct pvr_spm_bgobj_state *spm_bgobj_state_per_render;
    struct pvr_spm_eot_state *spm_eot_state_per_render;
@@ -211,10 +210,11 @@ pvr_render_pass_get_scratch_buffer_size(struct pvr_device *device,
       rstate->height);
 }
 
-VkResult pvr_CreateFramebuffer(VkDevice _device,
-                               const VkFramebufferCreateInfo *pCreateInfo,
-                               const VkAllocationCallbacks *pAllocator,
-                               VkFramebuffer *pFramebuffer)
+VkResult
+PVR_PER_ARCH(CreateFramebuffer)(VkDevice _device,
+                                const VkFramebufferCreateInfo *pCreateInfo,
+                                const VkAllocationCallbacks *pAllocator,
+                                VkFramebuffer *pFramebuffer)
 {
    VK_FROM_HANDLE(pvr_render_pass, pass, pCreateInfo->renderPass);
    VK_FROM_HANDLE(pvr_device, device, _device);
@@ -288,9 +288,9 @@ err_free_framebuffer:
    return result;
 }
 
-void pvr_DestroyFramebuffer(VkDevice _device,
-                            VkFramebuffer _fb,
-                            const VkAllocationCallbacks *pAllocator)
+void PVR_PER_ARCH(DestroyFramebuffer)(VkDevice _device,
+                                      VkFramebuffer _fb,
+                                      const VkAllocationCallbacks *pAllocator)
 {
    VK_FROM_HANDLE(pvr_framebuffer, framebuffer, _fb);
    VK_FROM_HANDLE(pvr_device, device, _device);

src/imagination/vulkan/pvr_formats.h
@@ -28,6 +28,9 @@
 #include <stdint.h>
 #include <vulkan/vulkan.h>
 
+#include "pvr_macros.h"
+#include "pvr_physical_device.h"
+
 #include "util/format/u_formats.h"
 #include "vk_format.h"
 
@@ -223,21 +226,13 @@ struct pvr_format {
    uint32_t bind;
 };
 
-const struct pvr_format *pvr_get_format_table(unsigned *num_formats);
-
 struct util_format_description;
+
 const uint8_t *
 pvr_get_format_swizzle_for_tpu(const struct util_format_description *desc);
 const uint8_t *pvr_get_format_swizzle(VkFormat vk_format);
-uint32_t pvr_get_tex_format(VkFormat vk_format);
-uint32_t pvr_get_tex_format_aspect(VkFormat vk_format,
-                                   VkImageAspectFlags aspect_mask);
-uint32_t pvr_get_pbe_packmode(VkFormat vk_format);
-uint32_t pvr_get_pbe_accum_format(VkFormat vk_format);
+uint32_t pvr_rogue_get_pbe_accum_format(VkFormat vk_format);
 uint32_t pvr_get_pbe_accum_format_size_in_bytes(VkFormat vk_format);
-bool pvr_format_is_pbe_downscalable(const struct pvr_device_info *dev_info,
-                                    VkFormat vk_format);
-
 void pvr_get_hw_clear_color(VkFormat vk_format,
                             VkClearColorValue value,
                             uint32_t packed_out[static const 4]);
@@ -307,5 +302,31 @@ pvr_vk_format_get_common_color_channel_count(VkFormat src_format,
 
    return count;
 }
+
+#ifdef PVR_PER_ARCH
+
+const struct pvr_format *PVR_PER_ARCH(get_format_table)(unsigned *num_formats);
+#   define pvr_get_format_table PVR_PER_ARCH(get_format_table)
+
+uint32_t PVR_PER_ARCH(get_tex_format)(VkFormat vk_format);
+#   define pvr_get_tex_format PVR_PER_ARCH(get_tex_format)
+
+uint32_t PVR_PER_ARCH(get_tex_format_aspect)(VkFormat vk_format,
+                                             VkImageAspectFlags aspect_mask);
+#   define pvr_get_tex_format_aspect PVR_PER_ARCH(get_tex_format_aspect)
+
+uint32_t PVR_PER_ARCH(get_pbe_packmode)(VkFormat vk_format);
+#   define pvr_get_pbe_packmode PVR_PER_ARCH(get_pbe_packmode)
+
+uint32_t PVR_PER_ARCH(get_pbe_accum_format)(VkFormat vk_format);
+#   define pvr_get_pbe_accum_format PVR_PER_ARCH(get_pbe_accum_format)
+
+bool PVR_PER_ARCH(format_is_pbe_downscalable)(
+   const struct pvr_device_info *dev_info,
+   VkFormat vk_format);
+#   define pvr_format_is_pbe_downscalable \
+      PVR_PER_ARCH(format_is_pbe_downscalable)
+
+#endif /* PVR_PER_ARCH */
 
 #endif /* PVR_FORMATS_H */

src/imagination/vulkan/meson.build
@@ -27,28 +27,6 @@ pvr_files = files(
   'winsys/powervr/pvr_drm_job_transfer.c',
   'winsys/pvr_winsys.c',
   'winsys/pvr_winsys_helper.c',
-  'pvr_arch_border.c',
-  'pvr_arch_cmd_buffer.c',
-  'pvr_arch_cmd_query.c',
-  'pvr_arch_csb.c',
-  'pvr_arch_descriptor_set.c',
-  'pvr_arch_device.c',
-  'pvr_arch_formats.c',
-  'pvr_arch_framebuffer.c',
-  'pvr_arch_hw_pass.c',
-  'pvr_arch_image.c',
-  'pvr_arch_job_common.c',
-  'pvr_arch_job_compute.c',
-  'pvr_arch_job_context.c',
-  'pvr_arch_job_render.c',
-  'pvr_arch_job_transfer.c',
-  'pvr_arch_mrt.c',
-  'pvr_arch_pass.c',
-  'pvr_arch_queue.c',
-  'pvr_arch_query_compute.c',
-  'pvr_arch_sampler.c',
-  'pvr_arch_spm.c',
-  'pvr_arch_tex_state.c',
   'pvr_bo.c',
   'pvr_csb.c',
   'pvr_descriptor_set.c',
@@ -59,7 +37,6 @@ pvr_files = files(
   'pvr_image.c',
   'pvr_instance.c',
   'pvr_physical_device.c',
-  'pvr_pipeline.c',
   'pvr_transfer_frag_store.c',
   'pvr_query.c',
   'pvr_robustness.c',
@@ -113,6 +90,32 @@ common_per_arch_files = [
   pvr_entrypoints[0],
   sha1_h,
 ]
+
+common_per_arch_files += files(
+  'pvr_arch_border.c',
+  'pvr_arch_cmd_buffer.c',
+  'pvr_arch_cmd_query.c',
+  'pvr_arch_csb.c',
+  'pvr_arch_descriptor_set.c',
+  'pvr_arch_device.c',
+  'pvr_arch_formats.c',
+  'pvr_arch_framebuffer.c',
+  'pvr_arch_hw_pass.c',
+  'pvr_arch_pass.c',
+  'pvr_arch_pipeline.c',
+  'pvr_arch_image.c',
+  'pvr_arch_job_common.c',
+  'pvr_arch_job_compute.c',
+  'pvr_arch_job_context.c',
+  'pvr_arch_job_render.c',
+  'pvr_arch_job_transfer.c',
+  'pvr_arch_mrt.c',
+  'pvr_arch_queue.c',
+  'pvr_arch_query_compute.c',
+  'pvr_arch_sampler.c',
+  'pvr_arch_spm.c',
+  'pvr_arch_tex_state.c',
+)
 
 rogue_files = files(
   'rogue/pvr_blit.c',

src/imagination/vulkan/pvr_arch_border.c
@@ -415,7 +415,7 @@ static inline void pvr_border_color_swizzle_to_tex_format(
    *color = swizzled_color;
 }
 
-VkResult pvr_border_color_table_init(struct pvr_device *const device)
+VkResult PVR_PER_ARCH(border_color_table_init)(struct pvr_device *const device)
 {
    struct pvr_border_color_table *table = device->border_color_table =
       vk_zalloc(&device->vk.alloc,
@@ -470,7 +470,7 @@ err_out:
    return result;
 }
 
-void pvr_border_color_table_finish(struct pvr_device *const device)
+void PVR_PER_ARCH(border_color_table_finish)(struct pvr_device *const device)
 {
 #if MESA_DEBUG
    BITSET_SET_RANGE_INSIDE_WORD(device->border_color_table->unused_entries,
@@ -593,7 +593,7 @@ err_out:
                     "Failed to allocate border color table entry");
 }
 
-VkResult pvr_border_color_table_get_or_create_entry(
+VkResult PVR_PER_ARCH(border_color_table_get_or_create_entry)(
    struct pvr_device *const device,
    const struct pvr_sampler *const sampler,
    struct pvr_border_color_table *const table,
@@ -612,7 +612,7 @@ VkResult pvr_border_color_table_get_or_create_entry(
                                                      index_out);
 }
 
-void pvr_border_color_table_release_entry(
+void PVR_PER_ARCH(border_color_table_release_entry)(
    struct pvr_border_color_table *const table,
    const uint32_t index)
 {

src/imagination/vulkan/pvr_arch_cmd_query.c
@@ -11,10 +11,10 @@
 #include "pvr_pass.h"
 #include "pvr_query.h"
 
-void pvr_CmdResetQueryPool(VkCommandBuffer commandBuffer,
-                           VkQueryPool queryPool,
-                           uint32_t firstQuery,
-                           uint32_t queryCount)
+void PVR_PER_ARCH(CmdResetQueryPool)(VkCommandBuffer commandBuffer,
+                                     VkQueryPool queryPool,
+                                     uint32_t firstQuery,
+                                     uint32_t queryCount)
 {
    VK_FROM_HANDLE(pvr_cmd_buffer, cmd_buffer, commandBuffer);
    struct pvr_query_info query_info;
@@ -64,14 +64,14 @@ void pvr_CmdResetQueryPool(VkCommandBuffer commandBuffer,
    };
 }
 
-void pvr_CmdCopyQueryPoolResults(VkCommandBuffer commandBuffer,
-                                 VkQueryPool queryPool,
-                                 uint32_t firstQuery,
-                                 uint32_t queryCount,
-                                 VkBuffer dstBuffer,
-                                 VkDeviceSize dstOffset,
-                                 VkDeviceSize stride,
-                                 VkQueryResultFlags flags)
+void PVR_PER_ARCH(CmdCopyQueryPoolResults)(VkCommandBuffer commandBuffer,
+                                           VkQueryPool queryPool,
+                                           uint32_t firstQuery,
+                                           uint32_t queryCount,
+                                           VkBuffer dstBuffer,
+                                           VkDeviceSize dstOffset,
+                                           VkDeviceSize stride,
+                                           VkQueryResultFlags flags)
 {
    VK_FROM_HANDLE(pvr_cmd_buffer, cmd_buffer, commandBuffer);
    struct pvr_query_info query_info;
@@ -151,10 +151,10 @@ pvr_cmd_buffer_state_get_view_count(const struct pvr_cmd_buffer_state *state)
    return view_count;
 }
 
-void pvr_CmdBeginQuery(VkCommandBuffer commandBuffer,
-                       VkQueryPool queryPool,
-                       uint32_t query,
-                       VkQueryControlFlags flags)
+void PVR_PER_ARCH(CmdBeginQuery)(VkCommandBuffer commandBuffer,
+                                 VkQueryPool queryPool,
+                                 uint32_t query,
+                                 VkQueryControlFlags flags)
 {
    VK_FROM_HANDLE(pvr_cmd_buffer, cmd_buffer, commandBuffer);
    struct pvr_cmd_buffer_state *state = &cmd_buffer->state;
@@ -208,9 +208,9 @@ void pvr_CmdBeginQuery(VkCommandBuffer commandBuffer,
    }
 }
 
-void pvr_CmdEndQuery(VkCommandBuffer commandBuffer,
-                     VkQueryPool queryPool,
-                     uint32_t query)
+void PVR_PER_ARCH(CmdEndQuery)(VkCommandBuffer commandBuffer,
+                               VkQueryPool queryPool,
+                               uint32_t query)
 {
    VK_FROM_HANDLE(pvr_cmd_buffer, cmd_buffer, commandBuffer);
    struct pvr_cmd_buffer_state *state = &cmd_buffer->state;

src/imagination/vulkan/pvr_arch_csb.c
@@ -188,7 +188,7 @@ static bool pvr_csb_buffer_extend(struct pvr_csb *csb)
  * \param[in] num_dwords Number of dwords to allocate.
  * \return Valid host virtual address or NULL otherwise.
  */
-void *pvr_csb_alloc_dwords(struct pvr_csb *csb, uint32_t num_dwords)
+void *PVR_PER_ARCH(csb_alloc_dwords)(struct pvr_csb *csb, uint32_t num_dwords)
 {
    const uint32_t required_space = PVR_DW_TO_BYTES(num_dwords);
    void *p;
@@ -241,7 +241,8 @@ void *pvr_csb_alloc_dwords(struct pvr_csb *csb, uint32_t num_dwords)
  * \param[in,out] csb_dst Destination control Stream Builder object.
  * \param[in]     csb_src Source Control Stream Builder object.
  */
-VkResult pvr_csb_copy(struct pvr_csb *csb_dst, struct pvr_csb *csb_src)
+VkResult PVR_PER_ARCH(csb_copy)(struct pvr_csb *csb_dst,
+                                struct pvr_csb *csb_src)
 {
    const uint8_t stream_reserved_space =
       PVR_DW_TO_BYTES(pvr_cmd_length(VDMCTRL_STREAM_LINK0) +
@@ -269,7 +270,7 @@ VkResult pvr_csb_copy(struct pvr_csb *csb_dst, struct pvr_csb *csb_src)
       assert(!"CSB source buffer too large to do a full copy");
    }
 
-   destination = pvr_csb_alloc_dwords(csb_dst, size);
+   destination = PVR_PER_ARCH(csb_alloc_dwords)(csb_dst, size);
    if (!destination) {
       assert(csb_dst->status != VK_SUCCESS);
       return csb_dst->status;
@@ -289,7 +290,9 @@ VkResult pvr_csb_copy(struct pvr_csb *csb_dst, struct pvr_csb *csb_src)
  * \param[in] ret  Selects whether the sub control stream will return or
  *                 terminate.
  */
-void pvr_csb_emit_link(struct pvr_csb *csb, pvr_dev_addr_t addr, bool ret)
+void PVR_PER_ARCH(csb_emit_link)(struct pvr_csb *csb,
+                                 pvr_dev_addr_t addr,
+                                 bool ret)
 {
    pvr_csb_set_relocation_mark(csb);
    pvr_csb_emit_link_unmarked(csb, addr, ret);
@@ -304,7 +307,7 @@ void pvr_csb_emit_link(struct pvr_csb *csb, pvr_dev_addr_t addr, bool ret)
  * \param[in] csb Control Stream Builder object to add VDMCTRL_STREAM_RETURN to.
  * \return VK_SUCCESS on success, or error code otherwise.
  */
-VkResult pvr_csb_emit_return(struct pvr_csb *csb)
+VkResult PVR_PER_ARCH(csb_emit_return)(struct pvr_csb *csb)
 {
    /* STREAM_RETURN is only supported by graphics control stream. */
    assert(csb->stream_type == PVR_CMD_STREAM_TYPE_GRAPHICS ||
@@ -327,7 +330,7 @@ VkResult pvr_csb_emit_return(struct pvr_csb *csb)
  * \param[in] csb Control Stream Builder object to terminate.
  * \return VK_SUCCESS on success, or error code otherwise.
  */
-VkResult pvr_csb_emit_terminate(struct pvr_csb *csb)
+VkResult PVR_PER_ARCH(csb_emit_terminate)(struct pvr_csb *csb)
 {
    pvr_csb_set_relocation_mark(csb);
 

src/imagination/vulkan/pvr_arch_descriptor_set.c
@@ -230,7 +230,7 @@ write_buffer_view(const struct pvr_descriptor_set *set,
    memcpy(desc_mapping, &buffer_view_state, sizeof(buffer_view_state));
 }
 
-void pvr_descriptor_set_write_immutable_samplers(
+void PVR_PER_ARCH(descriptor_set_write_immutable_samplers)(
    struct pvr_descriptor_set_layout *layout,
    struct pvr_descriptor_set *set)
 {
@@ -247,11 +247,12 @@ void pvr_descriptor_set_write_immutable_samplers(
    }
 }
 
-void pvr_UpdateDescriptorSets(VkDevice _device,
-                              uint32_t descriptorWriteCount,
-                              const VkWriteDescriptorSet *pDescriptorWrites,
-                              uint32_t descriptorCopyCount,
-                              const VkCopyDescriptorSet *pDescriptorCopies)
+void PVR_PER_ARCH(UpdateDescriptorSets)(
+   VkDevice _device,
+   uint32_t descriptorWriteCount,
+   const VkWriteDescriptorSet *pDescriptorWrites,
+   uint32_t descriptorCopyCount,
+   const VkCopyDescriptorSet *pDescriptorCopies)
 {
    VK_FROM_HANDLE(pvr_device, device, _device);
    const struct pvr_device_info *dev_info = &device->pdevice->dev_info;
@@ -416,7 +417,7 @@ void pvr_UpdateDescriptorSets(VkDevice _device,
    }
 }
 
-void pvr_UpdateDescriptorSetWithTemplate(
+void PVR_PER_ARCH(UpdateDescriptorSetWithTemplate)(
    VkDevice _device,
    VkDescriptorSet descriptorSet,
    VkDescriptorUpdateTemplate descriptorUpdateTemplate,

commit 8e1de64f612c15c279e6b4a13165fb9a5253649f

pvr: clean up include
We don't need pvr_physical_device.h included here, but we *do* need it
in pvr_arch_framebuffer.c, so let's move the include there. This will
help with avoiding circular includes.

Fixes: 30a1714176d ("pvr: build pvr_arch_*.c as a multi-arch sources")
Reviewed-by: Ashish Chauhan <ashish.chauhan@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39035>

src/imagination/vulkan/pvr_arch_framebuffer.c
@@ -23,6 +23,7 @@
 #include "pvr_hw_pass.h"
 #include "pvr_image.h"
 #include "pvr_pass.h"
+#include "pvr_physical_device.h"
 #include "pvr_rt_dataset.h"
 #include "pvr_spm.h"
 

src/imagination/vulkan/pvr_formats.h
@@ -29,7 +29,6 @@
 #include <vulkan/vulkan.h>
 
 #include "pvr_macros.h"
-#include "pvr_physical_device.h"
 
 #include "util/format/u_formats.h"
 #include "vk_format.h"