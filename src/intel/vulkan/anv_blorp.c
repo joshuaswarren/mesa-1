joshuaswarren/mesa-1

commit d97bda29483eef0fa4a80921ebb9b2ac9458ad1c

anv: Stop using RCS companion for MSAA copy/clear on Xe3+
On Xe3+, we have typed MSAA load/store message support. We can use them
during MSAA copies. We don't have to fallback on RCS companion queue
anymore.

Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33905>

src/intel/vulkan/anv_blorp.c
@@ -587,14 +587,17 @@ anv_blorp_execute_on_companion(struct anv_cmd_buffer *cmd_buffer,
    if (anv_cmd_buffer_is_render_queue(cmd_buffer))
       return false;
 
-   /* MSAA images have to be dealt with on the companion RCS command buffer
-    * for both CCS && BCS engines.
-    *
-    * TODO: relax this for Xe3+ on CCS when we have Blorp MSAA copies.
-    */
    if ((src_image && is_image_multisampled(src_image)) ||
-       (dst_image && is_image_multisampled(dst_image)))
-      return true;
+       (dst_image && is_image_multisampled(dst_image))) {
+      /* MSAA images have to be dealt with on the companion RCS command buffer
+       * for both CCS && BCS engines pre-Xe3.
+       */
+      if (devinfo->ver < 30)
+         return true;
+      /* Even on Xe3, no support for MSAA on BCS. */
+      if (anv_cmd_buffer_is_compute_queue(cmd_buffer))
+         return true;
+   }
 
    if (anv_cmd_buffer_is_blitter_queue(cmd_buffer)) {
       /* Emulation of formats is done through a compute shader, so we need the

commit 6bb1b90d2a669aeb12241ecad2887a8ec0b00096

anv: fix queue check in anv_blorp_execute_on_companion on xe3
Fixes:
   dEQP-VK.api.copy_and_blit.dedicated_allocation.resolve_image.whole_copy_before_resolving_transfer.2_bit

Otherwise we attempt to use blorp and hit various asserts later in:

- blorp_copy_supports_blitter
- blorp_xy_block_copy_blt

Fixes: d97bda29483 ("anv: Stop using RCS companion for MSAA copy/clear on Xe3+")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39346>

src/intel/vulkan/anv_blorp.c
@@ -595,7 +595,7 @@ anv_blorp_execute_on_companion(struct anv_cmd_buffer *cmd_buffer,
       if (devinfo->ver < 30)
          return true;
       /* Even on Xe3, no support for MSAA on BCS. */
-      if (anv_cmd_buffer_is_compute_queue(cmd_buffer))
+      if (anv_cmd_buffer_is_blitter_queue(cmd_buffer))
          return true;
    }
 