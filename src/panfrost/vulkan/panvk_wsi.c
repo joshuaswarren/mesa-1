joshuaswarren/mesa-1

commit a8f4dff10a8ddad7e8130c9499b83d078f42351e

panvk: override can_present_on_device
Non-PCI devices are responsible for checking device matches
themselves by overriding can_present_on_device on wsi_device
to take the non-blitting WSI path.

We can allow on-device presentation for both the display
controller, and any panthor node. The simplest way of
achieving that is allowing this for all devices with bus
type platform, as there's nothing else that has this
property.

Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37543>

src/panfrost/vulkan/panvk_wsi.c
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2021 Collabora Ltd.
+ * Copyright © 2025 Arm Ltd.
  *
  * Derived from tu_wsi.c:
  * Copyright © 2016 Red Hat
@@ -41,6 +42,17 @@ panvk_wsi_proc_addr(VkPhysicalDevice physicalDevice, const char *pName)
    return vk_instance_get_proc_addr_unchecked(&instance->vk, pName);
 }
 
+static bool
+panvk_can_present_on_device(VkPhysicalDevice pdevice, int fd)
+{
+   drmDevicePtr device;
+   if (drmGetDevice2(fd, 0, &device) != 0)
+      return false;
+   /* Allow on-device presentation for all devices with bus type PLATFORM.
+    * Other device types such as PCI or USB should use the PRIME blit path. */
+   return device->bustype == DRM_BUS_PLATFORM;
+}
+
 VkResult
 panvk_wsi_init(struct panvk_physical_device *physical_device)
 {
@@ -57,6 +69,8 @@ panvk_wsi_init(struct panvk_physical_device *physical_device)
       return result;
 
    physical_device->wsi_device.supports_modifiers = true;
+   physical_device->wsi_device.can_present_on_device =
+      panvk_can_present_on_device;
 
    physical_device->vk.wsi_device = &physical_device->wsi_device;
 

commit 58b0652dc854064600b8273cb94475afb3ae509c

panvk: Free drm device in can_present_on_device
Fixes: a8f4dff10a8 ("panvk: override can_present_on_device")
Signed-off-by: Kitlith <kitlith@kitl.pw>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39415>

src/panfrost/vulkan/panvk_wsi.c
@@ -33,7 +33,11 @@ panvk_can_present_on_device(VkPhysicalDevice pdevice, int fd)
       return false;
    /* Allow on-device presentation for all devices with bus type PLATFORM.
     * Other device types such as PCI or USB should use the PRIME blit path. */
-   return device->bustype == DRM_BUS_PLATFORM;
+   bool match = device->bustype == DRM_BUS_PLATFORM;
+
+   drmFreeDevice(&device);
+
+   return match;
 }
 
 VkResult