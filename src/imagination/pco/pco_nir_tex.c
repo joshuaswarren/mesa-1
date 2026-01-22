joshuaswarren/mesa-1

commit 30ed798e8b829ba1c214add7a88de72c022a3215

pco: restrict shadow sampler comparator clamping to unorm formats
Only clamp shadow sampler comparators for "unsigned normalized
fixed-point format[s]" as per the Vulkan spec.

Fixes: 4e9ce387cf4 ("pco: Fix for shadow sampler comparison not clamping the compare value")
Reported-by: Georg Lehmann <dadschoorse@gmail.com>
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39389>

src/imagination/pco/pco_nir_tex.c
@@ -429,9 +429,6 @@ static nir_def *lower_tex_shadow(nir_builder *b,
 {
    nir_def *result_comps[NIR_MAX_VEC_COMPONENTS];
 
-   /* Need to clamp the comparator to the range 0.0 - 1.0 */
-   comparator = nir_fsat(b, comparator);
-
    for (unsigned u = 0; u < data->num_components; ++u) {
       result_comps[u] =
          nir_alphatst_pco(b, nir_channel(b, data, u), comparator, compare_op);
@@ -744,6 +741,21 @@ static nir_def *lower_tex(nir_builder *b, nir_instr *instr, void *cb_data)
                                .desc_set = smp_desc_set,
                                .binding = smp_binding,
                                .component = PCO_SAMPLER_META_COMPARE_OP);
+
+      /*
+       * From the Vulkan 1.4.339 spec, 17.2.4. Depth Compare Operation:
+       *
+       * "If the image being sampled has an unsigned normalized fixed-point
+       * format, then Dref is clamped to [0,1] before the compare operation."
+       *
+       * -> Clamp the comparator to the range 0.0 - 1.0 for unorm formats.
+       */
+
+      nir_def *pck_info = nir_channel(b, tex_meta, PCO_IMAGE_META_PCK_INFO);
+      nir_def *is_unorm = nir_ubitfield_extract_imm(b, pck_info, 8, 1);
+      is_unorm = nir_ine_imm(b, is_unorm, 0);
+
+      comparator = nir_bcsel(b, is_unorm, nir_fsat(b, comparator), comparator);
 
       result = lower_tex_shadow(b, result, comparator, compare_op);
    }

src/imagination/vulkan/pvr_arch_tex_state.c
@@ -135,8 +135,9 @@ static uint32_t setup_pck_info(VkFormat vk_format)
       break;
    }
 
+   /* Invalidate the format bits, but clear the rest so they can still be set. */
    if (pck_format == ~0)
-      return pck_format;
+      pck_format = 0b11111;
 
    uint32_t pck_info = pck_format;
    if (split)
@@ -147,6 +148,9 @@ static uint32_t setup_pck_info(VkFormat vk_format)
 
    if (roundzero)
       pck_info |= BITFIELD_BIT(7);
+
+   if (util_format_is_unorm(format))
+      pck_info |= BITFIELD_BIT(8);
 
    return pck_info;
 }

commit deeab1f095032be4674ee2c52902e194e9c1512a

pco: update formatless skip check
The skip check should only be checking the format rather than the entire
packed word.

Fixes: 30ed798e8b8 ("pco: restrict shadow sampler comparator clamping to unorm formats")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39428>

src/imagination/pco/pco_nir_tex.c
@@ -1067,8 +1067,8 @@ static nir_def *lower_image(nir_builder *b, nir_instr *instr, void *cb_data)
                                                    .binding = binding);
 
          nir_def *pck_info = nir_channel(b, tex_meta, PCO_IMAGE_META_PCK_INFO);
-         nir_def *pck_skip = nir_ieq_imm(b, pck_info, 0xffffffff);
          nir_def *pck_format = nir_ubitfield_extract_imm(b, pck_info, 0, 5);
+         nir_def *pck_skip = nir_ieq_imm(b, pck_format, 0b11111);
          nir_def *pck_split = nir_ubitfield_extract_imm(b, pck_info, 5, 1);
          pck_split = nir_ine_imm(b, pck_split, 0);
          nir_def *pck_scale = nir_ubitfield_extract_imm(b, pck_info, 6, 1);