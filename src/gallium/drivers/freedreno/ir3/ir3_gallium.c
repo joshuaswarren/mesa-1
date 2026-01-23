joshuaswarren/mesa-1

commit 27434a45c6f90a01bc09c28c8c09558ab27c66db

ir3, tu: Plumb through support for per-shader robustness
We need to pass through the robust_modes flag to nir_opt_vectorize based
on a flag set when compiling the shader, not globally in the compiler,
for VK_EXT_pipeline_robustness. Refactor the ir3 compiler interface
to add an ir3_shader_nir_options struct that can be passed around to
the appropriate places, and wire it up in turnip to the shader key. The
shader key replaces the old mechanism of hashing in the compiler
options.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31687>

src/gallium/drivers/freedreno/ir3/ir3_gallium.c
@@ -275,6 +275,17 @@ ir3_shader_compute_state_create(struct pipe_context *pctx,
       return NULL;
    }
 
+   enum ir3_wavesize_option api_wavesize = IR3_SINGLE_OR_DOUBLE;
+   enum ir3_wavesize_option real_wavesize = IR3_SINGLE_OR_DOUBLE;
+
+   const struct ir3_shader_options ir3_options = {
+      /* TODO: force to single on a6xx with legacy ballot extension that uses
+       * 64-bit masks
+       */
+      .api_wavesize = api_wavesize,
+      .real_wavesize = real_wavesize,
+   };
+
    struct ir3_compiler *compiler = ctx->screen->compiler;
    nir_shader *nir;
 
@@ -290,7 +301,7 @@ ir3_shader_compute_state_create(struct pipe_context *pctx,
       blob_reader_init(&reader, hdr->blob, hdr->num_bytes);
       nir = nir_deserialize(NULL, options, &reader);
 
-      ir3_finalize_nir(compiler, nir);
+      ir3_finalize_nir(compiler, &ir3_options.nir_options, nir);
    } else {
       assert(cso->ir_type == PIPE_SHADER_IR_TGSI);
       if (ir3_shader_debug & IR3_DBG_DISASM) {
@@ -302,22 +313,13 @@ ir3_shader_compute_state_create(struct pipe_context *pctx,
    if (ctx->screen->gen >= 6)
       ir3_nir_lower_io_to_bindless(nir);
 
-   enum ir3_wavesize_option api_wavesize = IR3_SINGLE_OR_DOUBLE;
-   enum ir3_wavesize_option real_wavesize = IR3_SINGLE_OR_DOUBLE;
-
    if (ctx->screen->gen >= 6 && !ctx->screen->info->a6xx.supports_double_threadsize) {
       api_wavesize = IR3_SINGLE_ONLY;
       real_wavesize = IR3_SINGLE_ONLY;
    }
 
    struct ir3_shader *shader =
-      ir3_shader_from_nir(compiler, nir, &(struct ir3_shader_options){
-                              /* TODO: force to single on a6xx with legacy
-                               * ballot extension that uses 64-bit masks
-                               */
-                              .api_wavesize = api_wavesize,
-                              .real_wavesize = real_wavesize,
-                          }, NULL);
+      ir3_shader_from_nir(compiler, nir, &ir3_options, NULL);
    shader->cs.req_input_mem = align(cso->req_input_mem, 4) / 4;     /* byte->dword */
    shader->cs.req_local_mem = cso->static_shared_mem;
 
@@ -504,10 +506,12 @@ ir3_screen_finalize_nir(struct pipe_screen *pscreen, void *nir)
 {
    struct fd_screen *screen = fd_screen(pscreen);
 
+   const struct ir3_shader_nir_options options = {};
+
    MESA_TRACE_FUNC();
 
    ir3_nir_lower_io_to_temporaries(nir);
-   ir3_finalize_nir(screen->compiler, nir);
+   ir3_finalize_nir(screen->compiler, &options, nir);
 
    return NULL;
 }

src/freedreno/ir3/ir3_compiler.h
@@ -24,11 +24,6 @@ struct ir3_ra_reg_set;
 struct ir3_shader;
 
 struct ir3_compiler_options {
-   /* If true, UBO/SSBO accesses are assumed to be bounds-checked as defined by
-    * VK_EXT_robustness2 and optimizations may have to be more conservative.
-    */
-   bool robust_buffer_access2;
-
    /* If true, promote UBOs (except for constant data) to constants using ldc.k
     * in the preamble. The driver should ignore everything in ubo_state except
     * for the constant data UBO, which is excluded because the command pushing
@@ -340,9 +335,6 @@ enum ir3_shader_debug {
    /* MESA_DEBUG-only options: */
    IR3_DBG_SCHEDMSGS = BITFIELD_BIT(20),
    IR3_DBG_RAMSGS = BITFIELD_BIT(21),
-
-   /* Only used for the disk-caching logic: */
-   IR3_DBG_ROBUST_UBO_ACCESS = BITFIELD_BIT(30),
 };
 
 extern enum ir3_shader_debug ir3_shader_debug;

src/freedreno/ir3/ir3_context.c
@@ -66,7 +66,7 @@ ir3_context_init(struct ir3_compiler *compiler, struct ir3_shader *shader,
     */
 
    ctx->s = nir_shader_clone(ctx, shader->nir);
-   ir3_nir_lower_variant(so, ctx->s);
+   ir3_nir_lower_variant(so, &shader->options.nir_options, ctx->s);
 
    bool progress = false;
    bool needs_late_alg = false;

src/freedreno/ir3/ir3_disk_cache.c
@@ -45,8 +45,6 @@ ir3_disk_cache_init(struct ir3_compiler *compiler)
    _mesa_sha1_format(timestamp, id_sha1);
 
    uint64_t driver_flags = ir3_shader_debug;
-   if (compiler->options.robust_buffer_access2)
-      driver_flags |= IR3_DBG_ROBUST_UBO_ACCESS;
    compiler->disk_cache = disk_cache_create(renderer, timestamp, driver_flags);
 }
 
@@ -76,6 +74,8 @@ ir3_disk_cache_init_shader_key(struct ir3_compiler *compiler,
                      sizeof(shader->options.api_wavesize));
    _mesa_sha1_update(&ctx, &shader->options.real_wavesize,
                      sizeof(shader->options.real_wavesize));
+   _mesa_sha1_update(&ctx, &shader->options.nir_options,
+                     sizeof(shader->options.nir_options));
 
    /* Note that on some gens stream-out is lowered in ir3 to stg.  For later
     * gens we maybe don't need to include stream-out in the cache key.

src/freedreno/ir3/ir3_nir.c
@@ -248,7 +248,9 @@ ir3_get_variable_size_align_bytes(const glsl_type *type, unsigned *size, unsigne
 #define OPT_V(nir, pass, ...) NIR_PASS_V(nir, pass, ##__VA_ARGS__)
 
 bool
-ir3_optimize_loop(struct ir3_compiler *compiler, nir_shader *s)
+ir3_optimize_loop(struct ir3_compiler *compiler,
+                  const struct ir3_shader_nir_options *options,
+                  nir_shader *s)
 {
    MESA_TRACE_FUNC();
 
@@ -324,8 +326,7 @@ ir3_optimize_loop(struct ir3_compiler *compiler, nir_shader *s)
       nir_load_store_vectorize_options vectorize_opts = {
          .modes = nir_var_mem_ubo | nir_var_mem_ssbo | nir_var_uniform,
          .callback = ir3_nir_should_vectorize_mem,
-         .robust_modes = compiler->options.robust_buffer_access2 ?
-               nir_var_mem_ubo | nir_var_mem_ssbo : 0,
+         .robust_modes = options->robust_modes,
          .cb_data = compiler,
       };
       progress |= OPT(s, nir_opt_load_store_vectorize, &vectorize_opts);
@@ -495,7 +496,9 @@ ir3_nir_lower_array_sampler(nir_shader *shader)
 }
 
 void
-ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
+ir3_finalize_nir(struct ir3_compiler *compiler,
+                 const struct ir3_shader_nir_options *options,
+                 nir_shader *s)
 {
    MESA_TRACE_FUNC();
 
@@ -536,7 +539,7 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
 
    OPT_V(s, nir_lower_is_helper_invocation);
 
-   ir3_optimize_loop(compiler, s);
+   ir3_optimize_loop(compiler, options, s);
 
    /* do idiv lowering after first opt loop to get a chance to propagate
     * constants for divide by immed power-of-two:
@@ -548,7 +551,7 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
    idiv_progress |= OPT(s, nir_lower_idiv, &idiv_options);
 
    if (idiv_progress)
-      ir3_optimize_loop(compiler, s);
+      ir3_optimize_loop(compiler, options, s);
 
    OPT_V(s, nir_remove_dead_variables, nir_var_function_temp, NULL);
 
@@ -856,7 +859,7 @@ ir3_nir_post_finalize(struct ir3_shader *shader)
    if (compiler->gen >= 6)
       OPT_V(s, ir3_nir_lower_ssbo_size, compiler->options.storage_16bit ? 1 : 2);
 
-   ir3_optimize_loop(compiler, s);
+   ir3_optimize_loop(compiler, &shader->options.nir_options, s);
 }
 
 static bool
@@ -939,7 +942,9 @@ ir3_mem_access_size_align(nir_intrinsic_op intrin, uint8_t bytes,
 }
 
 void
-ir3_nir_lower_variant(struct ir3_shader_variant *so, nir_shader *s)
+ir3_nir_lower_variant(struct ir3_shader_variant *so,
+                      const struct ir3_shader_nir_options *options,
+                      nir_shader *s)
 {
    MESA_TRACE_FUNC();
 
@@ -1103,17 +1108,17 @@ ir3_nir_lower_variant(struct ir3_shader_variant *so, nir_shader *s)
    progress |= OPT(s, ir3_nir_lower_io_offsets);
 
    if (progress)
-      ir3_optimize_loop(so->compiler, s);
+      ir3_optimize_loop(so->compiler, options, s);
 
    /* verify that progress is always set */
-   assert(!ir3_optimize_loop(so->compiler, s));
+   assert(!ir3_optimize_loop(so->compiler, options, s));
 
    /* Fixup indirect load_const_ir3's which end up with a const base offset
     * which is too large to encode.  Do this late(ish) so we actually
     * can differentiate indirect vs non-indirect.
     */
    if (OPT(s, ir3_nir_fixup_load_const_ir3))
-      ir3_optimize_loop(so->compiler, s);
+      ir3_optimize_loop(so->compiler, options, s);
 
    /* Do late algebraic optimization to turn add(a, neg(b)) back into
     * subs, then the mandatory cleanup after algebraic.  Note that it may

src/freedreno/ir3/ir3_nir.h
@@ -58,11 +58,17 @@ nir_mem_access_size_align ir3_mem_access_size_align(
    uint32_t align_offset, bool offset_is_const, const void *cb_data);
 
 bool ir3_nir_opt_branch_and_or_not(nir_shader *nir);
-bool ir3_optimize_loop(struct ir3_compiler *compiler, nir_shader *s);
+bool ir3_optimize_loop(struct ir3_compiler *compiler,
+                       const struct ir3_shader_nir_options *options,
+                       nir_shader *s);
 void ir3_nir_lower_io_to_temporaries(nir_shader *s);
-void ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s);
+void ir3_finalize_nir(struct ir3_compiler *compiler,
+                      const struct ir3_shader_nir_options *options,
+                      nir_shader *s);
 void ir3_nir_post_finalize(struct ir3_shader *shader);
-void ir3_nir_lower_variant(struct ir3_shader_variant *so, nir_shader *s);
+void ir3_nir_lower_variant(struct ir3_shader_variant *so,
+                           const struct ir3_shader_nir_options *options,
+                           nir_shader *s);
 
 void ir3_setup_const_state(nir_shader *nir, struct ir3_shader_variant *v,
                            struct ir3_const_state *const_state);

src/freedreno/ir3/ir3_shader.c
@@ -493,9 +493,9 @@ ir3_shader_passthrough_tcs(struct ir3_shader *vs, unsigned patch_vertices)
 
       nir_shader_gather_info(tcs, nir_shader_get_entrypoint(tcs));
 
-      ir3_finalize_nir(vs->compiler, tcs);
-
       struct ir3_shader_options ir3_options = {};
+
+      ir3_finalize_nir(vs->compiler, &ir3_options.nir_options, tcs);
 
       vs->vs.passthrough_tcs[n] =
             ir3_shader_from_nir(vs->compiler, tcs, &ir3_options, NULL);

src/freedreno/ir3/ir3_shader.h
@@ -507,6 +507,17 @@ struct ir3_disasm_info {
 /* Represents half register in regid */
 #define HALF_REG_ID 0x100
 
+/* Options for common NIR optimization passes done in ir3. This is used for both
+ * finalize and post-finalize (where it has to be in the shader).
+ */
+struct ir3_shader_nir_options {
+   /* For the modes specified, accesses are assumed to be bounds-checked as
+    * defined by VK_EXT_robustness2 and optimizations may have to be more
+    * conservative.
+    */
+   nir_variable_mode robust_modes;
+};
+
 struct ir3_shader_options {
    unsigned num_reserved_user_consts;
    /* What API-visible wavesizes are allowed. Even if only double wavesize is
@@ -522,6 +533,8 @@ struct ir3_shader_options {
 
    uint32_t push_consts_base;
    uint32_t push_consts_dwords;
+
+   struct ir3_shader_nir_options nir_options;
 };
 
 /**

commit 774d08f73d7addc859e364a100bcc2cd9d4a330a

freedreno: Force single wavesize if double threadsize is unsupported
Turns out ir3 isn't enforcing this itself.

Fixes: 27434a45c6f9 ("ir3, tu: Plumb through support for per-shader robustness")
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39470>

src/gallium/drivers/freedreno/ir3/ir3_gallium.c
@@ -243,6 +243,11 @@ ir3_shader_compute_state_create(struct pipe_context *pctx,
    enum ir3_wavesize_option api_wavesize = IR3_SINGLE_OR_DOUBLE;
    enum ir3_wavesize_option real_wavesize = IR3_SINGLE_OR_DOUBLE;
 
+   if (ctx->screen->gen >= 6 && !ctx->screen->info->props.supports_double_threadsize) {
+      api_wavesize = IR3_SINGLE_ONLY;
+      real_wavesize = IR3_SINGLE_ONLY;
+   }
+
    const struct ir3_shader_options ir3_options = {
       /* TODO: force to single on a6xx with legacy ballot extension that uses
        * 64-bit masks
@@ -267,11 +272,6 @@ ir3_shader_compute_state_create(struct pipe_context *pctx,
 
    if (ctx->screen->gen >= 6)
       ir3_nir_lower_io_to_bindless(nir);
-
-   if (ctx->screen->gen >= 6 && !ctx->screen->info->props.supports_double_threadsize) {
-      api_wavesize = IR3_SINGLE_ONLY;
-      real_wavesize = IR3_SINGLE_ONLY;
-   }
 
    struct ir3_shader *shader =
       ir3_shader_from_nir(compiler, nir, &ir3_options);
@@ -327,6 +327,14 @@ ir3_shader_state_create(struct pipe_context *pctx,
    if (ctx->screen->gen >= 6)
       ir3_nir_lower_io_to_bindless(nir);
 
+   enum ir3_wavesize_option api_wavesize = IR3_SINGLE_OR_DOUBLE;
+   enum ir3_wavesize_option real_wavesize = IR3_SINGLE_OR_DOUBLE;
+
+   if (ctx->screen->gen >= 6 && !ctx->screen->info->props.supports_double_threadsize) {
+      api_wavesize = IR3_SINGLE_ONLY;
+      real_wavesize = IR3_SINGLE_ONLY;
+   }
+
    /*
     * Create ir3_shader:
     *
@@ -338,8 +346,8 @@ ir3_shader_state_create(struct pipe_context *pctx,
                               /* TODO: force to single on a6xx with legacy
                                * ballot extension that uses 64-bit masks
                                */
-                              .api_wavesize = IR3_SINGLE_OR_DOUBLE,
-                              .real_wavesize = IR3_SINGLE_OR_DOUBLE,
+                              .api_wavesize = api_wavesize,
+                              .real_wavesize = real_wavesize,
                           });
 
    /*