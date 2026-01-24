joshuaswarren/mesa-1

commit 32f6cee661638f4e1eb0be91856711c5e290e2be

brw: Add EU assembler support for float8
Decode logic in Gfx12+ has become complex with the new types, so Caio
suggested that we move to the table like other gens.

Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39007>

src/intel/compiler/brw/brw_reg_type.c
@@ -46,12 +46,22 @@ brw_type_encode(const struct intel_device_info *devinfo,
                                : devinfo->has_64bit_float))
       return INVALID_HW_REG_TYPE;
 
+   if (brw_type_is_float_or_bfloat(type) &&
+       brw_type_size_bits(type) == 8 &&
+       !devinfo->has_fp8)
+      return INVALID_HW_REG_TYPE;
+
    if (brw_type_is_bfloat(type) && !devinfo->has_bfloat16)
       return INVALID_HW_REG_TYPE;
 
    if (devinfo->ver >= 12) {
       if (brw_type_is_vector_imm(type))
          return type & ~(BRW_TYPE_VECTOR | BRW_TYPE_SIZE_MASK);
+
+      if (type == BRW_TYPE_BF8)
+         return 0b1000;
+      if (type == BRW_TYPE_HF8)
+         return 0b1100;
 
       return type & (BRW_TYPE_BASE_MASK | BRW_TYPE_SIZE_MASK);
    } else if (devinfo->ver >= 11) {
@@ -117,18 +127,45 @@ brw_type_decode(const struct intel_device_info *devinfo,
       return BRW_TYPE_INVALID;
 
    if (devinfo->ver >= 12) {
-      enum brw_reg_type t = (enum brw_reg_type) hw_type;
-      if (brw_type_size_bits(t) == 8) {
-         if (brw_type_is_float(t))
-            return file == IMM ? BRW_TYPE_VF : BRW_TYPE_INVALID;
-         else if (file == IMM)
-            return (t & BRW_TYPE_BASE_SINT) ? BRW_TYPE_V : BRW_TYPE_UV;
+      static const enum brw_reg_type tbl[16] = {
+         [0b0000] = BRW_TYPE_UB, /* or UV */
+         [0b0001] = BRW_TYPE_UW,
+         [0b0010] = BRW_TYPE_UD,
+         [0b0011] = BRW_TYPE_UQ,
+         [0b0100] = BRW_TYPE_B, /* or V */
+         [0b0101] = BRW_TYPE_W,
+         [0b0110] = BRW_TYPE_D,
+         [0b0111] = BRW_TYPE_Q,
+         [0b1000] = BRW_TYPE_BF8, /* or VF */
+         [0b1001] = BRW_TYPE_HF,
+         [0b1010] = BRW_TYPE_F,
+         [0b1011] = BRW_TYPE_DF,
+         [0b1100] = BRW_TYPE_HF8,
+         [0b1101] = BRW_TYPE_BF,
+         [0b1110] = BRW_TYPE_INVALID,
+         [0b1111] = BRW_TYPE_INVALID,
+      };
+
+      enum brw_reg_type t = tbl[hw_type];
+
+      if (file == IMM) {
+         switch (t) {
+         case BRW_TYPE_UB:  return BRW_TYPE_UV;
+         case BRW_TYPE_B:   return BRW_TYPE_V;
+         case BRW_TYPE_BF8: return BRW_TYPE_VF;
+         case BRW_TYPE_HF8: return BRW_TYPE_VF;
+         default:           break;
+         }
       }
-      if (brw_type_is_bfloat(t) && !devinfo->has_bfloat16)
+
+      if ((t == BRW_TYPE_HF8 || t == BRW_TYPE_BF8) &&
+          !devinfo->has_fp8)
          return BRW_TYPE_INVALID;
-      if (brw_type_is_float_or_bfloat(t) && brw_type_size_bits(t) < 16)
+      if (t == BRW_TYPE_BF && !devinfo->has_bfloat16)
          return BRW_TYPE_INVALID;
+
       return t;
+
    } else if (devinfo->ver >= 11) {
       static const enum brw_reg_type tbl[] = {
          [0] = BRW_TYPE_UD,
@@ -196,6 +233,10 @@ brw_type_encode_for_3src(const struct intel_device_info *devinfo,
                          enum brw_reg_type type)
 {
    if (brw_type_is_bfloat(type) && !devinfo->has_bfloat16)
+      return INVALID_HW_REG_TYPE;
+   if (brw_type_is_float_or_bfloat(type) &&
+       brw_type_size_bits(type) == 8 &&
+       !devinfo->has_fp8)
       return INVALID_HW_REG_TYPE;
 
    if (devinfo->ver >= 12) {
@@ -292,9 +333,11 @@ brw_data_type_encode(const struct intel_device_info *devinfo,
       [BRW_TYPE_W]              = { BRW_TYPE_INT_W },
       [BRW_TYPE_D]              = { BRW_TYPE_INT_D },
       [BRW_TYPE_Q]              = { BRW_TYPE_INT_Q },
+      [BRW_TYPE_HF8]            = { BRW_TYPE_FLOAT_HF8 },
       [BRW_TYPE_HF]             = { BRW_TYPE_FLOAT_HF },
       [BRW_TYPE_F]              = { BRW_TYPE_FLOAT_F },
       [BRW_TYPE_DF]             = { BRW_TYPE_FLOAT_DF },
+      [BRW_TYPE_BF8]            = { BRW_TYPE_FLOAT_BF8 },
       [BRW_TYPE_BF]             = { BRW_TYPE_FLOAT_BF },
    };
 
@@ -328,9 +371,11 @@ brw_data_type_decode(const struct intel_device_info *devinfo,
       },
       [1 /* float exec_type */] = {
          [0 ... 7]              = BRW_TYPE_INVALID,
+         [BRW_TYPE_FLOAT_BF8]   = BRW_TYPE_BF8,
          [BRW_TYPE_FLOAT_HF]    = BRW_TYPE_HF,
          [BRW_TYPE_FLOAT_F]     = BRW_TYPE_F,
          [BRW_TYPE_FLOAT_DF]    = BRW_TYPE_DF,
+         [BRW_TYPE_FLOAT_HF8]   = BRW_TYPE_HF8,
          [BRW_TYPE_FLOAT_BF]    = BRW_TYPE_BF,
       },
    };

src/intel/compiler/brw/brw_eu_validate.c
@@ -387,6 +387,8 @@ execution_type_for_type(enum brw_reg_type type)
    case BRW_TYPE_DF:
    case BRW_TYPE_F:
    case BRW_TYPE_HF:
+   case BRW_TYPE_BF8:
+   case BRW_TYPE_HF8:
       return type;
 
    case BRW_TYPE_VF:

src/intel/compiler/brw/brw_gram.y
@@ -358,8 +358,8 @@ i965_asm_set_instruction_options(struct brw_codegen *p,
 %token <integer> TYPE_D TYPE_UD
 %token <integer> TYPE_Q TYPE_UQ
 %token <integer> TYPE_V TYPE_UV
-%token <integer> TYPE_F TYPE_HF
-%token <integer> TYPE_BF
+%token <integer> TYPE_F TYPE_HF TYPE_HF8
+%token <integer> TYPE_BF TYPE_BF8
 %token <integer> TYPE_DF
 %token <integer> TYPE_VF
 
@@ -1872,6 +1872,8 @@ reg_type:
    | TYPE_Q    { $$ = BRW_TYPE_Q;  }
    | TYPE_HF   { $$ = BRW_TYPE_HF; }
    | TYPE_BF   { $$ = BRW_TYPE_BF; }
+   | TYPE_HF8  { $$ = BRW_TYPE_HF8; }
+   | TYPE_BF8  { $$ = BRW_TYPE_BF8; }
    ;
 
 imm_type:

src/intel/compiler/brw/brw_lex.l
@@ -302,10 +302,12 @@ BranchCtrl      { return BRANCH_CTRL; }
  /* data types */
 :?B     { return TYPE_B; }
 :?BF    { return TYPE_BF; }
+:?BF8   { return TYPE_BF8; }
 :?D     { return TYPE_D; }
 :?DF    { return TYPE_DF; }
 :?F     { return TYPE_F; }
 :?HF    { return TYPE_HF; }
+:?HF8   { return TYPE_HF8; }
 :?Q     { return TYPE_Q; }
 :?UB    { return TYPE_UB; }
 :?UD    { return TYPE_UD; }

src/intel/compiler/brw/test_eu_validate.cpp
@@ -436,16 +436,18 @@ TEST_P(validation_test, invalid_type_encoding_3src_a1)
       bool expected_result;
    } test_case[] = {
 #define E(x) ((unsigned)BRW_ALIGN1_3SRC_EXEC_TYPE_##x)
-      { BRW_TYPE_DF, E(FLOAT), devinfo.has_64bit_float },
-      { BRW_TYPE_F,  E(FLOAT), true  },
-      { BRW_TYPE_HF, E(FLOAT), true  },
-      { BRW_TYPE_Q,  E(INT),   devinfo.has_64bit_int },
-      { BRW_TYPE_UQ, E(INT),   devinfo.has_64bit_int },
-      { BRW_TYPE_D,  E(INT),   true  },
-      { BRW_TYPE_UD, E(INT),   true  },
-      { BRW_TYPE_W,  E(INT),   true  },
-      { BRW_TYPE_UW, E(INT),   true  },
-      { BRW_TYPE_BF, E(FLOAT), devinfo.has_bfloat16 },
+      { BRW_TYPE_DF,  E(FLOAT), devinfo.has_64bit_float },
+      { BRW_TYPE_F,   E(FLOAT), true  },
+      { BRW_TYPE_HF,  E(FLOAT), true  },
+      { BRW_TYPE_Q,   E(INT),   devinfo.has_64bit_int },
+      { BRW_TYPE_UQ,  E(INT),   devinfo.has_64bit_int },
+      { BRW_TYPE_D,   E(INT),   true  },
+      { BRW_TYPE_UD,  E(INT),   true  },
+      { BRW_TYPE_W,   E(INT),   true  },
+      { BRW_TYPE_UW,  E(INT),   true  },
+      { BRW_TYPE_BF,  E(FLOAT), devinfo.has_bfloat16 },
+      { BRW_TYPE_BF8, E(FLOAT), devinfo.has_fp8 },
+      { BRW_TYPE_HF8, E(FLOAT), devinfo.has_fp8 },
 
       /* There are no ternary instructions that can operate on B-type sources
        * on Gfx11-12. Src1/Src2 cannot be B-typed either.

src/intel/dev/intel_device_info.py
@@ -284,6 +284,7 @@ Struct("intel_device_info",
         Member("bool", "has_64bit_float_via_math_pipe", compiler_field=True),
         Member("bool", "has_64bit_int", compiler_field=True),
         Member("bool", "has_bfloat16", compiler_field=True),
+        Member("bool", "has_fp8", compiler_field=True),
         Member("bool", "has_integer_dword_mul", compiler_field=True),
         Member("bool", "has_systolic", compiler_field=True),
         Member("bool", "supports_simd16_3src", compiler_field=True),

commit ef58a49ea966734a357ca314db53f620561628d6

brw: Use lookup tables for Gfx12+ 3src type encoding/decoding
The previous Gfx12+ implementation using bit masking is failing for FP8
types, so replacing with explicit lookup tables.
For float types, the encoding now aligns with brw_data_type_float, ensuring
correct behavior for DPAS and other 3-source instructions.

Fixes: 32f6cee6616 ("brw: Add EU assembler support for float8")

Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39448>

src/intel/compiler/brw/brw_reg_type.c
@@ -240,8 +240,25 @@ brw_type_encode_for_3src(const struct intel_device_info *devinfo,
       return INVALID_HW_REG_TYPE;
 
    if (devinfo->ver >= 12) {
-      /* size mask and SINT type bit match exactly */
-      return type & 0b111;
+      static const uint8_t map[] = {
+         [0 ... BRW_TYPE_LAST]  = INVALID_HW_REG_TYPE,
+         [BRW_TYPE_UB]  = 0b000,
+         [BRW_TYPE_UW]  = 0b001,
+         [BRW_TYPE_UD]  = 0b010,
+         [BRW_TYPE_UQ]  = 0b011,
+         [BRW_TYPE_B]   = 0b100,
+         [BRW_TYPE_W]   = 0b101,
+         [BRW_TYPE_D]   = 0b110,
+         [BRW_TYPE_Q]   = 0b111,
+         [BRW_TYPE_BF8] = 0b000,
+         [BRW_TYPE_HF]  = 0b001,
+         [BRW_TYPE_F]   = 0b010,
+         [BRW_TYPE_DF]  = 0b011,
+         [BRW_TYPE_HF8] = 0b100,
+         [BRW_TYPE_BF]  = 0b101,
+      };
+      assert(type < ARRAY_SIZE(map));
+      return map[type];
    } else if (devinfo->ver >= 11) {
       if (brw_type_is_float(type)) {
          /* HF: 0b000 | F: 0b001 | DF: 0b010; subtract 1 from our size mask */
@@ -282,14 +299,38 @@ brw_type_decode_for_3src(const struct intel_device_info *devinfo,
    assert(exec_type == 0 || exec_type == 1);
 
    if (devinfo->ver >= 12) {
-      unsigned size_field = hw_type & BRW_TYPE_SIZE_MASK;
-      unsigned base_field = hw_type & BRW_TYPE_BASE_MASK;
-      if (exec_type == BRW_ALIGN1_3SRC_EXEC_TYPE_FLOAT) {
-         base_field |= BRW_TYPE_BASE_FLOAT;
-         if (base_field == BRW_TYPE_BASE_BFLOAT && !devinfo->has_bfloat16)
-            return BRW_TYPE_INVALID;
-      }
-      return (enum brw_reg_type) (base_field | size_field);
+      static const uint8_t map[2][8] = {
+         [BRW_ALIGN1_3SRC_EXEC_TYPE_INT] = {
+            [0b000] = BRW_TYPE_UB,
+            [0b001] = BRW_TYPE_UW,
+            [0b010] = BRW_TYPE_UD,
+            [0b011] = BRW_TYPE_UQ,
+            [0b100] = BRW_TYPE_B,
+            [0b101] = BRW_TYPE_W,
+            [0b110] = BRW_TYPE_D,
+            [0b111] = BRW_TYPE_Q,
+         },
+         [BRW_ALIGN1_3SRC_EXEC_TYPE_FLOAT] = {
+            [0b000] = BRW_TYPE_BF8,
+            [0b001] = BRW_TYPE_HF,
+            [0b010] = BRW_TYPE_F,
+            [0b011] = BRW_TYPE_DF,
+            [0b100] = BRW_TYPE_HF8,
+            [0b101] = BRW_TYPE_BF,
+            [0b110] = BRW_TYPE_INVALID,
+            [0b111] = BRW_TYPE_INVALID,
+         },
+      };
+
+      assert(hw_type < 8);
+      enum brw_reg_type result = map[exec_type][hw_type];
+
+      if ((result == BRW_TYPE_HF8 || result == BRW_TYPE_BF8) && !devinfo->has_fp8)
+         return BRW_TYPE_INVALID;
+      if (result == BRW_TYPE_BF && !devinfo->has_bfloat16)
+         return BRW_TYPE_INVALID;
+
+      return result;
    } else if (devinfo->ver >= 11) {
       if (exec_type == BRW_ALIGN1_3SRC_EXEC_TYPE_FLOAT) {
          return hw_type > 1 ? BRW_TYPE_INVALID :