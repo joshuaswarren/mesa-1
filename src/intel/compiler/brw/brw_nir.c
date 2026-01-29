joshuaswarren/mesa-1

commit 27f264e23355712451d517ebc50e61f3bbc1c062

brw: Use nir_imul_imm in per-vertex/per-primitive offset calculation
This avoids generating some useless math that would need to be cleaned
up later, without complicating things too much.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39250>

src/intel/compiler/brw/brw_nir.c
@@ -150,12 +150,12 @@ urb_offset(nir_builder *b,
 
       offset = nir_iadd(b, offset, nir_imul_imm(b, index->ssa, stride));
    } else if (index) {
-      nir_def *stride = cb_data->dynamic_tes
-         ? intel_nir_tess_field(b, PER_VERTEX_SLOTS)
-         : nir_imm_int(b, cb_data->per_vertex_stride /
-                          (cb_data->vec4_access ? 16 : 4));
+      const unsigned unit = cb_data->vec4_access ? 16 : 4;
+      nir_def *index_offset = cb_data->dynamic_tes
+         ? nir_imul(b, index->ssa, intel_nir_tess_field(b, PER_VERTEX_SLOTS))
+         : nir_imul_imm(b, index->ssa, cb_data->per_vertex_stride / unit);
 
-      offset = nir_iadd(b, offset, nir_imul(b, index->ssa, stride));
+      offset = nir_iadd(b, offset, index_offset);
 
       /* In the Tessellation evaluation shader, reposition the offset of
        * builtins when using separate layout.

commit 19678a1a0007c75bfca33d88bced28546c3f6b92

brw: Fix geometry shaders with non-constant vertex indices
Geometry shaders load from separate handles for each vertex, so they
don't incorporate the vertex index in the URB offset like tessellation
shaders do.  This means we can have a constant offset (within a vertex's
section) but not have a constant vertex index.

Prior to 27f264e2335 we were emitting non-folded ALU so we thought the
offset was non-constant at this point.  Now we can properly detect
constant offsets...but still don't want to use push inputs for
non-constant vertex indices.

Fixes: 27f264e2335 ("brw: Use nir_imul_imm in per-vertex/per-primitive offset calculation")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39603>

src/intel/compiler/brw/brw_nir.c
@@ -363,6 +363,11 @@ try_load_push_input(nir_builder *b,
       return NULL;
 
    if (stage == MESA_SHADER_GEOMETRY) {
+      /* GS vertex index isn't part of the offset */
+      nir_src *index = nir_get_io_arrayed_index_src(io);
+      if (!nir_src_is_const(*index))
+         return NULL;
+
       /* GS push inputs still use load_per_vertex_input */
       const nir_io_semantics io_sem = nir_intrinsic_io_semantics(io);
       const int slot = cb_data->varying_to_slot[io_sem.location];