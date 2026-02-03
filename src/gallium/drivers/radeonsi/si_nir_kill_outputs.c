joshuaswarren/mesa-1

commit fd5a6ec0d2e09338028b46a1f6f8ecc2f50f3a45

radeonsi: kill outputs for mesh shader
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37505>

src/gallium/drivers/radeonsi/si_nir_kill_outputs.c
@@ -9,7 +9,8 @@ bool si_nir_kill_outputs(nir_shader *nir, const union si_shader_key *key)
 {
    nir_function_impl *impl = nir_shader_get_entrypoint(nir);
    assert(impl);
-   assert(nir->info.stage <= MESA_SHADER_GEOMETRY);
+   assert(nir->info.stage <= MESA_SHADER_GEOMETRY ||
+          nir->info.stage == MESA_SHADER_MESH);
 
    if (!key->ge.opt.kill_outputs &&
        !key->ge.opt.kill_pointsize &&

src/gallium/drivers/radeonsi/si_shader.c
@@ -1321,7 +1321,7 @@ static void run_pre_link_optimization_passes(struct si_nir_shader_ctx *ctx)
    bool progress = false;
 
    /* Kill outputs according to the shader key. */
-   if (nir->info.stage <= MESA_SHADER_GEOMETRY)
+   if (nir->info.stage <= MESA_SHADER_GEOMETRY || nir->info.stage == MESA_SHADER_MESH)
       NIR_PASS(progress, nir, si_nir_kill_outputs, key);
 
    bool inline_uniforms = false;

commit 5c5d664f8f1b70b25b9f67d7956954f61380526f

radeonsi: fix mesh shader outputs kill
Mesh shader uses store per vertex output for point size
and store per primitive output for layer id.

This fixes gpu-ratemeter run slow for kill point size
and layer id cases when mono shader is used which expect
to kill these outputs.

Also gather fragment shader per primitive input info
to kill mesh shader per primitive output.

Fixes: fd5a6ec0d2e ("radeonsi: kill outputs for mesh shader")
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39644>

src/gallium/drivers/radeonsi/si_nir_kill_outputs.c
@@ -35,7 +35,10 @@ bool si_nir_kill_outputs(nir_shader *nir, const union si_shader_key *key)
             continue;
 
          nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
-         if (intr->intrinsic != nir_intrinsic_store_output)
+         if (intr->intrinsic != nir_intrinsic_store_output &&
+             /* for mesh shader outputs */
+             intr->intrinsic != nir_intrinsic_store_per_vertex_output &&
+             intr->intrinsic != nir_intrinsic_store_per_primitive_output)
             continue;
 
          /* No indirect indexing allowed. */

src/gallium/drivers/radeonsi/si_shader_info.c
@@ -407,6 +407,7 @@ static void gather_instruction(const struct nir_shader *nir, struct si_shader_in
          break;
       case nir_intrinsic_load_input:
       case nir_intrinsic_load_per_vertex_input:
+      case nir_intrinsic_load_per_primitive_input:
       case nir_intrinsic_load_input_vertex:
       case nir_intrinsic_load_interpolated_input:
          gather_io_instrinsic(nir, info, intr, true);