joshuaswarren/mesa-1

commit 58d6a0697e7853d7a16bdc454a21bea806358483

vulkan: Avoid NAN in the IR BVH
Build and encoding stages should be able to assume that AABBs don't have
NANs. This commit covers all possible sources of NAN.

Fixes: 2021462 ("radv: Use HPLOC for TLAS builds")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14696
Reviewed-by: Natalie Vock <natalie.vock@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39508>

src/vulkan/runtime/bvh/leaf.h
@@ -37,16 +37,6 @@ build_triangle(inout vk_aabb bounds, VOID_REF dst_ptr, vk_bvh_geometry_data geom
 
    triangle_vertices vertices = load_vertices(geom_data.data, indices, geom_data.vertex_format, geom_data.stride);
 
-   /* An inactive triangle is one for which the first (X) component of any vertex is NaN. If any
-    * other vertex component is NaN, and the first is not, the behavior is undefined. If the vertex
-    * format does not have a NaN representation, then all triangles are considered active.
-    */
-   if (isnan(vertices.vertex[0].x) || isnan(vertices.vertex[1].x) || isnan(vertices.vertex[2].x)) {
-      is_valid = false;
-      if (!VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
-         return false;
-   }
-
    if (geom_data.transform != NULL) {
       mat4 transform = mat4(1.0);
 
@@ -56,6 +46,17 @@ build_triangle(inout vk_aabb bounds, VOID_REF dst_ptr, vk_bvh_geometry_data geom
 
       for (uint32_t i = 0; i < 3; i++)
       vertices.vertex[i] = transform * vertices.vertex[i];
+   }
+
+   /* An inactive triangle is one for which the first (X) component of any vertex is NaN. If any
+    * other vertex component is NaN, and the first is not, the behavior is undefined. We treat those
+    * undefined cases as inactive to filter out NaNs. If the vertex format does not have a NaN
+    * representation, then all triangles are considered active.
+    */
+   if (any(isnan(vertices.vertex[0])) || any(isnan(vertices.vertex[1])) || any(isnan(vertices.vertex[2]))) {
+      is_valid = false;
+      if (!VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
+         return false;
    }
 
    REF(vk_ir_triangle_node) node = REF(vk_ir_triangle_node)(dst_ptr);
@@ -94,9 +95,10 @@ build_aabb(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_t ge
    }
 
    /* An inactive AABB is one for which the minimum X coordinate is NaN. If any other component is
-    * NaN, and the first is not, the behavior is undefined.
+    * NaN, and the first is not, the behavior is undefined. We treat those undefined cases as inactive
+    * to filter out NaNs.
     */
-   if (isnan(bounds.min.x)) {
+   if (any(isnan(bounds.min)) || any(isnan(bounds.max))) {
       is_valid = false;
       if (!VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
          return false;
@@ -147,10 +149,10 @@ build_instance(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_
 
    vk_aabb blas_aabb = DEREF(REF(vk_aabb)(instance.accelerationStructureReference + BVH_BOUNDS_OFFSET));
 
-   if (any(isnan(blas_aabb.min)) || any(isnan(blas_aabb.max)))
-      return false;
-
    bounds = calculate_instance_node_bounds(blas_aabb, mat3x4(transform));
+
+   if (any(isnan(bounds.min)) || any(isnan(bounds.max)))
+      return false;
 
 #ifdef CALCULATE_FINE_INSTANCE_NODE_BOUNDS
    vec3 blas_aabb_extent = blas_aabb.max - blas_aabb.min;

commit 1ae68cd547f4841fbdb12bb5ae15252822982f08

vulkan: Make sure no NaNs end up in the BVH
Fixes: 58d6a06 ("vulkan: Avoid NAN in the IR BVH")
Reviewed-by: Natalie Vock <natalie.vock@gmx.de>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39640>

src/vulkan/runtime/bvh/leaf.h
@@ -54,9 +54,13 @@ build_triangle(inout vk_aabb bounds, VOID_REF dst_ptr, vk_bvh_geometry_data geom
     * representation, then all triangles are considered active.
     */
    if (any(isnan(vertices.vertex[0])) || any(isnan(vertices.vertex[1])) || any(isnan(vertices.vertex[2]))) {
-      is_valid = false;
       if (!VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
          return false;
+
+      is_valid = false;
+      vertices.vertex[0] = vec4(0);
+      vertices.vertex[1] = vec4(0);
+      vertices.vertex[2] = vec4(0);
    }
 
    REF(vk_ir_triangle_node) node = REF(vk_ir_triangle_node)(dst_ptr);
@@ -99,9 +103,12 @@ build_aabb(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_t ge
     * to filter out NaNs.
     */
    if (any(isnan(bounds.min)) || any(isnan(bounds.max))) {
-      is_valid = false;
       if (!VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
          return false;
+
+      is_valid = false;
+      bounds.min = vec3(0);
+      bounds.max = vec3(0);
    }
 
    DEREF(node).base.aabb = bounds;
@@ -151,9 +158,6 @@ build_instance(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_
 
    bounds = calculate_instance_node_bounds(blas_aabb, mat3x4(transform));
 
-   if (any(isnan(bounds.min)) || any(isnan(bounds.max)))
-      return false;
-
 #ifdef CALCULATE_FINE_INSTANCE_NODE_BOUNDS
    vec3 blas_aabb_extent = blas_aabb.max - blas_aabb.min;
    float blas_aabb_volume = blas_aabb_extent.x * blas_aabb_extent.y * blas_aabb_extent.z;
@@ -170,6 +174,9 @@ build_instance(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_
    if (bounds_volume > 1.4f * blas_aabb_volume)
       bounds = CALCULATE_FINE_INSTANCE_NODE_BOUNDS(instance.accelerationStructureReference, mat3x4(transform));
 #endif
+
+   if (any(isnan(bounds.min)) || any(isnan(bounds.max)))
+      return false;
 
    DEREF(node).base.aabb = bounds;
    DEREF(node).custom_instance_and_mask = instance.custom_instance_and_mask;
@@ -233,12 +240,8 @@ main(void)
       is_active = build_instance(bounds, src_ptr, dst_ptr, global_id);
    }
 
-   if (VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE) &&
-       !is_active && args.geom_data.geometry_type != VK_GEOMETRY_TYPE_INSTANCES_KHR) {
-      bounds.min = vec3(0.0);
-      bounds.max = vec3(0.0);
+   if (VK_BUILD_FLAG(VK_BUILD_FLAG_ALWAYS_ACTIVE))
       is_active = true;
-   }
 
    DEREF(id_ptr).id = is_active ? pack_ir_node_id(dst_offset, node_type) : VK_BVH_INVALID_NODE;
 