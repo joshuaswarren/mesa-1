joshuaswarren/mesa-1

commit 1046b4df237db08e2ec4958d6f0f8e8707a9291b

tu: Allocate cmd_buffer from its pool
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14760
Signed-off-by: Karmjit Mahil <karmjit.mahil@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39623>

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -4307,7 +4307,7 @@ tu_create_cmd_buffer(struct vk_command_pool *pool,
    struct tu_cmd_buffer *cmd_buffer;
 
    cmd_buffer = (struct tu_cmd_buffer *) vk_zalloc2(
-      &device->vk.alloc, NULL, sizeof(*cmd_buffer), 8,
+      &device->vk.alloc, &pool->alloc, sizeof(*cmd_buffer), 8,
       VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
 
    if (cmd_buffer == NULL)

commit 91575d8a5140c370f53aa0837ffdbb6fcfb3866c

tu: Free cmd_buffer from its pool
Fixes: 1046b4df237 ("tu: Allocate cmd_buffer from its pool")
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39714>

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -4316,7 +4316,7 @@ tu_create_cmd_buffer(struct vk_command_pool *pool,
    VkResult result = vk_command_buffer_init(pool, &cmd_buffer->vk,
                                             &tu_cmd_buffer_ops, level);
    if (result != VK_SUCCESS) {
-      vk_free2(&device->vk.alloc, NULL, cmd_buffer);
+      vk_free2(&device->vk.alloc, &pool->alloc, cmd_buffer);
       return result;
    }
 