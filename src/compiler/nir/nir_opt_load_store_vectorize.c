joshuaswarren/mesa-1

commit 6d83b019aa469f165710503acf15ce549e603ce1

nir/load_store_vectorize: Skip new bit-sizes that are unaligned with high_offset
Otherwise this would require combining two values to produce a single
(new bit-size) channel, which vectorize_stores() don't handle.  The pass
can still keep trying smaller bit-sizes.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12946
Fixes: 264f4d01f17 ("nir: add a load/store vectorization pass")
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34414>

src/compiler/nir/nir_opt_load_store_vectorize.c
@@ -681,6 +681,10 @@ new_bitsize_acceptable(struct vectorize_ctx *ctx, unsigned new_bit_size,
 
    unsigned high_offset = high->offset_signed - low->offset_signed;
 
+   /* This can cause issues when combining store data. */
+   if (high_offset % (new_bit_size / 8) != 0)
+      return false;
+
    /* check nir_extract_bits limitations */
    unsigned common_bit_size = MIN2(get_bit_size(low), get_bit_size(high));
    common_bit_size = MIN2(common_bit_size, new_bit_size);

src/compiler/nir/tests/load_store_vectorizer_tests.cpp
@@ -755,6 +755,20 @@ TEST_F(nir_load_store_vectorize_test, ssbo_store_intersecting)
    ASSERT_EQ(nir_const_value_as_uint(cv[2], 32), 0x21);
 }
 
+TEST_F(nir_load_store_vectorize_test, gitlab_issue_12946)
+{
+   create_store(nir_var_mem_ssbo, 0, 0, 0x1, 32, 2, 0x3);
+   create_store(nir_var_mem_ssbo, 0, 3, 0x2, 32, 1, 0x1);
+
+   nir_validate_shader(b->shader, NULL);
+   ASSERT_EQ(count_intrinsics(nir_intrinsic_store_ssbo), 2);
+
+   /* The original issue was the crash when running the pass. */
+   EXPECT_TRUE(run_vectorizer(nir_var_mem_ssbo));
+
+   EXPECT_EQ(count_intrinsics(nir_intrinsic_store_ssbo), 2);
+}
+
 TEST_F(nir_load_store_vectorize_test, ssbo_store_identical)
 {
    create_store(nir_var_mem_ssbo, 0, 0, 0x1);

commit 0efe7bcfeee2abc4e2a2862b99b961d3fc14e076

nir/opt_vectorize_load_store: allow sizes unaligned with high offset for loads
This was added specifically for vectorized stores, so allow for loads.

Without this, the pass will fail to vectorize 2 consecutive 16-bit loads
into a single 32-bit load.

Fixes: 6d83b019aa46 ("nir/load_store_vectorize: Skip new bit-sizes that are unaligned with high_offset")
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39713>

src/compiler/nir/nir_opt_load_store_vectorize.c
@@ -863,7 +863,7 @@ new_bitsize_acceptable(struct vectorize_ctx *ctx, unsigned new_bit_size,
    unsigned high_offset = get_offset_diff(low, high);
 
    /* This can cause issues when combining store data. */
-   if (high_offset % (new_bit_size / 8) != 0)
+   if (low->is_store && (high_offset % (new_bit_size / 8) != 0))
       return false;
 
    /* check nir_extract_bits limitations */