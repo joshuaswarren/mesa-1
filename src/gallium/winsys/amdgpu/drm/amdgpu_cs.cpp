joshuaswarren/mesa-1

commit 109574035cb276b15ac099fae47da28eae31410e

winsys/amdgpu: print userq job info
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39206>

src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp
@@ -1628,6 +1628,18 @@ static int amdgpu_cs_submit_ib_userq(struct amdgpu_userq *userq,
    if (r)
       mesa_loge("amdgpu: getting wait fences failed\n");
 
+   if (aws->userq_job_log) {
+      for (unsigned i = 0; i < userq_wait_data.num_fences; i++) {
+         /* The uq_va memory is allocated in kernel from a memory chunk. This memory chunk is
+          * mapped to same address for all process/apps. Once uq_va is guess mapped to a
+          * given queue, cross process/queue fence dependency can be analyzed.
+          */
+         mesa_logi("amdgpu: uq_log: %s:  num_wait_fences=%d  uq_va=%llx  job=%llx\n",
+                   amdgpu_userq_str[acs->queue_index], userq_wait_data.num_fences, fence_info[i].va,
+                   fence_info[i].value);
+      }
+   }
+
    simple_mtx_lock(&userq->lock);
    amdgpu_cs_add_userq_packets(aws, userq, csc, userq_wait_data.num_fences, fence_info);
    struct drm_amdgpu_userq_signal userq_signal_data = {
@@ -1657,6 +1669,11 @@ static int amdgpu_cs_submit_ib_userq(struct amdgpu_userq *userq,
 #endif
    userq->doorbell_bo_map[AMDGPU_USERQ_DOORBELL_INDEX] = userq->next_wptr;
    r = ac_drm_userq_signal(aws->dev, &userq_signal_data);
+
+   if (aws->userq_job_log) {
+      mesa_logi("amdgpu: uq_log: %s:  submitted_job=%llx\n", amdgpu_userq_str[acs->queue_index],
+                (long long)*userq->wptr_bo_map);
+   }
 
    *seq_no = userq->user_fence_seq_num;
    simple_mtx_unlock(&userq->lock);

src/gallium/winsys/amdgpu/drm/amdgpu_userq.c
@@ -72,6 +72,41 @@ amdgpu_userq_ring_init(struct amdgpu_winsys *aws, struct amdgpu_userq *userq,
    return true;
 }
 
+static void *
+userq_job_log_thread(void *data)
+{
+   struct amdgpu_winsys *aws = data;
+   struct amdgpu_userq *userq;
+
+   while (aws->userq_job_log) {
+      os_time_sleep(1000 * 700);
+      for (unsigned i = 0; i < AMDGPU_MAX_QUEUES; i++) {
+         userq = &aws->queues[i].userq;
+         if (userq->userq_handle) {
+            uint64_t last_submitted_job = *userq->wptr_bo_map;
+            uint64_t last_completed_job = *userq->user_fence_ptr;
+
+            if (userq->last_submitted_job != last_submitted_job ||
+                userq->last_completed_job != last_completed_job) {
+               mesa_logi("amdgpu: uq_log: %s:  submitted_job=%llx  completed_job=%llx\n",
+                         amdgpu_userq_str[i], (long long)last_submitted_job,
+                         (long long)last_completed_job);
+               userq->last_submitted_job = last_submitted_job;
+               userq->last_completed_job = last_completed_job;
+            }
+         }
+      }
+   }
+
+   return NULL;
+}
+
+void
+amdgpu_userq_start_job_log_thread(struct amdgpu_winsys *aws)
+{
+   pthread_create(&aws->userq_job_log_thread, NULL, userq_job_log_thread, aws);
+}
+
 void
 amdgpu_userq_deinit(struct amdgpu_winsys *aws, struct amdgpu_userq *userq)
 {

src/gallium/winsys/amdgpu/drm/amdgpu_userq.h
@@ -99,7 +99,14 @@ struct amdgpu_userq {
       struct amdgpu_userq_compute_data compute_data;
       struct amdgpu_userq_sdma_data sdma_data;
    };
+
+   /* Used in userq job log thread to only print if data has changed */
+   uint64_t last_submitted_job;
+   uint64_t last_completed_job;
 };
+
+void
+amdgpu_userq_start_job_log_thread(struct amdgpu_winsys *aws);
 
 bool
 amdgpu_userq_init(struct amdgpu_winsys *aws, struct amdgpu_userq *userq, enum amd_ip_type ip_type,

src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
@@ -25,6 +25,13 @@
 #include <fcntl.h>
 #include "sid.h"
 
+char amdgpu_userq_str[AMDGPU_MAX_QUEUES][8] = {
+   "gfx",
+   "gfx_hi",
+   "comp",
+   "sdma"
+};
+
 static struct hash_table *dev_tab = NULL;
 static simple_mtx_t dev_tab_mutex = SIMPLE_MTX_INITIALIZER;
 
@@ -59,6 +66,7 @@ static bool do_winsys_init(struct amdgpu_winsys *aws,
                       strstr(debug_get_option("AMD_DEBUG", ""), "sqtt") != NULL;
    aws->zero_all_vram_allocs = strstr(debug_get_option("R600_DEBUG", ""), "zerovram") != NULL ||
                               driQueryOptionb(config->options, "radeonsi_zerovram");
+   aws->userq_job_log = strstr(debug_get_option("AMD_DEBUG", ""), "userqjoblog") != NULL;
 
    for (unsigned i = 0; i < ARRAY_SIZE(aws->queues); i++)
       simple_mtx_init(&aws->queues[i].userq.lock, mtx_plain);
@@ -66,6 +74,9 @@ static bool do_winsys_init(struct amdgpu_winsys *aws,
    /* TODO: Enable this once the kernel handles it efficiently. */
    if (!aws->info.userq_ip_mask)
       aws->info.has_vm_always_valid = false;
+
+   if (aws->userq_job_log)
+      amdgpu_userq_start_job_log_thread(aws);
 
    return true;
 
@@ -79,6 +90,11 @@ static void do_winsys_deinit(struct amdgpu_winsys *aws)
 {
    if (aws->reserve_vmid)
       ac_drm_vm_unreserve_vmid(aws->dev, 0);
+
+   if (aws->userq_job_log) {
+      aws->userq_job_log = false;
+      pthread_join(aws->userq_job_log_thread, NULL);
+   }
 
    for (unsigned i = 0; i < ARRAY_SIZE(aws->queues); i++) {
       for (unsigned j = 0; j < ARRAY_SIZE(aws->queues[i].fences); j++)

src/gallium/winsys/amdgpu/drm/amdgpu_winsys.h
@@ -126,6 +126,8 @@ enum amdgpu_queue_index {
    AMDGPU_QUEUE_USES_ALT_FENCE = INT_MAX,
 };
 
+extern char amdgpu_userq_str[AMDGPU_MAX_QUEUES][8];
+
 /* This can use any integer type because the logic handles integer wraparounds robustly, but
  * uint8_t wraps around so quickly that some BOs might never become idle because we don't
  * remove idle fences from BOs, so they become "busy" again after a queue sequence number wraps
@@ -207,6 +209,8 @@ struct amdgpu_winsys {
 
    /* Protected by bo_fence_lock. */
    struct amdgpu_queue queues[AMDGPU_MAX_QUEUES];
+   pthread_t userq_job_log_thread;
+   bool userq_job_log; /* enable userq job log thread */
 
    struct pb_cache bo_cache;
    struct pb_slabs bo_slabs;  /* Slab allocator. */

commit e943f68fcfebe296cf11a8c636d0c2e2c96b829a

winsys/amdgpu: Fix userq job info log on PPC
On the ppc64le architecture the macro printing the userq job info fails
to compile with error:

   In file included from ../src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp:11:
   ../src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp: In function ‘int amdgpu_cs_submit_ib_userq(amdgpu_userq*, amdgpu_cs*, uint32_t*, unsigned int, uint32_t*, unsigned int, uint64_t*, uint64_t)’:
   ../src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp:1652:20: error: format ‘%llx’ expects argument of type ‘long long unsigned int’, but argument 6 has type ‘__u64’ {aka ‘long unsigned int’} [-Werror=format=]
   1652 |          mesa_logi("amdgpu: uq_log: %s:  num_wait_fences=%d  uq_va=%llx  job=%llx\n",
   1653 |                    amdgpu_userq_str[acs->queue_index], userq_wait_data.num_fences, fence_info[i].va,
         |                                                                                    ~~~~~~~~~~~~~~~~
         |                                                                                                  |
         |                                                                                                  __u64 {aka long unsigned int}
   ../src/util/log.h:78:70: note: in definition of macro ‘mesa_logi’
      78 | #define mesa_logi(fmt, ...) mesa_log(MESA_LOG_INFO, (MESA_LOG_TAG), (fmt), ##__VA_ARGS__)
         |                                                                      ^~~
   ../src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp:1652:20: error: format ‘%llx’ expects argument of type ‘long long unsigned int’, but argument 7 has type ‘__u64’ {aka ‘long unsigned int’} [-Werror=format=]
   1652 |          mesa_logi("amdgpu: uq_log: %s:  num_wait_fences=%d  uq_va=%llx  job=%llx\n",
   1653 |                    amdgpu_userq_str[acs->queue_index], userq_wait_data.num_fences, fence_info[i].va,
   1654 |                    fence_info[i].value);
         |                    ~~~~~~~~~~~~~~~~~~~
         |                                  |
         |                                  __u64 {aka long unsigned int}
   ../src/util/log.h:78:70: note: in definition of macro ‘mesa_logi’
      78 | #define mesa_logi(fmt, ...) mesa_log(MESA_LOG_INFO, (MESA_LOG_TAG), (fmt), ##__VA_ARGS__)
         |                                                                      ^~~

Parse the parameters to fix the failure.

Fixes: 109574035cb2 ("winsys/amdgpu: print userq job info")

src/gallium/winsys/amdgpu/drm/amdgpu_cs.cpp
@@ -1650,8 +1650,8 @@ static int amdgpu_cs_submit_ib_userq(struct amdgpu_userq *userq,
           * given queue, cross process/queue fence dependency can be analyzed.
           */
          mesa_logi("amdgpu: uq_log: %s:  num_wait_fences=%d  uq_va=%llx  job=%llx\n",
-                   amdgpu_userq_str[acs->queue_index], userq_wait_data.num_fences, fence_info[i].va,
-                   fence_info[i].value);
+                   amdgpu_userq_str[acs->queue_index], userq_wait_data.num_fences,
+                   (long long)fence_info[i].va, (long long)fence_info[i].value);
       }
    }
 