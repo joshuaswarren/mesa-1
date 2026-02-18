joshuaswarren/mesa-1

commit 992cd1dd704d4727f5880df3cfcd6de29a440937

pvr: drop pvr_assert macro
The only uses of the macro can be fatal assertions instead.
No point keeping it around, especially as it doesn't work with the ASSERTED
hint to suppress warnings either.

Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Reviewed-by: Karmjit Mahil <karmjit.mahil@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39677>

src/imagination/vulkan/pvr_image.c
@@ -423,8 +423,8 @@ void pvr_get_image_subresource_layout(const struct pvr_image *image,
    const struct pvr_mip_level *mip_level =
       &plane->mip_levels[subresource->mipLevel];
 
-   pvr_assert(subresource->mipLevel < image->vk.mip_levels);
-   pvr_assert(subresource->arrayLayer < image->vk.array_layers);
+   assert(subresource->mipLevel < image->vk.mip_levels);
+   assert(subresource->arrayLayer < image->vk.array_layers);
 
    layout->offset = plane->offset +
                     subresource->arrayLayer * plane->layer_size +

src/imagination/vulkan/pvr_arch_pass.c
@@ -825,7 +825,7 @@ PVR_PER_ARCH(CreateRenderPass2)(VkDevice _device,
       const VkAttachmentDescription2 *desc = &pCreateInfo->pAttachments[i];
       struct pvr_render_pass_attachment *attachment = &pass->attachments[i];
 
-      pvr_assert(!(desc->flags & ~VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT));
+      assert(!(desc->flags & ~VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT));
 
       attachment->load_op = desc->loadOp;
       attachment->store_op = desc->storeOp;

src/imagination/vulkan/pvr_macros.h
@@ -49,17 +49,6 @@
       _buffer[__offset / __nr_dwords] = __value;                 \
    } while (0)
 
-/* A non-fatal assert. Useful for debugging. */
-#if MESA_DEBUG
-#   define pvr_assert(x)                                           \
-      ({                                                           \
-         if (unlikely(!(x)))                                       \
-            mesa_loge("%s:%d ASSERT: %s", __FILE__, __LINE__, #x); \
-      })
-#else
-#   define pvr_assert(x)
-#endif
-
 #define PVR_ARCH_NAME(name, arch) pvr_##arch##_##name
 
 #define PVR_ARCH_DISPATCH(name, arch, ...)        \

commit e8ac4f55308139b89c0a50f63b099bf15af34c81

pvr: remove asserts in pvr_get_image_subresource_layout()
These asserts were there to validate this bit of the Vulkan spec:

   VUID-vkGetImageSubresourceLayout-mipLevel-01716
   The mipLevel member of pSubresource must be less than the mipLevels specified in image

   VUID-vkGetImageSubresourceLayout-arrayLayer-01717
   The arrayLayer member of pSubresource must be less than the arrayLayers specified in
   image

However, this function isn't just called via vkGetImageSubresourceLayout(), but
from elsewhere in the driver. At least in the case of arrayLayer, the assert
doesn't always hold true, for example, in the case of deferred RTA clears on a
2D array image view of a 3D image.

The array layer assert was being hit by the following test cases:
dEQP-VK.renderpass.remaining_array_layers.multi_layer_fb.*
dEQP-VK.renderpass2.remaining_array_layers.multi_layer_fb.*

As the asserts don't really add any value, rather than moving them into
pvr_GetImageSubresourceLayout(), just drop them entirely.

Fixes: 992cd1dd704d ("pvr: drop pvr_assert macro")
Signed-off-by: Frank Binns <frank.binns@imgtec.com>
Reviewed-by: Simon Perretta <simon.perretta@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39916>

src/imagination/vulkan/pvr_image.c
@@ -423,9 +423,6 @@ void pvr_get_image_subresource_layout(const struct pvr_image *image,
    const struct pvr_mip_level *mip_level =
       &plane->mip_levels[subresource->mipLevel];
 
-   assert(subresource->mipLevel < image->vk.mip_levels);
-   assert(subresource->arrayLayer < image->vk.array_layers);
-
    layout->offset = plane->offset +
                     subresource->arrayLayer * plane->layer_size +
                     mip_level->offset;