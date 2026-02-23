joshuaswarren/mesa-1

commit 1fb00604c40049c64edead22f5182fbc07d21937

intel/perf: Change mdapi switch cases from ver to verx
We are missing handling for gfx12.5 so to add it we will need a switch case over
verx.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Tested-by: Lukasz Stalmirski <lukasz.stalmirski@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32842>

src/intel/perf/intel_perf_mdapi.c
@@ -34,8 +34,8 @@ intel_perf_query_result_write_mdapi(void *data, uint32_t data_size,
                                     const struct intel_perf_query_info *query,
                                     const struct intel_perf_query_result *result)
 {
-   switch (devinfo->ver) {
-   case 7: {
+   switch (devinfo->verx10) {
+   case 70: {
       struct gfx7_mdapi_metrics *mdapi_data = (struct gfx7_mdapi_metrics *) data;
 
       if (data_size < sizeof(*mdapi_data))
@@ -62,7 +62,7 @@ intel_perf_query_result_write_mdapi(void *data, uint32_t data_size,
       mdapi_data->SplitOccured = result->query_disjoint;
       return sizeof(*mdapi_data);
    }
-   case 8: {
+   case 80: {
       struct gfx8_mdapi_metrics *mdapi_data = (struct gfx8_mdapi_metrics *) data;
 
       if (data_size < sizeof(*mdapi_data))
@@ -94,9 +94,9 @@ intel_perf_query_result_write_mdapi(void *data, uint32_t data_size,
       mdapi_data->SplitOccured = result->query_disjoint;
       return sizeof(*mdapi_data);
    }
-   case 9:
-   case 11:
-   case 12:{
+   case 90:
+   case 110:
+   case 120:{
       struct gfx9_mdapi_metrics *mdapi_data = (struct gfx9_mdapi_metrics *) data;
 
       if (data_size < sizeof(*mdapi_data))
@@ -128,8 +128,8 @@ intel_perf_query_result_write_mdapi(void *data, uint32_t data_size,
       mdapi_data->SplitOccured = result->query_disjoint;
       return sizeof(*mdapi_data);
    }
-   case 20:
-   case 30: {
+   case 200:
+   case 300: {
       struct gfx20_mdapi_metrics *mdapi_data = (struct gfx20_mdapi_metrics *) data;
 
       if (data_size < sizeof(*mdapi_data))
@@ -278,8 +278,8 @@ intel_perf_register_mdapi_oa_query(struct intel_perf_config *perf,
    if (!(devinfo->ver >= 7 && devinfo->ver <= 30))
       return;
 
-   switch (devinfo->ver) {
-   case 7: {
+   switch (devinfo->verx10) {
+   case 70: {
       query = intel_perf_append_query_info(perf, 1 + 45 + 16 + 7);
 
       struct gfx7_mdapi_metrics metric_data;
@@ -303,7 +303,7 @@ intel_perf_register_mdapi_oa_query(struct intel_perf_config *perf,
       MDAPI_QUERY_ADD_COUNTER(query, metric_data, ReportsCount, UINT32);
       break;
    }
-   case 8: {
+   case 80: {
       query = intel_perf_append_query_info(perf, 2 + 36 + 16 + 16);
 
       struct gfx8_mdapi_metrics metric_data;
@@ -337,9 +337,9 @@ intel_perf_register_mdapi_oa_query(struct intel_perf_config *perf,
       MDAPI_QUERY_ADD_COUNTER(query, metric_data, ReportsCount, UINT32);
       break;
    }
-   case 9:
-   case 11:
-   case 12: {
+   case 90:
+   case 110:
+   case 120: {
       query = intel_perf_append_query_info(perf, 2 + 36 + 16 + 16 + 16 + 2);
 
       struct gfx9_mdapi_metrics metric_data;
@@ -379,8 +379,8 @@ intel_perf_register_mdapi_oa_query(struct intel_perf_config *perf,
       MDAPI_QUERY_ADD_COUNTER(query, metric_data, Reserved4, UINT32);
       break;
    }
-   case 20:
-   case 30: {
+   case 200:
+   case 300: {
       query = intel_perf_append_query_info(perf, 2 + 64 + 16 + 14 + 16 + 2);
 
       struct gfx20_mdapi_metrics metric_data;

commit 2cc930e09db176bac7c3671684df91ff6655b04d

intel/perf: Add HSW verx10 to intel_perf_query_result_write_mdapi()
HSW is verx10 75 and when we switched from ver to verx10 I forgot to add the case
75.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 1fb00604c400 ("intel/perf: Change mdapi switch cases from ver to verx")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14902
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40039>

src/intel/perf/intel_perf_mdapi.c
@@ -35,7 +35,8 @@ intel_perf_query_result_write_mdapi(void *data, uint32_t data_size,
                                     const struct intel_perf_query_result *result)
 {
    switch (devinfo->verx10) {
-   case 70: {
+   case 70:
+   case 75: {
       struct gfx7_mdapi_metrics *mdapi_data = (struct gfx7_mdapi_metrics *) data;
 
       if (data_size < sizeof(*mdapi_data))
@@ -315,7 +316,8 @@ intel_perf_register_mdapi_oa_query(struct intel_perf_config *perf,
       return;
 
    switch (devinfo->verx10) {
-   case 70: {
+   case 70:
+   case 75: {
       query = intel_perf_append_query_info(perf, 1 + 45 + 16 + 7);
 
       struct gfx7_mdapi_metrics metric_data;