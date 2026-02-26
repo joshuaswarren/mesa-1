joshuaswarren/mesa-1

commit 7b183f1c724be1cd5896e1c54ea69bfdc12fb10c

panvk: Use resolve shaders for color resolves
This massively improves the performance of the Sascha Willems
multisampling example:

 - With 2x MSAA: 590 -> 2605 (4.4x speedup)
 - With 4x MSAA: 347 -> 2570 (7.4x speedup)
 - With 8x MSAA: 188 FPS -> 2494 FPS (13.2x speedup)
 - With 16x MSAA: 96.7 FPS -> 2483 FPS (25.7x speedup)

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Acked-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39759>

src/panfrost/vulkan/panvk_vX_cmd_draw.c
@@ -45,6 +45,18 @@ get_att_fb_load_op(const VkRenderingAttachmentInfo *att)
    }
 }
 
+static enum pan_fb_msaa_copy_op
+vk_to_pan_fb_resolve_mode(VkResolveModeFlagBits resolveMode)
+{
+   switch (resolveMode) {
+   case VK_RESOLVE_MODE_SAMPLE_ZERO_BIT:  return PAN_FB_MSAA_COPY_SAMPLE_0;
+   case VK_RESOLVE_MODE_AVERAGE_BIT:      return PAN_FB_MSAA_COPY_AVERAGE;
+   case VK_RESOLVE_MODE_MIN_BIT:          return PAN_FB_MSAA_COPY_MIN;
+   case VK_RESOLVE_MODE_MAX_BIT:          return PAN_FB_MSAA_COPY_MAX;
+   default: UNREACHABLE("Unsupported resolveMode");
+   }
+}
+
 static struct panvk_image_view *
 get_ms2ss_image_view(struct panvk_image_view *iview, uint32_t nr_samples)
 {
@@ -163,9 +175,25 @@ render_state_set_color_attachment(struct panvk_cmd_buffer *cmdbuf,
       assert(resolve.dst_iview != NULL);
       assert(resolve.dst_iview->pview.nr_samples == 1);
 
-      /* We need to store so we can do the MSAA resolve later */
-      render->fb.store.rts[index] = pan_fb_store_iview(&iview->pview);
-      render->color_attachments.resolve[index] = resolve;
+      if (ms2ss || att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) {
+         render->fb.resolve.rts[index] = (struct pan_fb_resolve_target) {
+            .in_bounds = {
+               .resolve = PAN_FB_RESOLVE_RT(index),
+               .msaa = vk_to_pan_fb_resolve_mode(att->resolveMode),
+            },
+            .border = {
+               .resolve = PAN_FB_RESOLVE_IMAGE,
+               .msaa = PAN_FB_MSAA_COPY_SINGLE,
+            },
+            .iview = &resolve.dst_iview->pview,
+         };
+         render->fb.store.rts[index] =
+            pan_fb_always_store_iview_s0(&resolve.dst_iview->pview);
+      } else {
+         /* We need to store so we can do the MSAA resolve later */
+         render->fb.store.rts[index] = pan_fb_store_iview(&iview->pview);
+         render->color_attachments.resolve[index] = resolve;
+      }
    }
 }
 

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -1308,7 +1308,8 @@ get_fb_descs(struct panvk_cmd_buffer *cmdbuf)
    };
 
    VkResult result = panvk_per_arch(cmd_get_frame_shaders)(
-      cmdbuf, fbd_info.fb, fbd_info.load, NULL, &fbd_info.frame_shaders);
+      cmdbuf, fbd_info.fb, fbd_info.load, &render->fb.resolve,
+      &fbd_info.frame_shaders);
    if (result != VK_SUCCESS)
       return result;
 
@@ -1348,7 +1349,9 @@ get_fb_descs(struct panvk_cmd_buffer *cmdbuf)
                        &render->fb.store : &render->fb.spill.store;
 
       VkResult result = panvk_per_arch(cmd_get_frame_shaders)(
-         cmdbuf, fbd_info.fb, fbd_info.load, NULL, &fbd_info.frame_shaders);
+         cmdbuf, fbd_info.fb, fbd_info.load,
+         ir_pass == PANVK_IR_LAST_PASS ? &render->fb.resolve : NULL,
+         &fbd_info.frame_shaders);
       if (result != VK_SUCCESS)
          return result;
 

src/panfrost/vulkan/jm/panvk_vX_cmd_buffer.c
@@ -155,7 +155,8 @@ panvk_per_arch(cmd_close_batch)(struct panvk_cmd_buffer *cmdbuf)
 
       struct pan_fb_frame_shaders fs;
       VkResult result = panvk_per_arch(cmd_get_frame_shaders)(
-         cmdbuf, &render->fb.layout, fbd_info.load, NULL, &fs);
+         cmdbuf, &render->fb.layout, fbd_info.load,
+         render->fb.needs_store ? &render->fb.resolve : NULL, &fs);
       if (result != VK_SUCCESS)
          return;
 

src/panfrost/vulkan/panvk_cmd_draw.h
@@ -72,6 +72,7 @@ struct panvk_rendering_state {
    struct {
       struct pan_fb_layout layout;
       struct pan_fb_load load;
+      struct pan_fb_resolve resolve;
       struct pan_fb_store store;
       struct {
          struct pan_fb_load load;

commit 0039406b47d51420ab012a6b18471ab0750b674b

panvk: Avoid direct MSAA resolves to AFBC on v6 and earlier
Fixes: 7b183f1c724b ("panvk: Use resolve shaders for color resolves")
Fixes: 89b4afb5b38a ("panvk: Also use resolve shaders for Z/S")
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39324>

src/panfrost/vulkan/panvk_vX_cmd_draw.c
@@ -93,6 +93,28 @@ get_ms2ss_image_view(struct panvk_image_view *iview, uint32_t nr_samples)
    return res;
 }
 
+static bool
+avoid_direct_resolve_to(const struct pan_image *img)
+{
+   /* There is an issue with AFBC and small tiles where, if the tile size is
+    * not a multiple of superblock size then writes and reads may race within
+    * a superblock.  On v7+, there's a hardware bit to help us work around
+    * this but it doesn't exist on v6 and earlier.
+    *
+    * This is particularly likely to happen with MSAA since the sample count
+    * is a multiplier on the color allocation, making it much more likely that
+    * we'll hit this case.  MSAA images are safe because they don't allow AFBC
+    * but we have a real problem if we attempt to resolve directly to an
+    * AFBC-compressed single-sampled image.  Skip the resolve optimization in
+    * this case.
+    *
+    * TODO: If we moved this decision later, it could be based on the final
+    * framebuffer layout and we could potentially allow direct resolves in
+    * more cases.
+    */
+   return PAN_ARCH < 7 && drm_is_afbc(img->props.modifier);
+}
+
 static void
 render_state_set_color_attachment(struct panvk_cmd_buffer *cmdbuf,
                                   const VkRenderingAttachmentInfo *att,
@@ -175,7 +197,11 @@ render_state_set_color_attachment(struct panvk_cmd_buffer *cmdbuf,
       assert(resolve.dst_iview != NULL);
       assert(resolve.dst_iview->pview.nr_samples == 1);
 
-      if (ms2ss || att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) {
+      const struct pan_image *resolve_pimage =
+         pan_image_view_get_color_plane(&resolve.dst_iview->pview).image;
+
+      if ((ms2ss || att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) &&
+          !avoid_direct_resolve_to(resolve_pimage)) {
          render->fb.resolve.rts[index] = (struct pan_fb_resolve_target) {
             .in_bounds = {
                .resolve = PAN_FB_RESOLVE_RT(index),
@@ -464,7 +490,11 @@ render_state_set_zs_attachments(struct panvk_cmd_buffer *cmdbuf,
       }
 
       if (z_resolve.mode != VK_RESOLVE_MODE_NONE) {
-         if (z_ms2ss || z_att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) {
+         const struct pan_image *z_resolve_pimage =
+            pan_image_view_get_zs_plane(&z_resolve.dst_iview->pview).image;
+
+         if ((z_ms2ss || z_att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) &&
+             !avoid_direct_resolve_to(z_resolve_pimage)) {
             render->z_pview.resolve = get_z_pan_image_view(z_resolve.dst_iview);
             render->fb.resolve.z = (struct pan_fb_resolve_target) {
                .in_bounds = {
@@ -487,7 +517,11 @@ render_state_set_zs_attachments(struct panvk_cmd_buffer *cmdbuf,
       }
 
       if (s_resolve.mode != VK_RESOLVE_MODE_NONE) {
-         if (s_ms2ss || s_att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) {
+         const struct pan_image *s_resolve_pimage =
+            pan_image_view_get_s_plane(&s_resolve.dst_iview->pview).image;
+
+         if ((s_ms2ss || s_att->storeOp != VK_ATTACHMENT_STORE_OP_STORE) &&
+             !avoid_direct_resolve_to(s_resolve_pimage)) {
             render->s_pview.resolve = get_s_pan_image_view(s_resolve.dst_iview);
             render->fb.resolve.s = (struct pan_fb_resolve_target) {
                .in_bounds = {