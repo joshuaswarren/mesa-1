joshuaswarren/mesa-1

commit 8045ff8eedcb435d3773ea9bebcfa7961ae52e94

d3d12: Add support for Xbox GDK.
The big items in this patch:
- New screen file, to support the Xbox "windowing" system
- Lots of small macros/changes to support the Xbox D3D12 API without messing with the Win32 path too much
- A few changes to avoid requiring COM interfaces (the big one was QueryInterface which is unsupported)

Co-authored-by: Ethan Lee <flibitijibibo@gmail.com>
Co-authored-by: David Jacewicz <david.jacewicz@protonmail.com>
Co-authored-by: tieuchanlong <tieuchanlong@gmail.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19022>

src/gallium/drivers/d3d12/d3d12_resource.cpp
@@ -41,8 +41,10 @@
 #include <dxguids/dxguids.h>
 #include <memory>
 
+#ifndef _GAMING_XBOX
 #include <wrl/client.h>
 using Microsoft::WRL::ComPtr;
+#endif
 
 #ifndef GENERIC_ALL
  // This is only added to winadapter.h in newer DirectX-Headers
@@ -462,6 +464,7 @@ d3d12_resource_from_handle(struct pipe_screen *pscreen,
    HANDLE d3d_handle = (HANDLE) (intptr_t) handle->handle;
 #endif
 
+#ifndef _GAMING_XBOX
    if (handle->type == WINSYS_HANDLE_TYPE_D3D12_RES) {
       ComPtr<IUnknown> screen_device;
       ComPtr<IUnknown> res_device;
@@ -487,6 +490,7 @@ d3d12_resource_from_handle(struct pipe_screen *pscreen,
          }
       }
    }
+#endif
 
 #ifdef _WIN32
    HANDLE d3d_handle_to_close = nullptr;
@@ -501,10 +505,11 @@ d3d12_resource_from_handle(struct pipe_screen *pscreen,
    if (res->bo) {
       d3d12_res = res->bo->res;
    } else if (handle->type == WINSYS_HANDLE_TYPE_D3D12_RES) {
-      IUnknown *obj = (IUnknown *)handle->com_obj;
-      (void)obj->QueryInterface(&d3d12_res);
-      (void)obj->QueryInterface(&d3d12_heap);
-      obj->Release();
+      if (handle->modifier == 1) {
+         d3d12_heap = (ID3D12Heap *) handle->com_obj;
+      } else {
+         d3d12_res = (ID3D12Resource *) handle->com_obj;
+      }
    } else {
       screen->dev->OpenSharedHandle(d3d_handle, IID_PPV_ARGS(&d3d12_res));
    }
@@ -892,7 +897,12 @@ d3d12_memobj_create_from_handle(struct pipe_screen *pscreen, struct winsys_handl
    }
 
    struct d3d12_screen *screen = d3d12_screen(pscreen);
-   IUnknown *obj;
+#ifdef _GAMING_XBOX
+   IGraphicsUnknown
+#else
+   IUnknown
+#endif
+      *obj;
 #ifdef _WIN32
       HANDLE d3d_handle = handle->handle;
 #else
@@ -927,8 +937,13 @@ d3d12_memobj_create_from_handle(struct pipe_screen *pscreen, struct winsys_handl
    }
    memobj->base.dedicated = dedicated;
 
-   (void)obj->QueryInterface(&memobj->res);
-   (void)obj->QueryInterface(&memobj->heap);
+   obj->AddRef();
+   if (handle->modifier == 1) {
+      memobj->heap = (ID3D12Heap *) obj;
+   } else {
+      memobj->res = (ID3D12Resource *) obj;
+   }
+
    obj->Release();
    if (!memobj->res && !memobj->heap) {
       debug_printf("d3d12: Memory object isn't a resource or heap\n");
@@ -969,6 +984,7 @@ d3d12_resource_from_memobj(struct pipe_screen *pscreen,
    whandle.com_obj = memobj->res ? (void *) memobj->res : (void *) memobj->heap;
    whandle.offset = offset;
    whandle.format = templ->format;
+   whandle.modifier = memobj->res ? 0 : 1;
 
    // WINSYS_HANDLE_TYPE_D3D12_RES implies taking ownership of the reference
    ((IUnknown *)whandle.com_obj)->AddRef();

src/gallium/drivers/d3d12/d3d12_batch.cpp
@@ -235,7 +235,9 @@ d3d12_end_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
 
    mtx_lock(&screen->submit_mutex);
 
+#ifndef _GAMING_XBOX
    d3d12_process_batch_residency(screen, batch);
+#endif
 
    bool has_state_fixup = d3d12_context_state_resolve_submission(ctx, batch);
 

src/gallium/drivers/d3d12/d3d12_common.h
@@ -33,8 +33,27 @@
 #endif
 
 #define D3D12_IGNORE_SDK_LAYERS
+#ifndef _GAMING_XBOX
 #include <directx/d3d12.h>
 #include <directx/d3d12video.h>
+#elif defined(__cplusplus)
+#ifdef _GAMING_XBOX_SCARLETT
+#include <d3d12_xs.h>
+#include <d3d12video_xs.h>
+#else
+#include <d3d12_x.h>
+#include <d3d12video_x.h>
+#endif
+#ifdef IID_PPV_ARGS
+#undef IID_PPV_ARGS
+#endif
+#define IID_PPV_ARGS IID_GRAPHICS_PPV_ARGS
+#define D3D12_HEAP_FLAG_CREATE_NOT_RESIDENT (D3D12_HEAP_FLAGS) 0x800
+#endif /* _GAMING_XBOX */
+
+#ifndef D3D12_TEXTURE_DATA_PITCH_ALIGNMENT
+#define D3D12_TEXTURE_DATA_PITCH_ALIGNMENT D3D12XBOX_TEXTURE_DATA_PITCH_ALIGNMENT
+#endif /* D3D12_TEXTURE_DATA_PITCH_ALIGNMENT */
 
 #if defined(__cplusplus)
 #if !defined(_WIN32) || defined(_MSC_VER) || D3D12_SDK_VERSION < 606

src/gallium/drivers/d3d12/d3d12_context.cpp
@@ -76,8 +76,10 @@ d3d12_context_destroy(struct pipe_context *pctx)
       dxil_destroy_validator(ctx->dxil_validator);
 #endif
 
+#ifndef _GAMING_XBOX
    if (ctx->dev_config)
       ctx->dev_config->Release();
+#endif
 
    if (ctx->timestamp_query)
       pctx->destroy_query(pctx, ctx->timestamp_query);
@@ -2546,7 +2548,9 @@ d3d12_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
 
    ctx->D3D12SerializeVersionedRootSignature =
       (PFN_D3D12_SERIALIZE_VERSIONED_ROOT_SIGNATURE)util_dl_get_proc_address(screen->d3d12_mod, "D3D12SerializeVersionedRootSignature");
+#ifndef _GAMING_XBOX
    (void)screen->dev->QueryInterface(&ctx->dev_config);
+#endif
 
    ctx->submit_id = (uint64_t)p_atomic_add_return(&screen->ctx_count, 1) << 32ull;
 

src/gallium/drivers/d3d12/d3d12_context.h
@@ -255,7 +255,9 @@ struct d3d12_context {
    struct d3d12_descriptor_handle null_sampler;
 
    PFN_D3D12_SERIALIZE_VERSIONED_ROOT_SIGNATURE D3D12SerializeVersionedRootSignature;
+#ifndef _GAMING_XBOX
    ID3D12DeviceConfiguration *dev_config;
+#endif
 #ifdef _WIN32
    struct dxil_validator *dxil_validator;
 #endif

src/gallium/drivers/d3d12/d3d12_draw.cpp
@@ -41,10 +41,7 @@
 #include "util/u_prim_restart.h"
 #include "util/u_math.h"
 
-static const D3D12_RECT MAX_SCISSOR = { D3D12_VIEWPORT_BOUNDS_MIN,
-                                        D3D12_VIEWPORT_BOUNDS_MIN,
-                                        D3D12_VIEWPORT_BOUNDS_MAX,
-                                        D3D12_VIEWPORT_BOUNDS_MAX };
+static const D3D12_RECT MAX_SCISSOR = { 0, 0, 16384, 16384 };
 
 static const D3D12_RECT MAX_SCISSOR_ARRAY[] = {
    MAX_SCISSOR, MAX_SCISSOR, MAX_SCISSOR, MAX_SCISSOR,

src/gallium/drivers/d3d12/d3d12_dxgi_xbox_screen.cpp
@@ -1,0 +1,137 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <windows.h>
+
+#include "d3d12_screen.h"
+#include "d3d12_public.h"
+#include "d3d12_debug.h"
+
+#include "util/u_debug.h"
+#include "util/u_memory.h"
+#include "util/u_dl.h"
+
+static const char*
+dxgi_get_name(struct pipe_screen *screen)
+{
+   struct d3d12_dxgi_screen *dxgi_screen = d3d12_dxgi_screen(d3d12_screen(screen));
+   static char buf[1000];
+   if (dxgi_screen->description[0] == L'\0')
+      return "D3D12 (Unknown)";
+
+   snprintf(buf, sizeof(buf), "D3D12 (%S)", dxgi_screen->description);
+   return buf;
+}
+
+static void
+dxgi_get_memory_info(struct d3d12_screen *screen, struct d3d12_memory_info *output)
+{
+   assert(0);
+}
+
+static void
+d3d12_deinit_dxgi_screen(struct d3d12_screen *dscreen)
+{
+   d3d12_deinit_screen(dscreen);
+   struct d3d12_dxgi_screen *screen = d3d12_dxgi_screen(dscreen);
+   if (screen->adapter) {
+      screen->adapter->Release();
+      screen->adapter = nullptr;
+   }
+}
+
+static void
+d3d12_destroy_dxgi_screen(struct pipe_screen *pscreen)
+{
+   struct d3d12_screen *screen = d3d12_screen(pscreen);
+   d3d12_deinit_dxgi_screen(screen);
+   d3d12_destroy_screen(screen);
+}
+
+static bool
+d3d12_init_dxgi_screen(struct d3d12_screen *dscreen)
+{
+   if (!d3d12_init_screen(dscreen, NULL)) {
+      debug_printf("D3D12: failed to initialize DXGI screen\n");
+      return false;
+   }
+
+   struct d3d12_dxgi_screen *screen = d3d12_dxgi_screen(dscreen);
+   IDXGIDevice1 *dxgiDevice = nullptr;
+
+   if (FAILED(dscreen->dev->QueryInterface(IID_PPV_ARGS(&dxgiDevice)))) {
+      debug_printf("D3D12: failed to query dxgi interface\n");
+      return false;
+   }
+
+   if (FAILED(dxgiDevice->GetAdapter(&screen->adapter))) {
+      debug_printf("D3D12: failed to get adapter\n");
+      return false;
+   }
+
+   dxgiDevice->Release();
+   dxgiDevice = nullptr;
+
+   DXGI_ADAPTER_DESC adapter_desc = {};
+   HRESULT res = screen->adapter->GetDesc(&adapter_desc);
+   if (FAILED(res)) {
+      debug_printf("D3D12: failed to retrieve adapter description\n");
+      return false;
+   }
+
+   screen->base.driver_version = 0;
+   screen->base.vendor_id = adapter_desc.VendorId;
+   screen->base.device_id = adapter_desc.DeviceId;
+   screen->base.subsys_id = adapter_desc.SubSysId;
+   screen->base.revision = adapter_desc.Revision;
+   // Note: memory sizes in bytes, but stored in size_t, so may be capped at 4GB.
+   // In that case, adding before conversion to MB can easily overflow.
+   screen->base.memory_size_megabytes = (adapter_desc.DedicatedVideoMemory >> 20) +
+      (adapter_desc.DedicatedSystemMemory >> 20) +
+      (adapter_desc.SharedSystemMemory >> 20);
+   wcsncpy(screen->description, adapter_desc.Description, ARRAY_SIZE(screen->description));
+   screen->base.base.get_name = dxgi_get_name;
+   screen->base.get_memory_info = dxgi_get_memory_info;
+
+   return true;
+}
+
+struct pipe_screen*
+   d3d12_create_dxgi_screen(struct sw_winsys *winsys, LUID *adapter_luid)
+{
+   struct d3d12_dxgi_screen *screen = CALLOC_STRUCT(d3d12_dxgi_screen);
+   if (!screen)
+      return nullptr;
+
+   d3d12_init_screen_base(&screen->base, winsys, adapter_luid);
+   screen->base.base.destroy = d3d12_destroy_dxgi_screen;
+   screen->base.init = d3d12_init_dxgi_screen;
+   screen->base.deinit = d3d12_deinit_dxgi_screen;
+
+   if (!d3d12_init_dxgi_screen(&screen->base)) {
+      d3d12_destroy_dxgi_screen(&screen->base.base);
+      return nullptr;
+   }
+
+   return &screen->base.base;
+}

src/gallium/drivers/d3d12/d3d12_fence.cpp
@@ -49,9 +49,9 @@ d3d12_create_fence(struct d3d12_screen *screen)
    ret->cmdqueue_fence = screen->fence;
    ret->value = ++screen->fence_value;
    ret->event = d3d12_fence_create_event(&ret->event_fd);
-   if (FAILED(screen->fence->SetEventOnCompletion(ret->value, ret->event)))
-      goto fail;
    if (FAILED(screen->cmdqueue->Signal(screen->fence, ret->value)))
+      goto fail;
+   if (FAILED(screen->fence->SetEventOnCompletion(ret->value, ret->event)))
       goto fail;
 
    pipe_reference_init(&ret->reference, 1);

commit 9403c3f83ef902847bc8fb6eb39bb9d390cf7dcc

d3d12: Fix importing external resources
Fixes: 8045ff8e ("d3d12: Add support for Xbox GDK.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40152>

src/gallium/drivers/d3d12/d3d12_resource.cpp
@@ -604,11 +604,18 @@ d3d12_resource_from_handle(struct pipe_screen *pscreen,
    if (res->bo) {
       d3d12_res = res->bo->res;
    } else if (handle->type == WINSYS_HANDLE_TYPE_D3D12_RES) {
+#ifdef _GAMING_XBOX
       if (handle->modifier == 1) {
          d3d12_heap = (ID3D12Heap *) handle->com_obj;
       } else {
          d3d12_res = (ID3D12Resource *) handle->com_obj;
       }
+#else
+      IUnknown *obj = (IUnknown *) handle->com_obj;
+      (void)obj->QueryInterface(&d3d12_res);
+      (void)obj->QueryInterface(&d3d12_heap);
+      obj->Release();
+#endif
    } else {
       screen->dev->OpenSharedHandle(d3d_handle, IID_PPV_ARGS(&d3d12_res));
    }
@@ -1070,12 +1077,17 @@ d3d12_memobj_create_from_handle(struct pipe_screen *pscreen, struct winsys_handl
    }
    memobj->base.dedicated = dedicated;
 
+#ifdef _GAMING_XBOX
    obj->AddRef();
    if (handle->modifier == 1) {
       memobj->heap = (ID3D12Heap *) obj;
    } else {
       memobj->res = (ID3D12Resource *) obj;
    }
+#else
+   (void)obj->QueryInterface(&memobj->heap);
+   (void)obj->QueryInterface(&memobj->res);
+#endif
 
    obj->Release();
    if (!memobj->res && !memobj->heap) {
@@ -1123,7 +1135,9 @@ d3d12_resource_from_memobj(struct pipe_screen *pscreen,
 
    whandle.offset = static_cast<unsigned int>(offset);
    whandle.format = templ->format;
+#ifdef _GAMING_XBOX
    whandle.modifier = memobj->res ? 0 : 1;
+#endif
 
    // WINSYS_HANDLE_TYPE_D3D12_RES implies taking ownership of the reference
    ((IUnknown *)whandle.com_obj)->AddRef();