openharmony/third_party_mesa3d

commit dc0bd65e167384a60cf4339e7a3b72d5f2c29177

r300: skip draws instead of using a dummy vertex shader
When we fail to compile some vertex shader, we currently use a very
simple dummy one, setting gl_Position to (0,0,0,1), effectively
rendering nothing. Unfortunately, the dummy vertex shader leads to
hangs with RV370 in some rare circumstances. Instead of trying to
fix the shader, just skip the draws altogether when the compilation
fails.

Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5870
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16387>

src/gallium/drivers/r300/r300_vs.c
@@ -178,30 +178,6 @@ void r300_init_vs_outputs(struct r300_context *r300,
     r300_shader_read_vs_outputs(r300, &vs->shader->info, &vs->shader->outputs);
 }
 
-static void r300_dummy_vertex_shader(
-    struct r300_context* r300,
-    struct r300_vertex_shader* vs)
-{
-    struct ureg_program *ureg;
-    struct ureg_dst dst;
-    struct ureg_src imm;
-
-    /* Make a simple vertex shader which outputs (0, 0, 0, 1),
-     * effectively rendering nothing. */
-    ureg = ureg_create(PIPE_SHADER_VERTEX);
-    dst = ureg_DECL_output(ureg, TGSI_SEMANTIC_POSITION, 0);
-    imm = ureg_imm4f(ureg, 0, 0, 0, 1);
-
-    ureg_MOV(ureg, dst, imm);
-    ureg_END(ureg);
-
-    vs->state.tokens = tgsi_dup_tokens(ureg_finalize(ureg));
-    ureg_destroy(ureg);
-
-    vs->shader->dummy = TRUE;
-    r300_translate_vertex_shader(r300, vs);
-}
-
 void r300_translate_vertex_shader(struct r300_context *r300,
                                   struct r300_vertex_shader *shader)
 {
@@ -219,8 +195,7 @@ void r300_translate_vertex_shader(struct r300_context *r300,
     DBG_ON(r300, DBG_VP) ? compiler.Base.Debug |= RC_DBG_LOG : 0;
     compiler.code = &vs->code;
     compiler.UserData = vs;
-    if (!vs->dummy)
-        compiler.Base.debug = &r300->debug;
+    compiler.Base.debug = &r300->debug;
     compiler.Base.is_r500 = r300->screen->caps.is_r500;
     compiler.Base.disable_optimizations = DBG_ON(r300, DBG_NO_OPT);
     compiler.Base.has_half_swizzles = FALSE;
@@ -245,8 +220,8 @@ void r300_translate_vertex_shader(struct r300_context *r300,
 
     if (ttr.error) {
         fprintf(stderr, "r300 VP: Cannot translate a shader. "
-                "Using a dummy shader instead.\n");
-        r300_dummy_vertex_shader(r300, shader);
+                "Corresponding draws will be skipped.\n");
+        vs->dummy = TRUE;
         return;
     }
 
@@ -264,17 +239,11 @@ void r300_translate_vertex_shader(struct r300_context *r300,
     /* Invoke the compiler */
     r3xx_compile_vertex_program(&compiler);
     if (compiler.Base.Error) {
-        fprintf(stderr, "r300 VP: Compiler error:\n%sUsing a dummy shader"
-                " instead.\n", compiler.Base.ErrorMsg);
-
-        if (vs->dummy) {
-            fprintf(stderr, "r300 VP: Cannot compile the dummy shader! "
-                    "Giving up...\n");
-            abort();
-        }
+        fprintf(stderr, "r300 VP: Compiler error:\n%sCorresponding draws will be"
+                " skipped.\n", compiler.Base.ErrorMsg);
 
         rc_destroy(&compiler.Base);
-        r300_dummy_vertex_shader(r300, shader);
+        vs->dummy = TRUE;
         return;
     }
 

src/gallium/drivers/r300/r300_render.c
@@ -40,6 +40,7 @@
 #include "r300_screen_buffer.h"
 #include "r300_emit.h"
 #include "r300_reg.h"
+#include "r300_vs.h"
 
 #include <limits.h>
 
@@ -814,6 +815,10 @@ static void r300_draw_vbo(struct pipe_context* pipe,
         }
 
     r300_update_derived_state(r300);
+
+    /* Skip draw if we failed to compile the vertex shader. */
+    if (r300_vs(r300)->shader->dummy)
+        return;
 
     /* Draw. */
     if (info.index_size) {

src/gallium/drivers/r300/r300_state_derived.c
@@ -1074,7 +1074,8 @@ static void r300_pick_vertex_shader(struct r300_context *r300)
             vs->first = vs->shader = CALLOC_STRUCT(r300_vertex_shader_code);
             vs->first->wpos = wpos;
             r300_translate_vertex_shader(r300, vs);
-            r300_mark_atom_dirty(r300, &r300->rs_block_state);
+            if (!vs->first->dummy)
+                r300_mark_atom_dirty(r300, &r300->rs_block_state);
             return;
         }
         /* Pick the vertex shader based on whether we need wpos */
@@ -1091,7 +1092,8 @@ static void r300_pick_vertex_shader(struct r300_context *r300)
                 vs->shader->wpos = wpos;
                 r300_translate_vertex_shader(r300, vs);
             }
-            r300_mark_atom_dirty(r300, &r300->rs_block_state);
+            if (!vs->first->dummy)
+                r300_mark_atom_dirty(r300, &r300->rs_block_state);
         }
     }
 }

joshuaswarren/mesa-1

commit 4d5858ae0ede4db5fa3ab6a52687f48297155af1

r300: pad short vertex shaders to avoid R3xx hangs
Vertex shaders shorter than four instructions can hard-lock R3xx GPUs.
This seems to happen in combination with a small vertex count. This was
seen before, most notably with dummy shaders, but the earlier fix only
removed those dummy shaders, so some occurrences could still slip
through the cracks. Pad all vertex shaders to four instructions on R3xx.

Reviewed-by: Filip Gawin <filip@gawin.net>
Fixes: dc0bd65e167 ("r300: skip draws instead of using a dummy vertex shader")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/337
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40331>

src/gallium/drivers/r300/compiler/r3xx_vertprog.c
@@ -351,6 +351,42 @@ ei_pow(struct r300_vertex_program_code *vp, struct rc_sub_instruction *vpi, unsi
 }
 
 static void
+ei_vector0(struct r300_vertex_program_code *vp,
+           struct rc_sub_instruction *vpi,
+           unsigned int hw_opcode,
+           unsigned int *inst)
+{
+   inst[0] = PVS_OP_DST_OPERAND(hw_opcode, 0, 0, 0, 0, PVS_DST_REG_TEMPORARY, 0);
+   inst[1] = __CONST(0, RC_SWIZZLE_ZERO);
+   inst[2] = inst[1];
+   inst[3] = inst[1];
+}
+
+static void
+pad_vertex_program_instructions(struct radeon_compiler *c)
+{
+   const unsigned min_inst_count = 4;
+   unsigned inst_count = 0;
+
+   for (struct rc_instruction *inst = c->Program.Instructions.Next;
+        inst != &c->Program.Instructions; inst = inst->Next)
+      inst_count++;
+
+   unsigned orig_inst_count = inst_count;
+
+   while (inst_count < min_inst_count) {
+      struct rc_instruction *inst = rc_insert_new_instruction(c, c->Program.Instructions.Prev);
+      inst->U.I.Opcode = RC_OPCODE_NOP;
+      inst->U.I.SrcReg[0].File = RC_FILE_NONE;
+      inst_count++;
+   }
+
+   if (orig_inst_count < min_inst_count)
+      rc_debug(c, "r300: padded tiny VS from %u to %u instructions\n",
+               orig_inst_count, inst_count);
+}
+
+static void
 translate_vertex_program(struct radeon_compiler *c, void *user)
 {
    struct r300_vertex_program_compiler *compiler = (struct r300_vertex_program_compiler *)c;
@@ -368,6 +404,10 @@ translate_vertex_program(struct radeon_compiler *c, void *user)
    compiler->code->last_pos_write = 0;
 
    compiler->SetHwInputOutput(compiler);
+
+   /* Vertex shaders shorter than 4 instructions can hard-lock r3xx GPUs. */
+   if (!c->is_r400 && !c->is_r500)
+      pad_vertex_program_instructions(c);
 
    for (rci = compiler->Base.Program.Instructions.Next; rci != &compiler->Base.Program.Instructions;
         rci = rci->Next) {
@@ -396,6 +436,9 @@ translate_vertex_program(struct radeon_compiler *c, void *user)
              (vpi->Opcode != RC_OPCODE_SEQ && vpi->Opcode != RC_OPCODE_SNE));
 
       switch (vpi->Opcode) {
+      case RC_OPCODE_NOP:
+         ei_vector0(compiler->code, vpi, VECTOR_NO_OP, inst);
+         break;
       case RC_OPCODE_ADD:
          ei_vector2(compiler->code, VE_ADD, vpi, inst);
          break;

src/gallium/drivers/r300/r300_vs.c
@@ -183,6 +183,7 @@ void r300_translate_vertex_shader(struct r300_context *r300,
     compiler.code = &vs->code;
     compiler.UserData = vs;
     compiler.Base.debug = &r300->context.debug;
+    compiler.Base.is_r400 = r300->screen->caps.is_r400;
     compiler.Base.is_r500 = r300->screen->caps.is_r500;
     compiler.Base.disable_optimizations = DBG_ON(r300, DBG_NO_OPT);
     /* Only R500 has few IEEE math opcodes. */