joshuaswarren/mesa-1

commit d717d5eb37051831d0fc71a0b37211b92fbcc022

pvr, pco: tile buffer support
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36412>

src/imagination/pco/pco_trans_nir.c
@@ -429,6 +429,7 @@ trans_load_input_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref dest)
 static pco_instr *
 trans_store_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref src)
 {
+   pco_fs_data *fs_data = &tctx->shader->data.fs;
    ASSERTED unsigned base = nir_intrinsic_base(intr);
    assert(!base);
 
@@ -440,15 +441,108 @@ trans_store_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref src)
 
    gl_frag_result location = nir_intrinsic_io_semantics(intr).location;
 
-   const pco_range *range = &tctx->shader->data.fs.outputs[location];
+   const pco_range *range = &fs_data->outputs[location];
    assert(component < range->count);
 
-   ASSERTED bool output_reg = tctx->shader->data.fs.output_reg[location];
-   assert(output_reg);
-   /* TODO: tile buffer support. */
+   unsigned idx = location - FRAG_RESULT_DATA0;
+   bool tile_buffer = fs_data->output_tile_buffers & BITFIELD_BIT(idx);
+   if (!tile_buffer) {
+      pco_ref dest =
+         pco_ref_hwreg(range->start + component, PCO_REG_CLASS_PIXOUT);
+      return pco_mov(&tctx->b, dest, src, .olchk = true);
+   }
 
-   pco_ref dest = pco_ref_hwreg(range->start + component, PCO_REG_CLASS_PIXOUT);
-   return pco_mov(&tctx->b, dest, src, .olchk = true);
+   unsigned tile_buffer_id = range->start;
+   pco_range *tile_buffers = &fs_data->tile_buffers;
+   assert(tile_buffer_id < (tile_buffers->count / tile_buffers->stride));
+   unsigned sh_index =
+      tile_buffers->start + tile_buffer_id * tile_buffers->stride;
+
+   pco_ref base_addr[2];
+   pco_ref_hwreg_addr_comps(sh_index, PCO_REG_CLASS_SHARED, base_addr);
+
+   pco_ref addr_data_comps[3] = {
+      [2] = src,
+   };
+   pco_ref_new_ssa_addr_comps(tctx->func, addr_data_comps);
+
+   component += range->offset;
+   assert(component < 8);
+
+   unsigned sr_index = component < 4 ? component + PCO_SR_TILED_ST_COMP0
+                                     : component + PCO_SR_TILED_ST_COMP4 - 4;
+   pco_ref tiled_offset = pco_ref_hwreg(sr_index, PCO_REG_CLASS_SPEC);
+
+   pco_add64_32(&tctx->b,
+                addr_data_comps[0],
+                addr_data_comps[1],
+                base_addr[0],
+                base_addr[1],
+                tiled_offset,
+                pco_ref_null(),
+                .olchk = true,
+                .s = true);
+
+   unsigned chans = pco_ref_get_chans(src);
+   pco_ref addr_data = pco_ref_new_ssa_addr_data(tctx->func, chans);
+   pco_vec(&tctx->b, addr_data, ARRAY_SIZE(addr_data_comps), addr_data_comps);
+
+   pco_ref data_comp =
+      pco_ref_new_ssa(tctx->func, pco_ref_get_bits(src), chans);
+   pco_comp(&tctx->b, data_comp, addr_data, pco_ref_val16(2));
+
+   pco_ref cov_mask = pco_ref_new_ssa32(tctx->func);
+   pco_ref sample_id = pco_ref_hwreg(PCO_SR_SAMP_NUM, PCO_REG_CLASS_SPEC);
+   pco_shift(&tctx->b,
+             cov_mask,
+             pco_one,
+             sample_id,
+             pco_ref_null(),
+             .shiftop = PCO_SHIFTOP_LSL);
+
+   return pco_st_tiled(&tctx->b,
+                       data_comp,
+                       pco_ref_imm8(PCO_DSIZE_32BIT),
+                       pco_ref_drc(PCO_DRC_0),
+                       pco_ref_imm8(chans),
+                       addr_data,
+                       cov_mask);
+}
+
+static pco_instr *trans_flush_tile_buffer(trans_ctx *tctx,
+                                          nir_intrinsic_instr *intr,
+                                          pco_ref src_addr_lo,
+                                          pco_ref src_addr_hi)
+{
+   pco_ref addr_comps[2];
+   pco_ref_new_ssa_addr_comps(tctx->func, addr_comps);
+
+   pco_ref tiled_offset =
+      pco_ref_hwreg(PCO_SR_TILED_LD_COMP0, PCO_REG_CLASS_SPEC);
+
+   pco_add64_32(&tctx->b,
+                addr_comps[0],
+                addr_comps[1],
+                src_addr_lo,
+                src_addr_hi,
+                tiled_offset,
+                pco_ref_null(),
+                .olchk = true,
+                .s = true);
+
+   pco_ref addr = pco_ref_new_ssa_addr(tctx->func);
+   pco_vec(&tctx->b, addr, ARRAY_SIZE(addr_comps), addr_comps);
+
+   unsigned idx_reg_num = 0;
+   pco_ref idx_reg =
+      pco_ref_hwreg_idx(idx_reg_num, idx_reg_num, PCO_REG_CLASS_INDEX);
+
+   pco_mbyp(&tctx->b, idx_reg, pco_zero);
+
+   pco_ref dest = pco_ref_hwreg(0, PCO_REG_CLASS_PIXOUT);
+   dest = pco_ref_hwreg_idx_from(idx_reg_num, dest);
+
+   return pco_ld_regbl(&tctx->b, dest, pco_ref_drc(PCO_DRC_0), pco_zero, addr);
 }
 
 static unsigned fetch_resource_base_reg(const pco_common_data *common,
@@ -515,7 +609,10 @@ static unsigned fetch_resource_base_reg_packed(const pco_common_data *common,
 static pco_instr *
 trans_load_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref dest)
 {
+   pco_fs_data *fs_data = &tctx->shader->data.fs;
    unsigned base = nir_intrinsic_base(intr);
+
+   assert(pco_ref_is_scalar(dest));
    unsigned component = nir_intrinsic_component(intr);
 
    ASSERTED const nir_src offset = intr->src[0];
@@ -524,26 +621,67 @@ trans_load_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref dest)
    gl_frag_result location = nir_intrinsic_io_semantics(intr).location;
 
    const pco_range *range;
+   bool tile_buffer;
    if (location >= FRAG_RESULT_DATA0) {
       assert(!base);
-
       range = &tctx->shader->data.fs.outputs[location];
-
-      ASSERTED bool output_reg = tctx->shader->data.fs.output_reg[location];
-      assert(output_reg);
-      /* TODO: tile buffer support. */
+      unsigned idx = location - FRAG_RESULT_DATA0;
+      tile_buffer = fs_data->output_tile_buffers & BITFIELD_BIT(idx);
    } else if (location == FRAG_RESULT_COLOR) {
       /* Special case for on-chip input attachments. */
       assert(base < ARRAY_SIZE(tctx->shader->data.fs.ias_onchip));
       range = &tctx->shader->data.fs.ias_onchip[base];
+      tile_buffer = fs_data->ia_tile_buffers & BITFIELD_BIT(base);
    } else {
       UNREACHABLE("");
    }
 
    assert(component < range->count);
 
-   pco_ref src = pco_ref_hwreg(range->start + component, PCO_REG_CLASS_PIXOUT);
-   return pco_mov(&tctx->b, dest, src, .olchk = true);
+   if (!tile_buffer) {
+      pco_ref src =
+         pco_ref_hwreg(range->start + component, PCO_REG_CLASS_PIXOUT);
+      return pco_mov(&tctx->b, dest, src, .olchk = true);
+   }
+
+   unsigned tile_buffer_id = range->start;
+   pco_range *tile_buffers = &fs_data->tile_buffers;
+   assert(tile_buffer_id < (tile_buffers->count / tile_buffers->stride));
+   unsigned sh_index =
+      tile_buffers->start + tile_buffer_id * tile_buffers->stride;
+
+   pco_ref base_addr[2];
+   pco_ref_hwreg_addr_comps(sh_index, PCO_REG_CLASS_SHARED, base_addr);
+
+   pco_ref addr_comps[2];
+   pco_ref_new_ssa_addr_comps(tctx->func, addr_comps);
+
+   component += range->offset;
+   assert(component < 8);
+
+   unsigned sr_index = component < 4 ? component + PCO_SR_TILED_LD_COMP0
+                                     : component + PCO_SR_TILED_LD_COMP4 - 4;
+   pco_ref tiled_offset = pco_ref_hwreg(sr_index, PCO_REG_CLASS_SPEC);
+
+   pco_add64_32(&tctx->b,
+                addr_comps[0],
+                addr_comps[1],
+                base_addr[0],
+                base_addr[1],
+                tiled_offset,
+                pco_ref_null(),
+                .olchk = true,
+                .s = true);
+
+   pco_ref addr = pco_ref_new_ssa_addr(tctx->func);
+   pco_vec(&tctx->b, addr, ARRAY_SIZE(addr_comps), addr_comps);
+
+   unsigned chans = pco_ref_get_chans(dest);
+   return pco_ld(&tctx->b,
+                 dest,
+                 pco_ref_drc(PCO_DRC_0),
+                 pco_ref_imm8(chans),
+                 addr);
 }
 
 static pco_instr *trans_load_common_store(trans_ctx *tctx,
@@ -1293,6 +1431,11 @@ static pco_instr *trans_intr(trans_ctx *tctx, nir_intrinsic_instr *intr)
    case nir_intrinsic_load_output:
       assert(tctx->stage == MESA_SHADER_FRAGMENT);
       instr = trans_load_output_fs(tctx, intr, dest);
+      break;
+
+   case nir_intrinsic_flush_tile_buffer_pco:
+      assert(tctx->stage == MESA_SHADER_FRAGMENT);
+      instr = trans_flush_tile_buffer(tctx, intr, src[0], src[1]);
       break;
 
    case nir_intrinsic_load_preamble:

src/compiler/nir/nir_intrinsics.py
@@ -2740,3 +2740,5 @@ system_value("face_ccw_pco", 1, bit_sizes=[32])
 system_value("front_face_op_pco", 1, bit_sizes=[32])
 
 system_value("fs_meta_pco", 1, bit_sizes=[32])
+
+intrinsic("flush_tile_buffer_pco", src_comp=[1, 1])

src/imagination/pco/pco_data.h
@@ -30,7 +30,10 @@ typedef struct _pco_shader pco_shader;
 typedef struct _pco_range {
    unsigned start;
    unsigned count;
-   unsigned stride;
+   union {
+      unsigned stride;
+      unsigned offset;
+   };
 } pco_range;
 
 /** PCO vertex shader-specific data. */
@@ -71,17 +74,21 @@ typedef struct _pco_fs_data {
    /** Results/output mappings. */
    pco_range outputs[FRAG_RESULT_MAX];
 
-   /** If outputs are to be placed in pixout regs. */
-   bool output_reg[FRAG_RESULT_MAX];
+   /** If outputs are to be placed in tile buffers. */
+   uint8_t output_tile_buffers;
 
    /** Fragment output formats. */
    enum pipe_format output_formats[FRAG_RESULT_MAX];
 
    /** On-chip input attachment mappings. */
-   pco_range ias_onchip[4];
+   pco_range ias_onchip[8];
 
    /** On-chip input attachment formats. */
-   enum pipe_format ia_formats[4];
+   enum pipe_format ia_formats[8];
+
+   uint8_t ia_tile_buffers;
+
+   unsigned num_tile_buffers;
 
    pco_range meta;
 
@@ -93,6 +100,8 @@ typedef struct _pco_fs_data {
    uint8_t blend_consts_needed;
 
    uint16_t rasterization_samples;
+
+   pco_range tile_buffers;
 
    struct {
       bool w; /** Whether the shader uses pos.w. */

src/imagination/pco/pco_map.py
@@ -1336,6 +1336,19 @@ encode_map(O_LD,
    op_ref_maps=[('backend', ['s3'], ['drc', 'imm', ['s0', 's1', 's2', 's3', 's4', 's5']])]
 )
 
+encode_map(O_LD_REGBL,
+   encodings=[
+      (I_LD_REGBL, [
+         ('drc', ('pco_ref_get_drc', SRC(0))),
+
+         ('srcseladd', ('pco_ref_srcsel', SRC(2))),
+         ('srcselbl', ('pco_ref_srcsel', SRC(1))),
+         ('cachemode_ld', OM_MCU_CACHE_MODE_LD)
+      ])
+   ],
+   op_ref_maps=[('backend', ['s3'], ['drc', ['s0', 's1', 's2', 's3', 's4', 's5'], ['s0', 's1', 's2', 's3', 's4', 's5']])]
+)
+
 encode_map(O_ST,
    encodings=[
       (I_ST_IMMBL, [
@@ -1345,6 +1358,21 @@ encode_map(O_ST,
          ('cachemode_st', OM_MCU_CACHE_MODE_ST),
          ('srcseldata', ('pco_ref_srcsel', SRC(0))),
          ('dsize', ('pco_ref_get_imm', SRC(1)))
+      ])
+   ],
+   op_ref_maps=[('backend', [], [['s0', 's1', 's2', 's3', 's4', 's5'], 'imm', 'drc', 'imm', ['s0', 's1', 's2', 's3', 's4', 's5'], ['s0', 's1', 's2', 's3', 's4', 's5', '_']])]
+)
+
+encode_map(O_ST_TILED,
+   encodings=[
+      (I_ST_IMMBL_TILED, [
+         ('drc', ('pco_ref_get_drc', SRC(2))),
+         ('srcseladd', ('pco_ref_srcsel', SRC(4))),
+         ('burstlen', ('pco_ref_get_imm', SRC(3))),
+         ('cachemode_st', OM_MCU_CACHE_MODE_ST),
+         ('srcseldata', ('pco_ref_srcsel', SRC(0))),
+         ('dsize', ('pco_ref_get_imm', SRC(1))),
+         ('srcmask', ('pco_ref_srcsel', SRC(5)))
       ])
    ],
    op_ref_maps=[('backend', [], [['s0', 's1', 's2', 's3', 's4', 's5'], 'imm', 'drc', 'imm', ['s0', 's1', 's2', 's3', 's4', 's5'], ['s0', 's1', 's2', 's3', 's4', 's5', '_']])]
@@ -2608,6 +2636,25 @@ group_map(O_ST32,
    ]
 )
 
+group_map(O_ST_TILED,
+   hdr=(I_IGRP_HDR_MAIN, [
+      ('oporg', 'be'),
+      ('olchk', OM_OLCHK),
+      ('w1p', False),
+      ('w0p', False),
+      ('cc', OM_EXEC_CND),
+      ('end', OM_END),
+      ('atom', OM_ATOM),
+      ('rpt', 1)
+   ]),
+   enc_ops=[('backend', O_ST_TILED)],
+   srcs=[
+      ('s[0]', ('backend', SRC(0)), 's0'),
+      ('s[3]', ('backend', SRC(4)), 's3'),
+      ('s[4]', ('backend', SRC(5)), 's4')
+   ]
+)
+
 group_map(O_IADD32_ATOMIC,
    hdr=(I_IGRP_HDR_MAIN, [
       ('oporg', 'p0_p1'),
@@ -2853,6 +2900,25 @@ group_map(O_LD,
    enc_ops=[('backend', O_LD)],
    srcs=[
       ('s[0]', ('backend', SRC(2)), 's0'),
+      ('s[3]', ('backend', DEST(0)), 's3')
+   ]
+)
+
+group_map(O_LD_REGBL,
+   hdr=(I_IGRP_HDR_MAIN, [
+      ('oporg', 'be'),
+      ('olchk', OM_OLCHK),
+      ('w1p', False),
+      ('w0p', False),
+      ('cc', OM_EXEC_CND),
+      ('end', OM_END),
+      ('atom', OM_ATOM),
+      ('rpt', 1)
+   ]),
+   enc_ops=[('backend', O_LD_REGBL)],
+   srcs=[
+      ('s[0]', ('backend', SRC(2)), 's0'),
+      ('s[1]', ('backend', SRC(1)), 's1'),
       ('s[3]', ('backend', DEST(0)), 's3')
    ]
 )

src/imagination/pco/pco_nir_vk.c
@@ -183,7 +183,7 @@ lower_image_derefs(nir_builder *b, nir_intrinsic_instr *intr, pco_data *data)
 
    if (ia) {
       unsigned ia_idx = var->data.index;
-      bool onchip = data->fs.ias_onchip[ia_idx].count > 0;
+      bool onchip = data->fs.ia_formats[ia_idx] != PIPE_FORMAT_NONE;
 
       if (onchip) {
          nir_def *elem = array_elem_from_deref(b, deref);

src/imagination/pco/pco_ops.py
@@ -380,7 +380,13 @@ O_FITR = hw_op('fitr', [OM_EXEC_CND, OM_END, OM_ITR_MODE, OM_SAT], 1, 3)
 O_FITRP = hw_op('fitrp', [OM_EXEC_CND, OM_END, OM_ITR_MODE, OM_SAT], 1, 4)
 
 O_LD = hw_op('ld', OM_ALU_RPT1 + [OM_MCU_CACHE_MODE_LD], 1, 3)
+O_LD_REGBL = hw_op('ld.regbl', OM_ALU_RPT1 + [OM_MCU_CACHE_MODE_LD], 1, 3)
+
 O_ST = hw_direct_op('st', [OM_MCU_CACHE_MODE_ST], 0, 6)
+
+# { data, data size, drc, chans, addr, cov_msk}
+O_ST_TILED = hw_op('st.tiled', OM_ALU_RPT1 + [OM_MCU_CACHE_MODE_ST], 0, 6)
+
 O_ATOMIC = hw_op('atomic', [OM_OLCHK, OM_EXEC_CND, OM_END, OM_ATOM_OP], 1, 2)
 
 O_SMP = hw_op('smp', OM_ALU_RPT1 + [OM_DIM, OM_PROJ, OM_FCNORM, OM_NNCOORDS,

src/imagination/vulkan/pds/pvr_pds.h
@@ -900,6 +900,7 @@ struct pvr_pds_descriptor_set {
 #define PVR_BUFFER_TYPE_IA_SAMPLER (8)
 #define PVR_BUFFER_TYPE_FRONT_FACE_OP (9)
 #define PVR_BUFFER_TYPE_FS_META (10)
+#define PVR_BUFFER_TYPE_TILE_BUFFERS (11)
 #define PVR_BUFFER_TYPE_INVALID (~0)
 
 struct pvr_pds_buffer {

src/imagination/vulkan/pds/pvr_pipeline_pds.c
@@ -1580,7 +1580,8 @@ void pvr_pds_generate_descriptor_upload_program(
       case PVR_BUFFER_TYPE_POINT_SAMPLER:
       case PVR_BUFFER_TYPE_IA_SAMPLER:
       case PVR_BUFFER_TYPE_FRONT_FACE_OP:
-      case PVR_BUFFER_TYPE_FS_META: {
+      case PVR_BUFFER_TYPE_FS_META:
+      case PVR_BUFFER_TYPE_TILE_BUFFERS: {
          struct pvr_const_map_entry_special_buffer *special_buffer_entry;
 
          special_buffer_entry =

commit 2328662666fde7b0c9fb0faefb1f466ab0c60618

pco: use vm/icm for tile buffer store coverage mask
Use the valid/input coverage masks for tile buffer store coverage masks
when running single/multi-sampled fragment shaders respectively.

Fixes: d717d5eb370 ("pvr, pco: tile buffer support")

Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Reported-by: Nick Hamilton <nick.hamilton@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40456>

src/imagination/pco/pco_trans_nir.c
@@ -657,14 +657,26 @@ trans_store_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref src)
       pco_ref_new_ssa(tctx->func, pco_ref_get_bits(src), chans);
    pco_comp(&tctx->b, data_comp, addr_data, pco_ref_val16(2));
 
+   pco_ref cov_mask_ss = pco_ref_new_ssa32(tctx->func);
+   pco_savmsk(&tctx->b,
+              cov_mask_ss,
+              pco_ref_null(),
+              .savmsk_mode = PCO_SAVMSK_MODE_VM);
+
+   pco_ref cov_mask_ms = pco_ref_new_ssa32(tctx->func);
+   pco_savmsk(&tctx->b,
+              cov_mask_ms,
+              pco_ref_null(),
+              .savmsk_mode = PCO_SAVMSK_MODE_ICM);
+
    pco_ref cov_mask = pco_ref_new_ssa32(tctx->func);
-   pco_ref sample_id = pco_ref_hwreg(PCO_SR_SAMP_NUM, PCO_REG_CLASS_SPEC);
-   pco_shift(&tctx->b,
-             cov_mask,
-             pco_one,
-             sample_id,
-             pco_ref_null(),
-             .shiftop = PCO_SHIFTOP_LSL);
+   pco_csel(&tctx->b,
+            cov_mask,
+            fs_is_single_sampled(tctx),
+            cov_mask_ss,
+            cov_mask_ms,
+            .tst_op_main = PCO_TST_OP_MAIN_GZERO,
+            .tst_type_main = PCO_TST_TYPE_MAIN_U32);
 
    return pco_st_tiled(&tctx->b,
                        data_comp,