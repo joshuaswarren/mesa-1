joshuaswarren/mesa-1

commit f4120743086d2e29f5087ede94711a3df01fee59

radv/rt: Use function call structure in NIR lowering
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29580>

src/amd/vulkan/radv_pipeline_rt.c
@@ -13,6 +13,7 @@
 
 #include "nir/radv_nir.h"
 #include "nir/radv_nir_rt_stage_cps.h"
+#include "nir/radv_nir_rt_stage_functions.h"
 #include "nir/radv_nir_rt_stage_monolithic.h"
 #include "nir/radv_nir_rt_traversal_shader.h"
 #include "ac_nir.h"
@@ -323,7 +324,6 @@ should_move_rt_instruction(nir_intrinsic_instr *instr)
    switch (instr->intrinsic) {
    case nir_intrinsic_load_hit_attrib_amd:
       return nir_intrinsic_base(instr) < RADV_MAX_HIT_ATTRIB_DWORDS;
-   case nir_intrinsic_load_rt_arg_scratch_offset_amd:
    case nir_intrinsic_load_ray_flags:
    case nir_intrinsic_load_ray_object_origin:
    case nir_intrinsic_load_ray_world_origin:
@@ -364,7 +364,7 @@ move_rt_instructions(nir_shader *shader)
 static VkResult
 radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
                    struct radv_ray_tracing_pipeline *pipeline, enum radv_rt_lowering_mode mode,
-                   struct radv_shader_stage *stage, uint32_t *stack_size,
+                   struct radv_shader_stage *stage, uint32_t *payload_size, uint32_t *stack_size,
                    struct radv_ray_tracing_stage_info *stage_info,
                    const struct radv_ray_tracing_stage_info *traversal_stage_info,
                    struct radv_serialized_shader_arena_block *replay_block, bool skip_shaders_cache,
@@ -383,6 +383,9 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
       break;
    case RADV_RT_LOWERING_MODE_CPS:
       radv_nir_lower_rt_io_cps(stage->nir);
+      break;
+   case RADV_RT_LOWERING_MODE_FUNCTION_CALLS:
+      radv_nir_lower_rt_io_functions(stage->nir);
       break;
    }
 
@@ -440,29 +443,39 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
       switch (mode) {
       case RADV_RT_LOWERING_MODE_MONOLITHIC:
          assert(num_shaders == 1);
-         radv_nir_lower_rt_abi_monolithic(temp_stage.nir, &temp_stage.args, stack_size, device, pipeline);
+         radv_nir_lower_rt_abi_monolithic(temp_stage.nir, device, pipeline);
          break;
       case RADV_RT_LOWERING_MODE_CPS:
-         radv_nir_lower_rt_abi_cps(temp_stage.nir, &temp_stage.args, &stage->info, stack_size, i > 0, device, pipeline,
-                                   has_position_fetch, traversal_stage_info);
+         radv_nir_lower_rt_abi_cps(temp_stage.nir, &stage->info, i > 0, device, pipeline, has_position_fetch,
+                                   traversal_stage_info);
+         break;
+      case RADV_RT_LOWERING_MODE_FUNCTION_CALLS:
+         assert(num_shaders == 1);
+         radv_nir_lower_rt_abi_functions(temp_stage.nir, &temp_stage.info, *payload_size, device, pipeline);
          break;
       }
 
       /* Info might be out-of-date after inlining in radv_nir_lower_rt_abi(). */
-      nir_shader_gather_info(temp_stage.nir, nir_shader_get_entrypoint(temp_stage.nir));
+      nir_shader_gather_info(temp_stage.nir, radv_get_rt_shader_entrypoint(temp_stage.nir));
 
       radv_nir_shader_info_pass(device, temp_stage.nir, &stage->layout, &stage->key, NULL, RADV_PIPELINE_RAY_TRACING,
                                 false, &stage->info);
 
-      radv_optimize_nir(temp_stage.nir, stage->key.optimisations_disabled);
+      radv_optimize_nir(temp_stage.nir, temp_stage.key.optimisations_disabled);
       radv_postprocess_nir(device, NULL, &temp_stage);
-      stage->info.nir_shared_size = MAX2(stage->info.nir_shared_size, temp_stage.info.nir_shared_size);
 
-      if (stage_info)
-         radv_gather_unused_args(stage_info, shaders[i]);
+      NIR_PASS(_, stage->nir, radv_nir_lower_call_abi, stage->info.wave_size);
+      NIR_PASS(_, stage->nir, nir_lower_global_vars_to_local);
+      NIR_PASS(_, stage->nir, nir_lower_vars_to_ssa);
+      if (!stage->key.optimisations_disabled)
+         NIR_PASS(_, stage->nir, nir_minimize_call_live_states);
+
+      stage->info.nir_shared_size = MAX2(stage->info.nir_shared_size, temp_stage.info.nir_shared_size);
+      if (stage_info && mode == RADV_RT_LOWERING_MODE_CPS)
+         radv_gather_unused_args(stage_info, temp_stage.nir);
    }
 
-   bool dump_shader = radv_can_dump_shader(device, shaders[0]);
+   bool dump_shader = radv_can_dump_shader(device, stage->nir);
    bool dump_nir = dump_shader && (instance->debug_flags & RADV_DEBUG_DUMP_NIR);
    bool replayable = (pipeline->base.base.create_flags &
                       VK_PIPELINE_CREATE_2_RAY_TRACING_SHADER_GROUP_HANDLE_CAPTURE_REPLAY_BIT_KHR) &&
@@ -493,7 +506,6 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
          if (dump_shader)
             simple_mtx_unlock(&instance->shader_dump_mtx);
 
-         ralloc_free(mem_ctx);
          free(binary);
          return result;
       }
@@ -502,6 +514,9 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
 
    if (shader) {
       shader->nir_string = nir_string;
+
+      if (stack_size)
+         *stack_size = DIV_ROUND_UP(shader->config.scratch_bytes_per_wave, shader->info.wave_size);
 
       radv_shader_dump_debug_info(device, dump_shader, binary, shader, shaders, num_shaders, &stage->info);
 
@@ -515,7 +530,6 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
    if (dump_shader)
       simple_mtx_unlock(&instance->shader_dump_mtx);
 
-   ralloc_free(mem_ctx);
    free(binary);
 
    *out_shader = shader;
@@ -633,6 +647,10 @@ radv_rt_compile_shaders(struct radv_device *device, struct vk_pipeline_cache *ca
    if (!stages)
       return VK_ERROR_OUT_OF_HOST_MEMORY;
 
+   uint32_t payload_size = 0;
+   if (pCreateInfo->pLibraryInterface)
+      payload_size = pCreateInfo->pLibraryInterface->maxPipelineRayPayloadSize;
+
    bool library = pipeline->base.base.create_flags & VK_PIPELINE_CREATE_2_LIBRARY_BIT_KHR;
 
    /* Beyond 50 shader stages, inlining everything bloats the shader a ton, increasing compile times and
@@ -656,6 +674,19 @@ radv_rt_compile_shaders(struct radv_device *device, struct vk_pipeline_cache *ca
       stage->nir = radv_shader_spirv_to_nir(device, stage, NULL, false);
 
       NIR_PASS(_, stage->nir, radv_nir_lower_hit_attrib_derefs);
+
+      nir_foreach_variable_with_modes (var, stage->nir, nir_var_shader_call_data) {
+         unsigned size, alignment;
+         glsl_get_natural_size_align_bytes(var->type, &size, &alignment);
+         payload_size = MAX2(payload_size, size);
+      }
+      nir_foreach_function_impl (impl, stage->nir) {
+         nir_foreach_variable_in_list (var, &impl->locals) {
+            unsigned size, alignment;
+            glsl_get_natural_size_align_bytes(var->type, &size, &alignment);
+            payload_size = MAX2(payload_size, size);
+         }
+      }
 
       rt_stages[i].info = radv_gather_ray_tracing_stage_info(stage->nir);
 
@@ -736,8 +767,9 @@ radv_rt_compile_shaders(struct radv_device *device, struct vk_pipeline_cache *ca
          enum radv_rt_lowering_mode mode =
             stage->stage == MESA_SHADER_RAYGEN ? raygen_lowering_mode : recursive_lowering_mode;
 
-         result = radv_rt_nir_to_asm(device, cache, pipeline, mode, stage, &stack_size, &rt_stages[idx].info, NULL,
-                                     replay_block, skip_shaders_cache, has_position_fetch, &rt_stages[idx].shader);
+         result =
+            radv_rt_nir_to_asm(device, cache, pipeline, mode, stage, &payload_size, &stack_size, &rt_stages[idx].info,
+                               NULL, replay_block, skip_shaders_cache, has_position_fetch, &rt_stages[idx].shader);
          if (result != VK_SUCCESS)
             goto cleanup;
 
@@ -787,17 +819,20 @@ radv_rt_compile_shaders(struct radv_device *device, struct vk_pipeline_cache *ca
       traversal_info.unset_flags &= info->unset_flags;
    }
 
+   radv_nir_traversal_preprocess_cb preprocess =
+      recursive_lowering_mode == RADV_RT_LOWERING_MODE_CPS ? radv_nir_lower_rt_io_cps : radv_nir_lower_rt_io_functions;
+
    /* create traversal shader */
-   nir_shader *traversal_nir = radv_build_traversal_shader(device, pipeline, &traversal_info);
+   nir_shader *traversal_nir = radv_build_traversal_shader(device, pipeline, &traversal_info, preprocess);
    struct radv_shader_stage traversal_stage = {
       .stage = MESA_SHADER_INTERSECTION,
       .nir = traversal_nir,
       .key = stage_keys[MESA_SHADER_INTERSECTION],
    };
    radv_shader_layout_init(pipeline_layout, MESA_SHADER_INTERSECTION, &traversal_stage.layout);
-   result = radv_rt_nir_to_asm(device, cache, pipeline, recursive_lowering_mode, &traversal_stage, NULL, NULL,
-                               &traversal_info, NULL, skip_shaders_cache, has_position_fetch,
-                               &pipeline->base.base.shaders[MESA_SHADER_INTERSECTION]);
+   result = radv_rt_nir_to_asm(device, cache, pipeline, recursive_lowering_mode, &traversal_stage, &payload_size,
+                               &pipeline->traversal_stack_size, NULL, &traversal_info, NULL, skip_shaders_cache,
+                               has_position_fetch, &pipeline->base.base.shaders[MESA_SHADER_INTERSECTION]);
    ralloc_free(traversal_nir);
 
 cleanup:
@@ -858,10 +893,11 @@ compute_rt_stack_size(const VkRayTracingPipelineCreateInfoKHR *pCreateInfo, stru
          UNREACHABLE("Invalid stage type in RT shader");
       }
    }
-   pipeline->stack_size =
-      raygen_size +
-      MIN2(pCreateInfo->maxPipelineRayRecursionDepth, 1) * MAX2(chit_miss_size, intersection_size + any_hit_size) +
-      MAX2(0, (int)(pCreateInfo->maxPipelineRayRecursionDepth) - 1) * chit_miss_size + 2 * callable_size;
+   pipeline->stack_size = raygen_size +
+                          MIN2(pCreateInfo->maxPipelineRayRecursionDepth, 1) *
+                             (chit_miss_size + intersection_size + any_hit_size + pipeline->traversal_stack_size) +
+                          MAX2(0, (int)(pCreateInfo->maxPipelineRayRecursionDepth) - 1) * chit_miss_size +
+                          2 * callable_size;
 }
 
 static void
@@ -1216,7 +1252,7 @@ radv_rt_pipeline_create(VkDevice _device, VkPipelineCache _cache, const VkRayTra
       if (pipeline->groups[i].recursive_shader != VK_SHADER_UNUSED_KHR) {
          struct radv_shader *shader = pipeline->stages[pipeline->groups[i].recursive_shader].shader;
          if (shader)
-            pipeline->groups[i].handle.recursive_shader_ptr = shader->va | radv_get_rt_priority(shader->info.stage);
+            pipeline->groups[i].handle.recursive_shader_ptr = shader->va;
       }
    }
 

src/amd/common/ac_shader_args.h
@@ -198,20 +198,6 @@ struct ac_shader_args {
       struct ac_arg shader_addr;
       struct ac_arg shader_record;
       struct ac_arg payload_offset;
-      struct ac_arg ray_origin;
-      struct ac_arg ray_tmin;
-      struct ac_arg ray_direction;
-      struct ac_arg ray_tmax;
-      struct ac_arg cull_mask_and_flags;
-      struct ac_arg sbt_offset;
-      struct ac_arg sbt_stride;
-      struct ac_arg miss_index;
-      struct ac_arg accel_struct;
-      struct ac_arg primitive_id;
-      struct ac_arg instance_addr;
-      struct ac_arg primitive_addr;
-      struct ac_arg geometry_id_and_flags;
-      struct ac_arg hit_kind;
    } rt;
 };
 

src/amd/compiler/aco_nir_call_attribs.h
@@ -26,4 +26,87 @@ enum aco_nir_parameter_attribs {
    ACO_NIR_PARAM_ATTRIB_DISCARDABLE = 0x1,
 };
 
+enum aco_nir_call_system_args {
+   ACO_NIR_CALL_SYSTEM_ARG_DIVERGENT_PC,
+   ACO_NIR_CALL_SYSTEM_ARG_UNIFORM_PC,
+   ACO_NIR_CALL_SYSTEM_ARG_COUNT,
+};
+
+enum aco_nir_rt_function_arg {
+   RT_ARG_LAUNCH_ID = 0,
+   RT_ARG_LAUNCH_SIZE,
+   RT_ARG_DESCRIPTORS,
+   RT_ARG_DYNAMIC_DESCRIPTORS,
+   RT_ARG_PUSH_CONSTANTS,
+   RT_ARG_SBT_DESCRIPTORS,
+   RT_ARG_COUNT,
+};
+
+enum aco_nir_raygen_function_arg {
+   RAYGEN_ARG_TRAVERSAL_ADDR = RT_ARG_COUNT,
+   RAYGEN_ARG_SHADER_RECORD_PTR,
+   RAYGEN_ARG_COUNT,
+};
+
+enum aco_nir_traversal_function_arg {
+   TRAVERSAL_ARG_TRAVERSAL_ADDR = RT_ARG_COUNT,
+   TRAVERSAL_ARG_SHADER_RECORD_PTR,
+   TRAVERSAL_ARG_ACCEL_STRUCT,
+   TRAVERSAL_ARG_CULL_MASK_AND_FLAGS,
+   TRAVERSAL_ARG_SBT_OFFSET,
+   TRAVERSAL_ARG_SBT_STRIDE,
+   TRAVERSAL_ARG_MISS_INDEX,
+   TRAVERSAL_ARG_RAY_ORIGIN,
+   TRAVERSAL_ARG_RAY_TMIN,
+   TRAVERSAL_ARG_RAY_DIRECTION,
+   TRAVERSAL_ARG_RAY_TMAX,
+   TRAVERSAL_ARG_PRIMITIVE_ADDR,
+   TRAVERSAL_ARG_PRIMITIVE_ID,
+   TRAVERSAL_ARG_INSTANCE_ADDR,
+   TRAVERSAL_ARG_GEOMETRY_ID_AND_FLAGS,
+   TRAVERSAL_ARG_HIT_KIND,
+   TRAVERSAL_ARG_PAYLOAD_BASE,
+};
+
+enum aco_nir_chit_miss_function_arg {
+   CHIT_MISS_ARG_TRAVERSAL_ADDR = RT_ARG_COUNT,
+   CHIT_MISS_ARG_SHADER_RECORD_PTR,
+   CHIT_MISS_ARG_ACCEL_STRUCT,
+   CHIT_MISS_ARG_CULL_MASK_AND_FLAGS,
+   CHIT_MISS_ARG_SBT_OFFSET,
+   CHIT_MISS_ARG_SBT_STRIDE,
+   CHIT_MISS_ARG_MISS_INDEX,
+   CHIT_MISS_ARG_RAY_ORIGIN,
+   CHIT_MISS_ARG_RAY_TMIN,
+   CHIT_MISS_ARG_RAY_DIRECTION,
+   CHIT_MISS_ARG_RAY_TMAX,
+   CHIT_MISS_ARG_PRIMITIVE_ADDR,
+   CHIT_MISS_ARG_PRIMITIVE_ID,
+   CHIT_MISS_ARG_INSTANCE_ADDR,
+   CHIT_MISS_ARG_GEOMETRY_ID_AND_FLAGS,
+   CHIT_MISS_ARG_HIT_KIND,
+   CHIT_MISS_ARG_PAYLOAD_BASE,
+};
+
+/* aco_nir_cps_function_arg extends aco_nir_raygen_function_arg */
+enum aco_nir_cps_function_arg {
+   CPS_ARG_PAYLOAD_SCRATCH_OFFSET = RAYGEN_ARG_COUNT,
+   CPS_ARG_STACK_PTR,
+   CPS_ARG_ACCEL_STRUCT,
+   CPS_ARG_CULL_MASK_AND_FLAGS,
+   CPS_ARG_SBT_OFFSET,
+   CPS_ARG_SBT_STRIDE,
+   CPS_ARG_MISS_INDEX,
+   CPS_ARG_RAY_ORIGIN,
+   CPS_ARG_RAY_TMIN,
+   CPS_ARG_RAY_DIRECTION,
+   CPS_ARG_RAY_TMAX,
+   CPS_ARG_PRIMITIVE_ID,
+   CPS_ARG_INSTANCE_ADDR,
+   CPS_ARG_GEOMETRY_ID_AND_FLAGS,
+   CPS_ARG_HIT_KIND,
+   CPS_ARG_PRIMITIVE_ADDR,
+   CPS_ARG_COUNT,
+};
+
 #endif /* ACO_NIR_CALL_ATTRIBS_H */

src/amd/vulkan/meson.build
@@ -71,6 +71,7 @@ libradv_files = files(
   'nir/radv_nir_apply_pipeline_layout.c',
   'nir/radv_nir_export_multiview.c',
   'nir/radv_nir_lower_abi.c',
+  'nir/radv_nir_lower_call_abi.c',
   'nir/radv_nir_lower_cooperative_matrix.c',
   'nir/radv_nir_lower_fs_barycentric.c',
   'nir/radv_nir_lower_fs_intrinsics.c',
@@ -90,6 +91,7 @@ libradv_files = files(
   'nir/radv_nir_rt_common.c',
   'nir/radv_nir_rt_stage_common.c',
   'nir/radv_nir_rt_stage_cps.c',
+  'nir/radv_nir_rt_stage_functions.c',
   'nir/radv_nir_rt_stage_monolithic.c',
   'nir/radv_nir_rt_traversal_shader.c',
   'nir/radv_nir_trim_fs_color_exports.c',

src/amd/vulkan/nir/radv_nir.h
@@ -103,6 +103,10 @@ bool radv_nir_opt_fs_builtins(nir_shader *shader, const struct radv_graphics_sta
 bool radv_nir_lower_immediate_samplers(nir_shader *shader, struct radv_device *device,
                                        const struct radv_shader_stage *stage);
 
+void radv_nir_lower_callee_signature(nir_function *function);
+
+bool radv_nir_lower_call_abi(nir_shader *shader, unsigned wave_size);
+
 #ifdef __cplusplus
 }
 #endif

src/amd/vulkan/nir/radv_nir_apply_pipeline_layout.c
@@ -5,6 +5,7 @@
  */
 #include "ac_descriptors.h"
 #include "ac_shader_util.h"
+#include "aco_nir_call_attribs.h"
 #include "nir.h"
 #include "nir_builder.h"
 #include "radv_descriptor_set.h"
@@ -38,6 +39,30 @@ get_scalar_arg(nir_builder *b, unsigned size, struct ac_arg arg)
 }
 
 static nir_def *
+get_indirect_descriptors_addr(nir_builder *b, apply_layout_state *state)
+{
+   if (mesa_shader_stage_is_rt(b->shader->info.stage))
+      return nir_load_param(b, RT_ARG_DESCRIPTORS);
+   return get_scalar_arg(b, 1, state->args->descriptors[0]);
+}
+
+static nir_def *
+get_indirect_push_constants_addr(nir_builder *b, apply_layout_state *state)
+{
+   if (mesa_shader_stage_is_rt(b->shader->info.stage))
+      return nir_load_param(b, RT_ARG_PUSH_CONSTANTS);
+   return get_scalar_arg(b, 1, state->args->ac.push_constants);
+}
+
+static nir_def *
+get_dynamic_descriptors_addr(nir_builder *b, apply_layout_state *state)
+{
+   if (mesa_shader_stage_is_rt(b->shader->info.stage))
+      return nir_load_param(b, RT_ARG_DYNAMIC_DESCRIPTORS);
+   return get_scalar_arg(b, 1, state->args->ac.dynamic_descriptors);
+}
+
+static nir_def *
 convert_pointer_to_64_bit(nir_builder *b, apply_layout_state *state, nir_def *ptr)
 {
    return nir_pack_64_2x32_split(b, ptr, nir_imm_int(b, state->address32_hi));
@@ -47,8 +72,9 @@ static nir_def *
 load_desc_ptr(nir_builder *b, apply_layout_state *state, unsigned set)
 {
    const struct radv_userdata_locations *user_sgprs_locs = &state->info->user_sgprs_locs;
-   if (user_sgprs_locs->shader_data[AC_UD_INDIRECT_DESCRIPTORS].sgpr_idx != -1) {
-      nir_def *addr = get_scalar_arg(b, 1, state->args->descriptors[0]);
+   if (user_sgprs_locs->shader_data[AC_UD_INDIRECT_DESCRIPTORS].sgpr_idx != -1 ||
+       mesa_shader_stage_is_rt(b->shader->info.stage)) {
+      nir_def *addr = get_indirect_descriptors_addr(b, state);
       addr = convert_pointer_to_64_bit(b, state, addr);
       return ac_nir_load_smem(b, 1, addr, nir_imm_int(b, set * 4), 4, 0);
    }
@@ -69,7 +95,7 @@ visit_vulkan_resource_index(nir_builder *b, apply_layout_state *state, nir_intri
    nir_def *set_ptr;
    if (vk_descriptor_type_is_dynamic(layout->binding[binding].type)) {
       unsigned idx = state->layout->set[desc_set].dynamic_offset_start + layout->binding[binding].dynamic_offset_offset;
-      set_ptr = get_scalar_arg(b, 1, state->args->ac.dynamic_descriptors);
+      set_ptr = get_dynamic_descriptors_addr(b, state);
       offset = idx * 16;
       stride = 16;
    } else {
@@ -341,8 +367,10 @@ load_push_constant(nir_builder *b, apply_layout_state *state, nir_intrinsic_inst
          continue;
       }
 
-      if (!state->args->ac.push_constants.used) {
-         /* Assume this is an inlined push constant load which was expanded to include dwords which are not inlined. */
+      if (!state->args->ac.push_constants.used && !mesa_shader_stage_is_rt(b->shader->info.stage)) {
+         /* Assume this is an inlined push constant load which was expanded to include dwords which are not inlined.
+          * RT stages use neither shader args nor inlined push constants, so skip this for RT shaders.
+          */
          assert(const_offset != -1);
          data[num_loads++] = nir_undef(b, 1, 32);
          start += 1;
@@ -350,7 +378,7 @@ load_push_constant(nir_builder *b, apply_layout_state *state, nir_intrinsic_inst
       }
 
       if (!offset) {
-         addr = get_scalar_arg(b, 1, state->args->ac.push_constants);
+         addr = get_indirect_push_constants_addr(b, state);
          addr = convert_pointer_to_64_bit(b, state, addr);
          offset = nir_iadd_imm_nuw(b, intrin->src[0].ssa, base);
       }

commit f0cb036d3f346c8230eea7d8ce0fd63b4b71b5dd

radv/rt: only run move_rt_instructions() for CPS shaders
move_rt_instructions() only makes sense for CPS recursive shaders, where
later rt_trace_ray calls can overwrite the current shader's RT system
values.

Running it on the function-call path can hoist load_hit_attrib_amd
above merged intersection writes, which corrupts any-hit
hitAttributeEXT. Move the pass into the existing CPS-only
non-intersection branch before nir_lower_shader_calls().

Fixes: f4120743086d ("radv/rt: Use function call structure in NIR lowering")
Closes: #15074

Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40531>

src/amd/vulkan/radv_pipeline_rt.c
@@ -411,16 +411,16 @@ radv_rt_nir_to_asm(struct radv_device *device, struct vk_pipeline_cache *cache,
    stage->info.inline_push_constant_mask = stage->args.ac.inline_push_const_mask;
    stage->info.type = radv_is_traversal_shader(stage->nir) ? RADV_SHADER_TYPE_RT_TRAVERSAL : RADV_SHADER_TYPE_DEFAULT;
 
-   /* Move ray tracing system values to the top that are set by rt_trace_ray
-    * to prevent them from being overwritten by other rt_trace_ray calls.
-    */
-   NIR_PASS(_, stage->nir, move_rt_instructions);
-
    uint32_t num_resume_shaders = 0;
    nir_shader **resume_shaders = NULL;
    void *mem_ctx = ralloc_context(NULL);
 
    if (stage->stage != MESA_SHADER_INTERSECTION && mode == RADV_RT_LOWERING_MODE_CPS) {
+      /* Move ray tracing system values to the top that are set by rt_trace_ray
+       * to prevent them from being overwritten by other rt_trace_ray calls.
+       */
+      NIR_PASS(_, stage->nir, move_rt_instructions);
+
       nir_builder b = nir_builder_at(nir_after_impl(nir_shader_get_entrypoint(stage->nir)));
       nir_rt_return_amd(&b);
 