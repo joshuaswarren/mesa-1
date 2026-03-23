joshuaswarren/mesa-1

commit 81f82ee181af88401a8cf1047ea40466aecd0ad6

venus: filter out venus incapable physical devices
VK_KHR_external_memory_fd is required by Venus at the bare minimum for
all different ways to support the current venus guest memory mapping.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34830>

src/virtio/vulkan/vn_physical_device.c
@@ -1503,6 +1503,12 @@ vn_physical_device_init_renderer_extensions(
 
    vk_free(alloc, exts);
 
+   /* VK_KHR_external_memory_fd is required for venus memory mapping */
+   if (!physical_dev->renderer_extensions.KHR_external_memory_fd) {
+      vk_free(alloc, physical_dev->extension_spec_versions);
+      return VK_ERROR_INCOMPATIBLE_DRIVER;
+   }
+
    return VK_SUCCESS;
 }
 
@@ -1628,10 +1634,6 @@ vn_physical_device_init(struct vn_physical_device *physical_dev)
    const VkAllocationCallbacks *alloc = &instance->base.vk.alloc;
    VkResult result;
 
-   result = vn_physical_device_init_renderer_extensions(physical_dev);
-   if (result != VK_SUCCESS)
-      return result;
-
    vn_physical_device_init_external_memory(physical_dev);
    vn_physical_device_init_external_fence_handles(physical_dev);
    vn_physical_device_init_external_semaphore_handles(physical_dev);
@@ -1663,7 +1665,6 @@ vn_physical_device_init(struct vn_physical_device *physical_dev)
    return VK_SUCCESS;
 
 fail:
-   vk_free(alloc, physical_dev->extension_spec_versions);
    vk_free(alloc, physical_dev->queue_family_properties);
    return result;
 }
@@ -1872,6 +1873,12 @@ filter_physical_devices(struct vn_physical_device *physical_devs,
       /* init renderer version and discard unsupported devices */
       VkResult result =
          vn_physical_device_init_renderer_version(physical_dev);
+      if (result != VK_SUCCESS) {
+         vn_physical_device_base_fini(&physical_dev->base);
+         continue;
+      }
+
+      result = vn_physical_device_init_renderer_extensions(physical_dev);
       if (result != VK_SUCCESS) {
          vn_physical_device_base_fini(&physical_dev->base);
          continue;

commit 37e62d392c7f50d7829b2c018ea1f08392617e50

venus: fix to relax the KHR_external_memory_fd requirement
This reverts commit 81f82ee181af88401a8cf1047ea40466aecd0ad6. The proper
way to filter out venus incapable physical devices is to do the platform
specific check during renderer side instance creation time.

Fixes: 81f82ee181a ("venus: filter out venus incapable physical devices")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40478>

src/virtio/vulkan/vn_physical_device.c
@@ -1567,12 +1567,6 @@ vn_physical_device_init_renderer_extensions(
 
    vk_free(alloc, exts);
 
-   /* VK_KHR_external_memory_fd is required for venus memory mapping */
-   if (!physical_dev->renderer_extensions.KHR_external_memory_fd) {
-      vk_free(alloc, physical_dev->extension_spec_versions);
-      return VK_ERROR_INCOMPATIBLE_DRIVER;
-   }
-
    return VK_SUCCESS;
 }
 
@@ -1692,6 +1686,10 @@ vn_physical_device_init(struct vn_physical_device *physical_dev)
    const VkAllocationCallbacks *alloc = &instance->base.vk.alloc;
    VkResult result;
 
+   result = vn_physical_device_init_renderer_extensions(physical_dev);
+   if (result != VK_SUCCESS)
+      return result;
+
    vn_physical_device_init_external_memory(physical_dev);
    vn_physical_device_init_external_fence_handles(physical_dev);
    vn_physical_device_init_external_semaphore_handles(physical_dev);
@@ -1723,6 +1721,7 @@ vn_physical_device_init(struct vn_physical_device *physical_dev)
    return VK_SUCCESS;
 
 fail:
+   vk_free(alloc, physical_dev->extension_spec_versions);
    vk_free(alloc, physical_dev->queue_family_properties);
    return result;
 }
@@ -1931,12 +1930,6 @@ filter_physical_devices(struct vn_physical_device *physical_devs,
       /* init renderer version and discard unsupported devices */
       VkResult result =
          vn_physical_device_init_renderer_version(physical_dev);
-      if (result != VK_SUCCESS) {
-         vn_physical_device_base_fini(&physical_dev->base);
-         continue;
-      }
-
-      result = vn_physical_device_init_renderer_extensions(physical_dev);
       if (result != VK_SUCCESS) {
          vn_physical_device_base_fini(&physical_dev->base);
          continue;