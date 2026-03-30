joshuaswarren/mesa-1

commit 0e9643726e4039019f8e8d46d6a7263160f5bd31

r300: Z16 polygon offset fixes
The way I understand the HW docs is that the polygon offset is applied
always in 24bit depth domain (there are no polygon offset depth format
control registers like r600 has), so we need to manually rescale for
16bit buffers.

Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39196>

src/gallium/drivers/r300/r300_state.c
@@ -1503,8 +1503,8 @@ static void* r300_create_rs_state(struct pipe_context* pipe,
 
     /* Build the two command buffers for polygon offset setup. */
     if (polygon_offset_enable) {
-        float scale = state->offset_scale * 12;
-        float offset = state->offset_units * 4;
+        float scale = state->offset_scale * 256 * 12;
+        float offset = state->offset_units * 256 * 2;
 
         BEGIN_CB(rs->cb_poly_offset_zb16, 5);
         OUT_CB_REG_SEQ(R300_SU_POLY_OFFSET_FRONT_SCALE, 4);
@@ -1515,6 +1515,7 @@ static void* r300_create_rs_state(struct pipe_context* pipe,
         END_CB;
 
         offset = state->offset_units * 2;
+        scale = state->offset_scale * 12;
 
         BEGIN_CB(rs->cb_poly_offset_zb24, 5);
         OUT_CB_REG_SEQ(R300_SU_POLY_OFFSET_FRONT_SCALE, 4);

commit 7b0ed6edccf6606aa2e278e75eeaa7155510a0de

r300: lower Z16 polygon offset scale coefficient
The previous value was too high and led to issues in tesseract.

Fixes: 0e9643726 ("r300: Z16 polygon offset fixes")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40674>

src/gallium/drivers/r300/r300_state.c
@@ -1546,7 +1546,7 @@ static void* r300_create_rs_state(struct pipe_context* pipe,
 
     /* Build the two command buffers for polygon offset setup. */
     if (polygon_offset_enable) {
-        float scale = state->offset_scale * 256 * 12;
+        float scale = state->offset_scale * 16 * 12;
         float offset = state->offset_units * 256 * 2;
 
         BEGIN_CB(rs->cb_poly_offset_zb16, 5);