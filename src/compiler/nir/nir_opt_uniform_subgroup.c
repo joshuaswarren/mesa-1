joshuaswarren/mesa-1

commit 93813d098e1e432cfc71b36b9747a809e7df4a78

nir/opt_uniform_subgroup: use ballot_bit_count
Using bit_count on the result of ballot doesn't work for targets where
ballot's num_components > 1.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Fixes: 58440a73075 ("ir3: enable nir_opt_uniform_subgroup")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35669>

src/compiler/nir/nir_opt_uniform_subgroup.c
@@ -70,8 +70,17 @@ opt_uniform_subgroup_filter(const nir_instr *instr, const void *_state)
 }
 
 static nir_def *
+ballot_bit_count(nir_builder *b, nir_def *ballot)
+{
+   return ballot->num_components == 1
+             ? nir_bit_count(b, ballot)
+             : nir_ballot_bit_count_reduce(b, ballot->bit_size, ballot);
+}
+
+static nir_def *
 count_active_invocations(nir_builder *b, nir_def *value, bool inclusive,
-                         bool has_mbcnt_amd)
+                         bool has_mbcnt_amd,
+                         const nir_lower_subgroups_options *options)
 {
    /* For the non-inclusive case, the two paths are functionally the same.
     * For the inclusive case, the are similar but very subtly different.
@@ -91,11 +100,13 @@ count_active_invocations(nir_builder *b, nir_def *value, bool inclusive,
    if (has_mbcnt_amd) {
       return nir_mbcnt_amd(b, value, nir_imm_int(b, (int)inclusive));
    } else {
-      nir_def *mask = inclusive
-                         ? nir_load_subgroup_le_mask(b, 1, 32)
-                         : nir_load_subgroup_lt_mask(b, 1, 32);
+      nir_def *mask =
+         inclusive ? nir_load_subgroup_le_mask(b, options->ballot_components,
+                                               options->ballot_bit_size)
+                   : nir_load_subgroup_lt_mask(b, options->ballot_components,
+                                               options->ballot_bit_size);
 
-      return nir_bit_count(b, nir_iand(b, value, mask));
+      return ballot_bit_count(b, nir_iand(b, value, mask));
    }
 }
 
@@ -119,11 +130,11 @@ opt_uniform_subgroup_instr(nir_builder *b, nir_instr *instr, void *_state)
                                       options->ballot_bit_size, nir_imm_true(b));
 
          if (intrin->intrinsic == nir_intrinsic_reduce) {
-            count = nir_bit_count(b, ballot);
+            count = ballot_bit_count(b, ballot);
          } else {
             count = count_active_invocations(b, ballot,
                                              intrin->intrinsic == nir_intrinsic_inclusive_scan,
-                                             false);
+                                             false, options);
          }
 
          const unsigned bit_size = intrin->src[0].ssa->bit_size;

commit 832a2a36a903b23bcec51e9800acd63ad2fddc40

nir/opt_uniform_subgroup: fix ballot_bit_count components
ballot_bit_count_reduce expects the ballot to have 4 components causing
validation failures on targets where 1 < ballot_components < 4. Fix this
by padding the ballot to 4 components.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Fixes: 93813d098e1 ("nir/opt_uniform_subgroup: use ballot_bit_count")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40792>

src/compiler/nir/nir_opt_uniform_subgroup.c
@@ -107,7 +107,7 @@ ballot_bit_count(nir_builder *b, nir_def *ballot)
 {
    return ballot->num_components == 1
              ? nir_bit_count(b, ballot)
-             : nir_ballot_bit_count_reduce(b, ballot);
+             : nir_ballot_bit_count_reduce(b, nir_pad_vec4(b, ballot));
 }
 
 static nir_def *