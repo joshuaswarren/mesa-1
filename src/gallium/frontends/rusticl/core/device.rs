joshuaswarren/mesa-1

commit 15942b9a622cae3c6ef4d563dbcd98ec66743af9

rusticl/device: support query_memory_info to retrieve available memory
Some drivers implement query_memory_info, but not the MAX_GLOBAL_SIZE
compute cap.

Long term we should drop the compute cap anyway.

Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28122>

src/gallium/frontends/rusticl/core/device.rs
@@ -785,8 +785,17 @@ impl Device {
     }
 
     pub fn global_mem_size(&self) -> cl_ulong {
-        self.screen
-            .compute_param(pipe_compute_cap::PIPE_COMPUTE_CAP_MAX_GLOBAL_SIZE)
+        if let Some(memory_info) = self.screen().query_memory_info() {
+            let memory: cl_ulong = if memory_info.total_device_memory != 0 {
+                memory_info.total_device_memory.into()
+            } else {
+                memory_info.total_staging_memory.into()
+            };
+            memory * 1024
+        } else {
+            self.screen
+                .compute_param(pipe_compute_cap::PIPE_COMPUTE_CAP_MAX_GLOBAL_SIZE)
+        }
     }
 
     pub fn image_2d_size(&self) -> usize {

src/gallium/frontends/rusticl/mesa/pipe/screen.rs
@@ -449,6 +449,14 @@ impl PipeScreen {
             );
         }
     }
+
+    pub fn query_memory_info(&self) -> Option<pipe_memory_info> {
+        let mut info = pipe_memory_info::default();
+        unsafe {
+            self.screen().query_memory_info?(self.screen.as_ptr(), &mut info);
+        }
+        Some(info)
+    }
 }
 
 impl Drop for PipeScreen {

commit d315767d5fb44d364f6c94c4237fb65e2c35045a

rusticl/device: Fix reporting of global memory on mixed memory devices
AMD APUs are hitting this case where they have very small discrete VRAM,
but a lot of staging memory, which can be used additionally.

Fixes: 15942b9a622 ("rusticl/device: support query_memory_info to retrieve available memory")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30123>

src/gallium/frontends/rusticl/core/device.rs
@@ -999,11 +999,19 @@ impl DeviceBase {
 
     pub fn global_mem_size(&self) -> cl_ulong {
         if let Some(memory_info) = self.screen.query_memory_info() {
-            let memory: cl_ulong = if memory_info.total_device_memory != 0 {
-                memory_info.total_device_memory.into()
+            let device_memory: cl_ulong = memory_info.total_device_memory.into();
+            let staging_memory: cl_ulong = memory_info.total_staging_memory.into();
+
+            // In case some driver doesn't set uma correctly.
+            let memory = if device_memory == 0 {
+                staging_memory
+            } else if self.unified_memory() {
+                // For UMA devices we expose both.
+                staging_memory + device_memory
             } else {
-                memory_info.total_staging_memory.into()
+                device_memory
             };
+
             memory * 1024
         } else {
             self.screen.compute_caps().max_global_size