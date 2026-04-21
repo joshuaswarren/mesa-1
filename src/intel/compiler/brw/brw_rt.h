joshuaswarren/mesa-1

commit 7765bae4f91e42a3e4b80be854e1eb7ee8b25761

anv: force StackIDControl value for Wa_14021821874
This is also encouraged by another wa, Wa_14018813551.

Both workarounds state that StackIDControlOverride_RTGlobals should
always be set to 0 (i.e. 2k).

Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30937>

src/intel/vulkan/genX_cmd_compute.c
@@ -74,6 +74,16 @@ genX(cmd_buffer_ensure_cfe_state)(struct anv_cmd_buffer *cmd_buffer,
       case 2048: cfe.StackIDControl = StackIDs2048; break;
       default:   unreachable("invalid stack_ids value");
       }
+
+#if INTEL_WA_14021821874_GFX_VER
+      /* Wa_14021821874: "StackIDControlOverride_RTGlobals = 0 (i.e. 2k)". We
+       * already set stack size per ray to 64 in brw_nir_lower_rt_intrinsics
+       * as the workaround also requires.
+       */
+      if (intel_needs_workaround(cmd_buffer->device->info, 14021821874))
+         cfe.StackIDControl = StackIDs2048;
+#endif
+
 #endif
 
       cfe.OverDispatchControl = 2; /* 50% overdispatch */

commit 7d6af33ae321c8417463b2a6da3b5e7d92e90fb8

anv: Fix Wa_14021821874, Wa_14018813551, Wa_14026600921
StackSizePerRay is the RTDispatchGlobals::AsyncStackSize and
DisableRTGlobalsKnownValues is to interpret how many Max BVH levels we
need to use. It's not relevant to Vulkan, since we have just 2 fixed BVH
levels.

Fixes: f9326250 ("anv: Fix Wa_14021821874, Wa_14018813551, Wa_14026600921")
Fixes: 7765bae4 ("anv: force StackIDControl value for Wa_14021821874")
Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41012>

src/intel/compiler/brw/brw_rt.h
@@ -216,7 +216,26 @@ brw_rt_compute_scratch_layout(struct brw_rt_scratch_layout *layout,
    assert(size % 64 == 0); /* Cache-line aligned */
    assert(size < UINT32_MAX);
    layout->ray_stack_start = size;
-   layout->ray_stack_stride = BRW_RT_ASYNC_STACK_STRIDE;
+
+   /* Wa_14021821874, Wa_14018813551, Wa_14026600921:
+    *
+    *    StackIDControlOverride_RTGlobals = 0 (i.e. 2k)
+    *    SetStackSizePerRay  = 64  (64*64B)
+    *    DisableRTGlobalsKnownValues = 1
+    *
+    * StackSizePerRay is the RTDispatchGlobals::AsyncStackSize and
+    * DisableRTGlobalsKnownValues is to interpret how many Max BVH levels we
+    * need to use. It's not relevant to Vulkan, since we have just 2 fixed BVH
+    * levels.
+    */
+   if (intel_needs_workaround(devinfo, 14021821874) ||
+       intel_needs_workaround(devinfo, 14018813551) ||
+       intel_needs_workaround(devinfo, 14026600921)) {
+      layout->ray_stack_stride = 64 * 64;
+   } else {
+      layout->ray_stack_stride = BRW_RT_ASYNC_STACK_STRIDE;
+   }
+
    size += num_stack_ids * layout->ray_stack_stride;
 
    /* Finally, we place the SW stacks for the individual ray-tracing shader
@@ -266,9 +285,14 @@ brw_rt_ray_queries_stack_ids_per_dss(const struct intel_device_info *devinfo)
 
    /* Wa_14021821874, Wa_14018813551, Wa_14026600921:
     *
-    * "StackIDControlOverride_RTGlobals = 0 (i.e. 2k)". We
-    * already set stack size per ray to 64 in brw_nir_lower_rt_intrinsics
-    * as the workaround also requires.
+    *    StackIDControlOverride_RTGlobals = 0 (i.e. 2k)
+    *    SetStackSizePerRay  = 64  (64*64B)
+    *    DisableRTGlobalsKnownValues = 1
+    *
+    * StackSizePerRay is the RTDispatchGlobals::AsyncStackSize and
+    * DisableRTGlobalsKnownValues is to interpret how many Max BVH levels we
+    * need to use. It's not relevant to Vulkan, since we have just 2 fixed BVH
+    * levels.
     */
    if (intel_needs_workaround(devinfo, 14021821874) ||
        intel_needs_workaround(devinfo, 14018813551) ||

src/intel/vulkan/genX_cmd_compute.c
@@ -890,7 +890,8 @@ genX(cmd_buffer_ray_query_globals)(struct anv_cmd_buffer *cmd_buffer)
             .bo = device->ray_query_bo[idx],
             .offset = (i + 1) * (device->ray_query_bo[idx]->size / 2),
          },
-         .AsyncRTStackSize = BRW_RT_SIZEOF_RAY_QUERY / 64,
+         .AsyncRTStackSize =
+            cmd_buffer->state.rt.scratch.layout.ray_stack_stride / 64,
          .NumDSSRTStacks = stack_ids_per_dss,
          .MaxBVHLevels = BRW_RT_MAX_BVH_LEVELS,
          .Flags = RT_DEPTH_TEST_LESS_EQUAL,