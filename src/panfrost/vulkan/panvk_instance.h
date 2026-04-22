joshuaswarren/mesa-1

commit 8f56c2192c0e1ca4c85c4c40a3a9f01d4bd47b71

panvk/v13: Support HSR Prepass
Add an option to enable HSR Prepass.

It is currently disabled by default as it might cause performance
regressions for content that:

- Has very simple fragment work.
- Already does a ZS prepass.

Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39615>

src/panfrost/vulkan/panvk_instance.h
@@ -34,6 +34,7 @@ enum panvk_debug_flags {
    PANVK_DEBUG_NO_USER_MMAP_SYNC = 1 << 15,
    PANVK_DEBUG_COHERENT_BEFORE_CACHED = 1 << 16,
    PANVK_DEBUG_NO_EXTENDED_VA_RANGE = 1 << 17,
+   PANVK_DEBUG_HSR_PREPASS = 1 << 17,
 };
 
 extern uint64_t panvk_debug;

src/panfrost/lib/pan_desc.c
@@ -1225,6 +1225,22 @@ GENX(pan_emit_fbd)(const struct pan_fb_info *fb, unsigned layer_idx,
       cfg.internal_layer_index = layer_idx - tiler_ctx->valhall.layer_offset;
       cfg.frame_argument = layer_idx;
 #endif
+
+#if PAN_ARCH >= 13
+      unsigned zs_bytes_per_pixel = pan_zsbuf_bytes_per_pixel(fb);
+      /* We can interleave HSR if we have space for two ZS tiles in the tile
+       * buffer. */
+      unsigned max_zs_tile_size_interleave =
+         fb->z_tile_buf_budget >> util_logbase2_ceil(zs_bytes_per_pixel);
+      bool hsr_can_interleave = fb->tile_size <= max_zs_tile_size_interleave;
+
+      /* Enabling prepass without interleave is generally not good for
+       * performance, so disable HSR in that case. */
+      cfg.hsr_prepass_enable = fb->allow_hsr_prepass && hsr_can_interleave;
+      cfg.hsr_prepass_interleaving_enable = hsr_can_interleave;
+      cfg.hsr_prepass_filter_enable = true;
+      cfg.hsr_hierarchical_optimizations_enable = true;
+#endif
    }
 
 #if PAN_ARCH >= 6

src/panfrost/lib/pan_desc.h
@@ -149,6 +149,7 @@ struct pan_fb_info {
    /* Only used on Valhall */
    bool sprite_coord_origin;
    bool first_provoking_vertex;
+   bool allow_hsr_prepass;
 
    /* indicates whether pixel local storage is enabled */
    bool pls_enabled;

src/panfrost/vulkan/panvk_instance.c
@@ -50,6 +50,7 @@ static const struct debug_control panvk_debug_options[] = {
    {"no_user_mmap_sync", PANVK_DEBUG_NO_USER_MMAP_SYNC},
    {"coherent_before_cached", PANVK_DEBUG_COHERENT_BEFORE_CACHED},
    {"no_extended_va_range", PANVK_DEBUG_NO_EXTENDED_VA_RANGE},
+   {"hsr_prepass", PANVK_DEBUG_HSR_PREPASS},
    {NULL, 0},
 };
 

src/panfrost/vulkan/panvk_vX_cmd_draw.c
@@ -394,6 +394,9 @@ panvk_per_arch(cmd_init_render_state)(struct panvk_cmd_buffer *cmdbuf,
       .z_tile_buf_budget = pan_query_optimal_z_tib_size(PAN_ARCH, phys_dev->model),
       .nr_samples = 0,
       .rt_count = pRenderingInfo->colorAttachmentCount,
+#if PAN_ARCH >= 13
+      .allow_hsr_prepass = PANVK_DEBUG(HSR_PREPASS),
+#endif
    };
    /* In case ms2ss is enabled, use the provided sample count.
     * All attachments need to have sample count == 1 or the provided value.

commit 12fe34401d89eca74adec08880e74af598c6465a

panvk: Fix debug flag overlap
PANVK_DEBUG_HSR_PREPASS and PANVK_DEBUG_NO_EXTENDED_VA_RANGE have the
same value, meaning they both get toggled when one is.

This commit moves PANVK_DEBUG_HSR_PREPASS to the following value.

Fixes: 8f56c2192c0 ("panvk/v13: Support HSR Prepass")
Reviewed-by: John Anthony <john.anthony@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41106>

src/panfrost/vulkan/panvk_instance.h
@@ -33,7 +33,7 @@ enum panvk_debug_flags {
    PANVK_DEBUG_NO_USER_MMAP_SYNC = 1 << 15,
    PANVK_DEBUG_COHERENT_BEFORE_CACHED = 1 << 16,
    PANVK_DEBUG_NO_EXTENDED_VA_RANGE = 1 << 17,
-   PANVK_DEBUG_HSR_PREPASS = 1 << 17,
+   PANVK_DEBUG_HSR_PREPASS = 1 << 18,
 };
 
 extern uint64_t panvk_debug;