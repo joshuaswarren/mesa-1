joshuaswarren/mesa-1

commit 5d65ad910ffca05397b1712411ccac3f271e8620

pco: initial texture/sampler compiler support
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36412>

src/compiler/nir/nir_intrinsics.py
@@ -2681,3 +2681,26 @@ intrinsic("enqueue_node_payloads", src_comp=[-1])
 
 # Returns true if it has been called for every payload.
 intrinsic("finalize_incoming_node_payload", src_comp=[-1], dest_comp=1)
+
+# Loads the texture/sampler state words for a given descriptor.
+intrinsic("load_tex_state_pco", dest_comp=0, indices=[DESC_SET, BINDING, COMPONENT], flags=[CAN_ELIMINATE, CAN_REORDER], bit_sizes=[32])
+intrinsic("load_smp_state_pco", dest_comp=0, indices=[DESC_SET, BINDING, COMPONENT], flags=[CAN_ELIMINATE, CAN_REORDER], bit_sizes=[32])
+
+# Loads the texture/sampler metadata for a given descriptor.
+intrinsic("load_tex_meta_pco", dest_comp=0, indices=[DESC_SET, BINDING, COMPONENT], flags=[CAN_ELIMINATE, CAN_REORDER], bit_sizes=[32])
+intrinsic("load_smp_meta_pco", dest_comp=0, indices=[DESC_SET, BINDING, COMPONENT], flags=[CAN_ELIMINATE, CAN_REORDER], bit_sizes=[32])
+
+index("uint16_t", "smp_flags_pco")
+
+# smp_pco(data, tex_state, smp_state)
+# Performs a standard sampling operation with the given data and state words.
+# Outputs between 1-4 comps.
+intrinsic("smp_pco", src_comp=[16, 4, 4], dest_comp=0, indices=[SMP_FLAGS_PCO, RANGE], bit_sizes=[32])
+
+# smp_coeffs_pco(data, tex_state, smp_state)
+# Returns the calculated sampling coefficients for the given data and state words.
+intrinsic("smp_coeffs_pco", src_comp=[16, 4, 4], dest_comp=8, indices=[SMP_FLAGS_PCO, RANGE], bit_sizes=[32])
+
+# alphatst_pco(data, comparator, comparison op)
+# Performs an alpha test on the given parameters, returning float 0/1 depending on the comparison result.
+intrinsic("alphatst_pco", src_comp=[1, 1, 1], dest_comp=1, flags=[CAN_ELIMINATE, CAN_REORDER], bit_sizes=[32])

src/imagination/pco/pco_trans_nir.c
@@ -423,13 +423,11 @@ trans_store_output_fs(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref src)
 }
 
 static unsigned fetch_resource_base_reg(const pco_common_data *common,
-                                        uint32_t packed_desc,
-                                        unsigned elem)
+                                        unsigned desc_set,
+                                        unsigned binding,
+                                        unsigned elem,
+                                        bool *is_img_smp)
 {
-   unsigned desc_set;
-   unsigned binding;
-   pco_unpack_desc(packed_desc, &desc_set, &binding);
-
    assert(desc_set < ARRAY_SIZE(common->desc_sets));
    const pco_descriptor_set_data *desc_set_data = &common->desc_sets[desc_set];
    assert(desc_set_data->used);
@@ -438,14 +436,27 @@ static unsigned fetch_resource_base_reg(const pco_common_data *common,
    const pco_binding_data *binding_data = &desc_set_data->bindings[binding];
    assert(binding_data->used);
 
+   if (is_img_smp)
+      *is_img_smp = binding_data->is_img_smp;
+
    unsigned reg_offset = elem * binding_data->range.stride;
    assert(reg_offset < binding_data->range.count);
 
    unsigned reg_index = binding_data->range.start + reg_offset;
-
    return reg_index;
 }
 
+static unsigned fetch_resource_base_reg_packed(const pco_common_data *common,
+                                               uint32_t packed_desc,
+                                               unsigned elem,
+                                               bool *is_img_smp)
+{
+   unsigned desc_set;
+   unsigned binding;
+   pco_unpack_desc(packed_desc, &desc_set, &binding);
+
+   return fetch_resource_base_reg(common, desc_set, binding, elem, is_img_smp);
+}
 static pco_instr *trans_load_buffer(trans_ctx *tctx,
                                     nir_intrinsic_instr *intr,
                                     pco_ref dest,
@@ -459,7 +470,8 @@ static pco_instr *trans_load_buffer(trans_ctx *tctx,
 
    uint32_t packed_desc = nir_src_comp_as_uint(intr->src[0], 0);
    unsigned elem = nir_src_comp_as_uint(intr->src[0], 1);
-   unsigned sh_index = fetch_resource_base_reg(common, packed_desc, elem);
+   unsigned sh_index =
+      fetch_resource_base_reg_packed(common, packed_desc, elem, NULL);
 
    pco_ref base_addr[2];
    pco_ref_hwreg_addr_comps(sh_index, PCO_REG_CLASS_SHARED, base_addr);
@@ -498,7 +510,8 @@ static pco_instr *trans_store_buffer(trans_ctx *tctx,
 
    uint32_t packed_desc = nir_src_comp_as_uint(intr->src[1], 0);
    unsigned elem = nir_src_comp_as_uint(intr->src[1], 1);
-   unsigned sh_index = fetch_resource_base_reg(common, packed_desc, elem);
+   unsigned sh_index =
+      fetch_resource_base_reg_packed(common, packed_desc, elem, NULL);
 
    pco_ref base_addr[2];
    pco_ref_hwreg_addr_comps(sh_index, PCO_REG_CLASS_SHARED, base_addr);
@@ -593,7 +606,8 @@ static pco_instr *trans_atomic_buffer(trans_ctx *tctx,
 
    uint32_t packed_desc = nir_src_comp_as_uint(intr->src[0], 0);
    unsigned elem = nir_src_comp_as_uint(intr->src[0], 1);
-   unsigned sh_index = fetch_resource_base_reg(common, packed_desc, elem);
+   unsigned sh_index =
+      fetch_resource_base_reg_packed(common, packed_desc, elem, NULL);
 
    pco_ref base_addr[2];
    pco_ref_hwreg_addr_comps(sh_index, PCO_REG_CLASS_SHARED, base_addr);
@@ -681,6 +695,162 @@ trans_load_sysval(trans_ctx *tctx, nir_intrinsic_instr *intr, pco_ref dest)
    return pco_mov(&tctx->b, dest, src, .rpt = chans);
 }
 
+static bool desc_set_binding_is_comb_img_smp(unsigned desc_set,
+                                             unsigned binding,
+                                             const pco_common_data *common)
+{
+   const pco_descriptor_set_data *desc_set_data = &common->desc_sets[desc_set];
+   assert(desc_set_data->used);
+   assert(desc_set_data->bindings && binding < desc_set_data->binding_count);
+
+   const pco_binding_data *binding_data = &desc_set_data->bindings[binding];
+   assert(binding_data->used);
+
+   return binding_data->is_img_smp;
+}
+
+static pco_instr *lower_load_tex_smp_state(trans_ctx *tctx,
+                                           nir_intrinsic_instr *intr,
+                                           pco_ref dest,
+                                           bool smp)
+{
+   unsigned desc_set = nir_intrinsic_desc_set(intr);
+   unsigned binding = nir_intrinsic_binding(intr);
+   unsigned start_comp = nir_intrinsic_component(intr);
+   unsigned chans = pco_ref_get_chans(dest);
+   assert(start_comp + chans <= ROGUE_NUM_TEXSTATE_DWORDS);
+
+   /* TODO: array support. */
+   const pco_common_data *common = &tctx->shader->data.common;
+   bool is_img_smp;
+   unsigned sh_index =
+      fetch_resource_base_reg(common, desc_set, binding, 0, &is_img_smp);
+   pco_ref state_words =
+      pco_ref_hwreg_vec(sh_index, PCO_REG_CLASS_SHARED, chans);
+
+   /* Sampler state comes after image state and metadata in combined image
+    * samplers.
+    */
+   if (smp && is_img_smp) {
+      state_words = pco_ref_offset(state_words, ROGUE_NUM_TEXSTATE_DWORDS);
+      state_words = pco_ref_offset(state_words, PCO_IMAGE_META_COUNT);
+   }
+
+   state_words = pco_ref_offset(state_words, start_comp);
+
+   return pco_mov(&tctx->b, dest, state_words, .rpt = chans);
+}
+
+static pco_instr *lower_load_tex_smp_meta(trans_ctx *tctx,
+                                          nir_intrinsic_instr *intr,
+                                          pco_ref dest,
+                                          bool smp)
+{
+   unsigned desc_set = nir_intrinsic_desc_set(intr);
+   unsigned binding = nir_intrinsic_binding(intr);
+   unsigned start_comp = nir_intrinsic_component(intr);
+   unsigned chans = pco_ref_get_chans(dest);
+
+   /* TODO: array support. */
+   const pco_common_data *common = &tctx->shader->data.common;
+   bool is_img_smp;
+   unsigned sh_index =
+      fetch_resource_base_reg(common, desc_set, binding, 0, &is_img_smp);
+   pco_ref state_words =
+      pco_ref_hwreg_vec(sh_index, PCO_REG_CLASS_SHARED, chans);
+
+   assert(start_comp + chans <=
+          (smp ? PCO_SAMPLER_META_COUNT : PCO_IMAGE_META_COUNT));
+
+   state_words = pco_ref_offset(state_words, ROGUE_NUM_TEXSTATE_DWORDS);
+
+   if (smp && is_img_smp) {
+      state_words = pco_ref_offset(state_words, PCO_IMAGE_META_COUNT);
+      state_words = pco_ref_offset(state_words, ROGUE_NUM_TEXSTATE_DWORDS);
+   }
+
+   state_words = pco_ref_offset(state_words, start_comp);
+
+   return pco_mov(&tctx->b, dest, state_words, .rpt = chans);
+}
+
+static pco_instr *lower_smp(trans_ctx *tctx,
+                            nir_intrinsic_instr *intr,
+                            pco_ref *dest,
+                            pco_ref data,
+                            pco_ref tex_state,
+                            pco_ref smp_state)
+{
+   pco_smp_flags smp_flags = { ._ = nir_intrinsic_smp_flags_pco(intr) };
+   unsigned data_comps = nir_intrinsic_range(intr);
+
+   data = pco_ref_chans(data, data_comps);
+
+   unsigned chans = pco_ref_get_chans(*dest);
+   enum pco_sb_mode sb_mode = PCO_SB_MODE_NONE;
+   switch (intr->intrinsic) {
+   case nir_intrinsic_smp_coeffs_pco:
+      /* Shrink the destination to its actual size. */
+      *dest = pco_ref_chans(*dest, ROGUE_SMP_COEFF_COUNT);
+      chans = 1; /* Chans must be 1 for coeff mode. */
+
+      sb_mode = PCO_SB_MODE_COEFFS;
+      break;
+
+   case nir_intrinsic_smp_pco:
+      /* Destination and chans should be correct. */
+      break;
+
+   default:
+      UNREACHABLE("");
+   }
+
+   pco_ref shared_lod = pco_ref_null();
+
+   pco_instr *smp = pco_smp(&tctx->b,
+                            *dest,
+                            pco_ref_drc(PCO_DRC_0),
+                            tex_state,
+                            data,
+                            smp_state,
+                            shared_lod,
+                            pco_ref_imm8(chans),
+                            .dim = smp_flags.dim,
+                            .proj = smp_flags.proj,
+                            .fcnorm = smp_flags.fcnorm,
+                            .nncoords = smp_flags.nncoords,
+                            .lod_mode = smp_flags.lod_mode,
+                            .pplod = smp_flags.pplod,
+                            .tao = smp_flags.tao,
+                            .soo = smp_flags.soo,
+                            .sno = smp_flags.sno,
+                            .sb_mode = sb_mode,
+                            .array = smp_flags.array,
+                            .integer = smp_flags.integer);
+
+   return smp;
+}
+
+static pco_instr *lower_alphatst(trans_ctx *tctx,
+                                 pco_ref dest,
+                                 pco_ref src0,
+                                 pco_ref src1,
+                                 pco_ref src2)
+{
+   pco_alphatst(&tctx->b,
+                pco_ref_pred(PCO_PRED_P0),
+                pco_ref_drc(PCO_DRC_0),
+                src0,
+                src1,
+                src2);
+
+   return pco_psel(&tctx->b,
+                   dest,
+                   pco_ref_pred(PCO_PRED_P0),
+                   pco_fone,
+                   pco_zero);
+}
+
 /**
  * \brief Translates a NIR intrinsic instruction into PCO.
  *
@@ -760,6 +930,32 @@ static pco_instr *trans_intr(trans_ctx *tctx, nir_intrinsic_instr *intr)
       instr = intr->intrinsic == nir_intrinsic_ddy_fine
                  ? pco_fdsyf(&tctx->b, dest, src[0])
                  : pco_fdsy(&tctx->b, dest, src[0]);
+      break;
+
+   /* Texture-related intrinsics. */
+   case nir_intrinsic_load_tex_state_pco:
+      instr = lower_load_tex_smp_state(tctx, intr, dest, false);
+      break;
+
+   case nir_intrinsic_load_smp_state_pco:
+      instr = lower_load_tex_smp_state(tctx, intr, dest, true);
+      break;
+
+   case nir_intrinsic_load_tex_meta_pco:
+      instr = lower_load_tex_smp_meta(tctx, intr, dest, false);
+      break;
+
+   case nir_intrinsic_load_smp_meta_pco:
+      instr = lower_load_tex_smp_meta(tctx, intr, dest, true);
+      break;
+
+   case nir_intrinsic_smp_coeffs_pco:
+   case nir_intrinsic_smp_pco:
+      instr = lower_smp(tctx, intr, &dest, src[0], src[1], src[2]);
+      break;
+
+   case nir_intrinsic_alphatst_pco:
+      instr = lower_alphatst(tctx, dest, src[0], src[1], src[2]);
       break;
 
    default:

src/imagination/include/hwdef/rogue_hw_defs.h
@@ -105,6 +105,8 @@
 /* Number of TEXSTATE_SAMPLER state words that need setting up. */
 #define ROGUE_NUM_TEXSTATE_SAMPLER_WORDS 2U
 
+#define ROGUE_NUM_TEXSTATE_DWORDS 4U
+
 /* 12 dwords reserved for shared register management. The first dword is the
  * number of shared register blocks to reload. Should be a multiple of 4 dwords,
  * size in bytes.
@@ -172,5 +174,18 @@ enum {
    ROGUE_USC_COEFFICIENT_SET_PAD,
    ROGUE_USC_COEFFICIENT_SET_SIZE,
 };
+
+enum {
+   ROGUE_SMP_COEFF_UFRAC = 0,
+   ROGUE_SMP_COEFF_VFRAC,
+   ROGUE_SMP_COEFF_SFRAC,
+   ROGUE_SMP_COEFF_LOD_DVAL_POST_CLAMP,
+   ROGUE_SMP_COEFF_LOD_DVAL_PRE_CLAMP,
+   ROGUE_SMP_COEFF_TFRAC_POST_CLAMP,
+   ROGUE_SMP_COEFF_TFRAC_PRE_CLAMP,
+   ROGUE_SMP_COEFF_COUNT,
+};
+
+#define ROGUE_SMP_MAX_DATA_WORDS 18U
 
 #endif /* ROGUE_HW_DEFS_H */

src/imagination/pco/meson.build
@@ -18,6 +18,7 @@ libpowervr_compiler_files = files(
   'pco_nir.c',
   'pco_nir_compute.c',
   'pco_nir_pvfio.c',
+  'pco_nir_tex.c',
   'pco_nir_vk.c',
   'pco_opt.c',
   'pco_print.c',

src/imagination/pco/pco_data.h
@@ -74,6 +74,26 @@ typedef struct _pco_cs_data {
    unsigned workgroup_size[3]; /** Workgroup size. */
 } pco_cs_data;
 
+/** PCO image descriptor metadata. */
+enum pco_image_meta {
+   PCO_IMAGE_META_LAYER_SIZE,
+   PCO_IMAGE_META_RSVD0,
+   PCO_IMAGE_META_RSVD1,
+   PCO_IMAGE_META_RSVD2,
+
+   PCO_IMAGE_META_COUNT,
+};
+
+/** PCO sampler descriptor metadata. */
+enum pco_sampler_meta {
+   PCO_SAMPLER_META_COMPARE_OP,
+   PCO_SAMPLER_META_RSVD0,
+   PCO_SAMPLER_META_RSVD1,
+   PCO_SAMPLER_META_RSVD2,
+
+   PCO_SAMPLER_META_COUNT,
+};
+
 /** PCO descriptor binding data. */
 typedef struct _pco_binding_data {
    pco_range range; /** Descriptor location range. */

src/imagination/pco/pco_internal.h
@@ -1522,6 +1522,28 @@ static inline bool pco_should_print_binary(pco_shader *shader)
    return true;
 }
 
+/* Interface with NIR. */
+typedef union PACKED _pco_smp_flags {
+   struct PACKED {
+      unsigned dim : 2;
+      bool proj : 1;
+      bool fcnorm : 1;
+      bool nncoords : 1;
+      enum pco_lod_mode lod_mode : 2;
+      bool pplod : 1;
+      bool tao : 1;
+      bool soo : 1;
+      bool sno : 1;
+      bool array : 1;
+      bool integer : 1;
+      unsigned pad : 3;
+   };
+
+   uint16_t _;
+} pco_smp_flags;
+static_assert(sizeof(pco_smp_flags) == sizeof(uint16_t),
+              "sizeof(pco_smp_flags) != sizeof(uint16_t)");
+
 /* PCO IR passes. */
 bool pco_const_imms(pco_shader *shader);
 bool pco_bool(pco_shader *shader);
@@ -1534,6 +1556,7 @@ bool pco_legalize(pco_shader *shader);
 bool pco_nir_compute_instance_check(nir_shader *shader);
 bool pco_nir_lower_algebraic(nir_shader *shader);
 bool pco_nir_lower_algebraic_late(nir_shader *shader);
+bool pco_nir_lower_tex(nir_shader *shader, pco_common_data *common);
 bool pco_nir_lower_vk(nir_shader *shader, pco_common_data *common);
 bool pco_nir_pfo(nir_shader *shader, pco_fs_data *fs);
 bool pco_nir_point_size(nir_shader *shader);

src/imagination/pco/pco_isa.py
@@ -1820,8 +1820,8 @@ F_SBMODE = field_enum_type(
 name='sbmode', num_bits=2,
 elems=[
    ('none', 0b00),
-   ('data', 0b01),
-   ('info', 0b10),
+   ('rawdata', 0b01),
+   ('coeffs', 0b10),
    ('both', 0b11),
 ])
 

src/imagination/pco/pco_map.py
@@ -310,6 +310,31 @@ enum_map(OM_FRED_PART.t, F_RED_PART, [
    ('b', 'b'),
 ])
 
+enum_map(OM_DIM.t, F_DMN, [
+   ('1d', '1d'),
+   ('2d', '2d'),
+   ('3d', '3d'),
+])
+
+enum_map(OM_LOD_MODE.t, F_LODM, [
+   ('normal', 'normal'),
+   ('bias', 'bias'),
+   ('replace', 'replace'),
+   ('gradients', 'gradients'),
+])
+
+enum_map(OM_SB_MODE.t, F_SBMODE, [
+   ('none', 'none'),
+   ('rawdata', 'rawdata'),
+   ('coeffs', 'coeffs'),
+   ('both', 'both'),
+])
+
+enum_map(OM_SCHEDSWAP.t, F_SCHED_CTRL, [
+   ('none', 'none'),
+   ('swap', 'swap'),
+])
+
 class OpRef(object):
    def __init__(self, ref_type, index, mods):
       self.type = ref_type
@@ -1320,6 +1345,75 @@ encode_map(O_ATOMIC,
    op_ref_maps=[('backend', [['s0', 's1', 's2', 's3', 's4', 's5']], ['drc', ['s0', 's1', 's2', 's3', 's4', 's5']])]
 )
 
+encode_map(O_SMP,
+   encodings=[
+      (I_SMP_EXTAB, [
+         ('fcnorm', OM_FCNORM),
+         ('drc', ('pco_ref_get_drc', SRC(0))),
+         ('dmn', OM_DIM),
+         ('chan', ('pco_ref_get_imm', SRC(5))),
+         ('lodm', OM_LOD_MODE),
+         ('pplod', OM_PPLOD),
+         ('proj', OM_PROJ),
+         ('sbmode', OM_SB_MODE),
+         ('nncoords', OM_NNCOORDS),
+         ('sno', OM_SNO),
+         ('soo', OM_SOO),
+         ('tao', OM_TAO),
+         ('f16', OM_F16),
+         ('swap', OM_SCHEDSWAP),
+         ('cachemode_ld', OM_MCU_CACHE_MODE_LD)
+      ]),
+      (I_SMP_EXTA, [
+         ('fcnorm', OM_FCNORM),
+         ('drc', ('pco_ref_get_drc', SRC(0))),
+         ('dmn', OM_DIM),
+         ('chan', ('pco_ref_get_imm', SRC(5))),
+         ('lodm', OM_LOD_MODE),
+         ('pplod', OM_PPLOD),
+         ('proj', OM_PROJ),
+         ('sbmode', OM_SB_MODE),
+         ('nncoords', OM_NNCOORDS),
+         ('sno', OM_SNO),
+         ('soo', OM_SOO),
+         ('tao', OM_TAO)
+      ], [
+         (OM_F16, '== false'),
+         (OM_SCHEDSWAP, '== PCO_SCHEDSWAP_NONE'),
+         (OM_MCU_CACHE_MODE_LD, '== PCO_CACHEMODE_LD_NORMAL')
+      ]),
+      (I_SMP_BRIEF, [
+         ('fcnorm', OM_FCNORM),
+         ('drc', ('pco_ref_get_drc', SRC(0))),
+         ('dmn', OM_DIM),
+         ('chan', ('pco_ref_get_imm', SRC(5))),
+         ('lodm', OM_LOD_MODE)
+      ], [
+         (OM_F16, '== false'),
+         (OM_SCHEDSWAP, '== PCO_SCHEDSWAP_NONE'),
+         (OM_MCU_CACHE_MODE_LD, '== PCO_CACHEMODE_LD_NORMAL'),
+         (OM_PPLOD, '== false'),
+         (OM_PROJ, '== false'),
+         (OM_SB_MODE, '== PCO_SB_MODE_NONE'),
+         (OM_NNCOORDS, '== false'),
+         (OM_SNO, '== false'),
+         (OM_SOO, '== false'),
+         (OM_TAO, '== false')
+      ])
+   ],
+   op_ref_maps=[('backend', ['s4'], ['drc', 's0', 's1', 's2', ['s3', '_'], 'imm'])]
+)
+
+encode_map(O_ALPHATST,
+   encodings=[
+      (I_VISTEST_ATST, [
+         ('pwen', ('!pco_ref_is_null', DEST(0))),
+         ('ifb', True)
+      ])
+   ],
+   op_ref_maps=[('backend', [['p0', '_']], ['drc', 's0', 's1', 's2'])]
+)
+
 encode_map(O_BBYP0BM,
    encodings=[
       (I_PHASE0_SRC, [
@@ -2088,6 +2182,38 @@ group_map(O_CSEL,
    ]
 )
 
+group_map(O_PSEL,
+   hdr=(I_IGRP_HDR_MAIN, [
+      ('oporg', 'p0_p1_p2'),
+      ('olchk', OM_OLCHK),
+      ('w1p', False),
+      ('w0p', True),
+      ('cc', OM_EXEC_CND),
+      ('end', OM_END),
+      ('atom', OM_ATOM),
+      ('rpt', OM_RPT)
+   ]),
+   enc_ops=[
+      ('0', O_IMADD64, ['ft0', 'fte'], ['pco_zero', 'pco_zero', 'pco_zero', 'is0', SRC(0)]),
+      ('1', O_MBYP, ['ft1'], [SRC(1)]),
+      ('2_tst', O_TST, ['ftt', '_'], ['is1', '_'], [(OM_TST_OP_MAIN, 'zero'), (OM_TST_TYPE_MAIN, 'u32'), (OM_PHASE2END, True)]),
+      ('2_mov', O_MOVC, [DEST(0), '_'], ['ftt', SRC(2), 'is4', '_', '_'])
+   ],
+   srcs=[
+      ('s[0]', ('0', SRC(0)), 's0'),
+      ('s[1]', ('0', SRC(1)), 's1'),
+      ('s[2]', ('0', SRC(2)), 's2'),
+      ('s[3]', ('1', SRC(0)), 's3'),
+      ('s[4]', ('2_mov', SRC(1)), 'fte'),
+   ],
+   iss=[
+      ('is[0]', 's4'),
+      ('is[1]', 'ft0'),
+      ('is[4]', 'ft1'),
+   ],
+   dests=[('w[0]', ('2_mov', DEST(0)), 'w0')]
+)
+
 group_map(O_PSEL_TRIG,
    hdr=(I_IGRP_HDR_MAIN, [
       ('oporg', 'p0_p1_p2'),
@@ -2532,6 +2658,46 @@ group_map(O_ATOMIC,
    srcs=[
       ('s[0]', ('backend', SRC(1)), 's0'),
       ('s[3]', ('backend', DEST(0)), 's3')
+   ]
+)
+
+group_map(O_SMP,
+   hdr=(I_IGRP_HDR_MAIN, [
+      ('oporg', 'be'),
+      ('olchk', OM_OLCHK),
+      ('w1p', False),
+      ('w0p', False),
+      ('cc', OM_EXEC_CND),
+      ('end', OM_END),
+      ('atom', False),
+      ('rpt', 1)
+   ]),
+   enc_ops=[('backend', O_SMP)],
+   srcs=[
+      ('s[0]', ('backend', SRC(1)), 's0'),
+      ('s[1]', ('backend', SRC(2)), 's1'),
+      ('s[2]', ('backend', SRC(3)), 's2'),
+      ('s[3]', ('backend', SRC(4)), 's3'),
+      ('s[4]', ('backend', DEST(0)), 's4')
+   ]
+)
+
+group_map(O_ALPHATST,
+   hdr=(I_IGRP_HDR_MAIN, [
+      ('oporg', 'be'),
+      ('olchk', OM_OLCHK),
+      ('w1p', False),
+      ('w0p', False),
+      ('cc', OM_EXEC_CND),
+      ('end', OM_END),
+      ('atom', OM_ATOM),
+      ('rpt', 1)
+   ]),
+   enc_ops=[('backend', O_ALPHATST)],
+   srcs=[
+      ('s[0]', ('backend', SRC(1)), 's0'),
+      ('s[1]', ('backend', SRC(2)), 's1'),
+      ('s[2]', ('backend', SRC(3)), 's2'),
    ]
 )
 

commit 6064105a465d45684faaeae78590b2e4a96e008c

pco: reserve additional outputs for trilinear sampled coeffs
Sampling coeffs with trilinear filtering will output 2x sets of data.
Whether bilinear or trilinear filtering is in use can't be determined
without checking state words, so unconditionally reserve 2x to avoid
clobbering output regs.

Fixes: 5d65ad910ff ("pco: initial texture/sampler compiler support")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Tested-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41051>

src/compiler/nir/nir_intrinsics.py
@@ -3011,7 +3011,8 @@ intrinsic("smp_pco", src_comp=[16, 4, 4], dest_comp=0, indices=[SMP_FLAGS_PCO, R
 
 # smp_coeffs_pco(data, tex_state, smp_state)
 # Returns the calculated sampling coefficients for the given data and state words.
-intrinsic("smp_coeffs_pco", src_comp=[16, 4, 4], dest_comp=8, indices=[SMP_FLAGS_PCO, RANGE], bit_sizes=[32])
+# Actually outputs 7/14 components, but NIR doesn't support those for num_components, so fake it as 16 for now.
+intrinsic("smp_coeffs_pco", src_comp=[16, 4, 4], dest_comp=16, indices=[SMP_FLAGS_PCO, RANGE], bit_sizes=[32])
 
 # smp_raw_pco(data, tex_state, smp_state)
 # Returns the raw sampling data for the given data and state words.

src/imagination/pco/pco_trans_nir.c
@@ -1697,8 +1697,11 @@ static pco_instr *lower_smp(trans_ctx *tctx,
    enum pco_sb_mode sb_mode = PCO_SB_MODE_NONE;
    switch (intr->intrinsic) {
    case nir_intrinsic_smp_coeffs_pco:
-      /* Shrink the destination to its actual size. */
-      *dest = pco_ref_chans(*dest, ROGUE_SMP_COEFF_COUNT);
+      /* Shrink the destination to its actual size.
+       * Trilinear filtering will produce two sets of coeffs;
+       * reserve both just in case so that we don't clobber output regs.
+       */
+      *dest = pco_ref_chans(*dest, ROGUE_SMP_COEFF_COUNT * 2u);
       chans = 1; /* Chans must be 1 for coeff mode. */
 
       sb_mode = PCO_SB_MODE_COEFFS;