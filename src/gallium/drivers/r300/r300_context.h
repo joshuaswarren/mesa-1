openharmony/third_party_mesa3d

commit bf96b9cf07b59777955eb8b925709292b2892290

r300: only output wpos in vertex shaders when needed
Right now we always copy pos to wpos regardless of if the fragment
shader needs it or not. Instead just do it only for the shaders
that really need it.

Shader-db is not able to measure preciselly the effect as we
build only the non-wpos variants, but given that majority of fragment
shaders don't needs the wpos, the real effect should be close:

total instructions in shared programs: 105427 -> 104313 (-1.06%)
instructions in affected programs: 53098 -> 51984 (-2.10%)
total temps in shared programs: 13991 -> 13958 (-0.24%)
temps in affected programs: 114 -> 81 (-28.95%)

Signed-off-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15321>

src/gallium/drivers/r300/r300_state_derived.c
@@ -1061,6 +1061,41 @@ static void r300_validate_fragment_shader(struct r300_context *r300)
     }
 }
 
+static void r300_pick_vertex_shader(struct r300_context *r300)
+{
+    struct r300_vertex_shader_code *ptr;
+    struct r300_vertex_shader *vs = r300_vs(r300);
+
+    if (r300->vs_state.state) {
+        bool wpos = r300_fs(r300)->shader->inputs.wpos != ATTR_UNUSED;
+
+        if (!vs->first) {
+            /* Build the vertex shader for the first time. */
+            vs->first = vs->shader = CALLOC_STRUCT(r300_vertex_shader_code);
+            vs->first->wpos = wpos;
+            r300_translate_vertex_shader(r300, vs);
+            r300_mark_atom_dirty(r300, &r300->rs_block_state);
+            return;
+        }
+        /* Pick the vertex shader based on whether we need wpos */
+        if (vs->first->wpos != wpos) {
+            if (vs->first->next && vs->first->next->wpos == wpos) {
+                ptr = vs->first->next;
+                vs->first->next = NULL;
+                ptr->next = vs->first;
+                vs->first = vs->shader = ptr;
+            } else {
+                ptr = CALLOC_STRUCT(r300_vertex_shader_code);
+                ptr->next = vs->first;
+                vs->first = vs->shader = ptr;
+                vs->shader->wpos = wpos;
+                r300_translate_vertex_shader(r300, vs);
+            }
+            r300_mark_atom_dirty(r300, &r300->rs_block_state);
+        }
+    }
+}
+
 void r300_update_derived_state(struct r300_context* r300)
 {
     if (r300->textures_state.dirty) {
@@ -1069,6 +1104,8 @@ void r300_update_derived_state(struct r300_context* r300)
     }
 
     r300_validate_fragment_shader(r300);
+    if (r300->screen->caps.has_tcl)
+        r300_pick_vertex_shader(r300);
 
     if (r300->rs_block_state.dirty) {
         r300_update_rs_block(r300);

src/gallium/drivers/r300/r300_vs.c
@@ -99,7 +99,7 @@ static void r300_shader_read_vs_outputs(
         }
     }
 
-    /* WPOS is a straight copy of POSITION and it's always emitted. */
+    /* WPOS is a straight copy of POSITION */
     vs_outputs->wpos = i;
 }
 
@@ -167,7 +167,8 @@ static void set_vertex_inputs_outputs(struct r300_vertex_program_compiler * c)
     }
 
     /* WPOS. */
-    c->code->outputs[outputs->wpos] = reg++;
+    if (vs->wpos)
+        c->code->outputs[outputs->wpos] = reg++;
 }
 
 void r300_init_vs_outputs(struct r300_context *r300,
@@ -253,11 +254,12 @@ void r300_translate_vertex_shader(struct r300_context *r300,
         compiler.Base.remove_unused_constants = TRUE;
     }
 
-    compiler.RequiredOutputs = ~(~0U << (vs->info.num_outputs + 1));
+    compiler.RequiredOutputs = ~(~0U << (vs->info.num_outputs + (vs->wpos ? 1 : 0)));
     compiler.SetHwInputOutput = &set_vertex_inputs_outputs;
 
     /* Insert the WPOS output. */
-    rc_copy_output(&compiler.Base, 0, vs->outputs.wpos);
+    if (vs->wpos)
+        rc_copy_output(&compiler.Base, 0, vs->outputs.wpos);
 
     /* Invoke the compiler */
     r3xx_compile_vertex_program(&compiler);

joshuaswarren/mesa-1

commit 5350a674b9451429be52eeda1edc90c301d9e770

r300: dirty VS state when switching variants
When r300_pick_vertex_shader switches to a WPOS variant, it only dirtied
rs_block_state, leaving vs_state with a stale code size. This caused
cs_count warnings (offset of -4 for one extra VS instruction) but was
mostly harmless since the emitted packet stream still used the current
shader.

Factor the VS code dirtying out of r300_bind_vs_state into a helper and
call it when selecting a new variant too.

Fixes: bf96b9cf07b ("r300: only output wpos in vertex shaders when needed")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41200>

src/gallium/drivers/r300/r300_context.h
@@ -804,6 +804,7 @@ enum r300_fb_state_change {
 
 void r300_mark_fb_state_dirty(struct r300_context *r300,
                               enum r300_fb_state_change change);
+void r300_mark_vs_code_dirty(struct r300_context *r300);
 void r300_mark_fs_code_dirty(struct r300_context *r300);
 
 struct pipe_sampler_view *

src/gallium/drivers/r300/r300_state.c
@@ -2212,6 +2212,27 @@ static void* r300_create_vs_state(struct pipe_context* pipe,
     return vs;
 }
 
+void r300_mark_vs_code_dirty(struct r300_context *r300)
+{
+    struct r300_vertex_shader *vs = r300_vs(r300);
+    unsigned fc_op_dwords = r300->screen->caps.is_r500 ? 3 : 2;
+
+    r300_mark_atom_dirty(r300, &r300->vs_state);
+    r300->vs_state.size = vs->shader->code.length + 9 +
+            (R300_VS_MAX_FC_OPS * fc_op_dwords + 4);
+
+    r300_mark_atom_dirty(r300, &r300->vs_constants);
+    r300->vs_constants.size =
+            2 +
+            (vs->shader->externals_count ? vs->shader->externals_count * 4 + 3 : 0) +
+            (vs->shader->immediates_count ? vs->shader->immediates_count * 4 + 3 : 0);
+
+    ((struct r300_constant_buffer*)r300->vs_constants.state)->remap_table =
+            vs->shader->code.constants_remap_table;
+
+    r300_mark_atom_dirty(r300, &r300->pvs_flush);
+}
+
 static void r300_bind_vs_state(struct pipe_context* pipe, void* shader)
 {
     struct r300_context* r300 = r300_context(pipe);
@@ -2230,21 +2251,7 @@ static void r300_bind_vs_state(struct pipe_context* pipe, void* shader)
     r300_mark_atom_dirty(r300, &r300->rs_block_state); /* Will be updated before the emission. */
 
     if (r300->screen->caps.has_tcl) {
-        unsigned fc_op_dwords = r300->screen->caps.is_r500 ? 3 : 2;
-        r300_mark_atom_dirty(r300, &r300->vs_state);
-        r300->vs_state.size = vs->shader->code.length + 9 +
-			(R300_VS_MAX_FC_OPS * fc_op_dwords + 4);
-
-        r300_mark_atom_dirty(r300, &r300->vs_constants);
-        r300->vs_constants.size =
-                2 +
-                (vs->shader->externals_count ? vs->shader->externals_count * 4 + 3 : 0) +
-                (vs->shader->immediates_count ? vs->shader->immediates_count * 4 + 3 : 0);
-
-        ((struct r300_constant_buffer*)r300->vs_constants.state)->remap_table =
-                vs->shader->code.constants_remap_table;
-
-        r300_mark_atom_dirty(r300, &r300->pvs_flush);
+        r300_mark_vs_code_dirty(r300);
     } else {
         draw_bind_vertex_shader(r300->draw,
                 (struct draw_vertex_shader*)vs->draw_vs);

src/gallium/drivers/r300/r300_state_derived.c
@@ -1056,8 +1056,10 @@ static void r300_pick_vertex_shader(struct r300_context *r300)
             vs->first = vs->shader = CALLOC_STRUCT(r300_vertex_shader_code);
             vs->first->wpos = wpos;
             r300_translate_vertex_shader(r300, vs);
-            if (!vs->first->dummy)
+            if (!vs->first->dummy) {
+                r300_mark_vs_code_dirty(r300);
                 r300_mark_atom_dirty(r300, &r300->rs_block_state);
+            }
             return;
         }
         /* Pick the vertex shader based on whether we need wpos */
@@ -1074,8 +1076,10 @@ static void r300_pick_vertex_shader(struct r300_context *r300)
                 vs->shader->wpos = wpos;
                 r300_translate_vertex_shader(r300, vs);
             }
-            if (!vs->first->dummy)
+            if (!vs->first->dummy) {
+                r300_mark_vs_code_dirty(r300);
                 r300_mark_atom_dirty(r300, &r300->rs_block_state);
+            }
         }
     }
 }