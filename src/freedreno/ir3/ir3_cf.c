openharmony/third_party_mesa3d

commit ac267be93ae05e448274af46936bff38ee566447

freedreno/ir3/cf: handle multiple cov's properly
There can be multiple (for ex.) f32f16's from a single source, in
particular appearing in different blocks.  We need to update all uses
of the src which had conversion folded in, not all the uses of the
individual cov.  Also, to avoid invalidating the ssa use info that was
gathered at the beginning of the pass, don't actually eliminate the
cov, but instead change it to a simple mov that the cp pass can gobble
up.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5048>

src/freedreno/ir3/ir3_cf.c
@@ -65,14 +65,24 @@ all_uses_fp16_conv(struct ir3_instruction *conv_src)
 	return true;
 }
 
+/* For an instruction which has a conversion folded in, re-write the
+ * uses of *all* conv's that used that src to be a simple mov that
+ * cp can eliminate.  This avoids invalidating the SSA uses, it just
+ * shifts the use to a simple mov.
+ */
 static void
-rewrite_uses(struct ir3_instruction *conv, struct ir3_instruction *replace)
+rewrite_src_uses(struct ir3_instruction *src)
 {
-	foreach_ssa_use (use, conv) {
-		foreach_ssa_src_n (src, n, use) {
-			if (src == conv)
-				use->regs[n]->instr = replace;
+	foreach_ssa_use (use, src) {
+		assert(is_fp16_conv(use));
+
+		if (is_half(src)) {
+			use->regs[1]->flags |= IR3_REG_HALF;
+		} else {
+			use->regs[1]->flags &= ~IR3_REG_HALF;
 		}
+
+		use->cat1.src_type = use->cat1.dst_type;
 	}
 }
 
@@ -113,6 +123,7 @@ try_conversion_folding(struct ir3_instruction *conv)
 		if (src->cat1.dst_type != src->cat1.src_type &&
 			conv->cat1.src_type != src->cat1.dst_type)
 			return false;
+		break;
 	default:
 		break;
 	}
@@ -137,13 +148,13 @@ try_conversion_folding(struct ir3_instruction *conv)
 		}
 	}
 
-	if (conv->regs[0]->flags & IR3_REG_HALF) {
+	if (is_half(conv)) {
 		src->regs[0]->flags |= IR3_REG_HALF;
 	} else {
 		src->regs[0]->flags &= ~IR3_REG_HALF;
 	}
 
-	rewrite_uses(conv, src);
+	rewrite_src_uses(src);
 
 	return true;
 }
@@ -157,7 +168,7 @@ ir3_cf(struct ir3 *ir)
 	ir3_find_ssa_uses(ir, mem_ctx, false);
 
 	foreach_block (block, &ir->block_list) {
-		foreach_instr_safe (instr, &block->instr_list) {
+		foreach_instr (instr, &block->instr_list) {
 			progress |= try_conversion_folding(instr);
 		}
 	}

joshuaswarren/mesa-1

commit e6a886ceb10950759cda8f4e31fad1ac7448b2cd

ir3/cf: fix rewriting uses with different dst types
When an instruction has multiple cov uses that have different dst types,
we cannot simple make them all a mov, as some may still have to perform
an actual conversion. Fix this by setting the use's src type to the dst
type of the conversion we just folded.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15395
Fixes: ac267be93ae ("freedreno/ir3/cf: handle multiple cov's properly")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41355>

src/freedreno/ir3/ir3_cf.c
@@ -132,7 +132,7 @@ all_uses_same_cov(struct ir3_instruction *movs)
  * shifts the use to a simple mov.
  */
 static void
-rewrite_src_uses(struct ir3_instruction *src)
+rewrite_src_uses(struct ir3_instruction *src, type_t src_type)
 {
    foreach_ssa_use (use, src) {
       assert(use->opc == OPC_MOV);
@@ -143,7 +143,7 @@ rewrite_src_uses(struct ir3_instruction *src)
          use->srcs[0]->flags &= ~IR3_REG_HALF;
       }
 
-      use->cat1.src_type = use->cat1.dst_type;
+      use->cat1.src_type = src_type;
    }
 }
 
@@ -215,7 +215,7 @@ try_conversion_folding(struct ir3_instruction *conv,
    }
 
    ir3_set_dst_type(src, is_half(conv));
-   rewrite_src_uses(src);
+   rewrite_src_uses(src, conv->cat1.dst_type);
 
    return true;
 }