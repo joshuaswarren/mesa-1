joshuaswarren/mesa-1

commit 95ff74ec2bfbc0d34594a4b9cbd86b6d0330a2c3

radv: Use compute copy for emulated formats
These aren't supported by the hardware, so better to use the
compute copy implementation with these formats.

Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25594>

src/amd/vulkan/radv_sdma.c
@@ -714,7 +714,8 @@ radv_sdma_supports_image(const struct radv_device *device, const struct radv_ima
 {
    const struct radv_physical_device *pdev = radv_device_physical(device);
 
-   /* TODO: Handle emulated formats and verify sparse image support. */
+   if (radv_is_format_emulated(pdev, image->vk.format))
+      return false;
 
    if (!pdev->info.sdma_supports_sparse &&
        (image->vk.create_flags & VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT))

commit b0dac23138b3eadc464e7efa69603293bcbeab53

radv: do not fallback to compute for image->buffer copies with emulated formats
This shouldn't be necessary because SDMA can detile the image just fine,
only buffer->image and image->image need to fallback.

It just works on GFX10+ because RADV is using NBC views, and I think
it works on eg. VEGA10 just by luck due to different
swizzles/alignments.

Fixes: 95ff74ec2bf ("radv: Use compute copy for emulated formats")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41384>

src/amd/vulkan/meta/radv_meta_copy.c
@@ -88,7 +88,7 @@ transfer_copy_memory_image(struct radv_cmd_buffer *cmd_buffer, VkAddressCopyFlag
    const struct radv_device *device = radv_cmd_buffer_device(cmd_buffer);
    struct radv_cmd_stream *cs = cmd_buffer->cs;
 
-   if (!radv_sdma_supports_image(device, image)) {
+   if (!radv_sdma_supports_image(device, image, to_image)) {
       if (!radv_gang_init(cmd_buffer))
          return;
 
@@ -598,7 +598,7 @@ transfer_copy_image(struct radv_cmd_buffer *cmd_buffer, struct radv_image *src_i
    struct radv_cmd_stream *cs = cmd_buffer->cs;
    unsigned int dst_aspect_mask_remaining = region->dstSubresource.aspectMask;
 
-   if (!radv_sdma_supports_image(device, src_image) || !radv_sdma_supports_image(device, dst_image)) {
+   if (!radv_sdma_supports_image(device, src_image, false) || !radv_sdma_supports_image(device, dst_image, true)) {
       if (!radv_gang_init(cmd_buffer))
          return;
 

src/amd/vulkan/radv_sdma.c
@@ -524,12 +524,12 @@ radv_sdma_copy_image_t2t_scanline(const struct radv_device *device, struct radv_
 }
 
 bool
-radv_sdma_supports_image(const struct radv_device *device, const struct radv_image *image)
+radv_sdma_supports_image(const struct radv_device *device, const struct radv_image *image, bool to_image)
 {
    const struct radv_physical_device *pdev = radv_device_physical(device);
 
    if (radv_is_format_emulated(pdev, image->vk.format))
-      return false;
+      return to_image ? false : true;
 
    if (!pdev->info.sdma_supports_sparse &&
        (image->vk.create_flags & VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT))

src/amd/vulkan/radv_sdma.h
@@ -68,7 +68,7 @@ void radv_sdma_copy_memory(const struct radv_device *device, struct radv_cmd_str
                            uint64_t dst_va, uint64_t size, bool tmz);
 void radv_sdma_fill_memory(const struct radv_device *device, struct radv_cmd_stream *cs, const uint64_t va,
                            const uint64_t size, const uint32_t value);
-bool radv_sdma_supports_image(const struct radv_device *device, const struct radv_image *image);
+bool radv_sdma_supports_image(const struct radv_device *device, const struct radv_image *image, bool to_image);
 
 void radv_sdma_emit_nop(const struct radv_device *device, struct radv_cmd_stream *cs);
 