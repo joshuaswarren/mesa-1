joshuaswarren/mesa-1

commit 6298e5e4773088a22c653f2b3ffa76d15a61da92

nvk: Reenable compression support with nouveau 1.4.2
Now that the small/large pages race is fixed, we can safely enable it
back when the kernel side report 1.4.2 support.

Fixes: f509e460abf4 ("nvk: Disable large pages for now")
Signed-off-by: Mary Guillemard <mary@mary.zone>
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39706>

src/nouveau/vulkan/nvkmd/nouveau/nvkmd_nouveau_pdev.c
@@ -101,13 +101,8 @@ nvkmd_nouveau_try_create_pdev(struct _drmDevice *drm_device,
       .has_alloc_tiled = nouveau_ws_device_has_tiled_bo(ws_dev),
       .has_map_fixed = true,
       .has_overmap = true,
-      /*
-       * Disabled for now due to
-       * https://gitlab.freedesktop.org/mesa/mesa/-/issues/14610
-       * .has_compression = ws_dev->nouveau_version >= 0x01000401 &&
-       *                    ws_dev->info.cls_eng3d >= TURING_A,
-       */
-      .has_compression = false,
+      .has_compression = ws_dev->nouveau_version >= 0x01000402 &&
+                         ws_dev->info.cls_eng3d >= TURING_A,
    };
 
    /* We get this ourselves */

commit 8c89a768d6d98a08ee15d226c555b8ade8562613

nvk: Disable compression on Turing
We've had multiple reports that this is broken on Turing right now.
Disable it until someone figures out what's wrong.

See also: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15358

Fixes: 6298e5e4773 ("nvk: Reenable compression support with nouveau 1.4.2")
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Reviewed-by: Mary Guillemard <mary@mary.zone>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41332>

src/nouveau/vulkan/nvkmd/nouveau/nvkmd_nouveau_pdev.c
@@ -16,7 +16,7 @@
 #include <sys/stat.h>
 #include <xf86drm.h>
 
-#include "clc597.h"
+#include "clc697.h"
 
 static bool
 drm_device_is_nouveau(const char *path)
@@ -102,7 +102,7 @@ nvkmd_nouveau_try_create_pdev(struct _drmDevice *drm_device,
       .has_map_fixed = true,
       .has_overmap = true,
       .has_compression = ws_dev->nouveau_version >= 0x01000402 &&
-                         ws_dev->info.cls_eng3d >= TURING_A,
+                         ws_dev->info.cls_eng3d >= AMPERE_A,
    };
 
    /* We get this ourselves */