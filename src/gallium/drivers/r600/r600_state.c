joshuaswarren/mesa-1

commit 1210c420d354e571ffd5114119bf23ba78167144

r600: fix rv770 border color
The border color of the rv770 gpu behaves the same way as
the evergreen border color. This change updates the software
accordingly.

This change is enabled for all the pre-evergreen gpus.

This change fixes 120 piglit tests. The rv770 ci is updated
as well.

Cc: mesa-stable
Signed-off-by: Patrick Lerda <patrick9876@free.fr>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34502>

src/gallium/drivers/r600/r600_state.c
@@ -1861,12 +1861,18 @@ static void r600_emit_sampler_states(struct r600_context *rctx,
 		radeon_emit_array(cs, rstate->tex_sampler_words, 3);
 
 		if (rstate->border_color_use) {
+			union pipe_color_union border_color = {{0,0,0,1}};
 			unsigned offset;
+
+			/* The rv770 border color is fully compatible with
+			 * evergreen. */
+			evergreen_convert_border_color(&rstate->border_color,
+						       &border_color, &rview->base);
 
 			offset = border_color_reg;
 			offset += i * 16;
 			radeon_set_config_reg_seq(cs, offset, 4);
-			radeon_emit_array(cs, rstate->border_color.ui, 4);
+			radeon_emit_array(cs, border_color.ui, 4);
 		}
 	}
 	texinfo->states.dirty_mask = 0;

src/gallium/drivers/r600/evergreen_state.c
@@ -2597,9 +2597,9 @@ static void cayman_convert_border_color(union pipe_color_union *in,
 	}
 }
 
-static void evergreen_convert_border_color(union pipe_color_union *in,
-                                           union pipe_color_union *out,
-                                           struct pipe_sampler_view *view)
+void evergreen_convert_border_color(const union pipe_color_union *in,
+				    union pipe_color_union *out,
+				    const struct pipe_sampler_view *view)
 {
 	const enum pipe_format format = view->format;
 	const struct util_format_description *d = util_format_description(format);

src/gallium/drivers/r600/r600_pipe.h
@@ -725,6 +725,10 @@ bool evergreen_adjust_gprs(struct r600_context *rctx);
 void evergreen_setup_scratch_buffers(struct r600_context *rctx);
 uint32_t evergreen_construct_rat_mask(struct r600_context *rctx, struct r600_cb_misc_state *a,
 				      unsigned nr_cbufs);
+void evergreen_convert_border_color(const union pipe_color_union *in,
+				    union pipe_color_union *out,
+				    const struct pipe_sampler_view *view);
+
 /* r600_blit.c */
 void r600_init_blit_functions(struct r600_context *rctx);
 void r600_decompress_depth_textures(struct r600_context *rctx,

commit 3f4f41b3ef53251fc99b88b74f86ac0c5b9dca95

r600: Fix crash on R600/R700 with custom border color
r600_pipe_sampler_view may be NULL. This is properly checked
when emmiting samplers on EG+, but not on R600/R700.
Fix a crash on Tesseract

Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Fixes: 1210c420
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41432>

src/gallium/drivers/r600/r600_state.c
@@ -1861,10 +1861,14 @@ static void r600_emit_sampler_states(struct r600_context *rctx,
 			union pipe_color_union border_color = {{0,0,0,1}};
 			unsigned offset;
 
-			/* The rv770 border color is fully compatible with
-			 * evergreen. */
-			evergreen_convert_border_color(&rstate->border_color,
-						       &border_color, &rview->base);
+			if (rview) {
+				/* The rv770 border color is fully compatible with
+				* evergreen. */
+				evergreen_convert_border_color(&rstate->border_color,
+				                	       &border_color, &rview->base);
+			} else {
+				border_color = rstate->border_color;
+			}
 
 			offset = border_color_reg;
 			offset += i * 16;