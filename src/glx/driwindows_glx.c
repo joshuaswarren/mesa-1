joshuaswarren/mesa-1

commit 156d3ef9eeb643e36e4b2243ac600b12eebcc1f0

glx: move driwindows_display::event_base to driwindows_context
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30375>

src/glx/driwindows_glx.c
@@ -30,7 +30,6 @@
 
 struct driwindows_display
 {
-   int event_base;
 };
 
 struct driwindows_context
@@ -50,6 +49,7 @@ struct driwindows_screen
    struct glx_screen base;
    __DRIscreen *driScreen;
    __GLXDRIscreen vtable;
+   int event_base;
    Bool copySubBuffer;
 };
 
@@ -466,6 +466,24 @@ driwindowsCreateScreen(int screen, struct glx_display *priv, bool driver_name_is
    struct driwindows_screen *psc;
    struct glx_config *configs = NULL, *visuals = NULL;
    int directCapable;
+   int eventBase, errorBase;
+   int major, minor, patch;
+
+   /* Verify server has Windows-DRI extension */
+   if (!XWindowsDRIQueryExtension(dpy, &eventBase, &errorBase)) {
+      ErrorMessageF("Windows-DRI extension not available\n");
+      return NULL;
+   }
+
+   if (!XWindowsDRIQueryVersion(dpy, &major, &minor, &patch)) {
+      ErrorMessageF("Fetching Windows-DRI extension version failed\n");
+      return NULL;
+   }
+
+   if (!windows_check_renderer()) {
+      ErrorMessageF("Windows-DRI extension disabled for GDI Generic renderer\n");
+      return NULL;
+   }
 
    psc = calloc(1, sizeof *psc);
    if (psc == NULL)
@@ -537,30 +555,9 @@ driwindowsCreateDisplay(Display * dpy)
 {
    struct driwindows_display *pdpyp;
 
-   int eventBase, errorBase;
-   int major, minor, patch;
-
-   /* Verify server has Windows-DRI extension */
-   if (!XWindowsDRIQueryExtension(dpy, &eventBase, &errorBase)) {
-      ErrorMessageF("Windows-DRI extension not available\n");
-      return NULL;
-   }
-
-   if (!XWindowsDRIQueryVersion(dpy, &major, &minor, &patch)) {
-      ErrorMessageF("Fetching Windows-DRI extension version failed\n");
-      return NULL;
-   }
-
-   if (!windows_check_renderer()) {
-      ErrorMessageF("Windows-DRI extension disabled for GDI Generic renderer\n");
-      return NULL;
-   }
-
    pdpyp = malloc(sizeof *pdpyp);
    if (pdpyp == NULL)
       return NULL;
-
-   pdpyp->event_base = eventBase;
 
    return &pdpyp->base;
 }

commit 43a1ccfd5fec777570d565e7c04ea849e8d73bf4

glx/windows: Fix compliation after code motion to put event base in 'dri' context
> ../src/glx/driwindows_glx.c: In function ‘driwindowsCreateScreen’:
> ../src/glx/driwindows_glx.c:479:48: error: ‘dpy’ undeclared (first use in this function)

156d3ef9eeb6 moved this code from driwindowsCreateDisplay to
driwindowsCreateScreen.

Unfortunately, dpy is no longer a parameter, so we need to look it up in
glx_display struct. (Looking it up in psc->base before it's initialized
here initialized would be incorrect).

Fixes: 156d3ef9eeb6 ("glx: move driwindows_display::event_base to driwindows_context")
Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41421>

src/glx/driwindows_glx.c
@@ -452,12 +452,12 @@ driwindowsCreateScreen(int screen, struct glx_display *priv, bool driver_name_is
    int major, minor, patch;
 
    /* Verify server has Windows-DRI extension */
-   if (!XWindowsDRIQueryExtension(dpy, &eventBase, &errorBase)) {
+   if (!XWindowsDRIQueryExtension(priv->dpy, &eventBase, &errorBase)) {
       ErrorMessageF("Windows-DRI extension not available\n");
       return NULL;
    }
 
-   if (!XWindowsDRIQueryVersion(dpy, &major, &minor, &patch)) {
+   if (!XWindowsDRIQueryVersion(priv->dpy, &major, &minor, &patch)) {
       ErrorMessageF("Fetching Windows-DRI extension version failed\n");
       return NULL;
    }