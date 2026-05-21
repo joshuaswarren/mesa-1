joshuaswarren/mesa-1

commit f90cb8edc3fdd08120fa71bf3169e1555664f4d8

zink: keep ffma_weak and use GLSLstd450Fma for it
This pushes the decision down to Vulkan drivers and they can do whatever
they think is best.

Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41684>

src/gallium/drivers/zink/zink_compiler.c
@@ -1304,6 +1304,7 @@ zink_screen_init_compiler(struct zink_screen *screen)
       .lower_mul_2x32_64 = true,
       .support_16bit_alu = true, /* not quite what it sounds like */
       .max_unroll_iterations = 0,
+      .float_mul_add32 = nir_float_muladd_support_keep_weak_ffma,
    };
 
    screen->nir_options = default_options;
@@ -1318,7 +1319,12 @@ zink_screen_init_compiler(struct zink_screen *screen)
        * stop Vulkan drivers from unrolling the loops.
        */
       screen->nir_options.max_unroll_iterations_fp64 = 32;
+   } else {
+      screen->nir_options.float_mul_add64 |= nir_float_muladd_support_keep_weak_ffma;
    }
+
+   if (screen->base.caps.fp16)
+      screen->nir_options.float_mul_add16 |= nir_float_muladd_support_keep_weak_ffma;
 
    /* XXX: do any drivers need different estimates? */
    screen->nir_options.varying_expression_max_cost = amd_varying_expression_max_cost;

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2290,6 +2290,13 @@ emit_alu(struct ntv_context *ctx, nir_alu_instr *alu)
       break;
    }
 
+   case nir_op_ffma_weak: {
+      assert(nir_op_infos[alu->op].num_inputs == 3);
+      result = emit_builtin_triop(ctx, GLSLstd450Fma, dest_type,
+                                  src[0], src[1], src[2]);
+      break;
+   }
+
    default:
       fprintf(stderr, "emit_alu: not implemented (%s)\n",
               nir_op_infos[alu->op].name);

commit e53251284749f656bd6cdd26b33224adf3c4aad1

zink: proper advertise keep_weak_ffma for fp16
Zink never sets the fp16 screen cap, but the caps also are initialized
after zink_screen_init_compiler. So just replicate the check to be safe
here.

Fixes: f90cb8edc3f ("zink: keep ffma_weak and use GLSLstd450Fma for it")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41722>

src/gallium/drivers/zink/zink_compiler.c
@@ -1323,7 +1323,8 @@ zink_screen_init_compiler(struct zink_screen *screen)
       screen->nir_options.float_mul_add64 |= nir_float_muladd_support_keep_weak_ffma;
    }
 
-   if (screen->base.caps.fp16)
+   if (screen->info.feats12.shaderFloat16 ||
+       (screen->info.have_KHR_shader_float16_int8 && screen->info.shader_float16_int8_feats.shaderFloat16))
       screen->nir_options.float_mul_add16 |= nir_float_muladd_support_keep_weak_ffma;
 
    /* XXX: do any drivers need different estimates? */