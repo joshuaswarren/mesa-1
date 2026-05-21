joshuaswarren/mesa-1

commit 59e900c92be5c6a95499a830a8069275c4ffbf0f

brw/scoreboard: Use a predicate helper for the nomask workaround
If it wasn't for the workaround, it wouldn't be necessary to track the
whether instructions are exec_all or not.  The workaround affects
results when mixing a dep and inst with different exec_all.

Add the predicate so that, when the workaround is disabled, none of
the effects of having different exec_all will kick in, all them will
be considered `exec_all = true`.

This patch don't change any behavior, just adds the predicate.

Reviewed-by: Francisco Jerez <currojerez@riseup.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36659>

src/intel/compiler/brw/brw_lower_scoreboard.cpp
@@ -855,6 +855,16 @@ namespace {
    };
 
    /**
+    * Whether it is needed to apply workaround to avoid
+    * data coherency issues due to Wa_1407528679.
+    */
+   bool
+   needs_nomask_workaround(const intel_device_info *devinfo)
+   {
+      return true;
+   }
+
+   /**
     * Add dependency \p dep to the list of dependencies of an instruction
     * \p deps.
     */
@@ -983,7 +993,8 @@ namespace {
                                    const dependency_list &deps,
                                    const ordered_address &jp)
    {
-      const bool exec_all = inst->force_writemask_all;
+      const bool exec_all = inst->force_writemask_all ||
+                            !needs_nomask_workaround(devinfo);
       const bool has_ordered = find_ordered_dependency(deps, jp, exec_all);
       const tgl_pipe ordered_pipe = ordered_dependency_swsb(deps, jp,
                                                             exec_all).pipe;
@@ -1224,7 +1235,8 @@ namespace {
       unsigned ip = 0;
 
       foreach_block_and_inst(block, brw_inst, inst, shader->cfg) {
-         const bool exec_all = inst->force_writemask_all;
+         const bool exec_all = inst->force_writemask_all ||
+                               !needs_nomask_workaround(devinfo);
          const tgl_pipe p = inferred_exec_pipe(devinfo, inst);
          scoreboard &sb = sbs[block->num];
 
@@ -1377,7 +1389,8 @@ namespace {
       unsigned ip = 0;
 
       foreach_block_and_inst_safe(block, brw_inst, inst, shader->cfg) {
-         const bool exec_all = inst->force_writemask_all;
+         const bool exec_all = inst->force_writemask_all ||
+                               !needs_nomask_workaround(devinfo);
          const bool ordered_mode =
             baked_ordered_dependency_mode(devinfo, inst, deps[ip], jps[ip]);
          const tgl_sbid_mode unordered_mode =

commit 174c7b51031c09133dce434d0d38317aa70343ec

brw: Fix ordered dependency exec_all handling on Xe2+
On Xe2+ the Wa_1407528679 NoMask workaround is disabled, so
baked_ordered_dependency_mode() should treat all instructions as
exec_all, matching the logic in gather_inst_dependencies() and
emit_inst_dependencies().

Without this, ordered RegDist dependencies from uniform/WE_all
producers (e.g. 'mov s0, imm') are not found during baking and
fall through as separate WE_all SYNC NOPs. Real shaders pile up
dozens of these in front of masked sends.

v2(Caio): Fix existing scalar_register test expectations

Signed-off-by: Michael Cheng <michael.cheng@intel.com>
Fixes: 59e900c92be5 ("brw/scoreboard: Use a predicate helper for the nomask workaround")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41713>

src/intel/compiler/brw/brw_lower_scoreboard.cpp
@@ -950,7 +950,8 @@ namespace {
                                  const std::vector<dependency> &deps,
                                  const ordered_address &jp)
    {
-      const bool exec_all = inst->force_writemask_all;
+      const bool exec_all = inst->force_writemask_all ||
+                            !needs_nomask_workaround(devinfo);
       const bool has_ordered = find_ordered_dependency(deps, jp, exec_all);
       const tgl_pipe ordered_pipe = ordered_dependency_swsb(deps, jp,
                                                             exec_all).pipe;

src/intel/compiler/brw/test_lower_scoreboard.cpp
@@ -1125,8 +1125,7 @@ TEST_F(scoreboard_test, scalar_register_mov_immediate_is_in_scalar_pipe)
    EXPECT_PROGRESS(brw_lower_scoreboard, bld);
 
    exp.uniform().MOV(scalar, imm);
-                 SYNC_NOP(exp   )->sched = SWSB("S@1");
-   exp          .MOV(r20, scalar);
+   exp          .MOV(r20, scalar)->sched = SWSB("S@1");
 
    EXPECT_SHADERS_MATCH(bld, exp);
 }
@@ -1148,8 +1147,7 @@ TEST_F(scoreboard_test, scalar_register_mov_grf_is_not_in_scalar_pipe)
    EXPECT_PROGRESS(brw_lower_scoreboard, bld);
 
    exp.uniform().MOV     (scalar, r10);
-                 SYNC_NOP(exp       )->sched = SWSB("I@1");
-   exp          .MOV     (r20, scalar);
+   exp          .MOV     (r20, scalar)->sched = SWSB("I@1");
 
    EXPECT_SHADERS_MATCH(bld, exp);
 }
@@ -1287,7 +1285,7 @@ TEST_F(scoreboard_test, implicit_dependency_inside_if)
    EXPECT_SHADERS_MATCH(bld, exp);
 }
 
-TEST_F(scoreboard_test, DISABLED_xe2_uniform_writer_baked_into_masked_consumer)
+TEST_F(scoreboard_test, xe2_uniform_writer_baked_into_masked_consumer)
 {
    set_gfx_verx10(200);
 
@@ -1308,7 +1306,7 @@ TEST_F(scoreboard_test, DISABLED_xe2_uniform_writer_baked_into_masked_consumer)
    EXPECT_SHADERS_MATCH(bld, exp);
 }
 
-TEST_F(scoreboard_test, DISABLED_xe2_uniform_writer_baked_into_masked_send)
+TEST_F(scoreboard_test, xe2_uniform_writer_baked_into_masked_send)
 {
    set_gfx_verx10(200);
 