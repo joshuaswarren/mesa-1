joshuaswarren/mesa-1

commit 4a347fb78823015d9b7cd6229bf8d5709848bf06

egl/gbm: Destroy excess BOs
Calculate the minimum available buffer age in addition to the maximum,
and if they differ for 1000 frames in a row, destroy the BO for the
highest-age unused buffer.

Without this, Wayland compositors using dynamic triple buffering always
get buffer age 3 once a third BO has been allocated.

v2:
* Rename function to destroy_oldest_unused_bo. (Marek Olšák)
* Move function call into if block.
* Use == instead of > as the condition for the function call.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37511>

src/egl/drivers/dri2/platform_drm.c
@@ -216,21 +216,58 @@ dri2_drm_destroy_surface(_EGLDisplay *disp, _EGLSurface *surf)
    return EGL_TRUE;
 }
 
+static void
+destroy_oldest_unused_bo(struct dri2_egl_surface *dri2_surf)
+{
+   int max_age = 0;
+   struct dri2_egl_buffer *oldest_buffer = NULL;
+
+   for (unsigned i = 0; i < ARRAY_SIZE(dri2_surf->color_buffers); i++) {
+      if (dri2_surf->color_buffers[i].locked ||
+          dri2_surf->back == &dri2_surf->color_buffers[i])
+         continue;
+
+      if (!max_age || dri2_surf->color_buffers[i].age > max_age) {
+         oldest_buffer = &dri2_surf->color_buffers[i];
+         max_age = dri2_surf->color_buffers[i].age;
+      }
+   }
+
+   gbm_bo_destroy(oldest_buffer->bo);
+   oldest_buffer->bo = NULL;
+   oldest_buffer->age = 0;
+
+   dri2_surf->excess_bo_frames = 0;
+}
+
 static int
 get_back_bo(struct dri2_egl_surface *dri2_surf)
 {
    struct dri2_egl_display *dri2_dpy =
       dri2_egl_display(dri2_surf->base.Resource.Display);
    struct gbm_dri_surface *surf = dri2_surf->gbm_surf;
-   int age = 0;
+   int min_age = 0, max_age = 0;
 
    if (dri2_surf->back == NULL) {
       for (unsigned i = 0; i < ARRAY_SIZE(dri2_surf->color_buffers); i++) {
-         if (!dri2_surf->color_buffers[i].locked &&
-             dri2_surf->color_buffers[i].age >= age) {
-            dri2_surf->back = &dri2_surf->color_buffers[i];
-            age = dri2_surf->color_buffers[i].age;
+         if (!dri2_surf->color_buffers[i].locked) {
+            int age = dri2_surf->color_buffers[i].age;
+
+            if (!min_age || age < min_age)
+               min_age = age;
+
+            if (!max_age || age > max_age) {
+               dri2_surf->back = &dri2_surf->color_buffers[i];
+               max_age = age;
+            }
          }
+      }
+
+      if (min_age && min_age < max_age) {
+         if (++dri2_surf->excess_bo_frames == 1000)
+            destroy_oldest_unused_bo(dri2_surf);
+      } else {
+         dri2_surf->excess_bo_frames = 0;
       }
    }
 

src/egl/drivers/dri2/egl_dri2.h
@@ -338,6 +338,7 @@ struct dri2_egl_surface {
 
 #ifdef HAVE_DRM_PLATFORM
    struct gbm_dri_surface *gbm_surf;
+   unsigned excess_bo_frames;
 #endif
 
 #if defined(HAVE_WAYLAND_PLATFORM) || defined(HAVE_DRM_PLATFORM)

commit c0119e6e6cbbaa77896a81539d87b20c0b13cdf0

egl/gbm: Do not destroy BO of current front buffer
It's not always locked. If it's not, and we destroy its BO,
lock_front_buffer will set dri2_surf->current->locked = true but then
return NULL, so release_buffer will never be called, preventing the
buffer from being used again.

Fixes: 4a347fb78823 ("egl/gbm: Destroy excess BOs")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41845>

src/egl/drivers/dri2/platform_drm.c
@@ -224,7 +224,8 @@ destroy_oldest_unused_bo(struct dri2_egl_surface *dri2_surf)
 
    for (unsigned i = 0; i < ARRAY_SIZE(dri2_surf->color_buffers); i++) {
       if (dri2_surf->color_buffers[i].locked ||
-          dri2_surf->back == &dri2_surf->color_buffers[i])
+          dri2_surf->back == &dri2_surf->color_buffers[i] ||
+          dri2_surf->current == &dri2_surf->color_buffers[i])
          continue;
 
       if (!max_age || dri2_surf->color_buffers[i].age > max_age) {