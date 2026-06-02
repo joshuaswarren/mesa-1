joshuaswarren/mesa-1

commit fed7a6c4adae1c586c657419aad04d4677de496e

tu: Implement VK_EXT_device_memory_report
This is a minimal implementation reporting only device memory allocations
and frees.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14687
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39406>

src/freedreno/vulkan/tu_device.cc
@@ -23,6 +23,7 @@
 #include "util/os_misc.h"
 #include "util/u_process.h"
 #include "vk_android.h"
+#include "vk_debug_utils.h"
 #include "vk_shader_module.h"
 #include "vk_sampler.h"
 #include "vk_util.h"
@@ -278,6 +279,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .EXT_descriptor_buffer = true,
       .EXT_descriptor_indexing = true,
       .EXT_device_address_binding_report = true,
+      .EXT_device_memory_report = true,
 #ifdef VK_USE_PLATFORM_DISPLAY_KHR
       .EXT_display_control = true,
 #endif
@@ -636,6 +638,9 @@ tu_get_features(struct tu_physical_device *pdevice,
 
    /* VK_EXT_device_address_binding_report */
    features->reportAddressBinding = true;
+
+   /* VK_EXT_device_memory_report */
+   features->deviceMemoryReport = true;
 
    /* VK_EXT_extended_dynamic_state */
    features->extendedDynamicState = true;
@@ -3441,6 +3446,40 @@ tu_destroy_memory(struct tu_device *device,
    vk_object_free(&device->vk, NULL, mem);
 }
 
+static void
+tu_memory_emit_report(struct tu_device *device,
+                      const struct tu_device_memory *mem,
+                      const VkMemoryAllocateInfo *alloc_info,
+                      VkResult result)
+{
+   if (likely(!device->vk.memory_reports))
+      return;
+
+   if (result != VK_SUCCESS) {
+      vk_emit_device_memory_report(
+         &device->vk, VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_ALLOCATION_FAILED_EXT,
+         /* mem_obj_id */ 0, alloc_info->allocationSize,
+         VK_OBJECT_TYPE_DEVICE_MEMORY,
+         /* obj_handle */ 0, alloc_info->memoryTypeIndex);
+      return;
+   }
+
+   VkDeviceMemoryReportEventTypeEXT type;
+   if (alloc_info) {
+      type = mem->vk.import_handle_type
+                ? VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_IMPORT_EXT
+                : VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_ALLOCATE_EXT;
+   } else {
+      type = mem->vk.import_handle_type
+                ? VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_UNIMPORT_EXT
+                : VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_FREE_EXT;
+   }
+
+   vk_emit_device_memory_report(&device->vk, type, mem->bo->unique_id,
+                                mem->bo->size, VK_OBJECT_TYPE_DEVICE_MEMORY,
+                                (uintptr_t)(mem), mem->vk.memory_type_index);
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL
 tu_AllocateMemory(VkDevice _device,
                   const VkMemoryAllocateInfo *pAllocateInfo,
@@ -3553,6 +3592,7 @@ tu_AllocateMemory(VkDevice _device,
 
    if (result != VK_SUCCESS) {
       vk_device_memory_destroy(&device->vk, pAllocator, &mem->vk);
+      tu_memory_emit_report(device, /* mem */ NULL, pAllocateInfo, result);
       return result;
    }
 
@@ -3565,6 +3605,8 @@ tu_AllocateMemory(VkDevice _device,
    }
 
    TU_RMV(heap_create, device, pAllocateInfo, mem);
+
+   tu_memory_emit_report(device, mem, pAllocateInfo, VK_SUCCESS);
 
    *pMem = tu_device_memory_to_handle(mem);
 
@@ -3630,6 +3672,8 @@ tu_FreeMemory(VkDevice _device,
       return;
 
    TU_RMV(resource_destroy, device, mem);
+
+   tu_memory_emit_report(device, mem, /* alloc_info */ NULL, VK_SUCCESS);
 
    _tu_destroy_memory(device, mem);
 

commit da00ed5497aad9bd6bb078a2f385aee9d03e1f23

tu: fix to report device memory with heapIndex
The report data requires VkDeviceMemoryReportCallbackDataEXT::heapIndex
instead of memory type index. Meanwhile, turnip only supports a single
tu_memory_heap and doesn't have a type-to-heap mapping. So here we fix
to report constant 0 for heapIndex.

Fixes: fed7a6c4ada ("tu: Implement VK_EXT_device_memory_report")
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41974>

src/freedreno/vulkan/tu_device.cc
@@ -3532,7 +3532,7 @@ tu_memory_emit_report(struct tu_device *device,
          &device->vk, VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_ALLOCATION_FAILED_EXT,
          /* mem_obj_id */ 0, alloc_info->allocationSize,
          VK_OBJECT_TYPE_DEVICE_MEMORY,
-         /* obj_handle */ 0, alloc_info->memoryTypeIndex);
+         /* obj_handle */ 0, /* heap_index */ 0);
       return;
    }
 
@@ -3549,7 +3549,7 @@ tu_memory_emit_report(struct tu_device *device,
 
    vk_emit_device_memory_report(&device->vk, type, mem->bo->unique_id,
                                 mem->bo->size, VK_OBJECT_TYPE_DEVICE_MEMORY,
-                                (uintptr_t)(mem), mem->vk.memory_type_index);
+                                (uintptr_t)(mem), /* heap_index */ 0);
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL