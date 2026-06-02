joshuaswarren/mesa-1

commit 1329e4adbb3da26109926796188ecd655b43156f

nvk: Advertises VK_EXT_mesh_shader
Signed-off-by: Mary Guillemard <mary@mary.zone>
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Tested-by: Thomas H.P. Andersen <phomes@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27196>

src/nouveau/vulkan/nvk_physical_device.c
@@ -257,6 +257,7 @@ nvk_get_device_extensions(const struct nvk_instance *instance,
       .EXT_legacy_vertex_attributes = true,
       .EXT_line_rasterization = true,
       .EXT_load_store_op_none = true,
+      .EXT_mesh_shader = info->cls_eng3d >= TURING_A,
       .EXT_map_memory_placed = true,
       .EXT_memory_budget = true,
       .EXT_multi_draw = true,
@@ -692,6 +693,14 @@ nvk_get_device_features(const struct nv_device_info *info,
       .memoryMapRangePlaced = false,
       .memoryUnmapReserve = true,
 
+      /* VK_EXT_mesh_shader */
+      .taskShader = info->cls_eng3d >= TURING_A,
+      .meshShader = info->cls_eng3d >= TURING_A,
+      .multiviewMeshShader = info->cls_eng3d >= TURING_A,
+      .primitiveFragmentShadingRateMeshShader = info->cls_eng3d >= TURING_A,
+      /* TODO: Implement TASK_SHADER_INVOCATIONS_BIT_EXT & MESH_SHADER_INVOCATIONS_BIT_EXT */
+      .meshShaderQueries = false,
+
       /* VK_EXT_multi_draw */
       .multiDraw = true,
 
@@ -950,12 +959,9 @@ nvk_get_device_properties(const struct nvk_instance *instance,
 
       /* Vulkan 1.1 properties */
       .subgroupSize = 32,
-      .subgroupSupportedStages = VK_SHADER_STAGE_VERTEX_BIT |
-                                 VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT |
-                                 VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT |
-                                 VK_SHADER_STAGE_GEOMETRY_BIT |
-                                 VK_SHADER_STAGE_FRAGMENT_BIT |
-                                 VK_SHADER_STAGE_COMPUTE_BIT,
+      .subgroupSupportedStages =
+         VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT |
+         VK_SHADER_STAGE_MESH_BIT_EXT | VK_SHADER_STAGE_TASK_BIT_EXT,
       .subgroupSupportedOperations = VK_SUBGROUP_FEATURE_ARITHMETIC_BIT |
                                      VK_SUBGROUP_FEATURE_BALLOT_BIT |
                                      VK_SUBGROUP_FEATURE_BASIC_BIT |
@@ -1204,6 +1210,36 @@ nvk_get_device_properties(const struct nvk_instance *instance,
 
       /* VK_EXT_multi_draw */
       .maxMultiDrawCount = UINT32_MAX,
+
+      /* VK_EXT_mesh_shader */
+      .maxTaskWorkGroupTotalCount = 1 << 22,
+      .maxTaskWorkGroupCount = {1 << 22, UINT16_MAX, UINT16_MAX},
+      .maxTaskWorkGroupInvocations = 128,
+      .maxTaskWorkGroupSize = {128, 128, 128},
+      .maxTaskPayloadSize = 1 << 14,
+      .maxTaskSharedMemorySize = 1 << 15,
+      .maxTaskPayloadAndSharedMemorySize = 1 << 15,
+      .maxMeshWorkGroupTotalCount = 1 << 22,
+      .maxMeshWorkGroupCount = {1 << 22, UINT16_MAX, UINT16_MAX},
+      .maxMeshWorkGroupInvocations = 128,
+      .maxMeshWorkGroupSize = {128, 128, 128},
+      .maxMeshSharedMemorySize = 28672,
+      .maxMeshPayloadAndSharedMemorySize = 28672,
+      .maxMeshOutputMemorySize = 1 << 15,
+      .maxMeshPayloadAndOutputMemorySize = 48128,
+      .maxMeshOutputComponents = 128,
+      .maxMeshOutputVertices = 256,
+      .maxMeshOutputPrimitives = 256,
+      .maxMeshOutputLayers = 2048,
+      .maxMeshMultiviewViewCount = 4,
+      .meshOutputPerVertexGranularity = 32,
+      .meshOutputPerPrimitiveGranularity = 32,
+      .maxPreferredTaskWorkGroupInvocations = 32,
+      .maxPreferredMeshWorkGroupInvocations = 32,
+      .prefersLocalInvocationVertexOutput = false,
+      .prefersLocalInvocationPrimitiveOutput = false,
+      .prefersCompactVertexOutput = false,
+      .prefersCompactPrimitiveOutput = true,
 
       /* VK_EXT_nested_command_buffer */
       .maxCommandBufferNestingLevel = UINT32_MAX,

commit 0af4ead5c1557383807464fdb7ded0f027cc7833

nvk: Do not report task and mesh stages as supported on pre-Turing
We should not report support for subgroup ops or DGC for mesh stages on
pre-Turing.

Signed-off-by: Mary Guillemard <mary@mary.zone>
Reported-by: Georg Lehmann <dadschoorse@gmail.com>
Fixes: 1329e4adbb3 ("nvk: Advertises VK_EXT_mesh_shader")
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41962>

src/nouveau/vulkan/nvk_physical_device.c
@@ -824,6 +824,13 @@ nvk_get_device_properties(const struct nvk_instance *instance,
    uint64_t os_page_size = 4096;
    os_get_page_size(&os_page_size);
 
+   uint32_t supported_shader_stages =
+      VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT;
+
+   if (info->cls_eng3d >= TURING_A)
+      supported_shader_stages |=
+         VK_SHADER_STAGE_TASK_BIT_EXT | VK_SHADER_STAGE_MESH_BIT_EXT;
+
    *properties = (struct vk_properties) {
       .apiVersion = nvk_get_vk_version(info),
       .driverVersion = vk_get_driver_version(),
@@ -959,9 +966,7 @@ nvk_get_device_properties(const struct nvk_instance *instance,
 
       /* Vulkan 1.1 properties */
       .subgroupSize = 32,
-      .subgroupSupportedStages =
-         VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT |
-         VK_SHADER_STAGE_MESH_BIT_EXT | VK_SHADER_STAGE_TASK_BIT_EXT,
+      .subgroupSupportedStages = supported_shader_stages,
       .subgroupSupportedOperations = VK_SUBGROUP_FEATURE_ARITHMETIC_BIT |
                                      VK_SUBGROUP_FEATURE_BALLOT_BIT |
                                      VK_SUBGROUP_FEATURE_BASIC_BIT |
@@ -1166,12 +1171,9 @@ nvk_get_device_properties(const struct nvk_instance *instance,
       .supportedIndirectCommandsInputModes =
          VK_INDIRECT_COMMANDS_INPUT_MODE_VULKAN_INDEX_BUFFER_EXT |
          VK_INDIRECT_COMMANDS_INPUT_MODE_DXGI_INDEX_BUFFER_EXT,
-      .supportedIndirectCommandsShaderStages =
-         NVK_SHADER_STAGE_GRAPHICS_BITS | VK_SHADER_STAGE_COMPUTE_BIT,
-      .supportedIndirectCommandsShaderStagesPipelineBinding =
-         NVK_SHADER_STAGE_GRAPHICS_BITS | VK_SHADER_STAGE_COMPUTE_BIT,
-      .supportedIndirectCommandsShaderStagesShaderBinding =
-         NVK_SHADER_STAGE_GRAPHICS_BITS | VK_SHADER_STAGE_COMPUTE_BIT,
+      .supportedIndirectCommandsShaderStages = supported_shader_stages,
+      .supportedIndirectCommandsShaderStagesPipelineBinding = supported_shader_stages,
+      .supportedIndirectCommandsShaderStagesShaderBinding = supported_shader_stages,
       .deviceGeneratedCommandsTransformFeedback = true,
       .deviceGeneratedCommandsMultiDrawIndirectCount = info->cls_eng3d >= TURING_A,
 