joshuaswarren/mesa-1

commit a1f58b2340ae14f7e5474f6a850554b2194cd20d

nir: handle new multadd opcodes in lowerings and opts
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41165>

src/compiler/nir/nir_opt_undef.c
@@ -222,7 +222,7 @@ visit_undef_use(nir_src *src, struct visit_info *info)
          info->replace_undef_with_constant = true;
          if (nir_op_infos[alu->op].input_types[i] & nir_type_float &&
              alu->op != nir_op_fmulz &&
-             (alu->op != nir_op_ffmaz_old || i == 2) &&
+             (nir_alu_instr_is_mul_add_z(alu) || i == 2) &&
              alu->op != nir_op_pack_half_2x16_rtz_split)
             info->prefer_nan = true;
       }

src/compiler/nir/nir_lower_double_ops.c
@@ -654,6 +654,7 @@ lower_doubles_instr_to_soft(nir_builder *b, nir_alu_instr *instr,
       name = "__fmul64";
       mangled_name = "__fmul64(u641;u641;";
       break;
+   case nir_op_fmad:
    case nir_op_ffma_old:
       name = "__fmad64";
       mangled_name = "__fmad64(u641;u641;u641;";

src/compiler/nir/nir_lower_floats.c
@@ -74,6 +74,7 @@ lower_float_instr_to_soft(nir_builder *b, nir_instr *instr,
    case nir_op_fmul:
       mangled_name = "__fmul32(u1;u1;";
       break;
+   case nir_op_fmad:
    case nir_op_ffma_old:
       mangled_name = "__fmad32(u1;u1;u1;";
       break;

src/compiler/nir/nir_opt_fp_math_ctrl.c
@@ -175,9 +175,14 @@ opt_alu_fp_math_ctrl(nir_alu_instr *alu, struct opt_fp_ctrl_state *state)
          break;
       }
 
+      case nir_op_ffmaz:
+      case nir_op_fmadz:
       case nir_op_ffmaz_old:
          src_mark_preserve_sz(&alu->src[2].src, NULL);
          break;
+      case nir_op_ffma:
+      case nir_op_ffma_weak:
+      case nir_op_fmad:
       case nir_op_ffma_old:
          if ((nir_analyze_fp_class(&state->fp_class_state, alu->src[2].src.ssa) & FP_CLASS_NEG_ZERO) &&
              !nir_alu_srcs_equal(alu, alu, 0, 1)) {

src/compiler/nir/nir_opt_varyings.c
@@ -3253,7 +3253,7 @@ find_tes_triangle_interp_1fmul_2ffma(struct linkage_info *linkage, unsigned i)
       /* Reject exact ops because we are going to do an inexact transformation
        * with it.
        */
-      if (!alu || (alu->op != nir_op_fmul && alu->op != nir_op_ffma_old) ||
+      if (!alu || (alu->op != nir_op_fmul && !nir_alu_instr_is_mul_add(alu)) ||
           nir_alu_instr_is_exact(alu) ||
           !gather_fmul_tess_coord(iter->instr, alu, vertex_index,
                                   &tess_coord_swizzle, &tess_coord_used,
@@ -3263,7 +3263,7 @@ find_tes_triangle_interp_1fmul_2ffma(struct linkage_info *linkage, unsigned i)
       /* The multiplication must only be used by ffma. */
       if (alu->op == nir_op_fmul) {
          nir_alu_instr *ffma = get_single_use_as_alu(&alu->def);
-         if (!ffma || ffma->op != nir_op_ffma_old)
+         if (!nir_alu_instr_is_mul_add(ffma))
             return false;
 
          if (num_fmuls == 1)
@@ -3388,6 +3388,11 @@ can_move_alu_across_interp(struct linkage_info *linkage, nir_alu_instr *alu)
     */
    case nir_op_fmul:
    case nir_op_fmulz:
+   case nir_op_ffma:
+   case nir_op_ffma_weak:
+   case nir_op_ffmaz:
+   case nir_op_fmad:
+   case nir_op_fmadz:
    case nir_op_ffma_old:
    case nir_op_ffmaz_old:
       return GET_SRC_INTERP(alu, 0) == FLAG_INTERP_CONVERGENT ||

src/compiler/nir/nir_range_analysis.c
@@ -838,6 +838,11 @@ process_fp_query(struct analysis_state *state, struct analysis_query *aq, uint32
          return;
       case nir_op_ffma_old:
       case nir_op_ffmaz_old:
+      case nir_op_ffma:
+      case nir_op_ffma_weak:
+      case nir_op_ffmaz:
+      case nir_op_fmad:
+      case nir_op_fmadz:
       case nir_op_flrp:
          push_fp_query(state, alu->src[0].src.ssa);
          push_fp_query(state, alu->src[1].src.ssa);
@@ -1320,9 +1325,13 @@ process_fp_query(struct analysis_state *state, struct analysis_query *aq, uint32
       break;
    }
 
+   case nir_op_fmad:
+   case nir_op_fmadz:
+   case nir_op_ffma:
    case nir_op_ffma_old:
+   case nir_op_ffmaz:
    case nir_op_ffmaz_old: {
-      bool mulz = alu->op == nir_op_ffmaz_old;
+      bool mulz = nir_alu_instr_is_mul_add_z(alu);
       bool src_eq = nir_alu_srcs_equal(alu, alu, 0, 1);
       bool src_neg_eq = !nir_src_is_const(alu->src[0].src) && nir_alu_srcs_negative_equal(alu, alu, 0, 1);
       fp_class_mask r_mul = fmul_fp_class(src_res[0], src_res[1], mulz, src_eq, src_neg_eq);

commit 2be1f4b10c95c08466cd9da5cf1fcf0f2adba490

nir/opt_undef: fix prefer_nan
fossil-db (gfx1201):
Totals from 279 (0.13% of 210263) affected shaders:
Instrs: 661579 -> 653245 (-1.26%); split: -1.29%, +0.03%
CodeSize: 3612816 -> 3572600 (-1.11%); split: -1.14%, +0.03%
SpillSGPRs: 313 -> 305 (-2.56%)
Latency: 5147724 -> 5139048 (-0.17%); split: -0.18%, +0.01%
InvThroughput: 939696 -> 937981 (-0.18%); split: -0.19%, +0.00%
VClause: 14732 -> 14696 (-0.24%); split: -0.29%, +0.05%
SClause: 12517 -> 12495 (-0.18%); split: -0.19%, +0.02%
Copies: 60783 -> 60472 (-0.51%); split: -0.61%, +0.09%
Branches: 20669 -> 20488 (-0.88%); split: -1.16%, +0.28%
PreSGPRs: 14960 -> 14968 (+0.05%); split: -0.03%, +0.08%
PreVGPRs: 15948 -> 15960 (+0.08%)
VALU: 306409 -> 304055 (-0.77%); split: -0.79%, +0.02%
SALU: 134363 -> 131367 (-2.23%); split: -2.27%, +0.04%
VMEM: 21760 -> 21715 (-0.21%); split: -0.24%, +0.04%
SMEM: 21358 -> 21323 (-0.16%)
VOPD: 32352 -> 32184 (-0.52%); split: +0.02%, -0.53%

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: a1f58b2340a ("nir: handle new multadd opcodes in lowerings and opts")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42145>

src/compiler/nir/nir_opt_undef.c
@@ -222,7 +222,7 @@ visit_undef_use(nir_src *src, struct visit_info *info)
          info->replace_undef_with_constant = true;
          if (nir_op_infos[alu->op].input_types[i] & nir_type_float &&
              alu->op != nir_op_fmulz &&
-             (nir_alu_instr_is_mul_add_z(alu) || i == 2) &&
+             (!nir_alu_instr_is_mul_add_z(alu) || i == 2) &&
              alu->op != nir_op_pack_half_2x16_rtz_split)
             info->prefer_nan = true;
       }