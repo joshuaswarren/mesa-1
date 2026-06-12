joshuaswarren/mesa-1

commit d7b060146e33ab547fc93b8b184a7151326a79a3

pan/compiler: Use SHADDX instruction for i64 add
For Valhall, use SHADDX instruction for 64-bit integer addition instead
of lowering it to 32-bit operations. The instruction sequence for doing
it in 32-bit costs 3 cycles but SHADDX only takes 2 cycles to perform.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40841>

src/panfrost/compiler/bifrost/bi_ra.c
@@ -8,6 +8,7 @@
 #include "bi_builder.h"
 #include "compiler.h"
 #include "nodearray.h"
+#include "valhall.h"
 
 struct lcra_state {
    unsigned node_count;
@@ -379,6 +380,9 @@ bi_mark_interference(bi_block *block, struct lcra_state *l, uint8_t *live,
       if (aligned_sr) {
          bi_foreach_ssa_src(ins, s) {
             if (bi_count_read_registers(ins, s) >= 2)
+               l->affinity[ins->src[s].value] &= EVEN_BITS_MASK;
+            else if (s < valhall_opcodes[ins->op].nr_srcs &&
+                     va_src_info(ins->op, s).size > VA_SIZE_32)
                l->affinity[ins->src[s].value] &= EVEN_BITS_MASK;
          }
       }

src/panfrost/compiler/bifrost/bifrost_compile.c
@@ -2310,6 +2310,15 @@ bi_alu_src_index(bi_builder *b, nir_alu_src src, unsigned comps)
 {
    unsigned bitsize = nir_src_bit_size(src.src);
 
+   if (b->shader->arch >= 9 && bitsize == 64) {
+      /* For Valhall, 64-bit instructions only encode one register but will read
+       * the adjacent register that comes right after as well. Therefore we
+       * don't need to extract a single register here.
+       */
+      assert(comps == 1);
+      return bi_src_index(&src.src);
+   }
+
    /* the bi_index carries the 32-bit (word) offset separate from the
     * subword swizzle, first handle the offset */
 
@@ -3373,7 +3382,14 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
       break;
 
    case nir_op_iadd:
-      bi_iadd_to(b, nir_type_int, sz, dst, s0, s1, false);
+      if (sz == 64) {
+         assert(b->shader->arch >= 9);
+         bi_shaddx_s64_to(b, dst, s0, s1, 0);
+         bi_index dsts[4] = {bi_null(), bi_null(), bi_null(), bi_null()};
+         bi_emit_split_i32(b, dsts, dst, 2);
+         bi_cache_collect(b, dst, dsts, 2);
+      } else
+         bi_iadd_to(b, nir_type_int, sz, dst, s0, s1, false);
       break;
 
    case nir_op_iadd_sat:

src/panfrost/compiler/bifrost/bifrost_compile.h
@@ -116,7 +116,7 @@ bool valhall_can_merge_workgroups(nir_shader *nir);
                                                                                \
       .lower_doubles_options =                                                 \
          nir_lower_dmod, /* TODO: Don't lower supported 64-bit operations */   \
-      .lower_int64_options = ~0, /* TODO: Use IMULD on v7 */                   \
+      .lower_int64_options = arch >= 9 ? ~(nir_lower_iadd64) : ~0,             \
       .lower_mul_high = true,                                                  \
       .lower_fisnormal = true,                                                 \
       .lower_uadd_carry = true,                                                \

src/panfrost/compiler/bifrost/bir.c
@@ -77,6 +77,8 @@ bi_count_read_registers(const bi_instr *ins, unsigned s)
       return ins->sr_count_2; /* Dual source blending */
    else if (s == 0 && ins->op == BI_OPCODE_SPLIT_I32)
       return ins->nr_dests;
+   else if (ins->op == BI_OPCODE_SHADDX_S64 || ins->op == BI_OPCODE_SHADDX_U64)
+      return 2;
    else
       return 1;
 }
@@ -123,7 +125,9 @@ bi_count_write_registers(const bi_instr *ins, unsigned d)
       default:
          return bi_count_staging_registers(ins);
       }
-   } else if (ins->op == BI_OPCODE_SEG_ADD_I64) {
+   } else if (ins->op == BI_OPCODE_SEG_ADD_I64 ||
+              ins->op == BI_OPCODE_SHADDX_S64 ||
+              ins->op == BI_OPCODE_SHADDX_U64) {
       return 2;
    } else if (ins->op == BI_OPCODE_TEXC_DUAL && d == 1) {
       return ins->sr_count_2;

src/panfrost/compiler/bifrost/valhall/ISA.xml
@@ -2626,7 +2626,7 @@
     <src widen="true">B</src>
   </group>
 
-  <group name="SHADDX" title="Shift, extend, and 64-bit add" dests="1" unused="true" unit="CVT">
+  <group name="SHADDX" title="Shift, extend, and 64-bit add" dests="1" unit="CVT">
     <desc>
       Sign or zero extend B to 64-bits, left-shift by `shift`, and add the
       64-bit value A. These instructions accelerate address arithmetic, but may

src/panfrost/compiler/bifrost/valhall/va_lower_split_64bit.c
@@ -26,14 +26,18 @@ lower_split_src(bi_context *ctx, bi_instr *I, unsigned s, bi_instr** lut)
       return;
    }
 
-   /* Check if the source regs are already coming from a split. */
+   /* Check if the source regs are already coming from a split/collect pair. */
    bi_index *src_a = &I->src[s];
    bi_index *src_b = &I->src[s + 1];
    if (bi_is_ssa(*src_a) && bi_is_ssa(*src_b)) {
       bi_instr *src_ins_a = lut[src_a->value];
       bi_instr *src_ins_b = lut[src_b->value];
-      if (src_ins_a->op == BI_OPCODE_SPLIT_I32 && src_ins_a == src_ins_b)
-         return;
+      if (src_ins_a->op == BI_OPCODE_SPLIT_I32 && src_ins_a == src_ins_b) {
+         bi_index split_src = src_ins_a->src[0];
+         if (!bi_is_ssa(split_src) ||
+             lut[split_src.value]->op == BI_OPCODE_COLLECT_I32)
+            return;
+      }
    }
 
    /* Allocate temporary before the instruction */
@@ -76,7 +80,9 @@ va_lower_split_64bit(bi_context *ctx)
 
          struct va_src_info info = va_src_info(I->op, s);
 
-         if (info.size == VA_SIZE_64)
+         /* Only split if the instruction expects 64-bit inputs as two separate
+          * sources. */
+         if (info.size == VA_SIZE_64 && bi_count_read_registers(I, s) == 1)
             lower_split_src(ctx, I, s, lut);
       }
    }

src/panfrost/compiler/bifrost/valhall/va_pack.c
@@ -325,6 +325,25 @@ va_pack_widen(const bi_instr *I, enum bi_swizzle swz, enum va_size size)
       default:
          invalid_instruction(I, "32-bit widen");
       }
+   } else if (size == VA_SIZE_64) {
+      switch (swz) {
+      case BI_SWIZZLE_H01:
+         return VA_SWIZZLES_64_BIT_NONE;
+      case BI_SWIZZLE_H0:
+         return VA_SWIZZLES_64_BIT_H0;
+      case BI_SWIZZLE_H1:
+         return VA_SWIZZLES_64_BIT_H1;
+      case BI_SWIZZLE_B0:
+         return VA_SWIZZLES_64_BIT_B0;
+      case BI_SWIZZLE_B1:
+         return VA_SWIZZLES_64_BIT_B1;
+      case BI_SWIZZLE_B2:
+         return VA_SWIZZLES_64_BIT_B2;
+      case BI_SWIZZLE_B3:
+         return VA_SWIZZLES_64_BIT_B3;
+      default:
+         invalid_instruction(I, "64-bit widen");
+      }
    } else {
       invalid_instruction(I, "type size for widen");
    }

src/panfrost/compiler/bifrost/valhall/valhall.c.py
@@ -34,8 +34,6 @@ SKIP = set([
         "ISUB.u64",
         "ISUB.s64",
         "IMULD.u64",
-        "SHADDX.u64",
-        "SHADDX.s64",
         "IMULD.u64",
         "LSHIFT_AND.i64",
         "RSHIFT_AND.i64",

commit 4a5e9c5f2f1c0d93365b127a00bb056fbc380fd5

pan/compiler: fix spilling for 64-bit values
For the BIR-compiler, 64-bit values were not properly tracked in the
spill logic and PHIs were always assumed to be 32-bits. This could
create issues were only one half of the value was reloaded or spills
would overlap each other leading to garbage values. This patch fixes
these issues by keeping track of how many words each value needs. Also,
it adds a constraint for SHADD sources where it splits and collects them
right before the SHADD instruction itself to make it easier for RA to
handle the register pairs.

Fixes: d7b060146e3 ("pan/compiler: Use SHADDX instruction for i64 add")

Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42080>

src/panfrost/compiler/bifrost/bi_lower_spill.c
@@ -51,13 +51,24 @@ lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
     * provide the actual TLS offset as bi_index::value.
     */
 
+   unsigned words = 1;
+
+   if (I->dest[0].memory)
+      words = MAX2(words, ctx->sizes[I->dest[0].value]);
+
+   bi_foreach_src(I, s) {
+      if (I->src[s].memory)
+         words = MAX2(words, ctx->sizes[I->src[s].value]);
+   }
+
+   assert(words <= 4);
+   I->table = words;
+
    if (I->dest[0].memory) {
       const bi_index dst = I->dest[0];
-      /* bi_repair_ssa could make PHIs for MEMMOV sources which could be
-       * wider. But, those should all get eliminated as trivial because they
-       * are only defined once so they would all look like mX = PHI mX, mX.
+      /* Preserve the memory value long enough to look up its assigned TLS
+       * slot. bi_out_of_ssa uses I->table to lower all words of the PHI.
        */
-      assert(ctx->sizes[dst.value] == 1);
       I->dest[0].value = tls_base + ctx->tls_loc[dst.value];
    }
 
@@ -67,7 +78,6 @@ lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
          continue;
 
       assert(ctx->tls_loc[src.value] != UINT32_MAX && "Undefined source");
-      assert(ctx->sizes[src.value] == 1);
       I->src[s].value = tls_base + ctx->tls_loc[src.value];
    }
 }
@@ -83,8 +93,12 @@ assign_tls_locations(struct lower_spill_ctx *ctx) {
          assert(I->op == BI_OPCODE_MEMMOV || I->op == BI_OPCODE_PHI);
          assert(ctx->tls_loc[dst.value] == UINT32_MAX && "Broken SSA");
 
+         unsigned size = ctx->sizes[dst.value];
+         unsigned alignment = (size <= 1) ? 4 : (size == 2) ? 8 : 16;
+
+         ctx->spill_count = ALIGN_POT(ctx->spill_count, alignment);
          ctx->tls_loc[dst.value] = ctx->spill_count;
-         ctx->spill_count += 4 * ctx->sizes[dst.value];
+         ctx->spill_count += 4 * size;
       }
    }
 }

src/panfrost/compiler/bifrost/bi_ra.c
@@ -733,7 +733,7 @@ bi_compute_reg_alignment(bi_context *ctx)
    bi_foreach_instr_global(ctx, I) {
       bi_foreach_ssa_dest(I, d) {
          idx = I->dest[d].value;
-         count = bi_count_write_registers(I, d);
+         count = bi_count_write_registers(I, d) + I->dest[d].offset;
          if (count == 3) count = 4;
          assert(idx < ctx->ssa_alloc);
          ctx->reg_alignment[idx] = MAX2(count*4, ctx->reg_alignment[idx]);
@@ -755,18 +755,16 @@ bi_spill_register(bi_context *ctx, bi_index index, uint32_t offset,
                   bi_index spill_point)
 {
    bi_builder b = {.shader = ctx};
-   unsigned alignment = 4;
-   unsigned channels = 0;
+   if (!ctx->reg_alignment)
+      bi_compute_reg_alignment(ctx);
+   assert(index.value < ctx->ssa_alloc);
+   unsigned channels = ctx->reg_alignment[index.value] / 4;
+   assert(channels > 0);
+   unsigned alignment = (ctx->arch >= 9) ? ctx->reg_alignment[index.value] : 4;
 
    /* first figure out the alignment we will need, based on the
     * maximum count we see
     */
-   if (ctx->arch >= 9) {
-      if (!ctx->reg_alignment)
-         bi_compute_reg_alignment(ctx);
-      assert(index.value < ctx->ssa_alloc);
-      alignment = ctx->reg_alignment[index.value];
-   }
    offset = ALIGN_POT(offset, alignment);
 
    /* Spill after every store, fill before every load */
@@ -782,20 +780,18 @@ bi_spill_register(bi_context *ctx, bi_index index, uint32_t offset,
             if (!bi_is_equiv(I->dest[d], index))
                continue;
 
+            I->no_spill = true;
             unsigned count = bi_count_write_registers(I, d);
             unsigned extra = I->dest[d].offset;
+            bi_index src = index;
+            src.offset = extra;
 
-            channels = MAX2(channels, extra + count);
-            I->no_spill = true;
+            b.cursor = bi_after_instr(I);
+            bi_store_tl(&b, count * 32, src, offset + (extra * 4));
 
-            if (channels == extra + count) {
-               b.cursor = bi_after_instr(I);
-               bi_store_tl(&b, channels * 32, index, offset);
-
-               ctx->spills++;
-               /* Don't disable filling if spill_point is before index. */
-               fill = found_spill_point;
-            }
+            ctx->spills++;
+            /* Don't disable filling if spill_point is before index. */
+            fill = found_spill_point;
          }
 
          if (bi_has_arg(I, index) && fill) {
@@ -980,6 +976,20 @@ squeeze_index(bi_context *ctx)
    ralloc_free(map);
 }
 
+static void
+bi_mov_words_to(bi_builder *b, bi_index dst, bi_index src, unsigned words)
+{
+   assert(words >= 1 && words <= 4);
+
+   for (unsigned i = 0; i < words; ++i) {
+      bi_index word_dst = dst, word_src = src;
+      word_dst.offset += i;
+      word_src.offset += i;
+
+      bi_mov_i32_to(b, word_dst, word_src);
+   }
+}
+
 /*
  * Brainless out-of-SSA pass. The eventual goal is to go out-of-SSA after RA and
  * coalesce implicitly with biased colouring in a tree scan allocator. For now,
@@ -997,6 +1007,9 @@ bi_out_of_ssa(bi_context *ctx)
       bi_foreach_instr_in_block_safe(block, I) {
          if (I->op != BI_OPCODE_PHI)
             break;
+
+         unsigned words = I->table ? I->table : 1;
+         assert(words >= 1 && words <= 4);
 
          /* Assign a register for the phi */
          bi_index reg = bi_temp(ctx);
@@ -1016,12 +1029,17 @@ bi_out_of_ssa(bi_context *ctx)
 
             if (I->src[i].memory)
                /* spilled register, need to un-spill */
-               bi_load_tl(&b, 32, reg, I->src[i].value);
-            else if (ctx->arch >= 9 && I->src[i].type == BI_INDEX_CONSTANT)
+               bi_load_tl(&b, words * 32, reg, I->src[i].value);
+            else if (ctx->arch >= 9 && I->src[i].type == BI_INDEX_CONSTANT) {
                /* MOV of immediate needs lowering on Valhall */
-               bi_iadd_imm_i32_to(&b, reg, zero, I->src[i].value);
-            else
-               bi_mov_i32_to(&b, reg, I->src[i]);
+               for (unsigned w = 0; w < words; ++w) {
+                  bi_index word_reg = reg;
+                  word_reg.offset += w;
+                  bi_iadd_imm_i32_to(&b, word_reg, zero,
+                                     w == 0 ? I->src[i].value : 0);
+               }
+            } else
+               bi_mov_words_to(&b, reg, I->src[i], words);
          }
 
          /* Replace the phi with a move */
@@ -1029,13 +1047,13 @@ bi_out_of_ssa(bi_context *ctx)
          bi_builder b = bi_init_builder(ctx, bi_before_instr(I));
          if (I->dest[0].memory) {
             /* dest was spilled to memory */
-            bi_store_tl(&b, 32, reg, I->dest[0].value);
+            bi_store_tl(&b, words * 32, reg, I->dest[0].value);
             allow_propagate = false;
          } else if (ctx->arch >= 9 && reg.type == BI_INDEX_CONSTANT)
             /* MOV of immediate needs lowering on Valhall */
             bi_iadd_imm_i32_to(&b, I->dest[0], zero, reg.value);
          else
-            bi_mov_i32_to(&b, I->dest[0], reg);
+            bi_mov_words_to(&b, I->dest[0], reg, words);
          bi_remove_instruction(I);
 
          /* Propagate that move within the block. The destination

src/panfrost/compiler/bifrost/bifrost_compile.c
@@ -235,6 +235,24 @@ bi_collect_v2i32(bi_builder *b, bi_index s0, bi_index s1)
    return dst;
 }
 
+static void
+bi_split_i64_for_shadd(bi_builder *b, bi_index src, bi_index out[2])
+{
+   bi_index tmp[4] = {bi_null(), bi_null(), bi_null(), bi_null()};
+   bi_emit_split_i32(b, tmp, src, 2);
+   out[0] = tmp[0];
+   out[1] = tmp[1];
+}
+
+static bi_index
+bi_materialize_i64_for_shadd(bi_builder *b, bi_index src)
+{
+   bi_index components[2] = {bi_null(), bi_null()};
+   bi_split_i64_for_shadd(b, src, components);
+
+   return bi_collect_v2i32(b, components[0], components[1]);
+}
+
 static inline bi_instr *
 bi_f32_to_f16_to(bi_builder *b, bi_index dest, bi_index src)
 {
@@ -3447,6 +3465,8 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
    case nir_op_iadd:
       if (sz == 64) {
          assert(b->shader->arch >= 9);
+         s0 = bi_materialize_i64_for_shadd(b, s0);
+         s1 = bi_materialize_i64_for_shadd(b, s1);
          bi_shaddx_s64_to(b, dst, s0, s1, 0);
          bi_index dsts[4] = {bi_null(), bi_null(), bi_null(), bi_null()};
          bi_emit_split_i32(b, dsts, dst, 2);