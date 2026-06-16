joshuaswarren/mesa-1

commit 2c09ab7928e9b6c937cbba3fbb0f53bb0d3d6209

aco/assembler: emit block_kind_loop_latch before the loop header
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39519>

src/amd/compiler/aco_assembler.cpp
@@ -36,8 +36,8 @@ struct asm_context {
    std::map<unsigned, constaddr_info> constaddrs;
    std::map<unsigned, constaddr_info> resumeaddrs;
    std::vector<struct aco_symbol>* symbols;
-   uint32_t loop_header = -1u;
-   uint32_t loop_exit = 0u;
+   uint32_t loop_latch = -1u;
+   uint32_t loop_exit = -1u;
    const int16_t* opcode;
    // TODO: keep track of branch instructions referring blocks
    // and, when emitting the block, correct the offset in instr
@@ -1421,6 +1421,8 @@ emit_instruction(asm_context& ctx, std::vector<uint32_t>& out, Instruction* inst
 void
 emit_block(asm_context& ctx, std::vector<uint32_t>& out, Block& block)
 {
+   block.offset = out.size();
+
    for (aco_ptr<Instruction>& instr : block.instructions) {
 #if 0
       int start_idx = out.size();
@@ -1714,16 +1716,16 @@ void
 align_block(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
 {
    /* Align the previous loop. */
-   if (ctx.loop_header != -1u &&
-       block.loop_nest_depth < ctx.program->blocks[ctx.loop_header].loop_nest_depth) {
+   if (ctx.loop_latch != -1u &&
+       block.loop_nest_depth < ctx.program->blocks[ctx.loop_latch].loop_nest_depth) {
       assert(ctx.loop_exit != -1u);
-      Block& loop_header = ctx.program->blocks[ctx.loop_header];
+      Block& loop_latch = ctx.program->blocks[ctx.loop_latch];
       Block& loop_exit = ctx.program->blocks[ctx.loop_exit];
-      ctx.loop_header = -1u;
+      ctx.loop_latch = -1u;
       ctx.loop_exit = -1u;
       std::vector<uint32_t> nops;
 
-      const unsigned loop_num_cl = DIV_ROUND_UP(block.offset - loop_header.offset, 16);
+      const unsigned loop_num_cl = DIV_ROUND_UP(code.size() - loop_latch.offset, 16);
 
       /* On GFX10.3+, change the prefetch mode if the loop fits into 2 or 3 cache lines.
        * Don't use the s_inst_prefetch instruction on GFX10 as it might cause hangs.
@@ -1733,11 +1735,11 @@ align_block(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
                                    loop_num_cl <= 3;
 
       if (change_prefetch) {
-         Builder bld(ctx.program, &ctx.program->blocks[loop_header.linear_preds[0]]);
+         Builder bld(ctx.program, &ctx.program->blocks[loop_latch.linear_preds[0]]);
          int16_t prefetch_mode = loop_num_cl == 3 ? 0x1 : 0x2;
          Instruction* instr = bld.sopp(aco_opcode::s_inst_prefetch, prefetch_mode);
          emit_instruction(ctx, nops, instr);
-         insert_code(ctx, code, loop_header.offset, nops.size(), nops.data());
+         insert_code(ctx, code, loop_latch.offset, nops.size(), nops.data());
 
          /* Change prefetch mode back to default (0x3) at the loop exit. */
          bld.reset(&loop_exit.instructions, loop_exit.instructions.begin());
@@ -1749,19 +1751,19 @@ align_block(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
          }
       }
 
-      const unsigned loop_start_cl = loop_header.offset >> 4;
-      const unsigned loop_end_cl = (block.offset - 1) >> 4;
+      const unsigned loop_start_cl = loop_latch.offset >> 4;
+      const unsigned loop_end_cl = (code.size() - 1) >> 4;
 
       /* Align the loop if it fits into the fetched cache lines or if we can
        * reduce the number of cache lines with less than 8 NOPs.
        */
       const bool align_loop = loop_end_cl - loop_start_cl >= loop_num_cl &&
-                              (loop_num_cl == 1 || change_prefetch || loop_header.offset % 16 > 8);
+                              (loop_num_cl == 1 || change_prefetch || loop_latch.offset % 16 > 8);
 
       if (align_loop) {
          nops.clear();
-         nops.resize(16 - (loop_header.offset % 16), 0xbf800000u);
-         insert_code(ctx, code, loop_header.offset, nops.size(), nops.data());
+         nops.resize(16 - (loop_latch.offset % 16), 0xbf800000u);
+         insert_code(ctx, code, loop_latch.offset, nops.size(), nops.data());
       }
    }
 
@@ -1771,19 +1773,27 @@ align_block(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
        * Also ignore loops without back-edge.
        */
       if (block.linear_preds.size() > 1) {
-         ctx.loop_header = block.index;
+         ctx.loop_latch = block.index;
          ctx.loop_exit = -1u;
       }
+   }
+
+   if (ctx.loop_latch != -1u && (block.kind & block_kind_loop_latch)) {
+      /* The loop latch is the block where where the loop re-enters.
+       * It might be the loop header or a block from the end of the loop.
+       */
+      if (ctx.program->blocks[ctx.loop_latch].loop_nest_depth == block.loop_nest_depth)
+         ctx.loop_latch = block.index;
    }
 
    /* Blocks with block_kind_loop_exit might be eliminated after jump threading,
     * so we instead find loop exits using the successors when in loop_nest_depth.
     * This works, because control flow always re-converges after loops.
     */
-   if (ctx.loop_header != -1u && ctx.loop_exit == -1u) {
+   if (ctx.loop_latch != -1u && ctx.loop_exit == -1u) {
       for (uint32_t succ_idx : block.linear_succs) {
          Block& succ = ctx.program->blocks[succ_idx];
-         if (succ.loop_nest_depth < ctx.program->blocks[ctx.loop_header].loop_nest_depth)
+         if (succ.loop_nest_depth < ctx.program->blocks[ctx.loop_latch].loop_nest_depth)
             ctx.loop_exit = succ_idx;
       }
    }
@@ -1793,6 +1803,28 @@ align_block(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
       size_t cache_aligned = align(code.size(), 16);
       code.resize(cache_aligned, 0xbf800000u); /* s_nop 0 */
       block.offset = code.size();
+   }
+}
+
+void
+emit_loop_latch(asm_context& ctx, std::vector<uint32_t>& code, Block& block)
+{
+   /* No actual loop. */
+   if (block.linear_preds.size() == 1)
+      return;
+
+   /* The loop header is also the loop latch. */
+   if (block.kind & block_kind_loop_latch)
+      return;
+
+   unsigned i = block.index;
+   while (ctx.program->blocks[i].loop_nest_depth >= block.loop_nest_depth) {
+      Block& latch_block = ctx.program->blocks[i++];
+      if (latch_block.loop_nest_depth == block.loop_nest_depth &&
+          (latch_block.kind & block_kind_loop_latch)) {
+         emit_block(ctx, code, latch_block);
+         break;
+      }
    }
 }
 
@@ -1815,8 +1847,13 @@ emit_program(Program* program, std::vector<uint32_t>& code, std::vector<struct a
       fix_exports(ctx, code, program);
 
    for (Block& block : program->blocks) {
-      block.offset = code.size();
       align_block(ctx, code, block);
+
+      if (block.kind & block_kind_loop_header)
+         emit_loop_latch(ctx, code, block);
+      else if (block.kind & block_kind_loop_latch)
+         continue;
+
       emit_block(ctx, code, block);
    }
 

commit e4b682c242e1d26e9929c5045e2d5e6716808ae0

aco/assembler: chain branches in emit order
Blocks aren't emitted in the order of program->blocks anymore,
use the real emitted order when chaining branches.

This fixes assumptions about block offsets only increasing.

Fixes: 2c09ab7928e ("aco/assembler: emit block_kind_loop_latch before the loop header")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15628
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42246>

src/amd/compiler/aco_assembler.cpp
@@ -32,6 +32,7 @@ struct branch_info {
 struct asm_context {
    Program* program;
    enum amd_gfx_level gfx_level;
+   std::vector<unsigned> block_emit_order;
    std::vector<branch_info> branches;
    std::map<unsigned, constaddr_info> constaddrs;
    std::map<unsigned, constaddr_info> resumeaddrs;
@@ -1425,6 +1426,7 @@ void
 emit_block(asm_context& ctx, std::vector<uint32_t>& out, Block& block)
 {
    block.offset = out.size();
+   ctx.block_emit_order.push_back(block.index);
 
    for (aco_ptr<Instruction>& instr : block.instructions) {
 #if 0
@@ -1578,9 +1580,9 @@ chain_branches(asm_context& ctx, std::vector<uint32_t>& out, branch_info& branch
    const unsigned lower_end = MAX2(ctx.program->blocks[target].offset, branch.pos) - half_dist;
    const unsigned lower_start = lower_end - half_dist;
    unsigned insert_at = 0;
-   for (unsigned i = 0; i < ctx.program->blocks.size() - 1; i++) {
-      Block& block = ctx.program->blocks[i];
-      Block& next = ctx.program->blocks[i + 1];
+   for (unsigned i = 0; i < ctx.block_emit_order.size() - 1; i++) {
+      Block& block = ctx.program->blocks[ctx.block_emit_order[i]];
+      Block& next = ctx.program->blocks[ctx.block_emit_order[i + 1]];
       if (next.offset >= lower_end)
          break;
       if (next.offset < upper_start || (next.offset > upper_end && next.offset < lower_start))
@@ -1602,21 +1604,22 @@ chain_branches(asm_context& ctx, std::vector<uint32_t>& out, branch_info& branch
    /* If we didn't find a suitable insertion point, split the existing code. */
    if (insert_at == 0) {
       /* Find the last block that is still within reach. */
+      unsigned idx = 0;
       unsigned insertion_block_idx = 0;
-      unsigned next_block = 0;
-      while (ctx.program->blocks[next_block + 1].offset < upper_end) {
-         if (!ctx.program->blocks[next_block].instructions.empty())
-            insertion_block_idx = next_block;
-         next_block++;
+      while (ctx.program->blocks[ctx.block_emit_order[idx + 1]].offset < upper_end) {
+         if (!ctx.program->blocks[ctx.block_emit_order[idx]].instructions.empty())
+            insertion_block_idx = ctx.block_emit_order[idx];
+         idx++;
       }
 
-      insert_at = ctx.program->blocks[next_block].offset;
+      Block& next_block = ctx.program->blocks[ctx.block_emit_order[idx]];
+      insert_at = next_block.offset;
       if (insert_at < upper_start) {
          /* Ensure some forward progress by splitting the block if necessary. */
-         auto it = ctx.program->blocks[next_block].instructions.begin();
+         auto it = next_block.instructions.begin();
          int skip = 0;
          while (skip-- > 0 || insert_at < upper_start) {
-            assert(it != ctx.program->blocks[next_block].instructions.end());
+            assert(it != next_block.instructions.end());
             Instruction* instr = (it++)->get();
             if (instr->isSOPP()) {
                if (instr->opcode == aco_opcode::s_clause)
@@ -1636,11 +1639,11 @@ chain_branches(asm_context& ctx, std::vector<uint32_t>& out, branch_info& branch
 
          /* If the insertion point is in the middle of the block, insert the branch instructions
           * into that block instead. */
-         bld.reset(&ctx.program->blocks[next_block].instructions, it);
+         bld.reset(&next_block.instructions, it);
       } else {
          /* Insert the additional branches at the end of the previous non-empty block. */
-         bld.reset(&ctx.program->blocks[insertion_block_idx].instructions);
-         skip_branch_target = next_block;
+         bld.reset(&ctx.program->blocks[ctx.block_emit_order[insertion_block_idx]].instructions);
+         skip_branch_target = next_block.index;
       }
 
       /* Since we insert a branch into existing code, mitigate LdsBranchVmemWARHazard on GFX10. */