joshuaswarren/mesa-1

commit 156295f1c56a7d91e3374866e479de6533415ee9

etnaviv: blt: Support 128 bit blit operations
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37597>

src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -643,15 +643,17 @@ etna_try_blt_blit(struct pipe_context *pctx,
       return false;
    }
 
+   const enum pipe_format fmt = translate_format_128bit_to_64bit(blit_info->dst.format);
+
    /* try to find a exact format match first */
-   uint32_t format = translate_blt_format(blit_info->dst.format);
+   uint32_t format = translate_blt_format(fmt);
    /* When not resolving MSAA, but only doing a layout conversion, we can get
     * away with a fallback format of matching size.
     */
    if (format == ETNA_NO_MATCH && !downsample_x && !downsample_y)
-      format = etna_compatible_blt_format(blit_info->dst.format);
+      format = etna_compatible_blt_format(fmt);
    if (format == ETNA_NO_MATCH) {
-      DBG("format not supported: %s", util_format_short_name(blit_info->dst.format));
+      DBG("format not supported: %s", util_format_short_name(fmt));
       return false;
    }
 
@@ -769,6 +771,13 @@ etna_try_blt_blit(struct pipe_context *pctx,
       etna_set_state(ctx->stream, VIVS_GL_FLUSH_CACHE, 0x00000c23);
       etna_set_state(ctx->stream, VIVS_TS_FLUSH_CACHE, 0x00000001);
       emit_blt_copyimage(ctx->stream, &op);
+
+      if (format_is_128bit(blit_info->dst.format)) {
+         op.src.addr.offset += src_lev->layer_stride;
+         op.dest.addr.offset += dst_lev->layer_stride;
+
+         emit_blt_copyimage(ctx->stream, &op);
+      }
    }
 
    /* Make FE wait for BLT, in case we want to do something with the image next.

commit 3da15985ad9c59868c7714fa26cddaf3ac5f1d53

etnaviv: blt: Use block-layout offset for 128-bit second-plane blit
The 128-bit emulation stores all RG halves in the first half of the BO
and all BA halves in the second half. The sampler descriptors, the CPU
upload and the BLT clear all compute the second plane as
(size * depth) / 2.

etna_try_blt_blit(..) advanced source and destination by layer_stride
instead, an interleaved layout nothing else uses. For single-layer 2D
targets both formulas coincide, so plain blits worked, but per-layer
blits of a multi-layer 128-bit array texture corrupted the BA half of
every layer. Use the same (size * depth) / 2 offset as the rest of the
emulation.

Fixes: 156295f1c56 ("etnaviv: blt: Support 128 bit blit operations")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Daniel Lang <dalang@gmx.at>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42201>

src/gallium/drivers/etnaviv/etnaviv_blt.c
@@ -951,8 +951,8 @@ etna_try_blt_blit(struct pipe_context *pctx,
       emit_blt_copyimage(ctx->stream, &op);
 
       if (format_is_128bit(blit_info->dst.format)) {
-         op.src.addr.offset += src_lev->layer_stride;
-         op.dest.addr.offset += dst_lev->layer_stride;
+         op.src.addr.offset += etna_resource_level_second_plane_offset(src_lev);
+         op.dest.addr.offset += etna_resource_level_second_plane_offset(dst_lev);
 
          emit_blt_copyimage(ctx->stream, &op);
       }