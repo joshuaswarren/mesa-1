joshuaswarren/mesa-1

commit 6e3a9833e4924fe286cda9bdcb405e228413e63a

anv: add lowering of descriptor heap intrinsics
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39478>

src/intel/vulkan/anv_shader_compile.c
@@ -728,25 +728,30 @@ lookup_ycbcr_conversion(const void *_stage, uint32_t set,
 {
    const struct vk_shader_compile_info *stage = _stage;
 
-   assert(set < MAX_SETS);
-   const struct anv_descriptor_set_layout *set_layout =
-      container_of(stage->set_layouts[set],
-                   struct anv_descriptor_set_layout, vk);
+   if (set == VK_NIR_YCBCR_SET_IMMUTABLE_SAMPLERS) {
+      assert(binding < stage->embedded_sampler_count);
+      return &stage->embedded_samplers[binding].ycbcr_conversion;
+   } else {
+      assert(set < MAX_SETS);
+      const struct anv_descriptor_set_layout *set_layout =
+         container_of(stage->set_layouts[set],
+                      struct anv_descriptor_set_layout, vk);
 
-   assert(binding < set_layout->binding_count);
-   const struct anv_descriptor_set_binding_layout *bind_layout =
-      &set_layout->binding[binding];
+      assert(binding < set_layout->binding_count);
+      const struct anv_descriptor_set_binding_layout *bind_layout =
+         &set_layout->binding[binding];
 
-   if (bind_layout->samplers == NULL)
-      return NULL;
+      if (bind_layout->samplers == NULL)
+         return NULL;
 
-   array_index = MIN2(array_index, bind_layout->array_size - 1);
+      array_index = MIN2(array_index, bind_layout->array_size - 1);
 
-   const struct anv_descriptor_set_layout_sampler *sampler =
-      &bind_layout->samplers[array_index];
+      const struct anv_descriptor_set_layout_sampler *sampler =
+         &bind_layout->samplers[array_index];
 
-   return sampler->has_ycbcr_conversion ?
-          &sampler->ycbcr_conversion_state : NULL;
+      return sampler->has_ycbcr_conversion ?
+             &sampler->ycbcr_conversion_state : NULL;
+   }
 }
 
 static void
@@ -1294,6 +1299,8 @@ accept_64bit_atomic_cb(const nir_intrinsic_instr *intrin, const void *data)
 {
    return (intrin->intrinsic == nir_intrinsic_image_atomic ||
            intrin->intrinsic == nir_intrinsic_image_atomic_swap ||
+           intrin->intrinsic == nir_intrinsic_image_heap_atomic ||
+           intrin->intrinsic == nir_intrinsic_image_heap_atomic_swap ||
            intrin->intrinsic == nir_intrinsic_image_deref_atomic ||
            intrin->intrinsic == nir_intrinsic_image_deref_atomic_swap) &&
           intrin->def.bit_size == 64;
@@ -1306,8 +1313,10 @@ lower_non_tg4_non_uniform_offsets(const nir_tex_instr *tex,
    /* HW cannot deal with divergent surfaces/samplers */
    if (tex->src[index].src_type == nir_tex_src_texture_offset ||
        tex->src[index].src_type == nir_tex_src_texture_handle ||
+       tex->src[index].src_type == nir_tex_src_texture_heap_offset ||
        tex->src[index].src_type == nir_tex_src_sampler_offset ||
-       tex->src[index].src_type == nir_tex_src_sampler_handle)
+       tex->src[index].src_type == nir_tex_src_sampler_handle ||
+       tex->src[index].src_type == nir_tex_src_sampler_heap_offset)
       return true;
 
    if (tex->src[index].src_type == nir_tex_src_offset) {
@@ -1555,7 +1564,6 @@ anv_shader_lower_nir(struct anv_device *device,
    if (nir->info.stage == MESA_SHADER_FRAGMENT)
       anv_shader_compute_fragment_rts(devinfo, state, shader_data);
 
-
    uint32_t dynamic_descriptors_offset = 0;
    uint32_t dynamic_descriptors_offsets[MAX_SETS] = {};
    for (uint32_t i = 0; i < set_layout_count; i++) {
@@ -1569,14 +1577,21 @@ anv_shader_lower_nir(struct anv_device *device,
       }
    }
 
-   /* Apply the actual pipeline layout to UBOs, SSBOs, and textures */
-   NIR_PASS(_, nir, anv_nir_apply_pipeline_layout,
+   /* Apply the actual layout to UBOs, SSBOs, and textures */
+   if (shader_data->info->flags & VK_SHADER_CREATE_DESCRIPTOR_HEAP_BIT_EXT) {
+      NIR_PASS(_, nir, anv_nir_lower_descriptor_heap, device,
+               shader_data->info->embedded_sampler_count,
+               shader_data->info->embedded_samplers,
+               &shader_data->bind_map);
+   } else {
+      NIR_PASS(_, nir, anv_nir_apply_pipeline_layout,
                pdevice, shader_data->key.base.robust_flags,
                set_layouts, set_layout_count,
                (shader_data->info->flags &
                 VK_SHADER_CREATE_INDEPENDENT_SETS_BIT_MESA) ? NULL:
                dynamic_descriptors_offsets,
                &shader_data->bind_map, &shader_data->push_map, mem_ctx);
+   }
 
    NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_ubo,
             anv_nir_ubo_addr_format(pdevice, shader_data->key.base.robust_flags));
@@ -1602,7 +1617,7 @@ anv_shader_lower_nir(struct anv_device *device,
 
    const bool lower_non_uniform_texture_offsets = device->info->ver < 20;
 
-   enum nir_lower_non_uniform_access_type lower_non_uniform_access_types =
+   const enum nir_lower_non_uniform_access_type lower_non_uniform_access_types =
       nir_lower_non_uniform_texture_access |
       nir_lower_non_uniform_texture_query |
       nir_lower_non_uniform_image_access |
@@ -1616,10 +1631,8 @@ anv_shader_lower_nir(struct anv_device *device,
     * need to lower those texture messages in the same way we lower
     * non-uniform texture/sampler handles.
     */
-   if (lower_non_uniform_texture_offsets) {
-      nir_foreach_function_impl(impl, nir)
-         nir_metadata_require(impl, nir_metadata_divergence);
-   }
+   if (lower_non_uniform_texture_offsets)
+      nir_divergence_analysis(nir);
 
    /* In practice, most shaders do not have non-uniform-qualified
     * accesses (see
@@ -1695,8 +1708,10 @@ anv_shader_lower_nir(struct anv_device *device,
                &shader_data->prog_data.base,
                &shader_data->bind_map, &shader_data->push_map);
 
-   NIR_PASS(_, nir, anv_nir_lower_resource_intel, pdevice,
-               shader_data->bind_map.layout_type);
+   if (!(shader_data->info->flags & VK_SHADER_CREATE_DESCRIPTOR_HEAP_BIT_EXT)) {
+      NIR_PASS(_, nir, anv_nir_lower_resource_intel, pdevice,
+                  shader_data->bind_map.layout_type);
+   }
 
    shader_data->push_desc_info.push_set_buffer =
       anv_nir_loads_push_desc_buffer(
@@ -2039,7 +2054,8 @@ anv_shader_compile(struct vk_device *vk_device,
          rzalloc_array(mem_ctx, struct anv_pipeline_binding, 256);
       shader_data->bind_map.embedded_sampler_to_binding =
          rzalloc_array(mem_ctx, struct anv_pipeline_embedded_sampler_binding,
-                       sets_layout_embedded_sampler_count(info));
+                       MAX2(sets_layout_embedded_sampler_count(info),
+                            info->embedded_sampler_count));
 
       shader_data->prog_data.base.stage = info->stage;
 

src/intel/vulkan/anv_nir.h
@@ -117,6 +117,13 @@ bool anv_nir_apply_pipeline_layout(nir_shader *shader,
                                    struct anv_pipeline_push_map *push_map,
                                    void *push_map_mem_ctx);
 
+bool
+anv_nir_lower_descriptor_heap(nir_shader *shader,
+                              const struct anv_device *device,
+                              uint32_t embedded_sampler_count,
+                              const struct vk_sampler_state* embedded_samplers,
+                              struct anv_pipeline_bind_map *map);
+
 struct anv_nir_push_layout_info {
    bool separate_tessellation;
    bool fragment_dynamic;

src/intel/vulkan/meson.build
@@ -178,6 +178,7 @@ libanv_files = files(
   'anv_nir_apply_pipeline_layout.c',
   'anv_nir_clear_shader_analysis.c',
   'anv_nir_compute_push_layout.c',
+  'anv_nir_lower_descriptor_heap.c',
   'anv_nir_lower_driver_values.c',
   'anv_nir_lower_multiview.c',
   'anv_nir_lower_ubo_loads.c',

commit 48182ad408b1b64e3db1feff6a201ef7aa4ee427

anv: hash binding tables for EXT_descriptor_heap too
Descriptor heap shaders, like descriptor buffer shaders, do not use
binding tables. Except for render targets...

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 6e3a9833e4 ("anv: add lowering of descriptor heap intrinsics")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42125>

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -2732,16 +2732,5 @@ anv_nir_apply_pipeline_layout(nir_shader *shader,
               map->sampler_to_descriptor[i].plane);
    }
 #endif
-
-   /* Now that we're done computing the surface and sampler portions of the
-    * bind map, hash them.  This lets us quickly determine if the actual
-    * mapping has changed and not just a no-op pipeline change.
-    */
-   _mesa_blake3_compute(map->surface_to_descriptor,
-                      map->surface_count * sizeof(struct anv_pipeline_binding),
-                      map->surface_blake3);
-   _mesa_blake3_compute(map->sampler_to_descriptor,
-                      map->sampler_count * sizeof(struct anv_pipeline_binding),
-                      map->sampler_blake3);
    return progress;
 }

src/intel/vulkan/anv_shader_compile.c
@@ -1458,6 +1458,17 @@ anv_shader_lower_nir(struct anv_device *device,
                &shader_data->bind_map, &shader_data->push_map, mem_ctx);
    }
 
+   /* Now that we're done computing the surface and sampler tables of the bind
+    * map, hash them. This lets us quickly determine if the actual mapping has
+    * changed and not just a no-op pipeline change.
+    */
+   _mesa_blake3_compute(shader_data->bind_map.surface_to_descriptor,
+                        shader_data->bind_map.surface_count * sizeof(struct anv_pipeline_binding),
+                        shader_data->bind_map.surface_blake3);
+   _mesa_blake3_compute(shader_data->bind_map.sampler_to_descriptor,
+                        shader_data->bind_map.sampler_count * sizeof(struct anv_pipeline_binding),
+                        shader_data->bind_map.sampler_blake3);
+
    NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_ubo,
             anv_nir_ubo_addr_format(pdevice, shader_data->key.base.robust_flags));
    NIR_PASS(_, nir, nir_lower_explicit_io, nir_var_mem_ssbo,