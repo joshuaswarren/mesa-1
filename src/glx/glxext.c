joshuaswarren/mesa-1

commit a810635e6295e8f86f228138fa0ab0a775f4fc65

glx: ignore zink check for has_explicit_modifiers and DRI3 on MacOS.
MacOS has neither of these so always fails to start up zink.

Reviewed-By: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32568>

src/glx/glxext.c
@@ -1039,6 +1039,7 @@ __glXInitialize(Display * dpy)
    if (glx_direct)
       glx_driver |= GLX_DRIVER_SW;
 
+#if !defined(GLX_USE_APPLE)
    if (!dpyPriv->has_explicit_modifiers && glx_accel && !debug_get_bool_option("LIBGL_KOPPER_DRI2", false)) {
       if (glx_driver & GLX_DRIVER_ZINK_YES) {
          /* only print error if zink was explicitly requested */
@@ -1049,6 +1050,7 @@ __glXInitialize(Display * dpy)
       /* if no dri3 and not using dri2, disable zink */
       glx_driver &= ~GLX_DRIVER_ZINK_INFER;
    }
+#endif
 
 #ifdef GLX_USE_WINDOWSGL
    if (glx_direct && glx_accel)

commit 39f47a0c7a5838bef027412e03353d8418c5f4ea

glx: fix DRI3-not-available diagnostic skip on macOS
a810635e629 ("glx: ignore zink check for has_explicit_modifiers and DRI3 on MacOS.") wrapped the
DRI3-not-available diagnostic in #if !defined(GLX_USE_APPLE), but the macro that indicates "this
is the macOS DRI platform" is GLX_USE_APPLEGL, not GLX_USE_APPLE.

Fixes: a810635e6295e8f86f228138fa0ab0a775f4fc65
CC: duncan.hopkins@foundry.com
Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42497>

src/glx/glxext.c
@@ -1023,7 +1023,7 @@ __glXInitialize(Display * dpy)
    if (glx_direct)
       glx_driver |= GLX_DRIVER_SW;
 
-#if !defined(GLX_USE_APPLE)
+#if !defined(GLX_USE_APPLEGL)
    if (!dpyPriv->has_multibuffer && glx_accel && !debug_get_bool_option("LIBGL_KOPPER_DRI2", false)) {
       if (glx_driver & GLX_DRIVER_ZINK_YES) {
          /* only print error if zink was explicitly requested */