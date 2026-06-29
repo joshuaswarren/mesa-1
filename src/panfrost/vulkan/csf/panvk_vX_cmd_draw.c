joshuaswarren/mesa-1

commit 16674d8b0738fa4e8e6fd89b58d7ab2b68564f0f

panvk/draw: Separate build from prepare functions
These changes aim to make it easier to reuse this code for implementing
a cmd_draw_rects PanVK specific function that will use RUN_FULLSCREEN.
For this we need to setup the dcd a bit differently and having separate
build functions helps a lot since we mostly want to skip checking the
dirty state and moving values into registers that are done in the
prepare_draw functions.

Tested-by: Marc Alcala Prieto <marc.alcalaprieto@arm.com>
Reviewed-by: Aksel Hjerpbakk <aksel.hjerpbakk@arm.com>
Reviewed-by: Marc Alcala Prieto <marc.alcalaprieto@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41732>

src/panfrost/vulkan/jm/panvk_vX_cmd_draw.c
@@ -1376,7 +1376,7 @@ prepare_draw(struct panvk_cmd_buffer *cmdbuf, struct panvk_draw_data *draw)
          return result;
    }
 
-   panvk_per_arch(cmd_prepare_draw_sysvals)(cmdbuf, &draw->info);
+   panvk_per_arch(cmd_prepare_draw_sysvals)(cmdbuf, &draw->info, fs);
 
    /* Viewport emission requires up-to-date {scale,offset}.z for min/max Z,
     * so we need to call it after calling cmd_prepare_draw_sysvals(), but

src/panfrost/vulkan/panvk_vX_cmd_draw.c
@@ -811,11 +811,10 @@ prepare_iam_sysvals(struct panvk_cmd_buffer *cmdbuf, BITSET_WORD *dirty_sysvals)
 
 void
 panvk_per_arch(cmd_prepare_draw_sysvals)(struct panvk_cmd_buffer *cmdbuf,
-                                         const struct panvk_draw_info *info)
+                                         const struct panvk_draw_info *info,
+                                         const struct panvk_shader_variant *fs)
 {
    struct vk_color_blend_state *cb = &cmdbuf->vk.dynamic_graphics_state.cb;
-   const struct panvk_shader_variant *fs =
-      panvk_shader_only_variant(get_fs(cmdbuf));
    uint32_t noperspective_varyings = fs ? fs->info.varyings.noperspective : 0;
    BITSET_DECLARE(dirty_sysvals, MAX_SYSVAL_FAUS) = {0};
 

src/panfrost/vulkan/panvk_cmd_draw.h
@@ -424,7 +424,8 @@ struct panvk_draw_info {
 
 void
 panvk_per_arch(cmd_prepare_draw_sysvals)(struct panvk_cmd_buffer *cmdbuf,
-                                         const struct panvk_draw_info *info);
+                                         const struct panvk_draw_info *info,
+                                         const struct panvk_shader_variant *fs);
 
 static inline uint32_t
 color_attachment_written_mask(

commit 7c69a226fa1afc5b8c340831f43c366659268e7a

panvk: set cfg cull status according to primitive topology
According to Vulkan spec, face culling applies only to polygon
primitives. Points and lines have no polygon winding and should not
be culled. The Mali hardware does not take primitive type into account
when culling, so we need to do that check ourselves.

This matches what the Gallium driver already does in pan_jm.c.

Affects:
dEQP-VK.glsl.builtin_var.frontfacing.*.line_list.*
dEQP-VK.glsl.builtin_var.frontfacing.*.point_list.*

Fixes: 16674d8 ("panvk/draw: Separate build from prepare functions")

Signed-off-by: Ryan Zhang <ryan.zhang@nxp.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42402>

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -6,6 +6,8 @@
  * Copyright © 2016 Red Hat.
  * Copyright © 2016 Bas Nieuwenhuizen
  * Copyright © 2015 Intel Corporation
+ * 
+ * Copyright 2026 NXP
  *
  * SPDX-License-Identifier: MIT
  */
@@ -2221,8 +2223,28 @@ build_dcd_flags(struct panvk_cmd_buffer *cmdbuf,
          cfg.aligned_line_ends = true;
 
       cfg.front_face_ccw = rs->front_face == VK_FRONT_FACE_COUNTER_CLOCKWISE;
-      cfg.cull_front_face = (rs->cull_mode & VK_CULL_MODE_FRONT_BIT) != 0;
-      cfg.cull_back_face = (rs->cull_mode & VK_CULL_MODE_BACK_BIT) != 0;
+
+      /*
+       * Vulkan face culling is polygon-facing state.  Points and lines do
+       * not have polygon winding, and FrontFacing is defined as true for
+       * non-polygon primitives.
+       *
+       * Do not let the Mali DCD front/back face cull bits discard point/line
+       * primitives before rasterization.
+       */
+      const VkPrimitiveTopology topology =
+         cmdbuf->vk.dynamic_graphics_state.ia.primitive_topology;
+      const bool non_polygon =
+         topology == VK_PRIMITIVE_TOPOLOGY_POINT_LIST ||
+         topology == VK_PRIMITIVE_TOPOLOGY_LINE_LIST ||
+         topology == VK_PRIMITIVE_TOPOLOGY_LINE_STRIP ||
+         topology == VK_PRIMITIVE_TOPOLOGY_LINE_LIST_WITH_ADJACENCY ||
+         topology == VK_PRIMITIVE_TOPOLOGY_LINE_STRIP_WITH_ADJACENCY;
+
+      cfg.cull_front_face =
+         !non_polygon && (rs->cull_mode & VK_CULL_MODE_FRONT_BIT) != 0;
+      cfg.cull_back_face =
+         !non_polygon && (rs->cull_mode & VK_CULL_MODE_BACK_BIT) != 0;
 
       cfg.multisample_enable = msaa;
       cfg.occlusion_query = cmdbuf->state.gfx.occlusion_query.mode;

src/panfrost/vulkan/jm/panvk_vX_cmd_draw.c
@@ -6,6 +6,8 @@
  * Copyright © 2016 Bas Nieuwenhuizen
  * Copyright © 2015 Intel Corporation
  *
+ * Copyright 2026 NXP
+ * 
  * SPDX-License-Identifier: MIT
  */
 
@@ -973,10 +975,22 @@ panvk_emit_tiler_dcd(struct panvk_cmd_buffer *cmdbuf,
    const struct vk_rasterization_state *rs =
       &cmdbuf->vk.dynamic_graphics_state.rs;
 
+   const VkPrimitiveTopology topology =
+      cmdbuf->vk.dynamic_graphics_state.ia.primitive_topology;
+   const bool non_polygon =
+      topology == VK_PRIMITIVE_TOPOLOGY_POINT_LIST ||
+      topology == VK_PRIMITIVE_TOPOLOGY_LINE_LIST ||
+      topology == VK_PRIMITIVE_TOPOLOGY_LINE_STRIP ||
+      topology == VK_PRIMITIVE_TOPOLOGY_LINE_LIST_WITH_ADJACENCY ||
+      topology == VK_PRIMITIVE_TOPOLOGY_LINE_STRIP_WITH_ADJACENCY;
+
    pan_pack(dcd, DRAW, cfg) {
       cfg.front_face_ccw = rs->front_face == VK_FRONT_FACE_COUNTER_CLOCKWISE;
-      cfg.cull_front_face = (rs->cull_mode & VK_CULL_MODE_FRONT_BIT) != 0;
-      cfg.cull_back_face = (rs->cull_mode & VK_CULL_MODE_BACK_BIT) != 0;
+      cfg.cull_front_face =
+         !non_polygon && (rs->cull_mode & VK_CULL_MODE_FRONT_BIT) != 0;
+      cfg.cull_back_face =
+         !non_polygon && (rs->cull_mode & VK_CULL_MODE_BACK_BIT) != 0;
+
       cfg.position = draw->position;
       cfg.state = draw->fs.rsd;
       cfg.attributes = fs_desc_state->img_attrib_table;