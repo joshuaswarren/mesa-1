joshuaswarren/mesa-1

commit 1b9e3f5afe82c3c106bb811f21462f4259ef4ee6

util, llvmpipe: flush subnormals to zero on ARM/AArch64
NEON always flushes subnormals to zero; previously lp_test_arit
special-cased vector paths to suppress the resulting failures.
The proper fix mirrors x86: set FPSCR/FPCR FZ so VFP also flushes,
keeping scalar and vector paths consistent with the C reference.

util_fpstate_{get,set,set_denorms_to_zero} now read/write FPSCR
(ARMv7) or FPCR (AArch64) via inline asm. flush_denorm_to_zero
in lp_test_arit flushes subnormal inputs on ARM/AArch64 to match.

Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42178>

src/util/u_math.c
@@ -68,22 +68,41 @@ util_init_math(void)
 }
 
 /**
- * Fetches the contents of the fpstate (mxcsr on x86) register.
+ * Fetches the contents of the fpstate (mxcsr on x86, fpscr/fpcr on arm) register.
  *
  * On platforms without support for it just returns 0.
  */
 unsigned
 util_fpstate_get(void)
 {
-   unsigned mxcsr = 0;
+   unsigned fpstate = 0;
 
 #if DETECT_ARCH_SSE
    if (util_get_cpu_caps()->has_sse) {
-      mxcsr = _mm_getcsr();
+      fpstate = _mm_getcsr();
+   }
+#endif
+#if DETECT_ARCH_ARM
+   if (util_get_cpu_caps()->has_neon) {
+#ifdef HAVE___BUILTIN_ARM_GET_FPSCR
+      fpstate = __builtin_arm_get_fpscr();
+#else
+      __asm__ volatile("vmrs %0, fpscr" : "=r"(fpstate));
+#endif
+   }
+#elif DETECT_ARCH_AARCH64
+   {
+#ifdef HAVE___BUILTIN_AARCH64_GET_FPCR
+      fpstate = (unsigned)__builtin_aarch64_get_fpcr();
+#else
+      uint64_t fpcr;
+      __asm__ volatile("mrs %0, fpcr" : "=r"(fpcr));
+      fpstate = (unsigned)fpcr;
+#endif
    }
 #endif
 
-   return mxcsr;
+   return fpstate;
 }
 
 /**
@@ -93,33 +112,57 @@ util_fpstate_get(void)
  * This is the behavior required by D3D10. OpenGL doesn't care.
  */
 unsigned
-util_fpstate_set_denorms_to_zero(unsigned current_mxcsr)
+util_fpstate_set_denorms_to_zero(unsigned current_fpstate)
 {
 #if DETECT_ARCH_SSE
    if (util_get_cpu_caps()->has_sse) {
       /* Enable flush to zero mode */
-      current_mxcsr |= _MM_FLUSH_ZERO_MASK;
+      current_fpstate |= _MM_FLUSH_ZERO_MASK;
       if (util_get_cpu_caps()->has_daz) {
          /* Enable denormals are zero mode */
-         current_mxcsr |= _MM_DENORMALS_ZERO_MASK;
+         current_fpstate |= _MM_DENORMALS_ZERO_MASK;
       }
-      util_fpstate_set(current_mxcsr);
+      util_fpstate_set(current_fpstate);
    }
 #endif
-   return current_mxcsr;
+#if DETECT_ARCH_ARM || DETECT_ARCH_AARCH64
+   if (util_get_cpu_caps()->has_neon) {
+      current_fpstate |= (1u << 24); /* FPSCR/FPCR FZ bit */
+      util_fpstate_set(current_fpstate);
+   }
+#endif
+   return current_fpstate;
 }
 
 /**
- * Set the state of the fpstate (mxcsr on x86) register.
+ * Set the state of the fpstate (mxcsr on x86, fpscr/fpcr on arm) register.
  *
  * On platforms without support for it's a noop.
  */
 void
-util_fpstate_set(unsigned mxcsr)
+util_fpstate_set(unsigned fpstate)
 {
 #if DETECT_ARCH_SSE
    if (util_get_cpu_caps()->has_sse) {
-      _mm_setcsr(mxcsr);
+      _mm_setcsr(fpstate);
+   }
+#endif
+#if DETECT_ARCH_ARM
+   if (util_get_cpu_caps()->has_neon) {
+#ifdef HAVE___BUILTIN_ARM_SET_FPSCR
+      __builtin_arm_set_fpscr(fpstate);
+#else
+      __asm__ volatile("vmsr fpscr, %0" :: "r"(fpstate));
+#endif
+   }
+#elif DETECT_ARCH_AARCH64
+   {
+#ifdef HAVE___BUILTIN_AARCH64_SET_FPCR
+      __builtin_aarch64_set_fpcr(fpstate);
+#else
+      uint64_t fpcr = fpstate;
+      __asm__ volatile("msr fpcr, %0" :: "r"(fpcr));
+#endif
    }
 #endif
 }

meson.build
@@ -1171,6 +1171,13 @@ foreach b : ['add_overflow', 'add_overflow_p', 'sub_overflow_p']
   endif
 endforeach
 
+# Check for ARM FP state builtins
+foreach b : ['arm_get_fpscr', 'arm_set_fpscr', 'aarch64_get_fpcr', 'aarch64_set_fpcr']
+  if cc.has_function('__builtin_' + b)
+    pre_args += '-DHAVE___BUILTIN_@0@'.format(b.to_upper())
+  endif
+endforeach
+
 # check for GCC __attribute__
 _attributes = [
   'const', 'flatten', 'malloc', 'pure', 'unused', 'warn_unused_result',

src/gallium/drivers/llvmpipe/lp_test_arit.c
@@ -394,9 +394,8 @@ flush_denorm_to_zero(float val)
     * denormals as zero (FTZ/DAZ). Not using fpclassify because
     * a) some compilers are stuck at c89 (msvc)
     * b) not sure it reliably works with non-standard ftz/daz mode
-    * And, right now we only disable denorms with jited code on x86/sse
-    * (albeit this should be classified as a bug) so to get results which
-    * match we must only flush them to zero here in that case too.
+    * We disable denorms with jited code on x86/sse and arm/aarch64 (neon)
+    * so to get results which match we must flush them to zero here too.
     */
    union fi fi_val;
 
@@ -404,6 +403,13 @@ flush_denorm_to_zero(float val)
 
 #if DETECT_ARCH_SSE
    if (util_get_cpu_caps()->has_sse) {
+      if ((fi_val.ui & 0x7f800000) == 0) {
+         fi_val.ui &= 0xff800000;
+      }
+   }
+#endif
+#if DETECT_ARCH_ARM || DETECT_ARCH_AARCH64
+   if (util_get_cpu_caps()->has_neon) {
       if ((fi_val.ui & 0x7f800000) == 0) {
          fi_val.ui &= 0xff800000;
       }

commit 8487d38ed98ecfc87198f37b6516078c63bde362

util: Fix u_math on MSVC arm64
MSVC can't do inline asm, use intrinsics to get at FPCR.
Note that MSVC arm32 support is dead so just no-op the
inline asm for that arch.

Fixes: 1b9e3f5afe8 ("util, llvmpipe: flush subnormals to zero on ARM/AArch64")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/15685
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/15677
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42496>

src/util/u_math.c
@@ -40,6 +40,10 @@
 #endif
 #endif
 
+#if DETECT_CC_MSVC && DETECT_ARCH_AARCH64
+#include <intrin.h>
+#endif
+
 
 /** log2(x), for x in [1.0, 2.0) */
 float log2_table[LOG2_TABLE_SIZE];
@@ -94,6 +98,8 @@ util_fpstate_get(void)
    {
 #ifdef HAVE___BUILTIN_AARCH64_GET_FPCR
       fpstate = (unsigned)__builtin_aarch64_get_fpcr();
+#elif DETECT_CC_MSVC
+      fpstate = (unsigned)_ReadStatusReg(ARM64_FPCR);
 #else
       uint64_t fpcr;
       __asm__ volatile("mrs %0, fpcr" : "=r"(fpcr));
@@ -159,6 +165,8 @@ util_fpstate_set(unsigned fpstate)
    {
 #ifdef HAVE___BUILTIN_AARCH64_SET_FPCR
       __builtin_aarch64_set_fpcr(fpstate);
+#elif DETECT_CC_MSVC
+      _WriteStatusReg(ARM64_FPCR, fpstate);
 #else
       uint64_t fpcr = fpstate;
       __asm__ volatile("msr fpcr, %0" :: "r"(fpcr));