joshuaswarren/mesa-1

commit ef026bb0389e826b7a62ce6c2da87f5c1d84c0e6

anv: Support putting image base address and image params in surface state
images params including pitch, width, height and tile mode
for image address caculation

Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32676>

src/intel/genxml/meson.build
@@ -57,6 +57,7 @@ genX_bits_included_symbols = [
   # structures
   'RENDER_SURFACE_STATE::Surface Base Address',
   'RENDER_SURFACE_STATE::Surface Pitch',
+  'RENDER_SURFACE_STATE::Surface QPitch',
   'RENDER_SURFACE_STATE::Auxiliary Surface Base Address',
   'RENDER_SURFACE_STATE::Auxiliary Surface Pitch',
   'RENDER_SURFACE_STATE::Clear Value Address',
@@ -69,6 +70,7 @@ genX_bits_included_symbols = [
   'RENDER_SURFACE_STATE::Depth',
   'RENDER_SURFACE_STATE::Surface Type',
   'RENDER_SURFACE_STATE::Render Target View Extent',
+  'RENDER_SURFACE_STATE::Tile Mode',
   'CLEAR_COLOR',
   'VERTEX_BUFFER_STATE::Buffer Starting Address',
   'CPS_STATE',

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -1662,6 +1662,121 @@ lower_get_ssbo_size(nir_builder *b, nir_intrinsic_instr *intrin,
 }
 
 static bool
+lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
+                                 struct apply_pipeline_layout_state *state)
+{
+   nir_deref_instr *deref = nir_src_as_deref(intrin->src[0]);
+   nir_variable *var = nir_deref_instr_get_variable(deref);
+
+   unsigned set = var->data.descriptor_set;
+   unsigned binding = var->data.binding;
+
+   b->cursor = nir_instr_remove(&intrin->instr);
+
+   nir_def *array_index;
+   if (deref->deref_type != nir_deref_type_var) {
+      assert(deref->deref_type == nir_deref_type_array);
+      assert(nir_deref_instr_parent(deref)->deref_type == nir_deref_type_var);
+      array_index = deref->arr.index.ssa;
+   } else {
+      array_index = nir_imm_int(b, 0);
+   }
+
+   nir_def *desc_addr = build_desc_addr_for_binding(
+      b, set, binding, array_index, 0 /* plane */, state);
+
+   nir_def *desc;
+
+   if (state->layout->type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+      switch (nir_intrinsic_base(intrin)) {
+      case ISL_SURF_PARAM_BASE_ADDRESSS:
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            offsetof(struct anv_storage_image_descriptor, image_address),
+            1, 64, state);
+         break;
+      case ISL_SURF_PARAM_TILE_MODE:
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            offsetof(struct anv_storage_image_descriptor, tile_mode),
+            1, 32, state);
+         break;
+      case ISL_SURF_PARAM_PITCH:
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            offsetof(struct anv_storage_image_descriptor, row_pitch_B),
+            1, 32, state);
+         break;
+      case ISL_SURF_PARAM_QPITCH:
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            offsetof(struct anv_storage_image_descriptor, qpitch),
+            1, 32, state);
+         break;
+      default:
+         unreachable("Invalid surface parameter");
+      }
+   } else {
+      const struct intel_device_info *devinfo = &state->pdevice->info;
+
+      switch (nir_intrinsic_base(intrin)) {
+      case ISL_SURF_PARAM_BASE_ADDRESSS: {
+         desc = build_load_descriptor_mem(
+            b, desc_addr,
+            RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 8,
+            intrin->def.num_components,
+            intrin->def.bit_size, state);
+         break;
+      }
+      case ISL_SURF_PARAM_TILE_MODE: {
+         // tile mode [13:12] is in the first dword
+         const unsigned tile_mode_bits =
+            RENDER_SURFACE_STATE_TileMode_bits(devinfo);
+         const unsigned tile_mode_start =
+            RENDER_SURFACE_STATE_TileMode_start(devinfo);
+
+         nir_def *dword =
+            build_load_descriptor_mem(b, desc_addr, tile_mode_start / 32, 1, 32, state);
+
+         desc = nir_iand_imm(b,
+                             nir_ishr_imm(b, dword, tile_mode_start % 32),
+                             (1u << tile_mode_bits) - 1);
+         break;
+      }
+      case ISL_SURF_PARAM_PITCH: {
+         assert(RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) % 32 == 0);
+         const unsigned surfPitch_bits =
+            RENDER_SURFACE_STATE_SurfacePitch_bits(devinfo);
+         nir_def *pitch_dword = build_load_descriptor_mem(
+            b, desc_addr,
+            RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 8,
+            1, 32, state);
+         desc = nir_iand_imm(b, pitch_dword, (1u << surfPitch_bits) - 1);
+         desc = nir_iadd_imm(b, desc, 1);
+         break;
+      }
+      case ISL_SURF_PARAM_QPITCH: {
+         assert(RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) % 32 == 0);
+         const unsigned surfQPitch_bits =
+            RENDER_SURFACE_STATE_SurfaceQPitch_bits(devinfo);
+         nir_def *pitch_dword = build_load_descriptor_mem(
+            b, desc_addr,
+            RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 8,
+            1, 32, state);
+         desc = nir_iand_imm(b, pitch_dword, (1u << surfQPitch_bits) - 1);
+         desc = nir_ishl_imm(b, desc, 2);
+         break;
+      }
+      default:
+         unreachable("Invalid surface parameter");
+      }
+   }
+
+   nir_def_rewrite_uses(&intrin->def, desc);
+   return true;
+}
+
+static bool
 lower_image_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
                       struct apply_pipeline_layout_state *state)
 {
@@ -1955,11 +2070,12 @@ apply_pipeline_layout(nir_builder *b, nir_instr *instr, void *_state)
       case nir_intrinsic_image_deref_atomic:
       case nir_intrinsic_image_deref_atomic_swap:
       case nir_intrinsic_image_deref_samples:
-      case nir_intrinsic_image_deref_load_param_intel:
       case nir_intrinsic_image_deref_load_raw_intel:
       case nir_intrinsic_image_deref_store_raw_intel:
       case nir_intrinsic_image_deref_sparse_load:
          return lower_image_intrinsic(b, intrin, state);
+      case nir_intrinsic_image_deref_load_param_intel:
+         return lower_image_load_intel_intrinsic(b, intrin, state);
       case nir_intrinsic_image_deref_size:
          return lower_image_size_intrinsic(b, intrin, state);
       case nir_intrinsic_load_constant:

src/intel/vulkan/anv_private.h
@@ -2776,6 +2776,21 @@ struct anv_storage_image_descriptor {
     * RESINFO result.
     */
    uint32_t image_depth;
+
+   /** Image address */
+   uint64_t image_address;
+
+   /** Image tiling mode
+    *
+    * 0 for linear, ~0 otherwise.
+    */
+   uint32_t tile_mode;
+
+   /** Image row pitch in bytes */
+   uint32_t row_pitch_B;
+
+   /** Image Q pitch (rows between array slices) */
+   uint32_t qpitch;
 };
 
 /** Struct representing a address/range descriptor

src/intel/vulkan/anv_descriptor_set.c
@@ -2258,6 +2258,15 @@ anv_descriptor_set_write_image_view(struct anv_device *device,
                device->physical,
                anv_image_view_storage_surface_state(image_view)->state),
             .image_depth = image_view->vk.storage.z_slice_count,
+            .image_address = (anv_image_is_sparse(image_view->image) ?
+                              image_view->image->bindings[
+                                 ANV_IMAGE_MEMORY_BINDING_MAIN].sparse_data.address :
+                              anv_address_physical(
+                                 image_view->image->bindings[
+                                    ANV_IMAGE_MEMORY_BINDING_MAIN].address)),
+            .tile_mode = image_view->image->planes[0].primary_surface.isl.tiling == ISL_TILING_LINEAR ? 0 : 0xffffffff,
+            .row_pitch_B = image_view->image->planes[0].primary_surface.isl.row_pitch_B,
+            .qpitch = image_view->image->planes[0].primary_surface.isl.array_pitch_el_rows,
          };
          memcpy(desc_surface_map, &desc_data, sizeof(desc_data));
       } else {
@@ -2392,6 +2401,8 @@ anv_descriptor_set_write_buffer_view(struct anv_device *device,
       struct anv_storage_image_descriptor desc_data = {
          .vanilla = anv_surface_state_to_handle(
             device->physical, buffer_view->storage.state),
+         .image_address = anv_address_physical(buffer_view->address),
+         /* tile_mode, row_pitch_B, qpitch = 0 */
       };
       memcpy(desc_map, &desc_data, sizeof(desc_data));
    }

commit 56477493033d75cbfdcc904319fcfb5c00f021e1

anv/brw: fixup 64bit array image accesses
We forgot to handle sliced view of an arrayed image.

The base address of the HW descriptor not changing we need to read the
base array index and add it to the Z coordinate to compute the right
texel location.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ef026bb038 ("anv: Support putting image base address and image params in surface state")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42522>

src/intel/compiler/brw/brw_nir_lower_texel_address.c
@@ -88,6 +88,7 @@ load_image_param(nir_builder *b,
    case ISL_SURF_PARAM_TILE_MODE:
    case ISL_SURF_PARAM_PITCH:
    case ISL_SURF_PARAM_QPITCH:
+   case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT:
       bit_size = 32;
       num_components = 1;
       break;
@@ -106,11 +107,23 @@ load_image_param(nir_builder *b,
 
 static nir_def *
 image_linear_address(nir_builder *b,
+                     enum glsl_sampler_dim dim,
                      unsigned bpp,
-                     nir_def **coords,
+                     nir_def **in_coords,
                      nir_def *pitch,
-                     nir_def *qpitch)
+                     nir_def *qpitch,
+                     nir_def *min_array_el)
 {
+   nir_def *coords[3];
+   for (uint32_t i = 0; i < 3; i++)
+      coords[i] = in_coords[i];
+
+   if (dim == GLSL_SAMPLER_DIM_1D) {
+      coords[1] = nir_iadd(b, coords[1], min_array_el);
+   } else {
+      coords[2] = nir_iadd(b, coords[2], min_array_el);
+   }
+
    nir_def *qpitch_in_bytes = nir_imul(b, qpitch, nir_imax_imm(b, pitch, bpp / 8));
    return nir_iadd(b,
                    nir_iadd(b,
@@ -133,10 +146,15 @@ image_tiled_address(nir_builder *b,
                     enum glsl_sampler_dim dim,
                     bool is_array,
                     unsigned bpp,
-                    nir_def **coords,
+                    nir_def *in_coords[3],
                     nir_def *pitch,
-                    nir_def *qpitch)
+                    nir_def *qpitch,
+                    nir_def *min_array_el)
 {
+   nir_def *coords[3];
+   for (uint32_t i = 0; i < 3; i++)
+      coords[i] = in_coords[i];
+
    struct isl_tile_info tile_info;
    isl_tiling_get_info(tiling,
                        glsl_sampler_dim_to_isl(dim),
@@ -149,9 +167,11 @@ image_tiled_address(nir_builder *b,
     * the TileId/IntraTile offset should be computed using the QPitch not
     * being aligned to a tile's height.
     */
-   if (dim == GLSL_SAMPLER_DIM_2D && is_array) {
-      coords[1] = nir_iadd(b, coords[1], nir_imul(b, qpitch, coords[2]));
+   if (dim == GLSL_SAMPLER_DIM_2D) {
+      coords[1] = nir_iadd(b, coords[1], nir_imul(b, qpitch, nir_iadd(b, coords[2], min_array_el)));
       coords[2] = nir_imm_int(b, 0);
+   } else if (dim == GLSL_SAMPLER_DIM_3D) {
+      coords[2] = nir_iadd(b, coords[2], min_array_el);
    }
 
    /* Compute the intra tile offset using the swizzle (swizzles use u,v,r,p
@@ -253,10 +273,14 @@ image_address(nir_builder *b,
       load_image_param(b, surface_handle, ISL_SURF_PARAM_PITCH, heap);
    nir_def *qpitch =
       load_image_param(b, surface_handle, ISL_SURF_PARAM_QPITCH, heap);
+   nir_def *min_array_el =
+      load_image_param(b, surface_handle, ISL_SURF_PARAM_MIN_ARRAY_ELEMENT, heap);
 
    if (!isl_tiling_supports_dimensions(devinfo, tiling,
-                                       glsl_sampler_dim_to_isl(dim)))
-      return image_linear_address(b, bpp, coords, pitch, qpitch);
+                                       glsl_sampler_dim_to_isl(dim))) {
+      return image_linear_address(b, dim, bpp, coords,
+                                  pitch, qpitch, min_array_el);
+   }
 
    nir_def *linear_addr = NULL;
    nir_def *tiled_addr = NULL;
@@ -265,12 +289,15 @@ image_address(nir_builder *b,
       load_image_param(b, surface_handle, ISL_SURF_PARAM_TILE_MODE, heap);
    nir_push_if(b, nir_ieq_imm(b, tile_mode, 0));
    {
-      linear_addr = image_linear_address(b, bpp, coords, pitch, qpitch);
+      linear_addr = image_linear_address(b, dim, bpp, coords,
+                                         pitch, qpitch,
+                                         min_array_el);
    }
    nir_push_else(b, NULL);
    {
       tiled_addr = image_tiled_address(b, tiling, dim, is_array,
-                                       bpp, coords, pitch, qpitch);
+                                       bpp, coords, pitch, qpitch,
+                                       min_array_el);
    }
    nir_pop_if(b, NULL);
 

src/intel/genxml/meson.build
@@ -142,6 +142,7 @@ genX_bits_included_symbols = [
   'RENDER_SURFACE_STATE::Surface Type',
   'RENDER_SURFACE_STATE::Render Target View Extent',
   'RENDER_SURFACE_STATE::Tile Mode',
+  'RENDER_SURFACE_STATE::Minimum Array Element',
   'CLEAR_COLOR',
   'VERTEX_BUFFER_STATE::Buffer Starting Address',
   'CPS_STATE',

src/intel/isl/isl.h
@@ -2045,6 +2045,7 @@ enum isl_surf_param {
    ISL_SURF_PARAM_PITCH,
    ISL_SURF_PARAM_QPITCH,
    ISL_SURF_PARAM_FORMAT,
+   ISL_SURF_PARAM_MIN_ARRAY_ELEMENT,
 };
 
 /*

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -1707,6 +1707,12 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
             offsetof(struct anv_storage_image_descriptor, format),
             1, 32, state);
          break;
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT:
+         desc = build_load_descriptor_mem_from_res_index(
+            b, res_index,
+            offsetof(struct anv_storage_image_descriptor, min_array_element),
+            1, 32, state);
+         break;
       default:
          UNREACHABLE("Invalid surface parameter");
       }
@@ -1717,7 +1723,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_BASE_ADDRESSS: {
          desc = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceBaseAddress_start(devinfo) / 32),
             intrin->def.num_components,
             intrin->def.bit_size, state);
          break;
@@ -1726,7 +1732,8 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          nir_def *dword =
             build_load_descriptor_mem_from_res_index(
                b, res_index,
-               RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32, 1, 32, state);
+               4 * (RENDER_SURFACE_STATE_TileMode_start(devinfo) / 32),
+               1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, dword,
             RENDER_SURFACE_STATE_TileMode_start(devinfo) % 32,
@@ -1737,7 +1744,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfacePitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1751,7 +1758,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
          assert(RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) % 32 == 0);
          nir_def *pitch_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceQPitch_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, pitch_dword,
@@ -1764,12 +1771,23 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       case ISL_SURF_PARAM_FORMAT: {
          nir_def *format_dword = build_load_descriptor_mem_from_res_index(
             b, res_index,
-            RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 8,
+            4 * (RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) / 32),
             1, 32, state);
          desc = nir_ubitfield_extract_imm(
             b, format_dword,
             RENDER_SURFACE_STATE_SurfaceFormat_start(devinfo) % 32,
             RENDER_SURFACE_STATE_SurfaceFormat_bits(devinfo));
+         break;
+      }
+      case ISL_SURF_PARAM_MIN_ARRAY_ELEMENT: {
+         nir_def *min_arr_el_dword = build_load_descriptor_mem_from_res_index(
+            b, res_index,
+            4 * (RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) / 32),
+            1, 32, state);
+         desc = nir_ubitfield_extract_imm(
+            b, min_arr_el_dword,
+            RENDER_SURFACE_STATE_MinimumArrayElement_start(devinfo) % 32,
+            RENDER_SURFACE_STATE_MinimumArrayElement_bits(devinfo));
          break;
       }
       default:

src/intel/vulkan/anv_private.h
@@ -3578,6 +3578,9 @@ struct anv_storage_image_descriptor {
 
    /** Image Format (enum isl_format) */
    uint32_t format;
+
+   /** Image View VkImageSubresourceRange::baseArrayLayer */
+   uint32_t min_array_element;
 };
 
 /** Struct representing a address/range descriptor