joshuaswarren/mesa-1

commit 865dd52114470f0192e2e7519c2b7a9b4210fd68

panvk: Implement RUN_FRAGMENT2
Added structure panvk_fb_layer_state and related logic to store and emit
per-layer fragment state.

Also, move some temporary registers to non-conflicting ones.

Incremental rendering is left as TODO for later.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41081>

src/panfrost/vulkan/csf/panvk_cmd_buffer.h
@@ -61,6 +61,37 @@ enum panvk_incremental_rendering_pass {
    PANVK_IR_PASS_COUNT
 };
 
+#if PAN_ARCH >= 14
+/* Framebuffer per-layer state. Keep this structure 64-byte aligned, since
+ * we want the adjacent ZS_CRC_EXTENSION and RENDER_TARGET descriptors
+ * aligned. */
+struct panvk_fb_layer_state {
+   /** GPU address to the tiler descriptor. */
+   uint64_t tiler;
+
+   /** Frame argument. */
+   uint64_t frame_argument;
+
+   /** An instance of Fragment Flags 0. */
+   struct mali_fragment_flags_0_packed flags0;
+
+   /** An instance of Fragment Flags 2. */
+   struct mali_fragment_flags_2_packed flags2;
+
+   /** Z clear value. */
+   uint32_t z_clear;
+
+   /** GPU address to the draw call descriptors. It may be 0. */
+   uint64_t dcd_pointer;
+
+   /** GPU address to the ZS_CRC_EXTENSION descriptor. It may be 0. */
+   uint64_t dbd_pointer;
+
+   /** GPU address to the RENDER_TARGET descriptors. */
+   uint64_t rtd_pointer;
+} __attribute__((aligned(64)));
+#endif /* PAN_ARCH >= 14 */
+
 static inline uint32_t
 get_tiler_oom_handler_idx(bool has_zs_ext, uint32_t rt_count)
 {
@@ -74,7 +105,11 @@ static inline uint32_t
 get_fbd_size(bool has_zs_ext, uint32_t rt_count)
 {
    assert(rt_count >= 1 && rt_count <= MAX_RTS);
+#if PAN_ARCH >= 14
+   uint32_t fbd_size = ALIGN_POT(sizeof(struct panvk_fb_layer_state), 64);
+#else
    uint32_t fbd_size = pan_size(FRAMEBUFFER);
+#endif
    if (has_zs_ext)
       fbd_size += pan_size(ZS_CRC_EXTENSION);
    fbd_size += pan_size(RENDER_TARGET) * rt_count;
@@ -209,13 +244,25 @@ enum panvk_cs_regs {
    PANVK_CS_REG_RUN_IDVS_SR_END = 60,
 #endif
 
+#if PAN_ARCH >= 14
+   /* RUN_FRAGMENT2 staging regs.
+    * SW ABI:
+    * - r58:59 contain the pointer to the first tiler descriptor. This is
+    *   needed to gather completed heap chunks after a run_fragment2.
+    */
+   PANVK_CS_REG_RUN_FRAGMENT_SR_START = 0,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_END = 55,
+   PANVK_CS_REG_TILER_DESC_PTR = 58,
+#else
    /* RUN_FRAGMENT staging regs.
     * SW ABI:
-    * - r38:39 contain the pointer to the first tiler descriptor. This is
+    * - r58:59 contain the pointer to the first tiler descriptor. This is
     *   needed to gather completed heap chunks after a run_fragment.
     */
    PANVK_CS_REG_RUN_FRAGMENT_SR_START = 38,
    PANVK_CS_REG_RUN_FRAGMENT_SR_END = 46,
+   PANVK_CS_REG_TILER_DESC_PTR = 58,
+#endif
 
    /* RUN_COMPUTE staging regs. */
    PANVK_CS_REG_RUN_COMPUTE_SR_START = 0,
@@ -869,5 +916,32 @@ vk_stages_to_subqueue_mask(VkPipelineStageFlags2 vk_stages,
 
 void panvk_per_arch(emit_barrier)(struct panvk_cmd_buffer *cmdbuf,
                                   struct panvk_cs_deps deps);
+
+#if PAN_ARCH >= 14
+static inline void
+cs_emit_layer_fragment_state(struct cs_builder *b, struct cs_index fbd_ptr)
+{
+   /* Emit the dynamic fragment state. This state may change per-layer. */
+
+   cs_load32_to(b, cs_sr_reg32(b, FRAGMENT, FLAGS_0), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, flags0));
+   cs_load32_to(b, cs_sr_reg32(b, FRAGMENT, FLAGS_2), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, flags2));
+   cs_load32_to(b, cs_sr_reg32(b, FRAGMENT, Z_CLEAR), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, z_clear));
+   cs_load64_to(b, cs_sr_reg64(b, FRAGMENT, TILER_DESCRIPTOR_POINTER), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, tiler));
+   cs_load64_to(b, cs_sr_reg64(b, FRAGMENT, RTD_POINTER), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, rtd_pointer));
+   cs_load64_to(b, cs_sr_reg64(b, FRAGMENT, DBD_POINTER), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, dbd_pointer));
+   cs_load64_to(b, cs_sr_reg64(b, FRAGMENT, FRAME_ARG), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, frame_argument));
+   cs_load64_to(b, cs_sr_reg64(b, FRAGMENT, FRAME_SHADER_DCD_POINTER), fbd_ptr,
+                offsetof(struct panvk_fb_layer_state, dcd_pointer));
+
+   cs_flush_loads(b);
+}
+#endif /* PAN_ARCH >= 14 */
 
 #endif /* PANVK_CMD_BUFFER_H */

src/panfrost/genxml/v14.xml
@@ -1482,6 +1482,7 @@
     <value name="FRAME_SHADER_DCD_POINTER" value="46"/>
     <value name="VRS_IMAGE" value="48"/>
     <!-- 50-51, 53 zero -->
+    <value name="FBD_POINTER" value="54"/> <!-- SW ABI -->
   </enum>
 
   <struct name="Descriptor Header" size="1" align="32">

src/panfrost/lib/pan_fb.c
@@ -76,20 +76,6 @@ GENX(pan_select_fb_tile_size)(struct pan_fb_layout *fb)
 #endif
 }
 
-/**
- * Returns true if there's enough space in the tile buffer for at least two
- * Z/S tiles.
- */
-static inline bool
-pan_fb_can_pipeline_zs(const struct pan_fb_layout *fb)
-{
-   const uint32_t z_B_per_px = sizeof(float) * fb->sample_count;
-   const uint32_t z_B_per_tile = z_B_per_px * fb->tile_size_px;
-
-   /* The budget is already half the available Z space */
-   return z_B_per_tile < fb->tile_z_budget_B;
-}
-
 static void
 align_fb_tiling_area_for_image_plane(struct pan_fb_layout *fb,
                                      struct pan_image_plane_ref pref)
@@ -377,13 +363,6 @@ GENX(pan_fill_fb_info)(const struct pan_fb_desc_info *info,
 }
 
 #if PAN_ARCH >= 5
-static bool
-target_has_clear(const struct pan_fb_load_target *target)
-{
-   return target->in_bounds_load == PAN_FB_LOAD_CLEAR ||
-          target->border_load == PAN_FB_LOAD_CLEAR;
-}
-
 static enum mali_msaa
 translate_msaa_copy_op(const struct pan_fb_layout *fb,
                        const struct pan_image_view *iview,
@@ -414,11 +393,6 @@ translate_msaa_copy_op(const struct pan_fb_layout *fb,
    }
 }
 
-struct pan_fb_clean_tile {
-   uint8_t rts;
-   bool zs, s;
-};
-
 static bool
 pan_fb_load_target_always(const struct pan_fb_load_target *target)
 {
@@ -435,8 +409,8 @@ pan_fb_store_target_always(const struct pan_fb_store_target *target)
    return target->store && target->always;
 }
 
-static struct pan_fb_clean_tile
-pan_fb_get_clean_tile(const struct pan_fb_desc_info *info)
+struct pan_fb_clean_tile
+GENX(pan_fb_get_clean_tile)(const struct pan_fb_desc_info *info)
 {
    const struct pan_fb_layout *fb = info->fb;
    const struct pan_fb_load *load = info->load;
@@ -614,7 +588,7 @@ emit_rgb_rt_desc(const struct pan_fb_desc_info *info,
       cfg.clean_pixel_write_enable = !!(ct.rts & BITFIELD_BIT(rt));
 #endif
 
-      if (load && target_has_clear(&load->rts[rt])) {
+      if (load && pan_target_has_clear(&load->rts[rt])) {
          uint32_t packed[4] = {};
          pan_pack_color(GENX(pan_blendable_formats), packed,
                         &load->rts[rt].clear.color, fb->rt_formats[rt],
@@ -649,33 +623,12 @@ emit_rgb_rt_desc(const struct pan_fb_desc_info *info,
    pan_merge(rgb_rt, &desc, RGB_RENDER_TARGET);
 }
 
-#if PAN_ARCH >= 6
-/* All GPUs starting from Bifrost are affected by issue TSIX-2033:
- *
- *      Forcing clean_tile_writes breaks INTERSECT readbacks
- *
- * To workaround, use the pre-frame shader mode ALWAYS instead of INTERSECT if
- * clean_tile_write_enable is set on either one of the color, depth or stencil
- * buffers. Since INTERSECT is a hint that the hardware may ignore, this
- * cannot affect correctness, only performance. */
-
-static enum mali_pre_post_frame_shader_mode
-pan_fix_frame_shader_mode(enum mali_pre_post_frame_shader_mode mode,
-                          bool force_clean_tile)
-{
-   if (force_clean_tile && mode == MALI_PRE_POST_FRAME_SHADER_MODE_INTERSECT)
-      return MALI_PRE_POST_FRAME_SHADER_MODE_ALWAYS;
-   else
-      return mode;
-}
-#endif
-
 static void
 emit_rts(const struct pan_fb_desc_info *info,
          struct mali_rgb_render_target_packed *rts)
 {
    const struct pan_fb_layout *fb = info->fb;
-   const struct pan_fb_clean_tile ct = pan_fb_get_clean_tile(info);
+   const struct pan_fb_clean_tile ct = GENX(pan_fb_get_clean_tile)(info);
 
    uint32_t tile_rt_offset_B = 0;
    for (unsigned rt = 0; rt < fb->rt_count; rt++) {
@@ -696,7 +649,7 @@ GENX(pan_emit_fb_desc)(const struct pan_fb_desc_info *info,
                        const struct pan_fb_descs *out)
 {
    if (pan_fb_has_zs(info->fb)) {
-      emit_zs_crc_desc(info, pan_fb_get_clean_tile(info), out->zs_crc);
+      emit_zs_crc_desc(info, GENX(pan_fb_get_clean_tile)(info), out->zs_crc);
    }
 
    emit_rts(info, out->rts);
@@ -711,7 +664,7 @@ GENX(pan_emit_fb_desc)(const struct pan_fb_desc_info *info,
    const struct pan_fb_layout *fb = info->fb;
    const struct pan_fb_load *load = info->load;
    const struct pan_fb_store *store = info->store;
-   const struct pan_fb_clean_tile ct = pan_fb_get_clean_tile(info);
+   const struct pan_fb_clean_tile ct = GENX(pan_fb_get_clean_tile)(info);
 
    const bool has_zs_crc_ext = pan_fb_has_zs(fb);
 
@@ -793,14 +746,14 @@ GENX(pan_emit_fb_desc)(const struct pan_fb_desc_info *info,
 
       if (fb->s_format != PIPE_FORMAT_NONE) {
          cfg.s_clear =
-            load && target_has_clear(&load->s) ? load->s.clear.stencil : 0;
+            load && pan_target_has_clear(&load->s) ? load->s.clear.stencil : 0;
          cfg.s_write_enable = store && store->s.store;
       }
 
       if (fb->z_format != PIPE_FORMAT_NONE) {
          cfg.z_internal_format = pan_get_z_internal_format(fb->z_format);
          cfg.z_clear =
-            load && target_has_clear(&load->z) ? load->z.clear.depth : 0;
+            load && pan_target_has_clear(&load->z) ? load->z.clear.depth : 0;
          cfg.z_write_enable = store && store->zs.store;
       } else {
          /* Default to 24 bit depth if there's no surface. */

src/panfrost/lib/pan_fb.h
@@ -618,15 +618,67 @@ bool GENX(pan_fb_load_shader_key_fill)(struct pan_fb_shader_key *key,
                                        const struct pan_fb_load *load,
                                        bool zs_prepass);
 
+#if PAN_ARCH >= 5
+struct pan_fb_clean_tile {
+   uint8_t rts;
+   bool zs, s;
+};
+
+struct pan_fb_clean_tile
+   GENX(pan_fb_get_clean_tile)(const struct pan_fb_desc_info *info);
+
+static inline bool
+pan_target_has_clear(const struct pan_fb_load_target *target)
+{
+   return target->in_bounds_load == PAN_FB_LOAD_CLEAR ||
+          target->border_load == PAN_FB_LOAD_CLEAR;
+}
+#endif /* PAN_ARCH >= 5 */
+
 #if PAN_ARCH >= 6
 bool GENX(pan_fb_resolve_shader_key_fill)(struct pan_fb_shader_key *key,
                                           const struct pan_fb_layout *fb,
                                           const struct pan_fb_resolve *resolve);
-#endif
+
+/* All GPUs starting from Bifrost are affected by issue TSIX-2033:
+ *
+ *      Forcing clean_tile_writes breaks INTERSECT readbacks
+ *
+ * To workaround, use the pre-frame shader mode ALWAYS instead of INTERSECT if
+ * clean_tile_write_enable is set on either one of the color, depth or stencil
+ * buffers. Since INTERSECT is a hint that the hardware may ignore, this
+ * cannot affect correctness, only performance. */
+
+static inline enum mali_pre_post_frame_shader_mode
+pan_fix_frame_shader_mode(enum mali_pre_post_frame_shader_mode mode,
+                          bool force_clean_tile)
+{
+   if (force_clean_tile && mode == MALI_PRE_POST_FRAME_SHADER_MODE_INTERSECT)
+      return MALI_PRE_POST_FRAME_SHADER_MODE_ALWAYS;
+   else
+      return mode;
+}
+#endif /* PAN_ARCH >= 6 */
 
 struct nir_shader *
 GENX(pan_get_fb_shader)(const struct pan_fb_shader_key *key,
                         const struct nir_shader_compiler_options *nir_options);
+
+#if PAN_ARCH >= 13
+/**
+ * Returns true if there's enough space in the tile buffer for at least two
+ * Z/S tiles.
+ */
+static inline bool
+pan_fb_can_pipeline_zs(const struct pan_fb_layout *fb)
+{
+   const uint32_t z_B_per_px = sizeof(float) * fb->sample_count;
+   const uint32_t z_B_per_tile = z_B_per_px * fb->tile_size_px;
+
+   /* The budget is already half the available Z space */
+   return z_B_per_tile < fb->tile_z_budget_B;
+}
+#endif
 #endif /* PAN_ARCH */
 
 #endif /* __PAN_FB_H */

commit 0372baa5c15b589f6a51b9fe39f29f86838a7b33

panvk/csf: Avoid poisoning read-only fragment SRs
Define the RUN_FRAGMENT[2] SR ranges that are writeable within
cs_update_frag_ctx() blocks. And use those ranges when generating the
fragment SR whitelist.

Modify the PANVK_DEBUG(CS) logic to skip poisoning read-only SRs. This
keeps these registers at their command stream initialization value (0).

Fixes: 865dd521144 ("panvk: Implement RUN_FRAGMENT2")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42481>

src/panfrost/vulkan/csf/panvk_cmd_buffer.h
@@ -256,6 +256,15 @@ enum panvk_cs_regs {
    PANVK_CS_REG_RUN_FRAGMENT_SR_START = 0,
    PANVK_CS_REG_RUN_FRAGMENT_SR_END = 55,
    PANVK_CS_REG_TILER_DESC_PTR = 58,
+
+   /* RUN_FRAGMENT2 RW staging regs. The rest are initialized to zero at
+    * command stream initialization, and should never be touched again. */
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_0_START = 28,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_0_END = 47,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_1_START = 52,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_1_END = 52,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_2_START = 54,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_2_END = 55,
 #else
    /* RUN_FRAGMENT staging regs.
     * SW ABI:
@@ -265,6 +274,11 @@ enum panvk_cs_regs {
    PANVK_CS_REG_RUN_FRAGMENT_SR_START = 38,
    PANVK_CS_REG_RUN_FRAGMENT_SR_END = 46,
    PANVK_CS_REG_TILER_DESC_PTR = 58,
+
+   /* RUN_FRAGMENT RW staging regs. The rest are initialized to zero at
+    * command stream initialization, and should never be touched again. */
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_0_START = 0,
+   PANVK_CS_REG_RUN_FRAGMENT_SR_RANGE_0_END = 43,
 #endif
 
    /* RUN_COMPUTE staging regs. */
@@ -445,7 +459,17 @@ panvk_cs_reg_whitelist(progress_seqno, PANVK_CS_REG_RANGE(PROGRESS_SEQNO));
 panvk_cs_reg_whitelist(compute_ctx, PANVK_CS_REG_RANGE(RUN_COMPUTE_SR));
 #define cs_update_compute_ctx(__b) panvk_cs_reg_upd_ctx(__b, compute_ctx)
 
-panvk_cs_reg_whitelist(frag_ctx, PANVK_CS_REG_RANGE(RUN_FRAGMENT_SR));
+#if PAN_ARCH >= 14
+#define PANVK_RUN_FRAG_SR_WHITELIST_RANGES                                     \
+   PANVK_CS_REG_RANGE(RUN_FRAGMENT_SR_RANGE_0),                                \
+      PANVK_CS_REG_RANGE(RUN_FRAGMENT_SR_RANGE_1),                             \
+      PANVK_CS_REG_RANGE(RUN_FRAGMENT_SR_RANGE_2)
+#else
+#define PANVK_RUN_FRAG_SR_WHITELIST_RANGES                                     \
+   PANVK_CS_REG_RANGE(RUN_FRAGMENT_SR_RANGE_0)
+#endif
+
+panvk_cs_reg_whitelist(frag_ctx, PANVK_RUN_FRAG_SR_WHITELIST_RANGES);
 #define cs_update_frag_ctx(__b) panvk_cs_reg_upd_ctx(__b, frag_ctx)
 
 panvk_cs_reg_whitelist(vt_ctx, PANVK_CS_REG_RANGE(RUN_IDVS_SR));

src/panfrost/vulkan/csf/panvk_vX_cmd_buffer.c
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2021 Collabora Ltd.
+ * Copyright © 2026 Arm Ltd.
  *
  * Derived from tu_cmd_buffer.c which is:
  * Copyright © 2016 Red Hat.
@@ -156,11 +157,29 @@ finish_cs(struct panvk_cmd_buffer *cmdbuf, uint32_t subqueue)
    if (PANVK_DEBUG(CS) &&
        cmdbuf->vk.level != VK_COMMAND_BUFFER_LEVEL_SECONDARY &&
        !cmdbuf->state.gfx.render.suspended) {
+      static const reg_perm_cb_t ctx_reg_perms[PANVK_SUBQUEUE_COUNT] = {
+         [PANVK_SUBQUEUE_VERTEX_TILER] = panvk_cs_vt_ctx_reg_perm,
+         [PANVK_SUBQUEUE_FRAGMENT] = panvk_cs_frag_ctx_reg_perm,
+         [PANVK_SUBQUEUE_COMPUTE] = panvk_cs_compute_ctx_reg_perm,
+      };
+      static const reg_perm_cb_t default_reg_perms[PANVK_SUBQUEUE_COUNT] = {
+         [PANVK_SUBQUEUE_VERTEX_TILER] = panvk_cs_vt_reg_perm,
+         [PANVK_SUBQUEUE_FRAGMENT] = panvk_cs_frag_reg_perm,
+         [PANVK_SUBQUEUE_COMPUTE] = panvk_cs_compute_reg_perm,
+      };
+      const reg_perm_cb_t ctx_perm = ctx_reg_perms[subqueue];
+      const reg_perm_cb_t default_perm = default_reg_perms[subqueue];
+
       cs_update_cmdbuf_regs(b) {
          /* Poison all cmdbuf registers to make sure we don't inherit state from
           * a previously executed cmdbuf. */
-         for (uint32_t i = 0; i <= PANVK_CS_REG_SCRATCH_END; i++)
-            cs_move32_to(b, cs_reg32(b, i), 0xdead | i << 24);
+         for (uint32_t i = 0; i <= PANVK_CS_REG_SCRATCH_END; i++) {
+            /* If the default or context access has the WR bit, we're good to
+             * poison, otherwise we assume this is a RO register that's expected
+             * to stay zero. */
+             if ((default_perm(b, i) | ctx_perm(b, i)) & CS_REG_WR)
+                cs_move32_to(b, cs_reg32(b, i), 0xdead | i << 24);
+         }
       }
    }
 