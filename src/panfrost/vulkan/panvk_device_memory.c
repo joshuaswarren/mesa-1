joshuaswarren/mesa-1

commit 8567f589234efadc3067586e4c0445edc756d954

panvk/v10+: Align allocations >= 64k to 64k
As interleaved_64k requires an alignment of 64k, align any allocation of
64k or more to at least 64k for GPUs that support interleaved_64k (v10+).

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Caterina Shablia <nanokatze@gmail.com>
Reviewed-by: Marc Alcala Prieto <marc.alcalaprieto@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42414>

src/panfrost/vulkan/panvk_device_memory.c
@@ -149,6 +149,14 @@ panvk_AllocateMemory(VkDevice _device,
    };
 
    if (!(device->kmod.vm->flags & PAN_KMOD_VM_FLAG_AUTO_VA)) {
+      uint64_t alignment =
+         pan_choose_gpu_va_alignment(device->kmod.vm, op.va.size);
+      unsigned arch = pan_arch(device->kmod.dev->props.gpu_id);
+      /* For sizes bigger than 64k, align the VA on 64k to meet the requirement
+       * for interleaved_64k images (added in v10). */
+      if (arch >= 10 && op.va.size > 64 * 1024)
+         alignment = MAX2(alignment, 64 * 1024);
+
       if (unlikely(mem->vk.alloc_flags &
                    VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_CAPTURE_REPLAY_BIT)) {
          const VkMemoryOpaqueCaptureAddressAllocateInfo *capture_alloc_info =
@@ -156,18 +164,16 @@ panvk_AllocateMemory(VkDevice _device,
                                  MEMORY_OPAQUE_CAPTURE_ADDRESS_ALLOCATE_INFO);
          if (capture_alloc_info == NULL ||
              capture_alloc_info->opaqueCaptureAddress == 0) {
-            op.va.start = panvk_as_alloc(
-               device, &device->as.fixed_heap, op.va.size,
-               pan_choose_gpu_va_alignment(device->kmod.vm, op.va.size));
+            op.va.start = panvk_as_alloc(device, &device->as.fixed_heap,
+                                         op.va.size, alignment);
          } else {
             op.va.start = panvk_as_alloc_fixed_address(
                device, &device->as.fixed_heap,
                capture_alloc_info->opaqueCaptureAddress, op.va.size);
          }
       } else {
-         op.va.start = panvk_as_alloc(
-            device, &device->as.heap, op.va.size,
-            pan_choose_gpu_va_alignment(device->kmod.vm, op.va.size));
+         op.va.start =
+            panvk_as_alloc(device, &device->as.heap, op.va.size, alignment);
       }
 
       if (!op.va.start) {

commit fec14128493f72788f3460f1579645ec0a38ebba

panvk/v10+: Fix size gt -> gte for 64k alignment
Allocations of exactly 64k can also use interleaved_64k, so they have
the same alignment requirements.

This was always the intention, but apparently not the implementation.

Fixes: 8567f589234 ("panvk/v10+: Align allocations >= 64k to 64k")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Caterina Shablia <nanokatze@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42628>

src/panfrost/vulkan/panvk_device_memory.c
@@ -152,9 +152,9 @@ panvk_AllocateMemory(VkDevice _device,
       uint64_t alignment =
          pan_choose_gpu_va_alignment(device->kmod.vm, op.va.size);
       unsigned arch = pan_arch(device->kmod.dev->props.gpu_id);
-      /* For sizes bigger than 64k, align the VA on 64k to meet the requirement
-       * for interleaved_64k images (added in v10). */
-      if (arch >= 10 && op.va.size > 64 * 1024)
+      /* For sizes larger than or equal to 64k, align the VA on 64k to meet the
+       * requirement for interleaved_64k images (added in v10). */
+      if (arch >= 10 && op.va.size >= 64 * 1024)
          alignment = MAX2(alignment, 64 * 1024);
 
       if (unlikely(mem->vk.alloc_flags &