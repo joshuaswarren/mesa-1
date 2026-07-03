joshuaswarren/mesa-1

commit 2196b8fd33aad89779c5ebe4b5921f4d64c8d471

kraid: Add OpTex*
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42608>

src/panfrost/compiler/kraid/encode_v9.rs
@@ -1803,6 +1803,205 @@ impl V9Instr for OpStore {
     }
 }
 
+impl From<TexCoordMode> for TexCoordinateModeM {
+    fn from(coord_mode: TexCoordMode) -> Self {
+        match coord_mode {
+            TexCoordMode::F32 => TexCoordinateModeM::FloatCoordinates,
+            TexCoordMode::I32 => TexCoordinateModeM::IntegerCoordinates,
+        }
+    }
+}
+
+impl From<TexDim> for TexDimensionalityM {
+    fn from(dim: TexDim) -> Self {
+        match dim {
+            TexDim::Cube => TexDimensionalityM::Cube,
+            TexDim::Tex1D => TexDimensionalityM::Tex1d,
+            TexDim::Tex2D => TexDimensionalityM::Tex2d,
+            TexDim::Tex3D => TexDimensionalityM::Tex3d,
+        }
+    }
+}
+
+impl From<TexGatherComp> for TexGatherComponentM {
+    fn from(comp: TexGatherComp) -> Self {
+        match comp {
+            TexGatherComp::A => TexGatherComponentM::Gather4A,
+            TexGatherComp::B => TexGatherComponentM::Gather4B,
+            TexGatherComp::G => TexGatherComponentM::Gather4G,
+            TexGatherComp::R => TexGatherComponentM::Gather4R,
+        }
+    }
+}
+
+impl From<TexLodMode> for TexLodModeM {
+    fn from(lod_mode: TexLodMode) -> Self {
+        match lod_mode {
+            TexLodMode::None => TexLodModeM::None,
+            TexLodMode::Computed => TexLodModeM::Computed,
+            TexLodMode::ComputedForceDelta => TexLodModeM::ComputedForceDelta,
+            TexLodMode::Explicit => TexLodModeM::Explicit,
+            TexLodMode::ComputedBias => TexLodModeM::ComputedBias,
+            TexLodMode::ComputedBiasForceDelta => {
+                TexLodModeM::ComputedBiasForceDelta
+            }
+            TexLodMode::GradientDesc => TexLodModeM::Grdesc,
+        }
+    }
+}
+
+impl From<DataType> for TexWidthM {
+    fn from(dst_type: DataType) -> Self {
+        assert!(dst_type.num_type() == NumericType::Auto);
+        match dst_type.bits() {
+            16 => TexWidthM::Dst16,
+            32 => TexWidthM::Dst32,
+            _ => panic!("Invalid texture dst_type: {dst_type}"),
+        }
+    }
+}
+
+impl From<TexWriteMask> for TexWriteMaskM {
+    fn from(mask: TexWriteMask) -> Self {
+        TexWriteMaskM::try_decode(mask.to_bits(), 9).unwrap()
+    }
+}
+
+impl V9Instr for OpTexFetch {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            TexFetch::get_info((), arch),
+            src_map! {
+                sr_src: data,
+                src0: handle,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(TexFetch {
+            array_enable: self.array_enable.into(),
+            dimensionality: self.dim.into(),
+            message_slot_index: e.get_msg_slot_idx().unwrap(),
+            register_width: self.dst_type.into(),
+            skip: self.skip.into(),
+            sr_dst: op_encode_sr_write(self, &self.dst),
+            sr_src: op_encode_sr_read(self, &self.data),
+            src0: op_encode_src(self, &self.handle),
+            texel_offset: self.texel_offset.into(),
+            wide_indices: self.wide_indices.into(),
+            write_mask: self.write_mask.into(),
+        })
+    }
+}
+
+impl V9Instr for OpTexGather {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            TexGather::get_info((), arch),
+            src_map! {
+                sr_src: data,
+                src0: handle,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(TexGather {
+            array_enable: self.array_enable.into(),
+            compare_enable: self.compare_enable.into(),
+            coordinate_mode: self.coord_mode.into(),
+            dimensionality: self.dim.into(),
+            gather_component: self.gather_comp.into(),
+            message_slot_index: e.get_msg_slot_idx().unwrap(),
+            projection_enable: self.projection_enable.into(),
+            register_width: self.dst_type.into(),
+            skip: self.skip.into(),
+            sr_dst: op_encode_sr_write(self, &self.dst),
+            sr_src: op_encode_sr_read(self, &self.data),
+            src0: op_encode_src(self, &self.handle),
+            texel_offset: self.texel_offset.into(),
+            wide_indices: self.wide_indices.into(),
+            write_mask: self.write_mask.into(),
+        })
+    }
+}
+
+impl From<TexGradientCoordMode> for TexCoordinateOrDerivativeM {
+    fn from(coord_mode: TexGradientCoordMode) -> Self {
+        match coord_mode {
+            TexGradientCoordMode::Coords => TexCoordinateOrDerivativeM::None,
+            TexGradientCoordMode::ForceDelta => {
+                TexCoordinateOrDerivativeM::ForceDelta
+            }
+            TexGradientCoordMode::Derivative => {
+                TexCoordinateOrDerivativeM::Derivative
+            }
+        }
+    }
+}
+
+impl V9Instr for OpTexGradient {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            TexGradient::get_info((), arch),
+            src_map! {
+                sr_src: data,
+                src0: handle,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(TexGradient {
+            coordinate_or_derivative: self.coord_mode.into(),
+            dimensionality: self.dim.into(),
+            lod_bias_disable: self.lod_bias_disable.into(),
+            lod_clamp_disable: self.lod_clamp_disable.into(),
+            message_slot_index: e.get_msg_slot_idx().unwrap(),
+            projection_enable: self.projection_enable.into(),
+            register_width: TexGradientWidthM::Dst32,
+            skip: self.skip.into(),
+            sr_dst: op_encode_sr_write(self, &self.dst),
+            sr_src: op_encode_sr_read(self, &self.data),
+            src0: op_encode_src(self, &self.handle),
+            wide_indices: self.wide_indices.into(),
+            write_mask: TexGradientWriteMaskM::Rg,
+        })
+    }
+}
+
+impl V9Instr for OpTexSingle {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            TexSingle::get_info((), arch),
+            src_map! {
+                sr_src: data,
+                src0: handle,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(TexSingle {
+            array_enable: self.array_enable.into(),
+            compare_enable: self.compare_enable.into(),
+            dimensionality: self.dim.into(),
+            lod_mode: self.lod_mode.into(),
+            message_slot_index: e.get_msg_slot_idx().unwrap(),
+            projection_enable: self.projection_enable.into(),
+            register_width: self.dst_type.into(),
+            skip: self.skip.into(),
+            sr_dst: op_encode_sr_write(self, &self.dst),
+            sr_src: op_encode_sr_read(self, &self.data),
+            src0: op_encode_src(self, &self.handle),
+            texel_offset: self.texel_offset.into(),
+            wide_indices: self.wide_indices.into(),
+            write_mask: self.write_mask.into(),
+        })
+    }
+}
+
 macro_rules! v9_op_match_else {
     ($op: expr, |$x: ident| $y: expr, $z: expr) => {
         match $op {
@@ -1848,6 +2047,9 @@ macro_rules! v9_op_match_else {
             Op::ShiftLop($x) => $y,
             Op::StCvt($x) => $y,
             Op::Store($x) => $y,
+            Op::TexFetch($x) => $y,
+            Op::TexGather($x) => $y,
+            Op::TexSingle($x) => $y,
             _ => $z,
         }
     };

src/panfrost/compiler/kraid/ops.rs
@@ -1986,6 +1986,322 @@ impl VirtualOpcode for OpSwz {
     }
 }
 
+#[derive(Clone, Copy, PartialEq)]
+pub enum TexDim {
+    Cube,
+    Tex1D,
+    Tex2D,
+    Tex3D,
+}
+
+impl fmt::Display for TexDim {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TexDim::Cube => write!(f, ".cube"),
+            TexDim::Tex1D => write!(f, ".1d"),
+            TexDim::Tex2D => write!(f, ".2d"),
+            TexDim::Tex3D => write!(f, ".3d"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum TexCoordMode {
+    F32,
+    I32,
+}
+
+impl fmt::Display for TexCoordMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TexCoordMode::F32 => write!(f, ".float_coordinates"),
+            TexCoordMode::I32 => write!(f, ".integer_coordinates"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum TexGatherComp {
+    R,
+    G,
+    B,
+    A,
+}
+
+impl fmt::Display for TexGatherComp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TexGatherComp::R => write!(f, ".gather4_r"),
+            TexGatherComp::G => write!(f, ".gather4_g"),
+            TexGatherComp::B => write!(f, ".gather4_b"),
+            TexGatherComp::A => write!(f, ".gather4_a"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Default, PartialEq)]
+pub enum TexLodMode {
+    #[default]
+    None,
+    Computed,
+    ComputedForceDelta,
+    Explicit,
+    ComputedBias,
+    ComputedBiasForceDelta,
+    GradientDesc,
+}
+
+impl TexLodMode {
+    pub fn force_delta(self) -> TexLodMode {
+        use TexLodMode::*;
+        match self {
+            Computed | ComputedForceDelta => ComputedForceDelta,
+            ComputedBias | ComputedBiasForceDelta => ComputedBiasForceDelta,
+            _ => panic!("No force_delta enum"),
+        }
+    }
+}
+
+impl fmt::Display for TexLodMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TexLodMode::None => Ok(()),
+            TexLodMode::Computed => write!(f, ".computed"),
+            TexLodMode::ComputedForceDelta => {
+                write!(f, ".computed_force_delta")
+            }
+            TexLodMode::Explicit => write!(f, ".explicit"),
+            TexLodMode::ComputedBias => write!(f, ".computed_bias"),
+            TexLodMode::ComputedBiasForceDelta => {
+                write!(f, ".computed_bias_force_delta")
+            }
+            TexLodMode::GradientDesc => write!(f, ".grdesc"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct TexWriteMask(u8);
+
+impl TexWriteMask {
+    pub fn new(mask: u8) -> TexWriteMask {
+        assert!(mask < 1 << 4);
+        TexWriteMask(mask)
+    }
+
+    pub fn to_bits(self) -> u8 {
+        self.0
+    }
+}
+
+impl fmt::Display for TexWriteMask {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, c) in "rgba".chars().enumerate() {
+            if (self.0 & (1 << i)) != 0 {
+                write!(f, "{c}")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+#[variants(dst_type in [
+    A16, V2A16, V3A16, V4A16,
+    A32, V2A32, V3A32, V4A32,
+])]
+pub struct OpTexFetch {
+    pub dst: Dst,
+    pub dst_type: DataType,
+
+    pub skip: bool,
+    pub dim: TexDim,
+    pub write_mask: TexWriteMask,
+    pub wide_indices: bool,
+    pub array_enable: bool,
+    pub texel_offset: bool,
+
+    #[src_type(SR)]
+    pub data: Src,
+    #[src_type(I64)]
+    pub handle: Src,
+}
+
+impl fmt::Display for OpTexFetch {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} = TEX_FETCH.{}{}{}{}{}{}{} {} {}",
+            &self.dst,
+            self.dst_type,
+            bool_as_mod_str!(self, skip),
+            self.dim,
+            self.write_mask,
+            bool_as_mod_str!(self, wide_indices),
+            bool_as_mod_str!(self, array_enable),
+            bool_as_mod_str!(self, texel_offset),
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.handle),
+        )
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+#[variants(dst_type in [
+    A16, V2A16, V3A16, V4A16,
+    A32, V2A32, V3A32, V4A32,
+])]
+pub struct OpTexGather {
+    pub dst: Dst,
+    pub dst_type: DataType,
+
+    pub skip: bool,
+    pub dim: TexDim,
+    pub projection_enable: bool,
+    pub write_mask: TexWriteMask,
+    pub wide_indices: bool,
+    pub array_enable: bool,
+    pub texel_offset: bool,
+    pub compare_enable: bool,
+    pub coord_mode: TexCoordMode,
+    pub gather_comp: TexGatherComp,
+
+    #[src_type(SR)]
+    pub data: Src,
+    #[src_type(I64)]
+    pub handle: Src,
+}
+
+impl fmt::Display for OpTexGather {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} = TEX_GATHER.{}{}{}{}{}{}{}{}{}{}{} {} {}",
+            &self.dst,
+            self.dst_type,
+            bool_as_mod_str!(self, skip),
+            self.dim,
+            bool_as_mod_str!(self, projection_enable),
+            self.write_mask,
+            bool_as_mod_str!(self, wide_indices),
+            bool_as_mod_str!(self, array_enable),
+            bool_as_mod_str!(self, texel_offset),
+            bool_as_mod_str!(self, compare_enable),
+            self.coord_mode,
+            self.gather_comp,
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.handle),
+        )
+    }
+}
+
+#[derive(Clone, Copy, Default, PartialEq)]
+pub enum TexGradientCoordMode {
+    #[default]
+    Coords,
+    ForceDelta,
+    Derivative,
+}
+
+impl fmt::Display for TexGradientCoordMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TexGradientCoordMode::Coords => Ok(()),
+            TexGradientCoordMode::ForceDelta => write!(f, ".force_delta"),
+            TexGradientCoordMode::Derivative => write!(f, ".derivative"),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+pub struct OpTexGradient {
+    #[dst_type(I64)]
+    pub dst: Dst,
+
+    pub skip: bool,
+    pub dim: TexDim,
+    pub projection_enable: bool,
+    pub wide_indices: bool,
+    pub coord_mode: TexGradientCoordMode,
+    pub lod_bias_disable: bool,
+    pub lod_clamp_disable: bool,
+
+    #[src_type(SR)]
+    pub data: Src,
+    #[src_type(I64)]
+    pub handle: Src,
+}
+
+impl fmt::Display for OpTexGradient {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} = TEX_GRADIENT.{}{}{}{}{}{}{} {} {}",
+            &self.dst,
+            bool_as_mod_str!(self, skip),
+            self.dim,
+            bool_as_mod_str!(self, projection_enable),
+            bool_as_mod_str!(self, wide_indices),
+            self.coord_mode,
+            bool_as_mod_str!(self, lod_bias_disable),
+            bool_as_mod_str!(self, lod_clamp_disable),
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.handle),
+        )
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+#[variants(dst_type in [
+    A16, V2A16, V3A16, V4A16,
+    A32, V2A32, V3A32, V4A32,
+])]
+pub struct OpTexSingle {
+    pub dst: Dst,
+    pub dst_type: DataType,
+
+    pub skip: bool,
+    pub dim: TexDim,
+    pub projection_enable: bool,
+    pub write_mask: TexWriteMask,
+    pub wide_indices: bool,
+    pub array_enable: bool,
+    pub texel_offset: bool,
+    pub compare_enable: bool,
+    pub lod_mode: TexLodMode,
+
+    #[src_type(SR)]
+    pub data: Src,
+    #[src_type(I64)]
+    pub handle: Src,
+}
+
+impl fmt::Display for OpTexSingle {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} = TEX_SINGLE.{}{}{}{}{}{}{}{}{}{} {} {}",
+            &self.dst,
+            self.dst_type,
+            bool_as_mod_str!(self, skip),
+            self.dim,
+            bool_as_mod_str!(self, projection_enable),
+            self.write_mask,
+            bool_as_mod_str!(self, wide_indices),
+            bool_as_mod_str!(self, array_enable),
+            bool_as_mod_str!(self, texel_offset),
+            bool_as_mod_str!(self, compare_enable),
+            self.lod_mode,
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.handle),
+        )
+    }
+}
+
 #[derive(Clone, FromVariants, Opcode)]
 pub enum Op {
     BitRev(Box<OpBitRev>),
@@ -2038,6 +2354,10 @@ pub enum Op {
     StCvt(Box<OpStCvt>),
     Store(Box<OpStore>),
     Swz(Box<OpSwz>),
+    TexFetch(Box<OpTexFetch>),
+    TexGather(Box<OpTexGather>),
+    TexGradient(Box<OpTexGradient>),
+    TexSingle(Box<OpTexSingle>),
 }
 
 #[cfg(target_arch = "aarch64")]

commit b0a185d6270ee79f907df3002fcd2dcde682d67c

kraid/v9: Actually encode OpTexGradient
We had all the code, we were just missing the match case.

Fixes: 2196b8fd33aa ("kraid: Add OpTex*")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42686>

src/panfrost/compiler/kraid/encode_v9.rs
@@ -2049,6 +2049,7 @@ macro_rules! v9_op_match_else {
             Op::Store($x) => $y,
             Op::TexFetch($x) => $y,
             Op::TexGather($x) => $y,
+            Op::TexGradient($x) => $y,
             Op::TexSingle($x) => $y,
             _ => $z,
         }