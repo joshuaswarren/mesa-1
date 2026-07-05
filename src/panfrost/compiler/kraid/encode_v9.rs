joshuaswarren/mesa-1

commit ab063824ee35b4cf96838652acafdc65f2ec2d05

kraid/v9: Allow immediates in logic ops
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42154>

src/panfrost/compiler/kraid/encode_v9.rs
@@ -648,13 +648,23 @@ impl V9Instr for OpNop {
 macro_rules! encode_lop {
     ($e:expr, $op:expr, $Instr:ident) => {
         paste! {
-            $e.encode(v9::$Instr {
-                variant: $op.dst_type.try_into().unwrap(),
-                dst: op_encode_dst($op, &$op.dst),
-                not_result: $op.not_result.into(),
-                src0: op_encode_src($op, &$op.src0),
-                src2: op_encode_src($op, &$op.src2),
-            })
+            if let SrcRef::Imm32(imm) = &$op.src2.src_ref {
+                assert!(!$op.not_result);
+                $e.encode(v9::[<$Instr Imm>] {
+                    variant: $op.dst_type.try_into().unwrap(),
+                    dst: op_encode_dst($op, &$op.dst),
+                    src0: op_encode_src($op, &$op.src0),
+                    imm1w: (*imm).into(),
+                })
+            } else {
+                $e.encode(v9::$Instr {
+                    variant: $op.dst_type.try_into().unwrap(),
+                    dst: op_encode_dst($op, &$op.dst),
+                    not_result: $op.not_result.into(),
+                    src0: op_encode_src($op, &$op.src0),
+                    src2: op_encode_src($op, &$op.src2),
+                })
+            }
         }
     };
 }
@@ -719,6 +729,23 @@ impl V9Instr for OpShiftLop {
             (RRot, And) => RrotAnd::get_info(self.dst_type, arch),
             (RRot, Or) => RrotOr::get_info(self.dst_type, arch),
             (RRot, Xor) => RrotXor::get_info(self.dst_type, arch),
+        }
+    }
+
+    fn src_supports_imm32(&self, src: &Src, arch: u8) -> bool {
+        if !self.shift_op.is_none() {
+            return false;
+        }
+
+        if !ptr_eq(src, &self.src2) || self.not_result {
+            return false;
+        }
+
+        match self.logic_op {
+            LogicOp::None => v9::Or::is_supported(self.dst_type, arch),
+            LogicOp::And => v9::And::is_supported(self.dst_type, arch),
+            LogicOp::Or => v9::Or::is_supported(self.dst_type, arch),
+            LogicOp::Xor => v9::Xor::is_supported(self.dst_type, arch),
         }
     }
 

commit aef953cc84c70c4bb3a8878b588b5427788b419c

kraid/v9: Don't allow src0 swizzles in OpShiftLop::src_supports_imm32()
While we're here, re-arrange the function to make it more clear.

Fixes: ab063824ee35 ("kraid/v9: Allow immediates in logic ops")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42702>

src/panfrost/compiler/kraid/encode_v9.rs
@@ -1845,11 +1845,13 @@ impl V9Instr for OpShiftLop {
     }
 
     fn src_supports_imm32(&self, src: &Src, arch: u8) -> bool {
-        if !self.shift_op.is_none() {
+        // Immediates are only supported on src2 of plane (no shift) logic ops
+        if !self.shift_op.is_none() || !ptr_eq(src, &self.src2) {
             return false;
         }
 
-        if !ptr_eq(src, &self.src2) || self.not_result {
+        // We can't support a src0 swizzle or not_result with _IMM forms
+        if !self.src0.swizzle.is_none() || self.not_result {
             return false;
         }
 