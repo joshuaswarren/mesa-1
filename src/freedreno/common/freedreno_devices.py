joshuaswarren/mesa-1

commit d2ce898cf9531c06f5af28f7238d3f969cb5be44

freedreno: Add A605, A608, A610, A612 GPUs definition
While we tested a610, a605/a608 are added by observing traces being same
to a610.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20991>

src/freedreno/common/freedreno_devices.py
@@ -243,6 +243,14 @@ a6xx_gen1 = dict(
         depth_bounds_require_depth_test_quirk = True,
     )
 
+# a605, a608, a610, 612
+a6xx_gen1_low = {**a6xx_gen1, **dict(
+        has_gmem_fast_clear = False,
+        sysmem_per_ccu_cache_size = 8 * 1024,
+        gmem_ccu_color_cache_fraction = CCUColorCacheFraction.HALF.value,
+        vs_max_inputs_count = 16,
+)}
+
 # a640, a680:
 a6xx_gen2 = dict(
         fibers_per_sp = 128 * 4 * 16,
@@ -303,6 +311,36 @@ a6xx_gen4 = dict(
         has_lrz_dir_tracking = True,
         has_per_view_viewport = True,
     )
+
+add_gpus([
+        GPUId(605), # TODO: Test it, based only on libwrapfake dumps
+        GPUId(608), # TODO: Test it, based only on libwrapfake dumps
+        GPUId(610),
+        GPUId(612), # TODO: Test it, based only on libwrapfake dumps
+    ], A6xxGPUInfo(
+        CHIP.A6XX,
+        a6xx_gen1_low,
+        num_ccu = 1,
+        tile_align_w = 32,
+        tile_align_h = 16,
+        num_vsc_pipes = 16,
+        cs_shared_mem_size = 16 * 1024,
+        magic_regs = dict(
+            PC_POWER_CNTL = 0,
+            TPL1_DBG_ECO_CNTL = 0,
+            GRAS_DBG_ECO_CNTL = 0,
+            SP_CHICKEN_BITS = 0,
+            UCHE_CLIENT_PF = 0x00000004,
+            PC_MODE_CNTL = 0xf,
+            SP_DBG_ECO_CNTL = 0x0,
+            RB_DBG_ECO_CNTL = 0x04100000,
+            RB_DBG_ECO_CNTL_blit = 0x04100000,
+            HLSQ_DBG_ECO_CNTL = 0,
+            RB_UNKNOWN_8E01 = 0x00000001,
+            VPC_DBG_ECO_CNTL = 0x0,
+            UCHE_UNKNOWN_0E12 = 0x10000000,
+        ),
+    ))
 
 add_gpus([
         GPUId(615),

commit 09855cb09cdb95ed5df8808ebc25c9ec8b31395e

freedreno: Modify reg_size_vec4 for a608 and a612 to 32
a6xx_gen1_low advertises reg_size_vec4 as 48. But this value is not
accurate for a608/a612. In some scenarios, when the allocated registers
exceed the actual number of vec4 registers available, it results into a
GPU hang. Fix this by using the correct register size.

Cc: mesa-stable
Fixes: d2ce898cf95 ("freedreno: Add A605, A608, A610, A612 GPUs definition")

Signed-off-by: Sahitya Kandru <kandru@qti.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42654>

src/freedreno/common/freedreno_devices.py
@@ -264,11 +264,28 @@ a6xx_gen4 = GPUProps(
         round_robin_errata = True,
     )
 
+a6xx_gen1_low_magic_regs = dict(
+        RB_DBG_ECO_CNTL = 0x04100000,
+        RB_DBG_ECO_CNTL_blit = 0x04100000,
+        RB_RBP_CNTL = 0x00000001,
+    )
+
+a6xx_gen1_low_raw_magic_regs = [
+        [A6XXRegs.REG_A6XX_PC_MODE_CNTL, 0xf],
+        [A6XXRegs.REG_A6XX_PC_POWER_CNTL, 0],
+        [A6XXRegs.REG_A6XX_VFD_POWER_CNTL, 0],
+        [A6XXRegs.REG_A6XX_TPL1_DBG_ECO_CNTL, 0],
+        [A6XXRegs.REG_A6XX_GRAS_DBG_ECO_CNTL, 0],
+        [A6XXRegs.REG_A6XX_SP_CHICKEN_BITS, 0],
+        [A6XXRegs.REG_A6XX_SP_DBG_ECO_CNTL, 0],
+        [A6XXRegs.REG_A6XX_HLSQ_DBG_ECO_CNTL, 0],
+        [A6XXRegs.REG_A6XX_VPC_DBG_ECO_CNTL, 0],
+        [A6XXRegs.REG_A6XX_UCHE_UNKNOWN_0E12, 0x10000000],
+    ]
+
 add_gpus([
         GPUId(605), # TODO: Test it, based only on libwrapfake dumps
-        GPUId(608), # TODO: Test it, based only on libwrapfake dumps
         GPUId(610),
-        GPUId(612), # TODO: Test it, based only on libwrapfake dumps
     ], A6xxGPUInfo(
         CHIP.A6XX,
         [a6xx_base, a6xx_gen1_low],
@@ -284,23 +301,30 @@ add_gpus([
         highest_bank_bit = 13,
         ubwc_swizzle = 0x7,
         macrotile_mode = 0,
-        magic_regs = dict(
-            RB_DBG_ECO_CNTL = 0x04100000,
-            RB_DBG_ECO_CNTL_blit = 0x04100000,
-            RB_RBP_CNTL = 0x00000001,
-        ),
-        raw_magic_regs = [
-            [A6XXRegs.REG_A6XX_PC_MODE_CNTL, 0xf],
-            [A6XXRegs.REG_A6XX_PC_POWER_CNTL, 0],
-            [A6XXRegs.REG_A6XX_VFD_POWER_CNTL, 0],
-            [A6XXRegs.REG_A6XX_TPL1_DBG_ECO_CNTL, 0],
-            [A6XXRegs.REG_A6XX_GRAS_DBG_ECO_CNTL, 0],
-            [A6XXRegs.REG_A6XX_SP_CHICKEN_BITS, 0],
-            [A6XXRegs.REG_A6XX_SP_DBG_ECO_CNTL, 0],
-            [A6XXRegs.REG_A6XX_HLSQ_DBG_ECO_CNTL, 0],
-            [A6XXRegs.REG_A6XX_VPC_DBG_ECO_CNTL, 0],
-            [A6XXRegs.REG_A6XX_UCHE_UNKNOWN_0E12, 0x10000000],
-        ],
+        magic_regs = a6xx_gen1_low_magic_regs,
+        raw_magic_regs = a6xx_gen1_low_raw_magic_regs,
+    ))
+
+add_gpus([
+        GPUId(608),
+        GPUId(612),
+    ], A6xxGPUInfo(
+        CHIP.A6XX,
+        [a6xx_base, a6xx_gen1_low, GPUProps(reg_size_vec4 = 32)],
+        num_ccu = 1,
+        tile_align_w = 32,
+        tile_align_h = 16,
+        tile_max_w = 1024,
+        tile_max_h = 1024,
+        num_vsc_pipes = 16,
+        cs_shared_mem_size = 16 * 1024,
+        wave_granularity = 1,
+        fibers_per_sp = 128 * 16,
+        highest_bank_bit = 13,
+        ubwc_swizzle = 0x7,
+        macrotile_mode = 0,
+        magic_regs = a6xx_gen1_low_magic_regs,
+        raw_magic_regs = a6xx_gen1_low_raw_magic_regs,
     ))
 
 add_gpus([