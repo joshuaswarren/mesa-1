joshuaswarren/mesa-1

commit 865956662a76d4bbaeaa2521e0337babd01e456f

kraid: Add OpAtom* and plumb them through
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42689>

src/panfrost/compiler/kraid/nir.rs
@@ -1147,6 +1147,64 @@ impl<'a> ShaderFromNir<'a> {
     ) {
         let srcs = intrin.srcs_as_slice();
         match intrin.intrinsic {
+            nir_intrinsic_global_atomic => {
+                let atom_op = match intrin.atomic_op() {
+                    nir_atomic_op_iadd => AtomOp::IAdd,
+                    nir_atomic_op_imin => AtomOp::SMin,
+                    nir_atomic_op_umin => AtomOp::UMin,
+                    nir_atomic_op_imax => AtomOp::SMax,
+                    nir_atomic_op_umax => AtomOp::UMax,
+                    nir_atomic_op_iand => AtomOp::And,
+                    nir_atomic_op_ior => AtomOp::Or,
+                    nir_atomic_op_ixor => AtomOp::Xor,
+                    nir_atomic_op_xchg => AtomOp::Xchg,
+                    _ => panic!("Unknown nir_atomic_op"),
+                };
+                let dst = if intrin.def.components_read() == 0 {
+                    DstRef::None
+                } else {
+                    self.alloc_ssa(b, &intrin.def).into()
+                };
+
+                let addr = self.get_src(&srcs[0]);
+                let data = self.get_src(&srcs[1]);
+                if let Some(atom1_op) =
+                    srcs[1].as_int().and_then(|data| atom_op.as_atom1(data))
+                {
+                    b.push_op(OpAtom1 {
+                        dst: dst.into(),
+                        data_type: DataType::i(intrin.def.bit_size),
+                        atom_op: atom1_op,
+                        addr,
+                        offset: 0,
+                    });
+                } else {
+                    b.push_op(OpAtom {
+                        dst: dst.into(),
+                        data_type: DataType::i(intrin.def.bit_size),
+                        atom_op,
+                        data,
+                        addr,
+                        offset: 0,
+                    });
+                }
+            }
+            nir_intrinsic_global_atomic_swap => {
+                let addr = self.get_src(&srcs[0]);
+                let cmpr = self.get_src_ssa(&srcs[1]);
+                let data = self.get_src_ssa(&srcs[2]);
+                let data = SSARef::from_iter(
+                    data.iter().cloned().chain(cmpr.iter().cloned()),
+                );
+                let dst = self.alloc_ssa(b, &intrin.def).into();
+                b.push_op(OpACmpXchg {
+                    dst,
+                    data_type: DataType::i(intrin.def.bit_size),
+                    data: data.into(),
+                    addr,
+                    offset: 0,
+                });
+            }
             nir_intrinsic_lea_buf_pan => {
                 let handle = self.get_src(&srcs[0]);
                 let index = self.get_src(&srcs[1]);

src/panfrost/compiler/kraid/data_type.rs
@@ -70,6 +70,7 @@ pub enum PartialDataType {
     U64,
     V2A16,
     V2F16,
+    V2IN,
     V2I8,
     V2I16,
     V2S8,
@@ -87,6 +88,7 @@ pub enum PartialDataType {
     V3U16,
     V2A32,
     V2F32,
+    V2I32,
     V2S32,
     V2U32,
     V4A16,
@@ -102,6 +104,7 @@ pub enum PartialDataType {
     V4F32,
     V4S32,
     V4U32,
+    V2I64,
 }
 
 impl PartialDataType {
@@ -190,6 +193,7 @@ pub enum DataType {
     V3U16,
     V2A32,
     V2F32,
+    V2I32,
     V2S32,
     V2U32,
     V4A16,
@@ -205,6 +209,7 @@ pub enum DataType {
     V4F32,
     V4S32,
     V4U32,
+    V2I64,
 }
 
 impl DataType {

src/panfrost/compiler/kraid/encode_v9.rs
@@ -493,6 +493,147 @@ impl From<MemAccess> for AccessStoreM {
     }
 }
 
+impl V9Instr for OpACmpXchg {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            Acmpxchg::get_info(self.data_type, arch),
+            src_map! {
+                sr_src: data,
+                src0: addr,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        e.encode(Acmpxchg {
+            variant: self.data_type.try_into().unwrap(),
+            message_slot_index: e.get_msg_slot_idx().unwrap(),
+            offset: self.offset,
+            sr_dst: op_encode_sr_write(self, &self.dst),
+            sr_src: op_encode_sr_read(self, &self.data),
+            src0: op_encode_src(self, &self.addr),
+        })
+    }
+}
+
+impl TryFrom<AtomOp> for AtomOperationM {
+    type Error = &'static str;
+
+    fn try_from(atom_op: AtomOp) -> Result<AtomOperationM, &'static str> {
+        match atom_op {
+            AtomOp::IAdd => Ok(AtomOperationM::Aadd),
+            AtomOp::SMin => Ok(AtomOperationM::Asmin),
+            AtomOp::SMax => Ok(AtomOperationM::Asmax),
+            AtomOp::UMin => Ok(AtomOperationM::Aumin),
+            AtomOp::UMax => Ok(AtomOperationM::Aumax),
+            AtomOp::And => Ok(AtomOperationM::Aand),
+            AtomOp::Or => Ok(AtomOperationM::Aor),
+            AtomOp::Xor => Ok(AtomOperationM::Axor),
+            AtomOp::Xchg => Err("Xchg is not a standard ATOM op"),
+        }
+    }
+}
+
+impl From<Atom1Op> for Atom1OperationM {
+    fn from(atom_op: Atom1Op) -> Atom1OperationM {
+        match atom_op {
+            Atom1Op::Dec => Atom1OperationM::Adec,
+            Atom1Op::Inc => Atom1OperationM::Ainc,
+            Atom1Op::Or1 => Atom1OperationM::Aor1,
+            Atom1Op::SMax1 => Atom1OperationM::Asmax1,
+            Atom1Op::UMax1 => Atom1OperationM::Aumax1,
+        }
+    }
+}
+
+impl V9Instr for OpAtom {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            if self.dst.dst_ref.is_none() {
+                assert_ne!(self.atom_op, AtomOp::Xchg);
+                Atom::get_info(self.data_type, arch)
+            } else if self.atom_op == AtomOp::Xchg {
+                Axchg::get_info(self.data_type, arch)
+            } else {
+                AtomReturn::get_info(self.data_type, arch)
+            },
+            src_map! {
+                sr_src: data,
+                src0: addr,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        if self.dst.dst_ref.is_none() {
+            assert_ne!(self.atom_op, AtomOp::Xchg);
+            e.encode(Atom {
+                variant: self.data_type.try_into().unwrap(),
+                atom_opc: self.atom_op.try_into().unwrap(),
+                message_slot_index: e.get_msg_slot_idx().unwrap(),
+                offset: self.offset,
+                sr_src: op_encode_sr_read(self, &self.data),
+                src0: op_encode_src(self, &self.addr),
+            })
+        } else if self.atom_op == AtomOp::Xchg {
+            e.encode(Axchg {
+                variant: self.data_type.try_into().unwrap(),
+                message_slot_index: e.get_msg_slot_idx().unwrap(),
+                offset: self.offset,
+                sr_dst: op_encode_sr_write(self, &self.dst),
+                sr_src: op_encode_sr_read(self, &self.data),
+                src0: op_encode_src(self, &self.addr),
+            })
+        } else {
+            e.encode(AtomReturn {
+                variant: self.data_type.try_into().unwrap(),
+                atom_opc: self.atom_op.try_into().unwrap(),
+                message_slot_index: e.get_msg_slot_idx().unwrap(),
+                offset: self.offset,
+                sr_dst: op_encode_sr_write(self, &self.dst),
+                sr_src: op_encode_sr_read(self, &self.data),
+                src0: op_encode_src(self, &self.addr),
+            })
+        }
+    }
+}
+
+impl V9Instr for OpAtom1 {
+    fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
+        V9InstrInfo::from_isa(
+            if self.dst.dst_ref.is_none() {
+                Atom1::get_info(self.data_type, arch)
+            } else {
+                Atom1Return::get_info(self.data_type, arch)
+            },
+            src_map! {
+                src0: addr,
+            },
+        )
+    }
+
+    fn encode(&self, e: V9Encoder) -> EncodedInstr {
+        if self.dst.dst_ref.is_none() {
+            e.encode(Atom1 {
+                variant: self.data_type.try_into().unwrap(),
+                atom1_opc: self.atom_op.try_into().unwrap(),
+                message_slot_index: e.get_msg_slot_idx().unwrap(),
+                offset: self.offset,
+                src0: op_encode_src(self, &self.addr),
+            })
+        } else {
+            e.encode(Atom1Return {
+                variant: self.data_type.try_into().unwrap(),
+                atom1_opc: self.atom_op.try_into().unwrap(),
+                message_slot_index: e.get_msg_slot_idx().unwrap(),
+                offset: self.offset,
+                sr_dst: op_encode_sr_write(self, &self.dst),
+                src0: op_encode_src(self, &self.addr),
+            })
+        }
+    }
+}
+
 impl V9Instr for OpBranch {
     fn get_info(&self, arch: u8) -> Option<V9InstrInfo> {
         V9InstrInfo::from_isa(Branch::get_info((), arch), src_map! {src0: cond})
@@ -2011,6 +2152,9 @@ impl V9Instr for OpTexSingle {
 macro_rules! v9_op_match_else {
     ($op: expr, |$x: ident| $y: expr, $z: expr) => {
         match $op {
+            Op::ACmpXchg($x) => $y,
+            Op::Atom($x) => $y,
+            Op::Atom1($x) => $y,
             Op::BitRev($x) => $y,
             Op::Branch($x) => $y,
             Op::Clz($x) => $y,

src/panfrost/compiler/kraid/ir.rs
@@ -798,6 +798,10 @@ impl DstRef {
         }
     }
 
+    pub fn is_none(&self) -> bool {
+        matches!(self, DstRef::None)
+    }
+
     pub fn bytes_written(&self) -> u8 {
         match self {
             DstRef::None => 0,

src/panfrost/compiler/kraid/ops.rs
@@ -38,6 +38,161 @@ macro_rules! bool_as_mod_str {
 
 #[repr(C)]
 #[derive(Clone, Opcode)]
+#[variants(data_type in [I32, I64])]
+pub struct OpACmpXchg {
+    pub dst: Dst,
+    pub data_type: DataType,
+
+    #[src_type(V2IN)]
+    pub data: Src,
+
+    #[src_type(I64)]
+    pub addr: Src,
+    pub offset: u8,
+}
+
+impl fmt::Display for OpACmpXchg {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} = ACMPXCHG{} {} {} #{}",
+            self.dst,
+            self.data_type,
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.addr),
+            self.offset
+        )
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum AtomOp {
+    // TODO: Model 64-bit atomics with 32-bit data
+    IAdd,
+    SMin,
+    SMax,
+    UMin,
+    UMax,
+    And,
+    Or,
+    Xor,
+    Xchg,
+}
+
+impl AtomOp {
+    pub fn as_atom1(self, data: i64) -> Option<Atom1Op> {
+        match (self, data) {
+            (AtomOp::IAdd, 1) => Some(Atom1Op::Inc),
+            (AtomOp::IAdd, -1) => Some(Atom1Op::Dec),
+            (AtomOp::Or, 1) => Some(Atom1Op::Or1),
+            (AtomOp::SMax, 1) => Some(Atom1Op::SMax1),
+            (AtomOp::UMax, 1) => Some(Atom1Op::UMax1),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for AtomOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AtomOp::IAdd => write!(f, ".iadd"),
+            AtomOp::SMin => write!(f, ".smin"),
+            AtomOp::SMax => write!(f, ".smax"),
+            AtomOp::UMin => write!(f, ".umin"),
+            AtomOp::UMax => write!(f, ".umax"),
+            AtomOp::And => write!(f, ".and"),
+            AtomOp::Or => write!(f, ".or"),
+            AtomOp::Xor => write!(f, ".xor"),
+            AtomOp::Xchg => write!(f, ".xchg"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Atom1Op {
+    Inc,
+    Dec,
+    Or1,
+    SMax1,
+    UMax1,
+}
+
+impl fmt::Display for Atom1Op {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Atom1Op::Inc => write!(f, ".inc"),
+            Atom1Op::Dec => write!(f, ".dec"),
+            Atom1Op::Or1 => write!(f, ".or1"),
+            Atom1Op::SMax1 => write!(f, ".smax1"),
+            Atom1Op::UMax1 => write!(f, ".umax1"),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+#[variants(data_type in [I32, I64])]
+pub struct OpAtom {
+    pub dst: Dst,
+    pub data_type: DataType,
+
+    pub atom_op: AtomOp,
+    pub data: Src,
+
+    #[src_type(I64)]
+    pub addr: Src,
+    pub offset: u8,
+}
+
+impl fmt::Display for OpAtom {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if !self.dst.dst_ref.is_none() {
+            write!(f, "{} = ", self.dst)?;
+        }
+        write!(
+            f,
+            "ATOM.{}{} {} {} #{}",
+            self.data_type,
+            self.atom_op,
+            self.fmt_src(&self.data),
+            self.fmt_src(&self.addr),
+            self.offset
+        )
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
+#[variants(data_type in [I32, I64])]
+pub struct OpAtom1 {
+    pub dst: Dst,
+    pub data_type: DataType,
+
+    pub atom_op: Atom1Op,
+
+    #[src_type(I64)]
+    pub addr: Src,
+    pub offset: u8,
+}
+
+impl fmt::Display for OpAtom1 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if !self.dst.dst_ref.is_none() {
+            write!(f, "{} = ", self.dst)?;
+        }
+        write!(
+            f,
+            "ATOM1.{}{} {} #{}",
+            self.data_type,
+            self.atom_op,
+            self.fmt_src(&self.addr),
+            self.offset
+        )
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Opcode)]
 pub struct OpBitRev {
     #[dst_type(I32)]
     pub dst: Dst,
@@ -2304,6 +2459,9 @@ impl fmt::Display for OpTexSingle {
 
 #[derive(Clone, FromVariants, Opcode)]
 pub enum Op {
+    ACmpXchg(Box<OpACmpXchg>),
+    Atom(Box<OpAtom>),
+    Atom1(Box<OpAtom1>),
     BitRev(Box<OpBitRev>),
     Branch(Box<OpBranch>),
     Clz(Box<OpClz>),
@@ -2383,7 +2541,13 @@ impl Op {
     pub fn can_eliminate(&self) -> bool {
         !matches!(
             self,
-            Op::Branch(_) | Op::RegOut(_) | Op::Store(_) | Op::StCvt(_)
+            Op::ACmpXchg(_)
+                | Op::Atom(_)
+                | Op::Atom1(_)
+                | Op::Branch(_)
+                | Op::RegOut(_)
+                | Op::Store(_)
+                | Op::StCvt(_)
         )
     }
 }

src/panfrost/compiler/kraid/validate.rs
@@ -66,6 +66,10 @@ fn validate_instr(instr: &Instr, ssa_vals: &mut FxHashSet<SSAValue>) {
     }
 
     for (dst, dst_type) in instr.dsts_types() {
+        if dst.dst_ref.is_none() {
+            continue;
+        }
+
         let dst_type_bits = dst_type.bits();
         let dst_type_comps = dst_type.comps();
         if dst_type_bits >= 32 {

commit b5d30915b2b4a96d82d0e03b120bc9d1a0026c6a

kraid/nir: Always use a destination for AtomOp::Xchg
Fixes: 865956662a76 ("kraid: Add OpAtom* and plumb them through")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42741>

src/panfrost/compiler/kraid/nir.rs
@@ -1160,7 +1160,11 @@ impl<'a> ShaderFromNir<'a> {
                     nir_atomic_op_xchg => AtomOp::Xchg,
                     _ => panic!("Unknown nir_atomic_op"),
                 };
-                let dst = if intrin.def.components_read() == 0 {
+
+                // There is no no-return version of AXCHG
+                let dst = if atom_op != AtomOp::Xchg
+                    && intrin.def.components_read() == 0
+                {
                     DstRef::None
                 } else {
                     self.alloc_ssa(b, &intrin.def).into()