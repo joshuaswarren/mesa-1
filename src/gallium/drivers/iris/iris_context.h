joshuaswarren/mesa-1

commit b7ccab2a0ddfba59b26a0468f6595711ac168a0b

intel/common: Add function to compute optimal compute engine async thread limits
Spec has several restrictions to the values we program to compute
engine async thread limits.
Without those we risk hit deadlocks, so here adding a function to
return the optimal value based on those restrictions.

Cc: stable
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35563>

src/intel/common/intel_common.c
@@ -30,3 +30,198 @@ void intel_common_update_device_info(int fd, struct intel_device_info *devinfo)
 
    free(engine_info);
 }
+
+void
+intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo,
+                                         uint32_t hw_threads_in_wg,
+                                         bool slm_or_barrier_enabled,
+                                         uint8_t *ret_pixel_async_compute_thread_limit,
+                                         uint8_t *ret_z_pass_async_compute_thread_limit,
+                                         uint8_t *ret_np_z_async_throttle_settings)
+{
+   /* Spec recommended SW values.
+    * IMPORTANT: values set to this variables are HW values
+    */
+   uint8_t pixel_async_compute_thread_limit = 2;
+   uint8_t z_pass_async_compute_thread_limit = 0;
+   uint8_t np_z_async_throttle_settings = 0;
+   bool has_vrt = devinfo->verx10 >= 300;
+
+   /* When VRT is enabled async threads limits don't have effect */
+   if (!slm_or_barrier_enabled || has_vrt) {
+      *ret_pixel_async_compute_thread_limit = pixel_async_compute_thread_limit;
+      *ret_z_pass_async_compute_thread_limit = z_pass_async_compute_thread_limit;
+      *ret_np_z_async_throttle_settings = np_z_async_throttle_settings;
+      return;
+   }
+
+   if (devinfo->verx10 >= 200) {
+      /* Spec give us a table with Throttle value | SIMD | MAX API threads(LWS).
+       * HW threads = MAX API threads(LWS) / SIMD
+       */
+      switch (hw_threads_in_wg) {
+      case 0 ... 2:
+         /* Max 2 */
+         pixel_async_compute_thread_limit = 1;
+         break;
+      case 3 ... 8:
+         /* Max 8 */
+         pixel_async_compute_thread_limit = 2;
+         break;
+      case 9 ... 16:
+         /* Max 16 */
+         pixel_async_compute_thread_limit = 3;
+         break;
+      case 17 ... 24:
+         /* Max 24 */
+         pixel_async_compute_thread_limit = 4;
+         break;
+      case 25 ... 32:
+         /* Max 32 */
+         pixel_async_compute_thread_limit = 5;
+         break;
+      case 33 ... 40:
+         /* Max 40 */
+         pixel_async_compute_thread_limit = 6;
+         break;
+      case 41 ... 48:
+         /* Max 48 */
+         pixel_async_compute_thread_limit = 7;
+         break;
+      default:
+         /* No limit applied */
+         pixel_async_compute_thread_limit = 0;
+      }
+
+      switch (hw_threads_in_wg) {
+      case 0 ... 32:
+         /* Max 32 */
+         z_pass_async_compute_thread_limit = 5;
+         break;
+      case 33 ... 40:
+         /* Max 40 */
+         z_pass_async_compute_thread_limit = 4;
+         break;
+      case 41 ... 48:
+         /* Max 48 */
+         z_pass_async_compute_thread_limit = 3;
+         break;
+      case 49 ... 56:
+         /* Max 56 */
+         z_pass_async_compute_thread_limit = 2;
+         break;
+      case 57 ... 60:
+         /* Max 60 */
+         z_pass_async_compute_thread_limit = 0;
+         break;
+      default:
+         /* No limit applied */
+         z_pass_async_compute_thread_limit = 1;
+      }
+
+      switch (hw_threads_in_wg) {
+      case 0 ... 32:
+         /* Max 32 */
+         np_z_async_throttle_settings = 1;
+         break;
+      case 33 ... 40:
+         /* Max 40 */
+         np_z_async_throttle_settings = 2;
+         break;
+      case 41 ... 48:
+         /* Max 48 */
+         np_z_async_throttle_settings = 3;
+         break;
+      default:
+         /* Use the same settings as the Pixel shader Async compute setting,
+          * for values >= async compute settings disables the limits
+          */
+         np_z_async_throttle_settings = 0;
+      }
+   } else {
+      switch (hw_threads_in_wg) {
+      case 0 ... 4:
+         /* Max 2 */
+         pixel_async_compute_thread_limit = 1;
+         break;
+      case 5 ... 16:
+         /* Max 8 */
+         pixel_async_compute_thread_limit = 2;
+         break;
+      case 17 ... 32:
+         /* Max 16 */
+         pixel_async_compute_thread_limit = 3;
+         break;
+      case 33 ... 48:
+         /* Max 24 */
+         pixel_async_compute_thread_limit = 4;
+         break;
+      case 49 ... 64:
+         /* Max 32 */
+         pixel_async_compute_thread_limit = 5;
+         break;
+      case 65 ... 80:
+         /* Max 40 */
+         pixel_async_compute_thread_limit = 6;
+         break;
+      case 81 ... 96:
+         /* Max 48 */
+         pixel_async_compute_thread_limit = 7;
+         break;
+      default:
+         /* No limit applied */
+         pixel_async_compute_thread_limit = 0;
+      }
+
+      switch (hw_threads_in_wg) {
+      case 0 ... 64:
+         /* Max 32 */
+         z_pass_async_compute_thread_limit = 5;
+         break;
+      case 65 ... 80:
+         /* Max 40 */
+         z_pass_async_compute_thread_limit = 4;
+         break;
+      case 81 ... 96:
+         /* Max 48 */
+         z_pass_async_compute_thread_limit = 3;
+         break;
+      case 97 ... 112:
+         /* Max 56 */
+         z_pass_async_compute_thread_limit = 2;
+         break;
+      case 113 ... 120:
+         /* Max 60 */
+         z_pass_async_compute_thread_limit = 0;
+         break;
+      default:
+         /* Max 64/No limit applied */
+         z_pass_async_compute_thread_limit = 1;
+      }
+
+      switch (hw_threads_in_wg) {
+      case 0 ... 64:
+         /* Max 32 */
+         np_z_async_throttle_settings = 1;
+         break;
+      case 65 ... 80:
+         /* Max 40 */
+         np_z_async_throttle_settings = 2;
+         break;
+      case 81 ... 96:
+         /* Max 48 */
+         np_z_async_throttle_settings = 3;
+         break;
+      default:
+         /* Use the same settings as the Pixel shader Async compute setting,
+          * for values >= async compute settings disables the limits
+          */
+         np_z_async_throttle_settings = 0;
+      }
+   }
+
+   assert(np_z_async_throttle_settings != 0 || pixel_async_compute_thread_limit == 0);
+   *ret_pixel_async_compute_thread_limit = pixel_async_compute_thread_limit;
+   *ret_z_pass_async_compute_thread_limit = z_pass_async_compute_thread_limit;
+   *ret_np_z_async_throttle_settings = np_z_async_throttle_settings;
+}

src/intel/common/intel_common.h
@@ -8,3 +8,10 @@
 #include "dev/intel_device_info.h"
 
 void intel_common_update_device_info(int fd, struct intel_device_info *devinfo);
+
+void
+intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo,
+                                         uint32_t hw_threads_in_wg, bool slm_or_barrier_enabled,
+                                         uint8_t *ret_pixel_async_compute_thread_limit,
+                                         uint8_t *ret_z_pass_async_compute_thread_limit,
+                                         uint8_t *ret_np_z_async_throttle_settings);

commit 8a49edc7913ad9a3bcf39f66acc351886cc9de96

intel: Fix async compute thread limit
Changing STATE_COMPUTE_MODE values force us to re-emit this NP-state
packet before every dispatch and toggle throttle values based on SLM or
barriers enabled to prevent deadlock issues.

Setting AsyncComputeThreadLimit to - No limit applied doesn't mean we
are not running RCS and CCS in parallel. They both would still progress
in parallel. In case if CCS hog up all EU's, RCS has to wait for free
once to pick up for execution and vice-versa.

With VRT, these bits are ignored.

These limit talks about when 3D pipe is active or Z-pass (fixed function
units) running and impose maximum number of active Compute CS threads to
run in DSS.

For example with Max 8, we could have max 8 threads * 8 Xe cores which
gives us 64threads active for compute. But if there is a cross thread
dependency and if we don't launch enough threads, we will hit deadlock.

we found such a case in Witcher3 workload.

   Compute walker tries to dispatch 114x1x1 threads with 2 threads per
   thread groups in SIMD 16 mode. 1 thread group does UGM fetch and
   another does UGM fence. Memory update is supposed to happen from
   differnt thread groups of the same walker.

   Since we set the max of 8 threads per DSS for compute engine while
   running things in async mode, we were not launching other thread
   groups which leads to deadlock.

Best way to deal with such kind of scenario is don't apply any limit
and use the default setting based on if shader uses fence.

Start with default value settings for STATE_COMPUTE_MODE instead of
recommended programming values. Bspec needs to be updated to cover above
condition which is missing currently.

Also, emitting NP-state always before each walker has cost to it
(probably equivalent to CS stall) so it's better not to change values.

This commit left the existing infrastructure in place in case in future
if we find some workload benefit from tweaked values.

Fixes: b7ccab2a0ddf ("intel/common: Add function to compute optimal compute engine async thread limits")

src/gallium/drivers/iris/iris_context.h
@@ -351,6 +351,7 @@ struct iris_cs_data {
    enum intel_compute_walk_order walk_order;
 
    bool uses_barrier;
+   bool uses_fence;
    bool uses_sampler;
    bool first_param_is_builtin_subgroup_id;
 };

src/gallium/drivers/iris/iris_program.c
@@ -167,6 +167,7 @@ iris_apply_brw_cs_prog_data(struct iris_compiled_shader *shader,
    iris->generate_local_id = brw->generate_local_id;
    iris->walk_order        = brw->walk_order;
    iris->uses_barrier      = brw->uses_barrier;
+   iris->uses_fence        = brw->uses_fence;
    iris->uses_sampler      = brw->uses_sampler;
    iris->prog_mask         = brw->prog_mask;
 

src/gallium/drivers/iris/iris_state.c
@@ -1574,7 +1574,7 @@ iris_init_compute_context(struct iris_batch *batch)
 
    uint8_t pixel_async_compute_thread_limit, z_pass_async_compute_thread_limit,
            np_z_async_throttle_settings;
-   intel_compute_engine_async_threads_limit(devinfo, 0, false,
+   intel_compute_engine_async_threads_limit(devinfo, 0, false, false,
                                             &pixel_async_compute_thread_limit,
                                             &z_pass_async_compute_thread_limit,
                                             &np_z_async_throttle_settings);
@@ -9108,6 +9108,7 @@ iris_upload_compute_walker(struct iris_context *ice,
 
    intel_compute_engine_async_threads_limit(devinfo, dispatch.threads,
                                             slm_or_barrier_enabled,
+                                            cs_data->uses_fence,
                                             &pixel_async_compute_thread_limit,
                                             &z_pass_async_compute_thread_limit,
                                             &np_z_async_throttle_settings);

src/intel/blorp/blorp_genX_exec_brw.h
@@ -1998,6 +1998,7 @@ blorp_exec_compute(struct blorp_batch *batch, const struct blorp_params *params)
 
    intel_compute_engine_async_threads_limit(devinfo, dispatch.threads,
                                             slm_or_barrier_enabled,
+                                            cs_prog_data->uses_fence,
                                             &pixel_async_compute_thread_limit,
                                             &z_pass_async_compute_thread_limit,
                                             &np_z_async_throttle_settings);

src/intel/common/intel_common.c
@@ -39,20 +39,48 @@ void
 intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo,
                                          uint32_t hw_threads_in_wg,
                                          bool slm_or_barrier_enabled,
+                                         bool uses_fence,
                                          uint8_t *ret_pixel_async_compute_thread_limit,
                                          uint8_t *ret_z_pass_async_compute_thread_limit,
                                          uint8_t *ret_np_z_async_throttle_settings)
 {
-   /* Spec recommended SW values.
+   /*
     * IMPORTANT: values set to this variables are HW values
+    *
+    * Bspec 45765: Pixel Async compute thread limit field states:
+    *
+    *    When SLM or barriers are enabled, the MAX_API_thread limit must be
+    *    less than or equal to the throttle limit to prevent deadlocks.
+    *
+    * These limit talks about when 3D pipe is active or Z-pass (fixed function
+    * units) running and impose maximum number of active Compute CS threads to
+    * run in DSS.
+    *
+    * For example with Max 8, we could have max 8 threads * 8 Xe cores which
+    * gives us 64threads active for compute. But if there is a cross thread
+    * dependency and if we don't launch enough threads, we will hit deadlock.
+    *
+    * we found such a case in Witcher3 workload.
+    *
+    *    Compute walker tries to dispatch 114x1x1 threads with 2 threads per
+    *    thread groups in SIMD 16 mode. 1 thread group does UGM fetch and
+    *    another does UGM fence. Memory update is supposed to happen from
+    *    differnt thread groups of the same walker.
+    *
+    *    Since we set the max of 8 threads per DSS for compute engine while
+    *    running things in async mode, we were not launching other thread
+    *    groups which leads to deadlock.
+    *
+    * Best way to deal with such kind of scenario is don't apply any limit
+    * and use the default setting.
     */
-   uint8_t pixel_async_compute_thread_limit = 2;
+   uint8_t pixel_async_compute_thread_limit = uses_fence ? 0 : 2;
    uint8_t z_pass_async_compute_thread_limit = 0;
    uint8_t np_z_async_throttle_settings = 0;
    bool has_vrt = devinfo->verx10 >= 300 && !INTEL_DEBUG(DEBUG_NO_VRT);
 
    /* When VRT is enabled async threads limits don't have effect */
-   if (!slm_or_barrier_enabled || has_vrt) {
+   if (!slm_or_barrier_enabled || has_vrt || uses_fence) {
       *ret_pixel_async_compute_thread_limit = pixel_async_compute_thread_limit;
       *ret_z_pass_async_compute_thread_limit = z_pass_async_compute_thread_limit;
       *ret_np_z_async_throttle_settings = np_z_async_throttle_settings;

src/intel/common/intel_common.h
@@ -12,6 +12,7 @@ void intel_common_update_device_info(int fd, struct intel_device_info *devinfo);
 void
 intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo,
                                          uint32_t hw_threads_in_wg, bool slm_or_barrier_enabled,
+                                         bool uses_fence,
                                          uint8_t *ret_pixel_async_compute_thread_limit,
                                          uint8_t *ret_z_pass_async_compute_thread_limit,
                                          uint8_t *ret_np_z_async_throttle_settings);

src/intel/vulkan/genX_cmd_compute.c
@@ -396,6 +396,7 @@ compute_update_async_threads_limit(struct anv_cmd_buffer *cmd_buffer,
 
    intel_compute_engine_async_threads_limit(devinfo, dispatch->threads,
                                             slm_or_barrier_enabled,
+                                            prog_data->uses_fence,
                                             &pixel_async_compute_thread_limit,
                                             &z_pass_async_compute_thread_limit,
                                             &np_z_async_throttle_settings);

src/intel/vulkan/genX_init_state.c
@@ -885,7 +885,7 @@ init_compute_queue_state(struct anv_queue *queue)
       cm.EnableVariableRegisterSizeAllocation = !INTEL_DEBUG(DEBUG_NO_VRT);
 #endif
 #if GFX_VER >= 20
-      cm.AsyncComputeThreadLimit = ACTL_Max8;
+      cm.AsyncComputeThreadLimit = ACTL_Disabled;
       cm.ZPassAsyncComputeThreadLimit = ZPACTL_Max60;
       cm.ZAsyncThrottlesettings = ZATS_DefertoAsyncComputeThreadLimit;
       cm.AsyncComputeThreadLimitMask = 0x7;
@@ -894,7 +894,7 @@ init_compute_queue_state(struct anv_queue *queue)
       cm.Mask2 = 0xffff;
       cm.UAVCoherencyMode = FlushDataportL1;
 #else
-      cm.PixelAsyncComputeThreadLimit = PACTL_Max24;
+      cm.PixelAsyncComputeThreadLimit = PACTL_Disabled;
       cm.ZPassAsyncComputeThreadLimit = ZPACTL_Max60;
       cm.PixelAsyncComputeThreadLimitMask = 0x7;
       cm.ZPassAsyncComputeThreadLimitMask = 0x7;

src/intel/vulkan/genX_simple_shader.c
@@ -583,6 +583,7 @@ genX(emit_simple_shader_dispatch)(struct anv_simple_shader *state,
 
       intel_compute_engine_async_threads_limit(devinfo, dispatch.threads,
                                                slm_or_barrier_enabled,
+                                               prog_data->uses_fence,
                                                &pixel_async_compute_thread_limit,
                                                &z_pass_async_compute_thread_limit,
                                                &np_z_async_throttle_settings);