joshuaswarren/mesa-1

commit af51b1209c516067883dd26338a38ad025a3252d

freedreno/decode: call show_descriptor() for UBO and SAMPLERs as well
With the additional shader info, we can determine when a given
descriptor is definitely not a UBO or sampler.

Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39636>

src/freedreno/decode/cffdec.c
@@ -1787,17 +1787,27 @@ dump_bindless_descriptors(bool is_compute, int level)
       unsigned desc_count = length / (16 * sizeof(uint32_t));
       for (unsigned desc_idx = 0; desc_idx < desc_count; desc_idx++) {
          if (memcmp(contents, empty_contents, sizeof(empty_contents))) {
-            printl(2, "%sUBO[%u]:\n", levels[level + 1], desc_idx);
-            dump_domain(contents, 2, level + 2, "A6XX_UBO");
+
+            if (show_descriptor(contents, 2, i + 1, desc_idx, "A6XX_UBO", "DESC_UBO")) {
+               printl(2, "%sUBO[%u]:\n", levels[level + 1], desc_idx);
+               dump_domain(contents, 2, level + 2, "A6XX_UBO");
+            }
 
             if ((6 <= options->info->chip) && (options->info->chip < 8)) {
                dump_tex_descriptor(contents, i + 1, desc_idx, level, "A6XX_TEX_MEMOBJ");
+
+               if (show_descriptor(contents, 16, i + 1, desc_idx, "A6XX_TEX_SAMP", "DESC_SAMPLER")) {
+                  printl(2, "%sSAMPLER[%u]:\n", levels[level + 1], desc_idx);
+                  dump_tex_samp(contents, STATE_SRC_BINDLESS, 1, level);
+               }
             } else if ((8 <= options->info->chip) && (options->info->chip < 9)) {
                dump_tex_descriptor(contents, i + 1, desc_idx, level, "A8XX_TEX_MEMOBJ");
-            }
 
-            printl(2, "%sSAMPLER[%u]:\n", levels[level + 1], desc_idx);
-            dump_tex_samp(contents, STATE_SRC_BINDLESS, 1, level);
+               if (show_descriptor(contents, 4, i + 1, desc_idx, "A8XX_TEX_SAMP", "DESC_SAMPLER")) {
+                  printl(2, "%sSAMPLER[%u]:\n", levels[level + 1], desc_idx);
+                  dump_tex_samp(contents, STATE_SRC_BINDLESS, 1, level);
+               }
+            }
          }
          contents += 16;
       }

src/freedreno/decode/cffdump-desc-handler.lua
@@ -8,6 +8,11 @@
 -- If it is ambiguous, be conservative and show the specified decoding.
 function show_descriptor(desc, type, pkt, base, idx)
 
+	if (type == r.desctype.DESC_SAMPLER) or (type == r.desctype.DESC_UBO) then
+		-- Everything below here only applies to TEX_MEMOBJ descriptors
+		return true
+	end
+
 	if type == r.desctype.DESC_BUFFER then
 		return r.a6xx_tex_type.A6XX_TEX_BUFFER == desc.TYPE
 	end

commit 60a95b03e2650aa41a4dae8863e2fc7506a8b16b

freedreno/decode: Fix shader stats in summary mode
We need to do an extra silent parse of updated shader stages, otherwise
we'll get the stats info from the previous draw (because register dump/
decode happens after the bindless descriptors are dumped).

Fixes: af51b1209c51 ("freedreno/decode: call show_descriptor() for UBO and SAMPLERs as well")
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42751>

src/freedreno/decode/cffdec.c
@@ -177,6 +177,20 @@ decode_shader_ir3(const uint32_t *dwords, uint32_t sizedwords, int level,
                         &shader_stats[stage]);
 }
 
+static void
+decode_shader_ir3_quiet(const uint32_t *dwords, uint32_t sizedwords, int level,
+                        enum mesa_shader_stage stage)
+{
+   static FILE *nullf;
+
+   if (!nullf)
+      nullf = fopen("/dev/null", "w");
+
+   try_disasm_a3xx_stat(dwords, sizedwords, level, nullf,
+                        options->info->chip * 100,
+                        &shader_stats[stage]);
+}
+
 struct shader_stats *
 get_shader_stats(enum mesa_shader_stage stage)
 {
@@ -1789,11 +1803,46 @@ dump_tex_const(const uint32_t *texconst, int num_unit, int level)
 }
 
 static void
+parse_shader_stats(const char *regname, enum mesa_shader_stage stage)
+{
+   uint32_t base = regbase(regname);
+
+   /* If it hasn't changed since last draw, nothing to do. */
+   if (!reg_written(base))
+      return;
+
+   uint64_t gpuaddr = reg_val(base + 1);
+   gpuaddr <<= 32;
+   gpuaddr |= reg_val(base);
+
+   void *buf = hostptr(gpuaddr);
+   if (!buf)
+      return;
+
+   uint32_t sizedwords = hostlen(gpuaddr) / 4;
+
+   decode_shader_ir3_quiet(buf, sizedwords, 0, stage);
+}
+
+static void
 dump_bindless_descriptors(bool is_compute, int level)
 {
    /* Skip for devices which do not support bindless: */
    if (options->info->chip < 6)
       return;
+
+   if (options->summary) {
+      /* Ensure that updated shader stages are parsed.. in summary mode
+       * this only happens when dumping reg values, which happens after
+       * this fxn is called, resulting in using stale shaders:
+       */
+      parse_shader_stats("SP_VS_BASE", MESA_SHADER_VERTEX);
+      parse_shader_stats("SP_HS_BASE", MESA_SHADER_TESS_CTRL);
+      parse_shader_stats("SP_DS_BASE", MESA_SHADER_TESS_EVAL);
+      parse_shader_stats("SP_GS_BASE", MESA_SHADER_GEOMETRY);
+      parse_shader_stats("SP_PS_BASE", MESA_SHADER_FRAGMENT);
+      parse_shader_stats("SP_CS_BASE", MESA_SHADER_COMPUTE);
+   }
 
    printl(2, "%sdraw[%i] bindless descriptors\n", levels[level], draw_count);
 