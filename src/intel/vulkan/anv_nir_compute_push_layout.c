joshuaswarren/mesa-1

commit e5ce40acadde6b8a60d5093f9db03afcc13ba4df

anv/brw: limit push constant promotion in vertex shaders
The uber shaders in DXVK are hitting the register limit from the
amount of payload data.

Ideally we want to implement BRW VS data pulling but it's not that
easy, for now limit the push constant data.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Cc: mesa-stable
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42665>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -650,11 +650,11 @@ anv_nir_compute_push_layout(nir_shader *nir,
        (nir->info.inputs_read & VARYING_BIT_PRIMITIVE_ID));
 
    unsigned n_push_ranges = 0;
-   unsigned total_push_regs = 0;
+   unsigned total_push_size = 0;
 
    if (push_constant_range.length > 0) {
       map->push_ranges[n_push_ranges++] = push_constant_range;
-      total_push_regs += push_constant_range.length;
+      total_push_size += push_constant_range.length * 32;
    }
 
    struct anv_push_range analysis_ranges[4] = {};
@@ -664,15 +664,20 @@ anv_nir_compute_push_layout(nir_shader *nir,
    }
 
    const unsigned max_push_buffers = needs_padding_per_primitive ? 3 : 4;
-   const unsigned max_push_regs = needs_padding_per_primitive ? 63 : 64;
+   const unsigned payload_size =
+      nir->info.stage == MESA_SHADER_VERTEX ?
+      brw_nir_vs_compute_payload_size(nir, devinfo) : 0;
+   const unsigned max_push_size =
+      MIN2(compiler->register_file_size - payload_size,
+           devinfo->grf_size * (needs_padding_per_primitive ? 63 : 64));
 
    for (unsigned i = 0; i < 4; i++) {
       struct anv_push_range *candidate_range = &analysis_ranges[i];
       if (n_push_ranges >= max_push_buffers)
          break;
 
-      if (candidate_range->length + total_push_regs > max_push_regs)
-         candidate_range->length = max_push_regs - total_push_regs;
+      if ((candidate_range->length * 32 + total_push_size) > max_push_size)
+         candidate_range->length = (max_push_size - total_push_size) / 32;
 
       if (candidate_range->length == 0)
          break;
@@ -683,7 +688,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
       }
 
       map->push_ranges[n_push_ranges++] = *candidate_range;
-      total_push_regs += candidate_range->length;
+      total_push_size += candidate_range->length * 32;
    }
 
    /* Pass a single-register push constant payload for the PS stage even if
@@ -715,7 +720,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
 
    if (!stage_has_inline_param(devinfo, nir->info.stage)) {
       assert(n_push_ranges <= 4);
-      assert(total_push_regs <= max_push_regs);
+      assert(total_push_size <= max_push_size);
    }
 
    struct lower_to_push_data_intel_state lower_state = {

src/intel/compiler/brw/brw_compiler.c
@@ -113,6 +113,9 @@ brw_compiler_create(void *mem_ctx, const struct intel_device_info *devinfo)
     * while letting almost all through to the backend for more detailed
     * throughput analysis.
     */
+   compiler->register_file_size = (devinfo->ver >= 30 ? XE3_MAX_GRF :
+                                   devinfo->ver >= 20 ? XE2_MAX_GRF :
+                                   BRW_MAX_GRF) * REG_SIZE;
    compiler->register_pressure_threshold = devinfo->ver >= 30 ? 268 : 134;
 
    nir_lower_int64_options int64_options =

src/intel/compiler/brw/brw_compiler.h
@@ -111,6 +111,14 @@ struct brw_compiler {
    int spilling_rate;
 
    /**
+    * Register file size
+    *
+    * Useful to calculate the amount of push data to deliver to a given
+    * shader.
+    */
+   unsigned register_file_size;
+
+   /**
     * We perform a quick register pressure estimate at the NIR level before
     * attempting backend compilation at various SIMD widths.  If the estimated
     * register pressure for a given SIMD width is beyond this threshold, we

src/intel/compiler/brw/brw_nir.c
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "brw_eu.h"
 #include "brw_nir.h"
 #include "brw_private.h"
 #include "brw_sampler.h"
@@ -4148,6 +4149,26 @@ brw_nir_quick_pressure_estimate(nir_shader *nir,
       simd_estimate[i] = DIV_ROUND_UP(convergent_size, 8 << base_simd) +
                          divergent_size * (1 << (i - base_simd));
    }
+}
+
+/**
+ * Provide a rough estimate of the payload size of a vertex shader based on
+ * the amount of inputs.
+ *
+ * This could be improved checking whether packing is enabled and what
+ * components are read.
+ */
+unsigned
+brw_nir_vs_compute_payload_size(nir_shader *nir,
+                                const struct intel_device_info *devinfo)
+{
+   unsigned n = 2; /* payload registers we can't avoid */
+
+   u_foreach_bit64(b, nir->info.inputs_read) {
+      n += 4 * ((BITFIELD64_BIT(b) & nir->info.dual_slot_inputs) ? 2 : 1);
+   }
+
+   return REG_SIZE * reg_unit(devinfo) * n;
 }
 
 unsigned

src/intel/compiler/brw/brw_nir.h
@@ -454,6 +454,9 @@ brw_nir_frag_convert_attrs_prim_to_vert_indirect(struct nir_shader *nir,
                                                  struct brw_compile_fs_params *params);
 
 unsigned
+brw_nir_vs_compute_payload_size(nir_shader *nir,
+                                const struct intel_device_info *devinfo);
+unsigned
 brw_nir_pack_vs_input(nir_shader *nir, struct brw_vs_prog_data *prog_data);
 
 bool brw_nir_opt_divergent_atomics(nir_shader *shader, enum brw_divergent_atomics_flags flags);

commit a6a2894f4157a7d073bb85574f9f8ff1939ec4c2

anv: fixup max push data delivered to shaders
We use to have a 64 * 32 limit and e5ce40acad ("anv/brw: limit push
constant promotion in vertex shaders") increased it to 64 * 64 on Xe2+
and that triggering issues with DXVK's ubershader.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: e5ce40acad ("anv/brw: limit push constant promotion in vertex shaders")
Acked-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42832>

src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -669,7 +669,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
       brw_nir_vs_compute_payload_size(nir, devinfo) : 0;
    const unsigned max_push_size =
       MIN2(compiler->register_file_size - payload_size,
-           devinfo->grf_size * (needs_padding_per_primitive ? 63 : 64));
+           32 * (needs_padding_per_primitive ? 63 : 64));
 
    for (unsigned i = 0; i < 4; i++) {
       struct anv_push_range *candidate_range = &analysis_ranges[i];