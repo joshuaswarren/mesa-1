joshuaswarren/mesa-1

commit b90e17432740db0e9c6703919bfe3ec6ad2b32a3

pvr, pco: add primitive support for terminate,demote_to_helper}_invocation
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37512>

src/imagination/pco/pco_nir.c
@@ -69,6 +69,8 @@ static const nir_shader_compiler_options nir_options = {
 
    .lower_interpolate_at = true,
 
+   .lower_helper_invocation = true,
+
    .max_unroll_iterations = 16,
 
    .io_options = nir_io_vectorizer_ignores_types,
@@ -537,6 +539,10 @@ void pco_preprocess_nir(pco_ctx *ctx, nir_shader *nir)
       };
       NIR_PASS(_, nir, nir_lower_sysvals_to_varyings, &sysvals_to_varyings);
       NIR_PASS(_, nir, nir_lower_helper_writes, true);
+      NIR_PASS(_, nir, nir_lower_is_helper_invocation);
+      NIR_PASS(_, nir, nir_lower_terminate_to_demote);
+      NIR_PASS(_, nir, nir_lower_halt_to_return);
+      NIR_PASS(_, nir, nir_lower_returns);
    }
 
    NIR_PASS(_, nir, nir_lower_system_values);

src/imagination/vulkan/pvr_device.c
@@ -202,6 +202,7 @@ static void pvr_physical_device_get_supported_extensions(
       .KHR_shader_float_controls = true,
       .KHR_shader_subgroup_extended_types = true,
       .KHR_spirv_1_4 = true,
+      .KHR_shader_terminate_invocation = true,
       .KHR_swapchain = PVR_USE_WSI_PLATFORM,
       .KHR_swapchain_mutable_format = PVR_USE_WSI_PLATFORM,
       .KHR_timeline_semaphore = true,
@@ -228,6 +229,7 @@ static void pvr_physical_device_get_supported_extensions(
       .EXT_queue_family_foreign = true,
       .EXT_separate_stencil_usage = true,
       .EXT_scalar_block_layout = true,
+      .EXT_shader_demote_to_helper_invocation = true,
       .EXT_texel_buffer_alignment = false,
       .EXT_tooling_info = true,
       .EXT_vertex_attribute_divisor = true,
@@ -447,6 +449,12 @@ static void pvr_physical_device_get_supported_features(
 
       /* VK_KHR_shader_expect_assume */
       .shaderExpectAssume = false,
+
+      /* VK_EXT_shader_demote_to_helper_invocation */
+      .shaderDemoteToHelperInvocation = true,
+
+      /* VK_KHR_shader_terminate_invocation */
+      .shaderTerminateInvocation = true,
 
       /* VK_KHR_present_id2 */
       .presentId2 = PVR_USE_WSI_PLATFORM,

commit f227fdc1e5ae3701cfb331a766b9f5295a5c5257

pco: lower sysvals early for fragment shaders
Ensure that SYSTEM_VALUE_HELPER_INVOCATION is lowered to an intrinsic
before the following NIR helper passes are run.

Fixes: b90e1743274 ("pvr, pco: add primitive support for terminate,demote_to_helper}_invocation")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42744>

src/imagination/pco/pco_nir.c
@@ -547,6 +547,7 @@ void pco_preprocess_nir(pco_ctx *ctx, nir_shader *nir)
          .primitive_id = nir->info.stage == MESA_SHADER_FRAGMENT,
       };
       NIR_PASS(_, nir, nir_lower_sysvals_to_varyings, &sysvals_to_varyings);
+      NIR_PASS(_, nir, nir_lower_system_values);
       NIR_PASS(_, nir, nir_lower_helper_writes, true);
       NIR_PASS(_, nir, nir_lower_is_helper_invocation);
       NIR_PASS(_, nir, nir_lower_terminate_to_demote);