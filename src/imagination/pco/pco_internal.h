joshuaswarren/mesa-1

commit 915d59b48e1230d7198b2316927211747e8a36c3

pco: Add pseudo instruction fencing for DITR and DITRP
Signed-off-by: Caius Moldovan <caius.moldovan@imgtec.com>
Reviewed-by: Simon Perretta <simon.perretta@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40724>

src/imagination/pco/pco_legalize.c
@@ -155,6 +155,29 @@ static inline bool xfer_op_mods(pco_instr *dest, pco_instr *src)
    return all_xfered;
 }
 
+/**
+ * \brief Legalize fence pseudo instruction.
+ *
+ * \param[in,out] instr PCO instr.
+ * \return True if progress was made.
+ */
+static bool legalize_fence(pco_instr *instr)
+{
+   if (instr->op != PCO_OP_FENCE)
+      return false;
+
+   pco_builder b =
+      pco_builder_create(instr->parent_func, pco_cursor_before_instr(instr));
+
+   pco_flush_p0(&b);
+   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z1);
+   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z0);
+
+   pco_instr_delete(instr);
+
+   return true;
+}
+
 static bool legalize_pseudo(pco_instr *instr)
 {
    switch (instr->op) {
@@ -396,6 +419,12 @@ static bool try_legalize_large_hwreg_offsets(pco_instr *instr,
    return true;
 }
 
+/**
+ * \brief Insert pseudo fences around DITR and DITRP instructions.
+ *
+ * \param[in,out] instr PCO instr.
+ * \return True if progress was made.
+ */
 static bool try_legalize_ditr_fence(pco_instr *instr)
 {
    if (instr->op != PCO_OP_DITR && instr->op != PCO_OP_DITRP)
@@ -403,14 +432,10 @@ static bool try_legalize_ditr_fence(pco_instr *instr)
 
    pco_builder b =
       pco_builder_create(instr->parent_func, pco_cursor_before_instr(instr));
-   pco_flush_p0(&b);
-   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z1);
-   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z0);
+   pco_fence(&b);
 
    b.cursor = pco_cursor_after_instr(instr);
-   pco_flush_p0(&b);
-   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z1);
-   pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z0);
+   pco_fence(&b);
 
    return true;
 }
@@ -457,6 +482,12 @@ bool pco_legalize(pco_shader *shader)
       }
    }
 
+   pco_foreach_func_in_shader (func, shader) {
+      pco_foreach_instr_in_func_safe (instr, func) {
+         progress |= legalize_fence(instr);
+      }
+   }
+
    shader->is_legalized = true;
    return progress;
 }
@@ -479,5 +510,10 @@ bool pco_post_ra_legalize(pco_shader *shader)
       }
    }
 
+   pco_foreach_func_in_shader (func, shader) {
+      pco_foreach_instr_in_func_safe (instr, func) {
+         progress |= legalize_fence(instr);
+      }
+   }
    return progress;
 }

src/imagination/pco/pco_ops.py
@@ -518,6 +518,7 @@ O_MOV = pseudo_op('mov', OM_ALU, 1, 1)
 O_MOV_OFFSET = pseudo_op('mov.offset', OM_ALU + [OM_OFFSET_SD], 1, 2)
 O_VEC = pseudo_op('vec', [OM_EXEC_CND], 1, VARIABLE)
 O_COMP = pseudo_op('comp', [], 1, 2)
+O_FENCE = pseudo_op('fence')
 
 O_OP_ATOMIC_OFFSET = pseudo_op('op.atomic.offset', OM_ALU_ATOMEXT + [OM_ATOM_OP], 2, 4, [], [[RM_ABS, RM_NEG], [RM_ABS, RM_NEG]])
 

commit bad07fe0819fac3894d17e8476bf0ec27b07f468

pco: allow fence ops to be legalized if they come last in a block
Fixes: 915d59b48e1 ("pco: Add pseudo instruction fencing for DITR and DITRP")
Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
Acked-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42744>

src/imagination/pco/pco_internal.h
@@ -1508,6 +1508,18 @@ static inline pco_instr *pco_last_instr(pco_block *block)
 }
 
 /**
+ * \brief Returns whether an instruction is the last instruction in its block.
+ *
+ * \param[in] instr The instruction.
+ * \return True if the instruction is the last instruction in its block.
+ */
+static inline bool pco_is_last_instr(pco_instr *instr)
+{
+   pco_instr *last_instr = pco_last_instr(instr->parent_block);
+   return instr == last_instr;
+}
+
+/**
  * \brief Returns the next instruction.
  *
  * \param[in] instr The current instruction.

src/imagination/pco/pco_legalize.c
@@ -164,7 +164,12 @@ static inline bool xfer_op_mods(pco_instr *dest, pco_instr *src)
 static bool legalize_fence(pco_instr *instr)
 {
    pco_builder b =
-      pco_builder_create(instr->parent_func, pco_cursor_before_instr(instr));
+      pco_builder_create(instr->parent_func, pco_cursor_after_instr(instr));
+
+   if (pco_is_last_instr(instr)) {
+      pco_nop(&b);
+      b.cursor = pco_cursor_after_instr(instr);
+   }
 
    pco_flush_p0(&b);
    pco_br_next(&b, .exec_cnd = PCO_EXEC_CND_E1_Z1);