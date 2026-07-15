joshuaswarren/mesa-1

commit 67b242482e3040a48bdc195e1fa24bb0b948c603

intel: Fix multipolygon flags in INTEL_SIMD_DEBUG default handling
We weren't including the multipolygon modes when checking if any modes
were set for a stage.  But we currently want them disabled by default.

Add a comment clarifying that we're initializing the default modes to
allow when no flags are specified.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41587>

src/intel/dev/intel_debug.c
@@ -180,8 +180,7 @@ intel_debug_flag_for_shader_stage(mesa_shader_stage stage)
    return flags[stage];
 }
 
-#define DEBUG_FS_SIMD  (DEBUG_FS_SIMD8  | DEBUG_FS_SIMD16  | \
-                        DEBUG_FS_SIMD32)
+#define DEBUG_FS_SIMD  BITFIELD_MASK(DEBUG_CS_SIMD8)
 #define DEBUG_CS_SIMD  (DEBUG_CS_SIMD8  | DEBUG_CS_SIMD16  | DEBUG_CS_SIMD32)
 #define DEBUG_TS_SIMD  (DEBUG_TS_SIMD8  | DEBUG_TS_SIMD16  | DEBUG_TS_SIMD32)
 #define DEBUG_MS_SIMD  (DEBUG_MS_SIMD8  | DEBUG_MS_SIMD16  | DEBUG_MS_SIMD32)
@@ -256,8 +255,11 @@ process_intel_debug_variable_once(void)
    intel_debug_bkp_after_dispatch_count =
       debug_get_num_option("INTEL_DEBUG_BKP_AFTER_DISPATCH_COUNT", 0);
 
+   /* If INTEL_SIMD_DEBUG doesn't specify any options for a stage, then all
+    * are allowed, except FS currently disables multipolygon modes by default.
+    */
    if (!(intel_simd & DEBUG_FS_SIMD))
-      intel_simd |=   DEBUG_FS_SIMD;
+      intel_simd |=   DEBUG_FS_SIMD8 | DEBUG_FS_SIMD16 | DEBUG_FS_SIMD32;
    if (!(intel_simd & DEBUG_CS_SIMD))
       intel_simd |=   DEBUG_CS_SIMD;
    if (!(intel_simd & DEBUG_TS_SIMD))

commit d68e8288ab2bcb2687b587df34de2aac403bdcea

intel: Fix DEBUG_FS_SIMD mask
Spell out the FS modes like the sibling DEBUG_{CS,TS,MS,RT}_SIMD masks.

Fixes: 67b242482e3 ("intel: Fix multipolygon flags in INTEL_SIMD_DEBUG default handling")

Signed-off-by: Michael Cheng <michael.cheng@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42996>

src/intel/dev/intel_debug.c
@@ -180,7 +180,8 @@ intel_debug_flag_for_shader_stage(mesa_shader_stage stage)
    return flags[stage];
 }
 
-#define DEBUG_FS_SIMD  BITFIELD_MASK(DEBUG_CS_SIMD8)
+#define DEBUG_FS_SIMD  (DEBUG_FS_SIMD8   | DEBUG_FS_SIMD16  | DEBUG_FS_SIMD32 | \
+                        DEBUG_FS_SIMD2X8 | DEBUG_FS_SIMD4X8 | DEBUG_FS_SIMD2X16)
 #define DEBUG_CS_SIMD  (DEBUG_CS_SIMD8  | DEBUG_CS_SIMD16  | DEBUG_CS_SIMD32)
 #define DEBUG_TS_SIMD  (DEBUG_TS_SIMD8  | DEBUG_TS_SIMD16  | DEBUG_TS_SIMD32)
 #define DEBUG_MS_SIMD  (DEBUG_MS_SIMD8  | DEBUG_MS_SIMD16  | DEBUG_MS_SIMD32)