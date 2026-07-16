joshuaswarren/mesa-1

commit b9efce45376dde5917ef06718e2206bbddfda8b3

venus: extract sync feedback host write and query
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42617>

src/virtio/vulkan/vn_feedback.c
@@ -561,7 +561,7 @@ vn_sync_feedback_cmd_alloc(struct vn_device *dev,
    return sfb_cmd;
 }
 
-void
+static void
 vn_sync_feedback_cmd_free(struct vn_device *dev,
                           struct vn_sync_feedback_cmd *sfb_cmd)
 {
@@ -615,6 +615,69 @@ vn_sync_feedback_command(struct vn_device *dev,
    assert(sfb_cmd_handle != VK_NULL_HANDLE);
 
    return sfb_cmd_handle;
+}
+
+bool
+vn_sync_feedback_query(struct vn_device *dev,
+                       struct vn_sync_feedback *sfb,
+                       uint64_t *out_counter)
+{
+   bool signaled_counter_updated = false;
+
+   /* If we are here when feedback is suspended, signaled_counter has been
+    * updated to the suspended counter value which must be greater than the
+    * feedback counter read from the feedback slot.
+    */
+   simple_mtx_lock(&sfb->counter_mtx);
+   const uint64_t counter = vn_feedback_get_counter(sfb->slot);
+   if (sfb->signaled_counter < counter) {
+      /* search pending cmds for already signaled values */
+      simple_mtx_lock(&sfb->cmd_mtx);
+      list_for_each_entry_safe(struct vn_sync_feedback_cmd, sfb_cmd,
+                               &sfb->pending_cmds, head) {
+         if (counter >= vn_feedback_get_counter(sfb_cmd->src_slot)) {
+            /* avoid over-caching more than normal runtime usage */
+            if (sfb->free_cmd_count > 5) {
+               list_del(&sfb_cmd->head);
+               vn_sync_feedback_cmd_free(dev, sfb_cmd);
+            } else {
+               list_move_to(&sfb_cmd->head, &sfb->free_cmds);
+               sfb->free_cmd_count++;
+            }
+         }
+      }
+      simple_mtx_unlock(&sfb->cmd_mtx);
+
+      sfb->signaled_counter = counter;
+      signaled_counter_updated = true;
+   }
+
+   /* vn_SignalSemaphore writes the sfb signaled_counter without updating
+    * the slot. So the semaphore counter query here must consider both.
+    */
+   *out_counter = MAX2(counter, sfb->signaled_counter);
+   simple_mtx_unlock(&sfb->counter_mtx);
+
+   return signaled_counter_updated;
+}
+
+void
+vn_sync_feedback_write(struct vn_sync_feedback *sfb, uint64_t counter)
+{
+   /* Must not update the sfb dst slot here because there's no followed
+    * submission to flush the cache (implicit sync guarantee) before the
+    * pending sfb cmd to update the slot. Otherwise, the slot update can be
+    * written by the racy update here.
+    */
+   simple_mtx_lock(&sfb->counter_mtx);
+
+   /* Update async counters. Since we're signaling, we're aligned with the
+    * renderer.
+    */
+   sfb->signaled_counter = counter;
+   sfb->pollable = true;
+
+   simple_mtx_unlock(&sfb->counter_mtx);
 }
 
 VkResult

src/virtio/vulkan/vn_feedback.h
@@ -210,9 +210,13 @@ vn_sync_feedback_command(struct vn_device *dev,
                          uint32_t qfi,
                          uint64_t counter);
 
+bool
+vn_sync_feedback_query(struct vn_device *dev,
+                       struct vn_sync_feedback *sfb,
+                       uint64_t *out_counter);
+
 void
-vn_sync_feedback_cmd_free(struct vn_device *dev,
-                          struct vn_sync_feedback_cmd *sfb_cmd);
+vn_sync_feedback_write(struct vn_sync_feedback *sfb, uint64_t counter);
 
 VkResult
 vn_sync_feedback_init(struct vn_device *dev,

src/virtio/vulkan/vn_queue.c
@@ -2170,15 +2170,7 @@ vn_get_semaphore_counter_value(VkDevice dev_handle,
             return result;
       }
 
-      assert(sem->feedback.slot);
-
-      /* If we are here when feedback is suspended, signaled_counter has been
-       * updated to the suspended counter value which must be greater than the
-       * feedback counter read from the feedback slot.
-       */
-      simple_mtx_lock(&sem->feedback.counter_mtx);
-      uint64_t counter = vn_feedback_get_counter(sem->feedback.slot);
-      if (sem->feedback.signaled_counter < counter) {
+      if (vn_sync_feedback_query(dev, &sem->feedback, out_value)) {
          /* When the timeline semaphore feedback slot gets signaled, the real
           * semaphore signal operation follows after but the signaling isr can
           * be deferred or preempted. To avoid racing, we let the renderer
@@ -2200,39 +2192,12 @@ vn_get_semaphore_counter_value(VkDevice dev_handle,
             .flags = 0,
             .semaphoreCount = 1,
             .pSemaphores = &sem_handle,
-            .pValues = &counter,
+            .pValues = out_value,
          };
 
          vn_async_vkWaitSemaphores(dev->primary_ring, dev_handle, &wait_info,
                                    UINT64_MAX);
-
-         /* search pending cmds for already signaled values */
-         simple_mtx_lock(&sem->feedback.cmd_mtx);
-         list_for_each_entry_safe(struct vn_sync_feedback_cmd, sfb_cmd,
-                                  &sem->feedback.pending_cmds, head) {
-            if (counter >= vn_feedback_get_counter(sfb_cmd->src_slot)) {
-               /* avoid over-caching more than normal runtime usage */
-               if (sem->feedback.free_cmd_count > 5) {
-                  list_del(&sfb_cmd->head);
-                  vn_sync_feedback_cmd_free(dev, sfb_cmd);
-               } else {
-                  list_move_to(&sfb_cmd->head, &sem->feedback.free_cmds);
-                  sem->feedback.free_cmd_count++;
-               }
-            }
-         }
-         simple_mtx_unlock(&sem->feedback.cmd_mtx);
-
-         sem->feedback.signaled_counter = counter;
       }
-
-      /* vn_SignalSemaphore writes the sfb signaled_counter without updating
-       * the slot. So the semaphore counter query here must consider both.
-       */
-      counter = MAX2(counter, sem->feedback.signaled_counter);
-      simple_mtx_unlock(&sem->feedback.counter_mtx);
-
-      *out_value = counter;
    } else {
       VkResult result = vn_call_vkGetSemaphoreCounterValue(
          dev->primary_ring, dev_handle, sem_handle, out_value);
@@ -2279,22 +2244,8 @@ vn_SignalSemaphore(VkDevice device, const VkSemaphoreSignalInfo *pSignalInfo)
 
    vn_async_vkSignalSemaphore(dev->primary_ring, device, pSignalInfo);
 
-   if (sem->feedback.slot) {
-      /* Must not update the sfb dst slot here because there's no followed
-       * submission to flush the cache (implicit sync guarantee) before the
-       * pending sfb cmd to update the slot. Otherwise, the slot update can be
-       * written by the racy update here.
-       */
-      simple_mtx_lock(&sem->feedback.counter_mtx);
-
-      /* Update async counters. Since we're signaling, we're aligned with
-       * the renderer.
-       */
-      sem->feedback.signaled_counter = pSignalInfo->value;
-      sem->feedback.pollable = true;
-
-      simple_mtx_unlock(&sem->feedback.counter_mtx);
-   }
+   if (sem->feedback.slot)
+      vn_sync_feedback_write(&sem->feedback, pSignalInfo->value);
 
    return VK_SUCCESS;
 }

commit cd1c836084cd7b76a83d5aca88086c942aa7cbb9

venus: recycle idle sfb cmds only after async wait
This fixes a regression introduced by a code refactor. The correct order
should be:
1. see sync feedback signaled to a new point
2. emit renderer side fence/semaphore wait for the new point
3. recycle sync feedback idle cmds

In (3), those exceeding the cache capacity will be purged, ending up
with renderer side cmd free, which must be executed after (2) for
validity of the renderer side Vulkan usage.

Fixes: b9efce45376 ("venus: extract sync feedback host write and query")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43004>

src/virtio/vulkan/vn_feedback.c
@@ -632,23 +632,6 @@ vn_sync_feedback_query(struct vn_device *dev,
    simple_mtx_lock(&sfb->counter_mtx);
    const uint64_t counter = vn_feedback_get_counter(sfb->slot);
    if (sfb->signaled_counter < counter) {
-      /* search pending cmds for already signaled values */
-      simple_mtx_lock(&sfb->cmd_mtx);
-      list_for_each_entry_safe(struct vn_sync_feedback_cmd, sfb_cmd,
-                               &sfb->pending_cmds, head) {
-         if (counter >= vn_feedback_get_counter(sfb_cmd->src_slot)) {
-            /* avoid over-caching more than normal runtime usage */
-            if (sfb->free_cmd_count > 5) {
-               list_del(&sfb_cmd->head);
-               vn_sync_feedback_cmd_free(dev, sfb_cmd);
-            } else {
-               list_move_to(&sfb_cmd->head, &sfb->free_cmds);
-               sfb->free_cmd_count++;
-            }
-         }
-      }
-      simple_mtx_unlock(&sfb->cmd_mtx);
-
       sfb->signaled_counter = counter;
       signaled_counter_updated = true;
    }
@@ -660,6 +643,29 @@ vn_sync_feedback_query(struct vn_device *dev,
    simple_mtx_unlock(&sfb->counter_mtx);
 
    return signaled_counter_updated;
+}
+
+void
+vn_sync_feedback_cmd_recycle(struct vn_device *dev,
+                             struct vn_sync_feedback *sfb)
+{
+   /* search pending cmds for already signaled values */
+   simple_mtx_lock(&sfb->cmd_mtx);
+   list_for_each_entry_safe(struct vn_sync_feedback_cmd, sfb_cmd,
+                            &sfb->pending_cmds, head) {
+      const uint64_t cmd_counter = vn_feedback_get_counter(sfb_cmd->src_slot);
+      if (sfb->signaled_counter >= cmd_counter) {
+         /* avoid over-caching more than normal runtime usage */
+         if (sfb->free_cmd_count > 5) {
+            list_del(&sfb_cmd->head);
+            vn_sync_feedback_cmd_free(dev, sfb_cmd);
+         } else {
+            list_move_to(&sfb_cmd->head, &sfb->free_cmds);
+            sfb->free_cmd_count++;
+         }
+      }
+   }
+   simple_mtx_unlock(&sfb->cmd_mtx);
 }
 
 void

src/virtio/vulkan/vn_feedback.h
@@ -216,6 +216,10 @@ vn_sync_feedback_query(struct vn_device *dev,
                        uint64_t *out_counter);
 
 void
+vn_sync_feedback_cmd_recycle(struct vn_device *dev,
+                             struct vn_sync_feedback *sfb);
+
+void
 vn_sync_feedback_write(struct vn_sync_feedback *sfb, uint64_t counter);
 
 void

src/virtio/vulkan/vn_sync.c
@@ -182,6 +182,9 @@ vn_get_fence_status(VkDevice dev_handle,
              */
             vn_async_vkWaitForFences(dev->primary_ring, dev_handle, 1,
                                      &fence_handle, VK_TRUE, UINT64_MAX);
+
+            /* Recycle idle cmds after async fence wait. */
+            vn_sync_feedback_cmd_recycle(dev, &fence->feedback);
          }
 
          const bool signaled = counter == fence->signal_counter;
@@ -607,6 +610,9 @@ vn_get_semaphore_counter_value(VkDevice dev_handle,
 
          vn_async_vkWaitSemaphores(dev->primary_ring, dev_handle, &wait_info,
                                    UINT64_MAX);
+
+         /* Recycle idle cmds after async semaphore wait. */
+         vn_sync_feedback_cmd_recycle(dev, &sem->feedback);
       }
    } else {
       VkResult result = vn_call_vkGetSemaphoreCounterValue(