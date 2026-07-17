joshuaswarren/mesa-1

commit bc63a6b5ef3c6dd2600606f8498a79461be20f9e

radv: disable AMD_device_coherent_memory on gfx12 due to out of order behavior
Cc: mesa-stable

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42346>

src/amd/vulkan/radv_physical_device.c
@@ -302,6 +302,16 @@ radv_are_dcc_mips_disabled(const struct radv_physical_device *pdev)
 
    return instance->drirc.debug.disable_dcc_mips && pdev->info.gfx_level < GFX12;
 }
+
+static bool
+radv_device_coherent_memory_enabled(const struct radv_physical_device *pdev)
+{
+   const struct radv_instance *instance = radv_physical_device_instance(pdev);
+
+   return pdev->info.has_l2_uncached && (!pdev->info.has_out_of_order_uncached_l2 ||
+                                         instance->drirc.features.device_coherent_memory);
+}
+
 static void
 parse_hex(char *out, const char *in, unsigned length)
 {
@@ -935,7 +945,7 @@ radv_physical_device_get_supported_extensions(const struct radv_physical_device 
       .EXT_ycbcr_image_arrays = true,
       .EXT_zero_initialize_device_memory = true,
       .AMD_buffer_marker = true,
-      .AMD_device_coherent_memory = pdev->info.has_l2_uncached,
+      .AMD_device_coherent_memory = radv_device_coherent_memory_enabled(pdev),
       .AMD_draw_indirect_count = true,
       .AMD_gcn_shader = true,
       .AMD_gpu_shader_half_float = pdev->info.compiler_info.has_packed_math_16bit,
@@ -1187,7 +1197,7 @@ radv_physical_device_get_features(const struct radv_physical_device *pdev, struc
       .texelBufferAlignment = true,
 
       /* VK_AMD_device_coherent_memory */
-      .deviceCoherentMemory = pdev->info.has_l2_uncached,
+      .deviceCoherentMemory = radv_device_coherent_memory_enabled(pdev),
 
       /* VK_KHR_line_rasterization */
       .rectangularLines = true,

src/amd/common/ac_gpu_info.c
@@ -558,6 +558,12 @@ ac_fill_memory_info(struct radeon_info *info, const struct drm_amdgpu_info_devic
    /* Set which chips have uncached device memory. */
    info->has_l2_uncached = info->gfx_level >= GFX9;
 
+   /* On GFX12, uncached reads and writes to the same address can execute out of order. That's
+    * OK for DRI_PRIME blits, but exposure via APIs should be disallowed if general shader access
+    * can occur. Alternatively, the shader compiler can add extra s_wait to enforce ordering.
+    */
+   info->has_out_of_order_uncached_l2 = info->gfx_level == GFX12;
+
    info->max_tcc_blocks = device_info->num_tcc_blocks;
    if (info->gfx_level >= GFX10) {
       info->tcc_cache_line_size = info->gfx_level >= GFX12 ? 256 : 128;
@@ -1952,6 +1958,7 @@ void ac_print_gpu_info(FILE *f, const struct radeon_info *info, int fd)
    fprintf(f, "    has_set_sh_pairs_packed = %i\n", info->has_set_sh_pairs_packed);
    fprintf(f, "    has_set_uconfig_pairs = %i\n", info->has_set_uconfig_pairs);
    fprintf(f, "    has_smem_partial_oob_access_bug = %i\n", info->has_smem_partial_oob_access_bug);
+   fprintf(f, "    has_out_of_order_uncached_l2 = %i\n", info->has_out_of_order_uncached_l2);
 
    if (info->gfx_level < GFX12) {
       fprintf(f, "Display features:\n");

src/amd/common/ac_gpu_info.h
@@ -300,6 +300,7 @@ struct radeon_info {
                              * the LLVM version doesn't work with multiparts shaders.
                              */
    bool has_smem_partial_oob_access_bug;
+   bool has_out_of_order_uncached_l2;
 
    /* Display features. */
    /* There are 2 display DCC codepaths, because display expects unaligned DCC. */

src/amd/vulkan/00-radv-defaults.conf
@@ -18,6 +18,7 @@ Application bugs worked around in this file:
             <option name="radv_disable_aniso_single_level" value="true" />
             <option name="radv_disable_trunc_coord" value="true" />
             <option name="radv_cooperative_matrix2_nv" value="true" />
+            <option name="radv_device_coherent_memory" value="true" />
         </engine>
 
         <engine engine_name_match="DXVK">

src/amd/vulkan/radv_drirc_gen.py
@@ -110,6 +110,9 @@ def declare_options():
     ]
 
     features_options = [
+        B("radv_device_coherent_memory", False,
+          "Expose VK_AMD_device_coherent_memory on GFX12 (RDNA4).",
+          c_name="device_coherent_memory"),
         B("radv_cooperative_matrix2_nv", False,
           "Expose VK_NV_cooperative_matrix2 on supported hardware.",
           c_name="cooperative_matrix2_nv"),

commit f4835ca284a85a0701c768918ac0d91f39122511

radv: don't expose memory types from AMD_device_coherent_memory without the ext
Fixes: bc63a6b5ef3 - radv: disable AMD_device_coherent_memory on gfx12 due to out of order behavior

Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43001>

src/amd/vulkan/radv_physical_device.c
@@ -639,7 +639,7 @@ radv_physical_device_init_mem_types(struct radv_physical_device *pdev)
       pdev->memory_properties.memoryTypeCount = type_count;
    }
 
-   if (pdev->info.has_l2_uncached) {
+   if (radv_device_coherent_memory_enabled(pdev)) {
       for (int i = 0; i < pdev->memory_properties.memoryTypeCount; i++) {
          VkMemoryType mem_type = pdev->memory_properties.memoryTypes[i];
 