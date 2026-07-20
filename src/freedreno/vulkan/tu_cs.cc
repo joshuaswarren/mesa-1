joshuaswarren/mesa-1

commit a148896aa398aadca1a8a398ae3702e00e0f7acc

tu/cs: Add support for CS patching
In order to patch the command stream on the gpu, we need two features:

1. The ability to use a read-write BO instead of a read-only one, when
   patching might be performed.
2. The ability to get the iova of the current position after reserving
   some number of dwords, even with externally-allocated command
   streams.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20304>

src/freedreno/vulkan/tu_cs.cc
@@ -31,7 +31,8 @@ tu_cs_init(struct tu_cs *cs,
  */
 void
 tu_cs_init_external(struct tu_cs *cs, struct tu_device *device,
-                    uint32_t *start, uint32_t *end)
+                    uint32_t *start, uint32_t *end, uint64_t iova,
+                    bool writeable)
 {
    memset(cs, 0, sizeof(*cs));
 
@@ -39,6 +40,8 @@ tu_cs_init_external(struct tu_cs *cs, struct tu_device *device,
    cs->mode = TU_CS_MODE_EXTERNAL;
    cs->start = cs->reserved_end = cs->cur = start;
    cs->end = end;
+   cs->external_iova = iova;
+   cs->writeable = writeable;
 }
 
 /**
@@ -66,15 +69,20 @@ tu_cs_init_suballoc(struct tu_cs *cs, struct tu_device *device,
 void
 tu_cs_finish(struct tu_cs *cs)
 {
-   for (uint32_t i = 0; i < cs->bo_count; ++i) {
-      tu_bo_finish(cs->device, cs->bos[i]);
+   for (uint32_t i = 0; i < cs->read_only.bo_count; ++i) {
+      tu_bo_finish(cs->device, cs->read_only.bos[i]);
+   }
+
+   for (uint32_t i = 0; i < cs->read_write.bo_count; ++i) {
+      tu_bo_finish(cs->device, cs->read_write.bos[i]);
    }
 
    if (cs->refcount_bo)
       tu_bo_finish(cs->device, cs->refcount_bo);
 
    free(cs->entries);
-   free(cs->bos);
+   free(cs->read_only.bos);
+   free(cs->read_write.bos);
 }
 
 static struct tu_bo *
@@ -83,8 +91,9 @@ tu_cs_current_bo(const struct tu_cs *cs)
    if (cs->refcount_bo) {
       return cs->refcount_bo;
    } else {
-      assert(cs->bo_count);
-      return cs->bos[cs->bo_count - 1];
+      const struct tu_bo_array *bos = cs->writeable ? &cs->read_write : &cs->read_only;
+      assert(bos->bo_count);
+      return bos->bos[bos->bo_count - 1];
    }
 }
 
@@ -96,6 +105,18 @@ static uint32_t
 tu_cs_get_offset(const struct tu_cs *cs)
 {
    return cs->start - (uint32_t *) tu_cs_current_bo(cs)->map;
+}
+
+/* Get the iova for the next dword to be emitted. Useful after
+ * tu_cs_reserve_space() to create a patch point that can be overwritten on
+ * the GPU.
+ */
+uint64_t
+tu_cs_get_cur_iova(const struct tu_cs *cs)
+{
+   if (cs->mode == TU_CS_MODE_EXTERNAL)
+      return cs->external_iova + ((char *) cs->cur - (char *) cs->start);
+   return tu_cs_current_bo(cs)->iova + ((char *) cs->cur - (char *) tu_cs_current_bo(cs)->map);
 }
 
 /*
@@ -113,23 +134,26 @@ tu_cs_add_bo(struct tu_cs *cs, uint32_t size)
    /* no dangling command packet */
    assert(tu_cs_is_empty(cs));
 
+   struct tu_bo_array *bos = cs->writeable ? &cs->read_write : &cs->read_only;
+
    /* grow cs->bos if needed */
-   if (cs->bo_count == cs->bo_capacity) {
-      uint32_t new_capacity = MAX2(4, 2 * cs->bo_capacity);
+   if (bos->bo_count == bos->bo_capacity) {
+      uint32_t new_capacity = MAX2(4, 2 * bos->bo_capacity);
       struct tu_bo **new_bos = (struct tu_bo **)
-         realloc(cs->bos, new_capacity * sizeof(struct tu_bo *));
+         realloc(bos->bos, new_capacity * sizeof(struct tu_bo *));
       if (!new_bos)
          return VK_ERROR_OUT_OF_HOST_MEMORY;
 
-      cs->bo_capacity = new_capacity;
-      cs->bos = new_bos;
+      bos->bo_capacity = new_capacity;
+      bos->bos = new_bos;
    }
 
    struct tu_bo *new_bo;
 
    VkResult result =
       tu_bo_init_new(cs->device, &new_bo, size * sizeof(uint32_t),
-                     (enum tu_bo_alloc_flags)(TU_BO_ALLOC_GPU_READ_ONLY |
+                     (enum tu_bo_alloc_flags)(COND(!cs->writeable,
+                                                   TU_BO_ALLOC_GPU_READ_ONLY) |
                                               TU_BO_ALLOC_ALLOW_DUMP),
                      cs->name);
    if (result != VK_SUCCESS) {
@@ -142,7 +166,7 @@ tu_cs_add_bo(struct tu_cs *cs, uint32_t size)
       return result;
    }
 
-   cs->bos[cs->bo_count++] = new_bo;
+   bos->bos[bos->bo_count++] = new_bo;
 
    cs->start = cs->cur = cs->reserved_end = (uint32_t *) new_bo->map;
    cs->end = cs->start + new_bo->size / sizeof(uint32_t);
@@ -174,6 +198,7 @@ tu_cs_reserve_entry(struct tu_cs *cs)
    return VK_SUCCESS;
 }
 
+
 /**
  * Add an IB entry for the command packets emitted since the last call to this
  * function.
@@ -191,7 +216,7 @@ tu_cs_add_entry(struct tu_cs *cs)
     * because we disallow empty entry, tu_cs_add_bo and tu_cs_reserve_entry
     * must both have been called
     */
-   assert(cs->bo_count);
+   assert(cs->writeable ? cs->read_write.bo_count : cs->read_only.bo_count);
    assert(cs->entry_count < cs->entry_capacity);
 
    /* add an entry for [cs->start, cs->cur] */
@@ -252,6 +277,30 @@ tu_cs_end(struct tu_cs *cs)
       tu_cs_add_entry(cs);
 }
 
+void
+tu_cs_set_writeable(struct tu_cs *cs, bool writeable)
+{
+   assert(cs->mode == TU_CS_MODE_GROW || cs->mode == TU_CS_MODE_SUB_STREAM);
+
+   if (cs->writeable != writeable) {
+      if (cs->mode == TU_CS_MODE_GROW && !tu_cs_is_empty(cs))
+         tu_cs_add_entry(cs);
+      struct tu_bo_array *old_bos = cs->writeable ? &cs->read_write : &cs->read_only;
+      struct tu_bo_array *new_bos = writeable ? &cs->read_write : &cs->read_only;
+
+      old_bos->start = cs->start;
+      cs->start = cs->cur = cs->reserved_end = new_bos->start;
+      if (new_bos->bo_count) {
+         struct tu_bo *bo = new_bos->bos[new_bos->bo_count - 1];
+         cs->end = (uint32_t *)bo->map + bo->size / sizeof(uint32_t);
+      } else {
+         cs->end = NULL;
+      }
+
+      cs->writeable = writeable;
+   }
+}
+
 /**
  * Begin command packet emission to a sub-stream.  \a cs must be in
  * TU_CS_MODE_SUB_STREAM mode.
@@ -270,7 +319,8 @@ tu_cs_begin_sub_stream(struct tu_cs *cs, uint32_t size, struct tu_cs *sub_cs)
    if (result != VK_SUCCESS)
       return result;
 
-   tu_cs_init_external(sub_cs, cs->device, cs->cur, cs->reserved_end);
+   tu_cs_init_external(sub_cs, cs->device, cs->cur, cs->reserved_end,
+                       tu_cs_get_cur_iova(cs), cs->writeable);
    tu_cs_begin(sub_cs);
    result = tu_cs_reserve_space(sub_cs, size);
    assert(result == VK_SUCCESS);
@@ -312,6 +362,7 @@ tu_cs_alloc(struct tu_cs *cs,
 
    memory->map = (uint32_t *) bo->map + offset;
    memory->iova = bo->iova + offset * sizeof(uint32_t);
+   memory->writeable = cs->writeable;
 
    cs->start = cs->cur = (uint32_t*) bo->map + offset + count * size;
 
@@ -424,21 +475,33 @@ void
 tu_cs_reset(struct tu_cs *cs)
 {
    if (cs->mode == TU_CS_MODE_EXTERNAL) {
-      assert(!cs->bo_count && !cs->refcount_bo && !cs->entry_count);
+      assert(!cs->read_only.bo_count && !cs->read_write.bo_count &&
+             !cs->refcount_bo && !cs->entry_count);
       cs->reserved_end = cs->cur = cs->start;
       return;
    }
 
-   for (uint32_t i = 0; i + 1 < cs->bo_count; ++i) {
-      tu_bo_finish(cs->device, cs->bos[i]);
+   for (uint32_t i = 0; i + 1 < cs->read_only.bo_count; ++i) {
+      tu_bo_finish(cs->device, cs->read_only.bos[i]);
    }
 
-   if (cs->bo_count) {
-      cs->bos[0] = cs->bos[cs->bo_count - 1];
-      cs->bo_count = 1;
+   for (uint32_t i = 0; i + 1 < cs->read_write.bo_count; ++i) {
+      tu_bo_finish(cs->device, cs->read_write.bos[i]);
+   }
 
-      cs->start = cs->cur = cs->reserved_end = (uint32_t *) cs->bos[0]->map;
-      cs->end = cs->start + cs->bos[0]->size / sizeof(uint32_t);
+   cs->writeable = false;
+
+   if (cs->read_only.bo_count) {
+      cs->read_only.bos[0] = cs->read_only.bos[cs->read_only.bo_count - 1];
+      cs->read_only.bo_count = 1;
+
+      cs->start = cs->cur = cs->reserved_end = (uint32_t *) cs->read_only.bos[0]->map;
+      cs->end = cs->start + cs->read_only.bos[0]->size / sizeof(uint32_t);
+   }
+
+   if (cs->read_write.bo_count) {
+      cs->read_write.bos[0] = cs->read_write.bos[cs->read_write.bo_count - 1];
+      cs->read_write.bo_count = 1;
    }
 
    cs->entry_count = 0;

src/freedreno/vulkan/tu_cs.h
@@ -62,11 +62,19 @@ struct tu_cs_entry
 struct tu_cs_memory {
    uint32_t *map;
    uint64_t iova;
+   bool writeable;
 };
 
 struct tu_draw_state {
    uint64_t iova : 48;
    uint32_t size : 16;
+};
+
+struct tu_bo_array {
+   struct tu_bo **bos;
+   uint32_t bo_count;
+   uint32_t bo_capacity;
+   uint32_t *start;
 };
 
 #define TU_COND_EXEC_STACK_SIZE 4
@@ -81,18 +89,20 @@ struct tu_cs
 
    struct tu_device *device;
    enum tu_cs_mode mode;
+   bool writeable;
    uint32_t next_bo_size;
 
    struct tu_cs_entry *entries;
    uint32_t entry_count;
    uint32_t entry_capacity;
 
-   struct tu_bo **bos;
-   uint32_t bo_count;
-   uint32_t bo_capacity;
+   struct tu_bo_array read_only, read_write;
 
    /* Optional BO that this CS is sub-allocated from for TU_CS_MODE_SUB_STREAM */
    struct tu_bo *refcount_bo;
+
+   /* iova that this CS starts with in TU_CS_MODE_EXTERNAL */
+   uint64_t external_iova;
 
    /* state for cond_exec_start/cond_exec_end */
    uint32_t cond_stack_depth;
@@ -116,7 +126,8 @@ tu_cs_init(struct tu_cs *cs,
 
 void
 tu_cs_init_external(struct tu_cs *cs, struct tu_device *device,
-                    uint32_t *start, uint32_t *end);
+                    uint32_t *start, uint32_t *end, uint64_t iova,
+                    bool writeable);
 
 void
 tu_cs_init_suballoc(struct tu_cs *cs, struct tu_device *device,
@@ -130,6 +141,9 @@ tu_cs_begin(struct tu_cs *cs);
 
 void
 tu_cs_end(struct tu_cs *cs);
+
+void
+tu_cs_set_writeable(struct tu_cs *cs, bool writeable);
 
 VkResult
 tu_cs_begin_sub_stream(struct tu_cs *cs, uint32_t size, struct tu_cs *sub_cs);
@@ -156,6 +170,9 @@ tu_cs_end_draw_state(struct tu_cs *cs, struct tu_cs *sub_cs)
 VkResult
 tu_cs_reserve_space(struct tu_cs *cs, uint32_t reserved_size);
 
+uint64_t
+tu_cs_get_cur_iova(const struct tu_cs *cs);
+
 static inline struct tu_draw_state
 tu_cs_draw_state(struct tu_cs *sub_cs, struct tu_cs *cs, uint32_t size)
 {
@@ -163,7 +180,8 @@ tu_cs_draw_state(struct tu_cs *sub_cs, struct tu_cs *cs, uint32_t size)
 
    /* TODO: clean this up */
    tu_cs_alloc(sub_cs, size, 1, &memory);
-   tu_cs_init_external(cs, sub_cs->device, memory.map, memory.map + size);
+   tu_cs_init_external(cs, sub_cs->device, memory.map, memory.map + size,
+                       memory.iova, memory.writeable);
    tu_cs_begin(cs);
    tu_cs_reserve_space(cs, size);
 

commit 52754365983c6fbabe7a59a3bf46abb600d606a4

tu: Fix resetting command streams with writeable BOs
We were forgetting to reset the start pointer that is used when
switching back to writeable, which meant that when using the CS again
and again making the CS writeable, we would keep going where we left off
in the BO instead of actually reusing it. We still threw away the other
BOs, but this meant that we would eventually run out of space and grow
the BO unnecessarily, and if every BO hit the maximum size of 4MB then
we could easily run out of memory eventually in games with hundreds of
command buffers.

Fixes: a148896aa39 ("tu/cs: Add support for CS patching")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43045>

src/freedreno/vulkan/tu_cs.cc
@@ -568,6 +568,8 @@ tu_cs_reset(struct tu_cs *cs)
    if (cs->read_write.bo_count) {
       cs->read_write.bos[0] = cs->read_write.bos[cs->read_write.bo_count - 1];
       cs->read_write.bo_count = 1;
+
+      cs->read_write.start = (uint32_t *) cs->read_write.bos[0]->map;
    }
 
    cs->entry_count = 0;