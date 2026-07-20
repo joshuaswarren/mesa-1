joshuaswarren/mesa-1

commit b6c74f10553a8d831a18bb82f248c979e972b9d0

tu: Move all physical_device compiler options to a dedicated struct
This helps guarantee that we don't miss anything when computing the
pipelineCacheUUID, and adds room for adding backwards-compatible
compiler options when using the build id override.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43051>

src/freedreno/vulkan/tu_device.h
@@ -152,8 +152,14 @@ struct tu_physical_device
    /* Whether performance counter selector registers can be written by userspace CSes. */
    bool is_perf_cntr_selectable;
 
-   bool enable_texel_buffer_emulation;
-   bool enable_ssbo_emulation;
+   struct {
+      bool enable_texel_buffer_emulation : 1;
+      bool enable_ssbo_emulation : 1;
+      bool allow_oob_indirect_ubo_loads : 1;
+      bool no_multi_pos : 1;
+      bool compute_round_robin : 1;
+      uint32_t padding : 28;
+   } compiler_options;
 
    struct {
       uint32_t non_lazy_type_count;

src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -4754,7 +4754,7 @@ tu_bind_descriptor_sets(struct tu_cmd_buffer *cmd,
       descriptors_state->set_iova[idx] = set ?
          (set->va | BINDLESS_DESCRIPTOR_64B) : 0;
 
-      if (cmd->device->physical_device->enable_ssbo_emulation)
+      if (cmd->device->physical_device->compiler_options.enable_ssbo_emulation)
          cmd->state.dirty |= TU_CMD_DIRTY_SHADER_CONSTS;
 
       if (!set)
@@ -4823,7 +4823,7 @@ tu_bind_descriptor_sets(struct tu_cmd_buffer *cmd,
                         va += desc_offset << offset_shift;
                         va += offset;
 
-                        if (cmd->device->physical_device->enable_ssbo_emulation) {
+                        if (cmd->device->physical_device->compiler_options.enable_ssbo_emulation) {
                            dst_desc[11] = va;
                            dst_desc[12] = va >> 32;
                         }
@@ -4930,7 +4930,7 @@ tu_set_descriptor_buffer_offsets(
           info->pOffsets[i]) |
          BINDLESS_DESCRIPTOR_64B;
 
-      if (cmd->device->physical_device->enable_ssbo_emulation ||
+      if (cmd->device->physical_device->compiler_options.enable_ssbo_emulation ||
           set_layout->has_inline_uniforms)
          cmd->state.dirty |= TU_CMD_DIRTY_SHADER_CONSTS;
    }

src/freedreno/vulkan/tu_descriptor_set.cc
@@ -1132,7 +1132,8 @@ write_buffer_descriptor_addr(const struct tu_device *device,
       return;
 
    enum fdl_ssbo_emulation_mode ssbo_emulation_mode =
-      device->physical_device->enable_ssbo_emulation ? FDL_SSBO_EMULATION_ENABLED : FDL_SSBO_EMULATION_DISABLED;
+      device->physical_device->compiler_options.enable_ssbo_emulation ?
+      FDL_SSBO_EMULATION_ENABLED : FDL_SSBO_EMULATION_DISABLED;
 
    uint64_t va = buffer_info->address;
    uint32_t range = buffer_info->range;
@@ -1299,7 +1300,7 @@ tu_GetDescriptorEXT(
       break;
    case VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
    case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
-      if (device->physical_device->enable_texel_buffer_emulation) {
+      if (device->physical_device->compiler_options.enable_texel_buffer_emulation) {
          write_emulated_texel_buffer_descriptor_addr<CHIP>(
             dest, pDescriptorInfo->data.pUniformTexelBuffer);
       } else {
@@ -1435,7 +1436,7 @@ tu_update_descriptor_sets(const struct tu_device *device,
             break;
          case VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
          case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
-            if (device->physical_device->enable_texel_buffer_emulation) {
+            if (device->physical_device->compiler_options.enable_texel_buffer_emulation) {
                write_emulated_texel_buffer_descriptor<CHIP>(
                   ptr, writeset->pTexelBufferView[j]);
             } else {
@@ -1792,7 +1793,7 @@ tu_update_descriptor_set_with_template(
             break;
          case VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER:
          case VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER:
-            if (device->physical_device->enable_texel_buffer_emulation) {
+            if (device->physical_device->compiler_options.enable_texel_buffer_emulation) {
                write_emulated_texel_buffer_descriptor<CHIP>(
                   ptr, *(VkBufferView *) src);
             } else {

src/freedreno/vulkan/tu_device.cc
@@ -74,18 +74,41 @@ tu_device_get_build_id(blake3_hasher *ctx)
 #endif
 }
 
-static int
-tu_device_get_cache_uuid(struct tu_physical_device *device, void *uuid)
+static void
+tu_physical_device_compiler_options_init(struct tu_physical_device *device,
+                                         struct tu_instance *instance)
 {
-   blake3_hasher ctx;
-   unsigned char blake3[BLAKE3_KEY_LEN];
+   /* D3D12 texel buffer range emulation requires the resbase instruction, which appeared in 7xx. */
+   if (fd_dev_gen(&device->dev_id) >= 7) {
+      if (device->info->props.max_texel_buffer_range_elements < TU_D3D12_MAX_TEXEL_BUFFER_ELEMENTS) {
+         assert(fd_dev_gen(&device->dev_id) == 7);
+         device->compiler_options.enable_texel_buffer_emulation =
+            instance->drirc.misc.enable_texel_buffer_emulation;
+      }
+      if (device->info->props.max_storage_buffer_range_bytes < TU_D3D12_MAX_STORAGE_BUFFER_RANGE_BYTES) {
+         assert(fd_dev_gen(&device->dev_id) == 7);
+         device->compiler_options.enable_ssbo_emulation =
+            instance->drirc.misc.enable_ssbo_emulation;
+      }
+   }
+
+   device->compiler_options.allow_oob_indirect_ubo_loads =
+      device->instance->drirc.misc.allow_oob_indirect_ubo_loads;
+
    /* Note: IR3_SHADER_DEBUG also affects compilation, but it's not
     * initialized until after compiler creation so we have to add it to the
     * shader hash instead, since the compiler is only created with the logical
     * device.
     */
-   uint64_t driver_flags = TU_DEBUG(NOMULTIPOS) |
-      (TU_DEBUG(COMPUTE_ROUND_ROBIN) << 1u);
+   device->compiler_options.no_multi_pos = TU_DEBUG(NOMULTIPOS);
+   device->compiler_options.compute_round_robin = TU_DEBUG(COMPUTE_ROUND_ROBIN);
+}
+
+static int
+tu_device_get_cache_uuid(struct tu_physical_device *device, void *uuid)
+{
+   blake3_hasher ctx;
+   unsigned char blake3[BLAKE3_KEY_LEN];
 
    /* Note: we intentionally drop the upper 32 bits since they contain fuse
     * values that don't affect compilation.
@@ -99,14 +122,9 @@ tu_device_get_cache_uuid(struct tu_physical_device *device, void *uuid)
       return -1;
 
    _mesa_blake3_update(&ctx, &chip_id, sizeof(chip_id));
-   _mesa_blake3_update(&ctx, &driver_flags, sizeof(driver_flags));
    _mesa_blake3_update(&ctx, &device->uche_trap_base, sizeof(device->uche_trap_base));
-   _mesa_blake3_update(&ctx, &device->instance->drirc.misc.allow_oob_indirect_ubo_loads,
-                       sizeof(device->instance->drirc.misc.allow_oob_indirect_ubo_loads));
-   _mesa_blake3_update(&ctx, &device->enable_texel_buffer_emulation,
-                       sizeof(device->enable_texel_buffer_emulation));
-   _mesa_blake3_update(&ctx, &device->enable_ssbo_emulation,
-                       sizeof(device->enable_ssbo_emulation));
+   _mesa_blake3_update(&ctx, &device->compiler_options,
+                       sizeof(device->compiler_options));
    _mesa_blake3_final(&ctx, blake3);
 
    memcpy(uuid, blake3, VK_UUID_SIZE);
@@ -1157,12 +1175,12 @@ tu_get_properties(struct tu_physical_device *pdevice,
    props->maxImageDimension3D = (1 << 11);
    props->maxImageDimensionCube = (1 << 14);
    props->maxImageArrayLayers = (1 << (pdevice->info->props.is_a702 ? 8 : 11));
-   props->maxTexelBufferElements = pdevice->enable_texel_buffer_emulation
+   props->maxTexelBufferElements = pdevice->compiler_options.enable_texel_buffer_emulation
                                       ? TU_D3D12_MAX_TEXEL_BUFFER_ELEMENTS
                                       : pdevice->info->props.max_texel_buffer_range_elements;
    props->maxUniformBufferRange = MAX_UNIFORM_BUFFER_RANGE;
    props->maxStorageBufferRange =
-      pdevice->enable_ssbo_emulation
+      pdevice->compiler_options.enable_ssbo_emulation
          ? TU_D3D12_MAX_STORAGE_BUFFER_RANGE_BYTES
          : pdevice->info->props.max_storage_buffer_range_bytes;
    props->maxPushConstantsSize = MAX_PUSH_CONSTANTS_SIZE;
@@ -1737,17 +1755,7 @@ tu_physical_device_init(struct tu_physical_device *device,
       goto fail_free_name;
    }
 
-   /* D3D12 texel buffer range emulation requires the resbase instruction, which appeared in 7xx. */
-   if (fd_dev_gen(&device->dev_id) >= 7) {
-      if (device->info->props.max_texel_buffer_range_elements < TU_D3D12_MAX_TEXEL_BUFFER_ELEMENTS) {
-         assert(fd_dev_gen(&device->dev_id) == 7);
-         device->enable_texel_buffer_emulation = instance->drirc.misc.enable_texel_buffer_emulation;
-      }
-      if (device->info->props.max_storage_buffer_range_bytes < TU_D3D12_MAX_STORAGE_BUFFER_RANGE_BYTES) {
-         assert(fd_dev_gen(&device->dev_id) == 7);
-         device->enable_ssbo_emulation = instance->drirc.misc.enable_ssbo_emulation;
-      }
-   }
+   tu_physical_device_compiler_options_init(device, instance);
 
    if (tu_device_get_cache_uuid(device, device->cache_uuid)) {
       result = vk_startup_errorf(instance, VK_ERROR_INITIALIZATION_FAILED,

src/freedreno/vulkan/tu_nir_lower_multiview.cc
@@ -106,7 +106,7 @@ tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, struct tu_device *dev,
    /* In addition to the generic checks done by NIR, check that we don't
     * overflow VPC with the extra copies of gl_Position.
     */
-   } else if (!TU_DEBUG(NOMULTIPOS) &&
+   } else if (!dev->physical_device->compiler_options.no_multi_pos &&
        num_views <= max_views_for_multipos && nir->num_outputs + (num_views - 1) <= 32 &&
        nir_can_lower_multiview(nir, options)) {
       /* It appears that the multiview mask is ignored when multi-position

src/freedreno/vulkan/tu_shader.cc
@@ -219,7 +219,7 @@ tu_spirv_to_nir(struct tu_device *dev,
    nir->info.num_ubos = 0;
    nir->info.num_ssbos = 0;
 
-   if (TU_DEBUG(COMPUTE_ROUND_ROBIN)) {
+   if (dev->physical_device->compiler_options.compute_round_robin) {
       nir->info.occupancy_bounded_workgroup_fairness = true;
    }
 
@@ -451,7 +451,7 @@ lower_ssbo_ubo_intrinsic(struct tu_device *dev,
    nir_def *descriptor_idx = nir_channel(b, intrin->src[buffer_src].ssa, 1);
 
    if (intrin->intrinsic == nir_intrinsic_load_ubo &&
-       dev->instance->drirc.misc.allow_oob_indirect_ubo_loads) {
+       dev->physical_device->compiler_options.allow_oob_indirect_ubo_loads) {
       nir_scalar offset = nir_scalar_resolved(intrin->src[1].ssa, 0);
       if (!nir_scalar_is_const(offset)) {
          nir_intrinsic_set_range(intrin, ~0);
@@ -685,7 +685,7 @@ lower_image_deref(struct tu_device *dev, nir_builder *b,
    nir_rewrite_image_intrinsic(instr, bindless,
                                nir_image_intrinsic_type_bindless);
 
-   if (dev->physical_device->enable_texel_buffer_emulation &&
+   if (dev->physical_device->compiler_options.enable_texel_buffer_emulation &&
        nir_intrinsic_image_dim(instr) == GLSL_SAMPLER_DIM_BUF) {
       lower_texel_buffers_to_image(b, instr, bindless);
    }
@@ -1008,7 +1008,7 @@ lower_tex_impl(nir_builder *b, nir_tex_instr *tex, struct tu_device *dev,
          tex->src[tex_src_idx].src_type = nir_tex_src_texture_offset;
    }
 
-   if (dev->physical_device->enable_texel_buffer_emulation &&
+   if (dev->physical_device->compiler_options.enable_texel_buffer_emulation &&
        tex->sampler_dim == GLSL_SAMPLER_DIM_BUF) {
       lower_tex_texel_buffer_to_image(b, tex, tex_src_idx);
    }
@@ -1362,7 +1362,7 @@ tu_lower_io(nir_shader *shader, struct tu_device *dev,
 
    ir3_const_alloc(const_allocs, IR3_CONST_ALLOC_INLINE_UNIFORM_ADDRS, ldgk_consts, 1);
 
-   if (dev->physical_device->enable_ssbo_emulation) {
+   if (dev->physical_device->compiler_options.enable_ssbo_emulation) {
       const_state->num_bindless_base_addresses = layout->num_sets;
       const_state->bindless_base_const_offset_vec4 = const_allocs->max_const_offset_vec4;
 
@@ -3400,7 +3400,7 @@ tu_shader_create(struct tu_device *dev,
     */
    NIR_PASS(_, nir, tu_nir_lower_ssbo_descriptor, dev);
 
-   if (dev->physical_device->enable_ssbo_emulation) {
+   if (dev->physical_device->compiler_options.enable_ssbo_emulation) {
       nir_lower_ssbo_options options = {
          .native_offset = true,
          .min_ssbo_size = dev->compiler->info->props.max_storage_buffer_range_bytes,

commit 8ef437f49f182acd73bd9f7a1f30bf68ea5fb707

tu: Fix compiler options padding size
It should be one uint32_t for the entire bitfield.

Fixes: b6c74f10553 ("tu: Move all physical_device compiler options to a dedicated struct")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43080>

src/freedreno/vulkan/tu_device.h
@@ -158,7 +158,7 @@ struct tu_physical_device
       bool allow_oob_indirect_ubo_loads : 1;
       bool no_multi_pos : 1;
       bool compute_round_robin : 1;
-      uint32_t padding : 28;
+      uint32_t padding : 27;
    } compiler_options;
 
    struct {