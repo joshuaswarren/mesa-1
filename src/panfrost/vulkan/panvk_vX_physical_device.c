joshuaswarren/mesa-1

commit 6342674dc1128aada9b51a876233e0646bd8fbcf

panvk: Advertise VK_KHR_internally_synchronized_queues
So far an app had to synchronize the access to a queue itself. The
extension moves that job to the driver. The Vulkan runtime already locks
a queue around submit when it carries the INTERNALLY_SYNCHRONIZED bit,
which is the same serialization panvk relied on. So the submit path stays
as is and only the extension and feature get exposed.

Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42397>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -75,6 +75,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_image_format_list = true,
       .KHR_imageless_framebuffer = true,
       .KHR_index_type_uint8 = true,
+      .KHR_internally_synchronized_queues = true,
       .KHR_line_rasterization = true,
       .KHR_load_store_op_none = true,
       .KHR_maintenance1 = true,
@@ -481,6 +482,9 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_KHR_maintenance9 */
       .maintenance9 = true,
+
+      /* VK_KHR_internally_synchronized_queues */
+      .internallySynchronizedQueues = true,
 
       /* VK_EXT_graphics_pipeline_library */
       .graphicsPipelineLibrary = true,

commit d687d7e18f16380651bc387c8ff0d448b425e97a

panvk: Disable VK_KHR_internally_synchronized_queues on Vulkan 1.0
The extension depends on Vulkan 1.1.

Fixes: 6342674dc11 ("panvk: Advertise VK_KHR_internally_synchronized_queues")
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Reviewed-by: Aksel Hjerpbakk <aksel.hjerpbakk@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43150>

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -75,7 +75,7 @@ panvk_per_arch(get_physical_device_extensions)(
       .KHR_image_format_list = true,
       .KHR_imageless_framebuffer = true,
       .KHR_index_type_uint8 = true,
-      .KHR_internally_synchronized_queues = true,
+      .KHR_internally_synchronized_queues = has_vk1_1,
       .KHR_line_rasterization = true,
       .KHR_load_store_op_none = true,
       .KHR_maintenance1 = true,