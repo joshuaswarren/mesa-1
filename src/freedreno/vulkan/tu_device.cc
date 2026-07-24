joshuaswarren/mesa-1

commit 21fa50d3c5802a16f8c44127e70780b9b5b6af0d

tu: Advertise VK_KHR_internally_synchronized_queues
This is implemented in common code in d787fc00dda ("vulkan: add support
for VK_KHR_internally_synchronized_queues").

Passes dEQP-VK.synchronization2.internally_synchronized_queues.*

Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41926>

src/freedreno/vulkan/tu_device.cc
@@ -204,6 +204,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .KHR_incremental_present = true,
 #endif
       .KHR_index_type_uint8 = true,
+      .KHR_internally_synchronized_queues = true,
       .KHR_line_rasterization = !device->info->props.is_a702,
       .KHR_load_store_op_none = true,
       .KHR_maintenance1 = true,
@@ -550,6 +551,9 @@ tu_get_features(struct tu_physical_device *pdevice,
 
    /* VK_KHR_index_type_uint8 */
    features->indexTypeUint8 = true;
+
+   /* VK_KHR_internally_synchronized_queues */
+   features->internallySynchronizedQueues = true;
 
    /* VK_KHR_line_rasterization */
    features->rectangularLines = !pdevice->info->props.is_a702;

commit 63be54be6c0c4c3327c79debd13dfad138aba9d6

tu: limit KHR_internally_synchronized_queues to Vulkan 1.1+
The extension depends on VK 1.1.

Fixes: 21fa50d3c58 ("tu: Advertise VK_KHR_internally_synchronized_queues")
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42343>

src/freedreno/vulkan/tu_device.cc
@@ -256,7 +256,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .KHR_incremental_present = true,
 #endif
       .KHR_index_type_uint8 = true,
-      .KHR_internally_synchronized_queues = true,
+      .KHR_internally_synchronized_queues = tu_is_vk_1_1(device),
       .KHR_line_rasterization = !device->info->props.is_a702,
       .KHR_load_store_op_none = true,
       .KHR_maintenance1 = true,