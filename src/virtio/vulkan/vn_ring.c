joshuaswarren/mesa-1

commit d9352137a0a3936e1907de61e43e118cdcdd364f

venus: fix unbound malloc leak in vn_ring_get_submits
Credits to Toshinari Morikawa, and this supersedes
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41904.

ring->free_submits can go unbound for heavy scenes that use 3 or more
shm pool allocs for CS storage. This change fixes to loop through the
free list for a cached entry, which is good enough in practice since
the most recently retired submit is cached at the list head.

Cc: mesa-stable
Reported-by: Toshinari Morikawa <morikawa.toshinari@jp.panasonic.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41917>

src/virtio/vulkan/vn_ring.c
@@ -418,22 +418,18 @@ vn_ring_get_id(struct vn_ring *ring)
 static struct vn_ring_submit *
 vn_ring_get_submit(struct vn_ring *ring, uint32_t shmem_count)
 {
-   const uint32_t min_shmem_count = 2;
-   struct vn_ring_submit *submit;
-
-   /* TODO this could be simplified if we could omit shmem_count */
-   if (shmem_count <= min_shmem_count &&
-       !list_is_empty(&ring->free_submits)) {
-      submit =
-         list_first_entry(&ring->free_submits, struct vn_ring_submit, head);
-      list_del(&submit->head);
-   } else {
-      const size_t submit_size = offsetof(
-         struct vn_ring_submit, shmems[MAX2(shmem_count, min_shmem_count)]);
-      submit = malloc(submit_size);
+   list_for_each_entry_safe(struct vn_ring_submit, submit,
+                            &ring->free_submits, head) {
+      if (submit->shmem_count >= shmem_count) {
+         list_del(&submit->head);
+         return submit;
+      }
    }
 
-   return submit;
+   const uint32_t min_shmem_count = 2;
+   const size_t submit_size = offsetof(
+      struct vn_ring_submit, shmems[MAX2(shmem_count, min_shmem_count)]);
+   return malloc(submit_size);
 }
 
 static bool

commit 34c5feac87758c6cd9cd41f6ab0e9477badf1c21

venus: ensure cached vn_ring_submit batches are bounded
The prior fix can still end up with unbounded leak since the actual
shmem count in the storage isn't tracked. This change reverts the prior
fix while avoiding caching over-sized batch. Practically, there's no
need to cache more since the malloc overhead is trivial as compared to
indirect uploads.

Fixes: d9352137a0a ("venus: fix unbound malloc leak in vn_ring_get_submits")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43229>

src/virtio/vulkan/vn_ring.c
@@ -17,6 +17,9 @@
 
 #define VN_RING_IDLE_TIMEOUT_NS (1ull * 1000 * 1000)
 
+/* for better recycle of the vn_ring_submit batch */
+#define VN_MIN_SHMEM_COUNT (2)
+
 static_assert(ATOMIC_INT_LOCK_FREE == 2 && sizeof(atomic_uint) == 4,
               "vn_ring_shared requires lock-free 32-bit atomic_uint");
 
@@ -168,7 +171,12 @@ vn_ring_retire_submits(struct vn_ring *ring, uint32_t seqno)
       for (uint32_t i = 0; i < submit->shmem_count; i++)
          vn_renderer_shmem_unref(renderer, submit->shmems[i]);
 
-      list_move_to(&submit->head, &ring->free_submits);
+      if (submit->shmem_count <= VN_MIN_SHMEM_COUNT) {
+         list_move_to(&submit->head, &ring->free_submits);
+      } else {
+         list_del(&submit->head);
+         free(submit);
+      }
    }
 }
 
@@ -418,18 +426,21 @@ vn_ring_get_id(struct vn_ring *ring)
 static struct vn_ring_submit *
 vn_ring_get_submit(struct vn_ring *ring, uint32_t shmem_count)
 {
-   list_for_each_entry_safe(struct vn_ring_submit, submit,
-                            &ring->free_submits, head) {
-      if (submit->shmem_count >= shmem_count) {
-         list_del(&submit->head);
-         return submit;
-      }
+   struct vn_ring_submit *submit;
+
+   if (shmem_count <= VN_MIN_SHMEM_COUNT &&
+       !list_is_empty(&ring->free_submits)) {
+      submit =
+         list_first_entry(&ring->free_submits, struct vn_ring_submit, head);
+      list_del(&submit->head);
+   } else {
+      const size_t submit_size =
+         offsetof(struct vn_ring_submit,
+                  shmems[MAX2(shmem_count, VN_MIN_SHMEM_COUNT)]);
+      submit = malloc(submit_size);
    }
 
-   const uint32_t min_shmem_count = 2;
-   const size_t submit_size = offsetof(
-      struct vn_ring_submit, shmems[MAX2(shmem_count, min_shmem_count)]);
-   return malloc(submit_size);
+   return submit;
 }
 
 static bool