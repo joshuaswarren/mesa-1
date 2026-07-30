joshuaswarren/mesa-1

commit 2aa5901546972fe0f99ebedbaa14459e1ef23a39

poly: move hk passthrough GS code to libpoly
We are going to use this in panfrost.

Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41192>

src/poly/nir/poly_nir.h
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include "poly/geometry.h"
 #include "nir.h"
+#include "nir_xfb_info.h"
 #include "shader_enums.h"
 
 struct nir_def *poly_load_per_vertex_input(struct nir_builder *b,
@@ -61,3 +62,32 @@ uint64_t poly_tcs_per_vertex_outputs(const struct nir_shader *nir);
 unsigned poly_tcs_output_stride(const struct nir_shader *nir);
 
 bool poly_nir_lower_sysvals(struct nir_shader *nir);
+
+struct poly_passthrough_gs_key {
+   /* Bit mask of outputs written by the VS/TES, to be passed through */
+   uint64_t outputs;
+
+   /* Clip/cull sizes, implies clip/cull written in output */
+   uint8_t clip_distance_array_size;
+   uint8_t cull_distance_array_size;
+
+   /* Transform feedback buffer strides */
+   uint8_t xfb_stride[MAX_XFB_BUFFERS];
+
+   /* Decomposed primitive */
+   enum mesa_prim prim;
+
+   /* Transform feedback info. Must use hk_passthrough_gs_key_size to get the
+    * key size */
+    nir_xfb_info xfb_info;
+ };
+
+static inline size_t
+poly_passthrough_gs_key_size(uint16_t output_count)
+{
+   return (sizeof(struct poly_passthrough_gs_key) - sizeof(nir_xfb_info)) +
+      nir_xfb_info_size(output_count);
+}
+
+
+void poly_nir_passthrough_gs(struct nir_builder *b, const void *key_);

src/asahi/vulkan/hk_cmd_draw.c
@@ -3235,10 +3235,10 @@ hk_handle_passthrough_gs(struct hk_cmd_buffer *cmd, struct agx_draw draw)
    }
 
    /* Else, we need to bind a passthrough GS */
-   size_t key_size = hk_passthrough_gs_key_size(xfb_outputs);
-   struct hk_passthrough_gs_key *key = alloca(key_size);
+   size_t key_size = poly_passthrough_gs_key_size(xfb_outputs);
+   struct poly_passthrough_gs_key *key = alloca(key_size);
 
-   *key = (struct hk_passthrough_gs_key){
+   *key = (struct poly_passthrough_gs_key){
       .prim = u_decomposed_prim(hk_gs_in_prim(cmd)),
       .outputs = last_sw->b.info.outputs,
       .clip_distance_array_size = last_sw->info.clip_distance_array_size,
@@ -3256,7 +3256,7 @@ hk_handle_passthrough_gs(struct hk_cmd_buffer *cmd, struct agx_draw draw)
    struct hk_device *dev = hk_cmd_buffer_device(cmd);
    perf_debug(cmd, "Binding passthrough GS for%s\n", xfb_outputs ? " XFB" : "");
 
-   gs = hk_meta_shader(dev, hk_nir_passthrough_gs, key, key_size);
+   gs = hk_meta_shader(dev, poly_nir_passthrough_gs, key, key_size);
    gs->is_passthrough = true;
    hk_cmd_bind_graphics_shader(cmd, MESA_SHADER_GEOMETRY, gs);
 }

src/asahi/vulkan/hk_shader.h
@@ -20,7 +20,6 @@
 #include "hk_device_memory.h"
 #include "hk_private.h"
 
-#include "nir_xfb_info.h"
 #include "shader_enums.h"
 #include "vk_pipeline_cache.h"
 
@@ -372,31 +371,3 @@ hk_get_nir_options(struct vk_physical_device *vk_pdev, mesa_shader_stage stage,
 struct hk_api_shader *hk_meta_shader(struct hk_device *dev,
                                      hk_internal_builder_t builder, void *data,
                                      size_t data_size);
-
-struct hk_passthrough_gs_key {
-   /* Bit mask of outputs written by the VS/TES, to be passed through */
-   uint64_t outputs;
-
-   /* Clip/cull sizes, implies clip/cull written in output */
-   uint8_t clip_distance_array_size;
-   uint8_t cull_distance_array_size;
-
-   /* Transform feedback buffer strides */
-   uint8_t xfb_stride[MAX_XFB_BUFFERS];
-
-   /* Decomposed primitive */
-   enum mesa_prim prim;
-
-   /* Transform feedback info. Must use hk_passthrough_gs_key_size to get the
-    * key size */
-   nir_xfb_info xfb_info;
-};
-
-static inline size_t
-hk_passthrough_gs_key_size(uint16_t output_count)
-{
-   return (sizeof(struct hk_passthrough_gs_key) - sizeof(nir_xfb_info)) +
-      nir_xfb_info_size(output_count);
-}
-
-void hk_nir_passthrough_gs(struct nir_builder *b, const void *key_);

src/asahi/vulkan/meson.build
@@ -33,7 +33,6 @@ hk_files = files(
   'hk_instance.c',
   'hk_instance.h',
   'hk_nir_lower_descriptors.c',
-  'hk_nir_passthrough_gs.c',
   'hk_physical_device.c',
   'hk_physical_device.h',
   'hk_private.h',

src/poly/nir/meson.build
@@ -6,6 +6,7 @@ libpoly_nir_files = files(
   'poly_nir_lower_sysvals.c',
   'poly_nir_lower_tess.c',
   'poly_nir_lower_vs.c',
+  'poly_nir_passthrough_gs.c',
 )
 
 libpoly_nir = static_library(

src/poly/nir/poly_nir_passthrough_gs.c
@@ -6,17 +6,17 @@
  */
 
 #include "util/bitscan.h"
-#include "hk_shader.h"
+#include "poly_nir.h"
 #include "nir.h"
 #include "nir_builder.h"
 #include "nir_xfb_info.h"
 #include "shader_enums.h"
 
 void
-hk_nir_passthrough_gs(nir_builder *b, const void *key_)
+poly_nir_passthrough_gs(nir_builder *b, const void *key_)
 {
    nir_shader *s = b->shader;
-   const struct hk_passthrough_gs_key *key = key_;
+   const struct poly_passthrough_gs_key *key = key_;
    assert(key->prim == u_decomposed_prim(key->prim));
    assert(key->prim != MESA_PRIM_PATCHES && "tessellation consumes patches");
 

commit 88ab896b4d3415d5d1f74c7c6f81cf75f890d384

poly: Fix comment after moving passthrough_gs
Replace outdated reference to hk_passthrough_gs_key_size after moving
passthrough_gs from hk to poly in
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41192 (noticed
as comment in the merge request).

Fixes: 2aa590154697 ("poly: move hk passthrough GS code to libpoly")
Signed-off-by: Janne Grunau <j@jannau.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43310>

src/poly/nir/poly_nir.h
@@ -83,7 +83,7 @@ struct poly_passthrough_gs_key {
    /* Decomposed primitive */
    enum mesa_prim prim;
 
-   /* Transform feedback info. Must use hk_passthrough_gs_key_size to get the
+   /* Transform feedback info. Must use poly_passthrough_gs_key_size to get the
     * key size */
     nir_xfb_info xfb_info;
  };