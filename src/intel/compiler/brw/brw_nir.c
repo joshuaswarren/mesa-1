joshuaswarren/mesa-1

commit f8e9f56c2a94f2a46f5f6a63affff8fa84e3d156

brw: Respect LSC data sizes and address alignment
For data size D32/D64, if address alignment is data size, then we could
write vector of 2/3/4/8.

and for data size D8U32/D16U32/D32/D64, if address alignment is 1 byte,
then we can write vector of 1 (1/2/4/8 size).

Fossil-db result on Xe2:

Totals:
Instrs: 719866 -> 719509 (-0.05%); split: -0.07%, +0.02%
CodeSize: 11068240 -> 11061040 (-0.07%); split: -0.11%, +0.04%
Send messages: 23200 -> 23025 (-0.75%)
Cycle count: 3882429518 -> 3885023550 (+0.07%); split: -0.05%, +0.12%
Spill count: 15503 -> 15485 (-0.12%)
Fill count: 24167 -> 24150 (-0.07%); split: -0.09%, +0.02%
Max live registers: 112922 -> 112870 (-0.05%); split: -0.05%, +0.01%
Non SSA regs after NIR: 116403 -> 116432 (+0.02%); split: -0.01%, +0.04%

Totals from 88 (5.72% of 1538) affected shaders:
Instrs: 238247 -> 237890 (-0.15%); split: -0.21%, +0.06%
CodeSize: 3823536 -> 3816336 (-0.19%); split: -0.31%, +0.12%
Send messages: 3799 -> 3624 (-4.61%)
Cycle count: 2545396786 -> 2547990818 (+0.10%); split: -0.08%, +0.18%
Spill count: 9659 -> 9641 (-0.19%)
Fill count: 18044 -> 18027 (-0.09%); split: -0.12%, +0.03%
Max live registers: 12110 -> 12058 (-0.43%); split: -0.50%, +0.07%
Non SSA regs after NIR: 24552 -> 24581 (+0.12%); split: -0.06%, +0.18%

Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41305>

src/intel/compiler/brw/brw_nir.c
@@ -3087,52 +3087,117 @@ get_mem_access_size_align(nir_intrinsic_op intrin, uint8_t bytes,
    const bool is_scratch = intrin == nir_intrinsic_load_scratch ||
                            intrin == nir_intrinsic_store_scratch;
 
-   if (align < 4 || bytes < 4) {
-      /* Choose a byte, word, or dword */
-      bytes = MIN2(bytes, 4);
-      if (bytes == 3)
-         bytes = (is_load && align >= 4) ? 4 : 2;
+   /* On older platforms, we have URB messages routing through HDC with LSC
+    * support in place. So make sure to use older code path for URB accesses.
+    */
+   const bool urb_access =
+      (intrin == nir_intrinsic_load_task_payload ||
+       intrin == nir_intrinsic_store_task_payload ||
+       intrin == nir_intrinsic_load_urb_lsc_intel ||
+       intrin == nir_intrinsic_store_urb_lsc_intel ||
+       intrin == nir_intrinsic_load_urb_vec4_intel ||
+       intrin == nir_intrinsic_store_urb_vec4_intel);
+   const bool via_lsc = urb_access ? devinfo->ver >= 20 : devinfo->has_lsc;
 
-      /* Ensure we split into aligned pieces. We cannot blindly turn an i8vec4
-       * into i32 due to the alignment requirements. It might be possible to
-       * relax this later, though.
+   if (via_lsc) {
+      /* Data size:           D64
+       * Address alignment:   8
+       * Vector size allowed: 2/3/4/8
        */
-      bytes = MIN2(bytes, align);
+      if (align == 8 && bit_size == 64 && bytes >= 8) {
+         bytes = MIN2(bytes, 64);
+         uint32_t comps = bytes / 8;
 
-      if (is_scratch) {
-         /* The way scratch address swizzling works in the back-end, it
-          * happens at a DWORD granularity so we can't have a single load
-          * or store cross a DWORD boundary.
-          */
-         if ((align_offset % 4) + bytes > MIN2(align_mul, 4))
-            bytes = MIN2(align_mul, 4) - (align_offset % 4);
+         /* We would need to SIMD split for dvec3+ */
+         comps = MIN2(comps, 2);
 
-         /* Must be a power of two */
-         if (bytes == 3)
-            bytes = 2;
+         return (nir_mem_access_size_align) {
+            .bit_size = 64,
+            .num_components = comps,
+            .align = 8,
+            .shift = nir_mem_access_shift_method_scalar,
+         };
       }
 
-      return (nir_mem_access_size_align) {
-         .bit_size = bytes * 8,
-         .num_components = 1,
-         .align = MIN2(align, 4),
-         .shift = nir_mem_access_shift_method_scalar,
-      };
-   } else {
-      bytes = MIN2(bytes, 16);
+      if (align < 4 || bytes < 8) {
+         /* Data size:           D8D32,D16D32,D32,D64
+          * Address alignment:   1
+          * Vector size allowed: 1
+          *
+          * When we have bytes 3 or greater than 4 and less than 8, for load,
+          * we can overfetch data but for store, we have only respect what HW
+          * allow us to write at once. For example, for 3 bytes store, we split
+          * it into 2 and next iteration of pass will take care of 1 byte.
+          */
+         bytes = MIN2(bytes, 8);
+         if (bytes == 3)
+            bytes = (is_load && align >= 4) ? 4 : 2;
+         if (bytes > 4 && bytes < 8)
+            bytes = (is_load && align >= 8) ? 8 : 4;
 
-      /* With UGM LSC dataport, we don't need to lower 64bit data access into
-       * two 32bit single vector access since it supports direct 64bit data
-       * operation.
-       */
-      if (devinfo->has_lsc && align == 8 && bit_size == 64 && !is_scratch) {
          return (nir_mem_access_size_align) {
-            .bit_size = bit_size,
-            .num_components = bytes / 8,
-            .align = bit_size / 8,
+            .bit_size = bytes * 8,
+            .num_components = 1,
+            .align = 1,
             .shift = nir_mem_access_shift_method_scalar,
          };
       } else {
+         /* Data size:           D32
+          * Address alignment:   4
+          * Vector size allowed: 2/3/4/8
+          */
+         bytes = MIN2(bytes, 32);
+         uint32_t comps = bytes / 4;
+
+         /* Reject V5/V6/V7 components and clamp it to supported size. */
+         if (comps > 4 && comps < 8)
+            comps = is_load ? 8 : 4;
+
+         /* We would need to SIMD split for vec8+ (vec16+ on Xe2+) */
+         comps = MIN2(devinfo->ver >= 20 ? 8 : 4, comps);
+
+         return (nir_mem_access_size_align) {
+            .bit_size = 32,
+            .num_components = is_scratch ? 1 : comps,
+            .align = 4,
+            .shift = nir_mem_access_shift_method_scalar,
+         };
+      }
+   } else {
+      if (align < 4 || bytes < 4) {
+         /* Choose a byte, word, or dword */
+         bytes = MIN2(bytes, 4);
+         if (bytes == 3)
+            bytes = (is_load && align >= 4) ? 4 : 2;
+
+         /* Ensure we split into aligned pieces. We cannot blindly turn an i8vec4
+          * into i32 due to the alignment requirements. It might be possible to
+          * relax this later, though.
+          */
+         bytes = MIN2(bytes, align);
+
+         if (is_scratch) {
+            /* The way scratch address swizzling works in the back-end, it
+             * happens at a DWORD granularity so we can't have a single load
+             * or store cross a DWORD boundary.
+             */
+            if ((align_offset % 4) + bytes > MIN2(align_mul, 4))
+               bytes = MIN2(align_mul, 4) - (align_offset % 4);
+
+            /* Must be a power of two */
+            if (bytes == 3)
+               bytes = 2;
+         }
+
+         return (nir_mem_access_size_align) {
+            .bit_size = bytes * 8,
+            .num_components = 1,
+            .align = MIN2(align, 4),
+            .shift = nir_mem_access_shift_method_scalar,
+         };
+      } else {
+         bytes = MIN2(bytes, 16);
+
          return (nir_mem_access_size_align) {
             .bit_size = 32,
             .num_components = is_scratch ? 1 :

src/intel/compiler/brw/brw_lower_logical_sends.cpp
@@ -1201,10 +1201,10 @@ lower_lsc_memory_logical_send(const brw_builder &bld, brw_mem_inst *mem)
          payload2 = data0;
          ex_mlen = DIV_ROUND_UP(components, 8);
       } else {
-         brw_reg data[8];
+         brw_reg data[NIR_MAX_VEC_COMPONENTS];
          unsigned size = 0;
 
-         assert(components < 8);
+         assert(components < NIR_MAX_VEC_COMPONENTS);
 
          for (unsigned i = 0; i < components; i++)
             data[size++] = offset(data0, bld, i);

commit efa6cf516364a74aa501a000c7b2e510b4ab8378

intel/nir: Avoid vec8 loads/stores even on Xe2+
In !41305 we thought the restriction was to avoid V8 loads/stores in
SIMD32 on pre-Xe2 systems, and V16 loads on Xe2+.  But I'm getting GPU
hangs in the CTS, and the simulator seems to think that the restriction
is actually V8 load/stores even on Xe2.

Fixes dEQP-VK.spirv_assembly.instruction.maint9_vectorization.bit_field_insert.result_v64i-base_v64i-insert_v64i-offset_s32u-count_s32u on Battlemage with Jay.

Fixes: f8e9f56c2a9 ("brw: Respect LSC data sizes and address alignment")
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43398>

src/intel/compiler/brw/brw_nir.c
@@ -3153,8 +3153,8 @@ get_mem_access_size_align(nir_intrinsic_op intrin, uint8_t bytes,
          if (comps > 4 && comps < 8)
             comps = is_load ? 8 : 4;
 
-         /* We would need to SIMD split for vec8+ (vec16+ on Xe2+) */
-         comps = MIN2(devinfo->ver >= 20 ? 8 : 4, comps);
+         /* We would need to SIMD split for vec8+ */
+         comps = MIN2(4, comps);
 
          return (nir_mem_access_size_align) {
             .bit_size = 32,