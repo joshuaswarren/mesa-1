joshuaswarren/mesa-1

commit bfdb2c1fcba1114eee090281918d3ce45e65ed9a

spirv: implement SPV_KHR_abort
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41651>

src/compiler/spirv/spirv_to_nir.c
@@ -36,6 +36,7 @@
  * suffixes do not exist and 8 comes before 16.
  */
 static const struct spirv_capabilities implemented_capabilities = {
+   .AbortKHR = true,
    .Addresses = true,
    .AtomicFloat16AddEXT = true,
    .AtomicFloat32AddEXT = true,
@@ -6776,6 +6777,21 @@ vtn_handle_allocate_node_payloads(struct vtn_builder *b, SpvOp opcode,
    nir_initialize_node_payloads(&b->nb, payloads, payload_count, node_index, .execution_scope = scope);
 }
 
+static void
+vtn_handle_abort(struct vtn_builder *b, const uint32_t *w, unsigned count)
+{
+   struct vtn_type *msg_type = vtn_get_type(b, w[1]);
+   struct vtn_ssa_value *msg = vtn_ssa_value(b, w[2]);
+
+   nir_variable *var =
+      nir_local_variable_create(b->nb.impl, msg_type->type, "abort_message");
+   nir_deref_instr *deref = nir_build_deref_var(&b->nb, var);
+
+   vtn_local_store(b, msg, deref, 0);
+
+   nir_abort(&b->nb, &deref->def);
+}
+
 static bool
 vtn_handle_body_instruction(struct vtn_builder *b, SpvOp opcode,
                             const uint32_t *w, unsigned count)
@@ -7322,6 +7338,10 @@ vtn_handle_body_instruction(struct vtn_builder *b, SpvOp opcode,
    case SpvOpCooperativeMatrixReduceNV:
    case SpvOpCooperativeMatrixPerElementOpNV:
       vtn_handle_cooperative_instruction(b, opcode, w, count);
+      break;
+
+   case SpvOpAbortKHR:
+      vtn_handle_abort(b, w, count);
       break;
 
    default:

commit 5d1a344de014108b81c901fb1824f8072e646c3e

spirv: fix handling OpAbortKHR
This should be considered a terminator block instruction as explained
in the SPIR-V spec.

This fixes parsing SPIR-V for
dEQP-VK-experimental.postmortem.shader_abort.* since the tests have
been updated.

Fixes: bfdb2c1fcba ("spirv: implement SPV_KHR_abort")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43452>

src/compiler/spirv/spirv_to_nir.c
@@ -6939,7 +6939,7 @@ vtn_handle_allocate_node_payloads(struct vtn_builder *b, SpvOp opcode,
    nir_initialize_node_payloads(&b->nb, payloads, payload_count, node_index, .execution_scope = scope);
 }
 
-static void
+void
 vtn_handle_abort(struct vtn_builder *b, const uint32_t *w, unsigned count)
 {
    struct vtn_type *msg_type = vtn_get_type(b, w[1]);
@@ -7523,10 +7523,6 @@ vtn_handle_body_instruction(struct vtn_builder *b, SpvOp opcode,
    case SpvOpCooperativeMatrixLoadTensorNV:
    case SpvOpCooperativeMatrixStoreTensorNV:
       vtn_handle_cooperative_instruction(b, opcode, w, count);
-      break;
-
-   case SpvOpAbortKHR:
-      vtn_handle_abort(b, w, count);
       break;
 
    default:

src/compiler/spirv/vtn_cfg.c
@@ -429,6 +429,7 @@ vtn_cfg_handle_prepass_instruction(struct vtn_builder *b, SpvOp opcode,
    case SpvOpReturn:
    case SpvOpReturnValue:
    case SpvOpUnreachable:
+   case SpvOpAbortKHR:
       if (b->wa_ignore_return_after_emit_mesh_tasks &&
           opcode == SpvOpReturn && !b->block) {
             /* At this point block was already reset by

src/compiler/spirv/vtn_private.h
@@ -238,6 +238,7 @@ void vtn_emit_cf_func_structured(struct vtn_builder *b, struct vtn_function *fun
 bool vtn_handle_phis_first_pass(struct vtn_builder *b, SpvOp opcode,
                                 const uint32_t *w, unsigned count);
 void vtn_emit_ret_store(struct vtn_builder *b, const struct vtn_block *block);
+void vtn_handle_abort(struct vtn_builder *b, const uint32_t *w, unsigned count);
 void vtn_build_structured_cfg(struct vtn_builder *b, const uint32_t *words,
                               const uint32_t *end);
 

src/compiler/spirv/vtn_structured_cfg.c
@@ -155,6 +155,7 @@ enum vtn_branch_type {
    vtn_branch_type_terminate_ray,
    vtn_branch_type_emit_mesh_tasks,
    vtn_branch_type_return,
+   vtn_branch_type_abort,
 };
 
 static const char *
@@ -176,6 +177,7 @@ vtn_branch_type_to_string(enum vtn_branch_type t)
    CASE(terminate_ray);
    CASE(emit_mesh_tasks);
    CASE(return);
+   CASE(abort);
    }
 #undef CASE
    UNREACHABLE("unknown branch type");
@@ -370,6 +372,7 @@ structured_post_order_traversal(struct vtn_builder *b, struct vtn_block *block)
    case SpvOpReturnValue:
    case SpvOpEmitMeshTasksEXT:
    case SpvOpUnreachable:
+   case SpvOpAbortKHR:
       block->successors_count = 1;
       block->successors = vtn_zalloc(b, struct vtn_successor);
       break;
@@ -972,6 +975,8 @@ branch_type_for_terminator(struct vtn_builder *b, struct vtn_block *block)
    case SpvOpReturnValue:
    case SpvOpUnreachable:
       return vtn_branch_type_return;
+   case SpvOpAbortKHR:
+      return vtn_branch_type_abort;
    default:
       UNREACHABLE("unexpected terminator operation");
       return vtn_branch_type_none;
@@ -1246,6 +1251,12 @@ vtn_emit_branch(struct vtn_builder *b, const struct vtn_block *block,
       vtn_assert(block);
       vtn_emit_ret_store(b, block);
       nir_jump(&b->nb, nir_jump_return);
+      break;
+
+   case vtn_branch_type_abort:
+      vtn_assert(block);
+      vtn_assert(block->branch);
+      vtn_handle_abort(b, block->branch, block->branch[0] >> SpvWordCountShift);
       break;
 
    case vtn_branch_type_discard: