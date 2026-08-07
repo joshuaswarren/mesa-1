joshuaswarren/mesa-1

commit e4fdf157888618645cfec7cd6879100c09cb37be

radv: implement VK_KHR_extended_flags
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42631>

src/amd/vulkan/radv_video.c
@@ -131,8 +131,14 @@ static VkResult
 create_intra_only_dpb(struct radv_device *dev, struct radv_physical_device *pdev, struct radv_video_session *vid,
                       const VkVideoProfileInfoKHR *profile, const VkAllocationCallbacks *alloc)
 {
+   VkImageUsageFlags2CreateInfoKHR usage_create_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_USAGE_FLAGS_2_CREATE_INFO_KHR,
+      .usage = vid->encode ? VK_IMAGE_USAGE_2_VIDEO_ENCODE_DPB_BIT_KHR : VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR,
+   };
+
    VkVideoProfileListInfoKHR profile_list = {
       .sType = VK_STRUCTURE_TYPE_VIDEO_PROFILE_LIST_INFO_KHR,
+      .pNext = &usage_create_info,
       .profileCount = 1,
       .pProfiles = profile,
    };
@@ -140,7 +146,6 @@ create_intra_only_dpb(struct radv_device *dev, struct radv_physical_device *pdev
    VkPhysicalDeviceVideoFormatInfoKHR format_info = {
       .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VIDEO_FORMAT_INFO_KHR,
       .pNext = &profile_list,
-      .imageUsage = vid->encode ? VK_IMAGE_USAGE_VIDEO_ENCODE_DPB_BIT_KHR : VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR,
    };
 
    VkVideoFormatPropertiesKHR format_props = {
@@ -803,6 +808,18 @@ radv_GetPhysicalDeviceVideoCapabilitiesKHR(VkPhysicalDevice physicalDevice, cons
    return VK_SUCCESS;
 }
 
+static VkImageUsageFlags2KHR
+vk_video_format_info_usage(const VkPhysicalDeviceVideoFormatInfoKHR *info)
+{
+   const VkImageUsageFlags2CreateInfoKHR *usage_create_info =
+      vk_find_struct_const(info->pNext, IMAGE_USAGE_FLAGS_2_CREATE_INFO_KHR);
+   if (usage_create_info) {
+      return usage_create_info->usage;
+   } else {
+      return info->imageUsage;
+   }
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL
 radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
                                                const VkPhysicalDeviceVideoFormatInfoKHR *pVideoFormatInfo,
@@ -810,25 +827,23 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
                                                VkVideoFormatPropertiesKHR *pVideoFormatProperties)
 {
    VK_FROM_HANDLE(radv_physical_device, pdev, physicalDevice);
+   const VkImageUsageFlags2KHR image_usage = vk_video_format_info_usage(pVideoFormatInfo);
 
-   if ((pVideoFormatInfo->imageUsage &
-        (VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR | VK_IMAGE_USAGE_VIDEO_ENCODE_DPB_BIT_KHR |
-         VK_IMAGE_USAGE_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR)) &&
+   if ((image_usage & (VK_IMAGE_USAGE_2_VIDEO_ENCODE_SRC_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_ENCODE_DPB_BIT_KHR |
+                       VK_IMAGE_USAGE_2_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR)) &&
        !pdev->video_encode_enabled)
       return VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR;
 
    /* Cannot be a QP map and other video enc/dec usages, as they are totally different formats. */
-   if ((pVideoFormatInfo->imageUsage & VK_IMAGE_USAGE_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR) &&
-       (pVideoFormatInfo->imageUsage &
-        (VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR | VK_IMAGE_USAGE_VIDEO_ENCODE_DPB_BIT_KHR |
-         VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR)))
+   if ((image_usage & VK_IMAGE_USAGE_2_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR) &&
+       (image_usage & (VK_IMAGE_USAGE_2_VIDEO_ENCODE_SRC_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_ENCODE_DPB_BIT_KHR |
+                       VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR)))
       return VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR;
 
    /* VCN < 5 requires separate allocates for DPB and decode video. */
    if (pdev->info.vcn_ip_version < VCN_5_0_0 &&
-       (pVideoFormatInfo->imageUsage &
-        (VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR)) ==
-          (VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR))
+       (image_usage & (VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR)) ==
+          (VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR))
       return VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR;
 
    VkFormat formats[2] = {VK_FORMAT_UNDEFINED, VK_FORMAT_UNDEFINED};
@@ -842,7 +857,7 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
       const VkVideoEncodeProfileRgbConversionInfoVALVE *rgbProfile =
          vk_find_struct_const(profile, VIDEO_ENCODE_PROFILE_RGB_CONVERSION_INFO_VALVE);
       bool rgb = rgbProfile && rgbProfile->performEncodeRgbConversion &&
-                 (pVideoFormatInfo->imageUsage & VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR);
+                 (image_usage & VK_IMAGE_USAGE_2_VIDEO_ENCODE_SRC_BIT_KHR);
       struct ac_video_dec_codec_caps *dcap = NULL;
       struct ac_video_enc_codec_caps *ecap = NULL;
 
@@ -856,7 +871,7 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
       radv_video_get_caps(pdev, profile->videoCodecOperation, &dcap, &ecap);
 
       VkFormat profile_formats[2] = {VK_FORMAT_UNDEFINED, VK_FORMAT_UNDEFINED};
-      if (pVideoFormatInfo->imageUsage & VK_IMAGE_USAGE_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR) {
+      if (image_usage & VK_IMAGE_USAGE_2_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR) {
          /* All profiles must share the same qp_map texel size. */
          if (qp_map_texel_size != 0 && qp_map_texel_size != ecap->qp_map_texel_size)
             return VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR;
@@ -900,11 +915,11 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
    if (formats[0] == VK_FORMAT_UNDEFINED)
       return VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR;
 
-   const bool qp_map = pVideoFormatInfo->imageUsage & VK_IMAGE_USAGE_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR;
-   const bool dpb = pVideoFormatInfo->imageUsage &
-                    (VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_VIDEO_ENCODE_DPB_BIT_KHR);
-   const bool src_dst = pVideoFormatInfo->imageUsage &
-                        (VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR | VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR);
+   const bool qp_map = image_usage & VK_IMAGE_USAGE_2_VIDEO_ENCODE_QUANTIZATION_DELTA_MAP_BIT_KHR;
+   const bool dpb =
+      image_usage & (VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_ENCODE_DPB_BIT_KHR);
+   const bool src_dst =
+      image_usage & (VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_ENCODE_SRC_BIT_KHR);
    VkImageTiling tiling[3];
    uint32_t num_tiling = 0;
 
@@ -916,13 +931,13 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
    if ((src_dst || qp_map) && pdev->info.gfx_level >= GFX9)
       tiling[num_tiling++] = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
 
-   VkImageUsageFlags usage_flags = pVideoFormatInfo->imageUsage;
+   VkImageUsageFlags2KHR usage_flags = image_usage;
 
-   if (usage_flags & VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR)
-      usage_flags |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+   if (usage_flags & VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR)
+      usage_flags |= VK_IMAGE_USAGE_2_TRANSFER_SRC_BIT_KHR | VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR;
 
-   if (usage_flags & VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR)
-      usage_flags |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+   if (usage_flags & VK_IMAGE_USAGE_2_VIDEO_ENCODE_SRC_BIT_KHR)
+      usage_flags |= VK_IMAGE_USAGE_2_TRANSFER_DST_BIT_KHR;
 
    VK_OUTARRAY_MAKE_TYPED(VkVideoFormatPropertiesKHR, out, pVideoFormatProperties, pVideoFormatPropertyCount);
 
@@ -940,8 +955,8 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
             p->componentMapping.a = VK_COMPONENT_SWIZZLE_IDENTITY;
             p->imageCreateFlags = 0;
             if (src_dst || qp_map)
-               p->imageCreateFlags |=
-                  VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT | VK_IMAGE_CREATE_EXTENDED_USAGE_BIT | VK_IMAGE_CREATE_ALIAS_BIT;
+               p->imageCreateFlags |= VK_IMAGE_CREATE_2_MUTABLE_FORMAT_BIT_KHR |
+                                      VK_IMAGE_CREATE_2_EXTENDED_USAGE_BIT_KHR | VK_IMAGE_CREATE_2_ALIAS_BIT_KHR;
             p->imageType = VK_IMAGE_TYPE_2D;
             p->imageTiling = tiling[j];
             p->imageUsageFlags = usage_flags;
@@ -1843,7 +1858,8 @@ select_tier(struct radv_device *device, struct radv_video_session *vid, const st
    struct radv_image_plane *luma = &dst_iv->image->planes[0];
 
    if (vid->dec->tiers & AC_VIDEO_DEC_TIER3) {
-      VkImageUsageFlags coincide = VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR;
+      VkImageUsageFlags2KHR coincide =
+         VK_IMAGE_USAGE_2_VIDEO_DECODE_DPB_BIT_KHR | VK_IMAGE_USAGE_2_VIDEO_DECODE_DST_BIT_KHR;
       if (!luma->surface.is_linear && (dst_iv->image->vk.usage & coincide) == coincide)
          return AC_VIDEO_DEC_TIER3;
    }

src/amd/vulkan/layers/radv_app_workarounds.c
@@ -45,7 +45,7 @@ no_mans_sky_CreateImageView(VkDevice _device, const VkImageViewCreateInfo *pCrea
 
    if ((iview->vk.aspects == (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) &&
        (iview->vk.usage &
-        (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT))) {
+        (VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR | VK_IMAGE_USAGE_2_STORAGE_BIT_KHR | VK_IMAGE_USAGE_2_INPUT_ATTACHMENT_BIT_KHR))) {
       /* No Man's Sky creates descriptors with depth/stencil aspects (only when Intel XESS is
        * enabled apparently). and this is illegal in Vulkan. Ignore them by using NULL descriptors
        * to workaroud GPU hangs.

src/amd/vulkan/meta/radv_meta_astc_decode.c
@@ -64,11 +64,11 @@ get_view_type(const struct radv_image *image)
 
 static void
 image_view_init(struct radv_device *device, struct radv_image *image, VkFormat format, VkImageAspectFlags aspectMask,
-                VkImageUsageFlags usage, uint32_t baseMipLevel, uint32_t baseArrayLayer, uint32_t layerCount,
+                VkImageUsageFlags2KHR usage, uint32_t baseMipLevel, uint32_t baseArrayLayer, uint32_t layerCount,
                 struct radv_image_view *iview)
 {
-   const VkImageViewUsageCreateInfo iview_usage_info = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
+   const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
       .usage = usage,
    };
 
@@ -106,10 +106,10 @@ radv_meta_decode_astc(struct radv_cmd_buffer *cmd_buffer, struct radv_image *ima
    offset = vk_image_sanitize_offset(&image->vk, offset);
 
    struct radv_image_view src_iview, dst_iview;
-   image_view_init(device, image, VK_FORMAT_R32G32B32A32_UINT, VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_USAGE_SAMPLED_BIT,
+   image_view_init(device, image, VK_FORMAT_R32G32B32A32_UINT, VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR,
                    subresource->mipLevel, subresource->baseArrayLayer,
                    vk_image_subresource_layer_count(&image->vk, subresource), &src_iview);
-   image_view_init(device, image, VK_FORMAT_R8G8B8A8_UINT, VK_IMAGE_ASPECT_PLANE_1_BIT, VK_IMAGE_USAGE_STORAGE_BIT,
+   image_view_init(device, image, VK_FORMAT_R8G8B8A8_UINT, VK_IMAGE_ASPECT_PLANE_1_BIT, VK_IMAGE_USAGE_2_STORAGE_BIT_KHR,
                    subresource->mipLevel, subresource->baseArrayLayer,
                    vk_image_subresource_layer_count(&image->vk, subresource), &dst_iview);
 

src/amd/vulkan/meta/radv_meta_blit.c
@@ -467,10 +467,10 @@ blit_image(struct radv_cmd_buffer *cmd_buffer, struct radv_image *src_image, VkI
       /* 3D images have just 1 layer */
       const uint32_t src_array_slice = src_image->vk.image_type == VK_IMAGE_TYPE_3D ? 0 : src_start + i;
 
-      const VkImageViewUsageCreateInfo dst_iview_usage_info = {
-         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-         .usage = vk_format_is_color(dst_image->vk.format) ? VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
-                                                           : VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+      const VkImageViewUsage2CreateInfoKHR dst_iview_usage_info = {
+         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+         .usage = vk_format_is_color(dst_image->vk.format) ? VK_IMAGE_USAGE_2_COLOR_ATTACHMENT_BIT_KHR
+                                                           : VK_IMAGE_USAGE_2_DEPTH_STENCIL_ATTACHMENT_BIT_KHR,
       };
 
       radv_image_view_init(&dst_iview, device,
@@ -489,9 +489,9 @@ blit_image(struct radv_cmd_buffer *cmd_buffer, struct radv_image *src_image, VkI
                            },
                            NULL);
 
-      const VkImageViewUsageCreateInfo src_iview_usage_info = {
-         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-         .usage = VK_IMAGE_USAGE_SAMPLED_BIT,
+      const VkImageViewUsage2CreateInfoKHR src_iview_usage_info = {
+         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+         .usage = VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR,
       };
 
       radv_image_view_init(&src_iview, device,

src/amd/vulkan/meta/radv_meta_blit2d.c
@@ -42,7 +42,7 @@ create_iview(struct radv_cmd_buffer *cmd_buffer, struct radv_meta_blit2d_surf *s
              VkFormat depth_format, VkImageAspectFlagBits aspects, bool is_dst)
 {
    struct radv_device *device = radv_cmd_buffer_device(cmd_buffer);
-   VkImageUsageFlags usage;
+   VkImageUsageFlags2KHR usage;
    VkFormat format;
 
    if (depth_format)
@@ -51,14 +51,14 @@ create_iview(struct radv_cmd_buffer *cmd_buffer, struct radv_meta_blit2d_surf *s
       format = surf->format;
 
    if (is_dst) {
-      usage = (vk_format_is_color(format) ? VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
-                                          : VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT);
+      usage = (vk_format_is_color(format) ? VK_IMAGE_USAGE_2_COLOR_ATTACHMENT_BIT_KHR
+                                          : VK_IMAGE_USAGE_2_DEPTH_STENCIL_ATTACHMENT_BIT_KHR);
    } else {
-      usage = VK_IMAGE_USAGE_SAMPLED_BIT;
+      usage = VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR;
    }
 
-   const VkImageViewUsageCreateInfo iview_usage_info = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
+   const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
       .usage = usage,
    };
 

src/amd/vulkan/meta/radv_meta_bufimage.c
@@ -423,15 +423,15 @@ get_cleari_96bit_pipeline(struct radv_device *device, VkPipeline *pipeline_out, 
 
 static void
 create_iview(struct radv_cmd_buffer *cmd_buffer, struct radv_meta_blit2d_surf *surf, struct radv_image_view *iview,
-             VkFormat format, VkImageAspectFlagBits aspects, VkImageUsageFlags usage)
+             VkFormat format, VkImageAspectFlagBits aspects, VkImageUsageFlags2KHR usage)
 {
    struct radv_device *device = radv_cmd_buffer_device(cmd_buffer);
 
    if (format == VK_FORMAT_UNDEFINED)
       format = surf->format;
 
-   const VkImageViewUsageCreateInfo iview_usage_info = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
+   const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
       .usage = usage,
    };
 
@@ -559,7 +559,7 @@ radv_meta_image_to_buffer(struct radv_cmd_buffer *cmd_buffer, struct radv_meta_b
       return;
    }
 
-   create_iview(cmd_buffer, src, &src_view, VK_FORMAT_UNDEFINED, src->aspect_mask, VK_IMAGE_USAGE_SAMPLED_BIT);
+   create_iview(cmd_buffer, src, &src_view, VK_FORMAT_UNDEFINED, src->aspect_mask, VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR);
 
    radv_meta_bind_descriptors(
       cmd_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, layout, 2,
@@ -620,7 +620,7 @@ radv_meta_buffer_to_image_cs(struct radv_cmd_buffer *cmd_buffer, struct radv_met
       return;
    }
 
-   create_iview(cmd_buffer, dst, &dst_view, VK_FORMAT_UNDEFINED, dst->aspect_mask, VK_IMAGE_USAGE_STORAGE_BIT);
+   create_iview(cmd_buffer, dst, &dst_view, VK_FORMAT_UNDEFINED, dst->aspect_mask, VK_IMAGE_USAGE_2_STORAGE_BIT_KHR);
 
    radv_meta_bind_descriptors(
       cmd_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, layout, 2,
@@ -724,10 +724,10 @@ radv_meta_image_to_image_cs(struct radv_cmd_buffer *cmd_buffer, struct radv_meta
 
       create_iview(cmd_buffer, src, &src_view,
                    (src_aspect_mask & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) ? depth_format : 0,
-                   src_aspect_mask, VK_IMAGE_USAGE_SAMPLED_BIT);
+                   src_aspect_mask, VK_IMAGE_USAGE_2_SAMPLED_BIT_KHR);
       create_iview(cmd_buffer, dst, &dst_view,
                    dst_aspect_mask & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT) ? depth_format : 0,
-                   dst_aspect_mask, VK_IMAGE_USAGE_STORAGE_BIT);
+                   dst_aspect_mask, VK_IMAGE_USAGE_2_STORAGE_BIT_KHR);
 
       radv_meta_bind_descriptors(cmd_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, layout, 2,
                                  (VkDescriptorGetInfoEXT[]){{.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_GET_INFO_EXT,
@@ -835,7 +835,7 @@ radv_meta_clear_image_cs(struct radv_cmd_buffer *cmd_buffer, struct radv_meta_bl
       return;
    }
 
-   create_iview(cmd_buffer, dst, &dst_iview, VK_FORMAT_UNDEFINED, dst->aspect_mask, VK_IMAGE_USAGE_STORAGE_BIT);
+   create_iview(cmd_buffer, dst, &dst_iview, VK_FORMAT_UNDEFINED, dst->aspect_mask, VK_IMAGE_USAGE_2_STORAGE_BIT_KHR);
 
    radv_meta_bind_descriptors(cmd_buffer, VK_PIPELINE_BIND_POINT_COMPUTE, layout, 1,
                               (VkDescriptorGetInfoEXT[]){

src/amd/vulkan/meta/radv_meta_clear.c
@@ -1002,9 +1002,9 @@ radv_clear_dcc_comp_to_single(struct radv_cmd_buffer *cmd_buffer, struct radv_im
       width = u_minify(image->vk.extent.width, range->baseMipLevel + l);
       height = u_minify(image->vk.extent.height, range->baseMipLevel + l);
 
-      const VkImageViewUsageCreateInfo iview_usage_info = {
-         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-         .usage = VK_IMAGE_USAGE_STORAGE_BIT,
+      const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+         .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+         .usage = VK_IMAGE_USAGE_2_STORAGE_BIT_KHR,
       };
 
       radv_image_view_init(&iview, device,
@@ -1655,10 +1655,10 @@ radv_clear_image_layer(struct radv_cmd_buffer *cmd_buffer, struct radv_image *im
    uint32_t width = u_minify(image->vk.extent.width, range->baseMipLevel + level);
    uint32_t height = u_minify(image->vk.extent.height, range->baseMipLevel + level);
 
-   const VkImageViewUsageCreateInfo iview_usage_info = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-      .usage = (vk_format_is_color(format) ? VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
-                                           : VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT),
+   const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+      .usage = (vk_format_is_color(format) ? VK_IMAGE_USAGE_2_COLOR_ATTACHMENT_BIT_KHR
+                                           : VK_IMAGE_USAGE_2_DEPTH_STENCIL_ATTACHMENT_BIT_KHR),
    };
 
    radv_image_view_init(&iview, device,
@@ -1745,9 +1745,9 @@ radv_fast_clear_range(struct radv_cmd_buffer *cmd_buffer, struct radv_image *ima
    struct radv_image_view iview;
    bool fast_cleared = false;
 
-   const VkImageViewUsageCreateInfo iview_usage_info = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-      .usage = VK_IMAGE_USAGE_STORAGE_BIT,
+   const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+      .usage = VK_IMAGE_USAGE_2_STORAGE_BIT_KHR,
    };
 
    radv_image_view_init(&iview, device,

src/amd/vulkan/meta/radv_meta_copy_indirect_cs.c
@@ -472,9 +472,9 @@ radv_compute_copy_memory_to_image_indirect(struct radv_cmd_buffer *cmd_buffer,
       const uint32_t slice_count = vk_image_subresource_layer_count(&dst_image->vk, imageSubresource);
 
       for (uint32_t slice = 0; slice < slice_count; slice++) {
-         const VkImageViewUsageCreateInfo iview_usage_info = {
-            .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO,
-            .usage = VK_IMAGE_USAGE_STORAGE_BIT,
+         const VkImageViewUsage2CreateInfoKHR iview_usage_info = {
+            .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_2_CREATE_INFO_KHR,
+            .usage = VK_IMAGE_USAGE_2_STORAGE_BIT_KHR,
          };
 
          radv_image_view_init(&dst_iview, device,

commit 0429dd6595a82d36c16cb67191fd7736477e9560

radv/video: Fix interaction with KHR_extended_flags
The vkGetPhysicalDeviceVideoFormatProperties call needs to also handle
the extended outputs.

Fixes: e4fdf157888 ("radv: implement VK_KHR_extended_flags")
Signed-off-by: Benjamin Cheng <benjamin.cheng@amd.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43537>

src/amd/vulkan/radv_video.c
@@ -970,18 +970,30 @@ radv_GetPhysicalDeviceVideoFormatPropertiesKHR(VkPhysicalDevice physicalDevice,
       for (uint32_t j = 0; j < num_tiling; j++) {
          vk_outarray_append_typed(VkVideoFormatPropertiesKHR, &out, p)
          {
+            VkImageCreateFlags2KHR create_flags = 0;
+            if (src_dst || qp_map)
+               create_flags |= VK_IMAGE_CREATE_2_MUTABLE_FORMAT_BIT_KHR |
+                               VK_IMAGE_CREATE_2_EXTENDED_USAGE_BIT_KHR | VK_IMAGE_CREATE_2_ALIAS_BIT_KHR;
+
             p->format = format;
             p->componentMapping.r = VK_COMPONENT_SWIZZLE_IDENTITY;
             p->componentMapping.g = VK_COMPONENT_SWIZZLE_IDENTITY;
             p->componentMapping.b = VK_COMPONENT_SWIZZLE_IDENTITY;
             p->componentMapping.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-            p->imageCreateFlags = 0;
-            if (src_dst || qp_map)
-               p->imageCreateFlags |= VK_IMAGE_CREATE_2_MUTABLE_FORMAT_BIT_KHR |
-                                      VK_IMAGE_CREATE_2_EXTENDED_USAGE_BIT_KHR | VK_IMAGE_CREATE_2_ALIAS_BIT_KHR;
+            p->imageCreateFlags = create_flags;
             p->imageType = VK_IMAGE_TYPE_2D;
             p->imageTiling = tiling[j];
             p->imageUsageFlags = usage_flags;
+
+            struct VkImageCreateFlags2CreateInfoKHR *create_flags2 =
+               vk_find_struct(p->pNext, IMAGE_CREATE_FLAGS_2_CREATE_INFO_KHR);
+            if (create_flags2)
+               create_flags2->flags = create_flags;
+
+            struct VkImageUsageFlags2CreateInfoKHR *usage_flags2 =
+               vk_find_struct(p->pNext, IMAGE_USAGE_FLAGS_2_CREATE_INFO_KHR);
+            if (usage_flags2)
+               usage_flags2->usage = usage_flags;
 
             if (qp_map) {
                struct VkVideoFormatQuantizationMapPropertiesKHR *qp_map_props =