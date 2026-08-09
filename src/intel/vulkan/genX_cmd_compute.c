joshuaswarren/mesa-1

commit 20f11c7325b9d2747b3fa2a7a57802a6d3817163

anv: unify descriptor set layout type and binding mode
Instead of 2 enums we use one.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43158>

src/intel/vulkan/genX_cmd_compute.c
@@ -104,8 +104,7 @@ cmd_buffer_flush_compute_state(struct anv_cmd_buffer *cmd_buffer,
 
    genX(cmd_buffer_update_color_aux_op)(cmd_buffer, ANV_COLOR_AUX_OP_CLASS_NONE);
 
-   genX(flush_descriptor_buffers)(cmd_buffer, bind_state,
-                                  VK_SHADER_STAGE_COMPUTE_BIT);
+   genX(flush_binding_mode)(cmd_buffer, bind_state, VK_SHADER_STAGE_COMPUTE_BIT);
 
    const bool uses_systolic = get_cs_prog_data(comp_state)->uses_systolic;
    genX(flush_pipeline_select_gpgpu)(cmd_buffer, uses_systolic);
@@ -1315,7 +1314,7 @@ cmd_buffer_flush_rt_state(struct anv_cmd_buffer *cmd_buffer,
 
    genX(cmd_buffer_update_color_aux_op)(cmd_buffer, ANV_COLOR_AUX_OP_CLASS_NONE);
 
-   genX(flush_descriptor_buffers)(cmd_buffer, bind_state, ANV_RT_STAGE_BITS);
+   genX(flush_binding_mode)(cmd_buffer, bind_state, ANV_RT_STAGE_BITS);
 
    genX(flush_pipeline_select_gpgpu)(cmd_buffer, false);
 

src/intel/vulkan/anv_cmd_buffer.c
@@ -59,6 +59,9 @@ anv_cmd_state_init(struct anv_cmd_buffer *cmd_buffer)
    state->compute.z_pass_async_compute_thread_limit = UINT8_MAX;
    state->compute.np_z_async_throttle_settings = UINT8_MAX;
 
+   state->descriptor_buffers.surfaces_buffer = -1;
+   state->descriptor_buffers.samplers_buffer = -1;
+
    BITSET_COPY(state->gfx.dyn_state.pack_dirty,
                cmd_buffer->device->gfx_dirty_state);
 }
@@ -546,26 +549,6 @@ update_push_descriptor_flags(struct anv_bind_point_state *state,
    }
 }
 
-static bool
-maybe_update_dynamic_buffers_indices(struct anv_bind_point_state *state,
-                                     const uint8_t *offsets)
-{
-   struct anv_push_constants *push = &state->push_constants;
-
-   bool modified = false;
-   for (uint32_t i = 0; i < MAX_SETS; i++) {
-      if ((push->desc_surface_offsets[i] &
-           ANV_DESCRIPTOR_SET_DYNAMIC_INDEX_MASK) !=
-          offsets[i]) {
-         push->desc_surface_offsets[i] &= ~ANV_DESCRIPTOR_SET_DYNAMIC_INDEX_MASK;
-         push->desc_surface_offsets[i] |= offsets[i];
-         modified = true;
-      }
-   }
-
-   return modified;
-}
-
 bool
 anv_cmd_buffer_alloc_bind_point_state(struct anv_cmd_buffer *cmd_buffer,
                                       struct anv_bind_point_state **out_state)
@@ -599,15 +582,14 @@ bind_point_stages(struct anv_device *device, VkPipelineBindPoint bind_point)
 }
 
 static void
-anv_cmd_buffer_maybe_dirty_descriptor_mode(struct anv_cmd_buffer *cmd_buffer,
-                                           enum anv_cmd_descriptor_buffer_mode new_mode)
+anv_cmd_buffer_update_binding_mode(struct anv_cmd_buffer *cmd_buffer,
+                                   enum anv_shader_binding_mode new_mode)
 {
-   if (cmd_buffer->state.pending_db_mode == new_mode)
+   if (cmd_buffer->state.pending_binding_mode == new_mode)
       return;
 
    /* Ensure we program the STATE_BASE_ADDRESS properly at least once */
-   cmd_buffer->state.descriptor_buffers.dirty = true;
-   cmd_buffer->state.pending_db_mode = new_mode;
+   cmd_buffer->state.pending_binding_mode = new_mode;
 }
 
 static void
@@ -632,12 +614,12 @@ anv_cmd_buffer_bind_descriptor_set(struct anv_cmd_buffer *cmd_buffer,
 
    struct anv_descriptor_set_layout *set_layout = set->layout;
 
-   anv_cmd_buffer_maybe_dirty_descriptor_mode(
+   anv_cmd_buffer_update_binding_mode(
       cmd_buffer,
       (set->layout->vk.flags &
        VK_DESCRIPTOR_SET_LAYOUT_CREATE_DESCRIPTOR_BUFFER_BIT_EXT) != 0 ?
-      ANV_CMD_DESCRIPTOR_BUFFER_MODE_BUFFER :
-      ANV_CMD_DESCRIPTOR_BUFFER_MODE_LEGACY);
+      ANV_SHADER_BINDING_MODE_BUFFER :
+      ANV_SHADER_BINDING_MODE_LEGACY);
 
    const VkShaderStageFlags stages =
       bind_point_stages(cmd_buffer->device, bind_point) & set_layout->shader_stages;
@@ -664,26 +646,6 @@ anv_cmd_buffer_bind_descriptor_set(struct anv_cmd_buffer *cmd_buffer,
          bind_state->descriptor_buffers[set_index].bound = true;
          anv_cmd_buffer_dirty_descriptors(cmd_buffer, stages, "push descriptor bind");
          cmd_buffer->state.descriptor_buffers.offsets_dirty |= stages;
-      } else {
-         /* Plaforms with LSC will use descriptor buffer push constant
-          * offsets, also with device generated commands, shaders are much
-          * more likely to access the offset on pre-LSC platforms.
-          */
-         bool update_desc_sets = cmd_buffer->device->vk.enabled_features.deviceGeneratedCommands ||
-                                 cmd_buffer->device->info->has_lsc;
-
-         if (update_desc_sets) {
-            struct anv_push_constants *push = &bind_state->push_constants;
-            uint64_t offset =
-               anv_address_physical(set->desc_surface_addr) -
-               anv_physical_device_get_internal_surface_state_pool_va(cmd_buffer->device->physical)->addr;
-            assert((offset & ~ANV_DESCRIPTOR_SET_OFFSET_MASK) == 0);
-            push->desc_surface_offsets[set_index] &= ~ANV_DESCRIPTOR_SET_OFFSET_MASK;
-            push->desc_surface_offsets[set_index] |= offset;
-            push->desc_sampler_offsets[set_index] =
-               anv_address_physical(set->desc_sampler_addr) -
-               anv_physical_device_get_dynamic_state_pool_va(cmd_buffer->device->physical)->addr;
-         }
       }
 
       /* Always add a reference to the buffers */
@@ -697,31 +659,11 @@ anv_cmd_buffer_bind_descriptor_set(struct anv_cmd_buffer *cmd_buffer,
 
    if (dynamic_offsets) {
       if (set_layout->vk.dynamic_descriptor_count > 0) {
-         struct anv_push_constants *push = &bind_state->push_constants;
-         assert(layout != NULL);
-         uint32_t dynamic_offset_start =
-            layout->dynamic_descriptor_offset[set_index];
-         uint32_t *push_offsets =
-            &push->dynamic_offsets[dynamic_offset_start];
-
-         memcpy(bind_state->dynamic_offsets[set_index].offsets,
-                *dynamic_offsets,
-                sizeof(uint32_t) * MIN2(*dynamic_offset_count,
-                                        set_layout->vk.dynamic_descriptor_count));
-
-         /* Assert that everything is in range */
-         assert(set_layout->vk.dynamic_descriptor_count <= *dynamic_offset_count);
-         assert(dynamic_offset_start + set_layout->vk.dynamic_descriptor_count <=
-                ARRAY_SIZE(push->dynamic_offsets));
-
-         for (uint32_t i = 0; i < set_layout->vk.dynamic_descriptor_count; i++) {
-            if (push_offsets[i] != (*dynamic_offsets)[i]) {
-               bind_state->dynamic_offsets[set_index].offsets[i] =
-                  push_offsets[i] = (*dynamic_offsets)[i];
-               /* dynamic_offset_stages[] elements could contain blanket
-                * values like VK_SHADER_STAGE_ALL, so limit this to the
-                * binding point's bits.
-                */
+         const uint32_t count = MIN2(set_layout->vk.dynamic_descriptor_count,
+                                     *dynamic_offset_count);
+         for (uint32_t i = 0; i < count; i++) {
+            if (bind_state->dynamic_offsets[set_index].offsets[i] != (*dynamic_offsets)[i]) {
+               bind_state->dynamic_offsets[set_index].offsets[i] = (*dynamic_offsets)[i];
                dirty_stages |= set_layout->dynamic_offset_stages[i] & stages;
             }
          }
@@ -741,8 +683,8 @@ anv_cmd_buffer_bind_descriptor_set(struct anv_cmd_buffer *cmd_buffer,
       cmd_buffer->state.push_descriptors_dirty |= dirty_stages;
    else
       anv_cmd_buffer_dirty_descriptors(cmd_buffer, dirty_stages, "descriptor bind");
-   cmd_buffer->state.push_constants_dirty |= dirty_stages;
-   bind_state->push_constants_state = ANV_STATE_NULL;
+
+   bind_state->max_bound_descriptors = MAX2(bind_state->max_bound_descriptors, set_index + 1);
 }
 
 void anv_CmdBindDescriptorSets2(
@@ -817,9 +759,9 @@ void anv_CmdBindDescriptorBuffersEXT(
       if (state->descriptor_buffers.address[i] != pBindingInfos[i].address) {
          state->descriptor_buffers.address[i] = pBindingInfos[i].address;
          if (pBindingInfos[i].usage & VK_BUFFER_USAGE_RESOURCE_DESCRIPTOR_BUFFER_BIT_EXT)
-            state->descriptor_buffers.surfaces_address = pBindingInfos[i].address;
+            state->descriptor_buffers.surfaces_buffer = i;
          if (pBindingInfos[i].usage & VK_BUFFER_USAGE_SAMPLER_DESCRIPTOR_BUFFER_BIT_EXT)
-            state->descriptor_buffers.samplers_address = pBindingInfos[i].address;
+            state->descriptor_buffers.samplers_buffer = i;
          state->descriptor_buffers.dirty = true;
          state->descriptor_buffers.offsets_dirty = ~0;
       }
@@ -858,6 +800,8 @@ anv_cmd_buffer_set_descriptor_buffer_offsets(struct anv_cmd_buffer *cmd_buffer,
          cmd_buffer->state.descriptor_buffers.offsets_dirty |= stages;
       }
       bind_state->descriptor_buffers[set_index].bound = true;
+
+      bind_state->max_bound_descriptors = MAX2(bind_state->max_bound_descriptors, set_index + 1);
    }
 }
 
@@ -896,8 +840,7 @@ void anv_CmdSetDescriptorBufferOffsets2EXT(
                                                    pSetDescriptorBufferOffsetsInfo->pBufferIndices);
    }
 
-   anv_cmd_buffer_maybe_dirty_descriptor_mode(cmd_buffer,
-                                              ANV_CMD_DESCRIPTOR_BUFFER_MODE_BUFFER);
+   anv_cmd_buffer_update_binding_mode(cmd_buffer, ANV_SHADER_BINDING_MODE_BUFFER);
 }
 
 void anv_CmdBindDescriptorBufferEmbeddedSamplers2EXT(
@@ -914,8 +857,10 @@ void anv_CmdBindSamplerHeapEXT(
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
    struct anv_cmd_state *state = &cmd_buffer->state;
 
-   if (state->descriptor_buffers.samplers_address != pBindInfo->heapRange.address)
-      state->descriptor_buffers.samplers_address = pBindInfo->heapRange.address;
+   if (state->descriptor_heap.samplers_address != pBindInfo->heapRange.address) {
+      state->descriptor_heap.samplers_address = pBindInfo->heapRange.address;
+      state->descriptor_heap.dirty = true;
+   }
 }
 
 void anv_CmdBindResourceHeapEXT(
@@ -925,13 +870,11 @@ void anv_CmdBindResourceHeapEXT(
    ANV_FROM_HANDLE(anv_cmd_buffer, cmd_buffer, commandBuffer);
    struct anv_cmd_state *state = &cmd_buffer->state;
 
-   if (state->descriptor_buffers.surfaces_address != pBindInfo->heapRange.address) {
-       state->descriptor_buffers.surfaces_address = pBindInfo->heapRange.address;
-       state->descriptor_buffers.dirty = true;
+   if (state->descriptor_heap.surfaces_address != pBindInfo->heapRange.address) {
+      state->descriptor_heap.surfaces_address = pBindInfo->heapRange.address;
+      state->descriptor_heap.dirty = true;
    }
-
-   anv_cmd_buffer_maybe_dirty_descriptor_mode(cmd_buffer,
-                                              ANV_CMD_DESCRIPTOR_BUFFER_MODE_HEAP);
+   anv_cmd_buffer_update_binding_mode(cmd_buffer, ANV_SHADER_BINDING_MODE_HEAP);
 }
 
 void anv_CmdPushDataEXT(
@@ -1369,12 +1312,6 @@ anv_cmd_buffer_set_rt_state(struct vk_command_buffer *vk_cmd_buffer,
       anv_cmd_buffer_set_rt_query_buffer(cmd_buffer, bind_state,
                                          ray_queries, ANV_RT_STAGE_BITS);
    }
-
-   if (maybe_update_dynamic_buffers_indices(bind_state,
-                                            dynamic_descriptor_offsets)) {
-      cmd_buffer->state.push_constants_dirty |= ANV_RT_STAGE_BITS;
-      bind_state->push_constants_state = ANV_STATE_NULL;
-   }
 }
 
 void
@@ -1582,12 +1519,13 @@ bind_compute_shader(struct anv_cmd_buffer *cmd_buffer,
    if (shader == NULL)
       return;
 
-   cmd_buffer->state.compute.pipeline_dirty = true;
-   set_dirty_for_bind_map(cmd_buffer, MESA_SHADER_COMPUTE, &shader->bind_map);
-
    if (!anv_cmd_buffer_ensure_bind_point_state(cmd_buffer,
                                                &cmd_buffer->state.compute.base))
       return;
+
+   cmd_buffer->state.compute.base->binding_mode = shader->bind_map.binding_mode;
+   cmd_buffer->state.compute.pipeline_dirty = true;
+   set_dirty_for_bind_map(cmd_buffer, MESA_SHADER_COMPUTE, &shader->bind_map);
 
    update_push_descriptor_flags(comp_state->base,
                                 &cmd_buffer->state.compute.shader, 1);
@@ -1693,12 +1631,15 @@ bind_graphics_shaders(struct anv_cmd_buffer *cmd_buffer,
 #undef diff_fix_state_stage
 #undef diff_var_state_stage
 
+   gfx->base->binding_mode = ANV_SHADER_BINDING_MODE_UNKNOWN;
    uint8_t dynamic_descriptors[MAX_SETS] = {};
    for (uint32_t s = 0; s < ANV_GRAPHICS_SHADER_STAGE_COUNT; s++) {
       struct anv_shader *shader = new_shaders[s];
 
       if (shader != NULL) {
          gfx->active_stages |= mesa_to_vk_shader_stage(s);
+
+         gfx->base->binding_mode = MAX2(shader->bind_map.binding_mode, gfx->base->binding_mode);
 
          ray_queries = MAX2(ray_queries, shader->vk.ray_queries);
          if (gfx->shaders[s] != shader)
@@ -1833,18 +1774,6 @@ bind_graphics_shaders(struct anv_cmd_buffer *cmd_buffer,
    update_push_descriptor_flags(gfx->base,
                                 cmd_buffer->state.gfx.shaders,
                                 ARRAY_SIZE(cmd_buffer->state.gfx.shaders));
-
-   uint8_t dynamic_descriptor_count = 0;
-   uint8_t dynamic_descriptor_offsets[MAX_SETS] = {};
-   for (uint32_t i = 0; i < MAX_SETS; i++) {
-      dynamic_descriptor_offsets[i] = dynamic_descriptor_count;
-      dynamic_descriptor_count += dynamic_descriptors[i];
-   }
-   if (maybe_update_dynamic_buffers_indices(gfx->base,
-                                            dynamic_descriptor_offsets)) {
-      cmd_buffer->state.push_constants_dirty |= gfx->active_stages;
-      gfx->base->push_constants_state = ANV_STATE_NULL;
-   }
 
    if (ray_queries > 0) {
       assert(cmd_buffer->device->info->verx10 >= 125);

src/intel/vulkan/anv_dgc_layout.c
@@ -394,7 +394,7 @@ anv_dgc_fill_gfx_state(struct anv_dgc_gfx_state *state,
 
             switch (range->set) {
             case ANV_DESCRIPTOR_SET_DESCRIPTORS:
-               if (bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER) {
+               if (bind_map->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER) {
                   state->push_constants.stages[gen_stage].addresses[i] =
                      anv_cmd_buffer_descriptor_buffer_address(
                         cmd_buffer,

src/intel/vulkan/anv_genX.h
@@ -216,9 +216,9 @@ void genX(emit_l3_config)(struct anv_batch *batch,
 void genX(cmd_buffer_config_l3)(struct anv_cmd_buffer *cmd_buffer,
                                 const struct intel_l3_config *cfg);
 
-void genX(flush_descriptor_buffers)(struct anv_cmd_buffer *cmd_buffer,
-                                    struct anv_bind_point_state *bind_state,
-                                    VkShaderStageFlags active_stages);
+void genX(flush_binding_mode)(struct anv_cmd_buffer *cmd_buffer,
+                              struct anv_bind_point_state *bind_state,
+                              VkShaderStageFlags active_stages);
 
 uint32_t
 genX(cmd_buffer_flush_descriptor_sets)(struct anv_cmd_buffer *cmd_buffer,

src/intel/vulkan/anv_nir.h
@@ -157,7 +157,7 @@ bool anv_nir_lower_unaligned_dispatch(nir_shader *shader);
 
 bool anv_nir_lower_resource_intel(nir_shader *shader,
                                   const struct anv_physical_device *device,
-                                  enum anv_descriptor_set_layout_type desc_type);
+                                  enum anv_shader_binding_mode binding_mode);
 
 bool anv_nir_add_base_work_group_id(nir_shader *shader);
 

src/intel/vulkan/anv_nir_apply_pipeline_layout.c
@@ -686,7 +686,7 @@ build_load_storage_3d_image_depth(nir_builder *b,
 {
    const struct intel_device_info *devinfo = &state->pdevice->info;
 
-   if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+   if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT) {
       return build_load_descriptor_mem_from_res_index(
          b, res_index,
          offsetof(struct anv_storage_image_descriptor, image_depth),
@@ -723,12 +723,12 @@ build_descriptor_set_bti(nir_builder *b,
 {
    if (state->pdevice->info.has_lsc) {
       nir_def *surface_handle =
-         (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT ||
-          state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_UNKNOWN) ?
+         (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT ||
+          state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_UNKNOWN) ?
          nir_imm_int(b, 0xdeaddead) :
          nir_load_reloc_const_intel(
             b,
-            state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER ?
+            state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER ?
             BRW_SHADER_RELOC_DESCRIPTORS_BUFFERS_VIEW_HANDLE :
             BRW_SHADER_RELOC_DESCRIPTORS_VIEW_HANDLE);
 
@@ -769,12 +769,12 @@ build_descriptor_set_base_address(nir_builder *b,
 
    if (state->pdevice->info.has_lsc) {
       reloc_id =
-         state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER ?
+         state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER ?
          BRW_SHADER_RELOC_DESCRIPTORS_BUFFER_ADDR_HIGH :
          BRW_SHADER_RELOC_DESCRIPTORS_ADDR_HIGH;
    } else {
       reloc_id =
-         state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER ?
+         state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER ?
          is_push_set ? BRW_SHADER_RELOC_PUSH_DESCRIPTORS_BUFFER_ADDR_HIGH :
          BRW_SHADER_RELOC_DESCRIPTORS_BUFFER_ADDR_HIGH :
          BRW_SHADER_RELOC_DESCRIPTORS_ADDR_HIGH;
@@ -921,7 +921,7 @@ binding_descriptor_offset(const struct apply_pipeline_layout_state *state,
                           bool sampler)
 {
    if (sampler &&
-       state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_DIRECT)
+       state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY)
       return bind_layout->descriptor_sampler_offset;
 
    return bind_layout->descriptor_surface_offset;
@@ -933,7 +933,7 @@ binding_descriptor_stride(const struct apply_pipeline_layout_state *state,
                           bool sampler)
 {
    if (sampler &&
-       state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_DIRECT)
+       state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY)
       return bind_layout->descriptor_sampler_stride;
 
    return bind_layout->descriptor_surface_stride;
@@ -958,7 +958,7 @@ build_surface_index_for_binding(nir_builder *b,
 
    nir_def *set_offset, *surface_index;
    if (is_bindless) {
-      if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+      if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT) {
          set_offset = nir_imm_int(b, 0xdeaddead);
 
          uint32_t final_offset = 0;
@@ -1047,7 +1047,7 @@ build_sampler_handle_for_binding(nir_builder *b,
          b, BRW_SHADER_RELOC_EMBEDDED_SAMPLER_HANDLE +
          state->set[set].binding[binding].embedded_sampler_index);
    } else if (is_bindless) {
-      if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+      if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT) {
          set_offset = nir_imm_int(b, 0xdeaddead);
 
          uint32_t final_offset = 0;
@@ -1241,7 +1241,7 @@ build_buffer_addr_for_res_index(nir_builder *b,
                                 nir_address_format addr_format,
                                 struct apply_pipeline_layout_state *state)
 {
-   if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT)
+   if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT)
       return build_indirect_buffer_addr_for_res_index(b, res_index, addr_format, state);
    else
       return build_direct_buffer_addr_for_res_index(b, res_index, addr_format, state);
@@ -1437,7 +1437,7 @@ try_lower_direct_buffer_intrinsic(nir_builder *b,
        * descriptors, we'll use A64 messages. This is handled in the main
        * lowering path.
        */
-      if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT &&
+      if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT &&
           !descriptor_has_bti(desc, state))
          return false;
    } else {
@@ -1451,7 +1451,7 @@ try_lower_direct_buffer_intrinsic(nir_builder *b,
        * descriptor set base address + offset. There is no indirect data to
        * fetch.
        */
-      if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT &&
+      if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT &&
           bind_layout->type != VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK &&
           !descriptor_has_bti(desc, state))
          return false;
@@ -1634,7 +1634,7 @@ lower_get_ssbo_size(nir_builder *b, nir_intrinsic_instr *intrin,
                                     intrin->src[0].ssa,
                                     addr_format, state);
    nir_def *desc_range;
-   if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+   if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT) {
       /* Load the anv_address_range_descriptor */
       desc_range = build_load_descriptor_mem_from_res_index(b, res_index, 0, 4, 32, state);
    } else {
@@ -1675,7 +1675,7 @@ lower_image_load_intel_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
 
    nir_def *desc;
 
-   if (state->bind_map->layout_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_INDIRECT) {
+   if (state->bind_map->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY_INDIRECT) {
       switch (nir_intrinsic_base(intrin)) {
       case ISL_SURF_PARAM_BASE_ADDRESSS:
          desc = build_load_descriptor_mem_from_res_index(

src/intel/vulkan/anv_nir_lower_resource_intel.c
@@ -75,7 +75,7 @@ anv_nir_update_resource_intel_block(nir_shader *shader)
 }
 
 struct lower_resource_state {
-   enum anv_descriptor_set_layout_type desc_type;
+   enum anv_shader_binding_mode binding_mode;
    const struct anv_physical_device *device;
 };
 
@@ -105,7 +105,7 @@ lower_resource_intel(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 
    /* Ignore binding table accesses & embedded samplers */
    if (is_embedded_sampler) {
-      assert(state->desc_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER);
+      assert(state->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER);
       return false;
    }
 
@@ -120,8 +120,8 @@ lower_resource_intel(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
    /* When using indirect descriptor, the surface handles are loaded from the
     * descriptor buffer and do not need any offset.
     */
-   if (state->desc_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_DIRECT ||
-       state->desc_type == ANV_PIPELINE_DESCRIPTOR_SET_LAYOUT_TYPE_BUFFER) {
+   if (state->binding_mode == ANV_SHADER_BINDING_MODE_LEGACY ||
+       state->binding_mode == ANV_SHADER_BINDING_MODE_BUFFER) {
       if (!intel_has_extended_bindless(&state->device->info)) {
          /* We're trying to reduce the number of instructions in the shaders
           * to compute surface handles. The assumption is that we're using
@@ -160,10 +160,10 @@ lower_resource_intel(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 bool
 anv_nir_lower_resource_intel(nir_shader *shader,
                              const struct anv_physical_device *device,
-                             enum anv_descriptor_set_layout_type desc_type)
+                             enum anv_shader_binding_mode binding_mode)
 {
    struct lower_resource_state state = {
-      .desc_type = desc_type,
+      .binding_mode = binding_mode,
       .device = device,
    };
    return nir_shader_intrinsics_pass(shader, lower_resource_intel,

commit ef235714e8b2fd713d0fd7973d8577de3e840d49

anv: always ensure push constant is allocated for compute
We end up not allocating the push constants because it's not marked as
dirty. Let's simplify the logic by always allocating if not already
there.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 20f11c7325 ("anv: unify descriptor set layout type and binding mode")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16032
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43586>

src/intel/vulkan/genX_cmd_compute.c
@@ -229,21 +229,21 @@ cmd_buffer_flush_compute_state(struct anv_cmd_buffer *cmd_buffer,
 #endif
 
    if (indirect_set == NULL &&
-       (cmd_buffer->state.push_constants_dirty & VK_SHADER_STAGE_COMPUTE_BIT)) {
-      if (bind_state->push_constants_state.alloc_size == 0) {
-         bind_state->push_constants_state =
-            anv_cmd_buffer_cs_push_constants(cmd_buffer);
-      }
+       bind_state->push_constants_state.alloc_size == 0) {
+      bind_state->push_constants_state =
+         anv_cmd_buffer_cs_push_constants(cmd_buffer);
+      cmd_buffer->state.push_constants_dirty |= VK_SHADER_STAGE_COMPUTE_BIT;
+   }
 
+   if (cmd_buffer->state.push_constants_dirty & VK_SHADER_STAGE_COMPUTE_BIT) {
 #if GFX_VERx10 < 125
-      if (bind_state->push_constants_state.alloc_size) {
+      if (bind_state->push_constants_state.alloc_size > 0) {
          anv_batch_emit(&cmd_buffer->batch, GENX(MEDIA_CURBE_LOAD), curbe) {
             curbe.CURBETotalDataLength    = bind_state->push_constants_state.alloc_size;
             curbe.CURBEDataStartAddress   = bind_state->push_constants_state.offset;
          }
       }
 #endif
-
       cmd_buffer->state.push_constants_dirty &= ~VK_SHADER_STAGE_COMPUTE_BIT;
    }
 