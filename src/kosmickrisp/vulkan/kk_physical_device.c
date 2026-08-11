joshuaswarren/mesa-1

commit 721c69763499d84d0eebb95c498bed3d345c7d9e

kk: Disable workaround 8 and 11 on macOS 27
The issue with parallel MTL4Compiler instances has been resolved in
macOS 27 beta 5.

The issue with GPU capture under Rosetta 2 was also solved in macOS 27
at some point, although it is unclear when.

Reviewed-by: Aitor Camacho <aitor@lunarg.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43639>

src/kosmickrisp/vulkan/kk_physical_device.c
@@ -1084,7 +1084,8 @@ kk_parse_environment_options(struct kk_physical_device *pdev)
 
    /* Workarounds resolved on macOS 27 */
    if (ns_is_os_version_at_least(27, 0, 0)) {
-      settings->disabled_workarounds |= BITFIELD64_MASK(7);
+      settings->disabled_workarounds |= BITFIELD64_MASK(8);
+      settings->disabled_workarounds |= BITFIELD64_BIT(11);
       settings->disabled_workarounds |= BITFIELD64_BIT(12);
       settings->disabled_workarounds |= BITFIELD64_BIT(17);
    }

commit e8ce914484f574abbc916fdadeffa7663800b591

kk: Fix disable of workaround 8
Workaround bits start at bit 1 instead of 0, 7 was disabled instead of 8.

Fixes: 721c6976349 ("kk: Disable workaround 8 and 11 on macOS 27")

Reviewed-by: Aitor Camacho <aitor@lunarg.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43660>

src/kosmickrisp/vulkan/kk_physical_device.c
@@ -1084,7 +1084,10 @@ kk_parse_environment_options(struct kk_physical_device *pdev)
 
    /* Workarounds resolved on macOS 27 */
    if (ns_is_os_version_at_least(27, 0, 0)) {
-      settings->disabled_workarounds |= BITFIELD64_MASK(8);
+      /* 1-6 */
+      settings->disabled_workarounds |= BITFIELD64_MASK(7);
+
+      settings->disabled_workarounds |= BITFIELD64_BIT(8);
       settings->disabled_workarounds |= BITFIELD64_BIT(11);
       settings->disabled_workarounds |= BITFIELD64_BIT(12);
       settings->disabled_workarounds |= BITFIELD64_BIT(17);