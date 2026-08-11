joshuaswarren/mesa-1

commit ccbab650b262d7853cc1055f3d974875b93f10e0

anv: metrics library oa configuration activation
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43630>

src/intel/vulkan/anv_batch_chain.c
@@ -39,6 +39,9 @@
 
 #include "util/perf/u_trace.h"
 
+#include "perf/intel_perf.h"
+#include "perf/intel_perf_metrics_library.h"
+
 /** \file anv_batch_chain.c
  *
  * This file contains functions related to anv_cmd_buffer as a data
@@ -1643,6 +1646,13 @@ anv_queue_submit(struct vk_queue *vk_queue,
       &utrace_submit);
    if (result != VK_SUCCESS)
       return result;
+
+   if (device->physical->perf->use_metrics_library && queue->metrics_library_configuration) {
+      if (!intel_perf_metrics_library_activate_configuration(device->physical->perf,
+                                                             queue->metrics_library_configuration)) {
+         return VK_ERROR_UNKNOWN;
+      }
+   }
 
    uint64_t start_ts = intel_ds_begin_submit(&queue->ds);
 

src/intel/perf/intel_perf_metrics_library.cpp
@@ -249,3 +249,22 @@ bool intel_perf_metrics_library_get_query_results(struct intel_perf_config *perf
       return false;
    }
 }
+
+bool intel_perf_metrics_library_activate_configuration(struct intel_perf_config *perf, uint64_t config_id)
+{
+   if (!perf->metrics_library.lib || !perf->metrics_library.context || !perf->metrics_library.api)
+      return false;
+
+   Interface_1_0* api = (Interface_1_0*)perf->metrics_library.api;
+   if (!api->ConfigurationActivate)
+      return false;
+
+   ConfigurationHandle_1_0 config_handle = {};
+   config_handle.data = reinterpret_cast<void*>(config_id);
+
+   ConfigurationActivateData_1_0 activate_data = {};
+   activate_data.Type = GpuConfigurationActivationType::Tbs;
+
+   StatusCode status = api->ConfigurationActivate(config_handle, &activate_data);
+   return status == StatusCode::Success;
+}

src/intel/perf/intel_perf_metrics_library.h
@@ -38,6 +38,7 @@ bool intel_perf_metrics_library_destroy_configuration(struct intel_perf_config *
 void* intel_perf_metrics_library_create_query_pool(struct intel_perf_config *perf, uint32_t query_count);
 bool intel_perf_metrics_library_destroy_query_pool(struct intel_perf_config *perf, void* query_pool);
 bool intel_perf_metrics_library_get_query_results(struct intel_perf_config *perf, void* query_pool, void* data, uint32_t query_index, bool* write_results);
+bool intel_perf_metrics_library_activate_configuration(struct intel_perf_config *perf, uint64_t config_id);
 
 #ifdef __cplusplus
 }

commit 983cd41b38c8bb685f0fb97e719b9993de64dbe7

anv: let's not crash everywhere
If we can't read the performance metrics stuff, device->perf will be
NULL and we explode.

Fixes: ccbab650b26 ("anv: metrics library oa configuration activation")

Signed-off-by: Iván Briano <ivan.briano@intel.com>
Reviewed-by: Omar Rashwan <omar.rashwan@intel.com>
Reviewed-by: adrian baker <adrian1.baker@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43681>

src/intel/vulkan/anv_batch_chain.c
@@ -1647,7 +1647,7 @@ anv_queue_submit(struct vk_queue *vk_queue,
    if (result != VK_SUCCESS)
       return result;
 
-   if (device->physical->perf->use_metrics_library && queue->metrics_library_configuration) {
+   if (device->physical->perf && device->physical->perf->use_metrics_library && queue->metrics_library_configuration) {
       if (!intel_perf_metrics_library_activate_configuration(device->physical->perf,
                                                              queue->metrics_library_configuration)) {
          return VK_ERROR_UNKNOWN;