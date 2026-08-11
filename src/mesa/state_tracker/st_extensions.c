joshuaswarren/mesa-1

commit 6e1be277cfd71721cc32bb99cdc9bfdcdd432990

gallium: Add native_fp32_depth cap to gate ARB_depth_buffer_float
st/mesa advertises ARB_depth_buffer_float as soon as the Z32_FLOAT and
Z32_FLOAT_S8X24 formats report depth-stencil and sampler support. A driver
that only emulates those formats over a lower precision native depth buffer
can not honor the extension's float precision and range, yet still wants to
provide the formats because OpenGL ES 3.0 requires them.

Add a native_fp32_depth screen cap, default true, that such a driver can
clear to drop the desktop extension while keeping the formats. No functional
change for existing drivers.

Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42448>

src/mesa/state_tracker/st_extensions.c
@@ -1185,6 +1185,10 @@ void st_init_extensions(struct pipe_screen *screen,
    init_format_extensions(screen, extensions, depthstencil_mapping,
                           ARRAY_SIZE(depthstencil_mapping), PIPE_TEXTURE_2D,
                           PIPE_BIND_DEPTH_STENCIL | PIPE_BIND_SAMPLER_VIEW);
+
+   if (!screen->caps.native_fp32_depth)
+      extensions->ARB_depth_buffer_float = GL_FALSE;
+
    init_format_extensions(screen, extensions, texture_mapping,
                           ARRAY_SIZE(texture_mapping), PIPE_TEXTURE_2D,
                           PIPE_BIND_SAMPLER_VIEW);

src/gallium/auxiliary/util/u_screen.c
@@ -144,6 +144,8 @@ u_init_pipe_screen_caps(struct pipe_screen *pscreen, int accel)
    /* Enables ARB_shadow */
    caps->texture_shadow_map = true;
 
+   caps->native_fp32_depth = true;
+
    caps->flatshade = true;
    caps->alpha_test = true;
    caps->point_size_fixed = true;

src/gallium/include/pipe/p_defines.h
@@ -927,6 +927,7 @@ struct pipe_caps {
    bool texture_float_linear;
    bool texture_half_float_linear;
    bool depth_bounds_test;
+   bool native_fp32_depth;
    bool texture_query_samples;
    bool force_persample_interp;
    bool shareable_shaders;

commit 75c6e613e2df4d16aa2b1e1d7aaa80a055ec57a2

st/mesa: Only drop ARB_depth_buffer_float on desktop GL
compute_version_es2(..) requires the ARB_depth_buffer_float bit for
OpenGL ES 3.0, so clearing it also took ES 3.0 away from drivers that
just emulate the fp32 depth formats.

Fixes: 6e1be277cfd ("gallium: Add native_fp32_depth cap to gate ARB_depth_buffer_float")
Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
Acked-by: Erik Faye-Lund <kusmabite@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43448>

src/mesa/state_tracker/st_extensions.c
@@ -1187,7 +1187,8 @@ void st_init_extensions(struct pipe_screen *screen,
                           ARRAY_SIZE(depthstencil_mapping), PIPE_TEXTURE_2D,
                           PIPE_BIND_DEPTH_STENCIL | PIPE_BIND_SAMPLER_VIEW);
 
-   if (!screen->caps.native_fp32_depth)
+   if (!screen->caps.native_fp32_depth &&
+       (api == API_OPENGL_CORE || api == API_OPENGL_COMPAT))
       extensions->ARB_depth_buffer_float = GL_FALSE;
 
    init_format_extensions(screen, extensions, texture_mapping,