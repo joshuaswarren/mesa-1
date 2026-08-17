joshuaswarren/mesa-1

commit 4a5e9c5f2f1c0d93365b127a00bb056fbc380fd5

pan/compiler: fix spilling for 64-bit values
For the BIR-compiler, 64-bit values were not properly tracked in the
spill logic and PHIs were always assumed to be 32-bits. This could
create issues were only one half of the value was reloaded or spills
would overlap each other leading to garbage values. This patch fixes
these issues by keeping track of how many words each value needs. Also,
it adds a constraint for SHADD sources where it splits and collects them
right before the SHADD instruction itself to make it easier for RA to
handle the register pairs.

Fixes: d7b060146e3 ("pan/compiler: Use SHADDX instruction for i64 add")

Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42080>

src/panfrost/compiler/bifrost/bi_lower_spill.c
@@ -51,13 +51,24 @@ lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
     * provide the actual TLS offset as bi_index::value.
     */
 
+   unsigned words = 1;
+
+   if (I->dest[0].memory)
+      words = MAX2(words, ctx->sizes[I->dest[0].value]);
+
+   bi_foreach_src(I, s) {
+      if (I->src[s].memory)
+         words = MAX2(words, ctx->sizes[I->src[s].value]);
+   }
+
+   assert(words <= 4);
+   I->table = words;
+
    if (I->dest[0].memory) {
       const bi_index dst = I->dest[0];
-      /* bi_repair_ssa could make PHIs for MEMMOV sources which could be
-       * wider. But, those should all get eliminated as trivial because they
-       * are only defined once so they would all look like mX = PHI mX, mX.
+      /* Preserve the memory value long enough to look up its assigned TLS
+       * slot. bi_out_of_ssa uses I->table to lower all words of the PHI.
        */
-      assert(ctx->sizes[dst.value] == 1);
       I->dest[0].value = tls_base + ctx->tls_loc[dst.value];
    }
 
@@ -67,7 +78,6 @@ lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
          continue;
 
       assert(ctx->tls_loc[src.value] != UINT32_MAX && "Undefined source");
-      assert(ctx->sizes[src.value] == 1);
       I->src[s].value = tls_base + ctx->tls_loc[src.value];
    }
 }
@@ -83,8 +93,12 @@ assign_tls_locations(struct lower_spill_ctx *ctx) {
          assert(I->op == BI_OPCODE_MEMMOV || I->op == BI_OPCODE_PHI);
          assert(ctx->tls_loc[dst.value] == UINT32_MAX && "Broken SSA");
 
+         unsigned size = ctx->sizes[dst.value];
+         unsigned alignment = (size <= 1) ? 4 : (size == 2) ? 8 : 16;
+
+         ctx->spill_count = ALIGN_POT(ctx->spill_count, alignment);
          ctx->tls_loc[dst.value] = ctx->spill_count;
-         ctx->spill_count += 4 * ctx->sizes[dst.value];
+         ctx->spill_count += 4 * size;
       }
    }
 }

src/panfrost/compiler/bifrost/bi_ra.c
@@ -733,7 +733,7 @@ bi_compute_reg_alignment(bi_context *ctx)
    bi_foreach_instr_global(ctx, I) {
       bi_foreach_ssa_dest(I, d) {
          idx = I->dest[d].value;
-         count = bi_count_write_registers(I, d);
+         count = bi_count_write_registers(I, d) + I->dest[d].offset;
          if (count == 3) count = 4;
          assert(idx < ctx->ssa_alloc);
          ctx->reg_alignment[idx] = MAX2(count*4, ctx->reg_alignment[idx]);
@@ -755,18 +755,16 @@ bi_spill_register(bi_context *ctx, bi_index index, uint32_t offset,
                   bi_index spill_point)
 {
    bi_builder b = {.shader = ctx};
-   unsigned alignment = 4;
-   unsigned channels = 0;
+   if (!ctx->reg_alignment)
+      bi_compute_reg_alignment(ctx);
+   assert(index.value < ctx->ssa_alloc);
+   unsigned channels = ctx->reg_alignment[index.value] / 4;
+   assert(channels > 0);
+   unsigned alignment = (ctx->arch >= 9) ? ctx->reg_alignment[index.value] : 4;
 
    /* first figure out the alignment we will need, based on the
     * maximum count we see
     */
-   if (ctx->arch >= 9) {
-      if (!ctx->reg_alignment)
-         bi_compute_reg_alignment(ctx);
-      assert(index.value < ctx->ssa_alloc);
-      alignment = ctx->reg_alignment[index.value];
-   }
    offset = ALIGN_POT(offset, alignment);
 
    /* Spill after every store, fill before every load */
@@ -782,20 +780,18 @@ bi_spill_register(bi_context *ctx, bi_index index, uint32_t offset,
             if (!bi_is_equiv(I->dest[d], index))
                continue;
 
+            I->no_spill = true;
             unsigned count = bi_count_write_registers(I, d);
             unsigned extra = I->dest[d].offset;
+            bi_index src = index;
+            src.offset = extra;
 
-            channels = MAX2(channels, extra + count);
-            I->no_spill = true;
+            b.cursor = bi_after_instr(I);
+            bi_store_tl(&b, count * 32, src, offset + (extra * 4));
 
-            if (channels == extra + count) {
-               b.cursor = bi_after_instr(I);
-               bi_store_tl(&b, channels * 32, index, offset);
-
-               ctx->spills++;
-               /* Don't disable filling if spill_point is before index. */
-               fill = found_spill_point;
-            }
+            ctx->spills++;
+            /* Don't disable filling if spill_point is before index. */
+            fill = found_spill_point;
          }
 
          if (bi_has_arg(I, index) && fill) {
@@ -980,6 +976,20 @@ squeeze_index(bi_context *ctx)
    ralloc_free(map);
 }
 
+static void
+bi_mov_words_to(bi_builder *b, bi_index dst, bi_index src, unsigned words)
+{
+   assert(words >= 1 && words <= 4);
+
+   for (unsigned i = 0; i < words; ++i) {
+      bi_index word_dst = dst, word_src = src;
+      word_dst.offset += i;
+      word_src.offset += i;
+
+      bi_mov_i32_to(b, word_dst, word_src);
+   }
+}
+
 /*
  * Brainless out-of-SSA pass. The eventual goal is to go out-of-SSA after RA and
  * coalesce implicitly with biased colouring in a tree scan allocator. For now,
@@ -997,6 +1007,9 @@ bi_out_of_ssa(bi_context *ctx)
       bi_foreach_instr_in_block_safe(block, I) {
          if (I->op != BI_OPCODE_PHI)
             break;
+
+         unsigned words = I->table ? I->table : 1;
+         assert(words >= 1 && words <= 4);
 
          /* Assign a register for the phi */
          bi_index reg = bi_temp(ctx);
@@ -1016,12 +1029,17 @@ bi_out_of_ssa(bi_context *ctx)
 
             if (I->src[i].memory)
                /* spilled register, need to un-spill */
-               bi_load_tl(&b, 32, reg, I->src[i].value);
-            else if (ctx->arch >= 9 && I->src[i].type == BI_INDEX_CONSTANT)
+               bi_load_tl(&b, words * 32, reg, I->src[i].value);
+            else if (ctx->arch >= 9 && I->src[i].type == BI_INDEX_CONSTANT) {
                /* MOV of immediate needs lowering on Valhall */
-               bi_iadd_imm_i32_to(&b, reg, zero, I->src[i].value);
-            else
-               bi_mov_i32_to(&b, reg, I->src[i]);
+               for (unsigned w = 0; w < words; ++w) {
+                  bi_index word_reg = reg;
+                  word_reg.offset += w;
+                  bi_iadd_imm_i32_to(&b, word_reg, zero,
+                                     w == 0 ? I->src[i].value : 0);
+               }
+            } else
+               bi_mov_words_to(&b, reg, I->src[i], words);
          }
 
          /* Replace the phi with a move */
@@ -1029,13 +1047,13 @@ bi_out_of_ssa(bi_context *ctx)
          bi_builder b = bi_init_builder(ctx, bi_before_instr(I));
          if (I->dest[0].memory) {
             /* dest was spilled to memory */
-            bi_store_tl(&b, 32, reg, I->dest[0].value);
+            bi_store_tl(&b, words * 32, reg, I->dest[0].value);
             allow_propagate = false;
          } else if (ctx->arch >= 9 && reg.type == BI_INDEX_CONSTANT)
             /* MOV of immediate needs lowering on Valhall */
             bi_iadd_imm_i32_to(&b, I->dest[0], zero, reg.value);
          else
-            bi_mov_i32_to(&b, I->dest[0], reg);
+            bi_mov_words_to(&b, I->dest[0], reg, words);
          bi_remove_instruction(I);
 
          /* Propagate that move within the block. The destination

src/panfrost/compiler/bifrost/bifrost_compile.c
@@ -235,6 +235,24 @@ bi_collect_v2i32(bi_builder *b, bi_index s0, bi_index s1)
    return dst;
 }
 
+static void
+bi_split_i64_for_shadd(bi_builder *b, bi_index src, bi_index out[2])
+{
+   bi_index tmp[4] = {bi_null(), bi_null(), bi_null(), bi_null()};
+   bi_emit_split_i32(b, tmp, src, 2);
+   out[0] = tmp[0];
+   out[1] = tmp[1];
+}
+
+static bi_index
+bi_materialize_i64_for_shadd(bi_builder *b, bi_index src)
+{
+   bi_index components[2] = {bi_null(), bi_null()};
+   bi_split_i64_for_shadd(b, src, components);
+
+   return bi_collect_v2i32(b, components[0], components[1]);
+}
+
 static inline bi_instr *
 bi_f32_to_f16_to(bi_builder *b, bi_index dest, bi_index src)
 {
@@ -3447,6 +3465,8 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
    case nir_op_iadd:
       if (sz == 64) {
          assert(b->shader->arch >= 9);
+         s0 = bi_materialize_i64_for_shadd(b, s0);
+         s1 = bi_materialize_i64_for_shadd(b, s1);
          bi_shaddx_s64_to(b, dst, s0, s1, 0);
          bi_index dsts[4] = {bi_null(), bi_null(), bi_null(), bi_null()};
          bi_emit_split_i32(b, dsts, dst, 2);

commit c8c8ce178120d515372b8a535b1917d9aba315b1

pan/bi: Fix vector phis from spilling
Spilling, or more specifically bi_repair_ssa, can create vector phis
that are not trivial and remain after spill lowering. For example in
dEQP-VK.glsl.indexing.matrix_subscript.mat4_dynamic_loop_write_dynamic_read_vertex
we end up with the following after bi_lower_spill:

block0 {
32x4    %93 = COLLECT.i32 %17, %19, %21, %12
        STORE.i128.tl %93, tls_ptr, tls_ptr[1], byte_offset:80
}

block10 {
32x4    %153 = LOAD.i128.tl tls_ptr, tls_ptr[1], byte_offset:80
}

block12 {
32      %169 = PHI.table1 %153, %93
}

Note the "table1" on the PHI indicating that we think the width of the
PHI is 1, which is obviously wrong.

Fix this by expanding the usage of the bi_instr::table sidechannel to
all phis during spill lowering. It's intentionally still part of the
spill lowering since those PHIs are created from spilling and we don't
want to fully support vector PHIs everywhere now when kraid is around
the corner.

Fixes: 4a5e9c5f2f1 ("pan/compiler: fix spilling for 64-bit values")
Acked-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Jakob Sinclair <jakob.sinclair@arm.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42862>

src/panfrost/compiler/bifrost/bi_lower_spill.c
@@ -41,8 +41,31 @@ lower_memmov(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
    bi_remove_instruction(I);
 }
 
+/* For vector phis, bi_out_of_ssa expects the width in bi_instr::table. */
 static void
-lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
+lower_phi_size(struct lower_spill_ctx *ctx, bi_instr *I)
+{
+   assert(I->op == BI_OPCODE_PHI);
+   assert(I->nr_dests == 1);
+
+#ifndef NDEBUG
+   /* Sanity check. */
+
+   unsigned words = 1;
+   words = MAX2(words, ctx->sizes[I->dest[0].value]);
+
+   bi_foreach_ssa_src(I, s)
+      words = MAX2(words, ctx->sizes[I->src[s].value]);
+
+   assert(words == ctx->sizes[I->dest[0].value]);
+#endif
+
+   I->table = ctx->sizes[I->dest[0].value];
+   assert(I->table <= 4);
+}
+
+static void
+lower_mem_phi(struct lower_spill_ctx *ctx, bi_instr *I, uint32_t tls_base)
 {
    assert(I->op == BI_OPCODE_PHI);
    assert(I->nr_dests == 1);
@@ -51,24 +74,8 @@ lower_mem_phi(struct lower_spill_ctx* ctx, bi_instr *I, uint32_t tls_base)
     * provide the actual TLS offset as bi_index::value.
     */
 
-   unsigned words = 1;
-
-   if (I->dest[0].memory)
-      words = MAX2(words, ctx->sizes[I->dest[0].value]);
-
-   bi_foreach_src(I, s) {
-      if (I->src[s].memory)
-         words = MAX2(words, ctx->sizes[I->src[s].value]);
-   }
-
-   assert(words <= 4);
-   I->table = words;
-
    if (I->dest[0].memory) {
       const bi_index dst = I->dest[0];
-      /* Preserve the memory value long enough to look up its assigned TLS
-       * slot. bi_out_of_ssa uses I->table to lower all words of the PHI.
-       */
       I->dest[0].value = tls_base + ctx->tls_loc[dst.value];
    }
 
@@ -121,6 +128,12 @@ bi_lower_spill(bi_context* ctx, uint32_t tls_base) {
    };
 
    assign_tls_locations(&lctx);
+
+   /* Before adding SSA values, lower phi sizes. */
+   bi_foreach_instr_global_safe(ctx, I) {
+      if (I->op == BI_OPCODE_PHI)
+         lower_phi_size(&lctx, I);
+   }
 
    bi_foreach_instr_global_safe(ctx, I) {
       switch (I->op) {