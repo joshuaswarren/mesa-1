joshuaswarren/mesa-1

commit 3349e65652125fbff6cb8fddf2a5d8e9928ca7cd

svga: Pass buffer handle for fd handles for resource_from_handle()
Currently we pass surface handles to the kernel in drmPrimeFDToHandle, and
this gives us an fd for a surface. This causes problems with kwin which imports
the handle expecting it to be a gem handle and attempts to close it with the
gem_close() ioctl.

The way vmwgfx kernel driver processes handle_to_fd, for surface handles,
it always gives surface fd, for gem buffers, it will give the gem buffer fd
for dumb buffers and buffers that don't back a surface, and a surface fd
for buffers that do back a surface.

Pass the gem buffer handle instead of the surface handle. For kwin dealing
with plain or dumb buffers, it gives an fd which can be imported as a gem
buffer, so kwin can close it with gem_close(). For other applications
dealing with surfaces, they will continue to get a surface fd, so will not see
any change in behaviour.

This fixes a memory leak seen on kde desktop.

Signed-off-by: Maaz Mombasawala <maaz.mombasawala@broadcom.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43386>

src/gallium/winsys/svga/drm/vmw_screen_dri.c
@@ -309,37 +309,46 @@ vmw_drm_surface_get_handle(struct svga_winsys_screen *sws,
                            unsigned stride,
                            struct winsys_handle *whandle)
 {
-    struct vmw_winsys_screen *vws = vmw_winsys_screen(sws);
-    struct vmw_svga_winsys_surface *vsrf;
-    int ret;
+   struct vmw_winsys_screen *vws = vmw_winsys_screen(sws);
+   struct vmw_svga_winsys_surface *vsrf;
+   int ret;
 
-    if (!surface)
-        return false;
+   if (!surface)
+      return false;
 
-    vsrf = vmw_svga_winsys_surface(surface);
-    whandle->handle = vsrf->sid;
-    whandle->stride = stride;
-    whandle->offset = 0;
-    whandle->modifier = DRM_FORMAT_MOD_LINEAR;
+   vsrf = vmw_svga_winsys_surface(surface);
+   whandle->handle = vsrf->sid;
+   whandle->stride = stride;
+   whandle->offset = 0;
+   whandle->modifier = DRM_FORMAT_MOD_LINEAR;
 
-    switch (whandle->type) {
-    case WINSYS_HANDLE_TYPE_SHARED:
-    case WINSYS_HANDLE_TYPE_KMS:
-       whandle->handle = vsrf->sid;
-       break;
-    case WINSYS_HANDLE_TYPE_FD:
-       ret = drmPrimeHandleToFD(vws->ioctl.drm_fd, vsrf->sid, DRM_CLOEXEC | DRM_RDWR,
-                                (int *)&whandle->handle);
-       if (ret) {
-          vmw_error("Failed to get file descriptor from prime.\n");
-          return false;
-       }
-       break;
-    default:
-       vmw_error("Attempt to export unsupported handle type %d.\n",
-                 whandle->type);
-       return false;
-    }
+   switch (whandle->type) {
+   case WINSYS_HANDLE_TYPE_SHARED:
+   case WINSYS_HANDLE_TYPE_KMS:
+      whandle->handle = vsrf->sid;
+      break;
+   case WINSYS_HANDLE_TYPE_FD:
+      if (vws->ioctl.have_drm_2_21 && vsrf->buf) {
+        struct vmw_region *region =
+           vmw_winsys_to_dma_buffer(vsrf->buf)->region;
+        ret = drmPrimeHandleToFD(vws->ioctl.drm_fd, region->handle,
+                                 DRM_CLOEXEC | DRM_RDWR,
+                                 (int *)&whandle->handle);
+      } else if (!vsrf->buf) {
+        ret = drmPrimeHandleToFD(vws->ioctl.drm_fd, vsrf->sid,
+                                 DRM_CLOEXEC | DRM_RDWR,
+                                 (int *)&whandle->handle);
+     }
+      if (ret) {
+         vmw_error("Failed to get file descriptor from prime.\n");
+         return false;
+      }
+      break;
+   default:
+      vmw_error("Attempt to export unsupported handle type %d.\n",
+                whandle->type);
+      return false;
+   }
 
-    return true;
+   return true;
 }

src/gallium/winsys/svga/drm/vmw_buffer.c
@@ -35,19 +35,6 @@
 struct vmw_dma_bufmgr;
 
 
-struct vmw_dma_buffer
-{
-   struct pb_buffer base;
-
-   struct vmw_dma_bufmgr *mgr;
-
-   struct vmw_region *region;
-   void *map;
-   unsigned map_flags;
-   unsigned map_count;
-};
-
-
 extern const struct pb_vtbl vmw_dma_buffer_vtbl;
 
 
@@ -59,6 +46,12 @@ vmw_pb_to_dma_buffer(struct pb_buffer *buf)
    return container_of(buf, struct vmw_dma_buffer, base);
 }
 
+struct vmw_dma_buffer *
+vmw_winsys_to_dma_buffer(struct svga_winsys_buffer *buffer)
+{
+   struct pb_buffer *p_buf = vmw_pb_buffer(buffer);
+   return vmw_pb_to_dma_buffer(p_buf);
+}
 
 struct vmw_dma_bufmgr
 {

src/gallium/winsys/svga/drm/vmw_buffer.h
@@ -31,6 +31,17 @@ struct vmw_buffer_desc {
    struct vmw_region *region;
 };
 
+struct vmw_dma_buffer
+{
+   struct pb_buffer base;
+
+   struct vmw_dma_bufmgr *mgr;
+
+   struct vmw_region *region;
+   void *map;
+   unsigned map_flags;
+   unsigned map_count;
+};
 
 #if MESA_DEBUG
 
@@ -56,6 +67,9 @@ vmw_svga_winsys_buffer_wrap(struct pb_buffer *buffer)
    return (struct svga_winsys_buffer *)buffer;
 }
 #endif
+
+struct vmw_dma_buffer *
+vmw_winsys_to_dma_buffer(struct svga_winsys_buffer *buffer);
 
 void
 vmw_svga_winsys_buffer_destroy(struct svga_winsys_screen *sws,

src/gallium/winsys/svga/drm/vmw_screen.h
@@ -38,7 +38,15 @@
 #define VMW_MAX_BUFFER_SIZE (512*1024*1024)
 
 struct pb_manager;
-struct vmw_region;
+struct vmw_region
+{
+   uint32_t handle;
+   uint64_t map_handle;
+   void *data;
+   uint32_t map_count;
+   int drm_fd;
+   uint32_t size;
+};
 
 struct vmw_cap_3d {
    bool has_cap;
@@ -66,6 +74,7 @@ struct vmw_winsys_screen
       bool have_drm_2_18;
       bool have_drm_2_19;
       bool have_drm_2_20;
+      bool have_drm_2_21;
    } ioctl;
 
    struct {

src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
@@ -41,16 +41,6 @@
 #define SVGA3D_FLAGS_LOWER_32(svga3d_flags) \
    (svga3d_flags & ((uint64_t)UINT32_MAX))
 
-struct vmw_region
-{
-   uint32_t handle;
-   uint64_t map_handle;
-   void *data;
-   uint32_t map_count;
-   int drm_fd;
-   uint32_t size;
-};
-
 uint32_t
 vmw_region_size(struct vmw_region *region)
 {
@@ -964,6 +954,13 @@ vmw_ioctl_parse_caps(struct vmw_winsys_screen *vws,
    return 0;
 }
 
+static inline bool
+vmw_ioctl_have_version(drmVersionPtr version, int major, int minor)
+{
+   return version->version_major > major ||
+      (version->version_major == major && version->version_minor >= minor);
+}
+
 bool
 vmw_ioctl_init(struct vmw_winsys_screen *vws)
 {
@@ -983,24 +980,16 @@ vmw_ioctl_init(struct vmw_winsys_screen *vws)
    if (!version)
       goto out_no_version;
 
-   have_drm_2_5 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 4);
-   vws->ioctl.have_drm_2_6 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 5);
-   vws->ioctl.have_drm_2_9 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 8);
-   vws->ioctl.have_drm_2_15 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 14);
-   vws->ioctl.have_drm_2_16 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 15);
-   vws->ioctl.have_drm_2_17 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 16);
-   vws->ioctl.have_drm_2_18 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 17);
-   vws->ioctl.have_drm_2_19 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 18);
-   vws->ioctl.have_drm_2_20 = version->version_major > 2 ||
-      (version->version_major == 2 && version->version_minor > 19);
+   have_drm_2_5 = vmw_ioctl_have_version(version, 2, 5);
+   vws->ioctl.have_drm_2_6 = vmw_ioctl_have_version(version, 2, 6);
+   vws->ioctl.have_drm_2_9 = vmw_ioctl_have_version(version, 2, 9);
+   vws->ioctl.have_drm_2_15 = vmw_ioctl_have_version(version, 2, 15);
+   vws->ioctl.have_drm_2_16 = vmw_ioctl_have_version(version, 2, 16);
+   vws->ioctl.have_drm_2_17 = vmw_ioctl_have_version(version, 2, 17);
+   vws->ioctl.have_drm_2_18 = vmw_ioctl_have_version(version, 2, 18);
+   vws->ioctl.have_drm_2_19 = vmw_ioctl_have_version(version, 2, 19);
+   vws->ioctl.have_drm_2_20 = vmw_ioctl_have_version(version, 2, 20);
+   vws->ioctl.have_drm_2_21 = vmw_ioctl_have_version(version, 2, 21);
 
    vws->ioctl.drm_execbuf_version = vws->ioctl.have_drm_2_9 ? 2 : 1;
 

commit abe71b717e6926825c8fe34b7f40cba94a0cbe00

svga: Remove erroneous else-if in surface_get_handle()
My previous commit introduced a subtle error for fd handles where for vmwgfx
versions < 2.21 and surfaces without a backing buffer, the function
vmw_drm_surface_get_handle() would do nothing.
Change the else-if to a plain else to fix the issue.

Fixes: 3349e656521 ("svga: Pass buffer handle for fd handles for resource_from_handle()")
Signed-off-by: Maaz Mombasawala <maaz.mombasawala@broadcom.com>
Backport-to: *
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43845>

src/gallium/winsys/svga/drm/vmw_screen_dri.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2024 Broadcom. All Rights Reserved.
+ * Copyright (c) 2009-2026 Broadcom. All Rights Reserved.
  * The term “Broadcom” refers to Broadcom Inc.
  * and/or its subsidiaries.
  * SPDX-License-Identifier: MIT
@@ -311,7 +311,7 @@ vmw_drm_surface_get_handle(struct svga_winsys_screen *sws,
 {
    struct vmw_winsys_screen *vws = vmw_winsys_screen(sws);
    struct vmw_svga_winsys_surface *vsrf;
-   int ret;
+   int ret = 0;
 
    if (!surface)
       return false;
@@ -334,11 +334,11 @@ vmw_drm_surface_get_handle(struct svga_winsys_screen *sws,
         ret = drmPrimeHandleToFD(vws->ioctl.drm_fd, region->handle,
                                  DRM_CLOEXEC | DRM_RDWR,
                                  (int *)&whandle->handle);
-      } else if (!vsrf->buf) {
+      } else {
         ret = drmPrimeHandleToFD(vws->ioctl.drm_fd, vsrf->sid,
                                  DRM_CLOEXEC | DRM_RDWR,
                                  (int *)&whandle->handle);
-     }
+      }
       if (ret) {
          vmw_error("Failed to get file descriptor from prime.\n");
          return false;