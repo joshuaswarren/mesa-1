joshuaswarren/mesa-1

commit bf9003617fa1021315379a37d68f1f46b9dda570

pan/compiler: Lower mediump varying floats to 16-bits early
If we want to have mediump lowered to 16-bits in the memory layout on
gallium too, we need to lower it before nir_opt_varyings, we laid out
most of the work in the previous commits, this is the core of the change.
We need to rework what each pass does:
- pan_nir_lower_mediump_io (called at "link time") lowers the varyings
  before nir_opt_varyings and removes every useless mediump.  The only
  remaining mediump usage is for smooth loads in FS as those change the
  interpolation behaviour (see pan_nir_fuse_io_cvt, that's more of a
  correctness problem as tests have bugs and rely on it)
- collect_varyings: collects the types used by the shader intrinsics in
  a single unified format for memory layout and/or hardware descriptors
- resize_varying_io: Adapts the shader load/stores to reflect the type
  in the memory layout and/or descriptors

nir_opt_varyings can also store different types on the same flat slot,
this requires lower_vs_outputs to trust the layout type.

We do all this in a single, big-ish atomic commit to remove any possible
regressions as these passes are all very tied together.

Additionally, pan_nir_lower_mediump_io scalarizes the conversions and
calls nir_opt_shrink_vectors, this helps nir_opt_varyings to look through
f2f16 conversions, and DCEs useless varying loads to the VS too.

Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Acked-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41578>

src/panfrost/compiler/midgard/midgard_compile.h
@@ -9,6 +9,7 @@
 
 #include "compiler/nir/nir.h"
 #include "panfrost/compiler/pan_compiler.h"
+#include "panfrost/compiler/pan_nir.h"
 #include "util/u_dynarray.h"
 
 void midgard_preprocess_nir(nir_shader *nir, uint64_t gpu_id);
@@ -87,6 +88,7 @@ static const nir_shader_compiler_options midgard_nir_options = {
    .lower_doubles_options = nir_lower_dmod,
    .support_indirect_inputs = 0 /* TODO support indirect varyings */,
    .max_samples = 16,
+   .lower_mediump_io = pan_nir_lower_mediump_io,
 };
 
 #endif

src/gallium/drivers/panfrost/pan_shader.c
@@ -589,8 +589,7 @@ panfrost_create_shader_state(struct pipe_context *pctx,
    if (nir->info.stage == MESA_SHADER_VERTEX) {
       struct pan_varying_layout *varying_layout = &so->vs_varying_layout;
       pan_varying_collect_formats(varying_layout, nir,
-                                  panfrost_device_gpu_id(dev),
-                                  false /* lower_mediump */);
+                                  panfrost_device_gpu_id(dev));
       pan_build_varying_layout_compact(varying_layout, nir,
                                        panfrost_device_gpu_id(dev));
    }

src/panfrost/compiler/bifrost/bifrost_compile.h
@@ -10,6 +10,7 @@
 #include <string.h>
 #include "compiler/nir/nir.h"
 #include "panfrost/compiler/pan_compiler.h"
+#include "panfrost/compiler/pan_nir.h"
 #include "util/u_dynarray.h"
 
 struct bifrost_precompiled_kernel_sysvals {
@@ -162,6 +163,8 @@ bool valhall_can_merge_workgroups(nir_shader *nir);
          (nir_divergence_across_subgroups |                                    \
           nir_divergence_multiple_workgroup_per_compute_subgroup)              \
          : 0,                                                                  \
+      .lower_mediump_io = pan_nir_lower_mediump_io,                            \
+      .io_options = nir_io_has_intrinsics, /* Skip unlowering IO to vars */    \
    };
 
 DEFINE_OPTIONS(bifrost_nir_options_v6, 6, false);

src/panfrost/compiler/bifrost/bifrost_nir.c
@@ -443,77 +443,6 @@ bifrost_preprocess_nir(nir_shader *nir, uint64_t gpu_id)
    NIR_PASS(_, nir, nir_lower_var_copies);
 }
 
-/*
- * Build a bit mask of varyings (by location) that are flatshaded. This
- * information is needed by lower_mediump_io, as we don't yet support 16-bit
- * flat varyings.
- *
- * Also varyings that are used as texture coordinates should be kept at fp32 so
- * the texture instruction may be promoted to VAR_TEX. In general this is a good
- * idea, as fp16 texture coordinates are not supported by the hardware and are
- * usually inappropriate. (There are both relevant CTS bugs here, even.)
- *
- * TODO: If we compacted the varyings with some fixup code in the vertex shader,
- * we could implement 16-bit flat varyings. Consider if this case matters.
- *
- * TODO: The texture coordinate handling could be less heavyhanded.
- */
-static bool
-bi_gather_texcoords(nir_builder *b, nir_instr *instr, void *data)
-{
-   uint64_t *mask = data;
-
-   if (instr->type != nir_instr_type_tex)
-      return false;
-
-   nir_tex_instr *tex = nir_instr_as_tex(instr);
-
-   int coord_idx = nir_tex_instr_src_index(tex, nir_tex_src_coord);
-   if (coord_idx < 0)
-      return false;
-
-   nir_src src = tex->src[coord_idx].src;
-   nir_scalar x = nir_scalar_resolved(src.ssa, 0);
-   nir_scalar y = nir_scalar_resolved(src.ssa, 1);
-
-   if (x.def != y.def)
-      return false;
-
-   nir_instr *parent = nir_def_instr(x.def);
-
-   if (parent->type != nir_instr_type_intrinsic)
-      return false;
-
-   nir_intrinsic_instr *intr = nir_instr_as_intrinsic(parent);
-
-   if (intr->intrinsic != nir_intrinsic_load_interpolated_input)
-      return false;
-
-   nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
-   *mask |= BITFIELD64_BIT(sem.location);
-   return false;
-}
-
-static uint64_t
-bi_fp32_varying_mask(nir_shader *nir)
-{
-   uint64_t mask = 0;
-
-   assert(nir->info.stage == MESA_SHADER_FRAGMENT);
-
-   nir_foreach_shader_in_variable(var, nir) {
-      if (var->data.interpolation == INTERP_MODE_FLAT) {
-         unsigned slots = glsl_count_attribute_slots(var->type, false);
-         mask |= BITFIELD64_RANGE(var->data.location, slots);
-      }
-   }
-
-   nir_shader_instructions_pass(nir, bi_gather_texcoords, nir_metadata_all,
-                                &mask);
-
-   return mask;
-}
-
 static bool
 bi_lower_subgroups(nir_builder *b, nir_intrinsic_instr *intr, void *data)
 {
@@ -907,19 +836,6 @@ bifrost_postprocess_nir(nir_shader *nir,
    NIR_PASS(_, nir, nir_opt_sink, move_all);
    NIR_PASS(_, nir, nir_opt_move, move_all);
 
-   /* The varying layout (if any) may have different bit sizes for some
-    * varyings than we have in the shader.  For descriptors, this isn't a
-    * problem as it's handled by the descriptor layout.  However, for direct
-    * loads and stores on Valhall+, we need the right bit sizes in the shader.
-    * We could do this in the back-end as we emit but it's easier for now to
-    * lower in NIR.  This also handles the case where we do a load from the
-    * fragment shader of something that isn't written by the vertex shader.
-    * In that case, we just return zero.
-    */
-   if (pan_arch(inputs->gpu_id) >= 9 && inputs->varying_layout)
-      NIR_PASS(_, nir, pan_nir_resize_varying_io, inputs->varying_layout,
-               inputs->varying_layout);
-
    if (nir->info.stage == MESA_SHADER_FRAGMENT) {
       NIR_PASS(_, nir, nir_lower_is_helper_invocation);
       NIR_PASS(_, nir, pan_nir_lower_helper_invocation);
@@ -929,18 +845,15 @@ bifrost_postprocess_nir(nir_shader *nir,
       NIR_PASS(_, nir, nir_lower_frag_coord_to_pixel_coord);
       NIR_PASS(_, nir, pan_nir_lower_var_special_pan);
 
-      /* TODO: should we do this in VS too? Should we do this earlier? */
-      NIR_PASS(_, nir, nir_lower_mediump_io,
-               nir_var_shader_in | nir_var_shader_out,
-               ~bi_fp32_varying_mask(nir), false);
+      NIR_PASS(_, nir, nir_lower_mediump_io, nir_var_shader_out, 0, false);
 
       NIR_PASS(_, nir, bifrost_nir_lower_load_output);
 
       /* Collect format varyings */
-      pan_varying_collect_formats(&info->varyings.formats,
-                                  nir, inputs->gpu_id,
-                                  false /* lower mediump */);
+      pan_varying_collect_formats(&info->varyings.formats, nir, inputs->gpu_id);
 
+      NIR_PASS(_, nir, pan_nir_resize_varying_io, &info->varyings.formats,
+               inputs->varying_layout ?: &info->varyings.formats);
       NIR_PASS(_, nir, nir_lower_io_to_scalar, nir_var_shader_in, NULL, NULL);
       NIR_PASS(_, nir, nir_opt_vectorize_io, nir_var_shader_in, false);
 
@@ -968,6 +881,9 @@ bifrost_postprocess_nir(nir_shader *nir,
       assert(inputs->varying_layout);
       memcpy(&info->varyings.formats, inputs->varying_layout,
              sizeof(*inputs->varying_layout));
+
+      NIR_PASS(_, nir, pan_nir_resize_varying_io, &info->varyings.formats,
+               &info->varyings.formats);
 
       info->vs.idvs = bi_should_idvs(nir, inputs);
 

src/panfrost/compiler/midgard/midgard_compile.c
@@ -2984,8 +2984,7 @@ midgard_compile_shader_nir(nir_shader *nir,
       memcpy(&info->varyings.formats, inputs->varying_layout,
              sizeof(*inputs->varying_layout));
    } else if (nir->info.stage == MESA_SHADER_FRAGMENT) {
-      pan_varying_collect_formats(&info->varyings.formats,
-                                  nir, inputs->gpu_id, false);
+      pan_varying_collect_formats(&info->varyings.formats, nir, inputs->gpu_id);
       info->varyings.noperspective =
          pan_nir_collect_noperspective_varyings_fs(nir);
    }

src/panfrost/compiler/pan_compiler.c
@@ -4,6 +4,7 @@
  */
 
 #include "pan_compiler.h"
+#include "nir_xfb_info.h"
 #include "pan_nir.h"
 
 #include "bifrost/bi_debug.h"
@@ -380,6 +381,139 @@ pan_shader_compile(nir_shader *s, struct pan_compile_inputs *inputs,
       midgard_compile_shader_nir(s, inputs, binary, info);
       pan_shader_update_info(info, s, inputs);
    }
+}
+
+static uint64_t
+pan_fixed_varying_mask(nir_shader *nir)
+{
+   uint64_t mask = 0;
+
+   assert(nir->info.stage == MESA_SHADER_FRAGMENT ||
+          nir->info.stage == MESA_SHADER_VERTEX);
+
+   nir_function_impl *impl = nir_shader_get_entrypoint(nir);
+   assert(impl);
+
+   nir_foreach_block(block, impl) {
+      nir_foreach_instr(instr, block) {
+         nir_variable_mode modes = nir_var_shader_in | nir_var_shader_out;
+         nir_variable_mode mode;
+         nir_intrinsic_instr *intr = nir_get_io_intrinsic(instr, modes, &mode);
+         if (!intr)
+            continue;
+
+         bool is_varying = !(nir->info.stage == MESA_SHADER_VERTEX &&
+                             mode == nir_var_shader_in) &&
+                           !(nir->info.stage == MESA_SHADER_FRAGMENT &&
+                             mode == nir_var_shader_out);
+
+         nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
+
+         if (!is_varying || sem.location < VARYING_SLOT_VAR0)
+            continue;
+
+         nir_alu_type type = nir_intrinsic_has_src_type(intr) ?
+            nir_intrinsic_src_type(intr) : nir_intrinsic_dest_type(intr);
+         bool is_float = nir_alu_type_get_base_type(type) == nir_type_float;
+
+         /* Only lower mediump floats, they must agree on ALL load/stores */
+         if (!(sem.medium_precision && is_float)) {
+            mask |= BITFIELD64_RANGE(sem.location, sem.num_slots);
+         }
+      }
+   }
+
+   return mask;
+}
+
+static bool
+clear_flat_mediump_io_flag(struct nir_builder *b, nir_intrinsic_instr *intr,
+                           void *data)
+{
+   /* The mediump flag must be preserved for XFB, we can remove it for all other
+    * flat IO.  It is still useful for interpolated input because of
+    * pan_nir_fuse_io_16.
+    */
+   bool is_flat = intr->intrinsic == nir_intrinsic_load_input ||
+                  (intr->intrinsic == nir_intrinsic_store_output &&
+                   b->shader->info.stage == MESA_SHADER_VERTEX);
+
+   if (nir_intrinsic_has_io_semantics(intr) &&
+       !nir_instr_xfb_write_mask(intr) && is_flat) {
+      nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
+
+      if (sem.medium_precision) {
+         sem.medium_precision = 0;
+         nir_intrinsic_set_io_semantics(intr, sem);
+         return true;
+      }
+   }
+   return false;
+}
+
+static bool
+is_mediump_varying_instr(const nir_intrinsic_instr *intr, const void *data)
+{
+   if (!nir_intrinsic_has_io_semantics(intr))
+      return false;
+
+   nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
+   uint64_t loc_mask = *(uint64_t *)data;
+
+   if (sem.location < VARYING_SLOT_VAR0 || sem.location > VARYING_SLOT_VAR31)
+      return false;
+
+   return loc_mask & BITFIELD64_RANGE(sem.location, sem.num_slots);
+}
+
+/* Lower VS/FS varyings early for linked shader, this permits us to do crazy
+ * compactions in nir_opt_varyings (and might save us from lots of bugs).
+ * Used by lower_mediump_io
+ */
+void
+pan_nir_lower_mediump_io(nir_shader *nir)
+{
+   /* I don't want to get headaches, XFB gets slowed down */
+   if (nir->info.prev_stage_has_xfb ||
+       nir->info.has_transform_feedback_varyings)
+      return;
+
+   nir_variable_mode modes = 0;
+
+   switch (nir->info.stage) {
+   case MESA_SHADER_VERTEX:
+      modes = nir_var_shader_out;
+      break;
+   case MESA_SHADER_FRAGMENT:
+      modes = nir_var_shader_in;
+      break;
+   default:
+      assert(!"Unsupported shader");
+      return;
+   }
+
+   uint64_t lower_mask = ~pan_fixed_varying_mask(nir);
+
+   /* nir_opt_varyings can see thorugh vecs but not through f2f16 of vecs, i.e.
+    * it can see a vec2(x, 1.0) but not through f2f16(vec2(x, 1.0)), if we
+    * scalarize it will only see f2f16(x) and f2f16(1.0).  nir_opt_varyings will
+    * scalarize IO internally anyways.
+    */
+   NIR_PASS(_, nir, nir_lower_io_to_scalar, modes, is_mediump_varying_instr,
+            &lower_mask);
+
+   NIR_PASS(_, nir, nir_lower_mediump_io, modes, lower_mask, false);
+
+   NIR_PASS(_, nir, nir_opt_cse);
+   NIR_PASS(_, nir, nir_opt_constant_folding);
+
+   /* By shrinking vectors we help nir_opt_varyings DCE unused FS loads even
+    * in the VS, it also helps collect a smaller varying layout in the future
+    */
+   NIR_PASS(_, nir, nir_opt_shrink_vectors, false);
+
+   NIR_PASS(_, nir, nir_shader_intrinsics_pass,
+            clear_flat_mediump_io_flag, nir_metadata_all, NULL);
 }
 
 void

src/panfrost/compiler/pan_compiler.h
@@ -355,8 +355,7 @@ void pan_build_varying_layout_compact(struct pan_varying_layout *layout,
                                       nir_shader *nir, uint64_t gpu_id);
 
 void pan_varying_collect_formats(struct pan_varying_layout *registry,
-                                 nir_shader *nir, uint64_t gpu_id,
-                                 bool lower_mediump);
+                                 nir_shader *nir, uint64_t gpu_id);
 
 struct pan_shader_varying {
    gl_varying_slot location;

src/panfrost/compiler/pan_nir.h
@@ -252,6 +252,8 @@ struct pan_va_tex_flags {
 PRAGMA_DIAGNOSTIC_POP
 static_assert(sizeof(struct pan_va_tex_flags) == 4, "Must fit in uint32_t");
 
+void pan_nir_lower_mediump_io(nir_shader *nir);
+
 bool pan_nir_lower_tex(nir_shader *nir, uint64_t gpu_id);
 bool pan_nir_lower_image(nir_shader *nir, uint64_t gpu_id);
 

commit 1e24a56106ead88f9215fe02f1e209139c30ba85

panfrost/mid: Disable mediump lowering entirely
We have more bugs in the midgard backend than what I initially thought,
instead of debugging them let's keep mediump disabled on midgard as it
was before the recent mediump IO MR.  This fixes many regressions caught
by the nightly CI run.

Signed-off-by: Lorenzo Rossi <lorenzo.rossi@collabora.com>
Fixes: bf9003617fa ("pan/compiler: Lower mediump varying floats to 16-bits early")
Reviewed-by: Aksel Hjerpbakk <aksel.hjerpbakk@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43708>

src/panfrost/compiler/midgard/midgard_compile.h
@@ -9,7 +9,6 @@
 
 #include "compiler/nir/nir.h"
 #include "panfrost/compiler/pan_compiler.h"
-#include "panfrost/compiler/pan_nir.h"
 #include "util/u_dynarray.h"
 
 void midgard_preprocess_nir(nir_shader *nir, uint64_t gpu_id);
@@ -88,7 +87,6 @@ static const nir_shader_compiler_options midgard_nir_options = {
    .lower_doubles_options = nir_lower_dmod,
    .support_indirect_inputs = 0 /* TODO support indirect varyings */,
    .max_samples = 16,
-   .lower_mediump_io = pan_nir_lower_mediump_io,
 };
 
 #endif