joshuaswarren/mesa-1

commit 29219129e9bee2466d998850b2ffba7af6950f43

frontends/va: Only export one handle for contiguous planes
If the driver stores all planes contiguously in memory, only one BO
needs to be exported from vaExportSurfaceHandle. This is required
for Chromium's VaapiVideoDecoder to work on radeonsi and r600.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26165>

src/gallium/frontends/va/surface.c
@@ -1643,8 +1643,11 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
 #else
    VADRMPRIMESurfaceDescriptor *desc = descriptor;
    desc->fourcc = PipeFormatToVaFourcc(surf->buffer->buffer_format);
-   desc->width  = surf->templat.width;
+   desc->width = surf->templat.width;
    desc->height = surf->templat.height;
+   desc->num_objects = 0;
+
+   bool supports_contiguous_planes = screen->resource_get_info && surf->buffer->contiguous_planes;
 
    for (p = 0; p < ARRAY_SIZE(desc->objects); p++) {
       struct winsys_handle whandle;
@@ -1662,36 +1665,51 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
          goto fail;
       }
 
-      memset(&whandle, 0, sizeof(whandle));
-      whandle.type = WINSYS_HANDLE_TYPE_FD;
+      /* If the driver stores all planes contiguously in memory, only one
+       * handle needs to be exported. resource_get_info is used to obtain
+       * pitch and offset for each layer. */
+      if (!desc->num_objects || !supports_contiguous_planes) {
+         memset(&whandle, 0, sizeof(whandle));
+         whandle.type = WINSYS_HANDLE_TYPE_FD;
 
-      if (!screen->resource_get_handle(screen, drv->pipe, resource,
-                                       &whandle, usage)) {
-         ret = VA_STATUS_ERROR_INVALID_SURFACE;
-         goto fail;
+         if (!screen->resource_get_handle(screen, drv->pipe, resource,
+                                          &whandle, usage)) {
+            ret = VA_STATUS_ERROR_INVALID_SURFACE;
+            goto fail;
+         }
+
+         desc->objects[desc->num_objects].fd = (int) whandle.handle;
+         /* As per VADRMPRIMESurfaceDescriptor documentation, size must be the
+         * "Total size of this object (may include regions which are not part
+         * of the surface)."" */
+         desc->objects[desc->num_objects].size = (uint32_t) whandle.size;
+         desc->objects[desc->num_objects].drm_format_modifier = whandle.modifier;
+
+         desc->num_objects++;
       }
 
-      desc->objects[p].fd   = (int)whandle.handle;
-      /* As per VADRMPRIMESurfaceDescriptor documentation, size must be the
-       * "Total size of this object (may include regions which are not part
-       * of the surface)."" */
-      desc->objects[p].size = (uint32_t) whandle.size;
-      desc->objects[p].drm_format_modifier = whandle.modifier;
-
       if (flags & VA_EXPORT_SURFACE_COMPOSED_LAYERS) {
-         desc->layers[0].object_index[p] = p;
-         desc->layers[0].offset[p]       = whandle.offset;
-         desc->layers[0].pitch[p]        = whandle.stride;
+         desc->layers[0].object_index[p] = desc->num_objects - 1;
+
+         if (supports_contiguous_planes) {
+            screen->resource_get_info(screen, resource, &desc->layers[0].pitch[p], &desc->layers[0].offset[p]);
+         } else {
+            desc->layers[0].pitch[p] = whandle.stride;
+            desc->layers[0].offset[p] = whandle.offset;
+         }
       } else {
          desc->layers[p].drm_format      = drm_format;
          desc->layers[p].num_planes      = 1;
-         desc->layers[p].object_index[0] = p;
-         desc->layers[p].offset[0]       = whandle.offset;
-         desc->layers[p].pitch[0]        = whandle.stride;
+         desc->layers[p].object_index[0] = desc->num_objects - 1;
+
+         if (supports_contiguous_planes) {
+            screen->resource_get_info(screen, resource, &desc->layers[p].pitch[0], &desc->layers[p].offset[0]);
+         } else {
+            desc->layers[p].pitch[0] = whandle.stride;
+            desc->layers[p].offset[0] = whandle.offset;
+         }
       }
    }
-
-   desc->num_objects = p;
 
    if (flags & VA_EXPORT_SURFACE_COMPOSED_LAYERS) {
       uint32_t drm_format = pipe_format_to_drm_format(surf->buffer->buffer_format);
@@ -1714,7 +1732,7 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
 
 fail:
 #ifndef _WIN32
-   for (i = 0; i < p; i++)
+   for (i = 0; i < desc->num_objects; i++)
       close(desc->objects[i].fd);
 #else
    if(whandle.handle)

commit b304915d370b8c592b8db2c1411656a321c6afc0

va: Compare file descriptions to deduplicate objects when exporting surface
Instead of trusting the contiguous_planes flag. This simplifies the code
and also propagates to driver that all planes are exported, not just first.

Fixes: 29219129e9b ("frontends/va: Only export one handle for contiguous planes")
Reviewed-by: Benjamin Cheng <benjamin.cheng@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43861>

src/gallium/frontends/va/surface.c
@@ -1254,8 +1254,6 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
    desc->height = surf->templat.height;
    desc->num_objects = 0;
 
-   bool supports_contiguous_planes = screen->resource_get_param && surf->buffer->contiguous_planes;
-
    for (p = 0; p < ARRAY_SIZE(desc->objects); p++) {
       struct winsys_handle whandle;
       struct pipe_resource *resource;
@@ -1272,20 +1270,24 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
          goto fail;
       }
 
-      /* If the driver stores all planes contiguously in memory, only one
-       * handle needs to be exported. resource_get_param is used to obtain
-       * pitch and offset for each layer. */
-      if (!desc->num_objects || !supports_contiguous_planes) {
-         memset(&whandle, 0, sizeof(whandle));
-         whandle.type = WINSYS_HANDLE_TYPE_FD;
+      memset(&whandle, 0, sizeof(whandle));
+      whandle.type = WINSYS_HANDLE_TYPE_FD;
 
-         if (!screen->resource_get_handle(screen, drv->pipe, resource,
-                                          &whandle, usage)) {
-            ret = VA_STATUS_ERROR_INVALID_SURFACE;
-            goto fail;
-         }
+      if (!screen->resource_get_handle(screen, drv->pipe, resource,
+                                       &whandle, usage)) {
+         ret = VA_STATUS_ERROR_INVALID_SURFACE;
+         goto fail;
+      }
 
+      /* If this plane shares storage with previous one, we can reuse
+       * the existing object (fd) instead of adding new one.
+       */
+      bool same_object = desc->num_objects &&
+          os_same_file_description(desc->objects[desc->num_objects - 1].fd,
+                                   whandle.handle) == 0;
+      if (!same_object) {
          desc->objects[desc->num_objects].fd = (int) whandle.handle;
+
          /* As per VADRMPRIMESurfaceDescriptor documentation, size must be the
          * "Total size of this object (may include regions which are not part
          * of the surface)."" */
@@ -1293,52 +1295,20 @@ vlVaExportSurfaceHandle(VADriverContextP ctx,
          desc->objects[desc->num_objects].drm_format_modifier = whandle.modifier;
 
          desc->num_objects++;
+      } else {
+         close(whandle.handle);
       }
 
       if (flags & VA_EXPORT_SURFACE_COMPOSED_LAYERS) {
          desc->layers[0].object_index[p] = desc->num_objects - 1;
-
-         if (supports_contiguous_planes) {
-            uint64_t value;
-            if (!screen->resource_get_param(screen, drv->pipe, resource, 0, 0, 0,
-                                            PIPE_RESOURCE_PARAM_STRIDE, 0, &value)) {
-               ret = VA_STATUS_ERROR_INVALID_SURFACE;
-               goto fail;
-            }
-            desc->layers[0].pitch[p] = value;
-            if (!screen->resource_get_param(screen, drv->pipe, resource, 0, 0, 0,
-                                            PIPE_RESOURCE_PARAM_OFFSET, 0, &value)) {
-               ret = VA_STATUS_ERROR_INVALID_SURFACE;
-               goto fail;
-            }
-            desc->layers[0].offset[p] = value;
-         } else {
-            desc->layers[0].pitch[p] = whandle.stride;
-            desc->layers[0].offset[p] = whandle.offset;
-         }
+         desc->layers[0].pitch[p] = whandle.stride;
+         desc->layers[0].offset[p] = whandle.offset;
       } else {
-         desc->layers[p].drm_format      = drm_format;
-         desc->layers[p].num_planes      = 1;
+         desc->layers[p].drm_format = drm_format;
+         desc->layers[p].num_planes = 1;
          desc->layers[p].object_index[0] = desc->num_objects - 1;
-
-         if (supports_contiguous_planes) {
-            uint64_t value;
-            if (!screen->resource_get_param(screen, drv->pipe, resource, 0, 0, 0,
-                                            PIPE_RESOURCE_PARAM_STRIDE, 0, &value)) {
-               ret = VA_STATUS_ERROR_INVALID_SURFACE;
-               goto fail;
-            }
-            desc->layers[p].pitch[0] = value;
-            if (!screen->resource_get_param(screen, drv->pipe, resource, 0, 0, 0,
-                                            PIPE_RESOURCE_PARAM_OFFSET, 0, &value)) {
-               ret = VA_STATUS_ERROR_INVALID_SURFACE;
-               goto fail;
-            }
-            desc->layers[p].offset[0] = value;
-         } else {
-            desc->layers[p].pitch[0] = whandle.stride;
-            desc->layers[p].offset[0] = whandle.offset;
-         }
+         desc->layers[p].pitch[0] = whandle.stride;
+         desc->layers[p].offset[0] = whandle.offset;
       }
    }
 