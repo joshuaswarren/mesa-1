joshuaswarren/mesa-1

commit d2ce898cf9531c06f5af28f7238d3f969cb5be44

freedreno: Add A605, A608, A610, A612 GPUs definition
While we tested a610, a605/a608 are added by observing traces being same
to a610.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20991>

src/freedreno/common/freedreno_devices.py
@@ -243,6 +243,14 @@ a6xx_gen1 = dict(
         depth_bounds_require_depth_test_quirk = True,
     )
 
+# a605, a608, a610, 612
+a6xx_gen1_low = {**a6xx_gen1, **dict(
+        has_gmem_fast_clear = False,
+        sysmem_per_ccu_cache_size = 8 * 1024,
+        gmem_ccu_color_cache_fraction = CCUColorCacheFraction.HALF.value,
+        vs_max_inputs_count = 16,
+)}
+
 # a640, a680:
 a6xx_gen2 = dict(
         fibers_per_sp = 128 * 4 * 16,
@@ -303,6 +311,36 @@ a6xx_gen4 = dict(
         has_lrz_dir_tracking = True,
         has_per_view_viewport = True,
     )
+
+add_gpus([
+        GPUId(605), # TODO: Test it, based only on libwrapfake dumps
+        GPUId(608), # TODO: Test it, based only on libwrapfake dumps
+        GPUId(610),
+        GPUId(612), # TODO: Test it, based only on libwrapfake dumps
+    ], A6xxGPUInfo(
+        CHIP.A6XX,
+        a6xx_gen1_low,
+        num_ccu = 1,
+        tile_align_w = 32,
+        tile_align_h = 16,
+        num_vsc_pipes = 16,
+        cs_shared_mem_size = 16 * 1024,
+        magic_regs = dict(
+            PC_POWER_CNTL = 0,
+            TPL1_DBG_ECO_CNTL = 0,
+            GRAS_DBG_ECO_CNTL = 0,
+            SP_CHICKEN_BITS = 0,
+            UCHE_CLIENT_PF = 0x00000004,
+            PC_MODE_CNTL = 0xf,
+            SP_DBG_ECO_CNTL = 0x0,
+            RB_DBG_ECO_CNTL = 0x04100000,
+            RB_DBG_ECO_CNTL_blit = 0x04100000,
+            HLSQ_DBG_ECO_CNTL = 0,
+            RB_UNKNOWN_8E01 = 0x00000001,
+            VPC_DBG_ECO_CNTL = 0x0,
+            UCHE_UNKNOWN_0E12 = 0x10000000,
+        ),
+    ))
 
 add_gpus([
         GPUId(615),

commit 29987ee5af74adff1a2b77ad2b427499c812b803

freedreno, tu: Limit XFB streams on GPUs lacking multi-stream query support
a6xx_gen1_low hardware does not support transform feedback queries on
streams > 0.

Also fix gallium's max_vertex_streams reporting for a702, which should be 1.

Fixes dEQP-VK.transform_feedback.primitives_generated_query.*.pgq_0_xfb_1.*
on a610.

Fixes: d2ce898cf95 ("freedreno: Add A605, A608, A610, A612 GPUs definition")
Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43947>

src/freedreno/common/freedreno_dev_info.h
@@ -310,6 +310,9 @@ struct fd_dev_info {
       /* A702 cuts A LOT of things.. */
       bool is_a702;
 
+      /* Number of transform feedback streams supported. */
+      uint32_t num_xfb_streams;
+
       /* maximum number of descriptor sets */
       uint32_t max_sets;
 

src/freedreno/common/freedreno_devices.py
@@ -135,6 +135,7 @@ a6xx_base = GPUProps(
         has_fs_tex_prefetch = True,
         has_sampler_minmax = True,
         has_astc_hdr = True,
+        num_xfb_streams = 4,
 
         supports_double_threadsize = True,
 
@@ -170,6 +171,7 @@ a6xx_gen1_low = GPUProps(
         has_sampler_minmax = False,
         has_astc_hdr = False,
         has_fs_tex_prefetch = False,
+        num_xfb_streams = 1,
         sysmem_per_ccu_color_cache_size = 8 * 1024,
         sysmem_per_ccu_depth_cache_size = 8 * 1024,
         gmem_ccu_color_cache_fraction = CCUColorCacheFraction.HALF.value,
@@ -778,6 +780,7 @@ a7xx_base = GPUProps(
         has_fs_tex_prefetch = True,
         has_sampler_minmax = True,
         has_astc_hdr = True,
+        num_xfb_streams = 4,
 
         supports_double_threadsize = True,
 

src/freedreno/vulkan/tu_device.cc
@@ -910,7 +910,7 @@ tu_get_features(struct tu_physical_device *pdevice,
 
    /* VK_EXT_transform_feedback */
    features->transformFeedback = true;
-   features->geometryStreams = !pdevice->info->props.is_a702;
+   features->geometryStreams = pdevice->info->props.num_xfb_streams > 1;
 
    /* VK_EXT_vertex_input_dynamic_state */
    features->vertexInputDynamicState = true;
@@ -1395,12 +1395,7 @@ tu_get_properties(struct tu_physical_device *pdevice,
    props->maxPushDescriptors = MAX_PUSH_DESCRIPTORS;
 
    /* VK_EXT_transform_feedback */
-   if (pdevice->info->props.is_a702) {
-       /* a702 only 32 streamout ram entries.. 1 stream, 64 components */
-      props->maxTransformFeedbackStreams = 1;
-   } else {
-      props->maxTransformFeedbackStreams = IR3_MAX_SO_STREAMS;
-   }
+   props->maxTransformFeedbackStreams = pdevice->info->props.num_xfb_streams;
    props->maxTransformFeedbackBuffers = IR3_MAX_SO_BUFFERS;
    props->maxTransformFeedbackBufferSize = UINT32_MAX;
    props->maxTransformFeedbackStreamDataSize = 512;

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -625,9 +625,7 @@ fd_init_screen_caps(struct fd_screen *screen)
       (is_a5xx(screen) ? 0 : 1);
 
    /* Stream output. */
-   caps->max_vertex_streams =
-      (is_a6xx(screen) && !screen->info->props.is_a702) ?  /* has SO + GS */
-         PIPE_MAX_SO_BUFFERS : 0;
+   caps->max_vertex_streams = screen->info->props.num_xfb_streams;
    caps->max_stream_output_buffers = is_ir3(screen) ? PIPE_MAX_SO_BUFFERS : 0;
    caps->stream_output_pause_resume =
    caps->stream_output_interleave_buffers =