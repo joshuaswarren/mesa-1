sailfishos-mirror/mesa

commit 8b77babdb2d9d323957f80cbc146f9d67ac4a8c5

r600/sfn: Avoid creating group-tagged registers for ALU dests
The scheduler expects that dest values that are marked as pin_group
are used as src values in some instruction that takes a vec4 as source,
otherwise the free channels in the vec4 group are not evaluated correctly.

Fix the extra instructions when lowering buf_txf to backend IR to use free
ALU dest registers.

Fixes: 13b1069a876 ("r600/sfn: Handle pre-EG buffer fetch")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15433

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
(cherry picked from commit b90baa07cbc38590685218c74936dc8b8eee055b)

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41985>

src/gallium/drivers/r600/sfn/sfn_instr_tex.cpp
@@ -577,50 +577,51 @@ bool
 TexInstr::emit_buf_txf(nir_tex_instr *tex, Inputs& src, Shader& shader)
 {
    auto& vf = shader.value_factory();
-   auto dst = vf.dest_vec4(tex->def, pin_group);
 
    PRegister tex_offset = nullptr;
    if (src.sampler_offset)
       tex_offset = shader.emit_load_to_register(src.sampler_offset);
 
-   auto *real_dst = &dst;
-   RegisterVec4 tmp = vf.temp_vec4(pin_group);
+   RegisterVec4 buf_dest = shader.chip_class() >= ISA_CC_EVERGREEN ?
+                         vf.dest_vec4(tex->def, pin_group) :
+                         vf.temp_vec4(pin_group);
 
-   if (shader.chip_class() < ISA_CC_EVERGREEN) {
-      real_dst = &tmp;
-   }
-
-   auto ir = new LoadFromBuffer(*real_dst,
+   auto ir = new LoadFromBuffer(buf_dest,
                                 {0, 1, 2, 3},
                                 src.coord[0],
                                 0,
                                 tex->texture_index + R600_MAX_CONST_BUFFERS,
                                 tex_offset,
                                 fmt_invalid);
+
    ir->set_fetch_flag(FetchInstr::use_const_field);
-   shader.emit_instruction(ir);
    shader.set_flag(Shader::sh_uses_tex_buffer);
+
+
+   shader.emit_instruction(ir);
 
    if (shader.chip_class() < ISA_CC_EVERGREEN) {
       auto tmp_w = vf.temp_register();
       int buf_sel = (512 + R600_BUFFER_INFO_OFFSET / 16) + 2 * tex->texture_index;
-      AluInstr *ir = nullptr;
       for (int i = 0; i < 4; ++i) {
-         auto d = i < 3 ? dst[i] : tmp_w;
-         ir = new AluInstr(op2_and_int,
-                           d,
-                           tmp[i],
-                           vf.uniform(buf_sel, i, R600_BUFFER_INFO_CONST_BUFFER),
-                           AluInstr::write);
-         shader.emit_instruction(ir);
-      }
+         auto dst  = vf.dest(tex->def, i, pin_free);
 
-      shader.emit_instruction(
-         new AluInstr(op2_or_int,
-                      dst[3],
-                      tmp_w,
-                      vf.uniform(buf_sel + 1, 0, R600_BUFFER_INFO_CONST_BUFFER),
-                      AluInstr::write));
+         auto d = i < 3 ? dst : tmp_w;
+         shader.emit_instruction(
+            new AluInstr(op2_and_int,
+                         d,
+                         buf_dest[i],
+                         vf.uniform(buf_sel, i, R600_BUFFER_INFO_CONST_BUFFER),
+                               AluInstr::write));
+
+         if (i == 3)
+            shader.emit_instruction(
+               new AluInstr(op2_or_int,
+                            dst,
+                            tmp_w,
+                            vf.uniform(buf_sel + 1, 0, R600_BUFFER_INFO_CONST_BUFFER),
+                            AluInstr::write));
+      }
    }
 
    return true;

joshuaswarren/mesa-1

commit 2501d07d7557924eac583e9d54b7cbc09171d6b9

r600/sfn: lower BUF txf in nir when on PRE-EG
By lowering this in nir we avoid the complications of usin the right
type of pinning when the values are used afterwards.

v2: Fix load_ubo4 args (pointed out by Copilot)

Fixes: 8b77babdb2d ("r600/sfn: Avoid creating group-tagged registers for ALU dests")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/15756

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43792>

src/gallium/drivers/r600/sfn/sfn_instr_tex.cpp
@@ -582,9 +582,7 @@ TexInstr::emit_buf_txf(nir_tex_instr *tex, Inputs& src, Shader& shader)
    if (src.sampler_offset)
       tex_offset = shader.emit_load_to_register(src.sampler_offset);
 
-   RegisterVec4 buf_dest = shader.chip_class() >= ISA_CC_EVERGREEN ?
-                         vf.dest_vec4(tex->def, pin_group) :
-                         vf.temp_vec4(pin_group);
+   RegisterVec4 buf_dest = vf.dest_vec4(tex->def, pin_group);
 
    auto ir = new LoadFromBuffer(buf_dest,
                                 {0, 1, 2, 3},
@@ -599,30 +597,6 @@ TexInstr::emit_buf_txf(nir_tex_instr *tex, Inputs& src, Shader& shader)
 
 
    shader.emit_instruction(ir);
-
-   if (shader.chip_class() < ISA_CC_EVERGREEN) {
-      auto tmp_w = vf.temp_register();
-      int buf_sel = R600_SHADER_BUFFER_INFO_SEL + 2 * tex->texture_index;
-      for (int i = 0; i < 4; ++i) {
-         auto dst  = vf.dest(tex->def, i, pin_free);
-
-         auto d = i < 3 ? dst : tmp_w;
-         shader.emit_instruction(
-            new AluInstr(op2_and_int,
-                         d,
-                         buf_dest[i],
-                         vf.uniform(buf_sel, i, R600_BUFFER_INFO_CONST_BUFFER),
-                               AluInstr::write));
-
-         if (i == 3)
-            shader.emit_instruction(
-               new AluInstr(op2_or_int,
-                            dst,
-                            tmp_w,
-                            vf.uniform(buf_sel + 1, 0, R600_BUFFER_INFO_CONST_BUFFER),
-                            AluInstr::write));
-      }
-   }
 
    return true;
 }
@@ -1259,5 +1233,49 @@ LowerTexToBackend::get_src_coords(nir_tex_instr *tex,
 
    return unnormalized_mask;
 }
+
+class LowerTxfOnPreEG : public NirLowerInstruction {
+
+private:
+   bool filter(const nir_instr *instr) const override;
+   nir_def *lower(nir_instr *instr) override;
+};
+
+bool
+LowerTxfOnPreEG::filter(const nir_instr *instr) const
+{
+   if (instr->type != nir_instr_type_tex)
+      return false;
+
+   auto tex = nir_instr_as_tex(instr);
+   return tex->sampler_dim == GLSL_SAMPLER_DIM_BUF &&
+          tex->op == nir_texop_txf;
+}
+
+nir_def *LowerTxfOnPreEG::lower(nir_instr *instr)
+{
+   auto tex = nir_instr_as_tex(instr);
+
+   auto buf_id = nir_imm_int(b, R600_BUFFER_INFO_CONST_BUFFER);
+   auto data_offset = 2 * tex->texture_index + R600_BUFFER_INFO_OFFSET / 16;
+
+   auto mask = nir_load_ubo_vec4(b, 4, 32, buf_id, nir_imm_int(b, data_offset));
+   auto mask2 = nir_load_ubo_vec4(b, 4, 32, buf_id, nir_imm_int(b, data_offset + 1));
+
+   auto part1 = nir_iand(b, &tex->def, mask);
+   auto part2 = nir_ior(b, nir_channel(b, part1, 3), nir_channel(b, mask2, 0));
+
+   return nir_vec4(b, nir_channel(b, part1, 0),
+                   nir_channel(b, part1, 1),
+                   nir_channel(b, part1, 2),
+                   part2);
+}
+
+bool
+r600_nir_lower_buf_txf_pre_eg(nir_shader *shader)
+{
+   return LowerTxfOnPreEG().run(shader);
+}
+
 
 } // namespace r600

src/gallium/drivers/r600/sfn/sfn_instr_tex.h
@@ -192,6 +192,8 @@ private:
 
 bool
 r600_nir_lower_tex_to_backend(nir_shader *shader, amd_gfx_level chip_class);
+bool
+r600_nir_lower_buf_txf_pre_eg(nir_shader *shader);
 
 } // namespace r600
 

src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -890,6 +890,10 @@ r600_lower_and_optimize_nir(nir_shader *sh,
    NIR_PASS(_, sh, nir_lower_phis_to_scalar, NULL, NULL);
    NIR_PASS(_, sh, nir_lower_alu_to_scalar, r600_lower_to_scalar_instr_filter, NULL);
    NIR_PASS(_, sh, r600_nir_lower_int_tg4);
+
+
+   if (gfx_level < EVERGREEN)
+      NIR_PASS(_, sh, r600::r600_nir_lower_buf_txf_pre_eg);
    NIR_PASS(_, sh, r600::r600_nir_lower_tex_to_backend, gfx_level);
 
    if (lower_64bit_io_to_vec2) {