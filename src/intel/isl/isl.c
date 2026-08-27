openharmony/third_party_mesa3d

commit e6edbecdef1c54e5360c6925322c0288b4fb7fce

isl: Add a helper for calculating subimage memory ranges
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3519>

src/intel/isl/isl.c
@@ -2514,6 +2514,56 @@ isl_surf_get_image_offset_B_tile_sa(const struct isl_surf *surf,
 }
 
 void
+isl_surf_get_image_range_B_tile(const struct isl_surf *surf,
+                                uint32_t level,
+                                uint32_t logical_array_layer,
+                                uint32_t logical_z_offset_px,
+                                uint32_t *start_tile_B,
+                                uint32_t *end_tile_B)
+{
+   uint32_t start_x_offset_el, start_y_offset_el;
+   isl_surf_get_image_offset_el(surf, level, logical_array_layer,
+                                logical_z_offset_px,
+                                &start_x_offset_el,
+                                &start_y_offset_el);
+
+   /* Compute the size of the subimage in surface elements */
+   const uint32_t subimage_w_sa = isl_minify(surf->phys_level0_sa.w, level);
+   const uint32_t subimage_h_sa = isl_minify(surf->phys_level0_sa.h, level);
+   const struct isl_format_layout *fmtl = isl_format_get_layout(surf->format);
+   const uint32_t subimage_w_el = isl_align_div_npot(subimage_w_sa, fmtl->bw);
+   const uint32_t subimage_h_el = isl_align_div_npot(subimage_h_sa, fmtl->bh);
+
+   /* Find the last pixel */
+   uint32_t end_x_offset_el = start_x_offset_el + subimage_w_el - 1;
+   uint32_t end_y_offset_el = start_y_offset_el + subimage_h_el - 1;
+
+   UNUSED uint32_t x_offset_el, y_offset_el;
+   isl_tiling_get_intratile_offset_el(surf->tiling, fmtl->bpb,
+                                      surf->row_pitch_B,
+                                      start_x_offset_el,
+                                      start_y_offset_el,
+                                      start_tile_B,
+                                      &x_offset_el,
+                                      &y_offset_el);
+
+   isl_tiling_get_intratile_offset_el(surf->tiling, fmtl->bpb,
+                                      surf->row_pitch_B,
+                                      end_x_offset_el,
+                                      end_y_offset_el,
+                                      end_tile_B,
+                                      &x_offset_el,
+                                      &y_offset_el);
+
+   /* We want the range we return to be exclusive but the tile containing the
+    * last pixel (what we just calculated) is inclusive.  Add one.
+    */
+   (*end_tile_B)++;
+
+   assert(*end_tile_B <= surf->size_B);
+}
+
+void
 isl_surf_get_image_surf(const struct isl_device *dev,
                         const struct isl_surf *surf,
                         uint32_t level,

src/intel/isl/isl.h
@@ -2105,6 +2105,27 @@ isl_surf_get_image_offset_B_tile_sa(const struct isl_surf *surf,
                                     uint32_t *y_offset_sa);
 
 /**
+ * Calculate the range in bytes occupied by a subimage, to the nearest tile.
+ *
+ * The range returned will be the smallest memory range in which the give
+ * subimage fits, rounded to even tiles.  Intel images do not usually have a
+ * direct subimage -> range mapping so the range returned may contain data
+ * from other sub-images.  The returned range is a half-open interval where
+ * all of the addresses within the subimage are < end_tile_B.
+ *
+ * @invariant level < surface levels
+ * @invariant logical_array_layer < logical array length of surface
+ * @invariant logical_z_offset_px < logical depth of surface at level
+ */
+void
+isl_surf_get_image_range_B_tile(const struct isl_surf *surf,
+                                uint32_t level,
+                                uint32_t logical_array_layer,
+                                uint32_t logical_z_offset_px,
+                                uint32_t *start_tile_B,
+                                uint32_t *end_tile_B);
+
+/**
  * Create an isl_surf that represents a particular subimage in the surface.
  *
  * The newly created surface will have a single miplevel and array slice.  The

joshuaswarren/mesa-1

commit 8abfb7ae71e095b52cf4e1a4c3bd1bdd55940f83

intel/isl: Support MSAA in subimage range helper
Prevents failures in a future commit with many multisampling tests,
including piglit's:

   arb_framebuffer_srgb-blit renderbuffer linear_to_srgb upsample
   disabled clear -auto -fbo

and dEQP's:

   dEQP-VK.pipeline.monolithic.multisample_with_fragment_shading_rate.
   std_sample_locations.draw.depth.samples_8.
   separate_renderpass_clear_image_same_pattern_secondary_cmd_buf_general_layout

Fixes: e6edbecdef1 ("isl: Add a helper for calculating subimage memory ranges")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39593>

src/intel/isl/isl.c
@@ -5335,16 +5335,32 @@ isl_tiling_get_intratile_range_el(enum isl_tiling tiling,
                                       z_offset_el,
                                       array_offset);
 
+   /* Find the last element */
+   uint32_t end_x_offset_el = total_x_offset_el + total_extent_el.w - 1;
+   uint32_t end_y_offset_el = total_y_offset_el + total_extent_el.h - 1;
+   uint32_t end_z_offset_el = total_z_offset_el + total_extent_el.d - 1;
+   uint32_t end_a_offset_el = total_array_offset + total_extent_el.a - 1;
+
+   struct isl_tile_info tile_info;
+   isl_tiling_get_info(tiling, dim, msaa_layout, bpb, samples, &tile_info);
+   if (msaa_layout == ISL_MSAA_LAYOUT_ARRAY) {
+      if (tile_info.logical_extent_el.a > 1)
+         end_a_offset_el += samples - 1;
+      else
+         end_y_offset_el += (samples - 1) * array_pitch_el_rows;
+   }
+
+
    UNUSED uint32_t _x_offset_el, _y_offset_el, _z_offset_el, _array_slice;
    isl_tiling_get_intratile_offset_el(tiling, dim,
                                       msaa_layout, bpb,
                                       samples,
                                       row_pitch_B,
                                       array_pitch_el_rows,
-                                      total_x_offset_el + total_extent_el.w - 1,
-                                      total_y_offset_el + total_extent_el.h - 1,
-                                      total_z_offset_el + total_extent_el.d - 1,
-                                      total_array_offset + total_extent_el.a - 1,
+                                      end_x_offset_el,
+                                      end_y_offset_el,
+                                      end_z_offset_el,
+                                      end_a_offset_el,
                                       end_offset_B,
                                       &_x_offset_el,
                                       &_y_offset_el,