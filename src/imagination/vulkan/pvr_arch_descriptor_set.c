joshuaswarren/mesa-1

commit 589bdfd8a4b607475ed616ee556d95c8085758bd

pvr: handle plane addresses for ycbcr images.
Reviewed-by: Simon Perretta <simon.perretta@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39231>

src/imagination/vulkan/pvr_arch_descriptor_set.c
@@ -120,6 +120,13 @@ write_image_sampler(const struct pvr_descriptor_set *set,
       VK_FROM_HANDLE(pvr_image_view, image_view, image_info->imageView);
       image_sampler_desc.image =
          image_view->image_state[PVR_TEXTURE_STATE_SAMPLE];
+      if (image_view->sampler_words[0]) {
+         for (unsigned i = 0; i < ROGUE_NUM_TEXSTATE_SAMPLER_WORDS; i++) {
+            image_sampler_desc.sampler.words[i] |= image_view->sampler_words[i];
+            image_sampler_desc.sampler.gather_words[i] |=
+               image_view->sampler_words[i];
+         }
+      }
    }
 
    memcpy(desc_mapping, &image_sampler_desc, sizeof(image_sampler_desc));
@@ -156,6 +163,9 @@ write_sampled_image(const struct pvr_descriptor_set *set,
 
    struct pvr_image_descriptor sampled_image_desc =
       image_view->image_state[PVR_TEXTURE_STATE_SAMPLE];
+
+   /* YCbCr must use combined image smapler */
+   assert(!image_view->sampler_words[0] && !image_view->sampler_words[1]);
 
    memcpy(desc_mapping, &sampled_image_desc, sizeof(sampled_image_desc));
 }

src/imagination/vulkan/pvr_arch_image.c
@@ -13,6 +13,7 @@
 #include "vk_ycbcr_conversion.h"
 
 #include "pvr_buffer.h"
+#include "pvr_csb.h"
 #include "pvr_device.h"
 #include "pvr_entrypoints.h"
 #include "pvr_macros.h"
@@ -124,6 +125,17 @@ VkResult PVR_PER_ARCH(CreateImageView)(VkDevice _device,
 
       if (conversion->state.mapping[3] == VK_COMPONENT_SWIZZLE_ZERO) {
          input_swizzle[3] = PIPE_SWIZZLE_0;
+      }
+
+      pvr_csb_pack (&iview->sampler_words[0], TEXSTATE_SAMPLER_WORD0, cfg) {
+         cfg.texaddr_plane2_lo =
+            PVR_DEV_ADDR_OFFSET(image->dev_addr, image->planes[1].offset);
+      }
+      pvr_csb_pack (&iview->sampler_words[1], TEXSTATE_SAMPLER_WORD1, cfg) {
+         cfg.texaddr_plane2_hi =
+            PVR_DEV_ADDR_OFFSET(image->dev_addr, image->planes[1].offset);
+         cfg.texaddr_plane3 =
+            PVR_DEV_ADDR_OFFSET(image->dev_addr, image->planes[2].offset);
       }
    } else {
       vk_component_mapping_to_pipe_swizzle(iview->vk.swizzle, input_swizzle);

src/imagination/vulkan/pvr_arch_sampler.c
@@ -190,9 +190,29 @@ VkResult PVR_PER_ARCH(CreateSampler)(VkDevice _device,
 
       if (pCreateInfo->unnormalizedCoordinates)
          word.non_normalized_coords = true;
+
+      /* These fields overlap with plane addresses on rogue */
+      if (sampler->vk.ycbcr_conversion) {
+         word.bordercolor_index = 0;
+         word.addrmode_w = ROGUE_TEXSTATE_ADDRMODE_REPEAT;
+      }
    }
 
    pvr_csb_pack (&sampler->descriptor.words[1], TEXSTATE_SAMPLER_WORD1, word) {}
+
+#ifndef NDEBUG
+   /* YUV plane addresses are stored in the sampler on rogue. We need to add
+    * them whilst writing descriptor sets. We assert here that none of the
+    * bits that overlap with plane addresses are set.
+    */
+   if (sampler->vk.ycbcr_conversion) {
+      uint64_t yuv_word0_addr_mask = 0;
+      pvr_csb_pack (&yuv_word0_addr_mask, TEXSTATE_SAMPLER_WORD0, word) {
+         word.texaddr_plane2_lo = PVR_DEV_ADDR(~0);
+      }
+      assert(!(sampler->descriptor.words[0] & yuv_word0_addr_mask));
+   }
+#endif
 
    /* Setup gather sampler. */
 

src/imagination/vulkan/pvr_image.h
@@ -139,6 +139,9 @@ struct pvr_image_view {
     * attachment cases.
     */
    struct pvr_image_descriptor image_state[PVR_TEXTURE_STATE_MAX_ENUM];
+
+   /* Prepacked Sampler Words with YCbCr plane addresses */
+   uint64_t sampler_words[ROGUE_NUM_TEXSTATE_SAMPLER_WORDS];
 };
 
 VK_DEFINE_NONDISP_HANDLE_CASTS(pvr_image, vk.base, VkImage, VK_OBJECT_TYPE_IMAGE)

commit 6555e43146c0d8628316898d73e070ad64ed2041

pvr: set ycbcr sampler state words when any sampler word is present
The code copying extra sampler state word fields currently only checks
whether the 1st sampler state word has any extra bits set. However, as
the only field to be set in such way is bit [15:2] of the 2nd plane
address, when these bits are all zero setting of extra sampler state
word fields are all skipped, which leads to the high bits of the 2nd
plane address and all of the 3rd plane address being not properly set.

Fix this by checking all extra sampler state word values before copying.

Fixes: 589bdfd8a4b6 ("pvr: handle plane addresses for ycbcr images.")
Signed-off-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
Reviewed-by: Frank Binns <frank.binns@imgtec.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44085>

src/imagination/vulkan/pvr_arch_descriptor_set.c
@@ -123,7 +123,16 @@ write_image_sampler(const struct pvr_descriptor_set *set,
       VK_FROM_HANDLE(pvr_image_view, image_view, image_info->imageView);
       image_sampler_desc.image =
          image_view->image_state[PVR_TEXTURE_STATE_SAMPLE];
-      if (image_view->sampler_words[0]) {
+
+      bool sampler_words_present = false;
+      for (unsigned i = 0; i < ROGUE_NUM_TEXSTATE_SAMPLER_WORDS; i++) {
+         if (image_view->sampler_words[i]) {
+            sampler_words_present = true;
+            break;
+         }
+      }
+
+      if (sampler_words_present) {
          for (unsigned i = 0; i < ROGUE_NUM_TEXSTATE_SAMPLER_WORDS; i++) {
             image_sampler_desc.sampler.words[i] |= image_view->sampler_words[i];
             image_sampler_desc.sampler.gather_words[i] |=