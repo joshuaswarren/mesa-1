joshuaswarren/mesa-1

commit 6048b449a406602a414580d35aaad6c8a4d21457

r600/sfn: handle nir_op_fadd_rtne
Treat it as just fadd to maintain the same behavior, as the compiler
doesn't seem to care about the shader rounding mode.

Signed-off-by: Iván Briano <ivan.briano@intel.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43285>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -1639,6 +1639,7 @@ AluInstr::from_nir(nir_alu_instr *alu, Shader& shader)
          return emit_alu_fma_64bit(*alu, op3_fma_64, shader);
 
       case nir_op_fadd:
+      case nir_op_fadd_rtne:
          return emit_alu_op2_64bit(*alu, op2_add_64, shader);
       case nir_op_fmul:
          return emit_alu_op2_64bit(*alu, op2_mul_64, shader);

commit 461cff4b94944b3af1eafbc1368eedb140bba10b

r600/sfn: replace fadd_rtne with fadd
The opcode needs to be handle in other locations too, which wasn't
done with 6048b449a40. However, by overriding the opcode, later
lowering passes handle this correctly, and the driver is set to
round_even anyway.

Also revert "r600/sfn: handle nir_op_fadd_rtne" because it is
no longer needed.

Fixes: 6048b449a40 ("r600/sfn: handle nir_op_fadd_rtne")

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44108>

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -1639,7 +1639,6 @@ AluInstr::from_nir(nir_alu_instr *alu, Shader& shader)
          return emit_alu_fma_64bit(*alu, op3_fma_64, shader);
 
       case nir_op_fadd:
-      case nir_op_fadd_rtne:
          return emit_alu_op2_64bit(*alu, op2_add_64, shader);
       case nir_op_fmul:
          return emit_alu_op2_64bit(*alu, op2_mul_64, shader);

src/gallium/drivers/r600/sfn/sfn_nir_algebraic.py
@@ -49,6 +49,8 @@ lower_alu = [
 
    (('seq', ('fadd', 'a', 'b'), 0.0), ('seq', 'a', ('fneg', 'b'))),
    (('sne', ('fadd', 'a', 'b'), 0.0), ('sne', 'a', ('fneg', 'b'))),
+
+   (('fadd_rtne', 'a', 'b'), ('fadd', 'a', 'b')),
 ]
 
 