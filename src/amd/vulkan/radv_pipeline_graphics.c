joshuaswarren/mesa-1

commit 7da5c3cf955071ccc1b24cf1081f7d47a7ed69d7

radv: disallow force_vrs_per_vertex with FragCoord when using GPL & ESO
force_vrs_per_vertex with FragCoord was only disallowed without GPL & ESO.
This extends it to all cases by making it dynamic.

The FragCoord usage is moved to shader info, which is then used in the VRS
override state or checked after shader info gathering.

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/42242>

src/amd/vulkan/radv_pipeline_graphics.c
@@ -1929,15 +1929,6 @@ radv_consider_force_vrs(const struct radv_graphics_state_key *gfx_state, const s
    if (last_vgt_stage->info.next_stage == MESA_SHADER_NONE)
       return false;
 
-   /* Do not enable if the PS uses gl_FragCoord because it breaks postprocessing in some games. */
-   nir_shader *fs_shader = fs_stage->nir;
-   if (fs_shader && (BITSET_TEST(fs_shader->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_XY) ||
-                     BITSET_TEST(fs_shader->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_Z) ||
-                     BITSET_TEST(fs_shader->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_W_RCP) ||
-                     BITSET_TEST(fs_shader->info.system_values_read, SYSTEM_VALUE_PIXEL_COORD))) {
-      return false;
-   }
-
    return true;
 }
 
@@ -2692,6 +2683,8 @@ radv_graphics_shaders_compile(const struct radv_compiler_info *compiler_info, st
             stages[i].nir->info.outputs_written &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
             stages[i].nir->info.per_primitive_outputs &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
          }
+      } else if (fs_stage && fs_stage->info.ps.disallow_force_vrs_per_vertex) {
+         stages[i].info.force_vrs_per_vertex = false;
       }
       break;
    }

src/amd/vulkan/radv_cmd_buffer.c
@@ -3974,10 +3974,11 @@ radv_emit_override_vrs_state(struct radv_cmd_buffer *cmd_buffer)
                                        S_028848_VERTEX_RATE_COMBINER_MODE(V_028848_SC_VRS_COMB_MODE_OVERRIDE));
          radeon_end();
 
-         /* If the shader is using discard, turn off coarse shading because discard at 2x2 pixel
-          * granularity degrades quality too much. MIN allows sample shading but not coarse shading.
+         /* If PS doesn't allow it, turn it off, which we must do by setting MIN because the VRS rate
+          * shader output is present. The behavior of MIN is explained above.
           */
-         mode = ps && ps->info.ps.can_discard ? V_028064_SC_VRS_COMB_MODE_MIN : V_028064_SC_VRS_COMB_MODE_PASSTHRU;
+         mode = ps && ps->info.ps.disallow_force_vrs_per_vertex ? V_028064_SC_VRS_COMB_MODE_MIN
+                                                                : V_028064_SC_VRS_COMB_MODE_PASSTHRU;
       }
 
       radeon_begin(cs);

src/amd/vulkan/radv_shader_info.c
@@ -883,6 +883,14 @@ gather_shader_info_fs(enum amd_gfx_level gfx_level, const nir_shader *nir,
          (info->ps.reads_sample_mask_in && !info->ps.needs_poly_line_smooth) ||
          (gfx_level == GFX10_3 && (nir->info.fs.sample_interlock_ordered || nir->info.fs.sample_interlock_unordered ||
                                    nir->info.fs.pixel_interlock_ordered || nir->info.fs.pixel_interlock_unordered));
+
+      /* Do not enable if the PS uses gl_FragCoord because it breaks postprocessing in some games. */
+      info->ps.disallow_force_vrs_per_vertex =
+         gfx_state->ps.force_vrs_enabled &&
+         (info->ps.can_discard || BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_XY) ||
+          BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_Z) ||
+          BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_FRAG_COORD_W_RCP) ||
+          BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PIXEL_COORD));
    }
 }
 

src/amd/vulkan/radv_shader_info.h
@@ -213,6 +213,7 @@ struct radv_shader_info {
       bool load_rasterization_prim : 1;
       bool force_disable_vrs : 1;
       bool allow_flat_shading : 1;
+      bool disallow_force_vrs_per_vertex : 1;
       bool selects_frag_coord_xy_dynamically : 1;
       bool selects_quad_pos_dynamically : 1;
       bool selects_sample_mask_in_dynamically : 1;

commit 80aca79e4770fcf3c26c9867e06d874292a3e261

radv: fix RADV_FORCE_VRS (Persona 3 Reload)
This field affects register programming and shouldn't be set when there is
no per-vertex VRS export.

Fixes: 7da5c3cf955 - radv: disallow force_vrs_per_vertex with FragCoord when using GPL & ESO
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16187

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44102>

src/amd/vulkan/radv_pipeline_graphics.c
@@ -2743,8 +2743,12 @@ radv_graphics_shaders_compile(const struct radv_compiler_info *compiler_info, st
             stages[i].nir->info.outputs_written &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
             stages[i].nir->info.per_primitive_outputs &= ~VARYING_BIT_PRIMITIVE_SHADING_RATE;
          }
-      } else if (fs_stage && fs_stage->info.ps.disallow_force_vrs_per_vertex) {
+      } else if (fs_stage && fs_stage->info.ps.disallow_force_vrs_per_vertex && stages[i].info.force_vrs_per_vertex) {
          stages[i].info.force_vrs_per_vertex = false;
+         stages[i].info.outinfo.writes_primitive_shading_rate = false;
+
+         assert(!(stages[i].nir->info.outputs_written & ~stages[i].nir->info.per_primitive_outputs &
+                  VARYING_BIT_PRIMITIVE_SHADING_RATE));
       }
       break;
    }