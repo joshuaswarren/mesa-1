joshuaswarren/mesa-1

commit d6b70cf5d91f16e1f3660f9b2956237400f50dc7

loader: check if the kernel driver is amdgpu
This allows using drm-shim for an emulated driver in AMD GPU host.

Otherwise we need to set MESA_LOADER_DRIVER_OVERRIDE to the emulated
driver in order to make it working.

Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
Acked-by: Iago Toral Quiroga <itoral@igalia.com>
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41779>

src/loader/loader.c
@@ -125,6 +125,16 @@ loader_get_kernel_driver_name(int fd)
 }
 
 bool
+amd_predicate(int fd, const char *driver)
+{
+   char *kernel_driver = loader_get_kernel_driver_name(fd);
+   bool ret = kernel_driver && (strcmp(kernel_driver, "amdgpu") == 0);
+
+   free(kernel_driver);
+   return ret;
+}
+
+bool
 iris_predicate(int fd, const char *driver)
 {
    char *kernel_driver = loader_get_kernel_driver_name(fd);

src/loader/pci_id_driver_map.h
@@ -44,6 +44,7 @@ static const int vmwgfx_chip_ids[] = {
 #undef CHIPSET
 };
 
+bool amd_predicate(int fd, const char *driver);
 bool iris_predicate(int fd, const char *driver);
 bool nouveau_zink_predicate(int fd, const char *driver);
 
@@ -59,7 +60,7 @@ static const struct {
    { 0x8086, "iris", NULL, -1, iris_predicate },
    { 0x1002, "r300", r300_chip_ids, ARRAY_SIZE(r300_chip_ids) },
    { 0x1002, "r600", r600_chip_ids, ARRAY_SIZE(r600_chip_ids) },
-   { 0x1002, "radeonsi", NULL, -1 },
+   { 0x1002, "radeonsi", NULL, -1, amd_predicate },
    { 0x10de, "nouveau", NULL, -1, nouveau_zink_predicate },
    { 0x10de, "zink", NULL, -1, nouveau_zink_predicate },
    { 0x1af4, "virtio_gpu", virtio_gpu_chip_ids, ARRAY_SIZE(virtio_gpu_chip_ids) },

commit 25e1859994a8e9adf790dc7bdff7c570922c58fd

loader: consider radeon in amd_predicate
Otherwise it'll break radeonsi on radeon.

Fixes: d6b70cf5d91 ("loader: check if the kernel driver is amdgpu")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/work_items/16186
Reviewed-by: Benjamin Cheng <benjamin.cheng@amd.com>
Reviewed-by: Marek Olšák <maraeo@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44052>

src/loader/loader.c
@@ -128,7 +128,8 @@ bool
 amd_predicate(int fd, const char *driver)
 {
    char *kernel_driver = loader_get_kernel_driver_name(fd);
-   bool ret = kernel_driver && (strcmp(kernel_driver, "amdgpu") == 0);
+   bool ret = kernel_driver &&
+              (strcmp(kernel_driver, "amdgpu") == 0 || strcmp(kernel_driver, "radeon") == 0);
 
    free(kernel_driver);
    return ret;