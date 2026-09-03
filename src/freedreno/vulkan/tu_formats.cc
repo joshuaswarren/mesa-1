joshuaswarren/mesa-1

commit 9334d3b3b89f9da016e84fe5a188224b2cc8ab57

tu: Support transfer commands for R64 formats
CTS tests that require transfer commands:
   dEQP-VK.robustness.*r64*
   dEQP-VK.api.image_clearing.*.r64*
   dEQP-VK.image.extend_operands_spirv1p4.r64_*
   dEQP-VK.image.nontemporal_operand.r64_*
   dEQP-VK.image.queue_transfer.*.r64_

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39932>

src/freedreno/vulkan/tu_formats.cc
@@ -144,12 +144,16 @@ tu_physical_device_get_format_properties(
        physical_device->info->props.has_64b_image_atomics) {
       assert(!(supported_vtx || supported_color || supported_tex));
       optimal = VK_FORMAT_FEATURE_2_STORAGE_IMAGE_BIT |
-                VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
+                VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT |
+                VK_FORMAT_FEATURE_TRANSFER_SRC_BIT |
+                VK_FORMAT_FEATURE_TRANSFER_DST_BIT;
       out_properties->linearTilingFeatures = optimal;
       out_properties->optimalTilingFeatures = optimal;
       out_properties->bufferFeatures =
          VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_BIT |
-         VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_ATOMIC_BIT;
+         VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_ATOMIC_BIT |
+         VK_FORMAT_FEATURE_TRANSFER_SRC_BIT |
+         VK_FORMAT_FEATURE_TRANSFER_DST_BIT;
       return;
    }
 

src/freedreno/vulkan/tu_clear_blit.cc
@@ -2005,6 +2005,10 @@ copy_format(VkFormat vk_format, VkImageAspectFlags aspect_mask)
        vk_format_get_ycbcr_info(vk_format))
       return tu_aspects_to_plane(vk_format, aspect_mask);
 
+   if (vk_format == VK_FORMAT_R64_SINT || vk_format == VK_FORMAT_R64_UINT) {
+      return PIPE_FORMAT_R32G32_UINT;
+   }
+
    /* Otherwise, simply return the pipe_format */
    return format;
 }
@@ -3964,6 +3968,9 @@ clear_image_event_blit(struct tu_cmd_buffer *cmd,
    }
 
    enum pipe_format format = vk_format_to_pipe_format(vk_format);
+   if (format == PIPE_FORMAT_R64_SINT || format == PIPE_FORMAT_R64_UINT) {
+      format = PIPE_FORMAT_R32G32_UINT;
+   }
 
    if (image->layout[0].depth0 > 1) {
       assert(layer_count == 1);

commit 5da874cecd4f11f75c97578cbeaac0d67c04089d

tu: Drop TRANSFER_SRC/DST from bufferFeatures.
It's not in the list of valid bits for VkFormatFeatureFlagBits2 as a
bufferFeatures.  A quick check of the CTS didn't show anything obviously
looking for these flags in bufferFeatures.

Fixes: 9334d3b3b89f ("tu: Support transfer commands for R64 formats")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44152>

src/freedreno/vulkan/tu_formats.cc
@@ -151,9 +151,7 @@ tu_physical_device_get_format_properties(
       out_properties->optimalTilingFeatures = optimal;
       out_properties->bufferFeatures =
          VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_BIT |
-         VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_ATOMIC_BIT |
-         VK_FORMAT_FEATURE_TRANSFER_SRC_BIT |
-         VK_FORMAT_FEATURE_TRANSFER_DST_BIT;
+         VK_FORMAT_FEATURE_2_STORAGE_TEXEL_BUFFER_ATOMIC_BIT;
       return;
    }
 
@@ -167,10 +165,6 @@ tu_physical_device_get_format_properties(
    /* We never have to spill to memory for MSRTSS. */
    if (msrtss_out)
       msrtss_out->optimal = true;
-
-   /* We don't support BufferToImage/ImageToBuffer for npot formats */
-   if (!is_npot)
-      buffer |= VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT | VK_FORMAT_FEATURE_2_TRANSFER_DST_BIT;
 
    if (supported_vtx)
       buffer |= VK_FORMAT_FEATURE_2_VERTEX_BUFFER_BIT;