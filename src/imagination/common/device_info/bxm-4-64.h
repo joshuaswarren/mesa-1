joshuaswarren/mesa-1

commit 8b04e7ecf09f132d9dc8fb5db13574946269f1b0

pvr: store arch in device-info
Right now all supported devices are Rogue devices. But in the future,
we're going to support multiple architectures. So let's add a way to
query this at run-time.

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38423>

src/imagination/common/device_info/bxm-4-64.h
@@ -12,7 +12,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_36_V_104_182 \
-   .device_id = 0x36104182, .series_name = "B-Series", .public_name = "BXM-4-64"
+   .device_id = 0x36104182, .series_name = "B-Series", \
+   .public_name = "BXM-4-64", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_36_V_104_182 = {
    .has_astc = true,

src/imagination/common/device_info/ge8300.h
@@ -12,7 +12,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_22_V_54_30 \
-   .device_id = 0x22054030, .series_name = "Rogue", .public_name = "GE8300"
+   .device_id = 0x22054030, .series_name = "Rogue", \
+   .public_name = "GE8300", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_22_V_54_30 = {
    .has_astc = true,

src/imagination/common/device_info/gx6250.h
@@ -29,7 +29,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_4_V_2_51 \
-   .device_id = 0x6250, .series_name = "Rogue", .public_name = "GX6250"
+   .device_id = 0x6250, .series_name = "Rogue", \
+   .public_name = "GX6250", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_4_V_2_51 = {
    .has_astc = true,

src/imagination/common/device_info/axe-1-16m.h
@@ -30,7 +30,8 @@
 
 #define PVR_DEVICE_IDENT_33_V_11_3                     \
    .device_id = 0x33011003, .series_name = "A-Series", \
-   .public_name = "AXE-1-16M"
+   .public_name = "AXE-1-16M", .arch = PVR_DEVICE_ARCH_ROGUE
+
 
 static const struct pvr_device_features pvr_device_features_33_V_11_3 = {
    .has_common_store_size_in_dwords = true,

src/imagination/common/device_info/bxe-2-32.h
@@ -12,7 +12,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_36_V_52_182 \
-   .device_id = 0x36052182, .series_name = "B-Series", .public_name = "BXE-2-32"
+   .device_id = 0x36052182, .series_name = "B-Series", \
+   .public_name = "BXE-2-32", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_36_V_52_182 = {
    .has_astc = true,

src/imagination/common/device_info/bxe-4-32.h
@@ -12,7 +12,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_36_V_54_182 \
-   .device_id = 0x36054182, .series_name = "B-Series", .public_name = "BXE-4-32"
+   .device_id = 0x36054182, .series_name = "B-Series", \
+   .public_name = "BXE-4-32", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_36_V_54_182 = {
    .has_astc = true,

src/imagination/common/device_info/bxs-4-64.h
@@ -29,7 +29,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_36_V_104_796 \
-   .device_id = 0x36104796, .series_name = "B-Series", .public_name = "BXS-4-64"
+   .device_id = 0x36104796, .series_name = "B-Series", \
+   .public_name = "BXS-4-64", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_36_V_104_796 = {
    .has_astc = true,

src/imagination/common/device_info/g6110.h
@@ -12,7 +12,8 @@
 #include "pvr_device_info.h"
 
 #define PVR_DEVICE_IDENT_5_V_1_46 \
-   .device_id = 0x6110, .series_name = "Rogue", .public_name = "GX6110"
+   .device_id = 0x6110, .series_name = "Rogue", \
+   .public_name = "GX6110", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_5_V_1_46 = {
    .has_common_store_size_in_dwords = true,

commit 0adc6ccce988eab23c8f7d56078fc133ea15a1b2

pvr: add missing GPU architecture to device info for some BVNCs
In the cases where there's device info for multiple BVNCs in a single header
file, the GPU architecture was only set for the first BVNC to appear in the
file. Adding this missing information for the other BVNCs.

Fixes: 8b04e7ecf09 ("pvr: store arch in device-info")
Signed-off-by: Frank Binns <frank.binns@imgtec.com>
Reviewed-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44091>

src/imagination/common/device_info/bxm-4-64.h
@@ -125,7 +125,8 @@ static const struct pvr_device_info pvr_device_info_36_52_104_182 = {
 };
 
 #define PVR_DEVICE_IDENT_36_V_104_183 \
-   .device_id = 0x36104183, .series_name = "B-Series", .public_name = "BXM-4-64"
+   .device_id = 0x36104183, .series_name = "B-Series", \
+   .public_name = "BXM-4-64", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_36_V_104_183 = {
    .has_astc = true,

src/imagination/common/device_info/ge8300.h
@@ -135,7 +135,8 @@ static const struct pvr_device_info pvr_device_info_22_68_54_30 = {
 };
 
 #define PVR_DEVICE_IDENT_22_V_54_38 \
-   .device_id = 0x22054038, .series_name = "Rogue", .public_name = "GE8300"
+   .device_id = 0x22054038, .series_name = "Rogue", \
+   .public_name = "GE8300", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_22_V_54_38 = {
    .has_astc = true,

src/imagination/common/device_info/gx6250.h
@@ -141,7 +141,8 @@ static const struct pvr_device_info pvr_device_info_4_40_2_51 = {
 };
 
 #define PVR_DEVICE_IDENT_4_V_2_58 \
-   .device_id = 0x6250, .series_name = "Rogue", .public_name = "GX6250"
+   .device_id = 0x6250, .series_name = "Rogue", \
+   .public_name = "GX6250", .arch = PVR_DEVICE_ARCH_ROGUE
 
 static const struct pvr_device_features pvr_device_features_4_V_2_58 = {
    .has_astc = true,