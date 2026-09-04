joshuaswarren/mesa-1

commit d1dc00919ff79f97d432b2817c33ab322637d32e

brw: support surface index in efficient 64bit mode
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43926>

src/intel/compiler/brw/brw_lower.cpp
@@ -871,6 +871,8 @@ brw_lower_send_gather_inst(brw_shader &s, brw_send_inst *inst)
       assert(nr <= UINT8_MAX);
       regs[count++] = nr;
    }
+   fprintf(stderr, "count=%u num_payload_sources=%u\n",
+           count, num_payload_sources);
 
    /* Fill out ARF scalar register with the physical register numbers
     * and use SEND_GATHER.

src/intel/compiler/brw/brw_from_nir.cpp
@@ -1838,6 +1838,12 @@ get_nir_src_internal(nir_to_brw_state &ntb, const nir_src &src)
    return ntb.ssa_bind_infos[src.ssa->index].internal;
 }
 
+static unsigned
+get_nir_src_block(nir_to_brw_state &ntb, const nir_src &src)
+{
+   return ntb.ssa_bind_infos[src.ssa->index].block;
+}
+
 /**
  * Specifying -1 for channel indicates that no channel selection should be applied.
  */
@@ -5764,6 +5770,8 @@ brw_from_nir_emit_memory_access(nir_to_brw_state &ntb,
 
    int32_t address_offset = 0;
 
+   uint8_t surface_index = 0;
+
    std::optional<memory_logical_mode> mode;
    std::optional<lsc_addr_surface_type> binding_type;
 
@@ -5802,6 +5810,8 @@ brw_from_nir_emit_memory_access(nir_to_brw_state &ntb,
 
       if (!binding_type.has_value())
          binding_type = LSC_ADDR_SURFTYPE_BTI;
+      if (s.key->use_efficient_64bit)
+         surface_index = get_nir_src_block(ntb, instr->src[0]);
 
       /* Payload lowering uses MEMORY_LOGICAL_ADDRESS as a vector, so don't
        * select a channel here.
@@ -5830,6 +5840,8 @@ brw_from_nir_emit_memory_access(nir_to_brw_state &ntb,
          LSC_ADDR_SURFTYPE_BTI;
       srcs[MEMORY_LOGICAL_BINDING] =
          get_nir_buffer_intrinsic_index(ntb, bld, instr, &no_mask_handle);
+      if (s.key->use_efficient_64bit)
+         surface_index = get_nir_src_block(ntb, instr->src[is_store ? 1 : 0]);
       srcs[MEMORY_LOGICAL_ADDRESS] =
          memory_address(ntb, bld, instr, *binding_type, &address_offset);
       break;
@@ -5990,6 +6002,7 @@ brw_from_nir_emit_memory_access(nir_to_brw_state &ntb,
       mem->components = components;
       mem->alignment = alignment;
       mem->flags = flags;
+      mem->surface_index = surface_index;
 
       if (dest.file != BAD_FILE && data_bit_size > nir_bit_size) {
          /* Shrink e.g. D16U32 result back to D16 */
@@ -6074,6 +6087,7 @@ brw_from_nir_emit_memory_access(nir_to_brw_state &ntb,
          mem->components = block_comps;
          mem->alignment = alignment;
          mem->flags = flags;
+         mem->surface_index = surface_index;
 
          done += block_comps;
 

src/intel/compiler/brw/brw_inst.h
@@ -343,6 +343,8 @@ struct brw_mem_inst : brw_inst {
    uint8_t coord_components;
    uint8_t components;
    uint8_t flags;
+   /** Texture index Gfx35+ only */
+   uint8_t surface_index;
 
    /** Required alignment of address in bytes; 0 for natural alignment */
    uint32_t alignment;

src/intel/compiler/brw/brw_lower_logical_sends.cpp
@@ -1298,6 +1298,7 @@ lower_lsc_memory_logical_send(const brw_builder &bld, brw_mem_inst *mem)
 
    const enum lsc_addr_size addr_size = lsc_addr_size_for_type(addr.type);
    const int32_t base_offset = mem->address_offset;
+   const uint8_t surface_index = mem->surface_index;
 
    /**
     * TGM messages cannot have a base offset
@@ -1459,7 +1460,7 @@ lower_lsc_memory_logical_send(const brw_builder &bld, brw_mem_inst *mem)
                                                cache_mode,
                                                0 /* scale_offset */,
                                                base_offset / data_size_B,
-                                               0 /* surface_state_index */);
+                                               surface_index);
       assert(binding_type == LSC_ADDR_SURFTYPE_FLAT || brw_type_size_bits(binding.type) == 64);
       send->src[SENDG_SRC_IND_0_DESC] = binding_type == LSC_ADDR_SURFTYPE_FLAT ?
                                         brw_reg() : retype(binding, BRW_TYPE_UQ);

src/intel/compiler/brw/brw_opt_cse.cpp
@@ -391,6 +391,7 @@ hash_inst(const void *v)
          mem->coord_components,
          mem->components,
          mem->flags,
+         mem->surface_index,
       };
       const uint32_t mem_u32data[] = {
          (uint32_t)mem->address_offset,

src/intel/compiler/brw/brw_print.cpp
@@ -515,6 +515,8 @@ brw_print_instruction(const brw_shader &s, const brw_inst *inst, FILE *file, con
          fprintf(file, " volatile");
       if (mem->flags & MEMORY_FLAG_COHERENT_ACCESS)
          fprintf(file, " coherent");
+      if (mem->surface_index)
+         fprintf(file, " surf_idx: %hhu", mem->surface_index);
    }
 
    const brw_tex_inst *tex = inst->as_tex();

src/intel/compiler/intel_nir_lower_vec2_surface_sampler.c
@@ -87,7 +87,7 @@ extract_handle_offset(nir_builder *b,
          s.resource_intel->src[3].ssa,
          .desc_set = nir_intrinsic_desc_set(s.resource_intel),
          .binding = nir_intrinsic_binding(s.resource_intel),
-         .resource_block_intel = nir_intrinsic_resource_block_intel(s.resource_intel),
+         .resource_block_intel = s.const_index,
          .resource_access_intel = nir_intrinsic_resource_access_intel(s.resource_intel));
    }
 
@@ -115,11 +115,9 @@ lower_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin, void *_)
    case nir_intrinsic_bindless_image_atomic_swap: {
       b->cursor = nir_before_instr(&intrin->instr);
 
-      /* TODO: find a way to pass constant surface handle offsets for
-       *       buffers, add a new indice to intrinsics?
-       */
       nir_src *surface = nir_get_io_index_src(intrin);
-      struct source_extract s = extract_handle_offset(b, surface->ssa, 0, 64);
+      struct source_extract s = extract_handle_offset(
+         b, surface->ssa, 64 * 31 /* 5 bits */, 64);
       nir_src_rewrite(surface, s.ret);
       return true;
    }

commit 1bb63aa656b3481ce319ab8c9ece4b563bb19f8f

brw: Drop debug fprintf accidentally left in d1dc00919ff7
Fixes: d1dc00919ff7 ("brw: support surface index in efficient 64bit mode")
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44254>

src/intel/compiler/brw/brw_lower.cpp
@@ -871,8 +871,6 @@ brw_lower_send_gather_inst(brw_shader &s, brw_send_inst *inst)
       assert(nr <= UINT8_MAX);
       regs[count++] = nr;
    }
-   fprintf(stderr, "count=%u num_payload_sources=%u\n",
-           count, num_payload_sources);
 
    /* Fill out ARF scalar register with the physical register numbers
     * and use SEND_GATHER.