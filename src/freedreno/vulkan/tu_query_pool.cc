joshuaswarren/mesa-1

commit 1b368b9bac4dfea67f6971054122561a3efe600f

tu: Use p_atomic_read to read from query's slot->available
Otherwise compiler could incorrectly optimize it.

Cc: mesa-stable
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44015>

src/freedreno/vulkan/tu_query_pool.cc
@@ -41,11 +41,11 @@
  */
 #define STAT_COUNT ((__COUNTER_REG(A6XX, CSINVOCATIONS).reg - __COUNTER_REG(A6XX, IAVERTICES).reg) / 2 + 1)
 
-struct PACKED query_slot {
-   uint64_t available;
+struct alignas(8) PACKED query_slot {
+   alignas(8) uint64_t available;
 };
 
-struct PACKED occlusion_query_slot {
+struct alignas(8) PACKED occlusion_query_slot {
    struct query_slot common;
    uint64_t _padding0;
 
@@ -55,16 +55,16 @@ struct PACKED occlusion_query_slot {
    uint64_t _padding1;
 };
 
-struct PACKED timestamp_query_slot {
+struct alignas(8) PACKED timestamp_query_slot {
    struct query_slot common;
    uint64_t result;
 };
 
-struct PACKED primitive_slot_value {
+struct alignas(8) PACKED primitive_slot_value {
    uint64_t values[2];
 };
 
-struct PACKED pipeline_stat_query_slot {
+struct alignas(8) PACKED pipeline_stat_query_slot {
    struct query_slot common;
    uint64_t results[STAT_COUNT];
 
@@ -72,7 +72,7 @@ struct PACKED pipeline_stat_query_slot {
    uint64_t end[STAT_COUNT];
 };
 
-struct PACKED primitive_query_slot {
+struct alignas(8) PACKED primitive_query_slot {
    struct query_slot common;
    /* The result of transform feedback queries is two integer values:
     *   results[0] is the count of primitives written,
@@ -88,25 +88,25 @@ struct PACKED primitive_query_slot {
    struct primitive_slot_value end[4];
 };
 
-struct PACKED perfcntr_query_slot {
+struct alignas(8) PACKED perfcntr_query_slot {
    uint64_t result;
    uint64_t begin;
    uint64_t end;
 };
 
-struct PACKED perf_query_raw_slot {
+struct alignas(8) PACKED perf_query_raw_slot {
    struct query_slot common;
    struct perfcntr_query_slot perfcntr;
 };
 
-struct PACKED primitives_generated_query_slot {
+struct alignas(8) PACKED primitives_generated_query_slot {
    struct query_slot common;
    uint64_t result;
    uint64_t begin;
    uint64_t end;
 };
 
-struct PACKED accel_struct_slot {
+struct alignas(8) PACKED accel_struct_slot {
    struct query_slot common;
    uint64_t result;
 };
@@ -167,7 +167,7 @@ struct PACKED accel_struct_slot {
    (uint64_t *) ((char *) pool->bo->map + pool->query_stride * (query) +   \
                  sizeof(struct query_slot) + sizeof(type) * (i))
 
-#define query_is_available(slot) slot->available
+#define query_is_available(slot) p_atomic_read(&slot->available)
 
 static const VkPerformanceCounterUnitKHR
 fd_perfcntr_type_to_vk_unit[] = {

commit 7bacf030a3a82aa270fda071a14821b53aaa697f

tu: Fix PACKED + alignas(8) application to the same struct on older GCC
The compatible way is to have `alignas` before struct name and `PACKED`
at the end.

Fixes: 1b368b9bac4 ("tu: Use p_atomic_read to read from query's slot->available")
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44326>

src/freedreno/vulkan/tu_query_pool.cc
@@ -41,11 +41,11 @@
  */
 #define STAT_COUNT ((__COUNTER_REG(A6XX, CSINVOCATIONS).reg - __COUNTER_REG(A6XX, IAVERTICES).reg) / 2 + 1)
 
-struct alignas(8) PACKED query_slot {
+struct alignas(8) query_slot {
    alignas(8) uint64_t available;
-};
+} PACKED;
 
-struct alignas(8) PACKED occlusion_query_slot {
+struct alignas(8) occlusion_query_slot {
    struct query_slot common;
    uint64_t _padding0;
 
@@ -53,26 +53,26 @@ struct alignas(8) PACKED occlusion_query_slot {
    uint64_t result;
    uint64_t end;
    uint64_t _padding1;
-};
+} PACKED;
 
-struct alignas(8) PACKED timestamp_query_slot {
+struct alignas(8) timestamp_query_slot {
    struct query_slot common;
    uint64_t result;
-};
+} PACKED;
 
-struct alignas(8) PACKED primitive_slot_value {
+struct alignas(8) primitive_slot_value {
    uint64_t values[2];
-};
+} PACKED;
 
-struct alignas(8) PACKED pipeline_stat_query_slot {
+struct alignas(8) pipeline_stat_query_slot {
    struct query_slot common;
    uint64_t results[STAT_COUNT];
 
    uint64_t begin[STAT_COUNT];
    uint64_t end[STAT_COUNT];
-};
+} PACKED;
 
-struct alignas(8) PACKED primitive_query_slot {
+struct alignas(8) primitive_query_slot {
    struct query_slot common;
    /* The result of transform feedback queries is two integer values:
     *   results[0] is the count of primitives written,
@@ -86,30 +86,30 @@ struct alignas(8) PACKED primitive_query_slot {
 
    struct primitive_slot_value begin[4];
    struct primitive_slot_value end[4];
-};
+} PACKED;
 
-struct alignas(8) PACKED perfcntr_query_slot {
+struct alignas(8) perfcntr_query_slot {
    uint64_t result;
    uint64_t begin;
    uint64_t end;
-};
+} PACKED;
 
-struct alignas(8) PACKED perf_query_raw_slot {
+struct alignas(8) perf_query_raw_slot {
    struct query_slot common;
    struct perfcntr_query_slot perfcntr;
-};
+} PACKED;
 
-struct alignas(8) PACKED primitives_generated_query_slot {
+struct alignas(8) primitives_generated_query_slot {
    struct query_slot common;
    uint64_t result;
    uint64_t begin;
    uint64_t end;
-};
+} PACKED;
 
-struct alignas(8) PACKED accel_struct_slot {
+struct alignas(8) accel_struct_slot {
    struct query_slot common;
    uint64_t result;
-};
+} PACKED;
 
 /* Returns the IOVA or mapped address of a given uint64_t field
  * in a given slot of a query pool. */