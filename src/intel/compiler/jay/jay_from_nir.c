joshuaswarren/mesa-1

commit ca7679358a657a0073bac5bed83758de69806b8c

jay: Handle reconvergence on the physical CFG correctly
We need to support more than 2 physical CFG successors per block to
correctly represent reconvergence from jump instructions. Right now we
fake it with a lot of nonsense edges, which works fine for CTS but
eventually falls apart on more complex workloads. This fixes validation
errors from the RT pipelines in DOOM: The Dark Ages.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43755>

src/intel/compiler/jay/jay_from_nir.c
@@ -107,6 +107,11 @@ struct nir_to_jay_state {
    jay_builder bld;
    jay_block *current_block, *after_block, *break_block, *exit_block;
 
+   /* Reconvergence tracking for break and halt instructions */
+   jay_block *converge_block;
+   struct util_dynarray converge_blocks;
+   unsigned loop_converge_block;
+
    /* Bitset of defs optimized for ballots */
    BITSET_WORD *zero_inactive;
 
@@ -3554,6 +3559,44 @@ jay_emit_instr(struct nir_to_jay_state *nj, jay_block *block, nir_instr *instr)
    }
 }
 
+static inline void
+jay_block_reconverge(struct nir_to_jay_state *nj,
+                     jay_block *current_block,
+                     jay_block *after_block)
+{
+   if (jay_num_predecessors(current_block, GPR) == 0) {
+      /* Don't insert edges from unreachable blocks, cleaning the dead
+       * reconvergence edges that result from this code after NIR->Jay
+       * translation is not practical.
+       */
+      return;
+   }
+
+   jay_inst *jump = jay_block_ending_jump(current_block);
+   if (jump && jump->op == JAY_OPCODE_BREAK) {
+      util_dynarray_foreach_reverse(&nj->converge_blocks, jay_block *, conv) {
+         /* Break statements don't reconverge outside the current loop */
+         if (nj->loop_converge_block &&
+             util_dynarray_element(&nj->converge_blocks, jay_block *,
+                                   nj->loop_converge_block - 1) == conv) {
+            break;
+         }
+
+         jay_block_add_successor(current_block, *conv, UGPR);
+      }
+   } else if (jump && jump->op == JAY_OPCODE_HALT) {
+      util_dynarray_foreach_reverse(&nj->converge_blocks, jay_block *, conv) {
+         /* Even though halt instructions skip the remainder of the program in
+          * the logical CFG, we could still reconverge physically
+          */
+         jay_block_add_successor(current_block, *conv, UGPR);
+      }
+   } else {
+      assert(!jump);
+      jay_block_add_successor(current_block, after_block, GPR);
+   }
+}
+
 static jay_block *
 jay_create_block(struct nir_to_jay_state *nj)
 {
@@ -3567,50 +3610,67 @@ jay_emit_if(struct nir_to_jay_state *nj, nir_if *nif)
 {
    jay_builder *b = &nj->bld;
    jay_def condition = nj_src(nif->condition);
+   bool uniform = jay_is_uniform(condition);
 
+   jay_block *converge_block = nj->converge_block;
    jay_block *before_block = nj->current_block;
    jay_block *after_block = jay_create_block(nj);
 
    /* Push */
    ++nj->indent;
 
+   if (converge_block) {
+      util_dynarray_append(&nj->converge_blocks, converge_block);
+   }
+
    jay_block *else_first = jay_create_block(nj);
+
+   /* Break and halt instructions in the then block may reconverge at the
+    * else block for a non-uniform IF.
+    */
+   nj->converge_block = !uniform ? else_first : NULL;
 
    jay_block *then_first = jay_emit_cf_list(nj, &nif->then_list);
    jay_block *then_last = nj->current_block;
 
    nj->after_block = else_first;
 
+   /* Break and halt instructions in the else block may reconverge at the
+    * after block for a non-uniform IF, unless the then block always ends
+    * with break or halt.
+    */
+   if (!uniform &&
+       jay_num_predecessors(then_last, GPR) > 0 &&
+       !jay_block_ending_jump(then_last)) {
+      nj->converge_block = after_block;
+   } else {
+      nj->converge_block = NULL;
+   }
+
    jay_block *else_first_2 = jay_emit_cf_list(nj, &nif->else_list);
    jay_block *else_last = nj->current_block;
    assert(else_first == else_first_2);
-
-   /* Pop */
-   --nj->indent;
-
-   bool uniform = jay_is_uniform(condition);
 
    /* Logical CFG edges */
    jay_block_add_successor(before_block, then_first, GPR);
    jay_block_add_successor(before_block, else_first, GPR);
 
-   if (!jay_block_ending_jump(then_last))
-      jay_block_add_successor(then_last, after_block, GPR);
-
-   if (!jay_block_ending_jump(else_last))
-      jay_block_add_successor(else_last, after_block, GPR);
+   jay_block_reconverge(nj, then_last, after_block);
+   jay_block_reconverge(nj, else_last, after_block);
 
    if (!uniform) {
       /* For a non-uniform IF, we fall through both sides in the physical CFG */
       jay_block_add_successor(then_last, else_first, UGPR);
-
-      /* Even if the else block logically ends in an unconditional jump,
-       * physically we will still reconverge. Add the physical edge.
-       */
-      jay_block_add_successor(else_last, after_block, UGPR);
    }
 
+   /* Pop */
+   --nj->indent;
    nj->after_block = after_block;
+   nj->converge_block = converge_block;
+
+   if (converge_block) {
+      (void) util_dynarray_pop_ptr(&nj->converge_blocks, jay_block *);
+   }
 
    /* Emit the if-else-endif sequence */
    b->cursor = jay_after_block(before_block);
@@ -3630,10 +3690,23 @@ jay_emit_loop(struct nir_to_jay_state *nj, nir_loop *nloop)
 
    jay_builder *b = &nj->bld;
    jay_block *saved_break = nj->break_block;
+   jay_block *converge_block = nj->converge_block;
+   unsigned saved_loop_converge = nj->loop_converge_block;
 
    /* Make the block that will be after the loop exit */
    nj->break_block = jay_create_block(nj);
    ++nj->indent;
+
+   if (converge_block) {
+      util_dynarray_append(&nj->converge_blocks, converge_block);
+   }
+
+   /* Halt instructions inside the loop node could reconverge at the break
+    * block if the loop node contains any divergent break instructions.
+    */
+   nj->converge_block = nir_loop_is_divergent(nloop) ? nj->break_block : NULL;
+   nj->loop_converge_block =
+      util_dynarray_num_elements(&nj->converge_blocks, jay_block *);
 
    /* Make a block for the loop body, which is also the loop header */
    jay_block *loop_header = jay_create_block(nj);
@@ -3647,21 +3720,32 @@ jay_emit_loop(struct nir_to_jay_state *nj, nir_loop *nloop)
    jay_emit_cf_list(nj, &nloop->body);
 
    /* Emit the backedge */
+   jay_block_reconverge(nj, nj->current_block, loop_header);
+
    jay_inst *jump = jay_block_ending_jump(nj->current_block);
    if (jump && jump->op == JAY_OPCODE_BREAK) {
       jump->op = JAY_OPCODE_LOOP_ONCE;
    } else if (jump && jump->op == JAY_OPCODE_HALT) {
       jump->op = JAY_OPCODE_LOOP_ONCE_HALT;
    } else {
-      jay_block_add_successor(nj->current_block, loop_header, GPR);
+      assert(!jump);
       jay_WHILE(b);
-      loop_header->loop_header = true;
+
+      if (jay_cfg_has_edge(nj->current_block, loop_header, GPR)) {
+         loop_header->loop_header = true;
+      }
    }
 
    /* Pop */
    --nj->indent;
    nj->after_block = nj->break_block;
    nj->break_block = saved_break;
+   nj->loop_converge_block = saved_loop_converge;
+   nj->converge_block = converge_block;
+
+   if (converge_block) {
+      (void) util_dynarray_pop_ptr(&nj->converge_blocks, jay_block *);
+   }
 
    b->cursor = jay_after_block(nj->after_block);
 }
@@ -4468,20 +4552,19 @@ jay_remove_unreachable_blocks(jay_function *func)
                    jay_num_successors(pred, cfg) > 0) {
 
                   jay_foreach_successor(pred, succ, cfg) {
-                     util_dynarray_delete_unordered(jay_predecessors(succ, cfg),
+                     util_dynarray_delete_unordered(jay_predecessors(*succ, cfg),
                                                     jay_block *, pred);
 
                      /* If we are removing the backedge of a loop, the loop is
                       * no longer a loop. Update to avoid validation issues.
                       */
-                     if (succ->index <= pred->index) {
-                        assert(succ->physical_loop_header);
-                        succ->loop_header = false;
+                     if ((*succ)->index <= pred->index) {
+                        assert((*succ)->physical_loop_header);
+                        (*succ)->loop_header = false;
                      }
                   }
 
-                  jay_successors(pred, cfg)[0] = NULL;
-                  jay_successors(pred, cfg)[1] = NULL;
+                  util_dynarray_clear(jay_successors(pred, cfg));
                   progress = true;
                }
             }
@@ -4507,6 +4590,10 @@ jay_from_nir_function(const struct intel_device_info *devinfo,
       .bld = (jay_builder){ .shader = s, .func = f },
    };
 
+   nir_block *conv_blocks_storage[32];
+   util_dynarray_init_from_stack(&nj.converge_blocks, conv_blocks_storage,
+                                 sizeof(conv_blocks_storage));
+
    /* Jay indices match NIR indices. Therefore the first impl->ssa_alloc
     * indices are reserved. Our own temporaries go after.
     */
@@ -4525,6 +4612,7 @@ jay_from_nir_function(const struct intel_device_info *devinfo,
    jay_emit_task_mesh_fence_workaround(&nj);
    jay_emit_eot(&nj);
    free(nj.zero_inactive);
+   util_dynarray_fini(&nj.converge_blocks);
 }
 
 static void

src/intel/compiler/jay/jay_assign_accumulators.c
@@ -55,7 +55,7 @@ postra_liveness(jay_function *func)
       /* Calculate liveness locally */
       jay_foreach_successor(blk, succ, GPR) {
          BITSET_OR(blk->postra_gpr_live_out, blk->postra_gpr_live_out,
-                   succ->postra_gpr_live_in);
+                   (*succ)->postra_gpr_live_in);
       }
 
       BITSET_DECLARE(live, JAY_MAX_PHYS_GRF);

src/intel/compiler/jay/jay_ir.h
@@ -1196,6 +1196,21 @@ struct jay_temp_regs {
    jay_reg gpr, gpr2, ugpr;
 };
 
+/** Optimized dynamic array for storing predecessors/successors */
+struct jay_cfg_edge {
+   struct util_dynarray arr;
+   void *_storage[2];
+};
+
+/** Indices of jay_block::cfg_edges */
+enum jay_cfg_edge_type {
+   JAY_LOGICAL_PREDS  = 0x00,
+   JAY_PHYSICAL_PREDS = JAY_LOGICAL_PREDS | JAY_UNIFORM,
+   JAY_LOGICAL_SUCCS  = 0x02,
+   JAY_PHYSICAL_SUCCS = JAY_LOGICAL_SUCCS | JAY_UNIFORM,
+   JAY_CFG_EDGE_MAX,
+};
+
 /**
  * A basic block representation
  */
@@ -1204,8 +1219,7 @@ typedef struct jay_block {
    struct list_head instructions;
 
    /** Control flow graph */
-   struct jay_block *logical_succs[2], *physical_succs[2];
-   struct util_dynarray logical_preds, physical_preds;
+   struct jay_cfg_edge cfg_edges[JAY_CFG_EDGE_MAX];
 
    /** Index of the block in source order */
    unsigned index;
@@ -1253,13 +1267,27 @@ typedef struct jay_block {
    unsigned demand_out[JAY_NUM_SSA_FILES];
 } jay_block;
 
+static void
+_jay_block_destructor(void *_block)
+{
+   jay_block *block = (jay_block *) _block;
+   for (unsigned i = 0; i < JAY_CFG_EDGE_MAX; ++i) {
+      util_dynarray_fini(&block->cfg_edges[i].arr);
+   }
+}
+
 static inline jay_block *
 jay_new_block(jay_function *f)
 {
    jay_block *block = rzalloc(f, jay_block);
+   ralloc_set_destructor(block, &_jay_block_destructor);
 
-   util_dynarray_init(&block->logical_preds, block);
-   util_dynarray_init(&block->physical_preds, block);
+   for (unsigned i = 0; i < JAY_CFG_EDGE_MAX; ++i) {
+      util_dynarray_init_from_stack(&block->cfg_edges[i].arr,
+                                    block->cfg_edges[i]._storage,
+                                    sizeof(block->cfg_edges[i]._storage));
+   }
+
    list_inithead(&block->instructions);
 
    block->index = f->num_blocks++;
@@ -1287,13 +1315,13 @@ jay_block_ending_jump(jay_block *block)
 static inline struct util_dynarray *
 jay_predecessors(jay_block *blk, enum jay_file file)
 {
-   return file & JAY_UNIFORM ? &blk->physical_preds : &blk->logical_preds;
+   return &blk->cfg_edges[JAY_LOGICAL_PREDS | (file & JAY_UNIFORM)].arr;
 }
 
-static inline jay_block **
+static inline struct util_dynarray *
 jay_successors(jay_block *blk, enum jay_file file)
 {
-   return file & JAY_UNIFORM ? blk->physical_succs : blk->logical_succs;
+   return &blk->cfg_edges[JAY_LOGICAL_SUCCS | (file & JAY_UNIFORM)].arr;
 }
 
 static inline unsigned
@@ -1303,12 +1331,9 @@ jay_num_predecessors(jay_block *blk, enum jay_file file)
 }
 
 static inline unsigned
-jay_num_successors(jay_block *block, enum jay_file file)
+jay_num_successors(jay_block *blk, enum jay_file file)
 {
-   static_assert(ARRAY_SIZE(block->logical_succs) == 2);
-   static_assert(ARRAY_SIZE(block->physical_succs) == 2);
-
-   return !!jay_successors(block, file)[0] + !!jay_successors(block, file)[1];
+   return util_dynarray_num_elements(jay_successors(blk, file), jay_block *);
 }
 
 static inline jay_block *
@@ -1395,20 +1420,8 @@ jay_first_predecessor(jay_block *block, enum jay_file file)
    jay_foreach_function(s, func)                                               \
       jay_foreach_inst_in_func_safe(func, v_block, inst)
 
-/*
- * Get the next successor, using the fact that there are at most 2 successors
- * and NULL successors cannot precede non-NULL successors.
- */
-static inline jay_block *
-jay_next_successor(jay_block *parent, enum jay_file file, jay_block *it)
-{
-   jay_block **succs = jay_successors(parent, file);
-   return succs[0] == it ? succs[1] : NULL;
-}
-
 #define jay_foreach_successor(blk, v, file)                                    \
-   for (jay_block *v = jay_successors(blk, file)[0]; v != NULL;                \
-        v = jay_next_successor(blk, file, v))
+   util_dynarray_foreach(jay_successors(blk, file), jay_block *, v)
 
 #define jay_foreach_predecessor(blk, v, file)                                  \
    util_dynarray_foreach(jay_predecessors(blk, file), jay_block *, v)
@@ -1493,6 +1506,16 @@ jay_first_inst(jay_block *block)
 }
 
 static inline jay_block *
+jay_first_successor(jay_block *block, enum jay_file file)
+{
+   if (!jay_num_successors(block, file))
+      return NULL;
+   else
+      return *util_dynarray_element(jay_successors(block, file),
+                                    jay_block *, 0);
+}
+
+static inline jay_block *
 jay_last_block(jay_function *f)
 {
    if (list_is_empty(&f->blocks))
@@ -1525,32 +1548,36 @@ jay_next_block(jay_block *block)
    return list_first_entry(&(block->link), jay_block, link);
 }
 
+static inline bool
+jay_cfg_has_edge(jay_block *pred, jay_block *succ, enum jay_file file)
+{
+   jay_foreach_successor(pred, existing_succ, file) {
+      if (succ == *existing_succ) {
+         return true;
+      }
+   }
+
+   return false;
+}
+
 static inline void
 jay_block_add_successor(jay_block *block, jay_block *succ, enum jay_file file)
 {
    /* Prune duplicate successors so the caller doesn't need to worry */
-   jay_block **succs = jay_successors(block, file);
-   if (succs[0] == succ || succs[1] == succ) {
+   if (jay_cfg_has_edge(block, succ, file)) {
       return;
    }
 
-   unsigned i = succs[0] ? 1 : 0;
-   assert(succ && succs[i] == NULL && "at most 2 successors");
+   assert(((file & JAY_UNIFORM) || jay_num_successors(block, file) < 2) &&
+          "at most 2 logical successors");
 
-   succs[i] = succ;
+   util_dynarray_append(jay_successors(block, file), succ);
    util_dynarray_append(jay_predecessors(succ, file), block);
 
    /* All logical CFG edges are also physical CFG edges */
    if (file == GPR) {
       jay_block_add_successor(block, succ, UGPR);
    }
-}
-
-static inline bool
-jay_cfg_has_edge(jay_block *pred, jay_block *succ, enum jay_file file)
-{
-   return jay_successors(pred, file)[0] == succ ||
-          jay_successors(pred, file)[1] == succ;
 }
 
 static inline unsigned

src/intel/compiler/jay/jay_lower_helpers.c
@@ -149,15 +149,21 @@ process_block(struct ctx *ctx, jay_builder *b, jay_block *block)
             /* The split block either falls through or jumps to the exit */
             for (unsigned file = GPR; file <= UGPR; ++file) {
                jay_foreach_predecessor(block, pred, file) {
-                  jay_block **succs = jay_successors(*pred, file);
-                  unsigned idx = succs[0] == block ? 0 : 1;
-                  succs[idx] = split;
+                  jay_foreach_successor(*pred, succ, file) {
+                     if (block == *succ) {
+                        *succ = split;
+                        break;
+                     }
+                  }
                }
             }
-            typed_memcpy(&split->physical_preds, &block->physical_preds, 1);
-            typed_memcpy(&split->logical_preds, &block->logical_preds, 1);
-            util_dynarray_init(&block->physical_preds, block);
-            util_dynarray_init(&block->logical_preds, block);
+
+            util_dynarray_append_dynarray(jay_predecessors(split, UGPR),
+                                          jay_predecessors(block, UGPR));
+            util_dynarray_append_dynarray(jay_predecessors(split, GPR),
+                                          jay_predecessors(block, GPR));
+            util_dynarray_clear(jay_predecessors(block, UGPR));
+            util_dynarray_clear(jay_predecessors(block, GPR));
 
             jay_block_add_successor(split, block, GPR);
             jay_block_add_successor(split, jay_last_block(b->func), GPR);

src/intel/compiler/jay/jay_lower_scoreboard.c
@@ -841,7 +841,7 @@ jay_lower_scoreboard(jay_shader *shader)
                       sizeof(regdists[0]) * shader->num_regs[f]);
             }
 
-            next[f] = jay_successors(block, f)[0];
+            next[f] = jay_first_successor(block, f);
          }
 
          jay_foreach_inst_in_block_safe(block, I) {

src/intel/compiler/jay/jay_opt_predicate.c
@@ -66,8 +66,10 @@ static void
 predicate_if(jay_function *f, jay_block *if_block, jay_inst *if_)
 {
    /* If's fallthrough to the then and branch to the else */
-   jay_block *then_block = if_block->logical_succs[0],
-             *else_block = if_block->logical_succs[1];
+   assert(jay_num_successors(if_block, GPR) == 2);
+   struct util_dynarray *succs = jay_successors(if_block, GPR);
+   jay_block *then_block = *util_dynarray_element(succs, jay_block *, 0),
+             *else_block = *util_dynarray_element(succs, jay_block *, 1);
    assert(then_block == jay_next_block(if_block) && "successors for if");
 
    jay_builder b = jay_init_builder(f, jay_before_inst(if_));

src/intel/compiler/jay/jay_print.c
@@ -276,12 +276,12 @@ jay_print_block(FILE *fp, jay_block *block)
    fprintf(fp, "}");
    first = true;
    jay_foreach_successor(block, succ, GPR) {
-      fprintf(fp, "%s B%d", first ? " ->" : "", succ->index);
+      fprintf(fp, "%s B%d", first ? " ->" : "", (*succ)->index);
       first = false;
    }
    first = true;
    jay_foreach_successor(block, succ, UGPR) {
-      fprintf(fp, "%s B%d", first ? " =>" : "", succ->index);
+      fprintf(fp, "%s B%d", first ? " =>" : "", (*succ)->index);
       first = false;
    }
    fprintf(fp, "\n\n");

src/intel/compiler/jay/jay_spill.c
@@ -894,7 +894,8 @@ jay_spill(jay_function *func, enum jay_file file, unsigned k)
       min_algorithm(&ctx, block, sb, next_ips, nu_cursor);
 
       /* Handle loop back edges */
-      struct jay_block *loop_head = block->logical_succs[0];
+      struct jay_block *loop_head =
+         *util_dynarray_element(jay_successors(block, GPR), jay_block *, 0);
 
       if ((loop_head && loop_head->loop_header) &&
           loop_head->index < block->index) {
@@ -910,7 +911,7 @@ jay_spill(jay_function *func, enum jay_file file, unsigned k)
          jay_foreach_block_from(func, loop_head, inside) {
             bool is_break_block = true;
             jay_foreach_successor(inside, succ, file) {
-               is_break_block &= succ->index > block->index;
+               is_break_block &= (*succ)->index > block->index;
             }
 
             /* Remap to use our phis inside the loop */

commit 86fa2af731dc8e2c51107515b0c9584611c63b3b

jay: Fix an additional reconvergence issue with the physical CFG
I messed up the case when we have a non-uniform if where the last
else block is reachable and ends with a jump, but the last then
block falls through. We have to add nj->converge_block as an else
successor manually in this case because it is not in the stack
anymore once we call jay_block_reconverge.

Fixes: ca76793 ("jay: Handle reconvergence on the physical CFG correctly")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44346>

src/intel/compiler/jay/jay_from_nir.c
@@ -3640,11 +3640,12 @@ jay_emit_if(struct nir_to_jay_state *nj, nir_if *nif)
    nj->after_block = else_first;
 
    /* Break and halt instructions in the else block may reconverge at the
-    * after block for a non-uniform IF, unless the then block always ends
-    * with break or halt.
+    * after block for a non-uniform IF, unless the then block is unreachable
+    * or always ends with a break or halt.
     */
    if (!uniform &&
-       jay_num_predecessors(then_last, GPR) > 0 &&
+       (then_first == then_last ||
+        jay_num_predecessors(then_last, GPR) > 0) &&
        !jay_block_ending_jump(then_last)) {
       nj->converge_block = after_block;
    } else {
@@ -3658,6 +3659,13 @@ jay_emit_if(struct nir_to_jay_state *nj, nir_if *nif)
    if (!uniform) {
       /* For a non-uniform IF, we fall through both sides in the physical CFG */
       jay_block_add_successor(then_last, else_first, UGPR);
+   }
+
+   if (nj->converge_block) {
+      /* For a non-uniform IF, we also have to consider if we may fall through
+       * after the else block, due to reconvergence from the then block
+       */
+      jay_block_add_successor(else_last, nj->converge_block, UGPR);
    }
 
    /* Logical CFG edges */