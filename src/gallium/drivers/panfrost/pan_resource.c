joshuaswarren/mesa-1

commit 7f2f2fbd6aa66720911b63a4fce2c41c9bfdfa63

panfrost: in-place map/unmap shouldn't grow
If we're doing in-place map/unmap of a tiled resource, that shouldn't
cause the size of the resource to grow. So let's drop this needless
complexity, and just assert this assumption instead.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32724>

src/gallium/drivers/panfrost/pan_resource.c
@@ -1815,14 +1815,11 @@ panfrost_ptr_unmap(struct pipe_context *pctx, struct pipe_transfer *transfer)
             if (panfrost_should_linear_convert(ctx, prsrc, transfer)) {
                panfrost_resource_setup(screen, prsrc, DRM_FORMAT_MOD_LINEAR,
                                        prsrc->image.layout.format);
-               if (prsrc->image.layout.data_size > panfrost_bo_size(bo)) {
-                  const char *label = bo->label;
-                  panfrost_bo_unreference(bo);
-                  bo = prsrc->bo = panfrost_bo_create(
-                     dev, prsrc->image.layout.data_size, 0, label);
-                  prsrc->image.data.base = prsrc->bo->ptr.gpu;
-                  assert(bo);
-               }
+
+               /* converting the resource from tiled to linear and back
+                * shouldn't increase memory usage...
+                */
+               assert(prsrc->image.layout.data_size <= panfrost_bo_size(bo));
 
                util_copy_rect(
                   bo->ptr.cpu + prsrc->image.layout.slices[0].offset,

commit 97fb7ead5a9b2007105075da5f6d2dbade9fe176

panfrost: Reallocate BO when linear layout grows
Commit 7f2f2fbd assumed that converting a tiled resource to linear
would never require a larger allocation. This is not true for some
resource sizes, because the linear layout may have a larger row stride
and additional alignment padding.

For some non-standard resource sizes, the linear layout can therefore
be larger than the original tiled BO. Reallocate the BO when the
linear layout does not fit instead of copying beyond the end of the
existing BO.

Fixes: 7f2f2fbd6aa66720911b63a4fce2c41c9bfdfa63
Signed-off-by: Takuro Ashie <ashie@clear-code.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@arm.com>
Reviewed-by: Loïc Molinari <loic.molinari@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43893>

src/gallium/drivers/panfrost/pan_resource.c
@@ -2343,10 +2343,16 @@ panfrost_ptr_unmap(struct pipe_context *pctx, struct pipe_transfer *transfer)
                panfrost_resource_setup(screen, prsrc, DRM_FORMAT_MOD_LINEAR,
                                        prsrc->image.props.format, 0);
 
-               /* converting the resource from tiled to linear and back
-                * shouldn't increase memory usage...
-                */
-               assert(prsrc->plane.layout.data_size_B <= panfrost_bo_size(bo));
+               if (prsrc->plane.layout.data_size_B > panfrost_bo_size(bo)) {
+                  struct panfrost_bo *newbo = panfrost_bo_create(
+                     dev, prsrc->plane.layout.data_size_B, 0, bo->label);
+
+                  assert(newbo);
+                  panfrost_bo_unreference(prsrc->bo);
+                  prsrc->bo = newbo;
+                  prsrc->plane.base = newbo->ptr.gpu;
+                  bo = newbo;
+               }
 
                util_copy_rect(
                   bo->ptr.cpu + prsrc->plane.layout.slices[0].offset_B,