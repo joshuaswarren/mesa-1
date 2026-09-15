joshuaswarren/mesa-1

commit 36ac26fa4f0384cdb9261e832408c6366b8ec50a

jay: defer moving SEND ex_desc to address register
Wait until jay_lower_pre_ra to move the ex_desc of SEND operations to an
address register. This makes certain optimizations easier, particularly
txf combining.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44150>

src/intel/compiler/jay/jay_builder.h
@@ -599,10 +599,8 @@ _jay_SEND(jay_builder *b, const struct jayb_send_params p)
    if (jay_is_null(p.ex_desc)) {
       I->src[1] =
          jay_imm(brw_message_ex_desc(devinfo, lens[2]) | (p.msg_desc >> 32));
-   } else if (p.ex_desc.file == J_ADDRESS) {
-      I->src[1] = p.ex_desc;
    } else {
-      I->src[1] = jay_alloc_def(b, J_ADDRESS, 1);
+      I->src[1] = jay_alloc_def(b, UGPR, 1);
       if (info->bindless) {
          jay_MOV(b, I->src[1], p.ex_desc);
       } else {

src/intel/compiler/jay/jay_from_nir.c
@@ -3454,10 +3454,10 @@ jay_emit_texture(struct nir_to_jay_state *nj, nir_tex_instr *tex)
       }
 
       /* We assume that the driver provided the handle in the top 20 bits so
-       * we can use the surface handle directly as the extended descriptor.
+-      * we can use the surface handle directly as the extended descriptor.
+       * This will be moved to an address register in jay_lower_pre_ra.
        */
-      desc_ex_src = jay_alloc_def(b, J_ADDRESS, 1);
-      jay_MOV(b, desc_ex_src, surface);
+      desc_ex_src = surface;
    } else {
       /* Immediate portion of the descriptor */
       desc = brw_sampler_desc(nj->devinfo, 0, 0, msg_type, simd_mode, ret_type);

src/intel/compiler/jay/jay_lower_pre_ra.c
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "compiler/brw/brw_eu_defines.h"
 #include "util/hash_table.h"
 #include "util/lut.h"
 #include "util/macros.h"
@@ -238,6 +239,21 @@ lower_repeated_phi_srcs(jay_function *f,
    u_sparse_bitset_clear_all(seen);
 }
 
+static void
+lower_ex_desc_address_registers(jay_inst *I, jay_function *f)
+{
+   if (I->op != JAY_OPCODE_SEND)
+      return;
+   if (jay_is_null(I->src[1]) || jay_is_imm(I->src[1]))
+      return;
+   if (I->src[1].file == J_ADDRESS)
+      return;
+   jay_builder b = jay_init_builder(f, jay_before_inst(I));
+   jay_def tmp = jay_alloc_def(&b, J_ADDRESS, 1);
+   jay_MOV(&b, tmp, I->src[1]);
+   jay_replace_src(&I->src[1], tmp);
+}
+
 void
 jay_lower_pre_ra(jay_shader *s)
 {
@@ -260,6 +276,8 @@ jay_lower_pre_ra(jay_shader *s)
          }
 
          jay_foreach_inst_in_block(block, I) {
+            lower_ex_desc_address_registers(I, f);
+
             jay_builder b = { .shader = s, .func = f };
 
             lower_bf16_restrictions(I, f);

commit f334af214bc34501d0e208078498be574555bba0

jay: use addr reg for SEND ex_desc if provided
Earlier I made a patch that modified jay_SEND to defer moving the
ex_desc to an address register until jay_lower_pre_ra. However this
didn't account for spill/fill code, which occurs after jay_lower_pre_ra
and thus is never moved to an address register.

This patch fixes this by explicitly using an address register for the
ex_desc if one is provided by the caller of jay_SEND. I also removed the
one final remaining direct usage of an address register for ex_desc
prior to jay_lower_pre_ra, so we can use more easily-optimized UGPRs for
them across the board.

Fixes: 36ac26fa4f0 ("jay: defer moving SEND ex_desc to address register")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44395>

src/intel/compiler/jay/jay_builder.h
@@ -601,7 +601,16 @@ _jay_SEND(jay_builder *b, const struct jayb_send_params p)
    if (jay_is_null(p.ex_desc)) {
       I->src[1] =
          jay_imm(brw_message_ex_desc(devinfo, lens[2]) | (p.msg_desc >> 32));
+   } else if (p.ex_desc.file == J_ADDRESS) {
+      /* p.ex_desc should end up in an address register, so use it if it is
+       * provided in one already. this is necessary for anything generating
+       * SENDs after jay_lower_pre_ra (e.g. spills/fills).
+       */
+      I->src[1] = p.ex_desc;
    } else {
+      /* Otherwise we assume jay_lower_pre_ra will move it to an address
+       * register, so we can just stuff it in a UGPR for now.
+       */
       I->src[1] = jay_alloc_def(b, UGPR, 1);
       if (info->bindless) {
          jay_MOV(b, I->src[1], p.ex_desc);

src/intel/compiler/jay/jay_from_nir.c
@@ -1558,7 +1558,7 @@ jay_emit_mem_access_lsc(struct nir_to_jay_state *nj, nir_intrinsic_instr *intr)
    uint32_t ex_desc_imm = 0;
    if (scratch) {
       /* TODO: Once we have an address register RA, we should CSE these */
-      ex_desc = jay_alloc_def(b, J_ADDRESS, 1);
+      ex_desc = jay_alloc_def(b, UGPR, 1);
       jay_SHR(b, JAY_TYPE_U32, ex_desc, jay_scratch_surface(nj), 4);
 
       if (has_dest) {

src/intel/compiler/jay/jay_lower_pre_ra.c
@@ -246,6 +246,10 @@ lower_ex_desc_address_registers(jay_inst *I, jay_function *f)
       return;
    if (jay_is_null(I->src[1]) || jay_is_imm(I->src[1]))
       return;
+   /* ex_desc should have been in a UGPR so we could have more easily performed
+    * optimizations with it. It doesn't need to be an address register yet since
+    * we're moving it to one here.
+    */
    assert(I->src[1].file == UGPR);
    jay_builder b = jay_init_builder(f, jay_before_inst(I));
    jay_def tmp = jay_alloc_def(&b, J_ADDRESS, 1);

src/intel/compiler/jay/jay_to_binary.c
@@ -495,6 +495,12 @@ emit(struct jay_codegen *jc,
    }
 
    case JAY_OPCODE_SEND: {
+      assert(
+         (jay_is_null(I->src[1]) ||
+          jay_is_imm(I->src[1]) ||
+          I->src[1].file == J_ADDRESS) &&
+         "if ex_desc is not null or immediate, it must be an address register");
+
       gen_operand ex_desc = to_gen_operand(f, I, 1, simd_offs, false);
       gen->src[0] =
          gen_retype(to_gen_operand(f, I, 2, simd_offs, false), GEN_TYPE_UD);