joshuaswarren/mesa-1

commit 2e3f40a85c76fb4d80aa967c93e39dfb6665e64c

nir: move exact bit to nir_fp_math_control
Unifies nir per instruction float control.

In the future this can be split into contract/reassoc/transform
like SPIR-V.

Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com> (except SPIR-V)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39103>

src/compiler/spirv/vtn_alu.c
@@ -413,11 +413,10 @@ handle_fp_fast_math(struct vtn_builder *b, UNUSED struct vtn_value *val,
       SpvFPFastMathModeAllowReassocMask |
       SpvFPFastMathModeAllowTransformMask;
 
-   if ((dec->operands[0] & can_fast_math) != can_fast_math)
-      b->nb.exact = true;
-
-   /* Decoration overrides defaults */
+   /* Decoration overrides defaults. */
    b->nb.fp_math_ctrl = 0;
+   if ((dec->operands[0] & can_fast_math) != can_fast_math)
+      b->nb.fp_math_ctrl |= nir_fp_exact;
    if (!(dec->operands[0] & SpvFPFastMathModeNSZMask))
       b->nb.fp_math_ctrl |= nir_fp_preserve_signed_zero;
    if (!(dec->operands[0] & SpvFPFastMathModeNotNaNMask))
@@ -728,8 +727,10 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
       return;
    }
 
-   b->nb.exact |= vtn_has_decoration(b, dest_val, SpvDecorationNoContraction);
    vtn_handle_fp_fast_math(b, dest_val);
+
+   if (b->exact || vtn_has_decoration(b, dest_val, SpvDecorationNoContraction))
+      b->nb.fp_math_ctrl |= nir_fp_exact;
    bool mediump_16bit = vtn_alu_op_mediump_16bit(b, opcode, dest_val);
 
    /* Collect the various SSA sources */
@@ -749,7 +750,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
          vtn_mediump_upconvert_value(b, dest);
 
       vtn_push_ssa_value(b, w[2], dest);
-      b->nb.exact = b->exact;
+      b->nb.fp_math_ctrl = b->exact ? nir_fp_exact : nir_fp_fast_math;
       return;
    }
 
@@ -838,53 +839,50 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
       dest->def = nir_fmul(&b->nb, src[0], src[1]);
       break;
 
-   case SpvOpIsNan: {
-      const bool save_exact = b->nb.exact;
+   case SpvOpIsNan:{
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
       dest->def = nir_fneu(&b->nb, src[0], src[0]);
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
    case SpvOpOrdered: {
-      const bool save_exact = b->nb.exact;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
       dest->def = nir_iand(&b->nb, nir_feq(&b->nb, src[0], src[0]),
                                    nir_feq(&b->nb, src[1], src[1]));
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
    case SpvOpUnordered: {
-      const bool save_exact = b->nb.exact;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
       dest->def = nir_ior(&b->nb, nir_fneu(&b->nb, src[0], src[0]),
                                   nir_fneu(&b->nb, src[1], src[1]));
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
    case SpvOpIsInf: {
-      const bool save_exact = b->nb.exact;
-      const unsigned save_math_ctrl = b->nb.fp_math_ctrl;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
       b->nb.fp_math_ctrl = nir_fp_no_fast_math;
       nir_def *inf = nir_imm_floatN_t(&b->nb, INFINITY, src[0]->bit_size);
       dest->def = nir_feq(&b->nb, nir_fabs(&b->nb, src[0]), inf);
 
-      b->nb.exact = save_exact;
-      b->nb.fp_math_ctrl = save_math_ctrl;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
    case SpvOpFUnordEqual: {
-      const bool save_exact = b->nb.exact;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
 
       /* This could also be implemented as !(a < b || b < a).  If one or both
        * of the source are numbers, later optimization passes can easily
@@ -900,7 +898,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
                          nir_fneu(&b->nb, src[0], src[0]),
                          nir_fneu(&b->nb, src[1], src[1])));
 
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
@@ -920,9 +918,9 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
          src[1] = tmp;
       }
 
-      const bool save_exact = b->nb.exact;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
 
       /* Use the property FUnordLessThan(a, b) ≡ !FOrdGreaterThanEqual(a, b). */
       switch (op) {
@@ -935,7 +933,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
          nir_inot(&b->nb,
                   nir_build_alu(&b->nb, op, src[0], src[1], NULL, NULL));
 
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
@@ -945,9 +943,9 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
        * from the ALU will probably already be false if the operands are not
        * ordered so we don’t need to handle it specially.
        */
-      const bool save_exact = b->nb.exact;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
-      b->nb.exact = true;
+      b->nb.fp_math_ctrl |= nir_fp_exact;
 
       /* This could also be implemented as (a < b || b < a).  If one or both
        * of the source are numbers, later optimization passes can easily
@@ -963,7 +961,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
                           nir_feq(&b->nb, src[0], src[0]),
                           nir_feq(&b->nb, src[1], src[1])));
 
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    }
 
@@ -1067,14 +1065,14 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
          break;
       }
 
-      const bool save_exact = b->nb.exact;
+      const bool save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
       if (exact)
-         b->nb.exact = true;
+         b->nb.fp_math_ctrl |= nir_fp_exact;
 
       dest->def = nir_build_alu(&b->nb, op, src[0], src[1], src[2], src[3]);
 
-      b->nb.exact = save_exact;
+      b->nb.fp_math_ctrl = save_fp_math_ctrl;
       break;
    } /* default */
    }
@@ -1101,7 +1099,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
       vtn_mediump_upconvert_value(b, dest);
    vtn_push_ssa_value(b, w[2], dest);
 
-   b->nb.exact = b->exact;
+   b->nb.fp_math_ctrl = b->exact ? nir_fp_exact : nir_fp_fast_math;
 }
 
 void

src/amd/vulkan/nir/radv_nir_rt_common.c
@@ -79,8 +79,8 @@ intersect_ray_amd_software_box(struct radv_device *device, nir_builder *b, nir_d
    const struct glsl_type *vec4_type = glsl_vector_type(GLSL_TYPE_FLOAT, 4);
    const struct glsl_type *uvec4_type = glsl_vector_type(GLSL_TYPE_UINT, 4);
 
-   bool old_exact = b->exact;
-   b->exact = true;
+   unsigned old_math_ctrl = b->fp_math_ctrl;
+   b->fp_math_ctrl |= nir_fp_exact;
 
    nir_def *node_addr = build_node_to_addr(device, b, bvh_node, false);
 
@@ -165,7 +165,7 @@ intersect_ray_amd_software_box(struct radv_device *device, nir_builder *b, nir_d
    nir_sort_hit_pair(b, distances, child_indices, 1, 3);
    nir_sort_hit_pair(b, distances, child_indices, 1, 2);
 
-   b->exact = old_exact;
+   b->fp_math_ctrl = old_math_ctrl;
    return nir_load_var(b, child_indices);
 }
 
@@ -196,8 +196,8 @@ intersect_ray_amd_software_tri(struct radv_device *device, nir_builder *b, nir_d
 {
    const struct glsl_type *vec4_type = glsl_vector_type(GLSL_TYPE_FLOAT, 4);
 
-   bool old_exact = b->exact;
-   b->exact = true;
+   unsigned old_math_ctrl = b->fp_math_ctrl;
+   b->fp_math_ctrl |= nir_fp_exact;
 
    nir_def *node_addr = build_node_to_addr(device, b, bvh_node, false);
 
@@ -375,7 +375,7 @@ intersect_ray_amd_software_tri(struct radv_device *device, nir_builder *b, nir_d
    }
    nir_pop_if(b, NULL);
 
-   b->exact = old_exact;
+   b->fp_math_ctrl = old_math_ctrl;
    return nir_load_var(b, result);
 }
 

src/asahi/compiler/agx_nir_lower_fminmax.c
@@ -62,7 +62,7 @@ lower(nir_builder *b, nir_alu_instr *alu, void *data)
 
    if (could_be_denorm(src0) || could_be_denorm(src1)) {
       b->cursor = nir_after_instr(&alu->instr);
-      b->exact = true;
+      b->fp_math_ctrl = nir_fp_no_fast_math;
 
       nir_def *canonicalized = nir_fadd_imm(b, &alu->def, -0.0);
       nir_def_rewrite_uses_after(&alu->def, canonicalized);

src/compiler/clc/nir_load_libclc.c
@@ -321,7 +321,7 @@ libclc_add_generic_variants(nir_shader *shader)
 static bool
 mark_exact(nir_builder *b, nir_alu_instr *alu, UNUSED void *_)
 {
-   alu->exact = true;
+   alu->fp_math_ctrl |= nir_fp_exact;
    return true;
 }
 

src/compiler/glsl/glsl_to_nir.cpp
@@ -1888,8 +1888,9 @@ nir_visitor::visit(ir_assignment *ir)
    unsigned num_components = ir->lhs->type->vector_elements;
    unsigned write_mask = ir->write_mask;
 
-   b.exact = ir->lhs->variable_referenced()->data.invariant ||
-             ir->lhs->variable_referenced()->data.precise;
+   b.fp_math_ctrl = ir->lhs->variable_referenced()->data.invariant ||
+                    ir->lhs->variable_referenced()->data.precise ?
+                       nir_fp_exact : nir_fp_fast_math;
 
    if ((ir->rhs->as_dereference() || ir->rhs->as_constant()) &&
        (write_mask == BITFIELD_MASK(num_components) || write_mask == 0)) {

src/compiler/nir/nir.h
@@ -1518,13 +1518,27 @@ typedef enum {
     */
    nir_fp_preserve_nan = BITFIELD_BIT(2),
 
+   /** Indicates that this ALU instruction generates an exact value
+    *
+    * This is kind of a mixture of GLSL "precise" and "invariant" and not
+    * really equivalent to either.  This indicates that the value generated by
+    * this operation is high-precision and any code transformations that touch
+    * it must ensure that the resulting value is bit-for-bit identical to the
+    * original, with the exception of undefindness allowed by other
+    * nir_fp_math_control bits and NaN patterns.
+    *
+    * TODO This is currently also used to control NaN behavior of
+    * floating point comparisons and fmin/fmax/fsat.
+    * This should be changed to only depend on nir_fp_preserve_nan.
+    */
+   nir_fp_exact = BITFIELD_BIT(3),
 
    nir_fp_preserve_sz_inf_nan = nir_fp_preserve_signed_zero |
                                 nir_fp_preserve_inf |
                                 nir_fp_preserve_nan,
 
    nir_fp_fast_math = 0,
-   nir_fp_no_fast_math = BITFIELD_MASK(3),
+   nir_fp_no_fast_math = BITFIELD_MASK(4),
 } nir_fp_math_control;
 
 /***/
@@ -1537,16 +1551,6 @@ typedef struct nir_alu_instr {
 
    /** Opcode */
    nir_op op;
-
-   /** Indicates that this ALU instruction generates an exact value
-    *
-    * This is kind of a mixture of GLSL "precise" and "invariant" and not
-    * really equivalent to either.  This indicates that the value generated by
-    * this operation is high-precision and any code transformations that touch
-    * it must ensure that the resulting value is bit-for-bit identical to the
-    * original.
-    */
-   bool exact : 1;
 
    /**
     * Indicates that this instruction doese not cause signed integer wrapping
@@ -1561,12 +1565,11 @@ typedef struct nir_alu_instr {
    bool no_unsigned_wrap : 1;
 
    /**
-    * The float controls bit float_controls2 cares about. That is,
-    * NAN/INF/SIGNED_ZERO_PRESERVE only. Allow{Contract,Reassoc,Transform} are
-    * still handled through the exact bit, and the other float controls bits
-    * (rounding mode and denorm handling) remain in the execution mode only.
+    * The float controls bit float_controls2 cares about. The float_controls1 bits
+    * that have no float_controls2 equivalent (rounding mode and denorm handling)
+    * remain in the execution mode only.
     */
-   uint32_t fp_math_ctrl : 3;
+   uint32_t fp_math_ctrl : 4;
 
    /** Sources
     *
@@ -1602,7 +1605,7 @@ nir_alu_instr_is_signed_zero_inf_nan_preserve(nir_alu_instr *alu)
 static inline bool
 nir_alu_instr_is_exact(nir_alu_instr *alu)
 {
-   return alu->exact;
+   return alu->fp_math_ctrl & nir_fp_exact;
 }
 
 void nir_alu_src_copy(nir_alu_src *dest, const nir_alu_src *src);

src/compiler/nir/nir_builder.c
@@ -49,7 +49,7 @@ nir_builder MUST_CHECK PRINTFLIKE(3, 4)
 
    nir_function *func = nir_function_create(b.shader, "main");
    func->is_entrypoint = true;
-   b.exact = false;
+   b.fp_math_ctrl = nir_fp_fast_math;
    b.impl = nir_function_impl_create(func);
    b.cursor = nir_after_cf_list(&b.impl->body);
 
@@ -71,7 +71,6 @@ nir_builder_alu_instr_finish_and_insert(nir_builder *build, nir_alu_instr *instr
 {
    const nir_op_info *op_info = &nir_op_infos[instr->op];
 
-   instr->exact = build->exact;
    instr->fp_math_ctrl = build->fp_math_ctrl;
 
    /* Guess the number of components the destination temporary should have
@@ -387,7 +386,6 @@ nir_vec_scalars(nir_builder *build, nir_scalar *comp, unsigned num_components)
       instr->src[i].src = nir_src_for_ssa(comp[i].def);
       instr->src[i].swizzle[0] = comp[i].comp;
    }
-   instr->exact = build->exact;
    instr->fp_math_ctrl = build->fp_math_ctrl;
 
    /* Note: not reusing nir_builder_alu_instr_finish_and_insert() because it

src/compiler/nir/nir_builder.h
@@ -37,9 +37,6 @@ struct exec_list;
 typedef struct nir_builder {
    nir_cursor cursor;
 
-   /* Whether new ALU instructions will be marked "exact" */
-   bool exact;
-
    /* Whether new ALU instruction will be constanst-folded if possible. */
    bool constant_fold_alu;
 
@@ -55,7 +52,7 @@ nir_builder_create(nir_function_impl *impl)
 {
    nir_builder b;
    memset(&b, 0, sizeof(b));
-   b.exact = false;
+   b.fp_math_ctrl = nir_fp_fast_math;
    b.impl = impl;
    b.shader = impl->function->shader;
    return b;
@@ -724,7 +721,6 @@ nir_mov_alu(nir_builder *build, nir_alu_src src, unsigned num_components)
    nir_alu_instr *mov = nir_alu_instr_create(build->shader, nir_op_mov);
    nir_def_init(&mov->instr, &mov->def, num_components,
                 nir_src_bit_size(src.src));
-   mov->exact = build->exact;
    mov->fp_math_ctrl = build->fp_math_ctrl;
    mov->src[0] = src;
    nir_builder_instr_insert(build, &mov->instr);

commit 7f9dc5e4d9e6170cba4e88a7b5585f24d02d6059

spirv: Preserve all fp_math_ctrl bits in vtn_handle_alu default case
Fixes: 2e3f40a85c7 ("nir: move exact bit to nir_fp_math_control")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44463>

src/compiler/spirv/vtn_alu.c
@@ -1141,7 +1141,7 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
          break;
       }
 
-      const bool save_fp_math_ctrl = b->nb.fp_math_ctrl;
+      const unsigned save_fp_math_ctrl = b->nb.fp_math_ctrl;
 
       b->nb.fp_math_ctrl |= extra_fp_math_ctrl;
 