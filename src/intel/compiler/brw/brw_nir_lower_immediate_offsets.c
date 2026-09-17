joshuaswarren/mesa-1

commit 276dc82e061f06efec29462181c429e8475a1f0d

intel/brw: Implement SENDG instruction + LSC store
This instruction will replace some of the send instructions when using
64bit addressing.
The format of message descriptor also changed, so here it implements
LSC store other messages will be implemented in the next patches.

With this patch we have 2 crucible tests passing:
- func.compute.basic.q0
- func.compute.local-id.basic.q0

Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/43926>

src/intel/compiler/brw/brw_nir_lower_immediate_offsets.c
@@ -7,10 +7,16 @@
 #include "brw_eu.h"
 #include "brw_nir.h"
 
+struct state {
+   bool efficient_64bit;
+};
+
 static bool
 lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 {
-   unsigned max_bits = 0;
+   struct state *state = data;
+   uint32_t max_bits = UINT32_MAX;
+   enum lsc_addr_surface_type binding_type;
 
    switch (intrin->intrinsic) {
    case nir_intrinsic_load_ssbo_intel:
@@ -23,16 +29,24 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
          nir_def_is_intrinsic(binding->ssa) &&
          nir_def_as_intrinsic(binding->ssa)->intrinsic ==
          nir_intrinsic_resource_intel;
-      bool ss_binding = false;
       bool bti_is_const;
       if (has_resource) {
          nir_intrinsic_instr *resource =
             nir_def_as_intrinsic(binding->ssa);
-         ss_binding = (nir_intrinsic_resource_access_intel(resource) &
-                       nir_resource_intel_bindless) != 0;
          bti_is_const = nir_src_is_const(resource->src[1]);
+         binding_type =
+            (nir_intrinsic_resource_access_intel(resource) &
+             nir_resource_intel_bindless) != 0 ? LSC_ADDR_SURFTYPE_BSS :
+            (nir_intrinsic_resource_access_intel(resource) &
+             nir_resource_intel_internal) != 0 ? LSC_ADDR_SURFTYPE_SS :
+            LSC_ADDR_SURFTYPE_BTI;
       } else {
-         bti_is_const = nir_src_is_const(*nir_get_io_index_src(intrin));
+         if (state->efficient_64bit) {
+            binding_type = LSC_ADDR_SURFTYPE_BSS;
+         } else {
+            bti_is_const = nir_src_is_const(*nir_get_io_index_src(intrin));
+            binding_type = LSC_ADDR_SURFTYPE_BTI;
+         }
       }
       /* The BTI index and the base offset got into the extended descriptor
        * (see BSpec 63997 for the format).
@@ -54,18 +68,18 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
        * So put set max bits to 0 in that case and set the base offset to 0
        * since it's unusable.
        */
-      max_bits = ss_binding ? LSC_ADDRESS_OFFSET_SS_BITS :
-         bti_is_const ? LSC_ADDRESS_OFFSET_BTI_BITS : 0;
+      if (binding_type == LSC_ADDR_SURFTYPE_BTI && !bti_is_const)
+         max_bits = 0;
       break;
    }
    case nir_intrinsic_load_global_intel:
    case nir_intrinsic_store_global_intel:
    case nir_intrinsic_load_global_constant_uniform_block_intel:
-      max_bits = LSC_ADDRESS_OFFSET_FLAT_BITS;
+      binding_type = LSC_ADDR_SURFTYPE_FLAT;
       break;
    default:
       if (nir_is_shared_access(intrin)) {
-         max_bits = LSC_ADDRESS_OFFSET_FLAT_BITS;
+         binding_type = LSC_ADDR_SURFTYPE_FLAT;
          break;
       }
 
@@ -76,6 +90,8 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 
    if (nir_intrinsic_base(intrin) == 0)
       return false;
+
+   max_bits = MIN2(brw_max_immediate_offset_bits(binding_type, state->efficient_64bit), max_bits);
 
    b->cursor = nir_before_instr(&intrin->instr);
 
@@ -100,7 +116,19 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
    int32_t addition = (base / (max + 1)) * (max + 1);
    int32_t new_base = base - addition;
 
-   int32_t unaligned = new_base % 4;
+   /* Xe3P+ : BSpec 71885/72045: Global Offset
+    *    "Specified the signed global offset (in number of data size
+    *     elements) applied to all addresses in the message"
+    *
+    * We often convert 8/16bits to D8U32/D16U32, so don't go down lower than 4
+    * bytes.
+    *
+    * TODO: better predict what types are going to be used?
+    */
+   const unsigned alignment =
+      !state->efficient_64bit ? 4 :
+      MAX2(4, brw_nir_intrinsic_data_element_size(intrin));
+   int32_t unaligned = new_base % alignment;
    addition += unaligned;
    new_base -= unaligned;
 
@@ -115,8 +143,11 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 }
 
 bool
-brw_nir_lower_immediate_offsets(nir_shader *shader)
+brw_nir_lower_immediate_offsets(nir_shader *shader, bool efficient_64bit)
 {
+   struct state state = {
+      .efficient_64bit = efficient_64bit,
+   };
    return nir_shader_intrinsics_pass(shader, lower_immediate_offsets,
-                                     nir_metadata_control_flow, NULL);
+                                     nir_metadata_control_flow, &state);
 }

src/intel/compiler/brw/brw_analysis_performance.cpp
@@ -120,11 +120,13 @@ namespace {
          td(inst->dst.type), sd(DIV_ROUND_UP(inst->size_written, REG_SIZE)),
          tx(get_exec_type(inst)), sx(0), ss(0),
          sc(has_bank_conflict(isa, inst) ? sd : 0),
-         desc(0), sfid(0), fused_send_disable(false)
+         sfid(0), fused_send_disable(false), efficient_64bit(false)
       {
          const brw_send_inst *send = inst->as_send();
          if (send) {
-            desc = send->desc;
+            efficient_64bit = send->efficient_64bit;
+            desc = efficient_64bit ? send->combined_desc : send->desc;
+
             sfid = send->sfid;
             /* We typically want the maximum source size, except for split send
              * messages which require the total size.
@@ -179,14 +181,16 @@ namespace {
       unsigned ss;
       /** Bank conflict penalty size in GRF units (equal to sd if non-zero). */
       unsigned sc;
-      /** Send message descriptor. */
-      uint32_t desc;
+      /** Send message descriptor (64bit in efficient_64bit mode, 32bit otherwise). */
+      uint64_t desc;
       /** Send message shared function ID. */
       uint8_t sfid;
       /** Repeat count for DPAS instructions. */
       uint8_t rcount;
       /** Whether SEND message fusion is disabled (Gfx12.x only) */
-      bool fused_send_disable;
+      bool fused_send_disable:1;
+      /** Efficient 64bit message (Gfx35+ only) */
+      bool efficient_64bit:1;
    };
 
    /**
@@ -598,6 +602,7 @@ namespace {
             return calculate_desc(info, EU_UNIT_DP_CC, 2, 0, 0, 0, 16 /* XXX */,
                                   10 /* XXX */, 100 /* XXX */, 0, 0, 0, 0);
          case GEN_SFID_RENDER_CACHE:
+            /* TODO: 64bit */
             switch (brw_dp_desc_msg_type(devinfo, info.desc)) {
             case GFX7_DATAPORT_RC_TYPED_ATOMIC_OP:
                return calculate_desc(info, EU_UNIT_DP_RC, 2, 0, 0,
@@ -661,8 +666,11 @@ namespace {
 
          case GEN_SFID_UGM:
          case GEN_SFID_TGM:
-         case GEN_SFID_SLM:
-            switch (lsc_msg_desc_opcode(devinfo, info.desc)) {
+         case GEN_SFID_SLM: {
+            uint8_t opcode = info.efficient_64bit ?
+                             gen_64bit_msg_desc_get_opcode(info.desc) :
+                             lsc_msg_desc_opcode(devinfo, info.desc);
+            switch (opcode) {
             case LSC_OP_LOAD:
             case LSC_OP_STORE:
             case LSC_OP_LOAD_CMASK:
@@ -705,6 +713,7 @@ namespace {
             default:
                abort();
             }
+         }
 
          case GEN_SFID_MESSAGE_GATEWAY:
          case GEN_SFID_BINDLESS_THREAD_DISPATCH: /* or THREAD_SPAWNER */
@@ -712,9 +721,12 @@ namespace {
             return calculate_desc(info, EU_UNIT_SPAWNER, 2, 0, 0, 0 /* XXX */, 0,
                                   10 /* XXX */, 0, 0, 0, 0, 0);
 
-         case GEN_SFID_URB:
-            if (brw_urb_desc_msg_type(devinfo, info.desc) ==
-                GEN_GFX125_URB_OPCODE_FENCE) {
+         case GEN_SFID_URB: {
+            uint8_t opcode = info.efficient_64bit ?
+                             gen_64bit_msg_desc_get_opcode(info.desc) :
+                             brw_urb_desc_msg_type(devinfo, info.desc);
+
+            if (opcode == GEN_GFX125_URB_OPCODE_FENCE) {
                return calculate_desc(info, EU_UNIT_DP_DC, 2, 0, 0,
                                      30 /* XXX */, 0,
                                      10 /* XXX */, 100 /* XXX */, 0, 0, 0, 0);
@@ -722,6 +734,7 @@ namespace {
 
             return calculate_desc(info, EU_UNIT_URB, 2, 0, 0, 0, 6 /* XXX */,
                                   32 /* XXX */, 200 /* XXX */, 0, 0, 0, 0);
+         }
 
          default:
             abort();

src/intel/compiler/brw/brw_compiler.h
@@ -236,6 +236,12 @@ struct brw_base_prog_key {
     */
    uint32_t view_mask;
 
+   /** Use efficient 64bit bit mode
+    *
+    * Gfx35+ only.
+    */
+   bool use_efficient_64bit : 1;
+
    enum brw_robustness_flags robust_flags:3;
 
    enum intel_vue_layout vue_layout:2;
@@ -244,7 +250,7 @@ struct brw_base_prog_key {
 
    enum intel_atomic_branch_cases atomic_branch_flags:3;
 
-   uint32_t padding:22;
+   uint32_t padding:21;
 };
 
 /**

src/intel/compiler/brw/brw_eu.c
@@ -219,6 +219,7 @@ static const struct opcode_desc opcode_descs[] = {
    { BRW_OPCODE_SEND,     49,  "send",    2,    1,    GFX_GE(GFX12) },
    { BRW_OPCODE_SENDC,    50,  "sendc",   2,    1,    GFX_GE(GFX12) },
    { BRW_OPCODE_SENDS,    51,  "sends",   2,    1,    GFX_LT(GFX12) },
+   { BRW_OPCODE_SENDG,    51,  "sendg",   2,    1,    GFX_GE(XE3P) },
    { BRW_OPCODE_SENDSC,   52,  "sendsc",  2,    1,    GFX_LT(GFX12) },
    { BRW_OPCODE_MATH,     56,  "math",    2,    1,    GFX_ALL },
    { BRW_OPCODE_ADD,      64,  "add",     2,    1,    GFX_ALL },

src/intel/compiler/brw/brw_eu.h
@@ -852,22 +852,40 @@ enum brw_conditional_mod brw_swap_cmod(enum brw_conditional_mod cmod);
 /** Maximum SEND message length */
 #define BRW_MAX_MSG_LENGTH 15
 
-/** Offset encoding signed size limits (top bit is the sign) */
+/** Xe2 Offset encoding signed size limits (top bit is the sign) */
 #define LSC_ADDRESS_OFFSET_FLAT_BITS 20
 #define LSC_ADDRESS_OFFSET_SS_BITS   17
 #define LSC_ADDRESS_OFFSET_BTI_BITS  12
 
+/** Xe3P-64bit Offset encoding signed size limits (top bit is the sign) */
+#define LSC_64BIT_STATELESS_OFFSET_BITS 21
+#define LSC_64BIT_STATEFUL_OFFSET_BITS  16
+
 static inline unsigned
-brw_max_immediate_offset_bits(enum lsc_addr_surface_type binding_type)
+brw_max_immediate_offset_bits(enum lsc_addr_surface_type binding_type,
+                              bool efficient_64bit)
 {
-   static const unsigned max_bits[] = {
-      [LSC_ADDR_SURFTYPE_FLAT] = LSC_ADDRESS_OFFSET_FLAT_BITS,
-      [LSC_ADDR_SURFTYPE_BSS]  = LSC_ADDRESS_OFFSET_SS_BITS,
-      [LSC_ADDR_SURFTYPE_SS]   = LSC_ADDRESS_OFFSET_SS_BITS,
-      [LSC_ADDR_SURFTYPE_BTI]  = LSC_ADDRESS_OFFSET_BTI_BITS,
-   };
-   assert(binding_type <= LSC_ADDR_SURFTYPE_BTI);
-   return max_bits[binding_type];
+   if (efficient_64bit) {
+      return binding_type == LSC_ADDR_SURFTYPE_FLAT ?
+             LSC_64BIT_STATELESS_OFFSET_BITS :
+             LSC_64BIT_STATEFUL_OFFSET_BITS;
+   } else {
+      static const unsigned max_bits[] = {
+         [LSC_ADDR_SURFTYPE_FLAT] = LSC_ADDRESS_OFFSET_FLAT_BITS,
+         [LSC_ADDR_SURFTYPE_BSS]  = LSC_ADDRESS_OFFSET_SS_BITS,
+         [LSC_ADDR_SURFTYPE_SS]   = LSC_ADDRESS_OFFSET_SS_BITS,
+         [LSC_ADDR_SURFTYPE_BTI]  = LSC_ADDRESS_OFFSET_BTI_BITS,
+      };
+      assert(binding_type <= LSC_ADDR_SURFTYPE_BTI);
+      return max_bits[binding_type];
+   }
+}
+
+static inline unsigned
+brw_immediate_offset_alignment(uint32_t element_size_B,
+                               bool efficient_64bit)
+{
+   return efficient_64bit ? element_size_B : 4;
 }
 
 static inline bool
@@ -878,10 +896,15 @@ brw_lsc_supports_base_offset(const struct intel_device_info *devinfo)
 
 static inline bool
 brw_lsc_can_use_instruction_offset(enum lsc_addr_surface_type binding_type,
+                                   bool efficient_64bit,
+                                   uint32_t element_size_B,
                                    int32_t offset)
 {
-   const unsigned max_bits = brw_max_immediate_offset_bits(binding_type);
-   return offset % 4 == 0 &&
+   const unsigned max_bits =
+      brw_max_immediate_offset_bits(binding_type, efficient_64bit);
+   return
+      offset % brw_immediate_offset_alignment(element_size_B,
+                                              efficient_64bit) == 0 &&
       offset >= u_intN_min(max_bits) &&
       offset <= u_intN_max(max_bits);
 }

src/intel/compiler/brw/brw_eu_defines.h
@@ -42,6 +42,7 @@
    })
 
 #define GET_BITS(data, high, low) ((data & INTEL_MASK((high), (low))) >> (low))
+#define GET_BITS_64(data, high, low) ((data & INTEL_MASK_64((high), (low))) >> (low))
 #define GET_FIELD(word, field) (((word)  & field ## _MASK) >> field ## _SHIFT)
 #define GET_BITS_64(data, high, low) ((data & INTEL_MASK_64((high), (low))) >> (low))
 
@@ -187,6 +188,7 @@ enum ENUM_PACKED opcode {
    BRW_OPCODE_SENDC,
    BRW_OPCODE_SENDS,
    BRW_OPCODE_SENDSC,
+   BRW_OPCODE_SENDG, /* Gfx35+ with 64bits addressing */
    BRW_OPCODE_MATH,
    BRW_OPCODE_ADD,
    BRW_OPCODE_MUL,
@@ -545,15 +547,19 @@ enum ENUM_PACKED opcode {
 
 enum send_srcs {
    /** The 32-bit message descriptor (can be a register) */
-   SEND_SRC_DESC,
+   SEND_SRC_DESC = 0,
    /** The 32-bit extended message descriptor (can be a register) */
-   SEND_SRC_EX_DESC,
+   SEND_SRC_EX_DESC = 1,
+   /** The 64-bit indirect 0 message descriptor (can be a register) */
+   SENDG_SRC_IND_0_DESC = 0,
+   /** The 64-bit indirect 1 message descriptor (can be a register) */
+   SENDG_SRC_IND_1_DESC = 1,
    /** The leading register for the first SEND payload */
-   SEND_SRC_PAYLOAD1,
+   SEND_SRC_PAYLOAD1 = 2,
    /** The leading register for the second split-SEND payload */
-   SEND_SRC_PAYLOAD2,
+   SEND_SRC_PAYLOAD2 = 3,
 
-   SEND_NUM_SRCS
+   SEND_NUM_SRCS = 4,
 };
 
 enum send_gather_srcs {

src/intel/compiler/brw/brw_from_nir.cpp
@@ -4099,6 +4099,7 @@ memory_address(nir_to_brw_state &ntb,
    const brw_builder ubld = (src_offset.is_scalar || src_offset.file == IMM) ?
       bld.scalar_group() : bld;
    brw_reg address;
+   const uint32_t element_size_B = brw_nir_intrinsic_data_element_size(instr);
 
    if ((brw_lsc_supports_base_offset(devinfo) == false) ||
        (!nir_intrinsic_has_base(instr) && !nir_src_is_const(*nir_src_offset))) {
@@ -4110,7 +4111,10 @@ memory_address(nir_to_brw_state &ntb,
       *address_offset = 0;
    } else if (!nir_intrinsic_has_base(instr) && nir_src_is_const(*nir_src_offset)) {
       const int32_t offset = nir_src_as_int(*nir_src_offset);
-      if (brw_lsc_can_use_instruction_offset(binding_type, offset)) {
+      if (brw_lsc_can_use_instruction_offset(binding_type,
+                                             ntb.s.key->use_efficient_64bit,
+                                             element_size_B,
+                                             offset)) {
          address = brw_imm_ud(0);
          *address_offset = offset;
       } else {
@@ -4120,7 +4124,10 @@ memory_address(nir_to_brw_state &ntb,
    } else {
       assert(nir_intrinsic_has_base(instr));
       const int32_t offset = nir_intrinsic_base(instr);
-      assert(brw_lsc_can_use_instruction_offset(binding_type, offset));
+      assert(brw_lsc_can_use_instruction_offset(binding_type,
+                                                ntb.s.key->use_efficient_64bit,
+                                                element_size_B,
+                                                offset));
       address = src_offset;
       *address_offset = offset;
    }

src/intel/compiler/brw/brw_inst.cpp
@@ -176,6 +176,9 @@ brw_transform_inst(brw_shader &s, brw_inst *inst, enum opcode new_opcode,
          new_src[i] = inst->src[i];
       inst->src = new_src;
    }
+   /* Initialize newer srcs */
+   for (unsigned i = inst->sources; i < new_num_sources; i++)
+      inst->src[i] = brw_reg();
 
    if (new_kind != kind)
       memset(((char *)inst) + sizeof(brw_inst), 0, new_inst_size - sizeof(brw_inst));
@@ -195,6 +198,7 @@ brw_inst_kind_for_opcode(enum opcode opcode)
    case BRW_OPCODE_SENDS:
    case BRW_OPCODE_SENDC:
    case BRW_OPCODE_SENDSC:
+   case BRW_OPCODE_SENDG:
    case SHADER_OPCODE_SEND:
    case SHADER_OPCODE_SEND_GATHER:
    case SHADER_OPCODE_BARRIER:

commit fc4face7697b04e412a69d3139e87bd5cf1c40d7

brw: fix alignment of base offsets on Xe3P+
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 276dc82e06 ("intel/brw: Implement SENDG instruction + LSC store")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44478>

src/intel/compiler/brw/brw_nir_lower_immediate_offsets.c
@@ -110,11 +110,6 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
    const int32_t max = u_intN_max(max_bits);
 
    const int32_t base = nir_intrinsic_base(intrin);
-   if ((base % 4) == 0 && base >= min && base <= max)
-      return false;
-
-   int32_t addition = (base / (max + 1)) * (max + 1);
-   int32_t new_base = base - addition;
 
    /* Xe3P+ : BSpec 71885/72045: Global Offset
     *    "Specified the signed global offset (in number of data size
@@ -127,7 +122,14 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
     */
    const unsigned alignment =
       !state->efficient_64bit ? 4 :
-      MAX2(4, brw_nir_intrinsic_data_element_size(intrin));
+      brw_nir_intrinsic_data_element_size(intrin);
+
+   if ((base % alignment) == 0 && base >= min && base <= max)
+      return false;
+
+   int32_t addition = (base / (max + 1)) * (max + 1);
+   int32_t new_base = base - addition;
+
    int32_t unaligned = new_base % alignment;
    addition += unaligned;
    new_base -= unaligned;

src/intel/compiler/brw/brw_validate.cpp
@@ -128,8 +128,10 @@ validate_memory_logical(const brw_shader &s, const brw_mem_inst *inst)
    if (mode == MEMORY_MODE_TYPED)
       VAL_ASSERT_EQ(inst->as_mem()->address_offset, 0);
 
-   /* Offset must be DWord aligned */
-   VAL_ASSERT_EQ((inst->as_mem()->address_offset % 4), 0);
+   if (!s.key->use_efficient_64bit) {
+      /* Offset must be DWord aligned */
+      VAL_ASSERT_EQ((inst->as_mem()->address_offset % 4), 0);
+   }
 
    switch (inst->opcode) {
    case SHADER_OPCODE_MEMORY_LOAD_LOGICAL: