joshuaswarren/mesa-1

commit 8b46c73711cf2ceb7f05973e50e2283950f5ba50

llvmpipe: force alignment for Windows texture allocations
aligning only the size isn't sufficient to get an aligned allocation

fixes #16239

cc: mesa-stable

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44245>

src/gallium/drivers/llvmpipe/lp_texture.c
@@ -1364,7 +1364,7 @@ llvmpipe_allocate_memory(struct pipe_screen *_screen, uint64_t size)
 
    mtx_unlock(&screen->mem_mutex);
 #else
-   mem->cpu_addr = malloc(mem->size);
+   mem->cpu_addr = os_malloc_aligned(mem->size, alignment);
    mem->fd = -1;
    mem->type = LLVMPIPE_MEMORY_FD_TYPE_INVALID;
 #endif

commit 32ef5386589da135bb3962937bc87892311f5438

llvmpipe: use os_free_aligned for non-vma case
Fixes: 8b46c73711c ("llvmpipe: force alignment for Windows texture allocations")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44506>

src/gallium/drivers/llvmpipe/lp_texture.c
@@ -1391,7 +1391,7 @@ llvmpipe_free_memory(struct pipe_screen *pscreen,
    if (mem->cpu_addr != MAP_FAILED)
       munmap(mem->cpu_addr, mem->size);
 #else
-   free(mem->cpu_addr);
+   os_free_aligned(mem->cpu_addr);
 #endif
 
    FREE(mem);